e81012026087bf614f06c782f1938a21a33e9de6
Andreas Kling <awesomekling@gmail.com>
2018-10-11 00:56:28 +0200

    Peanut8x8 can paint all the ASCII characters!

--- Widgets/test.cpp
+++ Widgets/test.cpp
@@ -14,10 +14,23 @@ int main(int c, char** v)
     RootWidget w;
     fb.setRootWidget(&w);
 
-    Label l(&w);
-    l.setRect(Rect(100, 100, 600, 100));
-    l.setText("()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_abcdefghijklmnopqrstuvwxyz");
-    //l.setText("Welcome to the Serenity Operating System");
+    auto* l1 = new Label(&w);
+    l1->setRect(Rect(100, 100, 300, 20));
+    l1->setText("0123456789");
+
+    auto* l2 = new Label(&w);
+    l2->setRect(Rect(100, 120, 300, 20));
+    l2->setText("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    
+    auto* l3 = new Label(&w);
+    l3->setRect(Rect(100, 140, 300, 20));
+    l3->setText("abcdefghijklmnopqrstuvwxyz");
+
+    auto* l4 = new Label(&w);
+    l4->setRect(Rect(100, 160, 300, 20));
+    l4->setText("!\"#$%&'()*+,-./:;<=>?@[\\]^_{|}~");
+
+    //l5->setText("Welcome to the Serenity Operating System");
 
     return loop.exec();
 }
