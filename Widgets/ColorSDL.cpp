fb4ae12bc246771b8ca0d46641017fddc6ee12b8
Andreas Kling <awesomekling@gmail.com>
2018-10-12 20:05:11 +0200

    Add a visual bell to TerminalWidget. Also backspace.

--- Widgets/ColorSDL.cpp
+++ Widgets/ColorSDL.cpp
@@ -5,3 +5,24 @@ Color::Color(byte r, byte g, byte b)
 {
     m_value = SDL_MapRGB(FrameBufferSDL::the().surface()->format, r, g, b);
 }
+
+Color::Color(NamedColor named)
+{
+    struct {
+        byte r;
+        byte g;
+        byte b;
+    } rgb;
+
+    switch (named) {
+    case Black: rgb = { 0, 0, 0 }; break;
+    case White: rgb = { 255, 255, 255 }; break;
+    case Red: rgb = { 255, 0, 0}; break;
+    case Green: rgb = { 0, 255, 0}; break;
+    case Blue: rgb = { 0, 0, 255}; break;
+    default: ASSERT_NOT_REACHED(); break;
+    }
+
+    m_value = SDL_MapRGB(FrameBufferSDL::the().surface()->format, rgb.r, rgb.g, rgb.g);
+
+}
