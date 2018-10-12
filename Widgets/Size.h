e23ac5601741441fea916f605a562d11c9982baa
Andreas Kling <awesomekling@gmail.com>
2018-10-12 12:29:58 +0200

    Add a Painter::drawBitmap() and make Painter::drawText() use it.

---
+++ Widgets/Size.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class Size {
+public:
+    Size() { }
+    Size(int w, int h) : m_width(w), m_height(h) { }
+
+    bool isEmpty() const { return !m_width || !m_height; }
+
+    int width() const { return m_width; }
+    int height() const { return m_height; }
+
+    void setWidth(int w) { m_width = w; }
+    void setHeight(int h) { m_height = h; }
+
+private:
+    int m_width { 0 };
+    int m_height { 0 };
+};
+
