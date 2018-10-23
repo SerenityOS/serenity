a5caf7ca991dc48959f75ebed70f5018302e53d2
Andreas Kling <awesomekling@gmail.com>
2018-10-24 01:02:55 +0200

    Make the kmalloc global stats variable volatile.
    I know I'm praying for cargo here, but this does fix a weird issue
    where logging the sum_alloc and sum_free globals wouldn't display
    symmetric values all the time.

--- Kernel/kmalloc.h
+++ Kernel/kmalloc.h
@@ -4,8 +4,8 @@ void kmalloc_init();
 void *kmalloc(DWORD size) __attribute__ ((malloc));
 void kfree(void*);
 
-extern DWORD sum_alloc;
-extern DWORD sum_free;
+extern volatile DWORD sum_alloc;
+extern volatile DWORD sum_free;
 
 inline void* operator new(size_t, void* p) { return p; }
 inline void* operator new[](size_t, void* p) { return p; }
