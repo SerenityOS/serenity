0627004e2c27491c847c6266175cf1a7d381c4a1
Andreas Kling <awesomekling@gmail.com>
2018-11-02 09:49:10 +0100

    Make IO helpers inline and use immediate-encoded ports when possible.

--- Kernel/IO.h
+++ Kernel/IO.h
@@ -4,12 +4,39 @@
 
 namespace IO {
 
-BYTE in8(WORD port);
-WORD in16(WORD port);
-DWORD in32(WORD port);
+inline byte in8(word port)
+{
+    byte value;
+    asm volatile("inb %1, %0":"=a"(value):"Nd"(port));
+    return value;
+}
 
-void out8(WORD port, BYTE data);
-void out16(WORD port, WORD data);
-void out32(WORD port, DWORD data);
+inline word in16(word port)
+{
+    word value;
+    asm volatile("inw %1, %0":"=a"(value):"Nd"(port));
+    return value;
+}
 
+inline dword in32(dword port)
+{
+    dword value;
+    asm volatile("inl %1, %0":"=a"(value):"Nd"(port));
+    return value;
+}
+
+inline void out8(word port, byte value)
+{
+    asm volatile("outb %0, %1"::"a"(value), "Nd"(port));
+}
+
+inline void out16(word port, word value)
+{
+    asm volatile("outw %0, %1"::"a"(value), "Nd"(port));
+}
+
+inline void out32(word port, dword value)
+{
+    asm volatile("outl %0, %1"::"a"(value), "Nd"(port));
+}
 }
