14fe2608c8c6a3dd3c704c1e82832fb5e1bfe704
Andreas Kling <awesomekling@gmail.com>
2018-11-04 13:13:57 +0100

    Mark LibC's assertion faillure helper with __NORETURN.

--- LibC/assert.cpp
+++ LibC/assert.cpp
@@ -4,11 +4,10 @@
 
 extern "C" {
 
-extern void __assertion_failed(const char* msg, const char* file, unsigned line, const char* func)
+void __assertion_failed(const char* msg, const char* file, unsigned line, const char* func)
 {
     fprintf(stderr, "ASSERTION FAILED: %s\n%s:%u in %s\n", msg, file, line, func);
     abort();
 }
 
 }
-
--- LibC/sys/cdefs.h
+++ LibC/sys/cdefs.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #define ALWAYS_INLINE inline __attribute__ ((always_inline))
+#define __NORETURN __attribute__ ((noreturn))
 
 #ifdef __cplusplus
 #define __BEGIN_DECLS extern "C" {
