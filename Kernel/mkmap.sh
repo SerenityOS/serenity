8a865c11ec25c1d313146c055f5efaea862e7a8d
Andreas Kling <awesomekling@gmail.com>
2018-11-09 20:40:39 +0100

    Pre-size the ksyms vector for speedier loading.
    Also show loading progress on the screen because it looks neat.

--- Kernel/mkmap.sh
+++ Kernel/mkmap.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 tmp=$(mktemp)
 nm -C kernel > $tmp
-perl -lpe '$_=hex' $tmp | paste -d" " - $tmp | sort -n | cut -d" " -f 2- > kernel.map
+perl -lpe '$_=hex' $tmp | paste -d" " - $tmp | sort -n | cut -d" " -f 2- > kernel.map.tmp
+printf "%08x\n" $(wc -l kernel.map.tmp | cut -f1 -d' ') > kernel.map
+cat kernel.map.tmp >> kernel.map
+rm -f kernel.map.tmp
 rm $tmp
