afc56d151f88b16ce91d5459edb1a27f344822ce
Andreas Kling <awesomekling@gmail.com>
2018-12-03 02:23:49 +0100

    Omit duplicate lines from the ksyms map.

--- Kernel/mkmap.sh
+++ Kernel/mkmap.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 tmp=$(mktemp)
-nm -C kernel > $tmp
-perl -lpe '$_=hex' $tmp | paste -d" " - $tmp | sort -n | cut -d" " -f 2- > kernel.map.tmp
-printf "%08x\n" $(wc -l kernel.map.tmp | cut -f1 -d' ') > kernel.map
-cat kernel.map.tmp >> kernel.map
-rm -f kernel.map.tmp
-rm $tmp
+nm -nC kernel | uniq > $tmp
+printf "%08x\n" $(wc -l $tmp | cut -f1 -d' ') > kernel.map
+cat $tmp >> kernel.map
+rm -f $tmp
