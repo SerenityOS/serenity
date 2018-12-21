4dd50b1f6d921db833e0ba6ea97cf3a526077bce
Andreas Kling <awesomekling@gmail.com>
2018-12-21 02:22:21 +0100

    Get rid of three test utilities that I no longer need.

--- Kernel/sync.sh
+++ Kernel/sync.sh
@@ -26,7 +26,6 @@ cp -v ../Userland/tst mnt/bin/tst
 cp -v ../Userland/mm mnt/bin/mm
 cp -v ../Userland/kill mnt/bin/kill
 cp -v ../Userland/tty mnt/bin/tty
-cp -v ../Userland/strsignal mnt/bin/strsignal
 cp -v ../Userland/mkdir mnt/bin/mkdir
 cp -v ../Userland/touch mnt/bin/touch
 cp -v ../Userland/sync mnt/bin/sync
