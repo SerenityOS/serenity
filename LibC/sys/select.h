480827a33e198fdcaebe3701bf81c61683431413
Andreas Kling <awesomekling@gmail.com>
2019-01-17 01:41:13 +0100

    Oops, forgot to add the select() files to LibC a while ago.

---
+++ LibC/sys/select.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <sys/cdefs.h>
+#include <sys/types.h>
+#include <string.h>
+
+__BEGIN_DECLS
+
+#define FD_SETSIZE 64
+#define FD_ZERO(set) memset((set), 0, sizeof(fd_set));
+#define FD_CLR(fd, set) ((set)->bits[(fd / 8)] &= ~(1 << (fd) % 8))
+#define FD_SET(fd, set) ((set)->bits[(fd / 8)] |= (1 << (fd) % 8))
+#define FD_ISSET(fd, set) ((set)->bits[(fd / 8)] & (1 << (fd) % 8))
+
+struct fd_set {
+    unsigned char bits[FD_SETSIZE / 8];
+};
+
+int select(int nfds, fd_set* readfds, fd_set* writefds, fd_set* exceptfds, struct timeval* timeout);
+
+__END_DECLS
+
