5605295d00ec1230b958c461f30736fc1603342d
Andreas Kling <awesomekling@gmail.com>
2019-01-17 01:41:13 +0100

    Oops, forgot to add the select() files to LibC a while ago.

---
+++ LibC/sys/select.cpp
@@ -0,0 +1,11 @@
+#include <sys/select.h>
+#include <Kernel/Syscall.h>
+#include <errno.h>
+#include <stdio.h>
+
+int select(int nfds, fd_set* readfds, fd_set* writefds, fd_set* exceptfds, struct timeval* timeout)
+{
+    Syscall::SC_select_params params { nfds, readfds, writefds, exceptfds, timeout };
+    int rc = syscall(SC_select, &params);
+    __RETURN_WITH_ERRNO(rc, rc, -1);
+}
