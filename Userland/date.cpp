95d3ec457ad648bd3000743acd3aa64e7ed5f506
Andreas Kling <awesomekling@gmail.com>
2019-02-03 02:47:37 +0100

    Userland: Make /bin/date pretty-print the date, too.

--- Userland/date.cpp
+++ Userland/date.cpp
@@ -5,8 +5,16 @@ int main(int argc, char** argv)
 {
     (void) argc;
     (void) argv;
+
     time_t now = time(nullptr);
-    printf("%u\n", now);
+    auto* tm = localtime(&now);
+    printf("%4u-%02u-%02u %02u:%02u:%02u\n",
+        tm->tm_year + 1900,
+        tm->tm_mon + 1,
+        tm->tm_mday,
+        tm->tm_hour,
+        tm->tm_min,
+        tm->tm_sec);
     return 0;
 }
 
