5b687ea843b80d7072156003f4c4a068473c768e
Andreas Kling <awesomekling@gmail.com>
2019-02-06 16:01:59 +0100

    AK: Add a TemporaryChange helper class.

---
+++ AK/TemporaryChange.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace AK {
+
+template<typename T>
+class TemporaryChange {
+public:
+    TemporaryChange(T& variable, T value) : m_variable(variable), m_old_value(variable) { m_variable = value; }
+    ~TemporaryChange() { m_variable = m_old_value; }
+
+private:
+    T& m_variable;
+    T m_old_value;
+};
+
+}
+
+using AK::TemporaryChange;
