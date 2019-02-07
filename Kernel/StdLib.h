2e663eda3665dfce16308bbea2cf568d7e9cdc09
Andreas Kling <awesomekling@gmail.com>
2019-02-07 10:29:26 +0100

    Kernel: Don't disable interrupts to access the system hostname.

--- Kernel/StdLib.h
+++ Kernel/StdLib.h
@@ -6,6 +6,7 @@ extern "C" {
 
 void memcpy(void*, const void*, dword);
 void strcpy(char*, const char*);
+char* strncpy(char*, const char*, size_t);
 int strcmp(char const*, const char*);
 size_t strlen(const char*);
 void *memset(void*, byte, dword);
