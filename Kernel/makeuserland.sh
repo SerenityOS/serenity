2cf1dd5b6f10b898f24203c879290d665531a989
Andreas Kling <awesomekling@gmail.com>
2019-02-10 08:35:01 +0100

    Move apps into a top-level Applications/ directory.

--- Kernel/makeuserland.sh
+++ Kernel/makeuserland.sh
@@ -6,12 +6,16 @@ make -C ../LibC clean && \
 make -C ../LibC && \
 make -C ../LibGUI clean && \
 make -C ../LibGUI && \
-make -C ../Terminal clean && \
-make -C ../Terminal && \
-make -C ../Clock clean && \
-make -C ../Clock && \
-make -C ../Launcher clean && \
-make -C ../Launcher && \
+make -C ../Applications/Terminal clean && \
+make -C ../Applications/Terminal && \
+make -C ../Applications/Clock clean && \
+make -C ../Applications/Clock && \
+make -C ../Applications/Launcher clean && \
+make -C ../Applications/Launcher && \
+make -C ../Applications/FileManager clean && \
+make -C ../Applications/FileManager && \
+make -C ../Applications/FontEditor clean && \
+make -C ../Applications/FontEditor && \
 make -C ../Userland clean && \
 make -C ../Userland && \
 sudo ./sync.sh
