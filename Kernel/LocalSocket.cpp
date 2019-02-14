1b0035ab5becdcf07ab9f7b41f43db2b9406b57e
Andreas Kling <awesomekling@gmail.com>
2019-02-14 14:38:30 +0100

    Kernel: Begin fleshing out bind() syscall.

--- Kernel/LocalSocket.cpp
+++ Kernel/LocalSocket.cpp
@@ -1,6 +1,8 @@
 #include <Kernel/LocalSocket.h>
 #include <Kernel/UnixTypes.h>
 #include <Kernel/Process.h>
+#include <Kernel/VirtualFileSystem.h>
+#include <LibC/errno_numbers.h>
 
 RetainPtr<LocalSocket> LocalSocket::create(int type)
 {
@@ -17,3 +19,29 @@ LocalSocket::~LocalSocket()
 {
 }
 
+bool LocalSocket::bind(const sockaddr* address, socklen_t address_size, int& error)
+{
+    if (address_size != sizeof(sockaddr_un)) {
+        error = -EINVAL;
+        return false;
+    }
+
+    if (address->sa_family != AF_LOCAL) {
+        error = -EINVAL;
+        return false;
+    }
+
+    const sockaddr_un& local_address = *reinterpret_cast<const sockaddr_un*>(address);
+    char safe_address[sizeof(local_address.sun_path) + 1];
+    memcpy(safe_address, local_address.sun_path, sizeof(local_address.sun_path));
+
+    kprintf("%s(%u) LocalSocket{%p} bind(%s)\n", current->name().characters(), current->pid(), safe_address);
+
+    auto descriptor = VFS::the().open(safe_address, error, O_CREAT | O_EXCL, S_IFSOCK | 0666, *current->cwd_inode());
+    if (!descriptor) {
+        if (error == -EEXIST)
+            error = -EADDRINUSE;
+        return error;
+    }
+    return true;
+}
--- Kernel/LocalSocket.h
+++ Kernel/LocalSocket.h
@@ -8,6 +8,8 @@ public:
     static RetainPtr<LocalSocket> create(int type);
     virtual ~LocalSocket() override;
 
+    virtual bool bind(const sockaddr*, socklen_t, int& error) override;
+
 private:
     explicit LocalSocket(int type);
 
