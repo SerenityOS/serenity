8321908abe7a3cfef4b90f80f40e6fa004cc8927
Andreas Kling <awesomekling@gmail.com>
2019-02-17 12:08:28 +0100

    LibGUI: Tweak the scrollbar and button styles a bit.

--- LibGUI/GStyle.cpp
+++ LibGUI/GStyle.cpp
@@ -33,19 +33,19 @@ void GStyle::paint_button(Painter& painter, const Rect& rect, bool pressed)
     } else {
         // Base
         painter.fill_rect({ 3, 3, rect.width() - 5, rect.height() - 5 }, button_color);
-        painter.fill_rect_with_gradient({ 3, 3, rect.width() - 5, rect.height() - 5 }, button_color, Color::White);
+        painter.fill_rect_with_gradient({ 2, 2, rect.width() - 3, rect.height() - 3 }, button_color, Color::White);
 
         // White highlight
         painter.draw_line({ 1, 1 }, { rect.width() - 2, 1 }, highlight_color);
-        painter.draw_line({ 1, 2 }, { rect.width() - 3, 2 }, highlight_color);
-        painter.draw_line({ 1, 3 }, { 1, rect.height() - 2 }, highlight_color);
-        painter.draw_line({ 2, 3 }, { 2, rect.height() - 3 }, highlight_color);
+        //painter.draw_line({ 1, 2 }, { rect.width() - 3, 2 }, highlight_color);
+        painter.draw_line({ 1, 2 }, { 1, rect.height() - 2 }, highlight_color);
+        //painter.draw_line({ 2, 3 }, { 2, rect.height() - 3 }, highlight_color);
 
         // Gray shadow
-        painter.draw_line({ rect.width() - 2, 1 }, { rect.width() - 2, rect.height() - 4 }, shadow_color);
-        painter.draw_line({ rect.width() - 3, 2 }, { rect.width() - 3, rect.height() - 4 }, shadow_color);
+        painter.draw_line({ rect.width() - 2, 1 }, { rect.width() - 2, rect.height() - 3 }, shadow_color);
+        //painter.draw_line({ rect.width() - 3, 2 }, { rect.width() - 3, rect.height() - 4 }, shadow_color);
         painter.draw_line({ 1, rect.height() - 2 }, { rect.width() - 2, rect.height() - 2 }, shadow_color);
-        painter.draw_line({ 2, rect.height() - 3 }, { rect.width() - 2, rect.height() - 3 }, shadow_color);
+        //painter.draw_line({ 2, rect.height() - 3 }, { rect.width() - 2, rect.height() - 3 }, shadow_color);
     }
 
     painter.translate(-rect.location().x(), -rect.location().y());
