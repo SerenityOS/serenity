c25ac5d398703ad527dc9817d647e19f728de88a
Andreas Kling <awesomekling@gmail.com>
2019-02-19 02:39:18 +0100

    Kernel: Fix wrong calculation of current Unix timestamp.

--- Kernel/RTC.cpp
+++ Kernel/RTC.cpp
@@ -92,7 +92,7 @@ time_t now()
 
     return days_in_years_since_epoch(year - 1) * 86400
          + days_in_months_since_start_of_year(month - 1, year) * 86400
-         + day * 86400
+         + (day - 1) * 86400
          + hour * 3600
          + minute * 60
          + second;
