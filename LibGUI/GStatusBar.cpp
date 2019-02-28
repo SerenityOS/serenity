c5d88ff0d8d9259bcfd612353ec0f7c36323362e
Andreas Kling <awesomekling@gmail.com>
2019-02-28 19:17:08 +0100

    LibGUI: Top line on GStatusBar should be a highlight, not a shadow.

--- LibGUI/GStatusBar.cpp
+++ LibGUI/GStatusBar.cpp
@@ -33,5 +33,5 @@ void GStatusBar::paint_event(GPaintEvent& event)
     Painter painter(*this);
     painter.set_clip_rect(event.rect());
     painter.fill_rect({ 0, 1, width(), height() - 1 }, Color::LightGray);
-    painter.draw_line({ 0, 0 }, { width() - 1, 0 }, Color::DarkGray);
+    painter.draw_line({ 0, 0 }, { width() - 1, 0 }, Color::White);
 }
