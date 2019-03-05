c4befb5a18b0847269d0e3d82a7a751d58560d0a
Andreas Kling <awesomekling@gmail.com>
2019-03-05 12:48:59 +0100

    LibGUI: Let GApplication::exec() call exit() instead of returning to main().
    This sidesteps the problem of having various things on the heap that don't
    get torn down. It's obviously not a great solution, but it'll work for now.

--- LibGUI/GApplication.cpp
+++ LibGUI/GApplication.cpp
@@ -22,11 +22,16 @@ GApplication::GApplication(int argc, char** argv)
 
 GApplication::~GApplication()
 {
+    s_the = nullptr;
 }
 
 int GApplication::exec()
 {
-    return m_event_loop->exec();
+    int exit_code = m_event_loop->exec();
+    // NOTE: Maybe it would be cool to return instead of exit()?
+    //       This would require cleaning up all the GObjects on the heap.
+    exit(exit_code);
+    return exit_code;
 }
 
 void GApplication::quit(int exit_code)
