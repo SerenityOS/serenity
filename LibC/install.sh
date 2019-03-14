6fcb5c214009ea755800ac94864100eab7534ec2
Andreas Kling <awesomekling@gmail.com>
2019-03-14 13:03:32 +0100

    LibC: Minor socket-related compat fixes.

--- LibC/install.sh
+++ LibC/install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 mkdir -p ../Root/usr/include/sys/
+mkdir -p ../Root/usr/include/netinet/
+mkdir -p ../Root/usr/include/arpa/
 mkdir -p ../Root/usr/lib/
 cp *.h ../Root/usr/include/
 cp sys/*.h ../Root/usr/include/sys/
+cp arpa/*.h ../Root/usr/include/arpa/
+cp netinet/*.h ../Root/usr/include/netinet/
 cp libc.a ../Root/usr/lib/
 cp crt0.o ../Root/usr/lib/
