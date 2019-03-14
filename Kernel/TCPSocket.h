d2176eddfce1b5f4ad8d6516cdc9c3596357142b
Andreas Kling <awesomekling@gmail.com>
2019-03-14 12:20:38 +0100

    Kernel: Factor out TCP parts of IPv4Socket into a TCPSocket class.

---
+++ Kernel/TCPSocket.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <Kernel/IPv4Socket.h>
+
+class TCPSocket final : public IPv4Socket {
+public:
+    static Retained<TCPSocket> create(int protocol);
+    virtual ~TCPSocket() override;
+
+    enum class State {
+        Disconnected,
+        Connecting,
+        Connected,
+        Disconnecting,
+    };
+
+    State state() const { return m_state; }
+    void set_state(State state) { m_state = state; }
+
+    void set_ack_number(dword n) { m_ack_number = n; }
+    void set_sequence_number(dword n) { m_sequence_number = n; }
+    dword ack_number() const { return m_ack_number; }
+    dword sequence_number() const { return m_sequence_number; }
+
+    void send_tcp_packet(word flags, const void* = nullptr, int = 0);
+
+private:
+    explicit TCPSocket(int protocol);
+
+    NetworkOrdered<word> compute_tcp_checksum(const IPv4Address& source, const IPv4Address& destination, const TCPPacket&, word payload_size);
+
+    virtual int protocol_receive(const ByteBuffer&, void* buffer, size_t buffer_size, int flags, sockaddr* addr, socklen_t* addr_length) override;
+    virtual int protocol_send(const void*, int) override;
+    virtual KResult protocol_connect() override;
+
+    dword m_sequence_number { 0 };
+    dword m_ack_number { 0 };
+    State m_state { State::Disconnected };
+};
+
+class TCPSocketHandle : public SocketHandle {
+public:
+    TCPSocketHandle() { }
+
+    TCPSocketHandle(RetainPtr<TCPSocket>&& socket)
+        : SocketHandle(move(socket))
+    {
+    }
+
+    TCPSocketHandle(TCPSocketHandle&& other)
+        : SocketHandle(move(other))
+    {
+    }
+
+    TCPSocketHandle(const TCPSocketHandle&) = delete;
+    TCPSocketHandle& operator=(const TCPSocketHandle&) = delete;
+
+    TCPSocket* operator->() { return &socket(); }
+    const TCPSocket* operator->() const { return &socket(); }
+
+    TCPSocket& socket() { return static_cast<TCPSocket&>(SocketHandle::socket()); }
+    const TCPSocket& socket() const { return static_cast<const TCPSocket&>(SocketHandle::socket()); }
+};
