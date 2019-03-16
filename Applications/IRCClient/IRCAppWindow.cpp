a7f559e35a0a53146990750888e5ed90ada35cfd
Andreas Kling <awesomekling@gmail.com>
2019-03-16 01:10:48 +0100

    IRCClient: Add menus.

--- Applications/IRCClient/IRCAppWindow.cpp
+++ Applications/IRCClient/IRCAppWindow.cpp
@@ -1,11 +1,14 @@
 #include "IRCAppWindow.h"
 #include "IRCClientWindow.h"
 #include "IRCClientWindowListModel.h"
+#include <LibGUI/GApplication.h>
 #include <LibGUI/GStackWidget.h>
 #include <LibGUI/GTableView.h>
 #include <LibGUI/GBoxLayout.h>
 #include <LibGUI/GToolBar.h>
 #include <LibGUI/GAction.h>
+#include <LibGUI/GMenu.h>
+#include <LibGUI/GMenuBar.h>
 #include <stdio.h>
 
 IRCAppWindow::IRCAppWindow()
@@ -15,6 +18,8 @@ IRCAppWindow::IRCAppWindow()
     set_title(String::format("IRC Client: %s@%s:%d", m_client.nickname().characters(), m_client.hostname().characters(), m_client.port()));
     set_rect(200, 200, 600, 400);
     setup_client();
+    setup_actions();
+    setup_menus();
     setup_widgets();
 }
 
@@ -43,45 +48,73 @@ void IRCAppWindow::setup_client()
     m_client.connect();
 }
 
+void IRCAppWindow::setup_actions()
+{
+    m_join_action = GAction::create("Join channel", GraphicsBitmap::load_from_file(GraphicsBitmap::Format::RGBA32, "/res/icons/16x16/irc-join.rgb", { 16, 16 }), [] (auto&) {
+        printf("FIXME: Implement join action\n");
+    });
+
+    m_part_action = GAction::create("Part from channel", GraphicsBitmap::load_from_file(GraphicsBitmap::Format::RGBA32, "/res/icons/16x16/irc-part.rgb", { 16, 16 }), [] (auto&) {
+        printf("FIXME: Implement part action\n");
+    });
+
+    m_whois_action = GAction::create("Whois user", GraphicsBitmap::load_from_file(GraphicsBitmap::Format::RGBA32, "/res/icons/16x16/irc-whois.rgb", { 16, 16 }), [] (auto&) {
+        printf("FIXME: Implement whois action\n");
+    });
+
+    m_open_query_action = GAction::create("Open query", GraphicsBitmap::load_from_file(GraphicsBitmap::Format::RGBA32, "/res/icons/16x16/irc-open-query.rgb", { 16, 16 }), [] (auto&) {
+        printf("FIXME: Implement open-query action\n");
+    });
+
+    m_close_query_action = GAction::create("Close query", GraphicsBitmap::load_from_file(GraphicsBitmap::Format::RGBA32, "/res/icons/16x16/irc-close-query.rgb", { 16, 16 }), [] (auto&) {
+        printf("FIXME: Implement close-query action\n");
+    });
+}
+
+void IRCAppWindow::setup_menus()
+{
+    auto menubar = make<GMenuBar>();
+    auto app_menu = make<GMenu>("IRC Client");
+    app_menu->add_action(GAction::create("Quit", { Mod_Alt, Key_F4 }, [] (const GAction&) {
+        dbgprintf("Terminal: Quit menu activated!\n");
+        GApplication::the().quit(0);
+        return;
+    }));
+    menubar->add_menu(move(app_menu));
+
+    auto server_menu = make<GMenu>("Server");
+    server_menu->add_action(*m_join_action);
+    server_menu->add_action(*m_part_action);
+    server_menu->add_separator();
+    server_menu->add_action(*m_whois_action);
+    server_menu->add_action(*m_open_query_action);
+    server_menu->add_action(*m_close_query_action);
+    menubar->add_menu(move(server_menu));
+
+    auto help_menu = make<GMenu>("Help");
+    help_menu->add_action(GAction::create("About", [] (const GAction&) {
+        dbgprintf("FIXME: Implement Help/About\n");
+    }));
+    menubar->add_menu(move(help_menu));
+
+    GApplication::the().set_menubar(move(menubar));
+}
+
 void IRCAppWindow::setup_widgets()
 {
     auto* widget = new GWidget(nullptr);
     set_main_widget(widget);
     widget->set_layout(make<GBoxLayout>(Orientation::Vertical));
 
-    printf("main_widget: %s{%p}\n", widget->class_name(), widget);
-
-    auto join_action = GAction::create("Join channel", GraphicsBitmap::load_from_file(GraphicsBitmap::Format::RGBA32, "/res/icons/16x16/irc-join.rgb", { 16, 16 }), [] (auto&) {
-        printf("FIXME: Implement join action\n");
-    });
-
-    auto part_action = GAction::create("Part from channel", GraphicsBitmap::load_from_file(GraphicsBitmap::Format::RGBA32, "/res/icons/16x16/irc-part.rgb", { 16, 16 }), [] (auto&) {
-        printf("FIXME: Implement part action\n");
-    });
-
-    auto whois_action = GAction::create("Whois user", GraphicsBitmap::load_from_file(GraphicsBitmap::Format::RGBA32, "/res/icons/16x16/irc-whois.rgb", { 16, 16 }), [] (auto&) {
-        printf("FIXME: Implement whois action\n");
-    });
-
-    auto open_query_action = GAction::create("Open query", GraphicsBitmap::load_from_file(GraphicsBitmap::Format::RGBA32, "/res/icons/16x16/irc-open-query.rgb", { 16, 16 }), [] (auto&) {
-        printf("FIXME: Implement open-query action\n");
-    });
-
-    auto close_query_action = GAction::create("Close query", GraphicsBitmap::load_from_file(GraphicsBitmap::Format::RGBA32, "/res/icons/16x16/irc-close-query.rgb", { 16, 16 }), [] (auto&) {
-        printf("FIXME: Implement close-query action\n");
-    });
-
     auto* toolbar = new GToolBar(widget);
-    toolbar->add_action(join_action.copy_ref());
-    toolbar->add_action(part_action.copy_ref());
+    toolbar->add_action(*m_join_action);
+    toolbar->add_action(*m_part_action.copy_ref());
     toolbar->add_separator();
-    toolbar->add_action(whois_action.copy_ref());
-    toolbar->add_action(open_query_action.copy_ref());
-    toolbar->add_action(close_query_action.copy_ref());
-
+    toolbar->add_action(*m_whois_action);
+    toolbar->add_action(*m_open_query_action);
+    toolbar->add_action(*m_close_query_action);
 
     auto* horizontal_container = new GWidget(widget);
-    printf("horizontal_widget: %s{%p}\n", horizontal_container->class_name(), horizontal_container);
     horizontal_container->set_layout(make<GBoxLayout>(Orientation::Horizontal));
 
     auto* window_list = new GTableView(horizontal_container);
@@ -95,7 +128,6 @@ void IRCAppWindow::setup_widgets()
     };
 
     m_container = new GStackWidget(horizontal_container);
-    printf("m_container: %s{%p}\n", ((GWidget*)m_container)->class_name(), m_container);
 
     create_subwindow(IRCClientWindow::Server, "Server");
 }
