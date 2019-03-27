fb7c7829c2a58ea55b7b0fa058d88c74738d859b
Andreas Kling <awesomekling@gmail.com>
2019-03-27 01:36:33 +0100

    Terminal: Export a simple PATH=/bin:/usr/bin to shells.

--- Applications/Terminal/main.cpp
+++ Applications/Terminal/main.cpp
@@ -65,7 +65,7 @@ static void make_shell(int ptm_fd)
             exit(1);
         }
         char* args[] = { "/bin/sh", nullptr };
-        char* envs[] = { "TERM=xterm", nullptr };
+        char* envs[] = { "TERM=xterm", "PATH=/bin:/usr/bin", nullptr };
         rc = execve("/bin/sh", args, envs);
         if (rc < 0) {
             perror("execve");
