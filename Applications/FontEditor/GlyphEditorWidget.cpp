dde224fe442751b09aa858c35479d2fa69ead1cb
Andreas Kling <awesomekling@gmail.com>
2019-04-03 15:23:13 +0200

    FontEditor: Break out classes into separate files.

---
+++ Applications/FontEditor/GlyphEditorWidget.cpp
@@ -0,0 +1,97 @@
+#include "GlyphEditorWidget.h"
+#include <LibGUI/GPainter.h>
+
+GlyphEditorWidget::GlyphEditorWidget(Font& mutable_font, GWidget* parent)
+    : GWidget(parent)
+    , m_font(mutable_font)
+{
+    set_relative_rect({ 0, 0, preferred_width(), preferred_height() });
+}
+
+GlyphEditorWidget::~GlyphEditorWidget()
+{
+}
+
+void GlyphEditorWidget::set_glyph(byte glyph)
+{
+    if (m_glyph == glyph)
+        return;
+    m_glyph = glyph;
+    update();
+}
+
+void GlyphEditorWidget::paint_event(GPaintEvent&)
+{
+    GPainter painter(*this);
+    painter.fill_rect(rect(), Color::White);
+    painter.draw_rect(rect(), Color::Black);
+
+    for (int y = 0; y < font().glyph_height(); ++y)
+        painter.draw_line({ 0, y * m_scale }, { font().max_glyph_width() * m_scale, y * m_scale }, Color::Black);
+
+    for (int x = 0; x < font().max_glyph_width(); ++x)
+        painter.draw_line({ x * m_scale, 0 }, { x * m_scale, font().glyph_height() * m_scale }, Color::Black);
+
+    painter.translate(1, 1);
+
+    auto bitmap = font().glyph_bitmap(m_glyph);
+
+    for (int y = 0; y < font().glyph_height(); ++y) {
+        for (int x = 0; x < font().max_glyph_width(); ++x) {
+            Rect rect { x * m_scale, y * m_scale, m_scale, m_scale };
+            if (x >= font().glyph_width(m_glyph)) {
+                painter.fill_rect(rect, Color::MidGray);
+            } else {
+                if (bitmap.bit_at(x, y))
+                    painter.fill_rect(rect, Color::Black);
+            }
+        }
+    }
+
+    if (is_focused()) {
+        painter.translate(-1, -1);
+        painter.draw_focus_rect(rect());
+    }
+}
+
+void GlyphEditorWidget::mousedown_event(GMouseEvent& event)
+{
+    draw_at_mouse(event);
+}
+
+void GlyphEditorWidget::mousemove_event(GMouseEvent& event)
+{
+    if (event.buttons() & (GMouseButton::Left | GMouseButton::Right))
+        draw_at_mouse(event);
+}
+
+void GlyphEditorWidget::draw_at_mouse(const GMouseEvent& event)
+{
+    bool set = event.buttons() & GMouseButton::Left;
+    bool unset = event.buttons() & GMouseButton::Right;
+    if (!(set ^ unset))
+        return;
+    int x = (event.x() - 1) / m_scale;
+    int y = (event.y() - 1) / m_scale;
+    auto bitmap = font().glyph_bitmap(m_glyph);
+    if (x >= bitmap.width())
+        return;
+    if (y >= bitmap.height())
+        return;
+    if (bitmap.bit_at(x, y) == set)
+        return;
+    bitmap.set_bit_at(x, y, set);
+    if (on_glyph_altered)
+        on_glyph_altered();
+    update();
+}
+
+int GlyphEditorWidget::preferred_width() const
+{
+    return font().max_glyph_width() * m_scale + 1;
+}
+
+int GlyphEditorWidget::preferred_height() const
+{
+    return font().glyph_height() * m_scale + 1;
+}
---
+++ Applications/FontEditor/GlyphEditorWidget.h
@@ -0,0 +1,31 @@
+#include <LibGUI/GWidget.h>
+#include <AK/Function.h>
+
+class GlyphEditorWidget final : public GWidget {
+public:
+    GlyphEditorWidget(Font&, GWidget* parent);
+    virtual ~GlyphEditorWidget() override;
+
+    byte glyph() const { return m_glyph; }
+    void set_glyph(byte);
+
+    int preferred_width() const;
+    int preferred_height() const;
+
+    Font& font() { return *m_font; }
+    const Font& font() const { return *m_font; }
+
+    Function<void()> on_glyph_altered;
+
+private:
+    virtual void paint_event(GPaintEvent&) override;
+    virtual void mousedown_event(GMouseEvent&) override;
+    virtual void mousemove_event(GMouseEvent&) override;
+    virtual bool accepts_focus() const override { return true; }
+
+    void draw_at_mouse(const GMouseEvent&);
+
+    RetainPtr<Font> m_font;
+    byte m_glyph { 0 };
+    int m_scale { 10 };
+};
