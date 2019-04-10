3c8e53ef2b70459e72088c681340a0cbdf5a73e3
Andreas Kling <awesomekling@gmail.com>
2019-04-11 01:59:07 +0200

    VisualBuilder: Work on selecting a widget and moving it around.

--- Applications/VisualBuilder/VBForm.h
+++ Applications/VisualBuilder/VBForm.h
@@ -9,12 +9,24 @@ public:
     explicit VBForm(const String& name, GWidget* parent = nullptr);
     virtual ~VBForm() override;
 
+    bool is_selected(const VBWidget&) const;
+    VBWidget* widget_at(const Point&);
+
+    void set_should_snap_to_grip(bool snap) { m_should_snap_to_grid = snap; }
+    bool should_snap_to_grid() const { return m_should_snap_to_grid; }
+
 protected:
     virtual void paint_event(GPaintEvent&) override;
+    virtual void mousedown_event(GMouseEvent&) override;
+    virtual void mousemove_event(GMouseEvent&) override;
+    virtual void mouseup_event(GMouseEvent&) override;
 
 private:
     String m_name;
     int m_grid_size { 5 };
-
-    Vector<VBWidget*> m_widgets;
+    bool m_should_snap_to_grid { true };
+    Vector<Retained<VBWidget>> m_widgets;
+    WeakPtr<VBWidget> m_selected_widget;
+    Point m_transform_event_origin;
+    Rect m_transform_widget_origin_rect;
 };
--- Applications/VisualBuilder/VBWidget.h
+++ Applications/VisualBuilder/VBWidget.h
@@ -1,15 +1,44 @@
 #pragma once
 
 #include <SharedGraphics/Rect.h>
+#include <AK/Retainable.h>
+#include <AK/Retained.h>
+#include <AK/Weakable.h>
 
-class VBWidget {
+class GPainter;
+class VBForm;
+
+enum class Direction { None, Left, UpLeft, Up, UpRight, Right, DownRight, Down, DownLeft };
+template<typename Callback>
+inline void for_each_direction(Callback callback)
+{
+    callback(Direction::Left);
+    callback(Direction::UpLeft);
+    callback(Direction::Up);
+    callback(Direction::UpRight);
+    callback(Direction::Right);
+    callback(Direction::DownRight);
+    callback(Direction::Down);
+    callback(Direction::DownLeft);
+}
+
+class VBWidget : public Retainable<VBWidget>, public Weakable<VBWidget> {
 public:
-    VBWidget();
+    static Retained<VBWidget> create(VBForm& form) { return adopt(*new VBWidget(form)); }
     virtual ~VBWidget();
+
+    bool is_selected() const;
 
     Rect rect() const { return m_rect; }
     void set_rect(const Rect& rect) { m_rect = rect; }
 
+    Rect grabber_rect(Direction) const;
+
+    void paint(GPainter&);
+
 private:
+    VBWidget(VBForm&);
+
+    VBForm& m_form;
     Rect m_rect;
 };
