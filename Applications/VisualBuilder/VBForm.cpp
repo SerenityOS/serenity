fdb2b1d9725a382cf8a969eb4cfd63269c44b6bb
Andreas Kling <awesomekling@gmail.com>
2019-04-11 01:59:07 +0200

    VisualBuilder: Work on selecting a widget and moving it around.

--- Applications/VisualBuilder/VBForm.cpp
+++ Applications/VisualBuilder/VBForm.cpp
@@ -1,4 +1,5 @@
 #include "VBForm.h"
+#include "VBWidget.h"
 #include <LibGUI/GPainter.h>
 
 VBForm::VBForm(const String& name, GWidget* parent)
@@ -6,6 +7,14 @@ VBForm::VBForm(const String& name, GWidget* parent)
 {
     set_fill_with_background_color(true);
     set_background_color(Color::LightGray);
+
+    auto box1 = VBWidget::create(*this);
+    box1->set_rect({ 10, 10, 61, 41 });
+    m_widgets.append(move(box1));
+
+    auto box2 = VBWidget::create(*this);
+    box2->set_rect({ 100, 100, 161, 141 });
+    m_widgets.append(move(box2));
 }
 
 VBForm::~VBForm()
@@ -21,5 +30,62 @@ void VBForm::paint_event(GPaintEvent& event)
         for (int x = 0; x < width(); x += m_grid_size) {
             painter.set_pixel({ x, y }, Color::Black);
         }
+    }
+
+    for (auto& widget : m_widgets) {
+        widget->paint(painter);
+    }
+}
+
+bool VBForm::is_selected(const VBWidget& widget) const
+{
+    return &widget == m_selected_widget.ptr();
+}
+
+VBWidget* VBForm::widget_at(const Point& position)
+{
+    for (int i = m_widgets.size() - 1; i >= 0; --i) {
+        auto& widget = *m_widgets[i];
+        if (widget.rect().contains(position))
+            return &widget;
+    }
+    return nullptr;
+}
+
+void VBForm::mousedown_event(GMouseEvent& event)
+{
+    auto* widget = widget_at(event.position());
+    if (!widget) {
+        if (m_selected_widget) {
+            m_selected_widget = nullptr;
+            update();
+        }
+        return;
+    }
+    if (event.button() == GMouseButton::Left) {
+        m_selected_widget = widget->make_weak_ptr();
+        m_transform_event_origin = event.position();
+        m_transform_widget_origin_rect = widget->rect();
+        update();
+    }
+}
+
+void VBForm::mousemove_event(GMouseEvent& event)
+{
+    if (event.buttons() & GMouseButton::Left && m_selected_widget) {
+        auto delta = event.position() - m_transform_event_origin;
+        auto new_rect = m_transform_widget_origin_rect.translated(delta);
+        new_rect.set_x(new_rect.x() - (new_rect.x() % m_grid_size));
+        new_rect.set_y(new_rect.y() - (new_rect.y() % m_grid_size));
+        m_selected_widget->set_rect(new_rect);
+        update();
+    }
+}
+
+void VBForm::mouseup_event(GMouseEvent& event)
+{
+    if (event.button() == GMouseButton::Left) {
+        m_transform_event_origin = { };
+        m_transform_widget_origin_rect = { };
     }
 }
---
+++ Applications/VisualBuilder/VBWidget.cpp
@@ -0,0 +1,55 @@
+#include "VBWidget.h"
+#include "VBForm.h"
+#include <LibGUI/GPainter.h>
+
+VBWidget::VBWidget(VBForm& form)
+    : m_form(form)
+{
+}
+
+VBWidget::~VBWidget()
+{
+}
+
+bool VBWidget::is_selected() const
+{
+    return m_form.is_selected(*this);
+}
+
+Rect VBWidget::grabber_rect(Direction direction) const
+{
+    int grabber_size = 5;
+    int half_grabber_size = grabber_size / 2;
+    switch (direction) {
+    case Direction::Left:
+        return { m_rect.x() - half_grabber_size, m_rect.center().y() - half_grabber_size, grabber_size, grabber_size };
+    case Direction::UpLeft:
+        return { m_rect.x() - half_grabber_size, m_rect.y() - half_grabber_size, grabber_size, grabber_size };
+    case Direction::Up:
+        return { m_rect.center().x() - half_grabber_size, m_rect.y() - half_grabber_size, grabber_size, grabber_size };
+    case Direction::UpRight:
+        return { m_rect.right() - half_grabber_size, m_rect.y() - half_grabber_size, grabber_size, grabber_size };
+    case Direction::Right:
+        return { m_rect.right() - half_grabber_size, m_rect.center().y() - half_grabber_size, grabber_size, grabber_size };
+    case Direction::DownLeft:
+        return { m_rect.x() - half_grabber_size, m_rect.bottom() - half_grabber_size, grabber_size, grabber_size };
+    case Direction::Down:
+        return { m_rect.center().x() - half_grabber_size, m_rect.bottom() - half_grabber_size, grabber_size, grabber_size };
+    case Direction::DownRight:
+        return { m_rect.right() - half_grabber_size, m_rect.bottom() - half_grabber_size, grabber_size, grabber_size };
+    default:
+        ASSERT_NOT_REACHED();
+    }
+}
+
+void VBWidget::paint(GPainter& painter)
+{
+    painter.fill_rect(m_rect, Color::White);
+    painter.draw_rect(m_rect, Color::Black);
+
+    if (is_selected()) {
+        for_each_direction([&] (Direction direction) {
+            painter.fill_rect(grabber_rect(direction), Color::Black);
+        });
+    }
+}
