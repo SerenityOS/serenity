383b2efc1f8c39c672f660ba63e9805571ae42ee
Andreas Kling <awesomekling@gmail.com>
2019-04-11 21:47:35 +0200

    VisualBuilder: Remove debug spam.

--- Applications/VisualBuilder/VBWidgetPropertyModel.cpp
+++ Applications/VisualBuilder/VBWidgetPropertyModel.cpp
@@ -14,7 +14,6 @@ VBWidgetPropertyModel::~VBWidgetPropertyModel()
 
 int VBWidgetPropertyModel::row_count(const GModelIndex&) const
 {
-    dbgprintf("row count: %d\n", m_widget.m_properties.size());
     return m_widget.m_properties.size();
 }
 
@@ -36,13 +35,8 @@ GModel::ColumnMetadata VBWidgetPropertyModel::column_metadata(int column) const
 GVariant VBWidgetPropertyModel::data(const GModelIndex& index, Role role) const
 {
     if (role == Role::Display) {
-        dbgprintf("Accessing prop #%d (size=%d) column %d\n", index.row(), m_widget.m_properties.size(), index.column());
         auto& property = *m_widget.m_properties[index.row()];
         auto value = property.value();
-        dbgprintf("value type=%u\n", (unsigned)value.type());
-        dbgprintf("value impl=%x\n", (unsigned)value.to_string().impl());
-        dbgprintf("value len=%x\n", (unsigned)value.to_string().impl()->length());
-        dbgprintf("for value='%s'\n", value.to_string().characters());
         switch (index.column()) {
         case Column::Name: return property.name();
         case Column::Value: return property.value();
