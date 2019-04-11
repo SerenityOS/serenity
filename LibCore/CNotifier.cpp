e06c5937cc5205c08946e106d1d494d877e0248d
Andreas Kling <awesomekling@gmail.com>
2019-04-12 00:03:21 +0200

    LibCore: Prune remaining knowledge about LibGUI.

--- LibCore/CNotifier.cpp
+++ LibCore/CNotifier.cpp
@@ -1,15 +1,16 @@
 #include <LibCore/CNotifier.h>
-#include <LibGUI/GEventLoop.h>
+#include <LibCore/CEventLoop.h>
+#include <LibCore/CEvent.h>
 
 CNotifier::CNotifier(int fd, unsigned event_mask)
     : m_fd(fd)
     , m_event_mask(event_mask)
 {
-    GEventLoop::register_notifier(Badge<CNotifier>(), *this);
+    CEventLoop::register_notifier(Badge<CNotifier>(), *this);
 }
 
 CNotifier::~CNotifier()
 {
-    GEventLoop::unregister_notifier(Badge<CNotifier>(), *this);
+    CEventLoop::unregister_notifier(Badge<CNotifier>(), *this);
 }
 
