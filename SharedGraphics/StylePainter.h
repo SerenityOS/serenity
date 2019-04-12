45ccc175ea5e6bf2327f6c64c30d160c394aea5a
Andreas Kling <awesomekling@gmail.com>
2019-04-12 02:51:59 +0200

    StylePainter: Don't paint hover frame around disabled CoolBar buttons.

--- SharedGraphics/StylePainter.h
+++ SharedGraphics/StylePainter.h
@@ -9,7 +9,7 @@ enum class FrameShape { NoFrame, Box, Container, Panel, VerticalLine, Horizontal
 
 class StylePainter {
 public:
-    static void paint_button(Painter&, const Rect&, ButtonStyle, bool pressed, bool hovered = false, bool checked = false);
+    static void paint_button(Painter&, const Rect&, ButtonStyle, bool pressed, bool hovered = false, bool checked = false, bool enabled = true);
     static void paint_surface(Painter&, const Rect&, bool paint_vertical_lines = true);
     static void paint_frame(Painter&, const Rect&, FrameShape, FrameShadow, int thickness, bool skip_vertical_lines = false);
 };
