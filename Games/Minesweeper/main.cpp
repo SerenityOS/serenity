198a1a6877d0183403a0394989607618a8d41832
Andreas Kling <awesomekling@gmail.com>
2019-04-13 14:32:18 +0200

    Minesweeper: Add some menus.

--- Games/Minesweeper/main.cpp
+++ Games/Minesweeper/main.cpp
@@ -3,6 +3,9 @@
 #include <LibGUI/GWindow.h>
 #include <LibGUI/GBoxLayout.h>
 #include <LibGUI/GButton.h>
+#include <LibGUI/GMenu.h>
+#include <LibGUI/GMenuBar.h>
+#include <LibGUI/GAction.h>
 
 int main(int argc, char** argv)
 {
@@ -29,6 +32,29 @@ int main(int argc, char** argv)
     face_button->on_click = [field] (auto&) {
         field->reset();
     };
+
+    auto menubar = make<GMenuBar>();
+
+    auto app_menu = make<GMenu>("Minesweeper");
+    app_menu->add_action(GAction::create("Quit", { Mod_Alt, Key_F4 }, [] (const GAction&) {
+        GApplication::the().quit(0);
+        return;
+    }));
+    menubar->add_menu(move(app_menu));
+
+    auto game_menu = make<GMenu>("Game");
+    game_menu->add_action(GAction::create("New game", { Mod_None, Key_F2 }, [field] (const GAction&) {
+        field->reset();
+    }));
+    menubar->add_menu(move(game_menu));
+
+    auto help_menu = make<GMenu>("Help");
+    help_menu->add_action(GAction::create("About", [] (const GAction&) {
+        dbgprintf("FIXME: Implement Help/About\n");
+    }));
+    menubar->add_menu(move(help_menu));
+
+    app.set_menubar(move(menubar));
 
     window->show();
 
