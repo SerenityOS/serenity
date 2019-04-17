52bd5988f6a2927dbde801b546fb36391137fd57
Andreas Kling <awesomekling@gmail.com>
2019-04-17 23:13:07 +0200

    LibC: Bring the C library close enough to newlib to trick GCC.
    Now we can build GCC with --with-newlib, which hopefully cuts down on weird
    toolchain build issues.

---
+++ LibC/iconv.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+typedef void * iconv_t;
+
+extern iconv_t iconv_open(const char* tocode, const char* fromcode);
+extern size_t iconv(iconv_t, char** inbuf, size_t* inbytesleft, char** outbuf, size_t* outbytesleft);
+extern int iconv_close(iconv_t);
+
+__END_DECLS
