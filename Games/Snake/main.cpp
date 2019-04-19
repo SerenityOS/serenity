eca9494adfcdd0acb6a27172053195b0c4c8ec09
Andreas Kling <awesomekling@gmail.com>
2019-04-20 01:54:10 +0200

    Snake: Import skeleton of a new snake game.

---
+++ Games/Snake/main.cpp
@@ -0,0 +1,52 @@
+#include "SnakeGame.h"
+#include <LibGUI/GApplication.h>
+#include <LibGUI/GWindow.h>
+#include <LibGUI/GBoxLayout.h>
+#include <LibGUI/GButton.h>
+#include <LibGUI/GMenu.h>
+#include <LibGUI/GMenuBar.h>
+#include <LibGUI/GAction.h>
+
+int main(int argc, char** argv)
+{
+    GApplication app(argc, argv);
+
+    auto* window = new GWindow;
+    window->set_title("Snake");
+    window->set_rect(100, 100, 139, 175);
+
+    auto* widget = new GWidget;
+    window->set_main_widget(widget);
+    widget->set_layout(make<GBoxLayout>(Orientation::Vertical));
+
+    auto* game = new SnakeGame(widget);
+
+    auto menubar = make<GMenuBar>();
+
+    auto app_menu = make<GMenu>("Snake");
+    app_menu->add_action(GAction::create("Quit", { Mod_Alt, Key_F4 }, [] (const GAction&) {
+        GApplication::the().quit(0);
+        return;
+    }));
+    menubar->add_menu(move(app_menu));
+
+    auto game_menu = make<GMenu>("Game");
+    game_menu->add_action(GAction::create("New game", { Mod_None, Key_F2 }, [&] (const GAction&) {
+        game->reset();
+    }));
+    menubar->add_menu(move(game_menu));
+
+    auto help_menu = make<GMenu>("Help");
+    help_menu->add_action(GAction::create("About", [] (const GAction&) {
+        dbgprintf("FIXME: Implement Help/About\n");
+    }));
+    menubar->add_menu(move(help_menu));
+
+    app.set_menubar(move(menubar));
+
+    window->show();
+
+    window->set_icon_path("/res/icons/minesweeper/mine.png");
+
+    return app.exec();
+}
