5451178c63d9bffddf48d2f854201b71f9d5e25c
Andreas Kling <awesomekling@gmail.com>
2019-04-21 02:59:20 +0200

    LibC: Minor compat tweak, move struct timezone to sys/time.h

--- LibC/sys/time.h
+++ LibC/sys/time.h
@@ -4,6 +4,11 @@
 
 __BEGIN_DECLS
 
+struct timezone {
+    int tz_minuteswest;
+    int tz_dsttime;
+};
+
 int gettimeofday(struct timeval* __restrict__, void* __restrict__)  __attribute__((nonnull(1)));
 
 __END_DECLS
