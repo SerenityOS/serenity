b6cf25ba27dd541798f56186657f76ac1266db8f
Andreas Kling <awesomekling@gmail.com>
2019-04-24 23:46:19 +0200

    SharedGraphics: Add is_right_text_alignment(TextAlignment) helper.
    This seems a tad bit more future-proof than manually checking all the valid
    right-side TextAlignment values in clients.

--- SharedGraphics/TextAlignment.h
+++ SharedGraphics/TextAlignment.h
@@ -1,3 +1,13 @@
 #pragma once
 
 enum class TextAlignment { TopLeft, CenterLeft, Center, CenterRight };
+
+inline bool is_right_text_alignment(TextAlignment alignment)
+{
+    switch (alignment) {
+    case TextAlignment::CenterRight:
+        return true;
+    default:
+        return false;
+    }
+}
