d3aaaa0f906c8fa8a0dc024f90fb2e4978862374
Andreas Kling <awesomekling@gmail.com>
2019-04-26 00:53:57 +0200

    Userland: Add a simple /bin/sort program.

---
+++ Userland/sort.cpp
@@ -0,0 +1,28 @@
+#include <AK/QuickSort.h>
+#include <AK/Vector.h>
+#include <AK/AKString.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+    Vector<String> lines;
+
+    for (;;) {
+        char buffer[BUFSIZ];
+        auto* str = fgets(buffer, sizeof(buffer), stdin);
+        if (!str)
+            break;
+        lines.append(buffer);
+    }
+
+    quick_sort(lines.begin(), lines.end(), [] (auto& a, auto& b) {
+        return strcmp(a.characters(), b.characters()) < 0;
+    });
+
+    for (auto& line : lines) {
+        fputs(line.characters(), stdout);
+    }
+
+    return 0;
+}
