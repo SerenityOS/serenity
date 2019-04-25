a2a5980f16e8e3f7c6faa17917fa8c3e6948d52e
Andreas Kling <awesomekling@gmail.com>
2019-04-26 00:57:37 +0200

    Userland: Add a simple /bin/tr program.

---
+++ Userland/tr.cpp
@@ -0,0 +1,28 @@
+#include <AK/QuickSort.h>
+#include <AK/Vector.h>
+#include <AK/AKString.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+    if (argc != 3) {
+        printf("usage: tr <from> <to>");
+        return 0;
+    }
+
+    char from = argv[1][0];
+    char to = argv[2][0];
+
+    for (;;) {
+        char ch = fgetc(stdin);
+        if (feof(stdin))
+            break;
+        if (ch == from)
+            putchar(to);
+        else
+            putchar(ch);
+    }
+
+    return 0;
+}
