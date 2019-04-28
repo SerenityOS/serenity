7ec1f6ab3c4ea71d51757ce689a6b4a991b121a0
Andreas Kling <awesomekling@gmail.com>
2019-04-28 14:53:50 +0200

    Kernel: Add File, a base class for anything that a FileDescriptor can wrap.
    FileDescriptor is getting out of control, and the layering isn't quite right
    so let's make a File class that everything can inherit from. Then we can
    stop worrying about all kinds of specifics in FileDescriptor.

---
+++ Kernel/File.cpp
@@ -0,0 +1,31 @@
+#include <Kernel/File.h>
+#include <Kernel/FileSystem/FileDescriptor.h>
+
+File::File()
+{
+}
+
+File::~File()
+{
+}
+
+KResultOr<Retained<FileDescriptor>> File::open(int options)
+{
+    UNUSED_PARAM(options);
+    return FileDescriptor::create(this);
+}
+
+void File::close()
+{
+}
+
+int File::ioctl(Process&, unsigned, unsigned)
+{
+    return -ENOTTY;
+}
+
+KResultOr<Region*> File::mmap(Process&, LinearAddress, size_t, size_t)
+{
+    return KResult(-ENODEV);
+}
+
