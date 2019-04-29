9c6be9b21e3da727ef41908f0377cf1547dfc9c3
Andreas Kling <awesomekling@gmail.com>
2019-04-29 19:20:34 +0200

    LibGUI: Add a GSlider widget.

---
+++ LibGUI/GSlider.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <LibGUI/GWidget.h>
+
+class GSlider : public GWidget {
+public:
+    explicit GSlider(GWidget*);
+    virtual ~GSlider() override;
+
+    int value() const { return m_value; }
+    int min() const { return m_min; }
+    int max() const { return m_max; }
+
+    void set_range(int min, int max);
+    void set_value(int);
+
+    int track_height() const { return 4; }
+    int knob_width() const { return 8; }
+    int knob_height() const { return 20; }
+
+    Rect knob_rect() const;
+    Rect inner_rect() const { return rect().shrunken(20, 0); }
+
+    Function<void(int)> on_value_changed;
+
+protected:
+    virtual void paint_event(GPaintEvent&) override;
+    virtual void mousedown_event(GMouseEvent&) override;
+    virtual void mousemove_event(GMouseEvent&) override;
+    virtual void mouseup_event(GMouseEvent&) override;
+
+private:
+    int m_value { 0 };
+    int m_min { 0 };
+    int m_max { 100 };
+
+    bool m_dragging { false };
+    int m_drag_origin_value { 0 };
+    Point m_drag_origin;
+};
