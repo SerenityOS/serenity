db2d2ae2455314e2d1607bbab6808a6579734363
Andreas Kling <awesomekling@gmail.com>
2019-05-04 16:50:36 +0200

    date: Add "-u" argument for printing current Unix timestamp.

--- Userland/date.cpp
+++ Userland/date.cpp
@@ -1,12 +1,16 @@
-#include <LibC/time.h>
-#include <LibC/stdio.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
 
 int main(int argc, char** argv)
 {
-    (void) argc;
-    (void) argv;
-
     time_t now = time(nullptr);
+
+    if (argc == 2 && !strcmp(argv[1], "-u")) {
+        printf("%u\n", now);
+        return 0;
+    }
+
     auto* tm = localtime(&now);
     printf("%4u-%02u-%02u %02u:%02u:%02u\n",
         tm->tm_year + 1900,
