ba7364b43b4de9f0b2f2f42832175b234608c01c
Andreas Kling <awesomekling@gmail.com>
2019-05-07 01:39:10 +0200

    Shell: Move line editing to a separate class.
    To be clear, there isn't really any line editing yet. But there is
    going to be, so let's have it in its own class.

---
+++ Shell/GlobalState.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <AK/AKString.h>
+#include <termios.h>
+
+struct GlobalState {
+    String cwd;
+    String username;
+    String home;
+    char ttyname[32];
+    char hostname[32];
+    pid_t sid;
+    uid_t uid;
+    struct termios termios;
+    bool was_interrupted { false };
+};
+
+extern GlobalState g;
