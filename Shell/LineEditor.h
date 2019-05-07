ad1c3c748f30deb825418e967170e6db21bd1d63
Andreas Kling <awesomekling@gmail.com>
2019-05-07 02:50:15 +0200

    Shell: Allow browsing history with up/down arrow keys.

--- Shell/LineEditor.h
+++ Shell/LineEditor.h
@@ -14,10 +14,21 @@ public:
     const Vector<String>& history() const { return m_history; }
 
 private:
+    void clear_line();
+    void append(const String&);
+
     Vector<char, 1024> m_buffer;
     int m_cursor { 0 };
 
     // FIXME: This should be something more take_first()-friendly.
     Vector<String> m_history;
+    int m_history_cursor { 0 };
     int m_history_capacity { 100 };
+
+    enum class InputState {
+        Free,
+        ExpectBracket,
+        ExpectFinal,
+    };
+    InputState m_state { InputState::Free };
 };
