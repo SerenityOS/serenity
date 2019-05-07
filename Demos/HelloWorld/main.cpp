69e4b37bfd254c1ed3f29b33a8452488cf6ab43d
Andreas Kling <awesomekling@gmail.com>
2019-05-08 01:18:36 +0200

    HelloWorld: Add a simple "Hello World!" app showing the basics.
    This also introduces a Demos/ directory where I hope to add cool things.

---
+++ Demos/HelloWorld/main.cpp
@@ -0,0 +1,37 @@
+#include <LibGUI/GApplication.h>
+#include <LibGUI/GWindow.h>
+#include <LibGUI/GWidget.h>
+#include <LibGUI/GLabel.h>
+#include <LibGUI/GButton.h>
+#include <LibGUI/GBoxLayout.h>
+
+int main(int argc, char** argv)
+{
+    GApplication app(argc, argv);
+
+    auto* window = new GWindow;
+    window->set_rect(100, 100, 240, 160);
+    window->set_title("Hello World!");
+
+    auto* main_widget = new GWidget;
+    window->set_main_widget(main_widget);
+    main_widget->set_fill_with_background_color(true);
+    main_widget->set_background_color(Color::White);
+    main_widget->set_layout(make<GBoxLayout>(Orientation::Vertical));
+    main_widget->layout()->set_margins({ 4, 4, 4, 4 });
+
+    auto* label = new GLabel(main_widget);
+    label->set_text("Hello World!");
+
+    auto* button = new GButton(main_widget);
+    button->set_caption("Good-bye");
+    button->set_size_policy(SizePolicy::Fill, SizePolicy::Fixed);
+    button->set_preferred_size({ 0, 20 });
+    button->on_click = [&] (GButton&) {
+        app.quit();
+    };
+
+    window->show();
+
+    return app.exec();
+}
