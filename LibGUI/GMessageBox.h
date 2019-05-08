5bdb87fdab7b5db66e65d43a91ccc1741abe15a4
Andreas Kling <awesomekling@gmail.com>
2019-05-08 20:13:39 +0200

    GMessageBox: Add icons to message boxes with 3 standard ones to choose from.

--- LibGUI/GMessageBox.h
+++ LibGUI/GMessageBox.h
@@ -4,11 +4,22 @@
 
 class GMessageBox : public GDialog {
 public:
-    explicit GMessageBox(const String& text, const String& title, CObject* parent = nullptr);
+    enum class Type {
+        None,
+        Information,
+        Warning,
+        Error,
+    };
+
+    explicit GMessageBox(const String& text, const String& title, Type type = Type::None, CObject* parent = nullptr);
     virtual ~GMessageBox() override;
+
+    static void show(const String& text, const String& title, Type type = Type::None, CObject* parent = nullptr);
 
 private:
     void build();
+    RetainPtr<GraphicsBitmap> icon() const;
 
     String m_text;
+    Type m_type { Type::None };
 };
