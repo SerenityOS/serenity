2be80e1d0fdf550852a687e0224d1c53ff94bc85
Andreas Kling <awesomekling@gmail.com>
2019-05-11 00:16:08 +0200

    IRCClient: Tweak appearance.

--- Applications/IRCClient/IRCAppWindow.cpp
+++ Applications/IRCClient/IRCAppWindow.cpp
@@ -133,7 +133,10 @@ void IRCAppWindow::setup_widgets()
 {
     auto* widget = new GWidget(nullptr);
     set_main_widget(widget);
+    widget->set_fill_with_background_color(true);
+    widget->set_background_color(Color::LightGray);
     widget->set_layout(make<GBoxLayout>(Orientation::Vertical));
+    widget->layout()->set_margins({ 4, 4, 4, 4 });
 
     auto* toolbar = new GToolBar(widget);
     toolbar->add_action(*m_change_nick_action);
