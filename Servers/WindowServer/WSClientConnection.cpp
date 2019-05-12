01886f63655950a130d13ed0422dd8d443271a0b
Andreas Kling <awesomekling@gmail.com>
2019-05-13 00:48:54 +0200

    WindowServer: Don't add maximize button to non-resizable windows.
    The minimize button can stay though, since it doesn't change the window
    size, just the visibility. :^)

--- Servers/WindowServer/WSClientConnection.cpp
+++ Servers/WindowServer/WSClientConnection.cpp
@@ -474,10 +474,9 @@ void WSClientConnection::handle_request(const WSAPIGetClipboardContentsRequest&)
 void WSClientConnection::handle_request(const WSAPICreateWindowRequest& request)
 {
     int window_id = m_next_window_id++;
-    auto window = make<WSWindow>(*this, request.window_type(), window_id, request.is_modal());
+    auto window = make<WSWindow>(*this, request.window_type(), window_id, request.is_modal(), request.is_resizable());
     window->set_background_color(request.background_color());
     window->set_has_alpha_channel(request.has_alpha_channel());
-    window->set_resizable(request.is_resizable());
     window->set_title(request.title());
     window->set_rect(request.rect());
     window->set_opacity(request.opacity());
