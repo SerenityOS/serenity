b7166385deb8939567735925f2e967f40452bec9
Andreas Kling <awesomekling@gmail.com>
2019-05-13 04:54:48 +0200

    RetroFetch: Add a silly neofetch-like program.
    The idea is to print out various system info suitable for screenshots. :^)

--- Kernel/makeall.sh
+++ Kernel/makeall.sh
@@ -50,6 +50,8 @@ $make_cmd -C ../Shell clean && \
 $make_cmd -C ../Shell && \
 $make_cmd -C ../Demos/HelloWorld clean && \
 $make_cmd -C ../Demos/HelloWorld && \
+$make_cmd -C ../Demos/RetroFetch clean && \
+$make_cmd -C ../Demos/RetroFetch && \
 $make_cmd clean &&\
 $make_cmd && \
 sudo ./sync.sh
