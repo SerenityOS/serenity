7b94a192b70540a70096e1460900c5f926a03ccd
Robin Burchell <robin+git@viroteck.net>
2019-05-16 09:27:42 +0200

    LibC: Implement str{n}casecmp

--- LibC/string.h
+++ LibC/string.h
@@ -8,6 +8,8 @@ __BEGIN_DECLS
 size_t strlen(const char*);
 int strcmp(const char*, const char*);
 int strncmp(const char*, const char*, size_t);
+int strcasecmp(const char*, const char*);
+int strncasecmp(const char*, const char*, size_t);
 int memcmp(const void*, const void*, size_t);
 void* memcpy(void*, const void*, size_t);
 void* memmove(void*, const void*, size_t);
