5cad215919b9083aff49f9ff86c2c63170329afb
Robin Burchell <robin+git@viroteck.net>
2019-05-17 12:46:51 +0200

    ArgsParser: Style fixup
    Remove tabs, make whitespace lines a little more in fitting with the rest of the codebase.

--- AK/ArgsParser.h
+++ AK/ArgsParser.h
@@ -14,48 +14,50 @@
 */
 
 namespace AK {
-    class ArgsParserResult {
-    public:
-	bool is_present(const String& arg_name) const;
-	String get(const String& arg_name) const;
-	const Vector<String>& get_single_values() const;
 
-    private:
-	HashMap<String, String> m_args;
-	Vector<String> m_single_values;
+class ArgsParserResult {
+public:
+    bool is_present(const String& arg_name) const;
+    String get(const String& arg_name) const;
+    const Vector<String>& get_single_values() const;
 
-	friend class ArgsParser;
+private:
+    HashMap<String, String> m_args;
+    Vector<String> m_single_values;
+
+    friend class ArgsParser;
+};
+
+class ArgsParser {
+public:
+    ArgsParser(const String& program_name, const String& prefix);
+
+    ArgsParserResult parse(const int argc, const char** argv);
+
+    void add_arg(const String& name, const String& description, bool required);
+    void add_arg(const String& name, const String& value_name, const String& description, bool required);
+    String get_usage() const;
+    void print_usage() const;
+
+private:
+    struct Arg {
+        inline Arg() {}
+        Arg(const String& name, const String& description, bool required);
+        Arg(const String& name, const String& value_name, const String& description, bool required);
+
+        String name;
+        String description;
+        String value_name;
+        bool required;
     };
 
-    class ArgsParser {
-    public:
-	ArgsParser(const String& program_name, const String& prefix);
+    int parse_next_param(const int index, const char** argv, const int params_left, ArgsParserResult& res);
+    bool is_param_valid(const String& param_name);
+    bool check_required_args(const ArgsParserResult& res);
 
-	ArgsParserResult parse(const int argc, const char** argv);
+    String m_program_name;
+    String m_prefix;
+    HashMap<String, Arg> m_args;
+};
 
-	void add_arg(const String& name, const String& description, bool required);
-	void add_arg(const String& name, const String& value_name, const String& description, bool required);
-	String get_usage() const;
-	void print_usage() const;
-
-    private:
-	struct Arg {
-	    inline Arg() {}
-	    Arg(const String& name, const String& description, bool required);
-	    Arg(const String& name, const String& value_name, const String& description, bool required);
-
-	    String name;
-	    String description;
-	    String value_name;
-	    bool required;
-	};
-
-	int parse_next_param(const int index, const char** argv, const int params_left, ArgsParserResult& res);
-	bool is_param_valid(const String& param_name);
-	bool check_required_args(const ArgsParserResult& res);
-
-	String m_program_name;
-	String m_prefix;
-	HashMap<String, Arg> m_args;
-    };
 }
