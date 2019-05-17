4f4143c45fc131cfd3ead52c9133585850e6fd3c
Robin Burchell <robin+git@viroteck.net>
2019-05-17 20:40:38 +0200

    Build: Install most headers to Root (and libcore.a/libgui.a)
    This makes out-of-tree linking possible. And at the same time, add a
    CMakeToolchain.txt file that can be used to build arbitrary cmake-using
    applications on Serenity by pointing to the CMAKE_TOOLCHAIN_FILE when
    running cmake:
    
        -DCMAKE_TOOLCHAIN_FILE=~/code/serenity/Toolchain/CMakeToolchain.txt

--- Kernel/makeall.sh
+++ Kernel/makeall.sh
@@ -33,6 +33,7 @@ build_targets="$build_targets ../Games/Snake"
 build_targets="$build_targets ../Shell"
 build_targets="$build_targets ../Demos/HelloWorld"
 build_targets="$build_targets ../Demos/RetroFetch"
+build_targets="$build_targets ." # the kernel
 
 for targ in $build_targets; do
     echo "Building $targ"
@@ -45,7 +46,9 @@ for targ in $build_targets; do
     fi
 done
 
-$make_cmd clean
-$make_cmd
+# has no need to build separately, but install headers.
+(cd ../SharedGraphics && ./install.sh)
+(cd ../AK && ./install.sh)
+
 sudo ./sync.sh
 
