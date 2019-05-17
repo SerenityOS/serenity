69578254449769c2c429827c0e1eea787dd35ebb
Andreas Kling <awesomekling@gmail.com>
2019-05-17 04:39:22 +0200

    Kernel: Factor out range allocation from Process::allocate_region*().
    These functions were doing exactly the same thing for range allocation, so
    share that code in an allocate_range() helper.
    
    Region allocation will now also fail if range allocation fails, which means
    that mmap() can actually fail without falling apart. Exciting times!

--- Kernel/FileSystem/FileDescriptor.cpp
+++ Kernel/FileSystem/FileDescriptor.cpp
@@ -346,6 +346,8 @@ KResultOr<Region*> FileDescriptor::mmap(Process& process, LinearAddress laddr, s
     // FIXME: Implement mapping at a client-specified address. Most of the support is already in plcae.
     ASSERT(laddr.as_ptr() == nullptr);
     auto* region = process.allocate_file_backed_region(LinearAddress(), size, inode(), move(region_name), prot & PROT_READ, prot & PROT_WRITE);
+    if (!region)
+        return KResult(-ENOMEM);
     region->page_in();
     return region;
 }
