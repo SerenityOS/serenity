ddb8d03042d177b008b535c39eccc47b5b9166d9
Andreas Kling <awesomekling@gmail.com>
2019-05-20 02:06:41 +0200

    Toolchain: Oops, let's unbreak UseIt.sh.

--- Toolchain/UseIt.sh
+++ Toolchain/UseIt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DIR=$( dirname $( readlink -e "$0" ) )
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PATH="$DIR/Local/bin:$PATH"
 export TOOLCHAIN="$DIR"
 export SERENITY_ROOT="$DIR/.."
