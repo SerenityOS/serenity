656290301eaaf709aaf9a9db82e65a7aa6f92a81
Andreas Kling <awesomekling@gmail.com>
2019-05-23 17:02:58 +0200

    Kernel: Return ENOSYS if an invalid syscall number is requested.

--- Kernel/Syscall.cpp
+++ Kernel/Syscall.cpp
@@ -278,7 +278,7 @@ static dword handle(RegisterDump& regs, dword function, dword arg1, dword arg2, 
         return current->process().sys$getpeername((int)arg1, (sockaddr*)arg2, (socklen_t*)arg3);
     default:
         kprintf("<%u> int0x82: Unknown function %u requested {%x, %x, %x}\n", current->process().pid(), function, arg1, arg2, arg3);
-        break;
+        return -ENOSYS;
     }
     return 0;
 }
