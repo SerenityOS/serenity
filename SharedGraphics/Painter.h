e8e27e915b7482b0bac0a96de72af4d7a9f801eb
Christopher Dumas <christopherdumas@gmail.com>
2019-05-27 08:36:44 -0700

    removed extra impl of scaling

--- SharedGraphics/Painter.h
+++ SharedGraphics/Painter.h
@@ -29,6 +29,7 @@ public:
     void blit_dimmed(const Point&, const GraphicsBitmap&, const Rect& src_rect);
     void blit_tiled(const Point&, const GraphicsBitmap&, const Rect& src_rect);
     void blit_offset(const Point&, const GraphicsBitmap&, const Rect& src_rect, const Point&);
+    void blit_scaled(const Point&, const GraphicsBitmap&, const Rect& src_rect, const Size&);
     void draw_text(const Rect&, const char* text, int length, const Font&, TextAlignment = TextAlignment::TopLeft, Color = Color::Black, TextElision = TextElision::None);
     void draw_text(const Rect&, const char* text, int length, TextAlignment = TextAlignment::TopLeft, Color = Color::Black, TextElision = TextElision::None);
     void draw_text(const Rect&, const String&, const Font&, TextAlignment = TextAlignment::TopLeft, Color = Color::Black, TextElision = TextElision::None);
