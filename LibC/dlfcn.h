db966e72014236d73f17eac8dbd485c6a7fbb490
Andreas Kling <awesomekling@gmail.com>
2019-05-28 14:30:55 +0200

    LibC: Make dlfcn stubs extern "C" like the outside world expects.

--- LibC/dlfcn.h
+++ LibC/dlfcn.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
 #define RTLD_LAZY 1
 #define RTLD_NOW 2
 #define RTLD_GLOBAL 3
@@ -9,3 +13,5 @@ int dlclose(void*);
 char *dlerror();
 void *dlopen(const char*, int);
 void *dlsym(void*, const char*);
+
+__END_DECLS
