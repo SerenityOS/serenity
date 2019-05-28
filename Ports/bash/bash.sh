9a284ad3f75629dc3d5b5a026a9a2b3c76704bca
Robin Burchell <robin+git@viroteck.net>
2019-05-28 02:58:36 +0200

    Move everything to a subdirectory
    Also don't run cd for the initial git clone (DERP!), and other bash port fixes.

--- Ports/bash/bash.sh
+++ Ports/bash/bash.sh
@@ -6,25 +6,25 @@ function fetch() {
     run_fetch_git "https://git.savannah.gnu.org/git/bash.git"
 
     # Add serenity as a system for configure
-    run_patch .bash-serenity-system.patch -p1
+    run_patch configure-system.patch -p1
 
     # For some reason, the build fails due to FILE* being undefined without this.
     # This is probably a LibC bug, but work around it for now.
-    run_patch .bash-serenity-include-stdio.patch -p1
+    run_patch include-stdio.patch -p1
 
     # Locale calls crash right now. LibC bug, probably.
-    run_patch .bash-serenity-disable-locale.patch -p1
+    run_patch disable-locale.patch -p1
 }
 function configure() {
     run_configure_autotools --disable-nls --without-bash-malloc
 }
 function build() {
     # Avoid some broken cross compile tests...
-    run_command perl -p -i -e "s/GETCWD_BROKEN 1/GETCWD_BROKEN 0/" config.h
-    run_command perl -p -i -e "s/CAN_REDEFINE_GETENV 1/CAN_REDEFINE_GETENV 0/" config.h
+    run_command perl -p -i -e "s/define GETCWD_BROKEN 1/undef GETCWD_BROKEN/" config.h
+    run_command perl -p -i -e "s/define CAN_REDEFINE_GETENV 1/undef CAN_REDEFINE_GETENV/" config.h
     run_make
 }
 function install() {
-    run_make_install
+    run_make_install DESTDIR="$SERENITY_ROOT"/Root
 }
-source ./.port_include.sh
+source ../.port_include.sh
