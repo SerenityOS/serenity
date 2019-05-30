bdfa081f0cca85644155ee383d664cb7c6613b6d
Andreas Kling <awesomekling@gmail.com>
2019-05-30 02:37:40 +0200

    LibC: Add malloc_size() to tell you how big an allocation might be.
    It can't be 100% precise but it doesn't really matter. Use this to implement
    realloc() nicely. This also fixes a bug in realloc() where we didn't take
    the size of the allocation metadata into account when computing the size of
    an allocation backed by a BigAllocationBlock.

--- LibC/stdlib.h
+++ LibC/stdlib.h
@@ -12,6 +12,7 @@ __BEGIN_DECLS
 
 __attribute__((malloc)) __attribute__((alloc_size(1))) void* malloc(size_t);
 __attribute__((malloc)) __attribute__((alloc_size(1, 2))) void* calloc(size_t nmemb, size_t);
+size_t malloc_size(void*);
 void free(void*);
 void* realloc(void* ptr, size_t);
 char* getenv(const char* name);
