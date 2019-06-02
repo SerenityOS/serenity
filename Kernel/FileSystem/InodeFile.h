5e1c7cb32c98dfb568dcd7d935a222bda9c42fda
Andreas Kling <awesomekling@gmail.com>
2019-06-02 10:14:28 +0200

    Kernel: Memory-mapped files now have the absolute path as their name.
    It's generated when the mapping is first created, so it won't update if
    the file moves. Maybe that's something we should support, too.

--- Kernel/FileSystem/InodeFile.h
+++ Kernel/FileSystem/InodeFile.h
@@ -21,7 +21,7 @@ public:
 
     virtual ssize_t read(FileDescriptor&, byte*, ssize_t) override;
     virtual ssize_t write(FileDescriptor&, const byte*, ssize_t) override;
-    virtual KResultOr<Region*> mmap(Process&, LinearAddress preferred_laddr, size_t offset, size_t size, int prot) override;
+    virtual KResultOr<Region*> mmap(Process&, FileDescriptor&, LinearAddress preferred_laddr, size_t offset, size_t size, int prot) override;
 
     virtual String absolute_path(const FileDescriptor&) const override;
 
