f23b6481ef0dae5cf6eae7e0c56eb10723c89df9
Conrad Pankoff <deoxxa@fknsrs.biz>
2019-06-02 19:29:17 +1000

    Kernel: Rename offset parameter of OffsetDiskDevice to block_offset

--- Kernel/Devices/OffsetDiskDevice.cpp
+++ Kernel/Devices/OffsetDiskDevice.cpp
@@ -2,13 +2,13 @@
 
 // #define OFFD_DEBUG
 
-Retained<OffsetDiskDevice> OffsetDiskDevice::create(Retained<DiskDevice>&& device, unsigned offset)
+Retained<OffsetDiskDevice> OffsetDiskDevice::create(Retained<DiskDevice>&& device, unsigned block_offset)
 {
-    return adopt(*new OffsetDiskDevice(move(device), offset));
+    return adopt(*new OffsetDiskDevice(move(device), block_offset));
 }
 
-OffsetDiskDevice::OffsetDiskDevice(Retained<DiskDevice>&& device, unsigned offset)
-    : m_device(move(device)), m_offset(offset)
+OffsetDiskDevice::OffsetDiskDevice(Retained<DiskDevice>&& device, unsigned block_offset)
+    : m_device(move(device)), m_block_offset(block_offset)
 {
 }
 
@@ -24,37 +24,37 @@ unsigned OffsetDiskDevice::block_size() const
 bool OffsetDiskDevice::read_block(unsigned index, byte* out) const
 {
 #ifdef OFFD_DEBUG
-    kprintf("OffsetDiskDevice::read_block %u (really: %u)\n", index, m_offset + index);
+    kprintf("OffsetDiskDevice::read_block %u (really: %u)\n", index, m_block_offset + index);
 #endif
 
-    return m_device->read_block(m_offset + index, out);
+    return m_device->read_block(m_block_offset + index, out);
 }
 
 bool OffsetDiskDevice::write_block(unsigned index, const byte* data)
 {
 #ifdef OFFD_DEBUG
-    kprintf("OffsetDiskDevice::write_block %u (really: %u)\n", index, m_offset + index);
+    kprintf("OffsetDiskDevice::write_block %u (really: %u)\n", index, m_block_offset + index);
 #endif
 
-    return m_device->write_block(m_offset + index, data);
+    return m_device->write_block(m_block_offset + index, data);
 }
 
 bool OffsetDiskDevice::read_blocks(unsigned index, word count, byte* out)
 {
 #ifdef OFFD_DEBUG
-    kprintf("OffsetDiskDevice::read_blocks %u (really: %u) count=%u\n", index, m_offset + index, count);
+    kprintf("OffsetDiskDevice::read_blocks %u (really: %u) count=%u\n", index, m_block_offset + index, count);
 #endif
 
-    return m_device->read_blocks(m_offset + index, count, out);
+    return m_device->read_blocks(m_block_offset + index, count, out);
 }
 
 bool OffsetDiskDevice::write_blocks(unsigned index, word count, const byte* data)
 {
 #ifdef OFFD_DEBUG
-    kprintf("OffsetDiskDevice::write_blocks %u (really: %u) count=%u\n", index, m_offset + index, count);
+    kprintf("OffsetDiskDevice::write_blocks %u (really: %u) count=%u\n", index, m_block_offset + index, count);
 #endif
 
-    return m_device->write_blocks(m_offset + index, count, data);
+    return m_device->write_blocks(m_block_offset + index, count, data);
 }
 
 const char* OffsetDiskDevice::class_name() const
