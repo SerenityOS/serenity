fba2c971e79f2728b2605b8d0cc84f8303343a8c
Andreas Kling <awesomekling@gmail.com>
2019-06-07 11:48:15 +0200

    Games: Run clang-format on everything.

--- Games/Snake/main.cpp
+++ Games/Snake/main.cpp
@@ -1,11 +1,11 @@
 #include "SnakeGame.h"
+#include <LibGUI/GAction.h>
 #include <LibGUI/GApplication.h>
-#include <LibGUI/GWindow.h>
 #include <LibGUI/GBoxLayout.h>
 #include <LibGUI/GButton.h>
 #include <LibGUI/GMenu.h>
 #include <LibGUI/GMenuBar.h>
-#include <LibGUI/GAction.h>
+#include <LibGUI/GWindow.h>
 
 int main(int argc, char** argv)
 {
@@ -23,20 +23,20 @@ int main(int argc, char** argv)
     auto menubar = make<GMenuBar>();
 
     auto app_menu = make<GMenu>("Snake");
-    app_menu->add_action(GAction::create("Quit", { Mod_Alt, Key_F4 }, [] (const GAction&) {
+    app_menu->add_action(GAction::create("Quit", { Mod_Alt, Key_F4 }, [](const GAction&) {
         GApplication::the().quit(0);
         return;
     }));
     menubar->add_menu(move(app_menu));
 
     auto game_menu = make<GMenu>("Game");
-    game_menu->add_action(GAction::create("New game", { Mod_None, Key_F2 }, [&] (const GAction&) {
+    game_menu->add_action(GAction::create("New game", { Mod_None, Key_F2 }, [&](const GAction&) {
         game->reset();
     }));
     menubar->add_menu(move(game_menu));
 
     auto help_menu = make<GMenu>("Help");
-    help_menu->add_action(GAction::create("About", [] (const GAction&) {
+    help_menu->add_action(GAction::create("About", [](const GAction&) {
         dbgprintf("FIXME: Implement Help/About\n");
     }));
     menubar->add_menu(move(help_menu));
