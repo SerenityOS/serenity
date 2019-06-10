ea9492e1bc759847cdb3e821d5f69c37128ba958
Andreas Kling <awesomekling@gmail.com>
2019-06-10 20:07:26 +0200

    PaintBrush: Make it possible to switch colors using the PaletteWidget.

--- Applications/PaintBrush/PaletteWidget.h
+++ Applications/PaintBrush/PaletteWidget.h
@@ -10,4 +10,12 @@ public:
     virtual ~PaletteWidget() override;
 
     virtual const char* class_name() const override { return "PaletteWidget"; }
+
+    void set_primary_color(Color);
+    void set_secondary_color(Color);
+
+private:
+    PaintableWidget& m_paintable_widget;
+    GWidget* m_primary_color_widget { nullptr };
+    GWidget* m_secondary_color_widget { nullptr };
 };
