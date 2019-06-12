eb2fd855048d0e46d3e597bfee7b410897a1da79
Andreas Kling <awesomekling@gmail.com>
2019-06-11 23:47:51 -0700

    AK: Delete the Badge copy and move constructors.

--- AK/Badge.h
+++ AK/Badge.h
@@ -1,7 +1,20 @@
 #pragma once
 
+namespace AK {
+
 template<typename T>
 class Badge {
     friend T;
     Badge() {}
+
+    Badge(const Badge&) = delete;
+    Badge& operator=(const Badge&) = delete;
+
+    Badge(Badge&&) = delete;
+    Badge& operator=(Badge&&) = delete;
 };
+
+}
+
+using AK::Badge;
+
