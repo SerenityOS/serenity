2f0c016e814026cd54b591aad0ab0c1967a45f05
Andreas Kling <awesomekling@gmail.com>
2019-06-14 17:52:51 +0200

    AK/Tests: Add a simple EXPECT_EQ macro and use it for the String test.

--- AK/Tests/TestString.cpp
+++ AK/Tests/TestString.cpp
@@ -16,8 +16,8 @@ int main()
     String test_string = "ABCDEF";
     EXPECT(!test_string.is_empty());
     EXPECT(!test_string.is_null());
-    EXPECT(test_string.length() == 6);
-    EXPECT(test_string.length() == (int)strlen(test_string.characters()));
+    EXPECT_EQ(test_string.length(), 6);
+    EXPECT_EQ(test_string.length(), (int)strlen(test_string.characters()));
     EXPECT(test_string.characters());
     EXPECT(!strcmp(test_string.characters(), "ABCDEF"));
 
@@ -25,8 +25,8 @@ int main()
     EXPECT(test_string != "ABCDE");
     EXPECT(test_string != "ABCDEFG");
 
-    EXPECT(test_string[0] == 'A');
-    EXPECT(test_string[1] == 'B');
+    EXPECT_EQ(test_string[0], 'A');
+    EXPECT_EQ(test_string[1], 'B');
 
     EXPECT(test_string.starts_with("AB"));
     EXPECT(test_string.starts_with("ABCDEF"));
@@ -37,16 +37,16 @@ int main()
     EXPECT(!test_string.ends_with("ABC"));
 
     auto test_string_copy = test_string;
-    EXPECT(test_string == test_string_copy);
-    EXPECT(test_string.characters() == test_string_copy.characters());
+    EXPECT_EQ(test_string, test_string_copy);
+    EXPECT_EQ(test_string.characters(), test_string_copy.characters());
 
     auto test_string_move = move(test_string_copy);
-    EXPECT(test_string == test_string_move);
+    EXPECT_EQ(test_string, test_string_move);
     EXPECT(test_string_copy.is_null());
 
-    EXPECT(String::repeated('x', 0) == "");
-    EXPECT(String::repeated('x', 1) == "x");
-    EXPECT(String::repeated('x', 2) == "xx");
+    EXPECT_EQ(String::repeated('x', 0), "");
+    EXPECT_EQ(String::repeated('x', 1), "x");
+    EXPECT_EQ(String::repeated('x', 2), "xx");
 
     bool ok;
     EXPECT(String("123").to_int(ok) == 123 && ok);
