af75ccee658425e59187d802d1fc4d43d542f009
Andreas Kling <awesomekling@gmail.com>
2019-06-22 22:46:05 +0200

    CNetworkJob: Automatically delete finished jobs after on_finish.
    Also, pretty-print any failure messages.

--- LibCore/CNetworkJob.cpp
+++ LibCore/CNetworkJob.cpp
@@ -16,12 +16,28 @@ void CNetworkJob::did_finish(NonnullRefPtr<CNetworkResponse>&& response)
     printf("%s{%p} job did_finish!\n", class_name(), this);
     ASSERT(on_finish);
     on_finish(true);
+    delete_later();
 }
 
 void CNetworkJob::did_fail(Error error)
 {
     m_error = error;
-    dbgprintf("%s{%p} job did_fail! error=%u\n", class_name(), this, (unsigned)error);
+    dbgprintf("%s{%p} job did_fail! error: %u (%s)\n", class_name(), this, (unsigned)error, to_string(error));
     ASSERT(on_finish);
     on_finish(false);
+    delete_later();
+}
+
+const char* to_string(CNetworkJob::Error error)
+{
+    switch (error) {
+    case CNetworkJob::Error::ProtocolFailed:
+        return "ProtocolFailed";
+    case CNetworkJob::Error::ConnectionFailed:
+        return "ConnectionFailed";
+    case CNetworkJob::Error::TransmissionFailed:
+        return "TransmissionFailed";
+    default:
+        return "(Unknown error)";
+    }
 }
