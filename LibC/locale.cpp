6f869f0a718a3465e367535b180a2793c8759d47
Andreas Kling <awesomekling@gmail.com>
2019-06-22 14:32:59 +0200

    LibC: Fix locale.cpp warnings.

--- LibC/locale.cpp
+++ LibC/locale.cpp
@@ -4,10 +4,14 @@
 
 extern "C" {
 
+static char default_decimal_point[] = ".";
+static char default_thousands_sep[] = ",";
+static char default_grouping[] = "\x03\x03";
+
 static struct lconv default_locale = {
-    ".",
-    ",",
-    "\x03\x03",
+    default_decimal_point,
+    default_thousands_sep,
+    default_grouping,
 };
 
 char* setlocale(int category, const char* locale)
