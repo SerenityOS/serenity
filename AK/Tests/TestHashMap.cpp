e5b985ea9a09a2e4c5fe06f67abc7c73978d2827
Andreas Kling <awesomekling@gmail.com>
2019-06-27 15:57:49 +0200

    AK: Consolidate iterators for HashTable and DoublyLinkedList respectively.
    Get rid of the ConstIterator classes for these containers and use templated
    FooIterator<T, ...> and FooIterator<const T, ...> helpers.
    
    This makes the HashTable class a lot easier to read.

---
+++ AK/Tests/TestHashMap.cpp
@@ -0,0 +1,28 @@
+#include "TestHelpers.h"
+#include <AK/AKString.h>
+#include <AK/HashMap.h>
+
+typedef HashMap<int, int> IntIntMap;
+
+int main()
+{
+    EXPECT(IntIntMap().is_empty());
+    EXPECT(IntIntMap().size() == 0);
+
+    HashMap<int, String> number_to_string;
+    number_to_string.set(1, "One");
+    number_to_string.set(2, "Two");
+    number_to_string.set(3, "Three");
+    
+    EXPECT_EQ(number_to_string.is_empty(), false);
+    EXPECT_EQ(number_to_string.size(), 3);
+
+    int loop_counter = 0;
+    for (auto& it : number_to_string) {
+        EXPECT(!it.value.is_null());
+        ++loop_counter;
+    }
+
+    EXPECT_EQ(loop_counter, 3);
+    return 0;
+}
