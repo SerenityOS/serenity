d5b88c2df6e6f97f8be1878d9c7bc43c00c39962
Andreas Kling <awesomekling@gmail.com>
2019-06-27 14:19:50 +0200

    AK: Simplify HashMap a bit.

--- AK/HashMap.h
+++ AK/HashMap.h
@@ -14,7 +14,7 @@ private:
         K key;
         V value;
 
-        bool operator==(const Entry& other)
+        bool operator==(const Entry& other) const
         {
             return key == other.key;
         }
@@ -39,9 +39,9 @@ public:
     int capacity() const { return m_table.capacity(); }
     void clear() { m_table.clear(); }
 
-    void set(const K&, const V&);
-    void set(const K&, V&&);
-    void remove(const K&);
+    void set(const K& key, const V& value) { m_table.set({ key, value }); }
+    void set(const K& key, V&& value) { m_table.set({ key, move(value) }); }
+    void remove(const K& key) { m_table.remove({ key, V() }); }
     void remove_one_randomly() { m_table.remove(m_table.begin()); }
 
     typedef HashTable<Entry, EntryTraits> HashTableType;
@@ -50,11 +50,11 @@ public:
 
     IteratorType begin() { return m_table.begin(); }
     IteratorType end() { return m_table.end(); }
-    IteratorType find(const K&);
+    IteratorType find(const K& key) { return m_table.find({ key, V() }); }
 
     ConstIteratorType begin() const { return m_table.begin(); }
     ConstIteratorType end() const { return m_table.end(); }
-    ConstIteratorType find(const K&) const;
+    ConstIteratorType find(const K& key) const { return m_table.find({ key, V() }); }
 
     void ensure_capacity(int capacity) { m_table.ensure_capacity(capacity); }
 
@@ -96,41 +96,8 @@ public:
     }
 
 private:
-    HashTable<Entry, EntryTraits> m_table;
+    HashTableType m_table;
 };
-
-template<typename K, typename V>
-void HashMap<K, V>::set(const K& key, V&& value)
-{
-    m_table.set(Entry { key, move(value) });
-}
-
-template<typename K, typename V>
-void HashMap<K, V>::set(const K& key, const V& value)
-{
-    m_table.set(Entry { key, value });
-}
-
-template<typename K, typename V>
-void HashMap<K, V>::remove(const K& key)
-{
-    Entry dummy { key, V() };
-    m_table.remove(dummy);
-}
-
-template<typename K, typename V>
-auto HashMap<K, V>::find(const K& key) -> IteratorType
-{
-    Entry dummy { key, V() };
-    return m_table.find(dummy);
-}
-
-template<typename K, typename V>
-auto HashMap<K, V>::find(const K& key) const -> ConstIteratorType
-{
-    Entry dummy { key, V() };
-    return m_table.find(dummy);
-}
 
 }
 
