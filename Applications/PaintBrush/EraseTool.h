d797664feb026542d775f144fd0613e69d3bfcbe
Robin Burchell <robin+git@viroteck.net>
2019-06-28 11:11:06 +0200

    PaintBrush: Add an erase tool

---
+++ Applications/PaintBrush/EraseTool.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Tool.h"
+#include <SharedGraphics/Point.h>
+
+class GMenu;
+
+class EraseTool final : public Tool {
+public:
+    EraseTool();
+    virtual ~EraseTool() override;
+
+    virtual void on_mousedown(GMouseEvent&) override;
+    virtual void on_mousemove(GMouseEvent&) override;
+    virtual void on_contextmenu(GContextMenuEvent&) override;
+
+private:
+    virtual const char* class_name() const override { return "EraseTool"; }
+    Rect build_rect(const Point& pos, const Rect& widget_rect);
+    OwnPtr<GMenu> m_context_menu;
+    int m_thickness { 1 };
+};
