9a7dc065676c5d60707c263f7c6b0f97fba38ad6
Andreas Kling <awesomekling@gmail.com>
2019-06-29 17:32:32 +0200

    LibHTML: Add Selector::specificity(), which returns a Specificity object.

--- LibHTML/CSS/Selector.cpp
+++ LibHTML/CSS/Selector.cpp
@@ -8,3 +8,28 @@ Selector::Selector(Vector<Component>&& components)
 Selector::~Selector()
 {
 }
+
+Specificity Selector::specificity() const
+{
+    unsigned ids = 0;
+    unsigned tag_names = 0;
+    unsigned classes = 0;
+
+    for (auto& component : m_components) {
+        switch (component.type) {
+        case Component::Type::Id:
+            ++ids;
+            break;
+        case Component::Type::Class:
+            ++classes;
+            break;
+        case Component::Type::TagName:
+            ++tag_names;
+            break;
+        default:
+            break;
+        }
+    }
+
+    return { ids, classes, tag_names };
+}
