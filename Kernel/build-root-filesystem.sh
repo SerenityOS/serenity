9a546e395e22246a8bf80b391534d6c523ddbc43
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2019-06-30 12:28:48 +0200

    Application: one makefile to rule them all

--- Kernel/build-root-filesystem.sh
+++ Kernel/build-root-filesystem.sh
@@ -11,96 +11,90 @@ if [ $(id -u) != 0 ]; then
     die "this script needs to run as root"
 fi
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export SYSROOT=$DIR/mnt
+
 echo -n "creating initial filesystem structure... "
-mkdir -p mnt/{bin,etc,proc,tmp}
-chmod 1777 mnt/tmp
+mkdir -p $SYSROOT/{bin,etc,proc,tmp}
+chmod 1777 $SYSROOT/tmp
 echo "done"
 
 echo -n "setting up device nodes... "
-mkdir -p mnt/dev
-mkdir -p mnt/dev/pts
-mknod -m 666 mnt/dev/bxvga b 82 413
-mknod mnt/dev/tty0 c 4 0
-mknod mnt/dev/tty1 c 4 1
-mknod mnt/dev/tty2 c 4 2
-mknod mnt/dev/tty3 c 4 3
-mknod mnt/dev/ttyS0 c 4 64
-mknod mnt/dev/ttyS1 c 4 65
-mknod mnt/dev/ttyS2 c 4 66
-mknod mnt/dev/ttyS3 c 4 67
-mknod -m 666 mnt/dev/random c 1 8
-mknod -m 666 mnt/dev/null c 1 3
-mknod -m 666 mnt/dev/zero c 1 5
-mknod -m 666 mnt/dev/full c 1 7
-mknod -m 666 mnt/dev/debuglog c 1 18
-mknod mnt/dev/keyboard c 85 1
-mknod mnt/dev/psaux c 10 1
-mknod -m 666 mnt/dev/ptmx c 5 2
-ln -s /proc/self/fd/0 mnt/dev/stdin
-ln -s /proc/self/fd/1 mnt/dev/stdout
-ln -s /proc/self/fd/2 mnt/dev/stderr
+mkdir -p $SYSROOT/dev
+mkdir -p $SYSROOT/dev/pts
+mknod -m 666 $SYSROOT/dev/bxvga b 82 413
+mknod $SYSROOT/dev/tty0 c 4 0
+mknod $SYSROOT/dev/tty1 c 4 1
+mknod $SYSROOT/dev/tty2 c 4 2
+mknod $SYSROOT/dev/tty3 c 4 3
+mknod $SYSROOT/dev/ttyS0 c 4 64
+mknod $SYSROOT/dev/ttyS1 c 4 65
+mknod $SYSROOT/dev/ttyS2 c 4 66
+mknod $SYSROOT/dev/ttyS3 c 4 67
+mknod -m 666 $SYSROOT/dev/random c 1 8
+mknod -m 666 $SYSROOT/dev/null c 1 3
+mknod -m 666 $SYSROOT/dev/zero c 1 5
+mknod -m 666 $SYSROOT/dev/full c 1 7
+mknod -m 666 $SYSROOT/dev/debuglog c 1 18
+mknod $SYSROOT/dev/keyboard c 85 1
+mknod $SYSROOT/dev/psaux c 10 1
+mknod -m 666 $SYSROOT/dev/ptmx c 5 2
+ln -s /proc/self/fd/0 $SYSROOT/dev/stdin
+ln -s /proc/self/fd/1 $SYSROOT/dev/stdout
+ln -s /proc/self/fd/2 $SYSROOT/dev/stderr
 echo "done"
 
 echo -n "installing base system... "
-cp -R ../Base/* mnt/
-cp -R ../Root/* mnt/
-cp kernel.map mnt/
+cp -R ../Base/* $SYSROOT/
+cp -R ../Root/* $SYSROOT/
+cp kernel.map $SYSROOT/
 echo "done"
 
 echo -n "installing users... "
-mkdir -p mnt/home/anon
-mkdir -p mnt/home/nona
-cp ../ReadMe.md mnt/home/anon/
-chown -R 100:100 mnt/home/anon
-chown -R 200:200 mnt/home/nona
+mkdir -p $SYSROOT/home/anon
+mkdir -p $SYSROOT/home/nona
+cp ../ReadMe.md $SYSROOT/home/anon/
+chown -R 100:100 $SYSROOT/home/anon
+chown -R 200:200 $SYSROOT/home/nona
 echo "done"
 
 echo -n "installing userland... "
-find ../Userland/ -type f -executable -exec cp {} mnt/bin/ \;
-chmod 4755 mnt/bin/su
+find ../Userland/ -type f -executable -exec cp {} $SYSROOT/bin/ \;
+chmod 4755 $SYSROOT/bin/su
 echo "done"
 
 echo -n "installing applications... "
-cp ../Applications/About/About mnt/bin/About
-cp ../Applications/Downloader/Downloader mnt/bin/Downloader
-cp ../Applications/FileManager/FileManager mnt/bin/FileManager
-cp ../Applications/FontEditor/FontEditor mnt/bin/FontEditor
-cp ../Applications/IRCClient/IRCClient mnt/bin/IRCClient
-cp ../Applications/Launcher/Launcher mnt/bin/Launcher
-cp ../Applications/ProcessManager/ProcessManager mnt/bin/ProcessManager
-cp ../Applications/Taskbar/Taskbar mnt/bin/Taskbar
-cp ../Applications/Terminal/Terminal mnt/bin/Terminal
-cp ../Applications/TextEditor/TextEditor mnt/bin/TextEditor
-cp ../Applications/PaintBrush/PaintBrush mnt/bin/PaintBrush
-cp ../Applications/QuickShow/QuickShow mnt/bin/QuickShow
-cp ../Demos/HelloWorld/HelloWorld mnt/bin/HelloWorld
-cp ../Demos/RetroFetch/RetroFetch mnt/bin/RetroFetch
-cp ../Demos/WidgetGallery/WidgetGallery mnt/bin/WidgetGallery
-cp ../Demos/Fire/Fire mnt/bin/Fire
-cp ../DevTools/VisualBuilder/VisualBuilder mnt/bin/VisualBuilder
-cp ../Games/Minesweeper/Minesweeper mnt/bin/Minesweeper
-cp ../Games/Snake/Snake mnt/bin/Snake
-cp ../Servers/LookupServer/LookupServer mnt/bin/LookupServer
-cp ../Servers/SystemServer/SystemServer mnt/bin/SystemServer
-cp ../Servers/WindowServer/WindowServer mnt/bin/WindowServer
-cp ../Shell/Shell mnt/bin/Shell
-cp ../LibHTML/tho mnt/bin/tho
+
+make -C ../Applications install
+
+cp ../Demos/HelloWorld/HelloWorld $SYSROOT/bin/HelloWorld
+cp ../Demos/RetroFetch/RetroFetch $SYSROOT/bin/RetroFetch
+cp ../Demos/WidgetGallery/WidgetGallery $SYSROOT/bin/WidgetGallery
+cp ../Demos/Fire/Fire $SYSROOT/bin/Fire
+cp ../DevTools/VisualBuilder/VisualBuilder $SYSROOT/bin/VisualBuilder
+cp ../Games/Minesweeper/Minesweeper $SYSROOT/bin/Minesweeper
+cp ../Games/Snake/Snake $SYSROOT/bin/Snake
+cp ../Servers/LookupServer/LookupServer $SYSROOT/bin/LookupServer
+cp ../Servers/SystemServer/SystemServer $SYSROOT/bin/SystemServer
+cp ../Servers/WindowServer/WindowServer $SYSROOT/bin/WindowServer
+cp ../Shell/Shell $SYSROOT/bin/Shell
+cp ../LibHTML/tho $SYSROOT/bin/tho
 echo "done"
 
 echo -n "installing shortcuts... "
-ln -s Downloader mnt/bin/dl
-ln -s FileManager mnt/bin/fm
-ln -s HelloWorld mnt/bin/hw
-ln -s IRCClient mnt/bin/irc
-ln -s Minesweeper mnt/bin/ms
-ln -s Shell mnt/bin/sh
-ln -s Snake mnt/bin/sn
-ln -s Taskbar mnt/bin/tb
-ln -s VisualBuilder mnt/bin/vb
-ln -s WidgetGallery mnt/bin/wg
-ln -s TextEditor mnt/bin/te
-ln -s PaintBrush mnt/bin/pb
-ln -s QuickShow mnt/bin/qs
+ln -s Downloader $SYSROOT/bin/dl
+ln -s FileManager $SYSROOT/bin/fm
+ln -s HelloWorld $SYSROOT/bin/hw
+ln -s IRCClient $SYSROOT/bin/irc
+ln -s Minesweeper $SYSROOT/bin/ms
+ln -s Shell $SYSROOT/bin/sh
+ln -s Snake $SYSROOT/bin/sn
+ln -s Taskbar $SYSROOT/bin/tb
+ln -s VisualBuilder $SYSROOT/bin/vb
+ln -s WidgetGallery $SYSROOT/bin/wg
+ln -s TextEditor $SYSROOT/bin/te
+ln -s PaintBrush $SYSROOT/bin/pb
+ln -s QuickShow $SYSROOT/bin/qs
 echo "done"
 
 # Run local sync script, if it exists
