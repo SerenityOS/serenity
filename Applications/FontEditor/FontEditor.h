2dc0ea1cf94c392f43bbc7ec6830b4bdb767b16c
Andreas Kling <awesomekling@gmail.com>
2019-07-11 20:19:26 +0200

    FontEditor: Initial port to using a VisualBuilder generated UI.
    Here goes the first attempt at using VisualBuilder to make an application.
    
    There are many features missing that we are gonna have to implement,
    noticeably custom widgets (for the glyph editor and glyph map widgets)
    but this patch already moves most of the UI layout to a form file. :^)

--- Applications/FontEditor/FontEditor.h
+++ Applications/FontEditor/FontEditor.h
@@ -7,6 +7,8 @@ class GlyphEditorWidget;
 class GlyphMapWidget;
 class GTextBox;
 
+struct UI_FontEditorBottom;
+
 class FontEditorWidget final : public GWidget {
 public:
     FontEditorWidget(const String& path, RefPtr<Font>&&, GWidget* parent = nullptr);
@@ -17,8 +19,8 @@ private:
 
     GlyphMapWidget* m_glyph_map_widget { nullptr };
     GlyphEditorWidget* m_glyph_editor_widget { nullptr };
-    GTextBox* m_name_textbox { nullptr };
-    GTextBox* m_path_textbox { nullptr };
 
     String m_path;
+
+    OwnPtr<UI_FontEditorBottom> m_ui;
 };
