20445d4feee1de62a36746d61a5423692bddf625
rhin123 <ryanrhin@gmail.com>
2019-07-11 12:34:58 -0500

    TextEditor: Widget class small fixes

--- Applications/TextEditor/main.cpp
+++ Applications/TextEditor/main.cpp
@@ -1,4 +1,4 @@
-#include "TextEditor.h"
+#include "TextEditorWidget.h"
 
 int main(int argc, char** argv)
 {
@@ -7,11 +7,15 @@ int main(int argc, char** argv)
     auto* window = new GWindow;
     window->set_title("Text Editor");
     window->set_rect(20, 200, 640, 400);
-    auto* text_widget = new TextEditorWidget(&app, window, argc, argv);
-    window->set_main_widget(text_widget);
     window->set_should_exit_event_loop_on_close(true);
     window->show();
     window->set_icon_path("/res/icons/TextEditor16.png");
+
+    auto* text_widget = new TextEditorWidget();
+    window->set_main_widget(text_widget);
+
+    if (argc >= 2)
+        text_widget->open_sesame(argv[1]);
 
     return app.exec();
 }
