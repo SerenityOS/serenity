e38d383abcf0df12d4f4118d97dc453dd8f80ffa
Andreas Kling <awesomekling@gmail.com>
2019-07-13 23:14:00 +0200

    Piano: Use a fixed-width font in the UI to align things nicely.

--- Applications/Piano/PianoWidget.cpp
+++ Applications/Piano/PianoWidget.cpp
@@ -9,6 +9,8 @@ PianoWidget::PianoWidget()
     memset(keys, 0, sizeof(keys));
     m_front_buffer = new Sample[2048];
     m_back_buffer = new Sample[2048];
+
+    set_font(Font::default_fixed_width_font());
 }
 
 PianoWidget::~PianoWidget()
