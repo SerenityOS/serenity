eec96667357719ec11275ba21d701b851f24d29d
Andreas Kling <awesomekling@gmail.com>
2019-07-13 20:20:08 +0200

    Piano: Use a secondary thread to write the audio stream.
    This frees up the main thread to draw the GUI. The secondary thread uses
    a pipe to trick the main thread's event loop to break out of select() and
    update() the PianoWidget. :^)

--- Applications/Piano/main.cpp
+++ Applications/Piano/main.cpp
@@ -1,19 +1,18 @@
 #include "Music.h"
 #include "PianoWidget.h"
 #include <LibCore/CFile.h>
+#include <LibCore/CNotifier.h>
 #include <LibGUI/GApplication.h>
 #include <LibGUI/GEventLoop.h>
 #include <LibGUI/GWindow.h>
 
+static int s_pipefds[2];
+
 int main(int argc, char** argv)
 {
-    CFile audio("/dev/audio");
-    if (!audio.open(CIODevice::WriteOnly)) {
-        dbgprintf("Can't open audio device: %s", audio.error_string());
-        return 1;
-    }
-
     GApplication app(argc, argv);
+
+    pipe(s_pipefds);
 
     auto* window = new GWindow;
     window->set_title("Piano");
@@ -24,12 +23,30 @@ int main(int argc, char** argv)
 
     window->show();
 
-    for (;;) {
-        GEventLoop::current().pump(GEventLoop::WaitMode::PollForEvents);
-        u8 buffer[4096];
-        piano_widget->fill_audio_buffer(buffer, sizeof(buffer));
-        audio.write(buffer, sizeof(buffer));
-    }
+    CNotifier notifier(s_pipefds[0], CNotifier::Read);
+    notifier.on_ready_to_read = [&] {
+        char buffer[32];
+        read(s_pipefds[1], buffer, sizeof(buffer));
+        piano_widget->update();
+    };
 
-    return 0;
+    create_thread([](void* context) -> int {
+        auto* piano_widget = (PianoWidget*)context;
+
+        CFile audio("/dev/audio");
+        if (!audio.open(CIODevice::WriteOnly)) {
+            dbgprintf("Can't open audio device: %s", audio.error_string());
+            return 1;
+        }
+
+        for (;;) {
+            u8 buffer[4096];
+            piano_widget->fill_audio_buffer(buffer, sizeof(buffer));
+            audio.write(buffer, sizeof(buffer));
+            char ch = '!';
+            write(s_pipefds[0], &ch, 1);
+        }
+    }, piano_widget);
+
+    return app.exec();
 }
