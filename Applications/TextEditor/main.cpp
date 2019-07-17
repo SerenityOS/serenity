9724d540b6407d49b9351d680b697effc1a0ea11
rhin123 <ryanrhin@gmail.com>
2019-07-17 14:06:11 -0500

    TextEditor: Show window icon

--- Applications/TextEditor/main.cpp
+++ Applications/TextEditor/main.cpp
@@ -6,10 +6,8 @@ int main(int argc, char** argv)
 
     auto* window = new GWindow;
     window->set_title("Text Editor");
-
     window->set_rect(20, 200, 640, 400);
     window->set_should_exit_event_loop_on_close(true);
-    window->set_icon_path("/res/icons/TextEditor16.png");
 
     auto* text_widget = new TextEditorWidget();
     window->set_main_widget(text_widget);
@@ -18,6 +16,7 @@ int main(int argc, char** argv)
         text_widget->open_sesame(argv[1]);
 
     window->show();
+    window->set_icon_path("/res/icons/TextEditor16.png");
 
     return app.exec();
 }
