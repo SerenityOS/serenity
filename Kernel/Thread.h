SerenityOS/serenity

commit 80a6df90220981e64e0ca192908163d3a263ffc6

Thread: More composition in for_each :)

Kernel/Thread.h
@@ -316,38 +316,20 @@ template<typename Callback>
 inline IterationDecision Thread::for_each_in_state(State state, Callback callback)
 {
     ASSERT_INTERRUPTS_DISABLED();
-    for (auto* thread = thread_list_for_state(state)->head(); thread;) {
-        auto* next_thread = thread->next();
-        if (thread->state() == state) {
-            if (callback(*thread) == IterationDecision::Break)
-                return IterationDecision::Break;
-        }
-        thread = next_thread;
-    }
-
-    return IterationDecision::Continue;
+    if (is_runnable_state(state))
+        return for_each_runnable(callback);
+    return for_each_nonrunnable(callback);
 }
 
 template<typename Callback>
 inline IterationDecision Thread::for_each_living(Callback callback)
 {
     ASSERT_INTERRUPTS_DISABLED();
-    for (auto* thread = g_runnable_threads->head(); thread;) {
-        auto* next_thread = thread->next();
-        if (thread->state() != Thread::State::Dead && thread->state() != Thread::State::Dying)
-            if (callback(*thread) == IterationDecision::Break)
-                return IterationDecision::Break;
-        thread = next_thread;
-    }
-    for (auto* thread = g_nonrunnable_threads->head(); thread;) {
-        auto* next_thread = thread->next();
-        if (thread->state() != Thread::State::Dead && thread->state() != Thread::State::Dying)
-            if (callback(*thread) == IterationDecision::Break)
-                return IterationDecision::Break;
-        thread = next_thread;
-    }
-
-    return IterationDecision::Continue;
+    return Thread::for_each([callback](Thread& thread) -> IterationDecision {
+        if (thread.state() != Thread::State::Dead && thread.state() != Thread::State::Dying)
+            return callback(thread);
+        return IterationDecision::Continue;
+    });
 }
 
 template<typename Callback>

commit 2a572f594dec612d25310e3a93197182f98ddd23

Thread: Fix a regression introduced in 80a6df90220981e64e0ca192908163d3a263ffc6
Accidentally forgot to check the state parameter, which made this rather useless.

Bug found, and cause identified by Andreas

Kernel/Thread.h
@@ -312,9 +312,14 @@ template<typename Callback>
 inline IterationDecision Thread::for_each_in_state(State state, Callback callback)
 {
     ASSERT_INTERRUPTS_DISABLED();
+    auto new_callback = [=](Thread& thread) -> IterationDecision {
+        if (thread.state() == state)
+            return callback(thread);
+        return IterationDecision::Continue;
+    };
     if (is_runnable_state(state))
-        return for_each_runnable(callback);
-    return for_each_nonrunnable(callback);
+        return for_each_runnable(new_callback);
+    return for_each_nonrunnable(new_callback);
 }
 
 template<typename Callback>