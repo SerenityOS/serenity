6ae95945aa0a5959e2ed3562baf8b78b348a8ce6
Andreas Kling <awesomekling@gmail.com>
2019-07-25 15:21:16 +0200

    AK: Add ScopedValueRollback::set_override_rollback_value().
    This can be used if you change your mind about what value we should roll
    back to. :^)

--- AK/ScopedValueRollback.h
+++ AK/ScopedValueRollback.h
@@ -16,6 +16,11 @@ public:
         m_variable = m_saved_value;
     }
 
+    void set_override_rollback_value(const T& value)
+    {
+        m_saved_value = value;
+    }
+
 private:
     T& m_variable;
     T m_saved_value;
