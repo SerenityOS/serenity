e6eba244055c19af660fb1ffa315c9325cf9d492
Andreas Kling <awesomekling@gmail.com>
2019-07-27 18:43:59 +0200

    WindowServer+AudioServer: Add some missing C_OBJECT macros.

--- Servers/AudioServer/ASClientConnection.h
+++ Servers/AudioServer/ASClientConnection.h
@@ -1,20 +1,18 @@
 #pragma once
 
-#include <LibCore/CoreIPCServer.h>
 #include <LibAudio/ASAPI.h>
+#include <LibCore/CoreIPCServer.h>
 
 class ASMixer;
 
-class ASClientConnection final : public IPC::Server::Connection<ASAPI_ServerMessage, ASAPI_ClientMessage>
-{
+class ASClientConnection final : public IPC::Server::Connection<ASAPI_ServerMessage, ASAPI_ClientMessage> {
+    C_OBJECT(ASClientConnection)
 public:
     explicit ASClientConnection(CLocalSocket&, int client_id, ASMixer& mixer);
     ~ASClientConnection() override;
     void send_greeting() override;
     bool handle_message(const ASAPI_ClientMessage&, const ByteBuffer&& = {}) override;
-    const char* class_name() const override { return "ASClientConnection"; }
 
 private:
     ASMixer& m_mixer;
 };
-
