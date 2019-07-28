77f4be76f24b3351bde9400abf33a948c964c786
Jesse <jesse.buhagiar@student.rmit.edu.au>
2019-07-28 23:44:01 +1000

    Kernel: Expand PATA driver to support multiple hard drives (#365)
    The previous implementation of the PIIX3/4 PATA/IDE channel driver only
    supported a single drive, as the object model was wrong (the channel
    inherits the IRQ, not the disk drive itself). This fixes it by 'attaching'
    two `PATADiskDevices` to a `PATAChannel`, which makes more sense.
    
    The reading/writing code is presented as is, which violates the spec
    outlined by Seagate in the linked datasheet. That spec is rather old,
    so it might not be 100% up to date, though may cause issues on real
    hardware, so until we can actually test it, this will suffice.

--- Kernel/Devices/PATADiskDevice.h
+++ Kernel/Devices/PATADiskDevice.h
@@ -1,36 +1,33 @@
+//
+// A Disk Device Connected to a PATA Channel
+//
+//
 #pragma once
 
 #include <AK/RefPtr.h>
 #include <Kernel/Devices/DiskDevice.h>
+#include <Kernel/Devices/PATAChannel.h>
 #include <Kernel/IRQHandler.h>
 #include <Kernel/Lock.h>
 #include <Kernel/PCI.h>
 #include <Kernel/VM/PhysicalAddress.h>
 #include <Kernel/VM/PhysicalPage.h>
 
-struct PhysicalRegionDescriptor {
-    PhysicalAddress offset;
-    u16 size { 0 };
-    u16 end_of_table { 0 };
-};
-
-class IDEDiskDevice final : public IRQHandler
-    , public DiskDevice {
+class PATADiskDevice final : public DiskDevice {
     AK_MAKE_ETERNAL
 public:
-
     // Type of drive this IDEDiskDevice is on the ATA channel.
     //
     // Each PATA channel can contain only two devices, which (I think) are
     // jumper selectable on the drive itself by shorting two pins.
     enum class DriveType : u8 {
-        MASTER,
-        SLAVE
+        Master,
+        Slave
     };
 
 public:
-    static NonnullRefPtr<IDEDiskDevice> create(DriveType type);
-    virtual ~IDEDiskDevice() override;
+    static NonnullRefPtr<PATADiskDevice> create(PATAChannel&, DriveType);
+    virtual ~PATADiskDevice() override;
 
     // ^DiskDevice
     virtual unsigned block_size() const override;
@@ -39,37 +36,27 @@ public:
     virtual bool read_blocks(unsigned index, u16 count, u8*) override;
     virtual bool write_blocks(unsigned index, u16 count, const u8*) override;
 
+    void set_drive_geometry(u16, u16, u16);
+
 protected:
-    explicit IDEDiskDevice(DriveType);
+    explicit PATADiskDevice(PATAChannel&, DriveType);
 
 private:
-    // ^IRQHandler
-    virtual void handle_irq() override;
-
     // ^DiskDevice
     virtual const char* class_name() const override;
 
-    void initialize();
     bool wait_for_irq();
     bool read_sectors_with_dma(u32 lba, u16 count, u8*);
     bool write_sectors_with_dma(u32 lba, u16 count, const u8*);
     bool read_sectors(u32 lba, u16 count, u8* buffer);
     bool write_sectors(u32 lba, u16 count, const u8* data);
-
     bool is_slave() const;
 
     Lock m_lock { "IDEDiskDevice" };
     u16 m_cylinders { 0 };
     u16 m_heads { 0 };
     u16 m_sectors_per_track { 0 };
-    u16 m_io_base { 0 };
-    volatile bool m_interrupted { false };
-    volatile u8 m_device_error { 0 };
+    DriveType m_drive_type { DriveType::Master };
 
-    DriveType m_drive_type { DriveType::MASTER };
-    PCI::Address m_pci_address;
-    PhysicalRegionDescriptor m_prdt;
-    RefPtr<PhysicalPage> m_dma_buffer_page;
-    u16 m_bus_master_base { 0 };
-    Lockable<bool> m_dma_enabled;
+    PATAChannel& m_channel;
 };
