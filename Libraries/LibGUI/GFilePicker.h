4626076b1d184f4c7a803faca54aa0cedcf7758f
Rhin <ryanrhin@gmail.com>
2019-07-28 13:04:57 -0500

    GFilePicker: Quality of life improvements (#370)
    Added:
    - Default to home directory on open.
    - Save button focus.
    - Correct title for the mode were in.
    - Home directory shortcut.

--- Libraries/LibGUI/GFilePicker.h
+++ Libraries/LibGUI/GFilePicker.h
@@ -1,5 +1,6 @@
 #include <AK/FileSystemPath.h>
 #include <AK/Optional.h>
+#include <LibCore/CUserInfo.h>
 #include <LibGUI/GDialog.h>
 #include <LibGUI/GTableView.h>
 
@@ -18,7 +19,11 @@ public:
     static Optional<String> get_save_filepath(const String& file_extension);
     static bool file_exists(const StringView& path);
 
+<<<<<<< HEAD
     GFilePicker(Mode type = Mode::Open, const String& file_extension = ".txt", const StringView& path = "/", CObject* parent = nullptr);
+=======
+    GFilePicker(Mode type = Mode::Open, const StringView& path = String(get_current_user_home_path()), CObject* parent = nullptr);
+>>>>>>> GFilePicker: Quality of life improvements (#370)
     virtual ~GFilePicker() override;
 
     FileSystemPath selected_file() const { return m_selected_file; }
