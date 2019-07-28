844de1568d3a12e281d4eaf242ca7953abdf8ac4
Andreas Kling <awesomekling@gmail.com>
2019-07-28 18:27:32 +0200

    AudioServer: Add a buffer queue so we can buffer some sound.
    The idea here is to keep a small number of sample buffers queued in the
    AudioServer so we don't get caught without something to play.

--- Libraries/LibAudio/AClientConnection.cpp
+++ Libraries/LibAudio/AClientConnection.cpp
@@ -25,3 +25,18 @@ void AClientConnection::play(const ABuffer& buffer, bool block)
     request.play_buffer.buffer_id = buffer.shared_buffer_id();
     sync_request(request, block ? ASAPI_ServerMessage::Type::FinishedPlayingBuffer : ASAPI_ServerMessage::Type::PlayingBuffer);
 }
+
+void AClientConnection::enqueue(const ABuffer& buffer)
+{
+    for (;;) {
+        const_cast<ABuffer&>(buffer).shared_buffer().share_with(server_pid());
+        ASAPI_ClientMessage request;
+        request.type = ASAPI_ClientMessage::Type::EnqueueBuffer;
+        request.play_buffer.buffer_id = buffer.shared_buffer_id();
+        auto response = sync_request(request, ASAPI_ServerMessage::Type::EnqueueBufferResponse);
+        if (response.success)
+            break;
+        dbg() << "EnqueueBuffer failed, retrying...";
+        sleep(1);
+    }
+}
