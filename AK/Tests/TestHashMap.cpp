a9a1a5dfa967356ffe733e36a010f472332b5fe7
Andreas Kling <awesomekling@gmail.com>
2019-08-02 09:24:15 +0200

    AK: Add a test for iterating a HashTable during clear (should assert)
    Ideally we should also verify that the assertion actually happens,
    but we need some support in the TestSuite framework for that.

--- AK/Tests/TestHashMap.cpp
+++ AK/Tests/TestHashMap.cpp
@@ -62,4 +62,18 @@ TEST_CASE(case_insensitive)
     EXPECT_EQ(casemap.size(), 1);
 }
 
+TEST_CASE(assert_on_iteration_during_clear)
+{
+    struct Object {
+        ~Object()
+        {
+            m_map->begin();
+        }
+        HashMap<int, Object>* m_map;
+    };
+    HashMap<int, Object> map;
+    map.set(0, { &map });
+    map.clear();
+}
+
 TEST_MAIN(HashMap)
