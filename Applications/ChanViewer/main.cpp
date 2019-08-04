d0c1724d5b06c39fe9ba88ad1e3870fe4e38de93
Andreas Kling <awesomekling@gmail.com>
2019-08-04 14:53:32 +0200

    ChanViewer: Give this application a simple window icon

--- Applications/ChanViewer/main.cpp
+++ Applications/ChanViewer/main.cpp
@@ -1,4 +1,5 @@
 #include "ThreadCatalogModel.h"
+#include <LibDraw/PNGLoader.h>
 #include <LibGUI/GApplication.h>
 #include <LibGUI/GBoxLayout.h>
 #include <LibGUI/GTableView.h>
@@ -11,6 +12,7 @@ int main(int argc, char** argv)
     auto* window = new GWindow;
     window->set_title("ChanViewer");
     window->set_rect(100, 100, 640, 480);
+    window->set_icon(load_png("/res/icons/16x16/app-chanviewer.png"));
 
     auto* widget = new GWidget;
     window->set_main_widget(widget);
