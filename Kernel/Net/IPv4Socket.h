6347e3aa51b45c7ecc35a7dd3fb4e6252529b83e
Andreas Kling <awesomekling@gmail.com>
2019-08-05 10:03:19 +0200

    Net: Let Socket have read/write wrappers around sendto/recvfrom
    The situations in IPv4Socket and LocalSocket were mirrors of each other
    where one had implemented read/write as wrappers and the other had
    sendto/recvfrom as wrappers.
    
    Instead of this silliness, move read and write up to the Socket base.
    Then mark them final, so subclasses have no choice but to implement
    sendto and recvfrom.

--- Kernel/Net/IPv4Socket.h
+++ Kernel/Net/IPv4Socket.h
@@ -28,8 +28,6 @@ public:
     virtual void attach(FileDescription&) override;
     virtual void detach(FileDescription&) override;
     virtual bool can_read(FileDescription&) const override;
-    virtual ssize_t read(FileDescription&, u8*, ssize_t) override;
-    virtual ssize_t write(FileDescription&, const u8*, ssize_t) override;
     virtual bool can_write(FileDescription&) const override;
     virtual ssize_t sendto(FileDescription&, const void*, size_t, int, const sockaddr*, socklen_t) override;
     virtual ssize_t recvfrom(FileDescription&, void*, size_t, int flags, sockaddr*, socklen_t*) override;
--- Kernel/Net/LocalSocket.h
+++ Kernel/Net/LocalSocket.h
@@ -10,6 +10,7 @@ public:
     static NonnullRefPtr<LocalSocket> create(int type);
     virtual ~LocalSocket() override;
 
+    // ^Socket
     virtual KResult bind(const sockaddr*, socklen_t) override;
     virtual KResult connect(FileDescription&, const sockaddr*, socklen_t, ShouldBlock = ShouldBlock::Yes) override;
     virtual bool get_local_address(sockaddr*, socklen_t*) override;
@@ -17,8 +18,6 @@ public:
     virtual void attach(FileDescription&) override;
     virtual void detach(FileDescription&) override;
     virtual bool can_read(FileDescription&) const override;
-    virtual ssize_t read(FileDescription&, u8*, ssize_t) override;
-    virtual ssize_t write(FileDescription&, const u8*, ssize_t) override;
     virtual bool can_write(FileDescription&) const override;
     virtual ssize_t sendto(FileDescription&, const void*, size_t, int, const sockaddr*, socklen_t) override;
     virtual ssize_t recvfrom(FileDescription&, void*, size_t, int flags, sockaddr*, socklen_t*) override;
