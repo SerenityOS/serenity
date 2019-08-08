314833a99750f1c8a3463631c945304f2a0c6831
Andreas Kling <awesomekling@gmail.com>
2019-08-08 19:50:33 +0200

    ProcessManager: Tweak memory stats widget layout to fit more text
    After a while, the kmalloc/kfree counts got too wide for the label.

--- Applications/ProcessManager/MemoryStatsWidget.cpp
+++ Applications/ProcessManager/MemoryStatsWidget.cpp
@@ -26,7 +26,7 @@ MemoryStatsWidget::MemoryStatsWidget(GraphWidget& graph, GWidget* parent)
         auto* container = new GWidget(this);
         container->set_layout(make<GBoxLayout>(Orientation::Horizontal));
         container->set_size_policy(SizePolicy::Fixed, SizePolicy::Fixed);
-        container->set_preferred_size(255, 12);
+        container->set_preferred_size(275, 12);
         auto* description_label = new GLabel(description, container);
         description_label->set_font(Font::default_bold_font());
         description_label->set_text_alignment(TextAlignment::CenterLeft);
