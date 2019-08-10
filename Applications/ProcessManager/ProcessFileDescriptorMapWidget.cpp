2eead3dfc0207729363d7c4620ecd7282c502a35
Andreas Kling <awesomekling@gmail.com>
2019-08-10 11:07:55 +0200

    ProcessManager: Use a GJsonArrayModel for the process file desciptors

--- Applications/ProcessManager/ProcessFileDescriptorMapWidget.cpp
+++ Applications/ProcessManager/ProcessFileDescriptorMapWidget.cpp
@@ -1,6 +1,6 @@
 #include "ProcessFileDescriptorMapWidget.h"
-#include "ProcessFileDescriptorMapModel.h"
 #include <LibGUI/GBoxLayout.h>
+#include <LibGUI/GJsonArrayModel.h>
 #include <LibGUI/GTableView.h>
 
 ProcessFileDescriptorMapWidget::ProcessFileDescriptorMapWidget(GWidget* parent)
@@ -10,7 +10,17 @@ ProcessFileDescriptorMapWidget::ProcessFileDescriptorMapWidget(GWidget* parent)
     layout()->set_margins({ 4, 4, 4, 4 });
     m_table_view = new GTableView(this);
     m_table_view->set_size_columns_to_fit_content(true);
-    m_table_view->set_model(adopt(*new ProcessFileDescriptorMapModel));
+
+    Vector<GJsonArrayModel::FieldSpec> pid_fds_fields;
+    pid_fds_fields.empend("fd", "FD", TextAlignment::CenterRight);
+    pid_fds_fields.empend("class", "Class", TextAlignment::CenterLeft);
+    pid_fds_fields.empend("offset", "Offset", TextAlignment::CenterRight);
+    pid_fds_fields.empend("absolute_path", "Path", TextAlignment::CenterLeft);
+    pid_fds_fields.empend("seekable", "Access", TextAlignment::CenterLeft, [](auto& data) {
+        return data.to_bool() ? "Seekable" : "Sequential";
+    });
+
+    m_table_view->set_model(GJsonArrayModel::create({}, move(pid_fds_fields)));
 }
 
 ProcessFileDescriptorMapWidget::~ProcessFileDescriptorMapWidget()
@@ -22,5 +32,5 @@ void ProcessFileDescriptorMapWidget::set_pid(pid_t pid)
     if (m_pid == pid)
         return;
     m_pid = pid;
-    static_cast<ProcessFileDescriptorMapModel*>(m_table_view->model())->set_pid(pid);
+    static_cast<GJsonArrayModel*>(m_table_view->model())->set_json_path(String::format("/proc/%d/fds", m_pid));
 }
