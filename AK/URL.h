063c836daa2f5041c8b204de66d1b076a2e9b890
Andreas Kling <awesomekling@gmail.com>
2019-08-10 17:27:56 +0200

    AK: Add a basic URL class to help us handle URL's
    We're gonna need these as we start to write more networking programs.

---
+++ AK/URL.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <AK/AKString.h>
+#include <AK/StringView.h>
+
+namespace AK {
+
+class URL {
+public:
+    URL() {}
+    URL(const StringView&);
+
+    bool is_valid() const { return m_valid; }
+    String protocol() const { return m_protocol; }
+    String host() const { return m_host; }
+    String path() const { return m_path; }
+    u16 port() const { return m_port; }
+
+    String to_string() const;
+
+private:
+    bool parse(const StringView&);
+
+    bool m_valid { false };
+    u16 m_port { 80 };
+    String m_protocol;
+    String m_host;
+    String m_path;
+};
+
+}
+
+using AK::URL;
+
+inline const LogStream& operator<<(const LogStream& stream, const URL& value)
+{
+    return stream << value.to_string();
+}
