c2d613e2f56f2f8073dcaec2b0945afd289f30b4
Conrad Pankoff <deoxxa@fknsrs.biz>
2019-08-14 01:22:02 +1000

    Kernel: Add more PCI configuration reading functions

--- Kernel/PCI.h
+++ Kernel/PCI.h
@@ -52,6 +52,11 @@ u32 get_BAR2(Address);
 u32 get_BAR3(Address);
 u32 get_BAR4(Address);
 u32 get_BAR5(Address);
+u8 get_revision_id(Address);
+u8 get_subclass(Address);
+u8 get_class(Address);
+u16 get_subsystem_id(Address);
+u16 get_subsystem_vendor_id(Address);
 void enable_bus_mastering(Address);
 
 }
