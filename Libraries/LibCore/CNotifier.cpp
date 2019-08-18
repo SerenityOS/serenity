1b3599fbbcdd6da0ad53d293d6f19c63721b257f
Andreas Kling <awesomekling@gmail.com>
2019-08-18 11:54:39 +0200

    LibCore: Make CSocket's notifiers into children of the CSocket
    The Inspector app quickly exposes crappy flat object hiearchies without
    parent/child relationships. This is one of many commits that improves
    the situation by making parent/child CObject relationships explicit.

--- Libraries/LibCore/CNotifier.cpp
+++ Libraries/LibCore/CNotifier.cpp
@@ -2,8 +2,9 @@
 #include <LibCore/CEventLoop.h>
 #include <LibCore/CNotifier.h>
 
-CNotifier::CNotifier(int fd, unsigned event_mask)
-    : m_fd(fd)
+CNotifier::CNotifier(int fd, unsigned event_mask, CObject* parent)
+    : CObject(parent)
+    , m_fd(fd)
     , m_event_mask(event_mask)
 {
     set_enabled(true);
