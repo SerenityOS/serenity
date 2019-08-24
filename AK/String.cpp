3a1122b890c77d9c6d101cfc7c7a9e0453d8a894
Andreas Kling <awesomekling@gmail.com>
2019-08-24 22:28:42 +0200

    AK: Add String::operator==(StringView)
    Comparing a String to a StringView would instantiate a temporary
    String just for the comparison. Let's not do that. :^)

--- AK/String.cpp
+++ AK/String.cpp
@@ -19,6 +19,20 @@ bool String::operator==(const String& other) const
     return !memcmp(characters(), other.characters(), length());
 }
 
+bool String::operator==(const StringView& other) const
+{
+    if (!m_impl)
+        return !other.m_characters;
+
+    if (!other.m_characters)
+        return false;
+
+    if (length() != other.length())
+        return false;
+
+    return !memcmp(characters(), other.characters_without_null_termination(), length());
+}
+
 bool String::operator<(const String& other) const
 {
     if (!m_impl)
