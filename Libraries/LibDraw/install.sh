9bc3444626ef07d7a99c2ee1944611f5416c772d
Andreas Kling <awesomekling@gmail.com>
2019-08-25 17:42:16 +0200

    LibDraw: Fix wrong installation path

--- Libraries/LibDraw/install.sh
+++ Libraries/LibDraw/install.sh
@@ -3,5 +3,5 @@
 set -e
 SERENITY_ROOT=../../
 
-mkdir -p $SERENITY_ROOT/Root/usr/include/LibGfx/
-cp *.h $SERENITY_ROOT/Root/usr/include/LibGfx/
+mkdir -p $SERENITY_ROOT/Root/usr/include/LibDraw/
+cp *.h $SERENITY_ROOT/Root/usr/include/LibDraw/
