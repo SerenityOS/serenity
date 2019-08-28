a2e435a09644bd0366066a332b027e8907816497
Conrad Pankoff <deoxxa@fknsrs.biz>
2019-08-28 21:55:49 +1000

    Kernel: Remove IP configuration from LoopbackAdapter
    This is configured in NetworkTask_main now, so there's no need to do it
    here as well.

--- Kernel/Net/LoopbackAdapter.cpp
+++ Kernel/Net/LoopbackAdapter.cpp
@@ -11,7 +11,6 @@ LoopbackAdapter& LoopbackAdapter::the()
 LoopbackAdapter::LoopbackAdapter()
 {
     set_interface_name("loop");
-    set_ipv4_address({ 127, 0, 0, 1 });
 }
 
 LoopbackAdapter::~LoopbackAdapter()
