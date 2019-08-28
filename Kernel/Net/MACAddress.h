4515605a31cf40dbd656b85395b07a2bbf9637b0
Conrad Pankoff <deoxxa@fknsrs.biz>
2019-08-28 10:46:30 +1000

    Kernel: Add list-compatible constructor for MACAddress

--- Kernel/Net/MACAddress.h
+++ Kernel/Net/MACAddress.h
@@ -13,6 +13,15 @@ public:
     {
         memcpy(m_data, data, 6);
     }
+    MACAddress(u8 a, u8 b, u8 c, u8 d, u8 e, u8 f)
+    {
+        m_data[0] = a;
+        m_data[1] = b;
+        m_data[2] = c;
+        m_data[3] = d;
+        m_data[4] = e;
+        m_data[5] = f;
+    }
     ~MACAddress() {}
 
     u8 operator[](int i) const
