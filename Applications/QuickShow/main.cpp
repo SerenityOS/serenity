90fe36b0efb993d110f50fa40711ecc89dd3448c
rhin123 <ryanrhin@gmail.com>
2019-09-04 16:32:26 -0500

    QuickShow: Added GCommonActions

--- Applications/QuickShow/main.cpp
+++ Applications/QuickShow/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char** argv)
     auto menubar = make<GMenuBar>();
 
     auto app_menu = make<GMenu>("QuickShow");
-    app_menu->add_action(GAction::create("Quit", { Mod_Alt, Key_F4 }, [](const GAction&) {
+    app_menu->add_action(GCommonActions::make_quit_action([] {
         GApplication::the().quit(0);
         return;
     }));
