116aa505c96c44c6428cb924a564cb56b2d6dfc6
rhin123 <ryanrhin@gmail.com>
2019-09-04 16:33:41 -0500

    Snake: Added GCommonActions

--- Games/Snake/main.cpp
+++ Games/Snake/main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char** argv)
     app_menu->add_action(GAction::create("New game", { Mod_None, Key_F2 }, [&](const GAction&) {
         game->reset();
     }));
-    app_menu->add_action(GAction::create("Quit", { Mod_Alt, Key_F4 }, [](const GAction&) {
+    app_menu->add_action(GCommonActions::make_quit_action([] {
         GApplication::the().quit(0);
     }));
 
