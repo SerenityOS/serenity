0ac34df0fd5d878087e6204db4e5662710c6c9a8
Andreas Kling <awesomekling@gmail.com>
2019-09-13 21:29:47 +0200

    GMenu: Allow constructing without a name
    We'll just default to "", which works fine for context menus.

--- Libraries/LibGUI/GMenu.h
+++ Libraries/LibGUI/GMenu.h
@@ -10,7 +10,7 @@ class Point;
 
 class GMenu {
 public:
-    explicit GMenu(const StringView& name);
+    explicit GMenu(const StringView& name = "");
     ~GMenu();
 
     static GMenu* from_menu_id(int);
