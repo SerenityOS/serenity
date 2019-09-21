0c72e0c09f085a371b0abdba172325e56b1e6a06
Andreas Kling <awesomekling@gmail.com>
2019-09-22 00:17:29 +0200

    AK: Add AK_MAKE_NONMOVABLE

--- AK/Noncopyable.h
+++ AK/Noncopyable.h
@@ -4,3 +4,8 @@
 private:                       \
     c(const c&) = delete;      \
     c& operator=(const c&) = delete;
+
+#define AK_MAKE_NONMOVABLE(c) \
+private:                       \
+    c(c&&) = delete;      \
+    c& operator=(c&&) = delete;
