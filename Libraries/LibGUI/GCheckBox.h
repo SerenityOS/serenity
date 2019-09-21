7b5342b2e301ad75f3c88c617680d2c29ae332ce
Andreas Kling <awesomekling@gmail.com>
2019-09-21 18:58:48 +0200

    LibGUI: Convert GCheckBox to ObjectPtr

--- Libraries/LibGUI/GCheckBox.h
+++ Libraries/LibGUI/GCheckBox.h
@@ -7,13 +7,14 @@
 class GCheckBox : public GAbstractButton {
     C_OBJECT(GCheckBox)
 public:
-    GCheckBox(const StringView&, GWidget* parent);
-    explicit GCheckBox(GWidget* parent);
     virtual ~GCheckBox() override;
 
     virtual void click() override;
 
 private:
+    GCheckBox(const StringView&, GWidget* parent);
+    explicit GCheckBox(GWidget* parent);
+
     // These don't make sense for a check box, so hide them.
     using GAbstractButton::auto_repeat_interval;
     using GAbstractButton::set_auto_repeat_interval;
