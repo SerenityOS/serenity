a7217a3ed9913090615fa6f24d035a212cdbbc7a
Sergey Bugaev <bugaevc@gmail.com>
2019-09-25 12:29:25 +0300

    LibHTML: Fix LayoutDocument height computation

--- Libraries/LibHTML/Layout/LayoutDocument.cpp
+++ Libraries/LibHTML/Layout/LayoutDocument.cpp
@@ -12,5 +12,13 @@ LayoutDocument::~LayoutDocument()
 void LayoutDocument::layout()
 {
     rect().set_width(style().size().width());
+
     LayoutNode::layout();
+
+    int lowest_bottom = 0;
+    for_each_child([&](auto& child) {
+        if (child.rect().bottom() > lowest_bottom)
+            lowest_bottom = child.rect().bottom();
+    });
+    rect().set_bottom(lowest_bottom);
 }
