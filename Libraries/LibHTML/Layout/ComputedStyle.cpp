90e4ed21607d5a58fc736340992c4d19e07ed844
Sergey Bugaev <bugaevc@gmail.com>
2019-09-25 12:00:50 +0300

    LibHTML: Add ComputedStyle::full_margin()
    This is an utility to easily get the full margin size
    (the sum of margin proper, border and padding) of an
    element in pixels.

--- Libraries/LibHTML/Layout/ComputedStyle.cpp
+++ Libraries/LibHTML/Layout/ComputedStyle.cpp
@@ -7,3 +7,13 @@ ComputedStyle::ComputedStyle()
 ComputedStyle::~ComputedStyle()
 {
 }
+
+ComputedStyle::PixelBox ComputedStyle::full_margin() const
+{
+    return {
+        m_margin.top.to_px() + m_border.top.to_px() + m_padding.top.to_px(),
+        m_margin.right.to_px() + m_border.right.to_px() + m_padding.right.to_px(),
+        m_margin.bottom.to_px() + m_border.bottom.to_px() + m_padding.bottom.to_px(),
+        m_margin.left.to_px() + m_border.left.to_px() + m_padding.left.to_px(),
+    };
+}
