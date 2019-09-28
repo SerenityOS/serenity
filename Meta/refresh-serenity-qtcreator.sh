b8cab2a934a8846b05c4f18453339a0b99f2c26b
Andreas Kling <awesomekling@gmail.com>
2019-09-28 21:39:11 +0200

    Meta: Tweak the Qt Creator project updater a bit

--- Meta/refresh-serenity-qtcreator.sh
+++ Meta/refresh-serenity-qtcreator.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd $SERENITY_ROOT
-find . -name '*.ipc' -or -name '*.cpp' -or -name '*.h' -or -name '*.S' | grep -Fv Patches/ | grep -Fv Root/ | grep -Fv Ports/ | grep -Fv Toolchain/ > serenity.files
+find . -name '*.ipc' -or -name '*.cpp' -or -name '*.h' -or -name '*.S' -or -name '*.css' | grep -Fv Patches/ | grep -Fv Root/ | grep -Fv Ports/ | grep -Fv Toolchain/ | grep -Fv Base/ > serenity.files
