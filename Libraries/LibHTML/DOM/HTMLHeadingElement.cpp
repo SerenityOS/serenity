36961c429136c95aaa6837313816a76faecfafb6
Andreas Kling <awesomekling@gmail.com>
2019-09-29 12:24:58 +0200

    LibHTML: Add HTMLHeadingElement for <h1> through <h6>

---
+++ Libraries/LibHTML/DOM/HTMLHeadingElement.cpp
@@ -0,0 +1,10 @@
+#include <LibHTML/DOM/HTMLHeadingElement.h>
+
+HTMLHeadingElement::HTMLHeadingElement(Document& document, const String& tag_name)
+    : HTMLElement(document, tag_name)
+{
+}
+
+HTMLHeadingElement::~HTMLHeadingElement()
+{
+}
