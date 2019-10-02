b5ba1e49e1174c3f1f95e7c22b49f2002fc71f8b
Sergey Bugaev <bugaevc@gmail.com>
2019-10-02 22:50:20 +0300

    Ports: Add a TinyCC port
    It does build, but doesn't quite work yet.

---
+++ Ports/tinycc/package.sh
@@ -0,0 +1,14 @@
+#!/bin/bash ../.port_include.sh
+port=tinycc
+workdir=tinycc-dev
+version=dev
+curlopts="-L"
+files="https://github.com/TinyCC/tinycc/archive/dev.tar.gz tinycc-dev.tar.gz"
+useconfigure=true
+makeopts=tcc
+
+configure() {
+    run ./configure --cross-prefix=i686-pc-serenity- --cpu=i686 --triplet=i686-pc-serenity --crtprefix=/usr/lib
+}
+
+export CONFIG_ldl=no
