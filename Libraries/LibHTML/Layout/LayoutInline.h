915d9a3d484f294c9b6731c5a35896bdeaca28ca
Andreas Kling <awesomekling@gmail.com>
2019-10-03 17:36:06 +0200

    LibHTML: Remove unused LayoutInline::layout()
    Inline layout now happens in the containing block, specifically in
    LayoutBlock::layout_inline_children().

--- Libraries/LibHTML/Layout/LayoutInline.h
+++ Libraries/LibHTML/Layout/LayoutInline.h
@@ -2,7 +2,6 @@
 
 #include <LibHTML/Layout/LayoutNode.h>
 
-class Element;
 class LayoutBlock;
 
 class LayoutInline : public LayoutNode {
@@ -12,8 +11,6 @@ public:
 
     virtual const char* class_name() const override { return "LayoutInline"; }
     virtual bool is_inline() const override { return true; }
-
-    virtual void layout() override;
 
     virtual void split_into_lines(LayoutBlock& container);
 
