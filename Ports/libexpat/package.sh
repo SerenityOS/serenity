c94e3be922a91749c7db8e39b637855b3e586e60
Vincent Sanders <vince@kyllikki.org>
2019-10-04 08:18:57 +0100

    Ports: Add expat port (#628)

---
+++ Ports/libexpat/package.sh
@@ -0,0 +1,12 @@
+#!/bin/bash ../.port_include.sh
+port=libexpat
+version=2.2.9
+useconfigure=true
+files="https://codeload.github.com/libexpat/libexpat/tar.gz/R_2_2_9 expat-2.2.9.tar.gz"
+workdir=libexpat-R_2_2_9/expat/
+
+configure() {
+    run ./buildconf.sh
+    run patch -p 0 < fix-autoconf.patch
+    run ./"$configscript" --host=i686-pc-serenity $configopts
+}
