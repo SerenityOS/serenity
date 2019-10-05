b8563c57285ec675e8972c9a0fab209c393a440d
Andreas Kling <awesomekling@gmail.com>
2019-10-05 23:41:14 +0200

    LibHTML: Support rendering <img src> with file:// URLs
    We can now show images loaded from local file:// URLs. Pretty neat :^)

--- Libraries/LibHTML/DOM/HTMLImageElement.cpp
+++ Libraries/LibHTML/DOM/HTMLImageElement.cpp
@@ -1,4 +1,5 @@
 #include <LibHTML/CSS/StyleResolver.h>
+#include <LibHTML/DOM/Document.h>
 #include <LibHTML/DOM/HTMLImageElement.h>
 #include <LibHTML/Layout/LayoutImage.h>
 
@@ -21,4 +22,19 @@ RefPtr<LayoutNode> HTMLImageElement::create_layout_node(const StyleResolver& res
     if (display == "none")
         return nullptr;
     return adopt(*new LayoutImage(*this, move(style)));
+}
+
+const GraphicsBitmap* HTMLImageElement::bitmap() const
+{
+    if (!m_bitmap) {
+        URL src_url = document().complete_url(this->src());
+        if (src_url.protocol() == "file") {
+            m_bitmap = GraphicsBitmap::load_from_file(src_url.path());
+        } else {
+            // FIXME: Implement! This whole thing should be at a different layer though..
+            ASSERT_NOT_REACHED();
+        }
+    }
+
+    return m_bitmap;
 }
--- Libraries/LibHTML/Layout/LayoutImage.cpp
+++ Libraries/LibHTML/Layout/LayoutImage.cpp
@@ -18,6 +18,9 @@ void LayoutImage::layout()
         auto& font = Font::default_font();
         rect().set_width(font.width(node().alt()) + 16);
         rect().set_height(font.glyph_height() + 16);
+    } else {
+        rect().set_width(node().bitmap()->width());
+        rect().set_height(node().bitmap()->height());
     }
 
     LayoutReplaced::layout();
@@ -29,11 +32,13 @@ void LayoutImage::render(RenderingContext& context)
         context.painter().set_font(Font::default_font());
         StylePainter::paint_frame(context.painter(), rect(), FrameShape::Container, FrameShadow::Sunken, 2);
         context.painter().draw_text(rect(), node().alt(), TextAlignment::Center, Color::White);
+    } else {
+        context.painter().draw_scaled_bitmap(rect(), *node().bitmap(), node().bitmap()->rect());
     }
     LayoutReplaced::render(context);
 }
 
 bool LayoutImage::renders_as_alt_text() const
 {
-    return true;
+    return !node().bitmap();
 }
