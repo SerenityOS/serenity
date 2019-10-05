b31688d2ec82d7856d75af462628abd5f587b383
Vincent Sanders <vince@kyllikki.org>
2019-10-05 21:34:19 +0100

    Ports: Add zlib port

--- Ports/curl/package.sh
+++ Ports/curl/package.sh
@@ -4,3 +4,4 @@ version=7.65.3
 useconfigure=true
 configopts="--disable-threaded-resolver"
 files="https://curl.haxx.se/download/curl-7.65.3.tar.bz2 curl-7.65.3.tar.bz2"
+depends=zlib
---
+++ Ports/zlib/package.sh
@@ -0,0 +1,9 @@
+#!/bin/bash ../.port_include.sh
+port=zlib
+version=1.2.11
+useconfigure=true
+files="https://www.zlib.net/zlib-1.2.11.tar.gz zlib-1.2.11.tar.gz"
+
+configure() {
+    run ./configure
+}
