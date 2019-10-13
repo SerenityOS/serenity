282456dc375d5b9fe532e260baa7e52649dd368b
Calvin Buckley <calvin@cmpct.info>
2019-10-13 13:09:58 -0300

    Ports: libarchive
    Provides a cpio and tar implementation. Unfortunately doesn't work
    yet; requires a working mbtowc implementation.

---
+++ Ports/libarchive/package.sh
@@ -0,0 +1,10 @@
+#!/bin/bash ../.port_include.sh
+port=libarchive
+version=3.4.0
+useconfigure=true
+files="https://libarchive.org/downloads/libarchive-3.4.0.tar.gz libarchive-3.4.0.tar.gz"
+patchlevel=1
+
+configure() {
+    run ./"$configscript" --host=i686-pc-serenity $configopts
+}
