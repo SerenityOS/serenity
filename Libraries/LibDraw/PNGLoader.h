0a8fa3bddbbc8f20548d92d022c82175b4ba287c
Andreas Kling <awesomekling@gmail.com>
2019-10-19 19:56:49 +0200

    LibDraw: Rename ImageLoader => ImageDecoder
    ImageLoader was not the right name for this, as there is no loading
    happening, only decoding. :^)

--- Libraries/LibDraw/PNGLoader.h
+++ Libraries/LibDraw/PNGLoader.h
@@ -1,17 +1,17 @@
 #pragma once
 
 #include <LibDraw/GraphicsBitmap.h>
-#include <LibDraw/ImageLoader.h>
+#include <LibDraw/ImageDecoder.h>
 
 RefPtr<GraphicsBitmap> load_png(const StringView& path);
 RefPtr<GraphicsBitmap> load_png_from_memory(const u8*, size_t);
 
 struct PNGLoadingContext;
 
-class PNGImageLoaderPlugin final : public ImageLoaderPlugin {
+class PNGImageDecoderPlugin final : public ImageDecoderPlugin {
 public:
-    virtual ~PNGImageLoaderPlugin() override;
-    PNGImageLoaderPlugin(const u8*, size_t);
+    virtual ~PNGImageDecoderPlugin() override;
+    PNGImageDecoderPlugin(const u8*, size_t);
 
     virtual Size size() override;
     virtual RefPtr<GraphicsBitmap> bitmap() override;
