a1c89c27345d02cb42ce409c2f9365906c0c5fd6
Brandon Scott <xeons@users.noreply.github.com>
2019-10-23 17:43:40 -0500

    LibGUI: Add very limited multi-label support to GStatusBar
    Created a constructor argument and getter/setter methods to allow
    you to have a multi-label status bar.

--- Libraries/LibGUI/GStatusBar.h
+++ Libraries/LibGUI/GStatusBar.h
@@ -11,13 +11,17 @@ public:
     virtual ~GStatusBar() override;
 
     String text() const;
+    String text(int index) const;
     void set_text(const StringView&);
+    void set_text(int index, const StringView&);
 
 protected:
     explicit GStatusBar(GWidget* parent);
+    explicit GStatusBar(int label_count, GWidget* parent);
     virtual void paint_event(GPaintEvent&) override;
 
 private:
-    RefPtr<GLabel> m_label;
+    NonnullRefPtr<GLabel> create_label();
+    NonnullRefPtrVector<GLabel> m_labels;
     RefPtr<GResizeCorner> m_corner;
 };
