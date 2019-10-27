9d0b4a0a424399bda84fa55309006acdc72a4ac9
Andreas Kling <awesomekling@gmail.com>
2019-10-27 11:23:53 +0100

    LibGUI: Move GTextRange and GTextPosition to their own header files

---
+++ Libraries/LibGUI/GTextPosition.h
@@ -0,0 +1,27 @@
+#pragma once
+
+class GTextPosition {
+public:
+    GTextPosition() {}
+    GTextPosition(int line, int column)
+        : m_line(line)
+        , m_column(column)
+    {
+    }
+
+    bool is_valid() const { return m_line >= 0 && m_column >= 0; }
+
+    int line() const { return m_line; }
+    int column() const { return m_column; }
+
+    void set_line(int line) { m_line = line; }
+    void set_column(int column) { m_column = column; }
+
+    bool operator==(const GTextPosition& other) const { return m_line == other.m_line && m_column == other.m_column; }
+    bool operator!=(const GTextPosition& other) const { return m_line != other.m_line || m_column != other.m_column; }
+    bool operator<(const GTextPosition& other) const { return m_line < other.m_line || (m_line == other.m_line && m_column < other.m_column); }
+
+private:
+    int m_line { -1 };
+    int m_column { -1 };
+};
