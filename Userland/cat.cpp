fd28e760bea04c5fd411800beca7ca2132b96620
balatt <56897347+balatt@users.noreply.github.com>
2019-10-27 22:41:53 -0400

    cat.cpp: updated to open mutliple files
    Before cat could only open one file so I modified it a bit to output multiple like other cat versions. Now it iterates through each file with a do-while loop.

--- Userland/cat.cpp
+++ Userland/cat.cpp
@@ -8,26 +8,29 @@
 
 int main(int argc, char** argv)
 {
-    int fd = argc > 1 ? open(argv[1], O_RDONLY) : 0;
-    if (fd == -1) {
-        fprintf(stderr, "Failed to open %s: %s\n", argv[1], strerror(errno));
-        return 1;
-    }
-    for (;;) {
-        char buf[4096];
-        ssize_t nread = read(fd, buf, sizeof(buf));
-        if (nread == 0)
-            break;
-        if (nread < 0) {
-            perror("read");
-            return 2;
+    int i = 1;
+    int fd = 0;
+    char buf[4096];
+    do {
+        if (argc > 1 && (fd = open(argv[i], O_RDONLY)) == -1) {
+            fprintf(stderr, "Failed to open %s: %s\n", argv[i], strerror(errno));
+            continue;
         }
-        ssize_t nwritten = write(1, buf, nread);
-        if (nwritten < 0) {
-            perror("write");
-            return 3;
+        for (;;) {
+            ssize_t nread = read(fd, buf, sizeof(buf));
+            if (nread == 0)
+                break;
+            if (nread < 0) {
+                perror("read");
+                return 2;
+            }
+            ssize_t nwritten = write(1, buf, nread);
+            if (nwritten < 0) {
+                perror("write");
+                return 3;
+            }
+            ASSERT(nwritten == nread);
         }
-        ASSERT(nwritten == nread);
-    }
+    } while (++i < argc);
     return 0;
 }
