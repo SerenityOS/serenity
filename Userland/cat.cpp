5442e365c9e9cbb43a4a86a71e92b52a1b4e8d81
balatt <56897347+balatt@users.noreply.github.com>
2019-10-29 11:08:09 -0400

    cat: Updated to handle multiple input files (#694)
    Now concatenates multiple files at once with a for loop iterating
    through a vector of file arguments.

--- Userland/cat.cpp
+++ Userland/cat.cpp
@@ -1,3 +1,4 @@
+#include <AK/Vector.h>
 #include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -8,26 +9,37 @@
 
 int main(int argc, char** argv)
 {
-    int fd = argc > 1 ? open(argv[1], O_RDONLY) : 0;
-    if (fd == -1) {
-        fprintf(stderr, "Failed to open %s: %s\n", argv[1], strerror(errno));
-        return 1;
+    Vector<int> fds;
+    if (argc > 1) {
+        for (int i = 0; i < argc; i++) {
+            int fd;
+            if ((fd = open(argv[i], O_RDONLY)) == -1) {
+                fprintf(stderr, "Failed to open %s: %s\n", argv[i], strerror(errno));
+                continue;
+            }
+            fds.append(fd);
+        }
+    } else {
+        fds.append(0);
     }
-    for (;;) {
-        char buf[4096];
-        ssize_t nread = read(fd, buf, sizeof(buf));
-        if (nread == 0)
-            break;
-        if (nread < 0) {
-            perror("read");
-            return 2;
+    for (auto& fd : fds) {
+        for (;;) {
+            char buf[4096];
+            ssize_t nread = read(fd, buf, sizeof(buf));
+            if (nread == 0)
+                break;
+            if (nread < 0) {
+                perror("read");
+                return 2;
+            }
+            ssize_t nwritten = write(1, buf, nread);
+            if (nwritten < 0) {
+                perror("write");
+                return 3;
+            }
+            ASSERT(nwritten == nread);
         }
-        ssize_t nwritten = write(1, buf, nread);
-        if (nwritten < 0) {
-            perror("write");
-            return 3;
-        }
-        ASSERT(nwritten == nread);
+        close(fd);
     }
     return 0;
 }
