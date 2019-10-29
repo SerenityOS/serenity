b1655e189989b922335d176309e46d090e7b5392
balatt <56897347+balatt@users.noreply.github.com>
2019-10-29 00:23:59 -0400

    Userland:

--- Userland/cat.cpp
+++ Userland/cat.cpp
@@ -1,3 +1,4 @@
+#include <AK/Vector.h>
 #include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -8,15 +9,23 @@
 
 int main(int argc, char** argv)
 {
-    int i = 1;
-    int fd = 0;
-    char buf[4096];
-    do {
-        if (argc > 1 && (fd = open(argv[i], O_RDONLY)) == -1) {
-            fprintf(stderr, "Failed to open %s: %s\n", argv[i], strerror(errno));
-            continue;
+    Vector<int> fds;
+    if (argc > 1) {
+        for (int i = 0; i < argc; i++) {
+            int fd;
+            if ((fd = open(argv[i], O_RDONLY)) == -1) {
+                fprintf(stderr, "Failed to open %s: %s\n", argv[i], strerror(errno));
+                continue;
+            }
+            fds.append(fd);
         }
+    } else {
+        fds.append(0);
+    }
+
+    for (auto& fd : fds) {
         for (;;) {
+            char buf[4096];
             ssize_t nread = read(fd, buf, sizeof(buf));
             if (nread == 0)
                 break;
@@ -31,6 +40,7 @@ int main(int argc, char** argv)
             }
             ASSERT(nwritten == nread);
         }
-    } while (++i < argc);
+        close(fd);
+    }
     return 0;
 }
