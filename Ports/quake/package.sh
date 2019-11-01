0dcce84c7c363e8d8ed2f57730d9ca9298b66a16
Jesse Buhagiar <jesse.buhagiar@student.rmit.edu.au>
2019-11-01 11:59:18 +1100

    Ports: Added quake package
    Quake now will build and run on Serenity. There are a few issues,
    that'll stop you from playing currently, however, such as SDL
    not having any keyboard input, as well as `printf_internal` throwing
    an assertion over the `.` format specifier. However, the game launches
    perfectly.

---
+++ Ports/quake/package.sh
@@ -0,0 +1,9 @@
+#!/bin/bash ../.port_include.sh
+port=quake
+version=0.65
+workdir=SerenityQuake-master
+useconfigure=false
+curlopts="-L"
+files="https://github.com/SerenityOS/SerenityQuake/archive/master.tar.gz quake.tar.gz"
+makeopts="V=1 SYMBOLS_ON=Y"
+depends=SDL2
