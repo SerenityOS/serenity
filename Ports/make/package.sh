6993b396e6b0a994f86ae902f2e7f7125f3afd7f
Stephen Gregoratto <dev@sgregoratto.me>
2019-11-03 14:10:52 +1100

    Ports: update make to 4.2.1, disable Guile
    Some systems (e.g. Arch Linux) build their gmake with Guile support and
    thus have it installed. This patch disables Guile autodetection in the
    configure script. It also updates the version of gmake to 4.2.1.
    
    Fixes #645.

--- Ports/make/package.sh
+++ Ports/make/package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash ../.port_include.sh
 port=make
-version=4.2
+version=4.2.1
 useconfigure=true
-files="https://ftp.gnu.org/gnu/make/make-4.2.tar.bz2 make-4.2.tar.bz2"
-configopts="--target=i686-pc-serenity --with-sysroot=/"
+files="https://ftp.gnu.org/gnu/make/make-4.2.1.tar.bz2 make-4.2.1.tar.bz2"
+configopts="--target=i686-pc-serenity --with-sysroot=/ --without-guile"
