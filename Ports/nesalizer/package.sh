cd861a70189a8b70f11db29194e87fef57aecbab
Dan MacDonald <allcoms@gmail.com>
2019-11-04 14:18:27 +0000

    Ports: Add nesalizer NES emulator (#722)

---
+++ Ports/nesalizer/package.sh
@@ -0,0 +1,7 @@
+#!/bin/bash ../.port_include.sh
+port=nesalizer
+version=git
+curlopts="-L"
+workdir=nesalizer-master
+files="https://github.com/ulfalizer/nesalizer/archive/master.tar.gz nesalizer-master.tar.gz"
+depends=SDL2
