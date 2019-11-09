438c92be3c06783d115a7b58b387910d6e1f2096
Dan MacDonald <allcoms@gmail.com>
2019-11-09 19:53:26 +0000

    Ports: Fix libarchive port (#746)

--- Ports/libarchive/package.sh
+++ Ports/libarchive/package.sh
@@ -3,8 +3,4 @@ port=libarchive
 version=3.4.0
 useconfigure=true
 files="https://libarchive.org/downloads/libarchive-3.4.0.tar.gz libarchive-3.4.0.tar.gz"
-patchlevel=1
-
-configure() {
-    run ./"$configscript" --host=i686-pc-serenity $configopts
-}
+depends=zlib
