874c2b06cda6dd5ec7b7b02dac673c6da98734d4
Andreas Kling <awesomekling@gmail.com>
2019-11-09 11:31:03 +0100

    LibHTML+Browser: Add a simple DOM inspector popup window
    LibHTML now provides a DOMTreeModel which can be used to view a given
    Document's DOM tree. :^)

---
+++ Libraries/LibHTML/DOMTreeModel.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <LibGUI/GModel.h>
+
+class Document;
+
+class DOMTreeModel final : public GModel {
+public:
+    static NonnullRefPtr<DOMTreeModel> create(Document& document)
+    {
+        return adopt(*new DOMTreeModel(document));
+    }
+
+    virtual ~DOMTreeModel() override;
+
+    virtual int row_count(const GModelIndex& = GModelIndex()) const override;
+    virtual int column_count(const GModelIndex& = GModelIndex()) const override;
+    virtual GVariant data(const GModelIndex&, Role = Role::Display) const override;
+    virtual GModelIndex index(int row, int column, const GModelIndex& parent = GModelIndex()) const override;
+    virtual GModelIndex parent_index(const GModelIndex&) const override;
+    virtual void update() override;
+
+private:
+    explicit DOMTreeModel(Document&);
+
+    Document& m_document;
+
+    GIcon m_element_icon;
+    GIcon m_text_icon;
+};
