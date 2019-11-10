b23ea1b247a0f5cb278fe7b19e08b22482c117e7
Andreas Kling <awesomekling@gmail.com>
2019-11-10 11:10:04 +0100

    HackStudio: Add placeholder code to test widget factory construction
    We now use the magical widget registry to factory-construct widgets and
    place them into the form.
    
    This will need all kinds of work, but it's nice that the mechanism is
    working as intended.

--- DevTools/HackStudio/FormEditorWidget.h
+++ DevTools/HackStudio/FormEditorWidget.h
@@ -9,6 +9,9 @@ class FormEditorWidget final : public GScrollableWidget {
 public:
     virtual ~FormEditorWidget() override;
 
+    FormWidget& form_widget() { return *m_form_widget; }
+    const FormWidget& form_widget() const { return *m_form_widget; }
+
 private:
     virtual void paint_event(GPaintEvent&) override;
 
