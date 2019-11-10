ad383c0ab12348e31765a6f0c37987297779d2a5
Andreas Kling <awesomekling@gmail.com>
2019-11-10 13:09:35 +0100

    Ports: Configure libarchive with --without-xml2
    Otherwise it won't build on my machine.

--- Ports/libarchive/package.sh
+++ Ports/libarchive/package.sh
@@ -2,5 +2,6 @@
 port=libarchive
 version=3.4.0
 useconfigure=true
+configopts="--without-xml2"
 files="https://libarchive.org/downloads/libarchive-3.4.0.tar.gz libarchive-3.4.0.tar.gz"
 depends=zlib
