4d98f8b78538d6cb3d95e1ec3f9e2163b8bfc6d2
Sergey Bugaev <bugaevc@gmail.com>
2019-11-13 16:04:31 +0300

    Terminal+HackStudio: Fix leaking PTM fd to child processes
    The pseudoterminal *master* fd is not supposed to be inherited,
    so make sure to open it with O_CLOEXEC.

--- Applications/Terminal/main.cpp
+++ Applications/Terminal/main.cpp
@@ -148,7 +148,7 @@ int main(int argc, char** argv)
     if (chdir(get_current_user_home_path().characters()) < 0)
         perror("chdir");
 
-    int ptm_fd = open("/dev/ptmx", O_RDWR);
+    int ptm_fd = open("/dev/ptmx", O_RDWR | O_CLOEXEC);
     if (ptm_fd < 0) {
         perror("open(ptmx)");
         return 1;
