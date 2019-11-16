57eb37160c64e4b955e31a98ae05f70390c40717
Brandon Scott <xeons@users.noreply.github.com>
2019-11-16 02:26:18 -0600

    LibC: Added additional fields to lconv struct
    Added some additional fields to the lconv structure that were missing
    and set all their defaults.

--- Libraries/LibC/locale.h
+++ Libraries/LibC/locale.h
@@ -17,6 +17,27 @@ struct lconv {
     char* decimal_point;
     char* thousands_sep;
     char* grouping;
+    char* int_curr_symbol;
+    char* currency_symbol;
+    char* mon_decimal_point;
+    char* mon_thousands_sep;
+    char* mon_grouping;
+    char* positive_sign;
+    char* negative_sign;
+    char int_frac_digits;
+    char frac_digits;
+    char p_cs_precedes;
+    char p_sep_by_space;
+    char n_cs_precedes;
+    char n_sep_by_space;
+    char p_sign_posn;
+    char n_sign_posn;
+    char int_p_cs_precedes;
+    char int_p_sep_by_space;
+    char int_n_cs_precedes;
+    char int_n_sep_by_space;
+    char int_p_sign_posn;
+    char int_n_sign_posn;
 };
 
 struct lconv* localeconv();
