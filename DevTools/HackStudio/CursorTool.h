197ed1bb2a56677c6311d440d6246c9cd4b0a767
Andreas Kling <awesomekling@gmail.com>
2019-11-16 22:26:46 +0100

    HackStudio: Move the rubber-banding state into CursorTool
    I originally put it in FormWidget because CursorTool was clueless about
    painting. This patch adds Tool::on_second_paint() which allows all the
    tools to hook into the second paint pass.

--- DevTools/HackStudio/CursorTool.h
+++ DevTools/HackStudio/CursorTool.h
@@ -20,8 +20,16 @@ private:
     virtual void on_mouseup(GMouseEvent&) override;
     virtual void on_mousemove(GMouseEvent&) override;
     virtual void on_keydown(GKeyEvent&) override;
+    virtual void on_second_paint(GPainter&, GPaintEvent&) override;
+
+    void set_rubber_band_position(const Point&);
+    Rect rubber_band_rect() const;
 
     Point m_drag_origin;
     HashMap<GWidget*, Point> m_positions_before_drag;
     bool m_dragging { false };
+
+    bool m_rubber_banding { false };
+    Point m_rubber_band_origin;
+    Point m_rubber_band_position;
 };
