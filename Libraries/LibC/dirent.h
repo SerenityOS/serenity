e99da0ff26eb2df96dc78118014cf423a6524173
Brandon Scott <xeons@users.noreply.github.com>
2019-11-16 02:28:12 -0600

    LibC: Added some missing directory type constants
    Added some missing directory type constants to dirent.h

--- Libraries/LibC/dirent.h
+++ Libraries/LibC/dirent.h
@@ -4,6 +4,26 @@
 #include <sys/types.h>
 
 __BEGIN_DECLS
+enum {
+    DT_UNKNOWN = 0,
+#define DT_UNKNOWN DT_UNKNOWN
+    DT_FIFO = 1,
+#define DT_FIFO DT_FIFO
+    DT_CHR = 2,
+#define DT_CHR DT_CHR
+    DT_DIR = 4,
+#define DT_DIR DT_DIR
+    DT_BLK = 6,
+#define DT_BLK DT_BLK
+    DT_REG = 8,
+#define DT_REG DT_REG
+    DT_LNK = 10,
+#define DT_LNK DT_LNK
+    DT_SOCK = 12,
+#define DT_SOCK DT_SOCK
+    DT_WHT = 14
+#define DT_WHT DT_WHT
+};
 
 struct dirent {
     ino_t d_ino;
