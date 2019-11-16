c031d3aa9ff18671076604a2970800be28bd8dd7
Brandon Scott <xeons@users.noreply.github.com>
2019-11-16 02:26:18 -0600

    LibC: Added additional fields to lconv struct
    Added some additional fields to the lconv structure that were missing
    and set all their defaults.

--- Libraries/LibC/locale.cpp
+++ Libraries/LibC/locale.cpp
@@ -8,10 +8,34 @@ static char default_decimal_point[] = ".";
 static char default_thousands_sep[] = ",";
 static char default_grouping[] = "\x03\x03";
 
+static char default_empty_string[] = "";
+static char default_empty_value = 127;
+
 static struct lconv default_locale = {
     default_decimal_point,
     default_thousands_sep,
     default_grouping,
+    default_empty_string,
+    default_empty_string,
+    default_empty_string,
+    default_empty_string,
+    default_empty_string,
+    default_empty_string,
+    default_empty_string,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value
 };
 
 char* setlocale(int category, const char* locale)
