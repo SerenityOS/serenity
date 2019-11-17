5f459ee9296a491ee2a67fe758a9a82d8af8dcdf
Andreas Kling <awesomekling@gmail.com>
2019-11-17 12:53:50 +0100

    LibC: Unbreak endian.h
    There were some missing semicolons so this file didn't compile.
    Also add some macro paranoia parentheses while we're here.

--- Libraries/LibC/endian.h
+++ Libraries/LibC/endian.h
@@ -18,64 +18,49 @@ __BEGIN_DECLS
 
 #include <stdint.h>
 
-static __inline uint16_t __bswap16(uint16_t x)
-{
-	return __builtin_bswap16(x);
-}
-
-static __inline uint32_t __bswap32(uint32_t x)
-{
-	return __builtin_bswap32(x)
-}
-
-static __inline uint64_t __bswap64(uint64_t x)
-{
-	return __builtin_bswap64(x)
-}
-
 #define LITTLE_ENDIAN	__LITTLE_ENDIAN
 #define BIG_ENDIAN	__BIG_ENDIAN
 #define PDP_ENDIAN	__PDP_ENDIAN
 #define BYTE_ORDER	__BYTE_ORDER
 
 #if __BYTE_ORDER == __LITTLE_ENDIAN
-#define htole16(x) (uint16_t)(x)
-#define le16toh(x) (uint16_t)(x)
-#define letoh16(x) (uint16_t)(x)
-#define htole32(x) (uint32_t)(x)
-#define le32toh(x) (uint32_t)(x)
-#define letoh32(x) (uint32_t)(x)
-#define htole64(x) (uint64_t)(x)
-#define le64toh(x) (uint64_t)(x)
-#define letoh64(x) (uint64_t)(x)
-#define htobe16(x) __bswap16(x)
-#define be16toh(x) __bswap16(x)
-#define betoh16(x) __bswap16(x)
-#define htobe32(x) __bswap32(x)
-#define be32toh(x) __bswap32(x)
-#define betoh32(x) __bswap32(x)
-#define htobe64(x) __bswap64(x)
-#define be64toh(x) __bswap64(x)
-#define betoh64(x) __bswap64(x)
+#define htole16(x) ((uint16_t)(x))
+#define le16toh(x) ((uint16_t)(x))
+#define letoh16(x) ((uint16_t)(x))
+#define htole32(x) ((uint32_t)(x))
+#define le32toh(x) ((uint32_t)(x))
+#define letoh32(x) ((uint32_t)(x))
+#define htole64(x) ((uint64_t)(x))
+#define le64toh(x) ((uint64_t)(x))
+#define letoh64(x) ((uint64_t)(x))
+#define htobe16(x) (__builtin_bswap16(x))
+#define be16toh(x) (__builtin_bswap16(x))
+#define betoh16(x) (__builtin_bswap16(x))
+#define htobe32(x) (__builtin_bswap32(x))
+#define be32toh(x) (__builtin_bswap32(x))
+#define betoh32(x) (__builtin_bswap32(x))
+#define htobe64(x) (__builtin_bswap64(x))
+#define be64toh(x) (__builtin_bswap64(x))
+#define betoh64(x) (__builtin_bswap64(x))
 #else
-#define ltobe16(x) (uint16_t)(x)
-#define le16toh(x) (uint16_t)(x)
-#define letoh16(x) (uint16_t)(x)
-#define htole32(x) (uint32_t)(x)
-#define le32toh(x) (uint32_t)(x)
-#define letoh32(x) (uint32_t)(x)
-#define htole64(x) (uint64_t)(x)
-#define le64toh(x) (uint64_t)(x)
-#define letoh64(x) (uint64_t)(x)
-#define htole16(x) __bswap16(x)
-#define le16toh(x) __bswap16(x)
-#define letoh16(x) __bswap16(x)
-#define htole32(x) __bswap32(x)
-#define le32toh(x) __bswap32(x)
-#define letoh32(x) __bswap32(x)
-#define htole64(x) __bswap64(x)
-#define le64toh(x) __bswap64(x)
-#define letoh64(x) __bswap64(x)
+#define ltobe16(x) ((uint16_t)(x))
+#define le16toh(x) ((uint16_t)(x))
+#define letoh16(x) ((uint16_t)(x))
+#define htole32(x) ((uint32_t)(x))
+#define le32toh(x) ((uint32_t)(x))
+#define letoh32(x) ((uint32_t)(x))
+#define htole64(x) ((uint64_t)(x))
+#define le64toh(x) ((uint64_t)(x))
+#define letoh64(x) ((uint64_t)(x))
+#define htole16(x) (__builtin_bswap16(x))
+#define le16toh(x) (__builtin_bswap16(x))
+#define letoh16(x) (__builtin_bswap16(x))
+#define htole32(x) (__builtin_bswap32(x))
+#define le32toh(x) (__builtin_bswap32(x))
+#define letoh32(x) (__builtin_bswap32(x))
+#define htole64(x) (__builtin_bswap64(x))
+#define le64toh(x) (__builtin_bswap64(x))
+#define letoh64(x) (__builtin_bswap64(x))
 #endif
 
 #endif
