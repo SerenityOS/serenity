0ceb3c2da1daabe08f951637ba71836235638689
Andrew Kaster <andrewdkaster@gmail.com>
2019-11-17 20:08:10 -0700

    Kernel+LibPthread: pthread_create handles pthread_attr_t
    Add an initial implementation of pthread attributes for:
      * detach state (joinable, detached)
      * schedule params (just priority)
      * guard page size (as skeleton) (requires kernel support maybe?)
      * stack size and user-provided stack location (4 or 8 MB only, must be aligned)
    
    Add some tests too, to the thread test program.
    
    Also, LibC: Move pthread declarations to sys/types.h, where they belong.

--- Kernel/Process.h
+++ Kernel/Process.h
@@ -203,7 +203,7 @@ public:
     int sys$sched_setparam(pid_t pid, const struct sched_param* param);
     int sys$sched_getparam(pid_t pid, struct sched_param* param);
     int sys$restore_signal_mask(u32 mask);
-    int sys$create_thread(void* (*)(void*), void* argument, void* stack);
+    int sys$create_thread(void* (*)(void*), void* argument, const Syscall::SC_create_thread_params*);
     void sys$exit_thread(void*);
     int sys$join_thread(int tid, void** exit_value);
     int sys$rename(const char* oldpath, const char* newpath);
