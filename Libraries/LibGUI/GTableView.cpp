0b0017bbbd74685fbf731a93b96537bd1705f0d8
Andreas Kling <awesomekling@gmail.com>
2019-11-20 21:52:34 +0100

    LibGUI: Make the GTableView row background colors a little brighter
    This has been bothering me for a long time.. :^)

--- Libraries/LibGUI/GTableView.cpp
+++ Libraries/LibGUI/GTableView.cpp
@@ -284,11 +284,11 @@ void GTableView::paint_event(GPaintEvent& event)
             key_column_background_color = is_focused() ? Color::from_rgb(0x84351a) : Color::from_rgb(0x606060);
         } else {
             if (alternating_row_colors() && (painted_item_index % 2)) {
-                background_color = Color(210, 210, 210);
-                key_column_background_color = Color(180, 180, 180);
+                background_color = Color(220, 220, 220);
+                key_column_background_color = Color(200, 200, 200);
             } else {
                 background_color = Color::White;
-                key_column_background_color = Color(210, 210, 210);
+                key_column_background_color = Color(220, 220, 220);
             }
         }
         painter.fill_rect(row_rect(painted_item_index), background_color);
