3a8bde413e3ca5d3c995d871c4426a8e0445ea92
Andreas Kling <awesomekling@gmail.com>
2019-11-23 21:42:02 +0100

    LibCore: Have CNetworkJob protect itself during the on_finish callback
    Otherwise, the callback may trigger the destruction of the CNetworkJob
    and make it difficult to continue execution correctly.

--- Libraries/LibCore/CNetworkJob.cpp
+++ Libraries/LibCore/CNetworkJob.cpp
@@ -14,6 +14,10 @@ CNetworkJob::~CNetworkJob()
 
 void CNetworkJob::did_finish(NonnullRefPtr<CNetworkResponse>&& response)
 {
+    // NOTE: We protect ourselves here, since the on_finish callback may otherwise
+    //       trigger destruction of this job somehow.
+    NonnullRefPtr<CNetworkJob> protector(*this);
+
     m_response = move(response);
 #ifdef CNETWORKJOB_DEBUG
     dbg() << *this << " job did_finish!";
@@ -25,6 +29,10 @@ void CNetworkJob::did_finish(NonnullRefPtr<CNetworkResponse>&& response)
 
 void CNetworkJob::did_fail(Error error)
 {
+    // NOTE: We protect ourselves here, since the on_finish callback may otherwise
+    //       trigger destruction of this job somehow.
+    NonnullRefPtr<CNetworkJob> protector(*this);
+
     m_error = error;
 #ifdef CNETWORKJOB_DEBUG
     dbgprintf("%s{%p} job did_fail! error: %u (%s)\n", class_name(), this, (unsigned)error, to_string(error));
