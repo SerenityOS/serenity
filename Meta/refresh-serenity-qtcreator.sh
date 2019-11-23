a1fe639f9a2de5a79f0393a42809b4423f0abd81
Andreas Kling <awesomekling@gmail.com>
2019-11-23 16:00:09 +0100

    Meta: The Qt Creator .files refresh script should pick up .c files

--- Meta/refresh-serenity-qtcreator.sh
+++ Meta/refresh-serenity-qtcreator.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd "$SERENITY_ROOT" || exit 1
-find . -name '*.ipc' -or -name '*.cpp' -or -name '*.h' -or -name '*.S' -or -name '*.css' | grep -Fv Patches/ | grep -Fv Root/ | grep -Fv Ports/ | grep -Fv Toolchain/ | grep -Fv Base/ > serenity.files
+find . -name '*.ipc' -or -name '*.cpp' -or -name '*.c' -or -name '*.h' -or -name '*.S' -or -name '*.css' | grep -Fv Patches/ | grep -Fv Root/ | grep -Fv Ports/ | grep -Fv Toolchain/ | grep -Fv Base/ > serenity.files
