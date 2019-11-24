653e61d9cfaf8bf3986361f2f27bae1f8d07405f
Andreas Kling <awesomekling@gmail.com>
2019-11-24 13:20:44 +0100

    LibProtocol: Add a Download object so users don't have to manage ID's
    LibProtocol::Client::start_download() now gives you a Download object
    with convenient hooks (on_finish & on_progress).
    
    Also, the IPC handshake is snuck into the Client constructor, so you
    don't need to perform it after instantiating a Client.
    
    This makes using LibProtocol much more pleasant. :^)

---
+++ Libraries/LibProtocol/Download.cpp
@@ -0,0 +1,38 @@
+#include <LibC/SharedBuffer.h>
+#include <LibProtocol/Client.h>
+#include <LibProtocol/Download.h>
+
+namespace LibProtocol {
+
+Download::Download(Client& client, i32 download_id)
+    : m_client(client.make_weak_ptr())
+    , m_download_id(download_id)
+{
+}
+
+bool Download::stop()
+{
+    return m_client->stop_download({}, *this);
+}
+
+void Download::did_finish(Badge<Client>, bool success, u32 total_size, i32 shared_buffer_id)
+{
+    if (!on_finish)
+        return;
+
+    ByteBuffer payload;
+    RefPtr<SharedBuffer> shared_buffer;
+    if (success && shared_buffer_id != -1) {
+        shared_buffer = SharedBuffer::create_from_shared_buffer_id(shared_buffer_id);
+        payload = ByteBuffer::wrap(shared_buffer->data(), total_size);
+    }
+    on_finish(success, payload, move(shared_buffer));
+}
+
+void Download::did_progress(Badge<Client>, u32 total_size, u32 downloaded_size)
+{
+    if (on_progress)
+        on_progress(total_size, downloaded_size);
+}
+
+}
---
+++ Libraries/LibProtocol/Download.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <AK/Badge.h>
+#include <AK/ByteBuffer.h>
+#include <AK/Function.h>
+#include <AK/RefCounted.h>
+#include <AK/WeakPtr.h>
+
+class SharedBuffer;
+
+namespace LibProtocol {
+
+class Client;
+
+class Download : public RefCounted<Download> {
+public:
+    static NonnullRefPtr<Download> create_from_id(Badge<Client>, Client& client, i32 download_id)
+    {
+        return adopt(*new Download(client, download_id));
+    }
+
+    int id() const { return m_download_id; }
+    bool stop();
+
+    Function<void(bool success, const ByteBuffer& payload, RefPtr<SharedBuffer> payload_storage)> on_finish;
+    Function<void(u32 total_size, u32 downloaded_size)> on_progress;
+
+    void did_finish(Badge<Client>, bool success, u32 total_size, i32 shared_buffer_id);
+    void did_progress(Badge<Client>, u32 total_size, u32 downloaded_size);
+
+private:
+    explicit Download(Client&, i32 download_id);
+    WeakPtr<Client> m_client;
+    int m_download_id { -1 };
+};
+
+}
