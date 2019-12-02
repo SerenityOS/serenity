9e78f64877105bb17f35c58a8bbf73be8db22d37
Andrés Vieira <anvieiravazquez@gmail.com>
2019-12-02 15:01:08 +0100

    Userland: Return 28 instead of 30 for non-leap years on cal command
    There was a bug on the days for the month of February on non-leap years
    that would return 30 insted of 28, which is the actual value.

--- Userland/cal.cpp
+++ Userland/cal.cpp
@@ -16,7 +16,7 @@ int get_number_of_days(int month, int year)
     bool is_long_month = (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12);
 
     if (month == 2)
-        return is_leap_year ? 29 : 30;
+        return is_leap_year ? 29 : 28;
 
     return is_long_month ? 31 : 30;
 }
