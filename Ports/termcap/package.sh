86ae65282ee5032b121887d0d97385a2ba5a76be
Larkin Nickle <me@larbob.org>
2019-12-06 13:32:58 -0500

    Ports: Add termcap port

---
+++ Ports/termcap/package.sh
@@ -0,0 +1,6 @@
+#!/bin/bash ../.port_include.sh
+port=termcap
+version=1.3.1
+useconfigure=true
+configopts="--prefix=$SERENITY_ROOT/Root/usr"
+files="https://ftp.gnu.org/gnu/termcap/termcap-1.3.1.tar.gz termcap-1.3.1.tar.gz"
