51c43f865678ab2f996502c28b1952f393e2a26a
Andreas Kling <awesomekling@gmail.com>
2019-12-10 20:46:33 +0100

    LibCore: Make CHttpJob::response() return a CHttpResponse*
    We know that the CNetworkResponse inside a CHttpJob is always going to
    be a CHttpResponse, so we can return a casted pointer to be nice. :^)

--- Libraries/LibCore/CHttpJob.h
+++ Libraries/LibCore/CHttpJob.h
@@ -2,6 +2,7 @@
 
 #include <AK/HashMap.h>
 #include <LibCore/CHttpRequest.h>
+#include <LibCore/CHttpResponse.h>
 #include <LibCore/CNetworkJob.h>
 
 class CTCPSocket;
@@ -14,6 +15,9 @@ public:
 
     virtual void start() override;
     virtual void shutdown() override;
+
+    CHttpResponse* response() { return static_cast<CHttpResponse*>(CNetworkJob::response()); }
+    const CHttpResponse* response() const { return static_cast<const CHttpResponse*>(CNetworkJob::response()); }
 
 private:
     void on_socket_connected();
