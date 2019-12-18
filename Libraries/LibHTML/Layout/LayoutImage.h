85ac9705bac2e1a494884874ff343ceabe88d32a
Andreas Kling <awesomekling@gmail.com>
2019-12-18 20:52:36 +0100

    LibHTML: Add LayoutNode::is_image() and is<LayoutImage> helper

--- Libraries/LibHTML/Layout/LayoutImage.h
+++ Libraries/LibHTML/Layout/LayoutImage.h
@@ -19,4 +19,11 @@ public:
 
 private:
     virtual const char* class_name() const override { return "LayoutImage"; }
+    virtual bool is_image() const override { return true; }
 };
+
+template<>
+inline bool is<LayoutImage>(const LayoutNode& node)
+{
+    return node.is_image();
+}
