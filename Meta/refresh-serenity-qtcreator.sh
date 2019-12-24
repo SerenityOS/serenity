13dd3b70238b723c8e997a615ae76d31604f4257
anon <anon@T420>
2019-12-24 10:23:43 +0200

    Meta: refresh-serenity-qtcreator.sh now checks if SERENITY_ROOT is set

--- Meta/refresh-serenity-qtcreator.sh
+++ Meta/refresh-serenity-qtcreator.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+if [ ! -n "$SERENITY_ROOT" ]
+then echo "Serenety root not set."
+fi
+
 cd "$SERENITY_ROOT" || exit 1
 find . -name '*.ipc' -or -name '*.cpp' -or -name '*.c' -or -name '*.h' -or -name '*.S' -or -name '*.css' | grep -Fv Patches/ | grep -Fv Root/ | grep -Fv Ports/ | grep -Fv Toolchain/ | grep -Fv Base/ > serenity.files
