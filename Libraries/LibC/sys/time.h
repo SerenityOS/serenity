11f0ee5c4b41b5a4e06aea3b2ff52baf1d10d133
Mauri de Souza Nunes <mauri870@gmail.com>
2019-12-27 00:29:31 -0300

    LibC: Add timeval operations
    This commit add macros for the timeval operations timeradd, timersub,
    timercmp, timerisset, timerclear.

--- Libraries/LibC/sys/time.h
+++ Libraries/LibC/sys/time.h
@@ -5,6 +5,29 @@
 
 __BEGIN_DECLS
 
+#define timerclear(tvp) (tvp)->tv_sec = (tvp)->tv_usec = 0
+#define timerisset(tvp) ((tvp)->tv_sec || (tvp)->tv_usec)
+#define timercmp(tvp, uvp, cmp) \
+    (((tvp)->tv_sec == (uvp)->tv_sec) ? ((tvp)->tv_usec cmp(uvp)->tv_usec) : ((tvp)->tv_sec cmp(uvp)->tv_sec))
+#define timeradd(tvp, uvp)                \
+    do {                                  \
+        (tvp)->tv_sec += (uvp)->tv_sec;   \
+        (tvp)->tv_usec += (uvp)->tv_usec; \
+        if ((tvp)->tv_usec >= 1000000) {  \
+            (tvp)->tv_sec++;              \
+            (tvp)->tv_usec -= 1000000;    \
+        }                                 \
+    } while (0)
+#define timersub(tvp, uvp)                \
+    do {                                  \
+        (tvp)->tv_sec -= (uvp)->tv_sec;   \
+        (tvp)->tv_usec -= (uvp)->tv_usec; \
+        if ((tvp)->tv_usec < 0) {         \
+            (tvp)->tv_sec--;              \
+            (tvp)->tv_usec += 1000000;    \
+        }                                 \
+    } while (0)
+
 struct timeval {
     time_t tv_sec;
     suseconds_t tv_usec;
