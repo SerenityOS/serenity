c24fe710d7a4cb344f076d1138553e550e7ddbf8
Andrew Kaster <andrewdkaster@gmail.com>
2019-12-29 01:17:04 -0500

    AK: Turn off demangler in userland
    For some reason, the default CXXFLAGS and such don't get us the
    __cxa_demangle symbol in userland.

--- AK/Demangle.h
+++ AK/Demangle.h
@@ -7,15 +7,18 @@ namespace AK {
 
 inline String demangle(const char* name)
 {
+#ifdef KERNEL
     int status = 0;
     auto* demangled_name = abi::__cxa_demangle(name, nullptr, nullptr, &status);
     auto string = String(status == 0 ? demangled_name : name);
     if (status == 0)
         kfree(demangled_name);
     return string;
+#else
+    return name;
+#endif
 }
 
 }
 
 using AK::demangle;
-
