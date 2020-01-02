7cdfb9c6b4b1470a6d27ebb1817614df864aa782
Andreas Kling <awesomekling@gmail.com>
2020-01-02 14:55:19 +0100

    Browser+LibHTML: Add resolved element style to the DOM inspector
    When selecting an element in the browser's DOM inspector, we now also
    show the resolved CSS properties (and their values) for that element.
    
    Since the inspector was growing a bit more complex, I moved it out of
    the "show inspector" action callback and into its own class.
    
    In the future, we will probably want to migrate the inspector down to
    LibHTML to make it accessible to other clients of the library, but for
    now we can keep working on it inside Browser. :^)

---
+++ Applications/Browser/InspectorWidget.h
@@ -0,0 +1,20 @@
+#include <LibGUI/GWidget.h>
+
+class Document;
+class GTableView;
+class GTreeView;
+
+class InspectorWidget final : public GWidget {
+    C_OBJECT(InspectorWidget)
+public:
+    virtual ~InspectorWidget();
+
+    void set_document(Document*);
+
+private:
+    explicit InspectorWidget(GWidget* parent);
+
+    RefPtr<GTreeView> m_dom_tree_view;
+    RefPtr<GTableView> m_style_table_view;
+    RefPtr<Document> m_document;
+};
