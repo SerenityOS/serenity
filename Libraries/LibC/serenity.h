35e786e5c0c7d9bccf4cf6b2a9e31c3d97a5ef8f
Andreas Kling <awesomekling@gmail.com>
2020-01-03 09:10:55 +0100

    Lib: Remove Stopwatch class
    This was a hack used to profile things before we had a proper profiler.
    Since RDTSC is not available in userspace, this is not useful anymore.

--- Libraries/LibC/serenity.h
+++ Libraries/LibC/serenity.h
@@ -3,39 +3,6 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#ifdef __cplusplus
-
-struct Stopwatch {
-    union SplitQword {
-        struct {
-            uint32_t lsw;
-            uint32_t msw;
-        };
-        uint64_t qw { 0 };
-    };
-
-public:
-    Stopwatch(const char* name)
-        : m_name(name)
-    {
-        read_tsc(&m_start.lsw, &m_start.msw);
-    }
-
-    ~Stopwatch()
-    {
-        SplitQword end;
-        read_tsc(&end.lsw, &end.msw);
-        uint64_t diff = end.qw - m_start.qw;
-        dbgprintf("Stopwatch(%s): %Q ticks\n", m_name, diff);
-    }
-
-private:
-    const char* m_name { nullptr };
-    SplitQword m_start;
-};
-
-#endif // __cplusplus
-
 __BEGIN_DECLS
 
 int module_load(const char* path, size_t path_length);
