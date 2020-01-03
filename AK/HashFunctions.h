d5fea1b2352912d727afc50e2c91504033988e0c
Shannon Booth <shannon.ml.booth@gmail.com>
2020-01-03 19:56:33 +1300

    AK: Add a u64 Trait type
    This allows u64s to be used in HashMaps.

--- AK/HashFunctions.h
+++ AK/HashFunctions.h
@@ -17,3 +17,10 @@ inline unsigned pair_int_hash(u32 key1, u32 key2)
 {
     return int_hash((int_hash(key1) * 209) ^ (int_hash(key2 * 413)));
 }
+
+inline unsigned u64_hash(u64 key)
+{
+    u32 first = key & 0xFFFFFFFF;
+    u32 last = key >> 32;
+    return pair_int_hash(first, last);
+}
