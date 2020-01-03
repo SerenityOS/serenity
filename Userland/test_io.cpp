1127de0c3b0559ba9b878a0b36faf108896a5634
Andreas Kling <awesomekling@gmail.com>
2020-01-03 03:16:47 +0100

    test_io: Add a simple test program that abuses some I/O syscalls
    This exposes some very bad behaviors that will need fixing.

---
+++ Userland/test_io.cpp
@@ -0,0 +1,71 @@
+#include <AK/Assertions.h>
+#include <AK/Types.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#define EXPECT_OK(syscall, address, size)                                                                                                   \
+    do {                                                                                                                                    \
+        rc = syscall(fd, (void*)(address));                                                                                                 \
+        if (rc < 0) {                                                                                                                       \
+            fprintf(stderr, "Expected success: " #syscall "(%p, %zu), got rc=%d, errno=%d\n", (void*)(address), (size_t)(size), rc, errno); \
+        }                                                                                                                                   \
+    } while (0)
+
+#define EXPECT_ERROR_2(err, syscall, arg1, arg2)                                                                                                                          \
+    do {                                                                                                                                                                  \
+        rc = syscall(arg1, arg2);                                                                                                                                         \
+        if (rc >= 0 || errno != err) {                                                                                                                                    \
+            fprintf(stderr, __FILE__ ":%d: Expected " #err ": " #syscall "(%p, %p), got rc=%d, errno=%d\n", __LINE__, (const void*)(arg1), (const void*)arg2, rc, errno); \
+        }                                                                                                                                                                 \
+    } while (0)
+
+#define EXPECT_ERROR_3(err, syscall, arg1, arg2, arg3)                                                                                                                                               \
+    do {                                                                                                                                                                                             \
+        rc = syscall(arg1, arg2, arg3);                                                                                                                                                              \
+        if (rc >= 0 || errno != err) {                                                                                                                                                               \
+            fprintf(stderr, __FILE__ ":%d: Expected " #err ": " #syscall "(%p, %p, %p), got rc=%d, errno=%d\n", __LINE__, (const void*)(arg1), (const void*)(arg2), (const void*)(arg3), rc, errno); \
+        }                                                                                                                                                                                            \
+    } while (0)
+
+void test_read_from_directory()
+{
+    char buffer[BUFSIZ];
+    int fd = open("/", O_DIRECTORY | O_RDONLY);
+    ASSERT(fd >= 0);
+    int rc;
+    EXPECT_ERROR_3(EISDIR, read, fd, buffer, sizeof(buffer));
+    rc = close(fd);
+    ASSERT(rc == 0);
+}
+
+void test_write_to_directory()
+{
+    char str[] = "oh frick";
+    int fd = open("/", O_DIRECTORY | O_RDONLY);
+    if (fd < 0)
+        perror("open");
+    ASSERT(fd >= 0);
+    int rc;
+    EXPECT_ERROR_3(EBADF, write, fd, str, sizeof(str));
+    rc = close(fd);
+    ASSERT(rc == 0);
+}
+
+int main(int, char**)
+{
+    int rc;
+    EXPECT_ERROR_2(ENOTDIR, open, "/dev/zero", (O_DIRECTORY | O_RDONLY));
+    EXPECT_ERROR_2(EINVAL, open, "/dev/zero", (O_DIRECTORY | O_CREAT | O_RDWR));
+    EXPECT_ERROR_2(EEXIST, open, "/dev/zero", (O_CREAT | O_EXCL | O_RDWR));
+    EXPECT_ERROR_2(EINVAL, open, "/tmp/abcdef", (O_DIRECTORY | O_CREAT | O_RDWR));
+    EXPECT_ERROR_2(EACCES, open, "/proc/all", (O_RDWR));
+    EXPECT_ERROR_2(ENOENT, open, "/boof/baaf/nonexistent", (O_CREAT | O_RDWR));
+    EXPECT_ERROR_2(EISDIR, open, "/tmp", (O_DIRECTORY | O_RDWR));
+
+    test_read_from_directory();
+    test_write_to_directory();
+
+    return 0;
+}
