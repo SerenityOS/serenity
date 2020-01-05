ad676378b04cec304c7878690f4be5546516e610
Andreas Kling <awesomekling@gmail.com>
2020-01-05 15:11:49 +0100

    LibCore: IDAllocator should never vend ID 0
    This was tripping up CObject which interprets timer ID 0 as "no timer".
    Once we got ID 0 assigned, it was impossible to turn it off and it
    would fire on every event loop iteration, causing CPU churn.

--- AK/IDAllocator.h
+++ AK/IDAllocator.h
@@ -16,6 +16,9 @@ public:
         int r = rand();
         for (int i = 0; i < 100000; ++i) {
             int allocated_id = r + i;
+            // Make sure we never vend ID 0, as some code may interpret that as "no ID"
+            if (allocated_id == 0)
+                ++allocated_id;
             if (!m_allocated_ids.contains(allocated_id)) {
                 m_allocated_ids.set(allocated_id);
                 return allocated_id;
