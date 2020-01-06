53bda09d15b9c48cc340e8ed3e09d734f1933c93
Andreas Kling <awesomekling@gmail.com>
2020-01-06 12:23:30 +0100

    Kernel: Make utime() take path+length, remove SmapDisabler

--- Libraries/LibC/utime.cpp
+++ Libraries/LibC/utime.cpp
@@ -1,12 +1,17 @@
 #include <Kernel/Syscall.h>
 #include <errno.h>
+#include <string.h>
 #include <utime.h>
 
 extern "C" {
 
 int utime(const char* pathname, const struct utimbuf* buf)
 {
-    int rc = syscall(SC_utime, (u32)pathname, (u32)buf);
+    if (!pathname) {
+        errno = EFAULT;
+        return -1;
+    }
+    int rc = syscall(SC_utime, pathname, strlen(pathname), buf);
     __RETURN_WITH_ERRNO(rc, rc, -1);
 }
 }
