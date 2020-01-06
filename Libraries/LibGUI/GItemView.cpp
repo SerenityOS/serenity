9b543363885138538f43061b224194f19e76d402
Dov Alperin <dzalperin@gmail.com>
2020-01-06 17:58:08 -0500

    LibGUI: clicking and dragging one item will drag other items in selection
    Previously if more than one item was selected clicking on one of
    them and dragging would de-select everything that is not the one that
    was clicked on. Now, if more than one items are selected and there
    is a mousedown it goes into a "mightdrag" state.
    The user can then perform a drag, if they don't everything that is not
    the item being clicked gets unselected in the mouseup event, mimicking
    the previous behavior.

--- Libraries/LibGUI/GItemView.cpp
+++ Libraries/LibGUI/GItemView.cpp
@@ -124,6 +124,8 @@ void GItemView::mousedown_event(GMouseEvent& event)
             auto index = model()->index(item_index, m_model_column);
             if (event.modifiers() & Mod_Ctrl)
                 selection().toggle(index);
+            else if (selection().size() > 1)
+                m_might_drag = true;
             else
                 selection().set(index);
         }
@@ -138,6 +140,12 @@ void GItemView::mouseup_event(GMouseEvent& event)
         m_rubber_banding = false;
         update();
         return;
+    }
+    int item_index = item_at_event_position(event.position());
+    auto index = model()->index(item_index, m_model_column);
+    if((selection().size() > 1) & m_might_drag) {
+        selection().set(index);
+        m_might_drag = false;
     }
     GAbstractView::mouseup_event(event);
 }
