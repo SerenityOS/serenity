9681d41bf0b56257ea1642844d6d2fbdfe11a8ec
Andrew Kaster <andrewdkaster@gmail.com>
2020-01-09 00:35:32 -0700

    AK: Add ArmedScopeGuard, a scope guard that can be disarmed

--- AK/ScopeGuard.h
+++ AK/ScopeGuard.h
@@ -21,6 +21,28 @@ private:
     Callback m_callback;
 };
 
+template<typename Callback>
+class ArmedScopeGuard {
+public:
+    ArmedScopeGuard(Callback callback)
+        : m_callback(move(callback))
+    {
+    }
+
+    ~ArmedScopeGuard()
+    {
+        if (m_armed)
+            m_callback();
+    }
+
+    void disarm() { m_armed = false; }
+
+private:
+    Callback m_callback;
+    bool m_armed { true };
+};
+
 }
 
 using AK::ScopeGuard;
+using AK::ArmedScopeGuard;
