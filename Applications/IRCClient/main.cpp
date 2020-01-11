2b20e8e187553d0f10a3a46fb4c91f99ba73a52d
Andreas Kling <awesomekling@gmail.com>
2020-01-11 23:53:54 +0100

    IRCClient: Use pledge()

--- Applications/IRCClient/main.cpp
+++ Applications/IRCClient/main.cpp
@@ -5,7 +5,17 @@
 
 int main(int argc, char** argv)
 {
+    if (pledge("stdio inet dns unix shared_buffer cpath rpath fattr", nullptr) < 0) {
+        perror("pledge");
+        return 1;
+    }
+
     GApplication app(argc, argv);
+
+    if (pledge("stdio inet dns unix shared_buffer rpath", nullptr) < 0) {
+        perror("pledge");
+        return 1;
+    }
 
     IRCAppWindow app_window;
     app_window.show();
