768dd5bad46398d8baa54beef753c5306fe87081
Andreas Kling <awesomekling@gmail.com>
2020-01-11 21:27:17 +0100

    AudioServer: Use pledge()

--- Servers/AudioServer/main.cpp
+++ Servers/AudioServer/main.cpp
@@ -4,6 +4,14 @@
 
 int main(int, char**)
 {
+    if (pledge("stdio thread shared_buffer rpath wpath cpath unix fattr", nullptr) < 0) {
+        perror("pledge");
+        return 1;
+    }
     ASEventLoop event_loop;
+    if (pledge("stdio thread shared_buffer rpath wpath unix", nullptr) < 0) {
+        perror("pledge");
+        return 1;
+    }
     return event_loop.exec();
 }
