0d41679f634d232564305e0df3587cb531e33129
Andreas Kling <awesomekling@gmail.com>
2020-01-11 21:17:47 +0100

    Taskbar: Use pledge()

--- Applications/Taskbar/main.cpp
+++ Applications/Taskbar/main.cpp
@@ -1,10 +1,22 @@
 #include "TaskbarWindow.h"
 #include <LibGUI/GApplication.h>
 #include <signal.h>
+#include <stdio.h>
 
 int main(int argc, char** argv)
 {
+    if (pledge("stdio shared_buffer proc rpath unix cpath fattr", nullptr) < 0) {
+        perror("pledge");
+        return 1;
+    }
+
     GApplication app(argc, argv);
+
+    if (pledge("stdio shared_buffer proc rpath unix", nullptr) < 0) {
+        perror("pledge");
+        return 1;
+    }
+
     TaskbarWindow window;
     window.show();
 
