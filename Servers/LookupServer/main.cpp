82af3368f7a4516cc683ac27ca52cdfccb2df6e7
Brian Gianforcaro <b.gianfo@gmail.com>
2020-01-12 16:46:03 -0800

    LookupServer: Use pledge()

--- Servers/LookupServer/main.cpp
+++ Servers/LookupServer/main.cpp
@@ -1,13 +1,24 @@
 #include "LookupServer.h"
 #include <LibCore/CEventLoop.h>
+#include <stdio.h>
 
 int main(int argc, char** argv)
 {
     (void)argc;
     (void)argv;
 
+    if (pledge("stdio unix inet cpath rpath fattr", nullptr) < 0) {
+        perror("pledge");
+        return 1;
+    }
+
     CEventLoop event_loop;
     LookupServer server;
+
+    if (pledge("stdio unix inet", nullptr) < 0) {
+        perror("pledge");
+        return 1;
+    }
 
     return event_loop.exec();
 }
