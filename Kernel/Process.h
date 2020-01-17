7ed02f1123414244fb429978d72f44727a042415
Sergey Bugaev <bugaevc@gmail.com>
2020-01-17 21:48:44 +0300

    Kernel+LibC: Unify sys$open() and sys$openat()
    The syscall is now called sys$open(), but it behaves like the old sys$openat().
    In userspace, open_with_path_length() is made a wrapper over openat_with_path_length().

--- Kernel/Process.h
+++ Kernel/Process.h
@@ -145,7 +145,6 @@ public:
     pid_t sys$getppid();
     mode_t sys$umask(mode_t);
     int sys$open(const Syscall::SC_open_params*);
-    int sys$openat(const Syscall::SC_openat_params*);
     int sys$close(int fd);
     ssize_t sys$read(int fd, u8*, ssize_t);
     ssize_t sys$write(int fd, const u8*, ssize_t);
