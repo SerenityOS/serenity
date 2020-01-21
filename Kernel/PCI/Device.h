1ee37245cd7342a649d50653b137e4ca626e88b4
Liav A <liavalb@gmail.com>
2020-01-21 13:20:16 +0200

    Kernel: Introduce IRQ sharing support
    The support is very basic - Each component that needs to handle IRQs
    inherits from InterruptHandler class. When the InterruptHandler
    constructor is called it registers itself in a SharedInterruptHandler.
    When an IRQ is fired, the SharedInterruptHandler is invoked, then it
    iterates through a list of the registered InterruptHandlers.
    
    Also, InterruptEnabler class was created to provide a way to enable IRQ
    handling temporarily, similar to InterruptDisabler (in CPU.h, which does
    the opposite).
    
    In addition to that a PCI::Device class has been added, that inherits
    from InterruptHandler.

---
+++ Kernel/PCI/Device.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2020, Liav A. <liavalb@hotmail.co.il>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include <AK/Types.h>
+#include <Kernel/InterruptHandler.h>
+#include <Kernel/PCI/Definitions.h>
+
+class PCI::Device : public InterruptHandler {
+public:
+    Address get_pci_address() const { return m_pci_address; };
+
+protected:
+    Device(Address pci_address);
+    Device(Address pci_address, u8 interrupt_vector);
+    ~Device();
+
+private:
+    Address m_pci_address;
+};
