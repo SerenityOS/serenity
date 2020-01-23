b90af9b1edfd14b106e92c50c9baea41e3c7feea
Brian Callahan <ibara@users.noreply.github.com>
2020-01-23 14:41:40 -0500

    Ports: Add mawk port (#1117)

---
+++ Ports/mawk/package.sh
@@ -0,0 +1,5 @@
+#!/bin/bash ../.port_include.sh
+port=mawk
+version=1.3.4-20200120
+files="https://invisible-mirror.net/archives/mawk/mawk-1.3.4-20200120.tgz mawk-1.3.4-20200120.tgz"
+useconfigure=true
