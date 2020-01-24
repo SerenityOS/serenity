8cef209be526351655d3bf877e3cda5d5ea74a4d
Sergey Bugaev <bugaevc@serenityos.org>
2020-01-24 16:45:29 +0300

    Meta: Claim copyright for files created by me
    This changes copyright holder to myself for the source code files that I've
    created or have (almost) completely rewritten. Not included are the files
    that were significantly changed by others even though it was me who originally
    created them (think HtmlView), or the many other files I've contributed code to.

--- Libraries/LibGUI/GColumnsView.h
+++ Libraries/LibGUI/GColumnsView.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2020, Sergey Bugaev <bugaevc@serenityos.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
