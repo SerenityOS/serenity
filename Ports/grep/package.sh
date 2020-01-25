d847368c8832da0ecb1363b1b7652c54a445cf81
Brian Callahan <bcallah@openbsd.org>
2020-01-25 12:23:47 -0500

    Ports: Add grep port

---
+++ Ports/grep/package.sh
@@ -0,0 +1,6 @@
+#!/bin/bash ../.port_include.sh
+port=grep
+version=2.5.4
+files="https://ftp.gnu.org/gnu/grep/grep-2.5.4.tar.gz grep-2.5.4.tar.gz"
+useconfigure=true
+configopts=--disable-perl-regexp
