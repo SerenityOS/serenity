61f06a02ede850a8702a6e7c36f8a9a1af9db182
Brian Callahan <bcallah@openbsd.org>
2020-01-25 21:20:39 -0500

    Ports: Add mandoc port

---
+++ Ports/mandoc/package.sh
@@ -0,0 +1,6 @@
+#!/bin/bash ../.port_include.sh
+port=mandoc
+version=1.14.5
+useconfigure=true
+files="https://mandoc.bsd.lv/snapshots/mandoc-1.14.5.tar.gz mandoc-1.14.5.tar.gz"
+depends="less pcre2 zlib"
