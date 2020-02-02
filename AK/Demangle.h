8ccf0e16a98d872d82226adf9e844122b3e76576
Andreas Kling <kling@serenityos.org>
2020-02-02 20:15:58 +0100

    AK: Turn demangling back on for userspace
    This didn't work at some point, but now it apparently works again. :^)

--- AK/Demangle.h
+++ AK/Demangle.h
@@ -33,16 +33,12 @@ namespace AK {
 
 inline String demangle(const StringView& name)
 {
-#ifdef KERNEL
     int status = 0;
     auto* demangled_name = abi::__cxa_demangle(String(name).characters(), nullptr, nullptr, &status);
     auto string = String(status == 0 ? demangled_name : name);
     if (status == 0)
         kfree(demangled_name);
     return string;
-#else
-    return name;
-#endif
 }
 
 }
