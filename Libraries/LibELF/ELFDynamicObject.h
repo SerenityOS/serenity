1c5e0e4fcf1c2c9b217de2d126d3dce56e3b0230
Andrew Kaster <andrewdkaster@gmail.com>
2020-02-01 17:57:12 -0700

    Meta: Claim copyright on files added by me
    Demos/DynamicLink, LibC/cxxabi.cpp, and LibELF/ELFDynamic*.[cpp/h]

--- Libraries/LibELF/ELFDynamicObject.h
+++ Libraries/LibELF/ELFDynamicObject.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2019-2020, Andrew Kaster <andrewdkaster@gmail.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
