c653a62ea7fce4fa0f76fea9e464e604f7fdcefe
joshua stein <jcs@jcs.org>
2020-02-04 15:22:23 -0600

    LibC: Move struct stat to sys/stat.h

--- Libraries/LibC/sys/stat.h
+++ Libraries/LibC/sys/stat.h
@@ -40,6 +40,22 @@ __BEGIN_DECLS
 #define S_ISLNK(m) (((m)&S_IFMT) == S_IFLNK)
 #define S_ISSOCK(m) (((m)&S_IFMT) == S_IFSOCK)
 
+struct stat {
+    dev_t st_dev;         /* ID of device containing file */
+    ino_t st_ino;         /* inode number */
+    mode_t st_mode;       /* protection */
+    nlink_t st_nlink;     /* number of hard links */
+    uid_t st_uid;         /* user ID of owner */
+    gid_t st_gid;         /* group ID of owner */
+    dev_t st_rdev;        /* device ID (if special file) */
+    off_t st_size;        /* total size, in bytes */
+    blksize_t st_blksize; /* blocksize for file system I/O */
+    blkcnt_t st_blocks;   /* number of 512B blocks allocated */
+    time_t st_atime;      /* time of last access */
+    time_t st_mtime;      /* time of last modification */
+    time_t st_ctime;      /* time of last status change */
+};
+
 mode_t umask(mode_t);
 int chmod(const char* pathname, mode_t);
 int fchmod(int fd, mode_t);
