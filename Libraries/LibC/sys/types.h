868b1a24bb98c095271e8c08d96a23d83ab8b112
joshua stein <jcs@jcs.org>
2020-02-04 15:22:23 -0600

    LibC: Move struct stat to sys/stat.h

--- Libraries/LibC/sys/types.h
+++ Libraries/LibC/sys/types.h
@@ -67,22 +67,6 @@ typedef uint32_t clock_t;
 #define __socklen_t uint32_t
 typedef __socklen_t socklen_t;
 
-struct stat {
-    dev_t st_dev;         /* ID of device containing file */
-    ino_t st_ino;         /* inode number */
-    mode_t st_mode;       /* protection */
-    nlink_t st_nlink;     /* number of hard links */
-    uid_t st_uid;         /* user ID of owner */
-    gid_t st_gid;         /* group ID of owner */
-    dev_t st_rdev;        /* device ID (if special file) */
-    off_t st_size;        /* total size, in bytes */
-    blksize_t st_blksize; /* blocksize for file system I/O */
-    blkcnt_t st_blocks;   /* number of 512B blocks allocated */
-    time_t st_atime;      /* time of last access */
-    time_t st_mtime;      /* time of last modification */
-    time_t st_ctime;      /* time of last status change */
-};
-
 struct utimbuf {
     time_t actime;
     time_t modtime;
