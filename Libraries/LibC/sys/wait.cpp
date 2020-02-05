756d3f70803a004699f2ef07c6fe0ade5174a67b
Sergey Bugaev <bugaevc@serenityos.org>
2020-02-05 19:12:14 +0300

    LibC: Fix misplaced brace
    It doesn't really make a difference since waitpid() is declared to have
    extern "C" linkage in the header anyway, but still.

--- Libraries/LibC/sys/wait.cpp
+++ Libraries/LibC/sys/wait.cpp
@@ -35,10 +35,10 @@ pid_t wait(int* wstatus)
 {
     return waitpid(-1, wstatus, 0);
 }
-}
 
 pid_t waitpid(pid_t waitee, int* wstatus, int options)
 {
     int rc = syscall(SC_waitpid, waitee, wstatus, options);
     __RETURN_WITH_ERRNO(rc, rc, -1);
+}
 }
