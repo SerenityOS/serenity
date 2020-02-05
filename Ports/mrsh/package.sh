c4b0f69debe715c23bfaf04e0f91db9126910c20
Sergey Bugaev <bugaevc@serenityos.org>
2020-02-05 19:53:19 +0300

    Ports: Add mrsh port

---
+++ Ports/mrsh/package.sh
@@ -0,0 +1,7 @@
+#!/bin/bash ../.port_include.sh
+port=mrsh
+version=d9763a32e7da572677d1681bb1fc67f117d641f3
+files="https://codeload.github.com/emersion/mrsh/legacy.tar.gz/d9763a32e7da572677d1681bb1fc67f117d641f3 emersion-mrsh-d9763a3.tar.gz"
+useconfigure=true
+makeopts=
+workdir=emersion-mrsh-d9763a3
