f9e69ba72ad8a55aa31d0e07eadadae58cffdc2b
Liav A <liavalb@gmail.com>
2020-02-09 16:28:56 +0200

    LibBareMetal: Creating a new library for freestanding environments

---
+++ Libraries/LibBareMetal/Output/Console.cpp
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <LibBareMetal/IO.h>
+#include <LibBareMetal/Output/Console.h>
+#include <LibBareMetal/Output/kstdio.h>
+
+// Bytes output to 0xE9 end up on the Bochs console. It's very handy.
+#define CONSOLE_OUT_TO_E9
+
+static Console* s_the;
+
+Console& Console::the()
+{
+    ASSERT(s_the);
+    return *s_the;
+}
+
+bool Console::is_initialized()
+{
+    return s_the != nullptr;
+}
+
+Console::Console()
+#if defined(KERNEL)
+    : CharacterDevice(5, 1)
+#endif
+{
+    s_the = this;
+}
+
+Console::~Console()
+{
+}
+
+#if defined(KERNEL)
+bool Console::can_read(const FileDescription&) const
+{
+    return false;
+}
+
+ssize_t Console::read(FileDescription&, u8*, ssize_t)
+{
+    // FIXME: Implement reading from the console.
+    //        Maybe we could use a ring buffer for this device?
+    return 0;
+}
+
+ssize_t Console::write(FileDescription&, const u8* data, ssize_t size)
+{
+    if (!size)
+        return 0;
+    if (!m_implementation)
+        return 0;
+    for (ssize_t i = 0; i < size; ++i)
+        put_char(data[i]);
+    return size;
+}
+#endif
+
+void Console::put_char(char ch)
+{
+#ifdef CONSOLE_OUT_TO_E9
+    //if (ch != 27)
+    IO::out8(0xe9, ch);
+#endif
+    m_logbuffer.enqueue(ch);
+    if (m_implementation)
+        m_implementation->on_sysconsole_receive(ch);
+}
+
+ConsoleImplementation::~ConsoleImplementation()
+{
+}
---
+++ Libraries/LibBareMetal/Output/kprintf.cpp
@@ -0,0 +1,167 @@
+/*
+ * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <AK/PrintfImplementation.h>
+#include <AK/Types.h>
+#include <LibBareMetal/IO.h>
+#include <LibBareMetal/Output/Console.h>
+#include <LibBareMetal/Output/kstdio.h>
+
+#include <LibC/stdarg.h>
+
+#if defined(KERNEL)
+#    include <Kernel/Process.h>
+#endif
+
+static bool serial_debug;
+
+void set_serial_debug(bool on_or_off)
+{
+    serial_debug = on_or_off;
+}
+
+int get_serial_debug()
+{
+    return serial_debug;
+}
+
+static void color_on()
+{
+    IO::out8(0xe9, 0x1b);
+    IO::out8(0xe9, '[');
+    IO::out8(0xe9, '3');
+    IO::out8(0xe9, '6');
+    IO::out8(0xe9, 'm');
+}
+
+static void color_off()
+{
+    IO::out8(0xe9, 0x1b);
+    IO::out8(0xe9, '[');
+    IO::out8(0xe9, '0');
+    IO::out8(0xe9, 'm');
+}
+
+static void serial_putch(char ch)
+{
+    static bool serial_ready = false;
+    static bool was_cr = false;
+
+    if (!serial_ready) {
+        IO::out8(0x3F8 + 1, 0x00);
+        IO::out8(0x3F8 + 3, 0x80);
+        IO::out8(0x3F8 + 0, 0x02);
+        IO::out8(0x3F8 + 1, 0x00);
+        IO::out8(0x3F8 + 3, 0x03);
+        IO::out8(0x3F8 + 2, 0xC7);
+        IO::out8(0x3F8 + 4, 0x0B);
+
+        serial_ready = true;
+    }
+
+    while ((IO::in8(0x3F8 + 5) & 0x20) == 0)
+        ;
+
+    if (ch == '\n' && !was_cr)
+        IO::out8(0x3F8, '\r');
+
+    IO::out8(0x3F8, ch);
+
+    if (ch == '\r')
+        was_cr = true;
+    else
+        was_cr = false;
+}
+
+static void console_putch(char*&, char ch)
+{
+    if (serial_debug)
+        serial_putch(ch);
+
+    // It would be bad to reach the assert in Console()::the() and do a stack overflow
+
+    if (Console::is_initialized()) {
+        Console::the().put_char(ch);
+    } else {
+        IO::out8(0xe9, ch);
+    }
+}
+
+int kprintf(const char* fmt, ...)
+{
+    color_on();
+    va_list ap;
+    va_start(ap, fmt);
+    int ret = printf_internal(console_putch, nullptr, fmt, ap);
+    va_end(ap);
+    color_off();
+    return ret;
+}
+
+static void buffer_putch(char*& bufptr, char ch)
+{
+    *bufptr++ = ch;
+}
+
+int sprintf(char* buffer, const char* fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    int ret = printf_internal(buffer_putch, buffer, fmt, ap);
+    buffer[ret] = '\0';
+    va_end(ap);
+    return ret;
+}
+
+static void debugger_out(char ch)
+{
+    if (serial_debug)
+        serial_putch(ch);
+    IO::out8(0xe9, ch);
+}
+
+static void debugger_putch(char*&, char ch)
+{
+    debugger_out(ch);
+}
+
+extern "C" int dbgputstr(const char* characters, int length)
+{
+    for (int i = 0; i < length; ++i)
+        debugger_out(characters[i]);
+    return 0;
+}
+
+extern "C" int dbgprintf(const char* fmt, ...)
+{
+    color_on();
+    va_list ap;
+    va_start(ap, fmt);
+    int ret = printf_internal(debugger_putch, nullptr, fmt, ap);
+    va_end(ap);
+    color_off();
+    return ret;
+}
