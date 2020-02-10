1bdbdd7c735472e6879aa981730738020625a372
William McPherson <willmcpherson2@gmail.com>
2020-02-10 20:53:52 +1100

    Userland: Fix segfault in chown
    chown takes 2 arguments, not 1.

--- Userland/chown.cpp
+++ Userland/chown.cpp
@@ -39,7 +39,7 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    if (argc < 2) {
+    if (argc < 3) {
         printf("usage: chown <uid[:gid]> <path>\n");
         return 0;
     }
