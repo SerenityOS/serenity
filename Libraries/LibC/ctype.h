57e44100445805ca3982632a8dc6f102435d0b30
Andreas Kling <kling@serenityos.org>
2020-02-11 19:36:18 +0100

    Revert "LibC: Remove isfoo() declarations from ctype.h"
    This reverts commit 2ba06662b67439facf7fb81d8b8b3d53cf7ee771.
    
    This broke the toolchain build. Apparently libstdc++v3 depends on the
    declarations of isfoo() helpers.

--- Libraries/LibC/ctype.h
+++ Libraries/LibC/ctype.h
@@ -45,6 +45,18 @@ extern const char _ctype_[256];
 
 int tolower(int);
 int toupper(int);
+int isalnum(int);
+int isalpha(int);
+int iscntrl(int);
+int isdigit(int);
+int isxdigit(int);
+int isspace(int);
+int ispunct(int);
+int isprint(int);
+int isgraph(int);
+int islower(int);
+int isupper(int);
+int isblank(int);
 
 #define isalnum(c) (_ctype_[(int)(c)] & (_U | _L | _N))
 #define isalpha(c) (_ctype_[(int)(c)] & (_U | _L))
