beea49a72c47f19f45c8d296d6e9837d600bcaf5
Andreas Kling <kling@serenityos.org>
2020-02-11 19:42:02 +0100

    LibCore: Add a basic Core::DateTime class
    This is just to have a pleasant way to print the current time for now:
    
        dbg() << Core::DateTime::now();
    
    Or if you want it as a string:
    
        Core::DateTime::now().to_string();

---
+++ Libraries/LibCore/DateTime.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <LibCore/DateTime.h>
+#include <sys/time.h>
+#include <time.h>
+
+namespace Core {
+
+DateTime DateTime::now()
+{
+    auto timestamp = time(nullptr);
+    struct tm tm;
+    localtime_r(&timestamp, &tm);
+    DateTime dt;
+    dt.m_year = tm.tm_year + 1900;
+    dt.m_month = tm.tm_mon + 1;
+    dt.m_day = tm.tm_mday;
+    dt.m_hour = tm.tm_hour;
+    dt.m_minute = tm.tm_min;
+    dt.m_second = tm.tm_sec;
+    dt.m_timestamp = timestamp;
+    return dt;
+}
+
+String DateTime::to_string() const
+{
+    return String::format("%04u-%02u-%02u %02u:%02u:%02u", m_year, m_month, m_day, m_hour, m_minute, m_day);
+}
+
+}
