576e6bd982ffeba62adc519020ca7b632a0d9efe
Andreas Kling <kling@serenityos.org>
2020-02-18 11:01:31 +0100

    whoami: Use pledge() and unveil()

--- Userland/whoami.cpp
+++ Userland/whoami.cpp
@@ -29,6 +29,18 @@
 
 int main(int, char**)
 {
+    if (pledge("stdio rpath", nullptr) < 0) {
+        perror("pledge");
+        return 1;
+    }
+
+    if (unveil("/etc/passwd", "r") < 0) {
+        perror("unveil");
+        return 1;
+    }
+
+    unveil(nullptr, nullptr);
+
     puts(getlogin());
     return 0;
 }
