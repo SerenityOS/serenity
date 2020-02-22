af777a075a6d6456450ef913f0d47c94bc9cf870
Liav A <liavalb@gmail.com>
2020-02-22 19:46:39 +0200

    Kernel: Introduce the PIT class
    The PIT class inherits from HardwareTimer class, and is replacing
    the PIT namespace.

---
+++ Kernel/Devices/PIT.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include <AK/Types.h>
+#include <Kernel/Devices/HardwareTimer.h>
+
+namespace Kernel {
+
+#define TICKS_PER_SECOND 1000
+/* Timer related ports */
+#define TIMER0_CTL 0x40
+#define TIMER1_CTL 0x41
+#define TIMER2_CTL 0x42
+#define PIT_CTL 0x43
+
+/* Building blocks for PIT_CTL */
+#define TIMER0_SELECT 0x00
+#define TIMER1_SELECT 0x40
+#define TIMER2_SELECT 0x80
+
+#define MODE_COUNTDOWN 0x00
+#define MODE_ONESHOT 0x02
+#define MODE_RATE 0x04
+#define MODE_SQUARE_WAVE 0x06
+
+#define WRITE_WORD 0x30
+
+#define BASE_FREQUENCY 1193182
+
+class PIT : public HardwareTimer {
+public:
+    static void initialize();
+    static PIT& the();
+    u32 ticks_this_second() const;
+
+private:
+    PIT();
+    // ^IRQHandler
+    virtual void handle_irq(RegisterState& regs) override;
+
+    u16 m_default_timer_reload;
+    u16 m_variable_timer_reload;
+    u32 m_ticks_this_second;
+};
+}
