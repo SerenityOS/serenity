980ba1d77aa9a616ec43b8b31a1b0b3b31efda60
Liav A <liavalb@gmail.com>
2020-02-22 20:52:54 +0200

    Userland: Add a utility for viewing interrupts from ProcFS

---
+++ Userland/lsinterrupts.cpp
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2020, Liav A. <liavalb@hotmail.co.il>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <AK/JsonArray.h>
+#include <AK/JsonObject.h>
+#include <AK/String.h>
+#include <LibCore/File.h>
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+    UNUSED_PARAM(argc);
+    UNUSED_PARAM(argv);
+
+    if (pledge("stdio rpath", nullptr) < 0) {
+        perror("pledge");
+        return 1;
+    }
+
+    if (unveil("/proc/interrupts", "r") < 0) {
+        perror("unveil");
+        return 1;
+    }
+
+    unveil(nullptr, nullptr);
+
+    auto proc_interrupts = Core::File::construct("/proc/interrupts");
+    if (!proc_interrupts->open(Core::IODevice::ReadOnly)) {
+        fprintf(stderr, "Error: %s\n", proc_interrupts->error_string());
+        return 1;
+    }
+
+    if (pledge("stdio", nullptr) < 0) {
+        perror("pledge");
+        return 1;
+    }
+
+    printf("%-18s %-10s %-13s\n", "Device", "Interrupt", "Call Counter");
+    auto file_contents = proc_interrupts->read_all();
+    auto json = JsonValue::from_string(file_contents).as_array();
+    json.for_each([](auto& value) {
+        auto handler = value.as_object();
+        auto purpose = handler.get("purpose").to_string();
+        auto interrupt = handler.get("interrupt_line").to_string();
+        auto call_count = handler.get("call_count").to_string();
+
+        printf("%-18s %-10s %-13s\n",
+            purpose.characters(), interrupt.characters(), call_count.characters());
+    });
+
+    return 0;
+}
