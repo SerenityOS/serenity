a7d7c0e60c7cfebbcbd20edf8df3f6897e125bf7
Liav A <liavalb@gmail.com>
2020-02-22 23:56:00 +0200

    Kernel: Change get_pci_address() to pci_address() in PCI::Device class
    The Serenity Coding Style tends to not accept the word "get" in
    methods' names if possible.

--- Kernel/Devices/PATAChannel.cpp
+++ Kernel/Devices/PATAChannel.cpp
@@ -157,9 +157,9 @@ void PATAChannel::initialize(bool force_pio)
     }
 
     // Let's try to set up DMA transfers.
-    PCI::enable_bus_mastering(get_pci_address());
+    PCI::enable_bus_mastering(pci_address());
     prdt().end_of_table = 0x8000;
-    m_bus_master_base = PCI::get_BAR4(get_pci_address()) & 0xfffc;
+    m_bus_master_base = PCI::get_BAR4(pci_address()) & 0xfffc;
     m_dma_buffer_page = MM.allocate_supervisor_physical_page();
     kprintf("PATAChannel: Bus master IDE: I/O @ %x\n", m_bus_master_base);
 }
