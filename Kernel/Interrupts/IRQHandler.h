5f9d0a30e038f83cccb2bf6df064be7513eed66f
Liav A <liavalb@gmail.com>
2020-02-22 19:40:15 +0200

    Kernel: Add new IRQHandler class
    This class will replace the old IRQHandler class later.

---
+++ Kernel/Interrupts/IRQHandler.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include <AK/String.h>
+#include <AK/Types.h>
+#include <Kernel/Arch/i386/CPU.h>
+#include <Kernel/Interrupts/GenericInterruptHandler.h>
+
+namespace Kernel {
+
+class IRQHandler : public GenericInterruptHandler {
+public:
+    virtual ~IRQHandler();
+
+    virtual void handle_interrupt(RegisterState& regs) { handle_irq(regs); }
+    virtual void handle_irq(RegisterState&) = 0;
+
+    void enable_irq();
+    void disable_irq();
+
+    virtual bool eoi() override;
+
+    virtual HandlerPurpose purpose() const override { return HandlerPurpose::IRQHandler; }
+
+    virtual size_t get_sharing_devices_count() const override { return 0; }
+    virtual bool is_shared_handler() const override { return false; }
+    virtual bool is_sharing_with_others() const override { return m_shared_with_others; }
+
+protected:
+    void change_irq_number(u8 irq);
+    explicit IRQHandler(u8 irq);
+
+private:
+    bool m_shared_with_others { false };
+    bool m_enabled { false };
+};
+
+}
