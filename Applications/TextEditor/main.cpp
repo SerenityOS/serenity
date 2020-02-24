ee3811dee81f72facccd080c1b8dad3721a1b398
Andreas Kling <kling@serenityos.org>
2020-02-24 19:51:22 +0100

    TextEditor: Pledge "thread" since it's needed by GUI::FilePicker
    This is a little bit awkward since it's only used for generating
    thumbnails on a background thread and it's not like I care about
    thumbnails very much in a text editor, but for now let's just pledge
    "thread" so I can get on with the thing I wanted to get on with.

--- Applications/TextEditor/main.cpp
+++ Applications/TextEditor/main.cpp
@@ -30,14 +30,14 @@
 
 int main(int argc, char** argv)
 {
-    if (pledge("stdio rpath accept cpath wpath shared_buffer unix fattr", nullptr) < 0) {
+    if (pledge("stdio thread rpath accept cpath wpath shared_buffer unix fattr", nullptr) < 0) {
         perror("pledge");
         return 1;
     }
 
     GUI::Application app(argc, argv);
 
-    if (pledge("stdio rpath accept cpath wpath shared_buffer", nullptr) < 0) {
+    if (pledge("stdio thread rpath accept cpath wpath shared_buffer", nullptr) < 0) {
         perror("pledge");
         return 1;
     }
