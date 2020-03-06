d057da2b1bab58bca215118a772ad692544efcc5
Liav A <liavalb@gmail.com>
2020-03-06 16:59:29 +0200

    Meta: Claim copyright on `PCI` files

--- Kernel/PCI/IOAccess.cpp
+++ Kernel/PCI/IOAccess.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2020, Liav A. <liavalb@hotmail.co.il>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
