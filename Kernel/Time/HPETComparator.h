6a28bc5f0cdfe01db106bd4147efaa611187a164
Liav A <liavalb@gmail.com>
2020-03-09 17:03:27 +0200

    Kernel: Introduce the new Time management subsystem
    This new subsystem includes better abstractions of how time will be
    handled in the OS. We take advantage of the existing RTC timer to aid
    in keeping time synchronized. This is standing in contrast to how we
    handled time-keeping in the kernel, where the PIT was responsible for
    that function in addition to update the scheduler about ticks.
    With that new advantage, we can easily change the ticking dynamically
    and still keep the time synchronized.
    
    In the process context, we no longer use a fixed declaration of
    TICKS_PER_SECOND, but we call the TimeManagement singleton class to
    provide us the right value. This allows us to use dynamic ticking in
    the future, a feature known as tickless kernel.
    
    The scheduler no longer does by himself the calculation of real time
    (Unix time), and just calls the TimeManagment singleton class to provide
    the value.
    
    Also, we can use 2 new boot arguments:
    - the "time" boot argument accpets either the value "modern", or
      "legacy". If "modern" is specified, the time management subsystem will
      try to setup HPET. Otherwise, for "legacy" value, the time subsystem
      will revert to use the PIT & RTC, leaving HPET disabled.
      If this boot argument is not specified, the default pattern is to try
      to setup HPET.
    - the "hpet" boot argumet accepts either the value "periodic" or
      "nonperiodic". If "periodic" is specified, the HPET will scan for
      periodic timers, and will assert if none are found. If only one is
      found, that timer will be assigned for the time-keeping task. If more
      than one is found, both time-keeping task & scheduler-ticking task
      will be assigned to periodic timers.
      If this boot argument is not specified, the default pattern is to try
      to scan for HPET periodic timers. This boot argument has no effect if
      HPET is disabled.
    
    In hardware context, PIT & RealTimeClock classes are merely inheriting
    from the HardwareTimer class, and they allow to use the old i8254 (PIT)
    and RTC devices, managing them via IO ports. By default, the RTC will be
    programmed to a frequency of 1024Hz. The PIT will be programmed to a
    frequency close to 1000Hz.
    
    About HPET, depending if we need to scan for periodic timers or not,
    we try to set a frequency close to 1000Hz for the time-keeping timer
    and scheduler-ticking timer. Also, if possible, we try to enable the
    Legacy replacement feature of the HPET. This feature if exists,
    instructs the chipset to disconnect both i8254 (PIT) and RTC.
    This behavior is observable on QEMU, and was verified against the source
    code:
    https://github.com/qemu/qemu/commit/ce967e2f33861b0e17753f97fa4527b5943c94b6
    
    The HPETComparator class is inheriting from HardwareTimer class, and is
    responsible for an individual HPET comparator, which is essentially a
    timer. Therefore, it needs to call the singleton HPET class to perform
    HPET-related operations.
    
    The new abstraction of Hardware timers brings an opportunity of more new
    features in the foreseeable future. For example, we can change the
    callback function of each hardware timer, thus it makes it possible to
    swap missions between hardware timers, or to allow to use a hardware
    timer for other temporary missions (e.g. calibrating the LAPIC timer,
    measuring the CPU frequency, etc).

---
+++ Kernel/Time/HPETComparator.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2020, Liav A. <liavalb@hotmail.co.il>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include <AK/Function.h>
+#include <AK/Types.h>
+#include <Kernel/Time/HPET.h>
+#include <Kernel/Time/HardwareTimer.h>
+
+namespace Kernel {
+class HPETComparator final : public HardwareTimer {
+    friend class HPET;
+
+public:
+    static NonnullRefPtr<HPETComparator> create(u8 number, u8 irq, bool periodic_capable);
+
+    virtual HardwareTimerType timer_type() const override { return HardwareTimerType::HighPrecisionEventTimer; }
+    virtual const char* model() const override { return "HPET"; }
+
+    u8 comparator_number() const { return m_comparator_number; }
+
+    virtual size_t ticks_per_second() const override;
+
+    virtual bool is_periodic() const override { return m_periodic; }
+    virtual bool is_periodic_capable() const override { return m_periodic_capable; }
+    virtual void set_periodic() override;
+    virtual void set_non_periodic() override;
+
+    virtual void reset_to_default_ticks_per_second() override;
+    virtual bool try_to_set_frequency(size_t frequency) override;
+    virtual bool is_capable_of_frequency(size_t frequency) const override;
+    virtual size_t calculate_nearest_possible_frequency(size_t frequency) const override;
+
+private:
+    void set_new_countdown();
+    virtual void handle_irq(const RegisterState&) override;
+    HPETComparator(u8 number, u8 irq, bool periodic_capable, Function<void(const RegisterState&)> callback);
+    bool m_periodic : 1;
+    bool m_periodic_capable : 1;
+    bool m_edge_triggered : 1;
+    u8 m_comparator_number { 0 };
+};
+}
