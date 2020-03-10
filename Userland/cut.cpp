8d0ecc336d5ead9b1551ba80eb4c1a4c47cc4253
marprok <mariosprokopakis@gmail.com>
2020-03-10 19:23:06 +0200

    Userland: Correction in the the copyright
    Change the date in the copyright.

--- Userland/cut.cpp
+++ Userland/cut.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2020, Marios Prokopakis <mariosprokopakis@gmail.com>
+ * Copyright (c) 2019-2020, Marios Prokopakis <mariosprokopakis@gmail.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
