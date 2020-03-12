44dbbd1fafc369b43d6662ad2e519916e6dc5d6d
Till Mayer <till.mayer@web.de>
2020-03-12 22:54:19 +0100

    Solitaire: Minor tweaks to animation x_velocity and bouncyness

--- Games/Solitaire/SolitaireWidget.cpp
+++ Games/Solitaire/SolitaireWidget.cpp
@@ -103,7 +103,7 @@ void SolitaireWidget::create_new_animation_card()
     card->set_position({ rand() % (SolitaireWidget::width - Card::width), rand() % (SolitaireWidget::height / 8) });
 
     int x_sgn = card->position().x() > (SolitaireWidget::width / 2) ? -1 : 1;
-    m_animation = Animation(card, rand_float() + .4, x_sgn * ((rand() % 3) + 3), .4 + rand_float() * .6);
+    m_animation = Animation(card, rand_float() + .4, x_sgn * ((rand() % 3) + 2), .6 + rand_float() * .4);
 }
 
 void SolitaireWidget::start_game_over_animation()
