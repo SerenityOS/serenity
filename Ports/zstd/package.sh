a4834c5b2f4d5a494ce26cde4810c765bf7fdb22
Brian Callahan <bcallah@openbsd.org>
2020-03-14 15:35:02 -0400

    Ports: Add zstd port

---
+++ Ports/zstd/package.sh
@@ -0,0 +1,6 @@
+#!/bin/bash ../.port_include.sh
+port=zstd
+version=1.4.4
+files="https://github.com/facebook/zstd/releases/download/v${version}/zstd-${version}.tar.gz zstd-${version}.tar.gz
+https://github.com/facebook/zstd/releases/download/v${version}/zstd-${version}.tar.gz.sha256 zstd-${version}.tar.gz.sha256"
+auth_type=sha256
