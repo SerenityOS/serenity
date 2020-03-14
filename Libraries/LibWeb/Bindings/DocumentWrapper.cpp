919617ede4b9327624bfc5ee2daa6c9292800191
Andreas Kling <kling@serenityos.org>
2020-03-14 13:24:15 +0100

    LibWeb: Implement Document.getElementById()
    This was pleasantly simple! We don't have an ElementWrapper yet, so it
    just returns a NodeWrapper, but it still basically works. :^)

--- Libraries/LibWeb/Bindings/DocumentWrapper.cpp
+++ Libraries/LibWeb/Bindings/DocumentWrapper.cpp
@@ -2,6 +2,7 @@
 #include <LibJS/Value.h>
 #include <LibWeb/Bindings/DocumentWrapper.h>
 #include <LibWeb/DOM/Document.h>
+#include <LibWeb/DOM/Element.h>
 
 namespace Web {
 namespace Bindings {
@@ -9,6 +10,15 @@ namespace Bindings {
 DocumentWrapper::DocumentWrapper(Document& document)
     : NodeWrapper(document)
 {
+    put_native_function("getElementById", [this](JS::Interpreter&, Vector<JS::Value> arguments) -> JS::Value {
+        if (arguments.is_empty())
+            return JS::js_null();
+        auto id = arguments[0].to_string();
+        auto* element = node().get_element_by_id(id);
+        if (!element)
+            return JS::js_null();
+        return wrap(heap(), const_cast<Element&>(*element));
+    });
 }
 
 DocumentWrapper::~DocumentWrapper()
