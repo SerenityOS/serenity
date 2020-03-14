a521d2f950c714dec450318afb601c32bb76a7e7
Brian Callahan <bcallah@openbsd.org>
2020-03-14 16:59:55 -0400

    Add the libpuffy helper library and jot, patch, and printf utils from OpenBSD.

---
+++ Ports/libpuffy/package.sh
@@ -0,0 +1,4 @@
+#!/bin/bash ../.port_include.sh
+port=libpuffy
+version=1.0
+files="https://github.com/ibara/libpuffy/releases/download/libpuffy-${version}/libpuffy-${version}.tar.gz libpuffy-${version}.tar.gz"
