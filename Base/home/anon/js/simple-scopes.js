f5b3b778fa5dcb4216a09c0c90f10e7c3f0b2c24
0xtechnobabble <0xtechnobabble@protonmail.com>
2020-03-16 01:17:24 +0200

    js: Fix simple scopes example
    Weirdly enough, the "simple-scopes" test doesn't return undefined
    anymore, at first I thought the scoping was somehow broken, turns out
    the interpreter doesn't consider the returned y as the last evaluated
    value anymore, possibly because it's undefined (?).

--- Base/home/anon/js/simple-scopes.js
+++ Base/home/anon/js/simple-scopes.js
@@ -1,9 +1,11 @@
+ //I should return `undefined` because y is bound to the inner-most enclosing function, i.e the nested one (bar()), therefore, it's undefined in the scope of foo()
 function foo() {
     function bar() {
         var y = 6;
     }
     
-    bar()
+    bar();
     return y;
 }
-foo(); //I should return `undefined` because y is bound to the inner-most enclosing function, i.e the nested one (bar()), therefore, it's undefined in the scope of foo()
+
+print(foo());
