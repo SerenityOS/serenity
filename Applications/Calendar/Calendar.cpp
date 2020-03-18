9e133bfe20978a337aafca0fc1fb9aae93ad235b
rhin123 <ryanrhin@gmail.com>
2020-03-18 11:25:48 -0500

    Calendar: Make const arrays static as well

--- Applications/Calendar/Calendar.cpp
+++ Applications/Calendar/Calendar.cpp
@@ -2,7 +2,7 @@
 
 const String name_of_month(int month)
 {
-    const String month_names[] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+    static const String month_names[] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
     return month_names[month - 1];
 }
 
