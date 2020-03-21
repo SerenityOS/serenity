8b1867422939d8f5ee66aafdd8684b7f0f0f492d
Andreas Kling <kling@serenityos.org>
2020-03-21 17:52:12 +0100

    LibJS: Add Math.random() :^)

--- Libraries/LibJS/Runtime/GlobalObject.cpp
+++ Libraries/LibJS/Runtime/GlobalObject.cpp
@@ -4,6 +4,7 @@
 #include <LibJS/Interpreter.h>
 #include <LibJS/Runtime/ConsoleObject.h>
 #include <LibJS/Runtime/GlobalObject.h>
+#include <LibJS/Runtime/MathObject.h>
 #include <LibJS/Runtime/NativeFunction.h>
 #include <LibJS/Runtime/Value.h>
 
@@ -17,6 +18,7 @@ GlobalObject::GlobalObject()
         this_object->heap().collect_garbage();
         return js_undefined();
     });
+    put("Math", heap().allocate<MathObject>());
 }
 
 GlobalObject::~GlobalObject()
