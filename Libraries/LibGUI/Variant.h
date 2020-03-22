cb62932ab59b5f8b40cf21e62ff4667f6b6c49be
Andreas Kling <kling@serenityos.org>
2020-03-22 19:06:31 +0100

    LibGUI: Allow constructing Variant from FlyString

--- Libraries/LibGUI/Variant.h
+++ Libraries/LibGUI/Variant.h
@@ -43,6 +43,7 @@ public:
     Variant(unsigned);
     Variant(const char*);
     Variant(const String&);
+    Variant(const FlyString&);
     Variant(const Gfx::Bitmap&);
     Variant(const GUI::Icon&);
     Variant(const Gfx::Point&);
