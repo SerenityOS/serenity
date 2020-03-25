926402521041e24db2af74b71ae74180df5641c7
Andreas Kling <kling@serenityos.org>
2020-03-25 14:26:31 +0100

    LibJS: Add a very basic test runner (shell script) + some tests

---
+++ Libraries/LibJS/Tests/String.prototype.charAt.js
@@ -0,0 +1,19 @@
+function assert(x) { if (!x) throw 1; }
+
+try {
+    var s = "foobar"
+    assert(typeof s === "string");
+    assert(s.length === 6);
+
+    assert(s.charAt(0) === 'f');
+    assert(s.charAt(1) === 'o');
+    assert(s.charAt(2) === 'o');
+    assert(s.charAt(3) === 'b');
+    assert(s.charAt(4) === 'a');
+    assert(s.charAt(5) === 'r');
+    assert(s.charAt(6) === '');
+
+    console.log("PASS");
+} catch (e) {
+    console.log("FAIL: " + e);
+}
---
+++ Libraries/LibJS/Tests/basic-array.js
@@ -0,0 +1,23 @@
+function assert(x) { if (!x) throw 1; }
+
+try {
+    var a = [1, 2, 3];
+
+    assert(typeof a === "object");
+    assert(a.length === 3);
+    assert(a[0] === 1);
+    assert(a[1] === 2);
+    assert(a[2] === 3);
+
+    a[1] = 5;
+    assert(a[1] === 5);
+    assert(a.length === 3);
+
+    a.push(7);
+    assert(a[3] === 7);
+    assert(a.length === 4);
+
+    console.log("PASS");
+} catch (e) {
+    console.log("FAIL: " + e);
+}
