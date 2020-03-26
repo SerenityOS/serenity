a6a11628100eaa1bd6dce81526c0ed30c554f453
Sergey Bugaev <bugaevc@serenityos.org>
2020-03-26 10:03:17 +0300

    Base: Add a man page for js(1)
    This also changes --ast-dump to --dump-ast, because I like it better
    and that is what the variable is actually called.

--- Userland/js.cpp
+++ Userland/js.cpp
@@ -115,7 +115,7 @@ int main(int argc, char** argv)
     const char* script_path = nullptr;
 
     Core::ArgsParser args_parser;
-    args_parser.add_option(dump_ast, "Dump the AST", "ast-dump", 'A');
+    args_parser.add_option(dump_ast, "Dump the AST", "dump-ast", 'A');
     args_parser.add_option(print_last_result, "Print last result", "print-last-result", 'l');
     args_parser.add_option(gc_on_every_allocation, "GC on every allocation", "gc-on-every-allocation", 'g');
     args_parser.add_positional_argument(script_path, "Path to script file", "script", Core::ArgsParser::Required::No);
