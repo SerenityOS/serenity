72576cff223015fec8d563ee485e320ffc743273
Andreas Kling <kling@serenityos.org>
2020-03-27 12:45:36 +0100

    LibJS: Add global isNaN() function

--- Libraries/LibJS/Tests/NaN-basic.js
+++ Libraries/LibJS/Tests/NaN-basic.js
@@ -3,6 +3,10 @@ function assert(x) { if (!x) throw 1; }
 try {
     var nan = undefined + 1;
     assert(nan + "" == "NaN");
+    assert(isNaN(nan) === true);
+    assert(isNaN(0) === false);
+    assert(isNaN(undefined) === false);
+    assert(isNaN(null) === false);
     console.log("PASS");
 } catch (e) {
     console.log("FAIL: " + e);
