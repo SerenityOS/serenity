d42d014655028a5d3a1573f3a9f894f2919322af
Itamar <itamar8910@gmail.com>
2020-03-28 11:47:16 +0300

    Kernel: Add 'ptrace' syscall
    This commit adds a basic implementation of
    the ptrace syscall, which allows one process
    (the tracer) to control another process (the tracee).
    
    While a process is being traced, it is stopped whenever a signal is
    received (other than SIGCONT).
    
    The tracer can start tracing another thread with PT_ATTACH,
    which causes the tracee to stop.
    
    From there, the tracer can use PT_CONTINUE
    to continue the execution of the tracee,
    or use other request codes (which haven't been implemented yet)
    to modify the state of the tracee.
    
    Additional request codes are PT_SYSCALL, which causes the tracee to
    continue exection but stop at the next entry or exit from a syscall,
    and PT_GETREGS which fethces the last saved register set of the tracee
    (can be used to inspect syscall arguments and return value).
    
    A special request code is PT_TRACE_ME, which is issued by the tracee
    and causes it to stop when it calls execve and wait for the
    tracer to attach.

---
+++ Kernel/ThreadTracer.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include <AK/CircularDeque.h>
+#include <AK/NonnullOwnPtr.h>
+#include <AK/Optional.h>
+#include <AK/RefCounted.h>
+#include <Kernel/Arch/i386/CPU.h>
+#include <Kernel/UnixTypes.h>
+#include <LibC/sys/arch/i386/regs.h>
+
+namespace Kernel {
+
+class ThreadTracer {
+public:
+    static NonnullOwnPtr<ThreadTracer> create(pid_t tracer) { return make<ThreadTracer>(tracer); }
+
+    pid_t tracer_pid() const { return m_tracer_pid; }
+    bool has_pending_signal(u32 signal) const { return m_pending_signals & (1 << (signal - 1)); }
+    void set_signal(u32 signal) { m_pending_signals |= (1 << (signal - 1)); }
+    void unset_signal(u32 signal) { m_pending_signals &= ~(1 << (signal - 1)); }
+
+    bool is_tracing_syscalls() const { return m_trace_syscalls; }
+    void set_trace_syscalls(bool val) { m_trace_syscalls = val; }
+
+    void set_regs(const RegisterState& regs);
+    bool has_regs() const { return m_regs.has_value(); }
+    const PtraceRegisters& regs() const
+    {
+        ASSERT(m_regs.has_value());
+        return m_regs.value();
+    }
+
+    explicit ThreadTracer(pid_t);
+
+private:
+    pid_t m_tracer_pid { -1 };
+
+    // This is a bitmap for signals that are sent from the tracer to the tracee
+    // TODO: Since we do not currently support sending signals
+    //       to the tracee via PT_CONTINUE, this bitmap is always zeroed
+    u32 m_pending_signals { 0 };
+
+    bool m_trace_syscalls { false };
+    Optional<PtraceRegisters> m_regs;
+};
+
+}
