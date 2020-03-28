cc65b7f1a38bf81e7c7d9d54efc6e613f22b20c6
Andreas Kling <kling@serenityos.org>
2020-03-28 17:23:54 +0100

    LibJS: Add a global "Object" constructor
    This patch adds an "Object" constructor to the global object. The only
    function it implements so far is Object.getPrototypeOf().

---
+++ Libraries/LibJS/Tests/Object.getPrototypeOf.js
@@ -0,0 +1,11 @@
+function assert(x) { if (!x) throw 1; }
+
+try {
+    var o1 = new Object();
+    var o2 = {};
+    assert(Object.getPrototypeOf(o1) === Object.getPrototypeOf(o2));
+    assert(Object.getPrototypeOf(Object.getPrototypeOf(o1)) === null);
+    console.log("PASS");
+} catch (e) {
+    console.log("FAIL: " + e);
+}
