b588e2670e6ec4d72c60a9ca3eda879710ed7ece
Tibor Nagy <xnagytibor@gmail.com>
2020-04-01 11:28:16 +0200

    HackStudio: Move the "Find in files" button in line with the search box

--- DevTools/HackStudio/FindInFilesWidget.cpp
+++ DevTools/HackStudio/FindInFilesWidget.cpp
@@ -130,12 +130,18 @@ static RefPtr<SearchResultsModel> find_in_files(const StringView& text)
 FindInFilesWidget::FindInFilesWidget()
 {
     set_layout<GUI::VerticalBoxLayout>();
-    m_textbox = add<GUI::TextBox>();
-    m_textbox->set_size_policy(GUI::SizePolicy::Fill, GUI::SizePolicy::Fixed);
-    m_textbox->set_preferred_size(0, 20);
-    m_button = add<GUI::Button>("Find in files");
-    m_button->set_size_policy(GUI::SizePolicy::Fill, GUI::SizePolicy::Fixed);
-    m_button->set_preferred_size(0, 20);
+
+    auto& top_container = add<Widget>();
+    top_container.set_layout<GUI::HorizontalBoxLayout>();
+    top_container.set_size_policy(GUI::SizePolicy::Fill, GUI::SizePolicy::Fixed);
+    top_container.set_preferred_size(0, 20);
+
+    m_textbox = top_container.add<GUI::TextBox>();
+    m_textbox->set_size_policy(GUI::SizePolicy::Fill, GUI::SizePolicy::Fill);
+
+    m_button = top_container.add<GUI::Button>("Find in files");
+    m_button->set_size_policy(GUI::SizePolicy::Fixed, GUI::SizePolicy::Fill);
+    m_button->set_preferred_size(100, 0);
 
     m_result_view = add<GUI::TableView>();
     m_result_view->set_size_columns_to_fit_content(true);
