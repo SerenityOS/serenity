beba6fb9305d9ad0736e45752a17272fa2ad5c93
Linus Groh <mail@linusgroh.de>
2020-04-01 21:00:09 +0100

    LibJS: Add NaN to global object

--- Libraries/LibJS/Tests/NaN-basic.js
+++ Libraries/LibJS/Tests/NaN-basic.js
@@ -3,7 +3,11 @@ function assert(x) { if (!x) throw 1; }
 try {
     var nan = undefined + 1;
     assert(nan + "" == "NaN");
+    assert(NaN + "" == "NaN");
+    assert(nan !== nan);
+    assert(NaN !== NaN);
     assert(isNaN(nan) === true);
+    assert(isNaN(NaN) === true);
     assert(isNaN(0) === false);
     assert(isNaN(undefined) === true);
     assert(isNaN(null) === false);
