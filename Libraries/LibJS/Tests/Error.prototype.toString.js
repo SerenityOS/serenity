94afcb54de07499d15fe8e601127148bba022f96
Linus Groh <mail@linusgroh.de>
2020-04-02 21:16:13 +0100

    LibJS: Implement Error.prototype.toString()

---
+++ Libraries/LibJS/Tests/Error.prototype.toString.js
@@ -0,0 +1,13 @@
+function assert(x) { if (!x) throw 1; }
+
+try {
+    assert(Error().toString() === "Error");
+    assert(Error(undefined).toString() === "Error");
+    assert(Error(null).toString() === "Error: null");
+    assert(Error("test").toString() === "Error: test");
+    assert(Error(42).toString() === "Error: 42");
+
+    console.log("PASS");
+} catch (e) {
+    console.log("FAIL: " + e);
+}
