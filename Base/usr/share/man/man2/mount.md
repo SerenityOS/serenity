9ae3cced764cabec52b15f50b648afe82eeac449
Andreas Kling <kling@serenityos.org>
2020-04-03 21:20:46 +0200

    Revert "Kernel & Userland: Allow to mount image files formatted with Ext2FS"
    This reverts commit a60ea79a41845767ce40f225de20da7c99534ad1.
    
    Reverting these changes since they broke things.
    Fixes #1608.

--- Base/usr/share/man/man2/mount.md
+++ Base/usr/share/man/man2/mount.md
@@ -7,13 +7,12 @@ mount - mount a filesystem
 ```**c++
 #include <unistd.h>
 
-int mount(int source_fd, const char* target, const char* fs_type, int flags);
+int mount(const char* source, const char* target, const char* fs_type, int flags);
 ```
 
 ## Description
 
-`mount()` mounts a filesystem stored at `source_fd` by overlaying its contents
-over `target`.
+`mount()` mounts a filesystem stored at `source` by overlaying its contents over `target`.
 
 `fs_type` must be one of the following supported filesystems:
 
@@ -22,10 +21,9 @@ over `target`.
 * `DevPtsFS` (or `devpts`): The pseudoterminal pseudo-filesystem (normally mounted at `/dev/pts`).
 * `TmpFS` (or `tmp`): A non-persistent filesystem that stores all its data in RAM. An instance of this filesystem is normally mounted at `/tmp`.
 
-For Ext2FS, `source_fd` must refer to an open file descriptor to a file containing
-the filesystem image. This may be a device file or any other seekable file. All
-the other filesystems ignore the `source_fd` — you can even pass an invalid file
-descriptor such as -1.
+For Ext2FS, `source` must be a path to a block device storing the filesystem contents. All
+the other filesystems ignore the `source` argument (by convention, it should have the same
+value as `fs_type`).
 
 The following `flags` are supported:
 
@@ -39,22 +37,14 @@ mounted file system.
 
 ### Bind mounts
 
-If `MS_BIND` is specified in `flags`, `fs_type` is ignored and a bind mount is
-performed instead. In this case, the file or directory specified by `source_fd`
-is overlayed over `target` — the target appears to be replaced by a copy of the
-source. This can be used as an alternative to symlinks or hardlinks.
+If `MS_BIND` is specified in `flags`, `fs_type` is ignored and a bind mount is performed
+instead. In this case `source` is treated as a path to a file or directory whose contents
+are overlayed over `target`. This can be used  as an alternative to symlinks or hardlinks.
 
 ## Errors
 
-* `EFAULT`: The `fs_type` or `target` are invalid strings.
 * `EPERM`: The current process does not have superuser privileges.
-* `ENODEV`: The `fs_type` is unrecognized, or the file descriptor to source is
-  not found, or the source doesn't contain a valid filesystem image. Also, this
-  error occurs if `fs_type` is valid, but the file descriptor from `source_fd`
-  is not seekable.
-* `EBADF`: If the `source_fd` is not valid, and either `fs_type` specifies a
-  file-backed filesystem (and not a pseudo filesystem), or `MS_BIND` is
-  specified in flags.
+* `ENODEV`: The `fs_type` is unrecognized, or the device is not found, or the device doesn't contain a valid filesystem image.
 
 All of the usual path resolution errors may also occur.
 
--- Kernel/Process.cpp
+++ Kernel/Process.cpp
@@ -4011,18 +4011,14 @@ int Process::sys$mount(const Syscall::SC_mount_params* user_params)
     if (!validate_read_and_copy_typed(&params, user_params))
         return -EFAULT;
 
-    auto source_fd = params.source_fd;
+    auto source = validate_and_copy_string_from_user(params.source);
     auto target = validate_and_copy_string_from_user(params.target);
     auto fs_type = validate_and_copy_string_from_user(params.fs_type);
 
-    if (target.is_null() || fs_type.is_null())
+    if (source.is_null() || target.is_null() || fs_type.is_null())
         return -EFAULT;
 
-    auto description = file_description(source_fd);
-    if (!description.is_null())
-        dbg() << "mount " << fs_type << ": source fd " << source_fd << " @ " << target;
-    else
-        dbg() << "mount " << fs_type << " @ " << target;
+    dbg() << "mount " << fs_type << ": source " << source << " @ " << target;
 
     auto custody_or_error = VFS::the().resolve_path(target, current_directory());
     if (custody_or_error.is_error())
@@ -4034,24 +4030,28 @@ int Process::sys$mount(const Syscall::SC_mount_params* user_params)
 
     if (params.flags & MS_BIND) {
         // We're doing a bind mount.
-        if (description.is_null())
-            return -EBADF;
-        ASSERT(description->custody());
-        return VFS::the().bind_mount(*description->custody(), target_custody, params.flags);
+        auto source_or_error = VFS::the().resolve_path(source, current_directory());
+        if (source_or_error.is_error())
+            return source_or_error.error();
+        auto& source_custody = source_or_error.value();
+        return VFS::the().bind_mount(source_custody, target_custody, params.flags);
     }
 
     if (fs_type == "ext2" || fs_type == "Ext2FS") {
-        if (description.is_null())
-            return -EBADF;
-        ASSERT(description->custody());
-        if (!description->file().is_seekable()) {
-            dbg() << "mount: this is not a seekable file";
+        auto source_or_error = VFS::the().open(source, O_RDWR, 0, current_directory());
+        if (source_or_error.is_error())
+            return source_or_error.error();
+
+        auto* device = source_or_error.value()->device();
+        if (!device || !device->is_block_device()) {
+            dbg() << "mount: this is not a BlockDevice";
             return -ENODEV;
         }
+        auto& block_device = static_cast<BlockDevice&>(*device);
 
-        dbg() << "mount: attempting to mount " << description->absolute_path() << " on " << target;
+        dbg() << "mount: attempting to mount " << block_device.absolute_path() << " on " << target;
 
-        fs = Ext2FS::create(*description);
+        fs = Ext2FS::create(block_device);
     } else if (fs_type == "proc" || fs_type == "ProcFS") {
         fs = ProcFS::create();
     } else if (fs_type == "devpts" || fs_type == "DevPtsFS") {
@@ -4063,15 +4063,12 @@ int Process::sys$mount(const Syscall::SC_mount_params* user_params)
     }
 
     if (!fs->initialize()) {
-        dbg() << "mount: failed to initialize " << fs_type << " filesystem, fd - " << source_fd;
+        dbg() << "mount: failed to initialize " << fs_type << " filesystem on " << source;
         return -ENODEV;
     }
 
     auto result = VFS::the().mount(fs.release_nonnull(), target_custody, params.flags);
-    if (!description.is_null())
-        dbg() << "mount: successfully mounted " << description->absolute_path() << " on " << target;
-    else
-        dbg() << "mount: successfully mounted " << target;
+    dbg() << "mount: successfully mounted " << source << " on " << target;
     return result;
 }
 
