693741899f6159beff7eb5a038c643728cd7e1ed
Linus Groh <mail@linusgroh.de>
2020-04-04 14:34:27 +0100

    LibJS: Add length property to ScriptFunction

---
+++ Libraries/LibJS/Tests/function-length.js
@@ -0,0 +1,17 @@
+function assert(x) { if (!x) throw 1; }
+
+try {
+    function foo() { }
+    assert(foo.length === 0);
+    assert((foo.length = 5) === 5);
+    assert(foo.length === 0);
+
+    function bar(a, b, c) {}
+    assert(bar.length === 3);
+    assert((bar.length = 5) === 5);
+    assert(bar.length === 3);
+
+    console.log("PASS");
+} catch (e) {
+    console.log("FAIL: " + e);
+}
