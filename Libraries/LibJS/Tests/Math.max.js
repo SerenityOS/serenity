945b32674ac3e7b98073adf0532d24a05f0031bf
Brian Gianforcaro <b.gianfo@gmail.com>
2020-04-04 23:25:23 -0700

    LibJS: Add support for arbitrary arguments to Math.max
    Address the FIXME in MathObject::max to handle an arbitrary
    number of arguments. Also adding a test case to verify the
    behavior of Math.max() while I'm here.

---
+++ Libraries/LibJS/Tests/Math.max.js
@@ -0,0 +1,14 @@
+function assert(x) { if (!x) throw 1; }
+
+try {
+    assert(Math.max.length === 2);
+    assert(Math.max(1) === 1);
+    assert(Math.max(2, 1) === 2);
+    assert(Math.max(1, 2, 3) === 3);
+    assert(isNaN(Math.max(NaN)));
+    assert(isNaN(Math.max("String", 1)));
+
+    console.log("PASS");
+} catch {
+    console.log("FAIL");
+}
