07bcf19a5b9897480294f445651df8e63c399bc3
Xiao NuoFu <xiaonuofu@email.it>
2020-04-05 18:08:11 +0200

    Ports: refresh OpenSSL, make it build its command line utils.
    This patch refreshes the openssl port and makes it build the utilities
    in apps/, e.g. the openssl utility.
    
    Now you can do this from Serenity:
    
        $ openssl s_client -connect example.org:443
        ...
        GET / HTTP/1.1
        Host: example.org
    
        <HTTP response here>
    
    The download URL was bit-rotten and needed a fix.

--- Ports/openssl/package.sh
+++ Ports/openssl/package.sh
@@ -1,19 +1,20 @@
 #!/bin/bash ../.port_include.sh
 port=openssl
-version=1.0.2t
+branch='1.0.2'
+version="${branch}t"
 useconfigure=true
 configscript=Configure
-files="https://www.openssl.org/source/openssl-${version}.tar.gz openssl-${version}.tar.gz
-https://www.openssl.org/source/openssl-${version}.tar.gz.asc openssl-${version}.tar.gz.asc"
+files="https://ftp.openssl.org/source/old/${branch}/openssl-${version}.tar.gz openssl-${version}.tar.gz
+https://ftp.openssl.org/source/old/${branch}/openssl-${version}.tar.gz.asc openssl-${version}.tar.gz.asc"
 auth_type="sig"
 auth_import_key="8657ABB260F056B1E5190839D9C4D26D0E604491"
 auth_opts="openssl-${version}.tar.gz.asc openssl-${version}.tar.gz"
 
 depends="zlib"
 usr_local=$SERENITY_ROOT/Root/usr/local/
-configopts="--prefix=$usr_local --openssldir=$usr_local/ssl BSD-x86 zlib no-tests no-threads no-asm no-sock"
+configopts="--prefix=$usr_local -DOPENSSL_SYS_SERENITY=1 --openssldir=$usr_local/ssl BSD-x86 zlib no-tests no-threads no-asm"
 
 configure() {
-    run rm -rf ./test/ ./apps/
+    run rm -rf ./test/
     run ./"$configscript" $configopts
 }
