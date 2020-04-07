fb09f59a889754197865d6fc5bfcd655ce030c73
Brian Gianforcaro <b.gianfo@gmail.com>
2020-04-07 01:09:17 -0700

    LibWeb: Add canvas.strokeRect(), and fix scale & translate
    Add an implementation of CanvasRenderingContext2DWrapper.strokeRect().
    While implementing this I fixed fillRect() and the new strokeRect() to
    honor the .scale() and .translate() values that had previously been plumbed.
    
    Also enhance the canvas.html demo to utilize strokeRect(), scale(), and translate().

--- Libraries/LibWeb/DOM/CanvasRenderingContext2D.cpp
+++ Libraries/LibWeb/DOM/CanvasRenderingContext2D.cpp
@@ -56,8 +56,29 @@ void CanvasRenderingContext2D::fill_rect(int x, int y, int width, int height)
     if (!painter)
         return;
 
-    Gfx::Rect rect(x, y, width, height);
+    Gfx::Rect rect = compute_rect(x, y, width, height);
     painter->fill_rect(rect, m_fill_style);
+    did_draw(rect);
+}
+
+void CanvasRenderingContext2D::set_stroke_style(String style)
+{
+    m_stroke_style = Gfx::Color::from_string(style).value_or(Color::Black);
+}
+
+String CanvasRenderingContext2D::stroke_style() const
+{
+    return m_fill_style.to_string();
+}
+
+void CanvasRenderingContext2D::stroke_rect(int x, int y, int width, int height)
+{
+    auto painter = this->painter();
+    if (!painter)
+        return;
+
+    Gfx::Rect rect = compute_rect(x, y, width, height);
+    painter->draw_rect(rect, m_stroke_style);
     did_draw(rect);
 }
 
@@ -75,6 +96,14 @@ void CanvasRenderingContext2D::translate(double x, double y)
     dbg() << "CanvasRenderingContext2D::translate(): " << String::format("%f", x) << ", " << String::format("%f", y);
     m_translate_x = x;
     m_translate_y = y;
+}
+
+Gfx::Rect CanvasRenderingContext2D::compute_rect(int x, int y, int width, int height)
+{
+    return Gfx::Rect((x + m_translate_x) * m_scale_x,
+                     (y + m_translate_y) * m_scale_y,
+                     width * m_scale_x,
+                     height * m_scale_y);
 }
 
 void CanvasRenderingContext2D::did_draw(const Gfx::Rect&)
