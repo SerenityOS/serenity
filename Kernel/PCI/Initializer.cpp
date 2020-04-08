f2ebfb9e4e591700fb679a2101017f25ac5e3800
Andreas Kling <kling@serenityos.org>
2020-04-08 17:39:17 +0200

    Kernel: Simplify PCI initialization logic
    - Get rid of the PCI::Initializer object which was not serving any real
      purpose or holding any data members.
    - Move command line parsing from init to PCI::initialize().

--- Kernel/PCI/Initializer.cpp
+++ Kernel/PCI/Initializer.cpp
@@ -36,36 +36,43 @@
 namespace Kernel {
 namespace PCI {
 
-static Initializer* s_pci_initializer;
+static void initialize_pci_mmio_access(PhysicalAddress mcfg);
+static void initialize_pci_io_access();
+static void test_and_initialize(bool disable_pci_mmio);
+static void detect_devices();
+static bool test_acpi();
+static bool test_pci_io();
+static bool test_pci_mmio();
+static void initialize_pci_mmio_access_after_test();
 
-Initializer& Initializer::the()
+void initialize()
 {
-    if (s_pci_initializer == nullptr) {
-        s_pci_initializer = new Initializer();
-    }
-    return *s_pci_initializer;
+    bool pci_mmio = kernel_command_line().lookup("pci_mmio").value_or("off") == "on";
+    test_and_initialize(!pci_mmio);
 }
-void Initializer::initialize_pci_mmio_access(PhysicalAddress mcfg)
+
+void initialize_pci_mmio_access(PhysicalAddress mcfg)
 {
     MMIOAccess::initialize(mcfg);
     detect_devices();
 }
-void Initializer::initialize_pci_io_access()
+
+void initialize_pci_io_access()
 {
     IOAccess::initialize();
     detect_devices();
 }
 
-void Initializer::detect_devices()
+void detect_devices()
 {
     enumerate_all([&](const Address& address, ID id) {
-        klog() << "PCI: device @ " << String::format("%w", address.seg()) << ":" << String::format("%b", address.bus()) << ":" << String::format("%b", address.slot()) << "." << String::format("%d", address.function()) << " [" << String::format("%w", id.vendor_id) << ":" << String::format("%w", id.device_id) << "]";
+        klog() << "PCI: Device @ " << String::format("%w", address.seg()) << ":" << String::format("%b", address.bus()) << ":" << String::format("%b", address.slot()) << "." << String::format("%d", address.function()) << " [" << String::format("%w", id.vendor_id) << ":" << String::format("%w", id.device_id) << "]";
         E1000NetworkAdapter::detect(address);
         RTL8139NetworkAdapter::detect(address);
     });
 }
 
-void Initializer::test_and_initialize(bool disable_pci_mmio)
+void test_and_initialize(bool disable_pci_mmio)
 {
     if (disable_pci_mmio) {
         if (test_pci_io()) {
@@ -96,18 +103,15 @@ void Initializer::test_and_initialize(bool disable_pci_mmio)
         }
     }
 }
-Initializer::Initializer()
-{
-}
-bool Initializer::test_acpi()
+
+bool test_acpi()
 {
     if ((kernel_command_line().contains("noacpi")) || !ACPI::Parser::the().is_operable())
         return false;
-    else
-        return true;
+    return true;
 }
 
-bool Initializer::test_pci_io()
+bool test_pci_io()
 {
     klog() << "Testing PCI via manual probing... ";
     u32 tmp = 0x80000000;
@@ -122,27 +126,14 @@ bool Initializer::test_pci_io()
     return false;
 }
 
-bool Initializer::test_pci_mmio()
+bool test_pci_mmio()
 {
     return !ACPI::Parser::the().find_table("MCFG").is_null();
 }
 
-void Initializer::initialize_pci_mmio_access_after_test()
+void initialize_pci_mmio_access_after_test()
 {
     initialize_pci_mmio_access(ACPI::Parser::the().find_table("MCFG"));
-}
-
-void Initializer::dismiss()
-{
-    if (s_pci_initializer == nullptr)
-        return;
-    klog() << "PCI Subsystem Initializer dismissed.";
-    delete s_pci_initializer;
-    s_pci_initializer = nullptr;
-}
-
-Initializer::~Initializer()
-{
 }
 
 }
