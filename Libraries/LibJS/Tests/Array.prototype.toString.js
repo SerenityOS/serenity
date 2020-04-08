ca997b592e1ad5d8afac95abcf04bb2e3c98ff3f
Linus Groh <mail@linusgroh.de>
2020-04-08 18:56:12 +0100

    LibJS: Handle empty values in Array.prototype.toString()

--- Libraries/LibJS/Tests/Array.prototype.toString.js
+++ Libraries/LibJS/Tests/Array.prototype.toString.js
@@ -6,6 +6,12 @@ try {
 
     assert("rgb(" + [10, 11, 12] + ")" === "rgb(10,11,12)");
 
+    a = new Array(5);
+    assert(a.toString() === ",,,,");
+    a[2] = "foo";
+    a[4] = "bar";
+    assert(a.toString() === ",,foo,,bar");
+
     console.log("PASS");
 } catch (e) {
     console.log("FAIL: " + e);
