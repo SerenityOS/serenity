6b3f0bf2d5c2b5350e9ce33d413ab035fba206c8
Linus Groh <mail@linusgroh.de>
2020-04-10 15:22:58 +0100

    LibJS: Add String.prototype.pad{Start,End}()

--- Base/home/anon/js/date.js
+++ Base/home/anon/js/date.js
@@ -1,29 +1,14 @@
 var now = Date.now();
 console.log("Unix timestamp: " + now / 1000);
 
-// FIXME: We need String.prototype.padStart() :^)
 var d = new Date();
 var year = d.getFullYear();
-var month = d.getMonth() + 1;
-if (month < 10)
-    month = "0" + month;
-var day = d.getDate();
-if (day < 10)
-    day = "0" + day;
-var hours = d.getHours();
-if (hours < 10)
-    hours = "0" + hours;
-var minutes = d.getMinutes();
-if (minutes < 10)
-    minutes = "0" + minutes;
-var seconds = d.getSeconds();
-if (seconds < 10)
-    seconds = "0" + seconds;
-var milliseconds = d.getMilliseconds();
-if (milliseconds < 10) {
-    milliseconds = "00" + milliseconds;
-} else if (milliseconds < 100) {
-    milliseconds = "0" + milliseconds;
-}
+var month = (d.getMonth() + 1).toString().padStart(2, "0");
+var day = d.getDate().toString().padStart(2, "0");
+var hours = d.getHours().toString().padStart(2, "0");
+var minutes = d.getMinutes().toString().padStart(2, "0");
+var seconds = d.getSeconds().toString().padStart(2, "0");
+var milliseconds = d.getMilliseconds().toString().padStart(3, "0");
+
 console.log("Date: " + year + "-" + month + "-" + day);
 console.log("Time: " + hours + ":" + minutes + ":" + seconds + "." + milliseconds);
