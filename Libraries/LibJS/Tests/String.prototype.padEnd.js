31505dde7ec5e8077a5a72e7e50d4e5d7203432d
Linus Groh <mail@linusgroh.de>
2020-04-10 15:22:58 +0100

    LibJS: Add String.prototype.pad{Start,End}()

---
+++ Libraries/LibJS/Tests/String.prototype.padEnd.js
@@ -0,0 +1,22 @@
+try {
+    assert(String.prototype.padEnd.length === 1);
+
+    var s = "foo";
+    assert(s.padEnd(-1) === "foo");
+    assert(s.padEnd(0) === "foo");
+    assert(s.padEnd(3) === "foo");
+    assert(s.padEnd(5) === "foo  ");
+    assert(s.padEnd(10) === "foo       ");
+    assert(s.padEnd("5") === "foo  ");
+    assert(s.padEnd([[["5"]]]) === "foo  ");
+    assert(s.padEnd(2, "+") === "foo");
+    assert(s.padEnd(5, "+") === "foo++");
+    assert(s.padEnd(5, 1) === "foo11");
+    assert(s.padEnd(10, null) === "foonullnul");
+    assert(s.padEnd(10, "bar") === "foobarbarb");
+    assert(s.padEnd(10, "123456789") === "foo1234567");
+
+    console.log("PASS");
+} catch (e) {
+    console.log("FAIL: " + e);
+}
