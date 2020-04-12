28232acc9d162d3febc148eff07a6bbd56843f34
Linus Groh <mail@linusgroh.de>
2020-04-12 17:00:06 +0100

    LibJS: Add console.{debug,info,warn,error}()

---
+++ Base/home/anon/js/console.js
@@ -0,0 +1,5 @@
+console.log("I am a generic log message");
+console.debug("I am a debug log message");
+console.info("I am an info log message");
+console.warn("I am a warning log message");
+console.error("I am an error log message");
