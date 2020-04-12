0bbd2b5956537265155671fb7ae68376c7f6a9e5
Andreas Kling <kling@serenityos.org>
2020-04-12 20:40:02 +0200

    LibJS: Cache a FlyString for "this" to speed up variable lookup
    We were hitting strcmp() in every variable lookup to see if the lookup
    was for "this". Caching a FlyString("this") turns that check into one
    pointer comparison instead. :^)

--- Libraries/LibJS/Interpreter.cpp
+++ Libraries/LibJS/Interpreter.cpp
@@ -158,7 +158,8 @@ void Interpreter::set_variable(const FlyString& name, Value value, bool first_as
 
 Optional<Value> Interpreter::get_variable(const FlyString& name)
 {
-    if (name == "this")
+    static FlyString this_name = "this";
+    if (name == this_name)
         return this_value();
 
     for (ssize_t i = m_scope_stack.size() - 1; i >= 0; --i) {
