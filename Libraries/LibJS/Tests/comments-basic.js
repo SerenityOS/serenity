9477efe97032f15438b51bf01e2958cc76c554be
Stephan Unverwerth <s.unverwerth@gmx.de>
2020-04-13 19:50:58 +0200

    LibJS: Handle HTML-style comments

---
+++ Libraries/LibJS/Tests/comments-basic.js
@@ -0,0 +1,20 @@
+try {
+    var i = 0;
+
+    // i++;
+    /* i++; */
+    /*
+    i++;
+    */
+    <!-- i++; --> i++;
+    <!-- i++;
+    i++;
+    --> i++;
+
+    assert(i === 1);
+
+    console.log('PASS');
+} catch (e) {
+    console.log('FAIL: ' + e);
+}
+
