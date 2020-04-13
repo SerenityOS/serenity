25ffa2b6ca5004d4ba83bf3960ecbe942fa2092b
Linus Groh <mail@linusgroh.de>
2020-04-13 14:08:27 +0100

    LibJS: Fix test files indentation (4 spaces)

--- Libraries/LibJS/Tests/arrow-functions.js
+++ Libraries/LibJS/Tests/arrow-functions.js
@@ -1,56 +1,56 @@
 try {
-  let getNumber = () => 42;
-  assert(getNumber() === 42);
+    let getNumber = () => 42;
+    assert(getNumber() === 42);
 
-  let add = (a, b) => a + b;
-  assert(add(2, 3) === 5);
+    let add = (a, b) => a + b;
+    assert(add(2, 3) === 5);
 
-  const addBlock = (a, b) => {
-    let res = a + b;
-    return res;
-  };
-  assert(addBlock(5, 4) === 9);
+    const addBlock = (a, b) => {
+        let res = a + b;
+        return res;
+    };
+    assert(addBlock(5, 4) === 9);
 
-  const makeObject = (a, b) => ({ a, b });
-  const obj = makeObject(33, 44);
-  assert(typeof obj === "object");
-  assert(obj.a === 33);
-  assert(obj.b === 44);
+    const makeObject = (a, b) => ({ a, b });
+    const obj = makeObject(33, 44);
+    assert(typeof obj === "object");
+    assert(obj.a === 33);
+    assert(obj.b === 44);
 
-  let returnUndefined = () => {};
-  assert(typeof returnUndefined() === "undefined");
+    let returnUndefined = () => { };
+    assert(typeof returnUndefined() === "undefined");
 
-  const makeArray = (a, b) => [a, b];
-  const array = makeArray("3", { foo: 4 });
-  assert(array[0] === "3");
-  assert(array[1].foo === 4);
+    const makeArray = (a, b) => [a, b];
+    const array = makeArray("3", { foo: 4 });
+    assert(array[0] === "3");
+    assert(array[1].foo === 4);
 
-  let square = x => x * x;
-  assert(square(3) === 9);
+    let square = x => x * x;
+    assert(square(3) === 9);
 
-  let squareBlock = x => {
-    return x * x;
-  };
-  assert(squareBlock(4) === 16);
+    let squareBlock = x => {
+        return x * x;
+    };
+    assert(squareBlock(4) === 16);
 
-  const message = (who => "Hello " + who)("friends!");
-  assert(message === "Hello friends!");
+    const message = (who => "Hello " + who)("friends!");
+    assert(message === "Hello friends!");
 
-  const sum = ((x, y, z) => x + y + z)(1, 2, 3);
-  assert(sum === 6);
+    const sum = ((x, y, z) => x + y + z)(1, 2, 3);
+    assert(sum === 6);
 
-  const product = ((x, y, z) => {
-    let res = x * y * z;
-    return res;
-  })(5, 4, 2);
-  assert(product === 40);
+    const product = ((x, y, z) => {
+        let res = x * y * z;
+        return res;
+    })(5, 4, 2);
+    assert(product === 40);
 
-  const half = (x => {
-    return x / 2;
-  })(10);
-  assert(half === 5);
+    const half = (x => {
+        return x / 2;
+    })(10);
+    assert(half === 5);
 
-  console.log("PASS");
+    console.log("PASS");
 } catch {
-  console.log("FAIL");
+    console.log("FAIL");
 }
