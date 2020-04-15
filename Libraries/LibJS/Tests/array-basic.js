cea950fd701e98de073e37a4476fc5ca1719dee0
Linus Groh <mail@linusgroh.de>
2020-04-15 20:09:06 +0100

    LibJS: Support empty values in array expression

--- Libraries/LibJS/Tests/array-basic.js
+++ Libraries/LibJS/Tests/array-basic.js
@@ -17,6 +17,29 @@ try {
     assert(a[3] === 7);
     assert(a.length === 4);
 
+    a = [,];
+    assert(a.length === 1);
+    assert(a.toString() === "");
+    assert(a[0] === undefined);
+
+    a = [,,,,];
+    assert(a.length === 4);
+    assert(a.toString() === ",,,");
+    assert(a[0] === undefined);
+    assert(a[1] === undefined);
+    assert(a[2] === undefined);
+    assert(a[3] === undefined);
+
+    a = [1,,2,,,3,];
+    assert(a.length === 6);
+    assert(a.toString() === "1,,2,,,3");
+    assert(a[0] === 1);
+    assert(a[1] === undefined);
+    assert(a[2] === 2);
+    assert(a[3] === undefined);
+    assert(a[4] === undefined);
+    assert(a[5] === 3);
+
     console.log("PASS");
 } catch (e) {
     console.log("FAIL: " + e);
