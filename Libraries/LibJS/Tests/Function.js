1869955e8ee370637d38dc9251b9276d35fcd52d
Linus Groh <mail@linusgroh.de>
2020-04-15 22:20:53 +0100

    LibJS: Remove outdated FIXME now that we have lexical environments

--- Libraries/LibJS/Tests/Function.js
+++ Libraries/LibJS/Tests/Function.js
@@ -17,11 +17,7 @@ try {
     assert(new Function("foo", "if (foo) { return 42; } else { return 'bar'; }")(true) === 42);
     assert(new Function("foo", "if (foo) { return 42; } else { return 'bar'; }")(false) === "bar");
     assert(new Function("return typeof Function()")() === "function");
-    // FIXME: This is equivalent to
-    //   (function (x) { return function (y) { return x + y;} })(1)(2)
-    // and should totally work, but both currently fail with
-    //   Uncaught exception: [ReferenceError]: 'x' not known
-    // assert(new Function("x", "return function (y) { return x + y };")(1)(2) === 3);
+    assert(new Function("x", "return function (y) { return x + y };")(1)(2) === 3);
 
     console.log("PASS");
 } catch (e) {
