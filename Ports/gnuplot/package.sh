a68cade9af1ec385b1780299a1743b8f84d4b4e6
Linus Groh <mail@linusgroh.de>
2020-04-15 14:23:25 +0100

    Ports: Add gnuplot port
    Obviously we don't support many of the common terminals as we're missing
    X11, Qt, WxWidgets, Cairo etc. - but at least the "dumb" terminal
    (ASCII output) and "canvas" terminal (generates JS to plot on a HTML
    <canvas>) are confirmed to be working :^)

---
+++ Ports/gnuplot/package.sh
@@ -0,0 +1,15 @@
+#!/bin/bash ../.port_include.sh
+port=gnuplot
+version=5.2.8
+useconfigure=true
+# Note: gnuplot's source code is hosted on SourceForge, but using the GitHub mirror makes downloading a versioned .tar.gz easier.
+files="https://github.com/gnuplot/gnuplot/archive/${version}.tar.gz gnuplot-${version}.tar.gz"
+configopts="--prefix=$SERENITY_ROOT/Root/usr/local --with-readline=builtin"
+
+pre_configure() {
+    run ./prepare
+}
+
+install() {
+    run make install-strip
+}
