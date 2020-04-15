6a04e7ed34b7a99c56d85186bd916ddd3373c8ca
Noah Rosamilia <ivoahivoah@gmail.com>
2020-04-15 02:46:11 -0400

    Ports: sl (#1802)

---
+++ Ports/sl/package.sh
@@ -0,0 +1,16 @@
+#!/bin/bash ../.port_include.sh
+port=sl
+version=git
+workdir=sl-master
+files="https://github.com/mtoyoda/sl/archive/master.tar.gz sl-git.tar.gz"
+depends="ncurses"
+
+build() {
+    run ${CC} -I${SERENITY_ROOT}/Root/usr/local/include/ncurses -L${SERENITY_ROOT}/Root/usr/local/lib -o sl sl.c -lncurses -ltinfo
+}
+
+post_install() {
+    # Dirty hack that seems to be necessary to make ncurses play nice
+    mkdir -p ${SERENITY_ROOT}/Root/usr/local/share/terminfo/x
+    cp ${SERENITY_ROOT}/Root/usr/local/share/terminfo/78/xterm ${SERENITY_ROOT}/Root/usr/local/share/terminfo/x/
+}
