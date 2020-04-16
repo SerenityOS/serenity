a4aab2c72f035c75f835bf7057214c3badb9c07f
Emanuel Sprung <emanuel.sprung@gmail.com>
2020-04-16 22:21:17 +0200

    Userland: Improve grep utility: recursive call, refactoring

--- Userland/grep.cpp
+++ Userland/grep.cpp
@@ -26,9 +26,12 @@
 
 #include <AK/ByteBuffer.h>
 #include <AK/String.h>
+#include <AK/Utf8View.h>
 #include <AK/Vector.h>
 #include <LibCore/ArgsParser.h>
+#include <LibCore/DirIterator.h>
 #include <LibCore/File.h>
+#include <cstring>
 
 #ifdef __serenity
 #    include <regex.h>
@@ -66,29 +69,93 @@ int main(int argc, char** argv)
     regex_t regex;
 
     if (use_ere) {
+
+        // rebuild grep behaviour: if -e is omitted, use first positional argument as pattern
+        if (pattern == nullptr && files.size())
+            pattern = files.take_first();
+
         if (regcomp(&regex, pattern, REG_EXTENDED) != REG_NOERR) {
             fprintf(stderr, "Error in regular expression pattern.\n");
             return 1;
         }
     }
 
-    if (!files.size()) {
+    auto match = [&](const char* str, const char* filename = "", bool print_filename = false) {
+        size_t last_printed_char_pos { 0 };
+        if (regexec(&regex, str, num_matches, matches, REG_MATCHALL) == REG_NOERR) {
+            size_t n = 0;
+            if (matches[0].match_count && print_filename) {
+                printf("\x1B[34m%s:\x1B[0m", filename);
+            }
+
+            for (size_t i = 0; i < matches[0].match_count; ++i) {
+                printf("%s\x1B[32m%s\x1B[0m",
+                    String(&str[last_printed_char_pos], matches[n].rm_so - last_printed_char_pos).characters(),
+                    String(&str[matches[n].rm_so], matches[n].rm_eo - matches[n].rm_so).characters());
+                last_printed_char_pos = matches[n].rm_eo;
+                n += regex.re_nsub + 1;
+            }
+            printf("%s", String(&str[last_printed_char_pos], strlen(str) - last_printed_char_pos).characters());
+        }
+    };
+
+    auto find_nul = [](const u8* str, size_t length) {
+        for (size_t i = 0; i < length - 1; ++i)
+            if (str[i] == 0) {
+                return true;
+            }
+
+        return false;
+    };
+    auto handle_file = [&match, &find_nul](const char* filename, bool print_filename) -> bool {
+        auto file = Core::File::construct(filename);
+        if (!file->open(Core::IODevice::ReadOnly)) {
+            fprintf(stderr, "Failed to open %s: %s\n", filename, file->error_string());
+            return false;
+        }
+
+        u8 check_buf[1024];
+        size_t bytes = file->read(check_buf, 1024);
+        Utf8View view { StringView { check_buf, bytes } };
+
+        if (!view.validate() || find_nul(check_buf, bytes)) {
+            printf("Skipping binary file (%s)\n", filename);
+            return true;
+        }
+
+        while (file->can_read_line()) {
+            auto line = file->read_line(1024);
+            auto str = String { reinterpret_cast<const char*>(line.data()), line.size() };
+            match(str.characters(), filename, print_filename);
+        }
+        return true;
+    };
+
+    auto add_directory = [&handle_file](String base, Optional<String> recursive, auto handle_directory) -> void {
+        Core::DirIterator it(recursive.value_or(base), Core::DirIterator::Flags::SkipDots);
+        while (it.has_next()) {
+            auto path = it.next_full_path();
+            if (!Core::File::is_directory(path)) {
+                auto key = path.substring(base.length() + 1, path.length() - base.length() - 1);
+                handle_file(key.characters(), true);
+            } else {
+                handle_directory(base, path, handle_directory);
+            }
+        }
+    };
+
+    if (!files.size() && !recursive) {
+        bool first = { true };
         for (;;) {
             char buf[4096];
             auto* str = fgets(buf, sizeof(buf), stdin);
 
-            size_t last_printed_char_pos { 0 };
-            if (regexec(&regex, str, num_matches, matches, REG_MATCHALL) == REG_NOERR) {
-                size_t n = 0;
-                for (size_t i = 0; i < matches[0].match_count; ++i) {
-                    printf("%s\x1B[32m%s\x1B[0m",
-                        String(&str[last_printed_char_pos], matches[n].rm_so - last_printed_char_pos).characters(),
-                        String(&str[matches[n].rm_so], matches[n].rm_eo - matches[n].rm_so).characters());
-                    last_printed_char_pos = matches[n].rm_eo;
-                    n += regex.re_nsub + 1;
-                }
-                printf("%s", String(&str[last_printed_char_pos], strlen(str) - last_printed_char_pos).characters());
-            }
+            if (first && strstr(str, "\0"))
+                printf("Skipping binary file (stdin)\n");
+            else
+                match(str);
+
+            first = false;
 
             if (feof(stdin)) {
                 regfree(&regex);
@@ -98,35 +165,14 @@ int main(int argc, char** argv)
         }
     } else {
         if (recursive) {
-            ASSERT_NOT_REACHED();
+            add_directory(".", {}, add_directory);
+
         } else {
             bool print_filename { files.size() > 1 };
             for (auto& filename : files) {
-                auto file = Core::File::construct(filename);
-                if (!file->open(Core::IODevice::ReadOnly)) {
-                    fprintf(stderr, "Failed to open %s: %s\n", filename, file->error_string());
+                if (!handle_file(filename, print_filename)) {
                     regfree(&regex);
                     return 1;
-                }
-
-                while (file->can_read_line()) {
-                    auto line = file->read_line(1024);
-                    auto str = String { reinterpret_cast<const char*>(line.data()), line.size() };
-                    size_t last_printed_char_pos { 0 };
-
-                    if (regexec(&regex, str.characters(), num_matches, matches, REG_MATCHALL) == REG_NOERR) {
-                        size_t n = 0;
-                        for (size_t i = 0; i < matches[0].match_count; ++i) {
-                            printf("%s%s%s\x1B[32m%s\x1B[0m",
-                                print_filename ? filename : "",
-                                print_filename ? ": " : "",
-                                String(&str.characters()[last_printed_char_pos], matches[n].rm_so - last_printed_char_pos).characters(),
-                                String(&str.characters()[matches[n].rm_so], matches[n].rm_eo - matches[n].rm_so).characters());
-                            last_printed_char_pos = matches[n].rm_eo;
-                            n += regex.re_nsub + 1;
-                        }
-                        printf("%s", String(&str.characters()[last_printed_char_pos], str.length() - last_printed_char_pos).characters());
-                    }
                 }
             }
         }
