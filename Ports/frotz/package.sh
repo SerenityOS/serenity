deca3f97463ae0e21dee6c9e411cdff56cc423fe
Noah Rosamilia <ivoahivoah@gmail.com>
2020-04-18 19:20:04 -0400

    Ports: add frotz port

---
+++ Ports/frotz/package.sh
@@ -0,0 +1,18 @@
+#!/bin/bash ../.port_include.sh
+port=frotz
+version=git
+workdir=frotz-master
+files="https://gitlab.com/DavidGriffith/frotz/-/archive/master/frotz-master.zip frotz-master.zip"
+depends="ncurses"
+
+build() {
+    run make \
+        AR=i686-pc-serenity-ar \
+        RANLIB=i686-pc-serenity-ranlib \
+        PKG_CONFIG_CURSES=no \
+        CURSES_CFLAGS="-I${SERENITY_ROOT}/Root/usr/local/include/ncurses" \
+        CURSES_LDFLAGS="-lncurses -ltinfo" \
+        CURSES=ncurses \
+        USE_UTF8=no \
+        nosound
+}
