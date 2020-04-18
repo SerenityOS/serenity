4e35e48043d9216779a68396d3ab701b6ed8632e
Noah Rosamilia <ivoahivoah@gmail.com>
2020-04-18 03:54:08 -0400

    Ports: Clean up lua port and enable dynamic library loading

--- Ports/lua/package.sh
+++ Ports/lua/package.sh
@@ -2,5 +2,5 @@
 port=lua
 version=5.3.5
 files="http://www.lua.org/ftp/lua-5.3.5.tar.gz lua-5.3.5.tar.gz 4f4b4f323fd3514a68e0ab3da8ce3455"
-makeopts="-j$(nproc) generic"
+makeopts="-j$(nproc) serenity"
 installopts="INSTALL_TOP=$SERENITY_ROOT/Root/"
