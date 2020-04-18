d0cbadad426b62d5173e5af3bd22deb6d6bb139e
Noah Rosamilia <ivoahivoah@gmail.com>
2020-04-18 04:27:43 -0400

    Ports: Install lua to /usr/local

--- Ports/lua/package.sh
+++ Ports/lua/package.sh
@@ -3,4 +3,4 @@ port=lua
 version=5.3.5
 files="http://www.lua.org/ftp/lua-5.3.5.tar.gz lua-5.3.5.tar.gz 4f4b4f323fd3514a68e0ab3da8ce3455"
 makeopts="-j$(nproc) serenity"
-installopts="INSTALL_TOP=$SERENITY_ROOT/Root/"
+installopts="INSTALL_TOP=$SERENITY_ROOT/Root/usr/local"
