2401c056caf4c2f1b4e2b0c40ed44c24d63a303d
theazgra <theazgra@gmail.com>
2020-04-19 09:43:09 +0200

    Browser: Make removal of bookmark persistent
    Json model wasn't stored after bookmark removal.
    Store the model if remove() was successful.

--- Applications/Browser/BookmarksBarWidget.cpp
+++ Applications/Browser/BookmarksBarWidget.cpp
@@ -184,7 +184,10 @@ bool BookmarksBarWidget::remove_bookmark(const String& url)
         auto item_url = model()->data(model()->index(item_index, 1)).to_string();
         if (item_url == url) {
             auto& json_model = *static_cast<GUI::JsonArrayModel*>(model());
-            json_model.remove(item_index);
+
+            if (json_model.remove(item_index))
+                json_model.store();
+
             return true;
         }
     }
