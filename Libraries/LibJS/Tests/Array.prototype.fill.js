5e736aa2f2ddbacd8571d1945ca05ff7c40f1712
angel <angel@ttm.sh>
2020-04-22 17:03:11 +0200

    LibJS: Fixed Array.prototype.fill tests

--- Libraries/LibJS/Tests/Array.prototype.fill.js
+++ Libraries/LibJS/Tests/Array.prototype.fill.js
@@ -1,18 +1,27 @@
 load("test-common.js");
 
+Array.prototype.equals = function(other) {
+    if (this.length != other.length)
+        return false;
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] != other[i])
+            return false;
+    }
+    return true;
+}
+
 try {
     assert(Array.prototype.fill.length === 3);
 
     var array = [1, 2, 3];
-
-    assert(array.fill(4) === [4, 4, 4]);
-    assert(array.fill(4, 1) === [1, 4, 4]);
-    assert(array.fill(4, 1, 2) === [1, 4, 3]);
-    assert(array.fill(4, 3, 3) == [1, 2, 3]);
-    assert(array.fill(4, -3, -2) === [4, 2, 3]);
-    assert(array.fill(4, NaN, NaN) === [1, 2, 3]);
-    assert(array.fill(4, 3, 5) === [1, 2, 3]);
-    assert(Array(3).fill(4) === [4, 4, 4]);
+    assert(array.fill(4).equals([4, 4, 4]));
+    assert(array.fill(4, 1).equals([1, 4, 4]));
+    assert(array.fill(4, 1, 2).equals([1, 4, 3]));
+    assert(array.fill(4, 3, 3).equals([1, 2, 3]));
+    assert(array.fill(4, -3, -2).equals([4, 2, 3]));
+    assert(array.fill(4, NaN, NaN).equals([1, 2, 3]));
+    assert(array.fill(4, 3, 5).equals([1, 2, 3]));
+    assert(Array(3).fill(4).equals([4, 4, 4]));
 
     console.log("PASS");
 } catch (e) {
