f46e9566c826fa2b2611bd9421032c2d1bd88aef
Andreas Kling <kling@serenityos.org>
2020-04-23 20:01:32 +0200

    LibGUI: Tweak default ScrollBar size to make arrow icons centered :^)

--- Libraries/LibGUI/ScrollBar.cpp
+++ Libraries/LibGUI/ScrollBar.cpp
@@ -100,9 +100,9 @@ ScrollBar::ScrollBar(Orientation orientation)
         s_right_arrow_bitmap = &Gfx::CharacterBitmap::create_from_ascii(s_right_arrow_bitmap_data, 9, 9).leak_ref();
 
     if (m_orientation == Orientation::Vertical) {
-        set_preferred_size(15, 0);
+        set_preferred_size(16, 0);
     } else {
-        set_preferred_size(0, 15);
+        set_preferred_size(0, 16);
     }
 
     m_automatic_scrolling_timer->set_interval(100);
