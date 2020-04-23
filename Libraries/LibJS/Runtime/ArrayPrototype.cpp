d158c7da9120303f6b1934b0a82ce359b2eef516
Kesse Jones <kjonesfc@outlook.com>
2020-04-23 08:19:57 -0300

    LibJS: Add Array.prototype.includes

--- Libraries/LibJS/Runtime/ArrayPrototype.cpp
+++ Libraries/LibJS/Runtime/ArrayPrototype.cpp
@@ -56,6 +56,7 @@ ArrayPrototype::ArrayPrototype()
     put_native_function("indexOf", index_of, 1);
     put_native_function("reverse", reverse, 0);
     put_native_function("lastIndexOf", last_index_of, 1);
+    put_native_function("includes", includes, 1);
     put("length", Value(0));
 }
 
@@ -398,6 +399,40 @@ Value ArrayPrototype::last_index_of(Interpreter& interpreter)
     }
 
     return Value(-1);
+}
+
+Value ArrayPrototype::includes(Interpreter& interpreter)
+{
+    auto* array = array_from(interpreter);
+    if (!array)
+        return {};
+
+    i32 array_size = static_cast<i32>(array->elements().size());
+    if (interpreter.argument_count() == 0 || array_size == 0)
+        return Value(false);
+
+    i32 from_index = 0;
+    if (interpreter.argument_count() >= 2) {
+        from_index = interpreter.argument(1).to_i32();
+
+        if (from_index >= array_size)
+            return Value(false);
+
+        auto negative_min_index = ((array_size - 1) * -1);
+        if (from_index < negative_min_index)
+            from_index = 0;
+        else if (from_index < 0)
+            from_index = array_size + from_index;
+    }
+
+    auto value_to_find = interpreter.argument(0);
+    for (i32 i = from_index; i < array_size; ++i) {
+        auto& element = array->elements().at(i);
+        if (typed_eq(interpreter, element, value_to_find).as_bool())
+            return Value(true);
+    }
+
+    return Value(false);
 }
 
 }
---
+++ Libraries/LibJS/Tests/Array.prototype.includes.js
@@ -0,0 +1,20 @@
+load("test-common.js");
+
+try {
+    assert(Array.prototype.includes.length === 1);
+
+    var array = ['hello', 'friends', 1, 2, false];
+
+    assert(array.includes('hello') === true);
+    assert(array.includes(1) === true);
+    assert(array.includes(1, -3) === true);
+    assert(array.includes('serenity') === false);
+    assert(array.includes(false, -1) === true);
+    assert(array.includes(2, -1) === false);
+    assert(array.includes(2, -100) === true);
+    assert(array.includes('friends', 100) === false);
+
+    console.log("PASS");
+} catch (e) {
+    console.log("FAIL: " + e);
+}
