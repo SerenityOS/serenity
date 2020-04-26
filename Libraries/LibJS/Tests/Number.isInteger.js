bf66aa1b0ba525dbd51101b42fd9a8f4eea0e073
Linus Groh <mail@linusgroh.de>
2020-04-26 18:49:23 +0100

    LibJS: Implement Number.isInteger()

---
+++ Libraries/LibJS/Tests/Number.isInteger.js
@@ -0,0 +1,38 @@
+load("test-common.js");
+
+try {
+    assert(Number.isInteger.length === 1);
+
+    assert(Number.isInteger(0) === true);
+    assert(Number.isInteger(42) === true);
+    assert(Number.isInteger(-10000) === true);
+    assert(Number.isInteger(5) === true);
+    assert(Number.isInteger(5.0) === true);
+    assert(Number.isInteger(5.0000000000000001) === true);
+    // FIXME: values outside of i32's range should still return true
+    // assert(Number.isInteger(+2147483647 + 1) === true);
+    // assert(Number.isInteger(-2147483648 - 1) === true);
+    // assert(Number.isInteger(99999999999999999999999999999999999) === true);
+
+    assert(Number.isInteger(5.000000000000001) === false);
+    assert(Number.isInteger(1.23) === false);
+    assert(Number.isInteger("") === false);
+    assert(Number.isInteger("0") === false);
+    assert(Number.isInteger("42") === false);
+    assert(Number.isInteger(true) === false);
+    assert(Number.isInteger(false) === false);
+    assert(Number.isInteger(null) === false);
+    assert(Number.isInteger([]) === false);
+    assert(Number.isInteger(Infinity) === false);
+    assert(Number.isInteger(-Infinity) === false);
+    assert(Number.isInteger(NaN) === false);
+    assert(Number.isInteger() === false);
+    assert(Number.isInteger(undefined) === false);
+    assert(Number.isInteger("foo") === false);
+    assert(Number.isInteger({}) === false);
+    assert(Number.isInteger([1, 2, 3]) === false);
+
+    console.log("PASS");
+} catch (e) {
+    console.log("FAIL: " + e.message);
+}
