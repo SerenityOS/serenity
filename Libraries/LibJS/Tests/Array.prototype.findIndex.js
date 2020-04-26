88b8fb042c20a8dbeba19d18052dfcc56237a717
Kesse Jones <kjonesfc@outlook.com>
2020-04-26 13:22:09 -0300

    LibJS: Add Array.prototype.findIndex

---
+++ Libraries/LibJS/Tests/Array.prototype.findIndex.js
@@ -0,0 +1,29 @@
+load("test-common.js");
+
+try {
+    assert(Array.prototype.findIndex.length === 1);
+
+    assertThrowsError(() => {
+        [].findIndex(undefined);
+    }, {
+        error: TypeError,
+        message: "undefined is not a function"
+    });
+
+    var array = ["hello", "friends", 1, 2, false];
+
+    assert(array.findIndex(value => value === "hello") === 0);
+    assert(array.findIndex((value, index, arr) => index === 1) === 1);
+    assert(array.findIndex(value => value == "1") === 2);
+    assert(array.findIndex(value => value === 1) === 2);
+    assert(array.findIndex(value => typeof(value) !== "string") === 2);
+    assert(array.findIndex(value => typeof(value) === "boolean") === 4);
+    assert(array.findIndex(value => value > 1) === 3);
+    assert(array.findIndex(value => value > 1 && value < 3) === 3);
+    assert(array.findIndex(value => value > 100) === -1);
+    assert([].findIndex(value => value === 1) === -1);
+
+    console.log("PASS");
+} catch (e) {
+    console.log("FAIL: " + e);
+}
