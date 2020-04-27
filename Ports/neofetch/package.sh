74941c0541451670f1f94c796f89816ef941ebdc
Linus Groh <mail@linusgroh.de>
2020-04-27 19:09:43 +0100

    Ports: Add neofetch

---
+++ Ports/neofetch/package.sh
@@ -0,0 +1,6 @@
+#!/bin/bash ../.port_include.sh
+port=neofetch
+version=7.0.0
+useconfigure=false
+depends=bash
+files="https://github.com/dylanaraps/neofetch/archive/${version}.tar.gz neofetch-${version}.tar.gz"
