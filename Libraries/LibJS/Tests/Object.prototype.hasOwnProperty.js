0a843bdc8f777f1ed51f5b9edda5259962a40e15
Linus Groh <mail@linusgroh.de>
2020-04-28 18:59:38 +0100

    LibJS: Handle Object.prototype.hasOwnProperty() with no arg correctly
    I.e. the same as hasOwnProperty(undefined) or
    hasOwnProperty("undefined") :^)

--- Libraries/LibJS/Tests/Object.prototype.hasOwnProperty.js
+++ Libraries/LibJS/Tests/Object.prototype.hasOwnProperty.js
@@ -5,6 +5,12 @@ try {
     o.foo = 1;
     assert(o.hasOwnProperty("foo") === true);
     assert(o.hasOwnProperty("bar") === false);
+    assert(o.hasOwnProperty() === false);
+    assert(o.hasOwnProperty(undefined) === false);
+    o.undefined = 2;
+    assert(o.hasOwnProperty() === true);
+    assert(o.hasOwnProperty(undefined) === true);
+
     console.log("PASS");
 } catch (e) {
     console.log("FAIL: " + e);
