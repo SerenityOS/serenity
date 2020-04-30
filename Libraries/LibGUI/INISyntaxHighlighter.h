5c022ac939257c125a0998ae2124df9ee54f8d12
Hüseyin ASLITÜRK <asliturk@hotmail.com>
2020-05-01 02:28:58 +0300

    LibGUI: INI file syntax highlighter

--- Libraries/LibGUI/INISyntaxHighlighter.h
+++ Libraries/LibGUI/INISyntaxHighlighter.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2020, the SerenityOS developers.
- * All rights reserved.
+ * Copyright (c) 2020, Hüseyin Aslıtürk <asliturk@hotmail.com>
+  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -26,63 +26,23 @@
 
 #pragma once
 
-#include <AK/Noncopyable.h>
-#include <AK/WeakPtr.h>
-#include <LibGUI/TextDocument.h>
-#include <LibGfx/Palette.h>
+#include <LibGUI/SyntaxHighlighter.h>
 
 namespace GUI {
 
-enum class SyntaxLanguage {
-    PlainText,
-    Cpp,
-    Javascript,
-    INI
-};
-
-struct TextStyle {
-    Color color;
-    const Gfx::Font* font { nullptr };
-};
-
-class SyntaxHighlighter {
-    AK_MAKE_NONCOPYABLE(SyntaxHighlighter);
-    AK_MAKE_NONMOVABLE(SyntaxHighlighter);
-
+class IniSyntaxHighlighter final : public SyntaxHighlighter {
 public:
-    virtual ~SyntaxHighlighter();
+    IniSyntaxHighlighter() {}
+    virtual ~IniSyntaxHighlighter() override;
 
-    virtual SyntaxLanguage language() const = 0;
-    virtual void rehighlight(Gfx::Palette) = 0;
-    virtual void highlight_matching_token_pair();
+    virtual bool is_identifier(void*) const override;
 
-    virtual bool is_identifier(void*) const { return false; };
-    virtual bool is_navigatable(void*) const { return false; };
-
-    void attach(TextEditor& editor);
-    void detach();
-    void cursor_did_change();
+    virtual SyntaxLanguage language() const override { return SyntaxLanguage::INI; }
+    virtual void rehighlight(Gfx::Palette) override;
 
 protected:
-    SyntaxHighlighter() {}
-
-    WeakPtr<TextEditor> m_editor;
-
-    struct MatchingTokenPair {
-        void* open;
-        void* close;
-    };
-
-    virtual Vector<MatchingTokenPair> matching_token_pairs() const = 0;
-    virtual bool token_types_equal(void*, void*) const = 0;
-
-    struct BuddySpan {
-        int index { -1 };
-        GUI::TextDocumentSpan span_backup;
-    };
-
-    bool m_has_brace_buddies { false };
-    BuddySpan m_brace_buddies[2];
+    virtual Vector<MatchingTokenPair> matching_token_pairs() const override;
+    virtual bool token_types_equal(void*, void*) const override;
 };
 
 }
