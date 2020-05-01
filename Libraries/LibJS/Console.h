6d84229367640bf9901a5a0be5f7e517570d43d6
Emanuele Torre <torreemanuele6@gmail.com>
2020-05-01 17:34:43 +0200

    LibJS: Add ConsoleMessage concept
    A ConsoleMessage is a struct cointaining:
     * AK::String text;         represents the text of the message sent
                                 to the console.
     * ConsoleMessageKind kind; represents the kind of JS `console` function
                                 from which the message was sent.
    
    Now, Javascript `console` functions only send a ConsoleMessage to the
    Interpreter's Console instead of printing text directly to stdout.
    The Console then stores the recived ConsoleMessage in
    Console::m_messages; the Console does not print to stdout by default.
    
    You can set Console::on_new_message to a void(ConsoleMessage&); this
    function will get call everytime a new message is added to the Console's
    messages and can be used, for example, to print ConsoleMessages to
    stdout or to color the output based on the kind of ConsoleMessage.
    
    In this patch, I also:
      * Re-implement all the previously implemented functions in the
         JavaScript ConsoleObject, as wrappers around Console functions
         that add new message to the Console.
      * Implement console.clear() like so:
        - m_messages get cleared;
        - a new_message with kind set ConsoleMessageKind::Clear gets added
           to m_messages, its text is an empty AK::String;
      * Give credit to linusg in Console.cpp since I used his
         console.trace() algorithm in Console::trace().
    
    I think that having this abstration will help us in the implementation
    of a browser console or a JS debugger. We could also add more MetaData
    to ConsoleMessage, e.g. Object IDs of the arguments passed to console
    functions in order to make hyperlinks, Timestamps, ecc.; which could be
    interesting to see.
    
    This will also help in implementing a `/bin/js` option to make, for
    example, return a ConsoleMessageWrapper to console functions instead of
    undefined. This will be useful to make tests for functions like
    console.count() and console.countClear(). :^)

--- Libraries/LibJS/Console.h
+++ Libraries/LibJS/Console.h
@@ -26,11 +26,28 @@
 
 #pragma once
 
+#include <AK/Function.h>
 #include <AK/HashMap.h>
 #include <AK/Noncopyable.h>
 #include <LibJS/Forward.h>
 
 namespace JS {
+
+enum class ConsoleMessageKind {
+    Clear,
+    Count,
+    Debug,
+    Error,
+    Info,
+    Log,
+    Trace,
+    Warn,
+};
+
+struct ConsoleMessage {
+    ConsoleMessageKind kind;
+    String text;
+};
 
 class Console {
     AK_MAKE_NONCOPYABLE(Console);
@@ -43,12 +60,27 @@ public:
 
     HashMap<String, unsigned>& counters() { return m_counters; }
 
+    void debug(String text);
+    void error(String text);
+    void info(String text);
+    void log(String text);
+    void warn(String text);
+
+    void clear();
+
+    void trace(String title);
+
     unsigned count(String label = "default");
     bool count_reset(String label = "default");
+
+    void add_message(ConsoleMessageKind, String text);
+
+    AK::Function<void(ConsoleMessage&)> on_new_message;
 
 private:
     Interpreter& m_interpreter;
 
+    Vector<ConsoleMessage> m_messages;
     HashMap<String, unsigned> m_counters;
 };
 
