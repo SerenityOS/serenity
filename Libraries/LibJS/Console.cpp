0f7c21ad40d89b0228f017fb9a6b76adb0401302
Emanuele Torre <torreemanuele6@gmail.com>
2020-05-01 15:19:43 +0200

    LibJS: Implement ConsoleObject::count() as a Console::count() wrapper
    Also implement ConsoleObject::count_clear() as a wrapper for
    Console::count_clear()

--- Libraries/LibJS/Console.cpp
+++ Libraries/LibJS/Console.cpp
@@ -24,13 +24,40 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <AK/String.h>
 #include <LibJS/Console.h>
+#include <stdio.h>
 
 namespace JS {
 
 Console::Console(Interpreter& interpreter)
     : m_interpreter(interpreter)
 {
+}
+
+unsigned Console::count(String label)
+{
+    auto counter_value = m_counters.get(label);
+    if (!counter_value.has_value()) {
+        printf("%s: 1\n", label.characters());
+        m_counters.set(label, 1);
+        return 1;
+    }
+
+    auto new_counter_value = counter_value.value() + 1;
+    printf("%s: %d\n", label.characters(), new_counter_value);
+    m_counters.set(label, new_counter_value);
+    return new_counter_value;
+}
+
+bool Console::count_reset(String label)
+{
+    if (!m_counters.contains(label))
+        return false;
+
+    m_counters.remove(label);
+    printf("%s: 0\n", label.characters());
+    return true;
 }
 
 }
