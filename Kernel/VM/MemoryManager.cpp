43593455db03ef82ad94864e220536bae4a20474
Andreas Kling <kling@serenityos.org>
2020-05-06 21:09:16 +0200

    Kernel: Don't assert on OOM in allocate_user_physical_page()
    We now give callers a chance to react to OOM situations.

--- Kernel/VM/MemoryManager.cpp
+++ Kernel/VM/MemoryManager.cpp
@@ -448,7 +448,6 @@ RefPtr<PhysicalPage> MemoryManager::allocate_user_physical_page(ShouldZeroFill s
                 if (purged_page_count) {
                     klog() << "MM: Purge saved the day! Purged " << purged_page_count << " pages from PurgeableVMObject{" << &purgeable_vmobject << "}";
                     page = find_free_user_physical_page();
-                    ASSERT(page);
                     return IterationDecision::Break;
                 }
             }
@@ -457,7 +456,6 @@ RefPtr<PhysicalPage> MemoryManager::allocate_user_physical_page(ShouldZeroFill s
 
         if (!page) {
             klog() << "MM: no user physical pages available";
-            ASSERT_NOT_REACHED();
             return {};
         }
     }
