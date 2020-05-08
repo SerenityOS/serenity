ffa25983144d8629479d0a6a8732568d59a8a61f
Nikolay Kochulin <porez0xfeedface@gmail.com>
2020-05-08 23:50:45 +0300

    Build: Use fakeroot if non-root build is possible
    If genext2fs and fakeroot are installed, the build will no longer
    require superuser privileges.

--- Kernel/sync.sh
+++ Kernel/sync.sh
@@ -4,4 +4,8 @@ set -e
 script_path=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 cd "$script_path"
 
-sudo -E PATH="$PATH" ./build-image-qemu.sh
+if command -v genext2fs 1>/dev/null && command -v fakeroot 1>/dev/null; then
+    fakeroot ./build-image-qemu.sh
+else
+    sudo -E PATH="$PATH" ./build-image-qemu.sh
+fi
