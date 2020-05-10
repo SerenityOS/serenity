f1d15dd39c259e3fe9bee990877637336a019472
Brian Callahan <bcallah@openbsd.org>
2020-05-10 16:35:24 -0400

    Ports: Build ncurses --without-ada to avoid potential build failures.

--- Ports/ncurses/package.sh
+++ Ports/ncurses/package.sh
@@ -2,7 +2,7 @@
 port=ncurses
 version=6.1
 useconfigure=true
-configopts="--with-termlib --enable-pc-files --with-pkg-config=/usr/local/lib/pkgconfig --with-pkg-config-libdir=/usr/local/lib/pkgconfig"
+configopts="--with-termlib --enable-pc-files --with-pkg-config=/usr/local/lib/pkgconfig --with-pkg-config-libdir=/usr/local/lib/pkgconfig --without-ada"
 files="ftp://ftp.gnu.org/gnu/ncurses/ncurses-${version}.tar.gz ncurses-${version}.tar.gz
 http://ftp.gnu.org/gnu/ncurses/ncurses-${version}.tar.gz.sig ncurses-${version}.tar.gz.sig
 https://ftp.gnu.org/gnu/gnu-keyring.gpg gnu-keyring.gpg"
