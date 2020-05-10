092bda2f14dd340e81f84b05aa213a00386ef63c
Brian Callahan <bcallah@openbsd.org>
2020-05-10 12:00:03 -0400

    Ports: Add tr utility

---
+++ Ports/tr/package.sh
@@ -0,0 +1,5 @@
+#!/bin/bash ../.port_include.sh
+port=tr
+version=6.7
+files="https://github.com/ibara/libpuffy/releases/download/libpuffy-1.0/tr-${version}.tar.gz tr-${version}.tar.gz"
+depends=libpuffy
