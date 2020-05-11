8435adf39636e61f6a5a4ee5749da93ce4704b40
AnotherTest <ali.mpfard@gmail.com>
2020-05-12 02:55:10 +0430

    LibHTTP: Support Transfer-Encoding: chunked
    We advertise ourselves to servers as supporting HTTP/1.1; we should put
    our money where our mouth is, and start supporting some of its features.

--- Libraries/LibHTTP/Job.h
+++ Libraries/LibHTTP/Job.h
@@ -27,6 +27,7 @@
 #pragma once
 
 #include <AK/HashMap.h>
+#include <AK/Optional.h>
 #include <LibCore/NetworkJob.h>
 #include <LibCore/TCPSocket.h>
 #include <LibHTTP/HttpRequest.h>
@@ -66,6 +67,7 @@ protected:
         InHeaders,
         InBody,
         Finished,
+        AfterChunkedEncodingTrailer,
     };
 
     HttpRequest m_request;
@@ -75,6 +77,8 @@ protected:
     Vector<ByteBuffer> m_received_buffers;
     size_t m_received_size { 0 };
     bool m_sent_data { 0 };
+    Optional<ssize_t> m_current_chunk_remaining_size;
+    Optional<size_t> m_current_chunk_total_size;
 };
 
 }
