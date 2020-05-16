1a5133aa13029efb8f858dc7882ad160a6fc1f64
Andrew Kaster <andrewdkaster@gmail.com>
2020-05-15 21:32:22 -0600

    Kernel: Add implementation of operator new and delete to kmalloc.cpp
    This was missing before, we were getting it for free from libstdc++

--- Kernel/Heap/kmalloc.cpp
+++ Kernel/Heap/kmalloc.cpp
@@ -218,3 +218,23 @@ void* operator new[](size_t size)
 {
     return kmalloc(size);
 }
+
+void operator delete(void* ptr)
+{
+    return kfree(ptr);
+}
+
+void operator delete(void* ptr, size_t)
+{
+    return kfree(ptr);
+}
+
+void operator delete[](void* ptr)
+{
+    return kfree(ptr);
+}
+
+void operator delete[](void* ptr, size_t)
+{
+    return kfree(ptr);
+}
