788547fe3d7bfe99438c411e2a5ef8f4a899305d
Andreas Kling <kling@serenityos.org>
2020-05-20 15:52:20 +0200

    Revert "AK: Add InitializerList, an implementation of std::initializer_list"
    This reverts commit 0a2cab09282edf357647d2f6e61f9b0680492dca.

--- AK/Vector.h
+++ AK/Vector.h
@@ -33,11 +33,15 @@
 #include <AK/Traits.h>
 #include <AK/kmalloc.h>
 
-#ifdef __serenity__
-#    include <AK/InitializerList.h>
-#else // !__serenity__
-#    include <new>
+// NOTE: We can't include <initializer_list> during the toolchain bootstrap,
+//       since it's part of libstdc++, and libstdc++ depends on LibC.
+//       For this reason, we don't support Vector(initializer_list) in LibC.
+#ifndef SERENITY_LIBC_BUILD
 #    include <initializer_list>
+#endif
+
+#ifndef __serenity__
+#    include <new>
 #endif
 
 namespace AK {
@@ -140,12 +144,14 @@ public:
         clear();
     }
 
+#ifndef SERENITY_LIBC_BUILD
     Vector(std::initializer_list<T> list)
     {
         ensure_capacity(list.size());
         for (auto& item : list)
             unchecked_append(item);
     }
+#endif
 
     Vector(Vector&& other)
         : m_size(other.m_size)
