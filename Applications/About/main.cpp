6a175d42fd46f06f55e5278f83f74bf34408b57d
Nick Vella <nick@nxk.io>
2020-05-24 14:26:33 +0000

    LibGUI, About: Implement system-wide W2k-esque About dialog
    The new About dialog reads version information from /res/version.ini,
    which is generated at build time.

--- Applications/About/main.cpp
+++ Applications/About/main.cpp
@@ -24,13 +24,9 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <LibGUI/AboutDialog.h>
 #include <LibGUI/Application.h>
-#include <LibGUI/BoxLayout.h>
-#include <LibGUI/Button.h>
-#include <LibGUI/Desktop.h>
-#include <LibGUI/Label.h>
-#include <LibGUI/Window.h>
-#include <LibGfx/Font.h>
+#include <LibGfx/Bitmap.h>
 #include <stdio.h>
 #include <sys/utsname.h>
 
@@ -55,73 +51,6 @@ int main(int argc, char** argv)
 
     unveil(nullptr, nullptr);
 
-    auto window = GUI::Window::construct();
-    window->set_title("About SerenityOS");
-    window->set_icon(Gfx::Bitmap::load_from_file("/res/icons/16x16/ladybug.png"));
-    Gfx::Rect window_rect { 0, 0, 224, 178 };
-    window_rect.center_within(GUI::Desktop::the().rect());
-    window->set_resizable(false);
-    window->set_rect(window_rect);
-
-    auto& outer_widget = window->set_main_widget<GUI::Widget>();
-    outer_widget.set_fill_with_background_color(true);
-    outer_widget.set_layout<GUI::VerticalBoxLayout>();
-    outer_widget.layout()->set_margins({ 8, 8, 8, 8 });
-
-    auto& inner_widget = outer_widget.add<GUI::Widget>();
-    inner_widget.set_layout<GUI::HorizontalBoxLayout>();
-    inner_widget.layout()->set_spacing(8);
-
-    auto& left_outer_container = inner_widget.add<GUI::Widget>();
-    left_outer_container.set_layout<GUI::HorizontalBoxLayout>();
-
-    auto& left_inner_container = left_outer_container.add<GUI::Widget>();
-    left_inner_container.set_layout<GUI::VerticalBoxLayout>();
-    left_inner_container.layout()->set_spacing(8);
-    left_inner_container.set_preferred_size(0, 50);
-    left_inner_container.set_size_policy(GUI::SizePolicy::Fill, GUI::SizePolicy::Fixed);
-
-    auto& label = left_inner_container.add<GUI::Label>();
-    label.set_text_alignment(Gfx::TextAlignment::CenterRight);
-    label.set_font(Gfx::Font::default_bold_font());
-    label.set_text("SerenityOS");
-    label.set_size_policy(GUI::SizePolicy::Fill, GUI::SizePolicy::Fixed);
-    label.set_preferred_size(0, 11);
-
-    utsname uts;
-    int rc = uname(&uts);
-    ASSERT(rc == 0);
-
-    auto& version_label = left_inner_container.add<GUI::Label>();
-    version_label.set_text_alignment(Gfx::TextAlignment::CenterRight);
-    version_label.set_text(String::format("Version %s", uts.release));
-    version_label.set_size_policy(GUI::SizePolicy::Fill, GUI::SizePolicy::Fixed);
-    version_label.set_preferred_size(0, 11);
-
-    auto& git_info_label = left_inner_container.add<GUI::Label>();
-    git_info_label.set_text_alignment(Gfx::TextAlignment::CenterRight);
-    git_info_label.set_text(String::format("%s@%s", GIT_BRANCH, GIT_COMMIT));
-    git_info_label.set_size_policy(GUI::SizePolicy::Fill, GUI::SizePolicy::Fixed);
-    git_info_label.set_preferred_size(0, 11);
-
-    auto& right_container = inner_widget.add<GUI::Widget>();
-    right_container.set_layout<GUI::VerticalBoxLayout>();
-
-    auto& icon_label = right_container.add<GUI::Label>();
-    icon_label.set_icon(Gfx::Bitmap::load_from_file("/res/icons/buggie.png"));
-    icon_label.set_tooltip("Buggie");
-    icon_label.set_size_policy(GUI::SizePolicy::Fixed, GUI::SizePolicy::Fixed);
-    icon_label.set_preferred_size(icon_label.icon()->size());
-
-    auto& quit_button = outer_widget.add<GUI::Button>();
-    quit_button.set_text("Okay");
-    quit_button.set_size_policy(GUI::SizePolicy::Fixed, GUI::SizePolicy::Fixed);
-    quit_button.set_preferred_size(100, 20);
-    quit_button.on_click = [](auto) {
-        GUI::Application::the().quit(0);
-    };
-
-    quit_button.set_focus(true);
-    window->show();
+    GUI::AboutDialog::show("SerenityOS", nullptr, nullptr, Gfx::Bitmap::load_from_file("/res/icons/16x16/ladybug.png"));
     return app.exec();
 }
--- Libraries/LibGUI/AboutDialog.h
+++ Libraries/LibGUI/AboutDialog.h
@@ -35,14 +35,17 @@ class AboutDialog final : public Dialog {
 public:
     virtual ~AboutDialog() override;
 
-    static void show(const StringView& name, const Gfx::Bitmap* icon = nullptr, Window* parent_window = nullptr)
+    static void show(const StringView& name, const Gfx::Bitmap* icon = nullptr, Window* parent_window = nullptr, const Gfx::Bitmap* window_icon = nullptr)
     {
         auto dialog = AboutDialog::construct(name, icon, parent_window);
+        if (window_icon)
+            dialog->set_icon(window_icon);
         dialog->exec();
     }
 
 private:
     AboutDialog(const StringView& name, const Gfx::Bitmap* icon = nullptr, Window* parent_window = nullptr);
+    String version_string() const;
 
     String m_name;
     RefPtr<Gfx::Bitmap> m_icon;
