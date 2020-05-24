03ec866fe04fc805bca7564dc2bafc0cb39961cc
Sergey Bugaev <bugaevc@serenityos.org>
2020-05-24 21:08:26 +0300

    Demos: Add Eyes demo
    This is a neat clone of xeyes. It demoes the new ellipse filling functionality,
    as well as the existing global mouse cursor tracking functionality :^)

---
+++ Demos/Eyes/main.cpp
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2020, Sergey Bugaev <bugaevc@serenityos.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "EyesWidget.h"
+#include <LibCore/ArgsParser.h>
+#include <LibGUI/Application.h>
+#include <LibGUI/Window.h>
+
+int main(int argc, char* argv[])
+{
+    int num_eyes = 2;
+
+    Core::ArgsParser args_parser;
+    args_parser.add_option(num_eyes, "Number of eyes", "num-eyes", 'n', "number");
+    args_parser.parse(argc, argv);
+
+    if (pledge("stdio shared_buffer accept rpath unix cpath wpath fattr thread", nullptr) < 0) {
+        perror("pledge");
+        return 1;
+    }
+
+    GUI::Application app(argc, argv);
+
+    if (pledge("stdio shared_buffer accept rpath cpath wpath thread", nullptr) < 0) {
+        perror("pledge");
+        return 1;
+    }
+
+    auto window = GUI::Window::construct();
+    window->set_title("Eyes");
+    window->set_rect(350, 270, 150, 100);
+    window->set_has_alpha_channel(true);
+
+    auto& eyes = window->set_main_widget<EyesWidget>(num_eyes);
+    window->show();
+    eyes.track_cursor_globally();
+
+    return app.exec();
+}
