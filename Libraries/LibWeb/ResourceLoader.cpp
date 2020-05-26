7722413ecaf5681f7f0df0477ea4157a44a34bab
AnotherTest <ali.mpfard@gmail.com>
2020-05-26 15:44:57 +0430

    LibWeb: Provide some properties to inspectors of ResourceLoader

--- Libraries/LibWeb/ResourceLoader.cpp
+++ Libraries/LibWeb/ResourceLoader.cpp
@@ -25,6 +25,7 @@
  */
 
 #include <AK/Base64.h>
+#include <AK/JsonObject.h>
 #include <AK/SharedBuffer.h>
 #include <LibCore/EventLoop.h>
 #include <LibCore/File.h>
@@ -155,6 +156,13 @@ bool ResourceLoader::is_port_blocked(int port)
         if (port == blocked_port)
             return true;
     return false;
+}
+
+void ResourceLoader::save_to(JsonObject& object)
+{
+    Object::save_to(object);
+    object.set("pending_loads", m_pending_loads);
+    object.set("user_agent", m_user_agent);
 }
 
 }
