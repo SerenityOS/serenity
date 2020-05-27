139dbfd5b508bacf7494ba2318eae80f96dc5e76
FalseHonesty <thefalsehonesty@gmail.com>
2020-05-26 22:17:06 -0400

    LibGUI: Add up & down arrow hooks and input history to TextBox
    This patch adds the ability to enable "input history" on a textbox,
    allowing to navigate between the history with the arrow keys.
    
    Also removes a custom TextBox subclass from HackStudio that added
    the exact same hooks, and moves it to use the now standard ones.

--- DevTools/HackStudio/Locator.h
+++ DevTools/HackStudio/Locator.h
@@ -28,8 +28,6 @@
 
 #include <LibGUI/Widget.h>
 
-class LocatorTextBox;
-
 class Locator final : public GUI::Widget {
     C_OBJECT(Locator)
 public:
@@ -44,7 +42,7 @@ private:
 
     Locator();
 
-    RefPtr<LocatorTextBox> m_textbox;
+    RefPtr<GUI::TextBox> m_textbox;
     RefPtr<GUI::Window> m_popup_window;
     RefPtr<GUI::TableView> m_suggestion_view;
 };
