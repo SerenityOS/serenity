7b9f54c7b131bca413b467557885b1b5221e614f
Emanuele Torre <torreemanuele6@gmail.com>
2020-05-28 14:27:20 +0200

    LibC: run clang-format on getopt.h to remove tab characters

--- Libraries/LibC/getopt.h
+++ Libraries/LibC/getopt.h
@@ -28,12 +28,12 @@
 
 #include <sys/cdefs.h>
 
-#define no_argument        0
-#define required_argument  1
-#define optional_argument  2
+#define no_argument 0
+#define required_argument 1
+#define optional_argument 2
 
 struct option {
-    const char *name;
+    const char* name;
     int has_arg;
     int* flag;
     int val;
@@ -41,20 +41,20 @@ struct option {
 
 __BEGIN_DECLS
 
-int	getopt_long(int, char* const*, const char*, const struct option*, int*);
-int	getopt_long_only(int, char* const*, const char*, const struct option*, int*);
+int getopt_long(int, char* const*, const char*, const struct option*, int*);
+int getopt_long_only(int, char* const*, const char*, const struct option*, int*);
 
 #ifndef _GETOPT_DECLARED
-#define	_GETOPT_DECLARED
-int	 getopt(int, char * const [], const char *);
-extern char *optarg;
+#    define _GETOPT_DECLARED
+int getopt(int, char* const[], const char*);
+extern char* optarg;
 extern int optind;
 extern int opterr;
 extern int optopt;
 #endif
 
 #ifndef _OPTRESET_DECLARED
-#define	_OPTRESET_DECLARED
+#    define _OPTRESET_DECLARED
 extern int optreset;
 #endif
 
