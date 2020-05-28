b9051263658c405a69bd8bd8030420157d3ca0e0
Sergey Bugaev <bugaevc@serenityos.org>
2020-05-28 17:52:02 +0300

    Kernel+LibC: Move O_* and MS_* flags to UnixTypes.h
    That's where the other similar definitions reside. Also, use bit shift
    operations for MS_* values.

--- Kernel/FileSystem/VirtualFileSystem.h
+++ Kernel/FileSystem/VirtualFileSystem.h
@@ -40,31 +40,6 @@
 
 namespace Kernel {
 
-#define O_RDONLY (1 << 0)
-#define O_WRONLY (1 << 1)
-#define O_RDWR (O_RDONLY | O_WRONLY)
-#define O_ACCMODE (O_RDONLY | O_WRONLY)
-#define O_EXEC (1 << 2)
-#define O_CREAT (1 << 3)
-#define O_EXCL (1 << 4)
-#define O_NOCTTY (1 << 5)
-#define O_TRUNC (1 << 6)
-#define O_APPEND (1 << 7)
-#define O_NONBLOCK (1 << 8)
-#define O_DIRECTORY (1 << 9)
-#define O_NOFOLLOW (1 << 10)
-#define O_CLOEXEC (1 << 11)
-#define O_DIRECT (1 << 12)
-
-// Kernel internal options
-#define O_NOFOLLOW_NOERROR (1 << 29)
-#define O_UNLINK_INTERNAL (1 << 30)
-
-#define MS_NODEV 1
-#define MS_NOEXEC 2
-#define MS_NOSUID 4
-#define MS_BIND 8
-
 class Custody;
 class Device;
 class FileDescription;
