f95c494dfc706367968504b1f64586867046751e
Sergey Bugaev <bugaevc@serenityos.org>
2020-05-28 18:25:34 +0300

    LibCore+Inspector: Move RPC sockets to /tmp/rpc
    We have a hierarchical filesystem, let's make use of it :^)

--- Libraries/LibCore/EventLoop.cpp
+++ Libraries/LibCore/EventLoop.cpp
@@ -45,6 +45,7 @@
 #include <string.h>
 #include <sys/select.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <sys/time.h>
 #include <time.h>
 #include <unistd.h>
@@ -250,8 +251,22 @@ EventLoop::~EventLoop()
 
 bool EventLoop::start_rpc_server()
 {
-    auto rpc_path = String::format("/tmp/rpc.%d", getpid());
-    int rc = unlink(rpc_path.characters());
+    // Create /tmp/rpc if it doesn't exist.
+    int rc = mkdir("/tmp/rpc", 0777);
+    if (rc == 0) {
+        // Ensure it gets created as 0777 despite our umask.
+        rc = chmod("/tmp/rpc", 0777);
+        if (rc < 0) {
+            perror("chmod /tmp/rpc");
+            // Continue further.
+        }
+    } else if (errno != EEXIST) {
+        perror("mkdir /tmp/rpc");
+        return false;
+    }
+
+    auto rpc_path = String::format("/tmp/rpc/%d", getpid());
+    rc = unlink(rpc_path.characters());
     if (rc < 0 && errno != ENOENT) {
         perror("unlink");
         return false;
