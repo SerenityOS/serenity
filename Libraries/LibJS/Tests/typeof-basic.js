ed5616e99eb710df53ae00bdebfa73ec046d6f28
Marcin Gasperowicz <xnooga@gmail.com>
2020-06-02 23:26:39 +0200

    LibJS: Make typeof return undefined for undefined variables
    This makes `typeof i_dont_exist` return `undefined` instead of 
    throwing an error.

--- Libraries/LibJS/Tests/typeof-basic.js
+++ Libraries/LibJS/Tests/typeof-basic.js
@@ -8,6 +8,19 @@ try {
     assert(typeof null === "object");
     assert(typeof undefined === "undefined");
 
+    var iExist = 1;
+    assert(typeof iExist === "number");
+    assert(typeof iDontExist === "undefined");
+
+    var calls = 0;
+    Object.defineProperty(globalThis, "foo", {
+        get() {
+            calls++;
+        },
+    });
+    assert(typeof foo === "undefined");
+    assert(calls === 1);
+
     console.log("PASS");
 } catch (e) {
     console.log("FAIL: " + e);
