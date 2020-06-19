31f7cfc82e011fda1aaf633219f9fc6e858ba860
Nico Weber <thakis@chromium.org>
2020-06-19 08:33:41 -0400

    Ports: Pull dropbear from a mirror
    The main web page has been offline for at least a week.
    This gets the dropbear port building again.

--- Ports/dropbear/package.sh
+++ Ports/dropbear/package.sh
@@ -1,9 +1,9 @@
 #!/bin/bash ../.port_include.sh
 port=dropbear
 version=2019.78
-files="https://matt.ucc.asn.au/dropbear/releases/dropbear-${version}.tar.bz2 dropbear-${version}.tar.bz2
-https://matt.ucc.asn.au/dropbear/releases/dropbear-${version}.tar.bz2.asc dropbear-${version}.tar.bz2.asc
-https://matt.ucc.asn.au/dropbear/releases/dropbear-key-2015.asc dropbear-key-2015.asc"
+files="https://mirror.dropbear.nl/mirror/releases/dropbear-${version}.tar.bz2 dropbear-${version}.tar.bz2
+https://mirror.dropbear.nl/mirror/releases/dropbear-${version}.tar.bz2.asc dropbear-${version}.tar.bz2.asc
+https://mirror.dropbear.nl/mirror/releases/dropbear-key-2015.asc dropbear-key-2015.asc"
 
 auth_type="sig"
 auth_opts="--keyring ./dropbear-key-2015.asc dropbear-${version}.tar.bz2.asc"
