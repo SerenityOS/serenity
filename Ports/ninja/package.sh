7f73f0300c16f9769590b1eef14c5e6c4918ace8
Nico Weber <thakis@chromium.org>
2020-06-21 18:41:01 -0400

    Ports: Add ninja
    - 1.8.2 for now, newer versions need high-res timestamp file APIs
      which serenity doesn't have yet
    - pselect() instead of ppoll() for now, same reason (depends on #2609)
    - no good default for -j yet (see nproc.patch)
    - `-l` probably doesn't work yet (see loadavg.patch), but I've never
      used that anyways
    - some minor include patches that I've also sent upstream
    
    Other than that, this seems to work reasonably well. It currently
    produces some spam on stdout from probably the shell.

---
+++ Ports/ninja/package.sh
@@ -0,0 +1,17 @@
+#!/bin/bash ../.port_include.sh
+port=ninja
+version=1.8.2
+workdir=ninja-1.8.2
+files="https://github.com/ninja-build/ninja/archive/v1.8.2.tar.gz ninja-v1.8.2.tar.gz"
+
+build() {
+    CXX=i686-pc-serenity-g++ CXXFLAGS="--sysroot=${SERENITY_ROOT}/Build/Root" \
+    LDFLAGS="--sysroot=${SERENITY_ROOT}/Build/Root" \
+    # platform=linux is close enough.
+    run ./configure.py --bootstrap --platform=linux --force-pselect
+    strip "${workdir}/ninja"
+}
+
+install() {
+    cp "${workdir}/ninja" "${SERENITY_ROOT}/Build/Root/usr/local/bin/ninja"
+}
