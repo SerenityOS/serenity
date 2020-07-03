6769f19f6295ae1f4d8ffd4b20227d0874a6aa63
AnotherTest <ali.mpfard@gmail.com>
2020-07-04 04:20:51 +0430

    LibC: Implement cf{g,s}et{i,o}speed

--- Libraries/LibC/termios.cpp
+++ Libraries/LibC/termios.cpp
@@ -74,4 +74,79 @@ speed_t cfgetospeed(const struct termios* tp)
 {
     return tp->c_ospeed;
 }
+
+static int baud_rate_from_speed(speed_t speed)
+{
+    int rate = -EINVAL;
+    switch (speed) {
+    case B0:
+        rate = 0;
+        break;
+    case B50:
+        rate = 50;
+        break;
+    case B75:
+        rate = 75;
+        break;
+    case B110:
+        rate = 110;
+        break;
+    case B134:
+        rate = 134;
+        break;
+    case B150:
+        rate = 150;
+        break;
+    case B200:
+        rate = 200;
+        break;
+    case B300:
+        rate = 300;
+        break;
+    case B600:
+        rate = 600;
+        break;
+    case B1200:
+        rate = 1200;
+        break;
+    case B1800:
+        rate = 1800;
+        break;
+    case B2400:
+        rate = 2400;
+        break;
+    case B4800:
+        rate = 4800;
+        break;
+    case B9600:
+        rate = 9600;
+        break;
+    case B19200:
+        rate = 19200;
+        break;
+    case B38400:
+        rate = 38400;
+        break;
+    }
+
+    return rate;
+}
+
+int cfsetispeed(struct termios* tp, speed_t speed)
+{
+    auto ispeed = baud_rate_from_speed(speed);
+    if (ispeed > 0) {
+        tp->c_ispeed = ispeed;
+    }
+    __RETURN_WITH_ERRNO(ispeed, 0, -1);
+}
+
+int cfsetospeed(struct termios* tp, speed_t speed)
+{
+    auto ospeed = baud_rate_from_speed(speed);
+    if (ospeed > 0) {
+        tp->c_ispeed = ospeed;
+    }
+    __RETURN_WITH_ERRNO(ospeed, 0, -1);
+}
 }
