69477f3d5982fc822ae89b8d5eec0e1eb65c7a88
Muhammad Zahalqa <m@tryfinally.com>
2020-07-19 18:03:32 +0300

    AK: fix overflow bug in binary_search
    fix mixed int/size_t assignment and overflow in computing middle index.

--- AK/BinarySearch.h
+++ AK/BinarySearch.h
@@ -38,13 +38,13 @@ int integral_compare(const T& a, const T& b)
 }
 
 template<typename T, typename Compare>
-T* binary_search(T* haystack, size_t haystack_size, const T& needle, Compare compare = integral_compare, int* nearby_index = nullptr)
+T* binary_search(T* haystack, size_t haystack_size, const T& needle, Compare compare = integral_compare, size_t* nearby_index = nullptr)
 {
-    int low = 0;
-    int high = haystack_size - 1;
+    size_t low = 0;
+    size_t high = haystack_size - 1;
     while (low <= high) {
-        int middle = (low + high) / 2;
-        int comparison = compare(needle, haystack[middle]);
+        size_t middle = low + ((high - low) / 2);
+        size_t comparison = compare(needle, haystack[middle]);
         if (comparison < 0)
             high = middle - 1;
         else if (comparison > 0)
