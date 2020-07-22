51345beee5e4edcda6a48309cbb8f71c5d800e1f
Matthew Olsson <matthewcolsson@gmail.com>
2020-07-22 14:50:54 -0700

    LibGfx: Add FloatPoint methods
    Adds some conversion constructors, as well as the missing arithmetic
    operations.

--- Libraries/LibGfx/Painter.cpp
+++ Libraries/LibGfx/Painter.cpp
@@ -1565,7 +1565,7 @@ void Painter::fill_path(Path& path, Color color, WindingRule winding_rule)
 #ifdef FILL_PATH_DEBUG
     size_t i { 0 };
     for (auto& segment : segments)
-        draw_line(IntPoint(segment.from.x(), segment.from.y()), IntPoint(segment.to.x(), segment.to.y()), Color::from_hsv(++i / segments.size() * 255, 255, 255), 1);
+        draw_line(segment.from, segment.to, Color::from_hsv(++i / segments.size() * 255, 255, 255), 1);
 #endif
 }
 
