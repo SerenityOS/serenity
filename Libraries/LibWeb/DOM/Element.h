d1fdc1a2cfdb6dc2992a8690cbc5f9a082c7fc1a
Andreas Kling <kling@serenityos.org>
2020-07-24 13:25:42 +0200

    LibWeb: Use [Reflect] for Element.id and Element.className :^)

--- Libraries/LibWeb/DOM/Element.h
+++ Libraries/LibWeb/DOM/Element.h
@@ -84,12 +84,6 @@ public:
     String inner_html() const;
     void set_inner_html(StringView);
 
-    String id() const { return attribute(HTML::AttributeNames::id); }
-    void set_id(const String& value) { set_attribute(HTML::AttributeNames::id, value); }
-
-    String class_name() const { return attribute(HTML::AttributeNames::class_); }
-    void set_class_name(const String& value) { set_attribute(HTML::AttributeNames::class_, value); }
-
 protected:
     RefPtr<LayoutNode> create_layout_node(const StyleProperties* parent_style) override;
 
