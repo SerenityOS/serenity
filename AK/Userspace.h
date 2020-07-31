e39a410546bb4bee172c8378afbc6bf99af73b30
Andreas Kling <kling@serenityos.org>
2020-07-31 16:26:44 +0200

    AK: Add Userspace<T>, a wrapper for userspace pointers
    This will be used in the kernel to wrap pointers into userspace memory
    without convenient direct access. The idea is to use the compiler to
    enforce that we don't dereference userspace pointers.

---
+++ AK/Userspace.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include <AK/Types.h>
+
+namespace AK {
+
+template<typename T>
+class Userspace {
+public:
+    Userspace() { }
+
+    operator bool() const { return m_ptr; }
+    operator FlatPtr() const { return (FlatPtr)m_ptr; }
+
+#if defined(KERNEL)
+    Userspace(FlatPtr ptr)
+        : m_ptr(ptr)
+    {
+    }
+
+    FlatPtr ptr() const { return m_ptr; }
+#else
+    Userspace(T ptr)
+        : m_ptr(ptr)
+    {
+    }
+
+    T ptr() const { return m_ptr; }
+#endif
+
+private:
+#if defined(KERNEL)
+    FlatPtr m_ptr { 0 };
+#else
+    T m_ptr { nullptr };
+#endif
+};
+
+}
+
+using AK::Userspace;
