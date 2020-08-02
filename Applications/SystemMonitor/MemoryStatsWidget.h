71fe04ec63b04530eaedaebd2f085540c4ec0da1
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2020-08-02 16:35:49 +0200

    SystemMonitor: Split up kmalloc labels
    Otherwise, the numbers overflow the space after a while.

--- Applications/SystemMonitor/MemoryStatsWidget.h
+++ Applications/SystemMonitor/MemoryStatsWidget.h
@@ -45,6 +45,8 @@ private:
     GraphWidget& m_graph;
     RefPtr<GUI::Label> m_user_physical_pages_label;
     RefPtr<GUI::Label> m_supervisor_physical_pages_label;
-    RefPtr<GUI::Label> m_kmalloc_label;
+    RefPtr<GUI::Label> m_kmalloc_space_label;
     RefPtr<GUI::Label> m_kmalloc_count_label;
+    RefPtr<GUI::Label> m_kfree_count_label;
+    RefPtr<GUI::Label> m_kmalloc_difference_label;
 };
