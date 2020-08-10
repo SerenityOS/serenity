95ed37db8651b2a6f2a85a312c7979ac8d19a1ae
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2020-08-10 23:50:38 +0200

    AK: Mark compilation-unit-only functions as static
    This enables a nice warning in case a function becomes dead code. Also, add forgotten
    header to Base64.cpp, which would cause an issue later when we enable -Wmissing-declarations.

--- AK/Base64.cpp
+++ AK/Base64.cpp
@@ -24,6 +24,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <AK/Base64.h>
 #include <AK/ByteBuffer.h>
 #include <AK/String.h>
 #include <AK/StringBuilder.h>
