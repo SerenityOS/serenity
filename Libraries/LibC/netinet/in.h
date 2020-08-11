c4d4d56a449ec8ee139b07795c8c2c4346455c7e
Andreas Kling <kling@serenityos.org>
2020-08-11 19:40:16 +0200

    LibC: Add in6_addr and sockaddr_in6 structs

--- Libraries/LibC/netinet/in.h
+++ Libraries/LibC/netinet/in.h
@@ -54,4 +54,16 @@ struct sockaddr_in {
     char sin_zero[8];
 };
 
+struct in6_addr {
+    uint8_t s6_addr[16];
+};
+
+struct sockaddr_in6 {
+    uint16_t sin6_family;      // AF_INET6.
+    uint16_t sin6_port;        // Port number.
+    uint32_t sin6_flowinfo;    // IPv6 traffic class and flow information.
+    struct in6_addr sin6_addr; // IPv6 address.
+    uint32_t sin6_scope_id;    // Set of interfaces for a scop
+};
+
 __END_DECLS
