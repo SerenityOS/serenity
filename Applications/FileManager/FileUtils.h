0d3418f4b055d8b4d853f152628d5e082cd2caaa
Andreas Kling <kling@serenityos.org>
2020-08-17 11:30:30 +0200

    FileManager: Don't close() twice when copying a file

--- Applications/FileManager/FileUtils.h
+++ Applications/FileManager/FileUtils.h
@@ -27,6 +27,7 @@
 #pragma once
 
 #include <AK/String.h>
+#include <LibCore/Forward.h>
 #include <sys/stat.h>
 
 namespace FileUtils {
@@ -34,7 +35,7 @@ namespace FileUtils {
 int delete_directory(String directory, String& file_that_caused_error);
 bool copy_file_or_directory(const String& src_path, const String& dst_path);
 String get_duplicate_name(const String& path, int duplicate_count);
-bool copy_file(const String& src_path, const String& dst_path, const struct stat& src_stat, int src_fd);
+bool copy_file(const String& dst_path, const struct stat& src_stat, Core::File&);
 bool copy_directory(const String& src_path, const String& dst_path, const struct stat& src_stat);
 
 }
