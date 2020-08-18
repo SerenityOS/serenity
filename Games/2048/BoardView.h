663397c04e1e10d8f4eeb068a45d37a8fd2d83cb
Sergey Bugaev <bugaevc@serenityos.org>
2020-08-18 16:01:25 +0300

    2048: Separate game logic from the view :^)
    Look Ali, it's simple:
    
    * The *model* (in many cases, an instance of GUI::Model, but it doesn't have to
      be) should implement the "business logic" (in this case, game logic) and
      should not concern itself with how the data/state is displayed to the user.
    
    * The *view*, conversely, should interact with the user (display data/state,
      accept input) and should not concern itself with the logic. As an example, a
      GUI::Button can display some text and accept clicks -- it doesn't know or care
      what that text *means*, or how that click affects the app state. All it does
      is it gets its text from *somebody* and notifies *somebody* of clicks.
    
    * The *controller* connects the model to the view, and acts as "glue" between
      them.
    
    You could connect *several different* views to one model (see FileManager), or
    use identical views with different models (e.g. a table view can display pretty
    much anything, depending on what model you connect to it).
    
    In this case, the model is the Game class, which maintains a board and
    implements the rules of 2048, including tracking the score. It does not display
    anything, and it does not concern itself with undo management. The view is the
    BoardView class, which displays a board and accepts keyboard input, but doesn't
    know how exactly the tiles move or merge -- all it gets is a board state, ready
    to be displayed. The controller is our main(), which connects the two classes
    and bridges between their APIs. It also implements undo management, by basically
    making straight-up copies of the game.
    
    Isn't this lovely?

---
+++ Games/2048/BoardView.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2020, the SerenityOS developers.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include "Game.h"
+#include <LibGUI/Widget.h>
+
+class BoardView final : public GUI::Widget {
+    C_OBJECT(BoardView)
+
+public:
+    BoardView(const Game::Board*);
+    virtual ~BoardView() override;
+
+    void set_score(size_t score);
+    void set_board(const Game::Board* board);
+
+    Function<void(Game::Direction)> on_move;
+
+private:
+    virtual void resize_event(GUI::ResizeEvent&) override;
+    virtual void paint_event(GUI::PaintEvent&) override;
+    virtual void keydown_event(GUI::KeyEvent&) override;
+
+    size_t rows() const;
+    size_t columns() const;
+
+    void pick_font();
+    Gfx::IntRect score_rect() const;
+
+    Color background_color_for_cell(u32 value);
+    Color text_color_for_cell(u32 value);
+
+    float m_padding { 0 };
+    float m_cell_size { 0 };
+
+    size_t m_score { 0 };
+    String m_score_text;
+    const Game::Board* m_board { nullptr };
+};
