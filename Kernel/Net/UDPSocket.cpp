8a985ec2bc65a29bd5fdbb91ea3c2bbb7f690c00
Muhammad Zahalqa <m@tryfinally.com>
2020-08-21 02:05:36 +0300

    Kernel: modify UDPSocket to use singelton hashmap for sockets by ports map

--- Kernel/Net/UDPSocket.cpp
+++ Kernel/Net/UDPSocket.cpp
@@ -24,6 +24,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <AK/Singleton.h>
 #include <Kernel/Devices/RandomDevice.h>
 #include <Kernel/Net/NetworkAdapter.h>
 #include <Kernel/Net/Routing.h>
@@ -43,10 +44,7 @@ void UDPSocket::for_each(Function<void(const UDPSocket&)> callback)
 
 Lockable<HashMap<u16, UDPSocket*>>& UDPSocket::sockets_by_port()
 {
-    static Lockable<HashMap<u16, UDPSocket*>>* s_map;
-    if (!s_map)
-        s_map = new Lockable<HashMap<u16, UDPSocket*>>;
-    return *s_map;
+    return Singleton<Lockable<HashMap<u16, UDPSocket*>>>::the();
 }
 
 SocketHandle<UDPSocket> UDPSocket::from_port(u16 port)
