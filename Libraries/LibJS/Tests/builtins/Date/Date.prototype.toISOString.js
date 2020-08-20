5262823cae6e999df1c8da90154497c5c92f5fd6
Nico Weber <thakis@chromium.org>
2020-08-20 16:29:27 -0400

    LibJS: Implement Date.prototype.toISOString()

---
+++ Libraries/LibJS/Tests/builtins/Date/Date.prototype.toISOString.js
@@ -0,0 +1,9 @@
+test("basic functionality", () => {
+    expect(new Date(1597955034555).toISOString()).toBe("2020-08-20T20:23:54.555Z");
+
+    // FIXME: Add these once they work.
+    //expect(new Date(Date.UTC(22020)).toISOString()).toBe("+022020-01-01T00:00:00.000Z");
+    //expect(new Date(Date.UTC(1950)).toISOString()).toBe("1950-01-01T00:00:00.000Z");
+    //expect(new Date(Date.UTC(1800)).toISOString()).toBe("1800-01-01T00:00:00.000Z");
+    //expect(new Date(Date.UTC(-100)).toISOString()).toBe("-000100-01-01T00:00:00.000Z");
+});
