690c736c3a568ed54d448eb31615496b45016ad3
Andreas Kling <kling@serenityos.org>
2020-08-23 14:02:04 +0200

    ThemeEditor: Add a handful of widgets to the preview
    This needs to be spruced up a bit, but having a set of common widgets
    gives you a quick idea of what the theme will look like. :^)

--- Applications/ThemeEditor/PreviewWidget.h
+++ Applications/ThemeEditor/PreviewWidget.h
@@ -31,6 +31,8 @@
 
 namespace ThemeEditor {
 
+class MiniWidgetGallery;
+
 class PreviewWidget final : public GUI::Frame {
     C_OBJECT(PreviewWidget);
 
@@ -44,11 +46,14 @@ private:
     explicit PreviewWidget(const Gfx::Palette&);
 
     virtual void paint_event(GUI::PaintEvent&) override;
+    virtual void resize_event(GUI::ResizeEvent&) override;
 
     Gfx::Palette m_preview_palette;
 
     RefPtr<Gfx::Bitmap> m_active_window_icon;
     RefPtr<Gfx::Bitmap> m_inactive_window_icon;
+
+    RefPtr<MiniWidgetGallery> m_gallery;
 };
 
 }
