2d9a03942a3381ccaaad82d84e67261c902ff0d9
Nico Weber <thakis@chromium.org>
2020-08-25 20:07:36 -0400

    LibJS: Add some more tests, mostly around leap years

--- Libraries/LibJS/Tests/builtins/Date/Date.prototype.getUTCMonth.js
+++ Libraries/LibJS/Tests/builtins/Date/Date.prototype.getUTCMonth.js
@@ -4,4 +4,23 @@ test("basic functionality", () => {
     expect(d.getUTCMonth()).not.toBeNaN();
     expect(d.getUTCMonth()).toBeGreaterThanOrEqual(0);
     expect(d.getUTCMonth()).toBeLessThanOrEqual(11);
+
+    expect(new Date(Date.UTC(2020, 11)).getUTCMonth()).toBe(11);
+});
+
+test("leap years", () => {
+    expect(new Date(Date.UTC(2019, 1, 29)).getUTCDate()).toBe(1);
+    expect(new Date(Date.UTC(2019, 1, 29)).getUTCMonth()).toBe(2);
+    expect(new Date(Date.UTC(2100, 1, 29)).getUTCDate()).toBe(1);
+    expect(new Date(Date.UTC(2100, 1, 29)).getUTCMonth()).toBe(2);
+
+    expect(new Date(Date.UTC(2000, 1, 29)).getUTCDate()).toBe(29);
+    expect(new Date(Date.UTC(2000, 1, 29)).getUTCMonth()).toBe(1);
+    expect(new Date(Date.UTC(2020, 1, 29)).getUTCDate()).toBe(29);
+    expect(new Date(Date.UTC(2020, 1, 29)).getUTCMonth()).toBe(1);
+
+    expect(new Date(Date.UTC(2019, 2, 1)).getUTCDate()).toBe(1);
+    expect(new Date(Date.UTC(2019, 2, 1)).getUTCMonth()).toBe(2);
+    expect(new Date(Date.UTC(2020, 2, 1)).getUTCDate()).toBe(1);
+    expect(new Date(Date.UTC(2020, 2, 1)).getUTCMonth()).toBe(2);
 });
