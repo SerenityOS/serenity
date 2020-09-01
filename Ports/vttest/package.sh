28ff8f3376f1ddd075df4db88f176b859ecb6f0b
Andreas Kling <kling@serenityos.org>
2020-09-01 17:48:37 +0200

    Ports: Bump version of the "vttest" port

--- Ports/vttest/package.sh
+++ Ports/vttest/package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash ../.port_include.sh
 port=vttest
-version=20200303
+version=20200610
 useconfigure=true
 files="https://invisible-island.net/datafiles/release/vttest.tar.gz vttest.tar.gz"
