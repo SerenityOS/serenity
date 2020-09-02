612c1bc84d63f8f296dc9292ff806d1be48b7156
asynts <asynts@gmail.com>
2020-09-02 17:12:53 +0200

    Userland: Use Buffered<T> in gunzip.

--- Libraries/LibCore/FileStream.h
+++ Libraries/LibCore/FileStream.h
@@ -26,6 +26,7 @@
 
 #pragma once
 
+#include <AK/Buffered.h>
 #include <AK/Stream.h>
 #include <LibCore/File.h>
 
@@ -48,6 +49,18 @@ public:
             return file_result.error();
 
         return InputFileStream { file_result.value() };
+    }
+
+    static Result<Buffered<InputFileStream>, String> open_buffered(StringView filename, IODevice::OpenMode mode = IODevice::OpenMode::ReadOnly, mode_t permissions = 0644)
+    {
+        ASSERT((mode & 0xf) == IODevice::OpenMode::ReadOnly || (mode & 0xf) == IODevice::OpenMode::ReadWrite);
+
+        auto file_result = File::open(filename, mode, permissions);
+
+        if (file_result.is_error())
+            return file_result.error();
+
+        return Buffered<InputFileStream> { file_result.value() };
     }
 
     size_t read(Bytes bytes) override
@@ -145,9 +158,26 @@ public:
         return OutputFileStream { file_result.value() };
     }
 
+    static Result<Buffered<OutputFileStream>, String> open_buffered(StringView filename, IODevice::OpenMode mode = IODevice::OpenMode::WriteOnly, mode_t permissions = 0644)
+    {
+        ASSERT((mode & 0xf) == IODevice::OpenMode::WriteOnly || (mode & 0xf) == IODevice::OpenMode::ReadWrite);
+
+        auto file_result = File::open(filename, mode, permissions);
+
+        if (file_result.is_error())
+            return file_result.error();
+
+        return Buffered<OutputFileStream> { file_result.value() };
+    }
+
     static OutputFileStream stdout()
     {
         return OutputFileStream { Core::File::stdout() };
+    }
+
+    static Buffered<OutputFileStream> stdout_buffered()
+    {
+        return Buffered<OutputFileStream> { Core::File::stdout() };
     }
 
     size_t write(ReadonlyBytes bytes) override
