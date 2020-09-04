914d53af24bd0c9f8e0a6e35346928388224a2ad
asynts <asynts@gmail.com>
2020-09-04 14:30:53 +0200

    Deflate: Fix deadly typo.

--- Libraries/LibCompress/Gzip.cpp
+++ Libraries/LibCompress/Gzip.cpp
@@ -97,7 +97,6 @@ size_t GzipDecompressor::read(Bytes bytes)
         if (m_input_stream.eof())
             return 0;
 
-        // FIXME: This fails with the new changes?
         BlockHeader header;
         m_input_stream >> Bytes { &header, sizeof(header) };
 
