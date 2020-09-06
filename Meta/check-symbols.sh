255bbd2eca5d0fc19f996d2d60f1ee72fd015fb6
Itamar <itamar8910@gmail.com>
2020-09-06 21:08:18 +0300

    Travis: Run script that checks for forbidden symbols in LibC
    check-symbols.sh fails the build if undefined __cx_guard_* symbols are
    found in  LibC.
    
    This will help us catch port breakage sooner.

---
+++ Meta/check-symbols.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+script_path=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+cd "$script_path/.."
+
+# The __cxa_guard_* calls are generated for (non trivial) initialzation of local static objects.
+# These symbols are OK to use within serenity code, but they are problematic in LibC because their
+# existence breaks ports (the implementation of these symbols resides in libstdc++.a which we do not link against in ports).
+# To eliminate the need for these symbols, avoid doing non-trivial construction of local statics in LibC.
+
+FORBIDDEN_SYMBOLS="__cxa_guard_acquire __cxa_guard_release"
+LIBC_PATH="Build/Libraries/LibC/libc.a" 
+for forbidden_symbol in $FORBIDDEN_SYMBOLS; do
+    # check if symbol is undefined
+    nm $LIBC_PATH | grep "U $forbidden_symbol"
+    if [ $? -eq 0 ]; then
+        echo "Forbidden undefined symbol in LibC: $forbidden_symbol"
+        echo "See comment in Meta/check-symbols.sh for more info"
+        exit 1
+    fi
+done
