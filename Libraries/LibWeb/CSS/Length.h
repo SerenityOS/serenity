dcc2c8a12545762746d2f14a3cb4781c7b75b9d1
Jakob-Niklas See <git@nwex.de>
2020-09-08 20:39:09 +0200

    LibWeb: Add support for viewport-relative length units (#3433)
    We can now use vh, vw, vmax and vmin as length units in CSS.

--- Libraries/LibWeb/CSS/Length.h
+++ Libraries/LibWeb/CSS/Length.h
@@ -42,6 +42,10 @@ public:
         Ex,
         Em,
         Rem,
+        Vh,
+        Vw,
+        Vmax,
+        Vmin,
     };
 
     Length() { }
@@ -85,7 +89,16 @@ public:
     bool is_percentage() const { return m_type == Type::Percentage; }
     bool is_auto() const { return m_type == Type::Auto; }
     bool is_absolute() const { return m_type == Type::Px || m_type == Type::Pt; }
-    bool is_relative() const { return m_type == Type::Ex || m_type == Type::Em || m_type == Type::Rem; }
+    bool is_relative() const
+    {
+        return m_type == Type::Ex
+            || m_type == Type::Em
+            || m_type == Type::Rem
+            || m_type == Type::Vh
+            || m_type == Type::Vw
+            || m_type == Type::Vmax
+            || m_type == Type::Vmin;
+    }
 
     float raw_value() const { return m_value; }
     ALWAYS_INLINE float to_px(const LayoutNode& layout_node) const
