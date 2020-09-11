c231abc410344c5bd84ce5e2cfbf78ff4041738b
Andreas Kling <kling@serenityos.org>
2020-09-11 14:51:02 +0200

    LibGUI: Use widget override cursors in ColorInput

--- Libraries/LibGUI/ColorInput.cpp
+++ Libraries/LibGUI/ColorInput.cpp
@@ -100,11 +100,11 @@ void ColorInput::mouseup_event(MouseEvent& event)
 void ColorInput::mousemove_event(MouseEvent& event)
 {
     if (color_rect().contains(event.position())) {
-        window()->set_cursor(Gfx::StandardCursor::Hand);
+        set_override_cursor(Gfx::StandardCursor::Hand);
         event.accept();
         return;
     } else {
-        window()->set_cursor(Gfx::StandardCursor::IBeam);
+        set_override_cursor(Gfx::StandardCursor::IBeam);
     }
 
     TextEditor::mousemove_event(event);
