2f975904099b89fc77966b9f68f56711c1cc91e5
Andreas Kling <kling@serenityos.org>
2020-09-14 16:31:19 +0200

    Meta: Make the text-to-cpp-string thingy pass shellcheck

--- Meta/text-to-cpp-string.sh
+++ Meta/text-to-cpp-string.sh
@@ -3,7 +3,7 @@
 # $2 input path
 
 echo "extern const char $1[];"
-echo -n "const char $1[] = R\"("
+printf "const char %s[] = R\"(" "$1"
 grep -v '^ *#' < "$2" | while IFS= read -r line; do
   echo "$line"
 done
