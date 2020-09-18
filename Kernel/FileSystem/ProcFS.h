ba238ac62a5edf24246179ebbb6d7a369636a6c2
Tom <tomut@yahoo.com>
2020-09-18 17:06:28 -0600

    Kernel: Simplify ProcFS callbacks by using function pointers directly

--- Kernel/FileSystem/ProcFS.h
+++ Kernel/FileSystem/ProcFS.h
@@ -59,12 +59,12 @@ private:
 
     struct ProcFSDirectoryEntry {
         ProcFSDirectoryEntry() { }
-        ProcFSDirectoryEntry(const char* a_name, unsigned a_proc_file_type, bool a_supervisor_only, Function<Optional<KBuffer>(InodeIdentifier)>&& a_read_callback = nullptr, Function<ssize_t(InodeIdentifier, const UserOrKernelBuffer&, size_t)>&& a_write_callback = nullptr, RefPtr<ProcFSInode>&& a_inode = nullptr)
+        ProcFSDirectoryEntry(const char* a_name, unsigned a_proc_file_type, bool a_supervisor_only, Optional<KBuffer>(*read_callback)(InodeIdentifier) = nullptr, ssize_t(*write_callback)(InodeIdentifier, const UserOrKernelBuffer&, size_t) = nullptr, RefPtr<ProcFSInode>&& a_inode = nullptr)
             : name(a_name)
             , proc_file_type(a_proc_file_type)
             , supervisor_only(a_supervisor_only)
-            , read_callback(move(a_read_callback))
-            , write_callback(move(a_write_callback))
+            , read_callback(read_callback)
+            , write_callback(write_callback)
             , inode(move(a_inode))
         {
         }
@@ -72,8 +72,8 @@ private:
         const char* name { nullptr };
         unsigned proc_file_type { 0 };
         bool supervisor_only { false };
-        Function<Optional<KBuffer>(InodeIdentifier)> read_callback;
-        Function<ssize_t(InodeIdentifier, const UserOrKernelBuffer&, size_t)> write_callback;
+        Optional<KBuffer>(*read_callback)(InodeIdentifier);
+        ssize_t(*write_callback)(InodeIdentifier, const UserOrKernelBuffer&, size_t);
         RefPtr<ProcFSInode> inode;
         InodeIdentifier identifier(unsigned fsid) const;
     };
