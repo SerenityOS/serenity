3f0ece34c22edd11b7321b4a33789deafc18c455
AnotherTest <ali.mpfard@gmail.com>
2020-09-28 14:28:10 +0330

    LibGUI: Add a Shell syntax highlighter

--- Libraries/LibGUI/ShellSyntaxHighlighter.h
+++ Libraries/LibGUI/ShellSyntaxHighlighter.h
@@ -26,64 +26,24 @@
 
 #pragma once
 
-#include <AK/Noncopyable.h>
-#include <AK/WeakPtr.h>
-#include <LibGUI/TextDocument.h>
-#include <LibGfx/Palette.h>
+#include <LibGUI/SyntaxHighlighter.h>
 
 namespace GUI {
 
-enum class SyntaxLanguage {
-    PlainText,
-    Cpp,
-    JavaScript,
-    INI,
-    Shell,
-};
-
-struct TextStyle {
-    Color color;
-    const Gfx::Font* font { nullptr };
-};
-
-class SyntaxHighlighter {
-    AK_MAKE_NONCOPYABLE(SyntaxHighlighter);
-    AK_MAKE_NONMOVABLE(SyntaxHighlighter);
-
+class ShellSyntaxHighlighter : public SyntaxHighlighter {
 public:
-    virtual ~SyntaxHighlighter();
+    ShellSyntaxHighlighter() { }
+    virtual ~ShellSyntaxHighlighter() override;
 
-    virtual SyntaxLanguage language() const = 0;
-    virtual void rehighlight(Gfx::Palette) = 0;
-    virtual void highlight_matching_token_pair();
+    virtual bool is_identifier(void*) const override;
+    virtual bool is_navigatable(void*) const override;
 
-    virtual bool is_identifier(void*) const { return false; };
-    virtual bool is_navigatable(void*) const { return false; };
-
-    void attach(TextEditor& editor);
-    void detach();
-    void cursor_did_change();
+    virtual SyntaxLanguage language() const override { return SyntaxLanguage::Shell; }
+    virtual void rehighlight(Gfx::Palette) override;
 
 protected:
-    SyntaxHighlighter() { }
-
-    WeakPtr<TextEditor> m_editor;
-
-    struct MatchingTokenPair {
-        void* open;
-        void* close;
-    };
-
-    virtual Vector<MatchingTokenPair> matching_token_pairs() const = 0;
-    virtual bool token_types_equal(void*, void*) const = 0;
-
-    struct BuddySpan {
-        int index { -1 };
-        GUI::TextDocumentSpan span_backup;
-    };
-
-    bool m_has_brace_buddies { false };
-    BuddySpan m_brace_buddies[2];
+    virtual Vector<MatchingTokenPair> matching_token_pairs() const override;
+    virtual bool token_types_equal(void*, void*) const override;
 };
 
 }
