e58ad1f7bd001efaa991f77e95720b2700c66fbc
Peter Elliott <pelliott@ualberta.ca>
2020-10-01 22:22:40 -0700

    LibTar: Create LibTar and TarStream
    TarStream parses the contents of a tar file from an InputStream

---
+++ Libraries/LibTar/TarStream.h
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2020, Peter Elliott <pelliott@ualberta.ca>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include <AK/Span.h>
+#include <AK/Stream.h>
+#include <LibTar/Tar.h>
+
+namespace Tar {
+
+class TarStream;
+
+class TarFileStream : public InputStream {
+public:
+    size_t read(Bytes) override;
+    bool unreliable_eof() const override;
+
+    bool read_or_error(Bytes) override;
+    bool discard_or_error(size_t count) override;
+
+private:
+    TarFileStream(TarStream& stream);
+    TarStream& m_tar_stream;
+    int m_generation;
+
+    friend class TarStream;
+};
+
+class TarStream {
+public:
+    TarStream(InputStream&);
+    void advance();
+    bool finished() const { return m_finished; }
+    const Header& header() const { return m_header; }
+    TarFileStream file_contents();
+
+private:
+    Header m_header;
+    InputStream& m_stream;
+    unsigned long m_file_offset { 0 };
+    int m_generation { 0 };
+    bool m_finished { false };
+
+    friend class TarFileStream;
+};
+
+}
