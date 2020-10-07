e49ea1b5209f3b76c9caae317554a6a4a6188d5f
Matthew Olsson <matthewcolsson@gmail.com>
2020-10-07 12:21:15 -0700

    LibJS: Disallow 'continue' & 'break' outside of their respective scopes
    'continue' is no longer allowed outside of a loop, and an unlabeled
    'break' is not longer allowed outside of a loop or switch statement.
    Labeled 'break' statements are still allowed everywhere, even if the
    label does not exist.

---
+++ Libraries/LibJS/Tests/break-continue-syntax-errors.js
@@ -0,0 +1,18 @@
+test("'break' syntax errors", () => {
+    expect("break").not.toEval();
+    expect("break label").not.toEval();
+    expect("{ break }").not.toEval();
+    // FIXME: Parser does not throw error on nonexistent label
+    // expect("{ break label }.not.toEval();
+    expect("label: { break label }").toEval();
+});
+
+test("'continue' syntax errors", () => {
+    expect("continue").not.toEval();
+    expect("continue label").not.toEval();
+    expect("{ continue }").not.toEval();
+    expect("{ continue label }").not.toEval();
+    expect("label: { continue label }").not.toEval();
+
+    expect("switch (true) { case true: continue; }").not.toEval();
+});
