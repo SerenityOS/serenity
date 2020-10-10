65ee2f07b725e8e71c156de8a8741e8e545c8786
Itamar <itamar8910@gmail.com>
2020-10-10 17:47:21 +0300

    LibC: Make it possible to use ASSERTs without initializing stdio

--- Libraries/LibC/sys/internals.h
+++ Libraries/LibC/sys/internals.h
@@ -37,6 +37,7 @@ extern void __malloc_init();
 extern void __stdio_init();
 extern void _init();
 extern bool __environ_is_malloced;
+extern bool __stdio_is_initialized;
 
 int __cxa_atexit(AtExitFunction exit_function, void* parameter, void* dso_handle);
 void __cxa_finalize(void* dso_handle);
