9b28297019359924544ed39121e7ea7ba6ab8ce9
Lenny Maiorani <lenny@colorado.edu>
2020-10-21 09:39:54 -0400

    HashFunctions: constexpr capability
    Problem:
    - Hash functions can be `constexpr`, but are not.
    
    Solution:
    - Change `inline` keyword to `constexpr`.
    - Add `static_assert` tests to ensure the hash functions work in a
      `constexpr` context.

--- AK/Tests/TestHashFunctions.cpp
+++ AK/Tests/TestHashFunctions.cpp
@@ -31,30 +31,32 @@
 
 TEST_CASE(int_hash)
 {
-    EXPECT_EQ(int_hash(42), 3564735745u);
-    EXPECT_EQ(int_hash(0), 1177991625u);
+    static_assert(int_hash(42) == 3564735745u);
+    static_assert(int_hash(0) == 1177991625u);
 }
 
 TEST_CASE(double_hash)
 {
-    EXPECT_EQ(double_hash(42), 524450u);
-    EXPECT_EQ(double_hash(0), 12384u);
+    static_assert(double_hash(42) == 524450u);
+    static_assert(double_hash(0) == 12384u);
 }
 
 TEST_CASE(pair_int_hash)
 {
-    EXPECT_EQ(pair_int_hash(42, 17), 339337046u);
-    EXPECT_EQ(pair_int_hash(0, 0), 954888656u);
+    static_assert(pair_int_hash(42, 17) == 339337046u);
+    static_assert(pair_int_hash(0, 0) == 954888656u);
 }
 
 TEST_CASE(u64_hash)
 {
-    EXPECT_EQ(u64_hash(42), 2824066580u);
-    EXPECT_EQ(u64_hash(0), 954888656u);
+    static_assert(u64_hash(42) == 2824066580u);
+    static_assert(u64_hash(0) == 954888656u);
 }
 
 TEST_CASE(ptr_hash)
 {
+    // These tests are not static_asserts because the values are
+    // different and the goal is to bind the behavior.
     if constexpr (sizeof(FlatPtr) == 8) {
         EXPECT_EQ(ptr_hash(FlatPtr(42)), 2824066580u);
         EXPECT_EQ(ptr_hash(FlatPtr(0)), 954888656u);
@@ -68,6 +70,14 @@ TEST_CASE(ptr_hash)
         EXPECT_EQ(ptr_hash(reinterpret_cast<const void*>(42)), 3564735745u);
         EXPECT_EQ(ptr_hash(reinterpret_cast<const void*>(0)), 1177991625u);
     }
+}
+
+TEST_CASE(constexpr_ptr_hash)
+{
+    // This test does not check the result because the goal is just to
+    // ensure the function can be executed in a constexpr context. The
+    // "ptr_hash" test binds the result.
+    static_assert(ptr_hash(FlatPtr(42)));
 }
 
 TEST_MAIN(HashFunctions)
