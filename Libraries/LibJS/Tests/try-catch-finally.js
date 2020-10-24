SerenityOS/serenity

commit 9ffe45bc769f3718c311e7f2e03f158634cd10b4

LibJS: Don't allow TryStatement without catch clause

Libraries/LibJS/Parser.cpp
@@ -1421,10 +1421,7 @@ NonnullRefPtr<TryStatement> Parser::parse_try_statement()
     consume(TokenType::Try);
 
     auto block = parse_block_statement();
-
-    RefPtr<CatchClause> handler;
-    if (match(TokenType::Catch))
-        handler = parse_catch_clause();
+    auto handler = parse_catch_clause();
 
     RefPtr<BlockStatement> finalizer;
     if (match(TokenType::Finally)) {

commit 23353353adb1aa0cc37782b70cde1dbf2765e2b9

LibJS: Allow try statement with only finally clause
This was a regression introduced by 9ffe45b - a TryStatement without
'catch' clause *is* allowed, if it has a 'finally' clause. It is now
checked properly that at least one of both is present.

Libraries/LibJS/Parser.cpp
@@ -1443,13 +1443,19 @@ NonnullRefPtr<TryStatement> Parser::parse_try_statement()
     consume(TokenType::Try);
 
     auto block = parse_block_statement();
-    auto handler = parse_catch_clause();
+
+    RefPtr<CatchClause> handler;
+    if (match(TokenType::Catch))
+        handler = parse_catch_clause();
 
     RefPtr<BlockStatement> finalizer;
     if (match(TokenType::Finally)) {
         consume();
         finalizer = parse_block_statement();
     }
+
+    if (!handler && !finalizer)
+        syntax_error("try statement must have a 'catch' or 'finally' clause");
 
     return create_ast_node<TryStatement>(move(block), move(handler), move(finalizer));
 }

Libraries/LibJS/Tests/try-catch-finally.js
@@ -161,3 +161,12 @@ test("try/catch/finally with exception in try, catch and finally", () => {
     expect(catchHasBeenExecuted).toBeTrue();
     expect(finallyHasBeenExecuted).toBeTrue();
 });
+
+test("try statement must have either 'catch' or 'finally' clause", () => {
+    expect("try {} catch {}").toEval();
+    expect("try {} catch (e) {}").toEval();
+    expect("try {} finally {}").toEval();
+    expect("try {} catch {} finally {}").toEval();
+    expect("try {} catch (e) {} finally {}").toEval();
+    expect("try {}").not.toEval();
+});