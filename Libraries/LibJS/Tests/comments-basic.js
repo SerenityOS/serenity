edfce1714819a6c37cef20b529eeabd7f68e910c
Linus Groh <mail@linusgroh.de>
2020-10-26 20:10:50 +0000

    LibJS: Emit TokenType::Invalid for unterminated multi-line comments

--- Libraries/LibJS/Tests/comments-basic.js
+++ Libraries/LibJS/Tests/comments-basic.js
@@ -21,3 +21,11 @@ return i;`;
 
     expect(source).toEvalTo(1);
 });
+
+test("unterminated multi-line comment", () => {
+    expect("/*").not.toEval();
+    expect("/**").not.toEval();
+    expect("/*/").not.toEval();
+    expect("/* foo").not.toEval();
+    expect("foo /*").not.toEval();
+});
