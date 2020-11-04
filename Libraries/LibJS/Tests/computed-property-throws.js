5243a239ebd957e89bb2e41e2d55989294a2fec8
Linus Groh <mail@linusgroh.de>
2020-11-04 09:48:48 +0000

    LibJS: Don't create "valid" PropertyName from null string
    When value.to_string() throws an exception it returns a null string in
    which case we must not construct a valid PropertyName.
    
    Also ASSERT in PropertyName(String) and PropertyName(FlyString) to
    prevent this from happening in the future.
    
    Fixes #3941.

--- Libraries/LibJS/Tests/computed-property-throws.js
+++ Libraries/LibJS/Tests/computed-property-throws.js
@@ -6,3 +6,14 @@ test("Issue #3459, exception in computed property expression", () => {
         "foo"[bar]();
     }).toThrow(ReferenceError);
 });
+
+test("Issue #3941, exception in computed property's toString()", () => {
+    expect(() => {
+        const o = {
+            toString() {
+                throw Error();
+            },
+        };
+        "foo"[o];
+    }).toThrow(Error);
+});
