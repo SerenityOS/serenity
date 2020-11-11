ff1ec4779ff7ccd0c62b8446e3f1224a18581655
Itamar <itamar8910@gmail.com>
2020-11-11 22:03:51 +0200

    LibELF: Temporarily disable name demangling
    Demangling is currently not supported with shared libraries, but we will fix this soon :)

--- Libraries/LibELF/Loader.cpp
+++ Libraries/LibELF/Loader.cpp
@@ -263,8 +263,12 @@ String Loader::symbolicate(u32 address, u32* out_offset) const
             auto demangled_name = demangle(symbol.name);
 #else
             auto& demangled_name = symbol.demangled_name;
-            if (demangled_name.is_null())
-                demangled_name = demangle(symbol.name);
+            if (demangled_name.is_null()) {
+
+                // FIXME: Temporarily disabled demangling since it is currently not supported with shared libraries
+                demangled_name = symbol.name;
+                // demangled_name = demangle(symbol.name);
+            }
 #endif
 
             if (out_offset) {
