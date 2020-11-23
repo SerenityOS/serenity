722165d164efc1a15e97aa40da1bae09b227b31d
Jakub Berkop <32750982+Safffatttu@users.noreply.github.com>
2020-11-23 18:41:15 +0100

    LibGUI: Widget::action_for_key_event() should fail for invalid shortcuts (#4137)
    Previously GUI::Actions which were constructed without initializing
    m_shortcut could be activated via an invalid GUI::Shortcut.
    
    Steps to reproduce:
    It was possible to enable TextEditor's markdown preview by pressing
    Ctrl or Alt (Cmd/Ctrl) keys, which should not happen, as this Action
    did not specify a shortcut.
    
    This fix should apply to all other cases where actions where declared
    without specifying a shortcut.

--- Libraries/LibGUI/Widget.cpp
+++ Libraries/LibGUI/Widget.cpp
@@ -739,6 +739,11 @@ bool Widget::is_backmost() const
 Action* Widget::action_for_key_event(const KeyEvent& event)
 {
     Shortcut shortcut(event.modifiers(), (KeyCode)event.key());
+
+    if (!shortcut.is_valid()) {
+        return nullptr;
+    }
+
     Action* found_action = nullptr;
     for_each_child_of_type<Action>([&](auto& action) {
         if (action.shortcut() == shortcut) {
