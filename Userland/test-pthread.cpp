b1fd06eb4cf27d0439e240551a632f31a6dfe6f9
Sergey Bugaev <bugaevc@serenityos.org>
2020-11-24 20:55:58 +0300

    Userland: Add a test for pthread_once()

---
+++ Userland/test-pthread.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2020, Sergey Bugaev <bugaevc@serenityos.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <AK/Assertions.h>
+#include <AK/NonnullRefPtrVector.h>
+#include <LibThread/Thread.h>
+#include <pthread.h>
+
+static void test_once()
+{
+    constexpr size_t threads_count = 10;
+
+    static Vector<int> v;
+    v.clear();
+    pthread_once_t once = PTHREAD_ONCE_INIT;
+    NonnullRefPtrVector<LibThread::Thread, threads_count> threads;
+
+    for (size_t i = 0; i < threads_count; i++) {
+        threads.append(LibThread::Thread::construct([&] {
+            return pthread_once(&once, [] {
+                v.append(35);
+                sleep(1);
+            });
+        }));
+        threads.last().start();
+    }
+    for (auto& thread : threads)
+        thread.join();
+
+    ASSERT(v.size() == 1);
+}
+
+int main()
+{
+    test_once();
+    return 0;
+}
