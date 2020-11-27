7c87fb9106b4d188550a5f0ea4c80255b1a81485
Luke <luke.wilde@live.co.uk>
2020-11-27 20:13:55 +0000

    LibCrypto: Require intent parameter in CTR constructor
    This was preventing clang from building.

--- Libraries/LibCrypto/Cipher/Mode/CTR.h
+++ Libraries/LibCrypto/Cipher/Mode/CTR.h
@@ -113,8 +113,10 @@ public:
     // Must intercept `Intent`, because AES must always be set to
     // Encryption, even when decrypting AES-CTR.
     // TODO: How to deal with ciphers that take different arguments?
+    // FIXME: Add back the default intent parameter once clang-11 is the default in GitHub Actions.
+    //        Once added back, remove the parameter where it's constructed in get_random_bytes in Kernel/Random.h.
     template<typename KeyType, typename... Args>
-    explicit constexpr CTR(const KeyType& user_key, size_t key_bits, Intent = Intent::Encryption, Args... args)
+    explicit constexpr CTR(const KeyType& user_key, size_t key_bits, Intent, Args... args)
         : Mode<T>(user_key, key_bits, Intent::Encryption, args...)
     {
     }
