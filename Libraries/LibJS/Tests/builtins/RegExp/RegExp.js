c51b4549861525b33472ec02fe53e7b88d206440
Linus Groh <mail@linusgroh.de>
2020-11-27 23:42:58 +0000

    LibJS: Make RegExp.prototype.source spec-compliant
    Basically:
    - And edge case for this object being RegExp.prototype.source
    - Return "(?:)" for empty pattern
    - Escape some things properly

--- Libraries/LibJS/Tests/builtins/RegExp/RegExp.js
+++ Libraries/LibJS/Tests/builtins/RegExp/RegExp.js
@@ -1,9 +1,8 @@
 test("basic functionality", () => {
-    // FIXME: update when toString is spec-compliant
-    expect(RegExp().toString()).toBe("//");
-    expect(RegExp(undefined).toString()).toBe("//");
+    expect(RegExp().toString()).toBe("/(?:)/");
+    expect(RegExp(undefined).toString()).toBe("/(?:)/");
     expect(RegExp("foo").toString()).toBe("/foo/");
     expect(RegExp("foo", undefined).toString()).toBe("/foo/");
     expect(RegExp("foo", "g").toString()).toBe("/foo/g");
-    expect(RegExp(undefined, "g").toString()).toBe("//g");
+    expect(RegExp(undefined, "g").toString()).toBe("/(?:)/g");
 });
