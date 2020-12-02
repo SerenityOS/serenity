e3c1a3d964a74d64080fe8f35f23b912a36911e0
Linus Groh <mail@linusgroh.de>
2020-12-02 12:11:21 +0000

    LibJS: Move TypedArray length getter to prototype

---
+++ Libraries/LibJS/Tests/builtins/TypedArray/TypedArray.prototype.length.js
@@ -0,0 +1,10 @@
+// Update when more typed arrays get added
+const TYPED_ARRAYS = [Uint8Array, Uint16Array, Uint32Array, Int8Array, Int16Array, Int32Array];
+
+test("basic functionality", () => {
+    TYPED_ARRAYS.forEach(T => {
+        const typedArray = new T(42);
+        expect(Object.hasOwnProperty(typedArray, "length")).toBeFalse();
+        expect(typedArray.length).toBe(42);
+    });
+});
