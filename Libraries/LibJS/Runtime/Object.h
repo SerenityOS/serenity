83443d13b44a354d447bdf471791ccdab88829e2
Linus Groh <mail@linusgroh.de>
2020-12-04 21:14:43 +0000

    LibJS: Move is_script_function() to Object
    Every Object should be able to tell us if it is a ScriptFunction, not
    only Function objects!

--- Libraries/LibJS/Runtime/Object.h
+++ Libraries/LibJS/Runtime/Object.h
@@ -117,6 +117,7 @@ public:
     virtual bool is_error() const { return false; }
     virtual bool is_function() const { return false; }
     virtual bool is_native_function() const { return false; }
+    virtual bool is_script_function() const { return false; }
     virtual bool is_bound_function() const { return false; }
     virtual bool is_proxy_object() const { return false; }
     virtual bool is_regexp_object() const { return false; }
