9d75e36c6ebb75cc03be502ae03dd0dddccbfb56
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2020-12-05 20:24:27 +0100

    Clipboard: Remove-unused bpp metadata
    It's just more attack surface, and can be deduced from the format anyway.

--- MenuApplets/ClipboardHistory/ClipboardHistoryModel.cpp
+++ MenuApplets/ClipboardHistory/ClipboardHistoryModel.cpp
@@ -51,6 +51,29 @@ String ClipboardHistoryModel::column_name(int column) const
     }
 }
 
+static const char* bpp_for_format_resilient(String format)
+{
+    unsigned format_uint = format.to_uint().value_or(static_cast<unsigned>(Gfx::BitmapFormat::Invalid));
+    // Cannot use Gfx::Bitmap::bpp_for_format here, as we have to accept invalid enum values.
+    switch (static_cast<Gfx::BitmapFormat>(format_uint)) {
+    case Gfx::BitmapFormat::Indexed1:
+        return "1";
+    case Gfx::BitmapFormat::Indexed2:
+        return "2";
+    case Gfx::BitmapFormat::Indexed4:
+        return "4";
+    case Gfx::BitmapFormat::Indexed8:
+        return "8";
+    case Gfx::BitmapFormat::RGB32:
+    case Gfx::BitmapFormat::RGBA32:
+        return "32";
+    case Gfx::BitmapFormat::Invalid:
+        /* fall-through */
+    default:
+        return "?";
+    }
+}
+
 GUI::Variant ClipboardHistoryModel::data(const GUI::ModelIndex& index, GUI::ModelRole role) const
 {
     if (role != GUI::ModelRole::Display)
@@ -67,7 +90,7 @@ GUI::Variant ClipboardHistoryModel::data(const GUI::ModelIndex& index, GUI::Mode
             builder.append('x');
             builder.append(data_and_type.metadata.get("height").value_or("?"));
             builder.append('x');
-            builder.append(data_and_type.metadata.get("bpp").value_or("?"));
+            builder.append(bpp_for_format_resilient(data_and_type.metadata.get("height").value_or("0")));
             builder.append(" bitmap");
             builder.append("]");
             return builder.to_string();
