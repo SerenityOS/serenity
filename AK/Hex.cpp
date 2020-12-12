2b91f08e165b0fa4fc8e201b8b4c6d2cfb707901
Conrad Pankoff <deoxxa@fknsrs.biz>
2020-12-13 09:35:14 +1100

    AK: Add {encode,decode}_hex similar to {encode,decode}_base64

---
+++ AK/Hex.cpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2020, Andreas Kling <kling@serenityos.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <AK/Array.h>
+#include <AK/ByteBuffer.h>
+#include <AK/Hex.h>
+#include <AK/String.h>
+#include <AK/StringBuilder.h>
+#include <AK/StringView.h>
+#include <AK/Types.h>
+#include <AK/Vector.h>
+
+namespace AK {
+
+static u8 decode_hex_digit(char digit)
+{
+    if (digit >= '0' && digit <= '9')
+        return digit - '0';
+    if (digit >= 'a' && digit <= 'f')
+        return 10 + (digit - 'a');
+    if (digit >= 'A' && digit <= 'F')
+        return 10 + (digit - 'A');
+    return 255;
+}
+
+Optional<ByteBuffer> decode_hex(const StringView& input)
+{
+    if ((input.length() % 2) != 0)
+        return {};
+
+    auto output = ByteBuffer::create_zeroed(input.length() / 2);
+
+    for (long unsigned int i = 0; i < input.length() / 2; i++) {
+        auto c1 = decode_hex_digit(input[i * 2]);
+        if (c1 >= 16)
+            return {};
+
+        auto c2 = decode_hex_digit(input[i * 2 + 1]);
+        if (c2 >= 16)
+            return {};
+
+        output[i] = (c1 << 4) + c2;
+    }
+
+    return output;
+}
+
+String encode_hex(ReadonlyBytes input)
+{
+    StringBuilder output(input.size() * 2);
+
+    for (auto ch : input)
+        output.appendf("%02x", ch);
+
+    return output.build();
+}
+
+}
