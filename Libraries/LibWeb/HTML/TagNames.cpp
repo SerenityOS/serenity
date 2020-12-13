b73d0bb6c87692b17c94960a1b60e7886f70846f
Andreas Kling <kling@serenityos.org>
2020-12-13 17:54:40 +0100

    LibWeb: Update stale #includes for HTML/TagNames.* move

--- Libraries/LibWeb/HTML/TagNames.cpp
+++ Libraries/LibWeb/HTML/TagNames.cpp
@@ -24,7 +24,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <LibWeb/DOM/TagNames.h>
+#include <LibWeb/HTML/TagNames.h>
 
 namespace Web::HTML::TagNames {
 
