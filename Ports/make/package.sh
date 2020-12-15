bcabbbda8b0e4446c3d38b2772f3dee5b3dcc4cf
Andreas Kling <kling@serenityos.org>
2020-12-15 01:06:18 +0100

    Ports: Bump GNU make to version 4.3

--- Ports/make/package.sh
+++ Ports/make/package.sh
@@ -1,10 +1,10 @@
 #!/bin/bash ../.port_include.sh
 port=make
-version=4.2.1
+version=4.3
 useconfigure=true
-files="https://ftp.gnu.org/gnu/make/make-${version}.tar.bz2 make-${version}.tar.bz2
-http://ftp.gnu.org/gnu/make/make-${version}.tar.bz2.sig make-${version}.tar.bz2.sig
+files="https://ftp.gnu.org/gnu/make/make-${version}.tar.gz make-${version}.tar.gz
+http://ftp.gnu.org/gnu/make/make-${version}.tar.gz.sig make-${version}.tar.gz.sig
 https://ftp.gnu.org/gnu/gnu-keyring.gpg gnu-keyring.gpg"
 auth_type="sig"
-auth_opts="--keyring ./gnu-keyring.gpg make-${version}.tar.bz2.sig"
+auth_opts="--keyring ./gnu-keyring.gpg make-${version}.tar.gz.sig"
 configopts="--target=i686-pc-serenity --with-sysroot=/ --without-guile"
