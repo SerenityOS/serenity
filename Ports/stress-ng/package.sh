1cb246ef7f8cc0002e26d15cce02c581538a3f73
Brian Gianforcaro <b.gianfo@gmail.com>
2020-12-20 01:23:10 -0800

    Ports: Add initial stress-ng port to find bugs in serenity
    This is a very WIP port bringing stress-ng to SerenityOS.
    
    stress-ng is great at doing multi-workload stress testing, this allows it to
    find unique and interesting intermixed pairs of stressful operations which cause bugs.
    
    This initial port just rips out an non applicable functionality in order to get
    the port to compile.

---
+++ Ports/stress-ng/package.sh
@@ -0,0 +1,10 @@
+#!/bin/bash ../.port_include.sh
+port=stress-ng
+version=0.11.23
+workdir=stress-ng-${version}
+files="https://github.com/ColinIanKing/stress-ng/archive/V${version}.tar.gz stress-ng-${version}.tar.gz"
+installopts="DESTDIR=$SERENITY_ROOT/Build/Root"
+
+build() {
+    run make STATIC=1
+}
