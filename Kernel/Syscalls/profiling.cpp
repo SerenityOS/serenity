41e3c695501c31cb3c473b81732ded4c6c303b77
Andreas Kling <kling@serenityos.org>
2020-12-22 09:59:14 +0100

    Kernel: Abort core dump generation if any substep fails
    And make an effort to propagate errors out from the inner parts.
    This fixes an issue where the kernel would infinitely loop in coredump
    generation if the TmpFS filled up.

--- Kernel/Syscalls/profiling.cpp
+++ Kernel/Syscalls/profiling.cpp
@@ -60,10 +60,14 @@ int Process::sys$profiling_disable(pid_t pid)
     // We explicitly unlock here because we can't hold the lock when writing the coredump VFS
     lock.unlock();
 
-    if (auto coredump = CoreDump::create(*process, String::formatted("/tmp/profiler_coredumps/{}", pid)))
-        coredump->write();
-    else
+    if (auto coredump = CoreDump::create(*process, String::formatted("/tmp/profiler_coredumps/{}", pid))) {
+        auto result = coredump->write();
+        if (result.is_error())
+            return result.error();
+    } else {
+        // FIXME: Return an error maybe?
         dbgln("Unable to create profiler coredump for PID {}", pid);
+    }
     return 0;
 }
 
