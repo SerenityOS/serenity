f1b1558c7b052d754a981840ab2f3dbcfc3cc1e6
Andreas Kling <kling@serenityos.org>
2020-12-25 12:15:16 +0100

    Everywhere: Tweak "2020-2020" => "2020" in copyright headers

--- Libraries/LibAudio/WavWriter.h
+++ Libraries/LibAudio/WavWriter.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2020, William McPherson <willmcpherson2@gmail.com>
+ * Copyright (c) 2020, William McPherson <willmcpherson2@gmail.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
