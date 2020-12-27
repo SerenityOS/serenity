defccf46f7c5269388e9068d2c2bc41b8d7c34cb
Rok Povsic <rok.povsic@gmail.com>
2020-12-27 09:48:26 +0100

    HexEditor: Select first character by default
    The first character was already selected (with the selection greyed-out),
    and a selection span of 1 was shown, so it's natural than the selection
    from/to numbers should be 0/0, not -1/-1.

--- Applications/HexEditor/HexEditor.h
+++ Applications/HexEditor/HexEditor.h
@@ -82,8 +82,8 @@ private:
     int m_bytes_per_row { 16 };
     ByteBuffer m_buffer;
     bool m_in_drag_select { false };
-    int m_selection_start { -1 };
-    int m_selection_end { -1 };
+    int m_selection_start { 0 };
+    int m_selection_end { 0 };
     HashMap<int, u8> m_tracked_changes;
     int m_position { 0 };
     int m_byte_position { 0 }; // 0 or 1
