740f6861cf9ef1bd73604c98a987fa1bed2cc8e8
Egor Ananyin <ananinegor@gmail.com>
2020-12-29 13:44:52 +0300

    LibJS: Uncomment the tests that pass now

--- Libraries/LibJS/Tests/builtins/Math/Math.cbrt.js
+++ Libraries/LibJS/Tests/builtins/Math/Math.cbrt.js
@@ -2,11 +2,11 @@ test("basic functionality", () => {
     expect(Math.cbrt).toHaveLength(1);
 
     expect(Math.cbrt(NaN)).toBeNaN();
-    // FIXME: expect(Math.cbrt(-1)).toBe(-1);
+    expect(Math.cbrt(-1)).toBe(-1);
     expect(Math.cbrt(-0)).toBe(-0);
-    // FIXME: expect(Math.cbrt(-Infinity)).toBe(-Infinity);
-    // FIXME: expect(Math.cbrt(1)).toBe(1);
-    // FIXME: expect(Math.cbrt(Infinity)).toBe(Infinity);
-    // FIXME: expect(Math.cbrt(null)).toBe(0);
-    // FIXME: expect(Math.cbrt(2)).toBeCloseTo(1.259921));
+    expect(Math.cbrt(-Infinity)).toBe(-Infinity);
+    expect(Math.cbrt(1)).toBe(1);
+    expect(Math.cbrt(Infinity)).toBe(Infinity);
+    expect(Math.cbrt(null)).toBe(0);
+    expect(Math.cbrt(2)).toBeCloseTo(1.259921);
 });
