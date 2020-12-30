016a6da385f5bfe8d9655766a89d11d60f93faf2
Andreas Kling <kling@serenityos.org>
2020-12-30 03:44:38 +0100

    TextEditor: Show "Untitled" when there's no current document path

--- Applications/TextEditor/TextEditorWidget.cpp
+++ Applications/TextEditor/TextEditorWidget.cpp
@@ -540,7 +540,10 @@ void TextEditorWidget::set_path(const LexicalPath& lexical_path)
 void TextEditorWidget::update_title()
 {
     StringBuilder builder;
-    builder.append(m_path);
+    if (m_path.is_empty())
+        builder.append("Untitled");
+    else
+        builder.append(m_path);
     if (m_document_dirty)
         builder.append(" (*)");
     builder.append(" - Text Editor");
