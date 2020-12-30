9f5eb4940736586b47fba24a7471cdb4fdb7c864
Andreas Kling <kling@serenityos.org>
2020-12-30 15:21:29 +0100

    WebContent: Pledge "recvfd" :^)
    This is needed so we can receive file descriptors from ProtocolServer.
    
    Fixes #4660.

--- Services/WebContent/main.cpp
+++ Services/WebContent/main.cpp
@@ -32,7 +32,7 @@
 int main(int, char**)
 {
     Core::EventLoop event_loop;
-    if (pledge("stdio shared_buffer accept unix rpath", nullptr) < 0) {
+    if (pledge("stdio shared_buffer accept unix rpath recvfd", nullptr) < 0) {
         perror("pledge");
         return 1;
     }
