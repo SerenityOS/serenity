32a2b967703c462e2ae9d5b18eba3f899f743afa
Andreas Kling <kling@serenityos.org>
2020-12-31 23:31:22 +0100

    Meta: Use a raw string delimiter in text-to-cpp-string.sh
    Otherwise we can't have the sequence ')"' in GML :^)

--- Meta/text-to-cpp-string.sh
+++ Meta/text-to-cpp-string.sh
@@ -3,9 +3,9 @@
 # $2 input path
 
 echo "extern const char $1[];"
-printf "const char %s[] = R\"(" "$1"
+printf "const char %s[] = R\"~~~(" "$1"
 grep -v '^ *#' < "$2" | while IFS= read -r line; do
   echo "$line"
 done
-echo ")\";"
+echo ")~~~\";"
 
