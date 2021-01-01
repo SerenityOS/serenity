6afc62f80cf77bc927d97805222159c0a2a1165d
Linus Groh <mail@linusgroh.de>
2021-01-01 14:57:50 +0100

    LibJS: Fix email in TypedArray{Constructor,Prototype} copyright headers

--- Libraries/LibJS/Runtime/TypedArrayConstructor.cpp
+++ Libraries/LibJS/Runtime/TypedArrayConstructor.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Linus Groh <mail@linusgroh.de
+ * Copyright (c) 2020, Linus Groh <mail@linusgroh.de>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
