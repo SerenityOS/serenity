b60881b6e54c4c6e0efa31dd64819fc532655230
Brian Gianforcaro <b.gianfo@gmail.com>
2021-01-02 04:27:35 -0800

    LibC: Randomize the stack check cookie value on initialization
    Previously we had a static stack check cookie value for LibC.
    Now we randomize the cookie value on LibC initialization, this should
    help make the stack check more difficult to attack (still possible just
    a bigger pain). This should also help to catch more bugs.

--- Libraries/LibC/crt0.cpp
+++ Libraries/LibC/crt0.cpp
@@ -33,6 +33,8 @@
 
 extern "C" {
 
+extern u32 __stack_chk_guard;
+
 int main(int, char**, char**);
 
 // Tell the compiler that this may be called from somewhere else.
@@ -40,6 +42,12 @@ int _start(int argc, char** argv, char** env);
 
 int _start(int argc, char** argv, char** env)
 {
+    u32 original_stack_chk = __stack_chk_guard;
+    arc4random_buf(&__stack_chk_guard, sizeof(__stack_chk_guard));
+
+    if (__stack_chk_guard == 0)
+        __stack_chk_guard = original_stack_chk;
+
     environ = env;
     __environ_is_malloced = false;
 
@@ -57,6 +65,11 @@ int _start(int argc, char** argv, char** env)
     int status = main(argc, argv, environ);
 
     exit(status);
+
+    // We should never get here, but if we ever do, make sure to
+    // restore the stack guard to the value we entered _start with.
+    // Then we won't trigger the stack canary check on the way out.
+    __stack_chk_guard = original_stack_chk;
 
     return 20150614;
 }
