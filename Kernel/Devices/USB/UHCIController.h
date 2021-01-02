5a3acc87ac96e288247e1df0e7d5380c1f136e72
Jesse Buhagiar <jooster669@gmail.com>
2021-01-03 00:12:54 +1100

    Kernel/USB: Add basic root port detection/management
    We can now read/write to the two root ports exposed to the
    UHCI controller, and detect when a device is plugged in or
    out via a kernel process that constantly scans the port
    for any changes. This is very basic, but is a bit of fun to see
    the kernel detecting hardware on the fly :^)

--- Kernel/Devices/USB/UHCIController.h
+++ Kernel/Devices/USB/UHCIController.h
@@ -31,6 +31,8 @@
 #include <Kernel/Devices/USB/UHCIDescriptorTypes.h>
 #include <Kernel/IO.h>
 #include <Kernel/PCI/Device.h>
+#include <Kernel/Process.h>
+#include <Kernel/Time/TimeManagement.h>
 #include <Kernel/VM/ContiguousVMObject.h>
 
 namespace Kernel::USB {
@@ -45,6 +47,7 @@ public:
     void reset();
     void stop();
     void start();
+    void spawn_port_proc();
 
 private:
     UHCIController(PCI::Address, PCI::ID);
@@ -55,6 +58,8 @@ private:
     u16 read_frnum() { return m_io_base.offset(0x6).in<u16>(); }
     u32 read_flbaseadd() { return m_io_base.offset(0x8).in<u32>(); }
     u8 read_sofmod() { return m_io_base.offset(0xc).in<u8>(); }
+    u16 read_portsc1() { return m_io_base.offset(0x10).in<u16>(); }
+    u16 read_portsc2() { return m_io_base.offset(0x12).in<u16>(); }
 
     void write_usbcmd(u16 value) { m_io_base.offset(0).out(value); }
     void write_usbsts(u16 value) { m_io_base.offset(0x2).out(value); }
@@ -62,6 +67,8 @@ private:
     void write_frnum(u16 value) { m_io_base.offset(0x6).out(value); }
     void write_flbaseadd(u32 value) { m_io_base.offset(0x8).out(value); }
     void write_sofmod(u8 value) { m_io_base.offset(0xc).out(value); }
+    void write_portsc1(u16 value) { m_io_base.offset(0x10).out(value); }
+    void write_portsc2(u16 value) { m_io_base.offset(0x12).out(value); }
 
     virtual void handle_irq(const RegisterState&) override;
 
