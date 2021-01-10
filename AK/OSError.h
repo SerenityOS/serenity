7eda856f70f9d9e36677278cf80ce395a8e12782
Andreas Kling <kling@serenityos.org>
2021-01-10 13:46:17 +0100

    AK: Add AK::OSError, a wrapper for errno codes
    This is a strongly typed carrier for "errno" error codes, intended for
    use in return types, e.g Result<String, OSError>.

---
+++ AK/OSError.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2021, Andreas Kling <kling@serenityos.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include <AK/Format.h>
+#include <string.h>
+
+namespace AK {
+
+class OSError {
+public:
+    explicit OSError(int error)
+        : m_error(error)
+    {
+    }
+
+    int error() const { return m_error; }
+    const char* string() const { return strerror(m_error); }
+
+private:
+    int m_error { 0 };
+};
+
+template<>
+struct Formatter<OSError> : Formatter<StringView> {
+    void format(FormatBuilder& builder, const OSError& value)
+    {
+        Formatter<StringView>::format(builder, value.string());
+    }
+};
+
+}
+
+using AK::OSError;
