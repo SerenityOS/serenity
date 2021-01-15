9229ba0fe9b04a7eab116925ecdee4797815281d
asynts <asynts@gmail.com>
2021-01-15 21:46:23 +0100

    Everywhere: Replace a bundle of dbg with dbgln.
    These changes are arbitrarily divided into multiple commits to make it
    easier to find potentially introduced bugs with git bisect.

--- Userland/Libraries/LibGemini/Job.cpp
+++ Userland/Libraries/LibGemini/Job.cpp
@@ -24,12 +24,11 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <AK/Debug.h>
 #include <LibGemini/GeminiResponse.h>
 #include <LibGemini/Job.h>
 #include <stdio.h>
 #include <unistd.h>
-
-//#define JOB_DEBUG
 
 namespace Gemini {
 
@@ -67,10 +66,11 @@ void Job::on_socket_connected()
             return;
         m_sent_data = true;
         auto raw_request = m_request.to_raw_request();
-#ifdef JOB_DEBUG
-        dbgln("Job: raw_request:");
-        dbg() << String::copy(raw_request).characters();
-#endif
+
+        if constexpr (debug_job) {
+            dbgln("Job: raw_request:");
+            dbgln("{}", String::copy(raw_request));
+        }
         bool success = write(raw_request);
         if (!success)
             deferred_invoke([this](auto&) { did_fail(Core::NetworkJob::Error::TransmissionFailed); });
