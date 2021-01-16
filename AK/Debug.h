7ecdec858b58ed7dc458fe7e9c88bc786d21018e
asynts <asynts@gmail.com>
2021-01-16 14:07:20 +0100

    Everywhere: Replace a bundle of dbg with dbgln.
    These changes are arbitrarily divided into multiple commits to make it
    easier to find potentially introduced bugs with git bisect.

--- AK/Debug.h
+++ AK/Debug.h
@@ -357,3 +357,9 @@ constexpr bool debug_gif = true;
 #else
 constexpr bool debug_gif = false;
 #endif
+
+#ifdef JPG_DEBUG
+constexpr bool debug_jpg = true;
+#else
+constexpr bool debug_jpg = false;
+#endif
