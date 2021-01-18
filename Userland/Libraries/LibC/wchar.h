ec42f864d4a420b66c6a02ccc43846d05e50553e
Linus Groh <mail@linusgroh.de>
2021-01-18 09:57:12 +0100

    LibC: Add wcsncmp()
    Taken from strncmp(), like wcscmp() in ef40ebb. :^)

--- Userland/Libraries/LibC/wchar.h
+++ Userland/Libraries/LibC/wchar.h
@@ -39,6 +39,7 @@ size_t wcslen(const wchar_t*);
 wchar_t* wcscpy(wchar_t*, const wchar_t*);
 wchar_t* wcsncpy(wchar_t*, const wchar_t*, size_t);
 int wcscmp(const wchar_t*, const wchar_t*);
+int wcsncmp(const wchar_t*, const wchar_t*, size_t);
 wchar_t* wcschr(const wchar_t*, int);
 const wchar_t* wcsrchr(const wchar_t*, wchar_t);
 wchar_t* wcscat(wchar_t*, const wchar_t*);
