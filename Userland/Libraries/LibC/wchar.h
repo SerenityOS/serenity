f9de40e21833edc9b17d2da49e37d740b3fcf138
Linus Groh <mail@linusgroh.de>
2021-01-18 09:59:44 +0100

    LibC: Add wcstol() and wcstoll() stubs

--- Userland/Libraries/LibC/wchar.h
+++ Userland/Libraries/LibC/wchar.h
@@ -43,7 +43,9 @@ int wcsncmp(const wchar_t*, const wchar_t*, size_t);
 wchar_t* wcschr(const wchar_t*, int);
 const wchar_t* wcsrchr(const wchar_t*, wchar_t);
 wchar_t* wcscat(wchar_t*, const wchar_t*);
-wchar_t* wcstok(wchar_t*, const wchar_t*, wchar_t**);
 wchar_t* wcsncat(wchar_t*, const wchar_t*, size_t);
+wchar_t* wcstok(wchar_t*, const wchar_t*, wchar_t**);
+long wcstol(const wchar_t*, wchar_t**, int);
+long long wcstoll(const wchar_t*, wchar_t**, int);
 
 __END_DECLS
