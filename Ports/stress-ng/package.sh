7cc7f7ac54a76e84a5f7ada2a8e23473ca0c82ea
Linus Groh <mail@linusgroh.de>
2021-01-21 19:12:25 +0100

    Ports: Clean up some package.sh scripts
    - Remove superfluous function overrides and use makeopts instead
    - Remove superfluous installopts
    - Use run rather than cd'ing manually
    - Ensure empty line between functions

--- Ports/stress-ng/package.sh
+++ Ports/stress-ng/package.sh
@@ -3,8 +3,4 @@ port=stress-ng
 version=0.11.23
 workdir=stress-ng-${version}
 files="https://github.com/ColinIanKing/stress-ng/archive/V${version}.tar.gz stress-ng-${version}.tar.gz"
-installopts="DESTDIR=$SERENITY_ROOT/Build/Root"
-
-build() {
-    run make STATIC=1
-}
+makeopts="STATIC=1"
