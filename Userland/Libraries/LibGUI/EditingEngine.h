22d11e0d0ad7253900ab3eae23229191421de223
Zac <zacary.gillerat@connect.qut.edu.au>
2021-01-24 22:51:54 +1000

    Vim: More Correct Word Jumping
    Implemented move_to_beginning_of_next(), move_to_end_of_next(),
    move_to_beginning_of_previous() and move_to_end_of_previous() functions
    for more correct word jumping than the move_to_xxx_span() methods that
    were previously used.

--- Userland/Libraries/LibGUI/EditingEngine.h
+++ Userland/Libraries/LibGUI/EditingEngine.h
@@ -68,6 +68,10 @@ protected:
     void move_page_down(const KeyEvent& event);
     void move_to_first_line();
     void move_to_last_line();
+    void move_to_beginning_of_next_word();
+    void move_to_end_of_next_word();
+    void move_to_end_of_previous_word();
+    void move_to_beginning_of_previous_word();
 
     void move_up(const KeyEvent& event, double page_height_factor);
     void move_down(const KeyEvent& event, double page_height_factor);
