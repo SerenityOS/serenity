40d5d6217304882136d43e4c5cecad3c013b9b80
Linus Groh <mail@linusgroh.de>
2021-01-29 22:27:31 +0100

    Ports: Update neofetch to 7.1.0

--- Ports/neofetch/package.sh
+++ Ports/neofetch/package.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
+
 port=neofetch
-version=7.0.0
+version=7.1.0
 useconfigure=false
 depends="bash jq"
 files="https://github.com/dylanaraps/neofetch/archive/${version}.tar.gz neofetch-${version}.tar.gz"
