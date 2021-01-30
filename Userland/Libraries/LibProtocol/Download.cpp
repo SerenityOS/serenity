01cafbddd1c798a0a4ee3d3187ddac2ae8aa0c7b
AnotherTest <ali.mpfard@gmail.com>
2021-01-30 14:23:34 +0330

    LibProtocol: Bump download stream buffer to PAGE_SIZE
    I think this should scale based on the network speed (or download
    rate?), but for the time being, 4K seems to be good-enough.

--- Userland/Libraries/LibProtocol/Download.cpp
+++ Userland/Libraries/LibProtocol/Download.cpp
@@ -57,7 +57,7 @@ void Download::stream_into(OutputStream& stream)
     };
 
     notifier->on_ready_to_read = [this, &stream, user_on_finish = move(user_on_finish)] {
-        constexpr size_t buffer_size = 1 * KiB;
+        constexpr size_t buffer_size = PAGE_SIZE;
         static char buf[buffer_size];
         auto nread = m_internal_stream_data->read_stream.read({ buf, buffer_size });
         if (!stream.write_or_error({ buf, nread })) {
