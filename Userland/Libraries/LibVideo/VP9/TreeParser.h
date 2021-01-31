4a3c4d71ca937555e36948a21c3df080f80347af
FalseHonesty <thefalsehonesty@gmail.com>
2021-01-31 00:42:36 -0500

    LibVideo/VP9: Refactor how TreeParser accesses decoder data
    The TreeParser requires information about a lot of the decoder's
    current state in order to parse syntax tree elements correctly, so
    there has to be some communication between the Decoder and the
    TreeParser. Previously, the Decoder would copy its state to the
    TreeParser when it changed, however, this was a poor choice. Now,
    the TreeParser simply has a reference to its owning Decoder, and
    accesses its state directly.

--- Userland/Libraries/LibVideo/VP9/TreeParser.h
+++ Userland/Libraries/LibVideo/VP9/TreeParser.h
@@ -13,10 +13,12 @@
 
 namespace Video::VP9 {
 
+class Decoder;
+
 class TreeParser {
 public:
-    explicit TreeParser(ProbabilityTables& probability_tables)
-        : m_probability_tables(probability_tables)
+    explicit TreeParser(Decoder& decoder)
+        : m_decoder(decoder)
     {
     }
 
@@ -44,52 +46,13 @@ public:
     u8 select_tree_probability(SyntaxElementType type, u8 node);
     void count_syntax_element(SyntaxElementType type, int value);
 
-    void set_bit_stream(BitStream* bit_stream) { m_bit_stream = bit_stream; }
-    void set_has_rows(bool has_rows) { m_has_rows = has_rows; }
-    void set_has_cols(bool has_cols) { m_has_cols = has_cols; }
-    void set_max_tx_size(TXSize max_tx_size) { m_max_tx_size = max_tx_size; }
-    void set_use_hp(bool use_hp) { m_use_hp = use_hp; }
-    void set_block_subsize(u8 block_subsize) { m_block_subsize = block_subsize; }
-    void set_num_8x8(u8 num_8x8) { m_num_8x8 = num_8x8; }
-    void set_above_partition_context(u8* above_partition_context) { m_above_partition_context = above_partition_context; }
-    void set_left_partition_context(u8* left_partition_context) { m_left_partition_context = left_partition_context; }
-    void set_col(u32 col) { m_col = col; }
-    void set_row(u32 row) { m_row = row; }
-    void set_frame_is_intra(bool frame_is_intra) { m_frame_is_intra = frame_is_intra; }
-    void set_syntax_element_counter(SyntaxElementCounter* syntax_element_counter) { m_syntax_element_counter = syntax_element_counter; }
-    void set_segmentation_tree_probs(u8* segmentation_tree_probs) { m_segmentation_tree_probs = segmentation_tree_probs; }
-    void set_available_u(bool available_u) { m_available_u = available_u; }
-    void set_available_l(bool available_l) { m_available_l = available_l; }
-    void set_mi_row(u32 mi_row) { m_mi_row = mi_row; }
-    void set_mi_col(u32 mi_col) { m_mi_col = mi_col; }
-
 private:
     u8 calculate_partition_probability(u8 node);
     u8 calculate_skip_probability();
 
-    ProbabilityTables& m_probability_tables;
-    BitStream* m_bit_stream { nullptr };
-    SyntaxElementCounter* m_syntax_element_counter { nullptr };
-
+    Decoder& m_decoder;
     // m_ctx is a member variable because it is required for syntax element counting (section 9.3.4)
     u8 m_ctx { 0 };
-
-    bool m_has_rows { false };
-    bool m_has_cols { false };
-    TXSize m_max_tx_size { TX_4x4 };
-    bool m_use_hp { false };
-    u8 m_block_subsize { 0 };
-    u8 m_num_8x8 { 0 };
-    u8* m_above_partition_context { nullptr };
-    u8* m_left_partition_context { nullptr };
-    u32 m_col { 0 };
-    u32 m_row { 0 };
-    bool m_frame_is_intra { false };
-    u8* m_segmentation_tree_probs { nullptr };
-    bool m_available_u { false };
-    bool m_available_l { false };
-    u32 m_mi_col { 0 };
-    u32 m_mi_row { 0 };
 };
 
 }
