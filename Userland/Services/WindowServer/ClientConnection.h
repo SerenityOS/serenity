1b35ee9a7a3a74e3f1793adf09da49dd394b4322
Andreas Kling <kling@serenityos.org>
2021-02-01 11:23:54 +0100

    LibGUI+WindowServer: Don't use a WM IPC to initiate own window resize
    The WM_* IPC messages are intended for "outsider" window management,
    not for a client's own windows. Make a separate StartWindowResize
    message for this.
    
    This was the only reason that every IPC client had to know its server
    side client ID.

--- Userland/Services/WindowServer/ClientConnection.h
+++ Userland/Services/WindowServer/ClientConnection.h
@@ -105,6 +105,7 @@ private:
     virtual OwnPtr<Messages::WindowServer::SetWindowTitleResponse> handle(const Messages::WindowServer::SetWindowTitle&) override;
     virtual OwnPtr<Messages::WindowServer::GetWindowTitleResponse> handle(const Messages::WindowServer::GetWindowTitle&) override;
     virtual OwnPtr<Messages::WindowServer::IsMaximizedResponse> handle(const Messages::WindowServer::IsMaximized&) override;
+    virtual void handle(const Messages::WindowServer::StartWindowResize&) override;
     virtual OwnPtr<Messages::WindowServer::SetWindowRectResponse> handle(const Messages::WindowServer::SetWindowRect&) override;
     virtual OwnPtr<Messages::WindowServer::GetWindowRectResponse> handle(const Messages::WindowServer::GetWindowRect&) override;
     virtual OwnPtr<Messages::WindowServer::GetWindowRectInMenubarResponse> handle(const Messages::WindowServer::GetWindowRectInMenubar&) override;
