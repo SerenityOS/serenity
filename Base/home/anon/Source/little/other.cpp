33a6f279520772299700d683e6fb2755c8a5e38d
Itamar <itamar8910@gmail.com>
2021-02-06 16:27:39 +0200

    LanguageServers/Cpp: ParserAutoComplete engine inspects header files
    ... and performs preprocessing on the source code before parsing.
    
    To support this, we are now able to keep track of multiple
    files in the autocomplete engine. We re-parse a file whenever it is
    edited.

--- Base/home/anon/Source/little/other.cpp
+++ Base/home/anon/Source/little/other.cpp
@@ -1,12 +1,13 @@
-#include <stdio.h>
 #include "other.h"
+#include <stdio.h>
 
 int func()
 {
     int x = 1;
     int y = 2;
+    StructInHeader mystruct;
     printf("x: %d\n", x);
     printf("y: %d\n", y);
-    printf("x+y: %d\n", x+y);
+    printf("x+y: %d\n", x + y);
     return x + y;
 }
