02038a0edee0f01f76e90c8b4fbdf208592ea262
Itamar <itamar8910@gmail.com>
2021-02-06 16:25:15 +0200

    LanguageServers/Cpp: Use FileDB and AutoCompleteEngine base class

--- Userland/DevTools/HackStudio/LanguageServers/Cpp/AutoCompleteEngine.h
+++ Userland/DevTools/HackStudio/LanguageServers/Cpp/AutoCompleteEngine.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Itamar S. <itamar8910@gmail.com>
+ * Copyright (c) 2021, Itamar S. <itamar8910@gmail.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,27 +26,24 @@
 
 #pragma once
 
-#include "AutoCompleteEngine.h"
-#include <AK/String.h>
-#include <AK/Vector.h>
+#include "FileDB.h"
 #include <DevTools/HackStudio/AutoCompleteResponse.h>
-#include <LibCpp/Lexer.h>
 #include <LibGUI/TextPosition.h>
 
-namespace LanguageServers::Cpp {
-
-using namespace ::Cpp;
-
-class LexerAutoComplete : public AutoCompleteEngine {
+class AutoCompleteEngine {
 public:
-    LexerAutoComplete(const FileDB& filedb);
+    AutoCompleteEngine(const FileDB& filedb);
+    virtual ~AutoCompleteEngine();
 
-    virtual Vector<GUI::AutocompleteProvider::Entry> get_suggestions(const String& file, const GUI::TextPosition& autocomplete_position) override;
+    virtual Vector<GUI::AutocompleteProvider::Entry> get_suggestions(const String& file, const GUI::TextPosition& autocomplete_position) = 0;
+
+    // TODO: In the future we can pass the range that was edited and only re-parse what we have to.
+    virtual void on_edit([[maybe_unused]] const String& file) {};
+    virtual void file_opened([[maybe_unused]] const String& file) {};
+
+protected:
+    const FileDB& filedb() const { return m_filedb; }
 
 private:
-    Optional<size_t> token_in_position(const Vector<Cpp::Token>&, const GUI::TextPosition&);
-    StringView text_of_token(const Vector<String>& lines, const Cpp::Token&);
-    Vector<GUI::AutocompleteProvider::Entry> identifier_prefixes(const Vector<String>& lines, const Vector<Cpp::Token>&, size_t target_token_index);
+    const FileDB& m_filedb;
 };
-
-}
