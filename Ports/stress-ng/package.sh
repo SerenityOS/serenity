d107cabf5d1321621d69aa1e3f858a9fe3684fe7
Brian Gianforcaro <b.gianfo@gmail.com>
2021-02-06 23:21:40 -0800

    Ports: Fix linking of the stress-ng port

--- Ports/stress-ng/package.sh
+++ Ports/stress-ng/package.sh
@@ -3,4 +3,4 @@ port=stress-ng
 version=0.11.23
 workdir=stress-ng-${version}
 files="https://github.com/ColinIanKing/stress-ng/archive/V${version}.tar.gz stress-ng-${version}.tar.gz"
-makeopts="STATIC=1"
+makeopts="STATIC=1 LDFLAGS=-L${SERENITY_ROOT}/Build/Root/usr/local/lib"
