115da366f86821c516b6dba3ac4df9ba027e211f
Brian Gianforcaro <b.gianfo@gmail.com>
2021-02-06 23:14:19 -0800

    Meta: Fix path to kernel binary in debug-kernel.sh
    This appears to have been broken since the migration to cmake.

--- Meta/debug-kernel.sh
+++ Meta/debug-kernel.sh
@@ -9,6 +9,6 @@
 # the debugger which binary to load symbols, etc from.
 #
 $SERENITY_KERNEL_DEBUGGER \
-    -ex "file $(pwd)/kernel" \
+    -ex "file $(dirname "$0")/../Build/Kernel/Kernel" \
     -ex 'set arch i386:intel' \
     -ex 'target remote localhost:1234'
