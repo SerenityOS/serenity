81103f149b613daa670280a14fd35aff6e909c5c
Linus Groh <mail@linusgroh.de>
2021-02-08 19:24:03 +0100

    LibC: Define ICMP header type constants in netinet/ip_icmp.h
    Let's get rid of some magic numbers soon. :^)

--- Userland/Libraries/LibC/netinet/ip_icmp.h
+++ Userland/Libraries/LibC/netinet/ip_icmp.h
@@ -44,4 +44,21 @@ struct icmphdr {
     } un;
 };
 
+// clang-format off
+#define ICMP_ECHOREPLY 0       // Echo Reply
+#define ICMP_DEST_UNREACH 3    // Destination Unreachable
+#define ICMP_SOURCE_QUENCH 4   // Source Quench
+#define ICMP_REDIRECT 5        // Redirect
+#define ICMP_ECHO 8            // Echo Request
+#define ICMP_TIME_EXCEEDED 11  // Time Rxceeded
+#define ICMP_PARAMETERPROB 12  // Parameter Problem
+#define ICMP_TIMESTAMP 13      // Timestamp Request
+#define ICMP_TIMESTAMPREPLY 14 // Timestamp Reply
+#define ICMP_INFO_REQUEST 15   // Information Request
+#define ICMP_INFO_REPLY 16     // Information Reply
+#define ICMP_ADDRESS 17        // Address Mask Request
+#define ICMP_ADDRESSREPLY 18   // Address Mask Reply
+#define NR_ICMP_TYPES 18
+//clang-format on
+
 __END_DECLS
