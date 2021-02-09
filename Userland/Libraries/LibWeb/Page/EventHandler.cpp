2c4829cb147957635af493257089179f48e71537
Andreas Kling <kling@serenityos.org>
2021-02-09 20:49:01 +0100

    LibWeb: Remove duplicated locals in EventHandler::handle_mousedown()

--- Userland/Libraries/LibWeb/Page/EventHandler.cpp
+++ Userland/Libraries/LibWeb/Page/EventHandler.cpp
@@ -174,8 +174,6 @@ bool EventHandler::handle_mousedown(const Gfx::IntPoint& position, unsigned butt
         auto url = document->complete_url(href);
         dbgln("Web::EventHandler: Clicking on a link to {}", url);
         if (button == GUI::MouseButton::Left) {
-            auto href = link->href();
-            auto url = document->complete_url(href);
             if (href.starts_with("javascript:")) {
                 document->run_javascript(href.substring_view(11, href.length() - 11));
             } else if (href.starts_with('#')) {
