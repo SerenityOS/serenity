51016a5567195379ca3c4de3fab5f4b7820ed82a
Andreas Kling <kling@serenityos.org>
2021-02-10 08:48:28 +0100

    LibWeb+WebContent: Support window.confirm() in OOPWV

--- Userland/Libraries/LibWeb/WebContentClient.h
+++ Userland/Libraries/LibWeb/WebContentClient.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2020-2021, Andreas Kling <kling@serenityos.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -65,6 +65,7 @@ private:
     virtual void handle(const Messages::WebContentClient::DidRequestContextMenu&) override;
     virtual void handle(const Messages::WebContentClient::DidRequestLinkContextMenu&) override;
     virtual OwnPtr<Messages::WebContentClient::DidRequestAlertResponse> handle(const Messages::WebContentClient::DidRequestAlert&) override;
+    virtual OwnPtr<Messages::WebContentClient::DidRequestConfirmResponse> handle(const Messages::WebContentClient::DidRequestConfirm&) override;
 
     OutOfProcessWebView& m_view;
 };
