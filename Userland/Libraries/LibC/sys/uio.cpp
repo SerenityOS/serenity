675536311714b0c076966eca60b7e95264c0a6a1
AnotherTest <ali.mpfard@gmail.com>
2021-02-12 05:40:03 +0330

    Kernel+LibC: Implement readv
    We already had writev, so let's just add readv too.

--- Userland/Libraries/LibC/sys/uio.cpp
+++ Userland/Libraries/LibC/sys/uio.cpp
@@ -35,4 +35,10 @@ ssize_t writev(int fd, const struct iovec* iov, int iov_count)
     int rc = syscall(SC_writev, fd, iov, iov_count);
     __RETURN_WITH_ERRNO(rc, rc, -1);
 }
+
+ssize_t readv(int fd, const struct iovec* iov, int iov_count)
+{
+    int rc = syscall(SC_readv, fd, iov, iov_count);
+    __RETURN_WITH_ERRNO(rc, rc, -1);
+}
 }
