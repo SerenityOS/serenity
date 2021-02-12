cb42ba0ab1f8b557a3906f596615f2c7c875b787
thankyouverycool <n/a>
2021-02-12 17:37:07 -0500

    FileManager: Save 'Show dotfiles' setting in FileManager.ini

--- Userland/Applications/FileManager/main.cpp
+++ Userland/Applications/FileManager/main.cpp
@@ -623,7 +623,13 @@ int run_in_windowed_mode(RefPtr<Core::ConfigFile> config, String initial_locatio
     auto action_show_dotfiles = GUI::Action::create_checkable("Show dotfiles", { Mod_Ctrl, Key_H }, [&](auto& action) {
         directory_view.set_should_show_dotfiles(action.is_checked());
         refresh_tree_view();
+        config->write_bool_entry("DirectoryView", "ShowDotFiles", action.is_checked());
+        config->sync();
     });
+
+    auto show_dotfiles = config->read_bool_entry("DirectoryView", "ShowDotFiles", false);
+    directory_view.set_should_show_dotfiles(show_dotfiles);
+    action_show_dotfiles->set_checked(show_dotfiles);
 
     auto& view_menu = menubar->add_menu("View");
     view_menu.add_action(*view_as_icons_action);
