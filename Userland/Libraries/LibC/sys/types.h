1df614e7c681e8fb98711fd7dc9fa917b60884e3
AnotherTest <ali.mpfard@gmail.com>
2021-02-13 12:06:05 +0330

    LibC+LibPthread: Stub out pthread_rwlock_* functions

--- Userland/Libraries/LibC/sys/types.h
+++ Userland/Libraries/LibC/sys/types.h
@@ -99,7 +99,7 @@ typedef struct __pthread_cond_t {
 } pthread_cond_t;
 
 typedef void* pthread_rwlock_t;
-typedef void* pthread_rwlockatrr_t;
+typedef void* pthread_rwlockattr_t;
 typedef void* pthread_spinlock_t;
 typedef struct __pthread_condattr_t {
     int clockid; // clockid_t
