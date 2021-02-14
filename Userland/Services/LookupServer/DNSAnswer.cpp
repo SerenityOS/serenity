ae1e82fd2f307ed49586c1bfc35f9f2fb30c8942
Sergey Bugaev <bugaevc@serenityos.org>
2021-02-14 15:02:02 +0300

    LookupServer: Introduce DNSName
    This is a wrapper around a string representing a domain name (such as
    "example.com"). It never has a trailing dot.
    
    For now, this class doesn't do much except wrap the raw string. Subsequent
    commits will add or move more functionality to it.

--- Userland/Services/LookupServer/DNSAnswer.cpp
+++ Userland/Services/LookupServer/DNSAnswer.cpp
@@ -29,7 +29,7 @@
 
 namespace LookupServer {
 
-DNSAnswer::DNSAnswer(const String& name, u16 type, u16 class_code, u32 ttl, const String& record_data)
+DNSAnswer::DNSAnswer(const DNSName& name, u16 type, u16 class_code, u32 ttl, const String& record_data)
     : m_name(name)
     , m_type(type)
     , m_class_code(class_code)
