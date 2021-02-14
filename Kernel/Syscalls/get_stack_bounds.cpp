41883730206a10fa3043c87135418e829938ad65
Andreas Kling <kling@serenityos.org>
2021-02-14 11:44:21 +0100

    Kernel: Fix TOCTOU in syscall entry region validation
    We were doing stack and syscall-origin region validations before
    taking the big process lock. There was a window of time where those
    regions could then be unmapped/remapped by another thread before we
    proceed with our syscall.
    
    This patch closes that window, and makes sys$get_stack_bounds() rely
    on the fact that we now know the userspace stack pointer to be valid.
    
    Thanks to @BenWiederhake for spotting this! :^)

--- Kernel/Syscalls/get_stack_bounds.cpp
+++ Kernel/Syscalls/get_stack_bounds.cpp
@@ -34,9 +34,9 @@ int Process::sys$get_stack_bounds(FlatPtr* user_stack_base, size_t* user_stack_s
 {
     FlatPtr stack_pointer = Thread::current()->get_register_dump_from_stack().userspace_esp;
     auto* stack_region = space().find_region_containing(Range { VirtualAddress(stack_pointer), 1 });
-    if (!stack_region) {
-        PANIC("sys$get_stack_bounds: No stack region found for process");
-    }
+
+    // The syscall handler should have killed us if we had an invalid stack pointer.
+    ASSERT(stack_region);
 
     FlatPtr stack_base = stack_region->range().base().get();
     size_t stack_size = stack_region->size();
