83704997b08663758a5ecf6790b716cfeb8c0637
Sergey Bugaev <bugaevc@serenityos.org>
2021-02-14 15:02:02 +0300

    LookupServer: Introduce DNSName
    This is a wrapper around a string representing a domain name (such as
    "example.com"). It never has a trailing dot.
    
    For now, this class doesn't do much except wrap the raw string. Subsequent
    commits will add or move more functionality to it.

--- Userland/Services/LookupServer/DNSName.h
+++ Userland/Services/LookupServer/DNSName.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2021, Sergey Bugaev <bugaevc@serenityos.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,31 +26,17 @@
 
 #pragma once
 
-#include "DNSName.h"
 #include <AK/String.h>
-#include <AK/Types.h>
 
 namespace LookupServer {
 
-class DNSAnswer {
+class DNSName {
 public:
-    DNSAnswer(const DNSName& name, u16 type, u16 class_code, u32 ttl, const String& record_data);
-
-    const DNSName& name() const { return m_name; }
-    u16 type() const { return m_type; }
-    u16 class_code() const { return m_class_code; }
-    u32 ttl() const { return m_ttl; }
-    const String& record_data() const { return m_record_data; }
-
-    bool has_expired() const;
+    DNSName(const String&);
+    const String& as_string() const { return m_name; }
 
 private:
-    DNSName m_name;
-    u16 m_type { 0 };
-    u16 m_class_code { 0 };
-    u32 m_ttl { 0 };
-    time_t m_expiration_time { 0 };
-    String m_record_data;
+    String m_name;
 };
 
 }
