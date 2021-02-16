07b6e287e077b07c20a0082f93ae5162c975692d
Breno Silva <brenophp@gmail.com>
2021-02-16 17:32:51 -0300

    LibJS: Implement tests for Array.prototype.flat

---
+++ Userland/Libraries/LibJS/Tests/builtins/Array/Array.prototype.flat.js
@@ -0,0 +1,43 @@
+test("length is 0", () => {
+    expect(Array.prototype.flat).toHaveLength(0);
+});
+
+describe("normal behavior", () => {
+    test("basic functionality", () => {
+        var array1 = [1, 2, [3, 4]];
+        var array2 = [1, 2, [3, 4, [5, 6]]];
+        var array3 = [1, 2, [3, 4, [5, 6]]];
+        expect(array1.flat()).toEqual([1, 2, 3, 4]);
+        expect(array2.flat()).toEqual([1, 2, 3, 4, [5, 6]]);
+        expect(array3.flat(2)).toEqual([1, 2, 3, 4, 5, 6]);
+    });
+
+    test("calls depth as infinity", () => {
+        var array1 = [1, 2, [3, 4, [5, 6, [7, 8]]]];
+        expect(array1.flat(Infinity)).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+        expect(array1.flat(-Infinity)).toEqual([1, 2, [3, 4, [5, 6, [7, 8]]]]);
+    });
+
+    test("calls depth as undefined", () => {
+        var array1 = [1, 2, [3, 4, [5, 6, [7, 8]]]];
+        expect(array1.flat(undefined)).toEqual([1, 2, 3, 4, [5, 6, [7, 8]]]);
+    });
+
+    test("calls depth as null", () => {
+        var array1 = [1, 2, [3, 4, [5, 6, [7, 8]]]];
+        expect(array1.flat(null)).toEqual([1, 2, [3, 4, [5, 6, [7, 8]]]]);
+        expect(array1.flat(NaN)).toEqual([1, 2, [3, 4, [5, 6, [7, 8]]]]);
+    });
+
+    test("calls depth as non integer", () => {
+        var array1 = [1, 2, [3, 4, [5, 6, [7, 8]]]];
+        expect(array1.flat("depth")).toEqual([1, 2, [3, 4, [5, 6, [7, 8]]]]);
+        expect(array1.flat("2")).toEqual([1, 2, 3, 4, 5, 6, [7, 8]]);
+        expect(array1.flat(2.1)).toEqual([1, 2, 3, 4, 5, 6, [7, 8]]);
+        expect(array1.flat(0.7)).toEqual([1, 2, [3, 4, [5, 6, [7, 8]]]]);
+        expect(array1.flat([2])).toEqual([1, 2, 3, 4, 5, 6, [7, 8]]);
+        expect(array1.flat([2, 1])).toEqual([1, 2, [3, 4, [5, 6, [7, 8]]]]);
+        expect(array1.flat({})).toEqual([1, 2, [3, 4, [5, 6, [7, 8]]]]);
+        expect(array1.flat({ depth: 2 })).toEqual([1, 2, [3, 4, [5, 6, [7, 8]]]]);
+    });
+});
