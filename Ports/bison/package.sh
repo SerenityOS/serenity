5733c9155eea3e61f33613c0374c7b5208a88bdc
jonno85uk <jonnobleuk@gmail.com>
2021-02-17 21:54:37 +0000

    Ports: Add bison 1.25 (#5382)
    bison -h works. Not tried with any grammar files

---
+++ Ports/bison/package.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port=bison
+version=1.25
+useconfigure=true
+configopts="--prefix=$SERENITY_ROOT/Build/Root/usr/local"
+files="https://ftp.gnu.org/gnu/bison/bison-${version}.tar.gz bison-${version}.tar.gz 65f577d0f8ffaf61ae21c23c0918d225"
+auth_type="md5"
