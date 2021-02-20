6119bee1d3b69d19a42962a835131fbe98e479e1
Andreas Kling <kling@serenityos.org>
2021-02-20 16:34:53 +0100

    Ports: Update vttest to latest version

--- Ports/vttest/package.sh
+++ Ports/vttest/package.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=vttest
-version=20200610
+version=20210210
 useconfigure=true
 files="https://invisible-island.net/datafiles/release/vttest.tar.gz vttest.tar.gz"
