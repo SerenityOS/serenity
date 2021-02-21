1ff85eb468dfb112a588a0030e15db4d548d6c54
Mițca Dumitru <dumitru0mitca@gmail.com>
2021-02-21 02:56:28 +0200

    FileManager: Use newly introduced LibCore file management helpers
    Most of the functions under FileUtils were removed, except those which
    dealt with file deletion, as they spawned MessageBoxes for errors, as
    such, those functions were written in terms of Core::File::remove.

--- Userland/Applications/FileManager/FileUtils.h
+++ Userland/Applications/FileManager/FileUtils.h
@@ -40,11 +40,4 @@ enum class FileOperation {
 
 void delete_path(const String&, GUI::Window*);
 void delete_paths(const Vector<String>&, bool should_confirm, GUI::Window*);
-int delete_directory(String directory, String& file_that_caused_error);
-bool copy_file_or_directory(const String& src_path, const String& dst_path);
-String get_duplicate_name(const String& path, int duplicate_count);
-bool copy_file(const String& dst_path, const struct stat& src_stat, Core::File&);
-bool copy_directory(const String& src_path, const String& dst_path, const struct stat& src_stat);
-bool link_file(const String& src_path, const String& dst_path);
-
 }
