7fd2c6c3f572075e7e863df26b1ead686dc678ff
AnotherTest <ali.mpfard@gmail.com>
2021-02-21 20:17:51 +0330

    LibC: Don't #define away __{BEGIN,END}_DECLS in stdarg.h
    That would force anything that includes this to have language-specific
    linkage, and absolutely break `sys/cdefs.h`.
    
    Fixes #5452.

--- Userland/Libraries/LibC/stdarg.h
+++ Userland/Libraries/LibC/stdarg.h
@@ -26,19 +26,8 @@
 
 #pragma once
 
-#if defined(KERNEL)
-#    define __BEGIN_DECLS
-#    define __END_DECLS
-#else
-#    include <sys/cdefs.h>
-#endif
-
-__BEGIN_DECLS
-
 typedef __builtin_va_list va_list;
 
 #define va_start(v, l) __builtin_va_start(v, l)
 #define va_end(v) __builtin_va_end(v)
 #define va_arg(v, l) __builtin_va_arg(v, l)
-
-__END_DECLS
