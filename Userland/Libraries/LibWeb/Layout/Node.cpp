e4735ce6e80ad6b4ad0980b31e50eaab561bcb12
Andreas Kling <kling@serenityos.org>
2021-02-22 19:48:24 +0100

    LibWeb: Allow scrolling overflowed content with the mouse wheel :^)
    This is rather crude, but you can now use the mouse wheel to scroll up
    and down in block-level boxes with clipped overflowing content.
    There's no limit to how far you can scroll in either direction, since
    we don't yet track how much overflow there is. But it's a start. :^)

--- Userland/Libraries/LibWeb/Layout/Node.cpp
+++ Userland/Libraries/LibWeb/Layout/Node.cpp
@@ -318,6 +318,15 @@ void Node::handle_mousemove(Badge<EventHandler>, const Gfx::IntPoint&, unsigned,
 {
 }
 
+void Node::handle_mousewheel(Badge<EventHandler>, const Gfx::IntPoint&, unsigned, unsigned, int wheel_delta)
+{
+    if (auto* containing_block = this->containing_block()) {
+        auto new_offset = containing_block->scroll_offset();
+        new_offset.move_by(0, wheel_delta);
+        containing_block->set_scroll_offset(new_offset);
+    }
+}
+
 bool Node::is_root_element() const
 {
     if (is_anonymous())
