cfcabc41035510806580350c5e3496fd0e4ee6b2
Andreas Kling <kling@serenityos.org>
2021-02-22 19:01:45 +0100

    LibGfx: Make Painter::draw_physical_pixel() clip with scaling in mind

--- Userland/Libraries/LibGfx/Painter.cpp
+++ Userland/Libraries/LibGfx/Painter.cpp
@@ -1253,7 +1253,7 @@ void Painter::draw_physical_pixel(const IntPoint& physical_position, Color color
     }
 
     IntRect rect { physical_position, { thickness, thickness } };
-    rect.intersect(clip_rect());
+    rect.intersect(clip_rect() * scale());
     fill_physical_rect(rect, color);
 }
 
