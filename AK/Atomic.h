cac3c9f8c32f8165b992a02bc9550b76a0857437
Liav A <liavalb@gmail.com>
2021-02-27 19:30:20 +0200

    AK: Add a full memory barrier function based on atomic operations
    We use atomic_signal_fence and atomic_thread_fence together to prevent
    reordering of memory accesses by the CPU and the compiler.
    
    The usage of these functions was suggested by @tomuta so we can be sure
    that important memory accesses happen in the expected order :)

--- AK/Atomic.h
+++ AK/Atomic.h
@@ -31,9 +31,20 @@
 
 namespace AK {
 
+static inline void atomic_signal_fence(MemoryOrder order) noexcept
+{
+    return __atomic_signal_fence(order);
+}
+
 static inline void atomic_thread_fence(MemoryOrder order) noexcept
 {
     return __atomic_thread_fence(order);
+}
+
+static inline void full_memory_barrier() noexcept
+{
+    atomic_signal_fence(AK::MemoryOrder::memory_order_acq_rel);
+    atomic_thread_fence(AK::MemoryOrder::memory_order_acq_rel);
 }
 
 template<typename T>
@@ -376,3 +387,4 @@ public:
 }
 
 using AK::Atomic;
+using AK::full_memory_barrier;
