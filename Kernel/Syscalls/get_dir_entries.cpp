02561a6e36a7e99ce37ca18fe53737b41a572980
Andreas Kling <kling@serenityos.org>
2021-03-01 15:04:31 +0100

    Kernel: Use Userspace<T> in sys$get_dir_entries()

--- Kernel/Syscalls/get_dir_entries.cpp
+++ Kernel/Syscalls/get_dir_entries.cpp
@@ -29,18 +29,18 @@
 
 namespace Kernel {
 
-KResultOr<ssize_t> Process::sys$get_dir_entries(int fd, void* buffer, ssize_t size)
+KResultOr<ssize_t> Process::sys$get_dir_entries(int fd, Userspace<void*> user_buffer, ssize_t user_size)
 {
     REQUIRE_PROMISE(stdio);
-    if (size < 0)
+    if (user_size < 0)
         return EINVAL;
     auto description = file_description(fd);
     if (!description)
         return EBADF;
-    auto user_buffer = UserOrKernelBuffer::for_user_buffer((u8*)buffer, size);
-    if (!user_buffer.has_value())
+    auto buffer = UserOrKernelBuffer::for_user_buffer(user_buffer, static_cast<size_t>(user_size));
+    if (!buffer.has_value())
         return EFAULT;
-    return description->get_dir_entries(user_buffer.value(), size);
+    return description->get_dir_entries(buffer.value(), user_size);
 }
 
 }
