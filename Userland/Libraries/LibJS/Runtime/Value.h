df499b981fe5586482eeb3998bd36a8b25f2ec64
Linus Groh <mail@linusgroh.de>
2021-03-02 18:04:13 +0100

    LibJS: Implement the GetMethod abstract operation
    https://tc39.es/ecma262/#sec-getmethod
    
    We have bunch of duplicated on-demand versions of this, let's do it
    properly.

--- Userland/Libraries/LibJS/Runtime/Value.h
+++ Userland/Libraries/LibJS/Runtime/Value.h
@@ -340,6 +340,7 @@ bool same_value(Value lhs, Value rhs);
 bool same_value_zero(Value lhs, Value rhs);
 bool same_value_non_numeric(Value lhs, Value rhs);
 TriState abstract_relation(GlobalObject&, bool left_first, Value lhs, Value rhs);
+Function* get_method(GlobalObject& global_object, Value, const PropertyName&);
 size_t length_of_array_like(GlobalObject&, const Object&);
 
 }
