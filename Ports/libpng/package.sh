a9cfc09eec68d560ff541390552076958ca97773
TheMorc <r.gracik@gmail.com>
2021-03-03 23:01:04 +0100

    Ports: Add libpng 1.6.37

---
+++ Ports/libpng/package.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port=libpng
+version=1.6.37
+useconfigure=true
+files="https://download.sourceforge.net/libpng/libpng-${version}.tar.gz libpng-${version}.tar.gz"
+workdir="libpng-$version"
