437b0b5183154dee20ce445ccf740b6c47566385
Mițca Dumitru <dumitru0mitca@gmail.com>
2021-03-04 19:38:21 +0200

    LibC: Fix typo in fenv.h
    `FE_TOWARDSZERO` is actually spelled `FE_TOWARDZERO` (without the 'S')

--- Userland/Libraries/LibC/fenv.h
+++ Userland/Libraries/LibC/fenv.h
@@ -77,7 +77,7 @@ int feraiseexcept(int exceptions);
 #define FE_TONEAREST 0
 #define FE_DOWNWARD 1
 #define FE_UPWARD 2
-#define FE_TOWARDSZERO 3
+#define FE_TOWARDZERO 3
 
 int fesetround(int round);
 int fegetround(void);
