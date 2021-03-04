19ec112dd7e98b4b8d380a30793e5972f1a453cb
Andreas Kling <kling@serenityos.org>
2021-03-04 16:33:16 +0100

    Kernel: Remove repeated_{in,out}16()
    These were some wrappers around x86 "insw/outsw" that are no longer
    used for anything, so let's remove them.

--- Kernel/IO.h
+++ Kernel/IO.h
@@ -61,14 +61,6 @@ inline u32 in32(u16 port)
     return value;
 }
 
-inline void repeated_in16(u16 port, u16* buffer, int buffer_size)
-{
-    asm volatile("rep insw"
-                 : "+D"(buffer), "+c"(buffer_size)
-                 : "d"(port)
-                 : "memory");
-}
-
 inline void out8(u16 port, u8 value)
 {
     asm volatile("outb %0, %1" ::"a"(value), "Nd"(port));
@@ -82,13 +74,6 @@ inline void out16(u16 port, u16 value)
 inline void out32(u16 port, u32 value)
 {
     asm volatile("outl %0, %1" ::"a"(value), "Nd"(port));
-}
-
-inline void repeated_out16(u16 port, const u16* data, int data_size)
-{
-    asm volatile("rep outsw"
-                 : "+S"(data), "+c"(data_size)
-                 : "d"(port));
 }
 
 inline void delay(size_t microseconds)
