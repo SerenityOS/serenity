6f7ef4ec65e0695c782e8378ed331585b23dbfd8
Itamar <itamar8910@gmail.com>
2021-03-05 20:55:17 +0200

    LanguageServers: Tweak FileDB API
    - FileDB::get() now returns nullptr if the file is not in the FileDB
    - Added FileDB::get_or_create_from_filesystem()
    - Added FileDB::add() version that receives that file's content as a
    parameter

--- Userland/DevTools/HackStudio/LanguageServers/FileDB.h
+++ Userland/DevTools/HackStudio/LanguageServers/FileDB.h
@@ -37,19 +37,23 @@ class FileDB final {
 public:
     RefPtr<const GUI::TextDocument> get(const String& file_name) const;
     RefPtr<GUI::TextDocument> get(const String& file_name);
+    RefPtr<const GUI::TextDocument> get_or_create_from_filesystem(const String& file_name) const;
+    RefPtr<GUI::TextDocument> get_or_create_from_filesystem(const String& file_name);
     bool add(const String& file_name, int fd);
+    bool add(const String& file_name, const String& content);
 
     void set_project_root(const String& root_path) { m_project_root = root_path; }
 
     void on_file_edit_insert_text(const String& file_name, const String& inserted_text, size_t start_line, size_t start_column);
     void on_file_edit_remove_text(const String& file_name, size_t start_line, size_t start_column, size_t end_line, size_t end_column);
     String to_absolute_path(const String& file_name) const;
-    bool is_open(String file_name) const;
+    bool is_open(const String& file_name) const;
 
 private:
     RefPtr<GUI::TextDocument> create_from_filesystem(const String& file_name) const;
     RefPtr<GUI::TextDocument> create_from_fd(int fd) const;
     RefPtr<GUI::TextDocument> create_from_file(Core::File&) const;
+    static RefPtr<GUI::TextDocument> create_with_content(const String&);
 
 private:
     HashMap<String, NonnullRefPtr<GUI::TextDocument>> m_open_files;
