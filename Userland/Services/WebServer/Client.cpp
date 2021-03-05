9795084c5896ce5b9d3a76bd741edabb9dbfb97f
Brendan Coles <bcoles@gmail.com>
2021-03-05 13:25:08 +0000

    WebServer: Serve X-Frame-Options and X-Content-Type-Options HTTP headers

--- Userland/Services/WebServer/Client.cpp
+++ Userland/Services/WebServer/Client.cpp
@@ -137,6 +137,8 @@ void Client::send_response(InputStream& response, const HTTP::HttpRequest& reque
     StringBuilder builder;
     builder.append("HTTP/1.0 200 OK\r\n");
     builder.append("Server: WebServer (SerenityOS)\r\n");
+    builder.append("X-Frame-Options: SAMEORIGIN\r\n");
+    builder.append("X-Content-Type-Options: nosniff\r\n");
     builder.append("Content-Type: ");
     builder.append(content_type);
     builder.append("\r\n");
