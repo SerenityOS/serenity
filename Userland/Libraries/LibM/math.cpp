77ca377698a11e316fb18aaf7c3359aaf44c1c3c
Jorropo <jorropo.pgm@gmail.com>
2021-03-06 04:53:12 +0100

    LibM: Less naive implementation of copysign() :)

--- Userland/Libraries/LibM/math.cpp
+++ Userland/Libraries/LibM/math.cpp
@@ -745,11 +745,12 @@ long double nexttowardl(long double, long double) NOEXCEPT
 
 double copysign(double x, double y)
 {
-    if (x < 0 && y < 0)
-        return x;
-    if (x >= 0 && y < 0)
+    if (x < 0) {
+        if (y < 0)
+            return x;
         return -x;
-    if (x < 0 && y >= 0)
+    }
+    if (y < 0)
         return -x;
     return x;
 }
