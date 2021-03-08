d33250faaec88e126b7404a2097df0c0ab52ad32
AnicJov <contact.andrija@gmail.com>
2021-03-08 16:04:27 +0100

    Ports: Add libogg
    This library will allow us to port more multimedia programs and games,
    since it's a common dependency of such projects. I was looking at
    Scummvm's dependencies in particular, with a goal of porting them all
    eventually, so that we can have Myst running in Serenity!

---
+++ Ports/libogg/package.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port=libogg
+version=1.3.4
+useconfigure=true
+files="https://github.com/xiph/ogg/releases/download/v${version}/libogg-${version}.tar.gz libogg-${version}.tar.gz"
+workdir="libogg-${version}"
