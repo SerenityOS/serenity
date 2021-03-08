74428f6aef02b71595143c0538a1121d5ea193bb
Andreas Kling <kling@serenityos.org>
2021-03-08 13:40:53 +0100

    LibWeb: Implement StyleSheet.ownerNode :^)

--- Userland/Libraries/LibWeb/CSS/StyleSheet.cpp
+++ Userland/Libraries/LibWeb/CSS/StyleSheet.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2018-2021, Andreas Kling <kling@serenityos.org>
  * Copyright (c) 2021, the SerenityOS developers.
  * All rights reserved.
  *
@@ -26,7 +26,16 @@
  */
 
 #include <LibWeb/CSS/StyleSheet.h>
+#include <LibWeb/DOM/Element.h>
 
 namespace Web::CSS {
+
+void StyleSheet::set_owner_node(DOM::Element* element)
+{
+    if (element)
+        m_owner_node = element->make_weak_ptr<DOM::Element>();
+    else
+        m_owner_node = nullptr;
+}
 
 }
