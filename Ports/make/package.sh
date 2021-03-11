95a1b2e2b5f52825e57e9642c4361d25ebe4847c
Linus Groh <mail@linusgroh.de>
2021-03-11 21:58:18 +0100

    Ports: Use SERENITY_ARCH in --target/--host
    Recent ports already do this, let's update the others as well. One step
    closer to multi-arch support for ports! :^)

--- Ports/make/package.sh
+++ Ports/make/package.sh
@@ -7,4 +7,4 @@ http://ftp.gnu.org/gnu/make/make-${version}.tar.gz.sig make-${version}.tar.gz.si
 https://ftp.gnu.org/gnu/gnu-keyring.gpg gnu-keyring.gpg"
 auth_type="sig"
 auth_opts="--keyring ./gnu-keyring.gpg make-${version}.tar.gz.sig"
-configopts="--target=i686-pc-serenity --with-sysroot=/ --without-guile"
+configopts="--target=${SERENITY_ARCH}-pc-serenity --with-sysroot=/ --without-guile"
