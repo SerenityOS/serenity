5f78d3ddd029938bc0c544a598a3406c323d1a87
Andreas Kling <kling@serenityos.org>
2021-03-11 13:23:23 +0100

    Kernel: Move process umask into protected data :^)

--- Kernel/Syscalls/umask.cpp
+++ Kernel/Syscalls/umask.cpp
@@ -32,6 +32,7 @@ KResultOr<mode_t> Process::sys$umask(mode_t mask)
 {
     REQUIRE_PROMISE(stdio);
     auto old_mask = m_umask;
+    ProtectedDataMutationScope scope { *this };
     m_umask = mask & 0777;
     return old_mask;
 }
