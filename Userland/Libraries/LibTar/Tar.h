b1a684019bd263e64e8fcb72f5ca0d9dc2986623
Idan Horowitz <idan.horowitz@gmail.com>
2021-03-13 01:40:04 +0200

    LibTar: Implement TarOutputStream
    This output stream creates gnu format tar archives
    and currently only supports files and directories.

--- Userland/Libraries/LibTar/Tar.h
+++ Userland/Libraries/LibTar/Tar.h
@@ -27,7 +27,9 @@
 
 #pragma once
 
+#include <AK/String.h>
 #include <AK/StringView.h>
+#include <string.h>
 #include <sys/types.h>
 
 namespace Tar {
@@ -52,9 +54,8 @@ constexpr const char* gnu_version = " ";     // gnu format version
 constexpr const char* ustar_magic = "ustar"; // ustar format magic
 constexpr const char* ustar_version = "00";  // ustar format version
 
-class Header {
+class [[gnu::packed]] Header {
 public:
-    // FIXME: support ustar filename prefix
     const StringView file_name() const { return m_file_name; }
     mode_t mode() const { return get_tar_field(m_mode); }
     uid_t uid() const { return get_tar_field(m_uid); }
@@ -70,6 +71,26 @@ public:
     const StringView group_name() const { return m_group_name; }
     int major() const { return get_tar_field(m_major); }
     int minor() const { return get_tar_field(m_minor); }
+    // FIXME: support ustar filename prefix
+    const StringView prefix() const { return m_prefix; }
+
+    void set_file_name(const String& file_name) { VERIFY(file_name.copy_characters_to_buffer(m_file_name, sizeof(m_file_name))); }
+    void set_mode(mode_t mode) { VERIFY(String::formatted("{:o}", mode).copy_characters_to_buffer(m_mode, sizeof(m_mode))); }
+    void set_uid(uid_t uid) { VERIFY(String::formatted("{:o}", uid).copy_characters_to_buffer(m_uid, sizeof(m_uid))); }
+    void set_gid(gid_t gid) { VERIFY(String::formatted("{:o}", gid).copy_characters_to_buffer(m_gid, sizeof(m_gid))); }
+    void set_size(size_t size) { VERIFY(String::formatted("{:o}", size).copy_characters_to_buffer(m_size, sizeof(m_size))); }
+    void set_timestamp(time_t timestamp) { VERIFY(String::formatted("{:o}", timestamp).copy_characters_to_buffer(m_timestamp, sizeof(m_timestamp))); }
+    void set_type_flag(FileType type) { m_type_flag = type; }
+    void set_link_name(const String& link_name) { VERIFY(link_name.copy_characters_to_buffer(m_link_name, sizeof(m_link_name))); }
+    void set_magic(const char* magic) { memcpy(m_magic, magic, sizeof(m_magic)); }           // magic doesnt necessarily include a null byte
+    void set_version(const char* version) { memcpy(m_version, version, sizeof(m_version)); } // version doesnt necessarily include a null byte
+    void set_owner_name(const String& owner_name) { VERIFY(owner_name.copy_characters_to_buffer(m_owner_name, sizeof(m_owner_name))); }
+    void set_group_name(const String& group_name) { VERIFY(group_name.copy_characters_to_buffer(m_group_name, sizeof(m_group_name))); }
+    void set_major(int major) { VERIFY(String::formatted("{:o}", major).copy_characters_to_buffer(m_major, sizeof(m_major))); }
+    void set_minor(int minor) { VERIFY(String::formatted("{:o}", minor).copy_characters_to_buffer(m_minor, sizeof(m_minor))); }
+    void set_prefix(const String& prefix) { VERIFY(prefix.copy_characters_to_buffer(m_prefix, sizeof(m_prefix))); }
+
+    void calculate_checksum();
 
 private:
     char m_file_name[100];
@@ -78,7 +99,7 @@ private:
     char m_gid[8];
     char m_size[12];
     char m_timestamp[12];
-    char m_checksum[8];
+    char m_checksum[8]; // an uninitialized header's checksum is filled with spaces
     char m_type_flag;
     char m_link_name[100];
     char m_magic[6];
@@ -87,7 +108,7 @@ private:
     char m_group_name[32];
     char m_major[8];
     char m_minor[8];
-    char m_prefix[155];
+    char m_prefix[155]; // zero out the prefix for archiving
 
     template<size_t N>
     static size_t get_tar_field(const char (&field)[N]);
@@ -106,6 +127,15 @@ size_t Header::get_tar_field(const char (&field)[N])
         value += field[i] - '0';
     }
     return value;
+}
+void Header::calculate_checksum()
+{
+    memset(m_checksum, ' ', sizeof(m_checksum));
+    auto checksum = 0u;
+    for (auto i = 0u; i < sizeof(Header); ++i) {
+        checksum += ((unsigned char*)this)[i];
+    }
+    VERIFY(String::formatted("{:o}", checksum).copy_characters_to_buffer(m_checksum, sizeof(m_checksum)));
 }
 
 }
