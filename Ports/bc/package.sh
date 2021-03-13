60b458f5b38cdb684d04f860ac5ac93a61f59914
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2021-03-13 19:46:57 +0100

    Ports: Fix 'bc' port
    Previously, these features were apparently auto-disabled, because we do not yet
    support and never did support translations or the FIONREAD ioctl.

--- Ports/bc/package.sh
+++ Ports/bc/package.sh
@@ -10,5 +10,7 @@ auth_import_key="E2A30324A4465A4D5882692EC08038BDF280D33E"
 auth_opts="bc-${version}.tar.xz.sig"
 
 configure() {
-    run env HOSTCC=gcc ./"$configscript"
+    # NLS needs many things, none of which we support.
+    # History needs FIONREAD ioctl, which we don't support yet.
+    run env HOSTCC=gcc ./"$configscript" --disable-nls --disable-history
 }
