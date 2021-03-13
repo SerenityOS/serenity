33f65c7e1fa360bd7ee94684011e27c1b794a4cf
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2021-03-13 22:37:22 +0100

    Ports: Make gnuplot build fail later

--- Ports/gnuplot/package.sh
+++ Ports/gnuplot/package.sh
@@ -4,7 +4,7 @@ version=5.2.8
 useconfigure=true
 # Note: gnuplot's source code is hosted on SourceForge, but using the GitHub mirror makes downloading a versioned .tar.gz easier.
 files="https://github.com/gnuplot/gnuplot/archive/${version}.tar.gz gnuplot-${version}.tar.gz"
-configopts="--prefix=${SERENITY_BUILD_DIR}/Root/usr/local --with-readline=builtin"
+configopts="--prefix=${SERENITY_BUILD_DIR}/Root/usr/local --with-readline=builtin --without-latex"
 
 pre_configure() {
     run ./prepare
