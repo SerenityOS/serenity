8e4133b5c57c3d49d5b3c0ee6d7d08491a7cd1cd
Mițca Dumitru <dumitru0mitca@gmail.com>
2021-03-14 20:13:25 +0200

    LibM: Define HUGE_VAL{F,L} in terms of compiler builtins

--- Userland/Libraries/LibM/math.h
+++ Userland/Libraries/LibM/math.h
@@ -42,7 +42,9 @@ __BEGIN_DECLS
 #define MATH_ERREXCEPT 2
 #define math_errhandling MATH_ERREXCEPT
 
-#define HUGE_VAL 1e10000
+#define HUGE_VALF __builtin_huge_valf()
+#define HUGE_VAL __builtin_huge_val()
+#define HUGE_VALL __builtin_huge_vall()
 #define INFINITY __builtin_huge_val()
 #define NAN __builtin_nan("")
 #define MAXFLOAT FLT_MAX
