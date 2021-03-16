fe9f4f9796487164eb45dccc02865345954857bf
Dan MacDonald <allcoms@gmail.com>
2021-03-16 16:39:40 +0000

    Fix hatari package.sh

--- Ports/hatari/package.sh
+++ Ports/hatari/package.sh
@@ -1,10 +1,10 @@
 #!/bin/bash ../.port_include.sh
 port=hatari
-version=git
 useconfigure=true
+version=2.4.0-devel
 depends="SDL2 zlib"
-workdir="${port}-${commit}"
 commit=353379e1f8a847cc0e284541d2b40fd49d175d22
+workdir="${port}-${commit}"
 configopts="-DCMAKE_TOOLCHAIN_FILE=$SERENITY_ROOT/Toolchain/CMakeToolchain.txt"
 files="https://github.com/hatari/hatari/archive/${commit}.tar.gz ${commit}.tar.gz"
 
