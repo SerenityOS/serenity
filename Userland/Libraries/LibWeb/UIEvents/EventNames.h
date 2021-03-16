07c727710d37f92a769bb21eec56fd0a58a740b7
Andreas Kling <kling@serenityos.org>
2021-03-16 18:03:43 +0100

    LibWeb: Dispatch "resize" events on the Window object
    It's a little awkward that we do this in two places, but IPWV and OOPWV
    currently implement resizing a little differently from each other so we
    need to cover both paths.

--- Userland/Libraries/LibWeb/UIEvents/EventNames.h
+++ Userland/Libraries/LibWeb/UIEvents/EventNames.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2020, the SerenityOS developers.
+ * Copyright (c) 2021, Andreas Kling <kling@serenityos.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -40,7 +41,8 @@ namespace Web::UIEvents::EventNames {
     __ENUMERATE_UI_EVENT(mousemove)  \
     __ENUMERATE_UI_EVENT(mouseout)   \
     __ENUMERATE_UI_EVENT(mouseover)  \
-    __ENUMERATE_UI_EVENT(mouseup)
+    __ENUMERATE_UI_EVENT(mouseup)    \
+    __ENUMERATE_UI_EVENT(resize)
 
 #define __ENUMERATE_UI_EVENT(name) extern FlyString name;
 ENUMERATE_UI_EVENTS
