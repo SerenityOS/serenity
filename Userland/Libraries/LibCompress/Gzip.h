6b2ef7cf48c9e353c3b509f65388939ff2d32216
Idan Horowitz <idan.horowitz@gmail.com>
2021-03-16 15:20:46 +0200

    LibCompress: Handle and propagate stream errors in GzipDecompressor
    This commit makes read short-circuit if its input stream errored,
    as well as propagate error handling to wrapped sub streams, similarly
    to DeflateDecompressor.

--- Userland/Libraries/LibCompress/Gzip.h
+++ Userland/Libraries/LibCompress/Gzip.h
@@ -67,6 +67,7 @@ public:
     bool discard_or_error(size_t) override;
 
     bool unreliable_eof() const override;
+    bool handle_any_error() override;
 
     static Optional<ByteBuffer> decompress_all(ReadonlyBytes);
     static bool is_likely_compressed(ReadonlyBytes bytes);
