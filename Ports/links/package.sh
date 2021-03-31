f340c061a02e35e9dafa11d599c04715966657db
Brendan Coles <bcoles@gmail.com>
2021-03-31 08:22:03 +0000

    Ports: Links: Bump version from 2.19 to 2.22

--- Ports/links/package.sh
+++ Ports/links/package.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=links
-version=2.19
+version=2.22
 useconfigure=true
-files="http://links.twibright.com/download/links-2.19.tar.bz2 links-2.19.tar.bz2 fa6df86919640e490187ee9d85a4f598"
+files="http://links.twibright.com/download/links-${version}.tar.bz2 links-${version}.tar.bz2 55f745dea500aac52cede98bab8d96e2"
