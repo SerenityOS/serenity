ec84de8d0f4973c09495b76f5eeb02add7ec48da
Jamie Mansfield <jmansfield@cadixdev.org>
2021-04-02 21:31:23 +0100

    LibJS: ArrayBuffer.prototype.slice
    Implements the aforementioned native Javascript function, following the
    specification's [1] implementation.
    
    [1] https://tc39.es/ecma262/#sec-arraybuffer.prototype.slice

--- Userland/Libraries/LibJS/Runtime/ArrayBufferPrototype.cpp
+++ Userland/Libraries/LibJS/Runtime/ArrayBufferPrototype.cpp
@@ -66,12 +66,51 @@ static ArrayBuffer* array_buffer_object_from(VM& vm, GlobalObject& global_object
     return static_cast<ArrayBuffer*>(&this_object);
 }
 
+// 25.1.5.3 ArrayBuffer.prototype.slice, https://tc39.es/ecma262/#sec-arraybuffer.prototype.slice
 JS_DEFINE_NATIVE_FUNCTION(ArrayBufferPrototype::slice)
 {
+    const auto start = vm.argument(0);
+    const auto end = vm.argument(1);
+
     auto array_buffer_object = array_buffer_object_from(vm, global_object);
     if (!array_buffer_object)
         return {};
-    TODO();
+
+    // FIXME: Check for shared buffer
+    // FIXME: Check for detached buffer
+
+    const auto len = array_buffer_object->byte_length();
+
+    const auto relative_start = start.is_negative_infinity()
+        ? 0
+        : start.to_integer_or_infinity(global_object);
+    if (vm.exception())
+        return {};
+
+    const auto first = relative_start < 0
+        ? max(len + relative_start, 0.0)
+        : min(relative_start, static_cast<double>(len));
+
+    const auto relative_end = end.is_undefined()
+        ? len
+        : end.to_integer_or_infinity(global_object);
+    if (vm.exception())
+        return {};
+
+    double final_;
+    if (end.is_negative_infinity())
+        final_ = 0;
+    else if (relative_end < 0)
+        final_ = max(len + relative_end, 0.0);
+    else
+        final_ = min(relative_end, static_cast<double>(len));
+
+    const auto new_len = max(final_ - first, 0.0);
+
+    // FIXME: this is a bit more involved in the specification
+    auto sliced = array_buffer_object->buffer().slice(first, new_len);
+    auto buffer = ArrayBuffer::create(global_object, sliced);
+    return buffer;
 }
 
 JS_DEFINE_NATIVE_GETTER(ArrayBufferPrototype::byte_length_getter)
