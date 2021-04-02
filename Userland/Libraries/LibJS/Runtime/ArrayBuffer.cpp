4a0a2fad87895ab39d893a40730e1550f33881c9
Jamie Mansfield <jmansfield@cadixdev.org>
2021-04-02 21:31:23 +0100

    LibJS: ArrayBuffer.prototype.slice
    Implements the aforementioned native Javascript function, following the
    specification's [1] implementation.
    
    [1] https://tc39.es/ecma262/#sec-arraybuffer.prototype.slice

--- Userland/Libraries/LibJS/Runtime/ArrayBuffer.cpp
+++ Userland/Libraries/LibJS/Runtime/ArrayBuffer.cpp
@@ -34,9 +34,20 @@ ArrayBuffer* ArrayBuffer::create(GlobalObject& global_object, size_t byte_size)
     return global_object.heap().allocate<ArrayBuffer>(global_object, byte_size, *global_object.array_buffer_prototype());
 }
 
+ArrayBuffer* ArrayBuffer::create(GlobalObject& global_object, ByteBuffer& buffer)
+{
+    return global_object.heap().allocate<ArrayBuffer>(global_object, buffer, *global_object.array_buffer_prototype());
+}
+
 ArrayBuffer::ArrayBuffer(size_t byte_size, Object& prototype)
     : Object(prototype)
     , m_buffer(ByteBuffer::create_zeroed(byte_size))
+{
+}
+
+ArrayBuffer::ArrayBuffer(ByteBuffer& buffer, Object& prototype)
+    : Object(prototype)
+    , m_buffer(buffer)
 {
 }
 
