fd4645c30429f87c5e6babbbb5dec77620584abc
Linus Groh <mail@linusgroh.de>
2021-04-02 21:00:37 +0200

    LibJS: Use empty value for Reference unresolvable state, not undefined
    This fixes an issue where `undefined.foo = "bar"` would throw a
    ReferenceError instead of a TypeError as undefined was also used for
    truly unresolvable references (e.g. `foo() = "bar"`). I also made the
    various error messages here a bit nicer, just "primitive value" is not
    very helpful.

--- Userland/Libraries/LibJS/Tests/strict-mode-errors.js
+++ Userland/Libraries/LibJS/Tests/strict-mode-errors.js
@@ -4,12 +4,26 @@ test("basic functionality", () => {
     [true, false, "foo", 123].forEach(primitive => {
         expect(() => {
             primitive.foo = "bar";
-        }).toThrowWithMessage(TypeError, "Cannot assign property foo to primitive value");
+        }).toThrowWithMessage(
+            TypeError,
+            `Cannot set property 'foo' of ${typeof primitive} '${primitive}'`
+        );
         expect(() => {
             primitive[Symbol.hasInstance] = 123;
         }).toThrowWithMessage(
             TypeError,
-            "Cannot assign property Symbol(Symbol.hasInstance) to primitive value"
+            `Cannot set property 'Symbol(Symbol.hasInstance)' of ${typeof primitive} '${primitive}'`
+        );
+    });
+    [null, undefined].forEach(primitive => {
+        expect(() => {
+            primitive.foo = "bar";
+        }).toThrowWithMessage(TypeError, `Cannot set property 'foo' of ${primitive}`);
+        expect(() => {
+            primitive[Symbol.hasInstance] = 123;
+        }).toThrowWithMessage(
+            TypeError,
+            `Cannot set property 'Symbol(Symbol.hasInstance)' of ${primitive}`
         );
     });
 });
