54910e1d86818488b464fdc2875a440f7a00810d
Linus Groh <mail@linusgroh.de>
2021-04-03 14:56:54 +0200

    LibJS: Fix returning from try statement
    Not sure if this regressed at some point or just never worked, it
    definitely wasn't tested at all. We would always return undefined when
    returning from a try statement block, handler, or finalizer.

---
+++ Userland/Libraries/LibJS/Tests/try-catch-finally-return.js
@@ -0,0 +1,34 @@
+test("return from try block", () => {
+    function foo() {
+        try {
+            return "foo";
+        } catch {
+            return "bar";
+        }
+    }
+    expect(foo()).toBe("foo");
+});
+
+test("return from catch block", () => {
+    function foo() {
+        try {
+            throw "foo";
+        } catch {
+            return "bar";
+        }
+    }
+    expect(foo()).toBe("bar");
+});
+
+test("return from finally block", () => {
+    function foo() {
+        try {
+            return "foo";
+        } catch {
+            return "bar";
+        } finally {
+            return "baz";
+        }
+    }
+    expect(foo()).toBe("baz");
+});
