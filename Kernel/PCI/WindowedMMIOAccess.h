e49f14699cc21334723970d0a497debf0d3bd70f
Liav A <liavalb@gmail.com>
2021-04-03 16:46:04 +0300

    Kernel/PCI: Introduce a new ECAM access mechanism
    Now the kernel supports 2 ECAM access methods.
    MMIOAccess was renamed to WindowedMMIOAccess and is what we had until
    now - each device that is detected on boot is assigned to a
    memory-mapped window, so IO operations on multiple devices can occur
    simultaneously due to creating multiple virtual mappings, hence the name
    is a memory-mapped window.
    
    This commit adds a new class called MMIOAccess (not to be confused with
    the old MMIOAccess class). This class creates one memory-mapped window.
    On each IO operation on a configuration space of a device, it maps the
    requested PCI bus region to that window. Therefore it holds a SpinLock
    during the operation to ensure that no other PCI bus region was mapped
    during the call.
    
    A user can choose to either use PCI ECAM with memory-mapped window
    for each device, or for an entire bus. By default, the kernel prefers to
    map the entire PCI bus region.

--- Kernel/PCI/WindowedMMIOAccess.h
+++ Kernel/PCI/WindowedMMIOAccess.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Liav A. <liavalb@hotmail.co.il>
+ * Copyright (c) 2020-2021, Liav A. <liavalb@hotmail.co.il>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -32,7 +32,7 @@
 #include <AK/Types.h>
 #include <Kernel/ACPI/Definitions.h>
 #include <Kernel/PCI/Access.h>
-#include <Kernel/SpinLock.h>
+#include <Kernel/PCI/MMIOAccess.h>
 #include <Kernel/VM/AnonymousVMObject.h>
 #include <Kernel/VM/PhysicalRegion.h>
 #include <Kernel/VM/Region.h>
@@ -41,39 +41,25 @@
 namespace Kernel {
 namespace PCI {
 
-#define PCI_MMIO_CONFIG_SPACE_SIZE 4096
-
-class MMIOAccess : public Access {
+class DeviceConfigurationSpaceMapping {
 public:
-    class MMIOSegment {
-    public:
-        MMIOSegment(PhysicalAddress, u8, u8);
-        u8 get_start_bus() const;
-        u8 get_end_bus() const;
-        size_t get_size() const;
-        PhysicalAddress get_paddr() const;
+    DeviceConfigurationSpaceMapping(Address, const MMIOAccess::MMIOSegment&);
+    VirtualAddress vaddr() const { return m_mapped_region->vaddr(); };
+    PhysicalAddress paddr() const { return m_mapped_region->physical_page(0)->paddr(); }
+    const Address& address() const { return m_device_address; };
 
-    private:
-        PhysicalAddress m_base_addr;
-        u8 m_start_bus;
-        u8 m_end_bus;
-    };
+private:
+    Address m_device_address;
+    NonnullOwnPtr<Region> m_mapped_region;
+};
+
+class WindowedMMIOAccess final : public MMIOAccess {
+public:
     static void initialize(PhysicalAddress mcfg);
 
 private:
-    PhysicalAddress determine_memory_mapped_bus_region(u32 segment, u8 bus) const;
-    void map_bus_region(u32, u8);
-    VirtualAddress get_device_configuration_space(Address address);
-    SpinLock<u8> m_access_lock;
-    u8 m_mapped_bus { 0 };
-    OwnPtr<Region> m_mapped_region;
-
-protected:
-    explicit MMIOAccess(PhysicalAddress mcfg);
-
-    virtual const char* access_type() const override { return "MMIOAccess"; };
-    virtual u32 segment_count() const override;
-    virtual void enumerate_hardware(Function<void(Address, ID)>) override;
+    explicit WindowedMMIOAccess(PhysicalAddress mcfg);
+    virtual const char* access_type() const override { return "WindowedMMIOAccess"; };
     virtual void write8_field(Address address, u32, u8) override;
     virtual void write16_field(Address address, u32, u16) override;
     virtual void write32_field(Address address, u32, u32) override;
@@ -81,11 +67,8 @@ protected:
     virtual u16 read16_field(Address address, u32) override;
     virtual u32 read32_field(Address address, u32) override;
 
-    virtual u8 segment_start_bus(u32) const override;
-    virtual u8 segment_end_bus(u32) const override;
-
-    PhysicalAddress m_mcfg;
-    HashMap<u16, MMIOSegment> m_segments;
+    Optional<VirtualAddress> get_device_configuration_space(Address address);
+    NonnullOwnPtrVector<DeviceConfigurationSpaceMapping> m_mapped_device_regions;
 };
 
 }
