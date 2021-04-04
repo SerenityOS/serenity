26da32c2546f191d922bdb8b86d10491f4ff2b04
AnicJov <contact.andrija@gmail.com>
2021-04-04 23:40:34 +0200

    Ports: Add libtheora

---
+++ Ports/libtheora/package.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port=libtheora
+version=1.1.1
+useconfigure=true
+files="http://downloads.xiph.org/releases/theora/libtheora-${version}.tar.bz2 libtheora-${version}.tar.bz2"
