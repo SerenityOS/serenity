8e4e640717cc23516ff0953154cd9a5867d0f987
Alexander <electrodeyt@gmail.com>
2021-04-04 13:26:42 +0200

    Ports: fix libvorbis installtion prefix

--- Ports/libvorbis/package.sh
+++ Ports/libvorbis/package.sh
@@ -2,6 +2,5 @@
 port=libvorbis
 version=1.3.7
 useconfigure=true
-configopts="--prefix=${SERENITY_BUILD_DIR}/Root/usr/local"
 files="https://github.com/xiph/vorbis/releases/download/v${version}/libvorbis-${version}.tar.gz libvorbis-${version}.tar.gz"
 depends=libogg
