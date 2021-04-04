6927ed4b7a731298f717ad72cbaec78728f9095f
Timothy Flynn <trflynn89@pm.me>
2021-04-03 21:50:53 -0400

    LibWeb: Convert RadioButton to be a LabelableNode

--- Userland/Libraries/LibWeb/Layout/RadioButton.h
+++ Userland/Libraries/LibWeb/Layout/RadioButton.h
@@ -27,25 +27,29 @@
 #pragma once
 
 #include <LibWeb/HTML/HTMLInputElement.h>
-#include <LibWeb/Layout/ReplacedBox.h>
+#include <LibWeb/Layout/LabelableNode.h>
 
 namespace Web::Layout {
 
-class RadioButton : public ReplacedBox {
+class RadioButton : public LabelableNode {
 public:
     RadioButton(DOM::Document&, HTML::HTMLInputElement&, NonnullRefPtr<CSS::StyleProperties>);
     virtual ~RadioButton() override;
 
     virtual void paint(PaintContext&, PaintPhase) override;
 
-    const HTML::HTMLInputElement& dom_node() const { return static_cast<const HTML::HTMLInputElement&>(ReplacedBox::dom_node()); }
-    HTML::HTMLInputElement& dom_node() { return static_cast<HTML::HTMLInputElement&>(ReplacedBox::dom_node()); }
+    const HTML::HTMLInputElement& dom_node() const { return static_cast<const HTML::HTMLInputElement&>(LabelableNode::dom_node()); }
+    HTML::HTMLInputElement& dom_node() { return static_cast<HTML::HTMLInputElement&>(LabelableNode::dom_node()); }
 
 private:
     virtual bool wants_mouse_events() const override { return true; }
     virtual void handle_mousedown(Badge<EventHandler>, const Gfx::IntPoint&, unsigned button, unsigned modifiers) override;
     virtual void handle_mouseup(Badge<EventHandler>, const Gfx::IntPoint&, unsigned button, unsigned modifiers) override;
     virtual void handle_mousemove(Badge<EventHandler>, const Gfx::IntPoint&, unsigned buttons, unsigned modifiers) override;
+
+    virtual void handle_associated_label_mousedown(Badge<Label>) override;
+    virtual void handle_associated_label_mouseup(Badge<Label>) override;
+    virtual void handle_associated_label_mousemove(Badge<Label>, bool is_inside_node_or_label) override;
 
     void set_checked_within_group();
 
