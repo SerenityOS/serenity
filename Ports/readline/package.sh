a7705547d438afe02b29d31b4db6acabaab395e2
AnicJov <contact.andrija@gmail.com>
2021-04-04 23:36:41 +0200

    Ports: Add readline

---
+++ Ports/readline/package.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port=readline
+version=8.1
+useconfigure=true
+files="ftp://ftp.gnu.org/gnu/readline/readline-${version}.tar.gz readline-${version}.tar.gz"
