f1b5b2f4c739f739e0c9ddcb4b05fb6d29abec79
Tom Needham <06needhamt@gmail.com>
2021-04-05 17:09:16 +0100

    Ports: Add Basic Skeleton for FFmpeg port

---
+++ Ports/FFmpeg/package.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port=ffmpeg
+version=4.3.2
+useconfigure=true
+files="https://ffmpeg.org/releases/ffmpeg-${version}.tar.xz ffmpeg-${version}.tar.xz
+https://ffmpeg.org/releases/ffmpeg-${version}.tar.xz.asc ffmpeg-${version}.tar.xz.asc"
+makeopts="HOSTCC=${CC} HOSTLD=${CC}"
+depends=
+
+configure() {
+	run ./configure --disable-everything --enable-cross-compile --target-os=serenity --arch=i386 --cross-prefix=${CC%gcc}
+}
