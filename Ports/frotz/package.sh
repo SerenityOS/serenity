42a6c42b26ebed177dcca280c40bbe8da40d8c53
Gunnar Beutner <gunnar@beutner.name>
2021-04-10 17:45:50 +0200

    Ports: Fix building the frotz port

--- Ports/frotz/package.sh
+++ Ports/frotz/package.sh
@@ -8,7 +8,7 @@ depends="ncurses"
 build() {
     run make \
         PKG_CONFIG_CURSES=no \
-        CURSES_CFLAGS="-I${SERENITY_ROOT}/Build/Root/usr/local/include/ncurses" \
+        CURSES_CFLAGS="-I${SERENITY_ROOT}/Build/i686/Root/usr/local/include/ncurses" \
         CURSES_LDFLAGS="-lncurses -ltinfo" \
         CURSES=ncurses \
         USE_UTF8=no \
