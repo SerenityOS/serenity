941f986b4db2096f50e503c887986ee6e5e1e1ba
Brendan Coles <bcoles@gmail.com>
2021-04-10 07:54:55 +0000

    Ports: Download GNU port archives and signatures using HTTPS

--- Ports/libiconv/package.sh
+++ Ports/libiconv/package.sh
@@ -3,7 +3,7 @@ port=libiconv
 version=1.16
 useconfigure=true
 files="https://ftp.gnu.org/pub/gnu/libiconv/libiconv-${version}.tar.gz libiconv-${version}.tar.gz
-http://ftp.gnu.org/gnu/libiconv/libiconv-${version}.tar.gz.sig libiconv-${version}.tar.gz.sig
+https://ftp.gnu.org/gnu/libiconv/libiconv-${version}.tar.gz.sig libiconv-${version}.tar.gz.sig
 https://ftp.gnu.org/gnu/gnu-keyring.gpg gnu-keyring.gpg"
 
 auth_type="sig"
