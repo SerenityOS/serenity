43ccff9f8cc0bd68895a9330354a23de2e99fc7d
Gunnar Beutner <gunnar@beutner.name>
2021-04-10 17:02:16 +0200

    Ports: Remove config opts for dash

--- Ports/dash/package.sh
+++ Ports/dash/package.sh
@@ -2,7 +2,6 @@
 port=dash
 version=0.5.10.2
 useconfigure=true
-configopts="--enable-static"
 files="http://gondor.apana.org.au/~herbert/dash/files/dash-${version}.tar.gz dash-${version}.tar.gz
 http://gondor.apana.org.au/~herbert/dash/files/dash-${version}.tar.gz.sha256sum dash-${version}.tar.gz.sha256sum"
 auth_type="sha256"
