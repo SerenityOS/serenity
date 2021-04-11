b1346171230db042c0ceb91532504f82a19ab64d
Brian Gianforcaro <b.gianfo@gmail.com>
2021-04-11 01:33:56 -0700

    LibGUI: Remove an unused AK/Debug.h include

--- Userland/Libraries/LibGUI/JsonArrayModel.cpp
+++ Userland/Libraries/LibGUI/JsonArrayModel.cpp
@@ -24,7 +24,6 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <AK/Debug.h>
 #include <AK/JsonObject.h>
 #include <LibCore/File.h>
 #include <LibGUI/JsonArrayModel.h>
