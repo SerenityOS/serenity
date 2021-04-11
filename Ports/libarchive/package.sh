bb23ab7040d057495d2288c5e6f7429ed7941f4b
Gunnar Beutner <gunnar@beutner.name>
2021-04-12 01:40:00 +0200

    Ports: Link libarchive against pcre because the system's libregex doesn't support re_nsub

--- Ports/libarchive/package.sh
+++ Ports/libarchive/package.sh
@@ -5,7 +5,9 @@ useconfigure=true
 configopts="--without-xml2"
 files="https://libarchive.org/downloads/libarchive-${version}.tar.gz libarchive-${version}.tar.gz
 https://libarchive.org/downloads/libarchive-${version}.tar.gz.asc libarchive-${version}.tar.gz.asc"
-depends=zlib
+depends="zlib pcre"
 auth_type="sig"
 auth_import_key="CB55788360B992FA0885C878F040F7196BA99AF4"
 auth_opts="libarchive-${version}.tar.gz.asc libarchive-${version}.tar.gz"
+
+export ac_cv_header_regex_h=no
