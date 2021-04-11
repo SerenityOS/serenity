5fe98bd1ab735c47db6fbca27b5b5ccaac801679
Gunnar Beutner <gunnar@beutner.name>
2021-04-11 23:21:31 +0200

    Ports: Add port for libicu
    I was trying to port openttd which I ultimately gave up on because
    too much of the C++ standard library's functionality is missing at this
    point. The libicu library was a dependency for that.
    
    In its current state the libicu port is not thread-safe because of
    missing functionality in the C++ standard library (mainly std::mutex,
    std::condition_variable, etc.).

---
+++ Ports/libicu/package.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port=libicu
+version=69_1
+useconfigure=true
+workdir=icu/source
+configopts=--with-cross-build=$(pwd)/${workdir}/../host-build
+files="https://github.com/unicode-org/icu/releases/download/release-${version//_/-}/icu4c-${version}-src.tgz icu4c-${version}-src.tgz"
+
+configure() {
+    host_env
+    run mkdir -p ../host-build
+    run sh -c "cd ../host-build && ../source/configure && make $makeopts"
+    target_env
+    run ./configure --host="${SERENITY_ARCH}-pc-serenity" $configopts
+}
+
+export CFLAGS="-DU_HAVE_NL_LANGINFO_CODESET=0 -DLC_MESSAGES=1"
+export CXXFLAGS="-DU_HAVE_NL_LANGINFO_CODESET=0 -DLC_MESSAGES=1"
