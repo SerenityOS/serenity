88771d7177e05d58be295ee3d523e3870a97a82b
Gunnar Beutner <gunnar@beutner.name>
2021-04-11 04:05:49 +0200

    Ports: Fix building the nesalizer port when SDL2 isn't installed on the host

--- Ports/nesalizer/package.sh
+++ Ports/nesalizer/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=nesalizer
 version=master
-makeopts="CONF=release"
+makeopts="CONF=release EXTRA=-I${SERENITY_ROOT}/Build/i686/Root/usr/include/SDL2"
 files="https://github.com/SerenityOS/nesalizer/archive/master.zip nesalizer-master.zip"
 depends=SDL2
