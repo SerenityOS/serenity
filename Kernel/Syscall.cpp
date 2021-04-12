96764dbab5bacc5c561ad254d81d251eda3d3fea
Brian Gianforcaro <b.gianfo@gmail.com>
2021-04-12 11:54:14 -0700

    Kernel: Suppress maybe-uninitialized' warning s_syscall_table in gcc-10.3.0

--- Kernel/Syscall.cpp
+++ Kernel/Syscall.cpp
@@ -138,7 +138,16 @@ KResultOr<FlatPtr> handle(RegisterState& regs, FlatPtr function, FlatPtr arg1, F
         dbgln("Null syscall {} requested, you probably need to rebuild this program!", function);
         return ENOSYS;
     }
+
+    // This appears to be a bogus warning, as s_syscall_table is always
+    // initialized, and the index (function) is always bounded.
+    // TODO: Figure out how to avoid the suppression.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+
     return (process.*(s_syscall_table[function]))(arg1, arg2, arg3);
+
+#pragma GCC diagnostic pop
 }
 
 }
