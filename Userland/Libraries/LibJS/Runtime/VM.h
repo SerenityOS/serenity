19c93aa33c1eb8692338d178e97b3a34b88abd57
Linus Groh <mail@linusgroh.de>
2021-04-13 01:04:32 +0200

    LibJS: Unset m_unwind_until_label in stop_unwind()
    I don't see a reason to keep this intact, that might only surprise us
    down the line...

--- Userland/Libraries/LibJS/Runtime/VM.h
+++ Userland/Libraries/LibJS/Runtime/VM.h
@@ -182,7 +182,11 @@ public:
         m_unwind_until = type;
         m_unwind_until_label = label;
     }
-    void stop_unwind() { m_unwind_until = ScopeType::None; }
+    void stop_unwind()
+    {
+        m_unwind_until = ScopeType::None;
+        m_unwind_until_label = {};
+    }
     bool should_unwind_until(ScopeType type, FlyString label = {}) const
     {
         if (m_unwind_until_label.is_null())
