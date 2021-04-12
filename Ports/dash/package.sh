3a58319dac0fbee4d1df9424852c2157453e88b0
Gunnar Beutner <gunnar@beutner.name>
2021-04-12 09:18:56 +0200

    Ports: Fix building the dash port

--- Ports/dash/package.sh
+++ Ports/dash/package.sh
@@ -5,3 +5,28 @@ useconfigure=true
 files="http://gondor.apana.org.au/~herbert/dash/files/dash-${version}.tar.gz dash-${version}.tar.gz
 http://gondor.apana.org.au/~herbert/dash/files/dash-${version}.tar.gz.sha256sum dash-${version}.tar.gz.sha256sum"
 auth_type="sha256"
+
+configure() {
+	host_env
+	run autoupdate
+	run autoconf
+	run aclocal
+	run automake --add-missing
+	run mkdir -p host-build
+	run sh -c "cd host-build && ../configure $configopts CFLAGS=-I."
+	target_env
+	run mkdir -p target-build
+	run sh -c "cd target-build && ../configure --host="${SERENITY_ARCH}-pc-serenity" --disable-helpers $configopts CFLAGS=-I."
+}
+
+build() {
+	host_env
+	run sh -c "cd host-build && make $makeopts"
+	run cp host-build/src/{mkinit,mksyntax,mknodes,mksignames} src
+	target_env
+	run sh -c "cd target-build && make $makeopts"
+}
+
+install() {
+    run sh -c "cd target-build && make DESTDIR="${SERENITY_BUILD_DIR}/Root" $installopts install"
+}
