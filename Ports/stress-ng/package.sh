76be1d4193d0eb6c61aeaef31a41545842ace500
Gunnar Beutner <gunnar@beutner.name>
2021-04-13 03:31:10 +0200

    Ports: Fix building the stress-ng port

--- Ports/stress-ng/package.sh
+++ Ports/stress-ng/package.sh
@@ -2,4 +2,9 @@
 port=stress-ng
 version=0.11.23
 files="https://github.com/ColinIanKing/stress-ng/archive/V${version}.tar.gz stress-ng-${version}.tar.gz"
-makeopts="STATIC=1 LDFLAGS=-L${SERENITY_BUILD_DIR}/Root/usr/local/lib"
+depends=zlib
+
+pre_configure() {
+    export CFLAGS="-I${SERENITY_BUILD_DIR}/Root/usr/local/include"
+    export LDFLAGS="-L${SERENITY_BUILD_DIR}/Root/usr/local/lib -lzlib"
+}
