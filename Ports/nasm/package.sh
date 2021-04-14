1942c1061c1bdd37f7a0155684e169e56075e6e1
Gunnar Beutner <gunnar@beutner.name>
2021-04-14 11:50:09 +0200

    Ports: Fix building the nasm port

--- Ports/nasm/package.sh
+++ Ports/nasm/package.sh
@@ -4,7 +4,3 @@ version=2.15.05
 files="https://www.nasm.us/pub/nasm/releasebuilds/${version}/nasm-${version}.tar.gz nasm-${version}.tar.gz"
 useconfigure=true
 makeopts=
-
-pre_configure() {
-	run ./autogen.sh
-}
