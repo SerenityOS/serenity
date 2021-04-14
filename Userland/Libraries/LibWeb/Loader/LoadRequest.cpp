3cc5286565721335e72dc92fd688a85505ef2568
Timothy Flynn <trflynn89@pm.me>
2021-04-14 10:34:18 -0400

    LibWeb: Helper for creating load requests with a Cookie header
    Instead of all interested parties needing to write out the code to get
    the cookie value for a load request, add a static helper to do it in
    one location.

---
+++ Userland/Libraries/LibWeb/Loader/LoadRequest.cpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2021, Tim Flynn <trflynn89@pm.me>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "LoadRequest.h"
+#include <LibWeb/Cookie/Cookie.h>
+#include <LibWeb/Page/Page.h>
+
+namespace Web {
+
+LoadRequest LoadRequest::create_for_url_on_page(const URL& url, Page* page)
+{
+    LoadRequest request;
+    request.set_url(url);
+
+    if (page) {
+        String cookie = page->client().page_did_request_cookie(url, Cookie::Source::Http);
+        if (!cookie.is_empty())
+            request.set_header("Cookie", cookie);
+    }
+
+    return request;
+}
+
+}
