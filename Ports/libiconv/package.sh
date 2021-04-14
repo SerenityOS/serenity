85218c086e9fbcd4eef697503407f10fdf5b878b
Gunnar Beutner <gunnar@beutner.name>
2021-04-14 04:35:01 +0200

    Ports: Build the shared library for the libiconv port
    I'd rather use libtool to build the library but that would
    require more extensive changes to the configure script
    and maybe even libtool itself. So instead I just build
    it manually.

--- Ports/libiconv/package.sh
+++ Ports/libiconv/package.sh
@@ -2,9 +2,16 @@
 port=libiconv
 version=1.16
 useconfigure=true
+configopts=--enable-shared
 files="https://ftp.gnu.org/pub/gnu/libiconv/libiconv-${version}.tar.gz libiconv-${version}.tar.gz
 https://ftp.gnu.org/gnu/libiconv/libiconv-${version}.tar.gz.sig libiconv-${version}.tar.gz.sig
 https://ftp.gnu.org/gnu/gnu-keyring.gpg gnu-keyring.gpg"
 
 auth_type="sig"
 auth_opts="--keyring ./gnu-keyring.gpg libiconv-${version}.tar.gz.sig"
+
+install() {
+    run make DESTDIR=$DESTDIR $installopts install
+    run ${SERENITY_ARCH}-pc-serenity-gcc -shared -o $DESTDIR/usr/local/lib/libiconv.so -Wl,--whole-archive $DESTDIR/usr/local/lib/libiconv.a -Wl,--no-whole-archive
+    run ln -sf ../local/lib/libiconv.so $DESTDIR/usr/lib/libiconv.so
+}
