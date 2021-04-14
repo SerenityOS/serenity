f0ec857a5b9f9778f3aa7abb29e335585811e69b
Gunnar Beutner <gunnar@beutner.name>
2021-04-14 20:32:43 +0200

    Ports: Don't export the DESTDIR variable by default
    Exporting DESTDIR interferes with cmake-based ports:
    
    Install the project...
    -- Install configuration: ""
    -- Up-to-date: /home/gunnar/serenity/Build/i686/Root/home/gunnar/serenity/Build/i686/Root/usr/lib/libSDL2.a
    -- Up-to-date: /home/gunnar/serenity/Build/i686/Root/home/gunnar/serenity/Build/i686/Root/usr/lib/libSDL2-2.0.a
    -- Up-to-date: /home/gunnar/serenity/Build/i686/Root/home/gunnar/serenity/Build/i686/Root/usr/lib/libSDL2main.a
    -- Up-to-date: /home/gunnar/serenity/Build/i686/Root/home/gunnar/serenity/Build/i686/Root/usr/lib/cmake/SDL2/SDL2Targets.cmake

--- Ports/.hosted_defs.sh
+++ Ports/.hosted_defs.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 export SERENITY_ROOT="$(realpath "${SCRIPT}/../")"
 export SERENITY_BUILD_DIR="${SERENITY_ROOT}/Build/${SERENITY_ARCH}"
 export CC="${SERENITY_ARCH}-pc-serenity-gcc"
@@ -7,7 +6,8 @@ export CXX="${SERENITY_ARCH}-pc-serenity-g++"
 export AR="${SERENITY_ARCH}-pc-serenity-ar"
 export RANLIB="${SERENITY_ARCH}-pc-serenity-ranlib"
 export PATH="${SERENITY_ROOT}/Toolchain/Local/${SERENITY_ARCH}/bin:${PATH}"
-export DESTDIR="${SERENITY_BUILD_DIR}/Root"
 export PKG_CONFIG_DIR=""
 export PKG_CONFIG_SYSROOT_DIR="${SERENITY_BUILD_DIR}/Root"
 export PKG_CONFIG_LIBDIR="${PKG_CONFIG_SYSROOT_DIR}/usr/lib/pkgconfig/:${PKG_CONFIG_SYSROOT_DIR}/usr/local/lib/pkgconfig"
+
+DESTDIR="${SERENITY_BUILD_DIR}/Root"
