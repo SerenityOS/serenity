3eae0d4594cb2872a98f192aa7f1fbdca9fb08ca
Gunnar Beutner <gunnar@beutner.name>
2021-04-15 13:58:15 +0200

    Ports: Fix download URL for libiconv

--- Ports/libiconv/package.sh
+++ Ports/libiconv/package.sh
@@ -3,7 +3,7 @@ port=libiconv
 version=1.16
 useconfigure=true
 configopts=--enable-shared
-files="https://ftpmirror.gnu.org/pub/gnu/libiconv/libiconv-${version}.tar.gz libiconv-${version}.tar.gz
+files="https://ftpmirror.gnu.org/gnu/libiconv/libiconv-${version}.tar.gz libiconv-${version}.tar.gz
 https://ftpmirror.gnu.org/gnu/libiconv/libiconv-${version}.tar.gz.sig libiconv-${version}.tar.gz.sig
 https://ftpmirror.gnu.org/gnu/gnu-keyring.gpg gnu-keyring.gpg"
 
