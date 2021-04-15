78ea4644b4dded5cf4718019f0be42312d40bc9a
Idan Horowitz <idan.horowitz@gmail.com>
2021-04-15 20:50:02 +0300

    LibWeb: Expose the MouseEvent::{clientX, clientY} attributes
    These provide the cursor coordinate within the viewport at which the
    event occurred (as opposed to the page relative coordinates exposed via
    offsetX, offsetY).

--- Userland/Libraries/LibWeb/UIEvents/MouseEvent.h
+++ Userland/Libraries/LibWeb/UIEvents/MouseEvent.h
@@ -35,24 +35,28 @@ class MouseEvent final : public UIEvents::UIEvent {
 public:
     using WrapperType = Bindings::MouseEventWrapper;
 
-    static NonnullRefPtr<MouseEvent> create(const FlyString& event_name, i32 offset_x, i32 offset_y)
+    static NonnullRefPtr<MouseEvent> create(const FlyString& event_name, i32 offset_x, i32 offset_y, i32 client_x, i32 client_y)
     {
-        return adopt(*new MouseEvent(event_name, offset_x, offset_y));
+        return adopt(*new MouseEvent(event_name, offset_x, offset_y, client_x, client_y));
     }
 
     virtual ~MouseEvent() override;
 
     i32 offset_x() const { return m_offset_x; }
     i32 offset_y() const { return m_offset_y; }
+    i32 client_x() const { return m_client_x; }
+    i32 client_y() const { return m_client_y; }
 
 protected:
-    MouseEvent(const FlyString& event_name, i32 offset_x, i32 offset_y);
+    MouseEvent(const FlyString& event_name, i32 offset_x, i32 offset_y, i32 client_x, i32 client_y);
 
 private:
     void set_event_characteristics();
 
     i32 m_offset_x { 0 };
     i32 m_offset_y { 0 };
+    i32 m_client_x { 0 };
+    i32 m_client_y { 0 };
 };
 
 }
