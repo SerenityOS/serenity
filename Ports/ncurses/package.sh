e932129e5aefc1ab4b51e88ba7fabbcdbcf6d29d
Gunnar Beutner <gunnar@beutner.name>
2021-04-15 13:50:42 +0200

    Ports: Use HTTPS when accessing ftpmirror.gnu.org
    Unlike what the name might suggest ftpmirror.gnu.org isn't
    accessible via FTP.

--- Ports/ncurses/package.sh
+++ Ports/ncurses/package.sh
@@ -3,7 +3,7 @@ port=ncurses
 version=6.2
 useconfigure=true
 configopts="--with-termlib --enable-pc-files --with-pkg-config=/usr/local/lib/pkgconfig --with-pkg-config-libdir=/usr/local/lib/pkgconfig --without-ada --enable-sigwinch"
-files="ftp://ftpmirror.gnu.org/gnu/ncurses/ncurses-${version}.tar.gz ncurses-${version}.tar.gz
+files="https://ftpmirror.gnu.org/gnu/ncurses/ncurses-${version}.tar.gz ncurses-${version}.tar.gz
 https://ftpmirror.gnu.org/gnu/ncurses/ncurses-${version}.tar.gz.sig ncurses-${version}.tar.gz.sig
 https://ftpmirror.gnu.org/gnu/gnu-keyring.gpg gnu-keyring.gpg"
 auth_type="sig"
