da92c0e1ca2be53df4a7889090656b428869d140
Gunnar Beutner <gunnar@beutner.name>
2021-04-15 15:46:40 +0200

    Ports: Build shared libraries for a few more ports
    This manually builds shared libraries for a bunch of ports. Using
    libtool would be preferable but that's currently broken so I'm
    linking the shared libraries manually.

--- Ports/SDL2_gfx/package.sh
+++ Ports/SDL2_gfx/package.sh
@@ -6,3 +6,8 @@ files="https://downloads.sourceforge.net/project/sdl2gfx/SDL2_gfx-${version}.tar
 depends="SDL2"
 useconfigure=true
 configopts="--with-sdl-prefix=${SERENITY_BUILD_DIR}/Root/usr/local"
+
+install() {
+    run make install DESTDIR=$DESTDIR $installopts
+    run ${CC} -shared -o $DESTDIR/usr/local/lib/libSDL2_gfx.so -Wl,--whole-archive $DESTDIR/usr/local/lib/libSDL2_gfx.a -Wl,--no-whole-archive
+}
