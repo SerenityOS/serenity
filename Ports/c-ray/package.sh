66a393e007a3fce76eda6ab0e16981c00ffc2993
Gunnar Beutner <gunnar@beutner.name>
2021-04-15 13:32:29 +0200

    Toolchain+Ports: Move the CMake toolchain file into a subdirectory

--- Ports/c-ray/package.sh
+++ Ports/c-ray/package.sh
@@ -4,7 +4,7 @@ version=git
 workdir=c-ray-master
 useconfigure=true
 files="https://github.com/vkoskiv/c-ray/archive/master.tar.gz c-ray-git.tar.gz"
-configopts="-DCMAKE_TOOLCHAIN_FILE=$SERENITY_ROOT/Toolchain/CMakeToolchain.txt"
+configopts="-DCMAKE_TOOLCHAIN_FILE=$SERENITY_ROOT/Toolchain/CMake/CMakeToolchain.txt"
 depends="SDL2"
 
 configure() {
