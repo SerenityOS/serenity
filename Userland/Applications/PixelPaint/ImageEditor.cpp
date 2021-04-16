0032399a049932aed14661fc2d92c724cc115af7
Jagger De Leo <jcdl@fastmail.com>
2021-04-15 22:06:25 -0400

    PixelPaint: Add keyboard zoom shortcuts
    You can now use Ctrl+= and Ctrl+- to zoom in and out.

--- Userland/Applications/PixelPaint/ImageEditor.cpp
+++ Userland/Applications/PixelPaint/ImageEditor.cpp
@@ -370,15 +370,19 @@ Layer* ImageEditor::layer_at_editor_position(const Gfx::IntPoint& editor_positio
     return nullptr;
 }
 
-void ImageEditor::scale_centered_on_position(const Gfx::IntPoint& position, float scale_delta)
+void ImageEditor::clamped_scale(float scale_delta)
 {
-    auto old_scale = m_scale;
-
     m_scale += scale_delta;
     if (m_scale < 0.1f)
         m_scale = 0.1f;
     if (m_scale > 100.0f)
         m_scale = 100.0f;
+}
+
+void ImageEditor::scale_centered_on_position(const Gfx::IntPoint& position, float scale_delta)
+{
+    auto old_scale = m_scale;
+    clamped_scale(scale_delta);
 
     auto focus_point = Gfx::FloatPoint(
         m_pan_origin.x() - ((float)position.x() - (float)width() / 2.0) / old_scale,
@@ -392,11 +396,19 @@ void ImageEditor::scale_centered_on_position(const Gfx::IntPoint& position, floa
         relayout();
 }
 
+void ImageEditor::scale_by(float scale_delta)
+{
+    if (scale_delta != 0) {
+        clamped_scale(scale_delta);
+        relayout();
+    }
+}
+
 void ImageEditor::reset_scale_and_position()
 {
     if (m_scale != 1.0f)
         m_scale = 1.0f;
-    
+
     m_pan_origin = Gfx::FloatPoint(0, 0);
     relayout();
 }
