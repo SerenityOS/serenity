a0c615e53e8854efa2fa791155cd9f7fc82675e1
Gunnar Beutner <gunnar@beutner.name>
2021-04-17 21:43:55 +0200

    Ports: Build shared library for libtiff

--- Ports/libtiff/package.sh
+++ Ports/libtiff/package.sh
@@ -7,3 +7,10 @@ http://download.osgeo.org/libtiff/tiff-${version}.tar.gz.sig tiff-${version}.tar
 auth_type="sig"
 auth_import_key="EBDFDB21B020EE8FD151A88DE301047DE1198975"
 auth_opts="tiff-${version}.tar.gz.sig tiff-${version}.tar.gz"
+
+install() {
+    run make DESTDIR=$DESTDIR $installopts install
+    ${CC} -shared -o $DESTDIR/usr/local/lib/libtiff.so -Wl,--whole-archive $DESTDIR/usr/local/lib/libtiff.a -Wl,--no-whole-archive
+    ${CC} -shared -o $DESTDIR/usr/local/lib/libtiffxx.so -Wl,--whole-archive $DESTDIR/usr/local/lib/libtiffxx.a -Wl,--no-whole-archive
+    rm -f $DESTDIR/usr/local/lib/libtiff.la $DESTDIR/usr/local/lib/libtiffxx.la
+}
