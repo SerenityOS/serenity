d5ae3d3ca6d682a6c64cbdc579ae20332bca3ea2
Gunnar Beutner <gunnar@beutner.name>
2021-04-17 10:47:55 +0200

    Ports: Enable building cmake on the host

--- Ports/cmake/package.sh
+++ Ports/cmake/package.sh
@@ -1,34 +1,16 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=cmake
 version=3.19.4
-useconfigure=false
+useconfigure=true
 files="https://github.com/Kitware/CMake/releases/download/v$version/cmake-$version.tar.gz cmake-$version.tar.gz 2a71f16c61bac5402004066d193fc14e"
 auth_type=md5
-depends="bash gcc make sed"
+depends="bash gcc make sed ncurses"
+configopts="-DCMAKE_TOOLCHAIN_FILE=$SERENITY_ROOT/Toolchain/CMake/CMakeToolchain.txt"
 
-port_path=$(realpath $(dirname ${BASH_SOURCE[0]}))
-
-build() {
-    return
+configure() {
+    run cmake $configopts .
 }
 
 install() {
-    cmake_dir="${SERENITY_BUILD_DIR}/Root/home/anon/Source/cmake"
-    run rm -rf "$cmake_dir"
-    run mkdir -p "$cmake_dir"
-    run cp -r . "$cmake_dir"
-}
-
-post_install() {
-    echo +===================================================================
-    echo "| Successfully prepared the sources for cmake v$version!"
-    echo "| The other half of the install has to be done inside serenity"
-    echo "| to continue, re-image and run the vm, then do the following:"
-    echo '| '
-    echo '| $ cd Source/cmake'
-    echo '| $ ./bootstrap'
-    echo '| $ make && make install'
-    echo '| '
-    echo "| (These instructions are also available in $port_path/howto.md)"
-    echo +===================================================================
+    run make install
 }
