4104846bd1bf440eb6790401f31058140001e6cf
Gunnar Beutner <gunnar@beutner.name>
2021-04-17 21:42:17 +0200

    Ports: Fix dependency and linker options for SDL2_image

--- Ports/SDL2_image/package.sh
+++ Ports/SDL2_image/package.sh
@@ -2,7 +2,7 @@
 port=SDL2_image
 useconfigure=true
 version=2.0.5
-depends="SDL2 libpng libjpeg"
+depends="SDL2 libpng libjpeg libtiff"
 files="https://www.libsdl.org/projects/SDL_image/release/SDL2_image-${version}.tar.gz SDL_image-${version}.tar.gz f26f3a153360a8f09ed5220ef7b07aea"
 auth_type=md5
 
@@ -20,6 +20,6 @@ build() {
 
 install() {
     run make -k DESTDIR="${SERENITY_BUILD_DIR}/Root" install
-    ${CC} -shared -o $DESTDIR/usr/local/lib/libSDL2_image.so -Wl,--whole-archive $DESTDIR/usr/local/lib/libSDL2_image.a -Wl,--no-whole-archive
+    ${CC} -shared -o $DESTDIR/usr/local/lib/libSDL2_image.so -Wl,--whole-archive $DESTDIR/usr/local/lib/libSDL2_image.a -Wl,--no-whole-archive -lpng -ljpeg -ltiff
     rm -f $DESTDIR/usr/local/lib/libSDL2_image.la
 }
