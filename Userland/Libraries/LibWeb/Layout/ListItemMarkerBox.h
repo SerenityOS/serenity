1e86d2bcbb61fdb7aa6bd0392f72d893ae59bd85
Tobias Christiansen <tobi@tobyase.de>
2021-04-17 23:10:10 +0200

    LibWeb: Make the ListItemMarkerBox index-aware.
    In the ListItemBox we get the index of the current <li> element in the
    parent and pass it to the ListItemMarkerBox.
    
    This patch is work towards #2059

--- Userland/Libraries/LibWeb/Layout/ListItemMarkerBox.h
+++ Userland/Libraries/LibWeb/Layout/ListItemMarkerBox.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2021, Tobias Christiansen <tobi@tobyase.de>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -32,10 +33,14 @@ namespace Web::Layout {
 
 class ListItemMarkerBox final : public Box {
 public:
-    explicit ListItemMarkerBox(DOM::Document&);
+    explicit ListItemMarkerBox(DOM::Document&, CSS::ListStyleType, size_t index);
     virtual ~ListItemMarkerBox() override;
 
     virtual void paint(PaintContext&, PaintPhase) override;
+
+private:
+    CSS::ListStyleType m_list_style_type { CSS::ListStyleType::None };
+    size_t m_index;
 };
 
 }
