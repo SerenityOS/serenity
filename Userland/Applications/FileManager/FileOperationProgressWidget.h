c8c364c94eb397d98b62856f8047c0eda1ee59d0
Leandro Pereira <leandro@hardinfo.org>
2021-04-16 18:41:44 -0700

    FileManager: Estimate transfer time
    Use the total bytes transferred count to estimate the time left
    for the copy operation to finish.  With the estimate label, the
    two progress bars were deemed superfluous, so the only remaining
    progress bar is the overall copy progress, that is updated more
    frequently.  (The same progress is also shown in the task bar,
    so you can minimize the window and still be informed of the
    progress.)

--- Userland/Applications/FileManager/FileOperationProgressWidget.h
+++ Userland/Applications/FileManager/FileOperationProgressWidget.h
@@ -26,6 +26,7 @@
 
 #pragma once
 
+#include <LibCore/ElapsedTimer.h>
 #include <LibGUI/Widget.h>
 
 namespace FileManager {
@@ -44,6 +45,9 @@ private:
     void did_progress(off_t bytes_done, off_t total_byte_count, size_t files_done, size_t total_file_count, off_t current_file_done, off_t current_file_size, const StringView& current_file_name);
 
     void close_pipe();
+
+    String estimate_time(off_t bytes_done, off_t total_byte_count);
+    Core::ElapsedTimer m_elapsed_timer;
 
     RefPtr<Core::Notifier> m_notifier;
     RefPtr<Core::File> m_helper_pipe;
