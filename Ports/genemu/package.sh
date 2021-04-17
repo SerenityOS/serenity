e1db0640fe09d7b05a03fcc6843fc8fd2241ff66
Brendan Coles <bcoles@gmail.com>
2021-04-17 17:16:24 +0000

    Ports: genemu: Pin version to latest release rather than use master branch

--- Ports/genemu/package.sh
+++ Ports/genemu/package.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-
 port="genemu"
-version=git
-workdir="${port}-master"
+version=3bf6f7cd893db3451019d6e18a2d9ad1de0e7c8c
 useconfigure=true
-files="https://github.com/rasky/genemu/archive/master.tar.gz ${version}.tar.gz a7b9f896c1fd99da03767068493ec89f"
+files="https://github.com/rasky/genemu/archive/${version}.tar.gz genemu-${version}.tar.gz 5704a21341ea56d026601e48e08f4605"
 auth_type=md5
 configopts="-DCMAKE_TOOLCHAIN_FILE=${SERENITY_ROOT}/Toolchain/CMake/CMakeToolchain.txt"
 depends="SDL2"
