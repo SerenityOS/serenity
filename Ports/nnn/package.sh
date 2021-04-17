1b5d45ccafc6fbb7a7ee685d4a5110101418d48b
Panagiotis Vasilopoulos <hello@alwayslivid.com>
2021-04-17 16:36:38 +0300

    Ports: Add nnn port

---
+++ Ports/nnn/package.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port=nnn
+version=4.0
+useconfigure=false
+workdir=nnn-4.0
+files="https://github.com/jarun/nnn/archive/v${version}/nnn-${version}.tar.gz nnn-v4.0.tar.gz c21421963ab6b6255a560e2a2c410468"
+auth_type=md5
