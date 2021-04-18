4d577ba07e2bf5cbe58dabfacb7c583984dc45dc
Xeriab Nabil <kodeburner@gmail.com>
2021-04-18 03:33:51 +0300

    Kernel: Reject execution of /usr/lib/Loader.so dynamic loader

--- Kernel/Syscalls/execve.cpp
+++ Kernel/Syscalls/execve.cpp
@@ -792,9 +792,12 @@ KResultOr<RefPtr<FileDescription>> Process::find_elf_interpreter_for_executable(
     if (main_program_header.e_type == ET_DYN) {
         // If it's ET_DYN with no PT_INTERP, then it's a dynamic executable responsible
         // for its own relocation (i.e. it's /usr/lib/Loader.so)
-        if (path != "/usr/lib/Loader.so")
-            dbgln("exec({}): WARNING - Dynamic ELF executable without a PT_INTERP header, and isn't /usr/lib/Loader.so", path);
-        return nullptr;
+        if (path != "/usr/lib/Loader.so") {
+            dmesgln("exec({}): WARNING - Dynamic ELF executable without a PT_INTERP header, and isn't /usr/lib/Loader.so", path);
+        } else {
+            dbgln("exec(={}): WARNING - Hold on, you cannot execute the loader!", path);
+        }
+        return ENOEXEC;
     }
 
     // No interpreter, but, path refers to a valid elf image
