bd6b559930191e6f1259b2c29280466a0cebc25e
Panagiotis Vasilopoulos <hello@alwayslivid.com>
2021-04-18 12:38:23 +0300

    Ports: Fix up imgcat

--- Ports/imgcat/package.sh
+++ Ports/imgcat/package.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=imgcat
 version=2.5.0
-useconfigure=false
-files="https://github.com/eddieantonio/imgcat/releases/download/v${version}/imgcat-${version}.tar.gz imgcat-v${version}.tar.gz 1b5d45ccafc6fbb7a7ee685d4a5110101418d48b"
+depends="ncurses"
+files="https://github.com/eddieantonio/imgcat/releases/download/v${version}/imgcat-${version}.tar.gz imgcat-v${version}.tar.gz 16e5386f5ce2237643785e7c8bb2cfce"
 auth_type=md5
 
-depends="ncurses"
+build() {
+    run make \
+        production=true
+}
+
+export CPPFLAGS="-I${SERENITY_INSTALL_ROOT}/usr/local/include/ncurses"
