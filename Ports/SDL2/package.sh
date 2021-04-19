cd56be4b91c6f32037062e96f763312af453eb8e
Gunnar Beutner <gunnar@beutner.name>
2021-04-19 21:49:13 +0200

    Ports: Remove auth_type for our own packages
    Otherwise we'd have to update them in this repository
    every time something changes.

--- Ports/SDL2/package.sh
+++ Ports/SDL2/package.sh
@@ -3,8 +3,7 @@ port=SDL2
 version=serenity-git
 workdir=SDL-main-serenity
 useconfigure=true
-files="https://github.com/SerenityOS/SDL/archive/main-serenity.tar.gz SDL2-git.tar.gz 18ce496be8644b0eb7fc4cad0d8dd5ff"
-auth_type=md5
+files="https://github.com/SerenityOS/SDL/archive/main-serenity.tar.gz SDL2-git.tar.gz"
 configopts="-DCMAKE_TOOLCHAIN_FILE=$SERENITY_SOURCE_DIR/Toolchain/CMake/CMakeToolchain.txt -DPULSEAUDIO=OFF -DJACK=OFF"
 
 configure() {
--- Ports/quake/package.sh
+++ Ports/quake/package.sh
@@ -3,8 +3,7 @@ port=quake
 version=0.65
 workdir=SerenityQuake-master
 useconfigure=false
-files="https://github.com/SerenityOS/SerenityQuake/archive/master.tar.gz quake.tar.gz 5d981f66accc580f80d20aee260e1f27"
-auth_type=md5
+files="https://github.com/SerenityOS/SerenityQuake/archive/master.tar.gz quake.tar.gz"
 makeopts="V=1 SYMBOLS_ON=Y "
 depends=SDL2
 
