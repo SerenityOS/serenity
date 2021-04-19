b9cedfbb6dd8a769f9b24250ab7e9ea7877cac85
Gunnar Beutner <gunnar@beutner.name>
2021-04-19 21:49:13 +0200

    Ports: Remove auth_type for our own packages
    Otherwise we'd have to update them in this repository
    every time something changes.

--- Ports/nesalizer/package.sh
+++ Ports/nesalizer/package.sh
@@ -2,6 +2,5 @@
 port=nesalizer
 version=master
 makeopts="CONF=release EXTRA=-I${SERENITY_SOURCE_DIR}/Build/i686/Root/usr/local/include/SDL2"
-files="https://github.com/SerenityOS/nesalizer/archive/master.zip nesalizer-master.zip 0bbcde24c2366ced827ad63935f557cf"
-auth_type=md5
+files="https://github.com/SerenityOS/nesalizer/archive/master.zip nesalizer-master.zip"
 depends=SDL2
