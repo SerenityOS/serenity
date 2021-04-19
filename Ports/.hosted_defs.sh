cc0c8347aa5f068ad78981e5f51d14d374af7a6e
Panagiotis Vasilopoulos <hello@alwayslivid.com>
2021-04-19 20:43:54 +0000

    Ports: Improve variable names in .hosted_defs.sh
    - SERENITY_ROOT is being kept around for compatibility reasons, and will
      be removed gradually
    - SERENITY_INSTALL_ROOT points to DESTDIR but will be preferred over
      that in the future
    - SERENITY_SOURCE_DIR points to the root folder of the repository. Let's
      keep the root terminology in the directory structure sort of sense out
      of here

--- Ports/.hosted_defs.sh
+++ Ports/.hosted_defs.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
-export SERENITY_ROOT="$(realpath "${SCRIPT}/../")"
-export SERENITY_BUILD_DIR="${SERENITY_ROOT}/Build/${SERENITY_ARCH}"
+export SERENITY_SOURCE_DIR="$(realpath "${SCRIPT}/../")"
+export SERENITY_BUILD_DIR="${SERENITY_SOURCE_DIR}/Build/${SERENITY_ARCH}"
 export CC="${SERENITY_ARCH}-pc-serenity-gcc"
 export CXX="${SERENITY_ARCH}-pc-serenity-g++"
 export AR="${SERENITY_ARCH}-pc-serenity-ar"
 export RANLIB="${SERENITY_ARCH}-pc-serenity-ranlib"
-export PATH="${SERENITY_ROOT}/Toolchain/Local/${SERENITY_ARCH}/bin:${PATH}"
+export PATH="${SERENITY_SOURCE_DIR}/Toolchain/Local/${SERENITY_ARCH}/bin:${PATH}"
 export PKG_CONFIG_DIR=""
 export PKG_CONFIG_SYSROOT_DIR="${SERENITY_BUILD_DIR}/Root"
 export PKG_CONFIG_LIBDIR="${PKG_CONFIG_SYSROOT_DIR}/usr/lib/pkgconfig/:${PKG_CONFIG_SYSROOT_DIR}/usr/local/lib/pkgconfig"
+
+# To be removed.
+export SERENITY_ROOT="$(realpath "${SCRIPT}/../")"
+
 enable_ccache
 
 DESTDIR="${SERENITY_BUILD_DIR}/Root"
+export SERENITY_INSTALL_ROOT="$DESTDIR"
