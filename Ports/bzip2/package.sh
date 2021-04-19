7bd25d4d7871c2a5236f5d1ed4b1c1cd193e6d25
Gunnar Beutner <gunnar@beutner.name>
2021-04-19 16:19:19 +0200

    Ports: Fix ports when building with ccache
    When building with ccache these ports failed to build because
    CC contains more than one word.
    
    The ncurses port also doesn't like how ccache preprocesses
    files. This patch fixes that.

--- Ports/bzip2/package.sh
+++ Ports/bzip2/package.sh
@@ -3,5 +3,13 @@ port=bzip2
 version=1.0.8
 files="https://sourceware.org/pub/bzip2/bzip2-${version}.tar.gz bzip2-${version}.tar.gz 67e051268d0c475ea773822f7500d0e5"
 auth_type=md5
-makeopts="bzip2 CC=${CC}"
+makeopts=bzip2
 installopts="PREFIX=${SERENITY_BUILD_DIR}/Root/usr/local"
+
+build() {
+    run make CC="${CC}" $makeopts bzip2
+}
+
+install() {
+    run make DESTDIR=$DESTDIR CC="${CC}" $installopts install
+}
