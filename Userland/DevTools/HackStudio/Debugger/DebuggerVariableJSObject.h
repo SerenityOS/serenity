271e53be09567de19595d78a91ac8d4e08e0e8f6
FalseHonesty <thefalsehonesty@gmail.com>
2021-04-20 18:59:53 -0400

    HackStudio: Display variable type name in expression evaluator

--- Userland/DevTools/HackStudio/Debugger/DebuggerVariableJSObject.h
+++ Userland/DevTools/HackStudio/Debugger/DebuggerVariableJSObject.h
@@ -14,13 +14,15 @@
 namespace HackStudio {
 
 class DebuggerVariableJSObject final : public JS::Object {
-    JS_OBJECT(DebuggerVariableJSObject, JS::Object);
+    using Base = JS::Object;
 
 public:
     static DebuggerVariableJSObject* create(DebuggerGlobalJSObject&, const Debug::DebugInfo::VariableInfo& variable_info);
 
     DebuggerVariableJSObject(const Debug::DebugInfo::VariableInfo& variable_info, JS::Object& prototype);
     virtual ~DebuggerVariableJSObject() override;
+
+    virtual const char* class_name() const override { return m_variable_info.type_name.characters(); }
 
     virtual bool put(const JS::PropertyName& name, JS::Value value, JS::Value) override;
     void finish_writing_properties() { m_is_writing_properties = false; }
