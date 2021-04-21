3aae30dd7bfdaa4b903e10d7eea1fd7e5f60f12f
Brian Gianforcaro <b.gianfo@gmail.com>
2021-04-21 02:11:04 -0700

    LibC: Add pwrite(..) implementation to match the existing pread(..)
    Add a basic non-thread safe implementation of pwrite, following the
    existing pread(..) design.
    
    This is needed for https://fio.readthedocs.io

--- Userland/Libraries/LibC/unistd.h
+++ Userland/Libraries/LibC/unistd.h
@@ -101,6 +101,7 @@ int tcsetpgrp(int fd, pid_t pgid);
 ssize_t read(int fd, void* buf, size_t count);
 ssize_t pread(int fd, void* buf, size_t count, off_t);
 ssize_t write(int fd, const void* buf, size_t count);
+ssize_t pwrite(int fd, const void* buf, size_t count, off_t);
 int close(int fd);
 int chdir(const char* path);
 int fchdir(int fd);
