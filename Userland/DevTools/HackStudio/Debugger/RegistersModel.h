31364da5901000b478412f516014e52b931bd0f2
Luke <luke.wilde@live.co.uk>
2021-04-22 22:12:55 +0100

    LibWeb+HackStudio: Use lukew@serenityos.org for my copyright headers

--- Userland/DevTools/HackStudio/Debugger/RegistersModel.h
+++ Userland/DevTools/HackStudio/Debugger/RegistersModel.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Luke Wilde <luke.wilde@live.co.uk>
+ * Copyright (c) 2020, Luke Wilde <lukew@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
--- Userland/Libraries/LibWeb/DOM/ElementFactory.cpp
+++ Userland/Libraries/LibWeb/DOM/ElementFactory.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2018-2021, Andreas Kling <kling@serenityos.org>
- * Copyright (c) 2020, Luke Wilde <luke.wilde@live.co.uk>
+ * Copyright (c) 2020, Luke Wilde <lukew@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
