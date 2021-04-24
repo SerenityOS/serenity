5b794f19ee0965c0c2890e7a25de7aa783b78636
Jesse Buhagiar <jooster669@gmail.com>
2021-04-24 22:43:42 +1000

    LibGL: Impement glLoadMatrixf and underlying function

--- Userland/Libraries/LibGL/GLMat.cpp
+++ Userland/Libraries/LibGL/GLMat.cpp
@@ -35,6 +35,17 @@ void glPopMatrix()
     g_gl_context->gl_pop_matrix();
 }
 
+void glLoadMatrixf(const GLfloat* matrix)
+{
+    FloatMatrix4x4 mat(
+        matrix[0], matrix[1], matrix[2], matrix[3],
+        matrix[4], matrix[5], matrix[6], matrix[7],
+        matrix[8], matrix[9], matrix[10], matrix[11],
+        matrix[12], matrix[13], matrix[14], matrix[15]);
+
+    g_gl_context->gl_load_matrix(mat);
+}
+
 void glLoadIdentity()
 {
     g_gl_context->gl_load_identity();
