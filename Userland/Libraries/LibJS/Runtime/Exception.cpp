c2a024749cc57fc6ab4cf266d206085466cf5af8
Linus Groh <mail@linusgroh.de>
2021-04-24 20:16:31 +0200

    LibJS: Use linusg@serenityos.org for my new copyright headers, too
    Whoops, I have a new email for these! :^)

--- Userland/Libraries/LibJS/Runtime/Exception.cpp
+++ Userland/Libraries/LibJS/Runtime/Exception.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2020, Andreas Kling <kling@serenityos.org>
- * Copyright (c) 2021, Linus Groh <mail@linusgroh.de>
+ * Copyright (c) 2021, Linus Groh <linusg@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
