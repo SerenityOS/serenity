b18509b04f6e6cbab658199762b7f76b565b90d2
Jean-Baptiste Boric <jblbeurope@gmail.com>
2021-04-25 21:10:55 +0200

    Userland: Move non-standard math constants from math.h

---
+++ AK/ExtraMathConstants.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021, the SerenityOS developers.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#define M_TAU 6.28318530717958647692
+#define M_DEG2RAD 0.01745329251994329576
+#define M_RAD2DEG 57.2957795130823208767
