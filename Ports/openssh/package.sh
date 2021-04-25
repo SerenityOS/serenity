ed6c8f29da4ba1af72a93bf7ec5642e143302aa6
Gunnar Beutner <gunnar@beutner.name>
2021-04-25 07:22:35 +0200

    Ports: Fix building openssh
    This fixes a spelling mistake in the timespeccmp() macro and
    enables debug symbols for OpenSSH to make diagnosing problems
    easier.

--- Ports/openssh/package.sh
+++ Ports/openssh/package.sh
@@ -6,7 +6,7 @@ files="https://github.com/openssh/openssh-portable/archive/9ca7e9c861775dd6c6312
 auth_type=md5
 depends="zlib openssl"
 useconfigure=true
-configopts="--prefix=/usr/local --disable-utmp --sysconfdir=/etc/ssh --with-ssl-dir=${SERENITY_INSTALL_ROOT}/usr/local/lib"
+configopts="--prefix=/usr/local --disable-utmp --disable-strip --sysconfdir=/etc/ssh --with-ssl-dir=${SERENITY_INSTALL_ROOT}/usr/local/lib"
 
 pre_configure() {
     run autoreconf
