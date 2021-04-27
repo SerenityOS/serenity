d4601f149b1a750003a0846af2ffb9acf92afc53
Jelle Raaijmakers <jelle@gmta.nl>
2021-04-27 22:01:15 +0200

    Ports/scummvm: Add libtheora as a dependency

--- Ports/scummvm/package.sh
+++ Ports/scummvm/package.sh
@@ -4,7 +4,7 @@ useconfigure="true"
 version="2.2.0"
 files="https://downloads.scummvm.org/frs/scummvm/${version}/scummvm-${version}.tar.gz scummvm-${version}.tar.gz 6ec5bd63b73861c10ca9869f27a74989a9ad6013bad30a1ef70de6ec146c2cb5"
 auth_type=sha256
-depends="SDL2"
+depends="libtheora SDL2"
 configopts="
     --enable-c++11
     --enable-release-mode
