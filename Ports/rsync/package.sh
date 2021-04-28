80cdcc7e51eb98551f961b49d10973043e11c132
Gunnar Beutner <gbeutner@serenityos.org>
2021-04-29 01:10:44 +0200

    Ports: Remove unused signature file for the rsync port

--- Ports/rsync/package.sh
+++ Ports/rsync/package.sh
@@ -2,7 +2,6 @@
 port=rsync
 version=3.1.3
 useconfigure="true"
-files="https://download.samba.org/pub/rsync/src/rsync-${version}.tar.gz rsync-${version}.tar.gz 55cc554efec5fdaad70de921cd5a5eeb6c29a95524c715f3bbf849235b0800c0
-https://download.samba.org/pub/rsync/src/rsync-${version}.tar.gz.asc rsync-${version}.tar.gz.asc 5cde11b63857d647f6cb9850f76c52cb"
+files="https://download.samba.org/pub/rsync/src/rsync-${version}.tar.gz rsync-${version}.tar.gz 55cc554efec5fdaad70de921cd5a5eeb6c29a95524c715f3bbf849235b0800c0"
 auth_type=sha256
 configopts="--target=${SERENITY_ARCH}-pc-serenity"
