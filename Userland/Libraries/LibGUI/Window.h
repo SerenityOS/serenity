466747df80fde3f45e34393c81b10a8b33e85871
Andreas Kling <kling@serenityos.org>
2021-05-01 18:22:29 +0200

    LibGUI: Add getter/setter for GUI::Window modified state
    This state lives in WindowServer and has no local copy in the client
    process for now. This may turn out to be a performance issue, and if
    it does we can easily cache it.

--- Userland/Libraries/LibGUI/Window.h
+++ Userland/Libraries/LibGUI/Window.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2018-2021, Andreas Kling <kling@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -28,6 +28,9 @@ public:
     virtual ~Window() override;
 
     static Window* from_window_id(int);
+
+    bool is_modified() const;
+    void set_modified(bool);
 
     bool is_modal() const { return m_modal; }
     void set_modal(bool);
