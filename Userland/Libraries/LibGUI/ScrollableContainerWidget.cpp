df67827cd2b6e501785162d0ff03443732d9225c
Andreas Kling <kling@serenityos.org>
2021-05-05 22:15:09 +0200

    LibGUI: Add ScrollableContainerWidget :^)
    This widget provides a scrollable view onto another (child) widget.
    If the child is larger than the parent, scrollbars are provided for
    panning around the child.

---
+++ Userland/Libraries/LibGUI/ScrollableContainerWidget.cpp
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2021, Andreas Kling <kling@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibGUI/Layout.h>
+#include <LibGUI/ScrollableContainerWidget.h>
+
+namespace GUI {
+
+ScrollableContainerWidget::ScrollableContainerWidget()
+{
+}
+
+ScrollableContainerWidget::~ScrollableContainerWidget()
+{
+}
+
+void ScrollableContainerWidget::did_scroll()
+{
+    AbstractScrollableWidget::did_scroll();
+    update_widget_position();
+}
+
+void ScrollableContainerWidget::update_widget_position()
+{
+    if (!m_widget)
+        return;
+    m_widget->move_to(-horizontal_scrollbar().value(), -vertical_scrollbar().value());
+}
+
+void ScrollableContainerWidget::update_widget_size()
+{
+    if (!m_widget)
+        return;
+    m_widget->do_layout();
+    if (m_widget->is_shrink_to_fit() && m_widget->layout()) {
+        auto new_size = frame_inner_rect().size();
+        auto preferred_size = m_widget->layout()->preferred_size();
+        if (preferred_size.width() != -1)
+            new_size.set_width(preferred_size.width());
+        if (preferred_size.height() != -1)
+            new_size.set_height(preferred_size.height());
+        m_widget->resize(new_size);
+        set_content_size(new_size);
+    }
+}
+
+void ScrollableContainerWidget::resize_event(GUI::ResizeEvent& event)
+{
+    AbstractScrollableWidget::resize_event(event);
+    update_widget_size();
+}
+
+void ScrollableContainerWidget::set_widget(GUI::Widget* widget)
+{
+    if (m_widget == widget)
+        return;
+
+    if (m_widget)
+        remove_child(*m_widget);
+
+    m_widget = widget;
+
+    if (m_widget) {
+        add_child(*m_widget);
+        m_widget->move_to_back();
+    }
+    update_widget_size();
+    update_widget_position();
+}
+
+}
---
+++ Userland/Libraries/LibGUI/ScrollableContainerWidget.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2021, Andreas Kling <kling@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibGUI/AbstractScrollableWidget.h>
+
+namespace GUI {
+
+class ScrollableContainerWidget : public GUI::AbstractScrollableWidget {
+    C_OBJECT(ScrollableContainerWidget);
+
+public:
+    virtual ~ScrollableContainerWidget();
+
+    void set_widget(GUI::Widget*);
+    GUI::Widget* widget() { return m_widget; }
+    GUI::Widget const* widget() const { return m_widget; }
+
+protected:
+    virtual void did_scroll() override;
+    virtual void resize_event(GUI::ResizeEvent&) override;
+
+private:
+    void update_widget_size();
+    void update_widget_position();
+
+    ScrollableContainerWidget();
+
+    RefPtr<GUI::Widget> m_widget;
+};
+
+}
