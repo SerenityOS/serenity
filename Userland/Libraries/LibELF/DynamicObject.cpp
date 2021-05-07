babb3a061a68f8eb2100992aacdcc7b5431e5b31
Andreas Kling <kling@serenityos.org>
2021-05-07 20:45:01 +0200

    LibELF: Convert StringBuilder::appendf() => AK::Format

--- Userland/Libraries/LibELF/DynamicObject.cpp
+++ Userland/Libraries/LibELF/DynamicObject.cpp
@@ -47,7 +47,7 @@ void DynamicObject::dump() const
 
     for_each_dynamic_entry([&](const DynamicObject::DynamicEntry& entry) {
         String name_field = String::formatted("({})", name_for_dtag(entry.tag()));
-        builder.appendf("0x%08X %-17s0x%X\n", entry.tag(), name_field.characters(), entry.val());
+        builder.appendff("{:#08x} {:17} {:#08x}\n", entry.tag(), name_field, entry.val());
         num_dynamic_sections++;
         return IterationDecision::Continue;
     });
