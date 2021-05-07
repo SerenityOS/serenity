295cc123c75def90e2ae2e8989d05082eb0e432c
Andreas Kling <kling@serenityos.org>
2021-05-07 23:33:31 +0200

    LibGUI: Rename UndoStack internals
    Since we keep a stack of command combos, let's call entries on the
    stack "Combo" instead of "UndoCommandsContainer".
    
    And since it has a vector of commands, let's call it "commands"
    instead of "m_undo_vector".

--- Userland/Libraries/LibGUI/UndoStack.h
+++ Userland/Libraries/LibGUI/UndoStack.h
@@ -19,7 +19,7 @@ public:
     void push(NonnullOwnPtr<Command>&&);
 
     bool can_undo() const { return m_stack_index < m_stack.size() && !m_stack.is_empty(); }
-    bool can_redo() const { return m_stack_index > 0 && !m_stack.is_empty() && m_stack[m_stack_index - 1].m_undo_vector.size() > 0; }
+    bool can_redo() const { return m_stack_index > 0 && !m_stack.is_empty() && m_stack[m_stack_index - 1].commands.size() > 0; }
 
     void undo();
     void redo();
@@ -32,11 +32,11 @@ public:
     void clear();
 
 private:
-    struct UndoCommandsContainer {
-        NonnullOwnPtrVector<Command> m_undo_vector;
+    struct Combo {
+        NonnullOwnPtrVector<Command> commands;
     };
 
-    NonnullOwnPtrVector<UndoCommandsContainer> m_stack;
+    NonnullOwnPtrVector<Combo> m_stack;
     size_t m_stack_index { 0 };
     Optional<size_t> m_clean_index;
 };
