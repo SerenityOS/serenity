5b87276841f5dd7d29e2e02fff5540dabc40bfa8
Andreas Kling <kling@serenityos.org>
2021-05-07 11:58:21 +0200

    LibDiff: Convert StringBuilder::appendf() => AK::Format

--- Userland/Libraries/LibDiff/Format.cpp
+++ Userland/Libraries/LibDiff/Format.cpp
@@ -7,16 +7,15 @@
 #include "Format.h"
 #include <AK/String.h>
 #include <AK/StringBuilder.h>
-#include <AK/Vector.h>
 
 namespace Diff {
 String generate_only_additions(const String& text)
 {
     auto lines = text.split('\n', true); // Keep empty
     StringBuilder builder;
-    builder.appendf("@@ -0,0 +1,%zu @@\n", lines.size());
+    builder.appendff("@@ -0,0 +1,{} @@\n", lines.size());
     for (const auto& line : lines) {
-        builder.appendf("+%s\n", line.characters());
+        builder.appendff("+{}\n", line);
     }
     return builder.to_string();
 }
