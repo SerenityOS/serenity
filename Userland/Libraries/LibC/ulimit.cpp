70c6f5b7e84c612ebb4c0d7ff703d109e6e371f8
Gunnar Beutner <gbeutner@serenityos.org>
2021-05-08 05:04:56 +0200

    LibC: Add stubs for getrlimit()/setrlimit()

--- Userland/Libraries/LibC/ulimit.cpp
+++ Userland/Libraries/LibC/ulimit.cpp
@@ -23,4 +23,16 @@ int getrusage([[maybe_unused]] int who, [[maybe_unused]] struct rusage* usage)
     dbgln("FIXME: Implement getrusage()");
     return -1;
 }
+
+int getrlimit([[maybe_unused]] int resource, rlimit* rl)
+{
+    rl->rlim_cur = RLIM_INFINITY;
+    rl->rlim_max = RLIM_INFINITY;
+    return 0;
+}
+
+int setrlimit([[maybe_unused]] int resource, [[maybe_unused]] rlimit const* rl)
+{
+    return 0;
+}
 }
