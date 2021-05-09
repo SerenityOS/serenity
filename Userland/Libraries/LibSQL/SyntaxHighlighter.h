e0c1df5b5cf3a61742b02b2279e7a172cf9daeb3
Dylan Katz <dykatz@uw.edu>
2021-05-08 18:30:18 -0700

    LibSQL: Add a syntax highlighter

---
+++ Userland/Libraries/LibSQL/SyntaxHighlighter.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021, Dylan Katz <dykatz@uw.edu>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibSyntax/Highlighter.h>
+
+namespace SQL {
+
+class SyntaxHighlighter final : public Syntax::Highlighter {
+public:
+    SyntaxHighlighter() { }
+    virtual ~SyntaxHighlighter() override;
+
+    virtual bool is_identifier(void*) const override;
+
+    virtual Syntax::Language language() const override { return Syntax::Language::SQL; }
+    virtual void rehighlight(const Palette&) override;
+
+protected:
+    virtual Vector<MatchingTokenPair> matching_token_pairs() const override;
+    virtual bool token_types_equal(void*, void*) const override;
+};
+
+}
