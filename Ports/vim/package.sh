55ed0bf9dc8698afbf9b0c55e2e862bf2b8fee91
Gunnar Beutner <gbeutner@serenityos.org>
2021-05-10 22:16:09 +0200

    Ports: Add missing dependency for vim
    The vim port links against gettext when it's available so we
    should add it as a dependency in the package.sh script.

--- Ports/vim/package.sh
+++ Ports/vim/package.sh
@@ -6,7 +6,7 @@ useconfigure="true"
 files="https://github.com/vim/vim/archive/refs/tags/v${version}.tar.gz vim-v${version}.tar.gz 47613400943bbf3e110c38e8c4923b9e51c1d63d9774313820e1d9b4c4bb9e11"
 auth_type=sha256
 configopts="--with-tlib=tinfo --with-features=normal"
-depends="ncurses"
+depends="ncurses gettext"
 
 export vim_cv_getcwd_broken=no
 export vim_cv_memmove_handles_overlap=yes
