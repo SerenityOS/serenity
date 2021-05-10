ae9e35210404e901d2ec9c936ee00fa7dafcbadb
sin-ack <sin-ack@users.noreply.github.com>
2021-05-10 09:29:41 +0000

    LibCore: Add unit to Core::Timer interval arguments
    This patch adds the _ms suffix to the interval arguments in Core::Timer,
    to make it a little clearer what the interval unit is to others.

--- Userland/Libraries/LibCore/Timer.cpp
+++ Userland/Libraries/LibCore/Timer.cpp
@@ -13,11 +13,11 @@ Timer::Timer(Object* parent)
 {
 }
 
-Timer::Timer(int interval, Function<void()>&& timeout_handler, Object* parent)
+Timer::Timer(int interval_ms, Function<void()>&& timeout_handler, Object* parent)
     : Object(parent)
     , on_timeout(move(timeout_handler))
 {
-    start(interval);
+    start(interval_ms);
 }
 
 Timer::~Timer()
@@ -26,28 +26,28 @@ Timer::~Timer()
 
 void Timer::start()
 {
-    start(m_interval);
+    start(m_interval_ms);
 }
 
-void Timer::start(int interval)
+void Timer::start(int interval_ms)
 {
     if (m_active)
         return;
-    m_interval = interval;
-    start_timer(interval);
+    m_interval_ms = interval_ms;
+    start_timer(interval_ms);
     m_active = true;
 }
 
 void Timer::restart()
 {
-    restart(m_interval);
+    restart(m_interval_ms);
 }
 
-void Timer::restart(int interval)
+void Timer::restart(int interval_ms)
 {
     if (m_active)
         stop();
-    start(interval);
+    start(interval_ms);
 }
 
 void Timer::stop()
@@ -65,7 +65,7 @@ void Timer::timer_event(TimerEvent&)
     else {
         if (m_interval_dirty) {
             stop();
-            start(m_interval);
+            start(m_interval_ms);
         }
     }
 
