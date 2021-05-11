6d39b792f0e890fc57a754664735a7bcdfa265e3
Brian Gianforcaro <bgianf@serenityos.org>
2021-05-11 03:51:31 -0700

    Kernel: Declare operator new/delete noexcept for MAKE_SLAB_ALLOCATED

--- Kernel/Heap/SlabAllocator.h
+++ Kernel/Heap/SlabAllocator.h
@@ -19,11 +19,11 @@ void slab_dealloc(void*, size_t slab_size);
 void slab_alloc_init();
 void slab_alloc_stats(Function<void(size_t slab_size, size_t allocated, size_t free)>);
 
-#define MAKE_SLAB_ALLOCATED(type)                                        \
-public:                                                                  \
-    void* operator new(size_t) { return slab_alloc(sizeof(type)); }      \
-    void operator delete(void* ptr) { slab_dealloc(ptr, sizeof(type)); } \
-                                                                         \
+#define MAKE_SLAB_ALLOCATED(type)                                                          \
+public:                                                                                    \
+    [[nodiscard]] void* operator new(size_t) noexcept { return slab_alloc(sizeof(type)); } \
+    void operator delete(void* ptr) noexcept { slab_dealloc(ptr, sizeof(type)); }          \
+                                                                                           \
 private:
 
 }
