9c9c4440481bf352f503ca32ed31459541667543
Linus Groh <mail@linusgroh.de>
2021-05-11 22:47:14 +0100

    LibJS: Bring back runtime validation of RegExp flags
    This is a partial revert of commit 60064e2, which removed the validation
    of RegExp flags during runtime and expected the parser to do that
    exclusively - however this was not taking into account the RegExp()
    constructor, which was subsequently crashing on invalid flags.
    
    Also adds test for these constructor error cases, which were obviously
    missing before.
    
    Fixes #7042.

--- Userland/Libraries/LibJS/Tests/builtins/RegExp/RegExp.js
+++ Userland/Libraries/LibJS/Tests/builtins/RegExp/RegExp.js
@@ -1,3 +1,26 @@
+describe("errors", () => {
+    test("invalid pattern", () => {
+        expect(() => {
+            RegExp("[");
+        }).toThrowWithMessage(
+            SyntaxError,
+            "RegExp compile error: Error during parsing of regular expression:"
+        );
+    });
+
+    test("invalid flag", () => {
+        expect(() => {
+            RegExp("", "x");
+        }).toThrowWithMessage(SyntaxError, "Invalid RegExp flag 'x'");
+    });
+
+    test("repeated flag", () => {
+        expect(() => {
+            RegExp("", "gg");
+        }).toThrowWithMessage(SyntaxError, "Repeated RegExp flag 'g'");
+    });
+});
+
 test("basic functionality", () => {
     expect(RegExp().toString()).toBe("/(?:)/");
     expect(RegExp(undefined).toString()).toBe("/(?:)/");
