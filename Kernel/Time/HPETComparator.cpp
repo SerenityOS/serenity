876bf0cebce4b4113643ae24455e3acdc54738b4
Tom <tomut@yahoo.com>
2021-05-11 21:20:06 -0600

    Kernel: Fix issues supporting HPETs with 32-bit-only main counter
    If the HPET main counter does not support full 64 bits, we should
    not expect the upper 32 bit to work. This is a problem when writing
    to the upper 32 bit of the comparator value, which requires the
    TimerConfiguration::ValueSet bit to be set, but if it's not 64 bit
    capable then the bit will not be cleared and leave it in a bad state.
    
    Fixes #6990

--- Kernel/Time/HPETComparator.cpp
+++ Kernel/Time/HPETComparator.cpp
@@ -11,18 +11,19 @@
 
 namespace Kernel {
 
-UNMAP_AFTER_INIT NonnullRefPtr<HPETComparator> HPETComparator::create(u8 number, u8 irq, bool periodic_capable)
+UNMAP_AFTER_INIT NonnullRefPtr<HPETComparator> HPETComparator::create(u8 number, u8 irq, bool periodic_capable, bool is_64bit_capable)
 {
-    auto timer = adopt_ref(*new HPETComparator(number, irq, periodic_capable));
+    auto timer = adopt_ref(*new HPETComparator(number, irq, periodic_capable, is_64bit_capable));
     timer->register_interrupt_handler();
     return timer;
 }
 
-UNMAP_AFTER_INIT HPETComparator::HPETComparator(u8 number, u8 irq, bool periodic_capable)
+UNMAP_AFTER_INIT HPETComparator::HPETComparator(u8 number, u8 irq, bool periodic_capable, bool is_64bit_capable)
     : HardwareTimer(irq)
     , m_periodic(false)
     , m_periodic_capable(periodic_capable)
     , m_enabled(false)
+    , m_is_64bit_capable(is_64bit_capable)
     , m_comparator_number(number)
 {
 }
