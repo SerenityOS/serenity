b89ead4c480f61e948cc3af42881b73d17e5b0ec
Andreas Kling <kling@serenityos.org>
2021-05-13 22:48:02 +0200

    Userland: Remove no-longer-needed unveil()'s of /tmp/rpc

--- Userland/Utilities/bt.cpp
+++ Userland/Utilities/bt.cpp
@@ -23,11 +23,6 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    if (unveil("/tmp/rpc", "crw") < 0) {
-        perror("unveil");
-        return 1;
-    }
-
     if (unveil("/tmp/portal/symbol", "rw") < 0) {
         perror("unveil");
         return 1;
