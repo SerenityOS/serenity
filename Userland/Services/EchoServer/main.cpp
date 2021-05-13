dd62e1316351faf703a15c11c25237e458dca593
Andreas Kling <kling@serenityos.org>
2021-05-13 22:48:02 +0200

    Userland: Remove no-longer-needed unveil()'s of /tmp/rpc

--- Userland/Services/EchoServer/main.cpp
+++ Userland/Services/EchoServer/main.cpp
@@ -21,11 +21,6 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    if (unveil("/tmp/rpc", "rwc") < 0) {
-        perror("unveil");
-        return 1;
-    }
-
     if (unveil(nullptr, nullptr) < 0) {
         perror("unveil");
         return 1;
