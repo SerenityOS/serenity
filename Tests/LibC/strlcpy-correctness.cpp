f6a56de970a7e638386be090ccf2671b715794ed
Gunnar Beutner <gbeutner@serenityos.org>
2021-05-14 20:53:04 +0200

    AK: Turn ByteBuffer into a value type
    Previously ByteBuffer would internally hold a RefPtr to the byte
    buffer and would behave like a reference type, i.e. copying a
    ByteBuffer would not create a duplicate byte buffer, but rather
    two objects which refer to the same internal buffer.
    
    This also changes ByteBuffer so that it has some internal capacity
    much like the Vector<T> type. Unlike Vector<T> however a byte
    buffer's data may be uninitialized.
    
    With this commit ByteBuffer makes use of the kmalloc_good_size()
    API to pick an optimal allocation size for its internal buffer.

--- Tests/LibC/strlcpy-correctness.cpp
+++ Tests/LibC/strlcpy-correctness.cpp
@@ -57,7 +57,7 @@ static bool test_single(const Testcase& testcase)
     // Setup
     ByteBuffer actual = ByteBuffer::create_uninitialized(SANDBOX_CANARY_SIZE + testcase.dest_n + SANDBOX_CANARY_SIZE);
     fill_with_random(actual.data(), actual.size());
-    ByteBuffer expected = actual.isolated_copy();
+    ByteBuffer expected = actual;
     VERIFY(actual.offset_pointer(0) != expected.offset_pointer(0));
     actual.overwrite(SANDBOX_CANARY_SIZE, testcase.dest, testcase.dest_n);
     expected.overwrite(SANDBOX_CANARY_SIZE, testcase.dest_expected, testcase.dest_expected_n);
