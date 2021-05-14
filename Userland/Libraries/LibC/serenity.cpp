d23d6b62aff2ac20196f300fe77f32884be103b0
Gunnar Beutner <gbeutner@serenityos.org>
2021-05-14 08:10:43 +0200

    Kernel+LibC: Add support for filtering profiling events
    This adds the -t command-line argument for the profile tool. Using this
    argument you can filter which event types you want in your profile.

--- Userland/Libraries/LibC/serenity.cpp
+++ Userland/Libraries/LibC/serenity.cpp
@@ -30,9 +30,9 @@ int module_unload(const char* name, size_t name_length)
     __RETURN_WITH_ERRNO(rc, rc, -1);
 }
 
-int profiling_enable(pid_t pid)
+int profiling_enable(pid_t pid, uint64_t event_mask)
 {
-    int rc = syscall(SC_profiling_enable, pid);
+    int rc = syscall(SC_profiling_enable, pid, event_mask);
     __RETURN_WITH_ERRNO(rc, rc, -1);
 }
 
