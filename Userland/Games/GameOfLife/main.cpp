85399fa9c1d6c681f3c93e5524d9d6fcd3a951e7
Linus Groh <mail@linusgroh.de>
2021-05-15 17:55:49 +0100

    GameOfLife: Spell about action app name as "Game Of Life"

--- Userland/Games/GameOfLife/main.cpp
+++ Userland/Games/GameOfLife/main.cpp
@@ -126,7 +126,7 @@ int main(int argc, char** argv)
     }));
 
     auto& help_menu = menubar->add_menu("&Help");
-    help_menu.add_action(GUI::CommonActions::make_about_action("GameOfLife", app_icon, window));
+    help_menu.add_action(GUI::CommonActions::make_about_action("Game Of Life", app_icon, window));
 
     window->set_menubar(move(menubar));
 
