0ec6c66d26b2fa496234dee9a1ac8f9ce3d60527
Andreas Kling <kling@serenityos.org>
2021-05-17 18:48:37 +0200

    Build: Stop using precompiled headers (PCH)
    This had very bad interactions with ccache, often leading to rebuilds
    with 100% cache misses, etc. Ali says it wasn't that big of a speedup
    in the end anyway, so let's not bother with it.
    
    We can always bring it back in the future if it seems like a good idea.

--- Meta/CMake/precompile-headers.cmake
+++
@@ -1,9 +0,0 @@
-function(serenity_add_precompiled_header_to_target target header)
-    if (PRECOMPILE_COMMON_HEADERS AND COMMAND target_precompile_headers)
-        target_precompile_headers(${target} PRIVATE ${header})
-    endif ()
-endfunction()
-
-function(serenity_add_ak_precompiled_headers_to_target target)
-    serenity_add_precompiled_header_to_target(${target} ${CMAKE_SOURCE_DIR}/Meta/Precompile/AK.h)
-endfunction()
