f02a13c8846af665cb41077fd97554187ac6baf8
Timothy Flynn <trflynn89@pm.me>
2021-05-18 08:47:52 -0400

    2048: Intialize settings window with current values
    Currently, each time you open the settings window in 2048, it displays
    the default values rather than the current values. This is confusing, so
    display the current values instead.

--- Userland/Games/2048/GameSizeDialog.cpp
+++ Userland/Games/2048/GameSizeDialog.cpp
@@ -11,9 +11,13 @@
 #include <LibGUI/CheckBox.h>
 #include <LibGUI/Label.h>
 #include <LibGUI/SpinBox.h>
+#include <math.h>
 
-GameSizeDialog::GameSizeDialog(GUI::Window* parent)
+GameSizeDialog::GameSizeDialog(GUI::Window* parent, size_t board_size, size_t target, bool evil_ai)
     : GUI::Dialog(parent)
+    , m_board_size(board_size)
+    , m_target_tile_power(log2(target))
+    , m_evil_ai(evil_ai)
 {
     set_rect({ 0, 0, 200, 150 });
     set_title("New Game");
