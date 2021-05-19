255fe6dae243de75a70910ae2375023950c0f608
Stephan Unverwerth <s.unverwerth@gmx.de>
2021-05-19 22:44:55 +0200

    LibGL: Implement all of glVertex{2,3,4}{d,dv,f,fv,i,iv,s,sv}

--- Userland/Libraries/LibGL/GLVert.cpp
+++ Userland/Libraries/LibGL/GLVert.cpp
@@ -20,6 +20,16 @@ void glEnd()
     g_gl_context->gl_end();
 }
 
+void glVertex2d(GLdouble x, GLdouble y)
+{
+    g_gl_context->gl_vertex(x, y, 0.0, 1.0);
+}
+
+void glVertex2dv(const GLdouble* v)
+{
+    g_gl_context->gl_vertex(v[0], v[1], 0.0, 1.0);
+}
+
 void glVertex2f(GLfloat x, GLfloat y)
 {
     g_gl_context->gl_vertex(x, y, 0.0, 1.0);
@@ -30,6 +40,36 @@ void glVertex2fv(const GLfloat* v)
     g_gl_context->gl_vertex(v[0], v[1], 0.0, 1.0);
 }
 
+void glVertex2i(GLint x, GLint y)
+{
+    g_gl_context->gl_vertex(x, y, 0.0, 1.0);
+}
+
+void glVertex2iv(const GLint* v)
+{
+    g_gl_context->gl_vertex(v[0], v[1], 0.0, 1.0);
+}
+
+void glVertex2s(GLshort x, GLshort y)
+{
+    g_gl_context->gl_vertex(x, y, 0.0, 1.0);
+}
+
+void glVertex2sv(const GLshort* v)
+{
+    g_gl_context->gl_vertex(v[0], v[1], 0.0, 1.0);
+}
+
+void glVertex3d(GLdouble x, GLdouble y, GLdouble z)
+{
+    g_gl_context->gl_vertex(x, y, z, 1.0);
+}
+
+void glVertex3dv(const GLdouble* v)
+{
+    g_gl_context->gl_vertex(v[0], v[1], v[2], 1.0);
+}
+
 void glVertex3f(GLfloat x, GLfloat y, GLfloat z)
 {
     g_gl_context->gl_vertex(x, y, z, 1.0);
@@ -38,6 +78,66 @@ void glVertex3f(GLfloat x, GLfloat y, GLfloat z)
 void glVertex3fv(const GLfloat* v)
 {
     g_gl_context->gl_vertex(v[0], v[1], v[2], 1.0);
+}
+
+void glVertex3i(GLint x, GLint y, GLint z)
+{
+    g_gl_context->gl_vertex(x, y, z, 1.0);
+}
+
+void glVertex3iv(const GLint* v)
+{
+    g_gl_context->gl_vertex(v[0], v[1], v[2], 1.0);
+}
+
+void glVertex3s(GLshort x, GLshort y, GLshort z)
+{
+    g_gl_context->gl_vertex(x, y, z, 1.0);
+}
+
+void glVertex3sv(const GLshort* v)
+{
+    g_gl_context->gl_vertex(v[0], v[1], v[2], 1.0);
+}
+
+void glVertex4d(GLdouble x, GLdouble y, GLdouble z, GLdouble w)
+{
+    g_gl_context->gl_vertex(x, y, z, w);
+}
+
+void glVertex4dv(const GLdouble* v)
+{
+    g_gl_context->gl_vertex(v[0], v[1], v[2], v[3]);
+}
+
+void glVertex4f(GLfloat x, GLfloat y, GLfloat z, GLfloat w)
+{
+    g_gl_context->gl_vertex(x, y, z, w);
+}
+
+void glVertex4fv(const GLfloat* v)
+{
+    g_gl_context->gl_vertex(v[0], v[1], v[2], v[3]);
+}
+
+void glVertex4i(GLint x, GLint y, GLint z, GLint w)
+{
+    g_gl_context->gl_vertex(x, y, z, w);
+}
+
+void glVertex4iv(const GLint* v)
+{
+    g_gl_context->gl_vertex(v[0], v[1], v[2], v[3]);
+}
+
+void glVertex4s(GLshort x, GLshort y, GLshort z, GLshort w)
+{
+    g_gl_context->gl_vertex(x, y, z, w);
+}
+
+void glVertex4sv(const GLshort* v)
+{
+    g_gl_context->gl_vertex(v[0], v[1], v[2], v[3]);
 }
 
 void glRotatef(GLfloat angle, GLfloat x, GLfloat y, GLfloat z)
