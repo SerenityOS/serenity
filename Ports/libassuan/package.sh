36001b49025b80e6b1bcda10b0a2192e6ac9602b
Gunnar Beutner <gbeutner@serenityos.org>
2021-05-19 21:46:22 +0200

    Ports: Build shared libraries for the GnuPG libraries
    This builds all the GnuPG libraries as shared libraries so that -lintl
    is linked when building the final GnuPG executable.

--- Ports/libassuan/package.sh
+++ Ports/libassuan/package.sh
@@ -2,15 +2,20 @@
 port=libassuan
 version=2.5.5
 useconfigure=true
-#configopts="--with-libgpg-error-prefix=${SERENITY_INSTALL_ROOT}/usr/local"
+depends="libgpg-error"
 files="https://gnupg.org/ftp/gcrypt/libassuan/libassuan-${version}.tar.bz2 libassuan-${version}.tar.bz2 8e8c2fcc982f9ca67dcbb1d95e2dc746b1739a4668bc20b3a3c5be632edb34e4"
 auth_type=sha256
 
 pre_configure() {
-    export gcry_cv_gcc_has_f_visibility=no
     export ac_cv_lib_pthread_pthread_create=no
 }
 
 configure() {
     run ./configure --host="${SERENITY_ARCH}-pc-serenity" --build="$($workdir/build-aux/config.guess)" $configopts
+}
+
+install() {
+    run make DESTDIR=${SERENITY_INSTALL_ROOT} $installopts install
+    ${CC} -shared -o ${SERENITY_INSTALL_ROOT}/usr/local/lib/libassuan.so -Wl,-soname,libassuan.so -Wl,--whole-archive ${SERENITY_INSTALL_ROOT}/usr/local/lib/libassuan.a -Wl,--no-whole-archive -lgpg-error
+    rm -f ${SERENITY_INSTALL_ROOT}/usr/local/lib/libassuan.la
 }
--- Ports/libgpg-error/package.sh
+++ Ports/libgpg-error/package.sh
@@ -2,14 +2,17 @@
 port=libgpg-error
 version=1.42
 useconfigure=true
+depends="gettext"
 configopts="--disable-tests --disable-threads"
 files="https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-${version}.tar.bz2 libgpg-error-${version}.tar.bz2 fc07e70f6c615f8c4f590a8e37a9b8dd2e2ca1e9408f8e60459c67452b925e23"
 auth_type=sha256
 
-pre_configure() {
-    export gcry_cv_gcc_has_f_visibility=no
-}
-
 configure() {
     run ./configure --host="${SERENITY_ARCH}-pc-serenity" --build="$($workdir/build-aux/config.guess)" $configopts
+}
+
+install() {
+    run make DESTDIR=${SERENITY_INSTALL_ROOT} $installopts install
+    ${CC} -shared -o ${SERENITY_INSTALL_ROOT}/usr/local/lib/libgpg-error.so -Wl,-soname,libgpg-error.so -Wl,--whole-archive ${SERENITY_INSTALL_ROOT}/usr/local/lib/libgpg-error.a -Wl,--no-whole-archive -lintl
+    rm -f ${SERENITY_INSTALL_ROOT}/usr/local/lib/libgpg-error.la
 }
