2eaa0232c751262c12d8a7f2af41936546d0e341
Idan Horowitz <idan.horowitz@gmail.com>
2021-05-22 02:38:44 +0300

    AK: Use calculate_base64_encoded_length in encode_base64
    We were accidentally calling calculate_base64_decoded_length instead,
    which resulted in extra allocations during the StringBuilder::append
    calls that can be avoided.

--- AK/Base64.cpp
+++ AK/Base64.cpp
@@ -96,7 +96,7 @@ ByteBuffer decode_base64(const StringView& input)
 String encode_base64(ReadonlyBytes input)
 {
     constexpr auto alphabet = make_alphabet();
-    StringBuilder output(calculate_base64_decoded_length(input));
+    StringBuilder output(calculate_base64_encoded_length(input));
 
     auto get = [&](const size_t offset, bool* need_padding = nullptr) -> u8 {
         if (offset >= input.size()) {
