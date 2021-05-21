5637220d0df8edeff1d47fe597f94d5b0854edde
Andreas Kling <kling@serenityos.org>
2021-05-21 18:59:07 +0200

    WindowServer: Store system font queries in WindowServer.ini :^)
    Changes to the system font settings are now persisted in /etc.
    Note that you still need to restart the system for changes to fully
    apply in all programs.

--- Userland/Services/WindowServer/main.cpp
+++ Userland/Services/WindowServer/main.cpp
@@ -62,8 +62,11 @@ int main(int, char**)
     Gfx::set_system_theme(theme);
     auto palette = Gfx::PaletteImpl::create_with_anonymous_buffer(theme);
 
-    Gfx::FontDatabase::set_default_font_query("Katica 10 400");
-    Gfx::FontDatabase::set_fixed_width_font_query("Csilla 10 400");
+    auto default_font_query = wm_config->read_entry("Fonts", "Default", "Katica 10 400");
+    auto fixed_width_font_query = wm_config->read_entry("Fonts", "FixedWidth", "Csilla 10 400");
+
+    Gfx::FontDatabase::set_default_font_query(default_font_query);
+    Gfx::FontDatabase::set_fixed_width_font_query(fixed_width_font_query);
 
     WindowServer::EventLoop loop;
 
