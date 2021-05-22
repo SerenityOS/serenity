35499e036a8f48b09190322e343d0608f86af8e0
Ali Mohammad Pur <ali.mpfard@gmail.com>
2021-05-22 23:33:26 +0430

    LibJS+LibWeb: Make Uint8ClampedArray use TypedArray
    Instead of being its own separate unrelated class.
    This automatically makes typed array properties available to it,
    as well as making it available to the runtime.

--- Userland/Libraries/LibJS/Forward.h
+++ Userland/Libraries/LibJS/Forward.h
@@ -54,14 +54,15 @@
     __JS_ENUMERATE(TypeError, type_error, TypeErrorPrototype, TypeErrorConstructor, void)                                                  \
     __JS_ENUMERATE(URIError, uri_error, URIErrorPrototype, URIErrorConstructor, void)
 
-#define JS_ENUMERATE_TYPED_ARRAYS                                                                      \
-    __JS_ENUMERATE(Uint8Array, uint8_array, Uint8ArrayPrototype, Uint8ArrayConstructor, u8)            \
-    __JS_ENUMERATE(Uint16Array, uint16_array, Uint16ArrayPrototype, Uint16ArrayConstructor, u16)       \
-    __JS_ENUMERATE(Uint32Array, uint32_array, Uint32ArrayPrototype, Uint32ArrayConstructor, u32)       \
-    __JS_ENUMERATE(Int8Array, int8_array, Int8ArrayPrototype, Int8ArrayConstructor, i8)                \
-    __JS_ENUMERATE(Int16Array, int16_array, Int16ArrayPrototype, Int16ArrayConstructor, i16)           \
-    __JS_ENUMERATE(Int32Array, int32_array, Int32ArrayPrototype, Int32ArrayConstructor, i32)           \
-    __JS_ENUMERATE(Float32Array, float32_array, Float32ArrayPrototype, Float32ArrayConstructor, float) \
+#define JS_ENUMERATE_TYPED_ARRAYS                                                                                               \
+    __JS_ENUMERATE(Uint8Array, uint8_array, Uint8ArrayPrototype, Uint8ArrayConstructor, u8)                                     \
+    __JS_ENUMERATE(Uint8ClampedArray, uint8_clamped_array, Uint8ClampedArrayPrototype, Uint8ClampedArrayConstructor, ClampedU8) \
+    __JS_ENUMERATE(Uint16Array, uint16_array, Uint16ArrayPrototype, Uint16ArrayConstructor, u16)                                \
+    __JS_ENUMERATE(Uint32Array, uint32_array, Uint32ArrayPrototype, Uint32ArrayConstructor, u32)                                \
+    __JS_ENUMERATE(Int8Array, int8_array, Int8ArrayPrototype, Int8ArrayConstructor, i8)                                         \
+    __JS_ENUMERATE(Int16Array, int16_array, Int16ArrayPrototype, Int16ArrayConstructor, i16)                                    \
+    __JS_ENUMERATE(Int32Array, int32_array, Int32ArrayPrototype, Int32ArrayConstructor, i32)                                    \
+    __JS_ENUMERATE(Float32Array, float32_array, Float32ArrayPrototype, Float32ArrayConstructor, float)                          \
     __JS_ENUMERATE(Float64Array, float64_array, Float64ArrayPrototype, Float64ArrayConstructor, double)
 
 #define JS_ENUMERATE_ITERATOR_PROTOTYPES          \
@@ -131,7 +132,6 @@ class Shape;
 class Statement;
 class Symbol;
 class Token;
-class Uint8ClampedArray;
 class VM;
 class Value;
 enum class DeclarationKind;
@@ -145,6 +145,9 @@ class ProxyConstructor;
 
 class TypedArrayConstructor;
 class TypedArrayPrototype;
+
+// Tag type used to differentiate between u8 as used by Uint8Array and u8 as used by Uint8ClampedArray.
+struct ClampedU8;
 
 #define __JS_ENUMERATE(ClassName, snake_name, ConstructorName, PrototypeName, ArrayType) \
     class ClassName;                                                                     \
