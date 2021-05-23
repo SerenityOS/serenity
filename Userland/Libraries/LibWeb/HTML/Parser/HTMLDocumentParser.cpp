c9f2bc41fb72986ade304dac26bb8e0e02908fc1
Gunnar Beutner <gbeutner@serenityos.org>
2021-05-23 08:20:03 +0200

    LibWeb: Remove StringBuilders from HTMLToken::m_doctype

--- Userland/Libraries/LibWeb/HTML/Parser/HTMLDocumentParser.cpp
+++ Userland/Libraries/LibWeb/HTML/Parser/HTMLDocumentParser.cpp
@@ -262,11 +262,11 @@ DOM::QuirksMode HTMLDocumentParser::which_quirks_mode(const HTMLToken& doctype_t
         return DOM::QuirksMode::Yes;
 
     // NOTE: The tokenizer puts the name into lower case for us.
-    if (doctype_token.m_doctype.name.to_string() != "html")
+    if (doctype_token.m_doctype.name != "html")
         return DOM::QuirksMode::Yes;
 
-    auto public_identifier = doctype_token.m_doctype.public_identifier.to_string();
-    auto system_identifier = doctype_token.m_doctype.system_identifier.to_string();
+    auto const& public_identifier = doctype_token.m_doctype.public_identifier;
+    auto const& system_identifier = doctype_token.m_doctype.system_identifier;
 
     if (public_identifier.equals_ignoring_case("-//W3O//DTD W3 HTML Strict 3.0//EN//"))
         return DOM::QuirksMode::Yes;
@@ -324,9 +324,9 @@ void HTMLDocumentParser::handle_initial(HTMLToken& token)
 
     if (token.is_doctype()) {
         auto doctype = adopt_ref(*new DOM::DocumentType(document()));
-        doctype->set_name(token.m_doctype.name.to_string());
-        doctype->set_public_id(token.m_doctype.public_identifier.to_string());
-        doctype->set_system_id(token.m_doctype.system_identifier.to_string());
+        doctype->set_name(token.m_doctype.name);
+        doctype->set_public_id(token.m_doctype.public_identifier);
+        doctype->set_system_id(token.m_doctype.system_identifier);
         document().append_child(move(doctype));
         document().set_quirks_mode(which_quirks_mode(token));
         m_insertion_mode = InsertionMode::BeforeHTML;
