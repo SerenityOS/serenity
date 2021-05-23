SerenityOS/serenity

commit f0375e3efe562caf5c981ba3d964bec69dea1b14

Kernel+LibC: Support more `termios` settings in TTY
This commit adds support for the various ECHO* lflags and fixes some
POSIX conformance issues around newline handling. Also included are
error messages when setting not implemented settings.

Kernel/TTY/TTY.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <AK/ScopeGuard.h>
+#include <AK/StringView.h>
 #include <Kernel/Debug.h>
 #include <Kernel/Process.h>
 #include <Kernel/TTY/TTY.h>
@@ -50,16 +51,12 @@ KResultOr<size_t> TTY::read(FileDescription&, u64, UserOrKernelBuffer& buffer, s
     if (m_input_buffer.size() < static_cast<size_t>(size))
         size = m_input_buffer.size();
 
-    KResultOr<size_t> result = 0;
     bool need_evaluate_block_conditions = false;
-    if (in_canonical_mode()) {
-        result = buffer.write_buffered<512>(size, [&](u8* data, size_t data_size) {
-            size_t i = 0;
-            for (; i < data_size; i++) {
-                u8 ch = m_input_buffer.dequeue();
+    auto result = buffer.write_buffered<512>(size, [&](u8* data, size_t data_size) {
+        for (size_t i = 0; i < data_size; ++i) {
+            u8 ch = m_input_buffer.dequeue();
+            if (in_canonical_mode()) {
                 if (ch == '\0') {
-                    //Here we handle a ^D line, so we don't add the
-                    //character to the output.
                     m_available_lines--;
                     need_evaluate_block_conditions = true;
                     break;
@@ -69,23 +66,11 @@ KResultOr<size_t> TTY::read(FileDescription&, u64, UserOrKernelBuffer& buffer, s
                     m_available_lines--;
                     break;
                 }
-                data[i] = ch;
             }
-            return i;
-        });
-    } else {
-        result = buffer.write_buffered<512>(size, [&](u8* data, size_t data_size) {
-            for (size_t i = 0; i < data_size; i++) {
-                auto ch = m_input_buffer.dequeue();
-                if (ch == '\r' && m_termios.c_iflag & ICRNL)
-                    ch = '\n';
-                else if (ch == '\n' && m_termios.c_iflag & INLCR)
-                    ch = '\r';
-                data[i] = ch;
-            }
-            return data_size;
-        });
-    }
+            data[i] = ch;
+        }
+        return data_size;
+    });
     if ((!result.is_error() && result.value() > 0) || need_evaluate_block_conditions)
         evaluate_block_conditions();
     return result;
@@ -104,9 +89,11 @@ KResultOr<size_t> TTY::write(FileDescription&, u64, const UserOrKernelBuffer& bu
         size_t extra_chars = 0;
         for (size_t i = 0; i < buffer_bytes; ++i) {
             auto ch = data[i];
-            if (ch == '\n' && (m_termios.c_oflag & (OPOST | ONLCR))) {
-                modified_data[i + extra_chars] = '\r';
-                extra_chars++;
+            if (m_termios.c_oflag & OPOST) {
+                if (ch == '\n' && (m_termios.c_oflag & ONLCR)) {
+                    modified_data[i + extra_chars] = '\r';
+                    extra_chars++;
+                }
             }
             modified_data[i + extra_chars] = ch;
         }
@@ -155,6 +142,9 @@ bool TTY::is_werase(u8 ch) const
 
 void TTY::emit(u8 ch, bool do_evaluate_block_conditions)
 {
+    if (m_termios.c_iflag & ISTRIP)
+        ch &= 0x7F;
+
     if (should_generate_signals()) {
         if (ch == m_termios.c_cc[VINFO]) {
             dbgln("{}: VINFO pressed!", tty_name());
@@ -187,6 +177,11 @@ void TTY::emit(u8 ch, bool do_evaluate_block_conditions)
             evaluate_block_conditions();
     });
 
+    if (ch == '\r' && (m_termios.c_iflag & ICRNL))
+        ch = '\n';
+    else if (ch == '\n' && (m_termios.c_iflag & INLCR))
+        ch = '\r';
+
     if (in_canonical_mode()) {
         if (is_eof(ch)) {
             m_available_lines++;
@@ -195,11 +190,11 @@ void TTY::emit(u8 ch, bool do_evaluate_block_conditions)
             m_input_buffer.enqueue('\0');
             return;
         }
-        if (is_kill(ch)) {
+        if (is_kill(ch) && m_termios.c_lflag & ECHOK) {
             kill_line();
             return;
         }
-        if (is_erase(ch)) {
+        if (is_erase(ch) && m_termios.c_lflag & ECHOE) {
             do_backspace();
             return;
         }
@@ -207,12 +202,23 @@ void TTY::emit(u8 ch, bool do_evaluate_block_conditions)
             erase_word();
             return;
         }
-        if (ch == '\n' || is_eol(ch)) {
+
+        if (is_eol(ch)) {
             m_available_lines++;
         }
+
+        if (ch == '\n') {
+            if (m_termios.c_lflag & ECHO || m_termios.c_lflag & ECHONL)
+                echo('\n');
+            m_input_buffer.enqueue('\n');
+            m_available_lines++;
+            return;
+        }
     }
+
     m_input_buffer.enqueue(ch);
-    echo(ch);
+    if (m_termios.c_lflag & ECHO)
+        echo(ch);
 }
 
 bool TTY::can_do_backspace() const
@@ -303,8 +309,9 @@ void TTY::flush_input()
     evaluate_block_conditions();
 }
 
-void TTY::set_termios(const termios& t)
+int TTY::set_termios(const termios& t)
 {
+    int rc = 0;
     m_termios = t;
 
     dbgln_if(TTY_DEBUG, "{} set_termios: ECHO={}, ISIG={}, ICANON={}, ECHOE={}, ECHOK={}, ECHONL={}, ISTRIP={}, ICRNL={}, INLCR={}, IGNCR={}, OPOST={}, ONLCR={}",
@@ -321,6 +328,79 @@ void TTY::set_termios(const termios& t)
         ((m_termios.c_iflag & IGNCR) != 0),
         ((m_termios.c_oflag & OPOST) != 0),
         ((m_termios.c_oflag & ONLCR) != 0));
+
+    struct FlagDescription {
+        tcflag_t value;
+        StringView name;
+    };
+
+    static constexpr FlagDescription unimplemented_iflags[] = {
+        { IGNBRK, "IGNBRK" },
+        { BRKINT, "BRKINT" },
+        { IGNPAR, "IGNPAR" },
+        { PARMRK, "PARMRK" },
+        { INPCK, "INPCK" },
+        { IGNCR, "IGNCR" },
+        { IUCLC, "IUCLC" },
+        { IXON, "IXON" },
+        { IXANY, "IXANY" },
+        { IXOFF, "IXOFF" },
+        { IMAXBEL, "IMAXBEL" },
+        { IUTF8, "IUTF8" }
+    };
+    for (auto flag : unimplemented_iflags) {
+        if (m_termios.c_iflag & flag.value) {
+            dbgln("FIXME: iflag {} unimplemented", flag.name);
+            rc = -ENOTIMPL;
+        }
+    }
+
+    static constexpr FlagDescription unimplemented_oflags[] = {
+        { OLCUC, "OLCUC" },
+        { ONOCR, "ONOCR" },
+        { ONLRET, "ONLRET" },
+        { OFILL, "OFILL" },
+        { OFDEL, "OFDEL" }
+    };
+    for (auto flag : unimplemented_oflags) {
+        if (m_termios.c_oflag & flag.value) {
+            dbgln("FIXME: oflag {} unimplemented", flag.name);
+            rc = -ENOTIMPL;
+        }
+    }
+
+    static constexpr FlagDescription unimplemented_cflags[] = {
+        { CSIZE, "CSIZE" },
+        { CS5, "CS5" },
+        { CS6, "CS6" },
+        { CS7, "CS7" },
+        { CS8, "CS8" },
+        { CSTOPB, "CSTOPB" },
+        { CREAD, "CREAD" },
+        { PARENB, "PARENB" },
+        { PARODD, "PARODD" },
+        { HUPCL, "HUPCL" },
+        { CLOCAL, "CLOCAL" }
+    };
+    for (auto flag : unimplemented_cflags) {
+        if (m_termios.c_cflag & flag.value) {
+            dbgln("FIXME: cflag {} unimplemented", flag.name);
+            rc = -ENOTIMPL;
+        }
+    }
+
+    static constexpr FlagDescription unimplemented_lflags[] = {
+        { TOSTOP, "TOSTOP" },
+        { IEXTEN, "IEXTEN" }
+    };
+    for (auto flag : unimplemented_lflags) {
+        if (m_termios.c_lflag & flag.value) {
+            dbgln("FIXME: lflag {} unimplemented", flag.name);
+            rc = -ENOTIMPL;
+        }
+    }
+
+    return rc;
 }
 
 int TTY::ioctl(FileDescription&, unsigned request, FlatPtr arg)
@@ -381,10 +461,10 @@ int TTY::ioctl(FileDescription&, unsigned request, FlatPtr arg)
         termios termios;
         if (!copy_from_user(&termios, user_termios))
             return -EFAULT;
-        set_termios(termios);
+        int rc = set_termios(termios);
         if (request == TCSETSF)
             flush_input();
-        return 0;
+        return rc;
     }
     case TCFLSH:
         // Serenity's TTY implementation does not use an output buffer, so ignore TCOFLUSH.

Kernel/TTY/TTY.h
@@ -38,7 +38,7 @@ public:
         return 0;
     }
 
-    void set_termios(const termios&);
+    int set_termios(const termios&);
     bool should_generate_signals() const { return m_termios.c_lflag & ISIG; }
     bool should_flush_on_signal() const { return !(m_termios.c_lflag & NOFLSH); }
     bool should_echo_input() const { return m_termios.c_lflag & ECHO; }

Userland/Libraries/LibC/sys/ttydefaults.h
@@ -6,7 +6,7 @@
 
 #pragma once
 
-#define TTYDEF_IFLAG (ICRNL | IXON | IXANY)
+#define TTYDEF_IFLAG (ICRNL)
 #define TTYDEF_OFLAG (OPOST | ONLCR)
 #define TTYDEF_LFLAG_NOECHO (ISIG | ICANON)
 #define TTYDEF_LFLAG_ECHO (TTYDEF_LFLAG_NOECHO | ECHO | ECHOE | ECHOK | ECHONL)

commit 7a9dfb6a0019fe15640f5a056d7d86b3c0bb460f

Kernel: Signal EOF/EOL characters properly in `TTY`
I introduced a regression in #7184 where `TTY` would report 1 byte read
in canonical mode even if we had no more characters left. This was
caused by counting the '\0' that denotes EOF into the number of
characters that were read.

The fix was simple: exclude the EOF character from the number of bytes.

This still wouldn't be correct by itself, as the EOF and EOL control
characters could change between when the data was written to the TTY and
when it is read. We fix this by signaling out-of-band whether something
is a special character. End-of-file markers have a value of zero and
have their special bits set. Any other bytes with a special flag are
treated as line endings. This is possible, as POSIX doesn't allow
special characters to be 0.

Fixes #7419

Kernel/TTY/TTY.cpp
@@ -53,23 +53,27 @@ KResultOr<size_t> TTY::read(FileDescription&, u64, UserOrKernelBuffer& buffer, s
 
     bool need_evaluate_block_conditions = false;
     auto result = buffer.write_buffered<512>(size, [&](u8* data, size_t data_size) {
-        for (size_t i = 0; i < data_size; ++i) {
-            u8 ch = m_input_buffer.dequeue();
-            if (in_canonical_mode()) {
+        size_t bytes_written = 0;
+        for (; bytes_written < data_size; ++bytes_written) {
+            auto bit_index = m_input_buffer.head_index();
+            bool is_special_character = m_special_character_bitmask[bit_index / 8] & (1 << (bit_index % 8));
+            if (in_canonical_mode() && is_special_character) {
+                u8 ch = m_input_buffer.dequeue();
                 if (ch == '\0') {
+                    // EOF
                     m_available_lines--;
                     need_evaluate_block_conditions = true;
                     break;
-                } else if (ch == '\n' || is_eol(ch)) {
-                    data[i] = ch;
-                    i++;
+                } else {
+                    // '\n' or EOL
+                    data[bytes_written++] = ch;
                     m_available_lines--;
                     break;
                 }
             }
-            data[i] = ch;
+            data[bytes_written] = m_input_buffer.dequeue();
         }
-        return data_size;
+        return bytes_written;
     });
     if ((!result.is_error() && result.value() > 0) || need_evaluate_block_conditions)
         evaluate_block_conditions();
@@ -203,11 +207,20 @@ void TTY::emit(u8 ch, bool do_evaluate_block_conditions)
     else if (ch == '\n' && (m_termios.c_iflag & INLCR))
         ch = '\r';
 
+    auto current_char_head_index = (m_input_buffer.head_index() + m_input_buffer.size()) % TTY_BUFFER_SIZE;
+    m_special_character_bitmask[current_char_head_index / 8] &= ~(1u << (current_char_head_index % 8));
+
+    auto set_special_bit = [&] {
+        m_special_character_bitmask[current_char_head_index / 8] |= (1u << (current_char_head_index % 8));
+    };
+
     if (in_canonical_mode()) {
         if (is_eof(ch)) {
+            // Since EOF might change between when the data came in and when it is read,
+            // we use '\0' along with the bitmask to signal EOF. Any non-zero byte with
+            // the special bit set signals an end-of-line.
+            set_special_bit();
             m_available_lines++;
-            //We use '\0' to delimit the end
-            //of a line.
             m_input_buffer.enqueue('\0');
             return;
         }
@@ -224,16 +237,18 @@ void TTY::emit(u8 ch, bool do_evaluate_block_conditions)
             return;
         }
 
-        if (is_eol(ch)) {
-            m_available_lines++;
-        }
-
         if (ch == '\n') {
             if (m_termios.c_lflag & ECHO || m_termios.c_lflag & ECHONL)
                 echo('\n');
+            set_special_bit();
             m_input_buffer.enqueue('\n');
             m_available_lines++;
             return;
+        }
+
+        if (is_eol(ch)) {
+            set_special_bit();
+            m_available_lines++;
         }
     }
 

Kernel/TTY/TTY.h
@@ -13,6 +13,8 @@
 #include <Kernel/ProcessGroup.h>
 #include <Kernel/UnixTypes.h>
 
+#define TTY_BUFFER_SIZE 1024
+
 namespace Kernel {
 
 class TTY : public CharacterDevice {
@@ -79,7 +81,10 @@ private:
     // ^CharacterDevice
     virtual bool is_tty() const final override { return true; }
 
-    CircularDeque<u8, 1024> m_input_buffer;
+    CircularDeque<u8, TTY_BUFFER_SIZE> m_input_buffer;
+    // FIXME: use something like AK::Bitmap but which takes a size template parameter
+    u8 m_special_character_bitmask[TTY_BUFFER_SIZE / 8];
+
     WeakPtr<Process> m_original_process_parent;
     WeakPtr<ProcessGroup> m_pg;
     termios m_termios;