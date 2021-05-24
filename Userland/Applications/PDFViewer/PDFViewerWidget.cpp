89434bb7a643b9a5935ba67cd05cc1e3b014c421
Matthew Olsson <matthewcolsson@gmail.com>
2021-05-23 22:34:44 -0700

    PDFViewer: Add a toolbar
    The toolbar has an option to toggle the sidebar, a number input to
    set the current page, and two buttons to go up and down by one page

--- Userland/Applications/PDFViewer/PDFViewerWidget.cpp
+++ Userland/Applications/PDFViewer/PDFViewerWidget.cpp
@@ -9,14 +9,19 @@
 #include <LibGUI/Application.h>
 #include <LibGUI/BoxLayout.h>
 #include <LibGUI/FilePicker.h>
+#include <LibGUI/Label.h>
 #include <LibGUI/Menu.h>
 #include <LibGUI/Menubar.h>
 #include <LibGUI/Splitter.h>
+#include <LibGUI/Toolbar.h>
+#include <LibGUI/ToolbarContainer.h>
 
 PDFViewerWidget::PDFViewerWidget()
 {
     set_fill_with_background_color(true);
     set_layout<GUI::VerticalBoxLayout>();
+
+    create_toolbar();
 
     auto& splitter = add<GUI::HorizontalSplitter>();
 
@@ -37,21 +42,53 @@ void PDFViewerWidget::initialize_menubar(GUI::Menubar& menubar)
     file_menu.add_action(GUI::CommonActions::make_quit_action([](auto&) {
         GUI::Application::the()->quit();
     }));
+}
 
-    auto& view_menu = menubar.add_menu("&View");
+void PDFViewerWidget::create_toolbar()
+{
+    auto& toolbar_container = add<GUI::ToolbarContainer>();
+    auto& toolbar = toolbar_container.add<GUI::Toolbar>();
 
-    auto open_sidebar_action = GUI::Action::create(
+    auto open_outline_action = GUI::Action::create(
         "Open &Sidebar", { Mod_Ctrl, Key_O }, Gfx::Bitmap::load_from_file("/res/icons/16x16/sidebar.png"), [&](auto& action) {
             m_sidebar_open = !m_sidebar_open;
             m_sidebar->set_fixed_width(m_sidebar_open ? 0 : 200);
             action.set_text(m_sidebar_open ? "Open &Sidebar" : "Close &Sidebar");
         },
         nullptr);
-    open_sidebar_action->set_enabled(false);
+    open_outline_action->set_enabled(false);
+    m_toggle_sidebar_action = open_outline_action;
 
-    view_menu.add_action(open_sidebar_action);
+    toolbar.add_action(*open_outline_action);
+    toolbar.add_separator();
 
-    m_open_outline_action = open_sidebar_action;
+    m_go_to_prev_page_action = GUI::Action::create("Go to &Previous Page", Gfx::Bitmap::load_from_file("/res/icons/16x16/go-up.png"), [&](auto&) {
+        if (m_viewer->current_page() > 0)
+            m_page_text_box->set_current_number(m_viewer->current_page());
+    });
+    m_go_to_prev_page_action->set_enabled(false);
+
+    m_go_to_next_page_action = GUI::Action::create("Go to &Next Page", Gfx::Bitmap::load_from_file("/res/icons/16x16/go-down.png"), [&](auto&) {
+        if (m_viewer->current_page() < m_viewer->document()->get_page_count() - 1)
+            m_page_text_box->set_current_number(m_viewer->current_page() + 2);
+    });
+    m_go_to_next_page_action->set_enabled(false);
+
+    toolbar.add_action(*m_go_to_prev_page_action);
+    toolbar.add_action(*m_go_to_next_page_action);
+
+    m_page_text_box = toolbar.add<NumericInput>();
+    m_page_text_box->set_enabled(false);
+    m_page_text_box->set_fixed_width(30);
+    m_page_text_box->set_min_number(1);
+
+    m_page_text_box->on_number_changed = [&](i32 number) {
+        VERIFY(number >= 1 && static_cast<u32>(number) <= m_viewer->document()->get_page_count());
+        m_viewer->set_current_page(static_cast<u32>(number) - 1);
+        m_viewer->update();
+    };
+
+    m_total_page_label = toolbar.add<GUI::Label>();
 }
 
 void PDFViewerWidget::open_file(const String& path)
@@ -62,17 +99,24 @@ void PDFViewerWidget::open_file(const String& path)
     m_buffer = file_result.value()->read_all();
     auto document = adopt_ref(*new PDF::Document(m_buffer));
     m_viewer->set_document(document);
+    m_total_page_label->set_text(String::formatted("of {}", document->get_page_count()));
+    m_total_page_label->set_fixed_width(30);
+
+    m_page_text_box->set_text(String::number(m_viewer->current_page() + 1));
+    m_page_text_box->set_enabled(true);
+    m_page_text_box->set_max_number(document->get_page_count());
+    m_go_to_prev_page_action->set_enabled(true);
+    m_go_to_next_page_action->set_enabled(true);
+    m_toggle_sidebar_action->set_enabled(true);
 
     if (document->outline()) {
         auto outline = document->outline();
         m_sidebar->set_outline(outline.release_nonnull());
         m_sidebar->set_fixed_width(200);
         m_sidebar_open = true;
-        m_open_outline_action->set_enabled(true);
     } else {
         m_sidebar->set_outline({});
         m_sidebar->set_fixed_width(0);
         m_sidebar_open = false;
-        m_open_outline_action->set_enabled(false);
     }
 }
