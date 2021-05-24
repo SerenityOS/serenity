d237e454444a3ebfdd58a5a6d6b03169889d833f
Matthew Olsson <matthewcolsson@gmail.com>
2021-05-23 21:34:06 -0700

    PDFViewer: Add a tab bar with outlines and thumbnails
    Outlines are in theory implemented (though I'm having trouble finding
    a simple PDF with outlines to test it on), and thumbnails are not.

--- Userland/Applications/PDFViewer/PDFViewerWidget.h
+++ Userland/Applications/PDFViewer/PDFViewerWidget.h
@@ -7,21 +7,28 @@
 #pragma once
 
 #include "PDFViewer.h"
+#include "SidebarWidget.h"
+#include <LibGUI/Action.h>
 #include <LibGUI/Widget.h>
 
 class PDFViewer;
 
 class PDFViewerWidget final : public GUI::Widget {
     C_OBJECT(PDFViewerWidget)
+
 public:
-    ~PDFViewerWidget() override;
+    ~PDFViewerWidget() override = default;
+
     void open_file(const String& path);
     void initialize_menubar(GUI::Menubar&);
 
 private:
     PDFViewerWidget();
 
+    RefPtr<GUI::Action> m_open_outline_action;
     RefPtr<PDFViewer> m_viewer;
+    RefPtr<SidebarWidget> m_sidebar;
+    bool m_sidebar_open { false };
     ByteBuffer m_buffer;
     RefPtr<GUI::Action> m_open_action;
 };
