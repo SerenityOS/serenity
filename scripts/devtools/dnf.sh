9e4b5022d281a3f692aeadf1a4dfb8b2e9692809
Mr. Hallway <git@mrhallway.me>
2021-05-26 21:45:34 -0700

    Create dnf.sh

---
+++ scripts/devtools/dnf.sh
@@ -0,0 +1,2 @@
+echo Installing tools for systems that use `dnf` as a package manager.
+sudo dnf install curl cmake mpfr-devel libmpc-devel gmp-devel e2fsprogs ninja-build patch @"C Development Tools and Libraries" @Virtualization -y
