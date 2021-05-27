db8a7888dd99b4fa92fd4e2b84e21ce86a2e7bd8
Mr. Hallway <git@mrhallway.me>
2021-05-26 21:47:09 -0700

    Create zypper.sh

---
+++ scripts/devtools/zypper.sh
@@ -0,0 +1,2 @@
+echo Installing tools for systems that use `zypper` as a package manager.
+sudo zypper install curl cmake mpfr-devel mpc-devel ninja gmp-devel e2fsprogs patch qemu-x86 qemu-audio-pa gcc gcc-c++ patterns-devel-C-C++-devel_C_C++
