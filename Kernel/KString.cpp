c7266e8835f85b949632932a77b8a8227195598a
Andreas Kling <kling@serenityos.org>
2021-05-28 09:25:02 +0200

    Kernel: Add KString, a single-owner string with OOM failure exposion
    This is a simple string class for use in the kernel. It encapsulates
    a length + character array in a single-allocation object.
    
    Main differences from AK::String:
    
    - Single-owner (no reference counting.)
    - Allocation failures are exposed, not hidden.
    
    The basic idea is to allow better and more precise string management
    in the kernel.

---
+++ Kernel/KString.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2021, Andreas Kling <kling@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <Kernel/KString.h>
+
+namespace Kernel {
+
+OwnPtr<KString> KString::try_create(StringView const& string)
+{
+    char* characters = nullptr;
+    size_t length = string.length();
+    auto new_string = KString::try_create_uninitialized(length, characters);
+    if (!new_string)
+        return {};
+    if (!string.is_empty())
+        __builtin_memcpy(characters, string.characters_without_null_termination(), length);
+    characters[length] = '\0';
+    return new_string;
+}
+
+OwnPtr<KString> KString::try_create_uninitialized(size_t length, char*& characters)
+{
+    size_t allocation_size = sizeof(KString) + (sizeof(char) * length) + sizeof(char);
+    auto* slot = kmalloc(allocation_size);
+    if (!slot)
+        return {};
+    auto* new_string = new (slot) KString(length);
+    characters = new_string->m_characters;
+    return adopt_own(*new_string);
+}
+
+OwnPtr<KString> KString::try_clone() const
+{
+    return try_create(view());
+}
+
+}
---
+++ Kernel/KString.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2021, Andreas Kling <kling@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/Format.h>
+#include <AK/OwnPtr.h>
+
+namespace Kernel {
+
+class KString {
+public:
+    static OwnPtr<KString> try_create_uninitialized(size_t, char*&);
+    static OwnPtr<KString> try_create(StringView const&);
+
+    OwnPtr<KString> try_clone() const;
+
+    bool is_empty() const { return m_length == 0; }
+    size_t length() const { return m_length; }
+    char const* characters() const { return m_characters; }
+    StringView view() const { return { characters(), length() }; }
+
+private:
+    explicit KString(size_t length)
+        : m_length(length)
+    {
+    }
+
+    size_t m_length { 0 };
+    char m_characters[0];
+};
+
+}
+
+namespace AK {
+
+template<>
+struct Formatter<Kernel::KString> : Formatter<StringView> {
+    void format(FormatBuilder& builder, Kernel::KString const& value)
+    {
+        Formatter<StringView>::format(builder, value.characters());
+    }
+};
+
+}
