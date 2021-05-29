162a3d8d15cff81174d4d9bc2562086e3ea192a0
Stephan Unverwerth <s.unverwerth@serenityos.org>
2021-05-29 12:38:28 +0200

    Everywhere: Use s.unverwerth@serenityos.org :^)

--- Userland/Libraries/LibGL/GLUtils.cpp
+++ Userland/Libraries/LibGL/GLUtils.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2021, Jesse Buhagiar <jooster669@gmail.com>
- * Copyright (c) 2021, Stephan Unverwerth <s.unverwerth@gmx.de>
+ * Copyright (c) 2021, Stephan Unverwerth <s.unverwerth@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
