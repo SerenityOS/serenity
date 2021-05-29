3fa9cce71d268c173755d54bbef84bb4c09bc61d
DexesTTP <dexes.ttp@gmail.com>
2021-05-29 07:29:56 +0200

    LibTLS: Use a more precise KeyExchangeAlgorithm enum
    The old enumeration didn't allow discriminating the key exchange
    algorithms used, but only allowed the handshake with the server. With
    this new enumeration, we can know which key exchange algorithm we are
    actually supposed to use :^)

--- Userland/Libraries/LibTLS/CipherSuite.h
+++ Userland/Libraries/LibTLS/CipherSuite.h
@@ -105,6 +105,49 @@ struct SignatureAndHashAlgorithm {
     SignatureAlgorithm signature;
 };
 
+enum class KeyExchangeAlgorithm {
+    Invalid,
+    // Defined in RFC 5246 section 7.4.2 / RFC 4279 section 4
+    RSA_PSK,
+    // Defined in RFC 5246 section 7.4.3
+    DHE_DSS,
+    DHE_RSA,
+    DH_anon,
+    RSA,
+    DH_DSS,
+    DH_RSA,
+    // Defined in RFC 4492 section 2
+    ECDHE_RSA,
+    ECDH_ECDSA,
+    ECDH_RSA,
+    ECDHE_ECDSA,
+    ECDH_anon,
+};
+
+// Defined in RFC 5246 section 7.4.1.4.1
+constexpr SignatureAlgorithm signature_for_key_exchange_algorithm(KeyExchangeAlgorithm algorithm)
+{
+    switch (algorithm) {
+    case KeyExchangeAlgorithm::RSA:
+    case KeyExchangeAlgorithm::DHE_RSA:
+    case KeyExchangeAlgorithm::DH_RSA:
+    case KeyExchangeAlgorithm::RSA_PSK:
+    case KeyExchangeAlgorithm::ECDH_RSA:
+    case KeyExchangeAlgorithm::ECDHE_RSA:
+        return SignatureAlgorithm::RSA;
+    case KeyExchangeAlgorithm::DHE_DSS:
+    case KeyExchangeAlgorithm::DH_DSS:
+        return SignatureAlgorithm::DSA;
+    case KeyExchangeAlgorithm::ECDH_ECDSA:
+    case KeyExchangeAlgorithm::ECDHE_ECDSA:
+        return SignatureAlgorithm::ECDSA;
+    case KeyExchangeAlgorithm::DH_anon:
+    case KeyExchangeAlgorithm::ECDH_anon:
+    default:
+        return SignatureAlgorithm::Anonymous;
+    }
+}
+
 enum class CipherAlgorithm {
     Invalid,
     AES_128_CBC,
