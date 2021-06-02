123848f0c1c4513fd5e175248b6da0f753c8ddaf
Matthew Jones <matthewbjones85@gmail.com>
2021-06-01 22:08:47 -0600

    LibGUI: Fixes Widget->set_visible(false) still maintains focus bug
    When setting a Widget->set_visible(false), if that Widget->has_focus()
    it will continue to have focus, even though it's not visible to the user
    anymore.
    
    Now calling Widget->set_visible(false) will remove focus from the Widget
    if it had focus, and the Window will give focus back to the Widget
    that had it previously, if there was one.

--- Userland/Libraries/LibGUI/Window.cpp
+++ Userland/Libraries/LibGUI/Window.cpp
@@ -654,11 +654,15 @@ void Window::set_focused_widget(Widget* widget, FocusSource source)
     WeakPtr<Widget> previously_focused_widget = m_focused_widget;
     m_focused_widget = widget;
 
+    if (!m_focused_widget && m_previously_focused_widget)
+        m_focused_widget = m_previously_focused_widget;
+
     if (previously_focused_widget) {
         Core::EventLoop::current().post_event(*previously_focused_widget, make<FocusEvent>(Event::FocusOut, source));
         previously_focused_widget->update();
         if (previously_focused_widget && previously_focused_widget->on_focus_change)
             previously_focused_widget->on_focus_change(previously_focused_widget->is_focused(), source);
+        m_previously_focused_widget = previously_focused_widget;
     }
     if (m_focused_widget) {
         Core::EventLoop::current().post_event(*m_focused_widget, make<FocusEvent>(Event::FocusIn, source));
