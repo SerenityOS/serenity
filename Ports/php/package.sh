e936f4a761ecdab76010563aaca80b22081b9358
Jelle Raaijmakers <jelle@gmta.nl>
2021-06-04 13:01:54 +0200

    Ports/PHP: Enable SQLite3 and iconv extensions

--- Ports/php/package.sh
+++ Ports/php/package.sh
@@ -4,7 +4,7 @@ useconfigure="true"
 version="8.0.6"
 files="https://www.php.net/distributions/php-${version}.tar.xz php-${version}.tar.xz e9871d3b6c391fe9e89f86f6334852dcc10eeaaa8d5565beb8436e7f0cf30e20"
 auth_type=sha256
-depends=""
+depends="libiconv sqlite"
 configopts="
     --disable-dom
     --disable-opcache
@@ -14,10 +14,10 @@ configopts="
     --disable-xmlreader
     --disable-xmlwriter
     --prefix=${SERENITY_INSTALL_ROOT}/usr/local
-    --without-iconv
+    --with-iconv=${SERENITY_INSTALL_ROOT}/usr/local
     --without-libxml
-    --without-pdo-sqlite
-    --without-sqlite3
 "
 
 export LIBS="-ldl"
+export SQLITE_CFLAGS="y"
+export SQLITE_LIBS="-lsqlite3 -lpthread"
