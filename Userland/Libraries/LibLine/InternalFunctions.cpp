a8431eb16b747ed9449cadd64f864003a3de59b5
brapru <brapru@pm.me>
2021-06-03 22:50:42 -0400

    LibLine: Actually cancel the search editor on Ctl-C
    When the search editor calls on really_quit_event_loop to cancel the
    search, the command loaded in m_buffer would actually execute because
    really_quit_event_loop sends a new line character and then afterwards
    clears the buffer.
    
    By using end_search prior to exiting the event loop, this patch will
    appropriately clear the buffer, not execute any commands, and
    preserve the original loaded buffer after returning from a canceled
    search.

--- Userland/Libraries/LibLine/InternalFunctions.cpp
+++ Userland/Libraries/LibLine/InternalFunctions.cpp
@@ -264,6 +264,7 @@ void Editor::enter_search()
         m_search_editor->register_key_input_callback(ctrl('C'), [this](Editor& search_editor) {
             search_editor.finish();
             m_reset_buffer_on_search_end = true;
+            search_editor.end_search();
             search_editor.deferred_invoke([&search_editor](auto&) { search_editor.really_quit_event_loop(); });
             return false;
         });
