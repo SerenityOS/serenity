7e1f413a89dffb76ce6347eb6fe0ef62220c3ff9
Gunnar Beutner <gbeutner@serenityos.org>
2021-06-04 23:19:15 +0200

    Ports: Build shared library for freetype

--- Ports/freetype/package.sh
+++ Ports/freetype/package.sh
@@ -5,3 +5,9 @@ useconfigure=true
 files="https://download.savannah.gnu.org/releases/freetype/freetype-${version}.tar.gz freetype-${version}.tar.gz 5eab795ebb23ac77001cfb68b7d4d50b5d6c7469247b0b01b2c953269f658dac"
 auth_type=sha256
 configopts="--with-brotli=no --with-bzip2=no --with-zlib=no --with-harfbuzz=no --with-png=no"
+
+install() {
+    run make DESTDIR=${SERENITY_INSTALL_ROOT} $installopts install
+    ${CC} -shared -o ${SERENITY_INSTALL_ROOT}/usr/local/lib/libfreetype.so -Wl,-soname,libfreetype.so -Wl,--whole-archive ${SERENITY_INSTALL_ROOT}/usr/local/lib/libfreetype.a -Wl,--no-whole-archive
+    rm -f ${SERENITY_INSTALL_ROOT}/usr/local/lib/libfreetype.la
+}
