18a5ac1e66a88eabe9725caeab6dbbe6246e37db
Jelle Raaijmakers <jelle@gmta.nl>
2021-06-04 17:48:32 +0200

    Ports: Enable PHP XML extensions

--- Ports/php/package.sh
+++ Ports/php/package.sh
@@ -4,20 +4,19 @@ useconfigure="true"
 version="8.0.6"
 files="https://www.php.net/distributions/php-${version}.tar.xz php-${version}.tar.xz e9871d3b6c391fe9e89f86f6334852dcc10eeaaa8d5565beb8436e7f0cf30e20"
 auth_type=sha256
-depends="libiconv sqlite"
+depends="libiconv libxml2 sqlite zlib"
 configopts="
-    --disable-dom
     --disable-opcache
     --disable-phar
-    --disable-simplexml
-    --disable-xml
-    --disable-xmlreader
-    --disable-xmlwriter
     --prefix=${SERENITY_INSTALL_ROOT}/usr/local
     --with-iconv=${SERENITY_INSTALL_ROOT}/usr/local
-    --without-libxml
 "
 
+export CFLAGS="-I${SERENITY_INSTALL_ROOT}/usr/local/include/libxml2/"
 export LIBS="-ldl"
+export LIBXML_CFLAGS="y"
+export LIBXML_LIBS="-lxml2"
 export SQLITE_CFLAGS="y"
 export SQLITE_LIBS="-lsqlite3 -lpthread"
+export ZLIB_CFLAGS="y"
+export ZLIB_LIBS="-lz"
