394fc2b7bb9e45575d011d81a572f4114a71fad4
Idan Horowitz <idan.horowitz@gmail.com>
2021-06-05 19:21:15 +0300

    LibJS: Add the global escape() & unescape() methods

---
+++ Userland/Libraries/LibJS/Tests/builtins/functions/escapeUnescape.js
@@ -0,0 +1,21 @@
+test("escape", () => {
+    [
+        ["abc123", "abc123"],
+        ["äöü", "%E4%F6%FC"],
+        ["ć", "%u0107"],
+        ["@*_+-./", "@*_+-./"],
+    ].forEach(test => {
+        expect(escape(test[0])).toBe(test[1]);
+    });
+});
+
+test("unescape", () => {
+    [
+        ["abc123", "abc123"],
+        ["%E4%F6%FC", "äöü"],
+        ["%u0107", "ć"],
+        ["@*_+-./", "@*_+-./"],
+    ].forEach(test => {
+        expect(unescape(test[0])).toBe(test[1]);
+    });
+});
