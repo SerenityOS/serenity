5b5f7bc360f3ec487ed6a21c350b7ca4ae165d17
Max Wipfli <mail@maxwipfli.ch>
2021-06-05 11:57:14 +0200

    LibProtocol: Change RequestClient.{cpp,h} to use east const style

--- Userland/Libraries/LibProtocol/RequestClient.h
+++ Userland/Libraries/LibProtocol/RequestClient.h
@@ -22,7 +22,7 @@ class RequestClient final
 
 public:
     template<typename RequestHashMapTraits = Traits<String>>
-    RefPtr<Request> start_request(const String& method, const String& url, const HashMap<String, String, RequestHashMapTraits>& request_headers = {}, ReadonlyBytes request_body = {});
+    RefPtr<Request> start_request(String const& method, String const& url, HashMap<String, String, RequestHashMapTraits> const& request_headers = {}, ReadonlyBytes request_body = {});
 
     bool stop_request(Badge<Request>, Request&);
     bool set_certificate(Badge<Request>, Request&, String, String);
