884ec1461dab7d4c2dfb103a591dba190e1b7e72
Gunnar Beutner <gbeutner@serenityos.org>
2021-06-06 17:40:11 +0200

    LibC+LibELF: Implement dladdr()
    This implements the dladdr() function which lets the caller look up
    the symbol name, symbol address as well as library name and library
    base address for an arbitrary address.

--- Userland/Libraries/LibDl/dlfcn_integration.h
+++ Userland/Libraries/LibDl/dlfcn_integration.h
@@ -23,12 +23,17 @@ struct DlErrorMessage {
     String text;
 };
 
+struct __Dl_info;
+typedef struct __Dl_info Dl_info;
+
 typedef Result<void, DlErrorMessage> (*DlCloseFunction)(void*);
 typedef Result<void*, DlErrorMessage> (*DlOpenFunction)(const char*, int);
 typedef Result<void*, DlErrorMessage> (*DlSymFunction)(void*, const char*);
+typedef Result<void, DlErrorMessage> (*DlAddrFunction)(void*, Dl_info*);
 
 extern "C" {
 extern DlCloseFunction __dlclose;
 extern DlOpenFunction __dlopen;
 extern DlSymFunction __dlsym;
+extern DlAddrFunction __dladdr;
 }
