9607fce67251041c0f1619c4428c0cafed666725
Linus Groh <mail@linusgroh.de>
2021-06-06 16:53:32 +0100

    LibJS: Implement String.prototype[@@toPrimitive]()

--- Userland/Libraries/LibJS/Runtime/SymbolPrototype.cpp
+++ Userland/Libraries/LibJS/Runtime/SymbolPrototype.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2020, Matthew Olsson <mattco@serenityos.org>
+ * Copyright (c) 2021, Linus Groh <linusg@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -30,8 +31,8 @@ void SymbolPrototype::initialize(GlobalObject& global_object)
     define_native_property(vm.names.description, description_getter, {}, Attribute::Configurable);
     define_native_function(vm.names.toString, to_string, 0, Attribute::Writable | Attribute::Configurable);
     define_native_function(vm.names.valueOf, value_of, 0, Attribute::Writable | Attribute::Configurable);
-
-    define_property(global_object.vm().well_known_symbol_to_string_tag(), js_string(global_object.heap(), "Symbol"), Attribute::Configurable);
+    define_native_function(vm.well_known_symbol_to_primitive(), symbol_to_primitive, Attribute::Configurable);
+    define_property(vm.well_known_symbol_to_string_tag(), js_string(global_object.heap(), "Symbol"), Attribute::Configurable);
 }
 
 SymbolPrototype::~SymbolPrototype()
@@ -70,4 +71,11 @@ JS_DEFINE_NATIVE_FUNCTION(SymbolPrototype::value_of)
 {
     return this_symbol_value(global_object, vm.this_value(global_object));
 }
+
+JS_DEFINE_NATIVE_FUNCTION(SymbolPrototype::symbol_to_primitive)
+{
+    // The hint argument is ignored.
+    return this_symbol_value(global_object, vm.this_value(global_object));
+}
+
 }
