194c82b61b0e234c69463103674ec6e9e8df7134
Max Wipfli <mail@maxwipfli.ch>
2021-06-06 17:06:10 +0200

    WebServer: Add support for HTTP basic authentication
    This enables the WebServer to run protected by a username and password.
    While it isn't possible to access such a protected server from inside
    Serenity as of now (because neither the Browser nor pro(1) support
    this), this may very well be the case in the future. :^)

--- Userland/Services/WebServer/Configuration.h
+++ Userland/Services/WebServer/Configuration.h
@@ -6,7 +6,9 @@
 
 #pragma once
 
+#include <AK/Optional.h>
 #include <AK/String.h>
+#include <LibHTTP/HttpRequest.h>
 
 namespace WebServer {
 
@@ -15,13 +17,16 @@ public:
     Configuration(String root_path);
 
     String const& root_path() const { return m_root_path; }
+    Optional<HTTP::HttpRequest::BasicAuthenticationCredentials> const& credentials() const { return m_credentials; }
 
     void set_root_path(String root_path) { m_root_path = move(root_path); }
+    void set_credentials(Optional<HTTP::HttpRequest::BasicAuthenticationCredentials> credentials) { m_credentials = move(credentials); }
 
     static Configuration const& the();
 
 private:
     String m_root_path;
+    Optional<HTTP::HttpRequest::BasicAuthenticationCredentials> m_credentials;
 };
 
 }
