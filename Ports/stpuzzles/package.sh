d97e268a1ff9f009bb20c0e671334c1fd1a81834
Gunnar Beutner <gbeutner@serenityos.org>
2021-06-06 19:27:12 +0200

    Ports: Remove checksum for the stpuzzles port
    The Git repository is hosted on GitHub and is owned by the SerenityOS
    GitHub organization which means our root of trust is GitHub anyway
    (where did you get _this_ repository from?) So let's remove the
    checksum.

--- Ports/stpuzzles/package.sh
+++ Ports/stpuzzles/package.sh
@@ -4,8 +4,7 @@ useconfigure=true
 version=git
 workdir=stpuzzles-main
 configopts="-DCMAKE_TOOLCHAIN_FILE=$SERENITY_SOURCE_DIR/Toolchain/CMake/CMakeToolchain.txt"
-files="https://github.com/SerenityOS/stpuzzles/archive/refs/heads/main.zip stpuzzles.zip 425addbfa4949f6f7341f476359789a33f3826c50f3dc2f6aa734c423a8926e7"
-auth_type=sha256
+files="https://github.com/SerenityOS/stpuzzles/archive/refs/heads/main.zip stpuzzles.zip"
 
 configure() {
     run cmake $configopts -DCMAKE_CXX_FLAGS="-std=c++2a -O2"
