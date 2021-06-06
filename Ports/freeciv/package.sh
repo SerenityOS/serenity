4ca4c43cbd2b31c79b505066b7f6d28dfe0a7eaa
Kenneth Myhra <kennethmyhra@gmail.com>
2021-06-06 08:59:13 -0700

    Ports/freeciv: Add missing dependencies SDL2_gfx and curl (libcurl)

--- Ports/freeciv/package.sh
+++ Ports/freeciv/package.sh
@@ -5,4 +5,4 @@ useconfigure=true
 configopts="--enable-client=sdl2 --enable-fcmp=no"
 files="http://files.freeciv.org/beta/freeciv-${version}.tar.xz freeciv-${version}.tar.xz 9f44dc28068b239c18bda68192ddb27622030880f8ab9c17f777eac28391269e"
 auth_type=sha256
-depends="SDL2 SDL2_image SDL2_mixer SDL2_ttf zstd libicu xz gettext"
+depends="SDL2 SDL2_image SDL2_mixer SDL2_ttf SDL2_gfx zstd libicu xz gettext curl"
