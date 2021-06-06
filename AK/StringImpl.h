0a7e91329ff583fbde23cffd6f1127d47ed7a0e2
Linus Groh <mail@linusgroh.de>
2021-06-06 08:05:49 +0100

    Revert "Revert "AK: Always inline FlyString::view()""
    This reverts commit f09216ac42bac9108e7f36ed2938c6f278f497e4.
    This was supposed to be a local test only, didn't mean to push it. :^)

--- AK/StringImpl.h
+++ AK/StringImpl.h
@@ -43,6 +43,7 @@ public:
     const char* characters() const { return &m_inline_buffer[0]; }
 
     ALWAYS_INLINE ReadonlyBytes bytes() const { return { characters(), length() }; }
+    ALWAYS_INLINE StringView view() const { return { characters(), length() }; }
 
     const char& operator[](size_t i) const
     {
