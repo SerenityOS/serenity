aaa7bc59d6beac685c4a50533cbfddd93ce375bb
Jelle Raaijmakers <jelle@gmta.nl>
2021-06-07 14:18:37 +0200

    Ports/ScummVM: Add freetype, libiconv, libjpeg and libpng to build deps

--- Ports/scummvm/package.sh
+++ Ports/scummvm/package.sh
@@ -4,7 +4,7 @@ useconfigure="true"
 version="2.2.0"
 files="https://downloads.scummvm.org/frs/scummvm/${version}/scummvm-${version}.tar.gz scummvm-${version}.tar.gz 6ec5bd63b73861c10ca9869f27a74989a9ad6013bad30a1ef70de6ec146c2cb5"
 auth_type=sha256
-depends="libtheora SDL2"
+depends="freetype libiconv libjpeg libpng libtheora SDL2"
 configopts="
     --enable-c++11
     --enable-release-mode
@@ -15,6 +15,9 @@ configopts="
 launcher_name=ScummVM
 launcher_category=Games
 launcher_command=/usr/local/bin/scummvm
+
+export FREETYPE2_CFLAGS="-I${SERENITY_INSTALL_ROOT}/usr/local/include/freetype2"
+export FREETYPE2_LIBS="-lfreetype"
 
 post_install() {
     if command -v convert >/dev/null; then
