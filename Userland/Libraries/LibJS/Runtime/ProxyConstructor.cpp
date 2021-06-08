24388428b295baa0ba0b47a77b30289c25d5f02b
Linus Groh <mail@linusgroh.de>
2021-06-08 21:48:43 +0100

    LibJS: Implement Proxy.revocable()

--- Userland/Libraries/LibJS/Runtime/ProxyConstructor.cpp
+++ Userland/Libraries/LibJS/Runtime/ProxyConstructor.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2020, Matthew Olsson <mattco@serenityos.org>
+ * Copyright (c) 2021, Linus Groh <linusg@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -12,6 +13,20 @@
 
 namespace JS {
 
+static ProxyObject* proxy_create(GlobalObject& global_object, Value target, Value handler)
+{
+    auto& vm = global_object.vm();
+    if (!target.is_object()) {
+        vm.throw_exception<TypeError>(global_object, ErrorType::ProxyConstructorBadType, "target", target.to_string_without_side_effects());
+        return {};
+    }
+    if (!handler.is_object()) {
+        vm.throw_exception<TypeError>(global_object, ErrorType::ProxyConstructorBadType, "handler", handler.to_string_without_side_effects());
+        return {};
+    }
+    return ProxyObject::create(global_object, target.as_object(), handler.as_object());
+}
+
 ProxyConstructor::ProxyConstructor(GlobalObject& global_object)
     : NativeFunction(vm().names.Proxy, *global_object.function_prototype())
 {
@@ -22,6 +37,8 @@ void ProxyConstructor::initialize(GlobalObject& global_object)
     auto& vm = this->vm();
     NativeFunction::initialize(global_object);
     define_property(vm.names.length, Value(2), Attribute::Configurable);
+    u8 attr = Attribute::Writable | Attribute::Configurable;
+    define_native_function(vm.names.revocable, revocable, 2, attr);
 }
 
 ProxyConstructor::~ProxyConstructor()
@@ -38,18 +55,33 @@ Value ProxyConstructor::call()
 Value ProxyConstructor::construct(Function&)
 {
     auto& vm = this->vm();
-    auto target = vm.argument(0);
-    auto handler = vm.argument(1);
+    return proxy_create(global_object(), vm.argument(0), vm.argument(1));
+}
 
-    if (!target.is_object()) {
-        vm.throw_exception<TypeError>(global_object(), ErrorType::ProxyConstructorBadType, "target", target.to_string_without_side_effects());
+// 28.2.2.1 Proxy.revocable, https://tc39.es/ecma262/multipage/reflection.html#sec-proxy.revocable
+JS_DEFINE_NATIVE_FUNCTION(ProxyConstructor::revocable)
+{
+    auto* proxy = proxy_create(global_object, vm.argument(0), vm.argument(1));
+    if (vm.exception())
         return {};
-    }
-    if (!handler.is_object()) {
-        vm.throw_exception<TypeError>(global_object(), ErrorType::ProxyConstructorBadType, "handler", handler.to_string_without_side_effects());
-        return {};
-    }
-    return ProxyObject::create(global_object(), target.as_object(), handler.as_object());
+
+    // 28.2.2.1.1 Proxy Revocation Functions, https://tc39.es/ecma262/multipage/reflection.html#sec-proxy-revocation-functions
+    auto* revoker = NativeFunction::create(global_object, "", [proxy_handle = make_handle(proxy)](auto&, auto&) -> Value {
+        auto& proxy = const_cast<ProxyObject&>(*proxy_handle.cell());
+        if (proxy.is_revoked())
+            return js_undefined();
+        // NOTE: The spec wants us to unset [[ProxyTarget]] and [[ProxyHandler]],
+        // which is their way of revoking the Proxy - this might affect GC-ability,
+        // but AFAICT not doing that should be ok compatibility-wise.
+        proxy.revoke();
+        return js_undefined();
+    });
+    revoker->define_property(vm.names.length, Value(0));
+
+    auto* result = Object::create_empty(global_object);
+    result->define_property(vm.names.proxy, proxy);
+    result->define_property(vm.names.revoke, revoker);
+    return result;
 }
 
 }
