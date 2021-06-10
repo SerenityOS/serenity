b2cccf5fed24ab623a5a07368ea71050579b77e4
Ali Mohammad Pur <ali.mpfard@gmail.com>
2021-06-11 01:32:48 +0430

    AK: Make NonnullRefPtrVector constructible from Vector<NonnullRefPtr>

--- AK/NonnullRefPtrVector.h
+++ AK/NonnullRefPtrVector.h
@@ -13,6 +13,7 @@ namespace AK {
 
 template<typename T, size_t inline_capacity>
 class NonnullRefPtrVector : public NonnullPtrVector<NonnullRefPtr<T>, inline_capacity> {
+    using NonnullPtrVector<NonnullRefPtr<T>, inline_capacity>::NonnullPtrVector;
 };
 
 }
