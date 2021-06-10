2b207ac1d0563612999a0e88f784741a654e8cf7
Jelle Raaijmakers <jelle@gmta.nl>
2021-06-11 01:17:50 +0200

    Ports/OpenSSL: Switch to sha256 signature check

--- Ports/openssl/package.sh
+++ Ports/openssl/package.sh
@@ -4,11 +4,8 @@ branch='1.0.2'
 version="${branch}t"
 useconfigure=true
 configscript=Configure
-files="https://ftp.openssl.org/source/old/${branch}/openssl-${version}.tar.gz openssl-${version}.tar.gz
-https://ftp.openssl.org/source/old/${branch}/openssl-${version}.tar.gz.asc openssl-${version}.tar.gz.asc"
-auth_type="sig"
-auth_import_key="8657ABB260F056B1E5190839D9C4D26D0E604491"
-auth_opts="openssl-${version}.tar.gz.asc openssl-${version}.tar.gz"
+files="https://ftp.openssl.org/source/old/${branch}/openssl-${version}.tar.gz openssl-${version}.tar.gz 14cb464efe7ac6b54799b34456bd69558a749a4931ecfd9cf9f71d7881cac7bc"
+auth_type=sha256
 
 depends="zlib"
 configopts="--prefix=/usr/local --install_prefix=${SERENITY_INSTALL_ROOT} -DOPENSSL_SYS_SERENITY=1 linux-elf zlib no-tests no-threads no-asm"
