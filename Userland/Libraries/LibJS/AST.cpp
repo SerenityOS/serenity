3b780942ffbb050937c344c5fbc5c77b0acd65a3
Andreas Kling <kling@serenityos.org>
2021-06-14 09:35:04 +0200

    LibJS: Access function arguments directly in AST interpreter
    Instead of doing a generic scoped variable lookup, function arguments
    now go directly to the call frame arguments list.
    
    This is a huge speedup on everything that uses arguments. :^)

--- Userland/Libraries/LibJS/AST.cpp
+++ Userland/Libraries/LibJS/AST.cpp
@@ -1301,6 +1301,9 @@ Value Identifier::execute(Interpreter& interpreter, GlobalObject& global_object)
 {
     InterpreterNodeScope node_scope { interpreter, *this };
 
+    if (m_argument_index.has_value())
+        return interpreter.vm().argument(m_argument_index.value());
+
     auto value = interpreter.vm().get_variable(string(), global_object);
     if (value.is_empty()) {
         if (!interpreter.exception())
