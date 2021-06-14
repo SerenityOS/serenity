5122b55133934614b557fe593cc43d56354f97c3
Idan Horowitz <idan.horowitz@gmail.com>
2021-06-15 02:40:55 +0300

    LibJS: Store and return undefined Symbol description
    Instead of the current incorrect behaviour of just defaulting to an
    empty string.

--- Userland/Libraries/LibJS/Runtime/Symbol.cpp
+++ Userland/Libraries/LibJS/Runtime/Symbol.cpp
@@ -10,7 +10,7 @@
 
 namespace JS {
 
-Symbol::Symbol(String description, bool is_global)
+Symbol::Symbol(Optional<String> description, bool is_global)
     : m_description(move(description))
     , m_is_global(is_global)
 {
@@ -20,12 +20,12 @@ Symbol::~Symbol()
 {
 }
 
-Symbol* js_symbol(Heap& heap, String description, bool is_global)
+Symbol* js_symbol(Heap& heap, Optional<String> description, bool is_global)
 {
     return heap.allocate_without_global_object<Symbol>(move(description), is_global);
 }
 
-Symbol* js_symbol(VM& vm, String description, bool is_global)
+Symbol* js_symbol(VM& vm, Optional<String> description, bool is_global)
 {
     return js_symbol(vm.heap(), move(description), is_global);
 }
