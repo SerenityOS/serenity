bc38382453c092421f0851f3ca47165f1df3a628
Andreas Kling <kling@serenityos.org>
2021-06-14 16:51:29 +0200

    PixelPaint: Add a new "Rectangle Select" tool :^)
    This patch only adds the tool along with a toolbar icon for it.
    It doesn't do anything yet.

---
+++ Userland/Applications/PixelPaint/RectangleSelectTool.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2021, Andreas Kling <kling@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include "Tool.h"
+
+namespace PixelPaint {
+
+class RectangleSelectTool final : public Tool {
+public:
+    RectangleSelectTool();
+    virtual ~RectangleSelectTool();
+
+    virtual void on_mousedown(Layer&, GUI::MouseEvent&, GUI::MouseEvent&) override;
+    virtual void on_mousemove(Layer&, GUI::MouseEvent&, GUI::MouseEvent&) override;
+    virtual void on_mouseup(Layer&, GUI::MouseEvent&, GUI::MouseEvent&) override;
+    virtual void on_second_paint(Layer const&, GUI::PaintEvent&) override;
+};
+
+}
