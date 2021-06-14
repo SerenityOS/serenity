d1cc39edb579b62a418609949d8aaad58e1a7a94
Andreas Kling <kling@serenityos.org>
2021-06-14 17:59:15 +0200

    PixelPaint: Add copy action (copies the selection from active layer)
    You can now select a part of a layer, copy it, and then paste it as
    a new layer. Very cool :^)

--- Userland/Applications/PixelPaint/Layer.cpp
+++ Userland/Applications/PixelPaint/Layer.cpp
@@ -6,6 +6,7 @@
 
 #include "Layer.h"
 #include "Image.h"
+#include "Selection.h"
 #include <LibGfx/Bitmap.h>
 
 namespace PixelPaint {
@@ -87,6 +88,13 @@ void Layer::set_name(String name)
         return;
     m_name = move(name);
     m_image.layer_did_modify_properties({}, *this);
+}
+
+RefPtr<Gfx::Bitmap> Layer::try_copy_bitmap(Selection const& selection) const
+{
+    auto bounding_rect = selection.bounding_rect().translated(-m_location);
+    // FIXME: This needs to be smarter once we add more complex selections.
+    return m_bitmap->cropped(bounding_rect);
 }
 
 }
