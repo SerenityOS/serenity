ccedec82523ea9725c8039b451ac84c4d7ec3c79
Peter Bocan <me@pbocan.net>
2021-06-15 23:28:58 +0100

    Crypto: Create testing libraries for LibCrypto and LibTLS

---
+++ Tests/LibCrypto/TestDiffieHellman.cpp
@@ -0,0 +1,18 @@
+#include <LibCrypto/KeyExchange/DH.h>
+#include <LibTest/TestCase.h>
+
+TEST_CASE(test_DH)
+{
+//    EXPECT_EQ(false, true);
+//    // 2^100 mod 251 = 1
+//    Crypto::KeyExchange::DHKey publicKey { 2, 0, 0, 0};
+//    Crypto::KeyExchange::DHKey privateKey { 0, 100, 251, 0, };
+//    Crypto::KeyExchange::DH dh{ publicKey, privateKey };
+//
+//    u8 buf[0xfff];
+//    Bytes sharedKey{ buf, 0xfff };
+//    dh.generate_shared_secret(sharedKey);
+//
+//    EXPECT_EQ(sharedKey.size(), 1u);
+//    EXPECT_EQ(sharedKey[0], 1u);
+}
