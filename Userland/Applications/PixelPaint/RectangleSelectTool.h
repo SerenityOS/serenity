606c201f0940c8b14e67addb6798cb2dca1fd8bf
Elliot Maisl <elliot.maisl@gmail.com>
2021-06-17 00:00:23 +0200

    PixelPaint: Add new selection moving modes
    If you press "spacebar" while moving a selection, it will now move the
    origin point of the selection; and if you press "control" it will move
    it relatively to the center.

--- Userland/Applications/PixelPaint/RectangleSelectTool.h
+++ Userland/Applications/PixelPaint/RectangleSelectTool.h
@@ -18,10 +18,19 @@ public:
     virtual void on_mousedown(Layer&, GUI::MouseEvent&, GUI::MouseEvent&) override;
     virtual void on_mousemove(Layer&, GUI::MouseEvent&, GUI::MouseEvent&) override;
     virtual void on_mouseup(Layer&, GUI::MouseEvent&, GUI::MouseEvent&) override;
+    virtual void on_keydown(GUI::KeyEvent&) override;
+    virtual void on_keyup(GUI::KeyEvent&) override;
     virtual void on_second_paint(Layer const&, GUI::PaintEvent&) override;
 
 private:
+    enum class MovingMode {
+        MovingOrigin,
+        AroundCenter,
+        None,
+    };
+
     bool m_selecting { false };
+    MovingMode m_moving_mode { MovingMode::None };
     Gfx::IntPoint m_selection_start;
     Gfx::IntPoint m_selection_end;
 };
