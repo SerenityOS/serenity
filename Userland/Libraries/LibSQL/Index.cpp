ba26fc7726ccbb7c60073dfa78a61235e403086e
Jan de Visser <jan@de-visser.net>
2021-06-17 13:47:42 -0400

    LibSQL: BTree index, Heap, and Meta objects for SQL Storage layer
    Unfortunately this patch is quite large.
    
    The main functionality included are a BTree index implementation and
    the Heap class which manages persistent storage.
    
    Also included are a Key subclass of the Tuple class, which is a
    specialization for index key tuples. This "dragged in" the Meta layer,
    which has classes defining SQL objects like tables and indexes.

---
+++ Userland/Libraries/LibSQL/Index.cpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2021, Jan de Visser <jan@de-visser.net>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibSQL/Heap.h>
+#include <LibSQL/Index.h>
+#include <LibSQL/Meta.h>
+
+namespace SQL {
+
+Index::Index(Heap& heap, TupleDescriptor const& descriptor, bool unique, u32 pointer)
+    : m_heap(heap)
+    , m_descriptor(descriptor)
+    , m_unique(unique)
+    , m_pointer(pointer)
+{
+}
+
+Index::Index(Heap& heap, TupleDescriptor const& descriptor, u32 pointer)
+    : m_heap(heap)
+    , m_descriptor(descriptor)
+    , m_pointer(pointer)
+{
+}
+
+ByteBuffer Index::read_block(u32 block)
+{
+    auto ret = m_heap.read_block(block);
+    if (ret.is_error()) {
+        warnln("Error reading block {}: {}", block, ret.error());
+        VERIFY_NOT_REACHED();
+    }
+    return ret.value();
+}
+
+void Index::add_to_write_ahead_log(IndexNode* node)
+{
+    VERIFY(node->pointer());
+    ByteBuffer buffer;
+    node->serialize(buffer);
+    m_heap.add_to_wal(node->pointer(), buffer);
+}
+
+}
---
+++ Userland/Libraries/LibSQL/Index.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2021, Jan de Visser <jan@de-visser.net>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibCore/Object.h>
+#include <LibSQL/Forward.h>
+#include <LibSQL/Meta.h>
+
+namespace SQL {
+
+class IndexNode {
+public:
+    virtual ~IndexNode() = default;
+    [[nodiscard]] u32 pointer() const { return m_pointer; }
+    virtual void serialize(ByteBuffer&) const = 0;
+    virtual IndexNode* as_index_node() = 0;
+
+protected:
+    explicit IndexNode(u32 pointer)
+        : m_pointer(pointer)
+    {
+    }
+
+    void set_pointer(u32 pointer) { m_pointer = pointer; }
+
+private:
+    u32 m_pointer;
+};
+
+class Index : public Core::Object {
+    C_OBJECT_ABSTRACT(Index);
+
+public:
+    ~Index() override = default;
+
+    TupleDescriptor descriptor() const { return m_descriptor; }
+    [[nodiscard]] bool duplicates_allowed() const { return !m_unique; }
+    [[nodiscard]] bool unique() const { return m_unique; }
+    [[nodiscard]] u32 pointer() const { return m_pointer; }
+
+protected:
+    Index(Heap& heap, TupleDescriptor const&, bool unique, u32 pointer);
+    Index(Heap& heap, TupleDescriptor const&, u32 pointer);
+
+    [[nodiscard]] Heap const& heap() const { return m_heap; }
+    [[nodiscard]] Heap& heap() { return m_heap; }
+    void set_pointer(u32 pointer) { m_pointer = pointer; }
+    u32 new_record_pointer() { return m_heap.new_record_pointer(); }
+    ByteBuffer read_block(u32);
+    void add_to_write_ahead_log(IndexNode*);
+
+private:
+    Heap& m_heap;
+    TupleDescriptor m_descriptor;
+    bool m_unique { false };
+    u32 m_pointer { 0 };
+};
+
+}
