528d38caa46fb5f49c3c073d279af1495de9726b
Luke <luke.wilde@live.co.uk>
2021-06-18 03:56:57 +0100

    LibJS: Add %TypedArray%.prototype.every

--- Userland/Libraries/LibJS/Runtime/TypedArrayPrototype.cpp
+++ Userland/Libraries/LibJS/Runtime/TypedArrayPrototype.cpp
@@ -26,6 +26,7 @@ void TypedArrayPrototype::initialize(GlobalObject& object)
     define_native_accessor(vm.names.byteLength, byte_length_getter, nullptr, Attribute::Configurable);
     define_native_accessor(vm.names.byteOffset, byte_offset_getter, nullptr, Attribute::Configurable);
     define_native_function(vm.names.at, at, 1, attr);
+    define_native_function(vm.names.every, every, 1, attr);
 }
 
 TypedArrayPrototype::~TypedArrayPrototype()
@@ -42,6 +43,49 @@ static TypedArrayBase* typed_array_from(VM& vm, GlobalObject& global_object)
         return nullptr;
     }
     return static_cast<TypedArrayBase*>(this_object);
+}
+
+static Function* callback_from_args(GlobalObject& global_object, const String& name)
+{
+    auto& vm = global_object.vm();
+    if (vm.argument_count() < 1) {
+        vm.throw_exception<TypeError>(global_object, ErrorType::TypedArrayPrototypeOneArg, name);
+        return nullptr;
+    }
+    auto callback = vm.argument(0);
+    if (!callback.is_function()) {
+        vm.throw_exception<TypeError>(global_object, ErrorType::NotAFunction, callback.to_string_without_side_effects());
+        return nullptr;
+    }
+    return &callback.as_function();
+}
+
+static void for_each_item(VM& vm, GlobalObject& global_object, const String& name, AK::Function<IterationDecision(size_t index, Value value, Value callback_result)> callback)
+{
+    auto* typed_array = typed_array_from(vm, global_object);
+    if (!typed_array)
+        return;
+
+    auto initial_length = typed_array->array_length();
+
+    auto* callback_function = callback_from_args(global_object, name);
+    if (!callback_function)
+        return;
+
+    auto this_value = vm.argument(1);
+
+    for (size_t i = 0; i < initial_length; ++i) {
+        auto value = typed_array->get(i);
+        if (vm.exception())
+            return;
+
+        auto callback_result = vm.call(*callback_function, this_value, value, Value((i32)i), typed_array);
+        if (vm.exception())
+            return;
+
+        if (callback(i, value, callback_result) == IterationDecision::Break)
+            break;
+    }
 }
 
 // 23.2.3.18 get %TypedArray%.prototype.length, https://tc39.es/ecma262/#sec-get-%typedarray%.prototype.length
@@ -79,6 +123,20 @@ JS_DEFINE_NATIVE_FUNCTION(TypedArrayPrototype::at)
     if (index.has_overflow() || index.value() >= length)
         return js_undefined();
     return typed_array->get(index.value());
+}
+
+// 23.2.3.7 %TypedArray%.prototype.every ( callbackfn [ , thisArg ] ), https://tc39.es/ecma262/#sec-%typedarray%.prototype.every
+JS_DEFINE_NATIVE_FUNCTION(TypedArrayPrototype::every)
+{
+    auto result = true;
+    for_each_item(vm, global_object, "every", [&](auto, auto, auto callback_result) {
+        if (!callback_result.to_boolean()) {
+            result = false;
+            return IterationDecision::Break;
+        }
+        return IterationDecision::Continue;
+    });
+    return Value(result);
 }
 
 // 23.2.3.1 get %TypedArray%.prototype.buffer, https://tc39.es/ecma262/#sec-get-%typedarray%.prototype.buffer
