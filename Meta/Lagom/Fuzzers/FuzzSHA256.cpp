f29036dc9870f7e4842150f16f6562a21f83d31f
Luke <luke.wilde@live.co.uk>
2021-06-18 16:31:31 +0100

    Lagom/Fuzzers: Add fuzzers for all current hashing functions
    Namely MD5, SHA1, SHA256, SHA384 and SHA512.

---
+++ Meta/Lagom/Fuzzers/FuzzSHA256.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021, Luke Wilde <lukew@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibCrypto/Hash/SHA2.h>
+#include <stddef.h>
+#include <stdint.h>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    auto buffer = ByteBuffer::copy(data, size);
+    Crypto::Hash::SHA256::hash(buffer);
+    return 0;
+}
