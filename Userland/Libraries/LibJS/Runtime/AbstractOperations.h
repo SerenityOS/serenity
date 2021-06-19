a17be6da9aeae2b1683259f6adc3229c917544a1
Linus Groh <mail@linusgroh.de>
2021-06-19 22:34:48 +0100

    LibJS: Implement the OrdinaryCreateFromConstructor() abstract operation

--- Userland/Libraries/LibJS/Runtime/AbstractOperations.h
+++ Userland/Libraries/LibJS/Runtime/AbstractOperations.h
@@ -8,6 +8,7 @@
 
 #include <AK/Forward.h>
 #include <LibJS/Forward.h>
+#include <LibJS/Runtime/GlobalObject.h>
 #include <LibJS/Runtime/Value.h>
 
 namespace JS {
@@ -19,5 +20,16 @@ MarkedValueList create_list_from_array_like(GlobalObject&, Value, AK::Function<R
 Function* species_constructor(GlobalObject&, Object const&, Function& default_constructor);
 GlobalObject* get_function_realm(GlobalObject&, Function const&);
 Object* get_prototype_from_constructor(GlobalObject&, Function const& constructor, Object* (GlobalObject::*intrinsic_default_prototype)());
+
+// 10.1.13 OrdinaryCreateFromConstructor ( constructor, intrinsicDefaultProto [ , internalSlotsList ] ), https://tc39.es/ecma262/#sec-ordinarycreatefromconstructor
+template<typename T, typename... Args>
+T* ordinary_create_from_constructor(GlobalObject& global_object, Function const& constructor, Object* (GlobalObject::*intrinsic_default_prototype)(), Args&&... args)
+{
+    auto& vm = global_object.vm();
+    auto* prototype = get_prototype_from_constructor(global_object, constructor, intrinsic_default_prototype);
+    if (vm.exception())
+        return nullptr;
+    return global_object.heap().allocate<T>(global_object, forward<Args>(args)..., *prototype);
+}
 
 }
