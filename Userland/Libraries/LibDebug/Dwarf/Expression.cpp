7fb875192f435c226d7d4b95284626b6cf22aa15
Gunnar Beutner <gbeutner@serenityos.org>
2021-06-23 21:47:19 +0200

    Userland: Add more TODO()s for arch-specific code
    This enables building more of the userspace applications for x86_64.

--- Userland/Libraries/LibDebug/Dwarf/Expression.cpp
+++ Userland/Libraries/LibDebug/Dwarf/Expression.cpp
@@ -12,7 +12,7 @@
 
 namespace Debug::Dwarf::Expression {
 
-Value evaluate(ReadonlyBytes bytes, PtraceRegisters const& regs)
+Value evaluate(ReadonlyBytes bytes, [[maybe_unused]] PtraceRegisters const& regs)
 {
     InputMemoryStream stream(bytes);
 
@@ -21,6 +21,7 @@ Value evaluate(ReadonlyBytes bytes, PtraceRegisters const& regs)
         stream >> opcode;
 
         switch (static_cast<Operations>(opcode)) {
+#if ARCH(I386)
         case Operations::RegEbp: {
             ssize_t offset = 0;
             stream.read_LEB128_signed(offset);
@@ -32,6 +33,7 @@ Value evaluate(ReadonlyBytes bytes, PtraceRegisters const& regs)
             stream.read_LEB128_signed(offset);
             return Value { Type::UnsignedIntetger, regs.ebp + 2 * sizeof(size_t) + offset };
         }
+#endif
 
         default:
             dbgln("DWARF expr addr: {}", (const void*)bytes.data());
