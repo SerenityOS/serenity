627e2ed70ca25e60185f8c9cfa8b63a7f6b59277
Gunnar Beutner <gbeutner@serenityos.org>
2021-06-24 13:08:19 +0200

    Kernel: Build the x86_64 kernel as an ELF32 executable
    Multiboot only supports ELF32 executables. This changes the build
    process to build an ELF32 executable which has a 32-bit entry point,
    but consists of mostly 64-bit code.

--- Kernel/mkmap.sh
+++ Kernel/mkmap.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 tmp=$(mktemp)
-nm -n Kernel | awk '{ if ($2 != "a") print; }' | uniq > "$tmp"
+if [ -f Kernel32 ]; then
+    kernel_binary=Kernel32
+else
+    kernel_binary=Kernel64
+fi
+nm -n $kernel_binary | awk '{ if ($2 != "a") print; }' | uniq > "$tmp"
 printf "%08x\n" "$(wc -l "$tmp" | cut -f1 -d' ')" > kernel.map
 cat "$tmp" >> kernel.map
 rm -f "$tmp"
