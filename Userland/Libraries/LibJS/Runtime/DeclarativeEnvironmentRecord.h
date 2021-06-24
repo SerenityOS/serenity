ce2747692d3d8b0b630f55367017a0891a0cc6af
Andreas Kling <kling@serenityos.org>
2021-06-24 13:28:15 +0200

    LibJS: Remove unused DeclarativeEnvironmentRecord::type()
    Nothing was using this, and we now have separate classes for the
    different types of environment records instead.

--- Userland/Libraries/LibJS/Runtime/DeclarativeEnvironmentRecord.h
+++ Userland/Libraries/LibJS/Runtime/DeclarativeEnvironmentRecord.h
@@ -25,18 +25,9 @@ class DeclarativeEnvironmentRecord : public EnvironmentRecord {
     JS_ENVIRONMENT_RECORD(DeclarativeEnvironmentRecord, EnvironmentRecord);
 
 public:
-    enum class EnvironmentRecordType {
-        Declarative,
-        Function,
-        Object,
-        Module,
-    };
-
     DeclarativeEnvironmentRecord();
-    DeclarativeEnvironmentRecord(EnvironmentRecordType);
     explicit DeclarativeEnvironmentRecord(EnvironmentRecord* parent_scope);
     DeclarativeEnvironmentRecord(HashMap<FlyString, Variable> variables, EnvironmentRecord* parent_scope);
-    DeclarativeEnvironmentRecord(HashMap<FlyString, Variable> variables, EnvironmentRecord* parent_scope, EnvironmentRecordType);
     virtual ~DeclarativeEnvironmentRecord() override;
 
     // ^EnvironmentRecord
@@ -45,8 +36,6 @@ public:
     virtual bool delete_from_environment_record(FlyString const&) override;
 
     HashMap<FlyString, Variable> const& variables() const { return m_variables; }
-
-    EnvironmentRecordType type() const { return m_environment_record_type; }
 
     virtual bool has_binding(FlyString const& name) const override;
     virtual void create_mutable_binding(GlobalObject&, FlyString const& name, bool can_be_deleted) override;
@@ -62,7 +51,6 @@ protected:
 private:
     virtual bool is_declarative_environment_record() const override { return true; }
 
-    EnvironmentRecordType m_environment_record_type : 8 { EnvironmentRecordType::Declarative };
     HashMap<FlyString, Variable> m_variables;
     HashMap<FlyString, Binding> m_bindings;
 };
