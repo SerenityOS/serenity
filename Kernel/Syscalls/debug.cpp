f4090d46de6e7b10d817aa67add21c37e26f4c9a
Andreas Kling <kling@serenityos.org>
2021-06-27 10:46:47 +0200

    Kernel: Don't kmalloc() for small (<=1024) dbgputstr() syscalls

--- Kernel/Syscalls/debug.cpp
+++ Kernel/Syscalls/debug.cpp
@@ -28,10 +28,18 @@ KResultOr<size_t> Process::sys$dbgputstr(Userspace<const u8*> characters, size_t
     if (size == 0)
         return 0;
 
+    if (size <= 1024) {
+        char buffer[1024];
+        if (!copy_from_user(buffer, characters, size))
+            return EFAULT;
+        dbgputstr(buffer, size);
+        return size;
+    }
+
     auto result = try_copy_kstring_from_user(reinterpret_cast<char const*>(characters.unsafe_userspace_ptr()), size);
     if (result.is_error())
         return result.error();
-    dbgputstr(reinterpret_cast<const char*>(result.value()->characters()), size);
+    dbgputstr(result.value()->characters(), size);
     return size;
 }
 
