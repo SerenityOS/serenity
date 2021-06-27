7a5d183022b332dd43fb5472e80ecd438cdccc83
Gunnar Beutner <gbeutner@serenityos.org>
2021-06-27 13:59:41 +0200

    Kernel: Implement initializing threads on x86_64

--- Kernel/Arch/x86/x86_64/CPU.cpp
+++ Kernel/Arch/x86/x86_64/CPU.cpp
@@ -19,18 +19,12 @@ namespace Kernel {
 // The compiler can't see the calls to these functions inside assembly.
 // Declare them, to avoid dead code warnings.
 extern "C" void enter_thread_context(Thread* from_thread, Thread* to_thread) __attribute__((used));
-extern "C" void context_first_init(Thread* from_thread, Thread* to_thread, TrapFrame* trap) __attribute__((used));
 extern "C" u32 do_init_context(Thread* thread, u32 flags) __attribute__((used));
 
 extern "C" void enter_thread_context(Thread* from_thread, Thread* to_thread)
 {
     (void)from_thread;
     (void)to_thread;
-    TODO();
-}
-
-extern "C" void context_first_init([[maybe_unused]] Thread* from_thread, [[maybe_unused]] Thread* to_thread, [[maybe_unused]] TrapFrame* trap)
-{
     TODO();
 }
 
