302ebdc730770badb6d89b625f3f7896aecf1d3d
davidot <david.tuin@gmail.com>
2021-06-27 19:14:11 +0200

    LibJS: Add String.prototype.indexOf position argument

--- Userland/Libraries/LibJS/Tests/builtins/String/String.prototype.indexOf.js
+++ Userland/Libraries/LibJS/Tests/builtins/String/String.prototype.indexOf.js
@@ -5,4 +5,19 @@ test("basic functionality", () => {
 
     expect(s.indexOf("friends")).toBe(6);
     expect(s.indexOf("enemies")).toBe(-1);
+
+    expect(s.indexOf("friends", 0)).toBe(6);
+    expect(s.indexOf("enemies", 0)).toBe(-1);
+
+    expect(s.indexOf("friends", 4)).toBe(6);
+    expect(s.indexOf("friends", 6)).toBe(6);
+    expect(s.indexOf("friends", 7)).toBe(-1);
+    expect(s.indexOf("friends", 8)).toBe(-1);
+
+    expect(s.indexOf("enemies", 2)).toBe(-1);
+    expect(s.indexOf("enemies", 7)).toBe(-1);
+
+    expect(s.indexOf("e")).toBe(1);
+    expect(s.indexOf("e", 0)).toBe(1);
+    expect(s.indexOf("e", 2)).toBe(9);
 });
