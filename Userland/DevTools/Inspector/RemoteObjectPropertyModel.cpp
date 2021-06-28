ed9614c2b8e24b9236e7fea056b09b75904ccce9
Max Wipfli <mail@maxwipfli.ch>
2021-06-28 11:57:37 +0200

    AK+Everywhere: Change int to size_t in JsonObject and JsonArray

--- Userland/DevTools/Inspector/RemoteObjectPropertyModel.cpp
+++ Userland/DevTools/Inspector/RemoteObjectPropertyModel.cpp
@@ -22,7 +22,7 @@ int RemoteObjectPropertyModel::row_count(const GUI::ModelIndex& index) const
             return value.as_array().size();
         else if (value.is_object())
             return value.as_object().size();
-        return 0;
+        return (size_t)0;
     };
 
     if (index.is_valid()) {
