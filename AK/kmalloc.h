a482a3e60915c389bbcb5eb6a54a82b7f607c13f
Daniel Bertalan <dani@danielbertalan.dev>
2021-06-29 22:57:27 +0200

    AK: Declare operators `new` and `delete` as global functions
    This fixes a build issue introduced in 23d66fe, where the compiler
    statically detected that that mismatching new and delete operators were
    used.
    
    Clang generates a warning for this, for the reasons described in the
    comment in `AK/kmalloc.cpp`, but GCC does not.
    
    Besides moving the allocator functions into a `.cpp` file, declarations
    in `AK/kmalloc.cpp` were reordered to have imports at the top, in order
    to make the code more readable.

--- AK/kmalloc.h
+++ AK/kmalloc.h
@@ -1,13 +1,27 @@
 /*
  * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2021, Daniel Bertalan <dani@danielbertalan.dev>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #pragma once
 
-#ifndef __serenity__
+#if defined(KERNEL)
+#    include <Kernel/Heap/kmalloc.h>
+#else
 #    include <new>
+#    include <stdlib.h>
+
+#    define kcalloc calloc
+#    define kmalloc malloc
+#    define kmalloc_good_size malloc_good_size
+#    define kfree free
+#    define krealloc realloc
+#endif
+
+#ifndef __serenity__
+#    include <AK/Types.h>
 
 #    ifndef AK_OS_MACOS
 extern "C" {
@@ -26,70 +40,6 @@ inline size_t malloc_good_size(size_t size) { return size; }
     private:
 #else
 #    define AK_MAKE_ETERNAL
-#endif
-
-#if defined(KERNEL)
-#    include <Kernel/Heap/kmalloc.h>
-#else
-#    include <stdlib.h>
-
-#    define kcalloc calloc
-#    define kmalloc malloc
-#    define kmalloc_good_size malloc_good_size
-#    define kfree free
-#    define krealloc realloc
-
-#    ifdef __serenity__
-
-#        include <AK/Assertions.h>
-#        include <new>
-
-inline void* operator new(size_t size)
-{
-    void* ptr = kmalloc(size);
-    VERIFY(ptr);
-    return ptr;
-}
-
-inline void* operator new(size_t size, const std::nothrow_t&) noexcept
-{
-    return kmalloc(size);
-}
-
-inline void operator delete(void* ptr) noexcept
-{
-    return kfree(ptr);
-}
-
-inline void operator delete(void* ptr, size_t) noexcept
-{
-    return kfree(ptr);
-}
-
-inline void* operator new[](size_t size)
-{
-    void* ptr = kmalloc(size);
-    VERIFY(ptr);
-    return ptr;
-}
-
-inline void* operator new[](size_t size, const std::nothrow_t&) noexcept
-{
-    return kmalloc(size);
-}
-
-inline void operator delete[](void* ptr) noexcept
-{
-    return kfree(ptr);
-}
-
-inline void operator delete[](void* ptr, size_t) noexcept
-{
-    return kfree(ptr);
-}
-
-#    endif
-
 #endif
 
 using std::nothrow;
