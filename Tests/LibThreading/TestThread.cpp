dd3996e20755d1a970963ab899b58f7f4696c270
Spencer Dixon <spencercdixon@gmail.com>
2021-07-02 08:06:48 -0400

    Tests+LibThreading: Add new tests for LibThreading for detach()

---
+++ Tests/LibThreading/TestThread.cpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2021, Spencer Dixon <spencercdixon@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibTest/TestCase.h>
+#include <LibThreading/Thread.h>
+#include <unistd.h>
+
+TEST_CASE(threads_can_detach)
+{
+    int should_be_42 = 0;
+
+    auto thread = Threading::Thread::construct([&should_be_42]() {
+        usleep(10 * 1000);
+        should_be_42 = 42;
+        return 0;
+    });
+    thread->start();
+    thread->detach();
+    usleep(20 * 1000);
+
+    EXPECT(should_be_42 == 42);
+}
+
+TEST_CASE(joining_detached_thread_errors)
+{
+    auto thread = Threading::Thread::construct([]() { return 0; });
+    thread->start();
+    thread->detach();
+
+    EXPECT(thread->join().is_error());
+}
