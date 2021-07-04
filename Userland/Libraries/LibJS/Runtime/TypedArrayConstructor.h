fac8f9a94d98baa435cc6ad4ee6fe9bab93ae87f
Idan Horowitz <idan.horowitz@gmail.com>
2021-07-05 01:25:40 +0300

    LibJS: Add the TypedArray.from() method

--- Userland/Libraries/LibJS/Runtime/TypedArrayConstructor.h
+++ Userland/Libraries/LibJS/Runtime/TypedArrayConstructor.h
@@ -25,6 +25,7 @@ public:
 private:
     virtual bool has_constructor() const override { return true; }
 
+    JS_DECLARE_NATIVE_FUNCTION(from);
     JS_DECLARE_NATIVE_FUNCTION(of);
     JS_DECLARE_NATIVE_GETTER(symbol_species_getter);
 };
