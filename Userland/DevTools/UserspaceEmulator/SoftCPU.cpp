c1018b7ea9fe564ec2533076e7d80ee80f82b013
Daniel Bertalan <dani@danielbertalan.dev>
2021-07-05 19:35:52 +0200

    UserspaceEmulator: Use long double in `FABS`
    `fpu_get` returns a long double and `fpu_set` expects a long double as
    its parameter, and the X87 FPU uses long doubles as its internal
    storage, meaning the `FABS` operates on them. This means the correct
    intrinsic function for implementing it is `__builtin_fabsl`.

--- Userland/DevTools/UserspaceEmulator/SoftCPU.cpp
+++ Userland/DevTools/UserspaceEmulator/SoftCPU.cpp
@@ -1565,7 +1565,7 @@ void SoftCPU::FCHS(const X86::Instruction&)
 
 void SoftCPU::FABS(const X86::Instruction&)
 {
-    fpu_set(0, __builtin_fabs(fpu_get(0)));
+    fpu_set(0, __builtin_fabsl(fpu_get(0)));
 }
 
 void SoftCPU::FTST(const X86::Instruction&) { TODO_INSN(); }
