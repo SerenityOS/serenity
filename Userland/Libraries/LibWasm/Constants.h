5b8a6823c0b5b8b0c2d863ec19210509980864fd
Ali Mohammad Pur <ali.mpfard@gmail.com>
2021-07-06 14:07:52 +0430

    LibWasm: Tweak the implementation-defined limits a bit

--- Userland/Libraries/LibWasm/Constants.h
+++ Userland/Libraries/LibWasm/Constants.h
@@ -36,8 +36,9 @@ static constexpr auto extern_global_tag = 0x03;
 
 static constexpr auto page_size = 64 * KiB;
 
-// Limits
-static constexpr auto max_allowed_call_stack_depth = 1000;
-static constexpr auto max_allowed_executed_instructions_per_call = 1024 * 1024 * 1024;
+// Implementation-defined limits
+// These are not concretely defined by the spec, so the values are only defined by us.
+static constexpr auto max_allowed_call_stack_depth = 512;
+static constexpr auto max_allowed_executed_instructions_per_call = 256 * 1024 * 1024;
 
 }
