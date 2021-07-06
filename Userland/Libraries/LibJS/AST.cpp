38da5706558e8eec7268b9cb202e6a11095f8d6a
Linus Groh <mail@linusgroh.de>
2021-07-06 12:21:12 +0100

    Revert "LibJS: Don't hoist functions under certain circumstances"
    This reverts commit 3411d50737725b382ae526e91a8bbd60656c3323.
    
    It was causing LeakSanitizer on CI to fail, possibly due to a circular
    reference.

--- Userland/Libraries/LibJS/AST.cpp
+++ Userland/Libraries/LibJS/AST.cpp
@@ -2377,9 +2377,9 @@ void ScopeNode::add_functions(NonnullRefPtrVector<FunctionDeclaration> functions
     m_functions.extend(move(functions));
 }
 
-void ScopeNode::add_hoisted_function(NonnullRefPtr<FunctionDeclaration> hoisted_function)
+void ScopeNode::add_hoisted_functions(NonnullRefPtrVector<FunctionDeclaration> hoisted_functions)
 {
-    m_hoisted_functions.append(hoisted_function);
+    m_hoisted_functions.extend(move(hoisted_functions));
 }
 
 }
