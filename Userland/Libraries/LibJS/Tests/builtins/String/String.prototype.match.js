8636382d6ee9175a5cdb8f693c9e82d3e938e14a
Timothy Flynn <trflynn89@pm.me>
2021-07-07 13:18:52 -0400

    LibJS: Implement RegExp.prototype.match with RegExpExec abstraction

--- Userland/Libraries/LibJS/Tests/builtins/String/String.prototype.match.js
+++ Userland/Libraries/LibJS/Tests/builtins/String/String.prototype.match.js
@@ -4,3 +4,38 @@ test("basic functionality", () => {
     expect("hello friends".match(/hello/)).not.toBeNull();
     expect("hello friends".match(/enemies/)).toBeNull();
 });
+
+test("override exec with function", () => {
+    let calls = 0;
+
+    let re = /test/;
+    let oldExec = re.exec.bind(re);
+    re.exec = function (...args) {
+        ++calls;
+        return oldExec(...args);
+    };
+
+    expect("test".match(re)).not.toBeNull();
+    expect(calls).toBe(1);
+});
+
+test("override exec with bad function", () => {
+    let calls = 0;
+
+    let re = /test/;
+    re.exec = function (...args) {
+        ++calls;
+        return 4;
+    };
+
+    expect(() => {
+        "test".match(re);
+    }).toThrow(TypeError);
+    expect(calls).toBe(1);
+});
+
+test("override exec with non-function", () => {
+    let re = /test/;
+    re.exec = 3;
+    expect("test".match(re)).not.toBeNull();
+});
