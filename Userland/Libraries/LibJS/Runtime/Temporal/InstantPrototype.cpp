a946766cfc148245edbbf7b4f080c83b3fb03978
Linus Groh <mail@linusgroh.de>
2021-07-08 21:06:50 +0100

    LibJS: Implement Temporal.Instant.prototype[@@toStringTag]

--- Userland/Libraries/LibJS/Runtime/Temporal/InstantPrototype.cpp
+++ Userland/Libraries/LibJS/Runtime/Temporal/InstantPrototype.cpp
@@ -18,6 +18,11 @@ InstantPrototype::InstantPrototype(GlobalObject& global_object)
 void InstantPrototype::initialize(GlobalObject& global_object)
 {
     Object::initialize(global_object);
+
+    auto& vm = this->vm();
+
+    // 8.3.2 Temporal.Instant.prototype[ @@toStringTag ], https://tc39.es/proposal-temporal/#sec-temporal.instant.prototype-@@tostringtag
+    define_direct_property(*vm.well_known_symbol_to_string_tag(), js_string(vm.heap(), "Temporal.Instant"), Attribute::Configurable);
 }
 
 }
