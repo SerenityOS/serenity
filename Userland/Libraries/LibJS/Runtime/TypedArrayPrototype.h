1866aff2a26820529fa351b637bb96a5436a0349
Luke <luke.wilde@live.co.uk>
2021-07-08 03:39:16 +0100

    LibJS: Add %TypedArray%.prototype.copyWithin
    This fixes 44 test262 cases.

--- Userland/Libraries/LibJS/Runtime/TypedArrayPrototype.h
+++ Userland/Libraries/LibJS/Runtime/TypedArrayPrototype.h
@@ -37,6 +37,7 @@ private:
     JS_DECLARE_NATIVE_FUNCTION(entries);
     JS_DECLARE_NATIVE_FUNCTION(set);
     JS_DECLARE_NATIVE_FUNCTION(reverse);
+    JS_DECLARE_NATIVE_FUNCTION(copy_within);
 };
 
 }
