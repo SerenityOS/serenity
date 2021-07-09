fda1c0786977b74f31facb51c6d4f4e99837d2bb
Ali Mohammad Pur <ali.mpfard@gmail.com>
2021-07-09 07:27:34 +0430

    Ports: Make the CMake port use the LibUV port
    Now that we've ported libuv, we can remove the libuv patches from cmake
    and make it use the ported libuv.

--- Ports/cmake/package.sh
+++ Ports/cmake/package.sh
@@ -4,13 +4,17 @@ version=3.19.4
 useconfigure=true
 files="https://github.com/Kitware/CMake/releases/download/v$version/cmake-$version.tar.gz cmake-$version.tar.gz 7d0232b9f1c57e8de81f38071ef8203e6820fe7eec8ae46a1df125d88dbcc2e1"
 auth_type=sha256
-depends="bash gcc make sed ncurses"
-configopts="-DCMAKE_TOOLCHAIN_FILE=$SERENITY_SOURCE_DIR/Toolchain/CMake/CMakeToolchain.txt"
+depends="bash make sed ncurses libuv"
+configopts="-DCMAKE_TOOLCHAIN_FILE=$SERENITY_SOURCE_DIR/Toolchain/CMake/CMakeToolchain.txt -DCMAKE_USE_SYSTEM_LIBRARY_LIBUV=1 -GNinja"
 
 configure() {
     run cmake $configopts .
 }
 
+build() {
+    run ninja
+}
+
 install() {
-    run make install
+    run ninja install
 }
