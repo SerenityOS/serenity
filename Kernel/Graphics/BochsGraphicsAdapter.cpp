7e833759d02ee23adf363c6700f9af84f9fd5a4d
Liav A <liavalb@gmail.com>
2021-07-09 10:25:03 +0300

    Kernel/Graphics: Remove unnecessary Bochs.h file
    Nobody excepts BochsGraphicsAdapter code uses these definitions, so
    let's put them back into the BochsGraphicsAdapter.cpp file.

--- Kernel/Graphics/BochsGraphicsAdapter.cpp
+++ Kernel/Graphics/BochsGraphicsAdapter.cpp
@@ -10,13 +10,29 @@
 #include <Kernel/Bus/PCI/Access.h>
 #include <Kernel/Bus/PCI/IDs.h>
 #include <Kernel/Debug.h>
-#include <Kernel/Graphics/Bochs.h>
 #include <Kernel/Graphics/BochsGraphicsAdapter.h>
 #include <Kernel/Graphics/Console/ContiguousFramebufferConsole.h>
 #include <Kernel/Graphics/GraphicsManagement.h>
 #include <Kernel/IO.h>
 #include <Kernel/Sections.h>
 #include <Kernel/VM/TypedMapping.h>
+
+#define VBE_DISPI_IOPORT_INDEX 0x01CE
+#define VBE_DISPI_IOPORT_DATA 0x01CF
+
+#define VBE_DISPI_INDEX_ID 0x0
+#define VBE_DISPI_INDEX_XRES 0x1
+#define VBE_DISPI_INDEX_YRES 0x2
+#define VBE_DISPI_INDEX_BPP 0x3
+#define VBE_DISPI_INDEX_ENABLE 0x4
+#define VBE_DISPI_INDEX_BANK 0x5
+#define VBE_DISPI_INDEX_VIRT_WIDTH 0x6
+#define VBE_DISPI_INDEX_VIRT_HEIGHT 0x7
+#define VBE_DISPI_INDEX_X_OFFSET 0x8
+#define VBE_DISPI_INDEX_Y_OFFSET 0x9
+#define VBE_DISPI_DISABLED 0x00
+#define VBE_DISPI_ENABLED 0x01
+#define VBE_DISPI_LFB_ENABLED 0x40
 
 namespace Kernel {
 
