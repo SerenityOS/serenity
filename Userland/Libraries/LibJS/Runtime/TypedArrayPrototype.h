d29a85f8eaff7489bc6739151e9220c0c8c5aa01
Idan Horowitz <idan.horowitz@gmail.com>
2021-07-09 23:11:14 +0300

    LibJS: Add %TypedArray%.prototype.toLocaleString

--- Userland/Libraries/LibJS/Runtime/TypedArrayPrototype.h
+++ Userland/Libraries/LibJS/Runtime/TypedArrayPrototype.h
@@ -49,6 +49,7 @@ private:
     JS_DECLARE_NATIVE_FUNCTION(copy_within);
     JS_DECLARE_NATIVE_FUNCTION(filter);
     JS_DECLARE_NATIVE_FUNCTION(map);
+    JS_DECLARE_NATIVE_FUNCTION(to_locale_string);
 };
 
 }
