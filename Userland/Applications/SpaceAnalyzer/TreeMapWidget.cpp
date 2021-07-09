b99558a89e98ec57b7fbba54e33bb94e48b38f96
Karol Kosek <krkk@krkk.ct8.pl>
2021-07-09 14:16:25 +0200

    SpaceAnalyzer: Show folder total size

--- Userland/Applications/SpaceAnalyzer/TreeMapWidget.cpp
+++ Userland/Applications/SpaceAnalyzer/TreeMapWidget.cpp
@@ -97,10 +97,12 @@ void TreeMapWidget::paint_cell_frame(GUI::Painter& painter, const TreeMapNode& n
         painter.add_clip_rect(cell_rect);
         Gfx::IntRect text_rect = remainder;
         text_rect.translate_by(2, 2);
-        painter.draw_text(text_rect, node.name(), font(), Gfx::TextAlignment::TopLeft, Color::Black);
         if (node_is_leaf(node)) {
+            painter.draw_text(text_rect, node.name(), font(), Gfx::TextAlignment::TopLeft, Color::Black);
             text_rect.translate_by(0, font().presentation_size() + 1);
             painter.draw_text(text_rect, human_readable_size(node.area()), font(), Gfx::TextAlignment::TopLeft, Color::Black);
+        } else {
+            painter.draw_text(text_rect, String::formatted("{} - {}", node.name(), human_readable_size(node.area())), font(), Gfx::TextAlignment::TopLeft, Color::Black);
         }
         painter.clear_clip_rect();
     }
