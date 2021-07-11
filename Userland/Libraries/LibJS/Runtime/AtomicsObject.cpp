ea0c8ef002704b4376be59f0f5b6e067f1f2dab4
Timothy Flynn <trflynn89@pm.me>
2021-07-11 13:28:52 -0400

    LibJS: Implement Atomics.sub

--- Userland/Libraries/LibJS/Runtime/AtomicsObject.cpp
+++ Userland/Libraries/LibJS/Runtime/AtomicsObject.cpp
@@ -121,6 +121,7 @@ void AtomicsObject::initialize(GlobalObject& global_object)
     define_native_function(vm.names.and_, and_, 3, attr);
     define_native_function(vm.names.load, load, 2, attr);
     define_native_function(vm.names.or_, or_, 3, attr);
+    define_native_function(vm.names.sub, sub, 3, attr);
 
     // 25.4.15 Atomics [ @@toStringTag ], https://tc39.es/ecma262/#sec-atomics-@@tostringtag
     define_direct_property(*vm.well_known_symbol_to_string_tag(), js_string(global_object.heap(), "Atomics"), Attribute::Configurable);
@@ -197,6 +198,24 @@ JS_DEFINE_NATIVE_FUNCTION(AtomicsObject::or_)
 #define __JS_ENUMERATE(ClassName, snake_name, PrototypeName, ConstructorName, Type) \
     if (is<ClassName>(typed_array))                                                 \
         return perform_atomic_operation<Type>(global_object, *typed_array, move(atomic_or));
+    JS_ENUMERATE_TYPED_ARRAYS
+#undef __JS_ENUMERATE
+
+    VERIFY_NOT_REACHED();
+}
+
+// 25.4.11 Atomics.sub ( typedArray, index, value ), https://tc39.es/ecma262/#sec-atomics.sub
+JS_DEFINE_NATIVE_FUNCTION(AtomicsObject::sub)
+{
+    auto* typed_array = typed_array_from(global_object, vm.argument(0));
+    if (!typed_array)
+        return {};
+
+    auto atomic_sub = [](auto* storage, auto value) { return AK::atomic_fetch_sub(storage, value); };
+
+#define __JS_ENUMERATE(ClassName, snake_name, PrototypeName, ConstructorName, Type) \
+    if (is<ClassName>(typed_array))                                                 \
+        return perform_atomic_operation<Type>(global_object, *typed_array, move(atomic_sub));
     JS_ENUMERATE_TYPED_ARRAYS
 #undef __JS_ENUMERATE
 
