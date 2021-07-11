a537121906ffb3104fe6b26271fb1a3945593730
Andreas Kling <kling@serenityos.org>
2021-07-11 13:30:19 +0200

    Kernel: Remove some no-longer-needed C library functions
    Now that we no longer demangle symbols in the kernel, we don't need to
    provide stuff like malloc(), memchr(), free(), etc to the demangler.

--- Kernel/StdLib.cpp
+++ Kernel/StdLib.cpp
@@ -396,32 +396,6 @@ char* strstr(const char* haystack, const char* needle)
     return const_cast<char*>(haystack);
 }
 
-void* memchr(const void* ptr, int c, size_t size)
-{
-    char ch = c;
-    auto* cptr = (const char*)ptr;
-    for (size_t i = 0; i < size; ++i) {
-        if (cptr[i] == ch)
-            return const_cast<char*>(cptr + i);
-    }
-    return nullptr;
-}
-
-void* malloc(size_t s)
-{
-    return kmalloc(s);
-}
-
-void* realloc(void* p, size_t s)
-{
-    return krealloc(p, s);
-}
-
-void free(void* p)
-{
-    return kfree(p);
-}
-
 // Functions that are automatically called by the C++ compiler.
 // Declare them first, to tell the silly compiler that they are indeed being used.
 [[noreturn]] void __stack_chk_fail() __attribute__((used));
