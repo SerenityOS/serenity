9cce7cc90c32f560e375c61cac0f6b0babc133e7
davidot <david.tuin@gmail.com>
2021-07-12 01:33:54 +0200

    LibJS: Fix that vm.in_strict_mode was propagated to eval and functions
    For eval it depends on the CallerMode and for a created function it
    depends on the function itself.

--- Userland/Libraries/LibJS/AST.cpp
+++ Userland/Libraries/LibJS/AST.cpp
@@ -111,7 +111,7 @@ Value FunctionExpression::execute(Interpreter& interpreter, GlobalObject& global
         func_env->create_immutable_binding(global_object, name(), false);
     }
 
-    auto closure = OrdinaryFunctionObject::create(global_object, name(), body(), parameters(), function_length(), func_env, kind(), is_strict_mode() || interpreter.vm().in_strict_mode(), is_arrow_function());
+    auto closure = OrdinaryFunctionObject::create(global_object, name(), body(), parameters(), function_length(), func_env, kind(), is_strict_mode(), is_arrow_function());
 
     if (has_identifier)
         func_env->initialize_binding(global_object, name(), closure);
