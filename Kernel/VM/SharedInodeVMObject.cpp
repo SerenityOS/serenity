af8c74a328b80b09a6e948ab2e9dc4e54ad91cec
Andreas Kling <kling@serenityos.org>
2021-07-11 17:52:07 +0200

    Kernel: Make SharedInodeVMObject allocation OOM-safe

--- Kernel/VM/SharedInodeVMObject.cpp
+++ Kernel/VM/SharedInodeVMObject.cpp
@@ -9,12 +9,14 @@
 
 namespace Kernel {
 
-NonnullRefPtr<SharedInodeVMObject> SharedInodeVMObject::create_with_inode(Inode& inode)
+RefPtr<SharedInodeVMObject> SharedInodeVMObject::try_create_with_inode(Inode& inode)
 {
     size_t size = inode.size();
     if (auto shared_vmobject = inode.shared_vmobject())
         return shared_vmobject.release_nonnull();
-    auto vmobject = adopt_ref(*new SharedInodeVMObject(inode, size));
+    auto vmobject = adopt_ref_if_nonnull(new (nothrow) SharedInodeVMObject(inode, size));
+    if (!vmobject)
+        return nullptr;
     vmobject->inode().set_shared_vmobject(*vmobject);
     return vmobject;
 }
