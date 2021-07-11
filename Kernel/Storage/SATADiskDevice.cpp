e18be7f5c7061c2a53a3c8b5f950e3459c7789fa
Brian Gianforcaro <bgianf@serenityos.org>
2021-07-11 11:44:08 -0700

    Kernel: Remove unused header includes in Storage subtree

--- Kernel/Storage/SATADiskDevice.cpp
+++ Kernel/Storage/SATADiskDevice.cpp
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#include <AK/Memory.h>
 #include <AK/StringView.h>
 #include <Kernel/FileSystem/FileDescription.h>
 #include <Kernel/Storage/AHCIController.h>
