6630a8707047bf146de6e1ec5b939200166091b8
Luke <luke.wilde@live.co.uk>
2021-07-12 21:48:27 +0100

    Lagom/Fuzzers: Add fuzzer for POSIX basic regex parser

---
+++ Meta/Lagom/Fuzzers/FuzzRegexPosixBasic.cpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021, Luke Wilde <lukew@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <AK/StringView.h>
+#include <LibRegex/Regex.h>
+#include <stddef.h>
+#include <stdint.h>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    auto pattern = StringView(static_cast<const unsigned char*>(data), size);
+    [[maybe_unused]] auto re = Regex<PosixBasic>(pattern);
+    return 0;
+}
