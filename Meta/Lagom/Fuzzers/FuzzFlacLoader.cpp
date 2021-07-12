0dd0e19f6ac56d69bb944a8034bed14680f85840
Luke <luke.wilde@live.co.uk>
2021-07-12 21:55:21 +0100

    Lagom/Fuzzers: Add fuzzer for FLAC loader

---
+++ Meta/Lagom/Fuzzers/FuzzFlacLoader.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021, Luke Wilde <lukew@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibAudio/FlacLoader.h>
+#include <stddef.h>
+#include <stdint.h>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    auto flac_data = ByteBuffer::copy(data, size);
+    auto flac = make<Audio::FlacLoaderPlugin>(flac_data);
+
+    if (!flac->sniff())
+        return 1;
+
+    while (flac->get_more_samples())
+        ;
+
+    return 0;
+}
