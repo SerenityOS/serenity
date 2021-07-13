e6d240e05dbcf2d700c86483aa805e328c43d2dd
Luke <luke.wilde@live.co.uk>
2021-07-13 20:47:12 +0100

    LibIMAP: Add quoted printable decoder
    This is a very common encoding for e-mail. Gmail seems to encode all
    HTML e-mail in it.

---
+++ Userland/Libraries/LibIMAP/QuotedPrintable.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, Luke Wilde <lukew@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/ByteBuffer.h>
+
+namespace IMAP {
+
+String decode_quoted_printable(StringView const&);
+
+}
