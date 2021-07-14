fa7a6fbeddf5ecfe43e796f0b038b0be51241389
Sam Atkins <atkinssj@gmail.com>
2021-07-14 20:10:29 +0100

    Meta: Fix SPICE detection in run.sh
    The previous fix did not work for me, but this does. :^)
    
    Credit goes to @X-yl for actually figuring it out.

--- Meta/run.sh
+++ Meta/run.sh
@@ -148,7 +148,7 @@ $SERENITY_SPICE_SERVER_CHARDEV
 -device ich9-ahci,bus=bridge3
 "
 
-if "${SERENITY_QEMU_BIN}" --help | grep -iq spice; then
+if "${SERENITY_QEMU_BIN}" -chardev help | grep -iq spice; then
     SERENITY_COMMON_QEMU_ARGS="$SERENITY_COMMON_QEMU_ARGS
     -spice port=5930,agent-mouse=off,disable-ticketing=on
     -device virtserialport,chardev=vdagent,nr=1
