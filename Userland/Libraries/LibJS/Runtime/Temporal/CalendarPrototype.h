7ff98703740be8e7cbcdafe2325db117ac3d57e2
Linus Groh <mail@linusgroh.de>
2021-07-14 21:12:37 +0100

    LibJS: Implement Temporal.Calendar.prototype.toString()

--- Userland/Libraries/LibJS/Runtime/Temporal/CalendarPrototype.h
+++ Userland/Libraries/LibJS/Runtime/Temporal/CalendarPrototype.h
@@ -17,6 +17,9 @@ public:
     explicit CalendarPrototype(GlobalObject&);
     virtual void initialize(GlobalObject&) override;
     virtual ~CalendarPrototype() override = default;
+
+private:
+    JS_DECLARE_NATIVE_FUNCTION(to_string);
 };
 
 }
