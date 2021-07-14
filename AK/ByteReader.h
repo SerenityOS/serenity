c0f6c7a5a12c20257f06b413ca1d3fa900b34069
Hediadyoin1 <leon2002.la@gmail.com>
2021-07-14 12:10:43 +0200

    AK: ALWAYS_INLINE ByteReader

--- AK/ByteReader.h
+++ AK/ByteReader.h
@@ -13,39 +13,39 @@ namespace AK {
 
 struct ByteReader {
     template<typename T>
-    requires(IsTriviallyCopyable<T>) static void store(u8* addr, T value)
+    requires(IsTriviallyCopyable<T>) ALWAYS_INLINE static void store(u8* addr, T value)
     {
         __builtin_memcpy(addr, &value, sizeof(T));
     }
     template<typename T>
-    requires(IsTriviallyConstructible<T>) static void load(const u8* addr, T& value)
+    requires(IsTriviallyCopyable<T>) ALWAYS_INLINE static void load(const u8* addr, T& value)
     {
         __builtin_memcpy(&value, addr, sizeof(T));
     }
 
     template<typename T>
-    static T* load_pointer(const u8* address)
+    ALWAYS_INLINE static T* load_pointer(const u8* address)
     {
         FlatPtr value;
         load<FlatPtr>(address, value);
         return reinterpret_cast<T*>(value);
     }
 
-    static u16 load16(const u8* address)
+    ALWAYS_INLINE static u16 load16(const u8* address)
     {
         u16 value;
         load(address, value);
         return value;
     }
 
-    static u32 load32(const u8* address)
+    ALWAYS_INLINE static u32 load32(const u8* address)
     {
         u32 value;
         load(address, value);
         return value;
     }
 
-    static u64 load64(const u8* address)
+    ALWAYS_INLINE static u64 load64(const u8* address)
     {
         u64 value;
         load(address, value);
