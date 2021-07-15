e9f92b04b789f50e569d90c168385cd5c6f45c73
Linus Groh <mail@linusgroh.de>
2021-07-16 00:15:56 +0100

    LibJS: Implement Temporal.Duration.prototype.blank

--- Userland/Libraries/LibJS/Runtime/Temporal/DurationPrototype.h
+++ Userland/Libraries/LibJS/Runtime/Temporal/DurationPrototype.h
@@ -30,6 +30,7 @@ private:
     JS_DECLARE_NATIVE_FUNCTION(microseconds_getter);
     JS_DECLARE_NATIVE_FUNCTION(nanoseconds_getter);
     JS_DECLARE_NATIVE_FUNCTION(sign_getter);
+    JS_DECLARE_NATIVE_FUNCTION(blank_getter);
 };
 
 }
