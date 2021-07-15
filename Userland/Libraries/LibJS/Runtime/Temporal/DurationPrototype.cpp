23766f28db52e0523aa436e8bd9c3c82ec21a15e
Linus Groh <mail@linusgroh.de>
2021-07-15 23:24:51 +0100

    LibJS: Implement Temporal.Duration.prototype[@@toStringTag]

--- Userland/Libraries/LibJS/Runtime/Temporal/DurationPrototype.cpp
+++ Userland/Libraries/LibJS/Runtime/Temporal/DurationPrototype.cpp
@@ -18,6 +18,11 @@ DurationPrototype::DurationPrototype(GlobalObject& global_object)
 void DurationPrototype::initialize(GlobalObject& global_object)
 {
     Object::initialize(global_object);
+
+    auto& vm = this->vm();
+
+    // 7.3.2 Temporal.Duration.prototype[ @@toStringTag ], https://tc39.es/proposal-temporal/#sec-temporal.duration.prototype-@@tostringtag
+    define_direct_property(*vm.well_known_symbol_to_string_tag(), js_string(vm.heap(), "Temporal.Duration"), Attribute::Configurable);
 }
 
 }
