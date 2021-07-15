e6d95194778efbcad404979eba1b50d8cb8e4634
Linus Groh <mail@linusgroh.de>
2021-07-15 23:34:53 +0100

    LibJS: Implement Temporal.Duration.prototype.years

--- Userland/Libraries/LibJS/Runtime/Temporal/DurationPrototype.cpp
+++ Userland/Libraries/LibJS/Runtime/Temporal/DurationPrototype.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <LibJS/Runtime/GlobalObject.h>
+#include <LibJS/Runtime/Temporal/Duration.h>
 #include <LibJS/Runtime/Temporal/DurationPrototype.h>
 
 namespace JS::Temporal {
@@ -23,6 +24,34 @@ void DurationPrototype::initialize(GlobalObject& global_object)
 
     // 7.3.2 Temporal.Duration.prototype[ @@toStringTag ], https://tc39.es/proposal-temporal/#sec-temporal.duration.prototype-@@tostringtag
     define_direct_property(*vm.well_known_symbol_to_string_tag(), js_string(vm.heap(), "Temporal.Duration"), Attribute::Configurable);
+
+    define_native_accessor(vm.names.years, years_getter, {}, Attribute::Configurable);
+}
+
+static Duration* typed_this(GlobalObject& global_object)
+{
+    auto& vm = global_object.vm();
+    auto* this_object = vm.this_value(global_object).to_object(global_object);
+    if (!this_object)
+        return {};
+    if (!is<Duration>(this_object)) {
+        vm.throw_exception<TypeError>(global_object, ErrorType::NotA, "Temporal.Duration");
+        return {};
+    }
+    return static_cast<Duration*>(this_object);
+}
+
+// 7.3.3 get Temporal.Duration.prototype.years, https://tc39.es/proposal-temporal/#sec-get-temporal.duration.prototype.years
+JS_DEFINE_NATIVE_FUNCTION(DurationPrototype::years_getter)
+{
+    // 1. Let duration be the this value.
+    // 2. Perform ? RequireInternalSlot(duration, [[InitializedTemporalDuration]]).
+    auto* duration = typed_this(global_object);
+    if (vm.exception())
+        return {};
+
+    // 3. Return duration.[[Years]].
+    return Value(duration->years());
 }
 
 }
