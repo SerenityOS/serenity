3099a6bf2add35ac6a0c7ce4cbb6c794d6269d19
Daniel Bertalan <dani@danielbertalan.dev>
2021-07-15 13:50:55 +0200

    Kernel+AK: Generate compile-time error for non-sized `delete`
    This is a much more ergonomic option than getting a
    `VERIFY_NOT_REACHED()` failure at run-time. I encountered this issue
    with Clang, where sized deallocation is not the default due to ABI
    breakage concerns.
    
    Note that we can't simply just not declare these functions, because the
    C++ standard states:
    > If this function with size parameter is defined, the program shall
    > also define the version without the size parameter.

--- Kernel/Heap/kmalloc.h
+++ Kernel/Heap/kmalloc.h
@@ -64,14 +64,15 @@ inline void* operator new[](size_t, void* p) { return p; }
 [[nodiscard]] void* operator new(size_t size, std::align_val_t);
 [[nodiscard]] void* operator new(size_t size, std::align_val_t, const std::nothrow_t&) noexcept;
 
-void operator delete(void* ptr) noexcept;
+void operator delete(void* ptr) noexcept DISALLOW("All deletes in the kernel should have a known size.");
 void operator delete(void* ptr, size_t) noexcept;
+void operator delete(void* ptr, std::align_val_t) noexcept DISALLOW("All deletes in the kernel should have a known size.");
 void operator delete(void* ptr, size_t, std::align_val_t) noexcept;
 
 [[nodiscard]] void* operator new[](size_t size);
 [[nodiscard]] void* operator new[](size_t size, const std::nothrow_t&) noexcept;
 
-void operator delete[](void* ptrs) noexcept;
+void operator delete[](void* ptrs) noexcept DISALLOW("All deletes in the kernel should have a known size.");
 void operator delete[](void* ptr, size_t) noexcept;
 
 [[gnu::malloc, gnu::returns_nonnull, gnu::alloc_size(1)]] void* kmalloc(size_t);
