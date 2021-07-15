e85daee3560322de2c38ab17cb5a5cc87a0c46ef
Gunnar Beutner <gbeutner@serenityos.org>
2021-07-15 18:17:58 +1000

    Kernel: Make new kernel build process work on macOS
    Use objcopy from the toolchain so that the changes introduced in
    7236584 will succeed on macOS.
    
    Fixes #8768.

--- Kernel/embedmap.sh
+++ Kernel/embedmap.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 tmp=$(mktemp)
 (cat kernel.map; printf '%b' '\0') > "$tmp"
-objcopy --update-section .ksyms="$tmp" Kernel
+OBJCOPY="${OBJCOPY:-objcopy}"
+"$OBJCOPY" --update-section .ksyms="$tmp" Kernel
 rm -f "$tmp"
