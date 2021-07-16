4d4810b7ba1ca4310b6bd772c8ee91fd73330efb
Tom <tomut@yahoo.com>
2021-07-15 19:45:22 -0600

    Kernel: Rename functions to be less confusing
    Thread::yield_and_release_relock_big_lock releases the big lock, yields
    and then relocks the big lock.
    
    Thread::yield_assuming_not_holding_big_lock yields assuming the big
    lock is not being held.

--- Kernel/Thread.h
+++ Kernel/Thread.h
@@ -819,7 +819,7 @@ public:
         while (state() == Thread::Stopped) {
             lock.unlock();
             // We shouldn't be holding the big lock here
-            yield_while_not_holding_big_lock();
+            yield_assuming_not_holding_big_lock();
             lock.lock();
         }
     }
@@ -905,7 +905,7 @@ public:
             // Yield to the scheduler, and wait for us to resume unblocked.
             VERIFY(!g_scheduler_lock.own_lock());
             VERIFY(Processor::current().in_critical());
-            yield_while_not_holding_big_lock();
+            yield_assuming_not_holding_big_lock();
             VERIFY(Processor::current().in_critical());
 
             ScopedSpinLock block_lock2(m_block_lock);
@@ -1341,8 +1341,8 @@ private:
 
     bool m_is_profiling_suppressed { false };
 
-    void yield_without_holding_big_lock();
-    void yield_while_not_holding_big_lock();
+    void yield_and_release_relock_big_lock();
+    void yield_assuming_not_holding_big_lock();
     void drop_thread_count(bool);
 };
 
