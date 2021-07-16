789e9afe0c28c9d69e312d33191ae6c768ed0bae
Timothy Flynn <trflynn89@pm.me>
2021-07-16 13:04:46 -0400

    LibJS: Report string length as the code point length, not byte length
    For example, U+180E is 3 bytes, but should have a string length of 1.

--- Userland/Libraries/LibJS/Tests/builtins/String/String.js
+++ Userland/Libraries/LibJS/Tests/builtins/String/String.js
@@ -7,3 +7,10 @@ test("typeof", () => {
     expect(typeof String()).toBe("string");
     expect(typeof new String()).toBe("object");
 });
+
+test("length", () => {
+    expect(new String().length).toBe(0);
+    expect(new String("a").length).toBe(1);
+    expect(new String("\u180E").length).toBe(1);
+    expect(new String("\uDBFF\uDFFF").length).toBe(2);
+});
