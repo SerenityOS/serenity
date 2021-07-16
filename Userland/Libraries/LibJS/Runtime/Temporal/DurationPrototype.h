9aa1e4b885c22fed0122644f7a5048b017a721c7
Linus Groh <mail@linusgroh.de>
2021-07-16 19:30:52 +0100

    LibJS: Implement Temporal.Duration.prototype.with()

--- Userland/Libraries/LibJS/Runtime/Temporal/DurationPrototype.h
+++ Userland/Libraries/LibJS/Runtime/Temporal/DurationPrototype.h
@@ -31,6 +31,7 @@ private:
     JS_DECLARE_NATIVE_FUNCTION(nanoseconds_getter);
     JS_DECLARE_NATIVE_FUNCTION(sign_getter);
     JS_DECLARE_NATIVE_FUNCTION(blank_getter);
+    JS_DECLARE_NATIVE_FUNCTION(with);
     JS_DECLARE_NATIVE_FUNCTION(value_of);
 };
 
