94e1324a6731c07de0f9a8b5fc3e594aa60bf754
Idan Horowitz <idan.horowitz@gmail.com>
2021-07-19 00:30:58 +0300

    LibJS: Implement Temporal.PlainDate.prototype[@@toStringTag]

--- Userland/Libraries/LibJS/Runtime/Temporal/PlainDatePrototype.cpp
+++ Userland/Libraries/LibJS/Runtime/Temporal/PlainDatePrototype.cpp
@@ -18,6 +18,11 @@ PlainDatePrototype::PlainDatePrototype(GlobalObject& global_object)
 void PlainDatePrototype::initialize(GlobalObject& global_object)
 {
     Object::initialize(global_object);
+
+    auto& vm = this->vm();
+
+    // 3.3.2 Temporal.PlainDate.prototype[ @@toStringTag ], https://tc39.es/proposal-temporal/#sec-temporal.plaindate.prototype-@@tostringtag
+    define_direct_property(*vm.well_known_symbol_to_string_tag(), js_string(vm.heap(), "Temporal.PlainDate"), Attribute::Configurable);
 }
 
 }
