a7920ce86a4f1837c38d11d76edaf2bdcf7479e2
Peter Elliott <pelliott@ualberta.ca>
2021-07-18 12:45:59 -0600

    Applets: Add DesktopPicker applet
    This applet displays a grid of desktops, and shows the user what virtual
    desktop they are on. When clicked, the desktop will be changed.

---
+++ Userland/Applets/DesktopPicker/main.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2021, Peter Elliott <pelliott@ualberta.ca>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include "DesktopStatusWindow.h"
+#include <LibGUI/Application.h>
+#include <LibGUI/Frame.h>
+#include <LibGUI/Painter.h>
+#include <LibGUI/Window.h>
+#include <LibGUI/WindowManagerServerConnection.h>
+#include <WindowServer/Window.h>
+#include <serenity.h>
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+    if (pledge("stdio recvfd sendfd rpath unix", nullptr) < 0) {
+        perror("pledge");
+        return 1;
+    }
+
+    auto app = GUI::Application::construct(argc, argv);
+
+    // We need to obtain the WM connection here as well before the pledge shortening.
+    GUI::WindowManagerServerConnection::the();
+
+    if (pledge("stdio recvfd sendfd rpath", nullptr) < 0) {
+        perror("pledge");
+        return 1;
+    }
+
+    auto window = DesktopStatusWindow::construct();
+    window->resize(28, 16);
+    window->show();
+    window->make_window_manager(WindowServer::WMEventMask::VirtualDesktopChanges);
+
+    return app->exec();
+}
