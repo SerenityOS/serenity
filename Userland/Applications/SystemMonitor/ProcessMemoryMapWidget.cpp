ed25a6ad0d170f1598aff8c68112751136080680
Gunnar Beutner <gbeutner@serenityos.org>
2021-07-18 02:45:59 +0200

    SystemMonitor: Add support for 64-bit addresses

--- Userland/Applications/SystemMonitor/ProcessMemoryMapWidget.cpp
+++ Userland/Applications/SystemMonitor/ProcessMemoryMapWidget.cpp
@@ -52,8 +52,8 @@ ProcessMemoryMapWidget::ProcessMemoryMapWidget()
     Vector<GUI::JsonArrayModel::FieldSpec> pid_vm_fields;
     pid_vm_fields.empend(
         "Address", Gfx::TextAlignment::CenterLeft,
-        [](auto& object) { return String::formatted("{:p}", object.get("address").to_u32()); },
-        [](auto& object) { return object.get("address").to_u32(); });
+        [](auto& object) { return String::formatted("{:p}", object.get("address").to_u64()); },
+        [](auto& object) { return object.get("address").to_u64(); });
     pid_vm_fields.empend("size", "Size", Gfx::TextAlignment::CenterRight);
     pid_vm_fields.empend("amount_resident", "Resident", Gfx::TextAlignment::CenterRight);
     pid_vm_fields.empend("amount_dirty", "Dirty", Gfx::TextAlignment::CenterRight);
