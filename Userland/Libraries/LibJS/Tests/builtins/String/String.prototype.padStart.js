13e36ca04f96c3c99a37e65caf92b27099e260bc
Timothy Flynn <trflynn89@pm.me>
2021-07-19 14:36:30 -0400

    LibJS: Implement StringPad abstract operation with UTF-16 code units
    Affects String.prototype.padStart and String.prototype.padEnd.

--- Userland/Libraries/LibJS/Tests/builtins/String/String.prototype.padStart.js
+++ Userland/Libraries/LibJS/Tests/builtins/String/String.prototype.padStart.js
@@ -16,3 +16,19 @@ test("basic functionality", () => {
     expect(s.padStart(10, "bar")).toBe("barbarbfoo");
     expect(s.padStart(10, "123456789")).toBe("1234567foo");
 });
+
+test("UTF-16", () => {
+    var s = "😀";
+    expect(s).toHaveLength(2);
+    expect(s.padStart(-1)).toBe("😀");
+    expect(s.padStart(0)).toBe("😀");
+    expect(s.padStart(1)).toBe("😀");
+    expect(s.padStart(2)).toBe("😀");
+    expect(s.padStart(3)).toBe(" 😀");
+    expect(s.padStart(10)).toBe("        😀");
+
+    expect(s.padStart(2, "😀")).toBe("😀");
+    expect(s.padStart(3, "😀")).toBe("\ud83d😀");
+    expect(s.padStart(4, "😀")).toBe("😀😀");
+    expect(s.padStart(5, "😀")).toBe("😀\ud83d😀");
+});
