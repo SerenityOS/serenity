3849fc2cac479ce537162ac50f78cdc68c991a78
Timothy Flynn <trflynn89@pm.me>
2021-07-19 12:46:35 -0400

    LibJS: Implement String.prototype.includes with UTF-16 code units
    This also implements String.prototype.includes more closely to the spec
    (such as returning false when the search string is a RegExp object).

--- Userland/Libraries/LibJS/Tests/builtins/String/String.prototype.includes.js
+++ Userland/Libraries/LibJS/Tests/builtins/String/String.prototype.includes.js
@@ -10,3 +10,11 @@ test("basic functionality", () => {
     expect("hello 10 friends".includes(10)).toBeTrue();
     expect("hello friends undefined".includes()).toBeTrue();
 });
+
+test("UTF-16", () => {
+    var s = "😀";
+    expect(s.includes("😀")).toBeTrue();
+    expect(s.includes("\ud83d")).toBeTrue();
+    expect(s.includes("\ude00")).toBeTrue();
+    expect(s.includes("a")).toBeFalse();
+});
