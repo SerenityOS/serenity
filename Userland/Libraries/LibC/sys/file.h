3ee281db9342e130ad07cbeb1125aacfbb4b3a60
Peter Elliott <pelliott@ualberta.ca>
2021-07-18 23:55:13 -0600

    LibC: Implement flock(2) using fcntl's F_SETLK
    While flock is not a posix interface, it exists on linux and all BSDs as
    far as I am aware.

--- Userland/Libraries/LibC/sys/file.h
+++ Userland/Libraries/LibC/sys/file.h
@@ -5,3 +5,16 @@
  */
 
 #pragma once
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+#define LOCK_SH 0
+#define LOCK_EX 1
+#define LOCK_UN 2
+#define LOCK_NB (1 << 2)
+
+int flock(int fd, int operation);
+
+__END_DECLS
