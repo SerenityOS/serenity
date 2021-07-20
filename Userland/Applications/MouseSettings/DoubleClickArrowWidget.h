ff084d78e285fb8a797bcfb19977a0e910e3e3e1
Mathias Jakobsen <mathias@jbcoding.com>
2021-07-20 23:49:47 +0100

    MouseSettings: Use latest (unsaved) setting for testing double-click
    Instead of using the doubleclick_event this uses the current double-
    click speed setting to check whether or not the colors of the double-
    click icon should be inverted. This allows us to use the current (and
    unsaved) setting for comparison instead of having to apply the settings
    first.

--- Userland/Applications/MouseSettings/DoubleClickArrowWidget.h
+++ Userland/Applications/MouseSettings/DoubleClickArrowWidget.h
@@ -1,11 +1,13 @@
 /*
  * Copyright (c) 2021, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2021, Mathias Jakobsen <mathias@jbcoding.com>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #pragma once
 
+#include <LibCore/ElapsedTimer.h>
 #include <LibGUI/Widget.h>
 
 namespace MouseSettings {
@@ -20,11 +22,12 @@ public:
 private:
     DoubleClickArrowWidget();
     virtual void paint_event(GUI::PaintEvent&) override;
-    virtual void doubleclick_event(GUI::MouseEvent&) override;
+    virtual void mousedown_event(GUI::MouseEvent&) override;
 
     RefPtr<Gfx::Bitmap> m_arrow_bitmap;
     int m_double_click_speed { 0 };
     bool m_inverted { false };
+    Core::ElapsedTimer m_double_click_timer;
 };
 
 }
