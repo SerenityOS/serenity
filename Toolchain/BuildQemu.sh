b211e61bf24dd31bd714db162f19d2e69f507ecc
LuK1337 <priv.luk@gmail.com>
2021-07-21 16:02:34 +0200

    Toolchain: Build QEMU with Spice support

--- Toolchain/BuildQemu.sh
+++ Toolchain/BuildQemu.sh
@@ -63,7 +63,8 @@ echo Using $UI_LIB based UI
 pushd "$DIR/Build/qemu"
     "$DIR"/Tarballs/$QEMU_VERSION/configure --prefix="$PREFIX" \
                                             --target-list=i386-softmmu,x86_64-softmmu \
-                                            --enable-$UI_LIB || exit 1
+                                            --enable-$UI_LIB \
+                                            --enable-spice || exit 1
     make -j "$MAKEJOBS" || exit 1
     make install || exit 1
 popd
