7a33a5c9b579cec9af8910258f4199b1a8145f2b
Idan Horowitz <idan.horowitz@gmail.com>
2021-07-22 00:25:45 +0300

    LibJS: Use trunc instead of a static_cast<i64> in is_integral_number
    This ensures we return true for integers that do not fit in an i64 aka,
    above 9223372036854775807. (2**63 - 1)

--- Userland/Libraries/LibJS/Runtime/Value.h
+++ Userland/Libraries/LibJS/Runtime/Value.h
@@ -72,7 +72,7 @@ public:
     bool is_negative_infinity() const { return is_number() && __builtin_isinf_sign(as_double()) < 0; }
     bool is_positive_zero() const { return is_number() && bit_cast<u64>(as_double()) == 0; }
     bool is_negative_zero() const { return is_number() && bit_cast<u64>(as_double()) == NEGATIVE_ZERO_BITS; }
-    bool is_integral_number() const { return is_finite_number() && static_cast<i64>(as_double()) == as_double(); }
+    bool is_integral_number() const { return is_finite_number() && trunc(as_double()) == as_double(); }
     bool is_finite_number() const
     {
         if (!is_number())
