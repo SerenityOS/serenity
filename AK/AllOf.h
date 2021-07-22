d50d0c440c7e5b45d7069064e103940a0519b184
Ali Mohammad Pur <ali.mpfard@gmail.com>
2021-07-22 19:13:56 +0430

    AK: Convert AnyOf/AllOf to east-const style

--- AK/AllOf.h
+++ AK/AllOf.h
@@ -13,9 +13,9 @@ namespace AK {
 
 template<typename Container, typename ValueType>
 constexpr bool all_of(
-    const SimpleIterator<Container, ValueType>& begin,
-    const SimpleIterator<Container, ValueType>& end,
-    const auto& predicate)
+    SimpleIterator<Container, ValueType> const& begin,
+    SimpleIterator<Container, ValueType> const& end,
+    auto const& predicate)
 {
     for (auto iter = begin; iter != end; ++iter) {
         if (!predicate(*iter)) {
