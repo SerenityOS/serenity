d40d10aae72d1d6d04b818d2173e5c5f5a731384
Ali Mohammad Pur <ali.mpfard@gmail.com>
2021-07-22 19:11:00 +0430

    AK: Implement {any,all}_of(IterableContainer&&, Predicate)
    This is a generally nicer-to-use version of the existing {any,all}_of()
    that doesn't require the user to explicitly provide two iterators.
    As a bonus, it also allows arbitrary iterators (as opposed to the hard
    requirement of providing SimpleIterators in the iterator version).

--- Tests/AK/TestAllOf.cpp
+++ Tests/AK/TestAllOf.cpp
@@ -19,3 +19,41 @@ TEST_CASE(should_determine_if_predicate_applies_to_all_elements_in_container)
     EXPECT(all_of(a.begin(), a.end(), [](auto elem) { return elem == 0; }));
     EXPECT(!all_of(a.begin(), a.end(), [](auto elem) { return elem == 1; }));
 }
+
+TEST_CASE(container_form)
+{
+    constexpr Array a { 10, 20, 30 };
+    static_assert(all_of(a, [](auto elem) { return elem > 0; }));
+    static_assert(!all_of(a, [](auto elem) { return elem > 10; }));
+    EXPECT(all_of(a, [](auto elem) { return elem > 0; }));
+    EXPECT(!all_of(a, [](auto elem) { return elem > 10; }));
+
+    Vector b { 10, 20, 30 };
+    EXPECT(all_of(b, [](auto elem) { return elem > 0; }));
+    EXPECT(!all_of(b, [](auto elem) { return elem > 10; }));
+
+    struct ArbitraryIterable {
+        struct ArbitraryIterator {
+            ArbitraryIterator(int v)
+                : value(v)
+            {
+            }
+
+            bool operator==(ArbitraryIterator const&) const = default;
+            int operator*() const { return value; }
+            ArbitraryIterator& operator++()
+            {
+                ++value;
+                return *this;
+            }
+
+            int value;
+        };
+        ArbitraryIterator begin() const { return 0; }
+        ArbitraryIterator end() const { return 20; }
+    };
+
+    ArbitraryIterable c;
+    EXPECT(all_of(c, [](auto elem) { return elem < 20; }));
+    EXPECT(!all_of(c, [](auto elem) { return elem > 10; }));
+}
