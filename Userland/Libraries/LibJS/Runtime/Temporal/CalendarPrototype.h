342172da3735f35aab04cac2ea706788de265e96
Idan Horowitz <idan.horowitz@gmail.com>
2021-07-23 18:52:55 +0300

    LibJS: Implement Temporal.Calendar.prototype.monthsInYear

--- Userland/Libraries/LibJS/Runtime/Temporal/CalendarPrototype.h
+++ Userland/Libraries/LibJS/Runtime/Temporal/CalendarPrototype.h
@@ -31,6 +31,7 @@ private:
     JS_DECLARE_NATIVE_FUNCTION(days_in_week);
     JS_DECLARE_NATIVE_FUNCTION(days_in_month);
     JS_DECLARE_NATIVE_FUNCTION(days_in_year);
+    JS_DECLARE_NATIVE_FUNCTION(months_in_year);
     JS_DECLARE_NATIVE_FUNCTION(to_string);
     JS_DECLARE_NATIVE_FUNCTION(to_json);
 };
