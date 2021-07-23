9269bdc10954a9f097e825371c0b6d9a844612a7
Peter Elliott <pelliott@ualberta.ca>
2021-07-22 23:43:17 -0600

    LibC: Modify fd_set to be compatible with X/OPEN
    For some reason X/OPEN requires that fd_set has a field fds_bits. Xproto
    requires either fds_bits or _fds_bits to be present, so the field 'bits'
    was renamed 'fds_bits'

--- Userland/Libraries/LibC/fd_set.h
+++ Userland/Libraries/LibC/fd_set.h
@@ -8,12 +8,12 @@
 
 #define FD_SETSIZE 1024
 #define FD_ZERO(set) memset((set), 0, sizeof(fd_set));
-#define FD_CLR(fd, set) ((set)->bits[(fd / 8)] &= ~(1 << (fd) % 8))
-#define FD_SET(fd, set) ((set)->bits[(fd / 8)] |= (1 << (fd) % 8))
-#define FD_ISSET(fd, set) ((set)->bits[(fd / 8)] & (1 << (fd) % 8))
+#define FD_CLR(fd, set) ((set)->fds_bits[(fd / 8)] &= ~(1 << (fd) % 8))
+#define FD_SET(fd, set) ((set)->fds_bits[(fd / 8)] |= (1 << (fd) % 8))
+#define FD_ISSET(fd, set) ((set)->fds_bits[(fd / 8)] & (1 << (fd) % 8))
 
 struct __fd_set {
-    unsigned char bits[FD_SETSIZE / 8];
+    unsigned char fds_bits[FD_SETSIZE / 8];
 };
 
 typedef struct __fd_set fd_set;
