dd15fc471cf30574a90dff7b62b14e77b47db3a6
Idan Horowitz <idan.horowitz@gmail.com>
2021-07-23 18:54:16 +0300

    LibJS: Implement Temporal.PlainDate.prototype.monthsInYear

---
+++ Userland/Libraries/LibJS/Tests/builtins/Temporal/PlainDate/PlainDate.prototype.monthsInYear.js
@@ -0,0 +1,14 @@
+describe("correct behavior", () => {
+    test("basic functionality", () => {
+        const date = new Temporal.PlainDate(2021, 7, 23);
+        expect(date.monthsInYear).toBe(12);
+    });
+});
+
+test("errors", () => {
+    test("this value must be a Temporal.PlainDate object", () => {
+        expect(() => {
+            Reflect.get(Temporal.PlainDate.prototype, "monthsInYear", "foo");
+        }).toThrowWithMessage(TypeError, "Not a Temporal.PlainDate");
+    });
+});
