d36c84c3314b1820b99fbca21fd71fd8bc0699c2
Owen Smith <yeeetari@gmail.com>
2021-07-23 21:41:57 +0100

    Kernel: Reorder the 64-bit GDT a bit
    Add a kernel data segment and make the user code segment come after
    the data segment. We need the GDT to be in a certain order to support
    the syscall and sysret instruction pair.

--- Kernel/Arch/x86/common/Processor.cpp
+++ Kernel/Arch/x86/common/Processor.cpp
@@ -1156,8 +1156,9 @@ UNMAP_AFTER_INIT void Processor::gdt_init()
     write_raw_gdt_entry(GDT_SELECTOR_DATA3, 0x0000ffff, 0x00cff200); // data3
 #else
     write_raw_gdt_entry(GDT_SELECTOR_CODE0, 0x0000ffff, 0x00af9a00); // code0
-    write_raw_gdt_entry(GDT_SELECTOR_CODE3, 0x0000ffff, 0x00affa00); // code3
+    write_raw_gdt_entry(GDT_SELECTOR_DATA0, 0x0000ffff, 0x00af9200); // data0
     write_raw_gdt_entry(GDT_SELECTOR_DATA3, 0x0000ffff, 0x008ff200); // data3
+    write_raw_gdt_entry(GDT_SELECTOR_CODE3, 0x0000ffff, 0x00affa00); // code3
 #endif
 
 #if ARCH(I386)
