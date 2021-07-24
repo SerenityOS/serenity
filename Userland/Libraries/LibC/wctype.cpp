7a4f59f638845a2a78d2fd947e297f30d452201f
Kenneth Myhra <kennethmyhra@gmail.com>
2021-07-24 21:44:24 +0200

    LibC: Declare wide character type functions in wctype.h
    Declares all wide character handling functions in wctype.h. All calls
    are forwarded to the corresponding character handling function in
    ctype.h.
    
    These functions are declared, but not implemented:
    - iswctype
    - wctype
    - towctrans
    - wctrans
    
    This should also resolve a build issue with the 'sed' port getting
    confused with iswprint being declared twice. Seems like it expected
    more functions from wctype.h and then had a backup strategy of adding
    its own wctype.h.

--- Userland/Libraries/LibC/wctype.cpp
+++ Userland/Libraries/LibC/wctype.cpp
@@ -4,16 +4,68 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#include <AK/Format.h>
-#include <assert.h>
 #include <wctype.h>
 
 extern "C" {
 
-wctype_t wctype(const char*)
+int iswalnum(wint_t wc)
 {
-    dbgln("FIXME: Implement wctype()");
-    TODO();
+    return __inline_isalnum(wc);
+}
+
+int iswalpha(wint_t wc)
+{
+    return __inline_isalpha(wc);
+}
+
+int iswcntrl(wint_t wc)
+{
+    return __inline_iscntrl(wc);
+}
+
+int iswdigit(wint_t wc)
+{
+    return __inline_isdigit(wc);
+}
+
+int iswxdigit(wint_t wc)
+{
+    return __inline_isxdigit(wc);
+}
+
+int iswspace(wint_t wc)
+{
+    return __inline_isspace(wc);
+}
+
+int iswpunct(wint_t wc)
+{
+    return __inline_ispunct(wc);
+}
+
+int iswprint(wint_t wc)
+{
+    return __inline_isprint(wc);
+}
+
+int iswgraph(wint_t wc)
+{
+    return __inline_isgraph(wc);
+}
+
+int iswlower(wint_t wc)
+{
+    return __inline_islower(wc);
+}
+
+int iswupper(wint_t wc)
+{
+    return __inline_isupper(wc);
+}
+
+int iswblank(wint_t wc)
+{
+    return __inline_isblank(wc);
 }
 
 int iswctype(wint_t, wctype_t)
@@ -22,9 +74,31 @@ int iswctype(wint_t, wctype_t)
     TODO();
 }
 
-int iswprint(wint_t)
+wctype_t wctype(const char*)
 {
-    dbgln("FIXME: Implement iswprint()");
+    dbgln("FIXME: Implement wctype()");
+    TODO();
+}
+
+wint_t towlower(wint_t wc)
+{
+    return __inline_tolower(wc);
+}
+
+wint_t towupper(wint_t wc)
+{
+    return __inline_toupper(wc);
+}
+
+wint_t towctrans(wint_t, wctrans_t)
+{
+    dbgln("FIXME: Implement towctrans()");
+    TODO();
+}
+
+wctrans_t wctrans(const char*)
+{
+    dbgln("FIXME: Implement wctrans()");
     TODO();
 }
 }
