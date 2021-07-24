8e4166fd10a198821cc89578078d42897b9c847b
Kenneth Myhra <kennethmyhra@gmail.com>
2021-07-24 21:44:24 +0200

    LibC: Declare wide character type functions in wctype.h
    Declares all wide character handling functions in wctype.h. All calls
    are forwarded to the corresponding character handling function in
    ctype.h.
    
    These functions are declared, but not implemented:
    - iswctype
    - wctype
    - towctrans
    - wctrans
    
    This should also resolve a build issue with the 'sed' port getting
    confused with iswprint being declared twice. Seems like it expected
    more functions from wctype.h and then had a backup strategy of adding
    its own wctype.h.

--- Userland/Libraries/LibC/wctype.h
+++ Userland/Libraries/LibC/wctype.h
@@ -6,12 +6,32 @@
 
 #pragma once
 
+#include <AK/Format.h>
+#include <assert.h>
+#include <ctype.h>
 #include <wchar.h>
 
 __BEGIN_DECLS
 
-wctype_t wctype(const char* name);
-int iswctype(wint_t wc, wctype_t desc);
+typedef const int* wctrans_t;
+
+int iswalnum(wint_t wc);
+int iswalpha(wint_t wc);
+int iswcntrl(wint_t wc);
+int iswdigit(wint_t wc);
+int iswxdigit(wint_t wc);
+int iswspace(wint_t wc);
+int iswpunct(wint_t wc);
 int iswprint(wint_t wc);
+int iswgraph(wint_t wc);
+int iswlower(wint_t wc);
+int iswupper(wint_t wc);
+int iswblank(wint_t wc);
+int iswctype(wint_t, wctype_t);
+wctype_t wctype(const char*);
+wint_t towlower(wint_t wc);
+wint_t towupper(wint_t wc);
+wint_t towctrans(wint_t, wctrans_t);
+wctrans_t wctrans(const char*);
 
 __END_DECLS
