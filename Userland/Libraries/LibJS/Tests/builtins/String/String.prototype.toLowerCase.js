eb8a907ae01cb1ef765cefa6af78f25aa8db9ba1
Timothy Flynn <trflynn89@pm.me>
2021-07-25 15:17:38 -0400

    LibJS: Implement Unicode aware String.prototype.to{Upper,Lower}Case

--- Userland/Libraries/LibJS/Tests/builtins/String/String.prototype.toLowerCase.js
+++ Userland/Libraries/LibJS/Tests/builtins/String/String.prototype.toLowerCase.js
@@ -1,6 +1,10 @@
 test("basic functionality", () => {
     expect(String.prototype.toLowerCase).toHaveLength(0);
 
+    expect("ω".toLowerCase()).toBe("ω");
+    expect("Ω".toLowerCase()).toBe("ω");
+    expect("😀".toLowerCase()).toBe("😀");
+
     expect("foo".toLowerCase()).toBe("foo");
     expect("Foo".toLowerCase()).toBe("foo");
     expect("FOO".toLowerCase()).toBe("foo");
--- Userland/Libraries/LibJS/Tests/builtins/String/String.prototype.toUpperCase.js
+++ Userland/Libraries/LibJS/Tests/builtins/String/String.prototype.toUpperCase.js
@@ -1,6 +1,10 @@
 test("basic functionality", () => {
     expect(String.prototype.toUpperCase).toHaveLength(0);
 
+    expect("ω".toUpperCase()).toBe("Ω");
+    expect("Ω".toUpperCase()).toBe("Ω");
+    expect("😀".toUpperCase()).toBe("😀");
+
     expect("foo".toUpperCase()).toBe("FOO");
     expect("Foo".toUpperCase()).toBe("FOO");
     expect("FOO".toUpperCase()).toBe("FOO");
