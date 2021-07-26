b7434774164a4a5cbb7ca75c1c001f2fbe7cf073
Daniel Bertalan <dani@danielbertalan.dev>
2021-07-26 18:08:16 +0200

    LibDebug: Keep track of 'prologue end'
    This LineProgram instruction is emitted by Clang. Although we currently
    have no use for it (it's mostly a debugger feature), we need to handle
    this opcode, as otherwise CrashReporter wouldn't work.

--- Userland/Libraries/LibDebug/Dwarf/LineProgram.cpp
+++ Userland/Libraries/LibDebug/Dwarf/LineProgram.cpp
@@ -246,6 +246,10 @@ void LineProgram::handle_standard_opcode(u8 opcode)
         m_basic_block = true;
         break;
     }
+    case StandardOpcodes::SetProlougeEnd: {
+        m_prologue_end = true;
+        break;
+    }
     default:
         dbgln("Unhandled LineProgram opcode {}", opcode);
         VERIFY_NOT_REACHED();
@@ -268,6 +272,7 @@ void LineProgram::handle_special_opcode(u8 opcode)
     append_to_line_info();
 
     m_basic_block = false;
+    m_prologue_end = false;
 }
 
 void LineProgram::run_program()
