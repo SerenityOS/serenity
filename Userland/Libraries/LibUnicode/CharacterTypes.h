39f971e42b25c6d3004e40a230dd6722dacb667b
Timothy Flynn <trflynn89@pm.me>
2021-07-27 13:46:08 -0400

    LibUnicode: Begin implementing special Unicode case folding
    This implements unconditional special case folding, and conditional
    folding for non-locale cases. Worth noting that the only conditional,
    non-locale special case is for converting an uppercase sigma to
    lowercase.

--- Userland/Libraries/LibUnicode/CharacterTypes.h
+++ Userland/Libraries/LibUnicode/CharacterTypes.h
@@ -6,11 +6,18 @@
 
 #pragma once
 
+#include <AK/Forward.h>
+#include <AK/String.h>
 #include <AK/Types.h>
 
 namespace Unicode {
 
+// Note: The single code point case conversions only perform simple case folding.
+// Use the full-string transformations for full case folding.
 u32 to_unicode_lowercase(u32 code_point);
 u32 to_unicode_uppercase(u32 code_point);
+
+String to_unicode_lowercase_full(StringView const&);
+String to_unicode_uppercase_full(StringView const&);
 
 }
