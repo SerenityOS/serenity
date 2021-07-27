ae97e3d9471dbd43f3659797da75ee777fbbdc7e
Liav A <liavalb@gmail.com>
2021-07-27 20:26:22 +0300

    Kernel: Ensure we read valid values from the RTC CMOS registers
    We try to read twice from the RTC CMOS registers, and if the values are
    not the same for 5 attempts, we know there's a malfunction with the
    hardware so we declare these values as bogus in the kernel log.

--- Kernel/RTC.h
+++ Kernel/RTC.h
@@ -13,6 +13,5 @@ namespace RTC {
 void initialize();
 time_t now();
 time_t boot_time();
-void read_registers(unsigned& year, unsigned& month, unsigned& day, unsigned& hour, unsigned& minute, unsigned& second);
 
 }
