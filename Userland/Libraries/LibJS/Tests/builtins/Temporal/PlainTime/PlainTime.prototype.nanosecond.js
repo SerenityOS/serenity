e09f2786bf443b96146758531f9b44916da2e239
Linus Groh <mail@linusgroh.de>
2021-07-28 19:07:29 +0100

    LibJS: Implement Temporal.PlainTime.prototype.nanosecond

---
+++ Userland/Libraries/LibJS/Tests/builtins/Temporal/PlainTime/PlainTime.prototype.nanosecond.js
@@ -0,0 +1,14 @@
+describe("correct behavior", () => {
+    test("basic functionality", () => {
+        const plainTime = new Temporal.PlainTime(0, 0, 0, 0, 0, 12);
+        expect(plainTime.nanosecond).toBe(12);
+    });
+});
+
+test("errors", () => {
+    test("this value must be a Temporal.PlainTime object", () => {
+        expect(() => {
+            Reflect.get(Temporal.PlainTime.prototype, "nanosecond", "foo");
+        }).toThrowWithMessage(TypeError, "Not a Temporal.PlainTime");
+    });
+});
