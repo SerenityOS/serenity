331911412745eded57f5e5c461d4cd7fbcdedb68
Ali Mohammad Pur <ali.mpfard@gmail.com>
2021-07-28 05:37:24 +0430

    LibCpp: Add support for parsing reference types

--- Userland/Libraries/LibCpp/AST.cpp
+++ Userland/Libraries/LibCpp/AST.cpp
@@ -100,6 +100,19 @@ String Pointer::to_string() const
     return builder.to_string();
 }
 
+String Reference::to_string() const
+{
+    if (!m_referenced_type)
+        return {};
+    StringBuilder builder;
+    builder.append(m_referenced_type->to_string());
+    if (m_kind == Kind::Lvalue)
+        builder.append("&");
+    else
+        builder.append("&&");
+    return builder.to_string();
+}
+
 void Parameter::dump(FILE* output, size_t indent) const
 {
     ASTNode::dump(output, indent);
@@ -356,6 +369,16 @@ void Pointer::dump(FILE* output, size_t indent) const
     ASTNode::dump(output, indent);
     if (!m_pointee.is_null()) {
         m_pointee->dump(output, indent + 1);
+    }
+}
+
+void Reference::dump(FILE* output, size_t indent) const
+{
+    ASTNode::dump(output, indent);
+    print_indent(output, indent + 1);
+    outln(output, "{}", m_kind == Kind::Lvalue ? "&" : "&&");
+    if (!m_referenced_type.is_null()) {
+        m_referenced_type->dump(output, indent + 1);
     }
 }
 
