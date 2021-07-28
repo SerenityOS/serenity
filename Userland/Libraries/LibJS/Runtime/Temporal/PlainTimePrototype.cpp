ad89a205bc002b68412c1554e70516efc4728137
Linus Groh <mail@linusgroh.de>
2021-07-28 18:42:34 +0100

    LibJS: Implement Temporal.PlainTime.prototype.valueOf()

--- Userland/Libraries/LibJS/Runtime/Temporal/PlainTimePrototype.cpp
+++ Userland/Libraries/LibJS/Runtime/Temporal/PlainTimePrototype.cpp
@@ -23,6 +23,17 @@ void PlainTimePrototype::initialize(GlobalObject& global_object)
 
     // 4.3.2 Temporal.PlainTime.prototype[ @@toStringTag ], https://tc39.es/proposal-temporal/#sec-temporal.plaintime.prototype-@@tostringtag
     define_direct_property(*vm.well_known_symbol_to_string_tag(), js_string(vm.heap(), "Temporal.PlainTime"), Attribute::Configurable);
+
+    u8 attr = Attribute::Writable | Attribute::Configurable;
+    define_native_function(vm.names.valueOf, value_of, 0, attr);
+}
+
+// 4.3.23 Temporal.PlainTime.prototype.valueOf ( ), https://tc39.es/proposal-temporal/#sec-temporal.plaintime.prototype.valueof
+JS_DEFINE_NATIVE_FUNCTION(PlainTimePrototype::value_of)
+{
+    // 1. Throw a TypeError exception.
+    vm.throw_exception<TypeError>(global_object, ErrorType::Convert, "Temporal.PlainTime", "a primitive value");
+    return {};
 }
 
 }
