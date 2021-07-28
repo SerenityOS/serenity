66dbfc03aa9401c4485017185e49331731534950
Linus Groh <mail@linusgroh.de>
2021-07-28 18:53:05 +0100

    LibJS: Implement Temporal.PlainTime.prototype.hour

--- Userland/Libraries/LibJS/Runtime/Temporal/PlainTimePrototype.cpp
+++ Userland/Libraries/LibJS/Runtime/Temporal/PlainTimePrototype.cpp
@@ -27,6 +27,7 @@ void PlainTimePrototype::initialize(GlobalObject& global_object)
     define_direct_property(*vm.well_known_symbol_to_string_tag(), js_string(vm.heap(), "Temporal.PlainTime"), Attribute::Configurable);
 
     define_native_accessor(vm.names.calendar, calendar_getter, {}, Attribute::Configurable);
+    define_native_accessor(vm.names.hour, hour_getter, {}, Attribute::Configurable);
 
     u8 attr = Attribute::Writable | Attribute::Configurable;
     define_native_function(vm.names.valueOf, value_of, 0, attr);
@@ -56,6 +57,19 @@ JS_DEFINE_NATIVE_FUNCTION(PlainTimePrototype::calendar_getter)
 
     // 3. Return temporalTime.[[Calendar]].
     return Value(&temporal_time->calendar());
+}
+
+// 4.3.4 get Temporal.PlainTime.prototype.hour, https://tc39.es/proposal-temporal/#sec-get-temporal.plaintime.prototype.hour
+JS_DEFINE_NATIVE_FUNCTION(PlainTimePrototype::hour_getter)
+{
+    // 1. Let temporalTime be the this value.
+    // 2. Perform ? RequireInternalSlot(temporalTime, [[InitializedTemporalTime]]).
+    auto* temporal_time = typed_this(global_object);
+    if (vm.exception())
+        return {};
+
+    // 3. Return 𝔽(temporalTime.[[ISOHour]]).
+    return Value(temporal_time->iso_hour());
 }
 
 // 4.3.23 Temporal.PlainTime.prototype.valueOf ( ), https://tc39.es/proposal-temporal/#sec-temporal.plaintime.prototype.valueof
