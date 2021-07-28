ac7f7d287bd5e06db0d4af627f6a0d8c6dad6c38
Linus Groh <mail@linusgroh.de>
2021-07-28 18:42:34 +0100

    LibJS: Implement Temporal.PlainTime.prototype.valueOf()

--- Userland/Libraries/LibJS/Runtime/Temporal/PlainTimePrototype.h
+++ Userland/Libraries/LibJS/Runtime/Temporal/PlainTimePrototype.h
@@ -17,6 +17,9 @@ public:
     explicit PlainTimePrototype(GlobalObject&);
     virtual void initialize(GlobalObject&) override;
     virtual ~PlainTimePrototype() override = default;
+
+private:
+    JS_DECLARE_NATIVE_FUNCTION(value_of);
 };
 
 }
---
+++ Userland/Libraries/LibJS/Tests/builtins/Temporal/PlainTime/PlainTime.prototype.valueOf.js
@@ -0,0 +1,7 @@
+test("errors", () => {
+    test("throws TypeError", () => {
+        expect(() => {
+            new Temporal.PlainTime(19, 54, 38).valueOf();
+        }).toThrowWithMessage(TypeError, "Cannot convert Temporal.PlainTime to a primitive value");
+    });
+});
