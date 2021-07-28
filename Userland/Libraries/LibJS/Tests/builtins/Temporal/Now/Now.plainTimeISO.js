5e64156fce5913665e598db4fe929b9c83bf744f
Linus Groh <mail@linusgroh.de>
2021-07-28 19:29:16 +0100

    LibJS: Implement Temporal.Now.plainTimeISO()

---
+++ Userland/Libraries/LibJS/Tests/builtins/Temporal/Now/Now.plainTimeISO.js
@@ -0,0 +1,22 @@
+describe("correct behavior", () => {
+    test("length is 0", () => {
+        expect(Temporal.Now.plainTimeISO).toHaveLength(0);
+    });
+
+    test("basic functionality", () => {
+        const plainTime = Temporal.Now.plainTimeISO();
+        expect(plainTime).toBeInstanceOf(Temporal.PlainTime);
+        expect(plainTime.calendar.id).toBe("iso8601");
+    });
+
+    test("custom time zone", () => {
+        const timeZone = {
+            getOffsetNanosecondsFor() {
+                return 86400000000000;
+            },
+        };
+        const plainTime = Temporal.Now.plainTimeISO();
+        const plainTimeWithOffset = Temporal.Now.plainTimeISO(timeZone);
+        // FIXME: Compare these in a sensible way
+    });
+});
