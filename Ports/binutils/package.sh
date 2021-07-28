102a17b7be94cc5d786eedfcdfab9e20b771d966
Gunnar Beutner <gbeutner@serenityos.org>
2021-07-28 18:21:20 +0200

    Ports: Update binutils to version 2.37

--- Ports/binutils/package.sh
+++ Ports/binutils/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=binutils
-version=2.36.1
+version=2.37
 useconfigure=true
 configopts="--target=${SERENITY_ARCH}-pc-serenity --with-sysroot=/ --with-build-sysroot=${SERENITY_INSTALL_ROOT} --disable-werror --disable-gdb --disable-nls"
 files="https://ftpmirror.gnu.org/gnu/binutils/binutils-${version}.tar.xz binutils-${version}.tar.xz
