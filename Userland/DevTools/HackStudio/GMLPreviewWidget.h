6dcb4a96a95af4f830411b0e0595bf2d9d8bdbd8
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2021-07-28 20:58:16 +0100

    HackStudio: Add GML Preview action tab
    This allows us to show a GML Preview in realtime via
    HackStudio::GMLPreviewWidget! :^)

---
+++ Userland/DevTools/HackStudio/GMLPreviewWidget.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021, Conor Byrne <cbyrneee@protonmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/LexicalPath.h>
+#include <LibGUI/Widget.h>
+
+namespace HackStudio {
+
+class GMLPreviewWidget final : public GUI::Widget {
+    C_OBJECT(GMLPreviewWidget)
+public:
+    void load_gml(String const&);
+
+private:
+    explicit GMLPreviewWidget(String const&);
+};
+
+}
