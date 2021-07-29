9a6ccfe90c02b0b984ebf4fe70b42a738746a0a7
Gunnar Beutner <gbeutner@serenityos.org>
2021-07-29 08:42:09 +0200

    Kernel: Embed the right symbol count into the kernel.map file on macOS
    On macOS the wc tool prefixes its output with a bunch of spaces which
    resulted in us incorrectly using "00000000" as the symbol count.
    
    Fixes #9080.

--- Kernel/mkmap.sh
+++ Kernel/mkmap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 tmp=$(mktemp)
 nm -n Kernel | grep -vE \\.Lubsan_data | awk '{ if ($2 != "a") print; }' | uniq > "$tmp"
-printf "%08x\n" "$(wc -l "$tmp" | cut -f1 -d' ')" > kernel.map
+printf "%08x\n" "$(wc -l "$tmp" | awk '{print $1}')" > kernel.map
 c++filt < "$tmp" >> kernel.map
 rm -f "$tmp"
