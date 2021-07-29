decd47c1c201779ff3f451801e1c432a9f95d09d
Gunnar Beutner <gbeutner@serenityos.org>
2021-07-29 08:48:47 +0200

    Kernel: Use our toolchain's c++filt tool for the kernel map
    The host's version of c++filt might not work on some operating systems,
    e.g. macOS.

--- Kernel/mkmap.sh
+++ Kernel/mkmap.sh
@@ -2,5 +2,6 @@
 tmp=$(mktemp)
 nm -n Kernel | grep -vE \\.Lubsan_data | awk '{ if ($2 != "a") print; }' | uniq > "$tmp"
 printf "%08x\n" "$(wc -l "$tmp" | awk '{print $1}')" > kernel.map
-c++filt < "$tmp" >> kernel.map
+CXXFILT="${CXXFILT:-c++filt}"
+"$CXXFILT" < "$tmp" >> kernel.map
 rm -f "$tmp"
