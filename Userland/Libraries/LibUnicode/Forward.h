f1809db9946200bcbe86df8c3d6ebf87b44ec0a1
Timothy Flynn <trflynn89@pm.me>
2021-07-28 21:45:09 -0400

    LibUnicode: Add public methods to compare and lookup Unicode properties
    Adds methods to retrieve a Unicode property from a string and to check
    if a code point matches a Unicode property.
    
    Also adds a <LibUnicode/Forward.h> header.

---
+++ Userland/Libraries/LibUnicode/Forward.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021, Tim Flynn <trflynn89@pm.me>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/Types.h>
+
+namespace Unicode {
+
+enum class Condition;
+enum class GeneralCategory;
+enum class Locale;
+enum class Property : u64;
+enum class WordBreakProperty;
+
+struct SpecialCasing;
+struct UnicodeData;
+
+}
