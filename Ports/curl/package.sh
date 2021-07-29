99d42ba732933d1b46bc0c29f2acdcca55ac160e
Luke <luke.wilde@live.co.uk>
2021-07-29 23:59:59 +0100

    Ports: Update curl to 7.78.0

--- Ports/curl/package.sh
+++ Ports/curl/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=curl
-version=7.77.0
+version=7.78.0
 useconfigure=true
-files="https://curl.se/download/curl-${version}.tar.bz2 curl-${version}.tar.bz2 6c0c28868cb82593859fc43b9c8fdb769314c855c05cf1b56b023acf855df8ea"
+files="https://curl.se/download/curl-${version}.tar.bz2 curl-${version}.tar.bz2 98530b317dc95ccb324bbe4f834f07bb642fbc393b794ddf3434f246a71ea44a"
 auth_type=sha256
 depends="openssl zlib"
 configopts="--disable-ntlm-wb --with-openssl=${SERENITY_INSTALL_ROOT}/usr/local"
