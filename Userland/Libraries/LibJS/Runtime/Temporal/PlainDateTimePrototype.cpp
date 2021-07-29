602a8be021e5cd65e2f7554128f7c658f4ce2120
Idan Horowitz <idan.horowitz@gmail.com>
2021-07-30 00:19:30 +0300

    LibJS: Implement Temporal.PlainDateTime.prototype.daysInWeek

--- Userland/Libraries/LibJS/Runtime/Temporal/PlainDateTimePrototype.cpp
+++ Userland/Libraries/LibJS/Runtime/Temporal/PlainDateTimePrototype.cpp
@@ -41,6 +41,7 @@ void PlainDateTimePrototype::initialize(GlobalObject& global_object)
     define_native_accessor(vm.names.dayOfWeek, day_of_week_getter, {}, Attribute::Configurable);
     define_native_accessor(vm.names.dayOfYear, day_of_year_getter, {}, Attribute::Configurable);
     define_native_accessor(vm.names.weekOfYear, week_of_year_getter, {}, Attribute::Configurable);
+    define_native_accessor(vm.names.daysInWeek, days_in_week_getter, {}, Attribute::Configurable);
 
     u8 attr = Attribute::Writable | Attribute::Configurable;
     define_native_function(vm.names.valueOf, value_of, 0, attr);
@@ -262,6 +263,22 @@ JS_DEFINE_NATIVE_FUNCTION(PlainDateTimePrototype::week_of_year_getter)
 
     // 4. Return ? CalendarWeekOfYear(calendar, dateTime).
     return calendar_week_of_year(global_object, calendar, *date_time);
+}
+
+// 5.3.17 get Temporal.PlainDateTime.prototype.daysInWeek, https://tc39.es/proposal-temporal/#sec-get-temporal.plaindatetime.prototype.daysinweek
+JS_DEFINE_NATIVE_FUNCTION(PlainDateTimePrototype::days_in_week_getter)
+{
+    // 1. Let dateTime be the this value.
+    // 2. Perform ? RequireInternalSlot(dateTime, [[InitializedTemporalDateTime]]).
+    auto* date_time = typed_this(global_object);
+    if (vm.exception())
+        return {};
+
+    // 3. Let calendar be dateTime.[[Calendar]].
+    auto& calendar = date_time->calendar();
+
+    // 4. Return ? CalendarDaysInWeek(calendar, dateTime).
+    return calendar_days_in_week(global_object, calendar, *date_time);
 }
 
 // 5.3.35 Temporal.PlainDateTime.prototype.valueOf ( ), https://tc39.es/proposal-temporal/#sec-temporal.plaindatetime.prototype.valueof
--- Userland/Libraries/LibJS/Runtime/Temporal/PlainDateTimePrototype.h
+++ Userland/Libraries/LibJS/Runtime/Temporal/PlainDateTimePrototype.h
@@ -33,6 +33,7 @@ private:
     JS_DECLARE_NATIVE_FUNCTION(day_of_week_getter);
     JS_DECLARE_NATIVE_FUNCTION(day_of_year_getter);
     JS_DECLARE_NATIVE_FUNCTION(week_of_year_getter);
+    JS_DECLARE_NATIVE_FUNCTION(days_in_week_getter);
     JS_DECLARE_NATIVE_FUNCTION(value_of);
     JS_DECLARE_NATIVE_FUNCTION(to_plain_date);
     JS_DECLARE_NATIVE_FUNCTION(get_iso_fields);
