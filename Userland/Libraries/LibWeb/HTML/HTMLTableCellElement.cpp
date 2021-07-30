f7c031e2f3358f59a4e219c1a96dfb7004bfcd0c
Sam Atkins <atkinssj@gmail.com>
2021-07-30 19:31:46 +0100

    LibWeb: Switch to new CSS Parser :^)
    Change all the places that were including the deprecated parser, to
    include the new one instead, and then delete the old parser code.
    
    `ParentNode::query_selector[_all]()` now treat their input as a
    comma-separated list of selectors, instead of just one, and return
    elements that match any of the selectors in that list. This is according
    to these specs:
    
    - querySelector/querySelectorAll:
    https://dom.spec.whatwg.org/#ref-for-dom-parentnode-queryselector%E2%91%A0
    - selector matching algorithm:
    https://www.w3.org/TR/selectors-4/#match-against-tree

--- Userland/Libraries/LibWeb/HTML/HTMLTableCellElement.cpp
+++ Userland/Libraries/LibWeb/HTML/HTMLTableCellElement.cpp
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#include <LibWeb/CSS/Parser/DeprecatedCSSParser.h>
+#include <LibWeb/CSS/Parser/Parser.h>
 #include <LibWeb/HTML/HTMLTableCellElement.h>
 
 namespace Web::HTML {
