5ab27cc3ce491fbe82a6314f184b47835992bda9
Brian Gianforcaro <bgianf@serenityos.org>
2021-07-30 02:05:15 -0700

    Tests: Validate unmapping 0x0 doesn't crash the Kernel
    Previously unmapping any offset starting at 0x0 would assert in the
    kernel, add a regression test to validate the fix.
    
    Co-authored-by: Federico Guerinoni <guerinoni.federico@gmail.com>

---
+++ Tests/Kernel/TestMunMap.cpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021, Brian Gianforcaro <bgianf@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibTest/TestCase.h>
+#include <errno.h>
+#include <sys/mman.h>
+
+TEST_CASE(munmap_zero_page)
+{
+    // munmap of the unmapped zero page should always fail.
+    auto res = munmap(0x0, 0xF);
+    EXPECT_EQ(res, -1);
+    EXPECT_EQ(errno, EINVAL);
+}
