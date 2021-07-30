8e3431d56d25dc90c588d2729feba5a9a133a289
Faeliore <taymor@pm.me>
2021-07-29 21:03:55 -0400

    MailSettings: Add basic mail settings dialog
    MailSettings: Add a GML file for Mail settings
    
    MailSettings: Add an AF desktop file for Mail Settings
    
    MailSettings: Unveil /res in mail settings, fix GML
    
    MailSettings: Mail settings texteditor->textbox
    
    MailSettings: Update mail username to correct category in settings
    
    Modified Mail settings GML to properly represent ports >100
    
    MailSettings: Update/fix mail settings GML
    
    MailSettings: Adjust GML, add icons for mail settings
    
    MailSettings: Change Okay button to OK
    
    MailSettings: Change mail setting reset button to revert
    
    MailSettings: Fix incorrect variable names in mail settings
    
    MailSettings: Add newlines af EOF of all mail setting files
    
    MailSettings: Mail settings linting issues fixed
    
    MailSettings: Increase size of icon features
    Code cleaning/styling changes as per gunnarbeutner review
    Made settings descriptions more friendly per sin-ack review
    
    MailSettings: Fixes as per PR comments
    
    MailSettings: Fix checkbox weirdness
    
    MailSettings: Adjust width of checkbox
    
    MailSettings: Remove unneccessary update() call
    
    MailSettings: Replace port SpinBox with ComboBox
    
    MailSettings: Add colons to labels, remove port 110 option
    
    MailSettings: Remove custom model, use ItemListModel
    
    MailSettings: Change relative icon paths to absolute ones

---
+++ Userland/Applications/MailSettings/MailSettingsWindow.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2021, The SerenityOS developers
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibGUI/TextEditor.h>
+#include <LibGUI/Window.h>
+
+class MailSettingsWindow final : public GUI::Window {
+    C_OBJECT(MailSettingsWindow)
+
+private:
+    MailSettingsWindow();
+
+    void reset_default_values();
+    void write_values();
+
+    String m_server;
+    String m_port;
+    bool m_tls { false };
+    String m_email;
+    Vector<String> m_common_ports;
+    RefPtr<Core::ConfigFile> m_config;
+
+    RefPtr<GUI::TextBox> m_server_inputbox;
+    RefPtr<GUI::ComboBox> m_port_combobox;
+    RefPtr<GUI::CheckBox> m_tls_checkbox;
+    RefPtr<GUI::TextBox> m_email_inputbox;
+
+    RefPtr<GUI::Button> m_reset_button;
+    RefPtr<GUI::Button> m_ok_button;
+    RefPtr<GUI::Button> m_cancel_button;
+    RefPtr<GUI::Button> m_apply_button;
+};
