7ddf2c424032fa960e392bdc80527dd85161f789
Faeliore <taymor@pm.me>
2021-07-29 21:03:55 -0400

    MailSettings: Add basic mail settings dialog
    MailSettings: Add a GML file for Mail settings
    
    MailSettings: Add an AF desktop file for Mail Settings
    
    MailSettings: Unveil /res in mail settings, fix GML
    
    MailSettings: Mail settings texteditor->textbox
    
    MailSettings: Update mail username to correct category in settings
    
    Modified Mail settings GML to properly represent ports >100
    
    MailSettings: Update/fix mail settings GML
    
    MailSettings: Adjust GML, add icons for mail settings
    
    MailSettings: Change Okay button to OK
    
    MailSettings: Change mail setting reset button to revert
    
    MailSettings: Fix incorrect variable names in mail settings
    
    MailSettings: Add newlines af EOF of all mail setting files
    
    MailSettings: Mail settings linting issues fixed
    
    MailSettings: Increase size of icon features
    Code cleaning/styling changes as per gunnarbeutner review
    Made settings descriptions more friendly per sin-ack review
    
    MailSettings: Fixes as per PR comments
    
    MailSettings: Fix checkbox weirdness
    
    MailSettings: Adjust width of checkbox
    
    MailSettings: Remove unneccessary update() call
    
    MailSettings: Replace port SpinBox with ComboBox
    
    MailSettings: Add colons to labels, remove port 110 option
    
    MailSettings: Remove custom model, use ItemListModel
    
    MailSettings: Change relative icon paths to absolute ones

---
+++ Userland/Applications/MailSettings/main.cpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2021, The SerenityOS developers
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include "MailSettingsWindow.h"
+#include <LibGUI/Application.h>
+#include <LibGUI/Icon.h>
+#include <unistd.h>
+
+int main(int argc, char** argv)
+{
+    if (pledge("stdio rpath cpath wpath recvfd sendfd unix proc exec", nullptr) < 0) {
+        perror("pledge");
+        return 1;
+    }
+
+    auto app = GUI::Application::construct(argc, argv);
+
+    if (pledge("stdio rpath cpath wpath recvfd sendfd proc exec", nullptr) < 0) {
+        perror("pledge");
+        return 1;
+    }
+
+    auto app_icon = GUI::Icon::default_icon("app-mail-settings");
+
+    auto window = MailSettingsWindow::construct();
+    window->set_title("Mail Settings");
+    window->resize(400, 480);
+    window->set_resizable(false);
+    window->set_minimizable(false);
+    window->set_icon(app_icon.bitmap_for_size(16));
+
+    window->show();
+    return app->exec();
+}
