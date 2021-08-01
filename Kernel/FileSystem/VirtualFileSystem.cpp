ff12d97b47e0d3b3fcbe8685e352efd0d308f9da
LuK1337 <priv.luk@gmail.com>
2021-08-01 22:58:50 +0200

    VirtualFileSystem: Don't let rename() overwrite non-empty directory
    According to POSIX, rename shouldn't succeed if newpath is a non-empty
    directory.

--- Kernel/FileSystem/VirtualFileSystem.cpp
+++ Kernel/FileSystem/VirtualFileSystem.cpp
@@ -499,6 +499,22 @@ KResult VirtualFileSystem::rename(StringView old_path, StringView new_path, Cust
         return EPERM;
     }
 
+    if (!new_custody_or_error.is_error()) {
+        auto& new_inode = new_custody_or_error.value()->inode();
+
+        if (old_inode.index() != new_inode.index() && old_inode.is_directory() && new_inode.is_directory()) {
+            size_t child_count = 0;
+            auto traversal_result = new_inode.traverse_as_directory([&child_count](auto&) {
+                ++child_count;
+                return child_count <= 2;
+            });
+            if (traversal_result.is_error())
+                return traversal_result;
+            if (child_count > 2)
+                return ENOTEMPTY;
+        }
+    }
+
     auto& old_parent_inode = old_parent_custody->inode();
     auto& new_parent_inode = new_parent_custody->inode();
 
