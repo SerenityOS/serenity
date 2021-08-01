b6ff7f4fcc90fbd378cc3886ae686fd54b8e6190
Timothy Flynn <trflynn89@pm.me>
2021-08-01 19:02:19 -0400

    LibJS: Allow PrimitiveString to be created with a UTF-16 string
    PrimitiveString may currently only be created with a UTF-8 string, and
    it transcodes on the fly when a UTF-16 string is needed. Allow creating
    a PrimitiveString from a UTF-16 string to avoid unnecessary transcoding
    when the caller only wants UTF-16.

--- Userland/Libraries/LibJS/Runtime/PrimitiveString.h
+++ Userland/Libraries/LibJS/Runtime/PrimitiveString.h
@@ -15,9 +15,13 @@ namespace JS {
 class PrimitiveString final : public Cell {
 public:
     explicit PrimitiveString(String);
+    explicit PrimitiveString(Vector<u16>);
     virtual ~PrimitiveString();
 
-    String const& string() const { return m_string; }
+    PrimitiveString(PrimitiveString const&) = delete;
+    PrimitiveString& operator=(PrimitiveString const&) = delete;
+
+    String const& string() const;
 
     Vector<u16> const& utf16_string() const;
     Utf16View utf16_string_view() const;
@@ -25,12 +29,18 @@ public:
 private:
     virtual const char* class_name() const override { return "PrimitiveString"; }
 
-    String m_string;
+    mutable String m_utf8_string;
+    mutable bool m_has_utf8_string { false };
+
     mutable Vector<u16> m_utf16_string;
+    mutable bool m_has_utf16_string { false };
 };
 
 PrimitiveString* js_string(Heap&, Utf16View const&);
 PrimitiveString* js_string(VM&, Utf16View const&);
+
+PrimitiveString* js_string(Heap&, Vector<u16>);
+PrimitiveString* js_string(VM&, Vector<u16>);
 
 PrimitiveString* js_string(Heap&, String);
 PrimitiveString* js_string(VM&, String);
