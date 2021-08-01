3ab6c13fbca7de0b477df84d946862c8ddd24576
Tobias Christiansen <tobi@tobyase.de>
2021-08-01 17:19:10 +0200

    PixelPaint: Add a Dialog for the creation of a Guide
    You can select whether you want a horizontal or a vertical Guide and
    specify the offset you want it to be at.

---
+++ Userland/Applications/PixelPaint/CreateNewGuideDialog.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2021, Tobias Christiansen <tobi@tobyase.de>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include "Guide.h"
+#include <LibGUI/Dialog.h>
+
+namespace PixelPaint {
+
+class CreateNewGuideDialog final : public GUI::Dialog {
+    C_OBJECT(CreateNewGuideDialog);
+
+public:
+    String const offset() const { return m_offset; }
+    Guide::Orientation orientation() const { return m_orientation; }
+
+private:
+    CreateNewGuideDialog(GUI::Window* parent_window);
+
+    String m_offset;
+    Guide::Orientation m_orientation;
+
+    bool m_is_horizontal_checked { false };
+    bool m_is_vertical_checked { false };
+};
+
+}
