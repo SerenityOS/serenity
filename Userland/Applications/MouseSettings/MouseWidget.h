171f35266a04db3ec0e34b8014630e09713911b0
Maciej Zygmanowski <sppmacd@pm.me>
2021-08-02 12:14:52 +0200

    MouseSettings: Add a tab to change cursor theme

---
+++ Userland/Applications/MouseSettings/MouseWidget.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2021, the SerenityOS developers.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibGUI/Window.h>
+
+#include "DoubleClickArrowWidget.h"
+
+class MouseWidget final : public GUI::Widget {
+    C_OBJECT(MouseWidget)
+public:
+    virtual ~MouseWidget() override;
+
+    void update_window_server();
+    void reset_default_values();
+
+private:
+    MouseWidget();
+
+    RefPtr<GUI::HorizontalSlider> m_speed_slider;
+    RefPtr<GUI::Label> m_speed_label;
+    RefPtr<GUI::SpinBox> m_scroll_length_spinbox;
+    RefPtr<GUI::HorizontalSlider> m_double_click_speed_slider;
+    RefPtr<GUI::Label> m_double_click_speed_label;
+    RefPtr<MouseSettings::DoubleClickArrowWidget> m_double_click_arrow_widget;
+};
