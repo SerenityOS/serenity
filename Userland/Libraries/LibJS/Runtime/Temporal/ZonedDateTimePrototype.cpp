b94b7cada0939e05646f4452a38f3913520cf68b
Linus Groh <mail@linusgroh.de>
2021-08-04 22:50:53 +0100

    LibJS: Implement Temporal.ZonedDateTime.prototype.monthCode

--- Userland/Libraries/LibJS/Runtime/Temporal/ZonedDateTimePrototype.cpp
+++ Userland/Libraries/LibJS/Runtime/Temporal/ZonedDateTimePrototype.cpp
@@ -34,6 +34,7 @@ void ZonedDateTimePrototype::initialize(GlobalObject& global_object)
     define_native_accessor(vm.names.timeZone, time_zone_getter, {}, Attribute::Configurable);
     define_native_accessor(vm.names.year, year_getter, {}, Attribute::Configurable);
     define_native_accessor(vm.names.month, month_getter, {}, Attribute::Configurable);
+    define_native_accessor(vm.names.monthCode, month_code_getter, {}, Attribute::Configurable);
 }
 
 static ZonedDateTime* typed_this(GlobalObject& global_object)
@@ -127,6 +128,33 @@ JS_DEFINE_NATIVE_FUNCTION(ZonedDateTimePrototype::month_getter)
 
     // 7. Return ? CalendarMonth(calendar, temporalDateTime).
     return Value(calendar_month(global_object, calendar, *temporal_date_time));
+}
+
+// 6.3.7 get Temporal.ZonedDateTime.prototype.monthCode, https://tc39.es/proposal-temporal/#sec-get-temporal.zoneddatetime.prototype.monthcode
+JS_DEFINE_NATIVE_FUNCTION(ZonedDateTimePrototype::month_code_getter)
+{
+    // 1. Let zonedDateTime be the this value.
+    // 2. Perform ? RequireInternalSlot(zonedDateTime, [[InitializedTemporalZonedDateTime]]).
+    auto* zoned_date_time = typed_this(global_object);
+    if (vm.exception())
+        return {};
+
+    // 3. Let timeZone be zonedDateTime.[[TimeZone]].
+    auto& time_zone = zoned_date_time->time_zone();
+
+    // 4. Let instant be ! CreateTemporalInstant(zonedDateTime.[[Nanoseconds]]).
+    auto* instant = create_temporal_instant(global_object, zoned_date_time->nanoseconds());
+
+    // 5. Let calendar be zonedDateTime.[[Calendar]].
+    auto& calendar = zoned_date_time->calendar();
+
+    // 6. Let temporalDateTime be ? BuiltinTimeZoneGetPlainDateTimeFor(timeZone, instant, calendar).
+    auto* temporal_date_time = builtin_time_zone_get_plain_date_time_for(global_object, &time_zone, *instant, calendar);
+    if (vm.exception())
+        return {};
+
+    // 7. Return ? CalendarMonthCode(calendar, temporalDateTime).
+    return js_string(vm, calendar_month_code(global_object, calendar, *temporal_date_time));
 }
 
 }
