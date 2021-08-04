30e27f6483b0b709fb0b729a9cabd5bece9d53e3
Linus Groh <mail@linusgroh.de>
2021-08-04 22:50:53 +0100

    LibJS: Implement Temporal.ZonedDateTime.prototype.monthCode

--- Userland/Libraries/LibJS/Runtime/Temporal/ZonedDateTimePrototype.h
+++ Userland/Libraries/LibJS/Runtime/Temporal/ZonedDateTimePrototype.h
@@ -23,6 +23,7 @@ private:
     JS_DECLARE_NATIVE_FUNCTION(time_zone_getter);
     JS_DECLARE_NATIVE_FUNCTION(year_getter);
     JS_DECLARE_NATIVE_FUNCTION(month_getter);
+    JS_DECLARE_NATIVE_FUNCTION(month_code_getter);
 };
 
 }
