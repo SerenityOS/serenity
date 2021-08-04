744d4526a29f7d63e68d65a0fa1a113facef2d62
Linus Groh <mail@linusgroh.de>
2021-08-04 22:53:37 +0100

    LibJS: Implement Temporal.ZonedDateTime.prototype.minute

---
+++ Userland/Libraries/LibJS/Tests/builtins/Temporal/ZonedDateTime/ZonedDateTime.prototype.minute.js
@@ -0,0 +1,15 @@
+describe("correct behavior", () => {
+    test("basic functionality", () => {
+        const timeZone = new Temporal.TimeZone("UTC");
+        const zonedDateTime = new Temporal.ZonedDateTime(1625614921000000000n, timeZone);
+        expect(zonedDateTime.minute).toBe(42);
+    });
+});
+
+test("errors", () => {
+    test("this value must be a Temporal.ZonedDateTime object", () => {
+        expect(() => {
+            Reflect.get(Temporal.ZonedDateTime.prototype, "minute", "foo");
+        }).toThrowWithMessage(TypeError, "Not a Temporal.ZonedDateTime");
+    });
+});
