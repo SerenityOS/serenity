c8cd412ead63008140ff3777b1090ef5829b2a57
Linus Groh <mail@linusgroh.de>
2021-08-05 22:13:12 +0100

    LibJS: Implement Temporal.ZonedDateTime.prototype.toPlainDateTime()

--- Userland/Libraries/LibJS/Runtime/Temporal/ZonedDateTimePrototype.h
+++ Userland/Libraries/LibJS/Runtime/Temporal/ZonedDateTimePrototype.h
@@ -49,6 +49,7 @@ private:
     JS_DECLARE_NATIVE_FUNCTION(to_instant);
     JS_DECLARE_NATIVE_FUNCTION(to_plain_date);
     JS_DECLARE_NATIVE_FUNCTION(to_plain_time);
+    JS_DECLARE_NATIVE_FUNCTION(to_plain_date_time);
 };
 
 }
