3592a748b69e36b654422193cd7564dbb7eb2706
Linus Groh <mail@linusgroh.de>
2021-08-08 00:01:12 +0100

    LibJS: Implement Temporal.PlainYearMonth.prototype.monthsInYear

--- Userland/Libraries/LibJS/Runtime/Temporal/PlainYearMonthPrototype.h
+++ Userland/Libraries/LibJS/Runtime/Temporal/PlainYearMonthPrototype.h
@@ -25,6 +25,7 @@ private:
     JS_DECLARE_NATIVE_FUNCTION(month_code_getter);
     JS_DECLARE_NATIVE_FUNCTION(days_in_year_getter);
     JS_DECLARE_NATIVE_FUNCTION(days_in_month_getter);
+    JS_DECLARE_NATIVE_FUNCTION(months_in_year_getter);
 };
 
 }
