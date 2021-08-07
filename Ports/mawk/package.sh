548dedeb7503c4b01f872fb5b2620de303c63378
Jean-Baptiste Boric <jblbeurope@gmail.com>
2021-08-07 14:04:29 +0200

    Ports: Add awk symlink to mawk port

--- Ports/mawk/package.sh
+++ Ports/mawk/package.sh
@@ -7,3 +7,7 @@ useconfigure=true
 auth_type="sig"
 auth_import_key="C52048C0C0748FEE227D47A2702353E0F7E48EDB"
 auth_opts="mawk-${version}.tgz.asc mawk-${version}.tgz"
+
+post_install() {
+    ln -sf mawk "${SERENITY_INSTALL_ROOT}/usr/local/bin/awk"
+}
