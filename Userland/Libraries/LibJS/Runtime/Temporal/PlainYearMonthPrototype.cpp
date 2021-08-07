19393a71821edf36dd31d0712c3f9eb3bab5e532
Linus Groh <mail@linusgroh.de>
2021-08-07 22:48:29 +0100

    LibJS: Implement Temporal.PlainYearMonth.prototype.calendar

--- Userland/Libraries/LibJS/Runtime/Temporal/PlainYearMonthPrototype.cpp
+++ Userland/Libraries/LibJS/Runtime/Temporal/PlainYearMonthPrototype.cpp
@@ -4,7 +4,9 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <AK/TypeCasts.h>
 #include <LibJS/Runtime/GlobalObject.h>
+#include <LibJS/Runtime/Temporal/PlainYearMonth.h>
 #include <LibJS/Runtime/Temporal/PlainYearMonthPrototype.h>
 
 namespace JS::Temporal {
@@ -23,6 +25,34 @@ void PlainYearMonthPrototype::initialize(GlobalObject& global_object)
 
     // 9.3.2 Temporal.PlainYearMonth.prototype[ @@toStringTag ], https://tc39.es/proposal-temporal/#sec-temporal.plainyearmonth.prototype-@@tostringtag
     define_direct_property(*vm.well_known_symbol_to_string_tag(), js_string(vm.heap(), "Temporal.PlainYearMonth"), Attribute::Configurable);
+
+    define_native_accessor(vm.names.calendar, calendar_getter, {}, Attribute::Configurable);
+}
+
+static PlainYearMonth* typed_this(GlobalObject& global_object)
+{
+    auto& vm = global_object.vm();
+    auto* this_object = vm.this_value(global_object).to_object(global_object);
+    if (!this_object)
+        return {};
+    if (!is<PlainYearMonth>(this_object)) {
+        vm.throw_exception<TypeError>(global_object, ErrorType::NotA, "Temporal.PlainYearMonth");
+        return {};
+    }
+    return static_cast<PlainYearMonth*>(this_object);
+}
+
+// 9.3.3 get Temporal.PlainYearMonth.prototype.calendar, https://tc39.es/proposal-temporal/#sec-get-temporal.plainyearmonth.prototype.calendar
+JS_DEFINE_NATIVE_FUNCTION(PlainYearMonthPrototype::calendar_getter)
+{
+    // 1. Let plainYearMonth be the this value.
+    // 2. Perform ? RequireInternalSlot(plainYearMonth, [[InitializedTemporalYearMonth]]).
+    auto* plain_year_month = typed_this(global_object);
+    if (vm.exception())
+        return {};
+
+    // 3. Return plainYearMonth.[[Calendar]].
+    return Value(&plain_year_month->calendar());
 }
 
 }
