c0982c7351287146970a37977c28f349425af093
Linus Groh <mail@linusgroh.de>
2021-08-08 18:27:28 +0100

    LibJS: Add the Intl namespace object :^)
    This is the start of implementing ECMA-402 in LibJS, better known as the
    ECMAScript Internationalization API.
    
    Much like Temporal this gets its own subdirectory (Runtime/Intl/) as
    well as a new C++ namespace (JS::Intl) so we don't have to prefix all
    the files and classes with "Intl".
    
    https://tc39.es/ecma402/

---
+++ Userland/Libraries/LibJS/Runtime/Intl/Intl.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021, Linus Groh <linusg@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibJS/Runtime/GlobalObject.h>
+#include <LibJS/Runtime/Intl/Intl.h>
+
+namespace JS::Intl {
+
+// 8 The Intl Object, https://tc39.es/ecma402/#intl-object
+Intl::Intl(GlobalObject& global_object)
+    : Object(*global_object.object_prototype())
+{
+}
+
+void Intl::initialize(GlobalObject& global_object)
+{
+    Object::initialize(global_object);
+}
+
+}
