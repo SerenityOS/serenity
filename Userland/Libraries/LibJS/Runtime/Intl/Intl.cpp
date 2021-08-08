757d203118102beaaa1ecf92bb12b2d8ddde19c8
Linus Groh <mail@linusgroh.de>
2021-08-08 18:39:20 +0100

    LibJS: Implement Intl[@@toStringTag]

--- Userland/Libraries/LibJS/Runtime/Intl/Intl.cpp
+++ Userland/Libraries/LibJS/Runtime/Intl/Intl.cpp
@@ -18,6 +18,11 @@ Intl::Intl(GlobalObject& global_object)
 void Intl::initialize(GlobalObject& global_object)
 {
     Object::initialize(global_object);
+
+    auto& vm = this->vm();
+
+    // 8.1.1 Intl[ @@toStringTag ], https://tc39.es/ecma402/#sec-Intl-toStringTag
+    define_direct_property(*vm.well_known_symbol_to_string_tag(), js_string(vm, "Intl"), Attribute::Configurable);
 }
 
 }
