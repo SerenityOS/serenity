70b2b8a06d546411358262fb20123528f932cdc3
Ali Mohammad Pur <ali.mpfard@gmail.com>
2021-08-11 22:16:05 +0430

    LibWasm: Replace memory write macros with templated functions

--- Userland/Libraries/LibWasm/AbstractMachine/BytecodeInterpreter.h
+++ Userland/Libraries/LibWasm/AbstractMachine/BytecodeInterpreter.h
@@ -37,6 +37,8 @@ protected:
     void branch_to_label(Configuration&, LabelIndex);
     template<typename ReadT, typename PushT>
     void load_and_push(Configuration&, Instruction const&);
+    template<typename PopT, typename StoreT>
+    void pop_and_store(Configuration&, Instruction const&);
     void store_to_memory(Configuration&, Instruction const&, ReadonlyBytes data);
     void call_address(Configuration&, FunctionAddress);
 
