61c0ebb9d05d4001db08ccdb73a88170ce3145e5
Andreas Kling <kling@serenityos.org>
2021-08-12 21:40:05 +0200

    Profiler: Remove "Signpost " prefix from timeline tooltips

--- Userland/DevTools/Profiler/TimelineTrack.cpp
+++ Userland/DevTools/Profiler/TimelineTrack.cpp
@@ -135,7 +135,7 @@ void TimelineTrack::mousemove_event(GUI::MouseEvent& event)
         constexpr int hoverable_padding = 2;
         Gfx::IntRect hoverable_rect { x - hoverable_padding, frame_thickness(), hoverable_padding * 2, height() - frame_thickness() * 2 };
         if (hoverable_rect.contains_horizontally(event.x())) {
-            GUI::Application::the()->show_tooltip_immediately(String::formatted("Signpost {}, {}", signpost.signpost_string, signpost.arg2), this);
+            GUI::Application::the()->show_tooltip_immediately(String::formatted("{}, {}", signpost.signpost_string, signpost.arg2), this);
             hovering_a_signpost = true;
             return IterationDecision::Break;
         }
