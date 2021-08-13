95059b954915b18d997520c0b9f6ea0aaa053ef0
Linus Groh <mail@linusgroh.de>
2021-08-13 23:57:33 +0100

    LibJS/Tests: Enable Proxy handler apply() thisArg test that now passes

--- Userland/Libraries/LibJS/Tests/builtins/Proxy/Proxy.handler-apply.js
+++ Userland/Libraries/LibJS/Tests/builtins/Proxy/Proxy.handler-apply.js
@@ -14,7 +14,7 @@ describe("[[Call]] trap normal behavior", () => {
             apply(target, this_, arguments_) {
                 expect(target).toBe(f);
                 // FIXME: `this_` is currently `handler`
-                // expect(this_).toBeUndefined();
+                expect(this_).toBeUndefined();
                 if (arguments_[2]) {
                     return arguments_[0] * arguments_[1];
                 }
