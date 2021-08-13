0bb16d04d0656b2069bd2e44c7ab2a9cfbae4bc5
TheFightingCatfish <seekingblues@gmail.com>
2021-08-13 18:34:32 +0800

    Ports: Support libcurl in the git build
    Previously, libcurl is not used in the git build because the test
    program in configure fails to link against it properly. We bypass the
    test and make sure we link against all necessary libraries (libssl,
    libcrypto and zlib).

--- Ports/git/package.sh
+++ Ports/git/package.sh
@@ -4,11 +4,11 @@ version=2.31.1
 useconfigure="true"
 files="https://mirrors.edge.kernel.org/pub/software/scm/git/git-${version}.tar.xz git-${version}.tar.xz 9f61417a44d5b954a5012b6f34e526a3336dcf5dd720e2bb7ada92ad8b3d6680"
 auth_type=sha256
-configopts="--target=${SERENITY_ARCH}-pc-serenity CFLAGS=-DNO_IPV6"
+configopts="--target=${SERENITY_ARCH}-pc-serenity --with-lib=${SERENITY_INSTALL_ROOT}/usr/local CFLAGS=-DNO_IPV6 LDFLAGS=-L${SERENITY_INSTALL_ROOT}/usr/local/lib"
 depends="zlib curl"
 
 build() {
-    run make $makeopts
+    run make $makeopts CURL_LDFLAGS="-lcurl -lssl -lcrypto -lz"
     run make strip
 }
 
@@ -17,7 +17,11 @@ post_install() {
     run cp "../default_gitconfig" "${SERENITY_INSTALL_ROOT}/home/anon/.gitconfig"
 }
 
-export NO_OPENSSL=1
+export NO_PERL=YesPlease
+export NO_PYTHON=YesPlease
+export NO_EXPAT=YesPlease
+export NO_TCLTK=YesPlease
 export ac_cv_fread_reads_directories=no
 export ac_cv_snprintf_returns_bogus=no
 export ac_cv_iconv_omits_bom=no
+export ac_cv_lib_curl_curl_global_init=yes
