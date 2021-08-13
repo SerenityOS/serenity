455b94af7ffdde38798fb6dad79e8a6ff04e386b
Linus Groh <mail@linusgroh.de>
2021-08-13 23:52:55 +0100

    LibJS/Tests: Test iteration order of Map.prototype.values()

--- Userland/Libraries/LibJS/Tests/builtins/Map/Map.prototype.values.js
+++ Userland/Libraries/LibJS/Tests/builtins/Map/Map.prototype.values.js
@@ -10,16 +10,9 @@ test("basic functionality", () => {
     ];
     const a = new Map(original);
     const it = a.values();
-    // FIXME: This test should be rewritten once we have proper iteration order
-    const first = it.next();
-    expect(first.done).toBeFalse();
-    expect([0, 1, 2].includes(first.value)).toBeTrue();
-    const second = it.next();
-    expect(second.done).toBeFalse();
-    expect([0, 1, 2].includes(second.value)).toBeTrue();
-    const third = it.next();
-    expect(third.done).toBeFalse();
-    expect([0, 1, 2].includes(third.value)).toBeTrue();
+    expect(it.next()).toEqual({ value: 0, done: false });
+    expect(it.next()).toEqual({ value: 1, done: false });
+    expect(it.next()).toEqual({ value: 2, done: false });
     expect(it.next()).toEqual({ value: undefined, done: true });
     expect(it.next()).toEqual({ value: undefined, done: true });
     expect(it.next()).toEqual({ value: undefined, done: true });
