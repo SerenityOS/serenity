23f853ad74e4710e997c80c3b8e7a47f3449e6a3
Stephan Unverwerth <s.unverwerth@serenityos.org>
2021-08-13 13:16:00 +0200

    LibGL: Implement glColorPointer

--- Userland/Libraries/LibGL/GLContext.h
+++ Userland/Libraries/LibGL/GLContext.h
@@ -67,6 +67,7 @@ public:
     virtual void gl_enable_client_state(GLenum cap) = 0;
     virtual void gl_disable_client_state(GLenum cap) = 0;
     virtual void gl_vertex_pointer(GLint size, GLenum type, GLsizei stride, const void* pointer) = 0;
+    virtual void gl_color_pointer(GLint size, GLenum type, GLsizei stride, const void* pointer) = 0;
 
     virtual void present() = 0;
 };
