07b9cba6e6aaa5baed9ae841890f477758a059f8
Stephan Unverwerth <s.unverwerth@serenityos.org>
2021-08-13 13:03:06 +0200

    LibGL: Implement glVertexPointer

---
+++ Userland/Libraries/LibGL/GLVertexArrays.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, Stephan Unverwerth <s.unverwerth@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include "GL/gl.h"
+#include "GLContext.h"
+
+extern GL::GLContext* g_gl_context;
+
+void glVertexPointer(GLint size, GLenum type, GLsizei stride, const void* pointer)
+{
+    g_gl_context->gl_vertex_pointer(size, type, stride, pointer);
+}
