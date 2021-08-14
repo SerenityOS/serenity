4de3f1001fcc8023b052763c15dbcf8c62316afc
Liav A <liavalb@gmail.com>
2021-08-14 14:13:58 +0300

    Kernel/DevFS: Remove unnecessary include

--- Kernel/FileSystem/DevFS.cpp
+++ Kernel/FileSystem/DevFS.cpp
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#include <AK/Singleton.h>
 #include <AK/StringView.h>
 #include <Kernel/FileSystem/DevFS.h>
 #include <Kernel/FileSystem/VirtualFileSystem.h>
