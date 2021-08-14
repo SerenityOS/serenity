37e98a55b7150f85284cac8b549b907fdb0e8031
Andreas Kling <kling@serenityos.org>
2021-08-14 19:35:40 +0200

    Kernel+LibC: Share definitions for sys/ptrace.h

--- Userland/Libraries/LibC/sys/ptrace.h
+++ Userland/Libraries/LibC/sys/ptrace.h
@@ -6,24 +6,9 @@
 
 #pragma once
 
-#include <sys/types.h>
+#include <Kernel/API/POSIX/sys/ptrace.h>
 
 __BEGIN_DECLS
-
-#define PT_TRACE_ME 1
-#define PT_ATTACH 2
-#define PT_CONTINUE 3
-#define PT_SYSCALL 4
-#define PT_GETREGS 5
-#define PT_DETACH 6
-#define PT_PEEK 7
-#define PT_POKE 8
-#define PT_SETREGS 9
-#define PT_POKEDEBUG 10
-#define PT_PEEKDEBUG 11
-
-#define DEBUG_STATUS_REGISTER 6
-#define DEBUG_CONTROL_REGISTER 7
 
 // FIXME: PID/TID ISSUE
 // Affects the entirety of LibDebug and Userland/strace.cpp.
