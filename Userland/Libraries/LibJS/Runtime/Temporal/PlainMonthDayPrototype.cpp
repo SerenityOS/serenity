eaf08ea50859d251d34a1ce1d8391b49fbf586b2
Linus Groh <mail@linusgroh.de>
2021-08-14 23:56:38 +0100

    LibJS: Implement Temporal.PlainMonthDay.prototype[@@toStringTag]

--- Userland/Libraries/LibJS/Runtime/Temporal/PlainMonthDayPrototype.cpp
+++ Userland/Libraries/LibJS/Runtime/Temporal/PlainMonthDayPrototype.cpp
@@ -18,6 +18,11 @@ PlainMonthDayPrototype::PlainMonthDayPrototype(GlobalObject& global_object)
 void PlainMonthDayPrototype::initialize(GlobalObject& global_object)
 {
     Object::initialize(global_object);
+
+    auto& vm = this->vm();
+
+    // 10.3.2 Temporal.PlainMonthDay.prototype[ @@toStringTag ], https://tc39.es/proposal-temporal/#sec-temporal.plainmonthday.prototype-@@tostringtag
+    define_direct_property(*vm.well_known_symbol_to_string_tag(), js_string(vm, "Temporal.PlainMonthDay"), Attribute::Configurable);
 }
 
 }
