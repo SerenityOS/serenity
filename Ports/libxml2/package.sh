4cc6fc960ff7306ff1dbb124b388448f8cc8418f
Gunnar Beutner <gbeutner@serenityos.org>
2021-08-14 11:26:52 +0200

    Ports: Add missing dependency for libxml2
    When xz was previously built we'd end up with a shared library for
    libxml2 that depends on xz features but isn't linked against liblzma.

--- Ports/libxml2/package.sh
+++ Ports/libxml2/package.sh
@@ -4,7 +4,7 @@ useconfigure="true"
 version="2.9.12"
 files="ftp://xmlsoft.org/libxml2/libxml2-${version}.tar.gz libxml2-${version}.tar.gz c8d6681e38c56f172892c85ddc0852e1fd4b53b4209e7f4ebf17f7e2eae71d92"
 auth_type=sha256
-depends="libiconv"
+depends="libiconv xz"
 configopts="--prefix=${SERENITY_INSTALL_ROOT}/usr/local --without-python"
 
 install() {
@@ -12,5 +12,5 @@ install() {
     run make install
 
     # Link shared library
-    run ${SERENITY_ARCH}-pc-serenity-gcc -shared -o ${SERENITY_INSTALL_ROOT}/usr/local/lib/libxml2.so -Wl,-soname,libxml2.so -Wl,--whole-archive ${SERENITY_INSTALL_ROOT}/usr/local/lib/libxml2.a -Wl,--no-whole-archive
+    run ${SERENITY_ARCH}-pc-serenity-gcc -shared -o ${SERENITY_INSTALL_ROOT}/usr/local/lib/libxml2.so -Wl,-soname,libxml2.so -Wl,--whole-archive ${SERENITY_INSTALL_ROOT}/usr/local/lib/libxml2.a -Wl,--no-whole-archive -llzma
 }
