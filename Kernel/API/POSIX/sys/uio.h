037c10bca2490cab93aaa28176bf1a1d085f2357
Andreas Kling <kling@serenityos.org>
2021-08-14 19:38:47 +0200

    Kernel+LibC: Share definitions for sys/uio.h

---
+++ Kernel/API/POSIX/sys/uio.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2018-2021, Andreas Kling <kling@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <Kernel/API/POSIX/sys/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct iovec {
+    void* iov_base;
+    size_t iov_len;
+};
+
+#ifdef __cplusplus
+}
+#endif
