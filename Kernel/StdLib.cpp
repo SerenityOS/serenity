96d5d017b70e26605d2340f5a27707db4302b4df
Andreas Kling <kling@serenityos.org>
2021-08-14 23:02:48 +0200

    Kernel: Remove copy_string_from_user() as it's no longer used

--- Kernel/StdLib.cpp
+++ Kernel/StdLib.cpp
@@ -13,35 +13,6 @@
 #include <Kernel/Memory/MemoryManager.h>
 #include <Kernel/StdLib.h>
 
-String copy_string_from_user(const char* user_str, size_t user_str_size)
-{
-    bool is_user = Kernel::Memory::is_user_range(VirtualAddress(user_str), user_str_size);
-    if (!is_user)
-        return {};
-    Kernel::SmapDisabler disabler;
-    void* fault_at;
-    ssize_t length = Kernel::safe_strnlen(user_str, user_str_size, fault_at);
-    if (length < 0) {
-        dbgln("copy_string_from_user({:p}, {}) failed at {} (strnlen)", static_cast<const void*>(user_str), user_str_size, VirtualAddress { fault_at });
-        return {};
-    }
-    if (length == 0)
-        return String::empty();
-
-    char* buffer;
-    auto copied_string = StringImpl::create_uninitialized((size_t)length, buffer);
-    if (!Kernel::safe_memcpy(buffer, user_str, (size_t)length, fault_at)) {
-        dbgln("copy_string_from_user({:p}, {}) failed at {} (memcpy)", static_cast<const void*>(user_str), user_str_size, VirtualAddress { fault_at });
-        return {};
-    }
-    return copied_string;
-}
-
-String copy_string_from_user(Userspace<const char*> user_str, size_t user_str_size)
-{
-    return copy_string_from_user(user_str.unsafe_userspace_ptr(), user_str_size);
-}
-
 Kernel::KResultOr<NonnullOwnPtr<Kernel::KString>> try_copy_kstring_from_user(Userspace<const char*> user_str, size_t user_str_size)
 {
     bool is_user = Kernel::Memory::is_user_range(VirtualAddress(user_str), user_str_size);
