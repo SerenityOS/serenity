ab4e395201d9cb2d23e5abba175596ada58561de
Gunnar Beutner <gbeutner@serenityos.org>
2021-08-14 19:51:55 +0200

    Ports: Remove SHA256 checksum for the doom port

--- Ports/doom/package.sh
+++ Ports/doom/package.sh
@@ -2,8 +2,7 @@
 port=doom
 workdir=SerenityDOOM-master
 version=git
-files="https://github.com/SerenityOS/SerenityDOOM/archive/master.tar.gz doom-git.tar.gz 13aaa5b1655b85190b744905c9a8a69f1a1e26e7331c9363a241e83510f5b26b"
-auth_type=sha256
+files="https://github.com/SerenityOS/SerenityDOOM/archive/master.tar.gz doom-git.tar.gz"
 makeopts="-C doomgeneric/"
 installopts="-C doomgeneric/"
 launcher_name=Doom
