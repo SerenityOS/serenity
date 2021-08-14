1382be17072bb119173c95b70d8e424983d26dad
Linus Groh <mail@linusgroh.de>
2021-08-14 23:59:43 +0100

    LibJS: Implement Temporal.PlainMonthDay.prototype.calendar

--- Userland/Libraries/LibJS/Runtime/Temporal/PlainMonthDayPrototype.h
+++ Userland/Libraries/LibJS/Runtime/Temporal/PlainMonthDayPrototype.h
@@ -17,6 +17,9 @@ public:
     explicit PlainMonthDayPrototype(GlobalObject&);
     virtual void initialize(GlobalObject&) override;
     virtual ~PlainMonthDayPrototype() override = default;
+
+private:
+    JS_DECLARE_NATIVE_FUNCTION(calendar_getter);
 };
 
 }
