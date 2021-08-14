d33b43276c6319055bc212d68fab9ca5f47cfa87
Andreas Kling <kling@serenityos.org>
2021-08-14 18:57:35 +0200

    Kernel+LibC: Share definitions for poll.h

--- Userland/Libraries/LibC/poll.h
+++ Userland/Libraries/LibC/poll.h
@@ -1,31 +1,15 @@
 /*
- * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2018-2021, Andreas Kling <kling@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #pragma once
 
+#include <Kernel/API/POSIX/poll.h>
 #include <signal.h>
-#include <sys/cdefs.h>
 
 __BEGIN_DECLS
-
-#define POLLIN (1u << 0)
-#define POLLPRI (1u << 1)
-#define POLLOUT (1u << 2)
-#define POLLERR (1u << 3)
-#define POLLHUP (1u << 4)
-#define POLLNVAL (1u << 5)
-#define POLLRDHUP (1u << 13)
-
-struct pollfd {
-    int fd;
-    short events;
-    short revents;
-};
-
-typedef unsigned nfds_t;
 
 int poll(struct pollfd* fds, nfds_t nfds, int timeout);
 int ppoll(struct pollfd* fds, nfds_t nfds, const struct timespec* timeout, const sigset_t* sigmask);
