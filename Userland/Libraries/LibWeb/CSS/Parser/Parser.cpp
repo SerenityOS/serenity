ddcaa4825362b903f5d6d9347cb635ca369499de
Sam Atkins <atkinssj@gmail.com>
2021-08-16 15:41:33 +0100

    LibWeb: Fix check for too many 'normal' values in font declaration
    I had the values backwards, oops!

--- Userland/Libraries/LibWeb/CSS/Parser/Parser.cpp
+++ Userland/Libraries/LibWeb/CSS/Parser/Parser.cpp
@@ -2400,14 +2400,13 @@ RefPtr<StyleValue> Parser::parse_font_value(ParsingContext const& context, Vecto
             font_families = maybe_font_families.release_nonnull();
             break;
         }
-
         return nullptr;
     }
 
     // Since normal is the default value for all the properties that can have it, we don't have to actually
     // set anything to normal here. It'll be set when we create the FontStyleValue below.
     // We just need to make sure we were not given more normals than will fit.
-    int unset_value_count = (font_style ? 1 : 0) + (font_weight ? 1 : 0);
+    int unset_value_count = (font_style ? 0 : 1) + (font_weight ? 0 : 1);
     if (unset_value_count < normal_count)
         return nullptr;
 
