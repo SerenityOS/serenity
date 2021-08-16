1432bb877e51ef3a1f3deae80ada40104401154e
Timothy Flynn <trflynn89@pm.me>
2021-08-16 16:23:12 -0400

    Toolchain: Make Kernel/API headers available to Clang toolchain build
    Copied from 9b7986790900a3a81edd879ea31583670977496f.

--- Toolchain/BuildClang.sh
+++ Toolchain/BuildClang.sh
@@ -229,9 +229,9 @@ mkdir -p "$BUILD"
 pushd "$BUILD"
     mkdir -p Root/usr/include/
     SRC_ROOT=$($REALPATH "$DIR"/..)
-    FILES=$(find "$SRC_ROOT"/Userland/Libraries/LibC "$SRC_ROOT"/Userland/Libraries/LibM "$SRC_ROOT"/Userland/Libraries/LibPthread "$SRC_ROOT"/Userland/Libraries/LibDl -name '*.h' -print)
+    FILES=$(find "$SRC_ROOT"/Kernel/API "$SRC_ROOT"/Userland/Libraries/LibC "$SRC_ROOT"/Userland/Libraries/LibM "$SRC_ROOT"/Userland/Libraries/LibPthread "$SRC_ROOT"/Userland/Libraries/LibDl -name '*.h' -print)
     for header in $FILES; do
-        target=$(echo "$header" | sed -e "s@$SRC_ROOT/Userland/Libraries/LibC@@" -e "s@$SRC_ROOT/Userland/Libraries/LibM@@" -e "s@$SRC_ROOT/Userland/Libraries/LibPthread@@" -e "s@$SRC_ROOT/Userland/Libraries/LibDl@@")
+        target=$(echo "$header" | sed -e "s@$SRC_ROOT/Userland/Libraries/LibC@@" -e "s@$SRC_ROOT/Userland/Libraries/LibM@@" -e "s@$SRC_ROOT/Userland/Libraries/LibPthread@@" -e "s@$SRC_ROOT/Userland/Libraries/LibDl@@" -e "s@$SRC_ROOT/Kernel/@Kernel/@")
         buildstep "system_headers" install -D "$header" "Root/usr/include/$target"
     done
     unset SRC_ROOT
