0b673698301fe1b95d7a8aeb0121aedfebf154c8
Jesse Buhagiar <jooster669@gmail.com>
2021-08-17 18:38:34 +1000

    LibGL: Implenent `glGetBooleanv`

--- Userland/Libraries/LibGL/GLUtils.cpp
+++ Userland/Libraries/LibGL/GLUtils.cpp
@@ -95,6 +95,11 @@ void glGetFloatv(GLenum pname, GLfloat* params)
     g_gl_context->gl_get_floatv(pname, params);
 }
 
+void glGetBooleanv(GLenum pname, GLboolean* data)
+{
+    g_gl_context->gl_get_booleanv(pname, data);
+}
+
 void glDepthMask(GLboolean flag)
 {
     g_gl_context->gl_depth_mask(flag);
