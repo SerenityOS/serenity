fa67cdb8d3e72d6e20db55d8b8d10cebd2fd2a10
Linus Groh <mail@linusgroh.de>
2021-08-19 00:47:17 +0100

    LibJS: Implement Temporal.PlainYearMonth.prototype.toLocaleString()

--- Userland/Libraries/LibJS/Runtime/Temporal/PlainYearMonthPrototype.h
+++ Userland/Libraries/LibJS/Runtime/Temporal/PlainYearMonthPrototype.h
@@ -28,6 +28,7 @@ private:
     JS_DECLARE_NATIVE_FUNCTION(months_in_year_getter);
     JS_DECLARE_NATIVE_FUNCTION(in_leap_year_getter);
     JS_DECLARE_NATIVE_FUNCTION(to_string);
+    JS_DECLARE_NATIVE_FUNCTION(to_locale_string);
     JS_DECLARE_NATIVE_FUNCTION(value_of);
     JS_DECLARE_NATIVE_FUNCTION(get_iso_fields);
 };
