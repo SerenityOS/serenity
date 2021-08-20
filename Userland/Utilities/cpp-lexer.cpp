88198c515b7d2537568d299728f745a87294365b
Itamar <itamar8910@gmail.com>
2021-08-20 15:05:11 +0300

    Utilities: Add cpp-lexer

---
+++ Userland/Utilities/cpp-lexer.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2021, the SerenityOS developers.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibCore/ArgsParser.h>
+#include <LibCore/File.h>
+#include <LibCpp/Lexer.h>
+
+int main(int argc, char** argv)
+{
+    Core::ArgsParser args_parser;
+    const char* path = nullptr;
+    args_parser.add_positional_argument(path, "Cpp File", "cpp-file", Core::ArgsParser::Required::Yes);
+    args_parser.parse(argc, argv);
+
+    auto file = Core::File::construct(path);
+    if (!file->open(Core::OpenMode::ReadOnly)) {
+        perror("open");
+        exit(1);
+    }
+    auto content = file->read_all();
+    StringView content_view(content);
+
+    Cpp::Lexer lexer(content);
+    auto tokens = lexer.lex();
+
+    for (auto& token : tokens) {
+        outln("{}", token.to_string());
+    }
+}
