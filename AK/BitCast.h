266c936dbb99c3fa94aa7085a86454f7cbe6f4e2
Jesse Buhagiar <jooster669@gmail.com>
2021-08-21 15:08:06 +1000

    AK: Use `__builtin_bit_cast` if available
    We now use the compiler's buitin version of bitcast if it's available
    instead of just resorting to using the builtin `memcpy`.

--- AK/BitCast.h
+++ AK/BitCast.h
@@ -11,11 +11,15 @@ namespace AK {
 template<typename T, typename U>
 inline T bit_cast(const U& a)
 {
+#if (__has_builtin(__builtin_bit_cast))
+    return __builtin_bit_cast(T, a);
+#else
     static_assert(sizeof(T) == sizeof(U));
 
     T result;
     __builtin_memcpy(&result, &a, sizeof(T));
     return result;
+#endif
 }
 
 }
