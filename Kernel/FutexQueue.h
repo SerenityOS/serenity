b012170d69c5d6a846034591b3a064d12346f8e1
Andreas Kling <kling@serenityos.org>
2021-08-24 01:13:53 +0200

    Kernel: Don't register thread as custom data for FutexBlocker
    Same deal as WaitQueueBlocker, we can get the blocked thread from
    Blocker::thread() now, so there's no need to register the current
    thread as custom data.

--- Kernel/FutexQueue.h
+++ Kernel/FutexQueue.h
@@ -42,7 +42,7 @@ public:
     bool is_empty_and_no_imminent_waits_locked();
 
 protected:
-    virtual bool should_add_blocker(Thread::Blocker& b, void* data) override;
+    virtual bool should_add_blocker(Thread::Blocker& b, void*) override;
 
 private:
     size_t m_imminent_waits { 1 }; // We only create this object if we're going to be waiting, so start out with 1
