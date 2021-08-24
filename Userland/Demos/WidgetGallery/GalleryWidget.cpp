a12aaa7c14d5f405eace5b1a3e677568d6e4c6e9
Andreas Kling <kling@serenityos.org>
2021-08-24 16:19:08 +0200

    Userland: Remove IRC Client
    The IRC Client application made some sense while our main communication
    hub was an IRC channel. Now that we've moved on, IRC is just a random
    protocol with no particular relevance to this project.
    
    This also has the benefit of removing one major client of the single-
    process Web::InProcessWebView class.

--- Userland/Demos/WidgetGallery/GalleryWidget.cpp
+++ Userland/Demos/WidgetGallery/GalleryWidget.cpp
@@ -131,7 +131,7 @@ GalleryWidget::GalleryWidget()
     };
 
     m_msgbox_button = basics_tab.find_descendant_of_type_named<GUI::Button>("msgbox_button");
-    m_msgbox_button->set_icon(Gfx::Bitmap::try_load_from_file("/res/icons/16x16/app-irc-client.png"));
+    m_msgbox_button->set_icon(Gfx::Bitmap::try_load_from_file("/res/icons/16x16/app-browser.png"));
 
     m_msgbox_type = GUI::MessageBox::Type::None;
     m_msgbox_input_type = GUI::MessageBox::InputType::OK;
