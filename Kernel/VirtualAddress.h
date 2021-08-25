510379eb1d5cfa50b1f5679c540691389c618200
Brian Gianforcaro <bgianf@serenityos.org>
2021-08-24 20:39:33 -0700

    Kernel: Make VirtualAddress methods constexpr
    In order to use VirtualAddresses as compile time constants in the
    AddressSanitizer implementation, we need to be able to use these
    methods in constexpr functions / variable initializations.

--- Kernel/VirtualAddress.h
+++ Kernel/VirtualAddress.h
@@ -12,7 +12,7 @@
 class VirtualAddress {
 public:
     VirtualAddress() = default;
-    explicit VirtualAddress(FlatPtr address)
+    constexpr explicit VirtualAddress(FlatPtr address)
         : m_address(address)
     {
     }
@@ -22,11 +22,11 @@ public:
     {
     }
 
-    [[nodiscard]] bool is_null() const { return m_address == 0; }
-    [[nodiscard]] bool is_page_aligned() const { return (m_address & 0xfff) == 0; }
+    [[nodiscard]] constexpr bool is_null() const { return m_address == 0; }
+    [[nodiscard]] constexpr bool is_page_aligned() const { return (m_address & 0xfff) == 0; }
 
-    [[nodiscard]] VirtualAddress offset(FlatPtr o) const { return VirtualAddress(m_address + o); }
-    [[nodiscard]] FlatPtr get() const { return m_address; }
+    [[nodiscard]] constexpr VirtualAddress offset(FlatPtr o) const { return VirtualAddress(m_address + o); }
+    [[nodiscard]] constexpr FlatPtr get() const { return m_address; }
     void set(FlatPtr address) { m_address = address; }
     void mask(FlatPtr m) { m_address &= m; }
 
