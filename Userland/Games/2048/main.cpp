9b65156e7f17976d7ea7454ad7913f658dde01f2
Ralf Donau <ruelle@volleyballschlaeger.de>
2021-08-26 20:51:53 +0200

    Games: Remove unveiling /tmp/portal/config
    Config::pledge_domains is applied before, hence /tmp/portal/config
    can and should be veiled.

--- Userland/Games/2048/main.cpp
+++ Userland/Games/2048/main.cpp
@@ -49,11 +49,6 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    if (unveil("/tmp/portal/config", "rw") < 0) {
-        perror("unveil");
-        return 1;
-    }
-
     if (unveil(nullptr, nullptr) < 0) {
         perror("unveil");
         return 1;
