b8ad4d302e3ac46900f819ac22cb1c96552685fb
Timothy Flynn <trflynn89@pm.me>
2021-08-26 06:42:11 -0400

    LibUnicode: Move Locale enumeration from generated UCD data to CLDR data
    The UCD set of data contained a very small subset of all locales just to
    handle some special casing rules. This enumeration will be needed within
    the CLDR generator as well. So rather than duplicate the enum, remove it
    from the UCD generator in favor of the full list of locales known by the
    CLDR generator.

--- Userland/Libraries/LibUnicode/Forward.h
+++ Userland/Libraries/LibUnicode/Forward.h
@@ -13,7 +13,7 @@ namespace Unicode {
 enum class Condition : u8;
 enum class GeneralCategory : u8;
 enum class Language : u8;
-enum class Locale : u8;
+enum class Locale : u16;
 enum class Property : u8;
 enum class Script : u8;
 enum class Territory : u8;
