16d0e7c4464e8d273e9109f69cf7e8a38ba6727c
Linus Groh <mail@linusgroh.de>
2021-08-27 20:30:03 +0100

    LibJS: Implement Temporal.ZonedDateTime.prototype.era

--- Userland/Libraries/LibJS/Runtime/Temporal/ZonedDateTimePrototype.h
+++ Userland/Libraries/LibJS/Runtime/Temporal/ZonedDateTimePrototype.h
@@ -45,6 +45,7 @@ private:
     JS_DECLARE_NATIVE_FUNCTION(in_leap_year_getter);
     JS_DECLARE_NATIVE_FUNCTION(offset_nanoseconds_getter);
     JS_DECLARE_NATIVE_FUNCTION(offset_getter);
+    JS_DECLARE_NATIVE_FUNCTION(era_getter);
     JS_DECLARE_NATIVE_FUNCTION(value_of);
     JS_DECLARE_NATIVE_FUNCTION(to_instant);
     JS_DECLARE_NATIVE_FUNCTION(to_plain_date);
