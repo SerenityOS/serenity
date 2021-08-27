b5d532dad71a85fba299bb770f3820bac98c13a2
Idan Horowitz <idan.horowitz@gmail.com>
2021-08-27 16:50:01 +0300

    LibJS: Implement Temporal.PlainDateTime.prototype.equals()

--- Userland/Libraries/LibJS/Runtime/Temporal/PlainDateTimePrototype.h
+++ Userland/Libraries/LibJS/Runtime/Temporal/PlainDateTimePrototype.h
@@ -40,6 +40,7 @@ private:
     JS_DECLARE_NATIVE_FUNCTION(in_leap_year_getter);
     JS_DECLARE_NATIVE_FUNCTION(with_plain_date);
     JS_DECLARE_NATIVE_FUNCTION(with_calendar);
+    JS_DECLARE_NATIVE_FUNCTION(equals);
     JS_DECLARE_NATIVE_FUNCTION(value_of);
     JS_DECLARE_NATIVE_FUNCTION(to_plain_date);
     JS_DECLARE_NATIVE_FUNCTION(to_plain_year_month);
