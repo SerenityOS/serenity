a62ad4d82a9430dfb62d71e84be2cb4826e27338
Luke Wilde <lukew@serenityos.org>
2021-08-27 14:49:33 +0100

    Pong: Remove wpath and cpath pledges
    With the move to LibConfig, these are no longer needed.

--- Userland/Games/Pong/main.cpp
+++ Userland/Games/Pong/main.cpp
@@ -15,14 +15,14 @@
 
 int main(int argc, char** argv)
 {
-    if (pledge("stdio rpath wpath cpath recvfd sendfd unix", nullptr) < 0) {
+    if (pledge("stdio rpath recvfd sendfd unix", nullptr) < 0) {
         perror("pledge");
         return 1;
     }
 
     auto app = GUI::Application::construct(argc, argv);
 
-    if (pledge("stdio rpath wpath cpath recvfd sendfd", nullptr) < 0) {
+    if (pledge("stdio rpath recvfd sendfd", nullptr) < 0) {
         perror("pledge");
         return 1;
     }
