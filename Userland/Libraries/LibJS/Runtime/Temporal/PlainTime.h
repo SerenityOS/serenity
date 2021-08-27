28fa4d15689a158a6b2915f43546ba56c5cac714
Idan Horowitz <idan.horowitz@gmail.com>
2021-08-27 16:33:15 +0300

    LibJS: Add the CompareTemporalTime & CompareISODateTime AOs
    These are required for implementing comparisons between `PlainTime`s and
    `PlainDateTime`s.

--- Userland/Libraries/LibJS/Runtime/Temporal/PlainTime.h
+++ Userland/Libraries/LibJS/Runtime/Temporal/PlainTime.h
@@ -87,5 +87,6 @@ DaysAndTime balance_time(i64 hour, i64 minute, i64 second, i64 millisecond, i64 
 TemporalTime constrain_time(double hour, double minute, double second, double millisecond, double microsecond, double nanosecond);
 PlainTime* create_temporal_time(GlobalObject&, u8 hour, u8 minute, u8 second, u16 millisecond, u16 microsecond, u16 nanosecond, FunctionObject* new_target = nullptr);
 Optional<TemporalTime> to_temporal_time_record(GlobalObject&, Object& temporal_time_like);
+i8 compare_temporal_time(u8 hour1, u8 minute1, u8 second1, u16 millisecond1, u16 microsecond1, u16 nanosecond1, u8 hour2, u8 minute2, u8 second2, u16 millisecond2, u16 microsecond2, u16 nanosecond2);
 
 }
