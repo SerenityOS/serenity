7779710bdbe9037a9bb204c27084a0ae7606841f
Peter Elliott <pelliott@ualberta.ca>
2021-08-29 13:19:12 -0700

    Tests: Test LibMarkdown against commonmark test suite
    TestCommonmark runs the CommonMark test suite
    (https://spec.commonmark.org/0.30/spec.json) against LibMarkdown.
    Currently 44/652 tests pass.

---
+++ Meta/CMake/commonmark_spec.cmake
@@ -0,0 +1,9 @@
+option(ENABLE_COMMONMARK_SPEC_DOWNLOAD "Enable download of commonmark test suite at build time" ON)
+
+set(MARKDOWN_TEST_PATH ${CMAKE_BINARY_DIR}/commonmark.spec.json)
+set(MARKDOWN_TEST_URL https://spec.commonmark.org/0.30/spec.json)
+
+if(ENABLE_COMMONMARK_SPEC_DOWNLOAD)
+    file(DOWNLOAD ${MARKDOWN_TEST_URL} ${MARKDOWN_TEST_PATH})
+    install(FILES ${MARKDOWN_TEST_PATH} DESTINATION home/anon)
+endif()
