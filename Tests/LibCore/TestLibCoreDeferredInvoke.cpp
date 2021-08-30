fedd86c450748ff0cb89a8b115dd05bc4308407c
sin-ack <sin-ack@users.noreply.github.com>
2021-08-30 13:31:09 +0000

    Tests: Add tests for Core::deferred_invoke

---
+++ Tests/LibCore/TestLibCoreDeferredInvoke.cpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2021, sin-ack <sin-ack@protonmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <AK/Format.h>
+#include <LibCore/EventLoop.h>
+#include <LibCore/Timer.h>
+#include <LibTest/TestCase.h>
+
+TEST_CASE(deferred_invoke)
+{
+    Core::EventLoop event_loop;
+    auto reaper = Core::Timer::create_single_shot(250, [] {
+        warnln("I waited for the deferred_invoke to happen, but it never did!");
+        VERIFY_NOT_REACHED();
+    });
+
+    Core::deferred_invoke([&event_loop] {
+        event_loop.quit(0);
+    });
+
+    event_loop.exec();
+}
