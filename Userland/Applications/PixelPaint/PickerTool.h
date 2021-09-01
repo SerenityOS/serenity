973bbf42866f2c8d55b17f3cc01b5467c05b495a
Mustafa Quraish <mustafaq9@gmail.com>
2021-08-31 22:21:39 -0400

    PixelPaint: Use Zoom and Eyedropper cursors
    Use the newly added cursors for ZoomTool and PickerTool

--- Userland/Applications/PixelPaint/PickerTool.h
+++ Userland/Applications/PixelPaint/PickerTool.h
@@ -16,7 +16,7 @@ public:
     virtual ~PickerTool() override;
 
     virtual void on_mousedown(Layer*, MouseEvent&) override;
-    virtual Gfx::StandardCursor cursor() override { return Gfx::StandardCursor::Crosshair; }
+    virtual Gfx::StandardCursor cursor() override { return Gfx::StandardCursor::Eyedropper; }
 };
 
 }
