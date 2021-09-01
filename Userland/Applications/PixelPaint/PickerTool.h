b8e07baaed7498b2b85f3cb606b34d722c244414
Mustafa Quraish <mustafaq9@gmail.com>
2021-09-01 00:12:15 -0400

    PixelPaint: Let PickerTool optionally sample all layers
    Previously, only the color from the selected layer would be picked.
    Now, we allow the user to select if they want to sample the color
    from all layers.

--- Userland/Applications/PixelPaint/PickerTool.h
+++ Userland/Applications/PixelPaint/PickerTool.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2021, Mustafa Quraish <mustafa@cs.toronto.edu>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -16,7 +17,13 @@ public:
     virtual ~PickerTool() override;
 
     virtual void on_mousedown(Layer*, MouseEvent&) override;
+
+    virtual GUI::Widget* get_properties_widget() override;
     virtual Gfx::StandardCursor cursor() override { return Gfx::StandardCursor::Eyedropper; }
+
+private:
+    RefPtr<GUI::Widget> m_properties_widget;
+    bool m_sample_all_layers { false };
 };
 
 }
