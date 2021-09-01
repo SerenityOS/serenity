957e3a843eca24afc2946f0216da3929af0b36d4
Peter Elliott <pelliott@ualberta.ca>
2021-08-31 19:32:46 -0700

    Everywhere: Use my cool new @serenityos.org email address

--- Kernel/Random.h
+++ Kernel/Random.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
- * Copyright (c) 2020, Peter Elliott <pelliott@ualberta.ca>
+ * Copyright (c) 2020, Peter Elliott <pelliott@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
