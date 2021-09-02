990dd037d2ba029332619641ed43d4896b4a679d
Timothy Flynn <trflynn89@pm.me>
2021-09-01 21:56:55 -0400

    LibJS: Implement Intl.Locale.prototype.toString()
    This isn't particularly testable yet without the Intl.Locale constructor
    but having this defined will make testing the constructor possible. So
    more specific tests for this prototype will come later.

--- Userland/Libraries/LibJS/Runtime/Intl/LocalePrototype.h
+++ Userland/Libraries/LibJS/Runtime/Intl/LocalePrototype.h
@@ -17,6 +17,9 @@ public:
     explicit LocalePrototype(GlobalObject&);
     virtual void initialize(GlobalObject&) override;
     virtual ~LocalePrototype() override = default;
+
+private:
+    JS_DECLARE_NATIVE_FUNCTION(to_string);
 };
 
 }
