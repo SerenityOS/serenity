0a34524d8b4f87d3412811da1af71eeecadd8208
Sam Atkins <atkinssj@gmail.com>
2021-09-03 10:22:47 +0100

    WebContent: Implement `$0` special variable in Browser JS Console
    `$0` is a helpful variable in other browsers' JS consoles, which points
    to whichever DOM Node is currently selected in the DOM Inspector. And
    now we have it too! :^)

--- Userland/Services/WebContent/ConsoleGlobalObject.cpp
+++ Userland/Services/WebContent/ConsoleGlobalObject.cpp
@@ -5,6 +5,8 @@
  */
 
 #include "ConsoleGlobalObject.h"
+#include <LibWeb/Bindings/NodeWrapper.h>
+#include <LibWeb/Bindings/NodeWrapperFactory.h>
 #include <LibWeb/Bindings/WindowObject.h>
 #include <LibWeb/DOM/Document.h>
 #include <LibWeb/DOM/Window.h>
@@ -18,6 +20,14 @@ ConsoleGlobalObject::ConsoleGlobalObject(Web::Bindings::WindowObject& parent_obj
 
 ConsoleGlobalObject::~ConsoleGlobalObject()
 {
+}
+
+void ConsoleGlobalObject::initialize_global_object()
+{
+    Base::initialize_global_object();
+
+    // $0 magic variable
+    define_native_accessor("$0", inspected_node_getter, nullptr, 0);
 }
 
 void ConsoleGlobalObject::visit_edges(Visitor& visitor)
@@ -85,6 +95,26 @@ bool ConsoleGlobalObject::internal_delete(JS::PropertyName const& property_name)
 JS::MarkedValueList ConsoleGlobalObject::internal_own_property_keys() const
 {
     return m_window_object->internal_own_property_keys();
+}
+
+JS_DEFINE_NATIVE_GETTER(ConsoleGlobalObject::inspected_node_getter)
+{
+    auto* this_object = vm.this_value(global_object).to_object(global_object);
+    if (!this_object)
+        return JS::js_null();
+
+    if (!is<ConsoleGlobalObject>(this_object)) {
+        vm.throw_exception<JS::TypeError>(global_object, JS::ErrorType::NotA, "ConsoleGlobalObject");
+        return {};
+    }
+
+    auto console_global_object = static_cast<ConsoleGlobalObject*>(this_object);
+    auto& window = console_global_object->m_window_object->impl();
+    auto* inspected_node = window.document().inspected_node();
+    if (!inspected_node)
+        return JS::js_undefined();
+
+    return Web::Bindings::wrap(global_object, *inspected_node);
 }
 
 }
