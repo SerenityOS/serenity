5cfb27fcc79654ad14ba5b92b29d1681e95f94b0
Sam Atkins <atkinssj@serenityos.org>
2021-09-04 12:12:12 +0100

    Browser: Convert JS ConsoleWidget to new API
    The console widget now requests messages and receives them in bulk,
    using the shiny new IPC calls. This lets it display console messages
    that occurred before the widget was created. :^)

--- Userland/Applications/Browser/ConsoleWidget.h
+++ Userland/Applications/Browser/ConsoleWidget.h
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2020, Hunter Salyer <thefalsehonesty@gmail.com>
  * Copyright (c) 2021, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2021, Sam Atkins <atkinssj@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -18,18 +19,27 @@ class ConsoleWidget final : public GUI::Widget {
 public:
     virtual ~ConsoleWidget();
 
-    void handle_js_console_output(const String& method, const String& line);
+    void notify_about_new_console_message(i32 message_index);
+    void handle_console_messages(i32 start_index, Vector<String> const& message_types, Vector<String> const& messages);
     void print_source_line(const StringView&);
     void print_html(const StringView&);
-    void clear_output();
+    void reset();
 
     Function<void(const String&)> on_js_input;
+    Function<void(i32)> on_request_messages;
 
 private:
     ConsoleWidget();
 
+    void request_console_messages();
+    void clear_output();
+
     RefPtr<GUI::TextBox> m_input;
     RefPtr<Web::OutOfProcessWebView> m_output_view;
+
+    i32 m_highest_notified_message_index { -1 };
+    i32 m_highest_received_message_index { -1 };
+    bool m_waiting_for_messages { false };
 };
 
 }
