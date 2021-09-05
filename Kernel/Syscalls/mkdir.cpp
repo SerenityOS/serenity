2658ab4a8093170f7cc3af2a151e9e380eea3958
Andreas Kling <kling@serenityos.org>
2021-09-05 16:18:00 +0200

    Kernel: Use TRY() in sys$mkdir()

--- Kernel/Syscalls/mkdir.cpp
+++ Kernel/Syscalls/mkdir.cpp
@@ -14,9 +14,7 @@ KResultOr<FlatPtr> Process::sys$mkdir(Userspace<const char*> user_path, size_t p
 {
     VERIFY_PROCESS_BIG_LOCK_ACQUIRED(this)
     REQUIRE_PROMISE(cpath);
-    auto path = get_syscall_path_argument(user_path, path_length);
-    if (path.is_error())
-        return path.error();
-    return VirtualFileSystem::the().mkdir(path.value()->view(), mode & ~umask(), current_directory());
+    auto path = TRY(get_syscall_path_argument(user_path, path_length));
+    return VirtualFileSystem::the().mkdir(path->view(), mode & ~umask(), current_directory());
 }
 }
