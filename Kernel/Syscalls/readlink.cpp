4e4b7c272c88b32f127bbb0970e659ac5d04754f
Andreas Kling <kling@serenityos.org>
2021-09-05 17:55:43 +0200

    Kernel: Use TRY() in sys$readlink()

--- Kernel/Syscalls/readlink.cpp
+++ Kernel/Syscalls/readlink.cpp
@@ -16,27 +16,17 @@ KResultOr<FlatPtr> Process::sys$readlink(Userspace<const Syscall::SC_readlink_pa
     REQUIRE_PROMISE(rpath);
     auto params = TRY(copy_typed_from_user(user_params));
 
-    auto path = get_syscall_path_argument(params.path);
-    if (path.is_error())
-        return path.error();
-
-    auto result = VirtualFileSystem::the().open(path.value()->view(), O_RDONLY | O_NOFOLLOW_NOERROR, 0, current_directory());
-    if (result.is_error())
-        return result.error();
-    auto description = result.value();
+    auto path = TRY(get_syscall_path_argument(params.path));
+    auto description = TRY(VirtualFileSystem::the().open(path->view(), O_RDONLY | O_NOFOLLOW_NOERROR, 0, current_directory()));
 
     if (!description->metadata().is_symlink())
         return EINVAL;
 
-    auto contents = description->read_entire_file();
-    if (contents.is_error())
-        return contents.error();
-
-    auto& link_target = *contents.value();
-    auto size_to_copy = min(link_target.size(), params.buffer.size);
-    TRY(copy_to_user(params.buffer.data, link_target.data(), size_to_copy));
+    auto link_target = TRY(description->read_entire_file());
+    auto size_to_copy = min(link_target->size(), params.buffer.size);
+    TRY(copy_to_user(params.buffer.data, link_target->data(), size_to_copy));
     // Note: we return the whole size here, not the copied size.
-    return link_target.size();
+    return link_target->size();
 }
 
 }
