38afb5955ed380df1cd1d082b330ac8dc6952a02
Andreas Kling <kling@serenityos.org>
2021-09-05 18:22:18 +0200

    Kernel: Use TRY() in sys$sethostname()

--- Kernel/Syscalls/hostname.cpp
+++ Kernel/Syscalls/hostname.cpp
@@ -30,12 +30,10 @@ KResultOr<FlatPtr> Process::sys$sethostname(Userspace<const char*> buffer, size_
         return EPERM;
     if (length > 64)
         return ENAMETOOLONG;
+    auto new_name = TRY(try_copy_kstring_from_user(buffer, length));
     return hostname().with_exclusive([&](auto& name) -> KResultOr<FlatPtr> {
-        auto name_or_error = try_copy_kstring_from_user(buffer, length);
-        if (name_or_error.is_error())
-            return name_or_error.error();
         // FIXME: Use KString instead of String here.
-        name = name_or_error.value()->view();
+        name = new_name->view();
         return 0;
     });
 }
