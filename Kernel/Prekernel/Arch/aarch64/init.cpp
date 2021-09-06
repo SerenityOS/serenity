b8f8c7443d2b05796cb0987183d041a42cc0fbcf
Nico Weber <thakis@chromium.org>
2021-09-06 13:11:16 -0400

    Kernel: Let aarch64 port call into C++
    Put all but the first core into a loop, make room for some stack,
    and call init().

---
+++ Kernel/Prekernel/Arch/aarch64/init.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021, Nico Weber <thakis@chromium.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+extern "C" [[noreturn]] void init();
+extern "C" [[noreturn]] void init()
+{
+    for (;;) { }
+}
