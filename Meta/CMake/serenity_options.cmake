904a268872416dc9b9f26c83fd0b1bd8b715c511
Andrew Kaster <akaster@serenityos.org>
2021-09-07 02:08:54 -0600

    Meta: Move all options to targetname_options.cmake files
    This common strategy of having a serenity_option() macro defined in
    either the Lagom or top level CMakeLists.txt allows us to do two things:
    
    First, we can more clearly see which options are Serenity-specific,
    Lagom-specific, or common between the target and host builds.
    
    Second, it enables the upcoming SuperBuild changes to set() the options
    in the SuperBuild's CMake cache and forward each target's options to the
    corresponding ExternalProject.

---
+++ Meta/CMake/serenity_options.cmake
@@ -0,0 +1,12 @@
+#
+# Options specific to the Serenity (target) build
+#
+
+include(${CMAKE_CURRENT_LIST_DIR}/common_options.cmake)
+
+serenity_option(ENABLE_PCI_IDS_DOWNLOAD ON CACHE BOOL "Enable download of the pci.ids database at build time")
+serenity_option(ENABLE_USB_IDS_DOWNLOAD ON CACHE BOOL "Enable download of the usb.ids database at build time")
+serenity_option(ENABLE_KERNEL_ADDRESS_SANITIZER OFF CACHE BOOL "Enable kernel address sanitizer testing in gcc/clang")
+serenity_option(ENABLE_KERNEL_COVERAGE_COLLECTION  OFF CACHE BOOL "Enable KCOV and kernel coverage instrumentation in gcc/clang")
+serenity_option(ENABLE_KERNEL_LTO OFF CACHE BOOL "Build the kernel with link-time optimization")
+serenity_option(ENABLE_EXTRA_KERNEL_DEBUG_SYMBOLS  OFF CACHE BOOL "Enable -Og and -ggdb3 options for Kernel code for easier debugging")
