4f2bcebe749a57f1367a9bd7d58b249a48981e23
Timothy Flynn <trflynn89@pm.me>
2021-09-08 15:25:35 -0400

    LibUnicode+LibJS: Store locale keyword values as a single string
    Previously, LibUnicode would store the values of a keyword as a Vector.
    For example, the locale "en-u-ca-abc-def" would have its keyword "ca"
    stored as {"abc, "def"}. Then, canonicalization would occur on each of
    the elements in that Vector.
    
    This is incorrect because, for example, the keyword value "true" should
    only be dropped if that is the entire value. That is, the canonical form
    of "en-u-kb-true" is "en-u-kb", but "en-u-kb-abc-true" does not change
    for canonicalization. However, we would canonicalize that locale as
    "en-u-kb-abc".

--- Userland/Libraries/LibJS/Runtime/Intl/Locale.cpp
+++ Userland/Libraries/LibJS/Runtime/Intl/Locale.cpp
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#include <AK/StringBuilder.h>
 #include <LibJS/Runtime/GlobalObject.h>
 #include <LibJS/Runtime/Intl/Locale.h>
 #include <LibUnicode/Locale.h>
@@ -27,29 +26,23 @@ Locale::Locale(Unicode::LocaleID const& locale_id, Object& prototype)
 {
     set_locale(locale_id.to_string());
 
-    auto join_keyword_types = [](auto const& types) {
-        StringBuilder builder;
-        builder.join('-', types);
-        return builder.build();
-    };
-
     for (auto const& extension : locale_id.extensions) {
         if (!extension.has<Unicode::LocaleExtension>())
             continue;
 
         for (auto const& keyword : extension.get<Unicode::LocaleExtension>().keywords) {
             if (keyword.key == "ca"sv) {
-                set_calendar(join_keyword_types(keyword.types));
+                set_calendar(keyword.value);
             } else if (keyword.key == "co"sv) {
-                set_collation(join_keyword_types(keyword.types));
+                set_collation(keyword.value);
             } else if (keyword.key == "hc"sv) {
-                set_hour_cycle(join_keyword_types(keyword.types));
+                set_hour_cycle(keyword.value);
             } else if (keyword.key == "kf"sv) {
-                set_case_first(join_keyword_types(keyword.types));
+                set_case_first(keyword.value);
             } else if (keyword.key == "kn"sv) {
-                set_numeric(keyword.types.is_empty());
+                set_numeric(keyword.value.is_empty());
             } else if (keyword.key == "nu"sv) {
-                set_numbering_system(join_keyword_types(keyword.types));
+                set_numbering_system(keyword.value);
             }
         }
 
