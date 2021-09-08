5d2a3449ebd8a1d4716ad6d523f97f4f76615fb3
Linus Groh <mail@linusgroh.de>
2021-09-08 23:02:53 +0100

    LibJS: Implement Temporal.PlainTime.prototype.toLocaleString()

--- Userland/Libraries/LibJS/Runtime/Temporal/PlainTimePrototype.h
+++ Userland/Libraries/LibJS/Runtime/Temporal/PlainTimePrototype.h
@@ -31,6 +31,7 @@ private:
     JS_DECLARE_NATIVE_FUNCTION(to_plain_date_time);
     JS_DECLARE_NATIVE_FUNCTION(get_iso_fields);
     JS_DECLARE_NATIVE_FUNCTION(to_string);
+    JS_DECLARE_NATIVE_FUNCTION(to_locale_string);
     JS_DECLARE_NATIVE_FUNCTION(value_of);
 };
 
