478c0c2444b9e5ce8a3ea77adec92a9f108eee1b
Andreas Kling <kling@serenityos.org>
2021-09-08 23:50:32 +0200

    LibWeb: Stub out HTML::EventLoop::spin_until() with spec FIXME's

--- Userland/Libraries/LibWeb/HTML/EventLoop/EventLoop.cpp
+++ Userland/Libraries/LibWeb/HTML/EventLoop/EventLoop.cpp
@@ -23,4 +23,30 @@ EventLoop& main_thread_event_loop()
     return static_cast<Bindings::WebEngineCustomData*>(Bindings::main_thread_vm().custom_data())->event_loop;
 }
 
+// https://html.spec.whatwg.org/multipage/webappapis.html#spin-the-event-loop
+void EventLoop::spin_until([[maybe_unused]] Function<bool()> goal_condition)
+{
+    // FIXME: 1. Let task be the event loop's currently running task.
+
+    // FIXME: 2. Let task source be task's source.
+
+    // FIXME: 3. Let old stack be a copy of the JavaScript execution context stack.
+
+    // FIXME: 4. Empty the JavaScript execution context stack.
+
+    // FIXME: 5. Perform a microtask checkpoint.
+
+    // FIXME: 6. In parallel:
+
+    // FIXME:    1. Wait until the condition goal is met.
+
+    // FIXME:    2. Queue a task on task source to:
+
+    // FIXME:       1. Replace the JavaScript execution context stack with old stack.
+
+    // FIXME:       2. Perform any steps that appear after this spin the event loop instance in the original algorithm.
+
+    // FIXME: 7. Stop task, allowing whatever algorithm that invoked it to resume.
+}
+
 }
