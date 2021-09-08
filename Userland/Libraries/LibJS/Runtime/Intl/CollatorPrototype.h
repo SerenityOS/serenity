4a99170cd2b0778b3445ade45f268d25f4822226
Timothy Flynn <trflynn89@pm.me>
2021-09-08 14:55:39 -0400

    LibJS: Implement Intl.Collator.prototype.resolvedOptions

--- Userland/Libraries/LibJS/Runtime/Intl/CollatorPrototype.h
+++ Userland/Libraries/LibJS/Runtime/Intl/CollatorPrototype.h
@@ -18,6 +18,9 @@ public:
     explicit CollatorPrototype(GlobalObject&);
     virtual void initialize(GlobalObject&) override;
     virtual ~CollatorPrototype() override = default;
+
+private:
+    JS_DECLARE_NATIVE_FUNCTION(resolved_options);
 };
 
 }
