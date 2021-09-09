efec020cf735663cb81952288ee0f303b68018c6
Ken Herner <kherner@navistone.com>
2021-09-09 17:17:26 -0400

    Ports: Update libarchive to v3.5.2
    Updated GPG fingerprint to Martin Matuska's latest public key
    Fixes #9921

--- Ports/libarchive/package.sh
+++ Ports/libarchive/package.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=libarchive
-version=3.5.1
+version=3.5.2
 useconfigure=true
 configopts="--without-xml2"
 files="https://libarchive.org/downloads/libarchive-${version}.tar.gz libarchive-${version}.tar.gz
 https://libarchive.org/downloads/libarchive-${version}.tar.gz.asc libarchive-${version}.tar.gz.asc"
 depends="zlib pcre"
 auth_type="sig"
-auth_import_key="CB55788360B992FA0885C878F040F7196BA99AF4"
+auth_import_key="A5A45B12AD92D964B89EEE2DEC560C81CEC2276E"
 auth_opts="libarchive-${version}.tar.gz.asc libarchive-${version}.tar.gz"
 
 export ac_cv_header_regex_h=no
