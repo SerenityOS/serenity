edf33f996da706e48b893524832d09b12fc9adf9
Timothy Flynn <trflynn89@pm.me>
2021-09-11 15:54:18 -0400

    LibJS: Convert WeakRef.prototype to be a PrototypeObject

--- Userland/Libraries/LibJS/Runtime/WeakRefPrototype.cpp
+++ Userland/Libraries/LibJS/Runtime/WeakRefPrototype.cpp
@@ -10,7 +10,7 @@
 namespace JS {
 
 WeakRefPrototype::WeakRefPrototype(GlobalObject& global_object)
-    : Object(*global_object.object_prototype())
+    : PrototypeObject(*global_object.object_prototype())
 {
 }
 
@@ -31,16 +31,12 @@ WeakRefPrototype::~WeakRefPrototype()
 // 26.1.3.2 WeakRef.prototype.deref ( ), https://tc39.es/ecma262/#sec-weak-ref.prototype.deref
 JS_DEFINE_NATIVE_FUNCTION(WeakRefPrototype::deref)
 {
-    auto* this_object = vm.this_value(global_object).to_object(global_object);
-    if (!this_object)
+    auto* weak_ref = typed_this_object(global_object);
+    if (vm.exception())
         return {};
-    if (!is<WeakRef>(this_object)) {
-        vm.throw_exception<TypeError>(global_object, ErrorType::NotAnObjectOfType, "WeakRef");
-        return {};
-    }
-    auto& weak_ref = static_cast<WeakRef&>(*this_object);
-    weak_ref.update_execution_generation();
-    return weak_ref.value() ?: js_undefined();
+
+    weak_ref->update_execution_generation();
+    return weak_ref->value() ?: js_undefined();
 }
 
 }
