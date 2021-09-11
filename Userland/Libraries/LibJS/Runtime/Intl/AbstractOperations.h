c35528f3a75ac2521f8fa1f021315ed671546c57
Timothy Flynn <trflynn89@pm.me>
2021-09-11 07:58:33 -0400

    LibJS: Sort Intl AbstractOperation declarations by spec ID
    The definitions in the .cpp file are already sorted this way.

--- Userland/Libraries/LibJS/Runtime/Intl/AbstractOperations.h
+++ Userland/Libraries/LibJS/Runtime/Intl/AbstractOperations.h
@@ -35,20 +35,20 @@ struct PatternPartition {
 };
 
 Optional<Unicode::LocaleID> is_structurally_valid_language_tag(StringView locale);
+String canonicalize_unicode_locale_id(Unicode::LocaleID& locale);
 bool is_well_formed_currency_code(StringView currency);
 bool is_well_formed_unit_identifier(StringView unit_identifier);
-String canonicalize_unicode_locale_id(Unicode::LocaleID& locale);
 Vector<String> canonicalize_locale_list(GlobalObject&, Value locales);
 Optional<String> best_available_locale(StringView const& locale);
-Vector<String> best_fit_supported_locales(Vector<String> const& requested_locales);
+String insert_unicode_extension_and_canonicalize(Unicode::LocaleID locale_id, Unicode::LocaleExtension extension);
+LocaleResult resolve_locale(Vector<String> const& requested_locales, LocaleOptions const& options, Vector<StringView> const& relevant_extension_keys);
 Vector<String> lookup_supported_locales(Vector<String> const& requested_locales);
+Vector<String> best_fit_supported_locales(Vector<String> const& requested_locales);
 Array* supported_locales(GlobalObject&, Vector<String> const& requested_locales, Value options);
 Object* coerce_options_to_object(GlobalObject& global_object, Value options);
 Value get_option(GlobalObject& global_object, Value options, PropertyName const& property, Value::Type type, Vector<StringView> const& values, Fallback fallback);
 Optional<int> default_number_option(GlobalObject& global_object, Value value, int minimum, int maximum, Optional<int> fallback);
 Optional<int> get_number_option(GlobalObject& global_object, Object& options, PropertyName const& property, int minimum, int maximum, Optional<int> fallback);
 Vector<PatternPartition> partition_pattern(StringView pattern);
-String insert_unicode_extension_and_canonicalize(Unicode::LocaleID locale_id, Unicode::LocaleExtension extension);
-LocaleResult resolve_locale(Vector<String> const& requested_locales, LocaleOptions const& options, Vector<StringView> const& relevant_extension_keys);
 
 }
