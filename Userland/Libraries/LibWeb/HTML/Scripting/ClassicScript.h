106f29591688bc7f430f58360a2f6171e37b01ba
Linus Groh <mail@linusgroh.de>
2021-09-12 12:02:20 +0100

    LibJS+LibWeb: Make JS::Script and Web::HTML::ClassicScript use Realms
    The spec wants Script Records to have a Realm, not a GlobalObject.

--- Userland/Libraries/LibWeb/HTML/Scripting/ClassicScript.h
+++ Userland/Libraries/LibWeb/HTML/Scripting/ClassicScript.h
@@ -20,7 +20,7 @@ public:
         No,
         Yes,
     };
-    static NonnullRefPtr<ClassicScript> create(String filename, StringView source, JS::GlobalObject&, URL base_url, MutedErrors = MutedErrors::No);
+    static NonnullRefPtr<ClassicScript> create(String filename, StringView source, JS::Realm&, URL base_url, MutedErrors = MutedErrors::No);
 
     JS::Script* script_record() { return m_script_record; }
     JS::Script const* script_record() const { return m_script_record; }
