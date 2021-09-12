cf44813d8b2a31130967272850d2359857b29f9e
Ben Wiederhake <see other commits>
2021-09-12 15:31:19 +0200

    Kernel: Remove unused imports in Syscalls 4/4

--- Kernel/Syscalls/ttyname.cpp
+++ Kernel/Syscalls/ttyname.cpp
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#include <Kernel/FileSystem/OpenFileDescription.h>
 #include <Kernel/Process.h>
 #include <Kernel/TTY/MasterPTY.h>
 #include <Kernel/TTY/TTY.h>
