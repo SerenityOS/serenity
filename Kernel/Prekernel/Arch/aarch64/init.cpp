a62cc3907b43a00b7ae6d118a2568806659fea4d
Nico Weber <thakis@chromium.org>
2021-09-12 09:37:28 -0400

    Kernel: Define __stack_chk_guard for aarch64 Prekernel
    Needed for functions that have local variables.
    
    In time we need to share this between aarch64 and intel, but while
    we figure out what exactly the aarch64 Prekernel should do, let's
    duplicate this.

--- Kernel/Prekernel/Arch/aarch64/init.cpp
+++ Kernel/Prekernel/Arch/aarch64/init.cpp
@@ -4,8 +4,14 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <AK/Types.h>
+
 extern "C" [[noreturn]] void init();
 extern "C" [[noreturn]] void init()
 {
     for (;;) { }
 }
+
+// FIXME: Share this with the Intel Prekernel.
+extern size_t __stack_chk_guard;
+size_t __stack_chk_guard;
