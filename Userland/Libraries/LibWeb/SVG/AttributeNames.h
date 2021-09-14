33e670a348b4b4f7f1adf751bf955a3c787d88bf
Andreas Kling <kling@serenityos.org>
2021-09-15 00:26:16 +0200

    LibWeb: Add an SVG::AttributeNames namespace
    There are a whole bunch of SVG attributes, and we shouldn't mix them in
    with the HTML attributes. This patch adds some of them to the new
    namespace, but there are more to be added. :^)

---
+++ Userland/Libraries/LibWeb/SVG/AttributeNames.h
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2021, Andreas Kling <kling@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/FlyString.h>
+
+namespace Web::SVG::AttributeNames {
+
+#define ENUMERATE_SVG_ATTRIBUTES(E) \
+    E(attributeName)                \
+    E(attributeType)                \
+    E(baseFrequency)                \
+    E(baseProfile)                  \
+    E(calcMode)                     \
+    E(clipPathUnits)                \
+    E(contentScriptType)            \
+    E(contentStyleType)             \
+    E(diffuseConstant)              \
+    E(edgeMode)                     \
+    E(filterUnits)                  \
+    E(glyphRef)                     \
+    E(gradientTransform)            \
+    E(gradientUnits)                \
+    E(height)                       \
+    E(kernelMatrix)                 \
+    E(kernelUnitLength)             \
+    E(keyPoints)                    \
+    E(keySplines)                   \
+    E(keyTimes)                     \
+    E(lengthAdjust)                 \
+    E(limitingConeAngle)            \
+    E(markerHeight)                 \
+    E(markerUnits)                  \
+    E(markerWidth)                  \
+    E(maskContentUnits)             \
+    E(maskUnits)                    \
+    E(numOctaves)                   \
+    E(pathLength)                   \
+    E(patternContentUnits)          \
+    E(patternTransform)             \
+    E(patternUnits)                 \
+    E(pointsAtX)                    \
+    E(pointsAtY)                    \
+    E(pointsAtZ)                    \
+    E(preserveAlpha)                \
+    E(preserveAspectRatio)          \
+    E(primitiveUnits)               \
+    E(refX)                         \
+    E(refY)                         \
+    E(repeatCount)                  \
+    E(repeatDur)                    \
+    E(requiredExtensions)           \
+    E(requiredFeatures)             \
+    E(specularConstant)             \
+    E(specularExponent)             \
+    E(spreadMethod)                 \
+    E(startOffset)                  \
+    E(stdDeviation)                 \
+    E(stitchTiles)                  \
+    E(surfaceScale)                 \
+    E(systemLanguage)               \
+    E(tableValues)                  \
+    E(targetX)                      \
+    E(targetY)                      \
+    E(textLength)                   \
+    E(version)                      \
+    E(viewBox)                      \
+    E(viewTarget)                   \
+    E(width)                        \
+    E(x)                            \
+    E(xChannelSelector)             \
+    E(y)                            \
+    E(yChannelSelector)             \
+    E(zoomAndPan)
+
+#define __ENUMERATE_SVG_ATTRIBUTE(name) extern FlyString name;
+ENUMERATE_SVG_ATTRIBUTES(__ENUMERATE_SVG_ATTRIBUTE)
+#undef __ENUMERATE_SVG_ATTRIBUTE
+
+}
