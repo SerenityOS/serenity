ad1f7a067e6d4399522d2acae4e6c07e28d318a6
Andreas Kling <kling@serenityos.org>
2021-09-15 01:12:41 +0200

    LibWeb: Parse the <svg viewBox> attribute
    Just parse it into an SVG::ViewBox object for now, we don't actually use
    it for anything yet.

--- Userland/Libraries/LibWeb/SVG/SVGSVGElement.cpp
+++ Userland/Libraries/LibWeb/SVG/SVGSVGElement.cpp
@@ -9,6 +9,7 @@
 #include <LibWeb/DOM/Document.h>
 #include <LibWeb/DOM/Event.h>
 #include <LibWeb/Layout/SVGSVGBox.h>
+#include <LibWeb/SVG/AttributeNames.h>
 #include <LibWeb/SVG/SVGSVGElement.h>
 
 namespace Web::SVG {
@@ -34,6 +35,14 @@ unsigned SVGSVGElement::width() const
 unsigned SVGSVGElement::height() const
 {
     return attribute(HTML::AttributeNames::height).to_uint().value_or(150);
+}
+
+void SVGSVGElement::parse_attribute(FlyString const& name, String const& value)
+{
+    SVGGraphicsElement::parse_attribute(name, value);
+
+    if (name.equals_ignoring_case(SVG::AttributeNames::viewBox))
+        m_view_box = try_parse_view_box(value);
 }
 
 }
