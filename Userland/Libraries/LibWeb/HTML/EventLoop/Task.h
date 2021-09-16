64df30230d75f1fa92231659a4cc3511740384e8
Andreas Kling <kling@serenityos.org>
2021-09-16 23:43:08 +0200

    LibWeb: Add HTML::Task::Source::IdleTask
    This represents what the spec calls the "idle-task task source".

--- Userland/Libraries/LibWeb/HTML/EventLoop/Task.h
+++ Userland/Libraries/LibWeb/HTML/EventLoop/Task.h
@@ -22,6 +22,7 @@ public:
         UserInteraction,
         Networking,
         HistoryTraversal,
+        IdleTask,
     };
 
     static NonnullOwnPtr<Task> create(Source source, DOM::Document* document, Function<void()> steps)
