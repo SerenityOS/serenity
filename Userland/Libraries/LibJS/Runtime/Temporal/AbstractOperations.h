2e28f0b371bb73d61f85203edd22f504e6933f5d
Linus Groh <mail@linusgroh.de>
2021-09-16 17:50:23 +0100

    LibJS: Convert parse_temporal_calendar_string() to ThrowCompletionOr

--- Userland/Libraries/LibJS/Runtime/Temporal/AbstractOperations.h
+++ Userland/Libraries/LibJS/Runtime/Temporal/AbstractOperations.h
@@ -105,7 +105,7 @@ double constrain_to_range(double x, double minimum, double maximum);
 BigInt* round_number_to_increment(GlobalObject&, BigInt const&, u64 increment, StringView rounding_mode);
 ThrowCompletionOr<ISODateTime> parse_iso_date_time(GlobalObject&, String const& iso_string);
 ThrowCompletionOr<TemporalInstant> parse_temporal_instant_string(GlobalObject&, String const& iso_string);
-Optional<String> parse_temporal_calendar_string(GlobalObject&, String const& iso_string);
+ThrowCompletionOr<String> parse_temporal_calendar_string(GlobalObject&, String const& iso_string);
 Optional<TemporalDate> parse_temporal_date_string(GlobalObject&, String const& iso_string);
 Optional<ISODateTime> parse_temporal_date_time_string(GlobalObject&, String const& iso_string);
 Optional<TemporalDuration> parse_temporal_duration_string(GlobalObject&, String const& iso_string);
