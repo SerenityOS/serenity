70b4ef426590a880746341e44cf9e684d0f4cf93
Nico Weber <thakis@chromium.org>
2021-09-17 10:51:13 -0400

    Kernel: Use new halt() for all our halting needs in aarch64 Prekernel

--- Kernel/Prekernel/Arch/aarch64/init.cpp
+++ Kernel/Prekernel/Arch/aarch64/init.cpp
@@ -7,12 +7,14 @@
 #include <AK/Types.h>
 #include <Kernel/Prekernel/Arch/aarch64/MainIdRegister.h>
 
+extern "C" [[noreturn]] void halt();
+
 extern "C" [[noreturn]] void init();
 extern "C" [[noreturn]] void init()
 {
     Prekernel::MainIdRegister id;
     [[maybe_unused]] unsigned part_num = id.part_num();
-    for (;;) { }
+    halt();
 }
 
 // FIXME: Share this with the Intel Prekernel.
@@ -20,7 +22,7 @@ extern size_t __stack_chk_guard;
 size_t __stack_chk_guard;
 extern "C" [[noreturn]] void __stack_chk_fail();
 
-[[noreturn]] static void halt()
+[[noreturn]] void halt()
 {
     for (;;) {
         asm volatile("wfi");
