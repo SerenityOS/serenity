9253f937a634d96b9260e450c2cf320755aff083
Andreas Kling <kling@serenityos.org>
2021-09-18 12:56:35 +0200

    LibWebSocket: Use deferred_invoke() when discarding a connection
    We don't want to destroy the WebSocketImpl while we're still using it
    higher up the stack. By using deferred_invoke(), we allow the stack
    to unwind before actually destroying any objects.
    
    This fixes an issue with the WebSocket service crashing on immediate
    connection failure.

--- Userland/Libraries/LibWebSocket/WebSocket.cpp
+++ Userland/Libraries/LibWebSocket/WebSocket.cpp
@@ -565,12 +565,14 @@ void WebSocket::fatal_error(WebSocket::Error error)
 
 void WebSocket::discard_connection()
 {
-    VERIFY(m_impl);
-    m_impl->discard_connection();
-    m_impl->on_connection_error = nullptr;
-    m_impl->on_connected = nullptr;
-    m_impl->on_ready_to_read = nullptr;
-    m_impl = nullptr;
+    deferred_invoke([this] {
+        VERIFY(m_impl);
+        m_impl->discard_connection();
+        m_impl->on_connection_error = nullptr;
+        m_impl->on_connected = nullptr;
+        m_impl->on_ready_to_read = nullptr;
+        m_impl = nullptr;
+    });
 }
 
 void WebSocket::notify_open()
