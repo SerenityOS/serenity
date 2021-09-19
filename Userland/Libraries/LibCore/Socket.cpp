de8860038d2214a1700c6ff2a12308e8f985f8cf
Ali Mohammad Pur <ali.mpfard@gmail.com>
2021-09-19 18:22:38 +0430

    LibCore: Don't double-check select() in Socket's read notifier callback
    This was used to work around a possible bug where select() would mark a
    socket readable, but another user of the same socket would read before
    the notifier's callback is run; let's remove this and fix any issues
    regarding that specific situation later when they pop up.

--- Userland/Libraries/LibCore/Socket.cpp
+++ Userland/Libraries/LibCore/Socket.cpp
@@ -213,8 +213,6 @@ void Socket::ensure_read_notifier()
     VERIFY(m_connected);
     m_read_notifier = Notifier::construct(fd(), Notifier::Event::Read, this);
     m_read_notifier->on_ready_to_read = [this] {
-        if (!can_read())
-            return;
         if (on_ready_to_read)
             on_ready_to_read();
     };
