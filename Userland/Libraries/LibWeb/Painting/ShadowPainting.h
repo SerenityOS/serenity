28a2a0913684d27c9732b8e5aee83e0e0386cf74
Sam Atkins <atkinssj@serenityos.org>
2021-09-19 15:00:47 +0100

    LibWeb: Move box-shadow painting out of Box to its own file
    This makes the code accessible to things that aren't a Box, such as
    InlineNode.

---
+++ Userland/Libraries/LibWeb/Painting/ShadowPainting.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021, Sam Atkins <atkinssj@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibGfx/Forward.h>
+
+namespace Web::Painting {
+
+struct BoxShadowData {
+    int offset_x;
+    int offset_y;
+    int blur_radius;
+    Gfx::Color color;
+};
+
+void paint_box_shadow(PaintContext&, Gfx::IntRect const&, BoxShadowData const&);
+
+}
