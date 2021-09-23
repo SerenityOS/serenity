SerenityOS/serenity

commit ee825d6d9e1cb4d106a2fc87d9dafe517915d4de

LibJS: Convert get_method to ThrowCompletionOr

Userland/Libraries/LibJS/Runtime/ArrayConstructor.cpp
@@ -108,9 +108,7 @@ JS_DEFINE_NATIVE_FUNCTION(ArrayConstructor::from)
     auto this_arg = vm.argument(2);
 
     auto items = vm.argument(0);
-    auto using_iterator = items.get_method(global_object, *vm.well_known_symbol_iterator());
-    if (vm.exception())
-        return {};
+    auto using_iterator = TRY_OR_DISCARD(items.get_method(global_object, *vm.well_known_symbol_iterator()));
     if (using_iterator) {
         Value array;
         if (constructor.is_constructor()) {

Userland/Libraries/LibJS/Runtime/IteratorOperations.cpp
@@ -124,16 +124,22 @@ void iterator_close(Object& iterator)
             vm.unwind(unwind_until, unwind_until_label);
     };
 
-    auto return_method = Value(&iterator).get_method(global_object, vm.names.return_);
-    if (!return_method)
-        return restore_completion(); // If return is undefined, return Completion(completion).
-
-    auto result_or_error = vm.call(*return_method, &iterator);
+    auto return_method_or_error = Value(&iterator).get_method(global_object, vm.names.return_);
+    Value result;
+    if (!return_method_or_error.is_error()) { // If innerResult.[[Type]] is normal, then
+        auto return_method = return_method_or_error.release_value();
+        if (!return_method)
+            return restore_completion(); // If return is undefined, return Completion(completion).
+        auto result_or_error = vm.call(*return_method, &iterator);
+        if (result_or_error.is_error())
+            return_method_or_error = result_or_error.release_error();
+        else
+            result = result_or_error.release_value();
+    }
     if (completion_exception)
         return restore_completion(); // If completion.[[Type]] is throw, return Completion(completion).
-    if (result_or_error.is_error())
+    if (return_method_or_error.is_error())
         return; // If innerResult.[[Type]] is throw, return Completion(innerResult).
-    auto result = result_or_error.release_value();
     if (!result.is_object()) {
         vm.throw_exception<TypeError>(global_object, ErrorType::IterableReturnBadReturn);
         return; // If Type(innerResult.[[Value]]) is not Object, throw a TypeError exception.

Userland/Libraries/LibJS/Runtime/ProxyObject.cpp
@@ -62,9 +62,7 @@ Object* ProxyObject::internal_get_prototype_of() const
     // 4. Let target be O.[[ProxyTarget]].
 
     // 5. Let trap be ? GetMethod(handler, "getPrototypeOf").
-    auto trap = Value(&m_handler).get_method(global_object, vm.names.getPrototypeOf);
-    if (vm.exception())
-        return {};
+    auto trap = TRY_OR_DISCARD(Value(&m_handler).get_method(global_object, vm.names.getPrototypeOf));
 
     // 6. If trap is undefined, then
     if (!trap) {
@@ -124,9 +122,7 @@ bool ProxyObject::internal_set_prototype_of(Object* prototype)
     // 5. Let target be O.[[ProxyTarget]].
 
     // 6. Let trap be ? GetMethod(handler, "setPrototypeOf").
-    auto trap = Value(&m_handler).get_method(global_object, vm.names.setPrototypeOf);
-    if (vm.exception())
-        return {};
+    auto trap = TRY_OR_DISCARD(Value(&m_handler).get_method(global_object, vm.names.setPrototypeOf));
 
     // 7. If trap is undefined, then
     if (!trap) {
@@ -183,9 +179,7 @@ bool ProxyObject::internal_is_extensible() const
     // 4. Let target be O.[[ProxyTarget]].
 
     // 5. Let trap be ? GetMethod(handler, "isExtensible").
-    auto trap = Value(&m_handler).get_method(global_object, vm.names.isExtensible);
-    if (vm.exception())
-        return {};
+    auto trap = TRY_OR_DISCARD(Value(&m_handler).get_method(global_object, vm.names.isExtensible));
 
     // 6. If trap is undefined, then
     if (!trap) {
@@ -229,9 +223,7 @@ bool ProxyObject::internal_prevent_extensions()
     // 4. Let target be O.[[ProxyTarget]].
 
     // 5. Let trap be ? GetMethod(handler, "preventExtensions").
-    auto trap = Value(&m_handler).get_method(global_object, vm.names.preventExtensions);
-    if (vm.exception())
-        return {};
+    auto trap = TRY_OR_DISCARD(Value(&m_handler).get_method(global_object, vm.names.preventExtensions));
 
     // 6. If trap is undefined, then
     if (!trap) {
@@ -281,9 +273,7 @@ Optional<PropertyDescriptor> ProxyObject::internal_get_own_property(const Proper
     // 5. Let target be O.[[ProxyTarget]].
 
     // 6. Let trap be ? GetMethod(handler, "getOwnPropertyDescriptor").
-    auto trap = Value(&m_handler).get_method(global_object, vm.names.getOwnPropertyDescriptor);
-    if (vm.exception())
-        return {};
+    auto trap = TRY_OR_DISCARD(Value(&m_handler).get_method(global_object, vm.names.getOwnPropertyDescriptor));
 
     // 7. If trap is undefined, then
     if (!trap) {
@@ -397,9 +387,7 @@ bool ProxyObject::internal_define_own_property(PropertyName const& property_name
     // 5. Let target be O.[[ProxyTarget]].
 
     // 6. Let trap be ? GetMethod(handler, "defineProperty").
-    auto trap = Value(&m_handler).get_method(global_object, vm.names.defineProperty);
-    if (vm.exception())
-        return {};
+    auto trap = TRY_OR_DISCARD(Value(&m_handler).get_method(global_object, vm.names.defineProperty));
 
     // 7. If trap is undefined, then
     if (!trap) {
@@ -496,9 +484,7 @@ bool ProxyObject::internal_has_property(PropertyName const& property_name) const
     // 5. Let target be O.[[ProxyTarget]].
 
     // 6. Let trap be ? GetMethod(handler, "has").
-    auto trap = Value(&m_handler).get_method(global_object, vm.names.has);
-    if (vm.exception())
-        return {};
+    auto trap = TRY_OR_DISCARD(Value(&m_handler).get_method(global_object, vm.names.has));
 
     // 7. If trap is undefined, then
     if (!trap) {
@@ -582,9 +568,7 @@ Value ProxyObject::internal_get(PropertyName const& property_name, Value receive
     }
 
     // 6. Let trap be ? GetMethod(handler, "get").
-    auto trap = Value(&m_handler).get_method(global_object, vm.names.get);
-    if (vm.exception())
-        return {};
+    auto trap = TRY_OR_DISCARD(Value(&m_handler).get_method(global_object, vm.names.get));
 
     // 7. If trap is undefined, then
     if (!trap) {
@@ -648,9 +632,7 @@ bool ProxyObject::internal_set(PropertyName const& property_name, Value value, V
     // 5. Let target be O.[[ProxyTarget]].
 
     // 6. Let trap be ? GetMethod(handler, "set").
-    auto trap = Value(&m_handler).get_method(global_object, vm.names.set);
-    if (vm.exception())
-        return {};
+    auto trap = TRY_OR_DISCARD(Value(&m_handler).get_method(global_object, vm.names.set));
 
     // 7. If trap is undefined, then
     if (!trap) {
@@ -715,9 +697,7 @@ bool ProxyObject::internal_delete(PropertyName const& property_name)
     // 5. Let target be O.[[ProxyTarget]].
 
     // 6. Let trap be ? GetMethod(handler, "deleteProperty").
-    auto trap = Value(&m_handler).get_method(global_object, vm.names.deleteProperty);
-    if (vm.exception())
-        return {};
+    auto trap = TRY_OR_DISCARD(Value(&m_handler).get_method(global_object, vm.names.deleteProperty));
 
     // 7. If trap is undefined, then
     if (!trap) {
@@ -780,9 +760,10 @@ MarkedValueList ProxyObject::internal_own_property_keys() const
     // 4. Let target be O.[[ProxyTarget]].
 
     // 5. Let trap be ? GetMethod(handler, "ownKeys").
-    auto trap = Value(&m_handler).get_method(global_object, vm.names.ownKeys);
-    if (vm.exception())
+    auto trap_or_error = Value(&m_handler).get_method(global_object, vm.names.ownKeys);
+    if (trap_or_error.is_error())
         return MarkedValueList { heap() };
+    auto trap = trap_or_error.release_value();
 
     // 6. If trap is undefined, then
     if (!trap) {
@@ -931,9 +912,7 @@ Value ProxyObject::call()
     // 4. Let target be O.[[ProxyTarget]].
 
     // 5. Let trap be ? GetMethod(handler, "apply").
-    auto trap = Value(&m_handler).get_method(global_object, vm.names.apply);
-    if (vm.exception())
-        return {};
+    auto trap = TRY_OR_DISCARD(Value(&m_handler).get_method(global_object, vm.names.apply));
 
     // 6. If trap is undefined, then
     if (!trap) {
@@ -976,9 +955,7 @@ Value ProxyObject::construct(FunctionObject& new_target)
     // 5. Assert: IsConstructor(target) is true.
 
     // 6. Let trap be ? GetMethod(handler, "construct").
-    auto trap = Value(&m_handler).get_method(global_object, vm.names.construct);
-    if (vm.exception())
-        return {};
+    auto trap = TRY_OR_DISCARD(Value(&m_handler).get_method(global_object, vm.names.construct));
 
     // 7. If trap is undefined, then
     if (!trap) {

Userland/Libraries/LibJS/Runtime/StringPrototype.cpp
@@ -710,9 +710,7 @@ JS_DEFINE_NATIVE_FUNCTION(StringPrototype::split)
     auto limit_argument = vm.argument(1);
 
     if (!separator_argument.is_nullish()) {
-        auto splitter = separator_argument.get_method(global_object, *vm.well_known_symbol_split());
-        if (vm.exception())
-            return {};
+        auto splitter = TRY_OR_DISCARD(separator_argument.get_method(global_object, *vm.well_known_symbol_split()));
         if (splitter)
             return TRY_OR_DISCARD(vm.call(*splitter, separator_argument, object, limit_argument));
     }
@@ -860,10 +858,8 @@ JS_DEFINE_NATIVE_FUNCTION(StringPrototype::match)
     auto this_object = TRY_OR_DISCARD(require_object_coercible(global_object, vm.this_value(global_object)));
     auto regexp = vm.argument(0);
     if (!regexp.is_nullish()) {
-        if (auto* matcher = regexp.get_method(global_object, *vm.well_known_symbol_match()))
+        if (auto* matcher = TRY_OR_DISCARD(regexp.get_method(global_object, *vm.well_known_symbol_match())))
             return TRY_OR_DISCARD(vm.call(*matcher, regexp, this_object));
-        if (vm.exception())
-            return {};
     }
 
     auto string = this_object.to_utf16_string(global_object);
@@ -896,10 +892,8 @@ JS_DEFINE_NATIVE_FUNCTION(StringPrototype::match_all)
                 return {};
             }
         }
-        if (auto* matcher = regexp.get_method(global_object, *vm.well_known_symbol_match_all()))
+        if (auto* matcher = TRY_OR_DISCARD(regexp.get_method(global_object, *vm.well_known_symbol_match_all())))
             return TRY_OR_DISCARD(vm.call(*matcher, regexp, this_object));
-        if (vm.exception())
-            return {};
     }
 
     auto string = this_object.to_utf16_string(global_object);
@@ -920,10 +914,8 @@ JS_DEFINE_NATIVE_FUNCTION(StringPrototype::replace)
     auto replace_value = vm.argument(1);
 
     if (!search_value.is_nullish()) {
-        if (auto* replacer = search_value.get_method(global_object, *vm.well_known_symbol_replace()))
+        if (auto* replacer = TRY_OR_DISCARD(search_value.get_method(global_object, *vm.well_known_symbol_replace())))
             return TRY_OR_DISCARD(vm.call(*replacer, search_value, this_object, replace_value));
-        if (vm.exception())
-            return {};
     }
 
     auto string = this_object.to_utf16_string(global_object);
@@ -991,9 +983,7 @@ JS_DEFINE_NATIVE_FUNCTION(StringPrototype::replace_all)
             }
         }
 
-        auto* replacer = search_value.get_method(global_object, *vm.well_known_symbol_replace());
-        if (vm.exception())
-            return {};
+        auto* replacer = TRY_OR_DISCARD(search_value.get_method(global_object, *vm.well_known_symbol_replace()));
         if (replacer)
             return TRY_OR_DISCARD(vm.call(*replacer, search_value, this_object, replace_value));
     }
@@ -1061,10 +1051,8 @@ JS_DEFINE_NATIVE_FUNCTION(StringPrototype::search)
     auto this_object = TRY_OR_DISCARD(require_object_coercible(global_object, vm.this_value(global_object)));
     auto regexp = vm.argument(0);
     if (!regexp.is_nullish()) {
-        if (auto* searcher = regexp.get_method(global_object, *vm.well_known_symbol_search()))
+        if (auto* searcher = TRY_OR_DISCARD(regexp.get_method(global_object, *vm.well_known_symbol_search())))
             return TRY_OR_DISCARD(vm.call(*searcher, regexp, this_object));
-        if (vm.exception())
-            return {};
     }
 
     auto string = this_object.to_utf16_string(global_object);

Userland/Libraries/LibJS/Runtime/Temporal/Calendar.cpp
@@ -90,9 +90,7 @@ ThrowCompletionOr<Vector<String>> calendar_fields(GlobalObject& global_object, O
     auto& vm = global_object.vm();
 
     // 1. Let fields be ? GetMethod(calendar, "fields").
-    auto fields = Value(&calendar).get_method(global_object, vm.names.fields);
-    if (auto* exception = vm.exception())
-        return throw_completion(exception->value());
+    auto fields = TRY(Value(&calendar).get_method(global_object, vm.names.fields));
 
     // 2. Let fieldsArray be ! CreateArrayFromList(fieldNames).
     auto field_names_values = MarkedValueList { vm.heap() };

Userland/Libraries/LibJS/Runtime/Temporal/TimeZone.cpp
@@ -365,9 +365,7 @@ ThrowCompletionOr<double> get_offset_nanoseconds_for(GlobalObject& global_object
     auto& vm = global_object.vm();
 
     // 1. Let getOffsetNanosecondsFor be ? GetMethod(timeZone, "getOffsetNanosecondsFor").
-    auto* get_offset_nanoseconds_for = time_zone.get_method(global_object, vm.names.getOffsetNanosecondsFor);
-    if (auto* exception = vm.exception())
-        return throw_completion(exception->value());
+    auto* get_offset_nanoseconds_for = TRY(time_zone.get_method(global_object, vm.names.getOffsetNanosecondsFor));
 
     // 2. If getOffsetNanosecondsFor is undefined, set getOffsetNanosecondsFor to %Temporal.TimeZone.prototype.getOffsetNanosecondsFor%.
     if (!get_offset_nanoseconds_for)

Userland/Libraries/LibJS/Runtime/TypedArray.cpp
@@ -363,9 +363,7 @@ void TypedArrayBase::visit_edges(Visitor& visitor)
                 if (vm.exception())                                                                                                    \
                     return {};                                                                                                         \
             } else {                                                                                                                   \
-                auto iterator = first_argument.get_method(global_object(), *vm.well_known_symbol_iterator());                          \
-                if (vm.exception())                                                                                                    \
-                    return {};                                                                                                         \
+                auto iterator = TRY_OR_DISCARD(first_argument.get_method(global_object(), *vm.well_known_symbol_iterator()));          \
                 if (iterator) {                                                                                                        \
                     auto values = iterable_to_list(global_object(), first_argument, iterator);                                         \
                     if (vm.exception())                                                                                                \

Userland/Libraries/LibJS/Runtime/TypedArrayConstructor.cpp
@@ -77,9 +77,7 @@ JS_DEFINE_NATIVE_FUNCTION(TypedArrayConstructor::from)
     auto source = vm.argument(0);
     auto this_arg = vm.argument(2);
 
-    auto using_iterator = source.get_method(global_object, *vm.well_known_symbol_iterator());
-    if (vm.exception())
-        return {};
+    auto using_iterator = TRY_OR_DISCARD(source.get_method(global_object, *vm.well_known_symbol_iterator()));
     if (using_iterator) {
         auto values = iterable_to_list(global_object, source, using_iterator);
         if (vm.exception())

commit a34fd4fdd30fe597cca5b93b678310c46571c5d8

LibJS: Close iterator on throw completion in Array.from
This regressed in #10190

Userland/Libraries/LibJS/Runtime/ArrayConstructor.cpp
@@ -149,11 +149,12 @@ JS_DEFINE_NATIVE_FUNCTION(ArrayConstructor::from)
 
             Value mapped_value;
             if (map_fn) {
-                mapped_value = TRY_OR_DISCARD(vm.call(*map_fn, this_arg, next_value, Value(k)));
-                if (vm.exception()) {
+                auto mapped_value_or_error = vm.call(*map_fn, this_arg, next_value, Value(k));
+                if (mapped_value_or_error.is_error()) {
                     iterator_close(*iterator);
                     return {};
                 }
+                mapped_value = mapped_value_or_error.release_value();
             } else {
                 mapped_value = next_value;
             }