8212aad1bb716b794af70f035ba35bc36aac31fa
Nico Weber <thakis@chromium.org>
2021-09-25 17:53:42 -0400

    Kernel: Add a GPIO class for aarch64
    This allows configuring the alternate pin functions and pin
    pull up/down states, which is needed for using the UART.

--- Kernel/Prekernel/Arch/aarch64/init.cpp
+++ Kernel/Prekernel/Arch/aarch64/init.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <AK/Types.h>
+#include <Kernel/Prekernel/Arch/aarch64/GPIO.h>
 #include <Kernel/Prekernel/Arch/aarch64/Mailbox.h>
 
 extern "C" [[noreturn]] void halt();
@@ -12,6 +13,12 @@ extern "C" [[noreturn]] void halt();
 extern "C" [[noreturn]] void init();
 extern "C" [[noreturn]] void init()
 {
+    auto& gpio = Prekernel::GPIO::the();
+    gpio.set_pin_function(14, Prekernel::GPIO::PinFunction::Alternate0);
+    gpio.set_pin_function(15, Prekernel::GPIO::PinFunction::Alternate0);
+
+    gpio.set_pin_pull_up_down_state(Array { 14, 15 }, Prekernel::GPIO::PullUpDownState::Disable);
+
     [[maybe_unused]] u32 firmware_version = Prekernel::Mailbox::query_firmware_version();
     halt();
 }
@@ -29,6 +36,11 @@ extern "C" [[noreturn]] void __stack_chk_fail();
 }
 
 void __stack_chk_fail()
+{
+    halt();
+}
+
+[[noreturn]] void __assertion_failed(char const*, char const*, unsigned int, char const*)
 {
     halt();
 }
