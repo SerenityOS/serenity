b8363fc84f63da1a434f2816ff9a6f20d6e5352a
Nico Weber <thakis@chromium.org>
2021-09-25 23:28:50 -0400

    Kernel: Add Mailbox::set_clock_rate()

--- Kernel/Prekernel/Arch/aarch64/Mailbox.h
+++ Kernel/Prekernel/Arch/aarch64/Mailbox.h
@@ -17,6 +17,25 @@ public:
     static bool call(u8 channel, u32 volatile* __attribute__((aligned(16))) data);
 
     static u32 query_firmware_version();
+
+    enum class ClockID {
+        Reserved = 0,
+        EMMC = 1,
+        UART = 2,
+        ARM = 3,
+        CORE = 4,
+        V3D = 5,
+        H264 = 6,
+        ISP = 7,
+        SDRAM = 8,
+        PIXEL = 9,
+        PWM = 10,
+        HEVC = 11,
+        EMMC2 = 12,
+        M2MC = 13,
+        PIXEL_BVB = 14,
+    };
+    static u32 set_clock_rate(ClockID, u32 rate_hz, bool skip_setting_turbo = true);
 };
 
 }
