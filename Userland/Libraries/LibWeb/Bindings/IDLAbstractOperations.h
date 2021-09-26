9774b13d655fc4521509707bb6dd87e3c4a02217
Luke Wilde <lukew@serenityos.org>
2021-09-26 14:53:28 +0100

    LibWeb: Add support for IDL legacy platform objects
    A legacy platform object is a non-global platform object that
    implements a special operation. A special operation is a getter, setter
    and/or deleter. This is particularly used for old collection types,
    such as HTMLCollection, NodeList, etc.
    
    This will be used to make these spec-compliant and remove their custom
    wrappers. Additionally, it will be used to implement collections that
    we don't have yet, such as DOMStringMap.

---
+++ Userland/Libraries/LibWeb/Bindings/IDLAbstractOperations.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, Luke Wilde <lukew@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibJS/Forward.h>
+
+namespace Web::Bindings::IDL {
+
+bool is_an_array_index(JS::GlobalObject&, JS::PropertyName const&);
+
+}
