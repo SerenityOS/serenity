d294a3f54a848f3165a71f134b3f09d556e34a11
Andreas Kling <kling@serenityos.org>
2021-09-28 21:58:05 +0200

    LibJS: Avoid unnecessary HashMap growth in Interpreter::enter_scope()
    Don't bother pre-allocating a hash map if we're not gonna put anything
    into it anyway.

--- Userland/Libraries/LibJS/Interpreter.cpp
+++ Userland/Libraries/LibJS/Interpreter.cpp
@@ -113,7 +113,9 @@ void Interpreter::enter_scope(const ScopeNode& scope_node, ScopeType scope_type,
     }
 
     HashMap<FlyString, Variable> scope_variables_with_declaration_kind;
-    scope_variables_with_declaration_kind.ensure_capacity(16);
+
+    if (!scope_node.variables().is_empty())
+        scope_variables_with_declaration_kind.ensure_capacity(16);
 
     bool is_program_node = is<Program>(scope_node);
 
