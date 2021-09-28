06f93950568b8e5167f64007fa4980a3050904a2
Sam Atkins <atkinssj@serenityos.org>
2021-09-28 16:20:32 +0100

    LibWeb: Add CSSGroupingRule
    This is an abstract base class for CSSRules that hold a CSSRuleList.

---
+++ Userland/Libraries/LibWeb/CSS/CSSGroupingRule.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2021, Sam Atkins <atkinssj@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/NonnullRefPtr.h>
+#include <LibWeb/CSS/CSSRule.h>
+#include <LibWeb/CSS/CSSRuleList.h>
+#include <LibWeb/Forward.h>
+
+namespace Web::CSS {
+
+class CSSGroupingRule : public CSSRule {
+    AK_MAKE_NONCOPYABLE(CSSGroupingRule);
+    AK_MAKE_NONMOVABLE(CSSGroupingRule);
+
+public:
+    ~CSSGroupingRule();
+
+    CSSRuleList const& css_rules() const { return m_rules; }
+    size_t insert_rule(StringView const& rule, size_t index = 0);
+    void delete_rule(size_t index);
+
+protected:
+    explicit CSSGroupingRule(NonnullRefPtrVector<CSSRule>&&);
+
+private:
+    CSSRuleList m_rules;
+};
+
+}
