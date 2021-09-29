0351db63a4752d8217357d081ec7b8548b00af38
Luke Wilde <lukew@serenityos.org>
2021-09-29 13:03:09 +0100

    LibWeb: Make StyleSheetList.item an IDL getter

--- Userland/Libraries/LibWeb/CSS/StyleSheetList.cpp
+++ Userland/Libraries/LibWeb/CSS/StyleSheetList.cpp
@@ -18,4 +18,15 @@ StyleSheetList::StyleSheetList(DOM::Document& document)
 {
 }
 
+// https://drafts.csswg.org/cssom/#ref-for-dfn-supported-property-indices%E2%91%A1
+bool StyleSheetList::is_supported_property_index(u32 index) const
+{
+    // The object’s supported property indices are the numbers in the range zero to one less than the number of CSS style sheets represented by the collection.
+    // If there are no such CSS style sheets, then there are no supported property indices.
+    if (m_sheets.is_empty())
+        return false;
+
+    return index < m_sheets.size();
+}
+
 }
