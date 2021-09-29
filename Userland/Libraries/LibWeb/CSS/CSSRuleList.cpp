30d710a0a230fd6f3473a9667ac08c46a84192d7
Andreas Kling <kling@serenityos.org>
2021-09-29 20:28:32 +0200

    LibWeb: Add CSSStyleSheet.{insert,delete,remove}Rule() APIs
    Note that insertRule() is really just a big TODO right now.

--- Userland/Libraries/LibWeb/CSS/CSSRuleList.cpp
+++ Userland/Libraries/LibWeb/CSS/CSSRuleList.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <LibWeb/CSS/CSSRuleList.h>
+#include <LibWeb/DOM/ExceptionOr.h>
 
 namespace Web::CSS {
 
@@ -22,6 +23,30 @@ bool CSSRuleList::is_supported_property_index(u32 index) const
     // The object’s supported property indices are the numbers in the range zero to one less than the number of CSSRule objects represented by the collection.
     // If there are no such CSSRule objects, then there are no supported property indices.
     return index < m_rules.size();
+}
+
+// https://drafts.csswg.org/cssom/#remove-a-css-rule
+DOM::ExceptionOr<void> CSSRuleList::remove_a_css_rule(u32 index)
+{
+    // 1. Set length to the number of items in list.
+    auto length = m_rules.size();
+
+    // 2. If index is greater than or equal to length, then throw an IndexSizeError exception.
+    if (index >= length)
+        return DOM::IndexSizeError::create("CSS rule index out of bounds.");
+
+    // 3. Set old rule to the indexth item in list.
+    auto& old_rule = m_rules[index];
+
+    // FIXME: 4. If old rule is an @namespace at-rule, and list contains anything other than @import at-rules, and @namespace at-rules, throw an InvalidStateError exception.
+    (void)old_rule;
+
+    // 5. Remove rule old rule from list at the zero-indexed position index.
+    m_rules.remove(index);
+
+    // FIXME: 6. Set old rule’s parent CSS rule and parent CSS style sheet to null.
+
+    return {};
 }
 
 }
--- Userland/Libraries/LibWeb/CSS/CSSStyleSheet.cpp
+++ Userland/Libraries/LibWeb/CSS/CSSStyleSheet.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <LibWeb/CSS/CSSStyleSheet.h>
+#include <LibWeb/DOM/ExceptionOr.h>
 
 namespace Web::CSS {
 
@@ -15,6 +16,44 @@ CSSStyleSheet::CSSStyleSheet(NonnullRefPtrVector<CSSRule> rules)
 
 CSSStyleSheet::~CSSStyleSheet()
 {
+}
+
+// https://drafts.csswg.org/cssom/#dom-cssstylesheet-insertrule
+DOM::ExceptionOr<unsigned> CSSStyleSheet::insert_rule(StringView rule, unsigned index)
+{
+    // FIXME: 1. If the origin-clean flag is unset, throw a SecurityError exception.
+
+    // FIXME: 2. If the disallow modification flag is set, throw a NotAllowedError DOMException.
+
+    // Let parsed rule be the return value of invoking parse a rule with rule.
+
+    // If parsed rule is a syntax error, return parsed rule.
+
+    // If parsed rule is an @import rule, and the constructed flag is set, throw a SyntaxError DOMException.
+
+    // Return the result of invoking insert a CSS rule rule in the CSS rules at index.
+
+    (void)index;
+    (void)rule;
+    TODO();
+}
+
+// https://drafts.csswg.org/cssom/#dom-cssstylesheet-deleterule
+DOM::ExceptionOr<void> CSSStyleSheet::delete_rule(unsigned index)
+{
+    // FIXME: 1. If the origin-clean flag is unset, throw a SecurityError exception.
+
+    // FIXME: 2. If the disallow modification flag is set, throw a NotAllowedError DOMException.
+
+    // 3. Remove a CSS rule in the CSS rules at index.
+    return m_rules->remove_a_css_rule(index);
+}
+
+// https://drafts.csswg.org/cssom/#dom-cssstylesheet-removerule
+DOM::ExceptionOr<void> CSSStyleSheet::remove_rule(unsigned index)
+{
+    // The removeRule(index) method must run the same steps as deleteRule().
+    return delete_rule(index);
 }
 
 }
