3b9e8ec597fa0260a60f1436e9158e5f18ef3483
Idan Horowitz <idan.horowitz@gmail.com>
2021-09-29 20:32:29 +0300

    LibWeb: Add the missing CustomEvent IDL constructor

--- Userland/Libraries/LibWeb/DOM/CustomEvent.h
+++ Userland/Libraries/LibWeb/DOM/CustomEvent.h
@@ -10,17 +10,25 @@
 
 namespace Web::DOM {
 
+struct CustomEventInit : public EventInit {
+    JS::Value detail { JS::js_null() };
+};
+
 // https://dom.spec.whatwg.org/#customevent
 class CustomEvent : public Event {
 public:
     using WrapperType = Bindings::CustomEventWrapper;
 
-    static NonnullRefPtr<CustomEvent> create(FlyString const& event_name)
+    static NonnullRefPtr<CustomEvent> create(FlyString const& event_name, CustomEventInit const& event_init = {})
     {
-        return adopt_ref(*new CustomEvent(event_name));
+        return adopt_ref(*new CustomEvent(event_name, event_init));
+    }
+    static NonnullRefPtr<CustomEvent> create_with_global_object(Bindings::WindowObject&, const FlyString& event_name, CustomEventInit const& event_init)
+    {
+        return CustomEvent::create(event_name, event_init);
     }
 
-    virtual ~CustomEvent() override;
+    virtual ~CustomEvent() override = default;
 
     // https://dom.spec.whatwg.org/#dom-customevent-detail
     JS::Value detail() const { return m_detail; }
@@ -30,7 +38,16 @@ public:
     void init_custom_event(String const& type, bool bubbles, bool cancelable, JS::Value detail);
 
 private:
-    CustomEvent(FlyString const&);
+    explicit CustomEvent(FlyString const& event_name)
+        : Event(event_name)
+    {
+    }
+
+    CustomEvent(FlyString const& event_name, CustomEventInit const& event_init)
+        : Event(event_name, event_init)
+        , m_detail(event_init.detail)
+    {
+    }
 
     // https://dom.spec.whatwg.org/#dom-customevent-initcustomevent-type-bubbles-cancelable-detail-detail
     JS::Value m_detail { JS::js_null() };
