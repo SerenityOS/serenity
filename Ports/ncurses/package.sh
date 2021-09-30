1d1bc16f1aac9057f78856b8ef017439bef4cb7b
Jelle Raaijmakers <jelle@gmta.nl>
2021-09-30 10:58:50 +0200

    Ports: Compile ncurses with `--enable-term-driver`
    In commit ba97548686 `--with-termlib` was added to produce a
    `libtinfo.a` file that nano then required. However, this causes ncurses
    to build with _only_ screen-pointer ext funcs: e.g.
    `reset_prog_mode_sp` exists, but `reset_prog_mode` does not.
    
    By switching to `--enable-term-driver`, all functions are properly
    exported again and the nano port compiles and runs just fine. :^)

--- Ports/ncurses/package.sh
+++ Ports/ncurses/package.sh
@@ -2,7 +2,15 @@
 port=ncurses
 version=6.2
 useconfigure=true
-configopts="--with-termlib --enable-pc-files --with-pkg-config=/usr/local/lib/pkgconfig --with-pkg-config-libdir=/usr/local/lib/pkgconfig --without-ada --enable-sigwinch --with-shared"
+configopts="
+    --enable-pc-files
+    --enable-sigwinch
+    --enable-term-driver
+    --with-pkg-config=/usr/local/lib/pkgconfig
+    --with-pkg-config-libdir=/usr/local/lib/pkgconfig
+    --with-shared
+    --without-ada
+"
 files="https://ftpmirror.gnu.org/gnu/ncurses/ncurses-${version}.tar.gz ncurses-${version}.tar.gz 30306e0c76e0f9f1f0de987cf1c82a5c21e1ce6568b9227f7da5b71cbea86c9d"
 auth_type="sha256"
 
