6cfe17ef2903911f038d0ff5112359aeda6ca165
Jelle Raaijmakers <jelle@gmta.nl>
2021-09-30 10:56:45 +0200

    Ports: Switch ncurses to SHA256 auth type

--- Ports/ncurses/package.sh
+++ Ports/ncurses/package.sh
@@ -3,11 +3,8 @@ port=ncurses
 version=6.2
 useconfigure=true
 configopts="--with-termlib --enable-pc-files --with-pkg-config=/usr/local/lib/pkgconfig --with-pkg-config-libdir=/usr/local/lib/pkgconfig --without-ada --enable-sigwinch --with-shared"
-files="https://ftpmirror.gnu.org/gnu/ncurses/ncurses-${version}.tar.gz ncurses-${version}.tar.gz
-https://ftpmirror.gnu.org/gnu/ncurses/ncurses-${version}.tar.gz.sig ncurses-${version}.tar.gz.sig
-https://ftpmirror.gnu.org/gnu/gnu-keyring.gpg gnu-keyring.gpg"
-auth_type="sig"
-auth_opts="--keyring ./gnu-keyring.gpg ncurses-${version}.tar.gz.sig"
+files="https://ftpmirror.gnu.org/gnu/ncurses/ncurses-${version}.tar.gz ncurses-${version}.tar.gz 30306e0c76e0f9f1f0de987cf1c82a5c21e1ce6568b9227f7da5b71cbea86c9d"
+auth_type="sha256"
 
 pre_configure() {
     export CPPFLAGS="-P"
