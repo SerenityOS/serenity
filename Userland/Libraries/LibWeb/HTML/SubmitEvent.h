1e8ba0d9d362dffb8e781d0a45155623c8bc2cea
Idan Horowitz <idan.horowitz@gmail.com>
2021-10-01 18:46:37 +0300

    LibWeb: Add the missing SubmitEvent IDL constructor
    This commit also removes the SubmitEvent.cpp file, as all of the method
    implementations were trivial and could be inlined into the header file.

--- Userland/Libraries/LibWeb/HTML/SubmitEvent.h
+++ Userland/Libraries/LibWeb/HTML/SubmitEvent.h
@@ -6,22 +6,39 @@
 
 #pragma once
 
+#include <LibWeb/Bindings/HTMLElementWrapper.h>
 #include <LibWeb/DOM/Event.h>
+#include <LibWeb/HTML/HTMLElement.h>
 
 namespace Web::HTML {
+
+struct SubmitEventInit : public DOM::EventInit {
+    RefPtr<HTMLElement> submitter { nullptr };
+};
 
 class SubmitEvent final : public DOM::Event {
 public:
     using WrapperType = Bindings::SubmitEventWrapper;
 
-    static NonnullRefPtr<SubmitEvent> create(const FlyString& event_name, RefPtr<HTMLElement> submitter);
+    static NonnullRefPtr<SubmitEvent> create(FlyString const& event_name, SubmitEventInit const& event_init)
+    {
+        return adopt_ref(*new SubmitEvent(event_name, event_init));
+    }
+    static NonnullRefPtr<SubmitEvent> create_with_global_object(Bindings::WindowObject&, FlyString const& event_name, SubmitEventInit const& event_init)
+    {
+        return SubmitEvent::create(event_name, event_init);
+    }
 
-    virtual ~SubmitEvent() override;
+    virtual ~SubmitEvent() override = default;
 
-    RefPtr<HTMLElement> submitter() const;
+    RefPtr<HTMLElement> submitter() const { return m_submitter; }
 
 private:
-    SubmitEvent(const FlyString& event_name, RefPtr<HTMLElement> submitter);
+    SubmitEvent(FlyString const& event_name, SubmitEventInit const& event_init)
+        : DOM::Event(event_name, event_init)
+        , m_submitter(event_init.submitter)
+    {
+    }
 
     RefPtr<HTMLElement> m_submitter;
 };
