2c8c177c134a22c0f3657029041f48634fc4d34c
Idan Horowitz <idan.horowitz@gmail.com>
2021-10-01 19:18:19 +0300

    LibWeb: Add the UIEvent::{view, detail} IDL attributes

--- Userland/Libraries/LibWeb/UIEvents/UIEvent.h
+++ Userland/Libraries/LibWeb/UIEvents/UIEvent.h
@@ -6,7 +6,9 @@
 
 #pragma once
 
+#include <AK/RefPtr.h>
 #include <LibWeb/DOM/Event.h>
+#include <LibWeb/DOM/Window.h>
 
 namespace Web::UIEvents {
 
@@ -16,11 +18,17 @@ public:
 
     virtual ~UIEvent() override { }
 
+    DOM::Window const* view() const { return m_window; }
+    int detail() const { return m_detail; }
+
 protected:
     explicit UIEvent(const FlyString& event_name)
         : Event(event_name)
     {
     }
+
+    RefPtr<DOM::Window> m_window;
+    int m_detail { 0 };
 };
 
 }
