c1d71ab397b057860dfa5336a69fa6437d655aa6
Nico Weber <thakis@chromium.org>
2021-10-01 19:33:46 -0400

    Prekernel: Better datasheet link for RPi3

--- Kernel/Prekernel/Arch/aarch64/MMIO.h
+++ Kernel/Prekernel/Arch/aarch64/MMIO.h
@@ -12,7 +12,7 @@ namespace Prekernel {
 
 // Knows about memory-mapped IO addresses on the Broadcom family of SOCs used in Raspberry Pis.
 // RPi3 is the first Raspberry Pi that supports aarch64.
-// https://datasheets.raspberrypi.org/bcm2836/bcm2836-peripherals.pdf (RPi3)
+// https://github.com/raspberrypi/documentation/files/1888662/BCM2837-ARM-Peripherals.-.Revised.-.V2-1.pdf (RPi3)
 // https://datasheets.raspberrypi.org/bcm2711/bcm2711-peripherals.pdf (RPi4 Model B)
 class MMIO {
 public:
