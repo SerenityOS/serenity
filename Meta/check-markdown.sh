89e745c2f2f9abc7cff84dccde65efe335f40d36
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2021-10-04 21:32:31 +0200

    Meta: Run check-markdown as part of 'Azure Linux NoFuzz'

--- Meta/check-markdown.sh
+++ Meta/check-markdown.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 script_path=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 cd "${script_path}/.."
 
-if [ -z "${CHECK_MARKDOWN_BINARY:-}" ] ; then
+if [ -z "${MARKDOWN_CHECK_BINARY:-}" ] ; then
     if ! [ -d Build/lagom/ ] ; then
         echo "Directory Build/lagom/ does not exist. Skipping markdown check."
         exit 0
@@ -15,7 +15,7 @@ if [ -z "${CHECK_MARKDOWN_BINARY:-}" ] ; then
         echo "To enable this check, you may need to run './Meta/serenity.sh build lagom' first."
         exit 0
     fi
-    CHECK_MARKDOWN_BINARY="Build/lagom/markdown-check"
+    MARKDOWN_CHECK_BINARY="Build/lagom/markdown-check"
 fi
 
-find AK Base Documentation Kernel Meta Ports Tests Userland -path 'Ports/*/*' -prune -o -type f -name '*.md' -print0 | xargs -0 "${CHECK_MARKDOWN_BINARY}" README.md
+find AK Base Documentation Kernel Meta Ports Tests Userland -path 'Ports/*/*' -prune -o -type f -name '*.md' -print0 | xargs -0 "${MARKDOWN_CHECK_BINARY}" README.md
