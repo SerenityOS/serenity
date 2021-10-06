6ef5464015b5d188c19e107826482096abe21dd8
Andreas Kling <kling@serenityos.org>
2021-10-06 23:38:46 +0200

    LibJS: Make DeclarativeEnvironment store bindings in a Vector
    The previous storage for DeclarativeEnvironment looked like this:
    
        HashMap<FlyString, Binding> m_bindings;
    
    This patch changes that to:
    
        HashMap<FlyString, size_t> m_names;
        Vector<Binding> m_bindings;
    
    The main goal here is to give each binding an index that can ultimately
    be cached and used for optimized environment accesses.

--- Userland/Libraries/LibJS/Runtime/DeclarativeEnvironment.cpp
+++ Userland/Libraries/LibJS/Runtime/DeclarativeEnvironment.cpp
@@ -30,59 +30,60 @@ DeclarativeEnvironment::~DeclarativeEnvironment()
 void DeclarativeEnvironment::visit_edges(Visitor& visitor)
 {
     Base::visit_edges(visitor);
-    for (auto& it : m_bindings)
-        visitor.visit(it.value.value);
+    for (auto& binding : m_bindings)
+        visitor.visit(binding.value);
 }
 
 // 9.1.1.1.1 HasBinding ( N ), https://tc39.es/ecma262/#sec-declarative-environment-records-hasbinding-n
 bool DeclarativeEnvironment::has_binding(FlyString const& name) const
 {
-    return m_bindings.contains(name);
+    return m_names.contains(name);
 }
 
 // 9.1.1.1.2 CreateMutableBinding ( N, D ), https://tc39.es/ecma262/#sec-declarative-environment-records-createmutablebinding-n-d
 void DeclarativeEnvironment::create_mutable_binding(GlobalObject&, FlyString const& name, bool can_be_deleted)
 {
-    auto result = m_bindings.set(name,
-        Binding {
-            .value = {},
-            .strict = false,
-            .mutable_ = true,
-            .can_be_deleted = can_be_deleted,
-            .initialized = false,
-        });
+    m_bindings.append(Binding {
+        .value = {},
+        .strict = false,
+        .mutable_ = true,
+        .can_be_deleted = can_be_deleted,
+        .initialized = false,
+    });
+    auto result = m_names.set(name, m_bindings.size() - 1);
     VERIFY(result == AK::HashSetResult::InsertedNewEntry);
 }
 
 // 9.1.1.1.3 CreateImmutableBinding ( N, S ), https://tc39.es/ecma262/#sec-declarative-environment-records-createimmutablebinding-n-s
 void DeclarativeEnvironment::create_immutable_binding(GlobalObject&, FlyString const& name, bool strict)
 {
-    auto result = m_bindings.set(name,
-        Binding {
-            .value = {},
-            .strict = strict,
-            .mutable_ = false,
-            .can_be_deleted = false,
-            .initialized = false,
-        });
+    m_bindings.append(Binding {
+        .value = {},
+        .strict = strict,
+        .mutable_ = false,
+        .can_be_deleted = false,
+        .initialized = false,
+    });
+    auto result = m_names.set(name, m_bindings.size() - 1);
     VERIFY(result == AK::HashSetResult::InsertedNewEntry);
 }
 
 // 9.1.1.1.4 InitializeBinding ( N, V ), https://tc39.es/ecma262/#sec-declarative-environment-records-initializebinding-n-v
 void DeclarativeEnvironment::initialize_binding(GlobalObject&, FlyString const& name, Value value)
 {
-    auto it = m_bindings.find(name);
-    VERIFY(it != m_bindings.end());
-    VERIFY(it->value.initialized == false);
-    it->value.value = value;
-    it->value.initialized = true;
+    auto it = m_names.find(name);
+    VERIFY(it != m_names.end());
+    auto& binding = m_bindings[it->value];
+    VERIFY(binding.initialized == false);
+    binding.value = value;
+    binding.initialized = true;
 }
 
 // 9.1.1.1.5 SetMutableBinding ( N, V, S ), https://tc39.es/ecma262/#sec-declarative-environment-records-setmutablebinding-n-v-s
 void DeclarativeEnvironment::set_mutable_binding(GlobalObject& global_object, FlyString const& name, Value value, bool strict)
 {
-    auto it = m_bindings.find(name);
-    if (it == m_bindings.end()) {
+    auto it = m_names.find(name);
+    if (it == m_names.end()) {
         if (strict) {
             global_object.vm().throw_exception<ReferenceError>(global_object, ErrorType::UnknownIdentifier, name);
             return;
@@ -92,16 +93,17 @@ void DeclarativeEnvironment::set_mutable_binding(GlobalObject& global_object, Fl
         return;
     }
 
-    if (it->value.strict)
+    auto& binding = m_bindings[it->value];
+    if (binding.strict)
         strict = true;
 
-    if (!it->value.initialized) {
+    if (!binding.initialized) {
         global_object.vm().throw_exception<ReferenceError>(global_object, ErrorType::BindingNotInitialized, name);
         return;
     }
 
-    if (it->value.mutable_) {
-        it->value.value = value;
+    if (binding.mutable_) {
+        binding.value = value;
     } else {
         if (strict) {
             global_object.vm().throw_exception<TypeError>(global_object, ErrorType::InvalidAssignToConst);
@@ -112,31 +114,36 @@ void DeclarativeEnvironment::set_mutable_binding(GlobalObject& global_object, Fl
 // 9.1.1.1.6 GetBindingValue ( N, S ), https://tc39.es/ecma262/#sec-declarative-environment-records-getbindingvalue-n-s
 Value DeclarativeEnvironment::get_binding_value(GlobalObject& global_object, FlyString const& name, bool)
 {
-    auto it = m_bindings.find(name);
-    VERIFY(it != m_bindings.end());
-    if (!it->value.initialized) {
+    auto it = m_names.find(name);
+    VERIFY(it != m_names.end());
+    auto& binding = m_bindings[it->value];
+    if (!binding.initialized) {
         global_object.vm().throw_exception<ReferenceError>(global_object, ErrorType::BindingNotInitialized, name);
         return {};
     }
-    return it->value.value;
+    return binding.value;
 }
 
 // 9.1.1.1.7 DeleteBinding ( N ), https://tc39.es/ecma262/#sec-declarative-environment-records-deletebinding-n
 bool DeclarativeEnvironment::delete_binding(GlobalObject&, FlyString const& name)
 {
-    auto it = m_bindings.find(name);
-    VERIFY(it != m_bindings.end());
-    if (!it->value.can_be_deleted)
+    auto it = m_names.find(name);
+    VERIFY(it != m_names.end());
+    auto& binding = m_bindings[it->value];
+    if (!binding.can_be_deleted)
         return false;
-    m_bindings.remove(it);
+    // NOTE: We keep the entry in m_bindings to avoid disturbing indices.
+    binding = {};
+    m_names.remove(it);
     return true;
 }
 
 void DeclarativeEnvironment::initialize_or_set_mutable_binding(Badge<ScopeNode>, GlobalObject& global_object, FlyString const& name, Value value)
 {
-    auto it = m_bindings.find(name);
-    VERIFY(it != m_bindings.end());
-    if (!it->value.initialized)
+    auto it = m_names.find(name);
+    VERIFY(it != m_names.end());
+    auto& binding = m_bindings[it->value];
+    if (!binding.initialized)
         initialize_binding(global_object, name, value);
     else
         set_mutable_binding(global_object, name, value, false);
@@ -145,8 +152,8 @@ void DeclarativeEnvironment::initialize_or_set_mutable_binding(Badge<ScopeNode>,
 Vector<String> DeclarativeEnvironment::bindings() const
 {
     Vector<String> names;
-    for (auto& binding : m_bindings) {
-        names.empend(binding.key);
+    for (auto& it : m_names) {
+        names.append(it.key);
     }
     return names;
 }
