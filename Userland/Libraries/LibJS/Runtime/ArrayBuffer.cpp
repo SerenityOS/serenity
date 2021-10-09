1fba5ca8c370ac1c42627761ddfeb8d3557e656b
Linus Groh <mail@linusgroh.de>
2021-10-09 12:31:13 +0100

    LibJS: Implement the AllocateArrayBuffer() AO
    This should be used instead of ArrayBuffer::create() in most places, as
    it uses OrdinaryCreateFromConstructor to allow for a custom prototype.
    
    The data block (ByteBuffer) is allocated separately and attached
    afterwards, if we didn't fail due to OOM.

--- Userland/Libraries/LibJS/Runtime/ArrayBuffer.cpp
+++ Userland/Libraries/LibJS/Runtime/ArrayBuffer.cpp
@@ -1,19 +1,20 @@
 /*
- * Copyright (c) 2020, Linus Groh <linusg@serenityos.org>
+ * Copyright (c) 2020-2021, Linus Groh <linusg@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <LibJS/Runtime/AbstractOperations.h>
 #include <LibJS/Runtime/ArrayBuffer.h>
 #include <LibJS/Runtime/GlobalObject.h>
 
 namespace JS {
 
-ArrayBuffer* ArrayBuffer::create(GlobalObject& global_object, size_t byte_size)
+ArrayBuffer* ArrayBuffer::create(GlobalObject& global_object, size_t byte_length)
 {
-    auto buffer = ByteBuffer::create_zeroed(byte_size);
+    auto buffer = ByteBuffer::create_zeroed(byte_length);
     if (!buffer.has_value()) {
-        global_object.vm().throw_exception<RangeError>(global_object, ErrorType::NotEnoughMemoryToAllocate, byte_size);
+        global_object.vm().throw_exception<RangeError>(global_object, ErrorType::NotEnoughMemoryToAllocate, byte_length);
         return nullptr;
     }
     return global_object.heap().allocate<ArrayBuffer>(global_object, buffer.release_value(), *global_object.array_buffer_prototype());
@@ -46,6 +47,26 @@ void ArrayBuffer::visit_edges(Cell::Visitor& visitor)
 {
     Base::visit_edges(visitor);
     visitor.visit(m_detach_key);
+}
+
+// 25.1.2.1 AllocateArrayBuffer ( constructor, byteLength ), https://tc39.es/ecma262/#sec-allocatearraybuffer
+ThrowCompletionOr<ArrayBuffer*> allocate_array_buffer(GlobalObject& global_object, FunctionObject& constructor, size_t byte_length)
+{
+    // 1. Let obj be ? OrdinaryCreateFromConstructor(constructor, "%ArrayBuffer.prototype%", « [[ArrayBufferData]], [[ArrayBufferByteLength]], [[ArrayBufferDetachKey]] »).
+    auto* obj = TRY(ordinary_create_from_constructor<ArrayBuffer>(global_object, constructor, &GlobalObject::array_buffer_prototype, nullptr));
+
+    // 2. Let block be ? CreateByteDataBlock(byteLength).
+    auto block = ByteBuffer::create_zeroed(byte_length);
+    if (!block.has_value())
+        return global_object.vm().throw_completion<RangeError>(global_object, ErrorType::NotEnoughMemoryToAllocate, byte_length);
+
+    // 3. Set obj.[[ArrayBufferData]] to block.
+    obj->set_buffer(move(*block));
+
+    // 4. Set obj.[[ArrayBufferByteLength]] to byteLength.
+
+    // 5. Return obj.
+    return obj;
 }
 
 }
