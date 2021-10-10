c1ee5f4bcfdec9bc14b16064a2402a0398c81906
Brian Gianforcaro <bgianf@serenityos.org>
2021-10-10 00:00:15 -0700

    LibJS: Optimize PropertyName and StringPrototype for size
    We can reduce the amount of padding the compiler adds in order to
    ensure data alignment of member variables by ordering the types in
    a struct by size in decending order.
    
    Found By PVS-Studio: https://pvs-studio.com/en/docs/warnings/v802/

--- Userland/Libraries/LibJS/Runtime/StringPrototype.h
+++ Userland/Libraries/LibJS/Runtime/StringPrototype.h
@@ -11,9 +11,9 @@
 namespace JS {
 
 struct CodePoint {
+    bool is_unpaired_surrogate { false };
     u32 code_point { 0 };
     size_t code_unit_count { 0 };
-    bool is_unpaired_surrogate { false };
 };
 
 CodePoint code_point_at(Utf16View const& string, size_t position);
