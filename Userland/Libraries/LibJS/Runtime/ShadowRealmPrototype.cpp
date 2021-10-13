3e18a0598ad7e913c49e0b2de2bcf3bc2d477b1a
Linus Groh <mail@linusgroh.de>
2021-10-13 21:10:22 +0100

    LibJS: Implement ShadowRealm.prototype[@@toStringTag]

--- Userland/Libraries/LibJS/Runtime/ShadowRealmPrototype.cpp
+++ Userland/Libraries/LibJS/Runtime/ShadowRealmPrototype.cpp
@@ -17,7 +17,11 @@ ShadowRealmPrototype::ShadowRealmPrototype(GlobalObject& global_object)
 
 void ShadowRealmPrototype::initialize(GlobalObject& global_object)
 {
+    auto& vm = this->vm();
     Object::initialize(global_object);
+
+    // 3.4.3 ShadowRealm.prototype [ @@toStringTag ], https://tc39.es/proposal-shadowrealm/#sec-shadowrealm.prototype-@@tostringtag
+    define_direct_property(*vm.well_known_symbol_to_string_tag(), js_string(vm, vm.names.ShadowRealm.as_string()), Attribute::Configurable);
 }
 
 }
