d14d7ee78b0deeecb03ca08ac0df1136b6c3ab2e
Marcin Undak <mcinek@gmail.com>
2021-10-14 12:08:39 -0400

    Kernel: Discover initial exception level when booting Aarch64
    When booting on RPI3 firmware puts CPU in EL2 mode which is
    different from QEMU's default EL3.
    
    I've added logic to discover initial mode at boot
    and then act accordingly. This results in Serenity corectly
    switching to EL1 on target hardware now.

--- Kernel/Prekernel/Arch/aarch64/Aarch64_asm_utils.h
+++ Kernel/Prekernel/Arch/aarch64/Aarch64_asm_utils.h
@@ -12,4 +12,5 @@ extern "C" uint8_t get_current_exception_level();
 extern "C" void wait_cycles(int n);
 
 // CPU initialization functions
+extern "C" [[noreturn]] void return_from_el2();
 extern "C" [[noreturn]] void return_from_el3();
