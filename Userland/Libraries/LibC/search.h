77371b3a9415476cbdceefc3f743a61d611295bc
Tim Schumacher <timschumi@gmx.de>
2021-10-14 20:46:26 +0200

    LibC: Implement twalk

--- Userland/Libraries/LibC/search.h
+++ Userland/Libraries/LibC/search.h
@@ -8,7 +8,15 @@
 
 __BEGIN_DECLS
 
+typedef enum {
+    preorder,
+    postorder,
+    endorder,
+    leaf,
+} VISIT;
+
 void* tsearch(const void*, void**, int (*)(const void*, const void*));
 void* tfind(const void*, void* const*, int (*)(const void*, const void*));
+void twalk(const void*, void (*)(const void*, VISIT, int));
 
 __END_DECLS
