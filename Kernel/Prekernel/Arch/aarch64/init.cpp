9bda418222a09630190886ea730d94c06ac17d04
Marcin Undak <mcinek@gmail.com>
2021-10-14 12:08:39 -0400

    Kernel: Discover initial exception level when booting Aarch64
    When booting on RPI3 firmware puts CPU in EL2 mode which is
    different from QEMU's default EL3.
    
    I've added logic to discover initial mode at boot
    and then act accordingly. This results in Serenity corectly
    switching to EL1 on target hardware now.

--- Kernel/Prekernel/Arch/aarch64/init.cpp
+++ Kernel/Prekernel/Arch/aarch64/init.cpp
@@ -18,7 +18,9 @@ extern "C" [[noreturn]] void os_start();
 static void set_up_el1_mode();
 static void set_up_el2_mode();
 static void set_up_el3_mode();
-[[noreturn]] static void switch_to_el1();
+static void print_current_exception_level(const char* msg);
+[[noreturn]] static void jump_to_os_start_from_el2();
+[[noreturn]] static void jump_to_os_start_from_el3();
 
 extern "C" [[noreturn]] void init()
 {
@@ -33,11 +35,43 @@ extern "C" [[noreturn]] void init()
     uart.print_num(firmware_version);
     uart.print_str("\r\n");
 
-    set_up_el3_mode();
+    print_current_exception_level("CPU started in:");
+
     set_up_el2_mode();
     set_up_el1_mode();
 
-    switch_to_el1();
+    auto current_exception_level = get_current_exception_level();
+    switch (current_exception_level) {
+    case 2:
+        jump_to_os_start_from_el2();
+        break;
+    case 3:
+        set_up_el3_mode();
+        jump_to_os_start_from_el3();
+        break;
+    default:
+        uart.print_str("FATAL: CPU booted in unsupported exception mode!\r\n");
+        halt();
+    }
+}
+
+extern "C" [[noreturn]] void os_start()
+{
+    auto& uart = Prekernel::UART::the();
+
+    print_current_exception_level("CPU switched to:");
+
+    auto& timer = Prekernel::Timer::the();
+    u64 start_musec = 0;
+    for (;;) {
+        u64 now_musec;
+        while ((now_musec = timer.microseconds_since_boot()) - start_musec < 1'000'000)
+            ;
+        start_musec = now_musec;
+        uart.print_str("Timer: ");
+        uart.print_num(now_musec);
+        uart.print_str("\r\n");
+    }
 }
 
 // FIXME: Share this with the Intel Prekernel.
@@ -128,7 +162,27 @@ static void set_up_el3_mode()
     asm("msr scr_el3, %[value]" ::[value] "r"(secure_configuration_register_el3));
 }
 
-[[noreturn]] static void switch_to_el1()
+[[noreturn]] static void jump_to_os_start_from_el2()
+{
+    // Processor state to set when returned from this function (in new EL1 world)
+    Kernel::Aarch64_SPSR_EL2 saved_program_status_register_el2 = {};
+
+    // Mask (disable) all interrupts
+    saved_program_status_register_el2.A = 1;
+    saved_program_status_register_el2.I = 1;
+    saved_program_status_register_el2.F = 1;
+
+    // Indicate EL1 as exception origin mode (so we go back there)
+    saved_program_status_register_el2.M = Kernel::Aarch64_SPSR_EL2::Mode::EL1h;
+
+    // Set the register
+    asm("msr spsr_el2, %[value]" ::[value] "r"(saved_program_status_register_el2));
+
+    // This will jump into os_start()
+    return_from_el2();
+}
+
+[[noreturn]] static void jump_to_os_start_from_el3()
 {
     // Processor state to set when returned from this function (in new EL1 world)
     Kernel::Aarch64_SPSR_EL3 saved_program_status_register_el3 = {};
@@ -144,28 +198,17 @@ static void set_up_el3_mode()
     // Set the register
     asm("msr spsr_el3, %[value]" ::[value] "r"(saved_program_status_register_el3));
 
-    // This will jump into os_start() below, but in EL1
+    // This will jump into os_start() below
     return_from_el3();
 }
 
-extern "C" [[noreturn]] void os_start()
+static void print_current_exception_level(const char* msg)
 {
     auto& uart = Prekernel::UART::the();
 
     auto exception_level = get_current_exception_level();
-    uart.print_str("Current CPU exception level: EL");
+    uart.print_str(msg);
+    uart.print_str(" EL");
     uart.print_num(exception_level);
     uart.print_str("\r\n");
-
-    auto& timer = Prekernel::Timer::the();
-    u64 start_musec = 0;
-    for (;;) {
-        u64 now_musec;
-        while ((now_musec = timer.microseconds_since_boot()) - start_musec < 1'000'000)
-            ;
-        start_musec = now_musec;
-        uart.print_str("Timer: ");
-        uart.print_num(now_musec);
-        uart.print_str("\r\n");
-    }
 }
