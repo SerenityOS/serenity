433e7e91ca531acf95542a666b77bf7561dd26c2
Sam Atkins <atkinssj@serenityos.org>
2021-10-14 16:50:43 +0100

    LibWeb: Implement more CSS serializers and make them more ergonomic
    This implements these algorithms from the CSSOM-1 spec:
    - serialize a string
    - serialize a URL
    
    Also, we now have two versions of each of the serialization functions:
    One that returns a String as before, and the other that takes a
    StringBuilder to write into. This saves creating extra StringBuilders
    when they are not needed. :^)

--- Userland/Libraries/LibWeb/CSS/Serialize.h
+++ Userland/Libraries/LibWeb/CSS/Serialize.h
@@ -7,13 +7,21 @@
 #pragma once
 
 #include <AK/String.h>
+#include <AK/StringBuilder.h>
 #include <AK/StringView.h>
 
 namespace Web::CSS {
 
+void escape_a_character(StringBuilder, u32 character);
+void escape_a_character_as_code_point(StringBuilder, u32 character);
+void serialize_an_identifier(StringBuilder, StringView const& ident);
+void serialize_a_string(StringBuilder, StringView const& string);
+void serialize_a_url(StringBuilder, StringView const& url);
+
 String escape_a_character(u32 character);
 String escape_a_character_as_code_point(u32 character);
-
 String serialize_an_identifier(StringView const& ident);
+String serialize_a_string(StringView const& string);
+String serialize_a_url(StringView const& url);
 
 }
