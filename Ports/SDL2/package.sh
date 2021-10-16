14c415d118ce3fca6ca8dc9783aa2d4f32fde3fb
Tim Schumacher <timschumi@gmx.de>
2021-10-16 15:03:00 +0200

    Ports: Fix dependencies for SDL2
    We are doing nonstandard stuff with our headers, so SDL assumed that
    both iconv and dlopen are available inside LibC, which they aren't.
    
    Fix that by adding a dependency on libiconv and adding additional
    linker flags.

--- Ports/SDL2/package.sh
+++ Ports/SDL2/package.sh
@@ -4,7 +4,8 @@ version=git
 workdir=SDL-main-serenity
 useconfigure=true
 files="https://github.com/SerenityPorts/SDL/archive/main-serenity.tar.gz SDL2-git.tar.gz"
-configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt" "-DPULSEAUDIO=OFF" "-DJACK=OFF")
+configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt" "-DPULSEAUDIO=OFF" "-DJACK=OFF" "-DEXTRA_LDFLAGS=-liconv;-ldl")
+depends=("libiconv")
 
 configure() {
     run cmake "${configopts[@]}"
