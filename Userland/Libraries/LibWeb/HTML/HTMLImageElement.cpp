c780faedd01a4d8db8bb87b5d667e7057773c9fa
Andreas Kling <kling@serenityos.org>
2021-10-18 10:37:26 +0200

    LibWeb: Update <img> style on resource load/failure

--- Userland/Libraries/LibWeb/HTML/HTMLImageElement.cpp
+++ Userland/Libraries/LibWeb/HTML/HTMLImageElement.cpp
@@ -21,7 +21,7 @@ HTMLImageElement::HTMLImageElement(DOM::Document& document, QualifiedName qualif
     , m_image_loader(*this)
 {
     m_image_loader.on_load = [this] {
-        this->document().schedule_style_update();
+        set_needs_style_update(true);
         queue_an_element_task(HTML::Task::Source::DOMManipulation, [this] {
             dispatch_event(DOM::Event::create(EventNames::load));
         });
@@ -29,7 +29,7 @@ HTMLImageElement::HTMLImageElement(DOM::Document& document, QualifiedName qualif
 
     m_image_loader.on_fail = [this] {
         dbgln("HTMLImageElement: Resource did fail: {}", src());
-        this->document().schedule_style_update();
+        set_needs_style_update(true);
         queue_an_element_task(HTML::Task::Source::DOMManipulation, [this] {
             dispatch_event(DOM::Event::create(EventNames::error));
         });
