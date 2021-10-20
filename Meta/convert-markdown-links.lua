5dddb88405fa6d59dfe902604b3b2baebf9d4924
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2021-10-20 21:46:39 +0200

    man.serenityos.org: Fix links to man(1), avoid unnecessary shells
    This fixes the current bug at the end of less(1), which links to the
    wrong file ".html" instead of "man.html".

--- Meta/convert-markdown-links.lua
+++ Meta/convert-markdown-links.lua
@@ -1,5 +1,4 @@
 function Link(el)
     el.target = string.gsub(el.target, "%.md", ".html") -- change .md to .html links
-    el.target = string.gsub(el.target, "man", "", 1) -- change man1/???.html to 1/???.html links
     return el
 end
