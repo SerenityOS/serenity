3d5871a50484d961cb570ac57f21ea326116fb0a
Timothy Flynn <trflynn89@pm.me>
2021-10-20 09:00:37 -0400

    LibJS: Convert IteratorComplete AO to ThrowCompletionOr

--- Userland/Libraries/LibJS/Runtime/IteratorOperations.h
+++ Userland/Libraries/LibJS/Runtime/IteratorOperations.h
@@ -22,7 +22,7 @@ enum class IteratorHint {
 ThrowCompletionOr<Object*> get_iterator(GlobalObject&, Value value, IteratorHint hint = IteratorHint::Sync, Value method = {});
 ThrowCompletionOr<Object*> iterator_next(Object& iterator, Value value = {});
 ThrowCompletionOr<Object*> iterator_step(GlobalObject&, Object& iterator);
-bool iterator_complete(GlobalObject&, Object& iterator_result);
+ThrowCompletionOr<bool> iterator_complete(GlobalObject&, Object& iterator_result);
 Value iterator_value(GlobalObject&, Object& iterator_result);
 void iterator_close(Object& iterator);
 Object* create_iterator_result_object(GlobalObject&, Value value, bool done);
