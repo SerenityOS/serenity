7241dcc3ebe635547b63a39d0b73bec1f391ab9b
Linus Groh <mail@linusgroh.de>
2021-10-21 20:07:40 +0100

    LibJS: Convert Temporal.PlainDate functions to ThrowCompletionOr

--- Userland/Libraries/LibJS/Runtime/Temporal/PlainDatePrototype.h
+++ Userland/Libraries/LibJS/Runtime/Temporal/PlainDatePrototype.h
@@ -20,31 +20,31 @@ public:
     virtual ~PlainDatePrototype() override = default;
 
 private:
-    JS_DECLARE_OLD_NATIVE_FUNCTION(calendar_getter);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(year_getter);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(month_getter);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(month_code_getter);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(day_getter);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(day_of_week_getter);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(day_of_year_getter);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(week_of_year_getter);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(days_in_week_getter);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(days_in_month_getter);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(days_in_year_getter);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(months_in_year_getter);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(in_leap_year_getter);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(era_getter);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(era_year_getter);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(to_plain_year_month);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(to_plain_month_day);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(get_iso_fields);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(with_calendar);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(equals);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(to_plain_date_time);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(to_string);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(to_locale_string);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(to_json);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(value_of);
+    JS_DECLARE_NATIVE_FUNCTION(calendar_getter);
+    JS_DECLARE_NATIVE_FUNCTION(year_getter);
+    JS_DECLARE_NATIVE_FUNCTION(month_getter);
+    JS_DECLARE_NATIVE_FUNCTION(month_code_getter);
+    JS_DECLARE_NATIVE_FUNCTION(day_getter);
+    JS_DECLARE_NATIVE_FUNCTION(day_of_week_getter);
+    JS_DECLARE_NATIVE_FUNCTION(day_of_year_getter);
+    JS_DECLARE_NATIVE_FUNCTION(week_of_year_getter);
+    JS_DECLARE_NATIVE_FUNCTION(days_in_week_getter);
+    JS_DECLARE_NATIVE_FUNCTION(days_in_month_getter);
+    JS_DECLARE_NATIVE_FUNCTION(days_in_year_getter);
+    JS_DECLARE_NATIVE_FUNCTION(months_in_year_getter);
+    JS_DECLARE_NATIVE_FUNCTION(in_leap_year_getter);
+    JS_DECLARE_NATIVE_FUNCTION(era_getter);
+    JS_DECLARE_NATIVE_FUNCTION(era_year_getter);
+    JS_DECLARE_NATIVE_FUNCTION(to_plain_year_month);
+    JS_DECLARE_NATIVE_FUNCTION(to_plain_month_day);
+    JS_DECLARE_NATIVE_FUNCTION(get_iso_fields);
+    JS_DECLARE_NATIVE_FUNCTION(with_calendar);
+    JS_DECLARE_NATIVE_FUNCTION(equals);
+    JS_DECLARE_NATIVE_FUNCTION(to_plain_date_time);
+    JS_DECLARE_NATIVE_FUNCTION(to_string);
+    JS_DECLARE_NATIVE_FUNCTION(to_locale_string);
+    JS_DECLARE_NATIVE_FUNCTION(to_json);
+    JS_DECLARE_NATIVE_FUNCTION(value_of);
 };
 
 }
