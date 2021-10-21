1dfbd41255e636707818ddd3948af1c80d6e3870
Timothy Flynn <trflynn89@pm.me>
2021-10-21 06:28:38 -0400

    Meta: Enable ccache for Lagom by default

--- Meta/CMake/lagom_options.cmake
+++ Meta/CMake/lagom_options.cmake
@@ -8,4 +8,4 @@ serenity_option(ENABLE_ADDRESS_SANITIZER OFF CACHE BOOL "Enable address sanitize
 serenity_option(ENABLE_MEMORY_SANITIZER OFF CACHE BOOL "Enable memory sanitizer testing in gcc/clang")
 serenity_option(ENABLE_FUZZER_SANITIZER OFF CACHE BOOL "Enable fuzzer sanitizer testing in clang")
 serenity_option(BUILD_LAGOM OFF CACHE BOOL "Build parts of the system targeting the host OS for fuzzing/testing")
-serenity_option(ENABLE_LAGOM_CCACHE OFF CACHE BOOL "Enable ccache for Lagom builds")
+serenity_option(ENABLE_LAGOM_CCACHE ON CACHE BOOL "Enable ccache for Lagom builds")
