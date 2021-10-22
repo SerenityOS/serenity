10db2e28200ae3a274f943a2f075ed571c96d462
Martin Bříza <m@rtinbriza.cz>
2021-10-22 22:48:05 +0200

    Make it build with Qt 6.2, very dirty

--- Ports/qt6-qtbase/package.sh
+++ Ports/qt6-qtbase/package.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=qt6-qtbase
-version=6.1.1
+version=6.2.0
 #depends="zlib"
-workdir=qtbase-everywhere-src-6.1.1
+workdir=qtbase-everywhere-src-6.2.0
 useconfigure=true
-files="https://download.qt.io/official_releases/qt/6.1/6.1.1/submodules/qtbase-everywhere-src-6.1.1.tar.xz qt6-qtbase-6.1.1.tar.xz"
-configopts="-GNinja -DCMAKE_CROSSCOMPILING=ON -DQT_BUILD_TOOLS_WHEN_CROSSCOMPILING=ON -DCMAKE_TOOLCHAIN_FILE=${SERENITY_SOURCE_DIR}/Toolchain/CMake/CMakeToolchain.txt -DQT_HOST_PATH=/usr -DQT_FEATURE_cxx20=ON -DBUILD_SHARED_LIBS=OFF"
+files="https://download.qt.io/official_releases/qt/6.2/6.2.0/submodules/qtbase-everywhere-src-6.2.0.tar.xz qt6-qtbase-6.2.0.tar.xz"
+configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt" "-GNinja" "-DCMAKE_CROSSCOMPILING=ON" "-DQT_BUILD_TOOLS_WHEN_CROSSCOMPILING=ON" "-DQT_HOST_PATH=/usr" "-DQT_FEATURE_cxx20=ON" "-DBUILD_SHARED_LIBS=OFF")
 
 QT_HOST_PATH=/usr
 QT_HOST_CMAKE_PATH=${QT_HOST_PATH}/lib64/cmake
@@ -24,7 +24,7 @@ configure() {
     MERGED_HOST_TOOLS=$(for i in ${QT_HOST_TOOLS}; do echo "-DQt6${i}_DIR=${QT_HOST_CMAKE_PATH}/Qt6${i}/"; done)
     MERGED_DISABLED_FEATURES=$(for i in ${QT_DISABLED_FEATURES}; do echo "-DQT_FEATURE_${i}=OFF"; done)
 
-    run cmake $configopts ${MERGED_HOST_TOOLS} ${MERGED_DISABLED_FEATURES}
+    run cmake ${configopts[@]} ${MERGED_HOST_TOOLS} ${MERGED_DISABLED_FEATURES}
 }
 
 build() {
