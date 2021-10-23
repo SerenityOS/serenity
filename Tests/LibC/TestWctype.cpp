bdfb471a2aa29f94de0463ae584ce2d219949812
Tim Schumacher <timschumi@gmx.de>
2021-10-23 02:40:20 +0200

    Tests: Use proper comparison macros for wctype

--- Tests/LibC/TestWctype.cpp
+++ Tests/LibC/TestWctype.cpp
@@ -11,33 +11,33 @@
 TEST_CASE(wctype)
 {
     // Test that existing properties return non-zero wctypes.
-    EXPECT(wctype("alnum") != 0);
-    EXPECT(wctype("alpha") != 0);
-    EXPECT(wctype("blank") != 0);
-    EXPECT(wctype("cntrl") != 0);
-    EXPECT(wctype("digit") != 0);
-    EXPECT(wctype("graph") != 0);
-    EXPECT(wctype("lower") != 0);
-    EXPECT(wctype("print") != 0);
-    EXPECT(wctype("punct") != 0);
-    EXPECT(wctype("space") != 0);
-    EXPECT(wctype("upper") != 0);
-    EXPECT(wctype("xdigit") != 0);
+    EXPECT_NE(wctype("alnum"), 0ul);
+    EXPECT_NE(wctype("alpha"), 0ul);
+    EXPECT_NE(wctype("blank"), 0ul);
+    EXPECT_NE(wctype("cntrl"), 0ul);
+    EXPECT_NE(wctype("digit"), 0ul);
+    EXPECT_NE(wctype("graph"), 0ul);
+    EXPECT_NE(wctype("lower"), 0ul);
+    EXPECT_NE(wctype("print"), 0ul);
+    EXPECT_NE(wctype("punct"), 0ul);
+    EXPECT_NE(wctype("space"), 0ul);
+    EXPECT_NE(wctype("upper"), 0ul);
+    EXPECT_NE(wctype("xdigit"), 0ul);
 
     // Test that invalid properties return the "invalid" wctype (0).
-    EXPECT(wctype("") == 0);
-    EXPECT(wctype("abc") == 0);
+    EXPECT_EQ(wctype(""), 0ul);
+    EXPECT_EQ(wctype("abc"), 0ul);
 }
 
 TEST_CASE(wctrans)
 {
     // Test that existing character mappings return non-zero wctrans values.
-    EXPECT(wctrans("tolower") != 0);
-    EXPECT(wctrans("toupper") != 0);
+    EXPECT_NE(wctrans("tolower"), 0);
+    EXPECT_NE(wctrans("toupper"), 0);
 
     // Test that invalid character mappings return the "invalid" wctrans value (0).
-    EXPECT(wctrans("") == 0);
-    EXPECT(wctrans("abc") == 0);
+    EXPECT_EQ(wctrans(""), 0);
+    EXPECT_EQ(wctrans("abc"), 0);
 }
 
 TEST_CASE(iswctype)
@@ -46,24 +46,24 @@ TEST_CASE(iswctype)
 
     // Test that valid properties are wired to the correct implementation.
     for (unsigned int i = 0; i < sizeof(test_chars) / sizeof(test_chars[0]); i++) {
-        EXPECT(iswctype(test_chars[i], wctype("alnum")) == iswalnum(test_chars[i]));
-        EXPECT(iswctype(test_chars[i], wctype("alpha")) == iswalpha(test_chars[i]));
-        EXPECT(iswctype(test_chars[i], wctype("blank")) == iswblank(test_chars[i]));
-        EXPECT(iswctype(test_chars[i], wctype("cntrl")) == iswcntrl(test_chars[i]));
-        EXPECT(iswctype(test_chars[i], wctype("digit")) == iswdigit(test_chars[i]));
-        EXPECT(iswctype(test_chars[i], wctype("graph")) == iswgraph(test_chars[i]));
-        EXPECT(iswctype(test_chars[i], wctype("lower")) == iswlower(test_chars[i]));
-        EXPECT(iswctype(test_chars[i], wctype("print")) == iswprint(test_chars[i]));
-        EXPECT(iswctype(test_chars[i], wctype("punct")) == iswpunct(test_chars[i]));
-        EXPECT(iswctype(test_chars[i], wctype("space")) == iswspace(test_chars[i]));
-        EXPECT(iswctype(test_chars[i], wctype("upper")) == iswupper(test_chars[i]));
-        EXPECT(iswctype(test_chars[i], wctype("xdigit")) == iswxdigit(test_chars[i]));
+        EXPECT_EQ(iswctype(test_chars[i], wctype("alnum")), iswalnum(test_chars[i]));
+        EXPECT_EQ(iswctype(test_chars[i], wctype("alpha")), iswalpha(test_chars[i]));
+        EXPECT_EQ(iswctype(test_chars[i], wctype("blank")), iswblank(test_chars[i]));
+        EXPECT_EQ(iswctype(test_chars[i], wctype("cntrl")), iswcntrl(test_chars[i]));
+        EXPECT_EQ(iswctype(test_chars[i], wctype("digit")), iswdigit(test_chars[i]));
+        EXPECT_EQ(iswctype(test_chars[i], wctype("graph")), iswgraph(test_chars[i]));
+        EXPECT_EQ(iswctype(test_chars[i], wctype("lower")), iswlower(test_chars[i]));
+        EXPECT_EQ(iswctype(test_chars[i], wctype("print")), iswprint(test_chars[i]));
+        EXPECT_EQ(iswctype(test_chars[i], wctype("punct")), iswpunct(test_chars[i]));
+        EXPECT_EQ(iswctype(test_chars[i], wctype("space")), iswspace(test_chars[i]));
+        EXPECT_EQ(iswctype(test_chars[i], wctype("upper")), iswupper(test_chars[i]));
+        EXPECT_EQ(iswctype(test_chars[i], wctype("xdigit")), iswxdigit(test_chars[i]));
     }
 
     // Test that invalid properties always return zero.
     for (unsigned int i = 0; i < sizeof(test_chars) / sizeof(test_chars[0]); i++) {
-        EXPECT(iswctype(test_chars[i], 0) == 0);
-        EXPECT(iswctype(test_chars[i], -1) == 0);
+        EXPECT_EQ(iswctype(test_chars[i], 0), 0);
+        EXPECT_EQ(iswctype(test_chars[i], -1), 0);
     }
 }
 
@@ -73,13 +73,13 @@ TEST_CASE(towctrans)
 
     // Test that valid mappings are wired to the correct implementation.
     for (unsigned int i = 0; i < sizeof(test_chars) / sizeof(test_chars[0]); i++) {
-        EXPECT(towctrans(test_chars[i], wctrans("tolower")) == towlower(test_chars[i]));
-        EXPECT(towctrans(test_chars[i], wctrans("toupper")) == towupper(test_chars[i]));
+        EXPECT_EQ(towctrans(test_chars[i], wctrans("tolower")), towlower(test_chars[i]));
+        EXPECT_EQ(towctrans(test_chars[i], wctrans("toupper")), towupper(test_chars[i]));
     }
 
     // Test that invalid mappings always return the character unchanged.
     for (unsigned int i = 0; i < sizeof(test_chars) / sizeof(test_chars[0]); i++) {
-        EXPECT(towctrans(test_chars[i], 0) == test_chars[i]);
-        EXPECT(towctrans(test_chars[i], -1) == test_chars[i]);
+        EXPECT_EQ(towctrans(test_chars[i], 0), test_chars[i]);
+        EXPECT_EQ(towctrans(test_chars[i], -1), test_chars[i]);
     }
 }
