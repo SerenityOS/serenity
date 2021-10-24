da13e21bacc15b95fe38b5beed4c62a5f9772c17
Hendiadyoin1 <leon2002.la@gmail.com>
2021-10-24 13:22:19 +0200

    AK: Move x86 specific code out of Math.h
    The implementations are now found in <AK/MathDetail/x86.h>

---
+++ AK/MathDetail/common.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2022, Leon Albrecht <leon2002.la@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/Concepts.h>
+
+namespace AK {
+
+template<FloatingPoint T>
+constexpr T NaN = __builtin_nan("");
+template<FloatingPoint T>
+constexpr T Pi = 3.141592653589793238462643383279502884L;
+template<FloatingPoint T>
+constexpr T E = 2.718281828459045235360287471352662498L;
+
+}
