f2c9664a6c1d99fd96b2bd594a7b055cdeee055b
Brendan Coles <bcoles@gmail.com>
2021-10-25 08:14:27 +0000

    Ports: Bump bc from version 2.5.1 to 5.1.1

--- Ports/bc/package.sh
+++ Ports/bc/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=bc
-version=2.5.1
+version=5.1.1
 files="https://github.com/gavinhoward/bc/releases/download/${version}/bc-${version}.tar.xz bc-${version}.tar.xz
 https://github.com/gavinhoward/bc/releases/download/${version}/bc-${version}.tar.xz.sig bc-${version}.tar.xz.sig"
 useconfigure=true
@@ -8,9 +8,8 @@ configscript=configure.sh
 auth_type="sig"
 auth_import_key="E2A30324A4465A4D5882692EC08038BDF280D33E"
 auth_opts=("bc-${version}.tar.xz.sig")
+configopts=("--prefix=/usr/local" "--disable-nls" "--disable-history")
 
 configure() {
-    # NLS needs many things, none of which we support.
-    # History needs FIONREAD ioctl, which we don't support yet.
-    run env HOSTCC=gcc ./"$configscript" --disable-nls --disable-history
+    run env HOSTCC=gcc ./"$configscript" "${configopts[@]}"
 }
