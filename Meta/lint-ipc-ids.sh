7ef1217ffebf6564558631f896505e14ec12aaa5
thislooksfun <tlf@thislooks.fun>
2021-10-26 16:02:58 -0500

    Meta: Run find in the current dir
    macOS's find requires a leading search scope. Without this change this
    lint step fails.

--- Meta/lint-ipc-ids.sh
+++ Meta/lint-ipc-ids.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 script_path=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 cd "$script_path/.."
 
-ALL_ENDPOINTS=$(find \( -name Toolchain -o -name Build -o -name .git -o -name Ports \) -prune -o -name '*.ipc' -print0 | xargs -0 grep -E '^endpoint ' | sort -k4 -n)
+ALL_ENDPOINTS=$(find . \( -name Toolchain -o -name Build -o -name .git -o -name Ports \) -prune -o -name '*.ipc' -print0 | xargs -0 grep -E '^endpoint ' | sort -k4 -n)
 
 BAD_ENDPOINTS=$(echo "${ALL_ENDPOINTS}" | cut -d' ' -f4 | uniq -d)
 
