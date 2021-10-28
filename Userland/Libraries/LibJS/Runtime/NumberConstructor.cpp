d8fc50b10b085d5e8df100bfcf96e33f173ac1be
Idan Horowitz <idan.horowitz@gmail.com>
2021-10-29 00:46:14 +0300

    LibJS: Convert NumberConstructor functions to ThrowCompletionOr

--- Userland/Libraries/LibJS/Runtime/NumberConstructor.cpp
+++ Userland/Libraries/LibJS/Runtime/NumberConstructor.cpp
@@ -37,10 +37,10 @@ void NumberConstructor::initialize(GlobalObject& global_object)
     define_direct_property(vm.names.prototype, global_object.number_prototype(), 0);
 
     u8 attr = Attribute::Writable | Attribute::Configurable;
-    define_old_native_function(vm.names.isFinite, is_finite, 1, attr);
-    define_old_native_function(vm.names.isInteger, is_integer, 1, attr);
-    define_old_native_function(vm.names.isNaN, is_nan, 1, attr);
-    define_old_native_function(vm.names.isSafeInteger, is_safe_integer, 1, attr);
+    define_native_function(vm.names.isFinite, is_finite, 1, attr);
+    define_native_function(vm.names.isInteger, is_integer, 1, attr);
+    define_native_function(vm.names.isNaN, is_nan, 1, attr);
+    define_native_function(vm.names.isSafeInteger, is_safe_integer, 1, attr);
     define_direct_property(vm.names.parseInt, global_object.get_without_side_effects(vm.names.parseInt), attr);
     define_direct_property(vm.names.parseFloat, global_object.get_without_side_effects(vm.names.parseFloat), attr);
     define_direct_property(vm.names.EPSILON, Value(EPSILON_VALUE), 0);
@@ -60,13 +60,13 @@ NumberConstructor::~NumberConstructor()
 }
 
 // Most of 21.1.1.1 Number ( value ) factored into a separate function for sharing between call() and construct().
-static Value get_value_from_constructor_argument(GlobalObject& global_object)
+static ThrowCompletionOr<Value> get_value_from_constructor_argument(GlobalObject& global_object)
 {
     auto& vm = global_object.vm();
 
     Value number;
     if (vm.argument_count() > 0) {
-        auto primitive = TRY_OR_DISCARD(vm.argument(0).to_numeric(global_object));
+        auto primitive = TRY(vm.argument(0).to_numeric(global_object));
         if (primitive.is_bigint()) {
             // FIXME: How should huge values be handled here?
             auto& big_integer = primitive.as_bigint().big_integer();
@@ -83,47 +83,38 @@ static Value get_value_from_constructor_argument(GlobalObject& global_object)
 // 21.1.1.1 Number ( value ), https://tc39.es/ecma262/#sec-number-constructor-number-value
 ThrowCompletionOr<Value> NumberConstructor::call()
 {
-    auto& vm = this->vm();
-    auto& global_object = this->global_object();
-
-    auto number = get_value_from_constructor_argument(global_object);
-    if (auto* exception = vm.exception())
-        return throw_completion(exception->value());
-    return number;
+    return get_value_from_constructor_argument(global_object());
 }
 
 // 21.1.1.1 Number ( value ), https://tc39.es/ecma262/#sec-number-constructor-number-value
 ThrowCompletionOr<Object*> NumberConstructor::construct(FunctionObject& new_target)
 {
-    auto& vm = this->vm();
     auto& global_object = this->global_object();
 
-    auto number = get_value_from_constructor_argument(global_object);
-    if (auto* exception = vm.exception())
-        return throw_completion(exception->value());
+    auto number = TRY(get_value_from_constructor_argument(global_object));
     return TRY(ordinary_create_from_constructor<NumberObject>(global_object, new_target, &GlobalObject::number_prototype, number.as_double()));
 }
 
 // 21.1.2.2 Number.isFinite ( number ), https://tc39.es/ecma262/#sec-number.isfinite
-JS_DEFINE_OLD_NATIVE_FUNCTION(NumberConstructor::is_finite)
+JS_DEFINE_NATIVE_FUNCTION(NumberConstructor::is_finite)
 {
     return Value(vm.argument(0).is_finite_number());
 }
 
 // 21.1.2.3 Number.isInteger ( number ), https://tc39.es/ecma262/#sec-number.isinteger
-JS_DEFINE_OLD_NATIVE_FUNCTION(NumberConstructor::is_integer)
+JS_DEFINE_NATIVE_FUNCTION(NumberConstructor::is_integer)
 {
     return Value(vm.argument(0).is_integral_number());
 }
 
 // 21.1.2.4 Number.isNaN ( number ), https://tc39.es/ecma262/#sec-number.isnan
-JS_DEFINE_OLD_NATIVE_FUNCTION(NumberConstructor::is_nan)
+JS_DEFINE_NATIVE_FUNCTION(NumberConstructor::is_nan)
 {
     return Value(vm.argument(0).is_nan());
 }
 
 // 21.1.2.5 Number.isSafeInteger ( number ), https://tc39.es/ecma262/#sec-number.issafeinteger
-JS_DEFINE_OLD_NATIVE_FUNCTION(NumberConstructor::is_safe_integer)
+JS_DEFINE_NATIVE_FUNCTION(NumberConstructor::is_safe_integer)
 {
     if (!vm.argument(0).is_number())
         return Value(false);
