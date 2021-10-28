131bef83b191bc20546b3148e61a092b4e005ae3
Idan Horowitz <idan.horowitz@gmail.com>
2021-10-29 00:16:59 +0300

    LibJS: Convert SymbolConstructor functions to ThrowCompletionOr

--- Userland/Libraries/LibJS/Runtime/SymbolConstructor.h
+++ Userland/Libraries/LibJS/Runtime/SymbolConstructor.h
@@ -24,8 +24,8 @@ public:
 private:
     virtual bool has_constructor() const override { return true; }
 
-    JS_DECLARE_OLD_NATIVE_FUNCTION(for_);
-    JS_DECLARE_OLD_NATIVE_FUNCTION(key_for);
+    JS_DECLARE_NATIVE_FUNCTION(for_);
+    JS_DECLARE_NATIVE_FUNCTION(key_for);
 };
 
 }
