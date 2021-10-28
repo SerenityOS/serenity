a5fb3f6855a10b1ebc13a1c7865f67b56ae296ba
Idan Horowitz <idan.horowitz@gmail.com>
2021-10-29 00:55:46 +0300

    LibJS: Convert StringIteratorPrototype functions to ThrowCompletionOr

--- Userland/Libraries/LibJS/Runtime/StringIteratorPrototype.cpp
+++ Userland/Libraries/LibJS/Runtime/StringIteratorPrototype.cpp
@@ -22,7 +22,7 @@ void StringIteratorPrototype::initialize(GlobalObject& global_object)
 {
     auto& vm = this->vm();
     Object::initialize(global_object);
-    define_old_native_function(vm.names.next, next, 0, Attribute::Configurable | Attribute::Writable);
+    define_native_function(vm.names.next, next, 0, Attribute::Configurable | Attribute::Writable);
 
     // 22.1.5.1.2 %StringIteratorPrototype% [ @@toStringTag ], https://tc39.es/ecma262/#sec-%stringiteratorprototype%-@@tostringtag
     define_direct_property(*vm.well_known_symbol_to_string_tag(), js_string(global_object.heap(), "String Iterator"), Attribute::Configurable);
@@ -33,9 +33,9 @@ StringIteratorPrototype::~StringIteratorPrototype()
 }
 
 // 22.1.5.1.1 %StringIteratorPrototype%.next ( ), https://tc39.es/ecma262/#sec-%stringiteratorprototype%.next
-JS_DEFINE_OLD_NATIVE_FUNCTION(StringIteratorPrototype::next)
+JS_DEFINE_NATIVE_FUNCTION(StringIteratorPrototype::next)
 {
-    auto* iterator = TRY_OR_DISCARD(typed_this_value(global_object));
+    auto* iterator = TRY(typed_this_value(global_object));
     if (iterator->done())
         return create_iterator_result_object(global_object, js_undefined(), true);
 
