b6dbf127c1123f6e2df7410f4e8524809798c2e3
Daniel Bertalan <dani@danielbertalan.dev>
2021-10-28 12:43:46 +0200

    Kernel: Don't use the host's `nm` in mkmap.sh
    Not relying on host tools working correctly is generally a good idea.
    
    Especially because we will soon use DT_RELR relocations, which are not
    supported by GNU binutils, in the Clang toolchain. This lets us avoid an
    obtrusive "unknown section" message.

--- Kernel/mkmap.sh
+++ Kernel/mkmap.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 tmp=$(mktemp)
-nm -n Kernel | grep -vE \\.Lubsan_data | awk '{ if ($2 != "a") print; }' | uniq > "$tmp"
+NM="${NM:-nm}"
+"$NM" -n Kernel | grep -vE \\.Lubsan_data | awk '{ if ($2 != "a") print; }' | uniq > "$tmp"
 printf "%08x\n" "$(wc -l "$tmp" | awk '{print $1}')" > kernel.map
 CXXFILT="${CXXFILT:-c++filt}"
 "$CXXFILT" < "$tmp" >> kernel.map
