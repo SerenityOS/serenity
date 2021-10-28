aa450f9a38d56a9abaaec3be9b7dd74acb417605
Idan Horowitz <idan.horowitz@gmail.com>
2021-10-29 00:34:42 +0300

    LibJS: Convert ErrorPrototype functions to ThrowCompletionOr

--- Userland/Libraries/LibJS/Runtime/ErrorPrototype.cpp
+++ Userland/Libraries/LibJS/Runtime/ErrorPrototype.cpp
@@ -26,28 +26,26 @@ void ErrorPrototype::initialize(GlobalObject& global_object)
     u8 attr = Attribute::Writable | Attribute::Configurable;
     define_direct_property(vm.names.name, js_string(vm, "Error"), attr);
     define_direct_property(vm.names.message, js_string(vm, ""), attr);
-    define_old_native_function(vm.names.toString, to_string, 0, attr);
+    define_native_function(vm.names.toString, to_string, 0, attr);
 }
 
 // 20.5.3.4 Error.prototype.toString ( ), https://tc39.es/ecma262/#sec-error.prototype.tostring
-JS_DEFINE_OLD_NATIVE_FUNCTION(ErrorPrototype::to_string)
+JS_DEFINE_NATIVE_FUNCTION(ErrorPrototype::to_string)
 {
     auto this_value = vm.this_value(global_object);
-    if (!this_value.is_object()) {
-        vm.throw_exception<TypeError>(global_object, ErrorType::NotAnObject, this_value.to_string_without_side_effects());
-        return {};
-    }
+    if (!this_value.is_object())
+        return vm.throw_completion<TypeError>(global_object, ErrorType::NotAnObject, this_value.to_string_without_side_effects());
     auto& this_object = this_value.as_object();
 
     String name = "Error";
-    auto name_property = TRY_OR_DISCARD(this_object.get(vm.names.name));
+    auto name_property = TRY(this_object.get(vm.names.name));
     if (!name_property.is_undefined())
-        name = TRY_OR_DISCARD(name_property.to_string(global_object));
+        name = TRY(name_property.to_string(global_object));
 
     String message = "";
-    auto message_property = TRY_OR_DISCARD(this_object.get(vm.names.message));
+    auto message_property = TRY(this_object.get(vm.names.message));
     if (!message_property.is_undefined())
-        message = TRY_OR_DISCARD(message_property.to_string(global_object));
+        message = TRY(message_property.to_string(global_object));
 
     if (name.is_empty())
         return js_string(vm, message);
