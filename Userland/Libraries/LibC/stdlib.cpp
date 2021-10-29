220c77bb58931a9be69e113335a1cf46879daec8
Jelle Raaijmakers <jelle@gmta.nl>
2021-10-30 00:51:36 +0200

    LibC: Add `labs()`
    We defined it in `stdlib.h` but forgot to implement it.

--- Userland/Libraries/LibC/stdlib.cpp
+++ Userland/Libraries/LibC/stdlib.cpp
@@ -725,6 +725,11 @@ int abs(int i)
     return i < 0 ? -i : i;
 }
 
+long int labs(long int i)
+{
+    return i < 0 ? -i : i;
+}
+
 long long int llabs(long long int i)
 {
     return i < 0 ? -i : i;
