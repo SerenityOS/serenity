124fcafa45aabbb439e009e12fcf8202bb0b84b8
Andrew Kaster <akaster@serenityos.org>
2021-10-30 15:55:49 -0600

    AK: Suppress false-positive clang-tidy warning in Assertions.h
    The definition of VERIFY_NOT_REACHED() as `assert(false)` causes the
    tool to suggest converting it to a static_assert. Which doesn't make
    any sense in context for what the macro is trying to do: crash the
    program at runtime.

--- AK/Assertions.h
+++ AK/Assertions.h
@@ -11,7 +11,7 @@
 #else
 #    include <assert.h>
 #    define VERIFY assert
-#    define VERIFY_NOT_REACHED() assert(false)
+#    define VERIFY_NOT_REACHED() assert(false) /* NOLINT(cert-dcl03-c,misc-static-assert) No, this can't be static_assert, it's a runtime check */
 static constexpr bool TODO = false;
 #    define TODO() VERIFY(TODO)
 #endif
