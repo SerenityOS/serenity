28d94ed3dfc41e1250aae90b482d4f865fcbad4e
Ali Mohammad Pur <ali.mpfard@gmail.com>
2021-11-01 01:36:35 +0330

    LibWasm: Implement module validation

---
+++ Userland/Libraries/LibWasm/Forward.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, Ali Mohammad Pur <mpfard@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+namespace Wasm {
+
+class AbstractMachine;
+class Validator;
+struct ValidationError;
+
+}
