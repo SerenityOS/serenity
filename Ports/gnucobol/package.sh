aea2583cdece852b075e952a515a5b538ddbb7a1
Brendan Coles <bcoles@gmail.com>
2021-11-01 08:20:40 +0000

    Ports: gnucobol: Build with ncurses and without NLS

--- Ports/gnucobol/package.sh
+++ Ports/gnucobol/package.sh
@@ -2,10 +2,21 @@
 port=gnucobol
 version=3.1.2
 useconfigure="true"
-depends=("gmp" "gcc" "bash")
+depends=("gmp" "gcc" "bash" "ncurses")
 files="https://ftpmirror.gnu.org/gnu/gnucobol/gnucobol-${version}.tar.bz2 gnucobol-${version}.tar.bz2
 https://ftpmirror.gnu.org/gnu/gnucobol/gnucobol-${version}.tar.bz2.sig gnucobol-${version}.tar.bz2.sig
 https://ftpmirror.gnu.org/gnu/gnu-keyring.gpg gnu-keyring.gpg"
 auth_type="sig"
 auth_opts=("--keyring" "./gnu-keyring.gpg" "gnucobol-${version}.tar.bz2.sig")
-configopts=("--prefix=/usr/local" "--enable-hardening" "--disable-rpath" "--with-gnu-ld" "--with-dl" "--with-math=gmp" "--with-db=no" "--with-json=no" "--with-curses=curses")
+configopts=(
+    "--prefix=/usr/local"
+    "--enable-hardening"
+    "--disable-rpath"
+    "--disable-nls"
+    "--with-gnu-ld"
+    "--with-dl"
+    "--with-math=gmp"
+    "--with-curses=ncurses"
+    "--with-db=no"
+    "--with-json=no"
+)
