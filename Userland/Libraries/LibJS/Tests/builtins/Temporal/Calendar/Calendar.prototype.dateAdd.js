d012a9142118cca749db5eee638cdc11832c90f9
Linus Groh <mail@linusgroh.de>
2021-11-02 00:44:53 +0100

    LibJS: Enable Temporal.Calendar.prototype.dateAdd() test that now passes

--- Userland/Libraries/LibJS/Tests/builtins/Temporal/Calendar/Calendar.prototype.dateAdd.js
+++ Userland/Libraries/LibJS/Tests/builtins/Temporal/Calendar/Calendar.prototype.dateAdd.js
@@ -3,8 +3,7 @@ describe("correct behavior", () => {
         expect(Temporal.Calendar.prototype.dateAdd).toHaveLength(2);
     });
 
-    // Asserts with TODO() since addition of partial balance_duration()
-    test.skip("basic functionality", () => {
+    test("basic functionality", () => {
         const calendar = new Temporal.Calendar("iso8601");
         const plainDate = new Temporal.PlainDate(1970, 1, 1);
         const duration = new Temporal.Duration(1, 2, 3, 4);
