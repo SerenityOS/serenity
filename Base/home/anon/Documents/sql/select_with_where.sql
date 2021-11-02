6309e7a9d63bbc301012e2ec08565b4b1a01433c
Jan de Visser <jan@de-visser.net>
2021-11-02 16:29:56 -0400

    LibSQL: Add SQL files to assist in troubleshooting
    These files contain the same SQL statements as the similarly named tests
    in Tests/LibSQL/TestSqlStatementExecution.cpp test suite. They can be
    fed to the sql utility to assist in troubleshooting failing tests.

---
+++ Base/home/anon/Documents/sql/select_with_where.sql
@@ -0,0 +1,15 @@
+CREATE SCHEMA TestSchema;
+CREATE TABLE TestSchema.TestTable
+(
+    TextColumn text,
+    IntColumn  integer
+);
+INSERT INTO TestSchema.TestTable (TextColumn, IntColumn)
+VALUES ('Test_1', 42),
+       ('Test_3', 44),
+       ('Test_2', 43),
+       ('Test_4', 45),
+       ('Test_5', 46);
+SELECT TextColumn, IntColumn
+FROM TestSchema.TestTable
+WHERE IntColumn > 44;
