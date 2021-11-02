5537334f66cae038ce8e2133347b67f284f8f14d
Brendan Coles <bcoles@gmail.com>
2021-11-02 16:56:01 +0000

    Ports: quake: Do no set PATH environment variable in package.sh

--- Ports/quake/package.sh
+++ Ports/quake/package.sh
@@ -9,6 +9,3 @@ depends=("SDL2")
 launcher_name=Quake
 launcher_category=Games
 launcher_command=quake
-
-# FIXME: Uhh, the things in this directory are not supposed to run on the host, why add this to $PATH?!
-export PATH="${SERENITY_INSTALL_ROOT}/usr/bin:${PATH}"
