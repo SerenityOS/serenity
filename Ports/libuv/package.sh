2a038226afce7994a315e382eca5620eb4cde643
EWouters <6179932+EWouters@users.noreply.github.com>
2021-11-03 15:29:23 +0100

    Ports/libuv: Update libuv to version 1.42.0 and fix cmake argument
    Add CMAKE_BUILD_WITH_INSTALL_RPATH=true to the cmake arguments as it
    wouldn't compile on MacOS without this argument

--- Ports/libuv/package.sh
+++ Ports/libuv/package.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=libuv
-version=b12699b1efabfd241324f4ab6cfd6ce576db491e
+version=1.42.0
 useconfigure=true
-files="https://github.com/libuv/libuv/archive/$version.tar.gz $port-$version.tar.gz bbbfa2bb50437047efc8fb29c243c914ae0de94107d7cc641c2f84e292904eb5"
+files="https://github.com/libuv/libuv/archive/refs/tags/v$version.tar.gz $port-$version.tar.gz 371e5419708f6aaeb8656671f89400b92a9bba6443369af1bb70bcd6e4b3c764"
 auth_type=sha256
-configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt" "-GNinja")
+configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt" "-GNinja" "-DCMAKE_BUILD_WITH_INSTALL_RPATH=true")
 
 configure() {
     run cmake "${configopts[@]}" .
