4ac7fe159784972afa689ab203f970a48ae90a78
Tim Schumacher <timschumi@gmx.de>
2021-11-06 00:13:24 +0100

    Ports: Update pcre2 to 10.39
    PCRE2 has moved to GitHub for code and release hosting, and ftp.pcre.org
    is decommissioned.

--- Ports/pcre2/package.sh
+++ Ports/pcre2/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=pcre2
-version=10.34
+version=10.39
 useconfigure=true
-files="https://ftp.pcre.org/pub/pcre/pcre2-${version}.tar.gz pcre2-${version}.tar.gz
-https://ftp.pcre.org/pub/pcre/pcre2-${version}.tar.gz.sig pcre2-${version}.tar.gz.sig"
+files="https://github.com/PhilipHazel/pcre2/releases/download/pcre2-${version}/pcre2-${version}.tar.gz pcre2-${version}.tar.gz
+https://github.com/PhilipHazel/pcre2/releases/download/pcre2-${version}/pcre2-${version}.tar.gz.sig pcre2-${version}.tar.gz.sig"
 
 auth_type="sig"
 auth_import_key="45F68D54BBE23FB3039B46E59766E084FB0F43D8"
