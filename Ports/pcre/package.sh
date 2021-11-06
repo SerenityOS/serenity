ce9a66686b6052f3c9a2a7d91a61cb2dedacad04
Tim Schumacher <timschumi@gmx.de>
2021-11-06 02:03:53 +0100

    Ports: Update pcre to 8.45

--- Ports/pcre/package.sh
+++ Ports/pcre/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=pcre
-version=8.44
+version=8.45
 useconfigure=true
-files="https://ftp.pcre.org/pub/pcre/pcre-${version}.tar.gz pcre-${version}.tar.gz
-https://ftp.pcre.org/pub/pcre/pcre-${version}.tar.gz.sig pcre-${version}.tar.gz.sig"
+files="https://downloads.sourceforge.net/project/pcre/pcre/${version}/pcre-${version}.tar.gz pcre-${version}.tar.gz
+https://downloads.sourceforge.net/project/pcre/pcre/${version}/pcre-${version}.tar.gz.sig pcre-${version}.tar.gz.sig"
 
 auth_type="sig"
 auth_import_key="45F68D54BBE23FB3039B46E59766E084FB0F43D8"
