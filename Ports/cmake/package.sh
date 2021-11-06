ba5484266ab215a2e71c48e53a0c6e4713183e4e
Tim Schumacher <timschumi@gmx.de>
2021-11-06 11:22:55 +0100

    Ports: Add a missing hyphen to GNinja in cmake

--- Ports/cmake/package.sh
+++ Ports/cmake/package.sh
@@ -5,7 +5,7 @@ useconfigure=true
 files="https://github.com/Kitware/CMake/releases/download/v$version/cmake-$version.tar.gz cmake-$version.tar.gz 7d0232b9f1c57e8de81f38071ef8203e6820fe7eec8ae46a1df125d88dbcc2e1"
 auth_type=sha256
 depends=("bash" "make" "sed" "ncurses" "libuv")
-configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt" "-DCMAKE_USE_SYSTEM_LIBRARY_LIBUV=1" "GNinja")
+configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt" "-DCMAKE_USE_SYSTEM_LIBRARY_LIBUV=1" "-GNinja")
 
 configure() {
     run cmake "${configopts[@]}" .
