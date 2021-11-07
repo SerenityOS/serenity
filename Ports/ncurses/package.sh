f88695759912d10475419bf5600d8ff730033bd4
Tim Schumacher <timschumi@gmx.de>
2021-11-07 02:15:17 +0100

    Ports: Update to ncurses 6.3

--- Ports/ncurses/package.sh
+++ Ports/ncurses/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=ncurses
-version=6.2
+version=6.3
 useconfigure=true
 configopts=(
     "--enable-pc-files"
@@ -12,7 +12,7 @@ configopts=(
     "--without-ada"
     "--enable-widec"
 )
-files="https://ftpmirror.gnu.org/gnu/ncurses/ncurses-${version}.tar.gz ncurses-${version}.tar.gz 30306e0c76e0f9f1f0de987cf1c82a5c21e1ce6568b9227f7da5b71cbea86c9d"
+files="https://invisible-mirror.net/archives/ncurses/ncurses-${version}.tar.gz ncurses-${version}.tar.gz 97fc51ac2b085d4cde31ef4d2c3122c21abc217e9090a43a30fc5ec21684e059"
 auth_type="sha256"
 
 pre_configure() {
