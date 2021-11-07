df2ccb3d38f9e91a3b421f453af8493c33c4287d
Linus Groh <mail@linusgroh.de>
2021-11-07 01:48:08 +0000

    LibJS: Implement Temporal.Duration.prototype.toLocaleString()

--- Userland/Libraries/LibJS/Runtime/Temporal/DurationPrototype.h
+++ Userland/Libraries/LibJS/Runtime/Temporal/DurationPrototype.h
@@ -37,6 +37,7 @@ private:
     JS_DECLARE_NATIVE_FUNCTION(abs);
     JS_DECLARE_NATIVE_FUNCTION(to_string);
     JS_DECLARE_NATIVE_FUNCTION(to_json);
+    JS_DECLARE_NATIVE_FUNCTION(to_locale_string);
     JS_DECLARE_NATIVE_FUNCTION(value_of);
 };
 
