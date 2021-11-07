5c75216361d79b1cf1c842c4ac023d71470b72d0
Tim Schumacher <timschumi@gmx.de>
2021-11-07 02:34:50 +0100

    Ports: Update byacc to 20210808

--- Ports/byacc/package.sh
+++ Ports/byacc/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=byacc
-version=20191125
+version=20210808
 files="https://invisible-mirror.net/archives/byacc/byacc-${version}.tgz byacc-${version}.tgz
 https://invisible-mirror.net/archives/byacc/byacc-${version}.tgz.asc byacc-${version}.tgz.asc"
 useconfigure=true
 auth_type="sig"
-auth_import_key="C52048C0C0748FEE227D47A2702353E0F7E48EDB"
+auth_import_key="19882D92DDA4C400C22C0D56CC2AF4472167BE03"
 auth_opts=("byacc-${version}.tgz.asc" "byacc-${version}.tgz")
