85ea095ba299c3fc88f73345245595c72403227b
faxe1008 <fabianblatz@gmail.com>
2021-11-08 11:43:31 +0100

    LibConfig: Add removal/notifcation for keys

--- Userland/Libraries/LibConfig/Client.h
+++ Userland/Libraries/LibConfig/Client.h
@@ -31,6 +31,7 @@ public:
     void write_string(StringView domain, StringView group, StringView key, StringView value);
     void write_i32(StringView domain, StringView group, StringView key, i32 value);
     void write_bool(StringView domain, StringView group, StringView key, bool value);
+    void remove_key(StringView domain, StringView group, StringView key);
 
     static Client& the();
 
@@ -43,6 +44,7 @@ private:
     void notify_changed_string_value(String const& domain, String const& group, String const& key, String const& value) override;
     void notify_changed_i32_value(String const& domain, String const& group, String const& key, i32 value) override;
     void notify_changed_bool_value(String const& domain, String const& group, String const& key, bool value) override;
+    void notify_removed_key(String const& domain, String const& group, String const& key) override;
 };
 
 inline String read_string(StringView domain, StringView group, StringView key, StringView fallback = {})
@@ -73,6 +75,11 @@ inline void write_i32(StringView domain, StringView group, StringView key, i32 v
 inline void write_bool(StringView domain, StringView group, StringView key, bool value)
 {
     Client::the().write_bool(domain, group, key, value);
+}
+
+inline void remove_key(StringView domain, StringView group, StringView key)
+{
+    Client::the().remove_key(domain, group, key);
 }
 
 inline void pledge_domains(Vector<String> const& domains)
