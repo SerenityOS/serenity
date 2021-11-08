1e3e0477cbb06e7510523d4a24b2bf9f3a400434
Linus Groh <mail@linusgroh.de>
2021-11-08 21:34:16 +0000

    LibJS: Implement Temporal.PlainMonthDay.prototype.with()

--- Userland/Libraries/LibJS/Runtime/Temporal/PlainMonthDayPrototype.h
+++ Userland/Libraries/LibJS/Runtime/Temporal/PlainMonthDayPrototype.h
@@ -23,6 +23,7 @@ private:
     JS_DECLARE_NATIVE_FUNCTION(calendar_getter);
     JS_DECLARE_NATIVE_FUNCTION(month_code_getter);
     JS_DECLARE_NATIVE_FUNCTION(day_getter);
+    JS_DECLARE_NATIVE_FUNCTION(with);
     JS_DECLARE_NATIVE_FUNCTION(equals);
     JS_DECLARE_NATIVE_FUNCTION(to_string);
     JS_DECLARE_NATIVE_FUNCTION(to_locale_string);
