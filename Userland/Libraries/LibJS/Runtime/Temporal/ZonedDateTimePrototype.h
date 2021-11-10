74679a9c0f04c148b3d19501ce0d0495bdf409ac
Luke Wilde <lukew@serenityos.org>
2021-11-10 00:26:33 +0000

    LibJS: Implement Temporal.ZonedDateTime.prototype.toString

--- Userland/Libraries/LibJS/Runtime/Temporal/ZonedDateTimePrototype.h
+++ Userland/Libraries/LibJS/Runtime/Temporal/ZonedDateTimePrototype.h
@@ -54,6 +54,7 @@ private:
     JS_DECLARE_NATIVE_FUNCTION(with_time_zone);
     JS_DECLARE_NATIVE_FUNCTION(with_calendar);
     JS_DECLARE_NATIVE_FUNCTION(equals);
+    JS_DECLARE_NATIVE_FUNCTION(to_string);
     JS_DECLARE_NATIVE_FUNCTION(value_of);
     JS_DECLARE_NATIVE_FUNCTION(start_of_day);
     JS_DECLARE_NATIVE_FUNCTION(to_instant);
