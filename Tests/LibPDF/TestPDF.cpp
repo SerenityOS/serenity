7ff0cd7ace005a7e17c8e4aad31a6c0b431f303b
Simon Woertz <simon@woertz.at>
2021-11-16 23:51:03 +0100

    Tests: Add test cases for #10702 and #10717
    Add test cases for parsing an empty file and a truncated file.

--- Tests/LibPDF/TestPDF.cpp
+++ Tests/LibPDF/TestPDF.cpp
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <AK/Forward.h>
+#include <AK/String.h>
 #include <LibCore/MappedFile.h>
 #include <LibPDF/Document.h>
 #include <LibTest/Macros.h>
@@ -28,4 +30,18 @@ TEST_CASE(complex_pdf)
     auto file = Core::MappedFile::map("complex.pdf").release_value();
     auto document = PDF::Document::create(file->bytes());
     EXPECT_EQ(document->get_page_count(), 3U);
+}
+
+TEST_CASE(empty_file_issue_10702)
+{
+    AK::ReadonlyBytes empty;
+    auto document = PDF::Document::create(empty);
+    EXPECT(document.is_null());
+}
+
+TEST_CASE(truncated_pdf_header_issue_10717)
+{
+    AK::String string { "%PDF-2.11%" };
+    auto document = PDF::Document::create(string.bytes());
+    EXPECT(document.is_null());
 }
