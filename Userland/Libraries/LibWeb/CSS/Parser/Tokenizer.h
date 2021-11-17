fdb5ad8ea21857d2fd030cbc361ec08be776cc88
Sam Atkins <atkinssj@serenityos.org>
2021-11-17 21:01:51 +0000

    LibWeb: Convert numeric tokens to numbers in CSS Tokenizer
    The spec wants us to produce numeric values as the Tokenizer sees them,
    rather than waiting until the parse stage. This is a first step towards
    that.

--- Userland/Libraries/LibWeb/CSS/Parser/Tokenizer.h
+++ Userland/Libraries/LibWeb/CSS/Parser/Tokenizer.h
@@ -59,7 +59,8 @@ public:
 
 class CSSNumber {
 public:
-    String value;
+    String string;
+    double value { 0 };
     Token::NumberType type {};
 };
 
@@ -86,6 +87,7 @@ private:
     [[nodiscard]] Token consume_a_numeric_token();
     [[nodiscard]] Token consume_an_ident_like_token();
     [[nodiscard]] CSSNumber consume_a_number();
+    [[nodiscard]] double convert_a_string_to_a_number(StringView);
     [[nodiscard]] String consume_a_name();
     [[nodiscard]] u32 consume_escaped_code_point();
     [[nodiscard]] Token consume_a_url_token();
