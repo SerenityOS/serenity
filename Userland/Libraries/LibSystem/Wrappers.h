3d34216b9a368f0c7ca66ab4d405cffdd62cee15
Andreas Kling <kling@serenityos.org>
2021-11-22 16:11:03 +0100

    LibSystem: Add ErrorOr<T> wrapper for sigaction() :^)

--- Userland/Libraries/LibSystem/Wrappers.h
+++ Userland/Libraries/LibSystem/Wrappers.h
@@ -7,10 +7,12 @@
 #pragma once
 
 #include <AK/Error.h>
+#include <signal.h>
 
 namespace System {
 
 ErrorOr<void> pledge(StringView promises, StringView execpromises);
 ErrorOr<void> unveil(StringView path, StringView permissions);
+ErrorOr<void> sigaction(int signal, struct sigaction const* action, struct sigaction* old_action);
 
 }
