a453ddf890c3142418b929eee74fb5731ea0d886
Andreas Kling <kling@serenityos.org>
2021-11-23 15:13:16 +0100

    utmpupdate: Port to LibMain :^)

--- Userland/Utilities/utmpupdate.cpp
+++ Userland/Utilities/utmpupdate.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2020-2021, Andreas Kling <kling@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -11,21 +11,15 @@
 #include <LibCore/ArgsParser.h>
 #include <LibCore/DateTime.h>
 #include <LibCore/File.h>
+#include <LibCore/System.h>
+#include <LibMain/Main.h>
 #include <unistd.h>
 
-int main(int argc, char** argv)
+ErrorOr<int> serenity_main(Main::Arguments arguments)
 {
-    if (pledge("stdio wpath cpath", nullptr) < 0) {
-        perror("pledge");
-        return 1;
-    }
-
-    if (unveil("/var/run/utmp", "rwc") < 0) {
-        perror("unveil");
-        return 1;
-    }
-
-    unveil(nullptr, nullptr);
+    TRY(Core::System::pledge("stdio wpath cpath", nullptr));
+    TRY(Core::System::unveil("/var/run/utmp", "rwc"));
+    TRY(Core::System::unveil(nullptr, nullptr));
 
     pid_t pid = 0;
     bool flag_create = false;
@@ -39,8 +33,7 @@ int main(int argc, char** argv)
     args_parser.add_option(pid, "PID", "PID", 'p', "PID");
     args_parser.add_option(from, "From", "from", 'f', "From");
     args_parser.add_positional_argument(tty_name, "TTY name", "tty");
-
-    args_parser.parse(argc, argv);
+    args_parser.parse(arguments);
 
     if (flag_create && flag_delete) {
         warnln("-c and -d are mutually exclusive");
@@ -49,24 +42,18 @@ int main(int argc, char** argv)
 
     dbgln("Updating utmp from UID={} GID={} EGID={} PID={}", getuid(), getgid(), getegid(), pid);
 
-    auto file_or_error = Core::File::open("/var/run/utmp", Core::OpenMode::ReadWrite);
-    if (file_or_error.is_error()) {
-        dbgln("Error: {}", file_or_error.error());
-        return 1;
-    }
+    auto file = TRY(Core::File::open("/var/run/utmp", Core::OpenMode::ReadWrite));
 
-    auto& file = *file_or_error.value();
-
-    auto file_contents = file.read_all();
-    auto previous_json = JsonValue::from_string(file_contents);
+    auto file_contents = file->read_all();
+    auto previous_json = TRY(JsonValue::from_string(file_contents));
 
     JsonObject json;
 
     if (!file_contents.is_empty()) {
-        if (previous_json.is_error() || !previous_json.value().is_object()) {
+        if (!previous_json.is_object()) {
             dbgln("Error: Could not parse JSON");
         } else {
-            json = previous_json.value().as_object();
+            json = previous_json.as_object();
         }
     }
 
@@ -83,17 +70,17 @@ int main(int argc, char** argv)
         json.remove(tty_name);
     }
 
-    if (!file.seek(0)) {
+    if (!file->seek(0)) {
         dbgln("Seek failed");
         return 1;
     }
 
-    if (!file.truncate(0)) {
+    if (!file->truncate(0)) {
         dbgln("Truncation failed");
         return 1;
     }
 
-    if (!file.write(json.to_string())) {
+    if (!file->write(json.to_string())) {
         dbgln("Write failed");
         return 1;
     }
