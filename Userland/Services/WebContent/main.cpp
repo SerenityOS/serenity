32e05f9b143e50412469251adce168ab4edf7841
Andreas Kling <kling@serenityos.org>
2021-11-23 10:31:08 +0100

    WebContent: Port to LibMain :^)

--- Userland/Services/WebContent/main.cpp
+++ Userland/Services/WebContent/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2020-2021, Andreas Kling <kling@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -7,35 +7,19 @@
 #include <LibCore/EventLoop.h>
 #include <LibCore/LocalServer.h>
 #include <LibIPC/ClientConnection.h>
+#include <LibMain/Main.h>
+#include <LibSystem/Wrappers.h>
 #include <WebContent/ClientConnection.h>
 
-int main(int, char**)
+ErrorOr<int> serenity_main(Main::Arguments)
 {
     Core::EventLoop event_loop;
-    if (pledge("stdio recvfd sendfd accept unix rpath", nullptr) < 0) {
-        perror("pledge");
-        return 1;
-    }
-    if (unveil("/res", "r") < 0) {
-        perror("unveil");
-        return 1;
-    }
-    if (unveil("/tmp/portal/request", "rw") < 0) {
-        perror("unveil");
-        return 1;
-    }
-    if (unveil("/tmp/portal/image", "rw") < 0) {
-        perror("unveil");
-        return 1;
-    }
-    if (unveil("/tmp/portal/websocket", "rw") < 0) {
-        perror("unveil");
-        return 1;
-    }
-    if (unveil(nullptr, nullptr) < 0) {
-        perror("unveil");
-        return 1;
-    }
+    TRY(System::pledge("stdio recvfd sendfd accept unix rpath", nullptr));
+    TRY(System::unveil("/res", "r"));
+    TRY(System::unveil("/tmp/portal/request", "rw"));
+    TRY(System::unveil("/tmp/portal/image", "rw"));
+    TRY(System::unveil("/tmp/portal/websocket", "rw"));
+    TRY(System::unveil(nullptr, nullptr));
 
     auto socket = Core::LocalSocket::take_over_accepted_socket_from_system_server();
     VERIFY(socket);
