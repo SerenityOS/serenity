0aabf40b1819d7a7883ca099e457e532c9ce9a30
Andreas Kling <kling@serenityos.org>
2021-11-23 15:30:59 +0100

    LookupServer: Port to LibMain :^)

--- Userland/Services/LookupServer/main.cpp
+++ Userland/Services/LookupServer/main.cpp
@@ -7,38 +7,18 @@
 #include "LookupServer.h"
 #include <LibCore/EventLoop.h>
 #include <LibCore/LocalServer.h>
-#include <stdio.h>
-#include <unistd.h>
+#include <LibCore/System.h>
+#include <LibMain/Main.h>
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
+ErrorOr<int> serenity_main(Main::Arguments)
 {
-    if (pledge("stdio accept unix inet rpath", nullptr) < 0) {
-        perror("pledge");
-        return 1;
-    }
-
+    TRY(Core::System::pledge("stdio accept unix inet rpath", nullptr));
     Core::EventLoop event_loop;
-    auto server = LookupServer::LookupServer::construct();
+    auto server = TRY(LookupServer::LookupServer::try_create());
 
-    if (pledge("stdio accept inet rpath", nullptr) < 0) {
-        perror("pledge");
-        return 1;
-    }
-
-    if (unveil("/proc/net/adapters", "r") < 0) {
-        perror("unveil");
-        return 1;
-    }
-
-    if (unveil("/etc/hosts", "r") < 0) {
-        perror("unveil");
-        return 1;
-    }
-
-    if (unveil(nullptr, nullptr) < 0) {
-        perror("unveil");
-        return 1;
-    }
-
+    TRY(Core::System::pledge("stdio accept inet rpath", nullptr));
+    TRY(Core::System::unveil("/proc/net/adapters", "r"));
+    TRY(Core::System::unveil("/etc/hosts", "r"));
+    TRY(Core::System::unveil(nullptr, nullptr));
     return event_loop.exec();
 }
