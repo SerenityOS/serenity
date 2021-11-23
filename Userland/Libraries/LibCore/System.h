4bf08e4d529a9181484825c73a75c2e10d150976
Andreas Kling <kling@serenityos.org>
2021-11-23 12:05:56 +0100

    LibCore: Add syscall wrapper for close()

--- Userland/Libraries/LibCore/System.h
+++ Userland/Libraries/LibCore/System.h
@@ -23,5 +23,6 @@ ErrorOr<int> fcntl(int fd, int command, ...);
 ErrorOr<void*> mmap(void* address, size_t, int protection, int flags, int fd, off_t, size_t alignment = 0, StringView name = {});
 ErrorOr<void> munmap(void* address, size_t);
 ErrorOr<int> open(StringView path, int options, ...);
+ErrorOr<void> close(int fd);
 
 }
