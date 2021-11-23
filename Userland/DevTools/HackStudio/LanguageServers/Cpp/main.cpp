39f283609597d5f35645adadc082ff1e6b9d0f0b
Andreas Kling <kling@serenityos.org>
2021-11-23 10:36:29 +0100

    LanguageServers/Cpp: Port to LibMain :^)

--- Userland/DevTools/HackStudio/LanguageServers/Cpp/main.cpp
+++ Userland/DevTools/HackStudio/LanguageServers/Cpp/main.cpp
@@ -9,48 +9,38 @@
 #include <AK/LexicalPath.h>
 #include <LibCore/ArgsParser.h>
 #include <LibCore/EventLoop.h>
-#include <LibCore/File.h>
 #include <LibCore/LocalServer.h>
 #include <LibIPC/ClientConnection.h>
-#include <sys/stat.h>
+#include <LibMain/Main.h>
+#include <LibSystem/Wrappers.h>
 #include <unistd.h>
 
-static int mode_server();
+static ErrorOr<int> mode_server();
 
-int main(int argc, char** argv)
+ErrorOr<int> serenity_main(Main::Arguments arguments)
 {
     bool tests = false;
 
     Core::ArgsParser parser;
     parser.add_option(tests, "Run tests", "tests", 't');
-    parser.parse(argc, argv);
+    parser.parse(arguments);
 
-    if (tests) {
+    if (tests)
         return run_tests();
-    }
 
     return mode_server();
 }
 
-int mode_server()
+ErrorOr<int> mode_server()
 {
     Core::EventLoop event_loop;
-    if (pledge("stdio unix recvfd rpath ", nullptr) < 0) {
-        perror("pledge");
-        return 1;
-    }
+    TRY(System::pledge("stdio unix recvfd rpath ", nullptr));
 
     auto socket = Core::LocalSocket::take_over_accepted_socket_from_system_server();
     IPC::new_client_connection<LanguageServers::Cpp::ClientConnection>(socket.release_nonnull(), 1);
-    if (pledge("stdio recvfd rpath", nullptr) < 0) {
-        perror("pledge");
-        return 1;
-    }
 
-    if (unveil("/usr/include", "r") < 0) {
-        perror("unveil");
-        return 1;
-    }
+    TRY(System::pledge("stdio recvfd rpath", nullptr));
+    TRY(System::unveil("/usr/include", "r"));
 
     // unveil will be sealed later, when we know the project's root path.
     return event_loop.exec();
