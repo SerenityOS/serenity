af0977c97faefa7dd138010a90b8a94a412699c6
Linus Groh <mail@linusgroh.de>
2021-11-23 20:46:24 +0000

    LibJS: Update to_largest_temporal_unit() to allow undefined fallback
    This is required for Temporal.Duration.prototype.round(). Subsequently,
    this now returns ThrowCompletionOr<Optional<String>>, which brings the
    signature in line with that of to_smallest_temporal_unit().
    Much nicer! :^)

--- Userland/Libraries/LibJS/Runtime/Temporal/CalendarPrototype.cpp
+++ Userland/Libraries/LibJS/Runtime/Temporal/CalendarPrototype.cpp
@@ -210,7 +210,7 @@ JS_DEFINE_NATIVE_FUNCTION(CalendarPrototype::date_until)
     auto largest_unit = TRY(to_largest_temporal_unit(global_object, *options, { "hour"sv, "minute"sv, "second"sv, "millisecond"sv, "microsecond"sv, "nanosecond"sv }, "auto"sv, "day"sv));
 
     // 8. Let result be ! DifferenceISODate(one.[[ISOYear]], one.[[ISOMonth]], one.[[ISODay]], two.[[ISOYear]], two.[[ISOMonth]], two.[[ISODay]], largestUnit).
-    auto result = difference_iso_date(global_object, one->iso_year(), one->iso_month(), one->iso_day(), two->iso_year(), two->iso_month(), two->iso_day(), largest_unit);
+    auto result = difference_iso_date(global_object, one->iso_year(), one->iso_month(), one->iso_day(), two->iso_year(), two->iso_month(), two->iso_day(), *largest_unit);
 
     // 9. Return ? CreateTemporalDuration(result.[[Years]], result.[[Months]], result.[[Weeks]], result.[[Days]], 0, 0, 0, 0, 0, 0).
     return TRY(create_temporal_duration(global_object, result.years, result.months, result.weeks, result.days, 0, 0, 0, 0, 0, 0));
