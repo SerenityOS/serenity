51eedc2d92dfac808ff663096cc25ed22fec76f4
Andreas Kling <kling@serenityos.org>
2021-11-23 15:44:17 +0100

    dmesg: Port to LibMain :^)

--- Userland/Utilities/dmesg.cpp
+++ Userland/Utilities/dmesg.cpp
@@ -1,33 +1,21 @@
 /*
- * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2018-2021, Andreas Kling <kling@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #include <LibCore/File.h>
-#include <stdio.h>
-#include <unistd.h>
+#include <LibCore/System.h>
+#include <LibMain/Main.h>
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
+ErrorOr<int> serenity_main(Main::Arguments)
 {
-    if (pledge("stdio rpath", nullptr) < 0) {
-        perror("pledge");
-        return 1;
-    }
+    TRY(Core::System::pledge("stdio rpath", nullptr));
+    TRY(Core::System::unveil("/proc/dmesg", "r"));
+    TRY(Core::System::unveil(nullptr, nullptr));
 
-    if (unveil("/proc/dmesg", "r") < 0) {
-        perror("unveil");
-        return 1;
-    }
-
-    unveil(nullptr, nullptr);
-
-    auto file = Core::File::construct("/proc/dmesg");
-    if (!file->open(Core::OpenMode::ReadOnly)) {
-        warnln("Failed to open {}: {}", file->name(), file->error_string());
-        return 1;
-    }
+    auto file = TRY(Core::File::open("/proc/dmesg", Core::OpenMode::ReadOnly));
     auto buffer = file->read_all();
-    out("{}", String::copy(buffer));
+    out("{}", StringView { buffer });
     return 0;
 }
