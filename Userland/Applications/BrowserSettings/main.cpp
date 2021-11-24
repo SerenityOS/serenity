b47531599e397df0cffc915ea05db10edf862da0
Sam Atkins <atkinssj@serenityos.org>
2021-11-24 16:47:39 +0000

    BrowserSettings: Create a BrowserSettings application :^)
    Browser has a bunch of settings, but most are non-trivial to add here.
    So far, these are implemented:
    
    - Homepage URL
    - Whether to close download windows when they complete
    
    The others will be added in subsequent commits.

---
+++ Userland/Applications/BrowserSettings/main.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2021, Sam Atkins <atkinssj@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include "BrowserSettingsWidget.h"
+#include <LibConfig/Client.h>
+#include <LibCore/System.h>
+#include <LibGUI/Application.h>
+#include <LibGUI/Icon.h>
+#include <LibGUI/SettingsWindow.h>
+#include <LibMain/Main.h>
+
+ErrorOr<int> serenity_main(Main::Arguments arguments)
+{
+    TRY(Core::System::pledge("stdio rpath recvfd sendfd unix", nullptr));
+    auto app = TRY(GUI::Application::try_create(arguments));
+    Config::pledge_domains("Browser");
+
+    TRY(Core::System::unveil("/res", "r"));
+    TRY(Core::System::unveil(nullptr, nullptr));
+
+    auto app_icon = GUI::Icon::default_icon("app-browser");
+
+    auto window = TRY(GUI::SettingsWindow::try_create("Browser Settings"));
+    window->set_icon(app_icon.bitmap_for_size(16));
+    window->add_tab<BrowserSettingsWidget>("Browser");
+
+    window->show();
+    return app->exec();
+}
