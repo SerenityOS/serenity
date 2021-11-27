6b862d506359e5eff6cc155ab18350c62c7ab06c
Andreas Kling <kling@serenityos.org>
2021-11-27 17:35:00 +0100

    TerminalSettings: Port to LibMain :^)

--- Userland/Applications/TerminalSettings/main.cpp
+++ Userland/Applications/TerminalSettings/main.cpp
@@ -5,51 +5,30 @@
  */
 
 #include "TerminalSettingsWidget.h"
+#include <LibCore/System.h>
 #include <LibGUI/Application.h>
 #include <LibGUI/SettingsWindow.h>
 #include <LibGUI/WindowServerConnection.h>
+#include <LibMain/Main.h>
 
 // Including this after to avoid LibIPC errors
 #include <LibConfig/Client.h>
 
-int main(int argc, char** argv)
+ErrorOr<int> serenity_main(Main::Arguments arguments)
 {
-    if (pledge("stdio rpath cpath wpath recvfd sendfd unix proc exec", nullptr) < 0) {
-        perror("pledge");
-        return 1;
-    }
-
-    auto app = GUI::Application::construct(argc, argv);
+    TRY(Core::System::pledge("stdio rpath cpath wpath recvfd sendfd unix proc exec", nullptr));
+    auto app = TRY(GUI::Application::try_create(arguments));
     Config::pledge_domains("Terminal");
 
-    if (pledge("stdio rpath cpath wpath recvfd sendfd proc exec", nullptr) < 0) {
-        perror("pledge");
-        return 1;
-    }
-
-    if (unveil("/res", "r") < 0) {
-        perror("unveil");
-        return 1;
-    }
-
-    if (unveil("/bin/keymap", "x") < 0) {
-        perror("unveil");
-        return 1;
-    }
-
-    if (unveil("/proc/keymap", "r") < 0) {
-        perror("unveil");
-        return 1;
-    }
-
-    if (unveil(nullptr, nullptr)) {
-        perror("unveil");
-        return 1;
-    }
+    TRY(Core::System::pledge("stdio rpath cpath wpath recvfd sendfd proc exec", nullptr));
+    TRY(Core::System::unveil("/res", "r"));
+    TRY(Core::System::unveil("/bin/keymap", "x"));
+    TRY(Core::System::unveil("/proc/keymap", "r"));
+    TRY(Core::System::unveil(nullptr, nullptr));
 
     auto app_icon = GUI::Icon::default_icon("app-terminal");
 
-    auto window = GUI::SettingsWindow::construct("Terminal Settings");
+    auto window = TRY(GUI::SettingsWindow::try_create("Terminal Settings"));
     window->set_icon(app_icon.bitmap_for_size(16));
     window->add_tab<TerminalSettingsMainWidget>("Terminal");
     window->add_tab<TerminalSettingsViewWidget>("View");
