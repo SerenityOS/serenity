b2f3b7dda0cc2b7455c7e0587d34f2ba55d967d3
Andreas Kling <kling@serenityos.org>
2021-11-27 17:37:00 +0100

    LibGUI: Make GUI::SettingsWindow::add_tab() return ErrorOr
    This allows us to use TRY() when creating settings UI.

--- Userland/Applications/BrowserSettings/main.cpp
+++ Userland/Applications/BrowserSettings/main.cpp
@@ -26,7 +26,7 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
 
     auto window = TRY(GUI::SettingsWindow::try_create("Browser Settings", GUI::SettingsWindow::ShowDefaultsButton::Yes));
     window->set_icon(app_icon.bitmap_for_size(16));
-    window->add_tab<BrowserSettingsWidget>("Browser");
+    TRY(window->add_tab<BrowserSettingsWidget>("Browser"));
 
     window->show();
     return app->exec();
