56c4f0aff57068383130b494a66c2c3d07238228
Andreas Kling <kling@serenityos.org>
2021-11-28 08:24:37 +0100

    LibGUI: Add fallible variants of the GUI::Layout add/insert APIs

--- Userland/Libraries/LibGUI/Layout.cpp
+++ Userland/Libraries/LibGUI/Layout.cpp
@@ -54,18 +54,28 @@ void Layout::notify_disowned(Badge<Widget>, Widget& widget)
     m_owner.clear();
 }
 
-void Layout::add_entry(Entry&& entry)
+ErrorOr<void> Layout::try_add_entry(Entry&& entry)
 {
-    m_entries.append(move(entry));
+    TRY(m_entries.try_append(move(entry)));
     if (m_owner)
         m_owner->notify_layout_changed({});
+    return {};
+}
+
+void Layout::add_entry(Entry&& entry)
+{
+    MUST(try_add_entry(move(entry)));
+}
+
+ErrorOr<void> Layout::try_add_spacer()
+{
+    TRY(try_add_entry(Entry { .type = Entry::Type::Spacer }));
+    return {};
 }
 
 void Layout::add_spacer()
 {
-    Entry entry;
-    entry.type = Entry::Type::Spacer;
-    add_entry(move(entry));
+    MUST(try_add_spacer());
 }
 
 void Layout::add_layout(OwnPtr<Layout>&& layout)
@@ -76,24 +86,36 @@ void Layout::add_layout(OwnPtr<Layout>&& layout)
     add_entry(move(entry));
 }
 
+ErrorOr<void> Layout::try_add_widget(Widget& widget)
+{
+    TRY(try_add_entry(Entry {
+        .type = Entry::Type::Widget,
+        .widget = widget,
+    }));
+    return {};
+}
+
 void Layout::add_widget(Widget& widget)
+{
+    MUST(try_add_widget(widget));
+}
+
+ErrorOr<void> Layout::try_insert_widget_before(Widget& widget, Widget& before_widget)
 {
     Entry entry;
     entry.type = Entry::Type::Widget;
     entry.widget = widget;
-    add_entry(move(entry));
+    TRY(m_entries.try_insert_before_matching(move(entry), [&](auto& existing_entry) {
+        return existing_entry.type == Entry::Type::Widget && existing_entry.widget.ptr() == &before_widget;
+    }));
+    if (m_owner)
+        m_owner->notify_layout_changed({});
+    return {};
 }
 
 void Layout::insert_widget_before(Widget& widget, Widget& before_widget)
 {
-    Entry entry;
-    entry.type = Entry::Type::Widget;
-    entry.widget = widget;
-    m_entries.insert_before_matching(move(entry), [&](auto& existing_entry) {
-        return existing_entry.type == Entry::Type::Widget && existing_entry.widget.ptr() == &before_widget;
-    });
-    if (m_owner)
-        m_owner->notify_layout_changed({});
+    MUST(try_insert_widget_before(widget, before_widget));
 }
 
 void Layout::remove_widget(Widget& widget)
