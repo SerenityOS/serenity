1c8663d898feb64fd8dca9f7265a944c3b63f110
James Mintram <me@jamesrm.com>
2021-12-01 17:11:10 +0000

    Kernel: Add an x86 include check+error in x86/CPU.h

--- Kernel/Arch/x86/CPU.h
+++ Kernel/Arch/x86/CPU.h
@@ -12,6 +12,9 @@
 
 #include <Kernel/Arch/x86/DescriptorTable.h>
 
+#include <AK/Platform.h>
+VALIDATE_IS_X86()
+
 /* Map IRQ0-15 @ ISR 0x50-0x5F */
 #define IRQ_VECTOR_BASE 0x50
 #define GENERIC_INTERRUPT_HANDLERS_COUNT (256 - IRQ_VECTOR_BASE)
