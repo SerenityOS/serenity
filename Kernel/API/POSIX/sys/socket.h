5e749666a784946830ef3586bc30e0ab3e2363c5
Idan Horowitz <idan.horowitz@gmail.com>
2021-12-02 01:07:29 +0200

    Kernel: Add the IPPROTO_{IGMP, IPIP, RAW} protocol macros
    These are not implemented currently, but are required for some ports to
    compile.

--- Kernel/API/POSIX/sys/socket.h
+++ Kernel/API/POSIX/sys/socket.h
@@ -43,10 +43,13 @@ extern "C" {
 
 #define IPPROTO_IP 0
 #define IPPROTO_ICMP 1
+#define IPPROTO_IGMP 2
+#define IPPROTO_IPIP 4
 #define IPPROTO_TCP 6
 #define IPPROTO_UDP 17
 #define IPPROTO_IPV6 41
 #define IPPROTO_ICMPV6 58
+#define IPPROTO_RAW 255
 
 #define MSG_TRUNC 0x1
 #define MSG_CTRUNC 0x2
