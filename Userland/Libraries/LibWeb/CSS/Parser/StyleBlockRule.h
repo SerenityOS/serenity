fa7f71d349db5f807ee166a3e06ee887cc1b94f9
Sam Atkins <atkinssj@serenityos.org>
2021-12-03 12:12:44 +0000

    LibWeb: Make StyleBlockRule more accessible to outsiders
    For our naive var() implementation, we need to be able to create
    StyleBlockRules outside of the Parser, and these changes make that
    possible.

--- Userland/Libraries/LibWeb/CSS/Parser/StyleBlockRule.h
+++ Userland/Libraries/LibWeb/CSS/Parser/StyleBlockRule.h
@@ -19,11 +19,18 @@ class StyleBlockRule : public RefCounted<StyleBlockRule> {
 
 public:
     StyleBlockRule();
+    explicit StyleBlockRule(Token token, Vector<StyleComponentValueRule>&& values)
+        : m_token(token)
+        , m_values(move(values))
+    {
+    }
     ~StyleBlockRule();
 
     bool is_curly() const { return m_token.is(Token::Type::OpenCurly); }
     bool is_paren() const { return m_token.is(Token::Type::OpenParen); }
     bool is_square() const { return m_token.is(Token::Type::OpenSquare); }
+
+    Token const& token() const { return m_token; }
 
     Vector<StyleComponentValueRule> const& values() const { return m_values; }
 
