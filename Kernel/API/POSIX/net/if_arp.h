edbf33367e3683e2bfe18a806fd6f22cee68be8b
Idan Horowitz <idan.horowitz@gmail.com>
2021-12-05 01:36:04 +0200

    Kernel: Define and return the ARPHRD_* device type in SIOCGIFHWADDR
    The sa_family field in SIOCGIFHWADDR specifies the underlying network
    interface's device type, this is hardcoded to generic "Ethernet" right
    now, as we don't have a nice way to query it.

--- Kernel/API/POSIX/net/if_arp.h
+++ Kernel/API/POSIX/net/if_arp.h
@@ -21,6 +21,14 @@ struct arpreq {
     char arp_dev[16];
 };
 
+#define ARPHRD_ETHER 1
+#define ARPHRD_IEEE802 6
+#define ARPHRD_SLIP 256
+#define ARPHRD_PPP 512
+#define ARPHRD_LOOPBACK 772
+#define ARPHRD_FDDI 774
+#define ARPHRD_IEEE802_TR 800
+
 #ifdef __cplusplus
 }
 #endif
