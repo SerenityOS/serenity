9d2b0fd3105f8bb088e3089e4491f2dcf1c29c90
Idan Horowitz <idan.horowitz@gmail.com>
2021-12-05 01:37:56 +0200

    LibC: Add the netinet/if_ether.h header and define ETH_ALEN
    This header defines various constants related to ethernet packets,
    with ETH_ALEN being the length of an ethernet address (which is needed
    by some ports).

---
+++ Userland/Libraries/LibC/netinet/if_ether.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2021, Idan Horowitz <idan.horowitz@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#define ETH_ALEN 6 // Length of an ethernet address (MAC) in bytes
