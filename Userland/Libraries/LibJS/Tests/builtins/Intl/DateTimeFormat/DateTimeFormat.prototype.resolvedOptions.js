26f966619139bedc56f1a6939f832e6936647df6
Timothy Flynn <trflynn89@pm.me>
2021-12-07 08:45:55 -0500

    LibJS: Do not override hour, minute, and second format field lengths
    This was an oversight in e42d954743056e476bbb1623cfc60e7797a8a6ca.
    
    These fields should always follow the locale preference in the CLDR.
    Overriding these fields would permit formats like "h:mm:ss" to result in
    strings like "1:2:3" instead of "1:02:03".

--- Userland/Libraries/LibJS/Tests/builtins/Intl/DateTimeFormat/DateTimeFormat.prototype.resolvedOptions.js
+++ Userland/Libraries/LibJS/Tests/builtins/Intl/DateTimeFormat/DateTimeFormat.prototype.resolvedOptions.js
@@ -165,14 +165,14 @@ describe("correct behavior", () => {
     test("minute", () => {
         ["2-digit", "numeric"].forEach(minute => {
             const en = new Intl.DateTimeFormat("en", { minute: minute });
-            expect(en.resolvedOptions().minute).toBe(minute);
+            expect(en.resolvedOptions().minute).toBe("2-digit");
         });
     });
 
     test("second", () => {
         ["2-digit", "numeric"].forEach(second => {
             const en = new Intl.DateTimeFormat("en", { second: second });
-            expect(en.resolvedOptions().second).toBe(second);
+            expect(en.resolvedOptions().second).toBe("2-digit");
         });
     });
 
