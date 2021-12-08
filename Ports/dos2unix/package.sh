78252d7777eb2570ecedac1466a8d5236ebade29
Andreas Kling <kling@serenityos.org>
2021-12-08 10:48:39 +0100

    Ports: Add SHA256 checksum to dos2unix port

--- Ports/dos2unix/package.sh
+++ Ports/dos2unix/package.sh
@@ -2,5 +2,6 @@
 port=dos2unix
 version=7.4.2
 workdir="${port}-${version}"
-files="https://waterlan.home.xs4all.nl/dos2unix/dos2unix-${version}.tar.gz ${port}-${version}.tar.gz"
+files="https://waterlan.home.xs4all.nl/dos2unix/dos2unix-${version}.tar.gz ${port}-${version}.tar.gz 6035c58df6ea2832e868b599dfa0d60ad41ca3ecc8aa27822c4b7a9789d3ae01"
 depends=("gettext")
+auth_type=sha256
