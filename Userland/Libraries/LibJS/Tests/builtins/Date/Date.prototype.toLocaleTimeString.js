9dfb160c8c2602e1cf8fcc7a3b3947495d9bdfce
Timothy Flynn <trflynn89@pm.me>
2021-12-09 22:57:25 -0500

    LibJS: Implement ECMA-402 Date.prototype.toLocaleTimeString

---
+++ Userland/Libraries/LibJS/Tests/builtins/Date/Date.prototype.toLocaleTimeString.js
@@ -0,0 +1,63 @@
+describe("errors", () => {
+    test("called on non-Date object", () => {
+        expect(() => {
+            Date.prototype.toLocaleTimeString();
+        }).toThrowWithMessage(TypeError, "Not an object of type Date");
+    });
+
+    test("called with value that cannot be converted to a number", () => {
+        expect(() => {
+            new Date(Symbol.hasInstance).toLocaleTimeString();
+        }).toThrowWithMessage(TypeError, "Cannot convert symbol to number");
+
+        expect(() => {
+            new Date(1n).toLocaleTimeString();
+        }).toThrowWithMessage(TypeError, "Cannot convert BigInt to number");
+    });
+
+    test("time value cannot be clipped", () => {
+        expect(() => {
+            new Date(-8.65e15).toLocaleTimeString();
+        }).toThrowWithMessage(RangeError, "Time value must be between -8.64E15 and 8.64E15");
+    });
+
+    test("dateStyle may not be specified", () => {
+        expect(() => {
+            new Date().toLocaleTimeString([], { dateStyle: "short" });
+        }).toThrowWithMessage(TypeError, "Option dateStyle cannot be set when also providing time");
+    });
+});
+
+describe("correct behavior", () => {
+    test("NaN", () => {
+        const d = new Date(NaN);
+        expect(d.toLocaleTimeString()).toBe("Invalid Date");
+    });
+
+    const d0 = new Date(Date.UTC(2021, 11, 7, 17, 40, 50, 456));
+    const d1 = new Date(Date.UTC(1989, 0, 23, 7, 8, 9, 45));
+
+    test("defaults to time", () => {
+        expect(d0.toLocaleTimeString("en", { timeZone: "UTC" })).toBe("5:40:50 PM");
+        expect(d1.toLocaleTimeString("en", { timeZone: "UTC" })).toBe("7:08:09 AM");
+
+        expect(d0.toLocaleTimeString("ar", { timeZone: "UTC" })).toBe("٥:٤٠:٥٠ م");
+        expect(d1.toLocaleTimeString("ar", { timeZone: "UTC" })).toBe("٧:٠٨:٠٩ ص");
+    });
+
+    test("timeStyle may be set", () => {
+        expect(d0.toLocaleTimeString("en", { timeStyle: "long", timeZone: "UTC" })).toBe(
+            "5:40:50 PM UTC"
+        );
+        expect(d1.toLocaleTimeString("en", { timeStyle: "long", timeZone: "UTC" })).toBe(
+            "7:08:09 AM UTC"
+        );
+
+        expect(d0.toLocaleTimeString("ar", { timeStyle: "long", timeZone: "UTC" })).toBe(
+            "٥:٤٠:٥٠ م UTC"
+        );
+        expect(d1.toLocaleTimeString("ar", { timeStyle: "long", timeZone: "UTC" })).toBe(
+            "٧:٠٨:٠٩ ص UTC"
+        );
+    });
+});
