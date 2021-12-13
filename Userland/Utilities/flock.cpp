18dfabbf701a9f1f8e9a825800f83026237f193a
Kenneth Myhra <kennethmyhra@gmail.com>
2021-12-13 18:45:02 +0100

    flock: Port to LibMain

--- Userland/Utilities/flock.cpp
+++ Userland/Utilities/flock.cpp
@@ -5,30 +5,21 @@
  */
 
 #include <AK/Format.h>
-#include <errno.h>
-#include <spawn.h>
-#include <stdio.h>
+#include <LibCore/System.h>
+#include <LibMain/Main.h>
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-int main(int argc, char** argv)
+ErrorOr<int> serenity_main(Main::Arguments arguments)
 {
-    if (argc < 3) {
+    if (arguments.strings.size() < 3) {
         warnln("usage: flock <path> <command...>");
         return 1;
     }
 
-    pid_t child_pid;
-    if ((errno = posix_spawnp(&child_pid, argv[2], nullptr, nullptr, &argv[2], environ))) {
-        perror("posix_spawn");
-        return 1;
-    }
+    pid_t child_pid = TRY(Core::System::posix_spawnp(arguments.strings[2], nullptr, nullptr, &arguments.argv[2], environ));
+    int status = TRY(Core::System::waitpid(child_pid, &status, 0));
 
-    int status;
-    if (waitpid(child_pid, &status, 0) < 0) {
-        perror("waitpid");
-        return 1;
-    }
     return WEXITSTATUS(status);
 }
