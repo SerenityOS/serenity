682f89d5bc1fbc28260ccf98f4ebc7798a5cd46c
Michel Hermier <michel.hermier@gmail.com>
2021-12-14 17:44:43 +0100

    LibC: Allow multiple includes of `<assert.h>`
    ISO C requires in section 7.2:
    The assert macro is redefined according to the current state of NDEBUG
    each time that <assert.h> is included.
    
    Also add tests for `assert` multiple inclusion accordingly.

--- Tests/LibC/TestAssert.cpp
+++ Tests/LibC/TestAssert.cpp
@@ -6,6 +6,7 @@
 
 #include <LibTest/TestCase.h>
 
+#undef NDEBUG
 #include <assert.h>
 #include <signal.h>
 
@@ -16,6 +17,28 @@ TEST_CASE(assert)
         return Test::Crash::Failure::DidNotCrash;
     });
     EXPECT_CRASH_WITH_SIGNAL("This should assert with SIGABRT signal", SIGABRT, [] {
+        assert(!"This should assert");
+        return Test::Crash::Failure::DidNotCrash;
+    });
+}
+
+#define NDEBUG
+#include <assert.h>
+
+TEST_CASE(assert_reinclude)
+{
+    EXPECT_NO_CRASH("This should not assert", [] {
+        assert(!"This should not assert");
+        return Test::Crash::Failure::DidNotCrash;
+    });
+}
+
+#undef NDEBUG
+#include <assert.h>
+
+TEST_CASE(assert_rereinclude)
+{
+    EXPECT_CRASH("This should assert", [] {
         assert(!"This should assert");
         return Test::Crash::Failure::DidNotCrash;
     });
