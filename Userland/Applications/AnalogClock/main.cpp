eb1f00a9403e7089bb86c5dd67136adb010d8798
Astraeus- <Astraeus-@users.noreply.github.com>
2021-12-15 00:11:47 +0100

    AnalogClock: Port to LibMain

--- Userland/Applications/AnalogClock/main.cpp
+++ Userland/Applications/AnalogClock/main.cpp
@@ -1,53 +1,45 @@
 /*
  * Copyright (c) 2021, Erlend Høier <Erlend@ReasonablePanic.com>
+ * Copyright (c) 2021, Julius Heijmen <julius.heijmen@gmail.com>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #include "AnalogClock.h"
 #include <LibCore/DateTime.h>
+#include <LibCore/System.h>
 #include <LibGUI/Application.h>
 #include <LibGUI/Icon.h>
 #include <LibGUI/Menu.h>
 #include <LibGUI/Menubar.h>
 #include <LibGUI/Window.h>
-#include <unistd.h>
+#include <LibMain/Main.h>
 
-int main(int argc, char** argv)
+ErrorOr<int> serenity_main(Main::Arguments arguments)
 {
-    auto app = GUI::Application::construct(argc, argv);
+    auto app = TRY(GUI::Application::try_create(arguments));
 
-    if (pledge("stdio recvfd sendfd rpath", nullptr) < 0) {
-        perror("pledge");
-        return 1;
-    }
+    TRY(Core::System::pledge("stdio recvfd sendfd rpath"));
+    TRY(Core::System::unveil("/res", "r"));
+    TRY(Core::System::unveil(nullptr, nullptr));
 
-    if (unveil("/res", "r") < 0) {
-        perror("unveil");
-        return 1;
-    }
-
-    if (unveil(nullptr, nullptr) < 0) {
-        perror("unveil");
-        return 1;
-    }
-
-    auto app_icon = GUI::Icon::default_icon("app-analog-clock");
-    auto window = GUI::Window::construct();
+    auto app_icon = TRY(GUI::Icon::try_create_default_icon("app-analog-clock"));
+    auto window = TRY(GUI::Window::try_create());
     window->set_title(Core::DateTime::now().to_string("%Y-%m-%d"));
     window->set_icon(app_icon.bitmap_for_size(16));
     window->resize(170, 170);
     window->set_resizable(false);
-    auto& clock = window->set_main_widget<AnalogClock>();
+    auto clock = TRY(window->try_set_main_widget<AnalogClock>());
 
     auto show_window_frame_action = GUI::Action::create_checkable(
         "Show Window &Frame", { Mod_Alt, KeyCode::Key_F }, [&](auto& action) {
-            clock.set_show_window_frame(action.is_checked());
+            clock->set_show_window_frame(action.is_checked());
         });
-    show_window_frame_action->set_checked(clock.show_window_frame());
-    auto menu = GUI::Menu::construct();
-    menu->add_action(move(show_window_frame_action));
-    clock.on_context_menu_request = [&](auto& event) {
+    show_window_frame_action->set_checked(clock->show_window_frame());
+    auto menu = TRY(GUI::Menu::try_create());
+    TRY(menu->try_add_action(*show_window_frame_action));
+
+    clock->on_context_menu_request = [&](auto& event) {
         menu->popup(event.screen_position());
     };
 
