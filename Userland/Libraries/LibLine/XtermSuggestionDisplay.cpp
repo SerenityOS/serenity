dd88ea9173935fbb5e8628c9efe3f7917b8247cd
Ali Mohammad Pur <ali.mpfard@gmail.com>
2021-12-14 14:03:57 +0330

    LibLine: Take the prompt into account when adjusting for a scrolled view
    Otherwise we'd end up putting the prompt *after* the previous prompt
    instead of *over* it when showing suggestions that span more lines than
    are available without scrolling.

--- Userland/Libraries/LibLine/XtermSuggestionDisplay.cpp
+++ Userland/Libraries/LibLine/XtermSuggestionDisplay.cpp
@@ -126,6 +126,9 @@ void XtermSuggestionDisplay::display(const SuggestionManager& manager)
 
     m_lines_used_for_last_suggestions = lines_used;
 
+    // The last line of the prompt is the same line as the first line of the buffer, so we need to subtract one here.
+    lines_used += m_prompt_lines_at_suggestion_initiation - 1;
+
     // If we filled the screen, move back the origin.
     if (m_origin_row + lines_used >= m_num_lines) {
         m_origin_row = m_num_lines - lines_used;
