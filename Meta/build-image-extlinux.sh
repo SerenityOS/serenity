4785c66f41bb3eadff4f4517590aa4a4e9a498db
Brandon Doornbos <santaclausnl@protonmail.com>
2021-12-16 00:46:14 +0100

    Meta: Make image building use `doas` instead of `sudo` when available
    This fixes not being able to run Serenity if you use `doas` and don't
    have `sudo` or are not in the sudoers file.
    
    This behavior can also be disabled via setting the `NO_DOAS`
    environment variable.

--- Meta/build-image-extlinux.sh
+++ Meta/build-image-extlinux.sh
@@ -8,9 +8,22 @@ die() {
 }
 
 if [ "$(id -u)" != 0 ]; then
-    exec sudo -E -- "$0" "$@" || die "this script needs to run as root"
+    # Check for doas instead of sudo, see https://man.openbsd.org/doas.conf#keepenv
+    # for the required configuration to pass through environment variables since we
+    # don't have -E.
+    # If we have doas and sudo installed but only want to use sudo, set the NO_DOAS
+    # environment variable to anything.
+    if [ -z "$NO_DOAS" ] && type doas > /dev/null 2>&1; then
+        exec doas -- "$0" "$@" || die "this script needs to run as root"
+    else
+        exec sudo -E -- "$0" "$@" || die "this script needs to run as root"
+    fi
 else
-    : "${SUDO_UID:=0}" "${SUDO_GID:=0}"
+    if [ -z "$NO_DOAS" ] && type doas > /dev/null 2>&1; then
+        : "${SUDO_UID:=$(id -u "$DOAS_USER")}" "${SUDO_GID:="$(id -g "$DOAS_USER")"}"
+    else
+        : "${SUDO_UID:=0}" "${SUDO_GID:=0}"
+    fi
 fi
 
 for dir in "/usr/lib/syslinux/bios" "/usr/lib/syslinux" "/usr/share/syslinux"; do
