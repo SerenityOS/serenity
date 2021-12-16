ef1d4aab73dd00449e35e0749d479fe85e599726
Rafał Babiarz <5783815+sauler@users.noreply.github.com>
2021-12-16 22:23:40 +0100

    Ports: Updated lua port to version 5.3.6

--- Ports/lua/package.sh
+++ Ports/lua/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=lua
-version=5.3.5
-files="http://www.lua.org/ftp/lua-${version}.tar.gz lua-${version}.tar.gz 0c2eed3f960446e1a3e4b9a1ca2f3ff893b6ce41942cf54d5dd59ab4b3b058ac"
+version=5.3.6
+files="http://www.lua.org/ftp/lua-${version}.tar.gz lua-${version}.tar.gz fc5fd69bb8736323f026672b1b7235da613d7177e72558893a0bdcd320466d60"
 auth_type=sha256
 makeopts=("-j$(nproc)" "serenity")
 installopts=("INSTALL_TOP=${SERENITY_INSTALL_ROOT}/usr/local")
