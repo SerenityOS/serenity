1f9f7c94ae6627daef002a19818625df5379361a
Timothy Flynn <trflynn89@pm.me>
2021-12-16 07:18:35 -0500

    LibUnicode: Add missing forward declarations to forwarding header

--- Userland/Libraries/LibUnicode/Forward.h
+++ Userland/Libraries/LibUnicode/Forward.h
@@ -10,25 +10,38 @@
 
 namespace Unicode {
 
+enum class Calendar : u8;
 enum class CalendarFormatType : u8;
 enum class CalendarPatternStyle : u8;
+enum class CalendarSymbol : u8;
 enum class CompactNumberFormatType : u8;
 enum class Condition : u8;
+enum class Currency : u16;
+enum class DayPeriod : u8;
+enum class Era : u8;
 enum class GeneralCategory : u8;
 enum class HourCycle : u8;
+enum class HourCycleRegion : u8;
+enum class Key : u8;
 enum class Language : u8;
 enum class ListPatternStyle : u8;
 enum class ListPatternType : u8;
 enum class Locale : u16;
+enum class Month : u8;
+enum class NumericSymbol : u8;
 enum class Property : u8;
 enum class Script : u8;
+enum class ScriptTag : u8;
 enum class StandardNumberFormatType : u8;
 enum class Style : u8;
 enum class Territory : u8;
+enum class TimeZone : u8;
+enum class Weekday : u8;
 enum class WordBreakProperty : u8;
 
 struct CalendarFormat;
 struct CalendarPattern;
+struct CalendarRangePattern;
 struct CurrencyCode;
 struct Keyword;
 struct LanguageID;
