4c6e826c055e23493a8b3d72f5f9e5af7a5432ad
Michel Hermier <michel.hermier@gmail.com>
2021-12-17 18:12:47 +0100

    LibTest: Add `EXPECT_CRASH_WITH_SIGNAL`

--- Tests/LibC/TestRaise.cpp
+++ Tests/LibC/TestRaise.cpp
@@ -10,7 +10,7 @@
 
 TEST_CASE(raise)
 {
-    EXPECT_CRASH("This should raise a SIGUSR1 signal", [] {
+    EXPECT_CRASH_WITH_SIGNAL("This should raise a SIGUSR1 signal", SIGUSR1, [] {
         raise(SIGUSR1);
         return Test::Crash::Failure::DidNotCrash;
     });
