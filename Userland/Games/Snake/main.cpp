b8a508adb5d6b6765516f33d5d6496d6d62b3dfe
Astraeus- <Astraeus-@users.noreply.github.com>
2021-12-18 11:38:35 +0100

    Snake: Convert to try_create_default_icon

--- Userland/Games/Snake/main.cpp
+++ Userland/Games/Snake/main.cpp
@@ -31,7 +31,7 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
     TRY(Core::System::unveil("/res", "r"));
     TRY(Core::System::unveil(nullptr, nullptr));
 
-    auto app_icon = GUI::Icon::default_icon("app-snake");
+    auto app_icon = TRY(GUI::Icon::try_create_default_icon("app-snake"));
 
     auto window = TRY(GUI::Window::try_create());
 
