5cbb956090c1b0515db5013d29031ee1d383a8ec
Linus Groh <mail@linusgroh.de>
2021-12-18 20:57:40 +0000

    LibJS: Fix fractionalSecondDigits behavior in Duration.proto.toString()
    This is a normative change in the Temporal spec.
    
    See: https://github.com/tc39/proposal-temporal/commit/3ee771e

--- Userland/Libraries/LibJS/Runtime/Temporal/Duration.cpp
+++ Userland/Libraries/LibJS/Runtime/Temporal/Duration.cpp
@@ -1778,8 +1778,8 @@ String temporal_duration_to_string(double years, double months, double weeks, do
         time_part.append('M');
     }
 
-    // 21. If any of seconds, milliseconds, microseconds, and nanoseconds are not 0; or years, months, weeks, days, hours, and minutes are all 0, then
-    if ((seconds != 0 || milliseconds != 0 || microseconds != 0 || nanoseconds != 0) || (years == 0 && months == 0 && weeks == 0 && days == 0 && hours == 0 && minutes == 0)) {
+    // 21. If any of seconds, milliseconds, microseconds, and nanoseconds are not 0; or years, months, weeks, days, hours, and minutes are all 0; or precision is not "auto"; then
+    if ((seconds != 0 || milliseconds != 0 || microseconds != 0 || nanoseconds != 0) || (years == 0 && months == 0 && weeks == 0 && days == 0 && hours == 0 && minutes == 0) || (precision.has<StringView>() && precision.get<StringView>() != "auto"sv)) {
         // a. Let fraction be abs(milliseconds) × 10^6 + abs(microseconds) × 10^3 + abs(nanoseconds).
         auto fraction = fabs(milliseconds) * 1'000'000 + fabs(microseconds) * 1'000 + fabs(nanoseconds);
 
