3ca72831d1327dc5bd184eb2313671d61e150b25
Astraeus- <Astraeus-@users.noreply.github.com>
2021-12-18 11:56:26 +0100

    Solitaire: Convert to try_create_default_icon

--- Userland/Games/Solitaire/main.cpp
+++ Userland/Games/Solitaire/main.cpp
@@ -27,7 +27,7 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
     TRY(Core::System::pledge("stdio recvfd sendfd rpath unix"));
 
     auto app = TRY(GUI::Application::try_create(arguments));
-    auto app_icon = GUI::Icon::default_icon("app-solitaire");
+    auto app_icon = TRY(GUI::Icon::try_create_default_icon("app-solitaire"));
 
     Config::pledge_domains("Solitaire");
 
