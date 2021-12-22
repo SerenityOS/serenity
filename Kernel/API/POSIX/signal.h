19f899f2f9d57c0df83c0ac540146973725e2be9
Idan Horowitz <idan.horowitz@gmail.com>
2021-12-22 21:55:10 +0200

    Kernel: Add the si_errno and si_band siginfo_t members
    These 2 members are required by POSIX and are also used by some ports.
    Zero is a valid value for both of these, so no further work to support
    them is required.

--- Kernel/API/POSIX/signal.h
+++ Kernel/API/POSIX/signal.h
@@ -26,10 +26,12 @@ union sigval {
 typedef struct siginfo {
     int si_signo;
     int si_code;
+    int si_errno;
     pid_t si_pid;
     uid_t si_uid;
     void* si_addr;
     int si_status;
+    int si_band;
     union sigval si_value;
 } siginfo_t;
 
