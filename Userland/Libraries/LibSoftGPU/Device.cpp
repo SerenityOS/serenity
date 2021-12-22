33e601800c4295796deb2f4800f96e46a3611532
Stephan Unverwerth <s.unverwerth@serenityos.org>
2021-12-22 23:31:44 +0100

    LibSoftGPU: Remove OpenGL type for draw buffer selection
    Replaces the GLenum used in RasterizerConfig to select the draw buffer
    with a simple boolean that disabled color output when the draw buffer
    is set to GL_NONE on the OpenGL side.

--- Userland/Libraries/LibSoftGPU/Device.cpp
+++ Userland/Libraries/LibSoftGPU/Device.cpp
@@ -348,7 +348,7 @@ static void rasterize_triangle(const RasterizerOptions& options, Gfx::Bitmap& re
             }
 
             // We will not update the color buffer at all
-            if (!options.color_mask || options.draw_buffer == GL_NONE)
+            if (!options.color_mask || !options.enable_color_write)
                 continue;
 
             // Draw the pixels according to the previously generated mask
