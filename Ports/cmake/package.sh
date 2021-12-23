08ebe72b94755a3ac0d3f52bd3b3567fb492f81c
Ali Mohammad Pur <ali.mpfard@gmail.com>
2021-12-24 03:05:00 +0330

    Ports: Upgrade CMake to 3.22.1
    That's the latest release, and includes one of the patches we had - so
    we can drop that patch :^)

--- Ports/cmake/package.sh
+++ Ports/cmake/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=cmake
-version=3.19.4
+version=3.22.1
 useconfigure=true
-files="https://github.com/Kitware/CMake/releases/download/v$version/cmake-$version.tar.gz cmake-$version.tar.gz 7d0232b9f1c57e8de81f38071ef8203e6820fe7eec8ae46a1df125d88dbcc2e1"
+files="https://github.com/Kitware/CMake/releases/download/v$version/cmake-$version.tar.gz cmake-$version.tar.gz 0e998229549d7b3f368703d20e248e7ee1f853910d42704aa87918c213ea82c0"
 auth_type=sha256
 depends=("bash" "make" "sed" "ncurses" "libuv")
 configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt" "-DCMAKE_USE_SYSTEM_LIBRARY_LIBUV=1" "-GNinja")
