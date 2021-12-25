da5c257e2e9e727694d309ce54f684c99a99e777
Andreas Kling <kling@serenityos.org>
2021-12-25 17:28:42 +0100

    Kernel: Remove unused function declaration for kmalloc_impl()

--- Kernel/Heap/kmalloc.h
+++ Kernel/Heap/kmalloc.h
@@ -39,7 +39,6 @@ enum class align_val_t : size_t {};
 };
 
 void kmalloc_init();
-[[gnu::malloc, gnu::returns_nonnull, gnu::alloc_size(1)]] void* kmalloc_impl(size_t);
 [[gnu::malloc, gnu::returns_nonnull, gnu::alloc_size(1)]] void* kmalloc_eternal(size_t);
 
 void kfree(void*);
