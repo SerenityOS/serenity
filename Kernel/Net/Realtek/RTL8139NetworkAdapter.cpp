e8f6ab879d1bffe9bac36d68d9b1ce0947f90f2c
Liav A <liavalb@gmail.com>
2021-12-25 11:09:34 +0200

    Kernel/Net: Move Realtek network adapters code to a separate directory

--- Kernel/Net/Realtek/RTL8139NetworkAdapter.cpp
+++ Kernel/Net/Realtek/RTL8139NetworkAdapter.cpp
@@ -9,7 +9,7 @@
 #include <Kernel/Bus/PCI/API.h>
 #include <Kernel/Debug.h>
 #include <Kernel/Net/NetworkingManagement.h>
-#include <Kernel/Net/RTL8139NetworkAdapter.h>
+#include <Kernel/Net/Realtek/RTL8139NetworkAdapter.h>
 #include <Kernel/Sections.h>
 
 namespace Kernel {
