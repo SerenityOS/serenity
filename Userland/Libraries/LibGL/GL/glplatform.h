2ce73b31b6aa416c7e0678a79d7d999155451a75
Jelle Raaijmakers <jelle@gmta.nl>
2021-12-26 13:28:40 +0100

    LibGL: Extract platform types to `glplatform.h`

---
+++ Userland/Libraries/LibGL/GL/glplatform.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2021, Jelle Raaijmakers <jelle@gmta.nl>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#ifndef GLAPI
+#    define GLAPI extern
+#endif
+#define GLAPIENTRY
+#define APIENTRY GLAPIENTRY
+#ifndef APIENTRYP
+#    define APIENTRYP APIENTRY*
+#endif
+
+//
+// OpenGL typedefs
+//
+// Defines types used by all OpenGL applications
+// https://www.khronos.org/opengl/wiki/OpenGL_Type
+typedef char GLchar;
+typedef char GLbyte;
+typedef unsigned char GLuchar;
+typedef unsigned char GLubyte;
+typedef unsigned char GLboolean;
+typedef short GLshort;
+typedef unsigned short GLushort;
+typedef int GLint;
+typedef unsigned int GLuint;
+typedef int GLfixed;
+typedef long long GLint64;
+typedef unsigned long long GLuint64;
+typedef int GLsizei;
+typedef void GLvoid;
+typedef float GLfloat;
+typedef float GLclampf;
+typedef double GLdouble;
+typedef unsigned int GLenum;
+typedef unsigned int GLbitfield;
