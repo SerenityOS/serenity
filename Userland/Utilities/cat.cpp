3f71b894da07cbb6e88d792b352e203d009ead8f
Astraeus- <Astraeus-@users.noreply.github.com>
2021-12-30 15:58:19 +0100

    Cat: Use the new TRY variants for ArgsParser

--- Userland/Utilities/cat.cpp
+++ Userland/Utilities/cat.cpp
@@ -20,7 +20,7 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
 
     Core::ArgsParser args_parser;
     args_parser.set_general_help("Concatenate files or pipes to stdout.");
-    args_parser.add_positional_argument(paths, "File path", "path", Core::ArgsParser::Required::No);
+    TRY(args_parser.try_add_positional_argument(paths, "File path", "path", Core::ArgsParser::Required::No));
     args_parser.parse(arguments);
 
     Vector<int> fds;
