c0652c006acc7e919d8a440e87223f5707d85ba0
Daniel Bertalan <dani@danielbertalan.dev>
2021-12-30 17:17:29 +0100

    Kernel: Use the toolchain's `nm` in mkmap.sh
    By using the binary from our build of binutils, we can be sure that `nm`
    supports demangling symbols, so we can avoid spawning a separate
    `c++filt` process.

--- Kernel/mkmap.sh
+++ Kernel/mkmap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 tmp=$(mktemp)
-nm -n Kernel | grep -vE \\.Lubsan_data | awk '{ if ($2 != "a") print; }' | uniq > "$tmp"
+NM="${NM:-nm}"
+"$NM" -C -n Kernel | grep -vE \\.Lubsan_data | awk '{ if ($2 != "a") print; }' | uniq > "$tmp"
 printf "%08x\n" "$(wc -l "$tmp" | awk '{print $1}')" > kernel.map
-CXXFILT="${CXXFILT:-c++filt}"
-"$CXXFILT" < "$tmp" >> kernel.map
+cat "$tmp" >> kernel.map
 rm -f "$tmp"
