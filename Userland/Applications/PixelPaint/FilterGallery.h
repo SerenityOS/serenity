f47026b1d695a055ad6cb99f4059e4da20e6ccc1
Tobias Christiansen <tobyase@serenityos.org>
2021-12-31 22:17:24 +0100

    PixelPaint: Add Filter Gallery Dialog to the Filter Menu
    This patch adds the bare bones of the new Filter Gallery.
    For now, only the gml and the basic layout got added, a fairly boringw
    indow pops up when "Filter Gallery" is called.
    
    The code for the Model used by the TreeView is taken in large parts from
    HackStudio's VariableModel.

---
+++ Userland/Applications/PixelPaint/FilterGallery.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2021, Tobias Christiansen <tobyase@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibGUI/Dialog.h>
+
+namespace PixelPaint {
+
+class FilterGallery final : public GUI::Dialog {
+    C_OBJECT(FilterGallery);
+
+private:
+    FilterGallery(GUI::Window* parent_window);
+};
+
+}
