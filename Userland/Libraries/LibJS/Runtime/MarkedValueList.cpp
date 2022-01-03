4a14455dff85cb7a37c89c8f1e7c756a218ab426
Luke Wilde <lukew@serenityos.org>
2022-01-03 20:26:38 +0000

    LibJS: Make MarkedValueList copyable and move assignable
    This is required to store a MarkedValueList as the value of a HashMap.

--- Userland/Libraries/LibJS/Runtime/MarkedValueList.cpp
+++ Userland/Libraries/LibJS/Runtime/MarkedValueList.cpp
@@ -10,21 +10,42 @@
 namespace JS {
 
 MarkedValueList::MarkedValueList(Heap& heap)
-    : m_heap(heap)
+    : m_heap(&heap)
 {
-    m_heap.did_create_marked_value_list({}, *this);
+    m_heap->did_create_marked_value_list({}, *this);
+}
+
+MarkedValueList::MarkedValueList(MarkedValueList const& other)
+    : Vector<Value, 32>(other)
+    , m_heap(other.m_heap)
+{
+    m_heap->did_create_marked_value_list({}, *this);
 }
 
 MarkedValueList::MarkedValueList(MarkedValueList&& other)
     : Vector<Value, 32>(move(static_cast<Vector<Value, 32>&>(other)))
     , m_heap(other.m_heap)
 {
-    m_heap.did_create_marked_value_list({}, *this);
+    m_heap->did_create_marked_value_list({}, *this);
 }
 
 MarkedValueList::~MarkedValueList()
 {
-    m_heap.did_destroy_marked_value_list({}, *this);
+    m_heap->did_destroy_marked_value_list({}, *this);
+}
+
+MarkedValueList& MarkedValueList::operator=(JS::MarkedValueList const& other)
+{
+    Vector<Value, 32>::operator=(other);
+
+    if (m_heap != other.m_heap) {
+        m_heap = other.m_heap;
+
+        // NOTE: IntrusiveList will remove this MarkedValueList from the old heap it was part of.
+        m_heap->did_create_marked_value_list({}, *this);
+    }
+
+    return *this;
 }
 
 }
