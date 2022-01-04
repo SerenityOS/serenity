792e9c9baf401196c4b54e53cee86749bed3822e
Jelle Raaijmakers <jelle@gmta.nl>
2022-01-04 15:01:25 +0100

    AK: Add `mix`

--- AK/StdLibExtras.h
+++ AK/StdLibExtras.h
@@ -92,6 +92,12 @@ constexpr T clamp(const T& value, const IdentityType<T>& min, const IdentityType
 }
 
 template<typename T, typename U>
+constexpr T mix(T const& v1, T const& v2, U const& interpolation)
+{
+    return v1 + (v2 - v1) * interpolation;
+}
+
+template<typename T, typename U>
 constexpr T ceil_div(T a, U b)
 {
     static_assert(sizeof(T) == sizeof(U));
@@ -167,6 +173,7 @@ using AK::exchange;
 using AK::is_constant_evaluated;
 using AK::max;
 using AK::min;
+using AK::mix;
 using AK::RawPtr;
 using AK::swap;
 using AK::to_underlying;
