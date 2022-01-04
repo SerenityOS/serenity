21319091d8ddec14d628b960f6c0b71500878811
mjz19910 <matthias291999@gmail.com>
2022-01-03 20:04:15 -0700

    yes: Port to LibMain

--- Userland/Utilities/yes.cpp
+++ Userland/Utilities/yes.cpp
@@ -5,21 +5,19 @@
  */
 
 #include <LibCore/ArgsParser.h>
+#include <LibCore/System.h>
 #include <stdio.h>
 #include <unistd.h>
 
-int main(int argc, char** argv)
+ErrorOr<int> serenity_main(Main::Arguments arguments)
 {
-    if (pledge("stdio", nullptr) < 0) {
-        perror("pledge");
-        return 1;
-    }
+    TRY(Core::System::pledge("stdio"));
 
     const char* string = "yes";
 
     Core::ArgsParser args_parser;
     args_parser.add_positional_argument(string, "String to output (defaults to 'yes')", "string", Core::ArgsParser::Required::No);
-    args_parser.parse(argc, argv);
+    args_parser.parse(arguments);
 
     for (;;)
         puts(string);
