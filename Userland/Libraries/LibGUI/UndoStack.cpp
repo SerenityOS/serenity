c4cc796f8697780db756af2811dd0f3c09bd38b3
Andreas Kling <kling@serenityos.org>
2022-01-04 17:32:19 +0100

    LibGUI: Make GUI::UndoStack remember time it was last set to unmodified
    This can be used to determine how much time has passed since a document
    was saved. :^)

--- Userland/Libraries/LibGUI/UndoStack.cpp
+++ Userland/Libraries/LibGUI/UndoStack.cpp
@@ -80,6 +80,7 @@ void UndoStack::set_current_unmodified()
         return;
 
     m_clean_index = m_stack_index;
+    m_last_unmodified_timestamp = Time::now_monotonic();
 
     if (on_state_change)
         on_state_change();
