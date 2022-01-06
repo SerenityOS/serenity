11e50591f24fcde3f1f44f339f252e6d1a3fef5f
Stephan Unverwerth <s.unverwerth@serenityos.org>
2022-01-06 21:45:02 +0100

    LibSoftGPU: Also interpolate normal during triangle clipping

--- Userland/Libraries/LibSoftGPU/Clipper.cpp
+++ Userland/Libraries/LibSoftGPU/Clipper.cpp
@@ -48,6 +48,7 @@ Vertex Clipper::clip_intersection_point(const Vertex& p1, const Vertex& p2, Clip
     out.clip_coordinates = mix(p1.clip_coordinates, p2.clip_coordinates, a);
     out.color = mix(p1.color, p2.color, a);
     out.tex_coord = mix(p1.tex_coord, p2.tex_coord, a);
+    out.normal = mix(p1.normal, p2.normal, a);
     return out;
 }
 
