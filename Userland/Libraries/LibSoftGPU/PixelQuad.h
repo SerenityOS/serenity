178a57bbf7cf3b0b70c3484f7324a659e8fcac33
Stephan Unverwerth <s.unverwerth@serenityos.org>
2022-01-06 16:55:08 +0100

    LibSoftGPU: Add PixelQuad struct that holds data for each rendered quad

---
+++ Userland/Libraries/LibSoftGPU/PixelQuad.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2021, Stephan Unverwerth <s.unverwerth@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/SIMD.h>
+#include <LibGfx/Vector2.h>
+#include <LibGfx/Vector3.h>
+#include <LibGfx/Vector4.h>
+
+namespace SoftGPU {
+
+struct PixelQuad final {
+    Vector2<AK::SIMD::i32x4> screen_coordinates;
+    Vector3<AK::SIMD::f32x4> barycentrics;
+    AK::SIMD::f32x4 depth;
+    Vector4<AK::SIMD::f32x4> vertex_color;
+    Vector4<AK::SIMD::f32x4> uv;
+    Vector4<AK::SIMD::f32x4> out_color;
+    AK::SIMD::f32x4 fog_depth;
+    AK::SIMD::i32x4 mask;
+};
+
+}
