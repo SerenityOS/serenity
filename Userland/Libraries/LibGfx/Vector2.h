010ec0cffc7a8b66ebdc22fa6267dfa2d867ef4b
Lenny Maiorani <lenny@colorado.edu>
2022-01-06 19:36:07 -0700

    LibGfx: Refactor Vector[2,3,4] to VectorN with specializations
    `Gfx::Vector[2,3,4]` are nearly identical implementations. This code
    redundancy does not follow the DRY (Don't Repeat Yourself) principle
    leading to possible out-of-sync errors between the classes.
    
    Combining these classes into a class template which can be specialized
    for each needed size makes the differences obvious through
    `constexpr-if` blocks and `requires` clauses.

--- Userland/Libraries/LibGfx/Vector2.h
+++ Userland/Libraries/LibGfx/Vector2.h
@@ -1,132 +1,24 @@
 /*
  * Copyright (c) 2020, Stephan Unverwerth <s.unverwerth@serenityos.org>
+ * Copyright (c) 2022, the SerenityOS developers.
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #pragma once
 
-#include <AK/Math.h>
-#include <AK/String.h>
+#include "VectorN.h"
+
+#include <AK/Error.h>
+#include <AK/Format.h>
+#include <AK/StringView.h>
 
 namespace Gfx {
-template<typename T>
-class Vector2 final {
-public:
-    constexpr Vector2() = default;
-    constexpr Vector2(T x, T y)
-        : m_x(x)
-        , m_y(y)
-    {
-    }
 
-    constexpr T x() const { return m_x; }
-    constexpr T y() const { return m_y; }
-
-    constexpr void set_x(T value) { m_x = value; }
-    constexpr void set_y(T value) { m_y = value; }
-
-    constexpr Vector2& operator+=(const Vector2& other)
-    {
-        m_x += other.m_x;
-        m_y += other.m_y;
-        return *this;
-    }
-
-    constexpr Vector2& operator-=(const Vector2& other)
-    {
-        m_x -= other.m_x;
-        m_y -= other.m_y;
-        return *this;
-    }
-
-    constexpr Vector2 operator+(const Vector2& other) const
-    {
-        return Vector2(m_x + other.m_x, m_y + other.m_y);
-    }
-
-    constexpr Vector2 operator-(const Vector2& other) const
-    {
-        return Vector2(m_x - other.m_x, m_y - other.m_y);
-    }
-
-    constexpr Vector2 operator-() const
-    {
-        return Vector2(-m_x, -m_y);
-    }
-
-    constexpr Vector2 operator*(const Vector2& other) const
-    {
-        return Vector2(m_x * other.m_x, m_y * other.m_y);
-    }
-
-    constexpr Vector2 operator/(const Vector2& other) const
-    {
-        return Vector2(m_x / other.m_x, m_y / other.m_y);
-    }
-
-    template<typename U>
-    constexpr Vector2 operator*(U f) const
-    {
-        return Vector2(m_x * f, m_y * f);
-    }
-
-    template<typename U>
-    constexpr Vector2 operator/(U f) const
-    {
-        return Vector2(m_x / f, m_y / f);
-    }
-
-    constexpr T dot(const Vector2& other) const
-    {
-        return m_x * other.m_x + m_y * other.m_y;
-    }
-
-    constexpr Vector2 normalized() const
-    {
-        T inv_length = 1 / length();
-        return *this * inv_length;
-    }
-
-    constexpr Vector2 clamped(T m, T x) const
-    {
-        Vector2 copy { *this };
-        copy.clamp(m, x);
-        return copy;
-    }
-
-    constexpr void clamp(T min_value, T max_value)
-    {
-        m_x = max(min_value, m_x);
-        m_y = max(min_value, m_y);
-        m_x = min(max_value, m_x);
-        m_y = min(max_value, m_y);
-    }
-
-    constexpr void normalize()
-    {
-        T inv_length = 1 / length();
-        m_x *= inv_length;
-        m_y *= inv_length;
-    }
-
-    constexpr T length() const
-    {
-        return AK::hypot(m_x, m_y);
-    }
-
-    String to_string() const
-    {
-        return String::formatted("[{},{}]", x(), y());
-    }
-
-private:
-    T m_x;
-    T m_y;
-};
-
-typedef Vector2<float> FloatVector2;
-typedef Vector2<double> DoubleVector2;
+template<class T>
+using Vector2 = VectorN<2, T>;
+using FloatVector2 = Vector2<float>;
+using DoubleVector2 = Vector2<double>;
 
 }
 
--- Userland/Libraries/LibGfx/Vector3.h
+++ Userland/Libraries/LibGfx/Vector3.h
@@ -1,149 +1,24 @@
 /*
  * Copyright (c) 2020, Stephan Unverwerth <s.unverwerth@serenityos.org>
+ * Copyright (c) 2022, the SerenityOS developers.
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #pragma once
 
-#include <AK/Math.h>
-#include <AK/String.h>
+#include "VectorN.h"
+
+#include <AK/Error.h>
+#include <AK/Format.h>
+#include <AK/StringView.h>
 
 namespace Gfx {
-template<typename T>
-class Vector3 final {
-public:
-    constexpr Vector3() = default;
-    constexpr Vector3(T x, T y, T z)
-        : m_x(x)
-        , m_y(y)
-        , m_z(z)
-    {
-    }
 
-    constexpr T x() const { return m_x; }
-    constexpr T y() const { return m_y; }
-    constexpr T z() const { return m_z; }
-
-    constexpr void set_x(T value) { m_x = value; }
-    constexpr void set_y(T value) { m_y = value; }
-    constexpr void set_z(T value) { m_z = value; }
-
-    constexpr Vector3& operator+=(const Vector3& other)
-    {
-        m_x += other.m_x;
-        m_y += other.m_y;
-        m_z += other.m_z;
-        return *this;
-    }
-
-    constexpr Vector3& operator-=(const Vector3& other)
-    {
-        m_x -= other.m_x;
-        m_y -= other.m_y;
-        m_z -= other.m_z;
-        return *this;
-    }
-
-    constexpr Vector3 operator+(const Vector3& other) const
-    {
-        return Vector3(m_x + other.m_x, m_y + other.m_y, m_z + other.m_z);
-    }
-
-    constexpr Vector3 operator-(const Vector3& other) const
-    {
-        return Vector3(m_x - other.m_x, m_y - other.m_y, m_z - other.m_z);
-    }
-
-    constexpr Vector3 operator-() const
-    {
-        return Vector3(-m_x, -m_y, -m_z);
-    }
-
-    constexpr Vector3 operator*(const Vector3& other) const
-    {
-        return Vector3(m_x * other.m_x, m_y * other.m_y, m_z * other.m_z);
-    }
-
-    constexpr Vector3 operator/(const Vector3& other) const
-    {
-        return Vector3(m_x / other.m_x, m_y / other.m_y, m_z / other.m_z);
-    }
-
-    template<typename U>
-    constexpr Vector3 operator*(U f) const
-    {
-        return Vector3(m_x * f, m_y * f, m_z * f);
-    }
-
-    template<typename U>
-    constexpr Vector3 operator/(U f) const
-    {
-        return Vector3(m_x / f, m_y / f, m_z / f);
-    }
-
-    constexpr T dot(const Vector3& other) const
-    {
-        return m_x * other.m_x + m_y * other.m_y + m_z * other.m_z;
-    }
-
-    constexpr Vector3 cross(const Vector3& other) const
-    {
-        return Vector3(
-            m_y * other.m_z - m_z * other.m_y,
-            m_z * other.m_x - m_x * other.m_z,
-            m_x * other.m_y - m_y * other.m_x);
-    }
-
-    constexpr Vector3 normalized() const
-    {
-        T inv_length = 1 / length();
-        return *this * inv_length;
-    }
-
-    constexpr Vector3 clamped(T m, T x) const
-    {
-        Vector3 copy { *this };
-        copy.clamp(m, x);
-        return copy;
-    }
-
-    constexpr void clamp(T min_value, T max_value)
-    {
-        m_x = max(min_value, m_x);
-        m_y = max(min_value, m_y);
-        m_z = max(min_value, m_z);
-        m_x = min(max_value, m_x);
-        m_y = min(max_value, m_y);
-        m_z = min(max_value, m_z);
-    }
-
-    constexpr void normalize()
-    {
-        T inv_length = 1 / length();
-        m_x *= inv_length;
-        m_y *= inv_length;
-        m_z *= inv_length;
-    }
-
-    constexpr T length() const
-    {
-        return AK::sqrt(m_x * m_x + m_y * m_y + m_z * m_z);
-    }
-
-    String to_string() const
-    {
-        return String::formatted("[{},{},{}]", x(), y(), z());
-    }
-
-private:
-    T m_x;
-    T m_y;
-    T m_z;
-};
-
-typedef Vector3<float> FloatVector3;
-typedef Vector3<double> DoubleVector3;
+template<class T>
+using Vector3 = VectorN<3, T>;
+using FloatVector3 = Vector3<float>;
+using DoubleVector3 = Vector3<double>;
 
 }
 
