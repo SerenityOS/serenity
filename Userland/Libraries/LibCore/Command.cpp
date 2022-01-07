fbdd6df1856483b3de1199ed810de6701690fc84
Itamar <itamar8910@gmail.com>
2022-01-07 16:17:19 +0200

    LibCore: Make Core::command return CommandResult struct
    Previously, Core::command only returned a String which contained the
    data from stdout.
    
    The CommandResult struct contains the exit code as well as the data
    from stdout and stderr.

--- Userland/Libraries/LibCore/Command.cpp
+++ Userland/Libraries/LibCore/Command.cpp
@@ -18,27 +18,26 @@ namespace Core {
 // Only supported in serenity mode because we use `posix_spawn_file_actions_addchdir`
 #ifdef __serenity__
 
-String command(const String& command_string, Optional<LexicalPath> chdir)
+ErrorOr<CommandResult> command(String const& command_string, Optional<LexicalPath> chdir)
 {
     auto parts = command_string.split(' ');
     if (parts.is_empty())
-        return {};
+        return Error::from_string_literal("empty command"sv);
     auto program = parts[0];
     parts.remove(0);
     return command(program, parts, chdir);
 }
 
-String command(const String& program, const Vector<String>& arguments, Optional<LexicalPath> chdir)
+ErrorOr<CommandResult> command(String const& program, Vector<String> const& arguments, Optional<LexicalPath> chdir)
 {
     int stdout_pipe[2] = {};
     int stderr_pipe[2] = {};
     if (pipe2(stdout_pipe, O_CLOEXEC)) {
-        perror("pipe2");
-        VERIFY_NOT_REACHED();
+        return Error::from_errno(errno);
     }
     if (pipe2(stderr_pipe, O_CLOEXEC)) {
         perror("pipe2");
-        VERIFY_NOT_REACHED();
+        return Error::from_errno(errno);
     }
 
     auto close_pipes = ScopeGuard([stderr_pipe, stdout_pipe] {
@@ -68,9 +67,6 @@ String command(const String& program, const Vector<String>& arguments, Optional<
         perror("posix_spawn");
         VERIFY_NOT_REACHED();
     }
-    int wstatus;
-    waitpid(pid, &wstatus, 0);
-    posix_spawn_file_actions_destroy(&action);
 
     // close the write-ends so reading wouldn't block
     close(stdout_pipe[1]);
@@ -84,18 +80,21 @@ String command(const String& program, const Vector<String>& arguments, Optional<
         }
         return String::copy(result_file->read_all());
     };
+    auto stdout = read_all_from_pipe(stdout_pipe);
+    auto stderr = read_all_from_pipe(stderr_pipe);
 
-    if (WEXITSTATUS(wstatus) != 0) {
+    int wstatus { 0 };
+    waitpid(pid, &wstatus, 0);
+    posix_spawn_file_actions_destroy(&action);
+    int exit_code = WEXITSTATUS(wstatus);
+
+    if (exit_code != 0) {
 #    ifdef DBG_FAILED_COMMANDS
-        dbgln("command failed. stderr: {}", read_all_from_pipe(stderr_pipe));
+        dbgln("command failed. stderr: {}", );
 #    endif
-        return {};
     }
 
-    auto result = read_all_from_pipe(stdout_pipe);
-    if (result.is_null())
-        return "";
-    return result;
+    return CommandResult { WEXITSTATUS(wstatus), stdout, stderr };
 }
 
 #endif
