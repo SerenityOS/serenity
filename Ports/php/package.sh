801bcd6e0304c8338e1ab7b5c2b676a8a6ef5790
Jelle Raaijmakers <jelle@gmta.nl>
2022-01-08 00:12:42 +0100

    Ports: Unset env vars after configure in `php`
    These environment variables would linger after the `php` port was done
    building. This would pose issues in the future if other ports depend on
    this package, since these vars then leak into the build scripts.

--- Ports/php/package.sh
+++ Ports/php/package.sh
@@ -22,13 +22,28 @@ launcher_command="/usr/local/bin/php -a"
 launcher_run_in_terminal="true"
 icon_file="win32/build/php.ico"
 
-export CFLAGS="-I${SERENITY_INSTALL_ROOT}/usr/local/include/libxml2"
-export LIBS="-ldl"
-export LIBXML_CFLAGS="y"
-export LIBXML_LIBS="-lxml2"
-export OPENSSL_CFLAGS="y"
-export OPENSSL_LIBS="-lssl -lcrypto"
-export SQLITE_CFLAGS="y"
-export SQLITE_LIBS="-lsqlite3 -lpthread"
-export ZLIB_CFLAGS="y"
-export ZLIB_LIBS="-lz"
+pre_configure() {
+    export CFLAGS="-I${SERENITY_INSTALL_ROOT}/usr/local/include/libxml2"
+    export LIBS="-ldl"
+    export LIBXML_CFLAGS="y"
+    export LIBXML_LIBS="-lxml2"
+    export OPENSSL_CFLAGS="y"
+    export OPENSSL_LIBS="-lssl -lcrypto"
+    export SQLITE_CFLAGS="y"
+    export SQLITE_LIBS="-lsqlite3 -lpthread"
+    export ZLIB_CFLAGS="y"
+    export ZLIB_LIBS="-lz"
+}
+
+post_configure() {
+    unset ZLIB_LIBS
+    unset ZLIB_CFLAGS
+    unset SQLITE_LIBS
+    unset SQLITE_CFLAGS
+    unset OPENSSL_LIBS
+    unset OPENSSL_CFLAGS
+    unset LIBXML_LIBS
+    unset LIBS
+    unset LIBXML_CFLAGS
+    unset CFLAGS
+}
