978343d523c7a0c7e481f21c039e3e0437e2d8e5
Federico Guerinoni <guerinoni.federico@gmail.com>
2022-01-08 18:13:48 +0100

    Userland: Port beep to LibMain

--- Userland/Utilities/beep.cpp
+++ Userland/Utilities/beep.cpp
@@ -1,12 +1,13 @@
 /*
- * Copyright (c) 2020, the SerenityOS developers.
+ * Copyright (c) 2022, the SerenityOS developers.
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <LibMain/Main.h>
 #include <unistd.h>
 
-int main()
+ErrorOr<int> serenity_main(Main::Arguments)
 {
     sysbeep();
     return 0;
