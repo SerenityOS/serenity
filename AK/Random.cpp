c2f229b5f10f73837687e2b101a2b54cac7d94e1
creator1creeper1 <creator1creeper1@airmail.cc>
2022-01-08 22:58:58 +0100

    AK: Explicitly name types in Random.cpp

--- AK/Random.cpp
+++ AK/Random.cpp
@@ -15,8 +15,8 @@ u32 get_random_uniform(u32 max_bounds)
     // `arc4random() % max_bounds` would be insufficient. Here we compute the last number of the
     // last "full group". Note that if max_bounds is a divisor of UINT32_MAX,
     // then we end up with UINT32_MAX:
-    const u32 max_usable = UINT32_MAX - (static_cast<u64>(UINT32_MAX) + 1) % max_bounds;
-    auto random_value = get_random<u32>();
+    u32 const max_usable = UINT32_MAX - (static_cast<u64>(UINT32_MAX) + 1) % max_bounds;
+    u32 random_value = get_random<u32>();
     for (int i = 0; i < 20 && random_value > max_usable; ++i) {
         // By chance we picked a value from the incomplete group. Note that this group has size at
         // most 2**31-1, so picking this group has a chance of less than 50%.
