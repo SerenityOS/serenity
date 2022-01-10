89f2c958132e9dbf8333f8eddf58fc5acad337bf
Michel Hermier <michel.hermier@gmail.com>
2022-01-10 22:14:56 +0100

    Kernel: Make `RamdiskDevice` creation use `(Major,Minor)Number`

--- Kernel/Storage/RamdiskDevice.cpp
+++ Kernel/Storage/RamdiskDevice.cpp
@@ -13,10 +13,10 @@
 
 namespace Kernel {
 
-NonnullRefPtr<RamdiskDevice> RamdiskDevice::create(const RamdiskController& controller, NonnullOwnPtr<Memory::Region>&& region, int major, int minor)
+NonnullRefPtr<RamdiskDevice> RamdiskDevice::create(const RamdiskController& controller, NonnullOwnPtr<Memory::Region>&& region, MajorNumber major, MinorNumber minor)
 {
     // FIXME: Try to not hardcode a maximum of 16 partitions per drive!
-    size_t drive_index = minor / 16;
+    size_t drive_index = minor.value() / 16;
     auto device_name = MUST(KString::formatted("ramdisk{}", drive_index));
 
     auto device_or_error = DeviceManagement::try_create_device<RamdiskDevice>(controller, move(region), major, minor, move(device_name));
@@ -25,7 +25,7 @@ NonnullRefPtr<RamdiskDevice> RamdiskDevice::create(const RamdiskController& cont
     return device_or_error.release_value();
 }
 
-RamdiskDevice::RamdiskDevice(const RamdiskController&, NonnullOwnPtr<Memory::Region>&& region, int major, int minor, NonnullOwnPtr<KString> device_name)
+RamdiskDevice::RamdiskDevice(const RamdiskController&, NonnullOwnPtr<Memory::Region>&& region, MajorNumber major, MinorNumber minor, NonnullOwnPtr<KString> device_name)
     : StorageDevice(major, minor, 512, region->size() / 512, move(device_name))
     , m_region(move(region))
 {
--- Kernel/Storage/RamdiskDevice.h
+++ Kernel/Storage/RamdiskDevice.h
@@ -18,14 +18,14 @@ class RamdiskDevice final : public StorageDevice {
     friend class DeviceManagement;
 
 public:
-    static NonnullRefPtr<RamdiskDevice> create(const RamdiskController&, NonnullOwnPtr<Memory::Region>&& region, int major, int minor);
+    static NonnullRefPtr<RamdiskDevice> create(const RamdiskController&, NonnullOwnPtr<Memory::Region>&& region, MajorNumber major, MinorNumber minor);
     virtual ~RamdiskDevice() override;
 
     // ^DiskDevice
     virtual StringView class_name() const override;
 
 private:
-    RamdiskDevice(const RamdiskController&, NonnullOwnPtr<Memory::Region>&&, int major, int minor, NonnullOwnPtr<KString> device_name);
+    RamdiskDevice(const RamdiskController&, NonnullOwnPtr<Memory::Region>&&, MajorNumber major, MinorNumber minor, NonnullOwnPtr<KString> device_name);
 
     // ^BlockDevice
     virtual void start_request(AsyncBlockDeviceRequest&) override;
