8dffb133640aa37a20b51bf34466ff62e7fac64a
Jelle Raaijmakers <jelle@gmta.nl>
2022-01-10 02:07:07 +0100

    LibGL: Implement `glColor3d` and `glColor3ubv`

--- Userland/Libraries/LibGL/GLColor.cpp
+++ Userland/Libraries/LibGL/GLColor.cpp
@@ -10,6 +10,11 @@
 
 extern GL::GLContext* g_gl_context;
 
+void glColor3d(GLdouble r, GLdouble g, GLdouble b)
+{
+    g_gl_context->gl_color(r, g, b, 1.0);
+}
+
 void glColor3dv(GLdouble const* v)
 {
     g_gl_context->gl_color(v[0], v[1], v[2], 1.0);
@@ -28,6 +33,11 @@ void glColor3fv(const GLfloat* v)
 void glColor3ub(GLubyte r, GLubyte g, GLubyte b)
 {
     g_gl_context->gl_color(r / 255.0, g / 255.0, b / 255.0, 1.0);
+}
+
+void glColor3ubv(GLubyte const* v)
+{
+    g_gl_context->gl_color(v[0] / 255.0f, v[1] / 255.0f, v[2] / 255.0f, 1.0);
 }
 
 void glColor4dv(GLdouble const* v)
