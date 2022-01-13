0ddba1568436e6e66094a2452c0daadb2828cc96
Lucas CHOLLET <lucas.chollet@free.fr>
2022-01-13 23:15:25 +0100

    hostname: Port to LibMain

--- Userland/Utilities/hostname.cpp
+++ Userland/Utilities/hostname.cpp
@@ -5,37 +5,29 @@
  */
 
 #include <LibCore/ArgsParser.h>
+#include <LibCore/System.h>
+#include <LibMain/Main.h>
 #include <limits.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc, char** argv)
+ErrorOr<int> serenity_main(Main::Arguments args)
 {
     const char* hostname = nullptr;
 
     Core::ArgsParser args_parser;
     args_parser.add_positional_argument(hostname, "Hostname to set", "hostname", Core::ArgsParser::Required::No);
-    args_parser.parse(argc, argv);
+    args_parser.parse(args);
 
     if (!hostname) {
-        char buffer[HOST_NAME_MAX];
-        int rc = gethostname(buffer, sizeof(buffer));
-        if (rc < 0) {
-            perror("gethostname");
-            return 1;
-        }
-        outln("{}", buffer);
+        outln("{}", TRY(Core::System::gethostname()));
     } else {
         if (strlen(hostname) >= HOST_NAME_MAX) {
             warnln("Hostname must be less than {} characters", HOST_NAME_MAX);
             return 1;
         }
-        int rc = sethostname(hostname, strlen(hostname));
-        if (rc < 0) {
-            perror("sethostname");
-            return 1;
-        }
+        TRY(Core::System::sethostname(hostname));
     }
     return 0;
 }
