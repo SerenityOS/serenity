837117378e64c9c52205589427f1733508b1cddb
Lucas CHOLLET <lucas.chollet@free.fr>
2022-01-13 21:03:08 +0100

    gml-format: Port to LibMain

--- Userland/Utilities/gml-format.cpp
+++ Userland/Utilities/gml-format.cpp
@@ -6,12 +6,13 @@
 
 #include <LibCore/ArgsParser.h>
 #include <LibCore/File.h>
+#include <LibCore/System.h>
 #include <LibGUI/GMLFormatter.h>
-#include <unistd.h>
+#include <LibMain/Main.h>
 
-bool format_file(StringView, bool);
+ErrorOr<bool> format_file(StringView, bool);
 
-bool format_file(StringView path, bool inplace)
+ErrorOr<bool> format_file(StringView path, bool inplace)
 {
     auto read_from_stdin = path == "-";
     RefPtr<Core::File> file;
@@ -19,12 +20,7 @@ bool format_file(StringView path, bool inplace)
         file = Core::File::standard_input();
     } else {
         auto open_mode = inplace ? Core::OpenMode::ReadWrite : Core::OpenMode::ReadOnly;
-        auto file_or_error = Core::File::open(path, open_mode);
-        if (file_or_error.is_error()) {
-            warnln("Could not open {}: {}", path, file_or_error.error());
-            return false;
-        }
-        file = file_or_error.value();
+        file = TRY(Core::File::open(path, open_mode));
     }
     auto formatted_gml = GUI::format_gml(file->read_all());
     if (formatted_gml.is_null()) {
@@ -46,13 +42,10 @@ bool format_file(StringView path, bool inplace)
     return true;
 }
 
-int main(int argc, char** argv)
+ErrorOr<int> serenity_main(Main::Arguments args)
 {
 #ifdef __serenity__
-    if (pledge("stdio rpath wpath cpath", nullptr) < 0) {
-        perror("pledge");
-        return 1;
-    }
+    TRY(Core::System::pledge("stdio rpath wpath cpath", nullptr));
 #endif
 
     bool inplace = false;
@@ -62,15 +55,11 @@ int main(int argc, char** argv)
     args_parser.set_general_help("Format GML files.");
     args_parser.add_option(inplace, "Write formatted contents back to file rather than standard output", "inplace", 'i');
     args_parser.add_positional_argument(files, "File(s) to process", "path", Core::ArgsParser::Required::No);
-    args_parser.parse(argc, argv);
+    args_parser.parse(args);
 
 #ifdef __serenity__
-    if (!inplace) {
-        if (pledge("stdio rpath", nullptr) < 0) {
-            perror("pledge");
-            return 1;
-        }
-    }
+    if (!inplace)
+        TRY(Core::System::pledge("stdio rpath", nullptr));
 #endif
 
     unsigned exit_code = 0;
@@ -78,7 +67,7 @@ int main(int argc, char** argv)
     if (files.is_empty())
         files.append("-");
     for (auto& file : files) {
-        if (!format_file(file, inplace))
+        if (!TRY(format_file(file, inplace)))
             exit_code = 1;
     }
 
