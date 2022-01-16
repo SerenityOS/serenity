f28168813e5fde00217aab273c8fec87c68fb4ab
czapek1337 <czapek1337@gmail.com>
2022-01-16 08:15:20 +0100

    Meta: Add support for the Limine bootloader

--- Meta/serenity.sh
+++ Meta/serenity.sh
@@ -226,6 +226,14 @@ build_target() {
     fi
 }
 
+build_image() {
+    if [ "$SERENITY_RUN" = "limine" ]; then
+        build_target limine-image
+    else
+        build_target image
+    fi
+}
+
 delete_target() {
     [ ! -d "$BUILD_DIR" ] || rm -rf "$BUILD_DIR"
     [ ! -d "$SUPER_BUILD_DIR" ] || rm -rf "$SUPER_BUILD_DIR"
@@ -320,14 +328,14 @@ if [[ "$CMD" =~ ^(build|install|image|copy-src|run|gdb|test|rebuild|recreate|kad
             lagom_unsupported
             build_target
             build_target install
-            build_target image
+            build_image
             ;;
         copy-src)
           lagom_unsupported
           build_target
           build_target install
           export SERENITY_COPY_SOURCE=1
-          build_target image
+          build_image
           ;;
         run)
             if [ "$TARGET" = "lagom" ]; then
@@ -336,7 +344,7 @@ if [[ "$CMD" =~ ^(build|install|image|copy-src|run|gdb|test|rebuild|recreate|kad
             else
                 build_target
                 build_target install
-                build_target image
+                build_image
                 if [ -n "${CMD_ARGS[0]}" ]; then
                     export SERENITY_KERNEL_CMDLINE="${CMD_ARGS[0]}"
                 fi
@@ -352,7 +360,7 @@ if [[ "$CMD" =~ ^(build|install|image|copy-src|run|gdb|test|rebuild|recreate|kad
             else
                 build_target
                 build_target install
-                build_target image
+                build_image
                 tmux new-session "$ARG0" __tmux_cmd "$TARGET" run "${CMD_ARGS[@]}" \; set-option -t 0 mouse on \; split-window "$ARG0" __tmux_cmd "$TARGET" gdb "${CMD_ARGS[@]}" \;
             fi
             ;;
@@ -362,7 +370,7 @@ if [[ "$CMD" =~ ^(build|install|image|copy-src|run|gdb|test|rebuild|recreate|kad
                 run_tests "${CMD_ARGS[0]}"
             else
                 build_target install
-                build_target image
+                build_image
                 # In contrast to CI, we don't set 'panic=shutdown' here,
                 # in case the user wants to inspect qemu some more.
                 export SERENITY_KERNEL_CMDLINE="fbdev=off system_mode=self-test"
