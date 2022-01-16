SerenityOS/serenity

commit cdd6d68b2ab56554e5ebcb61f0977534153530f1

Ports: Remove some config.sub patches and download a fresh one if needed
All of these patches did the same thing, which is already in upstream
config.sub.
With this change, we need only add `use_fresh_config_sub=true` to
the package.sh file.
Note that this is not done automatically in case the port has a modified
config.sub file.

Ports/freetype/package.sh
@@ -2,6 +2,7 @@
 port=freetype
 version=2.10.4
 useconfigure=true
+use_fresh_config_sub=true
 files="https://download.savannah.gnu.org/releases/freetype/freetype-${version}.tar.gz freetype-${version}.tar.gz 5eab795ebb23ac77001cfb68b7d4d50b5d6c7469247b0b01b2c953269f658dac"
 auth_type=sha256
 configopts=("--with-brotli=no" "--with-bzip2=no" "--with-zlib=no" "--with-harfbuzz=no" "--with-png=no")

Ports/SDL2_gfx/package.sh
@@ -6,6 +6,7 @@ files="https://downloads.sourceforge.net/project/sdl2gfx/SDL2_gfx-${version}.tar
 auth_type=sha256
 depends=("SDL2")
 useconfigure=true
+use_fresh_config_sub=true
 configopts=("--with-sdl-prefix=${SERENITY_INSTALL_ROOT}/usr/local")
 
 install() {

Ports/SDL2_image/package.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=SDL2_image
 useconfigure=true
+use_fresh_config_sub=true
 version=2.0.5
 depends=("SDL2" "libpng" "libjpeg" "libtiff")
 files="https://www.libsdl.org/projects/SDL_image/release/SDL2_image-${version}.tar.gz SDL_image-${version}.tar.gz bdd5f6e026682f7d7e1be0b6051b209da2f402a2dd8bd1c4bd9c25ad263108d0"

Ports/SDL2_mixer/package.sh
@@ -2,6 +2,7 @@
 port=SDL2_mixer
 version=2.0.4
 useconfigure=true
+use_fresh_config_sub=true
 files="https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-${version}.tar.gz SDL2_mixer-${version}.tar.gz b4cf5a382c061cd75081cf246c2aa2f9df8db04bdda8dcdc6b6cca55bede2419"
 auth_type=sha256
 depends=("libmodplug" "libvorbis" "SDL2")

Ports/SDL2_net/package.sh
@@ -2,6 +2,7 @@
 port=SDL2_net
 version=2.0.1
 useconfigure=true
+use_fresh_config_sub=true
 configopts=("--with-sdl-prefix=${SERENITY_INSTALL_ROOT}/usr/local")
 files="https://www.libsdl.org/projects/SDL_net/release/SDL2_net-${version}.tar.gz SDL2_net-${version}.tar.gz 15ce8a7e5a23dafe8177c8df6e6c79b6749a03fff1e8196742d3571657609d21"
 auth_type=sha256

Ports/SDL2_ttf/package.sh
@@ -2,6 +2,7 @@
 port=SDL2_ttf
 version=2.0.15
 useconfigure=true
+use_fresh_config_sub=true
 files="https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-${version}.tar.gz SDL2_ttf-${version}.tar.gz a9eceb1ad88c1f1545cd7bd28e7cbc0b2c14191d40238f531a15b01b1b22cd33"
 auth_type=sha256
 depends=("SDL2" "freetype")

Ports/angband/package.sh
@@ -3,6 +3,7 @@ port=angband
 version=4.2.3
 workdir="Angband-${version}"
 useconfigure=true
+use_fresh_config_sub=true
 files="https://github.com/angband/angband/releases/download/${version}/Angband-${version}.tar.gz Angband-${version}.tar.gz 833c4f8cff2aee61ad015f9346fceaa4a8c739fe2dbe5bd1acd580c91818e6bb"
 auth_type=sha256
 depends=("ncurses" "SDL2" "SDL2_image" "SDL2_ttf" "SDL2_mixer")

Ports/bash/package.sh
@@ -2,6 +2,8 @@
 port=bash
 version=5.1.16
 useconfigure=true
+use_fresh_config_sub=true
+config_sub_path=support/config.sub
 configopts=("--disable-nls" "--without-bash-malloc")
 files="https://ftpmirror.gnu.org/gnu/bash/bash-${version}.tar.gz bash-${version}.tar.gz 5bac17218d3911834520dad13cd1f85ab944e1c09ae1aba55906be1f8192f558"
 auth_type="sha256"

commit 1f854a78c8419670067c8ae3b76ceace078738de

Ports: Correct the config.sub path for freetype
This regressed in cdd6d68, setting the correct path makes it build
again.

Ports/freetype/package.sh
@@ -3,6 +3,7 @@ port=freetype
 version=2.10.4
 useconfigure=true
 use_fresh_config_sub=true
+config_sub_path=builds/unix/config.sub
 files="https://download.savannah.gnu.org/releases/freetype/freetype-${version}.tar.gz freetype-${version}.tar.gz 5eab795ebb23ac77001cfb68b7d4d50b5d6c7469247b0b01b2c953269f658dac"
 auth_type=sha256
 configopts=("--with-brotli=no" "--with-bzip2=no" "--with-zlib=no" "--with-harfbuzz=no" "--with-png=no")