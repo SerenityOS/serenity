249dde24e10760819f8ebf568c4ef4039d44aa7f
Tobias Christiansen <tobyase@serenityos.org>
2022-01-17 10:43:28 +0100

    Meta: Add icons to online man pages
    Previously all the added icons weren't available in the online version
    of the man pages. This patch adds functionality to copy all the used
    icons over when assembling the HTML version.

--- Meta/convert-markdown-links.lua
+++ Meta/convert-markdown-links.lua
@@ -3,3 +3,14 @@ function Link(el)
     el.target = string.gsub(el.target, "help://man/([^/]*)/(.*)", "../man%1/%2.html")
     return el
 end
+
+function Image(el)
+    local pattern = "/res/icons/(.*)"
+    local image = string.gsub(el.src, pattern, "%1")
+
+    el.src = "../icons/" .. image
+    file = io.open("icons.txt", "a+")
+    file:write(image .. "\n")
+    file:close()
+    return el
+end
