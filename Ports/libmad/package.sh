9703fbf52b5b1d127e4404c4ea340894376fa8b8
Jelle Raaijmakers <jelle@gmta.nl>
2022-01-17 18:03:59 +0100

    Ports: Add libmad

---
+++ Ports/libmad/package.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port=libmad
+version=0.15.1b
+useconfigure=true
+use_fresh_config_sub=true
+configopts=("--disable-static")
+files="https://downloads.sourceforge.net/mad/libmad-${version}.tar.gz libmad-${version}.tar.gz bbfac3ed6bfbc2823d3775ebb931087371e142bb0e9bb1bee51a76a6e0078690"
+auth_type=sha256
