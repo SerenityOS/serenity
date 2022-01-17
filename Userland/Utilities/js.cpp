ba4e443b08900d82840728466bf0c97fd39dbbe2
David Lindbom <hi@davidlindbom.se>
2022-01-17 20:23:04 +0100

    Utilities: Remove ifdef __serenity__ for calls to Core::System::pledge()

--- Userland/Utilities/js.cpp
+++ Userland/Utilities/js.cpp
@@ -1220,9 +1220,7 @@ private:
 
 ErrorOr<int> serenity_main(Main::Arguments arguments)
 {
-#ifdef __serenity__
     TRY(Core::System::pledge("stdio rpath wpath cpath tty sigaction"));
-#endif
 
     bool gc_on_every_allocation = false;
     bool disable_syntax_highlight = false;
