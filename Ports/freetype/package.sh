62192c88a6445a736e9948b8d2c890260da5d055
Sahan Fernando <sahan.h.fernando@gmail.com>
2022-01-17 22:44:56 +1100

    Ports: Fix freetype port

--- Ports/freetype/package.sh
+++ Ports/freetype/package.sh
@@ -7,6 +7,7 @@ config_sub_path=builds/unix/config.sub
 files="https://download.savannah.gnu.org/releases/freetype/freetype-${version}.tar.gz freetype-${version}.tar.gz 5eab795ebb23ac77001cfb68b7d4d50b5d6c7469247b0b01b2c953269f658dac"
 auth_type=sha256
 configopts=("--with-brotli=no" "--with-bzip2=no" "--with-zlib=no" "--with-harfbuzz=no" "--with-png=no")
+config_sub_path="builds/unix/config.sub"
 
 install() {
     run make DESTDIR=${SERENITY_INSTALL_ROOT} "${installopts[@]}" install
