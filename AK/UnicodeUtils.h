b87e517debb09f5bc2bf698e23ac3e3ede1eaf95
Timothy Flynn <trflynn89@pm.me>
2022-01-18 08:35:25 -0500

    AK: Remove now-unused AK::UnicodeUtils methods

--- AK/UnicodeUtils.h
+++ AK/UnicodeUtils.h
@@ -10,13 +10,6 @@
 
 namespace AK::UnicodeUtils {
 
-constexpr bool is_unicode_control_code_point(u32 code_point)
-{
-    return code_point < 0x20 || (code_point >= 0x80 && code_point < 0xa0);
-}
-
-Optional<StringView> get_unicode_control_code_point_alias(u32);
-
 template<typename Callback>
 [[nodiscard]] constexpr int code_point_to_utf8(u32 code_point, Callback callback)
 {
