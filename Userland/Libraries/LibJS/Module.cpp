384bbe78c045139bdcbb44c39d8c72de2c1d8537
davidot <davidot@serenityos.org>
2022-01-18 19:29:17 +0100

    LibJS: Implement Module linking and evaluating

--- Userland/Libraries/LibJS/Module.cpp
+++ Userland/Libraries/LibJS/Module.cpp
@@ -1,22 +1,120 @@
 /*
  * Copyright (c) 2021, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2022, David Tuin <davidot@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <LibJS/CyclicModule.h>
 #include <LibJS/Module.h>
+#include <LibJS/Runtime/ModuleNamespaceObject.h>
 
 namespace JS {
 
-Module::Module(Realm& realm, StringView filename)
-    : m_vm(realm.vm())
-    , m_realm(make_handle(&realm))
-    , m_filename(filename)
+Module::Module(Realm& realm, String filename)
+    : m_realm(make_handle(&realm))
+    , m_filename(move(filename))
 {
 }
 
 Module::~Module()
 {
+}
+
+// 16.2.1.5.1.1 InnerModuleLinking ( module, stack, index ), https://tc39.es/ecma262/#sec-InnerModuleLinking
+ThrowCompletionOr<u32> Module::inner_module_linking(VM& vm, Vector<Module*>&, u32 index)
+{
+    // Note: Until we have something extending module which is not SourceTextModule we crash.
+    VERIFY_NOT_REACHED();
+
+    // 1. If module is not a Cyclic Module Record, then
+
+    // a. Perform ? module.Link().
+    TRY(link(vm));
+    // b. Return index.
+    return index;
+}
+
+// 16.2.1.5.2.1 InnerModuleEvaluation ( module, stack, index ), https://tc39.es/ecma262/#sec-innermoduleevaluation
+ThrowCompletionOr<u32> Module::inner_module_evaluation(VM& vm, Vector<Module*>&, u32 index)
+{
+    // Note: Until we have something extending module which is not SourceTextModule we crash.
+    VERIFY_NOT_REACHED();
+
+    // 1. If module is not a Cyclic Module Record, then
+    // a. Let promise be ! module.Evaluate().
+    auto* promise = TRY(evaluate(vm));
+
+    // b. Assert: promise.[[PromiseState]] is not pending.
+    VERIFY(promise->state() != Promise::State::Pending);
+
+    // c. If promise.[[PromiseState]] is rejected, then
+    if (promise->state() == Promise::State::Rejected) {
+        // i. Return ThrowCompletion(promise.[[PromiseResult]]).
+        return throw_completion(promise->result());
+    }
+
+    // d. Return index.
+    return index;
+}
+
+// 16.2.1.10 GetModuleNamespace ( module ), https://tc39.es/ecma262/#sec-getmodulenamespace
+ThrowCompletionOr<Object*> Module::get_module_namespace(VM& vm)
+{
+    // 1. Assert: If module is a Cyclic Module Record, then module.[[Status]] is not unlinked.
+    // FIXME: How do we check this without breaking encapsulation?
+
+    // 2. Let namespace be module.[[Namespace]].
+    auto* namespace_ = m_namespace.is_null() ? nullptr : m_namespace.cell();
+
+    // 3. If namespace is empty, then
+    if (!namespace_) {
+        // a. Let exportedNames be ? module.GetExportedNames().
+        auto exported_names = TRY(get_exported_names(vm));
+
+        // b. Let unambiguousNames be a new empty List.
+        Vector<FlyString> unambiguous_names;
+
+        // c. For each element name of exportedNames, do
+        for (auto& name : exported_names) {
+            // i. Let resolution be ? module.ResolveExport(name).
+            auto resolution = TRY(resolve_export(vm, name));
+
+            // ii. If resolution is a ResolvedBinding Record, append name to unambiguousNames.
+            if (resolution.is_valid())
+                unambiguous_names.append(name);
+        }
+
+        // d. Set namespace to ModuleNamespaceCreate(module, unambiguousNames).
+        namespace_ = module_namespace_create(vm, unambiguous_names);
+        VERIFY(!m_namespace.is_null());
+        // Note: This set the local variable 'namespace' and not the member variable which is done by ModuleNamespaceCreate
+    }
+
+    // 4. Return namespace.
+    return namespace_;
+}
+
+// 10.4.6.12 ModuleNamespaceCreate ( module, exports ), https://tc39.es/ecma262/#sec-modulenamespacecreate
+Object* Module::module_namespace_create(VM& vm, Vector<FlyString> unambiguous_names)
+{
+    // 1. Assert: module.[[Namespace]] is empty.
+    VERIFY(m_namespace.is_null());
+
+    // 2. Let internalSlotsList be the internal slots listed in Table 34.
+    // 3. Let M be ! MakeBasicObject(internalSlotsList).
+    // 4. Set M's essential internal methods to the definitions specified in 10.4.6.
+    // 5. Set M.[[Module]] to module.
+    // 6. Let sortedExports be a List whose elements are the elements of exports ordered as if an Array of the same values had been sorted using %Array.prototype.sort% using undefined as comparefn.
+    // 7. Set M.[[Exports]] to sortedExports.
+    // 8. Create own properties of M corresponding to the definitions in 28.3.
+    Object* module_namespace = vm.heap().allocate<ModuleNamespaceObject>(realm().global_object(), realm().global_object(), this, move(unambiguous_names));
+
+    // 9. Set module.[[Namespace]] to M.
+    m_namespace = make_handle(module_namespace);
+
+    // 10. Return M.
+    return module_namespace;
 }
 
 }
--- Userland/Libraries/LibJS/SourceTextModule.h
+++ Userland/Libraries/LibJS/SourceTextModule.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2021, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2022, David Tuin <davidot@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -7,24 +8,56 @@
 #pragma once
 
 #include <LibJS/AST.h>
+#include <LibJS/CyclicModule.h>
 #include <LibJS/Forward.h>
-#include <LibJS/Module.h>
 #include <LibJS/Parser.h>
 
 namespace JS {
 
 // 16.2.1.6 Source Text Module Records, https://tc39.es/ecma262/#sec-source-text-module-records
-class SourceTextModule final : public Module {
+class SourceTextModule final : public CyclicModule {
 public:
+    using ImportEntry = ImportStatement::ImportEntry;
+    using ExportEntry = ExportStatement::ExportEntry;
+
     static Result<NonnullRefPtr<SourceTextModule>, Vector<Parser::Error>> parse(StringView source_text, Realm&, StringView filename = {});
-    virtual ~SourceTextModule();
 
     Program const& parse_node() const { return *m_ecmascript_code; }
 
-private:
-    explicit SourceTextModule(Realm&, StringView filename, NonnullRefPtr<Program>);
+    virtual ThrowCompletionOr<Vector<FlyString>> get_exported_names(VM& vm, Vector<Module*> export_star_set) override;
+    virtual ThrowCompletionOr<ResolvedBinding> resolve_export(VM& vm, FlyString const& export_name, Vector<ResolvedBinding> resolve_set = {}) override;
 
-    NonnullRefPtr<Program> m_ecmascript_code; // [[ECMAScriptCode]]
+    Object* import_meta()
+    {
+        if (m_import_meta.is_null())
+            return nullptr;
+        return m_import_meta.cell();
+    }
+
+    void set_import_meta(Badge<MetaProperty>, Object* import_meta)
+    {
+        m_import_meta = make_handle(import_meta);
+    }
+
+protected:
+    virtual Completion initialize_environment(VM& vm) override;
+    virtual Completion execute_module(VM& vm, Optional<PromiseCapability> capability) override;
+
+private:
+    SourceTextModule(Realm&, StringView filename, bool has_top_level_await, NonnullRefPtr<Program> body, Vector<FlyString> requested_modules,
+        Vector<ImportEntry> import_entries, Vector<ExportEntry> local_export_entries,
+        Vector<ExportEntry> indirect_export_entries, Vector<ExportEntry> star_export_entries,
+        RefPtr<ExportStatement> default_export);
+
+    NonnullRefPtr<Program> m_ecmascript_code;      // [[ECMAScriptCode]]
+    ExecutionContext m_execution_context;          // [[Context]]
+    Handle<Object> m_import_meta;                  // [[ImportMeta]]
+    Vector<ImportEntry> m_import_entries;          // [[ImportEntries]]
+    Vector<ExportEntry> m_local_export_entries;    // [[LocalExportEntries]]
+    Vector<ExportEntry> m_indirect_export_entries; // [[IndirectExportEntries]]
+    Vector<ExportEntry> m_star_export_entries;     // [[StarExportEntries]]
+
+    RefPtr<ExportStatement> m_default_export; // Note: Not from the spec
 };
 
 }
