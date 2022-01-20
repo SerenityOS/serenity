ece59948c3e102dc6a40166cb397bcc6e6f775f7
Tobias Christiansen <tobyase@serenityos.org>
2022-01-20 19:58:02 +0100

    LibGfx: Expand TextAttributes with more information about underlining
    This adds a seperate Color to be used for underlines as well as support
    for different underline styles.

--- Userland/Libraries/LibGfx/TextAttributes.h
+++ Userland/Libraries/LibGfx/TextAttributes.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2021, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2022, Tobias Christiansen <tobyase@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -12,10 +13,18 @@
 namespace Gfx {
 
 struct TextAttributes {
+    enum class UnderlineStyle {
+        Solid,
+        Wavy
+    };
+
     Color color;
-    Optional<Color> background_color;
+    Optional<Color> background_color {};
     bool underline { false };
     bool bold { false };
+
+    Optional<Color> underline_color {};
+    UnderlineStyle underline_style { UnderlineStyle::Solid };
 };
 
 }
