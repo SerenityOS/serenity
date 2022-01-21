ce2f8b5b1b436d98f2e93763aeed8102c7b31e19
Mustafa Quraish <mustafaq9@gmail.com>
2022-01-21 03:59:25 -0500

    PixelPaint: Change menu shortcut for "Fit Image to View"
    This makes it consistent with `ImageViewer`, where `F` was unavailable
    for use as a shortcut. A small change, but it's nice having the same
    keyboard shortcuts where we can :^)

--- Userland/Applications/PixelPaint/MainWidget.cpp
+++ Userland/Applications/PixelPaint/MainWidget.cpp
@@ -378,7 +378,7 @@ void MainWidget::initialize_menubar(GUI::Window& window)
     m_view_menu->add_action(*m_zoom_out_action);
     m_view_menu->add_action(*m_reset_zoom_action);
     m_view_menu->add_action(GUI::Action::create(
-        "&Fit Image To View", [&](auto&) {
+        "Fit Image To &View", [&](auto&) {
             auto* editor = current_image_editor();
             VERIFY(editor);
             editor->fit_image_to_view();
