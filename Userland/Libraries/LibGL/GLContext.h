170739fe3905ed40603bd08af436fa31ef56cdbc
Jesse Buhagiar <jooster669@gmail.com>
2022-01-24 18:15:40 +1100

    LibGL: Implement `glLighti{v}`

--- Userland/Libraries/LibGL/GLContext.h
+++ Userland/Libraries/LibGL/GLContext.h
@@ -116,6 +116,7 @@ public:
     virtual void gl_tex_gen_floatv(GLenum coord, GLenum pname, GLfloat const* params) = 0;
     virtual void gl_lightf(GLenum light, GLenum pname, GLfloat param) = 0;
     virtual void gl_lightfv(GLenum light, GLenum pname, GLfloat const* params) = 0;
+    virtual void gl_lightiv(GLenum light, GLenum pname, GLint const* params) = 0;
     virtual void gl_materialf(GLenum face, GLenum pname, GLfloat param) = 0;
     virtual void gl_materialfv(GLenum face, GLenum pname, GLfloat const* params) = 0;
     virtual void gl_color_material(GLenum face, GLenum mode) = 0;
