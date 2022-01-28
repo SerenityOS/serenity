125c05f420a7e8fbb809e65f4070cba93bc8f400
Timothy Flynn <trflynn89@pm.me>
2022-01-28 07:40:29 -0500

    Revert "FileManager: Display times in the user's local time zone"
    This reverts commit 080b054695f03df2f731d9a519432e3f196f48e6.

--- Userland/Applications/FileManager/main.cpp
+++ Userland/Applications/FileManager/main.cpp
@@ -50,7 +50,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/wait.h>
-#include <time.h>
 #include <unistd.h>
 
 using namespace FileManager;
@@ -88,7 +87,6 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
     auto app = TRY(GUI::Application::try_create(arguments));
 
     TRY(Core::System::pledge("stdio thread recvfd sendfd cpath rpath wpath fattr proc exec unix"));
-    tzset();
 
     Config::pledge_domains({ "FileManager", "WindowManager" });
     Config::monitor_domain("FileManager");
