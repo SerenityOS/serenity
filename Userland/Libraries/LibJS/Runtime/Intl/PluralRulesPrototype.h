74939eb9434284437386404da4292d42ba1e7a1b
Timothy Flynn <trflynn89@pm.me>
2022-01-28 13:42:20 -0500

    LibJS: Implement Intl.PluralRules.prototype.resolvedOptions

--- Userland/Libraries/LibJS/Runtime/Intl/PluralRulesPrototype.h
+++ Userland/Libraries/LibJS/Runtime/Intl/PluralRulesPrototype.h
@@ -18,6 +18,9 @@ public:
     explicit PluralRulesPrototype(GlobalObject&);
     virtual void initialize(GlobalObject&) override;
     virtual ~PluralRulesPrototype() override = default;
+
+private:
+    JS_DECLARE_NATIVE_FUNCTION(resolved_options);
 };
 
 }
