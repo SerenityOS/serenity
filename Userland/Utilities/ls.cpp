0de985853b9c7871fb53f5489b0d84126d65b9c4
Timothy Flynn <trflynn89@pm.me>
2022-01-28 07:40:19 -0500

    Revert "ls: Display times in the user's local time zone"
    This reverts commit 0c13a3a8ff07a7dde8acc30ede885685c224ef64.

--- Userland/Utilities/ls.cpp
+++ Userland/Utilities/ls.cpp
@@ -77,7 +77,6 @@ static bool is_a_tty = false;
 ErrorOr<int> serenity_main(Main::Arguments arguments)
 {
     TRY(Core::System::pledge("stdio rpath tty"));
-    tzset();
 
     struct winsize ws;
     int rc = ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
