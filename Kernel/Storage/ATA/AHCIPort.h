e1ad9326df316949ded81dfba4c68bc4eea608c8
Idan Horowitz <idan.horowitz@gmail.com>
2022-01-28 16:37:50 +0200

    Kernel: Remove unimplemented AHCIPort::is_hot_pluggable declaration

--- Kernel/Storage/ATA/AHCIPort.h
+++ Kernel/Storage/ATA/AHCIPort.h
@@ -42,7 +42,6 @@ public:
     u32 port_index() const { return m_port_index; }
     u32 representative_port_index() const { return port_index() + 1; }
     bool is_operable() const;
-    bool is_hot_pluggable() const;
     bool is_atapi_attached() const { return m_port_registers.sig == (u32)AHCI::DeviceSignature::ATAPI; };
 
     RefPtr<StorageDevice> connected_device() const { return m_connected_device; }
