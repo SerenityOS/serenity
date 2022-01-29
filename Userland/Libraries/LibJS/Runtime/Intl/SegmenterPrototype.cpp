0707cbd496d744b61c60bca7826854130beaf412
Idan Horowitz <idan.horowitz@gmail.com>
2022-01-30 00:12:47 +0200

    LibJS: Implement Intl.Segmenter.prototype.resolvedOptions

--- Userland/Libraries/LibJS/Runtime/Intl/SegmenterPrototype.cpp
+++ Userland/Libraries/LibJS/Runtime/Intl/SegmenterPrototype.cpp
@@ -24,6 +24,31 @@ void SegmenterPrototype::initialize(GlobalObject& global_object)
 
     // 18.3.2 Intl.Segmenter.prototype [ @@toStringTag ], https://tc39.es/ecma402/#sec-intl.segmenter.prototype-@@tostringtag
     define_direct_property(*vm.well_known_symbol_to_string_tag(), js_string(vm, "Intl.Segmenter"), Attribute::Configurable);
+
+    u8 attr = Attribute::Writable | Attribute::Configurable;
+    define_native_function(vm.names.resolvedOptions, resolved_options, 0, attr);
+}
+
+// 18.3.4 Intl.Segmenter.prototype.resolvedOptions ( ), https://tc39.es/ecma402/#sec-intl.segmenter.prototype.resolvedoptions
+JS_DEFINE_NATIVE_FUNCTION(SegmenterPrototype::resolved_options)
+{
+    // 1. Let segmenter be the this value.
+    // 2. Perform ? RequireInternalSlot(segmenter, [[InitializedSegmenter]]).
+    auto* segmenter = TRY(typed_this_object(global_object));
+
+    // 3. Let options be ! OrdinaryObjectCreate(%Object.prototype%).
+    auto* options = Object::create(global_object, global_object.object_prototype());
+
+    // 4. For each row of Table 16, except the header row, in table order, do
+    //     a. Let p be the Property value of the current row.
+    //     b. Let v be the value of segmenter's internal slot whose name is the Internal Slot value of the current row.
+    //     c. Assert: v is not undefined.
+    //     d. Perform ! CreateDataPropertyOrThrow(options, p, v).
+    MUST(options->create_data_property_or_throw(vm.names.locale, js_string(vm, segmenter->locale())));
+    MUST(options->create_data_property_or_throw(vm.names.granularity, js_string(vm, segmenter->segmenter_granularity_string())));
+
+    // 5. Return options.
+    return options;
 }
 
 }
--- Userland/Libraries/LibJS/Runtime/Intl/SegmenterPrototype.h
+++ Userland/Libraries/LibJS/Runtime/Intl/SegmenterPrototype.h
@@ -18,6 +18,9 @@ public:
     explicit SegmenterPrototype(GlobalObject&);
     virtual void initialize(GlobalObject&) override;
     virtual ~SegmenterPrototype() override = default;
+
+private:
+    JS_DECLARE_NATIVE_FUNCTION(resolved_options);
 };
 
 }
