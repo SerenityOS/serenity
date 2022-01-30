454e7a9c34e3bacf67a3489717467ccc365e6d82
Kenneth Myhra <kennethmyhra@gmail.com>
2022-01-30 20:01:49 +0100

    open: Port to LibMain

--- Userland/Utilities/open.cpp
+++ Userland/Utilities/open.cpp
@@ -11,15 +11,16 @@
 #include <LibCore/EventLoop.h>
 #include <LibCore/File.h>
 #include <LibDesktop/Launcher.h>
+#include <LibMain/Main.h>
 
-int main(int argc, char* argv[])
+ErrorOr<int> serenity_main(Main::Arguments arguments)
 {
     Core::EventLoop loop;
     Vector<const char*> urls_or_paths;
     Core::ArgsParser parser;
     parser.set_general_help("Open a file or URL by executing the appropriate program.");
     parser.add_positional_argument(urls_or_paths, "URL or file path to open", "url-or-path");
-    parser.parse(argc, argv);
+    parser.parse(arguments);
 
     bool all_ok = true;
 
