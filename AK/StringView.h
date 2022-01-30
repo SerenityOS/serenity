SerenityOS/serenity

commit ca58c71faa6e31721b6094d380732d2aa6f3d791

AK: Implement all comparison operators for StringView

AK/StringView.h
@@ -204,28 +204,37 @@ public:
 
     bool operator==(const String&) const;
 
+    [[nodiscard]] constexpr int compare(StringView other) const
+    {
+        size_t rlen = min(length(), other.length());
+        int c = (rlen != 0) ? __builtin_memcmp(m_characters, other.m_characters, rlen) : 0;
+        if (c == 0) {
+            if (length() < other.length())
+                return -1;
+            if (length() == other.length())
+                return 0;
+            return 1;
+        }
+        return c;
+    }
+
     constexpr bool operator==(StringView other) const
     {
-        if (is_null())
-            return other.is_null();
-        if (other.is_null())
-            return false;
-        if (length() != other.length())
-            return false;
-        return __builtin_memcmp(m_characters, other.m_characters, m_length) == 0;
+        return length() == other.length() && compare(other) == 0;
     }
 
     constexpr bool operator!=(StringView other) const
     {
-        return !(*this == other);
+        return length() != other.length() || compare(other) != 0;
     }
 
-    bool operator<(StringView other) const
-    {
-        if (int c = __builtin_memcmp(m_characters, other.m_characters, min(m_length, other.m_length)))
-            return c < 0;
-        return m_length < other.m_length;
-    }
+    constexpr bool operator<(StringView other) const { return compare(other) < 0; }
+
+    constexpr bool operator<=(StringView other) const { return compare(other) <= 0; }
+
+    constexpr bool operator>(StringView other) const { return compare(other) > 0; }
+
+    constexpr bool operator>=(StringView other) const { return compare(other) >= 0; }
 
     [[nodiscard]] String to_string() const;
 

AK/StringView.cpp
@@ -203,15 +203,7 @@ template Optional<long long> StringView::to_uint() const;
 
 bool StringView::operator==(const String& string) const
 {
-    if (string.is_null())
-        return !m_characters;
-    if (!m_characters)
-        return false;
-    if (m_length != string.length())
-        return false;
-    if (m_characters == string.characters())
-        return true;
-    return !__builtin_memcmp(m_characters, string.characters(), m_length);
+    return *this == string.view();
 }
 
 String StringView::to_string() const { return String { *this }; }

commit d0a1c185f3ca77e0fb91a71ad28a1463e92f1295

AK+Tests: Make null strings compare less than non-null strings
This behavior regressed in ca58c71faa6e31721b6094d380732d2aa6f3d791.

Fixes #12213

AK/StringView.h
@@ -206,8 +206,14 @@ public:
 
     [[nodiscard]] constexpr int compare(StringView other) const
     {
-        size_t rlen = min(length(), other.length());
-        int c = (rlen != 0) ? __builtin_memcmp(m_characters, other.m_characters, rlen) : 0;
+        if (m_characters == nullptr)
+            return other.m_characters ? -1 : 0;
+
+        if (other.m_characters == nullptr)
+            return 1;
+
+        size_t rlen = min(m_length, other.m_length);
+        int c = __builtin_memcmp(m_characters, other.m_characters, rlen);
         if (c == 0) {
             if (length() < other.length())
                 return -1;

Tests/AK/TestString.cpp
@@ -42,10 +42,7 @@ TEST_CASE(construct_contents)
 
 TEST_CASE(equal)
 {
-    // FIXME: Enable this as soon as it's fixed.
-#if 0
     EXPECT_NE(String::empty(), String {});
-#endif
 }
 
 TEST_CASE(compare)