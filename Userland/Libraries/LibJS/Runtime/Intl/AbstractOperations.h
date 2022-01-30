a120e85596cc671a60ed1f29c7e6d8f95870f203
Timothy Flynn <trflynn89@pm.me>
2022-01-30 17:46:38 -0500

    LibJS: Provide public access to list of sanctioned unit identifiers

--- Userland/Libraries/LibJS/Runtime/Intl/AbstractOperations.h
+++ Userland/Libraries/LibJS/Runtime/Intl/AbstractOperations.h
@@ -6,6 +6,7 @@
 
 #pragma once
 
+#include <AK/Array.h>
 #include <AK/Span.h>
 #include <AK/String.h>
 #include <AK/Variant.h>
@@ -52,6 +53,11 @@ struct PatternPartition {
     StringView type;
     String value;
 };
+
+constexpr auto sanctioned_simple_unit_identifiers()
+{
+    return AK::Array { "acre"sv, "bit"sv, "byte"sv, "celsius"sv, "centimeter"sv, "day"sv, "degree"sv, "fahrenheit"sv, "fluid-ounce"sv, "foot"sv, "gallon"sv, "gigabit"sv, "gigabyte"sv, "gram"sv, "hectare"sv, "hour"sv, "inch"sv, "kilobit"sv, "kilobyte"sv, "kilogram"sv, "kilometer"sv, "liter"sv, "megabit"sv, "megabyte"sv, "meter"sv, "mile"sv, "mile-scandinavian"sv, "milliliter"sv, "millimeter"sv, "millisecond"sv, "minute"sv, "month"sv, "ounce"sv, "percent"sv, "petabyte"sv, "pound"sv, "second"sv, "stone"sv, "terabit"sv, "terabyte"sv, "week"sv, "yard"sv, "year"sv };
+}
 
 Optional<Unicode::LocaleID> is_structurally_valid_language_tag(StringView locale);
 String canonicalize_unicode_locale_id(Unicode::LocaleID& locale);
