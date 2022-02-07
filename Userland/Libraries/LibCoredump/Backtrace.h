14900fa4a799b03dbf54f54709c717ef4e95ab34
Sviatoslav Peleshko <speles@mail.ua>
2022-02-07 05:42:05 +0200

    LibCoredump: Respect coredump's LD_LIBRARY_PATH when searching libraries
    Previously, we would only resolve libraries from `/usr/lib`, which is
    not the only path from which the crashed process could've loaded the
    libraries from.

--- Userland/Libraries/LibCoredump/Backtrace.h
+++ Userland/Libraries/LibCoredump/Backtrace.h
@@ -45,7 +45,7 @@ public:
 
 private:
     void add_entry(const Reader&, FlatPtr ip);
-    ELFObjectInfo const* object_info_for_region(MemoryRegionInfo const&);
+    ELFObjectInfo const* object_info_for_region(Reader const&, MemoryRegionInfo const&);
 
     bool m_skip_loader_so { false };
     ELF::Core::ThreadInfo m_thread_info;
