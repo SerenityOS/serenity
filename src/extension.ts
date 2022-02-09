d2303efed2d8d4504a1de3120ddabf50417e33ce
Hendiadyoin1 <leon2002.la@gmail.com>
2022-02-09 21:48:15 +0100

    Meta/VSCodeExtension: Fix gml-formatter not informing vsCode of changes
    This also removes a document.save() which did not actually work.

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,25 @@
 'use strict';
 import * as vscode from 'vscode';
+import {Range, Position, TextEdit} from 'vscode';
 import * as fs from 'fs';
+import * as tmp from 'tmp';
 import {spawnSync} from 'child_process';
 
 export function activate(context: vscode.ExtensionContext) {
   vscode.languages.registerDocumentFormattingEditProvider('serenity-gml', {
     provideDocumentFormattingEdits(document: vscode.TextDocument) {
-      // FIXME: gml-format does not support files over stdin, yet,
-      //        so we have to force-save it for now
       // FIXME: Allow partial re-formatting
-      document.save();
+
       let filename = document.fileName;
       let rootPath = vscode.workspace.rootPath;
-      let eof = document.positionAt(document.getText().length);
+      let fileContent = document.getText()
+      let eof = document.positionAt(fileContent.length);
+
+      // FIXME: gml-format does not support files over stdin, yet,
+      //        so we have to create a temporary file with the files content
+      let tmpFile = tmp.fileSync();
+      fs.writeSync(tmpFile.fd, fileContent);
+      fs.closeSync(tmpFile.fd);
 
       let executablePath = rootPath + '/Build/lagom/gml-format'
 
@@ -22,14 +29,17 @@ export function activate(context: vscode.ExtensionContext) {
         return [];
       }
 
-      let childOutput = spawnSync(executablePath, ['-i', filename]);
+      let childOutput = spawnSync(executablePath, [tmpFile.name]);
       if (childOutput.status != 0) {
         vscode.window.showErrorMessage(
             `gml-format failed with exit-code: ${childOutput.status}`,
             childOutput.stderr.toString());
+        return [];
       }
+      console.log(childOutput.stdout);
 
-      return [];
+      return [TextEdit.replace(
+          new Range(new Position(0, 0), eof), childOutput.stdout.toString())];
     }
   });
 }
