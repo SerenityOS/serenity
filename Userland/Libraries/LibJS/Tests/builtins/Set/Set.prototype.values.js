821ae3a4798b01eeca44304b4145f899011d1c1b
davidot <davidot@serenityos.org>
2022-02-10 11:33:05 +0100

    LibJS: Add tests for Set.prototype.keys which is an alias for values

--- Userland/Libraries/LibJS/Tests/builtins/Set/Set.prototype.values.js
+++ Userland/Libraries/LibJS/Tests/builtins/Set/Set.prototype.values.js
@@ -12,3 +12,20 @@ test("basic functionality", () => {
     expect(it.next()).toEqual({ value: undefined, done: true });
     expect(it.next()).toEqual({ value: undefined, done: true });
 });
+
+describe("keys is an alias for values", () => {
+    test("length", () => {
+        expect(Set.prototype.keys.length).toBe(0);
+    });
+
+    test("basic functionality", () => {
+        const a = new Set([1, 2, 3]);
+        const it = a.keys();
+        expect(it.next()).toEqual({ value: 1, done: false });
+        expect(it.next()).toEqual({ value: 2, done: false });
+        expect(it.next()).toEqual({ value: 3, done: false });
+        expect(it.next()).toEqual({ value: undefined, done: true });
+        expect(it.next()).toEqual({ value: undefined, done: true });
+        expect(it.next()).toEqual({ value: undefined, done: true });
+    });
+});
