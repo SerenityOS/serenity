7b14fa557c41ab9e269133349ef0eb54a12082d2
sin-ack <sin-ack@users.noreply.github.com>
2022-02-10 11:26:26 +0000

    [DO NOT MERGE] DynamicLoader: Disable check for 0-sized TLS symbols

--- Userland/Libraries/LibELF/DynamicLoader.cpp
+++ Userland/Libraries/LibELF/DynamicLoader.cpp
@@ -577,7 +577,7 @@ void DynamicLoader::copy_initial_tls_data_into(ByteBuffer& buffer) const
             return IterationDecision::Continue;
 
         ssize_t negative_offset = negative_offset_from_tls_block_end(m_tls_offset, symbol.value());
-        VERIFY(symbol.size() != 0);
+        // VERIFY(symbol.size() != 0);
         VERIFY(buffer.size() + negative_offset + symbol.size() <= buffer.size());
         memcpy(buffer.data() + buffer.size() + negative_offset, tls_data + symbol.value(), symbol.size());
 
