b0efca5742ff2ef86268fd10bdedd2dda5034da3
Ali Mohammad Pur <ali.mpfard@gmail.com>
2022-02-11 17:49:54 +0330

    Ports: Add a sparsehash port

---
+++ Ports/sparsehash/package.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port=sparsehash
+version=2.0.4
+useconfigure=true
+files="https://github.com/sparsehash/sparsehash/archive/refs/tags/$port-$version.tar.gz $port-$version.tar.gz 8cd1a95827dfd8270927894eb77f62b4087735cbede953884647f16c521c7e58"
+auth_type=sha256
+workdir=$port-$port-$version
