b760a1a4bacfbaf57dbb7d424a9c25d8db96f10f
Ali Mohammad Pur <ali.mpfard@gmail.com>
2022-02-13 17:06:03 +0330

    Ports: Update the citron patches after upstreaming a few
    This also switches to checking out a specific commit instead of just the
    master HEAD, as the port linter requires a hash (which is imo pointless
    in this case), and we can't provide a stable hash for the master branch
    HEAD.

--- Ports/citron/package.sh
+++ Ports/citron/package.sh
@@ -3,9 +3,11 @@ port=citron
 version=0.0.9.3
 useconfigure=false
 depends=(sparsehash libffi)
-files="https://github.com/alimpfard/citron/archive/refs/heads/master.tar.gz citron.tar.gz d2a607bb662e2d862503a5eeb034ae65ce0bb9da5ad48c289d5325f2d0e14167"
+commit_hash=d28b7d62bd61397e46152aa6e4ee59b115c0e2d7
+archive_hash=0e31ab638c4fd1438f68fdf069336e2541eb4cfc5db2f55888f6175e0171a2ef
+files="https://github.com/alimpfard/citron/archive/$commit_hash.tar.gz citron.tar.gz $archive_hash"
 auth_type=sha256
-workdir=citron-master
+workdir="citron-$commit_hash"
 
 pre_install() {
     pushd "$workdir"
