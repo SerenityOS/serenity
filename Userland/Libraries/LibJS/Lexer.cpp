92e0378dbd416633013377ace597e1c02ae6bef4
Andreas Kling <kling@serenityos.org>
2022-02-13 13:53:52 +0100

    LibJS: Always inline Lexer::current_code_point()
    This gives a ~1% speedup when parsing the largest Discord JS file.

--- Userland/Libraries/LibJS/Lexer.cpp
+++ Userland/Libraries/LibJS/Lexer.cpp
@@ -358,7 +358,7 @@ ALWAYS_INLINE bool Lexer::is_unicode_character() const
     return (m_current_char & 128) != 0;
 }
 
-u32 Lexer::current_code_point() const
+ALWAYS_INLINE u32 Lexer::current_code_point() const
 {
     static constexpr const u32 REPLACEMENT_CHARACTER = 0xFFFD;
     if (m_position == 0)
