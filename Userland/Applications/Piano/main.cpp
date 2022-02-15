af5037eb5d038887a6dd3a4d094d718c69dd5803
electrikmilk <brandonjordan124@gmail.com>
2022-02-14 22:46:03 -0500

    Base+Piano: Insert missing icons into menubar
    Inserts icons for 'Export', 'Add Track' and 'Next Track'.

--- Userland/Applications/Piano/main.cpp
+++ Userland/Applications/Piano/main.cpp
@@ -54,7 +54,7 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
     main_widget_updater->start();
 
     auto& file_menu = window->add_menu("&File");
-    file_menu.add_action(GUI::Action::create("Export", { Mod_Ctrl, Key_E }, [&](const GUI::Action&) {
+    file_menu.add_action(GUI::Action::create("Export", { Mod_Ctrl, Key_E }, TRY(Gfx::Bitmap::try_load_from_file("/res/icons/16x16/file-export.png")), [&](const GUI::Action&) {
         save_path = GUI::FilePicker::get_save_filepath(window, "Untitled", "wav");
         if (!save_path.has_value())
             return;
