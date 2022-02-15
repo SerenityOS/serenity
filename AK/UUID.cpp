8f3d4a300796fda525f72a5d2f3544e7f118ab3d
Idan Horowitz <idan.horowitz@gmail.com>
2022-02-15 23:25:25 +0200

    AK: Return KString instead of String from encode_hex in the Kernel
    This let's us propagate allocation errors from this API.

--- AK/UUID.cpp
+++ AK/UUID.cpp
@@ -80,15 +80,20 @@ UUID::UUID(StringView uuid_string_view, Endianness endianness)
 ErrorOr<NonnullOwnPtr<Kernel::KString>> UUID::to_string() const
 {
     StringBuilder builder(36);
-    TRY(builder.try_append(encode_hex(m_uuid_buffer.span().trim(4)).view()));
+    auto nibble0 = TRY(encode_hex(m_uuid_buffer.span().trim(4)));
+    TRY(builder.try_append(nibble0->view()));
     TRY(builder.try_append('-'));
-    TRY(builder.try_append(encode_hex(m_uuid_buffer.span().slice(4).trim(2)).view()));
+    auto nibble1 = TRY(encode_hex(m_uuid_buffer.span().slice(4).trim(2)));
+    TRY(builder.try_append(nibble1->view()));
     TRY(builder.try_append('-'));
-    TRY(builder.try_append(encode_hex(m_uuid_buffer.span().slice(6).trim(2)).view()));
+    auto nibble2 = TRY(encode_hex(m_uuid_buffer.span().slice(6).trim(2)));
+    TRY(builder.try_append(nibble2->view()));
     TRY(builder.try_append('-'));
-    TRY(builder.try_append(encode_hex(m_uuid_buffer.span().slice(8).trim(2)).view()));
+    auto nibble3 = TRY(encode_hex(m_uuid_buffer.span().slice(8).trim(2)));
+    TRY(builder.try_append(nibble3->view()));
     TRY(builder.try_append('-'));
-    TRY(builder.try_append(encode_hex(m_uuid_buffer.span().slice(10).trim(6)).view()));
+    auto nibble4 = TRY(encode_hex(m_uuid_buffer.span().slice(10).trim(6)));
+    TRY(builder.try_append(nibble4->view()));
     return Kernel::KString::try_create(builder.string_view());
 }
 #else
