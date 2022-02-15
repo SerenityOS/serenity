928eef14c26b9914776198f2734c58942fb63fad
Timothy Flynn <trflynn89@pm.me>
2022-02-15 14:31:07 -0500

    LibUnicode: Use BCP 47 data to filter valid numbering system names
    There isn't too much of an effective difference here other than that the
    BCP 47 data contains some aliases we would otherwise not handle.

--- Userland/Libraries/LibUnicode/NumberFormat.cpp
+++ Userland/Libraries/LibUnicode/NumberFormat.cpp
@@ -16,7 +16,6 @@
 
 namespace Unicode {
 
-Optional<NumberSystem> __attribute__((weak)) number_system_from_string(StringView) { return {}; }
 Optional<StringView> __attribute__((weak)) get_number_system_symbol(StringView, StringView, NumericSymbol) { return {}; }
 Optional<NumberGroupings> __attribute__((weak)) get_number_system_groupings(StringView, StringView) { return {}; }
 Optional<NumberFormat> __attribute__((weak)) get_standard_number_system_format(StringView, StringView, StandardNumberFormatType) { return {}; }
