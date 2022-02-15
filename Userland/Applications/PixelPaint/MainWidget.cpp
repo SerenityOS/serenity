7e5b3ce26b96a68b271c0e5737a4dd80bbffdf0a
electrikmilk <brandonjordan124@gmail.com>
2022-02-14 23:49:20 -0500

    PixelPaint: Implement Merge Active Layer Up
    Implements 'Merge Active Layer Up' and adds it to the 'Layer' menu.

--- Userland/Applications/PixelPaint/MainWidget.cpp
+++ Userland/Applications/PixelPaint/MainWidget.cpp
@@ -592,6 +592,17 @@ void MainWidget::initialize_menubar(GUI::Window& window)
         }));
 
     m_layer_menu->add_action(GUI::Action::create(
+        "Merge &Active Layer Up", [&](auto&) {
+            auto* editor = current_image_editor();
+            VERIFY(editor);
+            auto active_layer = editor->active_layer();
+            if (!active_layer)
+                return;
+            editor->image().merge_active_layer_up(*active_layer);
+            editor->did_complete_action();
+        }));
+
+    m_layer_menu->add_action(GUI::Action::create(
         "M&erge Active Layer Down", { Mod_Ctrl, Key_E }, [&](auto&) {
             auto* editor = current_image_editor();
             VERIFY(editor);
