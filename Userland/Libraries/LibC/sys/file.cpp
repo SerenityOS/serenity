eaaa90504bd9e2acf2172b013642ace2d418e9a8
sin-ack <sin-ack@users.noreply.github.com>
2022-02-15 16:52:55 +0000

    [DO NOT MERGE] LibC: Comment out flock implementation
    Otherwise Zig locks up and/or crashes due to our implementation not
    being up to par.

--- Userland/Libraries/LibC/sys/file.cpp
+++ Userland/Libraries/LibC/sys/file.cpp
@@ -13,14 +13,17 @@ extern "C" {
 
 int flock(int fd, int operation)
 {
-    struct flock lock {
-        short(operation & 0b11), SEEK_SET, 0, 0, 0
-    };
-    if (operation & LOCK_NB) {
-        return fcntl(fd, F_SETLK, &lock);
-    }
+    (void)fd;
+    (void)operation;
+    // struct flock lock {
+    //     short(operation & 0b11), SEEK_SET, 0, 0, 0
+    // };
+    // if (operation & LOCK_NB) {
+    //     return fcntl(fd, F_SETLK, &lock);
+    // }
 
     // FIXME: Implement F_SETLKW in fcntl.
-    VERIFY_NOT_REACHED();
+    // VERIFY_NOT_REACHED();
+    return 0;
 }
 }
