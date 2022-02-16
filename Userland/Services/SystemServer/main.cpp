7ab8f6af356e88f0a6a0bef3cf0ef636ff6907d4
Michel Hermier <michel.hermier@gmail.com>
2022-02-16 15:24:23 +0100

    SystemServer: Use `wellknown_char_devices` table for `/dev/zero`

--- Userland/Services/SystemServer/main.cpp
+++ Userland/Services/SystemServer/main.cpp
@@ -197,6 +197,7 @@ struct wellknown_devices {
 static wellknown_devices const wellknown_char_devices[] = {
     { makedev(  1,   1), "mem",       0660 },
     { makedev(  1,   3), "null",      0666 },
+    { makedev(  1,   5), "zero",      0666 },
 };
 // clang-format on
 
@@ -290,10 +291,6 @@ static void populate_devtmpfs_devices_based_on_devctl()
         case 1: {
             if (!is_block_device) {
                 switch (minor_number) {
-                case 5: {
-                    create_devtmpfs_char_device("/dev/zero", 0666, 1, 5);
-                    break;
-                }
                 case 8: {
                     create_devtmpfs_char_device("/dev/random", 0666, 1, 8);
                     break;
