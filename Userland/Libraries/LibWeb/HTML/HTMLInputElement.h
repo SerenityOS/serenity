8edade071d5d404f40b0056e71e2161868951ed8
Adam Hodgen <ant1441@gmail.com>
2022-02-17 21:22:36 +0000

    LibWeb: Reflect only known values for <input> element's type attribute

--- Userland/Libraries/LibWeb/HTML/HTMLInputElement.h
+++ Userland/Libraries/LibWeb/HTML/HTMLInputElement.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2018-2022, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2022, Adam Hodgen <ant1441@gmail.com>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -10,6 +11,31 @@
 #include <LibWeb/HTML/HTMLElement.h>
 
 namespace Web::HTML {
+
+// https://html.spec.whatwg.org/multipage/input.html#attr-input-type
+#define ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTES                \
+    __ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTE(hidden)           \
+    __ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTE(text)             \
+    __ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTE(search)           \
+    __ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTE(tel)              \
+    __ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTE(url)              \
+    __ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTE(email)            \
+    __ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTE(password)         \
+    __ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTE(date)             \
+    __ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTE(month)            \
+    __ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTE(week)             \
+    __ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTE(time)             \
+    __ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTE("datetime-local") \
+    __ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTE(number)           \
+    __ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTE(range)            \
+    __ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTE(color)            \
+    __ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTE(checkbox)         \
+    __ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTE(radio)            \
+    __ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTE(file)             \
+    __ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTE(submit)           \
+    __ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTE(image)            \
+    __ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTE(reset)            \
+    __ENUMERATE_HTML_INPUT_TYPE_ATTRIBUTE(button)
 
 class HTMLInputElement final : public FormAssociatedElement {
 public:
