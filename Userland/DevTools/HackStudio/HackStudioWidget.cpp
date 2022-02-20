653f01616ce3ea434d6ce9d21b3c7ba1ecea608e
thankyouverycool <66646555+thankyouverycool@users.noreply.github.com>
2022-02-20 09:50:44 -0500

    LibGUI+Apps: Adjust Splitter spacings
    Different thread highlights between widgets lead to different
    visual weights between splitters, even when they have the same
    width or height. This means some splitters look best at odd
    sizes while others even. This sets the default spacing to the
    most commonly used, depending on orientation, and adjusts
    spacing for a few apps based on the new paint rect.
    
    The most consistent look across apps requires some manual
    tweaking occassionally. Knurlheads, use your discretion!

--- Userland/DevTools/HackStudio/HackStudioWidget.cpp
+++ Userland/DevTools/HackStudio/HackStudioWidget.cpp
@@ -91,10 +91,10 @@ HackStudioWidget::HackStudioWidget(String path_to_project)
     auto& toolbar_container = add<GUI::ToolbarContainer>();
 
     auto& outer_splitter = add<GUI::HorizontalSplitter>();
-    outer_splitter.layout()->set_spacing(5);
+    outer_splitter.layout()->set_spacing(4);
 
     auto& left_hand_splitter = outer_splitter.add<GUI::VerticalSplitter>();
-    left_hand_splitter.layout()->set_spacing(5);
+    left_hand_splitter.layout()->set_spacing(6);
     left_hand_splitter.set_fixed_width(150);
     create_project_tab(left_hand_splitter);
     m_project_tree_view_context_menu = create_project_tree_view_context_menu();
@@ -110,7 +110,6 @@ HackStudioWidget::HackStudioWidget(String path_to_project)
     m_diff_viewer = m_right_hand_stack->add<DiffViewer>();
 
     m_editors_splitter = m_right_hand_stack->add<GUI::VerticalSplitter>();
-    m_editors_splitter->layout()->set_spacing(5);
     m_editors_splitter->layout()->set_margins({ 3, 0, 0 });
     add_new_editor(*m_editors_splitter);
 
