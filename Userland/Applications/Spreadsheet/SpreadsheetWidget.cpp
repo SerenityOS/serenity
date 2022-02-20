5c978266e3028dd5560f121f079ed28301ba0218
Karol Kosek <krkk@serenityos.org>
2022-02-20 18:39:37 +0100

    Spreadsheet: Ask about unsaved changes on "Open..." action activation

--- Userland/Applications/Spreadsheet/SpreadsheetWidget.cpp
+++ Userland/Applications/Spreadsheet/SpreadsheetWidget.cpp
@@ -122,6 +122,9 @@ SpreadsheetWidget::SpreadsheetWidget(GUI::Window& parent_window, NonnullRefPtrVe
     });
 
     m_open_action = GUI::CommonActions::make_open_action([&](auto&) {
+        if (!request_close())
+            return;
+
         Optional<String> load_path = GUI::FilePicker::get_open_filepath(window());
         if (!load_path.has_value())
             return;
