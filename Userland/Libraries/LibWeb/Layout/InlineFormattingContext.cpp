46b596faf5336850560627132a953d978ad399df
Vrins <michiel_678@hotmail.com>
2022-02-21 02:16:21 +0100

    LibWeb: Allow <input type="button/submit/reset"> to be styled
    Previously we used a native ui button to draw the buttons.
    These buttons can however not be styled with css.
    To allow these to be styled with css, we create a button with
    the UA stylesheet that resembles the system ui button.

--- Userland/Libraries/LibWeb/Layout/InlineFormattingContext.cpp
+++ Userland/Libraries/LibWeb/Layout/InlineFormattingContext.cpp
@@ -117,6 +117,10 @@ void InlineFormattingContext::dimension_box_on_line(Box const& box, LayoutMode l
     box_state.margin_right = computed_values.margin().right.resolved(box, width_of_containing_block).to_px(box);
     box_state.border_right = computed_values.border_right().width;
     box_state.padding_right = computed_values.padding().right.resolved(box, width_of_containing_block).to_px(box);
+    box_state.padding_top = computed_values.padding().top.resolved(box, width_of_containing_block).to_px(box);
+    box_state.padding_bottom = computed_values.padding().bottom.resolved(box, width_of_containing_block).to_px(box);
+    box_state.margin_top = computed_values.margin().top.resolved(box, width_of_containing_block).to_px(box);
+    box_state.margin_bottom = computed_values.margin().bottom.resolved(box, width_of_containing_block).to_px(box);
 
     if (is<ReplacedBox>(box)) {
         auto& replaced = verify_cast<ReplacedBox>(box);
