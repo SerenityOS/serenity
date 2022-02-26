4e9532bc7260972befa679945bd7ff61539c6280
Lenny Maiorani <lenny@serenityos.org>
2022-02-26 10:55:15 -0700

    Libraries: Use default constructors/destructors in LibGemini
    https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#cother-other-default-operation-rules
    
    "The compiler is more likely to get the default semantics right and
    you cannot implement these functions better than the compiler."

--- Userland/Libraries/LibGemini/GeminiRequest.cpp
+++ Userland/Libraries/LibGemini/GeminiRequest.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, the SerenityOS developers.
+ * Copyright (c) 2020-2022, the SerenityOS developers.
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -9,14 +9,6 @@
 #include <LibGemini/GeminiRequest.h>
 
 namespace Gemini {
-
-GeminiRequest::GeminiRequest()
-{
-}
-
-GeminiRequest::~GeminiRequest()
-{
-}
 
 ByteBuffer GeminiRequest::to_raw_request() const
 {
--- Userland/Libraries/LibGemini/GeminiRequest.h
+++ Userland/Libraries/LibGemini/GeminiRequest.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, the SerenityOS developers.
+ * Copyright (c) 2020-2022, the SerenityOS developers.
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -15,8 +15,8 @@ namespace Gemini {
 
 class GeminiRequest {
 public:
-    GeminiRequest();
-    ~GeminiRequest();
+    GeminiRequest() = default;
+    ~GeminiRequest() = default;
 
     const URL& url() const { return m_url; }
     void set_url(const URL& url) { m_url = url; }
