2e1c017bce31757b19903d26cb5fba934778ccee
Nícolas F. R. A. Prado <n@nfraprado.net>
2022-02-27 20:18:28 -0500

    SoundPlayer: Add start_new_file() to VisualizationWidget's API
    This adds a new start_new_file() function to VisualizationWidget which
    is called when the player starts a new file, passing the filename of the
    file. This allows VisualizationWidget subclasses to do any setup needed
    when a new file is started.

--- Userland/Applications/SoundPlayer/SoundPlayerWidgetAdvancedView.h
+++ Userland/Applications/SoundPlayer/SoundPlayerWidgetAdvancedView.h
@@ -32,6 +32,8 @@ public:
         auto new_visualization = T::construct();
         m_player_view->insert_child_before(new_visualization, *static_cast<Core::Object*>(m_playback_progress_slider.ptr()));
         m_visualization = new_visualization;
+        if (!loaded_filename().is_empty())
+            m_visualization->start_new_file(loaded_filename());
     }
 
     virtual void play_state_changed(PlayState) override;
