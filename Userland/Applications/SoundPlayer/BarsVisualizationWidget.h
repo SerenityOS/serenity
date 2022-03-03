21266f42f10fba4a9259e53ab466051b2d724c85
kleines Filmröllchen <filmroellchen@serenityos.org>
2022-03-03 22:32:45 +0100

    SoundPlayer: Use overlapping windows for bars visualization
    For DSP reasons I can't explain myself (yet, sorry), short-time Fourier
    transform (STFT) is much more accurate and aesthetically pleasing when
    the windows that select the samples for STFT overlap. This implements
    that behavior by storing the previous samples and performing windowed
    FFT over both it as well as the current samples. This gives us 50%
    overlap between windows, a common standard that is nice to look at.

--- Userland/Applications/SoundPlayer/BarsVisualizationWidget.h
+++ Userland/Applications/SoundPlayer/BarsVisualizationWidget.h
@@ -26,12 +26,13 @@ private:
     void render(GUI::PaintEvent&, FixedArray<double> const&) override;
     void context_menu_event(GUI::ContextMenuEvent& event) override;
 
-    static constexpr size_t fft_size = 256;
+    static constexpr size_t fft_size = 512;
     static constexpr size_t bar_count = 64;
     static constexpr size_t values_per_bar = (fft_size / 2) / bar_count;
 
     Array<Complex<double>, fft_size> m_fft_samples {};
     Array<double, fft_size> m_fft_window {};
+    Array<double, fft_size / 2> m_previous_samples {};
     Array<int, bar_count> m_gfx_falling_bars {};
     bool m_is_using_last;
     bool m_adjust_frequencies;
