bbfe17f1fe920b9266a84632be999b8dc030449b
Wuzado <23276409+Wuzado@users.noreply.github.com>
2022-03-03 23:51:39 +0100

    reboot: Port to LibMain
    First commit to the project! :^)

--- Userland/Utilities/reboot.cpp
+++ Userland/Utilities/reboot.cpp
@@ -5,21 +5,16 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#include <fcntl.h>
-#include <stdio.h>
-#include <unistd.h>
+#include <LibCore/Stream.h>
+#include <LibMain/Main.h>
 
-int main(int, char**)
+ErrorOr<int> serenity_main(Main::Arguments)
 {
-    int power_state_switch_node = open("/sys/firmware/power_state", O_WRONLY);
-    if (power_state_switch_node < 0) {
-        perror("open");
-        return 1;
-    }
-    const char* value = "1";
-    if (write(power_state_switch_node, value, 1) < 0) {
-        perror("write");
-        return 1;
-    }
+    auto file = TRY(Core::Stream::File::open("/sys/firmware/power_state", Core::Stream::OpenMode::Write));
+
+    const String file_contents = "1";
+    TRY(file->write(file_contents.bytes()));
+    file->close();
+
     return 0;
 }
