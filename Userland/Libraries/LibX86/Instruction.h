1c70c46bfe44e17285a3fc35375044b77abfc4a6
Lenny Maiorani <lenny@serenityos.org>
2022-03-04 14:08:53 -0700

    Libraries: Use default constructors/destructors in LibX86
    https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#cother-other-default-operation-rules
    
    "The compiler is more likely to get the default semantics right and
    you cannot implement these functions better than the compiler."

--- Userland/Libraries/LibX86/Instruction.h
+++ Userland/Libraries/LibX86/Instruction.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2022, the SerenityOS developers.
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -489,7 +490,7 @@ class Instruction {
 public:
     template<typename InstructionStreamType>
     static Instruction from_stream(InstructionStreamType&, bool o32, bool a32);
-    ~Instruction() { }
+    ~Instruction() = default;
 
     ALWAYS_INLINE MemoryOrRegisterReference& modrm() const { return m_modrm; }
 
