6f941433d6403d45a71036b77de7e1a0e762c923
Linus Groh <mail@linusgroh.de>
2022-03-08 19:32:35 +0100

    LibWeb: Stub out 'check if access between two BCs should be reported'
    I put this is a CrossOrigin/ subdirectory in anticipation of a lot more
    cross-origin related ground to cover. :^)

---
+++ Userland/Libraries/LibWeb/HTML/CrossOrigin/Reporting.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2022, Linus Groh <linusg@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibJS/Forward.h>
+#include <LibWeb/Forward.h>
+
+namespace Web::HTML {
+
+// https://html.spec.whatwg.org/multipage/origin.html#accessor-accessed-relationship
+enum class AccessorAccessedRelationship {
+    AccessorIsOpener,
+    AccessorIsOpenee,
+    None,
+};
+
+void check_if_access_between_two_browsing_contexts_should_be_reported(BrowsingContext const& accessor, BrowsingContext const& accessed, JS::PropertyKey const&, EnvironmentSettingsObject const&);
+
+}
