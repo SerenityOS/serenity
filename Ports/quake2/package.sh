80cdc8e60baa1011459e01ea2b6459ad88fa562b
Jelle Raaijmakers <jelle@gmta.nl>
2022-03-08 10:07:08 +0100

    Ports: Add OpenGL include dir to quake2 build

--- Ports/quake2/package.sh
+++ Ports/quake2/package.sh
@@ -5,7 +5,10 @@ workdir=SerenityQuakeII-master
 useconfigure=true
 files="https://github.com/SerenityPorts/SerenityQuakeII/archive/master.tar.gz quake2.tar.gz"
 makeopts=()
-configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt")
+configopts=(
+    "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
+    "-DOPENGL_INCLUDE_DIR=${SERENITY_INSTALL_ROOT}/usr/include/LibGL"
+)
 depends=("SDL2")
 launcher_name=QuakeII
 launcher_category=Games
