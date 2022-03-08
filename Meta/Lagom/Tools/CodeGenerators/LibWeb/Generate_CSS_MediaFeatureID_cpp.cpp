001af7cb44d9c3ec514f4e0bb041fbfcaf07f06e
Sam Atkins <atkinssj@serenityos.org>
2022-03-08 15:00:27 +0000

    Meta: Generate CSS::MediaFeatureID enum
    This works largely the same as the PropertyID and ValueID generators,
    but using LibMain, Core::Stream, and TRY().
    
    Rather than have a MediaFeatureID::Invalid, I decided to return an
    Optional. We'll see if that turns out better or not. :^)

---
+++ Meta/Lagom/Tools/CodeGenerators/LibWeb/Generate_CSS_MediaFeatureID_cpp.cpp
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2022, Sam Atkins <atkinssj@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include "GeneratorUtil.h"
+#include <AK/SourceGenerator.h>
+#include <AK/StringBuilder.h>
+#include <LibMain/Main.h>
+
+ErrorOr<int> serenity_main(Main::Arguments arguments)
+{
+    if (arguments.argc != 2) {
+        warnln("usage: {} <path/to/CSS/MediaFeatures.json", arguments.strings[0]);
+        return 1;
+    }
+
+    auto json = TRY(read_entire_file_as_json(arguments.strings[1]));
+    VERIFY(json.is_object());
+
+    StringBuilder builder;
+    SourceGenerator generator { builder };
+    generator.append(R"~~~(#include <LibWeb/CSS/MediaFeatureID.h>
+
+namespace Web::CSS {
+
+Optional<MediaFeatureID> media_feature_id_from_string(StringView string)
+{)~~~");
+
+    json.as_object().for_each_member([&](auto& name, auto&) {
+        auto member_generator = generator.fork();
+        member_generator.set("name", name);
+        member_generator.set("name:titlecase", title_casify(name));
+        member_generator.append(R"~~~(
+    if (string.equals_ignoring_case("@name@"sv))
+        return MediaFeatureID::@name:titlecase@;
+)~~~");
+    });
+
+    generator.append(R"~~~(
+    return {};
+}
+
+char const* string_from_media_feature_id(MediaFeatureID media_feature_id)
+{
+    switch (media_feature_id) {)~~~");
+
+    json.as_object().for_each_member([&](auto& name, auto&) {
+        auto member_generator = generator.fork();
+        member_generator.set("name", name);
+        member_generator.set("name:titlecase", title_casify(name));
+        member_generator.append(R"~~~(
+    case MediaFeatureID::@name:titlecase@:
+        return "@name@";)~~~");
+    });
+
+    generator.append(R"~~~(
+    }
+    VERIFY_NOT_REACHED();
+}
+
+}
+)~~~");
+
+    outln("{}", generator.as_string_view());
+
+    return 0;
+}
