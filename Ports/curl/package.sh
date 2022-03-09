1b6a1748f06078084e53c8ff58802c97cf5bbf8d
Tim Schumacher <timschumi@gmx.de>
2022-03-09 02:21:59 +0100

    Ports: Add ca-certificates as a dependency to curl

--- Ports/curl/package.sh
+++ Ports/curl/package.sh
@@ -4,7 +4,7 @@ version=7.81.0
 useconfigure=true
 files="https://curl.se/download/curl-${version}.tar.bz2 curl-${version}.tar.bz2 1e7a38d7018ec060f1f16df839854f0889e94e122c4cfa5d3a37c2dc56f1e258"
 auth_type=sha256
-depends=("openssl" "zlib" "zstd")
+depends=("ca-certificates" "openssl" "zlib" "zstd")
 configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt")
 
 configure() {
