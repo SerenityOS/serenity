2dada83857c2297e0081d2aaaa39f92a26c47538
Luke Wilde <lukew@serenityos.org>
2022-03-13 16:45:19 +0000

    Ports: Update zstd to 1.5.2

--- Ports/zstd/package.sh
+++ Ports/zstd/package.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=zstd
-version=1.5.1
-files="https://github.com/facebook/zstd/releases/download/v${version}/zstd-${version}.tar.gz zstd-${version}.tar.gz e28b2f2ed5710ea0d3a1ecac3f6a947a016b972b9dd30242369010e5f53d7002"
+version=1.5.2
+files="https://github.com/facebook/zstd/releases/download/v${version}/zstd-${version}.tar.gz zstd-${version}.tar.gz 7c42d56fac126929a6a85dbc73ff1db2411d04f104fae9bdea51305663a83fd0"
 auth_type=sha256
