c0ba5f6156e4c1eb5db57300e1ca76dae3068b9a
Tim Schumacher <timschumi@gmx.de>
2022-03-13 02:15:41 +0100

    Utilities: Add the `-c` option to `install`

--- Userland/Utilities/install.cpp
+++ Userland/Utilities/install.cpp
@@ -19,6 +19,7 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
     StringView destination;
 
     Core::ArgsParser args_parser;
+    args_parser.add_ignored(nullptr, 'c'); // "copy files" is the default, no contradicting options exist.
     args_parser.add_option(create_leading_dest_components, "Create leading components of the destination path", nullptr, 'D');
     args_parser.add_positional_argument(source, "Source path", "source");
     args_parser.add_positional_argument(destination, "Destination path", "destination");
