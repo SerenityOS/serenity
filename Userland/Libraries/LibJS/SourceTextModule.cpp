29964dc15261d18327b9e0cf3c6273bdf719363a
Linus Groh <mail@linusgroh.de>
2022-03-17 22:42:41 +0000

    LibJS: Use TRY(push_execution_context()) in places where we can recover

--- Userland/Libraries/LibJS/SourceTextModule.cpp
+++ Userland/Libraries/LibJS/SourceTextModule.cpp
@@ -404,7 +404,7 @@ Completion SourceTextModule::initialize_environment(VM& vm)
     // Note: We're already working on that one.
 
     // 17. Push moduleContext onto the execution context stack; moduleContext is now the running execution context.
-    vm.push_execution_context(m_execution_context, realm().global_object());
+    TRY(vm.push_execution_context(m_execution_context, realm().global_object()));
 
     // 18. Let code be module.[[ECMAScriptCode]].
 
@@ -657,7 +657,7 @@ Completion SourceTextModule::execute_module(VM& vm, Optional<PromiseCapability> 
         // a. Assert: capability is not present.
         VERIFY(!capability.has_value());
         // b. Push moduleContext onto the execution context stack; moduleContext is now the running execution context.
-        vm.push_execution_context(module_context, realm().global_object());
+        TRY(vm.push_execution_context(module_context, realm().global_object()));
 
         // c. Let result be the result of evaluating module.[[ECMAScriptCode]].
         auto result = m_ecmascript_code->execute(vm.interpreter(), realm().global_object());
