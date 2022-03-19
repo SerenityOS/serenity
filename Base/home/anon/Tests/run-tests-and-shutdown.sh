ff62237dc070db78b778c76d6907967516eddcb1
Andrew Kaster <akaster@serenityos.org>
2022-03-19 14:06:52 -0600

    Base: Set $LLVM_PROFILE_FILE before running run-tests
    Set LLVM_PROFILE_FILE to a pattern that is easily extractable as "these
    are the profile data files we generated while running tests" for later
    post-processing.

--- Base/home/anon/Tests/run-tests-and-shutdown.sh
+++ Base/home/anon/Tests/run-tests-and-shutdown.sh
@@ -3,8 +3,12 @@
 echo
 echo "==== Running Tests on SerenityOS ===="
 
+export LLVM_PROFILE_FILE="$HOME/profiles/%p-profile.profraw"
+
 run-tests --show-progress=false
 fail_count=$?
+
+unset LLVM_PROFILE_FILE
 
 echo "Failed: $fail_count" > ./test-results.log
 
