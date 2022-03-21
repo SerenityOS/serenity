ac4b2adb3ea31315fe10d5e7a2d8f3a070320b13
Brian Gianforcaro <bgianf@serenityos.org>
2022-03-21 00:05:42 -0700

    LibCore: Add Core::System::drop_privileges()
    In a few places we intentionally drop privileges to reduce the potential
    security surface area of networked program, with the pattern of:
    
    ```
    if (setgid(getgid()) || setuid(getuid()) {
        return 1;
    }
    ```
    
    We can make this a bit nicer to use by creating a wrapper.

--- Userland/Libraries/LibCore/System.h
+++ Userland/Libraries/LibCore/System.h
@@ -109,6 +109,7 @@ ErrorOr<void> setgid(gid_t);
 ErrorOr<void> setegid(gid_t);
 ErrorOr<void> setpgid(pid_t pid, pid_t pgid);
 ErrorOr<pid_t> setsid();
+ErrorOr<void> drop_privileges();
 ErrorOr<bool> isatty(int fd);
 ErrorOr<void> symlink(StringView target, StringView link_path);
 ErrorOr<void> mkdir(StringView path, mode_t);
