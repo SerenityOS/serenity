cd6896d3433f236a5353d7b42fef2ae7e691dd2a
Linus Groh <mail@linusgroh.de>
2022-03-22 12:40:42 +0000

    LibWeb: Convert URL to use TRY for error propagation

--- Userland/Libraries/LibWeb/URL/URL.cpp
+++ Userland/Libraries/LibWeb/URL/URL.cpp
@@ -35,10 +35,9 @@ DOM::ExceptionOr<NonnullRefPtr<URL>> URL::create_with_global_object(Bindings::Wi
     auto& query = parsed_url.query().is_null() ? String::empty() : parsed_url.query();
     // 6. Set this’s URL to parsedURL.
     // 7. Set this’s query object to a new URLSearchParams object.
-    auto query_object = URLSearchParams::create_with_global_object(window_object, query);
-    VERIFY(!query_object.is_exception()); // The string variant of the constructor can't throw.
+    auto query_object = MUST(URLSearchParams::create_with_global_object(window_object, query));
     // 8. Initialize this’s query object with query.
-    auto result_url = URL::create(move(parsed_url), query_object.release_value());
+    auto result_url = URL::create(move(parsed_url), move(query_object));
     // 9. Set this’s query object’s URL object to this.
     result_url->m_query->m_url = result_url;
 
