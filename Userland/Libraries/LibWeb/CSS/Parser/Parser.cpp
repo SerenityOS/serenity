b476a2a0d8c2b326622fa125b297ff8dfacc01a5
Sam Atkins <atkinssj@serenityos.org>
2022-03-24 16:38:34 +0000

    LibWeb+Base: Fix `An+B of foo` parsing
    When I wrote the An+B parser, it was guaranteed to have no
    non-whitespace tokens after it. This is no longer true with the `of
    foo` syntax, so this patch corrects the logic when there is no `+B`
    segment.
    
    This makes this case shown on Twitter work correctly. :^)
    https://twitter.com/simevidas/status/1506657566012678151

--- Userland/Libraries/LibWeb/CSS/Parser/Parser.cpp
+++ Userland/Libraries/LibWeb/CSS/Parser/Parser.cpp
@@ -4486,12 +4486,14 @@ Optional<Selector::SimpleSelector::ANPlusBPattern> Parser::parse_a_n_plus_b_patt
     if (is_n_dimension(first_value)) {
         a = first_value.token().dimension_value_int();
 
-        values.skip_whitespace();
-        auto& second_value = values.next_token();
-        if (second_value.is(Token::Type::EndOfFile)) {
+        if (!values.has_next_token() || values.peek_token().is(Token::Type::Whitespace)) {
             // <n-dimension>
             return make_return_value();
-        } else if (is_signed_integer(second_value)) {
+        }
+
+        values.skip_whitespace();
+        auto& second_value = values.next_token();
+        if (is_signed_integer(second_value)) {
             // <n-dimension> <signed-integer>
             b = second_value.token().to_integer();
             return make_return_value();
@@ -4547,12 +4549,14 @@ Optional<Selector::SimpleSelector::ANPlusBPattern> Parser::parse_a_n_plus_b_patt
     // -n ['+' | '-'] <signless-integer>
     if (is_dashn(first_value)) {
         a = -1;
-        values.skip_whitespace();
-        auto& second_value = values.next_token();
-        if (second_value.is(Token::Type::EndOfFile)) {
+        if (!values.has_next_token() || values.peek_token().is(Token::Type::Whitespace)) {
             // -n
             return make_return_value();
-        } else if (is_signed_integer(second_value)) {
+        }
+
+        values.skip_whitespace();
+        auto& second_value = values.next_token();
+        if (is_signed_integer(second_value)) {
             // -n <signed-integer>
             b = second_value.token().to_integer();
             return make_return_value();
@@ -4600,12 +4604,14 @@ Optional<Selector::SimpleSelector::ANPlusBPattern> Parser::parse_a_n_plus_b_patt
     // '+'?† n ['+' | '-'] <signless-integer>
     if (is_n(first_after_plus)) {
         a = 1;
-        values.skip_whitespace();
-        auto& second_value = values.next_token();
-        if (second_value.is(Token::Type::EndOfFile)) {
+        if (!values.has_next_token() || values.peek_token().is(Token::Type::Whitespace)) {
             // '+'?† n
             return make_return_value();
-        } else if (is_signed_integer(second_value)) {
+        }
+
+        values.skip_whitespace();
+        auto& second_value = values.next_token();
+        if (is_signed_integer(second_value)) {
             // '+'?† n <signed-integer>
             b = second_value.token().to_integer();
             return make_return_value();
