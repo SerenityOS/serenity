e831c374f4f2fd7a7a8fff46aca6eff7c7f17509
Matthew Olsson <matthewcolsson@gmail.com>
2022-03-25 08:19:26 -0700

    LibPDF: Abstract Type1 font data
    TTF font types will use the same data

--- Userland/Libraries/LibPDF/Fonts/Type1Font.h
+++ Userland/Libraries/LibPDF/Fonts/Type1Font.h
@@ -13,19 +13,26 @@ namespace PDF {
 
 class Type1Font : public PDFFont {
 public:
+    // Also used by TrueTypeFont, which is very similar to Type1
+    struct Data {
+        RefPtr<StreamObject> to_unicode;
+        NonnullRefPtr<Encoding> encoding;
+        HashMap<u16, u16> widths;
+        u16 missing_width;
+    };
+
+    static PDFErrorOr<Data> parse_data(Document*, NonnullRefPtr<DictObject> font_dict);
+
     static PDFErrorOr<NonnullRefPtr<Type1Font>> create(Document*, NonnullRefPtr<DictObject>);
 
-    Type1Font(RefPtr<StreamObject> to_unicode, NonnullRefPtr<Encoding>, HashMap<u16, u16> const& m_widths, u16 missing_width);
+    Type1Font(Data);
     ~Type1Font() override = default;
 
     u32 char_code_to_code_point(u16 char_code) const override;
     float get_char_width(u16 char_code, float font_size) const override;
 
 private:
-    RefPtr<StreamObject> m_to_unicode;
-    NonnullRefPtr<Encoding> m_encoding;
-    HashMap<u16, u16> m_widths;
-    u16 m_missing_width;
+    Data m_data;
 };
 
 }
