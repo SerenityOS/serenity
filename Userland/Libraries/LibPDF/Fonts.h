d5b1eb48d6dff48ff6bd0e3a915a504160ab33ee
Matthew Olsson <matthewcolsson@gmail.com>
2022-03-24 21:56:14 -0700

    LibPDF: Use Font /Widths array to derive character widths
    This makes the spacing between chars _much_ better!

--- Userland/Libraries/LibPDF/Fonts.h
+++ Userland/Libraries/LibPDF/Fonts.h
@@ -18,20 +18,24 @@ public:
     virtual ~PDFFont() = default;
 
     virtual u32 char_code_to_code_point(u16 char_code) const = 0;
+    virtual float get_char_width(u16 char_code) const = 0;
 };
 
 class Type1Font : public PDFFont {
 public:
     static PDFErrorOr<NonnullRefPtr<Type1Font>> create(Document*, NonnullRefPtr<DictObject>);
 
-    Type1Font(RefPtr<StreamObject> to_unicode, NonnullRefPtr<Encoding>);
+    Type1Font(RefPtr<StreamObject> to_unicode, NonnullRefPtr<Encoding>, HashMap<u16, u16> const& m_widths, u16 missing_width);
     ~Type1Font() override = default;
 
     u32 char_code_to_code_point(u16 char_code) const override;
+    float get_char_width(u16 char_code) const override;
 
 private:
     RefPtr<StreamObject> m_to_unicode;
     NonnullRefPtr<Encoding> m_encoding;
+    HashMap<u16, u16> m_widths;
+    u16 m_missing_width;
 };
 
 }
