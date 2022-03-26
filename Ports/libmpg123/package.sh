7dfb167b181bb39bd03f16e1f7edc79f7d02b1f5
Michael Manganiello <adamantike@users.noreply.github.com>
2022-03-26 13:15:31 -0300

    Ports: Add libmpg123 port
    Add `libmpg123` port, for dependant projects to be able to use it.
    
    This port doesn't include the entire `mpg123` project, just the
    `libmpg123` library.

---
+++ Ports/libmpg123/package.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port=libmpg123
+version=1.29.3
+useconfigure=true
+workdir=mpg123-${version}
+use_fresh_config_sub=true
+config_sub_path=build/config.sub
+files="https://download.sourceforge.net/project/mpg123/mpg123/${version}/mpg123-${version}.tar.bz2 mpg123-${version}.tar.bz2
+https://download.sourceforge.net/project/mpg123/mpg123/${version}/mpg123-${version}.tar.bz2.sig mpg123-${version}.tar.bz2.sig"
+
+auth_type="sig"
+auth_import_key="D021FF8ECF4BE09719D61A27231C4CBC60D5CAFE"
+auth_opts=("mpg123-${version}.tar.bz2.sig")
