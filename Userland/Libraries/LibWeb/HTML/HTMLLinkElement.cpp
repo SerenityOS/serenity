05bd0ca3ee6f63b3af1290eda6c8d310e2277b39
Sam Atkins <atkinssj@serenityos.org>
2022-03-29 12:42:57 +0100

    LibWeb: Rename `parse_css()` -> `parse_css_stylesheet()`

--- Userland/Libraries/LibWeb/HTML/HTMLLinkElement.cpp
+++ Userland/Libraries/LibWeb/HTML/HTMLLinkElement.cpp
@@ -97,7 +97,7 @@ void HTMLLinkElement::resource_did_load()
         }
     }
 
-    auto sheet = parse_css(CSS::ParsingContext(document(), resource()->url()), resource()->encoded_data());
+    auto sheet = parse_css_stylesheet(CSS::ParsingContext(document(), resource()->url()), resource()->encoded_data());
     if (!sheet) {
         dbgln_if(CSS_LOADER_DEBUG, "HTMLLinkElement: Failed to parse stylesheet: {}", resource()->url());
         return;
