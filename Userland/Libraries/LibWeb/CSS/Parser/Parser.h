34b3c094628e381d53c3a3caa6d77611d71217d0
Sam Atkins <atkinssj@serenityos.org>
2022-03-30 12:32:42 +0100

    LibWeb: Spec-comment `parse_a_component_value()`

--- Userland/Libraries/LibWeb/CSS/Parser/Parser.h
+++ Userland/Libraries/LibWeb/CSS/Parser/Parser.h
@@ -89,8 +89,6 @@ public:
     Parser(ParsingContext const&, StringView input, String const& encoding = "utf-8");
     ~Parser() = default;
 
-    // For things that need to consume a single value, like the parsing rules for attr().
-    Optional<StyleComponentValueRule> parse_as_component_value();
     // For the contents of presentational attributes, which parse text into a single declaration’s value, or for parsing a stand-alone selector [SELECT] or list of Media Queries [MEDIAQ], as in Selectors API or the media HTML attribute.
     Vector<StyleComponentValueRule> parse_as_list_of_component_values();
     Vector<Vector<StyleComponentValueRule>> parse_as_comma_separated_list_of_component_values();
@@ -150,6 +148,8 @@ private:
     // "Parse a list of declarations" is for the contents of a style attribute, which parses text into the contents of a single style rule.
     template<typename T>
     Vector<DeclarationOrAtRule> parse_a_list_of_declarations(TokenStream<T>&);
+
+    // "Parse a component value" is for things that need to consume a single value, like the parsing rules for attr().
     template<typename T>
     Optional<StyleComponentValueRule> parse_a_component_value(TokenStream<T>&);
     template<typename T>
