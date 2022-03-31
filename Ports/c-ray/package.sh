a98d063ce9a7dba899082b9487e0e548264a7fb2
EWouters <6179932+EWouters@users.noreply.github.com>
2022-04-01 01:55:25 +0200

    Ports/c-ray: Update c-ray to git commit 8f30eb9
    Part of the fix-linkage patch is removed as the changes are no
    longer required.

--- Ports/c-ray/package.sh
+++ Ports/c-ray/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=c-ray
-version=c094d64570c30c70f4003e9428d31a2a0d9d3d41
+version=8f30eb9904a4d20a78e9387d79dc049c5ed69b0c
 useconfigure=true
-files="https://github.com/vkoskiv/c-ray/archive/${version}.tar.gz ${version}.tar.gz 1e0663a1d83e8a9984aced33b9307471f3302c8a5ea7ec47954854d60902a747"
+files="https://github.com/vkoskiv/c-ray/archive/${version}.tar.gz ${version}.tar.gz 27fa6496721faf69f18dc0946f0747b64f3ced748440a8f906f51fcb7e5cb008"
 auth_type=sha256
 configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt")
 depends=("SDL2")
