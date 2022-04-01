a9d53b1b885087d7140b64d5a049c155f25400ba
EWouters <6179932+EWouters@users.noreply.github.com>
2022-04-01 04:50:09 +0200

    Ports/mrsh: Update mrsh to git commit cd3c3a4
    Also preserve `-j$(nproc)` flag.

--- Ports/mrsh/package.sh
+++ Ports/mrsh/package.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=mrsh
-version=d9763a32e7da572677d1681bb1fc67f117d641f3
+version=cd3c3a48055ab4085d83f149ff4b4feba40b40cb
 files="https://codeload.github.com/emersion/mrsh/legacy.tar.gz/${version} emersion-mrsh-d9763a3.tar.gz 6896493a1020774715ccca28e8d8f4ec722af63a93543fb6dd2762f7b1de9c8a"
 auth_type=sha256
 useconfigure=true
-makeopts=()
 workdir=emersion-mrsh-d9763a3
 
 export CFLAGS=-Wno-deprecated-declarations
