73399a3a0194998478fa11cd774bffd7a535f4bb
EWouters <6179932+EWouters@users.noreply.github.com>
2022-04-01 02:01:15 +0200

    Ports/ccache: Update ccache to version 4.6

--- Ports/ccache/package.sh
+++ Ports/ccache/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=ccache
-version=4.5.1
+version=4.6
 useconfigure=true
-files="https://github.com/ccache/ccache/releases/download/v${version}/ccache-${version}.tar.gz ccache-$version.tar.gz f0d3cff5d555d6868f14a7d05696f0370074e475304fd5aa152b98f892364981"
+files="https://github.com/ccache/ccache/releases/download/v${version}/ccache-${version}.tar.gz ccache-$version.tar.gz 73a1767ac6b7c0404a1a55f761a746d338e702883c7137fbf587023062258625"
 auth_type=sha256
 depends=("zstd")
 configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt" "-DCMAKE_BUILD_TYPE=Release" "-DREDIS_STORAGE_BACKEND=OFF" "-GNinja")
