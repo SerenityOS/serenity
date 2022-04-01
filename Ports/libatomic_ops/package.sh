cbb2d7d6d401a271b5f892ed932f196eba115f5f
EWouters <6179932+EWouters@users.noreply.github.com>
2022-04-01 03:18:01 +0200

    Ports/libatomic_ops: Update libatomic_ops to version 7.6.12

--- Ports/libatomic_ops/package.sh
+++ Ports/libatomic_ops/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 
 port=libatomic_ops
-version=7.6.10
+version=7.6.12
 useconfigure=true
 use_fresh_config_sub=true
-files="https://www.hboehm.info/gc/gc_source/libatomic_ops-${version}.tar.gz libatomic_ops-${version}.tar.gz 587edf60817f56daf1e1ab38a4b3c729b8e846ff67b4f62a6157183708f099af"
+files="https://www.hboehm.info/gc/gc_source/libatomic_ops-${version}.tar.gz libatomic_ops-${version}.tar.gz f0ab566e25fce08b560e1feab6a3db01db4a38e5bc687804334ef3920c549f3e"
 auth_type=sha256
