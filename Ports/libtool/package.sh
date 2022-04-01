a37ce939ee487a866d928101e83ac8dcda365aff
EWouters <6179932+EWouters@users.noreply.github.com>
2022-04-01 03:37:46 +0200

    Ports/libtool: Update libtool to version 2.4.7

--- Ports/libtool/package.sh
+++ Ports/libtool/package.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=libtool
-version=2.4
-useconfigure="true"
-use_fresh_config_sub="true"
-config_sub_path=libltdl/config/config.sub
+version=2.4.7
+useconfigure=true
+use_fresh_config_sub=true
+config_sub_path=build-aux/config.sub
 depends=("bash" "sed")
 files="https://ftpmirror.gnu.org/gnu/libtool/libtool-${version}.tar.xz libtool-${version}.tar.xz
 https://ftpmirror.gnu.org/gnu/libtool/libtool-${version}.tar.xz.sig libtool-${version}.tar.xz.sig
 https://ftpmirror.gnu.org/gnu/gnu-keyring.gpg gnu-keyring.gpg"
-auth_type="sig"
+auth_type=sig
 auth_opts=("--keyring" "./gnu-keyring.gpg" "libtool-${version}.tar.xz.sig")
 configopts=("--prefix=/usr/local")
 
