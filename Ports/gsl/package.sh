3b1b869bfcc5498e582e8f18829324afff9f9334
EWouters <6179932+EWouters@users.noreply.github.com>
2022-04-01 03:11:42 +0200

    Ports/gsl: Update gsl to version 2.7.1

--- Ports/gsl/package.sh
+++ Ports/gsl/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 
-port="gsl"
-version="2.7"
-useconfigure="true"
+port=gsl
+version=2.7.1
+useconfigure=true
 files="https://ftp.gnu.org/gnu/gsl/gsl-${version}.tar.gz gsl-${version}.tar.gz
 https://ftp.gnu.org/gnu/gsl/gsl-${version}.tar.gz.sig gsl-${version}.tar.gz.sig
 https://ftpmirror.gnu.org/gnu/gnu-keyring.gpg gnu-keyring.gpg"
