0029cb1d95130afc14155f1848407d0a4d5d11f7
EWouters <6179932+EWouters@users.noreply.github.com>
2022-04-01 03:05:25 +0200

    Ports/genemu: Update genemu to git commit e39f690

--- Ports/genemu/package.sh
+++ Ports/genemu/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port="genemu"
-version=3bf6f7cd893db3451019d6e18a2d9ad1de0e7c8c
+port=genemu
+version=e39f690157d8f969adfbaba30a4e639d20b34768
 useconfigure=true
-files="https://github.com/rasky/genemu/archive/${version}.tar.gz genemu-${version}.tar.gz 07e4f6aba1778143796bc0a571dfc7a693a2cbc5cf303a31df19d74e12f8cf54"
+files="https://github.com/rasky/genemu/archive/${version}.tar.gz genemu-${version}.tar.gz 9b9616f6237e621a169422058caeccb2d0f4399374dc38f34837980154c89497"
 auth_type=sha256
 configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt")
 depends=("SDL2")
