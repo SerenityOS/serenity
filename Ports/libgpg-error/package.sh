964aa0e4105f37d07a6cb39b6169c59404705bd4
EWouters <6179932+EWouters@users.noreply.github.com>
2022-04-01 03:29:18 +0200

    Ports/libgpg-error: Update libgpg-error to version 1.44
    The syntax-error patch is no longer needed. :^)

--- Ports/libgpg-error/package.sh
+++ Ports/libgpg-error/package.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=libgpg-error
-version=1.42
+version=1.44
 useconfigure=true
 use_fresh_config_sub=true
 config_sub_path=build-aux/config.sub
 depends=("gettext")
 configopts=("--disable-tests" "--disable-threads")
-files="https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-${version}.tar.bz2 libgpg-error-${version}.tar.bz2 fc07e70f6c615f8c4f590a8e37a9b8dd2e2ca1e9408f8e60459c67452b925e23"
+files="https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-${version}.tar.bz2 libgpg-error-${version}.tar.bz2 8e3d2da7a8b9a104dd8e9212ebe8e0daf86aa838cc1314ba6bc4de8f2d8a1ff9"
 auth_type=sha256
 
 configure() {
