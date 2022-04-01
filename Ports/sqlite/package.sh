0d0ab08ff60d04ca3f5409dab9cceeb5b1b13831
EWouters <6179932+EWouters@users.noreply.github.com>
2022-04-01 05:11:00 +0200

    Ports/sqlite: Update sqlite to version 3380200

--- Ports/sqlite/package.sh
+++ Ports/sqlite/package.sh
@@ -2,7 +2,7 @@
 port=sqlite
 useconfigure="true"
 use_fresh_config_sub="true"
-version="3350500"
-files="https://www.sqlite.org/2021/sqlite-autoconf-${version}.tar.gz sqlite-autoconf-${version}.tar.gz f52b72a5c319c3e516ed7a92e123139a6e87af08a2dc43d7757724f6132e6db0"
+version="3380200"
+files="https://www.sqlite.org/2022/sqlite-autoconf-${version}.tar.gz sqlite-autoconf-${version}.tar.gz e7974aa1430bad690a5e9f79a6ee5c8492ada8269dc675875ad0fb747d7cada4"
 auth_type=sha256
 workdir="sqlite-autoconf-${version}"
