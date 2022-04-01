20d4566ad418519970a09c22450791ea247cdfb1
EWouters <6179932+EWouters@users.noreply.github.com>
2022-04-01 05:05:34 +0200

    Ports/SDL2_ttf: Update SDL2_ttf to version 2.0.18

--- Ports/SDL2_ttf/package.sh
+++ Ports/SDL2_ttf/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=SDL2_ttf
-version=2.0.15
+version=2.0.18
 useconfigure=true
 use_fresh_config_sub=true
-files="https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-${version}.tar.gz SDL2_ttf-${version}.tar.gz a9eceb1ad88c1f1545cd7bd28e7cbc0b2c14191d40238f531a15b01b1b22cd33"
+files="https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-${version}.tar.gz SDL2_ttf-${version}.tar.gz 7234eb8883514e019e7747c703e4a774575b18d435c22a4a29d068cb768a2251"
 auth_type=sha256
 depends=("SDL2" "freetype")
 
