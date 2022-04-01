1d47699e6ad50ac7039d8f97a3c9a61ac309f6de
EWouters <6179932+EWouters@users.noreply.github.com>
2022-04-01 05:12:17 +0200

    Ports/tcl: Update tcl to version 8.6.12

--- Ports/tcl/package.sh
+++ Ports/tcl/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=tcl
-version=8.6.11
+version=8.6.12
 workdir=tcl${version}/unix
 useconfigure=true
-files="https://prdownloads.sourceforge.net/tcl/tcl${version}-src.tar.gz tcl${version}.tar.gz 8c0486668586672c5693d7d95817cb05a18c5ecca2f40e2836b9578064088258"
+files="https://prdownloads.sourceforge.net/tcl/tcl${version}-src.tar.gz tcl${version}.tar.gz 26c995dd0f167e48b11961d891ee555f680c175f7173ff8cb829f4ebcde4c1a6"
 auth_type=sha256
