5f4e6095c0b2a27975bda6846dc12376e88c6cfe
EWouters <6179932+EWouters@users.noreply.github.com>
2022-04-01 03:27:25 +0200

    Ports/libgcrypt: Update libgcrypt to version 1.10.1

--- Ports/libgcrypt/package.sh
+++ Ports/libgcrypt/package.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=libgcrypt
-version=1.9.2
+version=1.10.1
 useconfigure=true
 use_fresh_config_sub=true
 config_sub_path=build-aux/config.sub
 configopts=("--with-libgpg-error-prefix=${SERENITY_INSTALL_ROOT}/usr/local")
 depends=("libgpg-error")
-files="https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-${version}.tar.bz2 libgcrypt-${version}.tar.bz2 b2c10d091513b271e47177274607b1ffba3d95b188bbfa8797f948aec9053c5a"
+files="https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-${version}.tar.bz2 libgcrypt-${version}.tar.bz2 ef14ae546b0084cd84259f61a55e07a38c3b53afc0f546bffcef2f01baffe9de"
 auth_type=sha256
 
 pre_configure() {
