29e97bb089f2ccd060a8a0ef207a1831ddadb705
EWouters <6179932+EWouters@users.noreply.github.com>
2022-04-01 04:59:20 +0200

    Ports/pt2-clone: Update pt2-clone to version 1.43
    Upgrade patches to git style, add ReadMe.md and remove from the list
    of ports missing descriptions.

--- Ports/pt2-clone/package.sh
+++ Ports/pt2-clone/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=pt2-clone
-version=1.28
+version=1.43
 useconfigure=true
-files="https://github.com/8bitbubsy/pt2-clone/archive/v${version}.tar.gz v${version}.tar.gz a3ce83e326d94f1abf6dd75fb788fe508922818c08e6f988155df9ed288f180e"
+files="https://github.com/8bitbubsy/pt2-clone/archive/v${version}.tar.gz v${version}.tar.gz 760c1545105fbf3798fd101c6f213e0fd60943869023ef735f16f4b35221e007"
 auth_type=sha256
 configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt")
 depends=("SDL2")
