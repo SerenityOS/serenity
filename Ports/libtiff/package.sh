459059ec5e264b3ae57a304255ffa13415d5af9c
EWouters <6179932+EWouters@users.noreply.github.com>
2022-04-02 12:34:18 +0200

    Ports/libtiff: Name port as folder name and fix workdir
    Previously when installing libtiff as a dependency for another port
    it would not find the entry in `packages.db` as the ports that
    depend on libtiff would use the folder name rather than the port
    name in the dependency list. They did this because there was is a
    bug in `.port_include.sh` that cds to the port name and not the
    folder the port lives in. This is a workaround for that issue.

--- Ports/libtiff/package.sh
+++ Ports/libtiff/package.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=tiff
+port=libtiff
 version=4.3.0
 useconfigure=true
+workdir="tiff-$version"
 files="http://download.osgeo.org/libtiff/tiff-${version}.tar.gz tiff-${version}.tar.gz 0e46e5acb087ce7d1ac53cf4f56a09b221537fc86dfc5daaad1c2e89e1b37ac8"
 auth_type="sha256"
 depends=("libjpeg" "zstd" "xz")
