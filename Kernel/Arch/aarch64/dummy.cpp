2f67eccf474989e2b19d4c6d6216c2d5bdc6149b
James Mintram <me@jamesrm.com>
2022-04-02 23:49:19 +0100

    Kernel: Add all memory files to aarch64 and fix resulting linker errors

--- Kernel/Arch/aarch64/dummy.cpp
+++ Kernel/Arch/aarch64/dummy.cpp
@@ -4,15 +4,121 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <AK/Singleton.h>
 #include <AK/Types.h>
+
+#include <Kernel/FileSystem/Inode.h>
 #include <Kernel/KString.h>
 #include <Kernel/KSyms.h>
+#include <Kernel/Locking/SpinlockProtected.h>
+#include <Kernel/Memory/SharedInodeVMObject.h>
+#include <Kernel/Panic.h>
+#include <Kernel/PhysicalAddress.h>
+#include <Kernel/Random.h>
 #include <Kernel/Sections.h>
+#include <Kernel/UserOrKernelBuffer.h>
 
 // This is a temporary file to get a non-empty Kernel binary on aarch64.
 // The prekernel currently never jumps to the kernel. This is dead code.
 void dummy();
 void dummy() { }
+
+// Scheduler
+namespace Kernel {
+
+READONLY_AFTER_INIT Thread* g_finalizer;
+
+}
+
+// Panic
+namespace Kernel {
+
+void __panic(char const*, unsigned int, char const*)
+{
+    for (;;) { }
+}
+
+}
+
+// Random
+namespace Kernel {
+
+void get_fast_random_bytes(Bytes) { }
+
+}
+
+// Inode
+namespace Kernel {
+
+static Singleton<SpinlockProtected<Inode::AllInstancesList>> s_all_instances;
+
+SpinlockProtected<Inode::AllInstancesList>& Inode::all_instances()
+{
+    return s_all_instances;
+}
+
+RefPtr<Memory::SharedInodeVMObject> Inode::shared_vmobject() const
+{
+    return RefPtr<Memory::SharedInodeVMObject>(nullptr);
+}
+
+void Inode::will_be_destroyed()
+{
+}
+
+ErrorOr<void> Inode::set_shared_vmobject(Memory::SharedInodeVMObject&)
+{
+    return {};
+}
+
+}
+
+// UserOrKernelBuffer.cpp
+namespace Kernel {
+
+ErrorOr<void> UserOrKernelBuffer::write(void const*, size_t, size_t)
+{
+    return {};
+}
+
+ErrorOr<void> UserOrKernelBuffer::read(void*, size_t, size_t) const
+{
+    return {};
+}
+
+}
+
+// x86 init
+
+multiboot_module_entry_t multiboot_copy_boot_modules_array[16];
+size_t multiboot_copy_boot_modules_count;
+
+extern "C" {
+READONLY_AFTER_INIT PhysicalAddress start_of_prekernel_image;
+READONLY_AFTER_INIT PhysicalAddress end_of_prekernel_image;
+READONLY_AFTER_INIT size_t physical_to_virtual_offset;
+// READONLY_AFTER_INIT FlatPtr kernel_mapping_base;
+READONLY_AFTER_INIT FlatPtr kernel_load_base;
+#if ARCH(X86_64)
+READONLY_AFTER_INIT PhysicalAddress boot_pml4t;
+#endif
+READONLY_AFTER_INIT PhysicalAddress boot_pdpt;
+READONLY_AFTER_INIT PhysicalAddress boot_pd0;
+READONLY_AFTER_INIT PhysicalAddress boot_pd_kernel;
+READONLY_AFTER_INIT Kernel::PageTableEntry* boot_pd_kernel_pt1023;
+READONLY_AFTER_INIT char const* kernel_cmdline;
+READONLY_AFTER_INIT u32 multiboot_flags;
+READONLY_AFTER_INIT multiboot_memory_map_t* multiboot_memory_map;
+READONLY_AFTER_INIT size_t multiboot_memory_map_count;
+READONLY_AFTER_INIT multiboot_module_entry_t* multiboot_modules;
+READONLY_AFTER_INIT size_t multiboot_modules_count;
+READONLY_AFTER_INIT PhysicalAddress multiboot_framebuffer_addr;
+READONLY_AFTER_INIT u32 multiboot_framebuffer_pitch;
+READONLY_AFTER_INIT u32 multiboot_framebuffer_width;
+READONLY_AFTER_INIT u32 multiboot_framebuffer_height;
+READONLY_AFTER_INIT u8 multiboot_framebuffer_bpp;
+READONLY_AFTER_INIT u8 multiboot_framebuffer_type;
+}
 
 // kmalloc.h
 size_t kmalloc_good_size(size_t);
