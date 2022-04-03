9840c6c223100d85323dacea9c17a70b5fa7debb
Hunter Kvalevog <hunterkva@protonmail.com>
2022-04-02 19:14:23 -0500

    Ports: Add x264

---
+++ Ports/x264/package.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+
+port=x264
+version=bfc87b7a330f75f5c9a21e56081e4b20344f139e
+useconfigure=true
+files="https://code.videolan.org/videolan/x264/-/archive/${version}/x264-master.tar.gz libx264-${version}.tar.gz 2ca2344fc2d657150599687e128dac8816b07e8d5c991fa2281e51fe647bbbe7"
+auth_type=sha256
+configopts=("--enable-shared")
---
+++ Ports/x264/patches/ReadMe.md
@@ -0,0 +1,5 @@
+# Patches for x264 on SerenityOS
+
+## `serenity-configure.patch`
+
+Adds `serenity` as a valid platform
