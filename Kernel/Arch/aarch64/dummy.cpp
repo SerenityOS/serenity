72ae123c59da741a16773179455f0492c00d5f7a
James Mintram <me@jamesrm.com>
2022-04-04 22:02:40 +0100

    Kernel: Remove unused dummy function from dummy.cpp

--- Kernel/Arch/aarch64/dummy.cpp
+++ Kernel/Arch/aarch64/dummy.cpp
@@ -18,11 +18,6 @@
 #include <Kernel/Sections.h>
 #include <Kernel/UserOrKernelBuffer.h>
 
-// This is a temporary file to get a non-empty Kernel binary on aarch64.
-// The prekernel currently never jumps to the kernel. This is dead code.
-void dummy();
-void dummy() { }
-
 // Scheduler
 namespace Kernel {
 
