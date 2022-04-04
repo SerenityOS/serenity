c9f631a7ea55fabf69aaef0785d34902932ea45c
James Mintram <me@jamesrm.com>
2022-04-04 22:17:02 +0100

    Kernel: Add VERIFY_NOT_REACHED to aarch64 locking functions

--- Kernel/Arch/aarch64/Spinlock.h
+++ Kernel/Arch/aarch64/Spinlock.h
@@ -24,20 +24,24 @@ public:
 
     ALWAYS_INLINE u32 lock()
     {
+        VERIFY_NOT_REACHED();
         return 0;
     }
 
     ALWAYS_INLINE void unlock(u32 /*prev_flags*/)
     {
+        VERIFY_NOT_REACHED();
     }
 
     [[nodiscard]] ALWAYS_INLINE bool is_locked() const
     {
+        VERIFY_NOT_REACHED();
         return false;
     }
 
     ALWAYS_INLINE void initialize()
     {
+        VERIFY_NOT_REACHED();
     }
 };
 
@@ -49,29 +53,35 @@ public:
     RecursiveSpinlock(LockRank rank = LockRank::None)
     {
         (void)rank;
+        VERIFY_NOT_REACHED();
     }
 
     ALWAYS_INLINE u32 lock()
     {
+        VERIFY_NOT_REACHED();
         return 0;
     }
 
     ALWAYS_INLINE void unlock(u32 /*prev_flags*/)
     {
+        VERIFY_NOT_REACHED();
     }
 
     [[nodiscard]] ALWAYS_INLINE bool is_locked() const
     {
+        VERIFY_NOT_REACHED();
         return false;
     }
 
     [[nodiscard]] ALWAYS_INLINE bool is_locked_by_current_processor() const
     {
+        VERIFY_NOT_REACHED();
         return false;
     }
 
     ALWAYS_INLINE void initialize()
     {
+        VERIFY_NOT_REACHED();
     }
 };
 
