7f81787f92c475b2a76b7cf31bf95bf44749df0f
Liav A <liavalb@gmail.com>
2022-04-09 17:39:27 +0300

    Kernel/IntelGraphics: Move DisplayPlane enable code to derived classes
    Instead of doing that on the IntelDisplayPlane class, let's have this in
    derived classes so these classes can decide how to use the settings that
    were provided before calling the enable method.

--- Kernel/Graphics/Intel/Plane/DisplayPlane.cpp
+++ Kernel/Graphics/Intel/Plane/DisplayPlane.cpp
@@ -20,13 +20,34 @@ IntelDisplayPlane::ShadowRegisters IntelDisplayPlane::shadow_registers() const
     return m_shadow_registers;
 }
 
-ErrorOr<void> IntelDisplayPlane::enable(Badge<IntelDisplayConnectorGroup>)
+ErrorOr<void> IntelDisplayPlane::set_horizontal_active_pixels_count(Badge<IntelDisplayConnectorGroup>, size_t horizontal_active_pixels_count)
 {
     SpinlockLocker locker(m_access_lock);
-    // Note: We use the shadow register so we don't have the already set
-    // settings being lost.
-    m_plane_registers->control = m_shadow_registers.control | (1 << 31);
-    m_shadow_registers.control |= (1 << 31);
+    m_horizontal_active_pixels_count = horizontal_active_pixels_count;
+    return {};
+}
+ErrorOr<void> IntelDisplayPlane::set_vertical_active_pixels_count(Badge<IntelDisplayConnectorGroup>, size_t vertical_active_pixels_count)
+{
+    SpinlockLocker locker(m_access_lock);
+    m_vertical_active_pixels_count = vertical_active_pixels_count;
+    return {};
+}
+ErrorOr<void> IntelDisplayPlane::set_horizontal_stride(Badge<IntelDisplayConnectorGroup>, size_t horizontal_stride)
+{
+    SpinlockLocker locker(m_access_lock);
+    m_horizontal_stride = horizontal_stride;
+    return {};
+}
+ErrorOr<void> IntelDisplayPlane::set_aperture_base(Badge<IntelDisplayConnectorGroup>, PhysicalAddress aperture_start)
+{
+    SpinlockLocker locker(m_access_lock);
+    m_aperture_start.set(aperture_start.get());
+    return {};
+}
+ErrorOr<void> IntelDisplayPlane::set_pipe(Badge<IntelDisplayConnectorGroup>, PipeSelect pipe_select)
+{
+    SpinlockLocker locker(m_access_lock);
+    m_pipe_select = pipe_select;
     return {};
 }
 
--- Kernel/Graphics/Intel/Plane/G33DisplayPlane.h
+++ Kernel/Graphics/Intel/Plane/G33DisplayPlane.h
@@ -18,7 +18,7 @@ class IntelG33DisplayPlane final : public IntelDisplayPlane {
 public:
     static ErrorOr<NonnullOwnPtr<IntelG33DisplayPlane>> create_with_physical_address(PhysicalAddress plane_registers_start_address);
 
-    virtual ErrorOr<void> set_plane_settings(Badge<IntelDisplayConnectorGroup>, PhysicalAddress aperture_start, PipeSelect, size_t horizontal_active_pixels_count) override;
+    virtual ErrorOr<void> enable(Badge<IntelDisplayConnectorGroup>) override;
 
 private:
     explicit IntelG33DisplayPlane(Memory::TypedMapping<volatile IntelDisplayPlane::PlaneRegisters> plane_registers_mapping);
