8ba0d6b93ce926b330da0f8ec68fdce68d2156ce
Thitat Auareesuksakul <thitat@flux.ci>
2022-04-12 16:56:26 +0700

    Services: Use Keymap::read_all() for KeyboardPreferenceLoader

--- Userland/Services/KeyboardPreferenceLoader/main.cpp
+++ Userland/Services/KeyboardPreferenceLoader/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, the SerenityOS developers.
+ * Copyright (c) 2021-2022, the SerenityOS developers.
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -7,6 +7,7 @@
 #include <LibCore/ConfigFile.h>
 #include <LibCore/File.h>
 #include <LibCore/System.h>
+#include <LibKeyboard/Keymap.h>
 #include <LibMain/Main.h>
 #include <errno.h>
 #include <spawn.h>
@@ -20,18 +21,14 @@ ErrorOr<int> serenity_main(Main::Arguments)
     auto keyboard_settings_config = TRY(Core::ConfigFile::open_for_app("KeyboardSettings"));
 
     TRY(Core::System::unveil("/bin/keymap", "x"));
-    TRY(Core::System::unveil("/etc/Keyboard.ini", "r"));
+    TRY(Core::System::unveil(Keyboard::Keymap::config_file_path(), "r"));
     TRY(Core::System::unveil("/dev/keyboard0", "r"));
     TRY(Core::System::unveil(nullptr, nullptr));
-    auto mapper_config = TRY(Core::ConfigFile::open("/etc/Keyboard.ini"));
-    auto keymaps = mapper_config->read_entry("Mapping", "Keymaps", "");
 
-    auto keymaps_vector = keymaps.split(',');
-    if (keymaps_vector.size() == 0)
-        exit(1);
+    auto keymaps_vector = TRY(Keyboard::Keymap::read_all());
 
     pid_t child_pid;
-    char const* argv[] = { "/bin/keymap", "-m", keymaps_vector.first().characters(), nullptr };
+    char const* argv[] = { "/bin/keymap", "-m", keymaps_vector.first().name().characters(), nullptr };
     if ((errno = posix_spawn(&child_pid, "/bin/keymap", nullptr, nullptr, const_cast<char**>(argv), environ))) {
         perror("posix_spawn");
         exit(1);
