2d51f414393837baeedace4ca6e18c23c46288e5
Thitat Auareesuksakul <thitat@flux.ci>
2022-04-12 21:03:42 +0700

    Applets: Add click to cycle functionality for Keymap applet

--- Userland/Applets/Keymap/KeymapStatusWindow.cpp
+++ Userland/Applets/Keymap/KeymapStatusWindow.cpp
@@ -1,22 +1,28 @@
 /*
  * Copyright (c) 2021, Timur Sultanov <SultanovTS@yandex.ru>
+ * Copyright (c) 2022, Thitat Auareesuksakul <thitat@flux.ci>
  * Copyright (c) 2022, the SerenityOS developers.
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #include "KeymapStatusWindow.h"
+#include <LibCore/ConfigFile.h>
 #include <LibCore/Process.h>
 #include <LibGUI/ConnectionToWindowMangerServer.h>
 #include <LibGUI/Painter.h>
 #include <LibKeyboard/CharacterMap.h>
+#include <spawn.h>
 
 void KeymapStatusWidget::mousedown_event(GUI::MouseEvent& event)
 {
-    if (event.button() != GUI::MouseButton::Primary)
-        return;
+    if (event.button() == GUI::MouseButton::Primary) {
+        if (parent())
+            static_cast<KeymapStatusWindow*>(parent())->cycle_keymaps();
+    }
 
-    Core::Process::spawn("/bin/KeyboardSettings");
+    if (event.button() == GUI::MouseButton::Secondary)
+        Core::Process::spawn("/bin/KeyboardSettings");
 }
 
 KeymapStatusWindow::KeymapStatusWindow()
@@ -25,10 +31,40 @@ KeymapStatusWindow::KeymapStatusWindow()
     set_has_alpha_channel(true);
     m_status_widget = &set_main_widget<KeymapStatusWidget>();
 
+    m_file_watcher = MUST(Core::FileWatcher::create());
+    m_file_watcher->on_change = [this](auto&) {
+        refresh_keymaps();
+    };
+
+    MUST(m_file_watcher->add_watch(Keyboard::Keymap::config_file_path(), Core::FileWatcherEvent::Type::ContentModified));
+
+    refresh_keymaps(false);
+}
+
+void KeymapStatusWindow::refresh_keymaps(bool repaint_background)
+{
+    m_keymaps.clear();
+    m_keymaps = MUST(Keyboard::Keymap::read_all());
+
     auto current_keymap = MUST(Keyboard::CharacterMap::fetch_system_map());
     auto current_keymap_name = current_keymap.character_map_name();
-    m_status_widget->set_tooltip(current_keymap_name);
-    m_status_widget->set_text(current_keymap_name.substring(0, 2));
+    auto it = m_keymaps.find_if([&](auto const& value) {
+        return value.name() == current_keymap_name;
+    });
+
+    if (!it.is_end())
+        m_keymap_index = it.index();
+    else
+        m_keymap_index = -1;
+
+    set_keymap_text(current_keymap_name, repaint_background);
+}
+
+void KeymapStatusWindow::cycle_keymaps()
+{
+    m_keymap_index = (m_keymap_index + 1) % m_keymaps.size();
+    auto keymap_name = m_keymaps[m_keymap_index].name();
+    set_keymap(keymap_name);
 }
 
 void KeymapStatusWindow::wm_event(GUI::WMEvent& event)
@@ -40,10 +76,24 @@ void KeymapStatusWindow::wm_event(GUI::WMEvent& event)
     }
 }
 
-void KeymapStatusWindow::set_keymap_text(String const& keymap)
+void KeymapStatusWindow::set_keymap(String const& keymap)
 {
-    GUI::Painter painter(*m_status_widget);
-    painter.clear_rect(m_status_widget->rect(), Color::from_argb(0));
+    pid_t child_pid;
+    char const* argv[] = { "/bin/keymap", "-m", keymap.characters(), nullptr };
+    if ((errno = posix_spawn(&child_pid, "/bin/keymap", nullptr, nullptr, const_cast<char**>(argv), environ))) {
+        perror("posix_spawn");
+        dbgln("Failed to call /bin/keymap, error: {} ({})", errno, strerror(errno));
+    }
+
+    set_keymap_text(keymap);
+}
+
+void KeymapStatusWindow::set_keymap_text(String const& keymap, bool repaint_background)
+{
+    if (repaint_background) {
+        GUI::Painter painter(*m_status_widget);
+        painter.clear_rect(m_status_widget->rect(), Color::from_argb(0));
+    }
 
     m_status_widget->set_tooltip(keymap);
     m_status_widget->set_text(keymap.substring(0, 2));
--- Userland/Applets/Keymap/KeymapStatusWindow.h
+++ Userland/Applets/Keymap/KeymapStatusWindow.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2021, Timur Sultanov <SultanovTS@yandex.ru>
+ * Copyright (c) 2022, Thitat Auareesuksakul <thitat@flux.ci>
  * Copyright (c) 2022, the SerenityOS developers.
  *
  * SPDX-License-Identifier: BSD-2-Clause
@@ -7,25 +8,33 @@
 
 #pragma once
 
+#include <LibCore/FileWatcher.h>
 #include <LibGUI/Label.h>
 #include <LibGUI/Window.h>
+#include <LibKeyboard/Keymap.h>
 
 class KeymapStatusWidget : public GUI::Label {
     C_OBJECT(KeymapStatusWidget);
 
     virtual void mousedown_event(GUI::MouseEvent& event) override;
 };
+
 class KeymapStatusWindow final : public GUI::Window {
     C_OBJECT(KeymapStatusWindow)
 public:
     virtual ~KeymapStatusWindow() override = default;
+    void cycle_keymaps();
 
 private:
     virtual void wm_event(GUI::WMEvent&) override;
 
     KeymapStatusWindow();
+    void refresh_keymaps(bool repaint_background = true);
+    void set_keymap(String const& keymap);
+    void set_keymap_text(String const& keymap, bool repaint_background = true);
 
     RefPtr<KeymapStatusWidget> m_status_widget;
-
-    void set_keymap_text(String const& keymap);
+    RefPtr<Core::FileWatcher> m_file_watcher;
+    Vector<Keyboard::Keymap> m_keymaps;
+    ssize_t m_keymap_index { -1 };
 };
