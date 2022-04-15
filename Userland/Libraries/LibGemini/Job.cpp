6993c6bd4d98dc7aaa35a31542f76889d1a3223a
Sam Atkins <atkinssj@serenityos.org>
2022-04-15 13:44:23 +0100

    LibCore+Everywhere: Make Core::Stream read_until() return Bytes
    This affects BufferedSeekable::read_until() and ::read_until_any_of().
    For the reasoning, see the previous commit about Core::Stream::read().

--- Userland/Libraries/LibGemini/Job.cpp
+++ Userland/Libraries/LibGemini/Job.cpp
@@ -56,8 +56,8 @@ bool Job::can_read_line() const
 String Job::read_line(size_t size)
 {
     ByteBuffer buffer = ByteBuffer::create_uninitialized(size).release_value_but_fixme_should_propagate_errors();
-    auto nread = MUST(m_socket->read_until(buffer, "\r\n"sv));
-    return String::copy(buffer.span().slice(0, nread));
+    auto bytes_read = MUST(m_socket->read_until(buffer, "\r\n"sv));
+    return String::copy(bytes_read);
 }
 
 ByteBuffer Job::receive(size_t size)
