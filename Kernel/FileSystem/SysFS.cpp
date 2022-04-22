0979852040ef09b3e1c07dc55310c12bfe507322
Liav A <liavalb@gmail.com>
2022-04-22 20:35:15 +0300

    Kernel/SysFS: Add Symbolic link functionality to the filesystem
    This will be used later on to help connecting a node at /sys/dev/block/
    that represents a Storage device to a directory in /sys/devices/storage/
    with details on that device in that directory.

--- Kernel/FileSystem/SysFS.cpp
+++ Kernel/FileSystem/SysFS.cpp
@@ -131,6 +131,31 @@ ErrorOr<void> SysFSInode::truncate(u64 size)
     return m_associated_component->truncate(size);
 }
 
+ErrorOr<NonnullRefPtr<SysFSLinkInode>> SysFSLinkInode::try_create(SysFS const& sysfs, SysFSComponent const& component)
+{
+    return adopt_nonnull_ref_or_enomem(new (nothrow) SysFSLinkInode(sysfs, component));
+}
+
+SysFSLinkInode::SysFSLinkInode(SysFS const& fs, SysFSComponent const& component)
+    : SysFSInode(fs, component)
+{
+}
+
+SysFSLinkInode::~SysFSLinkInode() = default;
+
+InodeMetadata SysFSLinkInode::metadata() const
+{
+    // NOTE: No locking required as m_associated_component or its component index will never change during our lifetime.
+    InodeMetadata metadata;
+    metadata.inode = { fsid(), m_associated_component->component_index() };
+    metadata.mode = S_IFLNK | S_IRUSR | S_IRGRP | S_IROTH | S_IXOTH;
+    metadata.uid = 0;
+    metadata.gid = 0;
+    metadata.size = 0;
+    metadata.mtime = TimeManagement::boot_time();
+    return metadata;
+}
+
 ErrorOr<NonnullRefPtr<SysFSDirectoryInode>> SysFSDirectoryInode::try_create(SysFS const& sysfs, SysFSComponent const& component)
 {
     return adopt_nonnull_ref_or_enomem(new (nothrow) SysFSDirectoryInode(sysfs, component));
