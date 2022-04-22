a87c85f4011169541583fe5530b5c5d1ca7f5991
Tim Schumacher <timschumi@gmx.de>
2022-04-22 11:07:18 +0200

    unzip: Create parent directory before extracting files

--- Userland/Utilities/unzip.cpp
+++ Userland/Utilities/unzip.cpp
@@ -27,6 +27,7 @@ static bool unpack_zip_member(Archive::ZipMember zip_member, bool quiet)
             outln(" extracting: {}", zip_member.name);
         return true;
     }
+    MUST(Core::Directory::create(LexicalPath(zip_member.name).parent(), Core::Directory::CreateDirectories::Yes));
     auto new_file = Core::File::construct(zip_member.name);
     if (!new_file->open(Core::OpenMode::WriteOnly)) {
         warnln("Can't write file {}: {}", zip_member.name, new_file->error_string());
