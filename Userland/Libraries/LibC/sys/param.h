577b4c73b0ae4dd9163be483de0b50d9d468c690
Andreas Kling <kling@serenityos.org>
2022-04-29 20:29:05 +0200

    LibC: Add MIN, MAX and howmany macros to sys/param.h

--- Userland/Libraries/LibC/sys/param.h
+++ Userland/Libraries/LibC/sys/param.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2018-2022, Andreas Kling <kling@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -8,3 +8,15 @@
 
 #include <endian.h>
 #include <limits.h>
+
+#ifndef MIN
+#    define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#endif
+
+#ifndef MAX
+#    define MAX(a, b) (((a) > (b)) ? (a) : (b))
+#endif
+
+#ifndef howmany
+#    define howmany(x, y) (((x) + ((y)-1)) / (y))
+#endif
