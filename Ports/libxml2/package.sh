f3e78836a7f012df697e86dee5d360e92c356592
Tim Schumacher <timschumi@gmx.de>
2022-04-30 01:13:14 +0200

    Ports: Set the correct prefix for libxml2
    The prefix should apply both inside and outside the system. Having the
    full host path there only confuses software that is built inside the
    system, as well as other ports that prepend the host path themselves
    additionally.

--- Ports/libxml2/package.sh
+++ Ports/libxml2/package.sh
@@ -6,11 +6,10 @@ use_fresh_config_sub=true
 files="https://download.gnome.org/sources/libxml2/2.9/libxml2-${version}.tar.xz libxml2-${version}.tar.xz 276130602d12fe484ecc03447ee5e759d0465558fbc9d6bd144e3745306ebf0e"
 auth_type=sha256
 depends=("libiconv" "xz")
-configopts=("--prefix=${SERENITY_INSTALL_ROOT}/usr/local" "--without-python")
+configopts=("--with-sysroot=${SERENITY_INSTALL_ROOT}" "--prefix=/usr/local" "--without-python")
 
 install() {
-    # Leave out DESTDIR - otherwise the prefix breaks
-    run make install
+    run make DESTDIR="${SERENITY_INSTALL_ROOT}" install
 
     # Link shared library
     run ${SERENITY_ARCH}-pc-serenity-gcc -shared -o ${SERENITY_INSTALL_ROOT}/usr/local/lib/libxml2.so -Wl,-soname,libxml2.so -Wl,--whole-archive ${SERENITY_INSTALL_ROOT}/usr/local/lib/libxml2.a -Wl,--no-whole-archive -llzma
