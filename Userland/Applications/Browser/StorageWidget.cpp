f3f19f83217e4c5b5e0e93e533e6116577cca7e2
networkException <git@nwex.de>
2022-05-01 00:47:01 +0200

    Browser: Consolidate StorageWidget tabs into a single gml file
    This patch removes the separate CookiesTab.gml file used to define the
    layout of the cookies and local storage tabs and moves those defintions
    into the StorageWidget.gml file.

--- Userland/Applications/Browser/StorageWidget.cpp
+++ Userland/Applications/Browser/StorageWidget.cpp
@@ -8,7 +8,6 @@
 #include "CookiesModel.h"
 #include "LocalStorageModel.h"
 #include <AK/Variant.h>
-#include <Applications/Browser/CookiesTabGML.h>
 #include <Applications/Browser/StorageWidgetGML.h>
 #include <LibGUI/TabWidget.h>
 #include <LibGUI/TableView.h>
@@ -21,10 +20,7 @@ StorageWidget::StorageWidget()
     load_from_gml(storage_widget_gml);
     auto& tab_widget = *find_descendant_of_type_named<GUI::TabWidget>("tab_widget");
 
-    auto cookies_tab = tab_widget.try_add_tab<GUI::Widget>("Cookies").release_value_but_fixme_should_propagate_errors();
-    cookies_tab->load_from_gml(cookies_tab_gml);
-
-    m_cookies_table_view = cookies_tab->find_descendant_of_type_named<GUI::TableView>("cookies_tableview");
+    m_cookies_table_view = tab_widget.find_descendant_of_type_named<GUI::TableView>("cookies_tableview");
     m_cookies_model = adopt_ref(*new CookiesModel());
 
     m_cookie_sorting_model = MUST(GUI::SortingProxyModel::create(*m_cookies_model));
@@ -34,10 +30,7 @@ StorageWidget::StorageWidget()
     m_cookies_table_view->set_column_headers_visible(true);
     m_cookies_table_view->set_alternating_row_colors(true);
 
-    auto local_storage_tab = tab_widget.try_add_tab<GUI::Widget>("Local Storage").release_value_but_fixme_should_propagate_errors();
-    local_storage_tab->load_from_gml(cookies_tab_gml);
-
-    m_local_storage_table_view = local_storage_tab->find_descendant_of_type_named<GUI::TableView>("cookies_tableview");
+    m_local_storage_table_view = tab_widget.find_descendant_of_type_named<GUI::TableView>("local_storage_tableview");
     m_local_storage_model = adopt_ref(*new LocalStorageModel());
 
     m_local_storage_sorting_model = MUST(GUI::SortingProxyModel::create(*m_local_storage_model));
