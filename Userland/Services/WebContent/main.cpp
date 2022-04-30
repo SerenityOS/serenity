bbd2d5976a5933d5a5d95f968f7376fe68bc6f6e
DexesTTP <dexes.ttp@gmail.com>
2022-04-30 12:06:30 +0200

    LibWeb: Abstract the LibProtocol ResourceLoader connection
    This is the final component that required LibProtocol as a dependency
    of LibWeb. With this, we can now remove the dependency, and LibWeb no
    longer requires IPC to work :^)

--- Userland/Services/WebContent/main.cpp
+++ Userland/Services/WebContent/main.cpp
@@ -10,8 +10,10 @@
 #include <LibIPC/SingleServer.h>
 #include <LibMain/Main.h>
 #include <LibWeb/ImageDecoding.h>
+#include <LibWeb/Loader/ResourceLoader.h>
 #include <LibWeb/WebSockets/WebSocket.h>
 #include <LibWebView/ImageDecoderClientAdapter.h>
+#include <LibWebView/RequestServerAdapter.h>
 #include <LibWebView/WebSocketClientAdapter.h>
 #include <WebContent/ConnectionFromClient.h>
 
@@ -28,6 +30,7 @@ ErrorOr<int> serenity_main(Main::Arguments)
 
     Web::ImageDecoding::Decoder::initialize(WebView::ImageDecoderClientAdapter::create());
     Web::WebSockets::WebSocketClientManager::initialize(TRY(WebView::WebSocketClientManagerAdapter::try_create()));
+    Web::ResourceLoader::initialize(TRY(WebView::RequestServerAdapter::try_create()));
 
     auto client = TRY(IPC::take_over_accepted_client_from_system_server<WebContent::ConnectionFromClient>());
     return event_loop.exec();
