0e9100e3c2568d9f6b27a5bed60a774011a89b2c
EWouters <6179932+EWouters@users.noreply.github.com>
2022-05-08 15:41:49 +0200

    Ports/cmake: Update cmake to version 3.23.1

--- Ports/cmake/package.sh
+++ Ports/cmake/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=cmake
-version=3.23.0
+version=3.23.1
 useconfigure=true
-files="https://github.com/Kitware/CMake/releases/download/v$version/cmake-$version.tar.gz cmake-$version.tar.gz 5ab0a12f702f44013be7e19534cd9094d65cc9fe7b2cd0f8c9e5318e0fe4ac82"
+files="https://github.com/Kitware/CMake/releases/download/v$version/cmake-$version.tar.gz cmake-$version.tar.gz 33fd10a8ec687a4d0d5b42473f10459bb92b3ae7def2b745dc10b192760869f3"
 auth_type=sha256
 depends=("bash" "make" "sed" "ncurses" "libuv" "openssl")
 configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt" "-DCMAKE_USE_SYSTEM_LIBRARY_LIBUV=1" "-DCMAKE_USE_OPENSSL=ON" "-GNinja")
