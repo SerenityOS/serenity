cc7723b6c4c06ea5509efb3f6ca2638141ac5ef8
Timon Kruiper <timonkruiper@gmail.com>
2022-05-11 10:31:16 +0200

    Meta: Add option to disable Kernel Address Sanitizer
    By default we enable the Kernel Undefined Behavior Sanitizer, which
    checks for undefined behavior at runtime. However, sometimes a developer
    might want to turn that off, so now there is a easy way to do that.

--- Meta/CMake/serenity_options.cmake
+++ Meta/CMake/serenity_options.cmake
@@ -10,6 +10,7 @@ serenity_option(ENABLE_PNP_IDS_DOWNLOAD ON CACHE BOOL "Enable download of the pn
 serenity_option(ENABLE_KERNEL_ADDRESS_SANITIZER OFF CACHE BOOL "Enable kernel address sanitizer testing in gcc/clang")
 serenity_option(ENABLE_KERNEL_COVERAGE_COLLECTION  OFF CACHE BOOL "Enable KCOV and kernel coverage instrumentation in gcc/clang")
 serenity_option(ENABLE_KERNEL_LTO OFF CACHE BOOL "Build the kernel with link-time optimization")
+serenity_option(ENABLE_KERNEL_UNDEFINED_SANITIZER ON CACHE BOOL "Enable the Kernel Undefined Behavior Sanitizer (KUBSAN)")
 serenity_option(ENABLE_EXTRA_KERNEL_DEBUG_SYMBOLS  OFF CACHE BOOL "Enable -Og and -ggdb3 options for Kernel code for easier debugging")
 serenity_option(ENABLE_MOLD_LINKER OFF CACHE BOOL "Link the SerenityOS userland with the mold linker")
 serenity_option(ENABLE_USERSPACE_COVERAGE_COLLECTION OFF CACHE BOOL "Enable code coverage instrumentation for userspace binaries in clang")
