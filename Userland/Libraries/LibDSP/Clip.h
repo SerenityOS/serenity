f23aea0c4b049c7b2ffab442175dfbd2ea7b54e8
kleines Filmröllchen <filmroellchen@serenityos.org>
2022-05-11 21:59:49 +0200

    LibDSP: Make the note frequencies an AK::Array instead of a C array
    This was a leftover from the early days of Piano, and there's no reason
    to leave it that way especially if we want to use more complex
    collection APIs in the future.

--- Userland/Libraries/LibDSP/Clip.h
+++ Userland/Libraries/LibDSP/Clip.h
@@ -47,10 +47,10 @@ class NoteClip final : public Clip {
 public:
     void set_note(RollNote note);
 
-    Array<SinglyLinkedList<RollNote>, note_count> const& notes() const { return m_notes; }
+    Array<SinglyLinkedList<RollNote>, note_frequencies.size()> const& notes() const { return m_notes; }
 
 private:
-    Array<SinglyLinkedList<RollNote>, note_count> m_notes;
+    Array<SinglyLinkedList<RollNote>, note_frequencies.size()> m_notes;
 };
 
 }
