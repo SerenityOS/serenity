5a24accfa71b551b85689e68f4615fc0aa81beb3
Tim Schumacher <timschumi@gmx.de>
2022-05-21 21:51:39 +0200

    Ports: Update SDL2 to 2.0.22

--- Ports/SDL2/package.sh
+++ Ports/SDL2/package.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=SDL2
-version=c63a62ae49cd20e46786a4437edac09ff5c24c16
-workdir=SDL-${version}
+version=2.0.22
 useconfigure=true
 auth_type=sha256
-files="https://github.com/libsdl-org/SDL/archive/${version}.tar.gz SDL2-${version}.tar.gz 9f1e1a00cf2f840839f0a3158e3acc046526e4418378625929cb583eae3acc10"
+files="https://github.com/libsdl-org/SDL/releases/download/release-${version}/SDL2-${version}.tar.gz SDL2-${version}.tar.gz fe7cbf3127882e3fc7259a75a0cb585620272c51745d3852ab9dd87960697f2e"
 configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt" "-DPULSEAUDIO=OFF" "-DJACK=OFF" "-DEXTRA_LDFLAGS=-laudio;-liconv;-ldl")
 depends=("libiconv")
 
