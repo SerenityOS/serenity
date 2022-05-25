aa18adc43f0baca35ea7a387c1ef5ba0029dacb0
Tim Schumacher <timschumi@gmx.de>
2022-05-25 18:38:29 +0200

    Ports: Replace the `jq` `config.sub` patch with our download

--- Ports/jq/package.sh
+++ Ports/jq/package.sh
@@ -6,11 +6,13 @@ configopts=("--with-oniguruma=builtin" "--disable-maintainer-mode")
 files="https://github.com/stedolan/jq/releases/download/jq-${version}/jq-${version}.tar.gz jq-${version}.tar.gz 5de8c8e29aaa3fb9cc6b47bb27299f271354ebb72514e3accadc7d38b5bbaa72"
 auth_type=sha256
 makeopts=("LDFLAGS=-all-static")
+use_fresh_config_sub=true
+config_sub_paths=("config/config.sub" "modules/oniguruma/config.sub")
 
 pre_configure() {
+    # FIXME: The maintainer forgot to run autoconf before creating the tarball.
+    # Remove this when the next update is released.
     pushd $workdir/modules/oniguruma
-    autoreconf -fi
-    rm config.sub
-    cp ../../config/config.sub config.sub
+    autoreconf -i
     popd
 }
