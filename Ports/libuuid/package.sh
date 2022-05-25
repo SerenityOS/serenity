2c886fa93f2466244daae4787921f5824780fb28
Tim Schumacher <timschumi@gmx.de>
2022-05-25 13:39:30 +0200

    Ports: Use the official release tarball for `libuuid`
    These already come preconfigured

--- Ports/libuuid/package.sh
+++ Ports/libuuid/package.sh
@@ -5,14 +5,11 @@ version=2.38
 workdir="util-linux-${version}"
 useconfigure=true
 configopts=("--prefix=/usr/local" "--disable-all-programs" "--enable-libuuid")
-files="https://github.com/karelzak/util-linux/archive/refs/tags/v${version}.tar.gz util-linux-${version}.tar.gz f3cf9d165f50f46e5c0a1076d178a75a5ae30463345e9c19335552b249fe0e67
+files="https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v${version}/util-linux-${version}.tar.gz util-linux-${version}.tar.gz c31d4e54f30b56b0f7ec8b342658c07de81378f2c067941c2b886da356f8ad42
 https://git.savannah.gnu.org/cgit/config.git/plain/config.sub?id=c179db1b6f2ae484bfca1e9f8bae273e3319fa7d config.sub deb02c26f43b2ea64276c9ede77ec0f53d08e6256710f3c0a12275712085c348"
 auth_type=sha256
 
 pre_configure() {
-    # NOTE: This requires various tools to exist on the host machine (it'll tell you).
-    run ./autogen.sh
-
     # The generated config.sub is a *symlink* to the host's.
     # The one from GNU config should be similar enough, and recognizes serenity.
     run rm -f config/config.sub
