0d11e526cc129f9b8f7132a2354d574319791bd8
Tim Schumacher <timschumi@gmx.de>
2022-05-25 22:32:13 +0200

    Ports: Replace the `x264` `config.sub` patch with our download

--- Ports/x264/package.sh
+++ Ports/x264/package.sh
@@ -6,3 +6,4 @@ useconfigure=true
 files="https://code.videolan.org/videolan/x264/-/archive/${version}/x264-master.tar.gz libx264-${version}.tar.gz 2ca2344fc2d657150599687e128dac8816b07e8d5c991fa2281e51fe647bbbe7"
 auth_type=sha256
 configopts=("--enable-shared")
+use_fresh_config_sub=true
