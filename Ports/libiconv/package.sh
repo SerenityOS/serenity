0901321a35aaf2a0a7ee17c3bf7340b4f44f6d5f
Tim Schumacher <timschumi@gmx.de>
2022-05-25 13:59:31 +0200

    Ports: Replace the `libiconv` `config.sub` patch with our download

--- Ports/libiconv/package.sh
+++ Ports/libiconv/package.sh
@@ -5,6 +5,8 @@ useconfigure=true
 configopts=("--enable-shared" "--disable-nls")
 files="https://ftpmirror.gnu.org/gnu/libiconv/libiconv-${version}.tar.gz libiconv-${version}.tar.gz e6a1b1b589654277ee790cce3734f07876ac4ccfaecbee8afa0b649cf529cc04"
 auth_type="sha256"
+use_fresh_config_sub=true
+config_sub_paths=("build-aux/config.sub" "libcharset/build-aux/config.sub")
 
 install() {
     run make DESTDIR=${SERENITY_INSTALL_ROOT} "${installopts[@]}" install
