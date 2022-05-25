d04e1ba68f1a87fe04eeb95fa770065782a49f94
Tim Schumacher <timschumi@gmx.de>
2022-05-25 17:48:01 +0200

    Ports: Replace the manual `glu` `config.sub` patch with our download

--- Ports/glu/package.sh
+++ Ports/glu/package.sh
@@ -5,11 +5,7 @@ version="9.0.2"
 files="https://archive.mesa3d.org/glu/glu-${version}.tar.gz glu-${version}.tar.gz 24effdfb952453cc00e275e1c82ca9787506aba0282145fff054498e60e19a65"
 auth_type=sha256
 depends=("pkgconf")
+use_fresh_config_sub=true
 
 export GL_CFLAGS="-I${SERENITY_INSTALL_ROOT}/usr/include/LibGL"
 export GL_LIBS="-lgl"
-
-pre_configure() {
-    # Manual config.sub patch
-    run sed -i 's/haiku/serenity/' config.sub
-}
