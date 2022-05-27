e51547986e7dca7e7a54eeebe6013c23c22eda8d
circl <circl.lastname@gmail.com>
2022-05-27 17:37:30 +0200

    Ports/luajit: Move warning to configure
    Putting it in preconfigure did not work, as useconfigure wasn't enabled.

--- Ports/luajit/package.sh
+++ Ports/luajit/package.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=luajit
 version=2.1.0-beta3
+useconfigure=true
 files="https://luajit.org/download/LuaJIT-${version}.tar.gz LuaJIT-${version}.tar.gz 1ad2e34b111c802f9d0cdf019e986909123237a28c746b21295b63c9e785d9c3"
 auth_type=sha256
 workdir="LuaJIT-${version}"
-
-preconfigure() {
-    printf "\x1b[31m\x1b[5mATTENTION: \x1b[0m\x1b[31m\x1b[1mIf this fails, install either libc6-dev-i386 or libc6-dev-amd64\x1b[0m\n"
-}
 
 if [ ${SERENITY_ARCH} = "i686" ]; then
     M_FLAG=-m32
@@ -16,3 +13,7 @@ elif [ ${SERENITY_ARCH} = "x86_64" ]; then
 fi
 
 makeopts=("CROSS=${SERENITY_ARCH}-pc-serenity-" "HOST_CC=gcc ${M_FLAG}")
+
+configure() {
+    printf "\x1b[31m\x1b[5mATTENTION: \x1b[0m\x1b[31m\x1b[1mIf this fails, install either libc6-dev-i386 or libc6-dev-amd64\x1b[0m\n"
+}
