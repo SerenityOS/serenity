6b16430d013eae7c226af2875bb98cb53da3b950
circl <circl.lastname@gmail.com>
2022-05-27 18:22:36 +0200

    Ports/powdertoy: Use the release build type
    This should speed up the game

--- Ports/powdertoy/package.sh
+++ Ports/powdertoy/package.sh
@@ -2,7 +2,7 @@
 port=powdertoy
 version=96.2.350
 useconfigure=true
-configopts=("--cross-file" "../cross_file-$SERENITY_ARCH.txt")
+configopts=("-Dbuildtype=release" "build-release" "--cross-file" "../cross_file-${SERENITY_ARCH}.txt")
 depends=("luajit" "curl" "libfftw3f" "zlib" "SDL2")
 files="https://github.com/The-Powder-Toy/The-Powder-Toy/archive/refs/tags/v${version}.tar.gz The-Powder-Toy-${version}.tar.gz d95cbadee22632687661e8fc488bd64405d81c0dca737e16420f26e93ea5bf58"
 auth_type=sha256
@@ -14,13 +14,13 @@ launcher_command="/usr/local/bin/powder"
 launcher_run_in_terminal=false
 
 configure() {
-    run meson build-debug "${configopts[@]}"
+    run meson "${configopts[@]}"
 }
 
 build() {
-    run ninja -C build-debug
+    run ninja -C build-release
 }
 
 install() {
-    run cp build-debug/powder "${SERENITY_INSTALL_ROOT}/usr/local/bin"
+    run cp build-release/powder "${SERENITY_INSTALL_ROOT}/usr/local/bin"
 }
