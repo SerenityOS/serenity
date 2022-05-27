ddfcefe31162b7af4bcf6576a9abaa8d3871a161
circl <circl.lastname@gmail.com>
2022-05-27 17:40:37 +0200

    Ports/luajit: Use HOST_CC instead of hardcoding gcc

--- Ports/luajit/package.sh
+++ Ports/luajit/package.sh
@@ -12,7 +12,7 @@ elif [ ${SERENITY_ARCH} = "x86_64" ]; then
     M_FLAG=-m64
 fi
 
-makeopts=("CROSS=${SERENITY_ARCH}-pc-serenity-" "HOST_CC=gcc ${M_FLAG}")
+makeopts=("CROSS=${SERENITY_ARCH}-pc-serenity-" "HOST_CC=${HOST_CC} ${M_FLAG}")
 
 configure() {
     printf "\x1b[31m\x1b[5mATTENTION: \x1b[0m\x1b[31m\x1b[1mIf this fails, install either libc6-dev-i386 or libc6-dev-amd64\x1b[0m\n"
