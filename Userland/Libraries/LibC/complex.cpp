6ae49591a266b865fc26a157821b846217aed661
strnik12 <106550487+strnik12@users.noreply.github.com>
2022-05-30 22:08:59 +0300

    LibC: Fix a typo on INFINITY.

--- Userland/Libraries/LibC/complex.cpp
+++ Userland/Libraries/LibC/complex.cpp
@@ -122,7 +122,7 @@ float complex cprojf(float complex z)
 double complex cproj(double complex z)
 {
     if (isinf(creal(z)) || isinf(cimag(z))) {
-        return CMPLX(INFNINITY, copysign(0.0, cimag(z)));
+        return CMPLX(INFINITY, copysign(0.0, cimag(z)));
     }
     return z;
 }
