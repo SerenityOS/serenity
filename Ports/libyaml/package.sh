e5d18e3ad816f1c52179bcc904fc39aaa394c419
Tim Schumacher <timschumi@gmx.de>
2022-06-01 01:36:19 +0200

    Ports: Use the official release tarball for `libyaml`

--- Ports/libyaml/package.sh
+++ Ports/libyaml/package.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port="libyaml"
 version="0.2.5"
-files="https://github.com/yaml/libyaml/archive/refs/tags/${version}.tar.gz yaml-${version}.tar.gz fa240dbf262be053f3898006d502d514936c818e422afdcf33921c63bed9bf2e"
+files="https://github.com/yaml/libyaml/releases/download/${version}/yaml-${version}.tar.gz yaml-${version}.tar.gz c642ae9b75fee120b2d96c712538bd2cf283228d2337df2cf2988e3c02678ef4"
 useconfigure="true"
 auth_type="sha256"
-
-pre_configure() {
-    run ./bootstrap
-}
+use_fresh_config_sub=true
+config_sub_paths=("config/config.sub")
