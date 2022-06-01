1da48b2c0ced72cbf5f9e7e252af0403b85a44f9
EWouters <6179932+EWouters@users.noreply.github.com>
2022-06-01 19:12:56 +0200

    Ports/bc: Update bc to version 5.2.5

--- Ports/bc/package.sh
+++ Ports/bc/package.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=bc
-version=5.2.3
+port='bc'
+version='5.2.5'
 files="https://github.com/gavinhoward/bc/releases/download/${version}/bc-${version}.tar.xz bc-${version}.tar.xz
 https://github.com/gavinhoward/bc/releases/download/${version}/bc-${version}.tar.xz.sig bc-${version}.tar.xz.sig"
-useconfigure=true
-configscript=configure.sh
+useconfigure='true'
+configscript='configure.sh'
 auth_type="sig"
 auth_import_key="E2A30324A4465A4D5882692EC08038BDF280D33E"
 auth_opts=("bc-${version}.tar.xz.sig")
