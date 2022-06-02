f24aa0d574c9130f1cfc08d21a97ad65daf9a88b
EWouters <6179932+EWouters@users.noreply.github.com>
2022-06-02 22:20:04 +0200

    Ports/dialog: Update dialog to version 1.3-20220526

--- Ports/dialog/package.sh
+++ Ports/dialog/package.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=dialog
-version=1.3-20220117
-depends=("ncurses")
+port='dialog'
+version='1.3-20220526'
 files="https://invisible-mirror.net/archives/dialog/dialog-${version}.tgz dialog-${version}.tgz
 https://invisible-mirror.net/archives/dialog/dialog-${version}.tgz.asc dialog-${version}.tgz.asc"
 auth_type="sig"
 auth_import_key="C52048C0C0748FEE227D47A2702353E0F7E48EDB"
 auth_opts=("dialog-${version}.tgz.asc" "dialog-${version}.tgz")
-useconfigure=true
-use_fresh_config_sub=true
+useconfigure='true'
+use_fresh_config_sub='true'
 configopts=("--prefix=/usr/local" "--with-ncurses" "--with-curses-dir=${SERENITY_INSTALL_ROOT}/usr/local/include/ncurses")
+depends=("ncurses")
