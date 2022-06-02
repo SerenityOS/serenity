ae2b6c1ba3bd17a425a526cfda43b50c00460628
SeekingBlues <seekingblues@gmail.com>
2022-06-02 11:46:30 -0500

    du: Use a global variable for options
    Since the options are the same in every recursion, there is no reason
    to pass them as parameters.

--- Userland/Utilities/du.cpp
+++ Userland/Utilities/du.cpp
@@ -30,33 +30,32 @@ struct DuOption {
     bool all = false;
     bool apparent_size = false;
     int threshold = 0;
+    unsigned max_depth = UINT_MAX;
     TimeType time_type = TimeType::NotUsed;
     Vector<String> excluded_patterns;
+    Vector<StringView> files;
 };
 
-static ErrorOr<void> parse_args(Main::Arguments arguments, Vector<String>& files, DuOption& du_option, int& max_depth);
-static ErrorOr<off_t> print_space_usage(String const& path, DuOption const& du_option, int max_depth, bool inside_dir = false);
+static DuOption s_option;
+
+static ErrorOr<void> parse_args(Main::Arguments const& arguments);
+static ErrorOr<off_t> print_space_usage(String const& path, unsigned max_depth, bool inside_dir = false);
 
 ErrorOr<int> serenity_main(Main::Arguments arguments)
 {
-    Vector<String> files;
-    DuOption du_option;
-    int max_depth = INT_MAX;
+    TRY(parse_args(arguments));
 
-    TRY(parse_args(arguments, files, du_option, max_depth));
-
-    for (auto const& file : files)
-        TRY(print_space_usage(file, du_option, max_depth));
+    for (auto const& file : s_option.files)
+        TRY(print_space_usage(file, s_option.max_depth));
 
     return 0;
 }
 
-ErrorOr<void> parse_args(Main::Arguments arguments, Vector<String>& files, DuOption& du_option, int& max_depth)
+ErrorOr<void> parse_args(Main::Arguments const& arguments)
 {
     bool summarize = false;
-    char const* pattern = nullptr;
+    char const* exclude_pattern = nullptr;
     char const* exclude_from = nullptr;
-    Vector<StringView> files_to_process;
 
     Core::ArgsParser::Option time_option {
         true,
@@ -65,13 +64,13 @@ ErrorOr<void> parse_args(Main::Arguments arguments, Vector<String>& files, DuOpt
         "time",
         0,
         "time-type",
-        [&du_option](StringView s) {
+        [](StringView s) {
             if (s == "mtime"sv || s == "modification"sv)
-                du_option.time_type = DuOption::TimeType::Modification;
+                s_option.time_type = DuOption::TimeType::Modification;
             else if (s == "ctime"sv || s == "status"sv || s == "use"sv)
-                du_option.time_type = DuOption::TimeType::Status;
+                s_option.time_type = DuOption::TimeType::Status;
             else if (s == "atime"sv || s == "access"sv)
-                du_option.time_type = DuOption::TimeType::Access;
+                s_option.time_type = DuOption::TimeType::Access;
             else
                 return false;
 
@@ -81,44 +80,37 @@ ErrorOr<void> parse_args(Main::Arguments arguments, Vector<String>& files, DuOpt
 
     Core::ArgsParser args_parser;
     args_parser.set_general_help("Display actual or apparent disk usage of files or directories.");
-    args_parser.add_option(du_option.all, "Write counts for all files, not just directories", "all", 'a');
-    args_parser.add_option(du_option.apparent_size, "Print apparent sizes, rather than disk usage", "apparent-size", 0);
-    args_parser.add_option(du_option.human_readable, "Print human-readable sizes", "human-readable", 'h');
-    args_parser.add_option(max_depth, "Print the total for a directory or file only if it is N or fewer levels below the command line argument", "max-depth", 'd', "N");
+    args_parser.add_option(s_option.all, "Write counts for all files, not just directories", "all", 'a');
+    args_parser.add_option(s_option.apparent_size, "Print apparent sizes, rather than disk usage", "apparent-size", 0);
+    args_parser.add_option(s_option.human_readable, "Print human-readable sizes", "human-readable", 'h');
+    args_parser.add_option(s_option.max_depth, "Print the total for a directory or file only if it is N or fewer levels below the command line argument", "max-depth", 'd', "N");
     args_parser.add_option(summarize, "Display only a total for each argument", "summarize", 's');
-    args_parser.add_option(du_option.threshold, "Exclude entries smaller than size if positive, or entries greater than size if negative", "threshold", 't', "size");
+    args_parser.add_option(s_option.threshold, "Exclude entries smaller than size if positive, or entries greater than size if negative", "threshold", 't', "size");
     args_parser.add_option(move(time_option));
-    args_parser.add_option(pattern, "Exclude files that match pattern", "exclude", 0, "pattern");
+    args_parser.add_option(exclude_pattern, "Exclude files that match pattern", "exclude", 0, "pattern");
     args_parser.add_option(exclude_from, "Exclude files that match any pattern in file", "exclude_from", 'X', "file");
-    args_parser.add_positional_argument(files_to_process, "File to process", "file", Core::ArgsParser::Required::No);
+    args_parser.add_positional_argument(s_option.files, "File to process", "file", Core::ArgsParser::Required::No);
     args_parser.parse(arguments);
 
     if (summarize)
-        max_depth = 0;
-
-    if (pattern)
-        du_option.excluded_patterns.append(pattern);
+        s_option.max_depth = 0;
+    if (exclude_pattern)
+        s_option.excluded_patterns.append(exclude_pattern);
     if (exclude_from) {
         auto file = TRY(Core::File::open(exclude_from, Core::OpenMode::ReadOnly));
-        auto const buff = file->read_all();
+        auto buff = file->read_all();
         if (!buff.is_empty()) {
-            String patterns = String::copy(buff, Chomp);
-            du_option.excluded_patterns.extend(patterns.split('\n'));
+            auto patterns = String::copy(buff, Chomp);
+            s_option.excluded_patterns.extend(patterns.split('\n'));
         }
     }
-
-    for (auto const& file : files_to_process) {
-        files.append(file);
-    }
-
-    if (files.is_empty()) {
-        files.append(".");
-    }
+    if (s_option.files.is_empty())
+        s_option.files.append(".");
 
     return {};
 }
 
-ErrorOr<off_t> print_space_usage(String const& path, DuOption const& du_option, int max_depth, bool inside_dir)
+ErrorOr<off_t> print_space_usage(String const& path, unsigned max_depth, bool inside_dir)
 {
     struct stat path_stat = TRY(Core::System::lstat(path.characters()));
     off_t directory_size = 0;
@@ -132,32 +124,32 @@ ErrorOr<off_t> print_space_usage(String const& path, DuOption const& du_option, 
 
         while (di.has_next()) {
             auto const child_path = di.next_full_path();
-            directory_size += TRY(print_space_usage(child_path, du_option, max_depth, true));
+            directory_size += TRY(print_space_usage(child_path, max_depth, true));
         }
     }
 
     auto const basename = LexicalPath::basename(path);
-    for (auto const& pattern : du_option.excluded_patterns) {
+    for (auto const& pattern : s_option.excluded_patterns) {
         if (basename.matches(pattern, CaseSensitivity::CaseSensitive))
             return { 0 };
     }
 
     off_t size = path_stat.st_size;
-    if (du_option.apparent_size) {
+    if (s_option.apparent_size) {
         constexpr auto block_size = 512;
         size = path_stat.st_blocks * block_size;
     }
 
-    if (inside_dir && !du_option.all && !is_directory)
+    if (inside_dir && !s_option.all && !is_directory)
         return size;
 
     if (is_directory)
         size = directory_size;
 
-    if ((du_option.threshold > 0 && size < du_option.threshold) || (du_option.threshold < 0 && size > -du_option.threshold))
+    if ((s_option.threshold > 0 && size < s_option.threshold) || (s_option.threshold < 0 && size > -s_option.threshold))
         return { 0 };
 
-    if (du_option.human_readable) {
+    if (s_option.human_readable) {
         out("{}", human_readable_size(size));
     } else {
         constexpr long long block_size = 1024;
@@ -165,11 +157,11 @@ ErrorOr<off_t> print_space_usage(String const& path, DuOption const& du_option, 
         out("{}", size);
     }
 
-    if (du_option.time_type == DuOption::TimeType::NotUsed) {
+    if (s_option.time_type == DuOption::TimeType::NotUsed) {
         outln("\t{}", path);
     } else {
         auto time = path_stat.st_mtime;
-        switch (du_option.time_type) {
+        switch (s_option.time_type) {
         case DuOption::TimeType::Access:
             time = path_stat.st_atime;
             break;
