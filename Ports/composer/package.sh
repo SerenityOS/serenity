2a9a2f17a1fd0a8da63714c5b4a1f30dcb676d71
EWouters <6179932+EWouters@users.noreply.github.com>
2022-06-02 22:05:04 +0200

    Ports/composer: Update composer to version 2.3.5

--- Ports/composer/package.sh
+++ Ports/composer/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=composer
-version="2.3.4"
-files="https://getcomposer.org/download/${version}/composer.phar composer.phar 1fc8fc5b43f081fe76fa85eb5a213412e55f54a60bae4880bc96521ae482d6c3"
-auth_type=sha256
+port='composer'
+version='2.3.5'
+files="https://getcomposer.org/download/${version}/composer.phar composer.phar 3b3b5a899c06a46aec280727bdf50aad14334f6bc40436ea76b07b650870d8f4"
+auth_type='sha256'
 depends=("php")
 
 build() {
@@ -10,7 +10,7 @@ build() {
 }
 
 install() {
-    local target_path=${SERENITY_INSTALL_ROOT}/usr/local/bin/composer
-    run_nocd cp composer.phar ${target_path}
-    run_nocd chmod +x ${target_path}
+    local target_path="${SERENITY_INSTALL_ROOT}/usr/local/bin/composer"
+    run_nocd cp composer.phar "${target_path}"
+    run_nocd chmod +x "${target_path}"
 }
