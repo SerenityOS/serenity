cd589b12c95bd1d72bd3d056f23baf31efcc3389
EWouters <6179932+EWouters@users.noreply.github.com>
2022-06-02 22:22:25 +0200

    Ports/dialog: Use sha256 to check the downloaded file

--- Ports/dialog/package.sh
+++ Ports/dialog/package.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='dialog'
 version='1.3-20220526'
-files="https://invisible-mirror.net/archives/dialog/dialog-${version}.tgz dialog-${version}.tgz
-https://invisible-mirror.net/archives/dialog/dialog-${version}.tgz.asc dialog-${version}.tgz.asc"
-auth_type="sig"
-auth_import_key="C52048C0C0748FEE227D47A2702353E0F7E48EDB"
-auth_opts=("dialog-${version}.tgz.asc" "dialog-${version}.tgz")
+files="https://invisible-mirror.net/archives/dialog/dialog-${version}.tgz dialog-${version}.tgz 858c9a625b20fde19fb7b19949ee9e9efcade23c56d917b1adb30e98ff6d6b33"
+auth_type='sha256'
 useconfigure='true'
 use_fresh_config_sub='true'
 configopts=("--prefix=/usr/local" "--with-ncurses" "--with-curses-dir=${SERENITY_INSTALL_ROOT}/usr/local/include/ncurses")
