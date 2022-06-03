866e654aefbd4e862b4a4ef4377ab577cd3031c3
Tim Schumacher <timschumi@gmx.de>
2022-06-03 16:46:28 +0200

    Ports: Replace manually linking `libksba` with a libtool patch

--- Ports/libksba/package.sh
+++ Ports/libksba/package.sh
@@ -15,9 +15,3 @@ pre_configure() {
 configure() {
     run ./configure --host="${SERENITY_ARCH}-pc-serenity" --build="$($workdir/build-aux/config.guess)" "${configopts[@]}"
 }
-
-install() {
-    run make DESTDIR=${SERENITY_INSTALL_ROOT} "${installopts[@]}" install
-    ${CC} -shared -o ${SERENITY_INSTALL_ROOT}/usr/local/lib/libksba.so -Wl,-soname,libksba.so -Wl,--whole-archive ${SERENITY_INSTALL_ROOT}/usr/local/lib/libksba.a -Wl,--no-whole-archive -lgpg-error
-    rm -f ${SERENITY_INSTALL_ROOT}/usr/local/lib/libksba.la
-}
