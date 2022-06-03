7e3a952158663e0e929889b6960c84f2e8908448
EWouters <6179932+EWouters@users.noreply.github.com>
2022-06-03 20:45:29 +0200

    Ports/radare2: Support `x86_64`
    This update was merged with a33b9a8, which has the `--disable-threads`
    flag. This update brings support for `x86_64` and uses a more up to date
    Github url. It also removes the `workdir` as it was set to the default.

--- Ports/radare2/package.sh
+++ Ports/radare2/package.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=radare2
-version=5.7.0
-useconfigure=true
-workdir=$port-$version
-files="https://codeload.github.com/radareorg/radare2/tar.gz/refs/tags/$version radare2-$version.tar.gz fe7ca861bf71dd3c4766a57f73fd97b216bcfde161720f949c05875df212976b"
-auth_type=sha256
-configopts=("--disable-debugger" "--disable-threads" "--with-ostype=serenityos" "--host=i686-serenityos")
+port='radare2'
+version='5.7.0'
+files="https://github.com/radareorg/radare2/archive/refs/tags/${version}.tar.gz radare2-${version}.tar.gz fe7ca861bf71dd3c4766a57f73fd97b216bcfde161720f949c05875df212976b"
+auth_type='sha256'
+useconfigure='true'
+configopts=("--disable-debugger" "--disable-threads" "--with-ostype=serenityos" "--host=${SERENITY_ARCH}-serenityos")
 
 pre_configure() {
-	cp -f "$workdir/dist/plugins-cfg/plugins.tiny.cfg" "$workdir/plugins.cfg"
+	cp -f "${workdir}/dist/plugins-cfg/plugins.tiny.cfg" "${workdir}/plugins.cfg"
 }
