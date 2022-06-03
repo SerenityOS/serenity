9d2d6d4fa6f74ef5ac2a0f124617f204b39c4a3f
Tim Schumacher <timschumi@gmx.de>
2022-06-03 17:12:43 +0200

    Ports: Replace manually linking `libxml2` with a libtool patch

--- Ports/libxml2/package.sh
+++ Ports/libxml2/package.sh
@@ -6,12 +6,4 @@ use_fresh_config_sub=true
 files="https://download.gnome.org/sources/libxml2/2.9/libxml2-${version}.tar.xz libxml2-${version}.tar.xz 276130602d12fe484ecc03447ee5e759d0465558fbc9d6bd144e3745306ebf0e"
 auth_type=sha256
 depends=("libiconv" "xz")
-configopts=("--with-sysroot=${SERENITY_INSTALL_ROOT}" "--prefix=/usr/local" "--without-python")
-
-install() {
-    run make DESTDIR="${SERENITY_INSTALL_ROOT}" install
-
-    # Link shared library
-    run ${SERENITY_ARCH}-pc-serenity-gcc -shared -o ${SERENITY_INSTALL_ROOT}/usr/local/lib/libxml2.so -Wl,-soname,libxml2.so -Wl,--whole-archive ${SERENITY_INSTALL_ROOT}/usr/local/lib/libxml2.a -Wl,--no-whole-archive -llzma
-    rm -f ${SERENITY_INSTALL_ROOT}/usr/local/lib/libxml2.la
-}
+configopts=("--with-sysroot=${SERENITY_INSTALL_ROOT}" "--prefix=/usr/local" "--without-python" "--disable-static" "--enable-shared")
