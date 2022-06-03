9846533ce4ca8d60562ec2e13ecce18f460f0914
EWouters <6179932+EWouters@users.noreply.github.com>
2022-06-03 17:59:09 +0200

    Ports/m4: Update m4 to version 1.4.19

--- Ports/m4/package.sh
+++ Ports/m4/package.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=m4
-version=1.4.9
-useconfigure=true
-use_fresh_config_sub=true
+port='m4'
+version='1.4.19'
+useconfigure='true'
+use_fresh_config_sub='true'
+config_sub_paths=("build-aux/config.sub")
 files="https://ftpmirror.gnu.org/gnu/m4/m4-${version}.tar.gz m4-${version}.tar.gz
 https://ftpmirror.gnu.org/gnu/m4/m4-${version}.tar.gz.sig m4-${version}.tar.gz.sig
 https://ftpmirror.gnu.org/gnu/gnu-keyring.gpg gnu-keyring.gpg"
