582192ce9134a74cda1adab9ead1336a3e562d5f
EWouters <6179932+EWouters@users.noreply.github.com>
2022-06-03 17:27:16 +0200

    Ports/libxml2: Update libxml2 to version 2.9.14

--- Ports/libxml2/package.sh
+++ Ports/libxml2/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=libxml2
-version=2.9.13
-useconfigure=true
-use_fresh_config_sub=true
-files="https://download.gnome.org/sources/libxml2/2.9/libxml2-${version}.tar.xz libxml2-${version}.tar.xz 276130602d12fe484ecc03447ee5e759d0465558fbc9d6bd144e3745306ebf0e"
-auth_type=sha256
-depends=("libiconv" "xz")
+port='libxml2'
+version='2.9.14'
+files="https://download.gnome.org/sources/libxml2/2.9/libxml2-${version}.tar.xz libxml2-${version}.tar.xz 60d74a257d1ccec0475e749cba2f21559e48139efba6ff28224357c7c798dfee"
+auth_type='sha256'
+useconfigure='true'
+use_fresh_config_sub='true'
 configopts=("--with-sysroot=${SERENITY_INSTALL_ROOT}" "--prefix=/usr/local" "--without-python" "--disable-static" "--enable-shared")
+depends=("libiconv" "xz")
