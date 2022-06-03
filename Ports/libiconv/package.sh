a97899c0883b3a0aa2cc2b1fc5645dc9e2e44c57
Tim Schumacher <timschumi@gmx.de>
2022-06-03 16:41:48 +0200

    Ports: Replace manually linking `libiconv` with a libtool patch

--- Ports/libiconv/package.sh
+++ Ports/libiconv/package.sh
@@ -7,9 +7,3 @@ files="https://ftpmirror.gnu.org/gnu/libiconv/libiconv-${version}.tar.gz libicon
 auth_type="sha256"
 use_fresh_config_sub=true
 config_sub_paths=("build-aux/config.sub" "libcharset/build-aux/config.sub")
-
-install() {
-    run make DESTDIR=${SERENITY_INSTALL_ROOT} "${installopts[@]}" install
-    run ${CC} -shared -o ${SERENITY_INSTALL_ROOT}/usr/local/lib/libiconv.so -Wl,-soname,libiconv.so -Wl,--whole-archive ${SERENITY_INSTALL_ROOT}/usr/local/lib/libiconv.a -Wl,--no-whole-archive
-    rm -f ${SERENITY_INSTALL_ROOT}/usr/local/lib/libiconv.la
-}
