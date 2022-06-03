164028268b9c11e76af0e7a3da1a06a8044dd504
Tim Schumacher <timschumi@gmx.de>
2022-06-03 16:50:47 +0200

    Ports: Replace manually linking `libmodplug` with a libtool patch

--- Ports/libmodplug/package.sh
+++ Ports/libmodplug/package.sh
@@ -7,11 +7,3 @@ configopts=("ac_cv_c_bigendian=no")
 files="https://download.sourceforge.net/modplug-xmms/libmodplug-${version}.tar.gz libmodplug-${version}.tar.gz 457ca5a6c179656d66c01505c0d95fafaead4329b9dbaa0f997d00a3508ad9de"
 auth_type=sha256
 workdir="libmodplug-$version"
-
-install() {
-    run make DESTDIR=${SERENITY_INSTALL_ROOT} "${installopts[@]}" install
-    MODPLUG_LIBDIR="${SERENITY_INSTALL_ROOT}/usr/local/lib"
-    ${CC} -shared -o ${MODPLUG_LIBDIR}/libmodplug.so.1 -Wl,-soname,libmodplug.so -Wl,--whole-archive ${MODPLUG_LIBDIR}/libmodplug.a -Wl,--no-whole-archive
-    ln -rsf ${MODPLUG_LIBDIR}/libmodplug.so.1 ${MODPLUG_LIBDIR}/libmodplug.so
-    rm -f ${MODPLUG_LIBDIR}/libmodplug.la
-}
