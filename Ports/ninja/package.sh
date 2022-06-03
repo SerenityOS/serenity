efa9b7d9a1bd19bc82f9f81c0bb0bbf1199882ac
EWouters <6179932+EWouters@users.noreply.github.com>
2022-06-03 18:44:01 +0200

    Ports/ninja: Update ninja to version 1.11.0

--- Ports/ninja/package.sh
+++ Ports/ninja/package.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=ninja
-version=1.10.2
-files="https://github.com/ninja-build/ninja/archive/v${version}.tar.gz ninja-v${version}.tar.gz ce35865411f0490368a8fc383f29071de6690cbadc27704734978221f25e2bed"
-auth_type=sha256
+port='ninja'
+version='1.11.0'
+files="https://github.com/ninja-build/ninja/archive/v${version}.tar.gz ninja-v${version}.tar.gz 3c6ba2e66400fe3f1ae83deb4b235faf3137ec20bd5b08c29bfc368db143e4c6"
+auth_type='sha256'
 
 build() {
     CXXFLAGS="--sysroot=${SERENITY_INSTALL_ROOT}" \
     LDFLAGS="--sysroot=${SERENITY_INSTALL_ROOT}" \
     # platform=linux is close enough.
-    run ./configure.py --bootstrap --platform=linux
+    run ./configure.py --bootstrap --platform='linux'
     run strip ninja
 }
 
 install() {
     run mkdir -p "${SERENITY_INSTALL_ROOT}/usr/local/bin"
-    run cp ninja "${SERENITY_INSTALL_ROOT}/usr/local/bin/ninja"
+    run cp ninja "${SERENITY_INSTALL_ROOT}/usr/local/bin"
 }
