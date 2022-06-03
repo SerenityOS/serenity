41bb21985ea1f1f79ff49f92faa1723a5b0544af
EWouters <6179932+EWouters@users.noreply.github.com>
2022-06-03 16:44:21 +0200

    Ports/libgpg-error: Update libgpg-error to version 1.45

--- Ports/libgpg-error/package.sh
+++ Ports/libgpg-error/package.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=libgpg-error
-version=1.44
-useconfigure=true
-use_fresh_config_sub=true
+port='libgpg-error'
+version='1.45'
+files="https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-${version}.tar.bz2 libgpg-error-${version}.tar.bz2 570f8ee4fb4bff7b7495cff920c275002aea2147e9a1d220c068213267f80a26"
+auth_type='sha256'
+useconfigure='true'
+use_fresh_config_sub='true'
 config_sub_paths=("build-aux/config.sub")
 depends=("gettext")
 configopts=("--disable-tests" "--disable-threads")
-files="https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-${version}.tar.bz2 libgpg-error-${version}.tar.bz2 8e3d2da7a8b9a104dd8e9212ebe8e0daf86aa838cc1314ba6bc4de8f2d8a1ff9"
-auth_type=sha256
 
 configure() {
-    run ./configure --host="${SERENITY_ARCH}-pc-serenity" --build="$($workdir/build-aux/config.guess)" "${configopts[@]}"
+    run ./configure --host="${SERENITY_ARCH}-pc-serenity" --build="$(${workdir}/build-aux/config.guess)" "${configopts[@]}"
 }
