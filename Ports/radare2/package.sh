9f393e32e45c2186abe1f6e842adb404a115efec
pancake <pancake@nopcode.org>
2022-06-05 00:20:43 +0200

    Ports: Update radare2 from 5.4.0 to the latest 5.7.0

--- Ports/radare2/package.sh
+++ Ports/radare2/package.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=radare2
-version=5.4.0-git
+version=5.7.0
 useconfigure=true
 workdir=$port-$version
-files="https://codeload.github.com/radareorg/radare2/tar.gz/refs/tags/$version radare2-$version.tar.gz c5e98cd4ea011dde2f08e68144e98e85e82fe45eef92a17dedc06e9404da117e"
+files="https://codeload.github.com/radareorg/radare2/tar.gz/refs/tags/$version radare2-$version.tar.gz fe7ca861bf71dd3c4766a57f73fd97b216bcfde161720f949c05875df212976b"
 auth_type=sha256
-configopts=("--disable-debugger" "--with-ostype=serenityos" "--host=i686-serenityos")
+configopts=("--disable-debugger" "--disable-threads" "--with-ostype=serenityos" "--host=i686-serenityos")
 
 pre_configure() {
 	cp -f "$workdir/dist/plugins-cfg/plugins.tiny.cfg" "$workdir/plugins.cfg"
