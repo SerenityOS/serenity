f5cd81f67f481ee99b7c7c24233e1188eb778e1e
EWouters <6179932+EWouters@users.noreply.github.com>
2022-06-07 14:04:08 +0200

    Ports/nano: Use sha256 to check the downloaded file

--- Ports/nano/package.sh
+++ Ports/nano/package.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='nano'
 version='6.3'
-useconfigure="true"
-use_fresh_config_sub=true
-files="https://www.nano-editor.org/dist/v6/nano-${version}.tar.xz nano-${version}.tar.xz
-https://www.nano-editor.org/dist/v6/nano-${version}.tar.xz.asc nano-${version}.tar.xz.asc"
+files="https://www.nano-editor.org/dist/v6/nano-${version}.tar.xz nano-${version}.tar.xz eb532da4985672730b500f685dbaab885a466d08fbbf7415832b95805e6f8687"
+auth_type='sha256'
+useconfigure='true'
+use_fresh_config_sub='true'
 configopts=("--target=${SERENITY_ARCH}-pc-serenity" "--disable-browser" "--disable-utf8")
 depends=("ncurses")
-auth_type="sig"
-auth_import_key="BFD009061E535052AD0DF2150D28D4D2A0ACE884"
-auth_opts=("nano-${version}.tar.xz.asc" "nano-${version}.tar.xz")
 
 export PKG_CONFIG_PATH="${SERENITY_INSTALL_ROOT}/usr/local/lib/pkgconfig"
