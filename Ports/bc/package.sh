19fb8deced8cfc56bb08498758405f55365e314e
EWouters <6179932+EWouters@users.noreply.github.com>
2022-06-07 14:09:58 +0200

    Ports/bc: Use sha256 to check the downloaded file

--- Ports/bc/package.sh
+++ Ports/bc/package.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='bc'
 version='5.2.5'
-files="https://github.com/gavinhoward/bc/releases/download/${version}/bc-${version}.tar.xz bc-${version}.tar.xz
-https://github.com/gavinhoward/bc/releases/download/${version}/bc-${version}.tar.xz.sig bc-${version}.tar.xz.sig"
+files="https://github.com/gavinhoward/bc/releases/download/${version}/bc-${version}.tar.xz bc-${version}.tar.xz 085d1f0d544f34c6e186a4b91e8978702eaa7645e39c630184efd49f17b3dbd5"
+auth_type='sha256'
 useconfigure='true'
 configscript='configure.sh'
-auth_type="sig"
-auth_import_key="E2A30324A4465A4D5882692EC08038BDF280D33E"
-auth_opts=("bc-${version}.tar.xz.sig")
 configopts=("--prefix=/usr/local" "--disable-nls")
 
-export CFLAGS="-O3 -flto"
+export CFLAGS='-O3 -flto'
 
 configure() {
     run env HOSTCC=gcc ./"$configscript" "${configopts[@]}"
