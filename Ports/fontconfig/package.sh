1cad2545569b313e93603e046dcb806636e8f63d
Tim Schumacher <timschumi@gmx.de>
2022-06-07 15:16:56 +0200

    Ports/fontconfig: Import the `libtool` patch
    This previously went unnoticed because `fontconfig` was never linked
    into a shared library manually.

--- Ports/fontconfig/package.sh
+++ Ports/fontconfig/package.sh
@@ -6,7 +6,14 @@ use_fresh_config_sub="true"
 depends=("libxml2" "freetype")
 files="https://www.freedesktop.org/software/fontconfig/release/fontconfig-${version}.tar.xz fontconfig-${version}.tar.xz dcbeb84c9c74bbfdb133d535fe1c7bedc9f2221a8daf3914b984c44c520e9bac"
 auth_type="sha256"
-configopts=("--prefix=/usr/local" "--enable-libxml2" "LDFLAGS=-ldl -lxml2")
+configopts=(
+    "--with-sysroot=${SERENITY_INSTALL_ROOT}"
+    "--prefix=/usr/local"
+    "--disable-static"
+    "--enable-shared"
+    "--enable-libxml2"
+    "LDFLAGS=-ldl -lxml2"
+)
 
 export CFLAGS="-I${SERENITY_INSTALL_ROOT}/usr/local/include/libxml2"
 export LIBXML2_CFLAGS="-I${SERENITY_INSTALL_ROOT}/usr/local/include/libxml2/"
