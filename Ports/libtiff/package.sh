6f06d27249797d69f9e094ff1a0fe5a85663b652
Tim Schumacher <timschumi@gmx.de>
2022-06-08 11:44:52 +0200

    Ports/libtiff: Set the correct sysroot

--- Ports/libtiff/package.sh
+++ Ports/libtiff/package.sh
@@ -4,6 +4,11 @@ version='4.4.0'
 files="http://download.osgeo.org/libtiff/tiff-${version}.tar.xz tiff-${version}.tar.xz 49307b510048ccc7bc40f2cba6e8439182fe6e654057c1a1683139bf2ecb1dc1"
 auth_type='sha256'
 useconfigure='true'
-configopts=("--disable-static" "--enable-shared")
+configopts=(
+    "--with-sysroot=${SERENITY_INSTALL_ROOT}"
+    "--prefix=/usr/local"
+    "--disable-static"
+    "--enable-shared"
+)
 workdir="tiff-${version}"
 depends=("libjpeg" "zstd" "xz")
