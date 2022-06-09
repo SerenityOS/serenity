5dfa193e5a05d5f3b79e9ceb971232cc324c99de
EWouters <6179932+EWouters@users.noreply.github.com>
2022-06-09 21:32:32 +0200

    Ports/stpuzzles: Return stpuzzles to upstream
    Stpuzzles was forked and maintained at https://github.com/SerenityPorts/stpuzzles.
    This commit returns stpuzzles to it's upstream repository and imports
    the downstream commits as patches. Many commits have been squashed into
    the first patch.
    
    The second patch adds binary files. Support for this was added in the
    previous commit.

--- Ports/stpuzzles/package.sh
+++ Ports/stpuzzles/package.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=stpuzzles
-useconfigure=true
-version=git
-workdir="${port}-main"
-configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt")
-files="https://github.com/SerenityPorts/stpuzzles/archive/refs/heads/main.zip ${port}.zip"
+port='stpuzzles'
+version='8f3413c31ffd43c4ebde40894ac1b2f7cdf222c3'
+files="https://git.tartarus.org/?p=simon/puzzles.git;a=snapshot;h=${version};sf=tgz puzzles-${version::7}.tar.gz 6af9bd93759d792e539131639dfae165398047262723827940da7209fc8260a7"
+auth_type='sha256'
+workdir="puzzles-${version::7}"
+useconfigure='true'
+configopts=(
+    "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
+    "-DCMAKE_CXX_FLAGS=-std=c++2a -O2"
+)
 
 configure() {
-    run cmake "${configopts[@]}" -DCMAKE_CXX_FLAGS="-std=c++2a -O2"
+    run cmake "${configopts[@]}"
 }
 
 install() {
     run make install
 
     for puzzle in bridges cube dominosa fifteen filling flip flood galaxies guess inertia keen lightup loopy magnets map mines mosaic net netslide palisade pattern pearl pegs range rect samegame signpost singles sixteen slant solo tents towers tracks twiddle undead unequal unruly untangle; do
-        install_launcher "$puzzle" "Games/Puzzles" "/usr/local/bin/$puzzle"
-        install_icon "static-icons/${puzzle}.ico" "/usr/local/bin/$puzzle"
+        install_launcher "${puzzle}" "Games/Puzzles" "/usr/local/bin/${puzzle}"
+        install_icon "static-icons/${puzzle}.ico" "/usr/local/bin/${puzzle}"
     done
 }
---
+++ Ports/stpuzzles/patches/ReadMe.md
@@ -0,0 +1,14 @@
+# Patches for stpuzzles on SerenityOS
+
+## `0001-Add-SerenityOS-platform-support.patch`
+
+Add SerenityOS platform support
+
+
+## `0002-Add-icon-files.patch`
+
+Add icon files
+
+These are statically generated because generating them during the build
+would require building the games for the host system.
+
