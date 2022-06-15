89cf63370205b8001fdeeab9726e2f0b96e20014
MacDue <macdue@dueutil.tech>
2022-06-15 21:29:55 +0100

    LibWeb: Support using border-radius on <img> tags

--- Userland/Libraries/LibWeb/Painting/ImagePaintable.cpp
+++ Userland/Libraries/LibWeb/Painting/ImagePaintable.cpp
@@ -7,6 +7,7 @@
 #include <LibGfx/StylePainter.h>
 #include <LibWeb/HTML/HTMLImageElement.h>
 #include <LibWeb/Layout/ImageBox.h>
+#include <LibWeb/Painting/BorderRadiusCornerClipper.h>
 #include <LibWeb/Painting/ImagePaintable.h>
 
 namespace Web::Painting {
@@ -47,7 +48,23 @@ void ImagePaintable::paint(PaintContext& context, PaintPhase phase) const
                 alt = image_element.src();
             context.painter().draw_text(enclosing_int_rect(absolute_rect()), alt, Gfx::TextAlignment::Center, computed_values().color(), Gfx::TextElision::Right);
         } else if (auto bitmap = layout_box().image_loader().bitmap(layout_box().image_loader().current_frame_index())) {
-            context.painter().draw_scaled_bitmap(absolute_rect().to_rounded<int>(), *bitmap, bitmap->rect(), 1.0f, to_gfx_scaling_mode(computed_values().image_rendering()));
+            auto image_rect = absolute_rect().to_rounded<int>();
+            auto border_radii_data = normalized_border_radii_data();
+
+            Optional<BorderRadiusCornerClipper> corner_clipper;
+            if (border_radii_data.has_any_radius()) {
+                auto clipper = BorderRadiusCornerClipper::create(image_rect, border_radii_data);
+                if (!clipper.is_error())
+                    corner_clipper = clipper.release_value();
+            }
+
+            if (corner_clipper.has_value())
+                corner_clipper->sample_under_corners(context.painter());
+
+            context.painter().draw_scaled_bitmap(image_rect, *bitmap, bitmap->rect(), 1.0f, to_gfx_scaling_mode(computed_values().image_rendering()));
+
+            if (corner_clipper.has_value())
+                corner_clipper->blit_corner_clipping(context.painter());
         }
     }
 }
