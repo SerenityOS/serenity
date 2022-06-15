455038d6fc809039b9a1a3606f84ed762b08bb8c
Andrew Kaster <akaster@serenityos.org>
2022-06-14 18:20:42 -0600

    Kernel: Add sysconf for IOV_MAX

--- Kernel/API/POSIX/unistd.h
+++ Kernel/API/POSIX/unistd.h
@@ -42,6 +42,7 @@ enum {
     _SC_SYMLOOP_MAX,
     _SC_MAPPED_FILES,
     _SC_ARG_MAX,
+    _SC_IOV_MAX,
 };
 
 #define _SC_MONOTONIC_CLOCK _SC_MONOTONIC_CLOCK
@@ -56,6 +57,7 @@ enum {
 #define _SC_SYMLOOP_MAX _SC_SYMLOOP_MAX
 #define _SC_MAPPED_FILES _SC_MAPPED_FILES
 #define _SC_ARG_MAX _SC_ARG_MAX
+#define _SC_IOV_MAX _SC_IOV_MAX
 
 #ifdef __cplusplus
 }
