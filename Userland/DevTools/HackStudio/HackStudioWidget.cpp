d17e952cbf17565b64479129141b9055ab18dc68
ferhatgec <ferhat2gecdogan_fix_me5@protonmail.com>
2022-06-20 19:14:00 +0300

    DevTools/HackStudio: Fix 'enabled' property of 'build' and 'run' buttons

--- Userland/DevTools/HackStudio/HackStudioWidget.cpp
+++ Userland/DevTools/HackStudio/HackStudioWidget.cpp
@@ -1078,6 +1078,10 @@ void HackStudioWidget::build()
     auto result = m_project_builder->build(active_file());
     if (result.is_error()) {
         GUI::MessageBox::show(window(), String::formatted("{}", result.error()), "Build failed", GUI::MessageBox::Type::Error);
+        m_build_action->set_enabled(true);
+        m_stop_action->set_enabled(false);
+    } else {
+        m_stop_action->set_enabled(true);
     }
 }
 
@@ -1086,6 +1090,10 @@ void HackStudioWidget::run()
     auto result = m_project_builder->run(active_file());
     if (result.is_error()) {
         GUI::MessageBox::show(window(), String::formatted("{}", result.error()), "Run failed", GUI::MessageBox::Type::Error);
+        m_run_action->set_enabled(true);
+        m_stop_action->set_enabled(false);
+    } else {
+        m_stop_action->set_enabled(true);
     }
 }
 
@@ -1227,7 +1235,6 @@ NonnullRefPtr<GUI::Action> HackStudioWidget::create_build_action()
 
         reveal_action_tab(*m_terminal_wrapper);
         build();
-        m_stop_action->set_enabled(true);
     });
 }
 
@@ -1236,7 +1243,6 @@ NonnullRefPtr<GUI::Action> HackStudioWidget::create_run_action()
     return GUI::Action::create("&Run", { Mod_Ctrl, Key_R }, Gfx::Bitmap::try_load_from_file("/res/icons/16x16/program-run.png").release_value_but_fixme_should_propagate_errors(), [this](auto&) {
         reveal_action_tab(*m_terminal_wrapper);
         run();
-        m_stop_action->set_enabled(true);
     });
 }
 
