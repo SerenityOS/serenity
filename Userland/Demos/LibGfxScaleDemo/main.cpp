6fc8ebf1008f58295126a8557de5fb22042d030f
FrHun <28605587+frhun@users.noreply.github.com>
2022-06-21 02:42:26 +0200

    Debug -- Change LibGfxScaleDemo

--- Userland/Demos/LibGfxScaleDemo/main.cpp
+++ Userland/Demos/LibGfxScaleDemo/main.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <LibCore/System.h>
+#include <LibCore/Timer.h>
 #include <LibGUI/Action.h>
 #include <LibGUI/Application.h>
 #include <LibGUI/Icon.h>
@@ -15,6 +16,7 @@
 #include <LibGUI/Widget.h>
 #include <LibGUI/Window.h>
 #include <LibGfx/Bitmap.h>
+#include <LibGfx/CharacterBitmap.h>
 #include <LibGfx/Font/Font.h>
 #include <LibGfx/Painter.h>
 #include <LibGfx/Palette.h>
@@ -31,6 +33,7 @@ class Canvas final : public GUI::Widget {
     C_OBJECT(Canvas)
 public:
     virtual ~Canvas() override = default;
+    void redo(Gfx::IntPoint offset);
 
 private:
     Canvas();
@@ -38,12 +41,17 @@ private:
     RefPtr<Gfx::Bitmap> m_bitmap_2x;
     RefPtr<Gfx::Bitmap> m_bitmap_2x_as_1x;
 
+    RefPtr<Gfx::Bitmap> buggie {};
+    RefPtr<Gfx::Bitmap> grid {};
+
     void draw(Gfx::Painter& painter);
     virtual void paint_event(GUI::PaintEvent&) override;
 };
 
 Canvas::Canvas()
 {
+    buggie = Gfx::Bitmap::try_load_from_file("/res/graphics/buggie.png").release_value_but_fixme_should_propagate_errors();
+    grid = Gfx::Bitmap::try_load_from_file("/res/wallpapers/grid.png").release_value_but_fixme_should_propagate_errors();
     m_bitmap_1x = Gfx::Bitmap::try_create(Gfx::BitmapFormat::BGRA8888, { WIDTH, HEIGHT }, 1).release_value_but_fixme_should_propagate_errors();
     m_bitmap_2x = Gfx::Bitmap::try_create(Gfx::BitmapFormat::BGRA8888, { WIDTH, HEIGHT }, 2).release_value_but_fixme_should_propagate_errors();
 
@@ -54,10 +62,17 @@ Canvas::Canvas()
     // so make a 1x alias of the 2x bitmap to make LibGfx paint it without any scaling at paint time, mapping once pixel to one pixel.
     m_bitmap_2x_as_1x = Gfx::Bitmap::try_create_wrapper(Gfx::BitmapFormat::BGRA8888, m_bitmap_2x->physical_size(), 1, m_bitmap_2x->pitch(), m_bitmap_2x->scanline(0)).release_value_but_fixme_should_propagate_errors();
 
+    redo({ 0, 0 });
+}
+
+void Canvas::redo(Gfx::IntPoint offset)
+{
     Gfx::Painter painter_1x(*m_bitmap_1x);
+    painter_1x.translate(offset);
     draw(painter_1x);
 
     Gfx::Painter painter_2x(*m_bitmap_2x);
+    painter_2x.translate(offset);
     draw(painter_2x);
 
     update();
@@ -83,7 +98,6 @@ void Canvas::draw(Gfx::Painter& painter)
     painter.draw_rect({ 24, 38, WIDTH - 48, HEIGHT - 53 }, palette().color(Gfx::ColorRole::Selection));
 
     // buggie.png has an alpha channel.
-    auto buggie = Gfx::Bitmap::try_load_from_file("/res/graphics/buggie.png").release_value_but_fixme_should_propagate_errors();
     painter.blit({ 25, 39 }, *buggie, { 2, 30, 62, 20 });
     painter.draw_scaled_bitmap({ 88, 39, 62 * 2, 20 * 2 }, *buggie, Gfx::IntRect { 2, 30, 62, 20 });
     painter.draw_scaled_bitmap({ 202, 39, 80, 40 }, *buggie, Gfx::IntRect { 2, 30, 62, 20 });
@@ -93,7 +107,6 @@ void Canvas::draw(Gfx::Painter& painter)
     painter.blit({ 25, 101 }, *buggie, { 2, 30, 3 * buggie->width(), 20 });
 
     // grid does not have an alpha channel.
-    auto grid = Gfx::Bitmap::try_load_from_file("/res/wallpapers/grid.png").release_value_but_fixme_should_propagate_errors();
     VERIFY(!grid->has_alpha_channel());
     painter.fill_rect({ 25, 122, 62, 20 }, Color::Green);
     painter.blit({ 25, 122 }, *grid, { (grid->width() - 62) / 2, (grid->height() - 20) / 2 + 40, 62, 20 }, 0.9);
@@ -101,6 +114,46 @@ void Canvas::draw(Gfx::Painter& painter)
     painter.blit_brightened({ 88, 122 }, *buggie, { 2, 30, 62, 20 });
     painter.blit_dimmed({ 140, 122 }, *buggie, { 2, 30, 62, 20 });
     painter.blit_disabled({ 192, 122 }, *buggie, { 2, 30, 62, 20 }, palette());
+
+    painter.translate(10, 10);
+
+    painter.draw_quadratic_bezier_curve({ 40, 140 }, { 80, 140 }, { 80, 160 }, Color::DarkBlue, 1, Gfx::Painter::LineStyle::Solid);
+
+    painter.draw_elliptical_arc({ 140, 80 }, { 180, 120 }, { 140, 120 }, { 40, 30 }, 0, 0.5, 0.3, Color::DarkCyan, 1, Gfx::Painter::LineStyle::Solid);
+
+    Gfx::IntRect rect_a { 20, 30, 50, 60 };
+
+    painter.fill_rect(rect_a, Color::Blue);
+
+    painter.fill_ellipse(rect_a, Color::MidGray);
+
+    painter.draw_focus_rect(rect_a.shrunken(20, 20), Color::White);
+
+    Gfx::IntRect rect_b { 80, 30, 50, 60 };
+
+    painter.fill_rect_with_checkerboard(rect_b, { 1, 2 }, Color::MidBlue, Color::Blue);
+
+    painter.draw_ellipse_intersecting(rect_b, Color::DarkRed, 2);
+
+    static constexpr Gfx::CharacterBitmap char_bitmap {
+        "           "
+        "     #     "
+        "    # #    "
+        "     #     "
+        "    # #    "
+        "   #   #   "
+        " ##     ## "
+        "           ",
+        11, 8
+    };
+
+    painter.draw_bitmap({ 105, 60 }, char_bitmap, Color::White);
+
+    painter.set_pixel_scaled({ 104, 59 }, Color::Green, false);
+
+    painter.draw_line({ 20, 30 }, { 70, 90 }, Color::DarkRed, 2);
+
+    painter.draw_text({ 140, 30, 70, 100 }, "Test und so \n ≥0 is a normal value.. or so; this is just some text, whatever", Gfx::TextAlignment::TopLeft, Color::DarkGreen, Gfx::TextElision::None, Gfx::TextWrapping::Wrap);
 }
 
 ErrorOr<int> serenity_main(Main::Arguments arguments)
@@ -121,7 +174,14 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
 
     auto app_icon = TRY(GUI::Icon::try_create_default_icon("app-libgfx-demo"));
     window->set_icon(app_icon.bitmap_for_size(16));
-    (void)TRY(window->try_set_main_widget<Canvas>());
+
+    Canvas& canvas = TRY(window->try_set_main_widget<Canvas>());
+    int current_offset = 0;
+    auto timer = Core::Timer::create_repeating(50, [&]() {
+        current_offset = (current_offset + 1) % 140;
+        canvas.redo({ (current_offset * 3) / 2, current_offset });
+    });
+    timer->start();
     window->show();
 
     return app->exec();
