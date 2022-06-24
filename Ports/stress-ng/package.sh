77a725b6f40e429983391c9b3ab5a650ef96424e
Brian Gianforcaro <bgianf@serenityos.org>
2022-06-24 01:51:22 -0700

    Ports: Update stress-ng to version 0.14.03
    The stress-ng project appears to have addressed all the issues
    we had been patching, and thus we no longer need almost all of
    these patches.

--- Ports/stress-ng/package.sh
+++ Ports/stress-ng/package.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=stress-ng
-version=0.13.12
-files="https://github.com/ColinIanKing/stress-ng/archive/V${version}.tar.gz stress-ng-${version}.tar.gz 16540d9cfa80d6a274fc0238d7251675ee38df6d5be805d14a67ce9efcb59ce9"
+version=0.14.03
+files="https://github.com/ColinIanKing/stress-ng/archive/V${version}.tar.gz stress-ng-${version}.tar.gz 95012c62883ab5826e6157557a075df98cce3cbce2a48bb40851bcc968a8441a"
 auth_type=sha256
 depends=("zlib")
 
 pre_configure() {
     export CFLAGS="-I${SERENITY_INSTALL_ROOT}/usr/local/include"
     export LDFLAGS="-L${SERENITY_INSTALL_ROOT}/usr/local/lib -lzlib"
-
 }
