67f352b82432365402deec68e6a9644effe355b2
Tim Schumacher <timschumi@gmx.de>
2022-06-29 20:20:13 +0200

    Kernel: Copy signal handlers when forking

--- Kernel/Process.cpp
+++ Kernel/Process.cpp
@@ -273,6 +273,12 @@ ErrorOr<void> Process::attach_resources(NonnullOwnPtr<Memory::AddressSpace>&& pr
 
     auto weak_ptr = TRY(this->try_make_weak_ptr());
     m_procfs_traits = TRY(ProcessProcFSTraits::try_create({}, move(weak_ptr)));
+
+    // This is not actually explicitly verified by any official documentation,
+    // but it's not listed anywhere as being cleared, and rsync expects it to work like this.
+    if (fork_parent)
+        m_signal_action_data = fork_parent->m_signal_action_data;
+
     return {};
 }
 
