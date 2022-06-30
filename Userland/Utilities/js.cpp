6b67ac8e8619d128ea67e1868289a97e036b3239
Linus Groh <mail@linusgroh.de>
2022-06-30 20:00:00 +0100

    js: Implement pretty-printing of WeakSet objects

--- Userland/Utilities/js.cpp
+++ Userland/Utilities/js.cpp
@@ -73,6 +73,7 @@
 #include <LibJS/Runtime/TypedArray.h>
 #include <LibJS/Runtime/Value.h>
 #include <LibJS/Runtime/WeakMap.h>
+#include <LibJS/Runtime/WeakSet.h>
 #include <LibJS/SourceTextModule.h>
 #include <LibLine/Editor.h>
 #include <LibMain/Main.h>
@@ -418,6 +419,13 @@ static void print_weak_map(JS::WeakMap const& weak_map, HashTable<JS::Object*>&)
 {
     print_type("WeakMap");
     js_out(" ({})", weak_map.values().size());
+    // Note: We could tell you what's actually inside, but not in insertion order.
+}
+
+static void print_weak_set(JS::WeakSet const& weak_set, HashTable<JS::Object*>&)
+{
+    print_type("WeakSet");
+    js_out(" ({})", weak_set.values().size());
     // Note: We could tell you what's actually inside, but not in insertion order.
 }
 
@@ -992,6 +1000,8 @@ static void print_value(JS::Value value, HashTable<JS::Object*>& seen_objects)
             return print_set(static_cast<JS::Set&>(object), seen_objects);
         if (is<JS::WeakMap>(object))
             return print_weak_map(static_cast<JS::WeakMap&>(object), seen_objects);
+        if (is<JS::WeakSet>(object))
+            return print_weak_set(static_cast<JS::WeakSet&>(object), seen_objects);
         if (is<JS::DataView>(object))
             return print_data_view(static_cast<JS::DataView&>(object), seen_objects);
         if (is<JS::ProxyObject>(object))
