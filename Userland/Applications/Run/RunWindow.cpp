e98f8f20df86ce0e0a48ec10d9406ec73ab83330
Andreas Kling <kling@serenityos.org>
2022-06-30 17:30:34 +0200

    Run: Don't try to set a history selection if history is empty
    This fixes an assertion on startup when there's no command history.

--- Userland/Applications/Run/RunWindow.cpp
+++ Userland/Applications/Run/RunWindow.cpp
@@ -47,7 +47,8 @@ RunWindow::RunWindow()
 
     m_path_combo_box = *main_widget.find_descendant_of_type_named<GUI::ComboBox>("path");
     m_path_combo_box->set_model(m_path_history_model);
-    m_path_combo_box->set_selected_index(0);
+    if (!m_path_history.is_empty())
+        m_path_combo_box->set_selected_index(0);
 
     m_ok_button = *main_widget.find_descendant_of_type_named<GUI::DialogButton>("ok_button");
     m_ok_button->on_click = [this](auto) {
