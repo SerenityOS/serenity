49478041218da0a4ae24b4854c9618f69807dfaf
Nathan Ell <nathanjamesell@gmail.com>
2022-07-01 22:25:43 -0600

    Ports: Update dos2unix to 7.4.3
    Update to the latest version of the upstream dos2unix, 7.4.3.
    Simplify the URL used to download the upstream PGP key for the
    dos2unix package, by leveraging the already well-used version
    variable.

--- Ports/dos2unix/package.sh
+++ Ports/dos2unix/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=dos2unix
-version=7.4.2
+version=7.4.3
 workdir="${port}-${version}"
 files="https://waterlan.home.xs4all.nl/dos2unix/dos2unix-${version}.tar.gz ${port}-${version}.tar.gz
-https://waterlan.home.xs4all.nl/dos2unix/dos2unix-7.4.2.tar.gz.asc ${port}-${version}.tar.gz.asc"
+https://waterlan.home.xs4all.nl/dos2unix/dos2unix-${version}.tar.gz.asc ${port}-${version}.tar.gz.asc"
 depends=("gettext")
 auth_type=sig
 auth_import_key="f8f1bea490496a09cca328cc38c1f572b12725be"
