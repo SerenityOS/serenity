dce36e5b0f422d24155cbc0b477d1bb69c0563ea
thankyouverycool <66646555+thankyouverycool@users.noreply.github.com>
2022-07-05 11:33:25 -0400

    LibGUI: Don't ignore invisible widgets during layout changes
    As they may be setting themselves visible again in the process.
    Fixes inability to toggle widgets on and off in some apps.

--- Userland/Libraries/LibGUI/Widget.cpp
+++ Userland/Libraries/LibGUI/Widget.cpp
@@ -201,12 +201,10 @@ Widget::~Widget() = default;
 
 void Widget::layout_relevant_change_occured()
 {
-    if (is_visible()) {
-        if (auto* parent = parent_widget())
-            parent->layout_relevant_change_occured();
-        else if (window())
-            window()->schedule_relayout();
-    }
+    if (auto* parent = parent_widget())
+        parent->layout_relevant_change_occured();
+    else if (window())
+        window()->schedule_relayout();
 }
 
 void Widget::child_event(Core::ChildEvent& event)
