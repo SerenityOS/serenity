554d4ff9bfbf79cab92d2937d89d952623f4e38e
Matthew Costa <ucosty@gmail.com>
2022-07-06 14:38:50 +0100

    Ladybird: Expanded toolbar with browser history and home button
    This patch takes the browser history code from the Serenity browser and
    wires it up to the QT interface. This is tied in with a few extra
    toolbar buttons associated with each tab.

---
+++ Ladybird/History.cpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2020, Andreas Kling <kling@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include "History.h"
+
+namespace Browser {
+
+void History::dump() const
+{
+    dbgln("Dump {} items(s)", m_items.size());
+    int i = 0;
+    for (auto& item : m_items) {
+        dbgln("[{}] {} '{}' {}", i, item.url, item.title, m_current == i ? '*' : ' ');
+        ++i;
+    }
+}
+
+void History::push(const URL& url, String const& title)
+{
+    if (!m_items.is_empty() && m_items[m_current].url == url)
+        return;
+    m_items.shrink(m_current + 1);
+    m_items.append(URLTitlePair {
+        .url = url,
+        .title = title,
+    });
+    m_current++;
+}
+
+History::URLTitlePair History::current() const
+{
+    if (m_current == -1)
+        return {};
+    return m_items[m_current];
+}
+
+void History::go_back(int steps)
+{
+    VERIFY(can_go_back(steps));
+    m_current -= steps;
+}
+
+void History::go_forward(int steps)
+{
+    VERIFY(can_go_forward(steps));
+    m_current += steps;
+}
+
+void History::clear()
+{
+    m_items = {};
+    m_current = -1;
+}
+
+void History::update_title(String const& title)
+{
+    m_items[m_current].title = title;
+}
+
+Vector<StringView> History::get_back_title_history()
+{
+    Vector<StringView> back_title_history;
+    for (int i = m_current - 1; i >= 0; i--) {
+        back_title_history.append(m_items[i].title);
+    }
+    return back_title_history;
+}
+
+Vector<StringView> History::get_forward_title_history()
+{
+    Vector<StringView> forward_title_history;
+    for (int i = m_current + 1; i < static_cast<int>(m_items.size()); i++) {
+        forward_title_history.append(m_items[i].title);
+    }
+    return forward_title_history;
+}
+
+}
---
+++ Ladybird/History.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/URL.h>
+#include <AK/Vector.h>
+
+namespace Browser {
+
+class History {
+public:
+    struct URLTitlePair {
+        URL url;
+        String title;
+    };
+    void dump() const;
+
+    void push(const URL& url, String const& title);
+    void update_title(String const& title);
+    URLTitlePair current() const;
+
+    Vector<StringView> get_back_title_history();
+    Vector<StringView> get_forward_title_history();
+
+    void go_back(int steps = 1);
+    void go_forward(int steps = 1);
+
+    bool can_go_back(int steps = 1) { return (m_current - steps) >= 0; }
+    bool can_go_forward(int steps = 1) { return (m_current + steps) < static_cast<int>(m_items.size()); }
+    void clear();
+
+private:
+    Vector<URLTitlePair> m_items;
+    int m_current { -1 };
+};
+
+}
