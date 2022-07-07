ec985cb7598f0a7dcfffccf108a1e7cb88395d9a
Liav A <liavalb@gmail.com>
2022-07-08 00:00:37 +0300

    Ports: Add stb header files

---
+++ Ports/stb/package.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port='stb'
+version='af1a5bc352164740c1cc1354942b1c6b72eacb8a'
+files="https://github.com/nothings/stb/archive/${version}.zip stb-${version}.zip e3d0edbecd356506d3d69b87419de2f9d180a98099134c6343177885f6c2cbef"
+auth_type='sha256'
+
+build() {
+    :
+}
+
+install() {
+    run cp -r "./" "${SERENITY_INSTALL_ROOT}/usr/local/include/"
+}
