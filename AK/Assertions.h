2217d91b8d7c5086fad76396d5b50e13d67aae65
Andreas Kling <kling@serenityos.org>
2022-07-08 14:16:53 +0200

    AK: Make VERIFY() work in MinSizeRel builds

--- AK/Assertions.h
+++ AK/Assertions.h
@@ -10,8 +10,15 @@
 #    include <Kernel/Assertions.h>
 #else
 #    include <assert.h>
-#    define VERIFY assert
-#    define VERIFY_NOT_REACHED() assert(false) /* NOLINT(cert-dcl03-c,misc-static-assert) No, this can't be static_assert, it's a runtime check */
+#    ifndef NDEBUG
+#        define VERIFY assert
+#    else
+#        define VERIFY(expr)              \
+            (__builtin_expect(!(expr), 0) \
+                    ? __builtin_trap()    \
+                    : (void)0)
+#    endif
+#    define VERIFY_NOT_REACHED() VERIFY(false) /* NOLINT(cert-dcl03-c,misc-static-assert) No, this can't be static_assert, it's a runtime check */
 static constexpr bool TODO = false;
 #    define TODO() VERIFY(TODO)                /* NOLINT(cert-dcl03-c,misc-static-assert) No, this can't be static_assert, it's a runtime check */
 #endif
