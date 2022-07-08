cbc951cc100c3cf2cf6503c513eab2de8dbb06d2
Tim Schumacher <timschumi@gmx.de>
2022-07-08 04:30:14 +0200

    Ports/libicu: Evaluate the host build folder later
    We need this to be the build directory, not the tarball directory.

--- Ports/libicu/package.sh
+++ Ports/libicu/package.sh
@@ -4,7 +4,6 @@ version=69.1
 useconfigure=true
 use_fresh_config_sub=true
 workdir=icu/source
-configopts=("--with-cross-build=$(pwd)/${workdir}/../host-build")
 files="https://github.com/unicode-org/icu/releases/download/release-${version//./-}/icu4c-${version//./_}-src.tgz icu4c-${version//./_}-src.tgz 4cba7b7acd1d3c42c44bb0c14be6637098c7faf2b330ce876bc5f3b915d09745"
 auth_type=sha256
 
@@ -13,7 +12,7 @@ configure() {
     run mkdir -p ../host-build
     run sh -c "cd ../host-build && ../source/configure && make ${makeopts[@]}"
     target_env
-    run ./configure --host="${SERENITY_ARCH}-pc-serenity" "${configopts[@]}"
+    run ./configure --host="${SERENITY_ARCH}-pc-serenity" "--with-cross-build=$(pwd)/${workdir}/../host-build"
 }
 
 export CFLAGS="-DU_HAVE_NL_LANGINFO_CODESET=0"
