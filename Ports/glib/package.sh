fb877effb8dba8909ad439eca9334f392e71a03e
Tim Schumacher <timschumi@gmx.de>
2022-07-09 16:14:03 +0200

    Meta+Ports: Automatically generate a meson cross file that we can use

--- Ports/glib/package.sh
+++ Ports/glib/package.sh
@@ -4,7 +4,7 @@ version='2.73.0'
 files="https://gitlab.gnome.org/GNOME/glib/-/archive/${version}/glib-${version}.tar.gz glib-${version}.tar.gz 3f573319adbdf572d79255e8bae85c7e2902d1aa6177d2646605a00c0a607eca"
 auth_type='sha256'
 useconfigure='true'
-configopts=("--cross-file" "../cross_file-${SERENITY_ARCH}.txt")
+configopts=("--cross-file" "${SERENITY_BUILD_DIR}/meson-cross-file.txt")
 depends=("libiconv" "libffi" "zlib" "gettext" "pcre")
 
 configure() {
