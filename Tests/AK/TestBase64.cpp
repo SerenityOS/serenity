ad11148ff545a14444d605c1d9662adec7c22864
sin-ack <sin-ack@users.noreply.github.com>
2022-07-11 20:30:25 +0000

    Tests: Convert TestBase64 decode test to use StringViews directly
    Previously it would rely on the implicit StringView conversions. Now the
    decode_equal function will directly use StringViews.

--- Tests/AK/TestBase64.cpp
+++ Tests/AK/TestBase64.cpp
@@ -12,46 +12,46 @@
 
 TEST_CASE(test_decode)
 {
-    auto decode_equal = [&](char const* input, char const* expected) {
-        auto decoded_option = decode_base64(StringView(input));
+    auto decode_equal = [&](StringView input, StringView expected) {
+        auto decoded_option = decode_base64(input);
         EXPECT(!decoded_option.is_error());
         auto decoded = decoded_option.release_value();
-        EXPECT(String::copy(decoded) == String(expected));
-        EXPECT(StringView(expected).length() <= calculate_base64_decoded_length(StringView(input).bytes()));
+        EXPECT(String::copy(decoded) == expected);
+        EXPECT(expected.length() <= calculate_base64_decoded_length(input.bytes()));
     };
 
-    decode_equal("", "");
-    decode_equal("Zg==", "f");
-    decode_equal("Zm8=", "fo");
-    decode_equal("Zm9v", "foo");
-    decode_equal("Zm9vYg==", "foob");
-    decode_equal("Zm9vYmE=", "fooba");
-    decode_equal("Zm9vYmFy", "foobar");
-    decode_equal("Z m\r9\n   v\v  Ym\tFy", "foobar");
+    decode_equal(""sv, ""sv);
+    decode_equal("Zg=="sv, "f"sv);
+    decode_equal("Zm8="sv, "fo"sv);
+    decode_equal("Zm9v"sv, "foo"sv);
+    decode_equal("Zm9vYg=="sv, "foob"sv);
+    decode_equal("Zm9vYmE="sv, "fooba"sv);
+    decode_equal("Zm9vYmFy"sv, "foobar"sv);
+    decode_equal("Z m\r9\n   v\v  Ym\tFy"sv, "foobar"sv);
     EXPECT_EQ(decode_base64(" ZD Qg\r\nPS An Zm91cic\r\n 7"sv).value(), decode_base64("ZDQgPSAnZm91cic7"sv).value());
 }
 
 TEST_CASE(test_decode_invalid)
 {
-    EXPECT(decode_base64(StringView("asdf\xffqwe")).is_error());
-    EXPECT(decode_base64(StringView("asdf\x80qwe")).is_error());
-    EXPECT(decode_base64(StringView("asdf:qwe")).is_error());
-    EXPECT(decode_base64(StringView("asdf=qwe")).is_error());
+    EXPECT(decode_base64(("asdf\xffqwe"sv)).is_error());
+    EXPECT(decode_base64(("asdf\x80qwe"sv)).is_error());
+    EXPECT(decode_base64(("asdf:qwe"sv)).is_error());
+    EXPECT(decode_base64(("asdf=qwe"sv)).is_error());
 }
 
 TEST_CASE(test_encode)
 {
-    auto encode_equal = [&](char const* input, char const* expected) {
-        auto encoded = encode_base64({ input, strlen(input) });
+    auto encode_equal = [&](StringView input, StringView expected) {
+        auto encoded = encode_base64(input.bytes());
         EXPECT(encoded == String(expected));
-        EXPECT_EQ(StringView(expected).length(), calculate_base64_encoded_length(StringView(input).bytes()));
+        EXPECT_EQ(expected.length(), calculate_base64_encoded_length(input.bytes()));
     };
 
-    encode_equal("", "");
-    encode_equal("f", "Zg==");
-    encode_equal("fo", "Zm8=");
-    encode_equal("foo", "Zm9v");
-    encode_equal("foob", "Zm9vYg==");
-    encode_equal("fooba", "Zm9vYmE=");
-    encode_equal("foobar", "Zm9vYmFy");
+    encode_equal(""sv, ""sv);
+    encode_equal("f"sv, "Zg=="sv);
+    encode_equal("fo"sv, "Zm8="sv);
+    encode_equal("foo"sv, "Zm9v"sv);
+    encode_equal("foob"sv, "Zm9vYg=="sv);
+    encode_equal("fooba"sv, "Zm9vYmE="sv);
+    encode_equal("foobar"sv, "Zm9vYmFy"sv);
 }
