47758a21c0433c50573279c3a49e6446dd3d54c5
MacDue <macdue@dueutil.tech>
2022-07-12 15:30:13 +0100

    LibWeb: Add serialize_a_srgb_value()
    This moves the logic from ColorStyleValue::to_string() to a standalone
    function.

--- Userland/Libraries/LibWeb/CSS/Serialize.h
+++ Userland/Libraries/LibWeb/CSS/Serialize.h
@@ -9,6 +9,7 @@
 #include <AK/String.h>
 #include <AK/StringBuilder.h>
 #include <AK/StringView.h>
+#include <LibGfx/Color.h>
 
 namespace Web::CSS {
 
@@ -17,11 +18,13 @@ void escape_a_character_as_code_point(StringBuilder&, u32 character);
 void serialize_an_identifier(StringBuilder&, StringView ident);
 void serialize_a_string(StringBuilder&, StringView string);
 void serialize_a_url(StringBuilder&, StringView url);
+void serialize_a_srgb_value(StringBuilder&, Color color);
 
 String escape_a_character(u32 character);
 String escape_a_character_as_code_point(u32 character);
 String serialize_an_identifier(StringView ident);
 String serialize_a_string(StringView string);
 String serialize_a_url(StringView url);
+String serialize_a_srgb_value(Color color);
 
 }
