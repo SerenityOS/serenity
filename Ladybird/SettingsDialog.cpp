496ed484561f4b9097c185ff70f25ccfaf6e9f82
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2022-07-14 06:36:11 +0200

    Ladybird: Allow for setting the hompage through SettingsDialog

--- Ladybird/SettingsDialog.cpp
+++ Ladybird/SettingsDialog.cpp
@@ -4,16 +4,45 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include "Settings.h"
 #include "SettingsDialog.h"
+#include <QCloseEvent>
+#include <QLabel>
+
+extern Browser::Settings* s_settings;
 
 SettingsDialog::SettingsDialog(QMainWindow* window)
     : m_window(window)
 {
-    m_layout = new QBoxLayout(QBoxLayout::Direction::TopToBottom, this);
+    m_layout = new QFormLayout;
+    m_homepage = new QLineEdit;
+    m_ok_button = new QPushButton("&Save");
+
+    m_layout->addWidget(new QLabel("Homepage"));
+    m_layout->addWidget(m_homepage);
+    m_layout->addWidget(m_ok_button);
+
+    m_homepage->setText(s_settings->homepage());
+
+    QObject::connect(m_ok_button, &QPushButton::released, this, [this] {
+        close();
+    });
     
     setWindowTitle("Settings");
-    resize(340, 400);
+    setFixedWidth(300);
     setLayout(m_layout);
     show();
     setFocus();
+}
+
+void SettingsDialog::closeEvent(QCloseEvent *event)
+{
+    save();
+    event->accept();
+}
+
+void SettingsDialog::save()
+{
+    // FIXME: Validate data.
+    s_settings->set_homepage(m_homepage->text());
 }
--- Ladybird/SettingsDialog.h
+++ Ladybird/SettingsDialog.h
@@ -4,9 +4,11 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#include <QBoxLayout>
+#include <QFormLayout>
 #include <QDialog>
 #include <QMainWindow>
+#include <QLineEdit>
+#include <QPushButton>
 
 #pragma once
 
@@ -14,8 +16,14 @@ class SettingsDialog : public QDialog {
     Q_OBJECT
 public:
     explicit SettingsDialog(QMainWindow* window);
+    
+    void save();
+
+    virtual void closeEvent(QCloseEvent*) override;
 
 private:
-    QBoxLayout* m_layout;
+    QFormLayout* m_layout;
+    QPushButton* m_ok_button { nullptr };
+    QLineEdit* m_homepage { nullptr };
     QMainWindow* m_window { nullptr };
 };
