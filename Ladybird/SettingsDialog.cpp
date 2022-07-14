8c163861741db7f77275ecd58e9469f6719d9bc7
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2022-07-14 06:36:11 +0200

    Ladybird: Allow for setting the hompage through SettingsDialog

--- Ladybird/SettingsDialog.cpp
+++ Ladybird/SettingsDialog.cpp
@@ -5,15 +5,44 @@
  */
 
 #include "SettingsDialog.h"
+#include "Settings.h"
+#include <QCloseEvent>
+#include <QLabel>
+
+extern Browser::Settings* s_settings;
 
 SettingsDialog::SettingsDialog(QMainWindow* window)
     : m_window(window)
 {
-    m_layout = new QBoxLayout(QBoxLayout::Direction::TopToBottom, this);
+    m_layout = new QFormLayout;
+    m_homepage = new QLineEdit;
+    m_ok_button = new QPushButton("&Save");
+
+    m_layout->addWidget(new QLabel("Homepage"));
+    m_layout->addWidget(m_homepage);
+    m_layout->addWidget(m_ok_button);
+
+    m_homepage->setText(s_settings->homepage());
+
+    QObject::connect(m_ok_button, &QPushButton::released, this, [this] {
+        close();
+    });
 
     setWindowTitle("Settings");
-    resize(340, 400);
+    setFixedWidth(300);
     setLayout(m_layout);
     show();
     setFocus();
+}
+
+void SettingsDialog::closeEvent(QCloseEvent* event)
+{
+    save();
+    event->accept();
+}
+
+void SettingsDialog::save()
+{
+    // FIXME: Validate data.
+    s_settings->set_homepage(m_homepage->text());
 }
