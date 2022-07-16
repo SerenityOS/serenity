8b1bc6e22002c88e7ab8fb1792bd345f94406305
Timothy Flynn <trflynn89@pm.me>
2022-07-16 12:38:26 -0400

    LibCrypto: Expose UnsignedBigInteger's is_zero() in SignedBigInteger
    Note we don't need to check the sign because negative zero became
    disallowed in b0d6399f60760e25a55ec9e8e95a1ad322b74b22.

--- Userland/Libraries/LibCrypto/BigInt/SignedBigInteger.h
+++ Userland/Libraries/LibCrypto/BigInt/SignedBigInteger.h
@@ -72,6 +72,7 @@ public:
     [[nodiscard]] UnsignedBigInteger const& unsigned_value() const { return m_unsigned_data; }
     [[nodiscard]] Vector<u32, STARTING_WORD_SIZE> const words() const { return m_unsigned_data.words(); }
     [[nodiscard]] bool is_negative() const { return m_sign; }
+    [[nodiscard]] bool is_zero() const { return m_unsigned_data.is_zero(); }
 
     void negate()
     {
