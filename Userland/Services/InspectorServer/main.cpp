c248569d6b42083cf39618adf5d38ad772a216e9
Lucas CHOLLET <lucas.chollet@free.fr>
2022-07-24 15:32:41 +0200

    Base: Launch InspectorServer at session start-up

--- Userland/Services/InspectorServer/main.cpp
+++ Userland/Services/InspectorServer/main.cpp
@@ -19,10 +19,10 @@ ErrorOr<int> serenity_main(Main::Arguments)
 
     TRY(Core::System::pledge("stdio unix accept"));
 
-    auto server = TRY(IPC::MultiServer<InspectorServer::ConnectionFromClient>::try_create("/tmp/portal/inspector"));
+    auto server = TRY(IPC::MultiServer<InspectorServer::ConnectionFromClient>::try_create("/tmp/user/%uid/portal/inspector"));
 
     auto inspectables_server = TRY(Core::LocalServer::try_create());
-    TRY(inspectables_server->take_over_from_system_server("/tmp/portal/inspectables"));
+    TRY(inspectables_server->take_over_from_system_server("/tmp/user/%uid/portal/inspectables"));
 
     inspectables_server->on_accept = [&](auto client_socket) {
         auto pid = client_socket->peer_pid().release_value_but_fixme_should_propagate_errors();
