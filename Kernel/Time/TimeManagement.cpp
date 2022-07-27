40dcf2df8e58f1532658ff006c2847cc52cc83de
Tim Schumacher <timschumi@gmx.de>
2022-07-28 01:55:34 +0200

    Kernel: Pass CLOCK_MONOTONIC through the time page
    This removes a lot of syscall overhead from the QEMU timing code,
    resulting in a decrease of Serenity boot time from 1h16m down to 42m
    when running on Serenity.
    
    However, note that the time page only appears to be updated around 250
    times a second, so we are likely losing a bit of accuracy there.

--- Kernel/Time/TimeManagement.cpp
+++ Kernel/Time/TimeManagement.cpp
@@ -438,6 +438,7 @@ void TimeManagement::update_time_page()
     auto& page = time_page();
     u32 update_iteration = AK::atomic_fetch_add(&page.update2, 1u, AK::MemoryOrder::memory_order_acquire);
     page.clocks[CLOCK_REALTIME_COARSE] = m_epoch_time;
+    page.clocks[CLOCK_MONOTONIC] = monotonic_time(TimePrecision::Precise).to_timespec();
     page.clocks[CLOCK_MONOTONIC_COARSE] = monotonic_time(TimePrecision::Coarse).to_timespec();
     AK::atomic_store(&page.update1, update_iteration + 1u, AK::MemoryOrder::memory_order_release);
 }
