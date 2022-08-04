b73bd4f98856d8983cfebb16db353a8e9d1032f7
Andreas Kling <kling@serenityos.org>
2022-08-04 20:10:09 +0200

    LibWeb: Add HTML::CrossOriginOpenerPolicy

---
+++ Userland/Libraries/LibWeb/HTML/CrossOrigin/CrossOriginOpenerPolicy.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2022, Andreas Kling <kling@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+namespace Web::HTML {
+
+// https://html.spec.whatwg.org/multipage/origin.html#cross-origin-opener-policy-value
+enum class CrossOriginOpenerPolicyValue {
+    UnsafeNone,
+    SameOriginAllowPopups,
+    SameOrigin,
+    SameOriginPlusCOEP,
+};
+
+// https://html.spec.whatwg.org/multipage/origin.html#cross-origin-opener-policy
+struct CrossOriginOpenerPolicy {
+    // A value, which is a cross-origin opener policy value, initially "unsafe-none".
+    CrossOriginOpenerPolicyValue value { CrossOriginOpenerPolicyValue::UnsafeNone };
+
+    // A reporting endpoint, which is string or null, initially null.
+    Optional<String> reporting_endpoint;
+
+    // A report-only value, which is a cross-origin opener policy value, initially "unsafe-none".
+    CrossOriginOpenerPolicyValue report_only_value { CrossOriginOpenerPolicyValue::UnsafeNone };
+
+    // A report-only reporting endpoint, which is a string or null, initially null.
+    Optional<String> report_only_reporting_endpoint;
+};
+
+}
