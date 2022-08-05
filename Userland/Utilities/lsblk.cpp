c85f81bc9db5f66d44dc68afb5ea6be2cef61525
Liav A <liavalb@gmail.com>
2022-08-05 13:30:49 +0300

    Utilities/lsblk: Remove Interface Type column
    We are going to remove this slice of data from the SysFS later on, so
    lsblk must not try to read it.

--- Userland/Utilities/lsblk.cpp
+++ Userland/Utilities/lsblk.cpp
@@ -13,7 +13,7 @@
 #include <LibCore/System.h>
 #include <LibMain/Main.h>
 
-static constexpr StringView format_row = "{:10s}\t{:10s}\t{:10s}\t{:10s}\t{:10s}"sv;
+static constexpr StringView format_row = "{:10s}\t{:10s}\t{:10s}\t{:10s}"sv;
 
 ErrorOr<int> serenity_main(Main::Arguments arguments)
 {
@@ -31,7 +31,7 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
         return 1;
     }
 
-    outln(format_row, "LUN"sv, "Interface type"sv, "Command set"sv, "Block Size"sv, "Last LBA"sv);
+    outln(format_row, "LUN"sv, "Command set"sv, "Block Size"sv, "Last LBA"sv);
 
     TRY(Core::System::pledge("stdio rpath"));
 
@@ -40,11 +40,6 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
         auto command_set_file = Core::File::construct(String::formatted("/sys/devices/storage/{}/command_set", dir));
         if (!command_set_file->open(Core::OpenMode::ReadOnly)) {
             dbgln("Error: Could not open {}: {}", command_set_file->name(), command_set_file->error_string());
-            continue;
-        }
-        auto interface_type_file = Core::File::construct(String::formatted("/sys/devices/storage/{}/interface_type", dir));
-        if (!interface_type_file->open(Core::OpenMode::ReadOnly)) {
-            dbgln("Error: Could not open {}: {}", interface_type_file->name(), interface_type_file->error_string());
             continue;
         }
         auto last_lba_file = Core::File::construct(String::formatted("/sys/devices/storage/{}/last_lba", dir));
@@ -59,11 +54,10 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
         }
 
         String command_set = StringView(command_set_file->read_all().bytes());
-        String interface_type = StringView(interface_type_file->read_all().bytes());
         String last_lba = StringView(last_lba_file->read_all().bytes());
         String sector_size = StringView(sector_size_file->read_all().bytes());
 
-        outln(format_row, dir, interface_type, command_set, sector_size, last_lba);
+        outln(format_row, dir, command_set, sector_size, last_lba);
     }
 
     return 0;
