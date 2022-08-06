e8a3b91355e054dec0e89da91fd8ae5c218293d8
Liav A <liavalb@gmail.com>
2022-08-06 17:40:07 +0300

    Kernel/FileSystem: Use a new debug flag for SysFS debug messages

--- Kernel/FileSystem/SysFS/Subsystems/Bus/USB/DeviceInformation.cpp
+++ Kernel/FileSystem/SysFS/Subsystems/Bus/USB/DeviceInformation.cpp
@@ -115,7 +115,7 @@ ErrorOr<void> SysFSUSBDeviceInformation::refresh_data(OpenFileDescription& descr
 
 ErrorOr<size_t> SysFSUSBDeviceInformation::read_bytes(off_t offset, size_t count, UserOrKernelBuffer& buffer, OpenFileDescription* description) const
 {
-    dbgln_if(PROCFS_DEBUG, "SysFSUSBDeviceInformation @ {}: read_bytes offset: {} count: {}", name(), offset, count);
+    dbgln_if(SYSFS_DEBUG, "SysFSUSBDeviceInformation @ {}: read_bytes offset: {} count: {}", name(), offset, count);
 
     VERIFY(offset >= 0);
     VERIFY(buffer.user_or_kernel_ptr());
