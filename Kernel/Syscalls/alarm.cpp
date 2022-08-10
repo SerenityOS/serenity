7d194a4ac9b3ad367ad0e64c182373556a7b2817
Brian Gianforcaro <b.gianfo@gmail.com>
2022-08-09 20:31:29 -0700

    Kernel: Validate the sys$alarm signal send always succeeds
    Previously we were ignoring this return code, instead use MUST(..)
    to make sure it always succeeds.

--- Kernel/Syscalls/alarm.cpp
+++ Kernel/Syscalls/alarm.cpp
@@ -34,7 +34,7 @@ ErrorOr<FlatPtr> Process::sys$alarm(unsigned seconds)
             m_alarm_timer = TRY(adopt_nonnull_ref_or_enomem(new (nothrow) Timer));
         }
         auto timer_was_added = TimerQueue::the().add_timer_without_id(*m_alarm_timer, CLOCK_REALTIME_COARSE, deadline, [this]() {
-            [[maybe_unused]] auto rc = send_signal(SIGALRM, nullptr);
+            MUST(send_signal(SIGALRM, nullptr));
         });
         if (!timer_was_added)
             return ENOMEM;
