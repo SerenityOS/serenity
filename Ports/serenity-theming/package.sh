8dcbc86ec0d6abd378baf4bcabe539dd41355c42
djwisdom <djwisdom@gmail.com>
2022-08-12 10:50:37 +0800

    Ports: Update serenity-theming use latest commit 018356b
    Improve theming support include color-palettes, wallpapers and
    terminal-colors folders

--- Ports/serenity-theming/package.sh
+++ Ports/serenity-theming/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=serenity-theming
-version=3dc8b8e4605f726da82d3262f6c4ea48ada8d569
+version=018356b4d5fa742efba21e3dd67c3159f44fd447
 workdir="theming-${version}"
-files="https://github.com/SerenityOS/theming/archive/${version}.zip serenity-theming-${version}.zip 20a6b48b8787fe263dcafae4f698913b0b93f6c6f74895b1c6df4f9a16f0b3ac"
+files="https://github.com/SerenityOS/theming/archive/${version}.zip serenity-theming-${version}.zip 061cc14c963d76382c4f4c478feede39a50403d6b0c67fc5199a0f1f0fa9b068"
 auth_type="sha256"
 
 build() {
@@ -19,4 +19,8 @@ post_install() {
     cp -r "${workdir}/icons" "${SERENITY_INSTALL_ROOT}/res/"
     cp -r "${workdir}/themes" "${SERENITY_INSTALL_ROOT}/res/"
     cp -r "${workdir}/fonts" "${SERENITY_INSTALL_ROOT}/res/"
+    cp -r "${workdir}/color-palettes" "${SERENITY_INSTALL_ROOT}/res/"
+    cp -r "${workdir}/emoji" "${SERENITY_INSTALL_ROOT}/res/"
+    cp -r "${workdir}/terminal-colors" "${SERENITY_INSTALL_ROOT}/res/"
+    cp -r "${workdir}/wallpapers" "${SERENITY_INSTALL_ROOT}/res/"
 }
