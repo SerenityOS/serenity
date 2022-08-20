b79f03182d98a10ee234ae33fe68d4a8f3d95406
davidot <davidot@serenityos.org>
2022-08-20 16:24:44 +0200

    LibJS: Add special cases for Math.cosh and add spec comments
    Although this already works in most cases in non-kvm serenity cases the
    cosh and other math function tend to return incorrect values for
    Infinity. This makes sure that whatever the underlying cosh function
    returns Math.cosh conforms to the spec.

--- Userland/Libraries/LibJS/Tests/builtins/Math/Math.cosh.js
+++ Userland/Libraries/LibJS/Tests/builtins/Math/Math.cosh.js
@@ -1,7 +1,19 @@
-test("basic functionality", () => {
-    expect(Math.cosh).toHaveLength(1);
+describe("basic functionality", () => {
+    test("length", () => {
+        expect(Math.cosh).toHaveLength(1);
+    });
 
-    expect(Math.cosh(0)).toBe(1);
-    expect(Math.cosh(1)).toBeCloseTo(1.5430806348152437);
-    expect(Math.cosh(-1)).toBeCloseTo(1.5430806348152437);
+    test("simple values", () => {
+        expect(Math.cosh(1)).toBeCloseTo(1.5430806348152437);
+        expect(Math.cosh(-1)).toBeCloseTo(1.5430806348152437);
+    });
+
+    test("special values", () => {
+        expect(Math.cosh(0)).toBe(1);
+        expect(Math.cosh(-0.0)).toBe(1);
+
+        expect(Math.cosh(NaN)).toBeNaN();
+        expect(Math.cosh(Infinity)).toBe(Infinity);
+        expect(Math.cosh(-Infinity)).toBe(Infinity);
+    });
 });
