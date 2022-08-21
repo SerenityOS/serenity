831afe3c3d0be9c23590186380938345a93efdf9
Tim Schumacher <timschumi@gmx.de>
2022-08-21 13:43:54 +0200

    Ports/SDL2: Update to 2.24.0

--- Ports/SDL2/package.sh
+++ Ports/SDL2/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=SDL2
-version=2.0.22
-useconfigure=true
-auth_type=sha256
-files="https://github.com/libsdl-org/SDL/releases/download/release-${version}/SDL2-${version}.tar.gz SDL2-${version}.tar.gz fe7cbf3127882e3fc7259a75a0cb585620272c51745d3852ab9dd87960697f2e"
+port='SDL2'
+version='2.24.0'
+useconfigure='true'
+auth_type='sha256'
+files="https://github.com/libsdl-org/SDL/releases/download/release-${version}/SDL2-${version}.tar.gz SDL2-${version}.tar.gz 91e4c34b1768f92d399b078e171448c6af18cafda743987ed2064a28954d6d97"
 configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt" "-DPULSEAUDIO=OFF" "-DJACK=OFF" "-DEXTRA_LDFLAGS=-laudio;-liconv;-ldl")
 depends=("libiconv")
 
--- Ports/SDL2/patches/ReadMe.md
+++ Ports/SDL2/patches/ReadMe.md
@@ -5,10 +5,3 @@
 Add SerenityOS platform support
 
 
-## `0002-Make-sdl2-config-prefixes-configurable-again.patch`
-
-Make sdl2-config prefixes configurable again
-
-These were accidentally removed in
-6956f4aa1982b66b234026b46f7bb2dd44c67894.
-
