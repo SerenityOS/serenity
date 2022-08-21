a00fa793b37550a8b44122192346eeeb1d692bf9
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2022-08-21 21:24:29 +0200

    LibGUI: Add on_segment_change handler to Breadcrumbbar
    This allows programs to respond to any selection changes of the
    Breadcrumbbar, not just ones made by clicking one of the buttons.

--- Userland/Libraries/LibGUI/Breadcrumbbar.h
+++ Userland/Libraries/LibGUI/Breadcrumbbar.h
@@ -29,6 +29,7 @@ public:
     void set_selected_segment(Optional<size_t> index);
     Optional<size_t> selected_segment() const { return m_selected_segment; }
 
+    Function<void(Optional<size_t> index)> on_segment_change;
     Function<void(size_t index)> on_segment_click;
     Function<void(size_t index, DropEvent&)> on_segment_drop;
     Function<void(size_t index, DragEvent&)> on_segment_drag_enter;
