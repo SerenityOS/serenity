ed124dff4eccfa9e80a5dee6d25b62d7e57c5cf3
kleines Filmröllchen <filmroellchen@serenityos.org>
2022-08-22 22:58:40 +0200

    Ports: Add the Small Device C Compiler (WIP)
    This commit fails to configure because of unknown configure script
    weirdness.

---
+++ Ports/sdcc/package.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port=sdcc
+version=20220820-13663
+useconfigure=true
+files="http://sourceforge.net/projects/sdcc/files/snapshot_builds/sdcc-src/sdcc-src-${version}.tar.bz2/download sdcc-${version}.tar.bz2 84d2b717530567ab487e3d436f87d9956ea985cc10b22c367339961e6293c6d6"
+auth_type='sha256'
+workdir='sdcc'
---
+++ Ports/sdcc/patches/ReadMe.md
@@ -0,0 +1,7 @@
+# Patches for sdcc on SerenityOS
+
+## `0001-Recognize-Serenity-as-a-system.patch`
+
+Recognize Serenity as a system
+
+
