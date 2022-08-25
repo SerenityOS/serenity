400b96761362401abc959b40ca09edbd34afbc81
plexx <plexx@plexx.dev>
2022-08-26 00:12:51 +0200

    Ports: Added / Fixed nettle version 3.8 Port

---
+++ Ports/nettle/package.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+
+port='nettle'
+version='3.8'
+useconfigure='true'
+files="https://ftp.gnu.org/gnu/nettle/nettle-${version}.tar.gz nettle-${version}.tar.gz 7576c68481c198f644b08c160d1a4850ba9449e308069455b5213319f234e8e6"
+auth_type=sha256
