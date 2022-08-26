0369669266943136cc34d9a92678b2c5fca813fb
thankyouverycool <66646555+thankyouverycool@users.noreply.github.com>
2022-08-25 20:10:50 -0400

    FileSystemAccessServer: Set dummy child to WindowMode::Passive
    so Dialogs can join the modal chain of the window parenting them.
    Fixes apps that use FileSystemAccess to sandbox FilePicker not
    respecting its blocking effect.

--- Userland/Services/FileSystemAccessServer/ConnectionFromClient.cpp
+++ Userland/Services/FileSystemAccessServer/ConnectionFromClient.cpp
@@ -37,6 +37,7 @@ RefPtr<GUI::Window> ConnectionFromClient::create_dummy_child_window(i32 window_s
     auto window = GUI::Window::construct();
     window->set_opacity(0);
     window->set_frameless(true);
+    window->set_window_mode(GUI::WindowMode::Passive);
     auto rect = GUI::ConnectionToWindowServer::the().get_window_rect_from_client(window_server_client_id, parent_window_id);
     window->set_rect(rect);
     window->show();
