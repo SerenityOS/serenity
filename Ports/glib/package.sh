92c066c1c5ea3ac7f0c2fff4b490c842da6d8492
Kenneth Myhra <kennethmyhra@gmail.com>
2022-08-27 21:48:39 +0200

    Ports/glib: Switch to the offical Release tarball for GLib
    The GLib repository contains several submodules which is part of the
    build process, but the source code for these submodule is not part of
    the Source code tarball generated by Gitlab. Switching to download the
    Release tarball from download.gnome.org solves this issue. It first
    became apparent in version 2.73.x.
    
    Additional information at: https://gitlab.gnome.org/GNOME/glib/-/issues/2716

--- Ports/glib/package.sh
+++ Ports/glib/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='glib'
 version='2.73.0'
-files="https://gitlab.gnome.org/GNOME/glib/-/archive/${version}/glib-${version}.tar.gz glib-${version}.tar.gz 3f573319adbdf572d79255e8bae85c7e2902d1aa6177d2646605a00c0a607eca"
+files="https://download.gnome.org/sources/glib/2.73/glib-${version}.tar.xz glib-${version}.tar.xz 3673f10515f4bcfb9ee2ce0a921a18fa359c36fab388b19819467e7b09506870"
 auth_type='sha256'
 useconfigure='true'
 configopts=("--cross-file" "${SERENITY_BUILD_DIR}/meson-cross-file.txt")
