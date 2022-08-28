71bf0b72ea6dfef147ef64f86bec08a9c430d76a
Luke Wilde <lukew@serenityos.org>
2022-08-28 18:08:12 +0100

    Ladybird: Turn initialize_global_object() into a regular initialize()
    See: https://github.com/SerenityOS/serenity/commit/cfa5885

--- Ladybird/ConsoleClient.cpp
+++ Ladybird/ConsoleClient.cpp
@@ -35,7 +35,7 @@ ConsoleClient::ConsoleClient(JS::Console& console, WeakPtr<JS::Interpreter> inte
     // It gets removed immediately after creating the interpreter in Document::interpreter().
     auto& eso = verify_cast<Web::HTML::EnvironmentSettingsObject>(*m_interpreter->realm().host_defined());
     vm.push_execution_context(eso.realm_execution_context());
-    console_global_object->initialize_global_object(realm);
+    console_global_object->initialize(realm);
     vm.pop_execution_context();
 
     m_console_global_object = JS::make_handle(console_global_object);
