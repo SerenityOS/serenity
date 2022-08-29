bb11e450b7d5a1fff4697f82137d87a8b4dee516
Crax97 <gsolimeno97@gmail.com>
2022-08-29 22:51:49 +0200

    PixelPaint: Execute mousemove_event early return check before
    This is done to allow querying the current active tool inside the
    event_with_pan_and_scale_applied and event_adjusted_for_layer
    functions without risking a null pointer dereference

--- Userland/Applications/PixelPaint/ImageEditor.cpp
+++ Userland/Applications/PixelPaint/ImageEditor.cpp
@@ -335,13 +335,13 @@ void ImageEditor::mousemove_event(GUI::MouseEvent& event)
         return;
     }
 
+    if (!m_active_tool)
+        return;
+
     auto image_event = event_with_pan_and_scale_applied(event);
     if (on_image_mouse_position_change) {
         on_image_mouse_position_change(image_event.position());
     }
-
-    if (!m_active_tool)
-        return;
 
     auto layer_event = m_active_layer ? event_adjusted_for_layer(event, *m_active_layer) : event;
     Tool::MouseEvent tool_event(Tool::MouseEvent::Action::MouseDown, layer_event, image_event, event);
