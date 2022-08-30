54d92c774e14a94b991c41b427dca9241a83da06
Timothy Flynn <trflynn89@pm.me>
2022-08-30 10:18:13 -0400

    LibJS: Default to 0 for DurationFormat's fractionalDigits option
    This is a normative change in the Intl.DurationFormat proposal. See:
    https://github.com/tc39/proposal-intl-duration-format/commit/ac7e184

--- Userland/Libraries/LibJS/Tests/builtins/Intl/DurationFormat/DurationFormat.prototype.resolvedOptions.js
+++ Userland/Libraries/LibJS/Tests/builtins/Intl/DurationFormat/DurationFormat.prototype.resolvedOptions.js
@@ -284,7 +284,7 @@ describe("correct behavior", () => {
 
     test("fractionalDigits", () => {
         const en1 = new Intl.DurationFormat("en");
-        expect(en1.resolvedOptions().fractionalDigits).toBeUndefined();
+        expect(en1.resolvedOptions().fractionalDigits).toBe(0);
 
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].forEach(fractionalDigits => {
             const en2 = new Intl.DurationFormat("en", { fractionalDigits: fractionalDigits });
