9d42f997243b91d9f6a65553e9e12d41c8e90d76
Karol Kosek <krkk@serenityos.org>
2022-09-01 14:16:32 +0200

    Kernel: Allow to remove files from sticky directory if user owns the dir
    It's what the Linux chmod(1) manpage says (in the 'Restricted Deletion
    Flag or Sticky Bit' section), and it just makes sense to me. :^)

--- Kernel/FileSystem/VirtualFileSystem.cpp
+++ Kernel/FileSystem/VirtualFileSystem.cpp
@@ -523,7 +523,7 @@ ErrorOr<void> VirtualFileSystem::rename(Credentials const& credentials, StringVi
         return EACCES;
 
     if (old_parent_inode.metadata().is_sticky()) {
-        if (!credentials.is_superuser() && old_inode.metadata().uid != credentials.euid())
+        if (!credentials.is_superuser() && old_parent_inode.metadata().uid != credentials.euid() && old_inode.metadata().uid != credentials.euid())
             return EACCES;
     }
 
@@ -671,7 +671,7 @@ ErrorOr<void> VirtualFileSystem::unlink(Credentials const& credentials, StringVi
         return EACCES;
 
     if (parent_inode.metadata().is_sticky()) {
-        if (!credentials.is_superuser() && inode.metadata().uid != credentials.euid())
+        if (!credentials.is_superuser() && parent_inode.metadata().uid != credentials.euid() && inode.metadata().uid != credentials.euid())
             return EACCES;
     }
 
@@ -731,7 +731,7 @@ ErrorOr<void> VirtualFileSystem::rmdir(Credentials const& credentials, StringVie
         return EACCES;
 
     if (parent_metadata.is_sticky()) {
-        if (!credentials.is_superuser() && inode.metadata().uid != credentials.euid())
+        if (!credentials.is_superuser() && parent_inode.metadata().uid != credentials.euid() && inode.metadata().uid != credentials.euid())
             return EACCES;
     }
 
