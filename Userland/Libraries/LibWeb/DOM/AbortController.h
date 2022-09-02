59634bb11bbe4a0d23b7871298550f2629f82da1
Andreas Kling <kling@serenityos.org>
2022-09-02 14:59:27 +0200

    LibWeb: Make AbortController GC-allocated

--- Userland/Libraries/LibWeb/DOM/AbortController.h
+++ Userland/Libraries/LibWeb/DOM/AbortController.h
@@ -6,29 +6,19 @@
 
 #pragma once
 
-#include <AK/RefCounted.h>
-#include <AK/Weakable.h>
-#include <LibWeb/Bindings/Wrappable.h>
+#include <LibWeb/Bindings/PlatformObject.h>
 #include <LibWeb/DOM/AbortSignal.h>
-#include <LibWeb/Forward.h>
-#include <LibWeb/HTML/Window.h>
 
 namespace Web::DOM {
 
 // https://dom.spec.whatwg.org/#abortcontroller
-class AbortController final
-    : public RefCounted<AbortController>
-    , public Weakable<AbortController>
-    , public Bindings::Wrappable {
+class AbortController final : public Bindings::PlatformObject {
+    WEB_PLATFORM_OBJECT(AbortController, Bindings::PlatformObject);
+
 public:
-    using WrapperType = Bindings::AbortControllerWrapper;
+    static JS::NonnullGCPtr<AbortController> create_with_global_object(HTML::Window&);
 
-    static NonnullRefPtr<AbortController> create_with_global_object(HTML::Window& window)
-    {
-        return adopt_ref(*new AbortController(window));
-    }
-
-    virtual ~AbortController() override = default;
+    virtual ~AbortController() override;
 
     // https://dom.spec.whatwg.org/#dom-abortcontroller-signal
     JS::NonnullGCPtr<AbortSignal> signal() const { return *m_signal; }
@@ -36,10 +26,14 @@ public:
     void abort(JS::Value reason);
 
 private:
-    explicit AbortController(HTML::Window&);
+    AbortController(HTML::Window&, JS::NonnullGCPtr<AbortSignal>);
+
+    virtual void visit_edges(Cell::Visitor&) override;
 
     // https://dom.spec.whatwg.org/#abortcontroller-signal
-    JS::Handle<AbortSignal> m_signal;
+    JS::NonnullGCPtr<AbortSignal> m_signal;
 };
 
 }
+
+WRAPPER_HACK(AbortController, Web::DOM)
