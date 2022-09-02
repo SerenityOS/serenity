0d2fee351ad6e8032bb5c97ac0a481f1f5d05112
Andreas Kling <kling@serenityos.org>
2022-09-02 23:17:45 +0200

    LibWeb: Make CanvasGradient GC-allocated

--- Userland/Libraries/LibWeb/HTML/Canvas/CanvasFillStrokeStyles.h
+++ Userland/Libraries/LibWeb/HTML/Canvas/CanvasFillStrokeStyles.h
@@ -42,19 +42,22 @@ public:
         return my_drawing_state().stroke_style.to_string();
     }
 
-    NonnullRefPtr<CanvasGradient> create_radial_gradient(double x0, double y0, double r0, double x1, double y1, double r1)
+    JS::NonnullGCPtr<CanvasGradient> create_radial_gradient(double x0, double y0, double r0, double x1, double y1, double r1)
     {
-        return CanvasGradient::create_radial(x0, y0, r0, x1, y1, r1);
+        auto& window = static_cast<IncludingClass&>(*this).global_object();
+        return CanvasGradient::create_radial(window, x0, y0, r0, x1, y1, r1);
     }
 
-    NonnullRefPtr<CanvasGradient> create_linear_gradient(double x0, double y0, double x1, double y1)
+    JS::NonnullGCPtr<CanvasGradient> create_linear_gradient(double x0, double y0, double x1, double y1)
     {
-        return CanvasGradient::create_linear(x0, y0, x1, y1);
+        auto& window = static_cast<IncludingClass&>(*this).global_object();
+        return CanvasGradient::create_linear(window, x0, y0, x1, y1);
     }
 
-    NonnullRefPtr<CanvasGradient> create_conic_gradient(double start_angle, double x, double y)
+    JS::NonnullGCPtr<CanvasGradient> create_conic_gradient(double start_angle, double x, double y)
     {
-        return CanvasGradient::create_conic(start_angle, x, y);
+        auto& window = static_cast<IncludingClass&>(*this).global_object();
+        return CanvasGradient::create_conic(window, start_angle, x, y);
     }
 
 protected:
