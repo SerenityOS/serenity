badda0c948186927691af5fe7e7c904e436617bf
Andreas Kling <kling@serenityos.org>
2022-09-02 14:48:36 +0200

    Fuzzers: Fix FuzzCSSParser build

--- Meta/Lagom/Fuzzers/FuzzCSSParser.cpp
+++ Meta/Lagom/Fuzzers/FuzzCSSParser.cpp
@@ -7,11 +7,15 @@
 #include <LibCore/EventLoop.h>
 #include <LibWeb/CSS/Parser/Parser.h>
 #include <LibWeb/DOM/Document.h>
+#include <LibWeb/HTML/Window.h>
 
 extern "C" int LLVMFuzzerTestOneInput(uint8_t const* data, size_t size)
 {
     Core::EventLoop loop;
-    auto document = Web::DOM::Document::create();
+    auto vm = JS::VM::create();
+    auto realm = JS::Realm::create(*vm);
+    auto window = Web::HTML::Window::create(*realm);
+    auto document = Web::DOM::Document::create(*window);
     (void)Web::parse_css_stylesheet(Web::CSS::Parser::ParsingContext(document), { data, size });
     return 0;
 }
