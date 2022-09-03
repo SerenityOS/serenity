18e9e2a8e185d4072528ae60780b193e40ecb155
EWouters <6179932+EWouters@users.noreply.github.com>
2022-09-03 20:00:39 +0200

    Ports/ccache: Update ccache to version 4.6.3

--- Ports/ccache/package.sh
+++ Ports/ccache/package.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=ccache
-version=4.6
-useconfigure=true
-files="https://github.com/ccache/ccache/releases/download/v${version}/ccache-${version}.tar.gz ccache-$version.tar.gz 73a1767ac6b7c0404a1a55f761a746d338e702883c7137fbf587023062258625"
-auth_type=sha256
-depends=("zstd")
-configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt" "-DCMAKE_BUILD_TYPE=Release" "-DREDIS_STORAGE_BACKEND=OFF" "-GNinja")
+port='ccache'
+version='4.6.3'
+useconfigure='true'
+files="https://github.com/ccache/ccache/releases/download/v${version}/ccache-${version}.tar.gz ccache-$version.tar.gz f46ba3706ad80c30d4d5874dee2bf9227a7fcd0ccaac31b51919a3053d84bd05"
+auth_type='sha256'
+depends=('zstd')
+configopts=(
+    "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
+    "-DCMAKE_BUILD_TYPE=Release"
+    "-DREDIS_STORAGE_BACKEND=OFF"
+    "-GNinja"
+)
 
 configure() {
     run cmake "${configopts[@]}" .
