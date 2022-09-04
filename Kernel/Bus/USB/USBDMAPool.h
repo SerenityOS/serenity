ef81ee8ba7356e3bbacdf9a62fba03c413052c77
b14ckcat <b14ckcat@protonmail.com>
2022-09-04 15:47:22 -0400

    Kernel/USB: Extend DMA pool class
    Allow for allocation of DMA buffer pools based upon runtime-dependent
    buffer size information in a type-agnostic way.

--- Kernel/Bus/USB/USBDMAPool.h
+++ Kernel/Bus/USB/USBDMAPool.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2021, Jesse Buhagiar <jooster669@gmail.com>
+ * Copyright (c) 2022, blackcat <b14ckcat@protonmail.com>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -9,12 +10,19 @@
 #include <AK/NonnullOwnPtr.h>
 #include <AK/OwnPtr.h>
 #include <AK/Stack.h>
+#include <AK/Vector.h>
 #include <Kernel/Locking/Spinlock.h>
 #include <Kernel/Memory/MemoryManager.h>
 #include <Kernel/Memory/Region.h>
 #include <Kernel/StdLib.h>
 
 namespace Kernel::USB {
+
+struct USBDMAHandle {
+    VirtualAddress vaddr;
+    PhysicalAddress paddr;
+    u16 size;
+};
 
 // This pool is bound by PAGE_SIZE / sizeof(T). The underlying allocation for the pointers
 // is AK::Stack. As such, we never dynamically allocate any memory past the amount
@@ -31,7 +39,7 @@ class USBDMAPool {
 public:
     static ErrorOr<NonnullOwnPtr<USBDMAPool<T>>> try_create(StringView name)
     {
-        auto pool_memory_block = TRY(MM.allocate_kernel_region(PAGE_SIZE, "USB DMA Pool"sv, Memory::Region::Access::ReadWrite));
+        auto pool_memory_block = TRY(MM.allocate_dma_buffer_page("USB DMA Pool"sv, Memory::Region::Access::ReadWrite));
         return adopt_nonnull_own_or_enomem(new (nothrow) USBDMAPool(move(pool_memory_block), name));
     }
 
@@ -84,6 +92,76 @@ private:
     StringView m_pool_name;                               // Name of this pool
     NonnullOwnPtr<Memory::Region> m_pool_region;          // Memory region buffers are allocated from
     Stack<T*, PAGE_SIZE / sizeof(T)> m_free_buffer_stack; // Stack of currently free buffer pointers
+    Spinlock m_pool_lock;
+};
+
+// For allocating a pool of generic memory buffers for use with DMA, where the size is known only at runtime
+template<>
+class USBDMAPool<USBDMAHandle> {
+    AK_MAKE_NONCOPYABLE(USBDMAPool);
+    AK_MAKE_NONMOVABLE(USBDMAPool);
+
+public:
+    static ErrorOr<NonnullOwnPtr<USBDMAPool<USBDMAHandle>>> try_create(StringView name, u16 buffer_size, u16 num_buffers)
+    {
+        u32 num_pool_pages = ((num_buffers * buffer_size) / PAGE_SIZE) + (((num_buffers * buffer_size) % PAGE_SIZE) != 0);
+        auto pool_memory_block = TRY(MM.allocate_dma_buffer_pages(PAGE_SIZE * num_pool_pages, "USB DMA Pool"sv, Memory::Region::Access::ReadWrite));
+        u32 num_handle_pages = ((num_buffers * sizeof(USBDMAHandle)) / PAGE_SIZE) + (((num_buffers * sizeof(USBDMAHandle)) % PAGE_SIZE) != 0);
+        auto handle_memory_block = TRY(MM.allocate_dma_buffer_pages(PAGE_SIZE * num_handle_pages, "USB DMA Handles"sv, Memory::Region::Access::ReadWrite));
+        return adopt_nonnull_own_or_enomem(new (nothrow) USBDMAPool(move(pool_memory_block), move(handle_memory_block), name, buffer_size, num_buffers));
+    }
+
+    ~USBDMAPool() = default;
+
+    [[nodiscard]] USBDMAHandle* try_take_free_buffer()
+    {
+        SpinlockLocker locker(m_pool_lock);
+
+        // We're out of buffers!
+        if (m_dma_handles.is_empty())
+            return nullptr;
+
+        dbgln_if(USB_DEBUG, "Got a free DMA handle @ {} from pool {}", m_dma_handles.last(), m_pool_name);
+        return m_dma_handles.take_last();
+    }
+
+    void release_to_pool(USBDMAHandle* handle)
+    {
+        SpinlockLocker locker(m_pool_lock);
+        dbgln_if(USB_DEBUG, "Returning handle @ {} to pool {}", handle, m_pool_name);
+        m_dma_handles.append(handle);
+    }
+
+    void print_pool_information()
+    {
+        dbgln("Pool {} allocated @ {}", m_pool_name, m_pool_region->physical_page(0)->paddr());
+    }
+
+private:
+    USBDMAPool(NonnullOwnPtr<Memory::Region> pool_memory_block, NonnullOwnPtr<Memory::Region> handle_memory_block, StringView name, u16 buffer_size, u16 num_buffers)
+        : m_pool_name(name)
+        , m_pool_region(move(pool_memory_block))
+        , m_handle_region(move(handle_memory_block))
+        , m_buffer_size(buffer_size)
+        , m_num_buffers(num_buffers)
+        , m_pool_lock(LockRank::None)
+    {
+        for (size_t i = 0; i < m_num_buffers; i++) {
+            VirtualAddress vaddr { m_pool_region->vaddr().get() + (m_buffer_size * i) };
+            auto page_idx = i / PAGE_SIZE;
+            PhysicalAddress paddr { m_pool_region->physical_page(page_idx)->paddr().offset(m_buffer_size * i) };
+            auto handle_addr = reinterpret_cast<void*>(m_handle_region->vaddr().get() + (i * sizeof(USBDMAHandle)));
+            USBDMAHandle* dma_handle = new (handle_addr) USBDMAHandle { vaddr, paddr, m_buffer_size };
+            m_dma_handles.append(dma_handle);
+        }
+    }
+
+    StringView m_pool_name;                        // Name of this pool
+    NonnullOwnPtr<Memory::Region> m_pool_region;   // Memory region buffers are allocated from
+    NonnullOwnPtr<Memory::Region> m_handle_region; // Memory region handles are stored in
+    u16 const m_buffer_size;                       // How big each DMA buffer is
+    u32 const m_num_buffers;                       // Number of DMA buffers
+    Vector<USBDMAHandle*> m_dma_handles;
     Spinlock m_pool_lock;
 };
 
