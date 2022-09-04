915a240944390fffb7a69793ab8e7fe9d9725cc0
Andreas Kling <kling@serenityos.org>
2022-09-04 13:26:36 +0200

    LibWeb: Make IdleDeadline GC-allocated

--- Userland/Libraries/LibWeb/RequestIdleCallback/IdleDeadline.h
+++ Userland/Libraries/LibWeb/RequestIdleCallback/IdleDeadline.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2021-2022, Andreas Kling <kling@serenityos.org>
  * Copyright (c) 2022, the SerenityOS developers.
  *
  * SPDX-License-Identifier: BSD-2-Clause
@@ -8,27 +8,26 @@
 #pragma once
 
 #include <AK/StdLibExtras.h>
-#include <LibWeb/Bindings/Wrappable.h>
+#include <LibWeb/Bindings/PlatformObject.h>
 
 namespace Web::RequestIdleCallback {
 
-class IdleDeadline final
-    : public RefCounted<IdleDeadline>
-    , public Bindings::Wrappable {
-public:
-    using WrapperType = Bindings::IdleDeadlineWrapper;
-    using AllowOwnPtr = TrueType;
+class IdleDeadline final : public Bindings::PlatformObject {
+    WEB_PLATFORM_OBJECT(IdleDeadline, Bindings::PlatformObject);
 
-    static NonnullRefPtr<IdleDeadline> create(bool did_timeout = false);
+public:
+    static JS::NonnullGCPtr<IdleDeadline> create(HTML::Window&, bool did_timeout = false);
     virtual ~IdleDeadline() override;
 
     double time_remaining() const;
     bool did_timeout() const { return m_did_timeout; }
 
 private:
-    IdleDeadline(bool did_timeout);
+    IdleDeadline(HTML::Window&, bool did_timeout);
 
     bool m_did_timeout { false };
 };
 
 }
+
+WRAPPER_HACK(IdleDeadline, Web::RequestIdleCallback)
