660d2b53b1206e868d5470eee80b5e62d7e30da7
Ali Mohammad Pur <ali.mpfard@gmail.com>
2022-09-06 23:55:09 +0430

    LibRegex: Account for eof after \<x> when 'x' leads to legacy behaviour

--- Tests/LibRegex/Regex.cpp
+++ Tests/LibRegex/Regex.cpp
@@ -603,6 +603,7 @@ TEST_CASE(ECMA262_parse)
         { "(?<𝓑𝓻𝓸𝔀𝓷>a)"sv },
         { "((?=lg)?[vl]k\\-?\\d{3}) bui| 3\\.[-\\w; ]{10}lg?-([06cv9]{3,4})"sv, regex::Error::NoError, ECMAScriptFlags::BrowserExtended }, // #12373, quantifiable assertions.
         { parse_test_case_long_disjunction_chain.view() },                                                                                 // A whole lot of disjunctions, should not overflow the stack.
+        { "(\"|')(?:(?!\\2)[^\\\\\\r\\n]|\\\\.)*\\2"sv, regex::Error::NoError, ECMAScriptFlags::BrowserExtended },                         // LegacyOctalEscapeSequence should not consume too many chars (and should not crash)
     };
 
     for (auto& test : tests) {
