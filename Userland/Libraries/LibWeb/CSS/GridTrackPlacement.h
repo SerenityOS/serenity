2034cdcc896dfb65b9bd2a2f2e3d85bc052c87ad
martinfalisse <martinmotteditfalisse@gmail.com>
2022-09-07 15:03:23 +0200

    LibWeb: Allow having auto GridTrackPlacements
    For grid-row-start, grid-column-end and similar values it is possible to
    have auto values (nothing was specified, for example).

--- Userland/Libraries/LibWeb/CSS/GridTrackPlacement.h
+++ Userland/Libraries/LibWeb/CSS/GridTrackPlacement.h
@@ -18,11 +18,21 @@ public:
 
     static GridTrackPlacement make_auto() { return GridTrackPlacement(); };
 
-    void set_position(int position) { m_position = position; }
+    void set_position(int position)
+    {
+        m_is_auto = false;
+        m_position = position;
+    }
     int position() const { return m_position; }
 
-    void set_has_span(bool has_span) { m_has_span = has_span; }
+    void set_has_span(bool has_span)
+    {
+        VERIFY(!m_is_auto);
+        m_has_span = has_span;
+    }
     bool has_span() const { return m_has_span; }
+
+    bool is_auto() const { return m_is_auto; }
 
     String to_string() const;
     bool operator==(GridTrackPlacement const& other) const
@@ -31,6 +41,7 @@ public:
     }
 
 private:
+    bool m_is_auto { false };
     int m_position { 0 };
     bool m_has_span { false };
 };
