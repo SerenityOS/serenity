dd510b24c9762a192d14f5c1ae741ef3df1b225c
djwisdom <djwisdom@gmail.com>
2022-09-09 00:20:26 +0800

    Ports: Update serenity-theming use latest commit 3b4e1f2

--- Ports/serenity-theming/package.sh
+++ Ports/serenity-theming/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=serenity-theming
-version=624c29a22d11fa01ea6729afbd96375d809cc99b
+version=3b4e1f2434e10b63f7b4cc6dc520e69de32671b2
 workdir="theming-${version}"
-files="https://github.com/SerenityOS/theming/archive/${version}.zip serenity-theming-${version}.zip a043589b82b52dbc7ad8f819f21955e51b4358196ee9efc6942720dd912662b7"
+files="https://github.com/SerenityOS/theming/archive/${version}.zip serenity-theming-${version}.zip 9c5cc4600f39d7f19bb1f3a7575bbc844d005aa8901e28073bc5830964542fba"
 auth_type="sha256"
 
 build() {
