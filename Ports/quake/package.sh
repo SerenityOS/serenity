933930e163d9cd764fb63402d8d3184b36cc1537
Jelle Raaijmakers <jelle@gmta.nl>
2022-09-08 11:49:43 +0200

    Ports: Build Quake in parallel
    Instead of overwriting the existing `-j` makeopt, we only append
    options. This brings the build time for the Quake port down from 24.3s
    to 4.4s on my machine.

--- Ports/quake/package.sh
+++ Ports/quake/package.sh
@@ -4,7 +4,7 @@ version=0.65
 workdir=SerenityQuake-master
 useconfigure=false
 files="https://github.com/SerenityPorts/SerenityQuake/archive/master.tar.gz quake.tar.gz"
-makeopts=("V=1" "SYMBOLS_ON=Y")
+makeopts+=("V=1" "SYMBOLS_ON=Y")
 depends=("SDL2")
 launcher_name=Quake
 launcher_category=Games
