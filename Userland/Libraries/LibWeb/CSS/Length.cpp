9ff6c1e692ed1b21ff0324e3ceaf9fca0ed670a4
networkException <git@nwex.de>
2022-09-10 12:43:05 +0200

    LibWeb: Avoid copying viewport rect when converting length to pixels
    See https://github.com/SerenityOS/serenity/pull/3433#discussion_r484570948

--- Userland/Libraries/LibWeb/CSS/Length.cpp
+++ Userland/Libraries/LibWeb/CSS/Length.cpp
@@ -104,7 +104,7 @@ float Length::to_px(Layout::Node const& layout_node) const
 
     if (!layout_node.document().browsing_context())
         return 0;
-    auto viewport_rect = layout_node.document().browsing_context()->viewport_rect();
+    auto const& viewport_rect = layout_node.document().browsing_context()->viewport_rect();
     auto* root_element = layout_node.document().document_element();
     if (!root_element || !root_element->layout_node())
         return 0;
