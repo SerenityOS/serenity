f45238db0f47250cd2e0af118f3b40087b292dd7
Tim Schumacher <timschumi@gmx.de>
2022-09-11 23:19:41 +0200

    Toolchain: Update to LLVM 15.0.0

--- Ports/llvm/package.sh
+++ Ports/llvm/package.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=llvm
 useconfigure=true
-version=14.0.1
+version=15.0.0
 workdir=llvm-project-${version}.src
 configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt")
-files="https://github.com/llvm/llvm-project/releases/download/llvmorg-${version}/llvm-project-${version}.src.tar.xz llvm-project-${version}.src.tar.xz 1a3c2e57916c5a70153aaf0a0e6f1230d6368b9e0f4d04dcb9e039a31b1cd4e6"
+files="https://github.com/llvm/llvm-project/releases/download/llvmorg-${version}/llvm-project-${version}.src.tar.xz llvm-project-${version}.src.tar.xz caaf8100365b6ebafc39fea803e902ca3ff38b4d5327b9927097808d32964db7"
 auth_type=sha256
 depends=("ncurses" "zlib")
 
