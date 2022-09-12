4fed262e0652bea5a56522ea84a93fbdf51a6f08
Timothy Flynn <trflynn89@pm.me>
2022-09-12 15:35:54 -0400

    Meta: Add Lagom option to disable building LibWeb
    Compiling LibWeb for Lagom can add quite a bit of compile time. Add a
    CMake option to disable it locally.

--- Meta/CMake/lagom_options.cmake
+++ Meta/CMake/lagom_options.cmake
@@ -11,3 +11,4 @@ serenity_option(ENABLE_FUZZERS_LIBFUZZER OFF CACHE BOOL "Build fuzzers using Cla
 serenity_option(ENABLE_FUZZERS_OSSFUZZ OFF CACHE BOOL "Build OSS-Fuzz compatible fuzzers")
 serenity_option(BUILD_LAGOM OFF CACHE BOOL "Build parts of the system targeting the host OS for fuzzing/testing")
 serenity_option(ENABLE_LAGOM_CCACHE ON CACHE BOOL "Enable ccache for Lagom builds")
+serenity_option(ENABLE_LAGOM_LIBWEB ON CACHE BOOL "Enable compiling LibWeb for Lagom builds")
