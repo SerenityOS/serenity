SerenityOS/serenity

commit 280694bb46202c4a4a704f227d6141dc483bbeb4

Kernel: Update atime/ctime/mtime timestamps atomically
Instead of having three separate APIs (one for each timestamp),
there's now only Inode::update_timestamps() and it takes 3x optional
timestamps. The non-empty timestamps are updated while holding the inode
mutex, and the outside world no longer has to look at intermediate
timestamp states.

Kernel/FileSystem/VirtualFileSystem.cpp
@@ -213,8 +213,7 @@ ErrorOr<void> VirtualFileSystem::utime(Credentials const& credentials, StringVie
     if (custody->is_readonly())
         return EROFS;
 
-    TRY(inode.set_atime(atime));
-    TRY(inode.set_mtime(mtime));
+    TRY(inode.update_timestamps(atime, {}, mtime));
     return {};
 }
 
@@ -228,10 +227,10 @@ ErrorOr<void> VirtualFileSystem::utimensat(Credentials const& credentials, Strin
         return EROFS;
 
     // NOTE: A standard ext2 inode cannot store nanosecond timestamps.
-    if (atime.tv_nsec != UTIME_OMIT)
-        TRY(inode.set_atime(atime.tv_sec));
-    if (mtime.tv_nsec != UTIME_OMIT)
-        TRY(inode.set_mtime(mtime.tv_sec));
+    TRY(inode.update_timestamps(
+        (atime.tv_nsec != UTIME_OMIT) ? atime.tv_nsec : Optional<time_t> {},
+        {},
+        (mtime.tv_nsec != UTIME_OMIT) ? mtime.tv_nsec : Optional<time_t> {}));
 
     return {};
 }
@@ -321,7 +320,7 @@ ErrorOr<NonnullLockRefPtr<OpenFileDescription>> VirtualFileSystem::open(Credenti
 
     if (should_truncate_file) {
         TRY(inode.truncate(0));
-        TRY(inode.set_mtime(kgettimeofday().to_truncated_seconds()));
+        TRY(inode.update_timestamps({}, {}, kgettimeofday().to_truncated_seconds()));
     }
     auto description = TRY(OpenFileDescription::try_create(custody));
     description->set_rw_mode(options);

Kernel/FileSystem/Ext2FileSystem.cpp
@@ -1537,34 +1537,23 @@ ErrorOr<NonnullLockRefPtr<Inode>> Ext2FSInode::lookup(StringView name)
     return fs().get_inode({ fsid(), inode_index });
 }
 
-ErrorOr<void> Ext2FSInode::set_atime(time_t t)
+ErrorOr<void> Ext2FSInode::update_timestamps(Optional<time_t> atime, Optional<time_t> ctime, Optional<time_t> mtime)
 {
     MutexLocker locker(m_inode_lock);
     if (fs().is_readonly())
         return EROFS;
-    if (t > INT32_MAX)
+    if (atime.value_or(0) > INT32_MAX)
         return EINVAL;
-    m_raw_inode.i_atime = t;
-    set_metadata_dirty(true);
-    return {};
-}
-
-ErrorOr<void> Ext2FSInode::set_ctime(time_t t)
-{
-    MutexLocker locker(m_inode_lock);
-    if (fs().is_readonly())
-        return EROFS;
-    m_raw_inode.i_ctime = t;
-    set_metadata_dirty(true);
-    return {};
-}
-
-ErrorOr<void> Ext2FSInode::set_mtime(time_t t)
-{
-    MutexLocker locker(m_inode_lock);
-    if (fs().is_readonly())
-        return EROFS;
-    m_raw_inode.i_mtime = t;
+    if (ctime.value_or(0) > INT32_MAX)
+        return EINVAL;
+    if (mtime.value_or(0) > INT32_MAX)
+        return EINVAL;
+    if (atime.has_value())
+        m_raw_inode.i_atime = atime.value();
+    if (ctime.has_value())
+        m_raw_inode.i_ctime = ctime.value();
+    if (mtime.has_value())
+        m_raw_inode.i_mtime = mtime.value();
     set_metadata_dirty(true);
     return {};
 }

Kernel/FileSystem/Ext2FileSystem.h
@@ -44,9 +44,7 @@ private:
     virtual ErrorOr<NonnullLockRefPtr<Inode>> create_child(StringView name, mode_t, dev_t, UserID, GroupID) override;
     virtual ErrorOr<void> add_child(Inode& child, StringView name, mode_t) override;
     virtual ErrorOr<void> remove_child(StringView name) override;
-    virtual ErrorOr<void> set_atime(time_t) override;
-    virtual ErrorOr<void> set_ctime(time_t) override;
-    virtual ErrorOr<void> set_mtime(time_t) override;
+    virtual ErrorOr<void> update_timestamps(Optional<time_t> atime, Optional<time_t> ctime, Optional<time_t> mtime) override;
     virtual ErrorOr<void> increment_link_count() override;
     virtual ErrorOr<void> decrement_link_count() override;
     virtual ErrorOr<void> chmod(mode_t) override;

Kernel/FileSystem/ISO9660FileSystem.cpp
@@ -528,17 +528,7 @@ ErrorOr<void> ISO9660Inode::truncate(u64)
     return EROFS;
 }
 
-ErrorOr<void> ISO9660Inode::set_atime(time_t)
-{
-    return EROFS;
-}
-
-ErrorOr<void> ISO9660Inode::set_ctime(time_t)
-{
-    return EROFS;
-}
-
-ErrorOr<void> ISO9660Inode::set_mtime(time_t)
+ErrorOr<void> ISO9660Inode::update_timestamps(Optional<time_t>, Optional<time_t>, Optional<time_t>)
 {
     return EROFS;
 }

Kernel/FileSystem/ISO9660FileSystem.h
@@ -359,9 +359,7 @@ public:
     virtual ErrorOr<void> chmod(mode_t) override;
     virtual ErrorOr<void> chown(UserID, GroupID) override;
     virtual ErrorOr<void> truncate(u64) override;
-    virtual ErrorOr<void> set_atime(time_t) override;
-    virtual ErrorOr<void> set_ctime(time_t) override;
-    virtual ErrorOr<void> set_mtime(time_t) override;
+    virtual ErrorOr<void> update_timestamps(Optional<time_t> atime, Optional<time_t> ctime, Optional<time_t> mtime) override;
 
 private:
     // HACK: The base ISO 9660 standard says the maximum filename length is 37

Kernel/FileSystem/Inode.cpp
@@ -106,17 +106,7 @@ void Inode::will_be_destroyed()
         (void)flush_metadata();
 }
 
-ErrorOr<void> Inode::set_atime(time_t)
-{
-    return ENOTIMPL;
-}
-
-ErrorOr<void> Inode::set_ctime(time_t)
-{
-    return ENOTIMPL;
-}
-
-ErrorOr<void> Inode::set_mtime(time_t)
+ErrorOr<void> Inode::update_timestamps([[maybe_unused]] Optional<time_t> atime, [[maybe_unused]] Optional<time_t> ctime, [[maybe_unused]] Optional<time_t> mtime)
 {
     return ENOTIMPL;
 }
@@ -236,8 +226,7 @@ void Inode::did_modify_contents()
     // FIXME: What happens if this fails?
     //        ENOTIMPL would be a meaningless error to return here
     auto time = kgettimeofday().to_truncated_seconds();
-    (void)set_mtime(time);
-    (void)set_ctime(time);
+    (void)update_timestamps({}, time, time);
 
     m_watchers.for_each([&](auto& watcher) {
         watcher->notify_inode_event({}, identifier(), InodeWatcherEvent::Type::ContentModified);

Kernel/FileSystem/Inode.h
@@ -80,9 +80,7 @@ public:
 
     bool is_metadata_dirty() const { return m_metadata_dirty; }
 
-    virtual ErrorOr<void> set_atime(time_t);
-    virtual ErrorOr<void> set_ctime(time_t);
-    virtual ErrorOr<void> set_mtime(time_t);
+    virtual ErrorOr<void> update_timestamps(Optional<time_t> atime, Optional<time_t> ctime, Optional<time_t> mtime);
     virtual ErrorOr<void> increment_link_count();
     virtual ErrorOr<void> decrement_link_count();
 

Kernel/FileSystem/InodeFile.cpp
@@ -49,7 +49,7 @@ ErrorOr<size_t> InodeFile::write(OpenFileDescription& description, u64 offset, U
         nwritten = TRY(m_inode->write_bytes(offset, count, data, &description));
     }
     if (nwritten > 0) {
-        auto mtime_result = m_inode->set_mtime(kgettimeofday().to_truncated_seconds());
+        auto mtime_result = m_inode->update_timestamps({}, {}, kgettimeofday().to_truncated_seconds());
         Thread::current()->did_file_write(nwritten);
         evaluate_block_conditions();
         if (mtime_result.is_error())
@@ -106,7 +106,7 @@ ErrorOr<NonnullOwnPtr<KString>> InodeFile::pseudo_path(OpenFileDescription const
 ErrorOr<void> InodeFile::truncate(u64 size)
 {
     TRY(m_inode->truncate(size));
-    TRY(m_inode->set_mtime(kgettimeofday().to_truncated_seconds()));
+    TRY(m_inode->update_timestamps({}, {}, kgettimeofday().to_truncated_seconds()));
     return {};
 }
 

commit 2cc947ede46824e8c57821769b7276856a459e83

Kernel: Use correct timestamp in sys$utimens()
We were mixing up the nanosecond and second parts of the timestamps.

Regressed in 280694bb46202c4a4a704f227d6141dc483bbeb4.

Kernel/FileSystem/VirtualFileSystem.cpp
@@ -228,9 +228,9 @@ ErrorOr<void> VirtualFileSystem::utimensat(Credentials const& credentials, Strin
 
     // NOTE: A standard ext2 inode cannot store nanosecond timestamps.
     TRY(inode.update_timestamps(
-        (atime.tv_nsec != UTIME_OMIT) ? atime.tv_nsec : Optional<time_t> {},
+        (atime.tv_nsec != UTIME_OMIT) ? atime.tv_sec : Optional<time_t> {},
         {},
-        (mtime.tv_nsec != UTIME_OMIT) ? mtime.tv_nsec : Optional<time_t> {}));
+        (mtime.tv_nsec != UTIME_OMIT) ? mtime.tv_sec : Optional<time_t> {}));
 
     return {};
 }