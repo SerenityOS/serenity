4f4e0d8166ae3170a882c32fc9f087aa4b54c40d
Sam Atkins <atkinssj@serenityos.org>
2022-09-14 17:00:12 +0100

    nproc: Port to Core::Stream

--- Userland/Utilities/nproc.cpp
+++ Userland/Utilities/nproc.cpp
@@ -5,17 +5,17 @@
  */
 
 #include <AK/JsonObject.h>
-#include <LibCore/File.h>
+#include <LibCore/Stream.h>
 #include <LibCore/System.h>
 #include <LibMain/Main.h>
 
 ErrorOr<int> serenity_main(Main::Arguments)
 {
     TRY(Core::System::pledge("stdio rpath"));
-    auto file = TRY(Core::File::open("/sys/kernel/cpuinfo", Core::OpenMode::ReadOnly));
+    auto file = TRY(Core::Stream::File::open("/sys/kernel/cpuinfo"sv, Core::Stream::OpenMode::Read));
 
-    auto buffer = file->read_all();
-    auto json = TRY(JsonValue::from_string({ buffer }));
+    auto buffer = TRY(file->read_all());
+    auto json = TRY(JsonValue::from_string(buffer));
     auto const& cpuinfo_array = json.as_array();
     outln("{}", cpuinfo_array.size());
 
