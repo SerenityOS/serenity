6150d29bcc5f14cdd6db097e741ebf21a49ab487
Sam Atkins <atkinssj@serenityos.org>
2022-09-14 11:24:13 +0100

    Utilities: Port cpp utilities to Core::Stream

--- Userland/Utilities/cpp-preprocessor.cpp
+++ Userland/Utilities/cpp-preprocessor.cpp
@@ -1,36 +1,33 @@
 /*
- * Copyright (c) 2021, the SerenityOS developers.
+ * Copyright (c) 2021-2022, the SerenityOS developers.
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #include <AK/LexicalPath.h>
 #include <LibCore/ArgsParser.h>
-#include <LibCore/File.h>
+#include <LibCore/Stream.h>
 #include <LibCpp/Preprocessor.h>
 #include <LibMain/Main.h>
 
 ErrorOr<int> serenity_main(Main::Arguments arguments)
 {
     Core::ArgsParser args_parser;
-    char const* path = nullptr;
+    StringView path;
     bool print_definitions = false;
     args_parser.add_positional_argument(path, "File", "file", Core::ArgsParser::Required::Yes);
     args_parser.add_option(print_definitions, "Print preprocessor definitions", "definitions", 'D');
     args_parser.parse(arguments);
-    auto file = Core::File::construct(path);
-    if (!file->open(Core::OpenMode::ReadOnly)) {
-        warnln("Failed to open {}: {}", path, file->error_string());
-        exit(1);
-    }
-    auto content = file->read_all();
+
+    auto file = TRY(Core::Stream::File::open(path, Core::Stream::OpenMode::Read));
+    auto content = TRY(file->read_all());
     String name = LexicalPath::basename(path);
     Cpp::Preprocessor cpp(name, StringView { content });
     auto tokens = cpp.process_and_lex();
 
     if (print_definitions) {
         outln("Definitions:");
-        for (auto& definition : cpp.definitions()) {
+        for (auto const& definition : cpp.definitions()) {
             if (definition.value.parameters.is_empty())
                 outln("{}: {}", definition.key, definition.value.value);
             else
