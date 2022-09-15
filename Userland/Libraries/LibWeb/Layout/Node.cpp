ec4de1e07d28cb6c41fda8c6e27dae98cf04e1b6
MacDue <macdue@dueutil.tech>
2022-09-15 08:31:19 +0100

    LibWeb: Plumb style/computed values for `backdrop-filter`

--- Userland/Libraries/LibWeb/Layout/Node.cpp
+++ Userland/Libraries/LibWeb/Layout/Node.cpp
@@ -393,6 +393,7 @@ void NodeWithStyle::apply_style(const CSS::StyleProperties& computed_style)
     computed_values.set_flex_shrink(computed_style.flex_shrink());
     computed_values.set_order(computed_style.order());
     computed_values.set_clip(computed_style.clip());
+    computed_values.set_backdrop_filter(computed_style.backdrop_filter());
 
     auto justify_content = computed_style.justify_content();
     if (justify_content.has_value())
