def452b81a778e307883f8aaa142391f4d868984
MacDue <macdue@dueutil.tech>
2022-09-15 08:31:27 +0100

    LibGfx: Add `BrightnessFilter`, `ContrastFilter`, and `OpacityFilter`
    These filters are based off the ones defined in:
    https://drafts.fxtf.org/filter-effects-1/#supported-filter-functions

---
+++ Userland/Libraries/LibGfx/Filters/OpacityFilter.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2022, MacDue <macdue@dueutil.tech>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/StringView.h>
+#include <LibGfx/Filters/ColorFilter.h>
+
+namespace Gfx {
+
+class OpacityFilter : public ColorFilter {
+public:
+    using ColorFilter::ColorFilter;
+    virtual ~OpacityFilter() = default;
+
+    virtual StringView class_name() const override { return "OpacityFilter"sv; }
+
+    virtual bool amount_handled_in_filter() const override
+    {
+        return true;
+    }
+
+protected:
+    Color convert_color(Color original) override
+    {
+        return original.with_alpha(m_amount * 255);
+    };
+};
+
+}
