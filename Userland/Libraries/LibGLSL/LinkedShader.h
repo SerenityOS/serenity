14cde1e731ed50257567c2f7ddc9fadcbca2903a
Stephan Unverwerth <s.unverwerth@serenityos.org>
2022-09-18 16:18:12 +0200

    LibGLSL: Fill LinkedShaders with dummy IR code

--- Userland/Libraries/LibGLSL/LinkedShader.h
+++ Userland/Libraries/LibGLSL/LinkedShader.h
@@ -11,12 +11,23 @@
 #include <AK/String.h>
 #include <AK/Vector.h>
 #include <LibGLSL/ObjectFile.h>
+#include <LibGPU/IR.h>
 
 namespace GLSL {
 
 // FIXME: Implement this class
 
 class LinkedShader final {
+public:
+    LinkedShader(const GPU::IR::Shader& intermediate_shader_representation)
+        : m_intermediate_shader_representation { intermediate_shader_representation }
+    {
+    }
+
+    GPU::IR::Shader const& intermediate_shader_representation() const { return m_intermediate_shader_representation; }
+
+private:
+    GPU::IR::Shader m_intermediate_shader_representation;
 };
 
 }
--- Userland/Libraries/LibGLSL/Linker.cpp
+++ Userland/Libraries/LibGLSL/Linker.cpp
@@ -12,7 +12,19 @@ ErrorOr<NonnullOwnPtr<LinkedShader>> Linker::link(Vector<ObjectFile const*> cons
 {
     // FIXME: implement this function
     m_messages = String::empty();
-    return adopt_own(*new LinkedShader());
+
+    GPU::IR::Shader shader;
+
+    shader.inputs.append({ "input0"sv, GPU::IR::StorageType::Vector4 });
+    shader.outputs.append({ "output0"sv, GPU::IR::StorageType::Vector4 });
+    GPU::IR::Instruction instruction {
+        GPU::IR::Opcode::Move,
+        { { GPU::IR::StorageLocation::Input, 0 } },
+        { GPU::IR::StorageLocation::Output, 0 }
+    };
+    shader.instructions.append(instruction);
+
+    return adopt_own(*new LinkedShader(shader));
 }
 
 }
