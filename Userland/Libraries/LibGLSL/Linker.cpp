c1c5247b6c07fe2518bf8a85d75822f9b1202e00
Stephan Unverwerth <s.unverwerth@serenityos.org>
2022-09-18 16:18:12 +0200

    LibGLSL: Fill LinkedShaders with dummy IR code

--- Userland/Libraries/LibGLSL/Linker.cpp
+++ Userland/Libraries/LibGLSL/Linker.cpp
@@ -12,7 +12,21 @@ ErrorOr<NonnullOwnPtr<LinkedShader>> Linker::link(Vector<ObjectFile const*> cons
 {
     // FIXME: implement this function
     m_messages = TRY(String::from_utf8(""sv));
-    return adopt_own(*new LinkedShader());
+
+    GPU::IR::Shader shader;
+
+    auto input_name = TRY(String::from_utf8("input0"sv));
+    auto output_name = TRY(String::from_utf8("output0"sv));
+    TRY(shader.inputs.try_append({ move(input_name), GPU::IR::StorageType::Vector4 }));
+    TRY(shader.outputs.try_append({ move(output_name), GPU::IR::StorageType::Vector4 }));
+    GPU::IR::Instruction instruction {
+        GPU::IR::Opcode::Move,
+        { { GPU::IR::StorageLocation::Input, 0 } },
+        { GPU::IR::StorageLocation::Output, 0 }
+    };
+    TRY(shader.instructions.try_append(instruction));
+
+    return adopt_own(*new LinkedShader(shader));
 }
 
 }
