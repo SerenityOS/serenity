8f7292e92a330b1a0c00cebd70c9d72563c74dd9
Andreas Kling <kling@serenityos.org>
2022-09-19 11:16:42 +0200

    Ladybird/WebView: Move WebSocket stuff to its own files

---
+++ Ladybird/WebSocketLadybird.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2022, Dex♪ <dexes.ttp@gmail.com>
+ * Copyright (c) 2022, Andreas Kling <kling@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibWeb/WebSockets/WebSocket.h>
+#include <LibWebSocket/WebSocket.h>
+
+namespace Ladybird {
+
+class WebSocketLadybird
+    : public Web::WebSockets::WebSocketClientSocket
+    , public Weakable<WebSocketLadybird> {
+public:
+    static NonnullRefPtr<WebSocketLadybird> create(NonnullRefPtr<WebSocket::WebSocket>);
+
+    virtual ~WebSocketLadybird() override;
+
+    virtual Web::WebSockets::WebSocket::ReadyState ready_state() override;
+    virtual void send(ByteBuffer binary_or_text_message, bool is_text) override;
+    virtual void send(StringView message) override;
+    virtual void close(u16 code, String reason) override;
+
+private:
+    explicit WebSocketLadybird(NonnullRefPtr<WebSocket::WebSocket>);
+
+    NonnullRefPtr<WebSocket::WebSocket> m_websocket;
+};
+
+}
