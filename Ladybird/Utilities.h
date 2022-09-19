3edcae008ff859b3f38f56b79fc7f80c683ee0fe
Andreas Kling <kling@serenityos.org>
2022-09-19 11:06:23 +0200

    Ladybird/WebView: Move our PageClient to its own file
    And rename it to PageClientLadybird while we're at it, it's not
    "headless" by any means.

---
+++ Ladybird/Utilities.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2022, Andreas Kling <kling@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/String.h>
+#include <QString>
+
+AK::String akstring_from_qstring(QString const&);
+QString qstring_from_akstring(AK::String const&);
