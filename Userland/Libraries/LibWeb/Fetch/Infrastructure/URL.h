010be491a998e5d7a4e4f6f05b10e19aa2d360a3
Andreas Kling <kling@serenityos.org>
2022-09-21 18:41:03 +0200

    LibWeb: Fix e-mail address mishap

--- Userland/Libraries/LibWeb/Fetch/Infrastructure/URL.h
+++ Userland/Libraries/LibWeb/Fetch/Infrastructure/URL.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2022, Linus Groh <linusg@serenityos.org>
- * Copyright (c) 2022, Andreas Kling <linusg@serenityos.org>
+ * Copyright (c) 2022, Andreas Kling <kling@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
