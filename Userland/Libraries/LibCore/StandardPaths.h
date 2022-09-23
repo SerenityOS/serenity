bd66453e8dd48104f2f1cd9bf0f2dc6666939f29
thankyouverycool <66646555+thankyouverycool@users.noreply.github.com>
2022-09-23 10:34:06 -0400

    LibCore: Add documents_directory() to StandardPaths

--- Userland/Libraries/LibCore/StandardPaths.h
+++ Userland/Libraries/LibCore/StandardPaths.h
@@ -14,6 +14,7 @@ class StandardPaths {
 public:
     static String home_directory();
     static String desktop_directory();
+    static String documents_directory();
     static String downloads_directory();
     static String tempfile_directory();
     static String config_directory();
