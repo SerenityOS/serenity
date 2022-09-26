e99277611b3e15d21fa15c6fd9f2afa054fb9921
thankyouverycool <66646555+thankyouverycool@users.noreply.github.com>
2022-09-26 08:57:11 -0400

    Magnifier+ThemeEditor: Use CommonMenus::make_accessibility_menu()

--- Userland/Applications/ThemeEditor/MainWidget.cpp
+++ Userland/Applications/ThemeEditor/MainWidget.cpp
@@ -241,66 +241,7 @@ ErrorOr<void> MainWidget::initialize_menubar(GUI::Window& window)
             GUI::Application::the()->quit();
     })));
 
-    auto accessibility_menu = TRY(window.try_add_menu("&Accessibility"));
-
-    auto default_accessibility_action = GUI::Action::create_checkable("Default - non-impaired", { Mod_AltGr, Key_1 }, [&](auto&) {
-        m_preview_widget->set_color_filter(nullptr);
-    });
-    default_accessibility_action->set_checked(true);
-
-    auto pratanopia_accessibility_action = GUI::Action::create_checkable("Protanopia", { Mod_AltGr, Key_2 }, [&](auto&) {
-        m_preview_widget->set_color_filter(Gfx::ColorBlindnessFilter::create_protanopia());
-    });
-
-    auto pratanomaly_accessibility_action = GUI::Action::create_checkable("Protanomaly", { Mod_AltGr, Key_3 }, [&](auto&) {
-        m_preview_widget->set_color_filter(Gfx::ColorBlindnessFilter::create_protanomaly());
-    });
-
-    auto tritanopia_accessibility_action = GUI::Action::create_checkable("Tritanopia", { Mod_AltGr, Key_4 }, [&](auto&) {
-        m_preview_widget->set_color_filter(Gfx::ColorBlindnessFilter::create_tritanopia());
-    });
-
-    auto tritanomaly_accessibility_action = GUI::Action::create_checkable("Tritanomaly", { Mod_AltGr, Key_5 }, [&](auto&) {
-        m_preview_widget->set_color_filter(Gfx::ColorBlindnessFilter::create_tritanomaly());
-    });
-
-    auto deuteranopia_accessibility_action = GUI::Action::create_checkable("Deuteranopia", { Mod_AltGr, Key_6 }, [&](auto&) {
-        m_preview_widget->set_color_filter(Gfx::ColorBlindnessFilter::create_deuteranopia());
-    });
-
-    auto deuteranomaly_accessibility_action = GUI::Action::create_checkable("Deuteranomaly", { Mod_AltGr, Key_7 }, [&](auto&) {
-        m_preview_widget->set_color_filter(Gfx::ColorBlindnessFilter::create_deuteranomaly());
-    });
-
-    auto achromatopsia_accessibility_action = GUI::Action::create_checkable("Achromatopsia", { Mod_AltGr, Key_8 }, [&](auto&) {
-        m_preview_widget->set_color_filter(Gfx::ColorBlindnessFilter::create_achromatopsia());
-    });
-
-    auto achromatomaly_accessibility_action = GUI::Action::create_checkable("Achromatomaly", { Mod_AltGr, Key_9 }, [&](auto&) {
-        m_preview_widget->set_color_filter(Gfx::ColorBlindnessFilter::create_achromatomaly());
-    });
-
-    m_preview_type_action_group = make<GUI::ActionGroup>();
-    m_preview_type_action_group->set_exclusive(true);
-    m_preview_type_action_group->add_action(*default_accessibility_action);
-    m_preview_type_action_group->add_action(*pratanopia_accessibility_action);
-    m_preview_type_action_group->add_action(*pratanomaly_accessibility_action);
-    m_preview_type_action_group->add_action(*tritanopia_accessibility_action);
-    m_preview_type_action_group->add_action(*tritanomaly_accessibility_action);
-    m_preview_type_action_group->add_action(*deuteranopia_accessibility_action);
-    m_preview_type_action_group->add_action(*deuteranomaly_accessibility_action);
-    m_preview_type_action_group->add_action(*achromatopsia_accessibility_action);
-    m_preview_type_action_group->add_action(*achromatomaly_accessibility_action);
-
-    TRY(accessibility_menu->try_add_action(default_accessibility_action));
-    TRY(accessibility_menu->try_add_action(pratanopia_accessibility_action));
-    TRY(accessibility_menu->try_add_action(pratanomaly_accessibility_action));
-    TRY(accessibility_menu->try_add_action(tritanopia_accessibility_action));
-    TRY(accessibility_menu->try_add_action(tritanomaly_accessibility_action));
-    TRY(accessibility_menu->try_add_action(deuteranopia_accessibility_action));
-    TRY(accessibility_menu->try_add_action(deuteranomaly_accessibility_action));
-    TRY(accessibility_menu->try_add_action(achromatopsia_accessibility_action));
-    TRY(accessibility_menu->try_add_action(achromatomaly_accessibility_action));
+    TRY(window.try_add_menu(TRY(GUI::CommonMenus::make_accessibility_menu(*m_preview_widget))));
 
     auto help_menu = TRY(window.try_add_menu("&Help"));
     TRY(help_menu->try_add_action(GUI::CommonActions::make_command_palette_action(&window)));
