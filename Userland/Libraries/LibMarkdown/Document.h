aea4fbdd7babfa503d679d32b65cda2fd974795f
Sam Atkins <atkinssj@serenityos.org>
2022-09-27 11:35:10 +0100

    LibMarkdown: Allow extra content to be inserted into HTML output's head
    This lets users supply custom styling and scripting, among other things.

--- Userland/Libraries/LibMarkdown/Document.h
+++ Userland/Libraries/LibMarkdown/Document.h
@@ -19,7 +19,7 @@ public:
         : m_container(move(container))
     {
     }
-    String render_to_html() const;
+    String render_to_html(StringView extra_head_contents = ""sv) const;
     String render_to_inline_html() const;
     String render_for_terminal(size_t view_width = 0) const;
 
