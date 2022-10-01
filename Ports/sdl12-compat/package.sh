b288cd6976c9e7d48114ad73531a9e2b973a183c
Tim Schumacher <timschumi@gmx.de>
2022-10-02 01:39:59 +0200

    Ports/sdl12-compat: Update to version 1.2.56

--- Ports/sdl12-compat/package.sh
+++ Ports/sdl12-compat/package.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=sdl12-compat
-version=1.2.52
+port='sdl12-compat'
+version='1.2.56'
 workdir=sdl12-compat-release-${version}
-useconfigure=true
-files="https://github.com/libsdl-org/sdl12-compat/archive/refs/tags/release-${version}.tar.gz ${port}-${version}.tar.gz 5bd7942703575554670a8767ae030f7921a0ac3c5e2fd173a537b7c7a8599014"
-auth_type=sha256
-
-configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt" "-DCMAKE_BUILD_TYPE=Release" "-DCMAKE_INSTALL_PREFIX=${SERENITY_INSTALL_ROOT}/usr/local/" "-B./build")
+useconfigure='true'
+files="https://github.com/libsdl-org/sdl12-compat/archive/refs/tags/release-${version}.tar.gz ${port}-${version}.tar.gz f62f3e15f95aade366ee6c03f291e8825c4689390a6be681535259a877259c58"
+auth_type='sha256'
+configopts=(
+    "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
+    "-DCMAKE_BUILD_TYPE=Release"
+    "-DCMAKE_INSTALL_PREFIX=${SERENITY_INSTALL_ROOT}/usr/local/"
+    "-B./build"
+)
 depends=("SDL2")
 
 configure() {
