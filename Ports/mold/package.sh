8eca5b92e42c152bac1a0d7a70836685d2d55ec9
Andrew Kaster <akaster@serenityos.org>
2022-10-02 21:01:33 -0600

    Ports/mold: Update to 1.5.1 and use CMake instead of Makefile
    Per the release notes for 1.5.0, the CMake build is preferred going
    forward. This lets us drop some Makefile patches and pass them as CMake
    options instead, with the exception of disabling mold-wrapper.so.

--- Ports/mold/package.sh
+++ Ports/mold/package.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=mold
-version=1.0.3
-files="https://github.com/rui314/mold/archive/refs/tags/v${version}.tar.gz mold-${version}.tgz 488c12058b4c7c77bff94c6f919e40b2f12c304214e2e0d7d4833c21167837c0"
+version=1.5.1
+files="https://github.com/rui314/mold/archive/refs/tags/v${version}.tar.gz mold-${version}.tgz ec94aa74758f1bc199a732af95c6304ec98292b87f2f4548ce8436a7c5b054a1"
 auth_type=sha256
-depends=("zlib" "openssl")
-makeopts=("OS=SerenityOS" "LDFLAGS=-L${DESTDIR}/usr/local/lib" "-j$(nproc)")
-installopts=("OS=SerenityOS")
+depends=("zlib" "openssl" "zstd")
+useconfigure='true'
+configopts=(
+    "-B build"
+    "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
+    "-DMOLD_USE_MIMALLOC=OFF"
+    "-DBUILD_TESTING=OFF"
+)
+
+configure() {
+    run cmake "${configopts[@]}"
+}
+
+build() {
+    run make -C build "${makeopts[@]}"
+}
+
+install() {
+    run make -C build install "${installopts[@]}"
+}
