ff9a80f54f09888f2057123df1c67225a52a7b35
Linus Groh <mail@linusgroh.de>
2022-10-04 21:03:50 +0100

    LibWeb: Implement 'coarsened shared current time'

--- Userland/Libraries/LibWeb/HighResolutionTime/CoarsenTime.cpp
+++ Userland/Libraries/LibWeb/HighResolutionTime/CoarsenTime.cpp
@@ -1,9 +1,11 @@
 /*
  * Copyright (c) 2022, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2022, Linus Groh <linusg@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <LibWeb/HTML/EventLoop/EventLoop.h>
 #include <LibWeb/HighResolutionTime/CoarsenTime.h>
 
 namespace Web::HighResolutionTime {
@@ -14,6 +16,13 @@ DOMHighResTimeStamp coarsen_time(DOMHighResTimeStamp timestamp, bool cross_origi
     // FIXME: Implement this.
     (void)cross_origin_isolated_capability;
     return timestamp;
+}
+
+// https://w3c.github.io/hr-time/#dfn-coarsened-shared-current-time
+DOMHighResTimeStamp coarsened_shared_current_time(bool cross_origin_isolated_capability)
+{
+    // The coarsened shared current time given an optional boolean crossOriginIsolatedCapability (default false), must return the result of calling coarsen time with the unsafe shared current time and crossOriginIsolatedCapability.
+    return coarsen_time(HTML::main_thread_event_loop().unsafe_shared_current_time(), cross_origin_isolated_capability);
 }
 
 }
--- Userland/Libraries/LibWeb/HighResolutionTime/CoarsenTime.h
+++ Userland/Libraries/LibWeb/HighResolutionTime/CoarsenTime.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2022, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2022, Linus Groh <linusg@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -11,5 +12,6 @@
 namespace Web::HighResolutionTime {
 
 DOMHighResTimeStamp coarsen_time(DOMHighResTimeStamp timestamp, bool cross_origin_isolated_capability = false);
+DOMHighResTimeStamp coarsened_shared_current_time(bool cross_origin_isolated_capability = false);
 
 }
