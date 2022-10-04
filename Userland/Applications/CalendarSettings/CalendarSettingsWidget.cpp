eca559d65d1f49d0606ef83d66ffe247c037d2e7
Tobias Christiansen <tobyase@serenityos.org>
2022-10-04 11:36:09 +0200

    CalendarSettings: Add Weekend-specific settings
    In some calendars, weekends start on other days than saturday and can
    also have different lengths than 2 days. This patch allows you to set
    these values, however they don't do anything yet as Serenity's Calendar
    doesn't care about Weekends at the moment.

--- Userland/Applications/CalendarSettings/CalendarSettingsWidget.cpp
+++ Userland/Applications/CalendarSettings/CalendarSettingsWidget.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2022-2022, Olivier De Cannière <olivier.decanniere96@gmail.com>
+ * Copyright (c) 2022, Tobias Christiansen <tobyase@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -8,17 +9,23 @@
 #include <AK/DateConstants.h>
 #include <Applications/CalendarSettings/CalendarSettingsWidgetGML.h>
 #include <LibConfig/Client.h>
+#include <LibGUI/ComboBox.h>
 #include <LibGUI/ItemListModel.h>
+#include <LibGUI/SpinBox.h>
 
 void CalendarSettingsWidget::apply_settings()
 {
     Config::write_string("Calendar"sv, "View"sv, "FirstDayOfWeek"sv, m_first_day_of_week_combobox->text());
+    Config::write_string("Calendar"sv, "View"sv, "FirstDayOfWeekend"sv, m_first_day_of_weekend_combobox->text());
+    Config::write_i32("Calendar"sv, "View"sv, "WeekendLength"sv, m_weekend_length_spinbox->value());
     Config::write_string("Calendar"sv, "View"sv, "DefaultView"sv, m_default_view_combobox->text());
 }
 
 void CalendarSettingsWidget::reset_default_values()
 {
     m_first_day_of_week_combobox->set_text("Sunday");
+    m_first_day_of_weekend_combobox->set_text("Saturday");
+    m_weekend_length_spinbox->set_value(2);
     m_default_view_combobox->set_text("Month");
 }
 
@@ -31,6 +38,20 @@ CalendarSettingsWidget::CalendarSettingsWidget()
     m_first_day_of_week_combobox->set_only_allow_values_from_model(true);
     m_first_day_of_week_combobox->set_model(*GUI::ItemListModel<StringView, Array<StringView, 7>>::create(AK::long_day_names));
     m_first_day_of_week_combobox->on_change = [&](auto, auto) {
+        set_modified(true);
+    };
+
+    m_first_day_of_weekend_combobox = *find_descendant_of_type_named<GUI::ComboBox>("first_day_of_weekend");
+    m_first_day_of_weekend_combobox->set_text(Config::read_string("Calendar"sv, "View"sv, "FirstDayOfWeekend"sv, "Saturday"sv));
+    m_first_day_of_weekend_combobox->set_only_allow_values_from_model(true);
+    m_first_day_of_weekend_combobox->set_model(*GUI::ItemListModel<StringView, Array<StringView, 7>>::create(AK::long_day_names));
+    m_first_day_of_weekend_combobox->on_change = [&](auto, auto) {
+        set_modified(true);
+    };
+
+    m_weekend_length_spinbox = *find_descendant_of_type_named<GUI::SpinBox>("weekend_length");
+    m_weekend_length_spinbox->set_value(Config::read_i32("Calendar"sv, "View"sv, "WeekendLength"sv, 2));
+    m_weekend_length_spinbox->on_change = [&](auto) {
         set_modified(true);
     };
 
--- Userland/Applications/CalendarSettings/CalendarSettingsWidget.h
+++ Userland/Applications/CalendarSettings/CalendarSettingsWidget.h
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2022-2022, Olivier De Cannière <olivier.decanniere96@gmail.com>
+ * Copyright (c) 2022, Tobias Christiansen <tobyase@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #pragma once
 
-#include <LibGUI/ComboBox.h>
 #include <LibGUI/SettingsWindow.h>
 
 class CalendarSettingsWidget final : public GUI::SettingsWindow::Tab {
@@ -21,5 +21,7 @@ private:
     static constexpr Array<StringView, 2> const m_view_modes = { "Month"sv, "Year"sv };
 
     RefPtr<GUI::ComboBox> m_first_day_of_week_combobox;
+    RefPtr<GUI::ComboBox> m_first_day_of_weekend_combobox;
+    RefPtr<GUI::SpinBox> m_weekend_length_spinbox;
     RefPtr<GUI::ComboBox> m_default_view_combobox;
 };
