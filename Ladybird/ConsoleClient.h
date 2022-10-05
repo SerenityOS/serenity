acec34351ff7ce6169e4362b1e7749c2cd80567e
Sam Atkins <atkinssj@gmail.com>
2022-10-05 12:24:08 +0100

    Ladybird/ConsoleClient: Implement console message styling with `%c`
    This matches the changes made to Serenity in:
    https://github.com/SerenityOS/serenity/commit/7a2da4cabf5627941cf1fc6eb7c5fd068fcc75f9

--- Ladybird/ConsoleClient.h
+++ Ladybird/ConsoleClient.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2022, Brandon Scott <xeon.productions@gmail.com>
  * Copyright (c) 2020, Hunter Salyer <thefalsehonesty@gmail.com>
- * Copyright (c) 2021, Sam Atkins <atkinssj@serenityos.org>
+ * Copyright (c) 2021-2022, Sam Atkins <atkinssj@serenityos.org>
  * Copyright (c) 2022, Andreas Kling <kling@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
@@ -32,6 +32,12 @@ private:
     virtual void clear() override;
     virtual JS::ThrowCompletionOr<JS::Value> printer(JS::Console::LogLevel log_level, PrinterArguments) override;
 
+    virtual void add_css_style_to_current_message(StringView style) override
+    {
+        m_current_message_style.append(style);
+        m_current_message_style.append(';');
+    }
+
     SimpleWebView& m_view;
 
     WeakPtr<JS::Interpreter> m_interpreter;
@@ -54,6 +60,8 @@ private:
         String data;
     };
     Vector<ConsoleOutput> m_message_log;
+
+    StringBuilder m_current_message_style;
 
     WeakPtr<JS::Realm> m_realm;
 };
