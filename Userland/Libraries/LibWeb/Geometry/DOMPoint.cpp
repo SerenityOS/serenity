52eb1984d09ef757902f451bd14c0c4e69355d2e
Sam Atkins <atkinssj@serenityos.org>
2022-10-06 15:25:08 +0100

    LibWeb: Implement DOMPoint/DOMPointReadOnly.fromPoint()

--- Userland/Libraries/LibWeb/Geometry/DOMPoint.cpp
+++ Userland/Libraries/LibWeb/Geometry/DOMPoint.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2022, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2022, Sam Atkins <atkinssj@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -18,6 +19,13 @@ DOMPoint::DOMPoint(JS::Realm& realm, double x, double y, double z, double w)
     : DOMPointReadOnly(realm, x, y, z, w)
 {
     set_prototype(&Bindings::cached_web_prototype(realm, "DOMPoint"));
+}
+
+// https://drafts.fxtf.org/geometry/#dom-dompoint-frompoint
+JS::NonnullGCPtr<DOMPoint> DOMPoint::from_point(JS::VM& vm, DOMPointInit const& other)
+{
+    // The fromPoint(other) static method on DOMPoint must create a DOMPoint from the dictionary other.
+    return construct_impl(*vm.current_realm(), other.x, other.y, other.z, other.w);
 }
 
 DOMPoint::~DOMPoint() = default;
