8082ab7f03d5083cb804e8d4cc1ee5f3706767f5
Liav A <liavalb@gmail.com>
2022-10-08 12:03:02 +0300

    Ports: Update dmidecode version to 3.4
    Also, let's stop use the signature file and instead just compare sha256
    checksums.

--- Ports/dmidecode/package.sh
+++ Ports/dmidecode/package.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='dmidecode'
-version='3.3'
+version='3.4'
 useconfigure='false'
-files="https://download.savannah.gnu.org/releases/dmidecode/dmidecode-${version}.tar.xz dmidecode-${version}.tar.xz
-https://download.savannah.gnu.org/releases/dmidecode/dmidecode-${version}.tar.xz.sig dmidecode-${version}.tar.xz.sig
-https://ftpmirror.gnu.org/gnu/gnu-keyring.gpg gnu-keyring.gpg"
-auth_type='sig'
-auth_import_key='90DFD6523C57373D81F63D19865688D038F02FC8'
-auth_opts=("--keyring" "./gnu-keyring.gpg" "dmidecode-${version}.tar.xz.sig")
+files="https://download.savannah.gnu.org/releases/dmidecode/dmidecode-${version}.tar.xz dmidecode-${version}.tar.xz 43cba851d8467c9979ccdbeab192eb6638c7d3a697eba5ddb779da8837542212"
+auth_type='sha256'
 
 install() {
     run make clean
