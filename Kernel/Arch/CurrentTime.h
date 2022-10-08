3651d9701e2a91b5c13012bdd3fbcf333ae2369a
Liav A <liavalb@gmail.com>
2022-10-08 04:27:48 +0300

    Kernel: Abstract platform-specific current time methods from Scheduler
    This change ensures that the scheduler doesn't depend on a platform
    specific or arch-specific code when it initializes itself, but rather we
    ensure that in compile-time we will generate the appropriate code to
    find the correct arch-specific current time methods.

---
+++ Kernel/Arch/CurrentTime.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2022, Liav A. <liavalb@hotmail.co.il>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/Types.h>
+
+namespace Kernel {
+
+typedef u64 (*fptr)();
+
+fptr optional_current_time();
+
+}
