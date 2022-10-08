33ce7c939a9dc71876915e9a7ce8aa2a7222dd6a
Liav A <liavalb@gmail.com>
2022-10-08 11:58:59 +0300

    Ports: Apply the known hygiene rules on the dmidecode port

--- Ports/dmidecode/package.sh
+++ Ports/dmidecode/package.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=dmidecode
-version=3.3
-useconfigure=false
+port='dmidecode'
+version='3.3'
+useconfigure='false'
 files="https://download.savannah.gnu.org/releases/dmidecode/dmidecode-${version}.tar.xz dmidecode-${version}.tar.xz
 https://download.savannah.gnu.org/releases/dmidecode/dmidecode-${version}.tar.xz.sig dmidecode-${version}.tar.xz.sig
 https://ftpmirror.gnu.org/gnu/gnu-keyring.gpg gnu-keyring.gpg"
-auth_type="sig"
-auth_import_key="90DFD6523C57373D81F63D19865688D038F02FC8"
+auth_type='sig'
+auth_import_key='90DFD6523C57373D81F63D19865688D038F02FC8'
 auth_opts=("--keyring" "./gnu-keyring.gpg" "dmidecode-${version}.tar.xz.sig")
 
 install() {
