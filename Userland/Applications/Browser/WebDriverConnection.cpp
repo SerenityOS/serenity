8c0f1da9f75ff5790d32ad58250a914178d16a17
Sam Atkins <atkinssj@serenityos.org>
2022-10-12 10:50:33 +0100

    Browser: Add a basic WebDriver API
    This adds a new option "--webdriver" that opens a local unix socket
    in /tmp/browser_{pid} which the WebDriver server can use to send
    commands to the Browser instance.
    
    Co-authored-by: Florent Castelli <florent.castelli@gmail.com>

---
+++ Userland/Applications/Browser/WebDriverConnection.cpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2022, Florent Castelli <florent.castelli@gmail.com>
+ * Copyright (c) 2022, Sam Atkins <atkinssj@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include "WebDriverConnection.h"
+#include "BrowserWindow.h"
+
+namespace Browser {
+
+WebDriverConnection::WebDriverConnection(NonnullOwnPtr<Core::Stream::LocalSocket> socket, NonnullRefPtr<BrowserWindow> browser_window)
+    : IPC::ConnectionToServer<WebDriverSessionClientEndpoint, WebDriverSessionServerEndpoint>(*this, move(socket))
+    , m_browser_window(move(browser_window))
+{
+}
+
+void WebDriverConnection::quit()
+{
+    dbgln("WebDriverConnection: quit");
+    if (auto browser_window = m_browser_window.strong_ref())
+        browser_window->close();
+}
+
+Messages::WebDriverSessionClient::GetUrlResponse WebDriverConnection::get_url()
+{
+    dbgln("WebDriverConnection: get_url");
+    if (auto browser_window = m_browser_window.strong_ref())
+        return { browser_window->active_tab().url() };
+    return { URL("") };
+}
+
+void WebDriverConnection::set_url(AK::URL const& url)
+{
+    dbgln("WebDriverConnection: set_url {}", url);
+    if (auto browser_window = m_browser_window.strong_ref())
+        browser_window->active_tab().load(url);
+}
+
+Messages::WebDriverSessionClient::GetTitleResponse WebDriverConnection::get_title()
+{
+    dbgln("WebDriverConnection: get_title");
+    if (auto browser_window = m_browser_window.strong_ref())
+        return { browser_window->active_tab().title() };
+    return { "" };
+}
+
+}
---
+++ Userland/Applications/Browser/WebDriverConnection.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2022, Florent Castelli <florent.castelli@gmail.com>
+ * Copyright (c) 2022, Sam Atkins <atkinssj@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include "BrowserWindow.h"
+#include <AK/Error.h>
+#include <AK/String.h>
+#include <Applications/Browser/WebDriverSessionClientEndpoint.h>
+#include <Applications/Browser/WebDriverSessionServerEndpoint.h>
+#include <LibCore/LocalServer.h>
+#include <LibGUI/Application.h>
+#include <LibIPC/ConnectionToServer.h>
+#include <unistd.h>
+
+namespace Browser {
+
+class WebDriverConnection final
+    : public IPC::ConnectionToServer<WebDriverSessionClientEndpoint, WebDriverSessionServerEndpoint> {
+    C_OBJECT_ABSTRACT(WebDriverConnection)
+public:
+    static ErrorOr<NonnullRefPtr<WebDriverConnection>> connect_to_webdriver(NonnullRefPtr<BrowserWindow> browser_window, String path)
+    {
+        dbgln("Trying to connect to {}", path);
+        auto result = TRY(Core::Stream::LocalSocket::connect(path));
+        dbgln("Connected to WebDriver");
+        return TRY(adopt_nonnull_ref_or_enomem(new (nothrow) WebDriverConnection(move(result), browser_window)));
+    }
+
+    virtual ~WebDriverConnection() = default;
+
+    virtual void die() override { }
+
+    virtual void quit() override;
+    virtual Messages::WebDriverSessionClient::GetUrlResponse get_url() override;
+    virtual void set_url(AK::URL const& url) override;
+    virtual Messages::WebDriverSessionClient::GetTitleResponse get_title() override;
+
+private:
+    WebDriverConnection(NonnullOwnPtr<Core::Stream::LocalSocket> socket, NonnullRefPtr<BrowserWindow> browser_window);
+
+    WeakPtr<BrowserWindow> m_browser_window;
+};
+
+}
