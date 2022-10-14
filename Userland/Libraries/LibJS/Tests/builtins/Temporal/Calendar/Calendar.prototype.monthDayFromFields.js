d5d1146cc31e1a9b7cdf2a10daa63013fa129d05
Luke Wilde <lukew@serenityos.org>
2022-10-14 16:10:04 +0100

    LibJS/Temporal: Fix inconsistency in order of observable operations
    This is a normative change in the Temporal spec.
    
    See: https://github.com/tc39/proposal-temporal/commit/a3a8237

--- Userland/Libraries/LibJS/Tests/builtins/Temporal/Calendar/Calendar.prototype.monthDayFromFields.js
+++ Userland/Libraries/LibJS/Tests/builtins/Temporal/Calendar/Calendar.prototype.monthDayFromFields.js
@@ -24,6 +24,49 @@ describe("correct behavior", () => {
         const fields = plainMonthDay.getISOFields();
         expect(fields.isoYear).toBe(1972);
     });
+
+    test("gets overflow after temporal fields", () => {
+        const operations = [];
+        const calendar = new Temporal.Calendar("iso8601");
+
+        const fields = {
+            get day() {
+                operations.push("get day");
+                return 3;
+            },
+
+            get month() {
+                operations.push("get month");
+                return 10;
+            },
+
+            get monthCode() {
+                operations.push("get monthCode");
+                return "M10";
+            },
+
+            get year() {
+                operations.push("get year");
+                return 2022;
+            },
+        };
+
+        const options = {
+            get overflow() {
+                operations.push("get overflow");
+                return "constrain";
+            },
+        };
+
+        expect(operations).toHaveLength(0);
+        calendar.monthDayFromFields(fields, options);
+        expect(operations).toHaveLength(5);
+        expect(operations[0]).toBe("get day");
+        expect(operations[1]).toBe("get month");
+        expect(operations[2]).toBe("get monthCode");
+        expect(operations[3]).toBe("get year");
+        expect(operations[4]).toBe("get overflow");
+    });
 });
 
 describe("errors", () => {
--- Userland/Libraries/LibJS/Tests/builtins/Temporal/Calendar/Calendar.prototype.yearMonthFromFields.js
+++ Userland/Libraries/LibJS/Tests/builtins/Temporal/Calendar/Calendar.prototype.yearMonthFromFields.js
@@ -18,6 +18,43 @@ describe("correct behavior", () => {
         expect(plainYearMonth.year).toBe(2021);
         expect(plainYearMonth.month).toBe(7);
     });
+
+    test("gets overflow after temporal fields", () => {
+        const operations = [];
+        const calendar = new Temporal.Calendar("iso8601");
+
+        const fields = {
+            get month() {
+                operations.push("get month");
+                return 10;
+            },
+
+            get monthCode() {
+                operations.push("get monthCode");
+                return "M10";
+            },
+
+            get year() {
+                operations.push("get year");
+                return 2022;
+            },
+        };
+
+        const options = {
+            get overflow() {
+                operations.push("get overflow");
+                return "constrain";
+            },
+        };
+
+        expect(operations).toHaveLength(0);
+        calendar.yearMonthFromFields(fields, options);
+        expect(operations).toHaveLength(4);
+        expect(operations[0]).toBe("get month");
+        expect(operations[1]).toBe("get monthCode");
+        expect(operations[2]).toBe("get year");
+        expect(operations[3]).toBe("get overflow");
+    });
 });
 
 describe("errors", () => {
