30be638fb2aa0f6653e6fa2d814f40912ac54946
cflip <cflip@cflip.net>
2022-10-13 23:15:47 -0600

    Ports/zlib: Update to 1.2.13

--- Ports/zlib/package.sh
+++ Ports/zlib/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=zlib
-version=1.2.12
+version=1.2.13
 useconfigure=true
-files="https://www.zlib.net/zlib-${version}.tar.gz zlib-${version}.tar.gz 91844808532e5ce316b3c010929493c0244f3d37593afd6de04f71821d5136d9"
+files="https://www.zlib.net/zlib-${version}.tar.gz zlib-${version}.tar.gz b3a24de97a8fdbc835b9833169501030b8977031bcb54b3b3ac13740f846ab30"
 auth_type="sha256"
 
 configure() {
