6669d71abae52871913ca8df330771732d599449
Liav A <liavalb@gmail.com>
2022-10-15 20:09:32 +0300

    Ports: Add GNU which port

---
+++ Ports/which/package.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port='which'
+version='2.21'
+useconfigure='true'
+files="https://ftp.gnu.org/gnu/which/which-${version}.tar.gz which-${version}.tar.gz f4a245b94124b377d8b49646bf421f9155d36aa7614b6ebf83705d3ffc76eaad"
+auth_type='sha256'
