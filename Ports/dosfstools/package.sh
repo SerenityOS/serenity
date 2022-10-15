414a22a386ad91d02c6eecf5d728cfaa551afe96
Tim Schumacher <timschumi@gmx.de>
2022-10-15 15:13:08 +0200

    Ports/dosfstools: Create symlinks automatically instead of manually
    Replace the manual creation of symlinks with the
    `--enable-compat-symlinks` configure option, which automatically creates
    the symlinks that we want and more.

--- Ports/dosfstools/package.sh
+++ Ports/dosfstools/package.sh
@@ -6,8 +6,6 @@ use_fresh_config_sub='true'
 config_sub_paths=('config.sub')
 files="https://github.com/dosfstools/dosfstools/releases/download/v${version}/dosfstools-${version}.tar.gz dosfstools-${version}.tar.gz 64926eebf90092dca21b14259a5301b7b98e7b1943e8a201c7d726084809b527"
 auth_type='sha256'
-
-post_install() {
-    ln -sf /usr/local/sbin/mkfs.fat "${SERENITY_INSTALL_ROOT}/usr/local/sbin/mkfs.vfat"
-    ln -sf /usr/local/sbin/mkfs.fat "${SERENITY_INSTALL_ROOT}/usr/local/sbin/mkfs.msdos"
-}
+configopts=(
+    "--enable-compat-symlinks"
+)
