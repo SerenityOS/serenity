2c2c84cff0322bde5836a1142d0fdf62b57993c3
Tim Schumacher <timschumi@gmx.de>
2022-10-15 15:11:52 +0200

    Ports/dosfstools: Don't run autogen
    The release tarball arrives preconfigured, so there is no need for us to
    run autogen manually.

--- Ports/dosfstools/package.sh
+++ Ports/dosfstools/package.sh
@@ -7,10 +7,6 @@ config_sub_paths=('config.sub')
 files="https://github.com/dosfstools/dosfstools/releases/download/v${version}/dosfstools-${version}.tar.gz dosfstools-${version}.tar.gz 64926eebf90092dca21b14259a5301b7b98e7b1943e8a201c7d726084809b527"
 auth_type='sha256'
 
-pre_configure() {
-    run ./autogen.sh 
-}
-
 post_install() {
     ln -sf /usr/local/sbin/mkfs.fat "${SERENITY_INSTALL_ROOT}/usr/local/sbin/mkfs.vfat"
     ln -sf /usr/local/sbin/mkfs.fat "${SERENITY_INSTALL_ROOT}/usr/local/sbin/mkfs.msdos"
