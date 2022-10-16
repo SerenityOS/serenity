a2ccf31a629ee0bbc51710612b427463aaf44f05
Liav A <liavalb@gmail.com>
2022-10-16 20:45:42 +0300

    Ports/dmidecode: Remove install and post_install overriden sequences
    We used to do whole bunch of unnecessary things in the install sequence
    which the default port_include script sequence can do just fine,
    therefore the install sequence is removed from the port script.
    
    The post_install sequence wrongly called "make install-bin" which could
    be done in the default install sequence, as well as to create the /bin
    directory which is completely unnecessary to do because the image build
    script already does that for us. Also, now /usr/local/bin is in the PATH
    environment variable, so the installed binaries are runnable without
    creating symlinks in the /bin directory, therefore making the sequence
    of post_install completely unnecessary in the script so it is removed
    too.

--- Ports/dmidecode/package.sh
+++ Ports/dmidecode/package.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='dmidecode'
 version='3.4'
-useconfigure='false'
 files="https://download.savannah.gnu.org/releases/dmidecode/dmidecode-${version}.tar.xz dmidecode-${version}.tar.xz 43cba851d8467c9979ccdbeab192eb6638c7d3a697eba5ddb779da8837542212"
 auth_type='sha256'
-
-install() {
-    run make clean
-    run make
-}
-
-post_install() {
-    mkdir -p "${SERENITY_INSTALL_ROOT}/bin"
-    run make install-bin DESTDIR="${SERENITY_INSTALL_ROOT}"
-    ln -sf /usr/local/sbin/dmidecode "${SERENITY_INSTALL_ROOT}/bin/dmidecode"
-    ln -sf /usr/local/sbin/biosdecode "${SERENITY_INSTALL_ROOT}/bin/biosdecode"
-    ln -sf /usr/local/sbin/vpddecode "${SERENITY_INSTALL_ROOT}/bin/vpddecode"
-}
