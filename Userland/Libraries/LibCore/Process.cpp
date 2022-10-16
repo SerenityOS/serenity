ea0a97d08273006a8645b1b13eb7ee53166717a0
cflip <cflip@cflip.net>
2022-10-15 19:04:18 -0600

    LibCore: Add argument to specify working directory in Process::spawn

--- Userland/Libraries/LibCore/Process.cpp
+++ Userland/Libraries/LibCore/Process.cpp
@@ -22,6 +22,7 @@ namespace Core {
 
 struct ArgvList {
     String m_path;
+    String m_working_directory;
     Vector<char const*, 10> m_argv;
 
     ArgvList(String path, size_t size)
@@ -43,25 +44,38 @@ struct ArgvList {
         return m_argv;
     }
 
+    void set_working_directory(String const& working_directory)
+    {
+        m_working_directory = working_directory;
+    }
+
     ErrorOr<pid_t> spawn()
     {
-        auto pid = TRY(System::posix_spawn(m_path.view(), nullptr, nullptr, const_cast<char**>(get().data()), environ));
 #ifdef AK_OS_SERENITY
+        posix_spawn_file_actions_t spawn_actions;
+        posix_spawn_file_actions_init(&spawn_actions);
+        if (!m_working_directory.is_empty())
+            posix_spawn_file_actions_addchdir(&spawn_actions, m_working_directory.characters());
+
+        auto pid = TRY(System::posix_spawn(m_path.view(), &spawn_actions, nullptr, const_cast<char**>(get().data()), environ));
         TRY(System::disown(pid));
+#else
+        auto pid = TRY(System::posix_spawn(m_path.view(), nullptr, nullptr, const_cast<char**>(get().data()), environ));
 #endif
         return pid;
     }
 };
 
-ErrorOr<pid_t> Process::spawn(StringView path, Span<String const> arguments)
+ErrorOr<pid_t> Process::spawn(StringView path, Span<String const> arguments, String working_directory)
 {
     ArgvList argv { path, arguments.size() };
     for (auto const& arg : arguments)
         argv.append(arg.characters());
+    argv.set_working_directory(working_directory);
     return argv.spawn();
 }
 
-ErrorOr<pid_t> Process::spawn(StringView path, Span<StringView const> arguments)
+ErrorOr<pid_t> Process::spawn(StringView path, Span<StringView const> arguments, String working_directory)
 {
     Vector<String> backing_strings;
     backing_strings.ensure_capacity(arguments.size());
@@ -70,14 +84,16 @@ ErrorOr<pid_t> Process::spawn(StringView path, Span<StringView const> arguments)
         backing_strings.append(arg);
         argv.append(backing_strings.last().characters());
     }
+    argv.set_working_directory(working_directory);
     return argv.spawn();
 }
 
-ErrorOr<pid_t> Process::spawn(StringView path, Span<char const* const> arguments)
+ErrorOr<pid_t> Process::spawn(StringView path, Span<char const* const> arguments, String working_directory)
 {
     ArgvList argv { path, arguments.size() };
     for (auto arg : arguments)
         argv.append(arg);
+    argv.set_working_directory(working_directory);
     return argv.spawn();
 }
 
