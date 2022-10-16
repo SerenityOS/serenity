52e1ced52ec1e603df0509a8cd7f1d9ccac23966
Andrew Kaster <akaster@serenityos.org>
2022-10-16 12:28:57 -0600

    Ladybird/cmake: Add Userland/ subdirectories of Lagom binary dir to include path
    Recent Serenity changes put generated files for libraries in
    _deps/lagom-build/Userland/Libraries rather than lagom-build/Libraries.

--- Ladybird/cmake/FetchLagom.cmake
+++ Ladybird/cmake/FetchLagom.cmake
@@ -24,10 +24,11 @@ if (NOT lagom_POPULATED)
     set(BUILD_LAGOM ON CACHE INTERNAL "Build all Lagom targets")
 
     # FIXME: Setting target_include_directories on Lagom libraries might make this unecessary?
-    include_directories(${lagom_SOURCE_DIR}/Userland/Libraries)
-    include_directories(${lagom_BINARY_DIR}/Services)
     include_directories(${lagom_SOURCE_DIR})
+    include_directories(${lagom_SOURCE_DIR}/Userland/Libraries)
     include_directories(${lagom_BINARY_DIR})
+    include_directories(${lagom_BINARY_DIR}/Userland/Services)
+    include_directories(${lagom_BINARY_DIR}/Userland/Libraries)
 
     # We set EXCLUDE_FROM_ALL to make sure that only required Lagom libraries are built
     add_subdirectory(${lagom_SOURCE_DIR}/Meta/Lagom ${lagom_BINARY_DIR} EXCLUDE_FROM_ALL)
