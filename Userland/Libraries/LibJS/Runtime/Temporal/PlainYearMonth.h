24bc63bbf0c8ad7f8b3aec40ae2d58780d824046
Moustafa Raafat <MoustafaRaafat2@gmail.com>
2022-10-16 13:36:59 +0100

    LibJS: Remove trivial operation IsValidISOMonth
    This is an editorial change in the Temporal spec.
    See: https://github.com/tc39/proposal-temporal/commit/a08330a

--- Userland/Libraries/LibJS/Runtime/Temporal/PlainYearMonth.h
+++ Userland/Libraries/LibJS/Runtime/Temporal/PlainYearMonth.h
@@ -43,7 +43,6 @@ struct ISOYearMonth {
 
 ThrowCompletionOr<PlainYearMonth*> to_temporal_year_month(VM&, Value item, Object const* options = nullptr);
 ThrowCompletionOr<ISOYearMonth> regulate_iso_year_month(VM&, double year, double month, StringView overflow);
-bool is_valid_iso_month(u8 month);
 bool iso_year_month_within_limits(i32 year, u8 month);
 ISOYearMonth balance_iso_year_month(double year, double month);
 ThrowCompletionOr<PlainYearMonth*> create_temporal_year_month(VM&, i32 iso_year, u8 iso_month, Object& calendar, u8 reference_iso_day, FunctionObject const* new_target = nullptr);
