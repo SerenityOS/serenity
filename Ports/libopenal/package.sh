13ca2133dc3ffd95306b28c86953065cdf76a28c
cflip <cflip@cflip.net>
2022-10-17 20:11:16 -0600

    Ports/libopenal: Build with SDL2 backend, and disable example programs
    This patch fixes libopenal's build by disabling the alsoft-config
    utility which required qt6, and disabling the example programs that were
    causing compiler errors.
    
    This also forces CMake to build with an SDL2 backend so audio can
    actually be played.

--- Ports/libopenal/package.sh
+++ Ports/libopenal/package.sh
@@ -3,8 +3,14 @@ port=libopenal
 useconfigure=true
 version=1.21.1
 workdir="openal-soft-${version}"
-depends=("ffmpeg")
-configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt")
+depends=('SDL2')
+configopts=(
+    "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
+    '-DALSOFT_NO_CONFIG_UTIL=ON'
+    '-DALSOFT_EXAMPLES=ON'
+    '-DHAVE_SDL2=1'
+    '-DALSOFT_BACKEND_SDL2=ON'
+)
 files="https://openal-soft.org/openal-releases/openal-soft-${version}.tar.bz2 openal-soft-${version}.tar.bz2 c8ad767e9a3230df66756a21cc8ebf218a9d47288f2514014832204e666af5d8"
 auth_type=sha256
 
