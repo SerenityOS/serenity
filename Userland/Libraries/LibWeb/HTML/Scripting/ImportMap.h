d7947995d955705b10c152545e9dfad200f95dd2
networkException <git@nwex.de>
2022-10-23 04:05:11 +0200

    LibWeb: Add the ImportMap struct

---
+++ Userland/Libraries/LibWeb/HTML/Scripting/ImportMap.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2022, networkException <networkexception@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/HashMap.h>
+
+namespace Web::HTML {
+
+using ModuleSpecifierMap = HashMap<String, Optional<AK::URL>>;
+
+// https://html.spec.whatwg.org/multipage/webappapis.html#import-map
+class ImportMap {
+public:
+    ImportMap() = default;
+
+    ModuleSpecifierMap const& imports() const { return m_imports; }
+    ModuleSpecifierMap& imports() { return m_imports; }
+
+    HashMap<AK::URL, ModuleSpecifierMap> const& scopes() const { return m_scopes; }
+    HashMap<AK::URL, ModuleSpecifierMap>& scopes() { return m_scopes; }
+
+private:
+    ModuleSpecifierMap m_imports;
+    HashMap<AK::URL, ModuleSpecifierMap> m_scopes;
+};
+
+}
