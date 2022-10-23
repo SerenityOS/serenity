939374a037d4c91857bb650a0b5f39f2b889a435
Moustafa Raafat <MoustafaRaafat2@gmail.com>
2022-10-23 19:53:23 +0100

    LibJS: Use the UnsignedBigInteger compare_to_double algorithm
    This also avoids an unnecessary copy

--- Userland/Libraries/LibJS/Runtime/Temporal/ZonedDateTime.cpp
+++ Userland/Libraries/LibJS/Runtime/Temporal/ZonedDateTime.cpp
@@ -555,8 +555,7 @@ ThrowCompletionOr<NanosecondsToDaysResult> nanoseconds_to_days(VM& vm, Crypto::S
         return vm.throw_completion<RangeError>(ErrorType::TemporalNanosecondsConvertedToRemainderOfNanosecondsWithOppositeSign);
 
     // 23. If abs(nanoseconds) ≥ abs(dayLengthNs), throw a RangeError exception.
-    auto nanoseconds_absolute = nanoseconds.is_negative() ? nanoseconds.negated_value() : nanoseconds;
-    auto compare_result = nanoseconds_absolute.compare_to_double(fabs(day_length_ns.to_double()));
+    auto compare_result = nanoseconds.unsigned_value().compare_to_double(fabs(day_length_ns.to_double()));
     if (compare_result == Crypto::UnsignedBigInteger::CompareResult::DoubleLessThanBigInt || compare_result == Crypto::UnsignedBigInteger::CompareResult::DoubleEqualsBigInt)
         return vm.throw_completion<RangeError>(ErrorType::TemporalNanosecondsConvertedToRemainderOfNanosecondsLongerThanDayLength);
 
