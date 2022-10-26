e4b3b706e5fd4f7b3d5d0cb927e46a1a58773f02
Kenneth Myhra <kennethmyhra@gmail.com>
2022-10-26 20:44:43 +0200

    Ports: Update curl to 7.86.0

--- Ports/curl/package.sh
+++ Ports/curl/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=curl
-version=7.84.0
+version=7.86.0
 useconfigure=true
-files="https://curl.se/download/curl-${version}.tar.bz2 curl-${version}.tar.bz2 702fb26e73190a3bd77071aa146f507b9817cc4dfce218d2ab87f00cd3bc059d"
+files="https://curl.se/download/curl-${version}.tar.bz2 curl-${version}.tar.bz2 f5ca69db03eea17fa8705bdfb1a9f58d76a46c9010518109bb38f313137e0a28"
 auth_type=sha256
 depends=("ca-certificates" "openssl" "zlib" "zstd")
 configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt")
