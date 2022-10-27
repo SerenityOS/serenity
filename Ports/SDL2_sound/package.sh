6d2ea087a5f04c7207153fcb3fa98edd2ff9c722
krutalevex <krutalevex@mail.ru>
2022-10-27 13:08:24 +0300

    Ports: Update RetroArch + SDL2_sound

--- Ports/SDL2_sound/package.sh
+++ Ports/SDL2_sound/package.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='SDL2_sound'
 version='git'
-_commit='df3fc779774c2c5dc1147239da1af858c88f1a74'
+_commit='301135a6d0d9bb77c9da0b7f809e9a10d579610f'
 workdir="SDL_sound-${_commit}"
 useconfigure='true'
 depends=('SDL2')
-files="https://github.com/icculus/SDL_sound/archive/${_commit}.zip ${_commit}.zip bf655a03ab96a49c4140e19135433d62893c124330955e85e3dfddbe9963bac2"
+files="https://github.com/icculus/SDL_sound/archive/${_commit}.zip ${_commit}.zip d29f90dd5abacf9f818f0b1567fab6b3dc6292d0a942e8e8d1e8f84130eea7a1"
 auth_type='sha256'
 configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt")
 
