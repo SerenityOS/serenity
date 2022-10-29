6614b0ba2cbeb57e98be20947034433a35660157
krutalevex <krutalevex@mail.ru>
2022-10-29 14:55:33 +0300

    Ports: Update NCDU to 2.2.1

--- Ports/ncdu/package.sh
+++ Ports/ncdu/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='ncdu'
-version='1.17'
-files="https://dev.yorhel.nl/download/ncdu-${version}.tar.gz ncdu-${version}.tar.gz 810745a8ed1ab3788c87d3aea4cc1a14edf6ee226f764bcc383e024ba56adbf1"
+version='2.2.1'
+files="https://dev.yorhel.nl/download/ncdu-${version}.tar.gz ncdu-${version}.tar.gz 5e4af8f6bcd8cf7ad8fd3d7900dab1320745a0453101e9e374f9a77f72aed141"
 auth_type='sha256'
 useconfigure='true'
 depends=("ncurses")
