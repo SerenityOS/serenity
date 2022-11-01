3f7b9ed0ffdfe414a67c04ece200c7e3b3bfe8e3
Clemens Wasser <clemens.wasser@gmail.com>
2022-11-01 15:13:43 +0100

    Ports: Add qt5compat port

---
+++ Ports/qt6-qt5compat/package.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port='qt6-qt5compat'
+version='6.2.3'
+workdir="qt5compat-everywhere-src-${version}"
+useconfigure='true'
+files="https://download.qt.io/official_releases/qt/$(cut -d. -f1,2 <<< ${version})/${version}/submodules/qt5compat-everywhere-src-${version}.tar.xz qt6-qt5compat-${version}.tar.xz 1cf89198cf2cf8a5c15336ccd69fa1f39b779feb64117d6bbf5509c21c123f53"
+auth_type='sha256'
+depends=(
+    'qt6-qtbase'
+    'libiconv'
+)
+
+configure() {
+    export LDFLAGS='-liconv'
+    run cmake -GNinja \
+        -DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt \
+        -DCMAKE_CROSSCOMPILING=ON \
+        -DQT_BUILD_TOOLS_WHEN_CROSSCOMPILING=ON \
+        -DQT_HOST_PATH=$(qmake6 -query QT_HOST_PREFIX) \
+        -DQT_HOST_PATH_CMAKE_DIR=$(qmake6 -query QT_HOST_LIBS)/cmake \
+        -DQT_FEATURE_cxx20=ON
+    unset LDFLAGS
+}
+
+build() {
+    run ninja
+}
+
+install() {
+    run ninja install
+}
