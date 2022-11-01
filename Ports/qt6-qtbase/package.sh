097df95a29ce00aa06c8260aec85add279e9d13a
Clemens Wasser <clemens.wasser@gmail.com>
2022-11-01 16:53:28 +0100

    Ports: Enable ssl and ztd features for Qt port

--- Ports/qt6-qtbase/package.sh
+++ Ports/qt6-qtbase/package.sh
@@ -12,9 +12,11 @@ configopts=(
     "-DQT_BUILD_TOOLS_WHEN_CROSSCOMPILING=ON"
     "-DQT_HOST_PATH=/usr"
     "-DQT_FEATURE_cxx20=ON"
+    "-DQT_FEATURE_ssl=ON"
+    "-DQT_FEATURE_zstd=ON"
     "-DINPUT_opengl=no"
 )
-depends=("md4c")
+depends=("md4c" "zstd" "openssl")
 
 QT_HOST_PATH=/usr
 QT_HOST_CMAKE_PATH=${QT_HOST_PATH}/lib64/cmake
