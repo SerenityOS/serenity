c8b1124e689158893344d6a85c587c8f4f2986aa
Xexxa <93391300+Xexxa@users.noreply.github.com>
2022-11-03 15:00:45 +0100

    Ports: Update serenity-theming use latest commit 490a15a

--- Ports/serenity-theming/package.sh
+++ Ports/serenity-theming/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=serenity-theming
-version=f0100c299a1ec3836d319827e24da0c1a8a72f92
+version=490a15af05e378f57891a2dc43178fdc9d4442a0
 workdir="theming-${version}"
-files="https://github.com/SerenityOS/theming/archive/${version}.zip serenity-theming-${version}.zip 5ef72b105b1de8c301b64af2cf910a724b83bb65ac65f1088f5be08eec2704bb"
+files="https://github.com/SerenityOS/theming/archive/${version}.zip serenity-theming-${version}.zip 09ee982972ccf4d6ee65361a7c54f946b3bba2c02a2296367e602fe154cd4c9a"
 auth_type="sha256"
 
 build() {
@@ -23,4 +23,5 @@ post_install() {
     cp -r "${workdir}/emoji" "${SERENITY_INSTALL_ROOT}/res/"
     cp -r "${workdir}/terminal-colors" "${SERENITY_INSTALL_ROOT}/res/"
     cp -r "${workdir}/wallpapers" "${SERENITY_INSTALL_ROOT}/res/"
+    cp "${workdir}/emoji-theming.txt" "${SERENITY_INSTALL_ROOT}/home/anon/Documents/emoji-theming.txt"
 }
