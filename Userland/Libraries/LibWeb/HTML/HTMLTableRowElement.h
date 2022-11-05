f367a581d2c2e2af9443404a30337271d7a0c6ac
Luke Wilde <lukew@serenityos.org>
2022-11-05 15:19:16 +0000

    LibWeb: Implement HTMLTableRowElement.deleteCell

--- Userland/Libraries/LibWeb/HTML/HTMLTableRowElement.h
+++ Userland/Libraries/LibWeb/HTML/HTMLTableRowElement.h
@@ -20,6 +20,8 @@ public:
 
     int row_index() const;
     int section_row_index() const;
+    WebIDL::ExceptionOr<JS::NonnullGCPtr<HTMLTableCellElement>> insert_cell(i32 index);
+    WebIDL::ExceptionOr<void> delete_cell(i32 index);
 
 private:
     HTMLTableRowElement(DOM::Document&, DOM::QualifiedName);
