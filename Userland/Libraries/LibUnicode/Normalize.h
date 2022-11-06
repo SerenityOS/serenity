2d3567ee92afcfcb17305a51b87158e2b9538308
Gunnar Beutner <gbeutner@serenityos.org>
2022-11-06 07:08:56 +0100

    Meta+LibUnicode: Avoid relocations for static unicode data
    Previously the s_decomposition_mappings variable would refer to other
    data in s_decomposition_mappings_data. This would cause thousands of
    avoidable relocations at load time.
    
    This saves about 128kB RAM for each process which uses LibUnicode.

--- Userland/Libraries/LibUnicode/Normalize.h
+++ Userland/Libraries/LibUnicode/Normalize.h
@@ -15,8 +15,8 @@
 
 namespace Unicode {
 
-Optional<CodePointDecomposition const&> code_point_decomposition(u32 code_point);
-Span<CodePointDecomposition const> code_point_decompositions();
+Optional<CodePointDecomposition const> code_point_decomposition(u32 code_point);
+Optional<CodePointDecomposition const> code_point_decomposition_by_index(size_t index);
 
 enum class NormalizationForm {
     NFD,
