267f188fbf26df9d126fc2dc6461ef6d339b8c64
Andreas Kling <kling@serenityos.org>
2022-11-08 19:17:53 +0100

    Ladybird: Add subclass of WebSocket::WebSocketImpl using Qt networking

---
+++ Ladybird/WebSocketImplQt.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2021, Dex♪ <dexes.ttp@gmail.com>
+ * Copyright (c) 2022, Ali Mohammad Pur <mpfard@serenityos.org>
+ * Copyright (c) 2022, the SerenityOS developers.
+ * Copyright (c) 2022, Andreas Kling <kling@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibWebSocket/Impl/WebSocketImpl.h>
+
+class QTcpSocket;
+
+namespace Ladybird {
+
+class WebSocketImplQt final : public WebSocket::WebSocketImpl {
+public:
+    explicit WebSocketImplQt();
+    virtual ~WebSocketImplQt() override;
+
+    virtual void connect(WebSocket::ConnectionInfo const&) override;
+    virtual bool can_read_line() override;
+    virtual ErrorOr<String> read_line(size_t) override;
+    virtual ErrorOr<ByteBuffer> read(int max_size) override;
+    virtual bool send(ReadonlyBytes) override;
+    virtual bool eof() override;
+    virtual void discard_connection() override;
+
+private:
+    OwnPtr<QTcpSocket> m_socket;
+};
+
+}
