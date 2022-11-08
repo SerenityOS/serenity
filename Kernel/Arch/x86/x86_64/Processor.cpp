05db5690f6c40d9f4c65ae8d300a755d61e7d265
Nico Weber <thakis@chromium.org>
2022-11-08 10:02:01 -0500

    Everywhere: Clean up "in in" comment typos
    Includes fetch editorial update
    https://github.com/whatwg/fetch/commit/3cafbdfc39250!

--- Kernel/Arch/x86/x86_64/Processor.cpp
+++ Kernel/Arch/x86/x86_64/Processor.cpp
@@ -65,7 +65,7 @@ FlatPtr Processor::init_context(Thread& thread, bool leave_crit)
     VERIFY(is_kernel_mode());
     VERIFY(g_scheduler_lock.is_locked());
     if (leave_crit) {
-        // Leave the critical section we set up in in Process::exec,
+        // Leave the critical section we set up in Process::exec,
         // but because we still have the scheduler lock we should end up with 1
         VERIFY(in_critical() == 2);
         m_in_critical = 1; // leave it without triggering anything or restoring flags
