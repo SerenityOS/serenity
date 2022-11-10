19fb49a77bfb708cd98906f9f9fb382842532b3a
Alex Chronopoulos <achronop@gmail.com>
2022-11-10 16:59:09 +0100

    AudioServer: Stop re-creating the device stream buffer
    The buffer provided to `OutputMemoryStream` was made a private class
    member. This is because there is no reason to re-create it in every
    iteration. Also, the logic becomes more symmetric with
    `m_zero_filled_buffer` which is already a class member.

--- Userland/Services/AudioServer/Mixer.h
+++ Userland/Services/AudioServer/Mixer.h
@@ -133,6 +133,7 @@ private:
     NonnullRefPtr<Core::ConfigFile> m_config;
     RefPtr<Core::Timer> m_config_write_timer;
 
+    Array<u8, HARDWARE_BUFFER_SIZE_BYTES> m_stream_buffer;
     Array<u8, HARDWARE_BUFFER_SIZE_BYTES> const m_zero_filled_buffer {};
 
     void mix();
