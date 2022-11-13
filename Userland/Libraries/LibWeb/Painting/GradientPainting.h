476acae04feb0be109c4a88c43a59c0fa19a0ea7
MacDue <macdue@dueutil.tech>
2022-11-13 00:33:42 +0000

    LibWeb: Paint `radial-gradient()`s
    This almost looks too easy now :^), but it's just another way to sample
    the gradient line.

--- Userland/Libraries/LibWeb/Painting/GradientPainting.h
+++ Userland/Libraries/LibWeb/Painting/GradientPainting.h
@@ -37,10 +37,16 @@ struct ConicGradientData {
     ColorStopData color_stops;
 };
 
+struct RadialGradientData {
+    ColorStopData color_stops;
+};
+
 LinearGradientData resolve_linear_gradient_data(Layout::Node const&, Gfx::FloatSize const&, CSS::LinearGradientStyleValue const&);
 ConicGradientData resolve_conic_gradient_data(Layout::Node const&, CSS::ConicGradientStyleValue const&);
+RadialGradientData resolve_radial_gradient_data(Layout::Node const&, Gfx::FloatSize const&, CSS::RadialGradientStyleValue const&);
 
 void paint_linear_gradient(PaintContext&, Gfx::IntRect const&, LinearGradientData const&);
 void paint_conic_gradient(PaintContext&, Gfx::IntRect const&, ConicGradientData const&, Gfx::IntPoint position);
+void paint_radial_gradient(PaintContext&, Gfx::IntRect const&, RadialGradientData const&, Gfx::IntPoint position, Gfx::FloatSize const& size);
 
 }
