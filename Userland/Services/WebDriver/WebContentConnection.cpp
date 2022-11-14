652c4506042657b6ba90e484819aed800544edb0
Timothy Flynn <trflynn89@pm.me>
2022-11-14 11:00:31 -0500

    WebDriver: Remove "Services/" prefix from local WebDriver includes
    This is to allow building some of these files from Ladybird. The Build
    directory for Ladybird does not include the "Services/" path.

--- Userland/Services/WebDriver/WebContentConnection.cpp
+++ Userland/Services/WebDriver/WebContentConnection.cpp
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#include <Services/WebDriver/Client.h>
-#include <Services/WebDriver/WebContentConnection.h>
+#include <WebDriver/Client.h>
+#include <WebDriver/WebContentConnection.h>
 
 namespace WebDriver {
 
