7bf8ed8e7aea38b98f0b55a1babb180f7a06c0e3
Andreas Kling <kling@serenityos.org>
2022-11-23 11:32:45 +0100

    LibJS: Move JobCallback functions out-of-line
    This allows JobCallback.h to not include Runtime/AbstractOperations.h
    and FunctionObject.h.

--- Userland/Libraries/LibJS/Runtime/JobCallback.h
+++ Userland/Libraries/LibJS/Runtime/JobCallback.h
@@ -6,9 +6,9 @@
 
 #pragma once
 
-#include <LibJS/Runtime/AbstractOperations.h>
+#include <AK/OwnPtr.h>
+#include <LibJS/Heap/Handle.h>
 #include <LibJS/Runtime/Completion.h>
-#include <LibJS/Runtime/FunctionObject.h>
 
 namespace JS {
 
@@ -22,21 +22,7 @@ struct JobCallback {
     OwnPtr<CustomData> custom_data { nullptr };
 };
 
-// 9.5.2 HostMakeJobCallback ( callback ), https://tc39.es/ecma262/#sec-hostmakejobcallback
-inline JobCallback make_job_callback(FunctionObject& callback)
-{
-    // 1. Return the JobCallback Record { [[Callback]]: callback, [[HostDefined]]: empty }.
-    return { make_handle(&callback) };
-}
-
-// 9.5.3 HostCallJobCallback ( jobCallback, V, argumentsList ), https://tc39.es/ecma262/#sec-hostcalljobcallback
-inline ThrowCompletionOr<Value> call_job_callback(VM& vm, JobCallback& job_callback, Value this_value, MarkedVector<Value> arguments_list)
-{
-    // 1. Assert: IsCallable(jobCallback.[[Callback]]) is true.
-    VERIFY(!job_callback.callback.is_null());
-
-    // 2. Return ? Call(jobCallback.[[Callback]], V, argumentsList).
-    return call(vm, job_callback.callback.cell(), this_value, move(arguments_list));
-}
+JobCallback make_job_callback(FunctionObject& callback);
+ThrowCompletionOr<Value> call_job_callback(VM&, JobCallback&, Value this_value, MarkedVector<Value> arguments_list);
 
 }
