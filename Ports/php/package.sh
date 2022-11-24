5c5cd89c6c701492e57045eb42278e6598a0d9c6
Jelle Raaijmakers <jelle@gmta.nl>
2022-11-24 11:30:20 +0100

    Ports: Clean up PHP's `package.sh`

--- Ports/php/package.sh
+++ Ports/php/package.sh
@@ -4,23 +4,30 @@ useconfigure='true'
 version='8.1.12'
 files="https://www.php.net/distributions/php-${version}.tar.xz php-${version}.tar.xz 08243359e2204d842082269eedc15f08d2eca726d0e65b93fb11f4bfc51bbbab"
 auth_type='sha256'
-depends=("libiconv" "libxml2" "openssl" "readline" "sqlite" "zlib")
+depends=(
+    'libiconv'
+    'libxml2'
+    'openssl'
+    'readline'
+    'sqlite'
+    'zlib'
+)
 configopts=(
-    "--disable-cgi"
-    "--disable-opcache"
-    "--enable-fpm"
+    '--disable-cgi'
+    '--disable-opcache'
+    '--enable-fpm'
     "--prefix=${SERENITY_INSTALL_ROOT}/usr/local"
     "--with-iconv=${SERENITY_INSTALL_ROOT}/usr/local"
-    "--with-openssl"
+    '--with-openssl'
     "--with-readline=${SERENITY_INSTALL_ROOT}/usr/local"
-    "--with-zlib"
-    "--without-pcre-jit"
+    '--with-zlib'
+    '--without-pcre-jit'
 )
-launcher_name="PHP"
-launcher_category="Development"
-launcher_command="/usr/local/bin/php -a"
-launcher_run_in_terminal="true"
-icon_file="win32/build/php.ico"
+launcher_name='PHP'
+launcher_category='Development'
+launcher_command='/usr/local/bin/php -a'
+launcher_run_in_terminal='true'
+icon_file='win32/build/php.ico'
 
 export CFLAGS="-I${SERENITY_INSTALL_ROOT}/usr/include/LibCrypt -I${SERENITY_INSTALL_ROOT}/usr/local/include/libxml2"
 export LIBS='-ldl'
