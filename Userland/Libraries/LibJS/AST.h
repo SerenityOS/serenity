c767535ca27a261e49ebcd38437034b645bf55af
Andreas Kling <kling@serenityos.org>
2022-11-27 12:32:05 +0100

    LibJS: Use move() on the SourceRange in create_ast_node()

--- Userland/Libraries/LibJS/AST.h
+++ Userland/Libraries/LibJS/AST.h
@@ -43,7 +43,7 @@ template<class T, class... Args>
 static inline NonnullRefPtr<T>
 create_ast_node(SourceRange range, Args&&... args)
 {
-    return adopt_ref(*new T(range, forward<Args>(args)...));
+    return adopt_ref(*new T(move(range), forward<Args>(args)...));
 }
 
 class ASTNode : public RefCounted<ASTNode> {
