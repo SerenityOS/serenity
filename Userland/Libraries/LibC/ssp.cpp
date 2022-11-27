5290d7d085a1778b6a15ca101f94a2f0c2809198
Keegan Saunders <keegan@undefinedbehaviour.org>
2022-11-27 15:46:17 -0500

    LibELF: Add stack guard hardening
    Employ the same hardening that glibc and the Linux kernel use for
    generating stack guards: zero the first byte of the guard such that
    if C-style string functions read out of bounds on the stack, we do
    not overwrite or potentially leak the stack guard.

--- Userland/Libraries/LibC/ssp.cpp
+++ Userland/Libraries/LibC/ssp.cpp
@@ -18,6 +18,7 @@
 extern "C" {
 
 extern uintptr_t __stack_chk_guard;
+// Initialized in `initialize_libc` (we leave a placeholder value here before initialization).
 __attribute__((used)) uintptr_t __stack_chk_guard = (uintptr_t)0xc6c7c8c9;
 
 __attribute__((noreturn)) void __stack_chk_fail()
