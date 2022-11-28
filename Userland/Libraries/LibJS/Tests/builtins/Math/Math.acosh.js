cf4daa39415e10d563752e7ca32114818af301ca
davidot <davidot@serenityos.org>
2022-11-28 11:58:21 +0100

    LibJS: Add spec comments and check for edge cases in Math.acosh

--- Userland/Libraries/LibJS/Tests/builtins/Math/Math.acosh.js
+++ Userland/Libraries/LibJS/Tests/builtins/Math/Math.acosh.js
@@ -6,4 +6,7 @@ test("basic functionality", () => {
     expect(Math.acosh(0.5)).toBeNaN();
     expect(Math.acosh(1)).toBeCloseTo(0);
     expect(Math.acosh(2)).toBeCloseTo(1.316957);
+    expect(Math.acosh(NaN)).toBe(NaN);
+    expect(Math.acosh(Number.POSITIVE_INFINITY)).toBe(Number.POSITIVE_INFINITY);
+    expect(Math.acosh(1.0)).toBe(0.0);
 });
