22d14040ab159f20b2e5b6efd12cbf74034e926a
davidot <davidot@serenityos.org>
2022-11-28 12:08:57 +0100

    LibJS: Add spec comments and check for edge cases in Math.tanh

--- Userland/Libraries/LibJS/Tests/builtins/Math/Math.tanh.js
+++ Userland/Libraries/LibJS/Tests/builtins/Math/Math.tanh.js
@@ -5,4 +5,6 @@ test("basic functionality", () => {
     expect(Math.tanh(Infinity)).toBe(1);
     expect(Math.tanh(-Infinity)).toBe(-1);
     expect(Math.tanh(1)).toBeCloseTo(0.7615941559557649);
+    expect(Math.tanh(NaN)).toBe(NaN);
+    expect(Math.tanh(-0.0)).toBe(-0.0);
 });
