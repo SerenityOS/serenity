e9ee0554a5f784971f2632cd2caf1166f05836fb
davidot <davidot@serenityos.org>
2022-11-28 12:06:50 +0100

    LibJS: Add spec comments and check for edge cases in Math.log10

--- Userland/Libraries/LibJS/Tests/builtins/Math/Math.log10.js
+++ Userland/Libraries/LibJS/Tests/builtins/Math/Math.log10.js
@@ -6,4 +6,7 @@ test("basic functionality", () => {
     expect(Math.log10(0)).toBe(-Infinity);
     expect(Math.log10(-2)).toBe(NaN);
     expect(Math.log10(100000)).toBe(5);
+    expect(Math.log10(NaN)).toBe(NaN);
+    expect(Math.log10(Number.POSITIVE_INFINITY)).toBe(Number.POSITIVE_INFINITY);
+    expect(Math.log10(-0.0)).toBe(Number.NEGATIVE_INFINITY);
 });
