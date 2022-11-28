ab19d7c99d1c234151a1bcc6b36b0226e3d7ba25
davidot <davidot@serenityos.org>
2022-11-28 12:23:59 +0100

    LibJS: Enable commented out tests in Math.asin

--- Userland/Libraries/LibJS/Tests/builtins/Math/Math.asin.js
+++ Userland/Libraries/LibJS/Tests/builtins/Math/Math.asin.js
@@ -5,8 +5,8 @@ test("basic functionality", () => {
     expect(Math.asin(null)).toBe(0);
     expect(Math.asin("")).toBe(0);
     expect(Math.asin([])).toBe(0);
-    // FIXME(LibM): expect(Math.asin(1)).toBeCloseTo(1.5707963267948966);
-    // FIXME(LibM): expect(Math.asin(-1)).toBeCloseTo(-1.5707963267948966);
+    expect(Math.asin(1)).toBeCloseTo(1.5707963267948966);
+    expect(Math.asin(-1)).toBeCloseTo(-1.5707963267948966);
     expect(Math.asin()).toBeNaN();
     expect(Math.asin(undefined)).toBeNaN();
     expect(Math.asin([1, 2, 3])).toBeNaN();
