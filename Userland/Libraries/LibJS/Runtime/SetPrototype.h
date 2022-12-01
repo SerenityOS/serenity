7d14c9e9b2d347301a965c9edfe93bee75a14628
Idan Horowitz <idan.horowitz@gmail.com>
2022-12-01 22:49:50 +0200

    LibJS: Implement Set.prototype.isSubsetOf

--- Userland/Libraries/LibJS/Runtime/SetPrototype.h
+++ Userland/Libraries/LibJS/Runtime/SetPrototype.h
@@ -32,6 +32,7 @@ private:
     JS_DECLARE_NATIVE_FUNCTION(intersection);
     JS_DECLARE_NATIVE_FUNCTION(difference);
     JS_DECLARE_NATIVE_FUNCTION(symmetric_difference);
+    JS_DECLARE_NATIVE_FUNCTION(is_subset_of);
 
     JS_DECLARE_NATIVE_FUNCTION(size_getter);
 };
