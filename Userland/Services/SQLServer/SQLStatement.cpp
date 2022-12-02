b13527b8b29adb45625bef529e43f6ed7ac1c3a7
Timothy Flynn <trflynn89@pm.me>
2022-12-02 08:04:05 -0500

    SQLServer: Parse SQL a single time to actually "prepare" the statement
    One of the benefits of prepared statements is that the SQL string is
    parsed just once and re-used. This updates SQLStatement to do just that
    and store the parsed result.

--- Userland/Services/SQLServer/SQLStatement.cpp
+++ Userland/Services/SQLServer/SQLStatement.cpp
@@ -24,12 +24,23 @@ RefPtr<SQLStatement> SQLStatement::statement_for(int statement_id)
 
 static int s_next_statement_id = 0;
 
-SQLStatement::SQLStatement(DatabaseConnection& connection, DeprecatedString sql)
+SQL::ResultOr<NonnullRefPtr<SQLStatement>> SQLStatement::create(DatabaseConnection& connection, StringView sql)
+{
+    auto parser = SQL::AST::Parser(SQL::AST::Lexer(sql));
+    auto statement = parser.next_statement();
+
+    if (parser.has_errors())
+        return SQL::Result { SQL::SQLCommand::Unknown, SQL::SQLErrorCode::SyntaxError, parser.errors()[0].to_deprecated_string() };
+
+    return TRY(adopt_nonnull_ref_or_enomem(new (nothrow) SQLStatement(connection, move(statement))));
+}
+
+SQLStatement::SQLStatement(DatabaseConnection& connection, NonnullRefPtr<SQL::AST::Statement> statement)
     : Core::Object(&connection)
     , m_statement_id(s_next_statement_id++)
-    , m_sql(move(sql))
+    , m_statement(move(statement))
 {
-    dbgln_if(SQLSERVER_DEBUG, "SQLStatement({}, {})", connection.connection_id(), sql);
+    dbgln_if(SQLSERVER_DEBUG, "SQLStatement({})", connection.connection_id());
     s_statements.set(m_statement_id, *this);
 }
 
@@ -47,7 +58,6 @@ void SQLStatement::report_error(SQL::Result result)
     else
         warnln("Cannot return execution error. Client disconnected");
 
-    m_statement = nullptr;
     m_result = {};
 }
 
@@ -61,12 +71,6 @@ void SQLStatement::execute()
     }
 
     deferred_invoke([this] {
-        auto parse_result = parse();
-        if (parse_result.is_error()) {
-            report_error(parse_result.release_error());
-            return;
-        }
-
         VERIFY(!connection()->database().is_null());
 
         auto execution_result = m_statement->execute(connection()->database().release_nonnull());
@@ -91,16 +95,6 @@ void SQLStatement::execute()
             client_connection->async_execution_success(statement_id(), false, 0, m_result->size(), 0);
         }
     });
-}
-
-SQL::ResultOr<void> SQLStatement::parse()
-{
-    auto parser = SQL::AST::Parser(SQL::AST::Lexer(m_sql));
-    m_statement = parser.next_statement();
-
-    if (parser.has_errors())
-        return SQL::Result { SQL::SQLCommand::Unknown, SQL::SQLErrorCode::SyntaxError, parser.errors()[0].to_deprecated_string() };
-    return {};
 }
 
 bool SQLStatement::should_send_result_rows() const
--- Userland/Services/SQLServer/SQLStatement.h
+++ Userland/Services/SQLServer/SQLStatement.h
@@ -18,28 +18,27 @@
 namespace SQLServer {
 
 class SQLStatement final : public Core::Object {
-    C_OBJECT(SQLStatement)
+    C_OBJECT_ABSTRACT(SQLStatement)
 
 public:
+    static SQL::ResultOr<NonnullRefPtr<SQLStatement>> create(DatabaseConnection&, StringView sql);
     ~SQLStatement() override = default;
 
     static RefPtr<SQLStatement> statement_for(int statement_id);
     int statement_id() const { return m_statement_id; }
-    DeprecatedString const& sql() const { return m_sql; }
     DatabaseConnection* connection() { return dynamic_cast<DatabaseConnection*>(parent()); }
     void execute();
 
 private:
-    SQLStatement(DatabaseConnection&, DeprecatedString sql);
-    SQL::ResultOr<void> parse();
+    SQLStatement(DatabaseConnection&, NonnullRefPtr<SQL::AST::Statement> statement);
+
     bool should_send_result_rows() const;
     void next();
     void report_error(SQL::Result);
 
     int m_statement_id;
-    DeprecatedString m_sql;
     size_t m_index { 0 };
-    RefPtr<SQL::AST::Statement> m_statement { nullptr };
+    NonnullRefPtr<SQL::AST::Statement> m_statement;
     Optional<SQL::ResultSet> m_result {};
 };
 
