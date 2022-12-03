d598e7a83682f13de8638e0fcda4c4b7619e2cf3
Andrew Kaster <akaster@serenityos.org>
2022-12-03 09:16:46 -0700

    Ports: Use CMake to build the zstd port
    This makes the port install drop the CMake install files into the
    sysroot, which is friendlier to macOS users. Homebrew CMake really
    likes to pick homebrew zstd, even for cross-builds.

--- Ports/zstd/package.sh
+++ Ports/zstd/package.sh
@@ -3,3 +3,16 @@ port=zstd
 version=1.5.2
 files="https://github.com/facebook/zstd/releases/download/v${version}/zstd-${version}.tar.gz zstd-${version}.tar.gz 7c42d56fac126929a6a85dbc73ff1db2411d04f104fae9bdea51305663a83fd0"
 auth_type=sha256
+useconfigure='true'
+configopts=(
+    "-Sbuild/cmake"
+    "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
+)
+
+configure() {
+    run cmake "${configopts[@]}"
+}
+
+install() {
+    run make install
+}
