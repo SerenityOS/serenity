966a279c73a1c2df1fe7c3b6ef4e19e8f9fbe29e
Andrew Kaster <akaster@serenityos.org>
2022-12-03 09:23:37 -0700

    Ports: Clean up host path detection in qt6-serenity
    Follow the same pattern as the other Qt ports to use qmake to determine
    the location of host binaries and libraries.

--- Ports/qt6-serenity/package.sh
+++ Ports/qt6-serenity/package.sh
@@ -9,15 +9,14 @@ configopts=(
     "-GNinja"
     "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
     "-DCMAKE_CROSSCOMPILING=ON"
-    "-DQT_HOST_PATH=/usr"
 )
 
-QT_HOST_PATH=/usr
-QT_HOST_CMAKE_PATH=${QT_HOST_PATH}/lib64/cmake
-QT_HOST_TOOLS="HostInfo CoreTools GuiTools WidgetsTools"
-QT_HOST_TOOLS_PATH="${QT_HOST_CMAKE_PATH}/Qt6%s/\n"
-
 configure() {
+    QT_HOST_PATH="$(qmake6 -query QT_HOST_PREFIX)"
+    QT_HOST_CMAKE_PATH="$(qmake6 -query QT_HOST_LIBS)/cmake"
+    QT_HOST_TOOLS="HostInfo CoreTools GuiTools WidgetsTools"
+    QT_HOST_TOOLS_PATH="${QT_HOST_CMAKE_PATH}/Qt6%s/\n"
+
     for host_tool in ${QT_HOST_TOOLS}; do
         if [[ ! -d $(printf $QT_HOST_TOOLS_PATH $host_tool) ]]; then
             echo "You need to have Qt installed on the host (path "$(printf $QT_HOST_TOOLS_PATH $host_tool)" is missing"
@@ -27,7 +26,7 @@ configure() {
 
     MERGED_HOST_TOOLS=$(for host_tool in ${QT_HOST_TOOLS}; do echo "-DQt6${host_tool}_DIR=${QT_HOST_CMAKE_PATH}/Qt6${host_tool}/"; done)
 
-    run cmake ${configopts[@]} ${MERGED_HOST_TOOLS}
+    run cmake ${configopts[@]} "-DQT_HOST_PATH=${QT_HOST_PATH}" ${MERGED_HOST_TOOLS}
 }
 
 build() {
