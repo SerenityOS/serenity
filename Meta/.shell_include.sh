0fde7fe3c5f1f4c8732a54eafe6ddfab6e7b54a8
Lucas CHOLLET <lucas.chollet@free.fr>
2022-12-06 18:17:41 +0100

    Meta: Factorize path resolution
    This patch adds the `find_executable()` function that will hopefully
    find executables in a distro-agnostic way and that is (hopefully as
    well) easily upgradable.
    
    The function uses some bash functionalities. So, we now require bash
    for each script that includes `.shell_include.sh`.

--- Meta/.shell_include.sh
+++ Meta/.shell_include.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # shellcheck disable=SC2034
 # SC2034: "Variable appears unused. Verify it or export it."
 #         Those are intentional here, as the file is meant to be included elsewhere.
@@ -16,3 +16,32 @@ die() {
     echo "die: $*"
     exit 1
 }
+
+find_executable() {
+  paths=("/usr/sbin" "/sbin")
+
+  if [ "$(uname -s)" = "Darwin" ]; then
+    paths+=("/usr/local/opt/e2fsprogs/bin" "/usr/local/opt/e2fsprogs/sbin")
+    paths+=("/opt/homebrew/opt/e2fsprogs/bin" "/opt/homebrew/opt/e2fsprogs/sbin")
+  fi
+
+  executable="${1}"
+
+  # Prefer tools from PATH over fallback paths
+  if command -v "${executable}"; then
+    return 0
+  fi
+
+  for path in "${paths[@]}"; do
+    if command -v "${path}/${executable}"; then
+      return 0
+    fi
+  done
+
+  # We return the executable's name back to provide meaningful messages on future failure
+  echo "${executable}"
+}
+
+FUSE2FS_PATH="$(find_executable fuse2fs)"
+RESIZE2FS_PATH="$(find_executable resize2fs)"
+E2FSCK="$(find_executable e2fsck)"
