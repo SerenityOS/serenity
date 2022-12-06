5f116cedbdd253300c0e5caa49c42e5f34c10dcf
Jan200101 <sentrycraft123@gmail.com>
2022-12-06 22:57:10 +0100

    Meta: Detect ccache being passed as the compiler
    This prevents ccache from being invoking itself.
    Icecc fails to ran this way because of recursion detection.

--- Meta/CMake/setup_ccache.cmake
+++ Meta/CMake/setup_ccache.cmake
@@ -2,8 +2,17 @@
 # ccache setup
 #
 
+list(APPEND COMPILERS
+    "CMAKE_C_COMPILER"
+    "CMAKE_CXX_COMPILER"
+)
 find_program(CCACHE_PROGRAM ccache)
 if(CCACHE_PROGRAM)
-    set(CMAKE_C_COMPILER_LAUNCHER "${CCACHE_PROGRAM}" CACHE FILEPATH "Path to a compiler launcher program, e.g. ccache")
-    set(CMAKE_CXX_COMPILER_LAUNCHER "${CCACHE_PROGRAM}" CACHE FILEPATH "Path to a compiler launcher program, e.g. ccache")
+    foreach(compiler ${COMPILERS})
+        get_filename_component(compiler_path "${${compiler}}" REALPATH)
+        get_filename_component(compiler_name "${compiler_path}" NAME)
+        if (NOT ${compiler_name} MATCHES "ccache")
+            set("${compiler}_LAUNCHER" "${CCACHE_PROGRAM}" CACHE FILEPATH "Path to a compiler launcher program, e.g. ccache")
+        endif()
+    endforeach()
 endif()
