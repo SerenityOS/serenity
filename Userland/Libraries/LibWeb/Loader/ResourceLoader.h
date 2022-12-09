484dde03e2d095be14852d74df72f3779d9207a2
Undefine <undefine@undefine.pl>
2022-12-09 12:59:04 +0100

    LibWeb: Display DragonFly in the user agent string
    Previously when build for DragonFly BSD we would error out with
    unknown os error. Now we just define OS_STRING as DragonFly.

--- Userland/Libraries/LibWeb/Loader/ResourceLoader.h
+++ Userland/Libraries/LibWeb/Loader/ResourceLoader.h
@@ -40,6 +40,8 @@ namespace Web {
 #    define OS_STRING "OpenBSD"
 #elif defined(AK_OS_NETBSD)
 #    define OS_STRING "NetBSD"
+#elif defined(AK_OS_DRAGONFLY)
+#    define OS_STRING "DragonFly"
 #else
 #    error Unknown OS
 #endif
