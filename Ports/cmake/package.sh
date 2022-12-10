232b40fefc03b9e1d7b5ccdfe2e4d89029b36717
Andrew Kaster <akaster@serenityos.org>
2022-12-10 16:33:08 -0700

    Ports: Update CMake port to 3.25.1

--- Ports/cmake/package.sh
+++ Ports/cmake/package.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=cmake
-version=3.23.2
-useconfigure=true
-files="https://github.com/Kitware/CMake/releases/download/v$version/cmake-$version.tar.gz cmake-$version.tar.gz f316b40053466f9a416adf981efda41b160ca859e97f6a484b447ea299ff26aa"
-auth_type=sha256
-depends=("bash" "make" "sed" "ncurses" "libuv" "openssl")
-configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt" "-DCMAKE_USE_SYSTEM_LIBRARY_LIBUV=1" "-DCMAKE_USE_OPENSSL=ON" "-GNinja")
+port='cmake'
+version='3.25.1'
+useconfigure='true'
+files="https://github.com/Kitware/CMake/releases/download/v${version}/cmake-${version}.tar.gz cmake-${version}.tar.gz 1c511d09516af493694ed9baf13c55947a36389674d657a2d5e0ccedc6b291d8"
+auth_type='sha256'
+depends=(
+    'bash'
+    'make'
+    'sed'
+    'ncurses'
+    'libuv'
+    'openssl'
+)
+
+configopts=(
+    "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
+    "-DCMAKE_USE_SYSTEM_LIBRARY_LIBUV=1"
+    "-DCMAKE_USE_OPENSSL=ON"
+    "-DBUILD_TESTING=OFF"
+    "-GNinja"
+)
 
 configure() {
     run cmake "${configopts[@]}" .
