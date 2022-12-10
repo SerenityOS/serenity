58abfbfc93b5bcee2904dad56b9734cec701618b
Eli Youngs <eli.m.youngs@gmail.com>
2022-12-09 21:02:50 -0800

    WIP

--- Userland/Utilities/tsort.cpp
+++ Userland/Utilities/tsort.cpp
@@ -19,43 +19,33 @@ enum NodeStatus {
 
 struct Node {
     StringView name;
-    HashTable<Node*> ancestors;
+    HashTable<StringView> ancestors;
     NodeStatus status;
 
     unsigned hash() const { return name.hash(); }
 };
 
-using NodeMap = HashMap<StringView, Node*>;
-using NodeStack = Vector<Node*>;
+using NodeMap = HashMap<StringView, Node>;
+using NodeStack = Vector<Node&>;
 
-static Node* get_or_create_node(NodeMap& map, StringView const key)
-{
-    Optional<Node*> maybe_child_node = map.get(key);
-    if (maybe_child_node.has_value())
-        return maybe_child_node.release_value();
-
-    Node* new_node = new Node { key, HashTable<Node*> {}, NodeStatus::NotSeen };
-    map.set(key, new_node);
-    return map.get(key).release_value();
-}
-
-static void handle_cycle(NodeStack* stack, Node* duplicated_node)
+static void handle_cycle(NodeStack& stack, Node& duplicated_node)
 {
     // Report on a cycle by moving down the stack of dependencies, logging every node
     // between the implicit top of the stack (represented by duplicate_node) and that
     // node's first appearance.
 
     warnln("tsort: The following nodes form a cycle");
-    for (auto it = stack->rbegin(); it != stack->rend(); ++it) {
-        auto* node = *it;
-        warnln("tsort: {}", node->name);
-        node->status = NodeStatus::NotSeen;
-        if (node == duplicated_node)
+
+    for (auto it = stack.rbegin(); it != stack.rend(); ++it) {
+        auto node = *it;
+        node.status = NodeStatus::NotSeen;
+        warnln("tsort: {}", node.name);
+        if (node.name == duplicated_node.name)
             return;
     }
 }
 
-static void prioritize_nodes(Node* start)
+static void prioritize_nodes(Node& start, NodeMap const& node_map)
 {
     // Prioritize (topologically sort) a subset of a directed graph using a depth first
     // search. The "deepest" nodes are the earliest ancestors of all other nodes and
@@ -63,41 +53,43 @@ static void prioritize_nodes(Node* start)
     // chains, this function does not call itself recursively. Instead, the recursive
     // algorithm is implemented on a heap-allocated stack.
 
-    static auto* stack = new NodeStack;
+    static NodeStack stack;
 
-    assert(stack->is_empty());
-    stack->append(start);
+    assert(stack.is_empty());
+    stack.append(start);
 
-    while (!stack->is_empty()) {
-        auto* node = stack->last();
+    while (!stack.is_empty()) {
+        auto node = stack.last();
 
         // If a node has already been prioritized, it can be ignored.
-        if (node->status == NodeStatus::Prioritized) {
-            stack->take_last();
+        if (node.status == NodeStatus::Prioritized) {
+            stack.take_last();
             continue;
         }
 
         // Keep track of which nodes have been seen to detect cycles.
-        node->status = NodeStatus::Seen;
+        node.status = NodeStatus::Seen;
 
-        if (node->ancestors.is_empty()) {
+        if (node.ancestors.is_empty()) {
             // If a node has no remaining ancestors (dependencies), it either never had
             // ancestors, or its ancestors have already been prioritized. In either case,
             // this is now the deepest un-prioritized node, which makes it the next
             // highest priority.
-            node->status = NodeStatus::Prioritized;
-            out("{}\n", stack->take_last()->name);
+            node.status = NodeStatus::Prioritized;
+            outln("{}", stack.take_last().name);
         } else {
-            auto* next_ancestor = node->ancestors.pop();
-            if (next_ancestor->status == NodeStatus::Seen)
+            auto next_ancestor_name = node.ancestors.pop();
+            auto next_ancestor = node_map.get(next_ancestor_name).release_value();
+            if (next_ancestor.status == NodeStatus::Seen)
                 // If the same node is seen multiple times, this represents a cycle in
                 // the graph. To avoid an infinite loop, the duplicate node is not added
                 // to the stack a second time. Instead, the edge is deliberately ignored,
                 // and the topological sort proceeds as though the cycle did not exist.
+
                 handle_cycle(stack, next_ancestor);
             else
                 // Recursively prioritize all ancestors.
-                stack->append(next_ancestor);
+                stack.append(next_ancestor);
         }
     }
 }
@@ -134,19 +126,19 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
         auto ancestor_name = inputs[i];
         auto descendent_name = inputs[i + 1];
 
-        auto* descendent_node = get_or_create_node(node_map, descendent_name);
+        auto& descendent_node = node_map.ensure(descendent_name, [&]() { return Node { descendent_name, HashTable<StringView> {}, NodeStatus::NotSeen }; });
         if (descendent_name != ancestor_name) {
-            auto* ancestor_node = get_or_create_node(node_map, ancestor_name);
-            descendent_node->ancestors.set(ancestor_node);
+            node_map.ensure(ancestor_name, [&]() { return Node { ancestor_name, HashTable<StringView> {}, NodeStatus::NotSeen }; });
+            descendent_node.ancestors.set(ancestor_name);
         }
     }
 
     // Each node must be checked individually, since any node could be disconnected from
     // the rest of the graph.
     for (auto& entry : node_map) {
-        auto* node_to_prioritize = entry.value;
-        if (node_to_prioritize->status == NodeStatus::NotSeen)
-            prioritize_nodes(node_to_prioritize);
+        auto& node_to_prioritize = entry.value;
+        if (node_to_prioritize.status == NodeStatus::NotSeen)
+            prioritize_nodes(node_to_prioritize, node_map);
     }
 
     return 0;
