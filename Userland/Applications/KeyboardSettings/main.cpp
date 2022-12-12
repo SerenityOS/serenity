d22b390567420e95c60928f80124264d9b209049
sbcohen2000 <sbcohen2000@gmail.com>
2022-12-12 13:35:55 -0600

    KeyboardSettings: Add checkbox to enable Caps Lock mapping to Ctrl
    This patch adds an additional control to KeyboardSettings allowing
    the user to map Caps Lock to Ctrl. Previously, this was only possible
    by writing to /sys/kernel/variables/caps_lock_to_ctrl.
    
    Writing to /sys/kernel/variables/caps_lock_to_ctrl requires root
    privileges, but KeyboardSettings will not attempt to elevate
    the privilege of the user if they are not root. Instead, the
    checkbox is rendered as un-editable.

--- Userland/Applications/KeyboardSettings/main.cpp
+++ Userland/Applications/KeyboardSettings/main.cpp
@@ -27,7 +27,9 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
     TRY(Core::System::pledge("stdio rpath recvfd sendfd proc exec"));
     TRY(Core::System::unveil("/res", "r"));
     TRY(Core::System::unveil("/bin/keymap", "x"));
+    TRY(Core::System::unveil("/bin/sysctl", "x"));
     TRY(Core::System::unveil("/sys/kernel/keymap", "r"));
+    TRY(Core::System::unveil("/sys/kernel/variables/caps_lock_to_ctrl", "r"));
     TRY(Core::System::unveil("/etc/Keyboard.ini", "r"));
     TRY(Core::System::unveil(nullptr, nullptr));
 
