83de01043f210530dabbd9e0e92b878af19d757f
Linus Groh <mail@linusgroh.de>
2022-12-13 20:49:50 +0000

    LibJS: Convert GeneratorObject::create() to NonnullGCPtr

--- Userland/Libraries/LibJS/Runtime/GeneratorObject.h
+++ Userland/Libraries/LibJS/Runtime/GeneratorObject.h
@@ -16,7 +16,7 @@ class GeneratorObject final : public Object {
     JS_OBJECT(GeneratorObject, Object);
 
 public:
-    static ThrowCompletionOr<GeneratorObject*> create(Realm&, Value, ECMAScriptFunctionObject*, ExecutionContext, Bytecode::RegisterWindow);
+    static ThrowCompletionOr<NonnullGCPtr<GeneratorObject>> create(Realm&, Value, ECMAScriptFunctionObject*, ExecutionContext, Bytecode::RegisterWindow);
     virtual void initialize(Realm&) override;
     virtual ~GeneratorObject() override = default;
     void visit_edges(Cell::Visitor&) override;
