9a849c10ae4f376034cfa34a730b1de5fa343601
EWouters <6179932+EWouters@users.noreply.github.com>
2022-12-13 05:05:32 +0100

    Ports/git: Update git to version 2.39.0

--- Ports/git/package.sh
+++ Ports/git/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='git'
-version='2.36.1'
-files="https://mirrors.edge.kernel.org/pub/software/scm/git/git-${version}.tar.xz git-${version}.tar.xz 405d4a0ff6e818d1f12b3e92e1ac060f612adcb454f6299f70583058cb508370"
+version='2.39.0'
+files="https://mirrors.edge.kernel.org/pub/software/scm/git/git-${version}.tar.xz git-${version}.tar.xz ba199b13fb5a99ca3dec917b0bd736bc0eb5a9df87737d435eddfdf10d69265b"
 auth_type='sha256'
 useconfigure='true'
 configopts=("--target=${SERENITY_ARCH}-pc-serenity" "--with-lib=${SERENITY_INSTALL_ROOT}/usr/local" "CFLAGS=-DNO_IPV6" "LDFLAGS=-L${SERENITY_INSTALL_ROOT}/usr/local/lib")
