d6f401a4200a57357ee3670fb3c01f4f67196199
Linus Groh <mail@linusgroh.de>
2022-12-13 20:49:50 +0000

    LibJS: Convert PromiseResolvingFunction::create() to NonnullGCPtr

--- Userland/Libraries/LibJS/Runtime/PromiseResolvingFunction.cpp
+++ Userland/Libraries/LibJS/Runtime/PromiseResolvingFunction.cpp
@@ -11,9 +11,9 @@
 
 namespace JS {
 
-PromiseResolvingFunction* PromiseResolvingFunction::create(Realm& realm, Promise& promise, AlreadyResolved& already_resolved, FunctionType function)
+NonnullGCPtr<PromiseResolvingFunction> PromiseResolvingFunction::create(Realm& realm, Promise& promise, AlreadyResolved& already_resolved, FunctionType function)
 {
-    return realm.heap().allocate<PromiseResolvingFunction>(realm, promise, already_resolved, move(function), *realm.intrinsics().function_prototype());
+    return *realm.heap().allocate<PromiseResolvingFunction>(realm, promise, already_resolved, move(function), *realm.intrinsics().function_prototype());
 }
 
 PromiseResolvingFunction::PromiseResolvingFunction(Promise& promise, AlreadyResolved& already_resolved, FunctionType native_function, Object& prototype)
