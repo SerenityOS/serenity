409d8197827e6de4d336aed165cc9da425d366c3
kleines Filmröllchen <filmroellchen@serenityos.org>
2022-12-14 17:19:14 +0100

    LibManual: Add SubsectionNode

---
+++ Userland/Libraries/LibManual/SubsectionNode.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2022, kleines Filmröllchen <filmroellchen@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibManual/SectionNode.h>
+
+namespace Manual {
+
+// A non-toplevel (i.e. not numbered) manual section.
+class SubsectionNode : public SectionNode {
+public:
+    SubsectionNode(NonnullRefPtr<SectionNode> parent, StringView name);
+    virtual ~SubsectionNode() = default;
+
+    virtual Node const* parent() const override;
+    virtual ErrorOr<String> path() const override;
+    virtual ErrorOr<String> name() const override;
+
+protected:
+    NonnullRefPtr<SectionNode> m_parent;
+};
+
+}
