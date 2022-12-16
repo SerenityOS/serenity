SerenityOS/serenity

commit 210bf8adf0c431be05659140fd6de3a2cc5b7d99

LibWeb: Check if block creates BFC even if all it's children are inline
Even if block has all children inline there need to be a check
if it creates BFC because otherwise IFC will be looking in
wrong parent BFC to calculate space used by floats.

Userland/Libraries/LibWeb/Layout/BlockFormattingContext.cpp
@@ -377,12 +377,11 @@ void BlockFormattingContext::layout_block_level_box(Box const& box, BlockContain
 
     OwnPtr<FormattingContext> independent_formatting_context;
     if (!box.is_replaced_box() && box.has_children()) {
-        if (box.children_are_inline()) {
-            layout_inline_children(verify_cast<BlockContainer>(box), layout_mode, box_state.available_inner_space_or_constraints_from(available_space));
+        if (auto independent_formatting_context = create_independent_formatting_context_if_needed(m_state, box)) {
+            independent_formatting_context->run(box, layout_mode, box_state.available_inner_space_or_constraints_from(available_space));
         } else {
-            independent_formatting_context = create_independent_formatting_context_if_needed(m_state, box);
-            if (independent_formatting_context)
-                independent_formatting_context->run(box, layout_mode, box_state.available_inner_space_or_constraints_from(available_space));
+            if (box.children_are_inline())
+                layout_inline_children(verify_cast<BlockContainer>(box), layout_mode, box_state.available_inner_space_or_constraints_from(available_space));
             else
                 layout_block_level_children(verify_cast<BlockContainer>(box), layout_mode, box_state.available_inner_space_or_constraints_from(available_space));
         }

Userland/Libraries/LibWeb/Layout/FormattingContext.cpp
@@ -155,7 +155,8 @@ OwnPtr<FormattingContext> FormattingContext::create_independent_formatting_conte
     }
 
     VERIFY(is_block_formatting_context());
-    VERIFY(!child_box.children_are_inline());
+    if (child_box.children_are_inline())
+        return {};
 
     // The child box is a block container that doesn't create its own BFC.
     // It will be formatted by this BFC.

commit ede555da047f9cd03731bb8e54e0b4a9a39b4414

LibWeb: Fix bug in BFC that independent FC assigned to wrong variable
Bug was introduced in 210bf8adf0c431be05659140fd6de3a2cc5b7d99
It makes new variable for independent_formatting_context instead
of using earlier declared one which means that
parent_context_did_dimension_child_root_box will never be called.

Userland/Libraries/LibWeb/Layout/BlockFormattingContext.cpp
@@ -377,7 +377,8 @@ void BlockFormattingContext::layout_block_level_box(Box const& box, BlockContain
 
     OwnPtr<FormattingContext> independent_formatting_context;
     if (!box.is_replaced_box() && box.has_children()) {
-        if (auto independent_formatting_context = create_independent_formatting_context_if_needed(m_state, box)) {
+        independent_formatting_context = create_independent_formatting_context_if_needed(m_state, box);
+        if (independent_formatting_context) {
             independent_formatting_context->run(box, layout_mode, box_state.available_inner_space_or_constraints_from(available_space));
         } else {
             if (box.children_are_inline())