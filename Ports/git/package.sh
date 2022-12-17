c4b1d0a6417f3cdb4afda7116bf8111078cb2f83
EWouters <6179932+EWouters@users.noreply.github.com>
2022-12-17 08:58:49 +0100

    Ports/git: Make git use OpenSSL in default location [WIP]

--- Ports/git/package.sh
+++ Ports/git/package.sh
@@ -3,12 +3,23 @@ port='git'
 version='2.39.0'
 files="https://mirrors.edge.kernel.org/pub/software/scm/git/git-${version}.tar.xz git-${version}.tar.xz ba199b13fb5a99ca3dec917b0bd736bc0eb5a9df87737d435eddfdf10d69265b"
 auth_type='sha256'
+depends=(
+    'zlib'
+    'curl'
+)
 useconfigure='true'
-configopts=("--target=${SERENITY_ARCH}-pc-serenity" "--with-lib=${SERENITY_INSTALL_ROOT}/usr/local" "CFLAGS=-DNO_IPV6" "LDFLAGS=-L${SERENITY_INSTALL_ROOT}/usr/local/lib")
-depends=("zlib" "curl")
+configopts=(
+    "--target=${SERENITY_ARCH}-pc-serenity"
+    "--with-lib=${SERENITY_INSTALL_ROOT}/usr/local"
+    "CFLAGS=-I${SERENITY_INSTALL_ROOT}/usr/local/ssl/include -DNO_IPV6"
+    "LDFLAGS=-L${SERENITY_INSTALL_ROOT}/usr/local/ssl/lib -L${SERENITY_INSTALL_ROOT}/usr/local/lib"
+)
+makeopts+=(
+    'CURL_LDFLAGS=-lcurl -lssl -lcrypto -lz'
+)
 
 build() {
-    run make "${makeopts[@]}" CURL_LDFLAGS="-lcurl -lssl -lcrypto -lz"
+    run make "${makeopts[@]}" 
     run make strip
 }
 
@@ -25,11 +36,11 @@ post_install() {
 EOF
 }
 
-export NO_PERL=YesPlease
-export NO_PYTHON=YesPlease
-export NO_EXPAT=YesPlease
-export NO_TCLTK=YesPlease
-export ac_cv_fread_reads_directories=no
-export ac_cv_snprintf_returns_bogus=no
-export ac_cv_iconv_omits_bom=no
-export ac_cv_lib_curl_curl_global_init=yes
+export NO_PERL='YesPlease'
+export NO_PYTHON='YesPlease'
+export NO_EXPAT='YesPlease'
+export NO_TCLTK='YesPlease'
+export ac_cv_fread_reads_directories='no'
+export ac_cv_snprintf_returns_bogus='no'
+export ac_cv_iconv_omits_bom='no'
+export ac_cv_lib_curl_curl_global_init='yes'
