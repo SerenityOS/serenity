12632493ecd1b6ac953cc8cd64a154c51f69e819
Timon Kruiper <timonkruiper@gmail.com>
2022-12-20 15:40:42 +0100

    Kernel/aarch64: Add function to convert Exception Class to StringView
    This is useful when printing information about the type of exception
    that happened.

--- Kernel/Arch/aarch64/init.cpp
+++ Kernel/Arch/aarch64/init.cpp
@@ -51,6 +51,7 @@ extern "C" void exception_common(Kernel::TrapFrame const* const trap_frame)
 
         auto esr_el1 = Kernel::Aarch64::ESR_EL1::read();
         dbgln("esr_el1: EC({:#b}) IL({:#b}) ISS({:#b}) ISS2({:#b})", esr_el1.EC, esr_el1.IL, esr_el1.ISS, esr_el1.ISS2);
+        dbgln("Exception Class: {}", Aarch64::exception_class_to_string(esr_el1.EC));
 
         dump_backtrace_from_base_pointer(regs->x[29]);
     }
