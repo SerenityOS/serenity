9cd8c1f44aacf530123f4f9e199f40b4d651a4c2
Stephan Unverwerth <s.unverwerth@serenityos.org>
2022-12-21 15:51:43 +0100

    LibVirtGPU: Drop gl_ prefix from CommandBufferBuilder methods

--- Userland/Libraries/LibVirtGPU/CommandBufferBuilder.h
+++ Userland/Libraries/LibVirtGPU/CommandBufferBuilder.h
@@ -21,7 +21,7 @@ public:
     void append_transfer3d(Protocol::ResourceID resource, size_t width, size_t height = 1, size_t depth = 1, size_t direction = VIRGL_DATA_DIR_GUEST_TO_HOST);
     void append_end_transfers_3d();
     void append_draw_vbo(u32 count);
-    void append_gl_clear(float r, float g, float b);
+    void append_clear(float r, float g, float b);
     void append_set_vertex_buffers(u32 stride, u32 offset, Protocol::ResourceID resource);
     void append_create_blend(Protocol::ObjectHandle handle);
     void append_bind_blend(Protocol::ObjectHandle handle);
@@ -29,7 +29,7 @@ public:
     void append_set_framebuffer_state(Protocol::ObjectHandle drawtarget, Protocol::ObjectHandle depthbuffer = 0);
     void append_create_vertex_elements(Protocol::ObjectHandle handle);
     void append_bind_vertex_elements(Protocol::ObjectHandle handle);
-    void append_gl_viewport();
+    void append_viewport();
     void append_set_framebuffer_state_no_attach();
     void append_set_constant_buffer(Vector<float> const& constant_buffer);
     void append_create_shader(Protocol::ObjectHandle handle, Gallium::ShaderType shader_type, StringView shader_data);
