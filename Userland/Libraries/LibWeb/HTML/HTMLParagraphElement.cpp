99ef6fbd3a3749ec70c5fab4ac811d1c738f40e8
implicitfield <114500360+implicitfield@users.noreply.github.com>
2022-12-21 12:05:03 +0200

    LibWeb: Support more presentational hints from the HTML spec
    Specifically, this adds support for the align attribute when applied to
    heading, paragraph and caption elements.

--- Userland/Libraries/LibWeb/HTML/HTMLParagraphElement.cpp
+++ Userland/Libraries/LibWeb/HTML/HTMLParagraphElement.cpp
@@ -17,4 +17,22 @@ HTMLParagraphElement::HTMLParagraphElement(DOM::Document& document, DOM::Qualifi
 
 HTMLParagraphElement::~HTMLParagraphElement() = default;
 
+// https://html.spec.whatwg.org/multipage/rendering.html#tables-2
+void HTMLParagraphElement::apply_presentational_hints(CSS::StyleProperties& style) const
+{
+    HTMLElement::apply_presentational_hints(style);
+    for_each_attribute([&](auto& name, auto& value) {
+        if (name.equals_ignoring_case("align"sv)) {
+            if (value == "left"sv)
+                style.set_property(CSS::PropertyID::TextAlign, CSS::IdentifierStyleValue::create(CSS::ValueID::Left));
+            else if (value == "right"sv)
+                style.set_property(CSS::PropertyID::TextAlign, CSS::IdentifierStyleValue::create(CSS::ValueID::Right));
+            else if (value == "center"sv)
+                style.set_property(CSS::PropertyID::TextAlign, CSS::IdentifierStyleValue::create(CSS::ValueID::Center));
+            else if (value == "justify"sv)
+                style.set_property(CSS::PropertyID::TextAlign, CSS::IdentifierStyleValue::create(CSS::ValueID::Justify));
+        }
+    });
+}
+
 }
