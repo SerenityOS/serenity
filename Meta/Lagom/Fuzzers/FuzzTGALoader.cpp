b2626d3bc1499332e4029e154a8eb7b553782a6b
Liav A <liavalb@gmail.com>
2022-12-31 13:37:35 +0200

    Lagom/Fuzzers: Add fuzzer for the TGALoader code

---
+++ Meta/Lagom/Fuzzers/FuzzTGALoader.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2022, Liav A. <liavalb@hotmail.co.il>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibGfx/TGALoader.h>
+#include <stddef.h>
+#include <stdint.h>
+
+extern "C" int LLVMFuzzerTestOneInput(uint8_t const* data, size_t size)
+{
+    Gfx::TGAImageDecoderPlugin decoder(data, size);
+    (void)decoder.frame(0);
+    return 0;
+}
