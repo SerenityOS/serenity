0a8cf0b7b33851d253738fadbb96afa53d4926a2
djwisdom <djwisdom@gmail.com>
2023-01-04 06:18:05 +0800

    Ports: Update serenity-theming use latest commit 7a39b7d

--- Ports/serenity-theming/package.sh
+++ Ports/serenity-theming/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=serenity-theming
-version=b621077636f00a2b78ba09d5596777e8ac061bcc
+version=7a39b7d6830aeba13ba88bb6e14a7a0d0825a370
 workdir="theming-${version}"
-files="https://github.com/SerenityOS/theming/archive/${version}.zip serenity-theming-${version}.zip 4f9304facd751b6096ee8a7617c80cedeb273d19336994712ba50a5b6c165ea3"
+files="https://github.com/SerenityOS/theming/archive/${version}.zip serenity-theming-${version}.zip e2b587906ce4d96a9d91f9774c204c3c82707e9d4763d6bc6b23966a6162381a"
 auth_type="sha256"
 
 build() {
@@ -22,7 +22,7 @@ post_install() {
     cp -r "${workdir}/color-palettes" "${SERENITY_INSTALL_ROOT}/res/"
     cp -r "${workdir}/emoji" "${SERENITY_INSTALL_ROOT}/res/"
     # FIXME: Update this path upstream.
-    cp -r "${workdir}/terminal-colors/*" "${SERENITY_INSTALL_ROOT}/res/color-schemes/"
+    cp -r "${workdir}/color-schemes/" "${SERENITY_INSTALL_ROOT}/res/color-schemes/"
     cp -r "${workdir}/wallpapers" "${SERENITY_INSTALL_ROOT}/res/"
     cp "${workdir}/emoji-theming.txt" "${SERENITY_INSTALL_ROOT}/home/anon/Documents/emoji-theming.txt"
 }
