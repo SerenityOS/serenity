b9e4b7c34e6a2cd89a1f1a7a5f177d31bf24820c
Timothy Flynn <trflynn89@pm.me>
2023-01-05 12:56:08 -0500

    LibJS: Add and begin using a completion-compatible string builder
    ThrowableStringBuilder is a thin wrapper around StringBuilder to map
    results from the try_* methods to a throw completion. This will let us
    try to throw on OOM conditions rather than just blowing up.

---
+++ Userland/Libraries/LibJS/Runtime/ThrowableStringBuilder.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2023, Tim Flynn <trflynn89@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/StringBuilder.h>
+#include <AK/StringView.h>
+#include <LibJS/Forward.h>
+#include <LibJS/Runtime/Completion.h>
+#include <LibJS/Runtime/ErrorTypes.h>
+#include <LibJS/Runtime/VM.h>
+
+namespace JS {
+
+class ThrowableStringBuilder : public AK::StringBuilder {
+public:
+    explicit ThrowableStringBuilder(VM&);
+
+    ThrowCompletionOr<void> append(char);
+    ThrowCompletionOr<void> append(StringView);
+    ThrowCompletionOr<void> append(Utf16View const&);
+    ThrowCompletionOr<void> append_code_point(u32 value);
+
+private:
+    VM& m_vm;
+};
+
+}
