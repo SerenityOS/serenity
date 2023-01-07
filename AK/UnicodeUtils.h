4306bbc1d20ef715ed3e9999183701ddf5f9fe62
Timothy Flynn <trflynn89@pm.me>
2023-01-07 14:48:20 -0500

    AK: Make StringBuilder::try_append_code_point actually fallible
    It currently uses the non-fallible `append` method to append each UTF-8
    encoded byte of the code point.

--- AK/UnicodeUtils.h
+++ AK/UnicodeUtils.h
@@ -6,6 +6,8 @@
 
 #pragma once
 
+#include <AK/Concepts.h>
+#include <AK/Error.h>
 #include <AK/Forward.h>
 
 namespace AK::UnicodeUtils {
@@ -30,6 +32,34 @@ template<typename Callback>
         callback((char)(((code_point >> 12) & 0x3f) | 0x80));
         callback((char)(((code_point >> 6) & 0x3f) | 0x80));
         callback((char)(((code_point >> 0) & 0x3f) | 0x80));
+        return 4;
+    }
+    return -1;
+}
+
+template<FallibleFunction<char> Callback>
+[[nodiscard]] ErrorOr<int> try_code_point_to_utf8(u32 code_point, Callback&& callback)
+{
+    if (code_point <= 0x7f) {
+        TRY(callback(static_cast<char>(code_point)));
+        return 1;
+    }
+    if (code_point <= 0x07ff) {
+        TRY(callback(static_cast<char>((((code_point >> 6) & 0x1f) | 0xc0))));
+        TRY(callback(static_cast<char>((((code_point >> 0) & 0x3f) | 0x80))));
+        return 2;
+    }
+    if (code_point <= 0xffff) {
+        TRY(callback(static_cast<char>((((code_point >> 12) & 0x0f) | 0xe0))));
+        TRY(callback(static_cast<char>((((code_point >> 6) & 0x3f) | 0x80))));
+        TRY(callback(static_cast<char>((((code_point >> 0) & 0x3f) | 0x80))));
+        return 3;
+    }
+    if (code_point <= 0x10ffff) {
+        TRY(callback(static_cast<char>((((code_point >> 18) & 0x07) | 0xf0))));
+        TRY(callback(static_cast<char>((((code_point >> 12) & 0x3f) | 0x80))));
+        TRY(callback(static_cast<char>((((code_point >> 6) & 0x3f) | 0x80))));
+        TRY(callback(static_cast<char>((((code_point >> 0) & 0x3f) | 0x80))));
         return 4;
     }
     return -1;
