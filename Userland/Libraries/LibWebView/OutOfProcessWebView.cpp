ab57fde8ecef1a4a6985f02afef6c6036585fbe8
Karol Kosek <krkk@serenityos.org>
2023-01-08 23:20:44 +0100

    LibWebView: Port to Core::Stream

--- Userland/Libraries/LibWebView/OutOfProcessWebView.cpp
+++ Userland/Libraries/LibWebView/OutOfProcessWebView.cpp
@@ -525,11 +525,11 @@ Gfx::IntRect OutOfProcessWebView::notify_server_did_request_fullscreen_window()
 
 void OutOfProcessWebView::notify_server_did_request_file(Badge<WebContentClient>, DeprecatedString const& path, i32 request_id)
 {
-    auto file = FileSystemAccessClient::Client::the().try_request_file_read_only_approved_deprecated(window(), path);
+    auto file = FileSystemAccessClient::Client::the().request_file_read_only_approved(window(), path);
     if (file.is_error())
         client().async_handle_file_return(file.error().code(), {}, request_id);
     else
-        client().async_handle_file_return(0, IPC::File(file.value()->leak_fd()), request_id);
+        client().async_handle_file_return(0, IPC::File(file.value().stream()), request_id);
 }
 
 void OutOfProcessWebView::did_scroll()
