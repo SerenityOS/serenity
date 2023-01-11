10ca4cb6ef7df1778bb5064fccf7c4685a6919bd
Linus Groh <mail@linusgroh.de>
2023-01-11 20:30:30 +0000

    LibCore: Add StandardPaths::font_directories()
    This provides a list of well-known directories containing font files,
    e.g. for use with Gfx::FontDatabase::load_all_fonts_from_path().

--- Userland/Libraries/LibCore/StandardPaths.h
+++ Userland/Libraries/LibCore/StandardPaths.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2020, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2023, Linus Groh <linusg@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -21,6 +22,7 @@ public:
     static DeprecatedString config_directory();
     static DeprecatedString data_directory();
     static ErrorOr<DeprecatedString> runtime_directory();
+    static ErrorOr<Vector<String>> font_directories();
 };
 
 }
