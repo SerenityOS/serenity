514224c015c28fc57e605f9ffb44d9ca48902e5b
MacDue <macdue@dueutil.tech>
2023-01-12 22:14:12 +0000

    AK: Fix ignored .to_string() errors in IPv6Address

--- AK/IPv6Address.h
+++ AK/IPv6Address.h
@@ -284,7 +284,7 @@ template<>
 struct Formatter<IPv6Address> : Formatter<ErrorOr<NonnullOwnPtr<Kernel::KString>>> {
     ErrorOr<void> format(FormatBuilder& builder, IPv6Address const& value)
     {
-        return Formatter<ErrorOr<NonnullOwnPtr<Kernel::KString>>>::format(builder, value.to_string());
+        return Formatter<ErrorOr<NonnullOwnPtr<Kernel::KString>>>::format(builder, TRY(value.to_string()));
     }
 };
 #else
