19bae983e46d7334f926842b91382851005a2aab
Niklas Poslovski <ni.pos@yandex.com>
2023-01-13 20:33:54 +0100

    LibCore: Enable file descriptor passing on FreeBSD
    Ladybird currently doesn't render any webpages on FreeBSD and throws
    hundreds of errors,beginning with this:
    IPC::ConnectionBase (0x0000000805bf2b00) had an error (File descriptor
    passing not supported on this platform), disconnecting.
    WebContent process crashed!

--- Userland/Libraries/LibCore/Stream.cpp
+++ Userland/Libraries/LibCore/Stream.cpp
@@ -579,7 +579,7 @@ ErrorOr<int> LocalSocket::receive_fd(int flags)
 {
 #if defined(AK_OS_SERENITY)
     return Core::System::recvfd(m_helper.fd(), flags);
-#elif defined(AK_OS_LINUX) || defined(AK_OS_MACOS)
+#elif defined(AK_OS_LINUX) || defined(AK_OS_MACOS) || defined(AK_OS_FREEBSD)
     union {
         struct cmsghdr cmsghdr;
         char control[CMSG_SPACE(sizeof(int))];
@@ -620,7 +620,7 @@ ErrorOr<void> LocalSocket::send_fd(int fd)
 {
 #if defined(AK_OS_SERENITY)
     return Core::System::sendfd(m_helper.fd(), fd);
-#elif defined(AK_OS_LINUX) || defined(AK_OS_MACOS)
+#elif defined(AK_OS_LINUX) || defined(AK_OS_MACOS) || defined(AK_OS_FREEBSD)
     char c = 'F';
     struct iovec iov {
         .iov_base = &c,
