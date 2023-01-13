471cf9cb6f6d45cef450a72a0c6921878c058369
MacDue <macdue@dueutil.tech>
2023-01-13 23:44:44 +0000

    LibWeb: Remove some unnecessary .release_value()s in StyleValue
    And instead propagate these errors.
    Also while here add my copyright (I've touched these files a lot)

--- Userland/Libraries/LibWeb/CSS/StyleValue.h
+++ Userland/Libraries/LibWeb/CSS/StyleValue.h
@@ -2,6 +2,7 @@
  * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
  * Copyright (c) 2021, Tobias Christiansen <tobyase@serenityos.org>
  * Copyright (c) 2021-2023, Sam Atkins <atkinssj@serenityos.org>
+ * Copyright (c) 2022-2023, MacDue <macdue@dueutil.tech>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -132,7 +133,7 @@ struct PositionValue {
     VerticalEdge y_relative_to { VerticalEdge::Top };
 
     CSSPixelPoint resolved(Layout::Node const& node, CSSPixelRect const& rect) const;
-    void serialize(StringBuilder&) const;
+    ErrorOr<void> serialize(StringBuilder&) const;
     bool operator==(PositionValue const&) const;
 };
 
