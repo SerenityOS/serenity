01db302a33d6441e4351089aae7128423a27751d
Liav A <liavalb@gmail.com>
2023-01-14 03:30:09 +0200

    Utilities: Add new utility for converting images to raw bitmap binaries
    I used this utility to check if the possible TGA images' cases for
    different origins (explictly the Y origin) are generating the same
    bitmap, as I felt that my eyes are not a good-enough measurement tool
    for this kind of task.
    This might be useful in the future for testing other implementations so
    I rather have this nice utility in our codebase.

---
+++ Userland/Utilities/image2bin.cpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2023, Liav A. <liavalb@hotmail.co.il>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <AK/DeprecatedString.h>
+#include <AK/Random.h>
+#include <AK/StringBuilder.h>
+#include <LibCore/ArgsParser.h>
+#include <LibCore/DirIterator.h>
+#include <LibCore/System.h>
+#include <LibGUI/Application.h>
+#include <LibGUI/Desktop.h>
+#include <LibMain/Main.h>
+
+ErrorOr<int> serenity_main(Main::Arguments arguments)
+{
+    TRY(Core::System::pledge("stdio rpath unix"));
+    DeprecatedString path;
+
+    Core::ArgsParser args_parser;
+    args_parser.add_positional_argument(path, "Path to image", "path");
+    args_parser.parse(arguments);
+
+    auto bitmap = TRY(Gfx::Bitmap::try_load_from_file(path));
+
+    TRY(Core::System::pledge("stdio"));
+    Vector<u8> data;
+    for (auto height = 0; height < bitmap->size().height(); height++) {
+        auto* scanline = bitmap->scanline_u8(height);
+        for (auto byte_index_in_row = 0u; byte_index_in_row < bitmap->pitch(); byte_index_in_row++) {
+            TRY(data.try_append(scanline[byte_index_in_row]));
+        }
+    }
+    VERIFY(data.size() == bitmap->size_in_bytes());
+    TRY(Core::System::write(STDOUT_FILENO, data.span()));
+    return 0;
+}
