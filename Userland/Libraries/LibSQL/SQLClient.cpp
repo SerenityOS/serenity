cf663c6208681c0b1ca73d6e8237200d64723774
nipos <ni.pos@yandex.com>
2023-01-14 09:52:03 +0100

    LibSQL: Don't do fchmod on FreeBSD

--- Userland/Libraries/LibSQL/SQLClient.cpp
+++ Userland/Libraries/LibSQL/SQLClient.cpp
@@ -37,7 +37,7 @@ static ErrorOr<int> create_database_socket(DeprecatedString const& socket_path)
     TRY(Core::System::fcntl(socket_fd, F_SETFD, FD_CLOEXEC));
 #    endif
 
-#    ifndef AK_OS_MACOS
+#    if !defined(AK_OS_MACOS) && !defined(AK_OS_FREEBSD)
     TRY(Core::System::fchmod(socket_fd, 0600));
 #    endif
 
