614cb7548bf22b1f9f2626f5d582223f2ae29308
Andrew Kaster <andrewdkaster@gmail.com>
2023-01-16 13:52:01 -0700

    CMake: Add definitions required to start building with clang-cl
    clang.exe and clang-cl.exe both work after these changes.
    The only difference is that the WIN32 CMake checks apply to both,
    while the MSVC checks only apply to clang-cl. Technically the
    MSVC checks apply to cl.exe as well, but there's way too much
    gcc-style intrinsics use in our code to make that viable.
    
    clang.exe can be used like so in a VS 2022 Developer Powershell:
    ```
    cmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang
        -GNinja -B Build/lagom -S Meta/Lagom
    ```
    
    Both compilers still can't compile LibCore, but these cmake changes
    make that the fault of the code, rather than the compiler options.

--- Meta/CMake/common_compile_options.cmake
+++ Meta/CMake/common_compile_options.cmake
@@ -2,8 +2,13 @@ set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
-add_compile_options(-Wall)
-add_compile_options(-Wextra)
+if (MSVC)
+    # -Wall with clang-cl is equivalent to -Weverything, which is extremely noisy
+    add_compile_options(/W4)
+else()
+    add_compile_options(-Wall)
+    add_compile_options(-Wextra)
+endif()
 
 if (NOT CMAKE_HOST_SYSTEM_NAME MATCHES SerenityOS)
     # FIXME: Something makes this go crazy and flag unused variables that aren't flagged as such when building with the toolchain.
