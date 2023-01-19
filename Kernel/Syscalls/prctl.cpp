b27f88f61de989b5c97f668a95467dbb0c6234f4
Liav A <liavalb@gmail.com>
2023-01-20 00:37:14 +0200

    Kernel+Userland: Refine preventing syscall annotations of Regions option
    Instead of using a special case of the annotate_mapping syscall, let's
    introduce a new prctl option to disallow further annotations of Regions
    as new syscall Region(s).

--- Kernel/Syscalls/prctl.cpp
+++ Kernel/Syscalls/prctl.cpp
@@ -21,6 +21,22 @@ ErrorOr<FlatPtr> Process::sys$prctl(int option, FlatPtr arg1, [[maybe_unused]] F
                 return EINVAL;
             protected_data.dumpable = arg1;
             return 0;
+        case PR_GET_NO_NEW_SYSCALL_REGION_ANNOTATIONS:
+            return address_space().with([&](auto& space) -> ErrorOr<FlatPtr> {
+                return space->enforces_syscall_regions();
+            });
+        case PR_SET_NO_NEW_SYSCALL_REGION_ANNOTATIONS:
+            if (arg1 != 0 && arg1 != 1)
+                return EINVAL;
+            bool prohibit_new_annotated_syscall_regions = (arg1 == 1);
+            return address_space().with([&](auto& space) -> ErrorOr<FlatPtr> {
+                if (space->enforces_syscall_regions() && !prohibit_new_annotated_syscall_regions)
+                    return EPERM;
+
+                space->set_enforces_syscall_regions(prohibit_new_annotated_syscall_regions);
+                return 0;
+            });
+            return 0;
         }
         return EINVAL;
     });
