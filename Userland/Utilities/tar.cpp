0245614a4fb7cf297d0e1c183fffc327de0b2162
Tim Schumacher <timschumi@gmx.de>
2023-01-20 00:15:02 +0100

    LibCore: Remove `FileStream`

--- Userland/Utilities/tar.cpp
+++ Userland/Utilities/tar.cpp
@@ -14,7 +14,6 @@
 #include <LibCore/DirIterator.h>
 #include <LibCore/Directory.h>
 #include <LibCore/File.h>
-#include <LibCore/FileStream.h>
 #include <LibCore/Stream.h>
 #include <LibCore/System.h>
 #include <LibMain/Main.h>
