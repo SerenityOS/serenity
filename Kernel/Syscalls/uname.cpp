79e27dad84d400ed148040db9c0b919e5ac838b2
konrad <konrad@serenityos.org>
2023-01-27 17:56:31 +0100

    Kernel: Use platform string in `uname` syscall

--- Kernel/Syscalls/uname.cpp
+++ Kernel/Syscalls/uname.cpp
@@ -6,8 +6,10 @@
  */
 
 #include <AK/TypedTransfer.h>
+#include <Kernel/Arch/Processor.h>
 #include <Kernel/Process.h>
 #include <Kernel/Version.h>
+#include <string.h>
 
 namespace Kernel {
 
@@ -16,25 +18,22 @@ ErrorOr<FlatPtr> Process::sys$uname(Userspace<utsname*> user_buf)
     VERIFY_NO_PROCESS_BIG_LOCK(this);
     TRY(require_promise(Pledge::stdio));
 
-    utsname buf
-    {
+    utsname buf {
         "SerenityOS",
-            {}, // Hostname, filled in below.
-            {}, // "Release" (1.0-dev), filled in below.
-            {}, // "Revision" (git commit hash), filled in below.
-#if ARCH(X86_64)
-            "x86_64",
-#elif ARCH(AARCH64)
-            "AArch64",
-#else
-#    error Unknown architecture
-#endif
+        {}, // Hostname, filled in below.
+        {}, // "Release" (1.0-dev), filled in below.
+        {}, // "Revision" (git commit hash), filled in below.
+        {}, // "Machine" (platform/architecture), filled in below.
     };
 
     auto version_string = TRY(KString::formatted("{}.{}-dev", SERENITY_MAJOR_REVISION, SERENITY_MINOR_REVISION));
     AK::TypedTransfer<u8>::copy(reinterpret_cast<u8*>(buf.release), version_string->bytes().data(), min(version_string->length(), UTSNAME_ENTRY_LEN - 1));
 
     AK::TypedTransfer<u8>::copy(reinterpret_cast<u8*>(buf.version), SERENITY_VERSION.bytes().data(), min(SERENITY_VERSION.length(), UTSNAME_ENTRY_LEN - 1));
+
+    if (!Processor::platform_string().copy_characters_to_buffer(buf.machine, UTSNAME_ENTRY_LEN - 1)) {
+        return EILSEQ;
+    }
 
     hostname().with_shared([&](auto const& name) {
         auto length = min(name->length(), UTSNAME_ENTRY_LEN - 1);
