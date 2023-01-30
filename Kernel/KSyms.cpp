b6d3f0d78772cc16b113f7bee85290738f7b15b7
Timon Kruiper <timonkruiper@gmail.com>
2023-01-30 10:51:40 +0100

    Kernel/aarch64: Set kernel_load_base and correctly calculate symbol addr
    Setting the kernel_load_base variable caused backtracking to regress, so
    to have proper backtracing the calculation of the symbol address in
    KSyms.cpp needs to keep into account that the aarch64 kernel is linked
    at a high virtual memory address.

--- Kernel/KSyms.cpp
+++ Kernel/KSyms.cpp
@@ -84,7 +84,17 @@ UNMAP_AFTER_INIT static void load_kernel_symbols_from_data(Bytes buffer)
             }
         }
         auto& ksym = s_symbols[current_symbol_index];
+
+        // FIXME: Remove this ifdef once the aarch64 kernel is loaded by the Prekernel.
+        //        Currently, the aarch64 kernel is linked at a high virtual memory address, instead
+        //        of zero, so the address of a symbol does not need to be offset by the kernel_load_base.
+#if ARCH(X86_64)
         ksym.address = kernel_load_base + address;
+#elif ARCH(AARCH64)
+        ksym.address = address;
+#else
+#    error "Unknown architecture"
+#endif
         ksym.name = start_of_name;
 
         *bufptr = '\0';
