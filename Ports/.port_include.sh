SerenityOS/serenity

commit 9b7e217dda0c45f0733f89a48e2cf483a6ba0a5e

Ports: Support multiple port directories
This allows Ports unfit for the main repository to be put elsewhere.

Ports/.port_include.sh
@@ -528,7 +528,17 @@ package_install_state() {
 installdepends() {
     for depend in "${depends[@]}"; do
         if [ -z "$(package_install_state $depend)" ]; then
-            (cd "${PORT_META_DIR}/../$depend" && ./package.sh --auto)
+            # Split colon seperated string into a list
+            IFS=':' read -ra port_directories <<< "$SERENITY_PORT_DIRS"
+            for port_dir in "${port_directories[@]}"; do
+                if [ -d "${port_dir}/$depend" ]; then
+                    (cd "${port_dir}/$depend" && ./package.sh --auto)
+                    return
+                fi
+            done
+
+            >&2 echo "Error: Dependency $depend could not be found."
+            exit 1
         fi
     done
 }

Ports/.hosted_defs.sh
@@ -56,3 +56,4 @@ export PKG_CONFIG_SYSROOT_DIR="${SERENITY_BUILD_DIR}/Root"
 export PKG_CONFIG_LIBDIR="${PKG_CONFIG_SYSROOT_DIR}/usr/local/lib/pkgconfig"
 
 export SERENITY_INSTALL_ROOT="${SERENITY_BUILD_DIR}/Root"
+export SERENITY_PORT_DIRS="${SERENITY_PORT_DIRS:+${SERENITY_PORT_DIRS}:}${SERENITY_SOURCE_DIR}/Ports"

commit dda05c5af285f3f6df73594777b177e27bfaf469

Ports: Install all dependencies instead of just one
Commit 9b7e217dda0c45 broke installation of port dependencies by
`return`ing as soon as the first dependency was found.

Ports/.port_include.sh
@@ -527,19 +527,21 @@ package_install_state() {
 }
 installdepends() {
     for depend in "${depends[@]}"; do
-        if [ -z "$(package_install_state $depend)" ]; then
-            # Split colon seperated string into a list
-            IFS=':' read -ra port_directories <<< "$SERENITY_PORT_DIRS"
-            for port_dir in "${port_directories[@]}"; do
-                if [ -d "${port_dir}/$depend" ]; then
-                    (cd "${port_dir}/$depend" && ./package.sh --auto)
-                    return
-                fi
-            done
-
-            >&2 echo "Error: Dependency $depend could not be found."
-            exit 1
+        if [ -n "$(package_install_state $depend)" ]; then
+            continue
         fi
+
+        # Split colon separated string into a list
+        IFS=':' read -ra port_directories <<< "$SERENITY_PORT_DIRS"
+        for port_dir in "${port_directories[@]}"; do
+            if [ -d "${port_dir}/$depend" ]; then
+                (cd "${port_dir}/$depend" && ./package.sh --auto)
+                continue 2
+            fi
+        done
+
+        >&2 echo "Error: Dependency $depend could not be found."
+        exit 1
     done
 }
 uninstall() {