65cf562a71f24f9475c82981c77f5424e7bf845c
martinfalisse <martinmotteditfalisse@gmail.com>
2023-02-01 18:47:56 +0100

    Ladybird: Add `ak_string_from_qstring` function
    This will be used later when doing the conversion from the QString in
    QSettings for the homepage URL.

--- Ladybird/Utilities.cpp
+++ Ladybird/Utilities.cpp
@@ -19,6 +19,11 @@ AK::DeprecatedString ak_deprecated_string_from_qstring(QString const& qstring)
     return AK::DeprecatedString(qstring.toUtf8().data());
 }
 
+ErrorOr<String> ak_string_from_qstring(QString const& qstring)
+{
+    return String::from_utf8(StringView(qstring.toUtf8().data(), qstring.size()));
+}
+
 QString qstring_from_ak_deprecated_string(AK::DeprecatedString const& ak_deprecated_string)
 {
     return QString::fromUtf8(ak_deprecated_string.characters(), ak_deprecated_string.length());
