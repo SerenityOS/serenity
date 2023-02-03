89da8de4cab589885fcaf8940b6e9022c637e24c
Timothy Flynn <trflynn89@pm.me>
2023-02-02 19:54:47 -0500

    LibJS+LibLocale: Propagate OOM from CLDR NumberFormat Vector operations

--- Userland/Libraries/LibLocale/NumberFormat.h
+++ Userland/Libraries/LibLocale/NumberFormat.h
@@ -69,7 +69,7 @@ ErrorOr<String> replace_digits_for_number_system(StringView system, StringView n
 
 ErrorOr<Optional<NumberFormat>> get_standard_number_system_format(StringView locale, StringView system, StandardNumberFormatType type);
 ErrorOr<Vector<NumberFormat>> get_compact_number_system_formats(StringView locale, StringView system, CompactNumberFormatType type);
-Vector<NumberFormat> get_unit_formats(StringView locale, StringView unit, Style style);
+ErrorOr<Vector<NumberFormat>> get_unit_formats(StringView locale, StringView unit, Style style);
 
 ErrorOr<Optional<String>> augment_currency_format_pattern(StringView currency_display, StringView base_pattern);
 ErrorOr<Optional<String>> augment_range_pattern(StringView range_separator, StringView lower, StringView upper);
