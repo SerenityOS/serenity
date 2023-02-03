7b92eff4a6b85398407ebc1c3ecac94cdaf975a6
Zaggy1024 <zaggy1024@gmail.com>
2023-02-02 23:14:59 -0600

    LibVideo/VP9: Use u32 to store the parsed value counts
    There were rare cases in which u8 was not large enough for the total
    count of values read, and increasing this to u32 should have no real
    effect on performance (hopefully).

--- Userland/Libraries/LibVideo/VP9/Decoder.h
+++ Userland/Libraries/LibVideo/VP9/Decoder.h
@@ -49,11 +49,11 @@ private:
 
     /* (8.4) Probability Adaptation Process */
     u8 merge_prob(u8 pre_prob, u32 count_0, u32 count_1, u8 count_sat, u8 max_update_factor);
-    u32 merge_probs(int const* tree, int index, u8* probs, u8* counts, u8 count_sat, u8 max_update_factor);
+    u32 merge_probs(int const* tree, int index, u8* probs, u32* counts, u8 count_sat, u8 max_update_factor);
     DecoderErrorOr<void> adapt_coef_probs(bool is_inter_predicted_frame);
     DecoderErrorOr<void> adapt_non_coef_probs(FrameContext const&);
-    void adapt_probs(int const* tree, u8* probs, u8* counts);
-    u8 adapt_prob(u8 prob, u8 counts[2]);
+    void adapt_probs(int const* tree, u8* probs, u32* counts);
+    u8 adapt_prob(u8 prob, u32 counts[2]);
 
     /* (8.5) Prediction Processes */
     // (8.5.1) Intra prediction process
