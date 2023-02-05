b8003fa027956aa2273116060b49369641a2430d
Mr.UNIX <99476714+mrunix00@users.noreply.github.com>
2023-02-05 16:49:38 +0000

    Ports/grep: Update to 3.8

--- Ports/grep/package.sh
+++ Ports/grep/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=grep
-version=3.7
+version=3.8
 files="https://ftpmirror.gnu.org/gnu/grep/grep-${version}.tar.gz grep-${version}.tar.gz
 https://ftpmirror.gnu.org/gnu/grep/grep-${version}.tar.gz.sig grep-${version}.tar.gz.sig
 https://ftpmirror.gnu.org/gnu/gnu-keyring.gpg gnu-keyring.gpg"
