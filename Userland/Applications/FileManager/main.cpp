f5cf41eb5dabba687100271aeb559eda45aaadb5
Sam Atkins <atkinssj@serenityos.org>
2023-02-06 15:42:13 +0000

    LibGUI+FileManager: Move `has_{parent,child}_segment` logic into BCB

--- Userland/Applications/FileManager/main.cpp
+++ Userland/Applications/FileManager/main.cpp
@@ -1155,8 +1155,8 @@ ErrorOr<int> run_in_windowed_mode(DeprecatedString const& initial_location, Depr
         paste_action->set_enabled(can_write_in_path && GUI::Clipboard::the().fetch_mime_type() == "text/uri-list");
         go_forward_action->set_enabled(directory_view->path_history_position() < directory_view->path_history_size() - 1);
         go_back_action->set_enabled(directory_view->path_history_position() > 0);
-        open_parent_directory_action->set_enabled(new_path != "/");
-        open_child_directory_action->set_enabled(breadcrumbbar.selected_segment().has_value() && *breadcrumbbar.selected_segment() < breadcrumbbar.segment_count() - 1);
+        open_parent_directory_action->set_enabled(breadcrumbbar.has_parent_segment());
+        open_child_directory_action->set_enabled(breadcrumbbar.has_child_segment());
         directory_view->view_as_table_action().set_enabled(can_read_in_path);
         directory_view->view_as_icons_action().set_enabled(can_read_in_path);
         directory_view->view_as_columns_action().set_enabled(can_read_in_path);
