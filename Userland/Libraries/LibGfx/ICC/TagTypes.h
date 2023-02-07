8bd64f001cde327d5c2b6f3ba90d9c31d29d8934
Nico Weber <thakis@chromium.org>
2023-02-07 15:15:55 -0500

    LibGfx+icc: Read signatureType
    This isn't used by any mandatory tags, and it's not terribly useful.
    But jpegs exported by Lightroom Classic write the 'tech' tag, and
    it seems nice to be able to dump its contents.
    
    signatureType stores a single u32 which for different tags with this
    type means different things.
    
    In each case, the value is one from a short table of valid values,
    suggesting this should be a per-tag enum class instead of a
    per-tag DistinctFourCC, per the comment at the top of DistincFourCC.h.
    On the other hand, 3 of the 4 tables have an explicit "It is possible
    that the ICC will define other signature values in the future" note,
    which suggests the FourCC might actually be the way to go.
    
    For now, just punt on that and manually dump the u32 in fourcc style
    in icc.cpp and don't add any to_string() methods that return a readable
    string based on the contents of these tables.

--- Userland/Libraries/LibGfx/ICC/TagTypes.h
+++ Userland/Libraries/LibGfx/ICC/TagTypes.h
@@ -464,6 +464,25 @@ private:
     Optional<String> m_macintosh_description;
 };
 
+// ICC v4, 10.23 signatureType
+class SignatureTagData : public TagData {
+public:
+    static constexpr TagTypeSignature Type { 0x73696720 }; // 'sig '
+
+    static ErrorOr<NonnullRefPtr<SignatureTagData>> from_bytes(ReadonlyBytes, u32 offset, u32 size);
+
+    SignatureTagData(u32 offset, u32 size, u32 signature)
+        : TagData(offset, size, Type)
+        , m_signature(signature)
+    {
+    }
+
+    u32 signature() const { return m_signature; }
+
+private:
+    u32 m_signature;
+};
+
 // ICC v4, 10.24 textType
 class TextTagData : public TagData {
 public:
