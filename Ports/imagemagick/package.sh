ce25bd5447534b1de769929b319f91e8995d97b0
mrunix00 <mrunix00@protonmail.com>
2023-02-07 16:34:52 +0100

    Ports/imagemagick: Update to 7.1.0-61

--- Ports/imagemagick/package.sh
+++ Ports/imagemagick/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=imagemagick
-version=7.1.0-29
+version=7.1.0-61
 workdir="ImageMagick-${version}"
 useconfigure="true"
-files="https://github.com/ImageMagick/ImageMagick/archive/refs/tags/${version}.tar.gz ${port}-v${version}.tar.gz 889be185fd17a9b9b3d4090e28aecdec289a4f690964a7964b4f893c7a8ec21c"
+files="https://github.com/ImageMagick/ImageMagick/archive/refs/tags/${version}.tar.gz ${port}-v${version}.tar.gz 04a55eaf8ce093112342d777fbe0d578a5567a6bebeff0e3de0d3092fc154a06"
 auth_type=sha256
 configopts=("--host=${SERENITY_ARCH}")
 depends=("libpng" "libtiff" "libjpeg")
