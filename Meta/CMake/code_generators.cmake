afbbc81d5fcd0d94d09170fb58eff78c10fb9643
kleines Filmröllchen <filmroellchen@serenityos.org>
2023-02-08 17:20:09 +0100

    Meta+Userland: Allow generating C++ initializer code from GML
    This does the exact same thing as the runtime initializer,
    except it is faster and can catch some errors much earlier.
    
    The code generator includes these important features:
    - Automatic include generation where necessary
    - Special-casing for TabWidget and ScrollableContainerWidget
    - No use of DeprecatedString where possible
    
    This commit also renames the old code generator to "stringify_gml",
    which is all it really does.

--- Meta/CMake/code_generators.cmake
+++ Meta/CMake/code_generators.cmake
@@ -2,7 +2,7 @@
 # Functions for generating sources using host tools
 #
 
-function(compile_gml source output string_name)
+function(stringify_gml source output string_name)
     set(source ${CMAKE_CURRENT_SOURCE_DIR}/${source})
     add_custom_command(
         OUTPUT ${output}
@@ -11,6 +11,22 @@ function(compile_gml source output string_name)
         COMMAND "${CMAKE_COMMAND}" -E remove ${output}.tmp
         VERBATIM
         DEPENDS ${SerenityOS_SOURCE_DIR}/Meta/text-to-cpp-string.sh
+        MAIN_DEPENDENCY ${source}
+    )
+    get_filename_component(output_name ${output} NAME)
+    add_custom_target(generate_${output_name} DEPENDS ${output})
+    add_dependencies(all_generated generate_${output_name})
+endfunction()
+
+function(compile_gml source output)
+    set(source ${CMAKE_CURRENT_SOURCE_DIR}/${source})
+    add_custom_command(
+        OUTPUT ${output}
+        COMMAND $<TARGET_FILE:Lagom::GMLCompiler> ${source} > ${output}.tmp
+        COMMAND "${CMAKE_COMMAND}" -E copy_if_different ${output}.tmp ${output}
+        COMMAND "${CMAKE_COMMAND}" -E remove ${output}.tmp
+        VERBATIM
+        DEPENDS Lagom::GMLCompiler
         MAIN_DEPENDENCY ${source}
     )
     get_filename_component(output_name ${output} NAME)
