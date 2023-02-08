e7be684ce0db07717d1f9994770686dbf7fc163e
Julian Offenhäuser <offenhaeuser@protonmail.com>
2023-02-08 12:12:44 +0100

    Ports: Add nlohmann-json

---
+++ Ports/nlohmann-json/package.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port='nlohmann-json'
+version='3.11.2'
+auth_type='sha256'
+workdir="json-${version}"
+files="https://github.com/nlohmann/json/archive/refs/tags/v${version}.tar.gz json-${version}.tar.gz d69f9deb6a75e2580465c6c4c5111b89c4dc2fa94e3a85fcd2ffcd9a143d9273"
+useconfigure='true'
+configopts=(
+    "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
+    "-DJSON_BuildTests=OFF"
+)
+
+configure() {
+    mkdir -p "${PORT_BUILD_DIR}/json-${version}-build"
+    cd "${PORT_BUILD_DIR}/json-${version}-build"
+    cmake "${configopts[@]}" "${PORT_BUILD_DIR}/json-${version}"
+}
+
+build() {
+    cd "${PORT_BUILD_DIR}/json-${version}-build"
+    make "${makeopts[@]}"
+}
+
+install() {
+    cd "${PORT_BUILD_DIR}/json-${version}-build"
+    make install
+}
