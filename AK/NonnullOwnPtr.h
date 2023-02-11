3c842d9d761ea04c9175668699e1a3aa4fced0ce
Nico Weber <thakis@chromium.org>
2023-02-11 06:36:18 -0500

    AK: Move try_make() to NonnullOwnPtr.h

--- AK/NonnullOwnPtr.h
+++ AK/NonnullOwnPtr.h
@@ -170,6 +170,20 @@ inline ErrorOr<NonnullOwnPtr<T>> adopt_nonnull_own_or_enomem(T* object)
     return NonnullOwnPtr<T>(NonnullOwnPtr<T>::Adopt, *object);
 }
 
+template<typename T, class... Args>
+requires(IsConstructible<T, Args...>) inline ErrorOr<NonnullOwnPtr<T>> try_make(Args&&... args)
+{
+    return adopt_nonnull_own_or_enomem(new (nothrow) T(forward<Args>(args)...));
+}
+
+// FIXME: Remove once P0960R3 is available in Clang.
+template<typename T, class... Args>
+inline ErrorOr<NonnullOwnPtr<T>> try_make(Args&&... args)
+
+{
+    return adopt_nonnull_own_or_enomem(new (nothrow) T { forward<Args>(args)... });
+}
+
 template<typename T>
 struct Traits<NonnullOwnPtr<T>> : public GenericTraits<NonnullOwnPtr<T>> {
     using PeekType = T*;
@@ -201,4 +215,5 @@ using AK::make;
 #    endif
 using AK::adopt_nonnull_own_or_enomem;
 using AK::NonnullOwnPtr;
+using AK::try_make;
 #endif
