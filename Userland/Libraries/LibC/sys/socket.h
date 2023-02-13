f20902deb3c88a16b1cd9fe8cd9d9db16494ddc9
Peter Elliott <pelliott@ualberta.ca>
2023-02-13 16:01:13 -0700

    Kernel: Support sending filedescriptors with sendmsg(2) and SCM_RIGHTS
    This is necessary to support the wayland protocol.
    I also moved the CMSG_* macros to the kernel API since they are used in
    both kernel and userspace.
    this does not break ntpquery/SCM_TIMESTAMP.

--- Userland/Libraries/LibC/sys/socket.h
+++ Userland/Libraries/LibC/sys/socket.h
@@ -33,30 +33,4 @@ int socketpair(int domain, int type, int protocol, int sv[2]);
 int sendfd(int sockfd, int fd);
 int recvfd(int sockfd, int options);
 
-// These three are non-POSIX, but common:
-#define CMSG_ALIGN(x) (((x) + sizeof(void*) - 1) & ~(sizeof(void*) - 1))
-#define CMSG_SPACE(x) (CMSG_ALIGN(sizeof(struct cmsghdr)) + CMSG_ALIGN(x))
-#define CMSG_LEN(x) (CMSG_ALIGN(sizeof(struct cmsghdr)) + (x))
-
-static inline struct cmsghdr* CMSG_FIRSTHDR(struct msghdr* msg)
-{
-    if (msg->msg_controllen < sizeof(struct cmsghdr))
-        return 0;
-    return (struct cmsghdr*)msg->msg_control;
-}
-
-static inline struct cmsghdr* CMSG_NXTHDR(struct msghdr* msg, struct cmsghdr* cmsg)
-{
-    struct cmsghdr* next = (struct cmsghdr*)((char*)cmsg + CMSG_ALIGN(cmsg->cmsg_len));
-    unsigned offset = (char*)next - (char*)msg->msg_control;
-    if (msg->msg_controllen < offset + sizeof(struct cmsghdr))
-        return NULL;
-    return next;
-}
-
-static inline void* CMSG_DATA(struct cmsghdr* cmsg)
-{
-    return (void*)(cmsg + 1);
-}
-
 __END_DECLS
