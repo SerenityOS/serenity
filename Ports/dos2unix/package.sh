3b446b873ec5176c16f83283d6b8aaf86a419f39
Nathan Ell <nathanjamesell@gmail.com>
2023-02-13 23:02:38 -0700

    Ports/dos2unix: Update to 7.4.4

--- Ports/dos2unix/package.sh
+++ Ports/dos2unix/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=dos2unix
-version=7.4.3
+version=7.4.4
 workdir="${port}-${version}"
 files="https://waterlan.home.xs4all.nl/dos2unix/dos2unix-${version}.tar.gz ${port}-${version}.tar.gz
 https://waterlan.home.xs4all.nl/dos2unix/dos2unix-${version}.tar.gz.asc ${port}-${version}.tar.gz.asc"
