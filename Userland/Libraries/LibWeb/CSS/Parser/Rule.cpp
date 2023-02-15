41c4cc95e4e2b89a4a53a7529d16e10189adc83e
Sam Atkins <atkinssj@serenityos.org>
2023-02-15 11:28:44 +0000

    LibWeb: Port CSS::Parser::Rule to new Strings
    `Rule::to_deprecated_string()` and
    `DeclarationOrAtRule::to_deprecated_string()` are not used anywhere, so
    we can just delete them.

--- Userland/Libraries/LibWeb/CSS/Parser/Rule.cpp
+++ Userland/Libraries/LibWeb/CSS/Parser/Rule.cpp
@@ -1,16 +1,15 @@
 /*
  * Copyright (c) 2020-2021, the SerenityOS developers.
- * Copyright (c) 2021-2022, Sam Atkins <atkinssj@serenityos.org>
+ * Copyright (c) 2021-2023, Sam Atkins <atkinssj@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #include <LibWeb/CSS/Parser/Rule.h>
-#include <LibWeb/CSS/Serialize.h>
 
 namespace Web::CSS::Parser {
 
-Rule::Rule(Rule::Type type, DeprecatedFlyString name, Vector<ComponentValue> prelude, RefPtr<Block> block)
+Rule::Rule(Rule::Type type, FlyString name, Vector<ComponentValue> prelude, RefPtr<Block> block)
     : m_type(type)
     , m_at_rule_name(move(name))
     , m_prelude(move(prelude))
@@ -19,24 +18,5 @@ Rule::Rule(Rule::Type type, DeprecatedFlyString name, Vector<ComponentValue> pre
 }
 
 Rule::~Rule() = default;
-
-DeprecatedString Rule::to_deprecated_string() const
-{
-    StringBuilder builder;
-
-    if (is_at_rule()) {
-        builder.append('@');
-        serialize_an_identifier(builder, m_at_rule_name);
-    }
-
-    builder.join(' ', m_prelude);
-
-    if (m_block)
-        builder.append(m_block->to_string().release_value_but_fixme_should_propagate_errors());
-    else
-        builder.append(';');
-
-    return builder.to_deprecated_string();
-}
 
 }
