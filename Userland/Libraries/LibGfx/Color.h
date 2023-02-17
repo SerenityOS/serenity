e5a39d134b6a18ea3b98be955f2edcc90eae2b3e
MacDue <macdue@dueutil.tech>
2023-02-17 19:02:03 +0000

    LibGfx: Move Color::mixed_with() inline
    This seems to give a small speedup to gradient painting and removes
    Color::mixed_with() (which was 10% of the time) from the profile.

--- Userland/Libraries/LibGfx/Color.h
+++ Userland/Libraries/LibGfx/Color.h
@@ -237,7 +237,29 @@ public:
 #endif
     }
 
-    Color mixed_with(Color other, float weight) const;
+    Color mixed_with(Color other, float weight) const
+    {
+        if (alpha() == other.alpha() || with_alpha(0) == other.with_alpha(0)) {
+            return Gfx::Color {
+                round_to<u8>(mix<float>(red(), other.red(), weight)),
+                round_to<u8>(mix<float>(green(), other.green(), weight)),
+                round_to<u8>(mix<float>(blue(), other.blue(), weight)),
+                round_to<u8>(mix<float>(alpha(), other.alpha(), weight)),
+            };
+        }
+        // Fallback to slower, but more visually pleasing premultiplied alpha mix.
+        // This is needed for linear-gradient()s in LibWeb.
+        auto mixed_alpha = mix<float>(alpha(), other.alpha(), weight);
+        auto premultiplied_mix_channel = [&](float channel, float other_channel, float weight) {
+            return round_to<u8>(mix<float>(channel * alpha(), other_channel * other.alpha(), weight) / mixed_alpha);
+        };
+        return Gfx::Color {
+            premultiplied_mix_channel(red(), other.red(), weight),
+            premultiplied_mix_channel(green(), other.green(), weight),
+            premultiplied_mix_channel(blue(), other.blue(), weight),
+            round_to<u8>(mixed_alpha),
+        };
+    }
 
     Color interpolate(Color other, float weight) const noexcept
     {
