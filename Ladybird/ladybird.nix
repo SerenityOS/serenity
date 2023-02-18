533f2a4980c294af8f1dfe84527a269fb6852412
Linus Groh <mail@linusgroh.de>
2023-02-18 16:14:19 +0100

    Ladybird: Add qtwayland to QT_PLUGIN_PATH in nix-shell script
    Otherwise Qt would not find the wayland plugin it is instructed to use
    via QT_QPA_PLATFORM, and would fall back to the second option, xcb,
    which looks rather sad in a modern Wayland environment :^)
    
    This feels like something that should be addressed upstream in nixpkgs
    eventually.

--- Ladybird/ladybird.nix
+++ Ladybird/ladybird.nix
@@ -19,6 +19,9 @@ pkgs.mkShell.override
   ];
 
   shellHook = ''
+    # NOTE: This is required to make it find the wayland platform plugin installed
+    #       above, but should probably be fixed upstream.
+    export QT_PLUGIN_PATH="$QT_PLUGIN_PATH:${pkgs.qt6.qtwayland}/lib/qt-6/plugins"
     export QT_QPA_PLATFORM="wayland;xcb"
   '';
 }
