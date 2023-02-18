61f4914d6e8c07352c9f1223709fe82a8432d66e
Liav A <liavalb@gmail.com>
2023-02-18 18:52:44 +0200

    Kernel+Userland: Add constants subdirectory at /sys/kernel directory
    This subdirectory is meant to hold all constant data related to the
    kernel. This means that this data is never meant to updated and is
    relevant from system boot to system shutdown.
    Move the inodes of "load_base", "cmdline" and "system_mode" to that
    directory. All nodes under this new subdirectory are generated during
    boot, and therefore don't require calling kmalloc each time we need to
    read them. Locking is also not necessary, because these nodes and their
    data are completely static once being generated.

---
+++ Kernel/FileSystem/SysFS/Subsystems/Kernel/Constants/ConstantInformation.cpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2023, Liav A. <liavalb@hotmail.co.il>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <Kernel/FileSystem/SysFS/Subsystems/Kernel/Constants/ConstantInformation.h>
+#include <Kernel/Process.h>
+#include <Kernel/Sections.h>
+
+namespace Kernel {
+
+StringView SysFSSystemConstantInformation::name() const
+{
+    switch (m_node_name) {
+    case NodeName::LoadBase:
+        return "load_base"sv;
+    case NodeName::CommandLine:
+        return "cmdline"sv;
+    case NodeName::SystemMode:
+        return "system_mode"sv;
+    default:
+        VERIFY_NOT_REACHED();
+    }
+}
+
+NonnullLockRefPtr<SysFSSystemConstantInformation> SysFSSystemConstantInformation::must_create(SysFSDirectory const& parent_directory, NonnullOwnPtr<KBuffer> constant_data_buffer, mode_t mode, ReadableByJailedProcesses readable_by_jailed_processes, NodeName name)
+{
+    auto node = adopt_lock_ref_if_nonnull(new (nothrow) SysFSSystemConstantInformation(parent_directory, move(constant_data_buffer), mode, readable_by_jailed_processes, name)).release_nonnull();
+    return node;
+}
+
+ErrorOr<size_t> SysFSSystemConstantInformation::read_bytes(off_t offset, size_t count, UserOrKernelBuffer& buffer, OpenFileDescription*) const
+{
+    if (offset < 0)
+        return EINVAL;
+    if (static_cast<size_t>(offset) >= m_constant_data_buffer->size())
+        return 0;
+    TRY(Process::current().jail().with([&](auto const& my_jail) -> ErrorOr<void> {
+        if (my_jail && m_readable_by_jailed_processes == ReadableByJailedProcesses::No)
+            return Error::from_errno(EPERM);
+        return {};
+    }));
+    ssize_t nread = min(static_cast<off_t>(m_constant_data_buffer->size() - offset), static_cast<off_t>(count));
+    TRY(buffer.write(m_constant_data_buffer->data() + offset, nread));
+    return nread;
+}
+
+}
--- Userland/Libraries/LibSymbolication/Symbolication.cpp
+++ Userland/Libraries/LibSymbolication/Symbolication.cpp
@@ -37,7 +37,7 @@ static KernelBaseState s_kernel_base_state = KernelBaseState::Uninitialized;
 Optional<FlatPtr> kernel_base()
 {
     if (s_kernel_base_state == KernelBaseState::Uninitialized) {
-        auto file = Core::DeprecatedFile::open("/sys/kernel/load_base", Core::OpenMode::ReadOnly);
+        auto file = Core::DeprecatedFile::open("/sys/kernel/constants/load_base", Core::OpenMode::ReadOnly);
         if (file.is_error()) {
             s_kernel_base_state = KernelBaseState::Invalid;
             return {};
