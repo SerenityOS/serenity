05d254f2430cfb7c35a63edca40a9d47bb1d9837
Timothy Flynn <trflynn89@pm.me>
2023-02-20 14:04:42 -0500

    AK: Prepare Utf32View for use within templated LibGfx contexts
    Forward declare its iterator and add a peek() method analagous to
    Utf8CodePointIterator::peek().

--- AK/Utf32View.cpp
+++ AK/Utf32View.cpp
@@ -9,6 +9,24 @@
 
 namespace AK {
 
+Optional<u32> Utf32CodePointIterator::peek(size_t offset) const
+{
+    if (offset == 0) {
+        if (this->done())
+            return {};
+        return this->operator*();
+    }
+
+    auto new_iterator = *this;
+    for (size_t index = 0; index < offset; ++index) {
+        ++new_iterator;
+        if (new_iterator.done())
+            return {};
+    }
+
+    return *new_iterator;
+}
+
 ErrorOr<void> Formatter<Utf32View>::format(FormatBuilder& builder, Utf32View const& string)
 {
     return builder.builder().try_append(string);
