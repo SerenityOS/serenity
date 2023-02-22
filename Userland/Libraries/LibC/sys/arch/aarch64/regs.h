200e91cd7f1ec5453799a2720d4dc114a59cc289
Timon Kruiper <timonkruiper@gmail.com>
2023-02-23 00:00:37 +0100

    Kernel+LibC: Modify aarch64's __mcontext to store registers in an array
    This commit also removes the unnecessary ifdefs from
    sys/arch/aarch64/regs.h. Contributed by konrad, thanks for that.

--- Userland/Libraries/LibC/sys/arch/aarch64/regs.h
+++ Userland/Libraries/LibC/sys/arch/aarch64/regs.h
@@ -21,46 +21,22 @@ struct [[gnu::packed]] PtraceRegisters : public __mcontext {
 #    if defined(__cplusplus) && defined(__cpp_concepts)
     FlatPtr ip() const
     {
-#        if ARCH(X86_64)
-        return rip;
-#        elif ARCH(AARCH64)
         return pc;
-#        else
-#            error Unknown architecture
-#        endif
     }
 
     void set_ip(FlatPtr ip)
     {
-#        if ARCH(X86_64)
-        rip = ip;
-#        elif ARCH(AARCH64)
         pc = ip;
-#        else
-#            error Unknown architecture
-#        endif
     }
 
     FlatPtr bp() const
     {
-#        if ARCH(X86_64)
-        return rbp;
-#        elif ARCH(AARCH64)
-        return r29;
-#        else
-#            error Unknown architecture
-#        endif
+        return x[29];
     }
 
     void set_bp(FlatPtr bp)
     {
-#        if ARCH(X86_64)
-        rbp = bp;
-#        elif ARCH(AARCH64)
-        r29 = bp;
-#        else
-#            error Unknown architecture
-#        endif
+        x[29] = bp;
     }
 #    endif
 };
