0f7a5006d1abd22739134e24e2781893239b271d
kleines Filmröllchen <filmroellchen@serenityos.org>
2023-02-25 00:41:42 +0100

    Meta/Lagom: Add a fuzzer for QOA

---
+++ Meta/Lagom/Fuzzers/FuzzQOALoader.cpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2023, kleines Filmröllchen <filmroellchen@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibAudio/QOALoader.h>
+#include <stddef.h>
+#include <stdint.h>
+
+extern "C" int LLVMFuzzerTestOneInput(uint8_t const* data, size_t size)
+{
+    auto qoa_data = ByteBuffer::copy(data, size).release_value();
+    auto qoa_or_error = Audio::QOALoaderPlugin::create(qoa_data.bytes());
+
+    if (qoa_or_error.is_error())
+        return 0;
+
+    auto qoa = qoa_or_error.release_value();
+
+    for (;;) {
+        auto samples = qoa->get_more_samples();
+        if (samples.is_error())
+            return 0;
+        if (samples.value().size() > 0)
+            break;
+    }
+
+    return 0;
+}
