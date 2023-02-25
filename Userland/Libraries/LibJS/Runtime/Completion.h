10aa423a43e46b600232e402638af59976b50ffc
Matthew Olsson <matthewcolsson@gmail.com>
2023-02-25 10:43:48 -0700

    LibJS: Use a forwarding reference in ThrowCompletion constructor
    This avoids compiler complaints when trying to use const types

--- Userland/Libraries/LibJS/Runtime/Completion.h
+++ Userland/Libraries/LibJS/Runtime/Completion.h
@@ -307,7 +307,7 @@ public:
     // Most commonly: Value from Object* or similar, so we can omit the curly braces from "return { TRY(...) };".
     // Disabled for POD types to avoid weird conversion shenanigans.
     template<typename WrappedValueType>
-    ThrowCompletionOr(WrappedValueType const& value)
+    ThrowCompletionOr(WrappedValueType&& value)
     requires(!IsPOD<ValueType>)
         : m_value_or_throw_completion(ValueType { value })
     {
