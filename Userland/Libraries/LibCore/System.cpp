20095d8cf94450c001145dd7e50712e885e0891d
nipos <ni.pos@yandex.com>
2023-02-25 19:45:42 +0100

    LibCore: Add support for Solaris in anon_create

--- Userland/Libraries/LibCore/System.cpp
+++ Userland/Libraries/LibCore/System.cpp
@@ -405,7 +405,7 @@ ErrorOr<int> anon_create([[maybe_unused]] size_t size, [[maybe_unused]] int opti
         TRY(close(fd));
         return Error::from_errno(saved_errno);
     }
-#elif defined(AK_OS_MACOS) || defined(AK_OS_EMSCRIPTEN) || defined(AK_OS_OPENBSD) || defined(AK_OS_NETBSD)
+#elif defined(AK_OS_BSD_GENERIC) || defined(AK_OS_EMSCRIPTEN)
     struct timespec time;
     clock_gettime(CLOCK_REALTIME, &time);
     auto name = DeprecatedString::formatted("/shm-{}{}", (unsigned long)time.tv_sec, (unsigned long)time.tv_nsec);
