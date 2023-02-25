fa3243fac5bc550aaea40a6cb51fc82a5404de23
Nico Weber <thakis@chromium.org>
2023-02-24 21:09:48 -0500

    Fuzzers: Add a WebP fuzzer

---
+++ Meta/Lagom/Fuzzers/FuzzWebPLoader.cpp
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2023, the SerenityOS developers.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibGfx/ImageFormats/WebPLoader.h>
+#include <stddef.h>
+#include <stdint.h>
+
+extern "C" int LLVMFuzzerTestOneInput(uint8_t const* data, size_t size)
+{
+    auto decoder_or_error = Gfx::WebPImageDecoderPlugin::create({ data, size });
+    if (decoder_or_error.is_error())
+        return 0;
+    auto decoder = decoder_or_error.release_value();
+    decoder->initialize();
+    (void)decoder->frame(0);
+    return 0;
+}
