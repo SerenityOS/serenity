68cd6f56142bf0c3bbc93170278208be38e4b668
Lucas CHOLLET <lucas.chollet@free.fr>
2023-02-27 14:32:41 -0500

    Tests: Add a test for SOF2 images with only spectral selection
    You can generate one by using `cjpeg` with the -scan argument.
    
    This image has been generated with the following scan file:
    0 1 2: 0 0 0 0;
    0: 1 9 0 0;
    2: 1 63 0 0 ;
    1: 1 63 0 0 ;
    0: 10 63 0 0;

--- Tests/LibGfx/TestImageDecoder.cpp
+++ Tests/LibGfx/TestImageDecoder.cpp
@@ -112,6 +112,17 @@ TEST_CASE(test_jpeg_sof0_several_scans)
     EXPECT_EQ(frame.image->size(), Gfx::IntSize(720, 859));
 }
 
+TEST_CASE(test_jpeg_sof2_spectral_selection)
+{
+    auto file = MUST(Core::MappedFile::map(TEST_INPUT("spectral_selection.jpg"sv)));
+    EXPECT(Gfx::JPEGImageDecoderPlugin::sniff(file->bytes()));
+    auto plugin_decoder = MUST(Gfx::JPEGImageDecoderPlugin::create(file->bytes()));
+    EXPECT(plugin_decoder->initialize());
+
+    auto frame = MUST(plugin_decoder->frame(0));
+    EXPECT_EQ(frame.image->size(), Gfx::IntSize(592, 800));
+}
+
 TEST_CASE(test_pbm)
 {
     auto file = MUST(Core::MappedFile::map(TEST_INPUT("buggie-raw.pbm"sv)));
