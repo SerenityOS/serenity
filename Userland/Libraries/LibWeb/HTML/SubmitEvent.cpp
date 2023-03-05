db0927b1b9b3647fbf7f65205befa02d9b7e187e
Kenneth Myhra <kennethmyhra@gmail.com>
2023-03-05 10:58:45 +0100

    LibWeb: Port SubmitEvent to new String

--- Userland/Libraries/LibWeb/HTML/SubmitEvent.cpp
+++ Userland/Libraries/LibWeb/HTML/SubmitEvent.cpp
@@ -9,18 +9,18 @@
 
 namespace Web::HTML {
 
-WebIDL::ExceptionOr<JS::NonnullGCPtr<SubmitEvent>> SubmitEvent::create(JS::Realm& realm, DeprecatedFlyString const& event_name, SubmitEventInit const& event_init)
+WebIDL::ExceptionOr<JS::NonnullGCPtr<SubmitEvent>> SubmitEvent::create(JS::Realm& realm, FlyString const& event_name, SubmitEventInit const& event_init)
 {
     return MUST_OR_THROW_OOM(realm.heap().allocate<SubmitEvent>(realm, realm, event_name, event_init));
 }
 
-WebIDL::ExceptionOr<JS::NonnullGCPtr<SubmitEvent>> SubmitEvent::construct_impl(JS::Realm& realm, DeprecatedFlyString const& event_name, SubmitEventInit const& event_init)
+WebIDL::ExceptionOr<JS::NonnullGCPtr<SubmitEvent>> SubmitEvent::construct_impl(JS::Realm& realm, FlyString const& event_name, SubmitEventInit const& event_init)
 {
     return create(realm, event_name, event_init);
 }
 
-SubmitEvent::SubmitEvent(JS::Realm& realm, DeprecatedFlyString const& event_name, SubmitEventInit const& event_init)
-    : DOM::Event(realm, event_name, event_init)
+SubmitEvent::SubmitEvent(JS::Realm& realm, FlyString const& event_name, SubmitEventInit const& event_init)
+    : DOM::Event(realm, event_name.to_deprecated_fly_string(), event_init)
     , m_submitter(event_init.submitter)
 {
 }
