62b20e92053a3ddf092c22b9d0e4becd52003605
Fabian Dellwing <fabian.dellwing@gmail.com>
2023-03-10 12:08:50 +0100

    Kernel: Add non standard value to sys$sysconf
    Add `_SC_PHYS_PAGES` to sys$sysconf syscall. This value is needed
    for a port I'm working on.

--- Kernel/Syscalls/sysconf.cpp
+++ Kernel/Syscalls/sysconf.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <Kernel/FileSystem/VirtualFileSystem.h>
+#include <Kernel/Memory/MemoryManager.h>
 #include <Kernel/Process.h>
 #include <Kernel/Time/TimeManagement.h>
 
@@ -37,6 +38,8 @@ ErrorOr<FlatPtr> Process::sys$sysconf(int name)
         return Process::max_arguments_size;
     case _SC_IOV_MAX:
         return IOV_MAX;
+    case _SC_PHYS_PAGES:
+        return MM.get_system_memory_info().physical_pages;
     default:
         return EINVAL;
     }
