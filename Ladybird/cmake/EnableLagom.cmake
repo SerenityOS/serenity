4abe9a4afeaed32edba05a50fce5337df3f0c4c7
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2023-03-13 11:32:43 +0300

    Ladybird: Fix build failure caused by missing WebDriver header
    Fix the problem that `cmake --build Build/ladybird` started
    failing with:
    
    fatal error: 'WebContent/WebDriverConnection.h' file not found
    
    after 11fe34ce0fb827f3bfe9c61c31b3dbe99a894233

--- Ladybird/cmake/EnableLagom.cmake
+++ Ladybird/cmake/EnableLagom.cmake
@@ -9,6 +9,7 @@ set(LAGOM_BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/Lagom")
 
 # FIXME: Setting target_include_directories on Lagom libraries might make this unnecessary?
 include_directories(${SERENITY_SOURCE_DIR})
+include_directories(${SERENITY_SOURCE_DIR}/Userland/Services)
 include_directories(${SERENITY_SOURCE_DIR}/Userland/Libraries)
 include_directories(${LAGOM_BINARY_DIR})
 include_directories(${LAGOM_BINARY_DIR}/Userland/Services)
