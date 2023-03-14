50afd0517d3abc12b567a682508cb6c0055dd9c7
MacDue <macdue@dueutil.tech>
2023-03-14 19:57:51 +0000

    LibGUI: Allow passing working directory to spawn_or_show_error()

--- Userland/Libraries/LibGUI/Process.cpp
+++ Userland/Libraries/LibGUI/Process.cpp
@@ -9,28 +9,28 @@
 #include <LibGUI/Process.h>
 
 template<typename StringType>
-void spawn_or_show_error(GUI::Window* parent_window, StringView path, ReadonlySpan<StringType> arguments)
+void spawn_or_show_error(GUI::Window* parent_window, StringView path, ReadonlySpan<StringType> arguments, StringView working_directory)
 {
-    auto spawn_result = Core::Process::spawn(path, arguments);
+    auto spawn_result = Core::Process::spawn(path, arguments, working_directory);
     if (spawn_result.is_error())
         GUI::MessageBox::show_error(parent_window, DeprecatedString::formatted("Failed to spawn {}: {}", path, spawn_result.error()));
 }
 
 namespace GUI {
 
-void Process::spawn_or_show_error(Window* parent_window, StringView path, ReadonlySpan<DeprecatedString> arguments)
+void Process::spawn_or_show_error(Window* parent_window, StringView path, ReadonlySpan<DeprecatedString> arguments, StringView working_directory)
 {
-    ::spawn_or_show_error<DeprecatedString>(parent_window, path, arguments);
+    ::spawn_or_show_error<DeprecatedString>(parent_window, path, arguments, working_directory);
 }
 
-void Process::spawn_or_show_error(Window* parent_window, StringView path, ReadonlySpan<StringView> arguments)
+void Process::spawn_or_show_error(Window* parent_window, StringView path, ReadonlySpan<StringView> arguments, StringView working_directory)
 {
-    ::spawn_or_show_error<StringView>(parent_window, path, arguments);
+    ::spawn_or_show_error<StringView>(parent_window, path, arguments, working_directory);
 }
 
-void Process::spawn_or_show_error(Window* parent_window, StringView path, ReadonlySpan<char const*> arguments)
+void Process::spawn_or_show_error(Window* parent_window, StringView path, ReadonlySpan<char const*> arguments, StringView working_directory)
 {
-    ::spawn_or_show_error<char const*>(parent_window, path, arguments);
+    ::spawn_or_show_error<char const*>(parent_window, path, arguments, working_directory);
 }
 
 }
