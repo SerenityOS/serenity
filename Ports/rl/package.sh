2ce9888f6553cec6ec9ff58559ea222dab591092
Seal Sealy <butilkama@gmail.com>
2023-03-15 16:09:28 +0300

    Ports: Add rl port

---
+++ Ports/rl/package.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port=rl
+version=0.2.7
+useconfigure="true"
+use_fresh_config_sub=true
+auth_type="sha256"
+files="https://arthurdejong.org/rl/rl-${version}.tar.gz rl-${version}.tar.gz 1cfca23d6a14acd190c5a6261923757d20cb94861c9b2066991ec7a7cae33bc8"
