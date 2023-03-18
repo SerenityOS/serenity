905dc35f33ff76952d91f7abcec7f683c87efb38
Lucas CHOLLET <lucas.chollet@free.fr>
2023-03-17 23:39:30 -0400

    Tests: Add a test for SOF2 JPEGs with successive approximations
    This image was generated using `cjpeg` with the following scan file:
    
    0 1 2: 0 0 0 2;
    0: 1 63 0 1;
    1: 1 63 0 1;
    2: 1 63 0 1;
    0 1 2: 0 0 2 1;
    0: 1 63 1 0;
    1: 1 63 1 0;
    2: 1 63 1 0;
    0 1 2: 0 0 1 0;

--- Tests/LibGfx/TestImageDecoder.cpp
+++ Tests/LibGfx/TestImageDecoder.cpp
@@ -145,6 +145,17 @@ TEST_CASE(test_jpeg_sof0_several_scans_odd_number_mcu)
     EXPECT_EQ(frame.image->size(), Gfx::IntSize(600, 600));
 }
 
+TEST_CASE(test_jpeg_sof2_successive_aproximation)
+{
+    auto file = MUST(Core::MappedFile::map(TEST_INPUT("successive_approximation.jpg"sv)));
+    EXPECT(Gfx::JPEGImageDecoderPlugin::sniff(file->bytes()));
+    auto plugin_decoder = MUST(Gfx::JPEGImageDecoderPlugin::create(file->bytes()));
+    EXPECT(plugin_decoder->initialize());
+
+    auto frame = MUST(plugin_decoder->frame(0));
+    EXPECT_EQ(frame.image->size(), Gfx::IntSize(600, 800));
+}
+
 TEST_CASE(test_pbm)
 {
     auto file = MUST(Core::MappedFile::map(TEST_INPUT("buggie-raw.pbm"sv)));
