72a45799e058d3a65da9e5bcbe5f08354eb98d9a
Caoimhe <caoimhebyrne06@gmail.com>
2023-03-19 19:59:49 +0000

    Presenter: Add a "View" menu
    It didn't really make sense for "Toggle Full Screen" to be under
    "Presentation", and now there's a place for any future view-related
    actions.

--- Userland/Applications/Presenter/PresenterWidget.h
+++ Userland/Applications/Presenter/PresenterWidget.h
@@ -46,6 +46,7 @@ private:
     OwnPtr<Presentation> m_current_presentation;
     RefPtr<GUI::Action> m_next_slide_action;
     RefPtr<GUI::Action> m_previous_slide_action;
-    RefPtr<GUI::Action> m_full_screen_action;
     RefPtr<GUI::Action> m_present_from_first_slide_action;
+
+    RefPtr<GUI::Action> m_full_screen_action;
 };
