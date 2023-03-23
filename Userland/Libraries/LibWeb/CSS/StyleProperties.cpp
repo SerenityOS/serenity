625efa0548b3d6151ad58a4cb6d96988de93f34c
Sam Atkins <atkinssj@serenityos.org>
2023-03-23 21:17:43 +0000

    LibWeb: Split ContentStyleValue out of StyleValue.{h,cpp}

--- Userland/Libraries/LibWeb/CSS/StyleProperties.cpp
+++ Userland/Libraries/LibWeb/CSS/StyleProperties.cpp
@@ -10,6 +10,7 @@
 #include <LibWeb/CSS/Clip.h>
 #include <LibWeb/CSS/StyleProperties.h>
 #include <LibWeb/CSS/StyleValues/AngleStyleValue.h>
+#include <LibWeb/CSS/StyleValues/ContentStyleValue.h>
 #include <LibWeb/FontCache.h>
 #include <LibWeb/Layout/BlockContainer.h>
 #include <LibWeb/Layout/Node.h>
