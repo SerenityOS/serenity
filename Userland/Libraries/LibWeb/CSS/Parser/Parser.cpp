aa6e190e73cdf168bc45b252e5dc1c50820ea2d2
Sam Atkins <atkinssj@serenityos.org>
2023-03-24 17:20:16 +0000

    LibWeb: Split OverflowStyleValue out of StyleValue.{h,cpp}

--- Userland/Libraries/LibWeb/CSS/Parser/Parser.cpp
+++ Userland/Libraries/LibWeb/CSS/Parser/Parser.cpp
@@ -57,6 +57,7 @@
 #include <LibWeb/CSS/StyleValues/LinearGradientStyleValue.h>
 #include <LibWeb/CSS/StyleValues/ListStyleStyleValue.h>
 #include <LibWeb/CSS/StyleValues/NumericStyleValue.h>
+#include <LibWeb/CSS/StyleValues/OverflowStyleValue.h>
 #include <LibWeb/CSS/StyleValues/RadialGradientStyleValue.h>
 #include <LibWeb/DOM/Document.h>
 #include <LibWeb/Dump.h>
