091abc8f98bde5592093fc8ba63eaf355600c11c
Sam Atkins <atkinssj@serenityos.org>
2023-03-25 00:02:50 +0000

    LibWeb: Split RectStyleValue out of StyleValue.{h,cpp}

---
+++ Userland/Libraries/LibWeb/CSS/StyleValues/RectStyleValue.cpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2021, Tobias Christiansen <tobyase@serenityos.org>
+ * Copyright (c) 2021-2023, Sam Atkins <atkinssj@serenityos.org>
+ * Copyright (c) 2022-2023, MacDue <macdue@dueutil.tech>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include "RectStyleValue.h"
+
+namespace Web::CSS {
+
+ValueComparingNonnullRefPtr<RectStyleValue> RectStyleValue::create(EdgeRect rect)
+{
+    return adopt_ref(*new RectStyleValue(rect));
+}
+
+ErrorOr<String> RectStyleValue::to_string() const
+{
+    return String::formatted("rect({} {} {} {})", m_rect.top_edge, m_rect.right_edge, m_rect.bottom_edge, m_rect.left_edge);
+}
+
+}
