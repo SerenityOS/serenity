6733232311e43c1f33fbeac1c5786c6795589c60
Cameron Youell <cameronyouell@gmail.com>
2023-03-27 19:28:03 +1100

    Ports/binutils: Update binutils to version 2.40

--- Ports/binutils/package.sh
+++ Ports/binutils/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='binutils'
-version='2.39'
+version='2.40'
 useconfigure='true'
 use_fresh_config_sub='true'
 configopts=(
@@ -12,10 +12,10 @@ configopts=(
     "--disable-nls"
     "--enable-libiberty"
 )
-files="https://ftpmirror.gnu.org/gnu/binutils/binutils-${version}.tar.xz binutils-${version}.tar.xz 645c25f563b8adc0a81dbd6a41cffbf4d37083a382e02d5d3df4f65c09516d00"
-auth_type="sha256"
+files="https://ftpmirror.gnu.org/gnu/binutils/binutils-${version}.tar.xz binutils-${version}.tar.xz 0f8a4c272d7f17f369ded10a4aca28b8e304828e95526da482b0ccc4dfc9d8e1"
+auth_type='sha256'
 auth_opts=("--keyring" "./gnu-keyring.gpg" "binutils-${version}.tar.xz.sig")
-depends=('zlib')
+depends=("zlib")
 
 export ac_cv_func_getrusage=no
 
