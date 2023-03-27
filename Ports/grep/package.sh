4b3491ebb6a5627a77295c673ddcf4ab2fddf374
Cameron Youell <cameronyouell@gmail.com>
2023-03-27 17:29:09 +1100

    Ports/grep: Update grep to version 3.10

--- Ports/grep/package.sh
+++ Ports/grep/package.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=grep
-version=3.8
+port='grep'
+version='3.10'
 files="https://ftpmirror.gnu.org/gnu/grep/grep-${version}.tar.gz grep-${version}.tar.gz
 https://ftpmirror.gnu.org/gnu/grep/grep-${version}.tar.gz.sig grep-${version}.tar.gz.sig
 https://ftpmirror.gnu.org/gnu/gnu-keyring.gpg gnu-keyring.gpg"
 
-useconfigure=true
+useconfigure='true'
 configopts=("--disable-perl-regexp")
-auth_type="sig"
+auth_type='sig'
 auth_opts=("--keyring" "./gnu-keyring.gpg" "grep-${version}.tar.gz.sig")
