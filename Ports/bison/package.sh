55cbfbed52872941e73bd002d3bc3de710305eec
Cameron Youell <cameronyouell@gmail.com>
2023-03-27 20:49:42 +1100

    Ports/bison: Update bison to version 3.8

--- Ports/bison/package.sh
+++ Ports/bison/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=bison
-version=1.25
-useconfigure=true
+port='bison'
+version='3.8'
+useconfigure='true'
 configopts=("--prefix=${SERENITY_INSTALL_ROOT}/usr/local")
-files="https://ftpmirror.gnu.org/gnu/bison/bison-${version}.tar.gz bison-${version}.tar.gz 356bff0a058ca3d59528e0c49e68b90cdeb09779e0d626fc78a94270beed93a6"
-auth_type=sha256
+files="https://ftpmirror.gnu.org/gnu/bison/bison-${version}.tar.gz bison-${version}.tar.gz d5d184d421aee15603939973a6b0f372f908edfb24c5bc740697497021ad9458"
+auth_type='sha256'
