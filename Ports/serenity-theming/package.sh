afc067bc380f0bebef9b9d1518dff4e90f805ec6
djwisdom <djwisdom@gmail.com>
2023-03-27 08:15:59 +0800

    Ports: Update serenity-theming use latest commit ba2ddd0
    Snake: Add Buggie skin
    Snake: Add CatDog skin
    Chess: Add ugly piece set
    Base: Add Hotdog Stand theme

--- Ports/serenity-theming/package.sh
+++ Ports/serenity-theming/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=serenity-theming
-version=7a39b7d6830aeba13ba88bb6e14a7a0d0825a370
+version=ba2ddd0c24fe584f7756285598accf12c093fa26
 workdir="theming-${version}"
-files="https://github.com/SerenityOS/theming/archive/${version}.zip serenity-theming-${version}.zip e2b587906ce4d96a9d91f9774c204c3c82707e9d4763d6bc6b23966a6162381a"
+files="https://github.com/SerenityOS/theming/archive/${version}.zip serenity-theming-${version}.zip 5adb965de29910bfa45b67106dc578948798c1fa5d1bafd2371f86117a38abdb"
 auth_type="sha256"
 
 build() {
@@ -21,8 +21,9 @@ post_install() {
     cp -r "${workdir}/fonts" "${SERENITY_INSTALL_ROOT}/res/"
     cp -r "${workdir}/color-palettes" "${SERENITY_INSTALL_ROOT}/res/"
     cp -r "${workdir}/emoji" "${SERENITY_INSTALL_ROOT}/res/"
+    cp -r "${workdir}/graphics" "${SERENITY_INSTALL_ROOT}/res/"
     # FIXME: Update this path upstream.
-    cp -r "${workdir}/color-schemes/" "${SERENITY_INSTALL_ROOT}/res/color-schemes/"
+    cp -r "${workdir}/color-schemes" "${SERENITY_INSTALL_ROOT}/res/color-schemes/"
     cp -r "${workdir}/wallpapers" "${SERENITY_INSTALL_ROOT}/res/"
     cp "${workdir}/emoji-theming.txt" "${SERENITY_INSTALL_ROOT}/home/anon/Documents/emoji-theming.txt"
 }
