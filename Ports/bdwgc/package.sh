05163d3c61ba69a7aad9fb879b764283394aebfc
Cameron Youell <cameronyouell@gmail.com>
2023-03-27 20:35:57 +1100

    Ports/bdwgc: Update bdwgc to version 8.2.2

--- Ports/bdwgc/package.sh
+++ Ports/bdwgc/package.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 
-port=bdwgc
-version=8.0.6
-use_fresh_config_sub=true
-files="https://github.com/ivmai/bdwgc/releases/download/v$version/gc-$version.tar.gz bdwgc.tar.gz 3b4914abc9fa76593596773e4da671d7ed4d5390e3d46fbf2e5f155e121bea11"
-depends=(libatomic_ops)
-workdir=gc-$version
-auth_type=sha256
+port='bdwgc'
+version='8.2.2'
+use_fresh_config_sub='true'
+files="https://github.com/ivmai/bdwgc/releases/download/v$version/gc-$version.tar.gz bdwgc.tar.gz f30107bcb062e0920a790ffffa56d9512348546859364c23a14be264b38836a0"
+depends=("libatomic_ops")
+workdir="gc-$version"
+auth_type='sha256'
 
-useconfigure=true
+useconfigure='true'
 configopts=("--enable-threads=posix")
 pre_configure() {
     run ./autogen.sh
