f22bf42566136d7dad9700b7e8a95c0d4eaac13d
Cameron Youell <cameronyouell@gmail.com>
2023-03-27 21:07:46 +1100

    Ports/cmake: Update cmake to version 3.26.1

--- Ports/cmake/package.sh
+++ Ports/cmake/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='cmake'
-version='3.26.0'
+version='3.26.1'
 useconfigure='true'
-files="https://github.com/Kitware/CMake/releases/download/v${version}/cmake-${version}.tar.gz cmake-${version}.tar.gz 4256613188857e95700621f7cdaaeb954f3546a9249e942bc2f9b3c26e381365"
+files="https://github.com/Kitware/CMake/releases/download/v${version}/cmake-${version}.tar.gz cmake-${version}.tar.gz f29964290ad3ced782a1e58ca9fda394a82406a647e24d6afd4e6c32e42c412f"
 auth_type='sha256'
 depends=(
     'bash'
