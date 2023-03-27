03af829d525aac6a0457c2a63d4580cbdfbb6fb1
Annie Song <afsong2018@gmail.com>
2023-03-27 01:01:28 -0400

    Chess: Check to see if parent has exited and quit event loop

--- Userland/Services/ChessEngine/main.cpp
+++ Userland/Services/ChessEngine/main.cpp
@@ -10,12 +10,26 @@
 #include <LibCore/System.h>
 #include <LibMain/Main.h>
 
+#include <unistd.h>
+
+namespace {
+
+bool is_parent_dead()
+{
+    pid_t ppid = getppid();
+    return kill(ppid, 0) < 0;
+}
+
+}
+
 ErrorOr<int> serenity_main(Main::Arguments)
 {
-    TRY(Core::System::pledge("stdio recvfd sendfd unix"));
+    TRY(Core::System::pledge("stdio recvfd sendfd unix proc"));
     Core::EventLoop loop;
     TRY(Core::System::unveil(nullptr, nullptr));
 
     auto engine = TRY(ChessEngine::try_create(Core::DeprecatedFile::standard_input(), Core::DeprecatedFile::standard_output()));
-    return loop.exec();
+
+    loop.spin_until(is_parent_dead);
+    return 0;
 }
