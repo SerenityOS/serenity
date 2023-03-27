09f7937f162396ea35f6d322b36ef341303f93f3
Cameron Youell <cameronyouell@gmail.com>
2023-03-27 21:57:39 +1100

    Ports/imagemagick: Update imagemagick to version 7.1.1-5

--- Ports/imagemagick/package.sh
+++ Ports/imagemagick/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=imagemagick
-version=7.1.0-29
+port='imagemagick'
+version='7.1.1-5'
 workdir="ImageMagick-${version}"
 useconfigure="true"
-files="https://github.com/ImageMagick/ImageMagick/archive/refs/tags/${version}.tar.gz ${port}-v${version}.tar.gz 889be185fd17a9b9b3d4090e28aecdec289a4f690964a7964b4f893c7a8ec21c"
+files="https://github.com/ImageMagick/ImageMagick/archive/refs/tags/${version}.tar.gz ${port}-v${version}.tar.gz dd23689304b8cf41572c3af6b0ddccfe21c5b9d9abddaf978f314696408d0750"
 auth_type=sha256
 configopts=("--host=${SERENITY_ARCH}")
 depends=("libpng" "libtiff" "libjpeg")
