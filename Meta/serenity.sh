0471ec05675809a346ce1f5c011b996456b8967f
Cameron Youell <cameronyouell@gmail.com>
2023-03-28 01:55:10 +1100

    Toolchain: Update binutils to version 2.40

--- Meta/serenity.sh
+++ Meta/serenity.sh
@@ -315,7 +315,7 @@ ensure_toolchain() {
     if [ "$TOOLCHAIN_TYPE" = "GNU" ]; then
         local ld_version
         ld_version="$("$TOOLCHAIN_DIR"/bin/"$TARGET"-pc-serenity-ld -v)"
-        local expected_version="GNU ld (GNU Binutils) 2.39"
+        local expected_version="GNU ld (GNU Binutils) 2.40"
         if [ "$ld_version" != "$expected_version" ]; then
             echo "Your toolchain has an old version of binutils installed."
             echo "    installed version: \"$ld_version\""
