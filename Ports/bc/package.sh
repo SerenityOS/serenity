fb3a1e2341d11877c49f70b94d7e55b0ac81269d
Cameron Youell <cameronyouell@gmail.com>
2023-03-27 19:49:43 +1100

    Ports/bc: Update bc to version 6.5.0

--- Ports/bc/package.sh
+++ Ports/bc/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='bc'
-version='6.1.1'
-files="https://github.com/gavinhoward/bc/releases/download/${version}/bc-${version}.tar.xz bc-${version}.tar.xz b6de9e2fa4fcb1902c1686760dd90217543128f0298d418951ab1e9a03964097"
+version='6.5.0'
+files="https://github.com/gavinhoward/bc/releases/download/${version}/bc-${version}.tar.xz bc-${version}.tar.xz b1afb1f50c0bce6119c98590bcc8afc22f520bc85c2b512c83938dbb8321cc30"
 auth_type='sha256'
 useconfigure='true'
 configscript='configure.sh'
