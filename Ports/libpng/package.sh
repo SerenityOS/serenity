8fa1e521086326a8a2d3b42346ff00163e283147
Cameron Youell <cameronyouell@gmail.com>
2023-03-27 21:44:55 +1100

    Ports/libpng: Update libpng to version 1.6.39

--- Ports/libpng/package.sh
+++ Ports/libpng/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=libpng
-version=1.6.37
-useconfigure=true
+port='libpng'
+version='1.6.39'
+useconfigure='true'
 configopts=("--disable-static" "--enable-shared")
-use_fresh_config_sub=true
-files="https://download.sourceforge.net/libpng/libpng-${version}.tar.gz libpng-${version}.tar.gz daeb2620d829575513e35fecc83f0d3791a620b9b93d800b763542ece9390fb4"
-auth_type=sha256
+use_fresh_config_sub='true'
+files="https://download.sourceforge.net/libpng/libpng-${version}.tar.gz libpng-${version}.tar.gz af4fb7f260f839919e5958e5ab01a275d4fe436d45442a36ee62f73e5beb75ba"
+auth_type='sha256'
 depends=("zlib")
