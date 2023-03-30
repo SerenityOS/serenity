d4b2544dc55785f7233f84943ae579be31e43e5e
Andreas Kling <kling@serenityos.org>
2023-03-30 11:14:26 +0200

    LibWeb: Make the Node.compareDocumentPosition() return value enum public
    This will allow other parts of LibWeb to understand these values.

--- Userland/Libraries/LibWeb/DOM/Node.h
+++ Userland/Libraries/LibWeb/DOM/Node.h
@@ -101,6 +101,17 @@ public:
     void insert_before(JS::NonnullGCPtr<Node> node, JS::GCPtr<Node> child, bool suppress_observers = false);
     void remove(bool suppress_observers = false);
     void remove_all_children(bool suppress_observers = false);
+
+    enum DocumentPosition : u16 {
+        DOCUMENT_POSITION_EQUAL = 0,
+        DOCUMENT_POSITION_DISCONNECTED = 1,
+        DOCUMENT_POSITION_PRECEDING = 2,
+        DOCUMENT_POSITION_FOLLOWING = 4,
+        DOCUMENT_POSITION_CONTAINS = 8,
+        DOCUMENT_POSITION_CONTAINED_BY = 16,
+        DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC = 32,
+    };
+
     u16 compare_document_position(JS::GCPtr<Node> other);
 
     WebIDL::ExceptionOr<JS::NonnullGCPtr<Node>> replace_child(JS::NonnullGCPtr<Node> node, JS::NonnullGCPtr<Node> child);
