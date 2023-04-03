3b56fd27a9b64f0e6b97d34e1aba9cef9af08213
Andreas Kling <kling@serenityos.org>
2023-04-03 16:37:34 +0200

    Kernel: Mark sys$umask as not needing the big lock
    The body of this syscall is already serialized by calling
    with_mutable_protected_data().

--- Kernel/Syscalls/umask.cpp
+++ Kernel/Syscalls/umask.cpp
@@ -10,7 +10,7 @@ namespace Kernel {
 
 ErrorOr<FlatPtr> Process::sys$umask(mode_t mask)
 {
-    VERIFY_PROCESS_BIG_LOCK_ACQUIRED(this);
+    VERIFY_NO_PROCESS_BIG_LOCK(this);
     TRY(require_promise(Pledge::stdio));
     return with_mutable_protected_data([&](auto& protected_data) -> ErrorOr<FlatPtr> {
         auto old_mask = protected_data.umask;
