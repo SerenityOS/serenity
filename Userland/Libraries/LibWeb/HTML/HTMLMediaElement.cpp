8f64a0d98dcb8979f5ece7d30131ed45d0185796
Timothy Flynn <trflynn89@pm.me>
2023-04-04 14:20:42 -0400

    LibWeb: Indicate that we may be able to play video MIME types

--- Userland/Libraries/LibWeb/HTML/HTMLMediaElement.cpp
+++ Userland/Libraries/LibWeb/HTML/HTMLMediaElement.cpp
@@ -22,6 +22,7 @@
 #include <LibWeb/HTML/TrackEvent.h>
 #include <LibWeb/HTML/VideoTrack.h>
 #include <LibWeb/HTML/VideoTrackList.h>
+#include <LibWeb/MimeSniff/MimeType.h>
 
 namespace Web::HTML {
 
@@ -66,8 +67,10 @@ void HTMLMediaElement::parse_attribute(DeprecatedFlyString const& name, Deprecat
 }
 
 // https://html.spec.whatwg.org/multipage/media.html#dom-navigator-canplaytype
-Bindings::CanPlayTypeResult HTMLMediaElement::can_play_type(DeprecatedString const& type) const
+WebIDL::ExceptionOr<Bindings::CanPlayTypeResult> HTMLMediaElement::can_play_type(DeprecatedString const& type) const
 {
+    auto& vm = this->vm();
+
     // The canPlayType(type) method must:
     // - return the empty string if type is a type that the user agent knows it cannot render or is the type "application/octet-stream"
     // - return "probably" if the user agent is confident that the type represents a media resource that it can render if used in with this audio or video element
@@ -75,10 +78,15 @@ Bindings::CanPlayTypeResult HTMLMediaElement::can_play_type(DeprecatedString con
     // Generally, a user agent should never return "probably" for a type that allows the codecs parameter if that parameter is not present.
     if (type == "application/octet-stream"sv)
         return Bindings::CanPlayTypeResult::Empty;
-    // FIXME: Eventually we should return `Maybe` here, but for now `Empty` is our best bet :^)
-    //        Being honest here leads to some apps and frameworks skipping things like audio loading,
-    //        which for the time being would create more issues than it solves - e.g. endless waiting
-    //        for audio that will never load.
+
+    auto mime_type = TRY_OR_THROW_OOM(vm, MimeSniff::MimeType::parse(type));
+
+    if (mime_type.has_value() && mime_type->type() == "video"sv) {
+        if (mime_type->subtype() == "webm"sv)
+            return Bindings::CanPlayTypeResult::Probably;
+        return Bindings::CanPlayTypeResult::Maybe;
+    }
+
     return Bindings::CanPlayTypeResult::Empty;
 }
 
