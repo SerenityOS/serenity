d1d3a020cb93c612b514896bf4eca79578d75138
Timothy Flynn <trflynn89@pm.me>
2023-04-04 09:02:00 -0400

    LibWeb: Support unique task sources
    Some elements, like HTMLMediaElement, must have a unique task sources
    for every instance of that element that is created. Support this with a
    simple wrapper around IDAllocator.

--- Userland/Libraries/LibWeb/HTML/EventLoop/Task.cpp
+++ Userland/Libraries/LibWeb/HTML/EventLoop/Task.cpp
@@ -4,10 +4,13 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <AK/IDAllocator.h>
 #include <LibWeb/DOM/Document.h>
 #include <LibWeb/HTML/EventLoop/Task.h>
 
 namespace Web::HTML {
+
+static IDAllocator s_unique_task_source_allocator { static_cast<int>(Task::Source::UniqueTaskSourceStart) };
 
 Task::Task(Source source, DOM::Document const* document, JS::SafeFunction<void()> steps)
     : m_source(source)
@@ -33,6 +36,16 @@ bool Task::is_runnable() const
 DOM::Document const* Task::document() const
 {
     return m_document.ptr();
+}
+
+UniqueTaskSource::UniqueTaskSource()
+    : source(static_cast<Task::Source>(s_unique_task_source_allocator.allocate()))
+{
+}
+
+UniqueTaskSource::~UniqueTaskSource()
+{
+    s_unique_task_source_allocator.deallocate(static_cast<int>(source));
 }
 
 }
