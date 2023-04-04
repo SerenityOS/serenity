ba1e46a0385a27e58f5141cbac960cd50d1fefca
Andreas Kling <kling@serenityos.org>
2023-04-04 15:35:32 +0200

    Kernel: Make SlavePTY store pointer to MasterPTY as NonnullRefPtr
    No need for LockRefPtr here, as the pointer never changes after
    initialization.

--- Kernel/TTY/SlavePTY.h
+++ Kernel/TTY/SlavePTY.h
@@ -42,9 +42,9 @@ private:
     virtual ErrorOr<void> close() override;
 
     friend class MasterPTY;
-    SlavePTY(MasterPTY&, unsigned index);
+    SlavePTY(NonnullRefPtr<MasterPTY>, unsigned index);
 
-    LockRefPtr<MasterPTY> m_master;
+    NonnullRefPtr<MasterPTY> const m_master;
     time_t m_time_of_last_write { 0 };
     unsigned m_index { 0 };
 
