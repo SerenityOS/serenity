6733fed7b9dd339d0ec088cdaf8a3acc32fceea1
Liav A <liavalb@gmail.com>
2023-04-05 06:20:36 +0300

    Kernel: Mark faccessat syscall as not needing the big lock
    Like other filesystem syscalls, there's nothing special about this kind
    of syscall, so no special locking is required.

--- Kernel/Syscalls/faccessat.cpp
+++ Kernel/Syscalls/faccessat.cpp
@@ -12,7 +12,7 @@ namespace Kernel {
 
 ErrorOr<FlatPtr> Process::sys$faccessat(Userspace<Syscall::SC_faccessat_params const*> user_params)
 {
-    VERIFY_PROCESS_BIG_LOCK_ACQUIRED(this);
+    VERIFY_NO_PROCESS_BIG_LOCK(this);
     TRY(require_promise(Pledge::rpath));
     auto params = TRY(copy_typed_from_user(user_params));
     auto pathname = TRY(get_syscall_path_argument(params.pathname));
