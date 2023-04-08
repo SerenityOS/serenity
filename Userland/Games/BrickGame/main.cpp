00249ac43d5426729ba2333c671c2d47c52d4acb
Karol Baraniecki <karol@baraniecki.eu>
2023-04-08 16:48:15 +0200

    BrickGame: Use title case in menu items consistently
    by fixing the "Toggle pause" option

--- Userland/Games/BrickGame/main.cpp
+++ Userland/Games/BrickGame/main.cpp
@@ -56,7 +56,7 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
     TRY(game_menu->try_add_action(GUI::Action::create("&New Game", { Mod_None, Key_F2 }, TRY(Gfx::Bitmap::load_from_file("/res/icons/16x16/reload.png"sv)), [&](auto&) {
         game->reset();
     })));
-    TRY(game_menu->try_add_action(GUI::Action::create("Toggle &pause", { Mod_None, Key_P }, [&](auto&) {
+    TRY(game_menu->try_add_action(GUI::Action::create("Toggle &Pause", { Mod_None, Key_P }, [&](auto&) {
         game->toggle_pause();
     })));
     TRY(game_menu->try_add_separator());
