fdd33d818cff589985ae50b8cf06d29608d93fbc
Kenneth Myhra <kennethmyhra@gmail.com>
2023-04-09 10:13:46 +0200

    LibWeb: Port DOMEventListener to new FlyString

--- Userland/Libraries/LibWeb/DOM/DOMEventListener.h
+++ Userland/Libraries/LibWeb/DOM/DOMEventListener.h
@@ -6,7 +6,7 @@
 
 #pragma once
 
-#include <AK/DeprecatedFlyString.h>
+#include <AK/FlyString.h>
 #include <LibJS/Heap/GCPtr.h>
 #include <LibJS/Runtime/Object.h>
 #include <LibWeb/Forward.h>
@@ -21,7 +21,7 @@ public:
     ~DOMEventListener();
 
     // type (a string)
-    DeprecatedFlyString type;
+    FlyString type;
 
     // callback (null or an EventListener object)
     JS::GCPtr<IDLEventListener> callback;
