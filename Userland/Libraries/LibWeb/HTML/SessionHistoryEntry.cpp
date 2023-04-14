aa33a3381bd88a88941ef89df79e6679fd02b82e
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2023-04-14 22:27:52 +0300

    LibWeb: Add missing properties from latest spec in SessionHistoryEntry
    Adds step and document_state properties. Both will be required for
    further navigables spec implementation.
    
    Co-authored-by: Andreas Kling <kling@serenityos.org>

--- Userland/Libraries/LibWeb/HTML/SessionHistoryEntry.cpp
+++ Userland/Libraries/LibWeb/HTML/SessionHistoryEntry.cpp
@@ -4,7 +4,8 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#include <LibWeb/DOM/Document.h>
+#include <LibWeb/HTML/BrowsingContext.h>
+#include <LibWeb/HTML/DocumentState.h>
 #include <LibWeb/HTML/SessionHistoryEntry.h>
 
 namespace Web::HTML {
@@ -12,7 +13,7 @@ namespace Web::HTML {
 void SessionHistoryEntry::visit_edges(Cell::Visitor& visitor)
 {
     Base::visit_edges(visitor);
-    visitor.visit(document);
+    visitor.visit(document_state);
     visitor.visit(original_source_browsing_context);
 }
 
