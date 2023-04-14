ba5bf412e53d71553893a7f3509b01816ee9ed96
Zaggy1024 <zaggy1024@gmail.com>
2023-04-13 20:48:55 -0500

    LibThreading: Create WorkerThread class run a single task concurrently
    This class can be used to run a task in another thread, and allows the
    caller to wait for the task to complete to retrieve any error that may
    have occurred.
    
    Currently, it doesn't support functions returning a value on success,
    but with some template magic that should be possible. :^)

---
+++ Userland/Libraries/LibThreading/Forward.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2019-2020, Sergey Bugaev <bugaevc@serenityos.org>
+ * Copyright (c) 2021, Spencer Dixon <spencercdixon@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+namespace Threading {
+
+template<typename ErrorType>
+class WorkerThread;
+
+}
