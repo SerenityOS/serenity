4aaefeb1d5b71405fbdd974c283e248c1a460a57
Nico Weber <thakis@chromium.org>
2023-04-15 17:34:18 -0400

    Meta: Move some flags to common_compile_options.cmake
    No behavior change.

--- Meta/CMake/common_compile_options.cmake
+++ Meta/CMake/common_compile_options.cmake
@@ -1,9 +1,16 @@
+# Flags shared by Lagom (including Ladybird) and Serenity.
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
 add_compile_options(-Wall)
 add_compile_options(-Wextra)
+
+add_compile_options(-Wno-unknown-warning-option)
+add_compile_options(-Wno-unused-command-line-argument)
+
+add_compile_options(-fdiagnostics-color=always)
+add_compile_options(-fno-exceptions)
 
 if (NOT CMAKE_HOST_SYSTEM_NAME MATCHES SerenityOS)
     # FIXME: Something makes this go crazy and flag unused variables that aren't flagged as such when building with the toolchain.
