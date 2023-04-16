4e661a400bdd0b29f6e24d3071b875dc4a06c81d
Kenneth Myhra <kennethmyhra@gmail.com>
2023-04-16 20:06:30 +0200

    Ports: Add libslirp version 4.7.0
    The slirp submodule was removed from QEMU in version 7.2, running
    Serenity in QEMU on a Serenity host could fail without this one if
    running with network enabled.
    
    For more information on the removal of the slirp submodule see the
    following link: https://wiki.qemu.org/ChangeLog/7.2#Removal_of_the_%22slirp%22_submodule_(affects_%22-netdev_user%22)

---
+++ Ports/libslirp/package.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port='libslirp'
+version='4.7.0'
+workdir="libslirp-v${version}"
+files="https://gitlab.freedesktop.org/slirp/libslirp/-/archive/v${version}/libslirp-v${version}.tar.gz libslirp-v${version}.tar.xz 9398f0ec5a581d4e1cd6856b88ae83927e458d643788c3391a39e61b75db3d3b"
+auth_type='sha256'
+useconfigure='true'
+configopts=("--cross-file=${SERENITY_BUILD_DIR}/meson-cross-file.txt")
+depends=('glib')
+
+configure() {
+    run meson _build "${configopts[@]}"
+}
+
+build() {
+    run ninja -C _build
+}
+
+install() {
+    export DESTDIR="${SERENITY_INSTALL_ROOT}"
+    run meson install -C _build
+}
