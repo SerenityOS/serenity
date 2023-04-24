1a3009fa2bce7942efeec1a64ed1d50b460cd1ff
LekKit <50500857+LekKit@users.noreply.github.com>
2023-04-24 09:14:14 +0300

    Ports: Fix RVVM port syntax

--- Ports/rvvm/package.sh
+++ Ports/rvvm/package.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='rvvm'
 version='0.5'
-archive_hash=3a1dbb91ad04f068078bc6c6c27cc5792eebc111907cb5a14bde158fe6e757c9
-files="https://github.com/LekKit/RVVM/archive/v$version.tar.gz rvvm.tar.gz $archive_hash"
+archive_hash='3a1dbb91ad04f068078bc6c6c27cc5792eebc111907cb5a14bde158fe6e757c9'
+files="https://github.com/LekKit/RVVM/archive/v${version}.tar.gz rvvm.tar.gz ${archive_hash}"
 auth_type='sha256'
-workdir="RVVM-$version"
+workdir="RVVM-${version}"
 depends=('sdl12-compat')
 
 build() {
