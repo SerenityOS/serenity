dc6f8679d88f207594950e52a542f512ec222534
implicitfield <114500360+implicitfield@users.noreply.github.com>
2023-04-29 23:05:59 +0400

    LibC: Drop complex.cpp and move its definitions to complex.h
    libc++ disallows including LibC's complex.h in C++ mode. This means that
    a C++ file cannot expect LibC's complex.h to be included, and thus
    cannot use c-prefixed complex number functions. As a result,
    complex.cpp is broken when libc++ has a higher include priority
    than LibC.
    
    A check for __cplusplus has been added to complex.h to warn users of
    toolchains that don't use libc++.

--- Userland/Libraries/LibC/complex.h
+++ Userland/Libraries/LibC/complex.h
@@ -11,6 +11,10 @@
 
 #pragma once
 
+#ifdef __cplusplus
+#    error "C++ code must not include complex.h. Use AK/Complex.h instead."
+#endif
+
 #include <stddef.h>
 #include <sys/cdefs.h>
 
@@ -25,14 +29,6 @@ __BEGIN_DECLS
 #define CMPLXF(x, y) ((float complex)__builtin_complex((float)x, (float)y))
 #define CMPLXL(x, y) ((long double complex)__builtin_complex((long double)x, (long double)y))
 
-float crealf(float complex z);
-double creal(double complex z);
-long double creall(long double complex z);
-
-double cimag(double complex z);
-float cimagf(float complex z);
-long double cimagl(long double complex z);
-
 // These are macro implementations of the above functions, so that they will always be inlined.
 #define creal(z) ((double)__real__((double complex)z))
 #define crealf(z) ((float)__real__((float complex)z))
@@ -41,5 +37,41 @@ long double cimagl(long double complex z);
 #define cimag(z) ((double)__imag__((double complex)z))
 #define cimagf(z) ((float)__imag__((float complex)z))
 #define cimagl(z) ((long double)__imag__((long double complex)z))
+
+// Function definitions of this form "type (name)(args)" are intentional, to
+// prevent macro versions of "name" from being incorrectly expanded. These
+// functions are here to provide external linkage to their macro implementations.
+
+// https://pubs.opengroup.org/onlinepubs/9699919799/functions/creal.html
+inline float(crealf)(float complex z)
+{
+    return crealf(z);
+}
+
+inline double(creal)(double complex z)
+{
+    return creal(z);
+}
+
+inline long double(creall)(long double complex z)
+{
+    return creall(z);
+}
+
+// https://pubs.opengroup.org/onlinepubs/9699919799/functions/cimag.html
+inline double(cimag)(double complex z)
+{
+    return cimag(z);
+}
+
+inline float(cimagf)(float complex z)
+{
+    return cimagf(z);
+}
+
+inline long double(cimagl)(long double complex z)
+{
+    return cimagl(z);
+}
 
 __END_DECLS
