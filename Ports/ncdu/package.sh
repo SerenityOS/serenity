e67f3c2291c8569ee0361f97ce35f41baa8c89fe
Daniel van Gerpen <daniel@vangerpen.de>
2023-04-30 12:45:47 +0200

    Ports/ncdu: Update to version 1.18.1

--- Ports/ncdu/package.sh
+++ Ports/ncdu/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='ncdu'
-version='1.17'
-files="https://dev.yorhel.nl/download/ncdu-${version}.tar.gz ncdu-${version}.tar.gz 810745a8ed1ab3788c87d3aea4cc1a14edf6ee226f764bcc383e024ba56adbf1"
+version='1.18.1'
+files="https://dev.yorhel.nl/download/ncdu-${version}.tar.gz ncdu-${version}.tar.gz 7c0fa1eb29d85aaed4ba174164bdbb8f011b5c390d017c57d668fc7231332405"
 auth_type='sha256'
 useconfigure='true'
 depends=("ncurses")
