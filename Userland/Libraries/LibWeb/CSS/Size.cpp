cdf0d3e905fea06ba6d2fbd00e6f3f1141b02099
Andreas Kling <kling@serenityos.org>
2023-05-06 16:33:06 +0200

    LibWeb: Add to_px() helpers to CSS::Size and CSS::PercentageOr<T>
    Old pattern:
    
        foo.resolved(node, reference_value).to_px(node)
    
    New pattern:
    
        foo.to_px(node, reference_value)
    
    Also, the reference value for to_px() is a CSSPixels, which means we
    don't have to synthesize a CSS::Length just for this call.

--- Userland/Libraries/LibWeb/CSS/Size.cpp
+++ Userland/Libraries/LibWeb/CSS/Size.cpp
@@ -14,6 +14,11 @@ Size::Size(Type type, LengthPercentage length_percentage)
 {
 }
 
+CSSPixels Size::to_px(Layout::Node const& node, CSSPixels reference_value) const
+{
+    return m_length_percentage.resolved(node, CSS::Length::make_px(reference_value)).to_px(node);
+}
+
 CSS::Length Size::resolved(Layout::Node const& node, Length const& reference_value) const
 {
     return m_length_percentage.resolved(node, reference_value);
