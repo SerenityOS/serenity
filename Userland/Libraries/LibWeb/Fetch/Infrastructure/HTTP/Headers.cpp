29d90ccf3b1cb1146573a7ff3b5e5dbe42863ce3
Timothy Flynn <trflynn89@pm.me>
2023-05-10 16:26:51 -0400

    LibWeb: Implement the legacy extracting an encoding AO

--- Userland/Libraries/LibWeb/Fetch/Infrastructure/HTTP/Headers.cpp
+++ Userland/Libraries/LibWeb/Fetch/Infrastructure/HTTP/Headers.cpp
@@ -15,6 +15,7 @@
 #include <LibJS/Heap/Heap.h>
 #include <LibJS/Runtime/VM.h>
 #include <LibRegex/Regex.h>
+#include <LibTextCodec/Decoder.h>
 #include <LibWeb/Fetch/Infrastructure/HTTP.h>
 #include <LibWeb/Fetch/Infrastructure/HTTP/Headers.h>
 #include <LibWeb/Fetch/Infrastructure/HTTP/Methods.h>
@@ -390,6 +391,29 @@ ErrorOr<Optional<MimeSniff::MimeType>> HeaderList::extract_mime_type() const
     // 7. If mimeType is null, then return failure.
     // 8. Return mimeType.
     return mime_type;
+}
+
+// https://fetch.spec.whatwg.org/#legacy-extract-an-encoding
+StringView legacy_extract_an_encoding(Optional<MimeSniff::MimeType> const& mime_type, StringView fallback_encoding)
+{
+    // 1. If mimeType is failure, then return fallbackEncoding.
+    if (!mime_type.has_value())
+        return fallback_encoding;
+
+    // 2. If mimeType["charset"] does not exist, then return fallbackEncoding.
+    auto charset = mime_type->parameters().get("charset"sv);
+    if (!charset.has_value())
+        return fallback_encoding;
+
+    // 3. Let tentativeEncoding be the result of getting an encoding from mimeType["charset"].
+    auto tentative_encoding = TextCodec::get_standardized_encoding(*charset);
+
+    // 4. If tentativeEncoding is failure, then return fallbackEncoding.
+    if (!tentative_encoding.has_value())
+        return fallback_encoding;
+
+    // 5. Return tentativeEncoding.
+    return *tentative_encoding;
 }
 
 // https://fetch.spec.whatwg.org/#convert-header-names-to-a-sorted-lowercase-set
