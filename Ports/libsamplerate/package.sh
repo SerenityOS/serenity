f947c4f4fccd214bc5b3a9b938bc2540769df4cb
Mike Swanson <mikeonthecomputer@gmail.com>
2023-05-10 16:00:53 -0700

    Ports: Add libsamplerate 0.2.2

---
+++ Ports/libsamplerate/package.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port='libsamplerate'
+version='0.2.2'
+useconfigure='true'
+use_fresh_config_sub='true'
+config_sub_paths=('build-aux/config.sub')
+configopts=(
+    '--disable-static'
+    '--enable-shared'
+)
+files="https://github.com/libsndfile/libsamplerate/releases/download/${version}/libsamplerate-${version}.tar.xz libsamplerate-${version}.tar.xz 3258da280511d24b49d6b08615bbe824d0cacc9842b0e4caf11c52cf2b043893"
+auth_type='sha256'
