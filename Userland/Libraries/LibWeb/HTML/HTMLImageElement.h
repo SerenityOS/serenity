c5f479fa7d122978b7fc9c18c9b444b2f553bebd
Andreas Kling <kling@serenityos.org>
2023-05-12 07:48:59 +0200

    LibWeb: Let HTMLImageElement delay the document load event again

--- Userland/Libraries/LibWeb/HTML/HTMLImageElement.h
+++ Userland/Libraries/LibWeb/HTML/HTMLImageElement.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2018-2023, Andreas Kling <kling@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
