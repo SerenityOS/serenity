f62bcba56ade843e91e9a22b3a43f3fa2aceca82
Luke Wilde <lukew@serenityos.org>
2023-05-13 13:27:30 +0100

    LibWeb: Specify snake case names for navigation timing entries too
    This will be used by performance.measure to read the value of a given
    entry in the NavigationTiming interface.

--- Userland/Libraries/LibWeb/NavigationTiming/EntryNames.h
+++ Userland/Libraries/LibWeb/NavigationTiming/EntryNames.h
@@ -10,30 +10,30 @@
 
 namespace Web::NavigationTiming::EntryNames {
 
-#define ENUMERATE_NAVIGATION_TIMING_ENTRY_NAMES                          \
-    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(navigationStart)            \
-    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(unloadEventStart)           \
-    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(unloadEventEnd)             \
-    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(redirectStart)              \
-    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(redirectEnd)                \
-    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(fetchStart)                 \
-    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(domainLookupStart)          \
-    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(domainLookupEnd)            \
-    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(connectStart)               \
-    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(connectEnd)                 \
-    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(secureConnectionStart)      \
-    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(requestStart)               \
-    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(responseStart)              \
-    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(responseEnd)                \
-    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(domLoading)                 \
-    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(domInteractive)             \
-    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(domContentLoadedEventStart) \
-    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(domContentLoadedEventEnd)   \
-    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(domComplete)                \
-    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(loadEventStart)             \
-    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(loadEventEnd)
+#define ENUMERATE_NAVIGATION_TIMING_ENTRY_NAMES                                                          \
+    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(navigationStart, navigation_start)                          \
+    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(unloadEventStart, unload_event_start)                       \
+    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(unloadEventEnd, unload_event_end)                           \
+    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(redirectStart, redirect_start)                              \
+    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(redirectEnd, redirect_end)                                  \
+    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(fetchStart, fetch_start)                                    \
+    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(domainLookupStart, domain_lookup_start)                     \
+    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(domainLookupEnd, domain_lookup_end)                         \
+    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(connectStart, connect_start)                                \
+    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(connectEnd, connect_end)                                    \
+    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(secureConnectionStart, secure_connection_start)             \
+    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(requestStart, request_start)                                \
+    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(responseStart, response_start)                              \
+    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(responseEnd, response_end)                                  \
+    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(domLoading, dom_loading)                                    \
+    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(domInteractive, dom_interactive)                            \
+    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(domContentLoadedEventStart, dom_content_loaded_event_start) \
+    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(domContentLoadedEventEnd, dom_content_loaded_event_end)     \
+    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(domComplete, dom_complete)                                  \
+    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(loadEventStart, load_event_start)                           \
+    __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(loadEventEnd, load_event_end)
 
-#define __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(name) extern FlyString name;
+#define __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME(name, _) extern FlyString name;
 ENUMERATE_NAVIGATION_TIMING_ENTRY_NAMES
 #undef __ENUMERATE_NAVIGATION_TIMING_ENTRY_NAME
 
