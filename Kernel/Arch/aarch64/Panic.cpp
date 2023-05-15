a30e9b0e46abd3c77a7167c0105781dd8a374cb1
Daniel Bertalan <dani@danielbertalan.dev>
2023-05-15 07:38:47 +0200

    Kernel: Unify x86-64 and AArch64 `__panic` implementation
    We now have everything in the AArch64 kernel to be able to use the full
    `__panic` implementation, so we can share the code with x86-64.
    
    I have kept `__assertion_failed` separate for now, as the x86-64 version
    directly executes inline assembly, thus `Kernel/Arch/aarch64/Panic.cpp`
    could not be removed.

--- Kernel/Arch/aarch64/Panic.cpp
+++ Kernel/Arch/aarch64/Panic.cpp
@@ -22,11 +22,3 @@ using namespace Kernel;
     // Used for printing a nice backtrace!
     PANIC("Aborted");
 }
-
-void Kernel::__panic(char const* file, unsigned int line, char const* function)
-{
-    critical_dmesgln("at {}:{} in {}", file, line, function);
-    dump_backtrace(PrintToScreen::Yes);
-
-    Processor::halt();
-}
