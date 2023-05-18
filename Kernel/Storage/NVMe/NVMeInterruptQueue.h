d6c9ba2a5e88c249b7a36fa116a4ace94cc06715
Pankaj Raghav <dev@pankajraghav.com>
2023-05-18 13:19:14 +0200

    Kernel: Move NVMeInterruptQueue initialization out of its constructor
    Add a helper initialize_interrupt_queue() helper to enable_irq instead
    of doing it as part of its object construction as it can fail. This is
    similar to how AHCI initializes its interrupt as well.

--- Kernel/Storage/NVMe/NVMeInterruptQueue.h
+++ Kernel/Storage/NVMe/NVMeInterruptQueue.h
@@ -18,6 +18,7 @@ public:
     void submit_sqe(NVMeSubmission& submission) override;
     virtual ~NVMeInterruptQueue() override {};
     virtual StringView purpose() const override { return "NVMe"sv; };
+    void initialize_interrupt_queue();
 
 protected:
     NVMeInterruptQueue(PCI::Device& device, NonnullOwnPtr<Memory::Region> rw_dma_region, NonnullRefPtr<Memory::PhysicalPage> rw_dma_page, u16 qid, u8 irq, u32 q_depth, OwnPtr<Memory::Region> cq_dma_region, OwnPtr<Memory::Region> sq_dma_region, Memory::TypedMapping<DoorbellRegister volatile> db_regs);
