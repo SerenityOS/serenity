SerenityOS/serenity

commit 25d2828ef5b2ec2d8a3ffb9ae105183e9f23c374

aplay: Prefer FileSystem over DeprecatedFile

Userland/Utilities/aplay.cpp
@@ -10,9 +10,9 @@
 #include <LibAudio/Loader.h>
 #include <LibAudio/Resampler.h>
 #include <LibCore/ArgsParser.h>
-#include <LibCore/DeprecatedFile.h>
 #include <LibCore/EventLoop.h>
 #include <LibCore/System.h>
+#include <LibFileSystem/FileSystem.h>
 #include <LibMain/Main.h>
 #include <math.h>
 #include <stdio.h>
@@ -35,10 +35,8 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
     args_parser.add_option(show_sample_progress, "Show playback progress in samples", "sample-progress", 's');
     args_parser.parse(arguments);
 
-    auto absolute_path = Core::DeprecatedFile::absolute_path(path);
-
     TRY(Core::System::unveil("/tmp/session/%sid/portal/audio", "rw"));
-    TRY(Core::System::unveil(absolute_path, "r"sv));
+    TRY(Core::System::unveil(TRY(FileSystem::absolute_path(path)), "r"sv));
     TRY(Core::System::unveil(nullptr, nullptr));
 
     Core::EventLoop loop;

Userland/Utilities/CMakeLists.txt
@@ -72,7 +72,7 @@ install(CODE "file(CREATE_LINK grep ${CMAKE_INSTALL_PREFIX}/bin/fgrep SYMBOLIC)"
 install(CODE "file(CREATE_LINK grep ${CMAKE_INSTALL_PREFIX}/bin/rgrep SYMBOLIC)")
 
 target_link_libraries(abench PRIVATE LibAudio LibFileSystem)
-target_link_libraries(aplay PRIVATE LibAudio LibIPC)
+target_link_libraries(aplay PRIVATE LibAudio LibFileSystem LibIPC)
 target_link_libraries(asctl PRIVATE LibAudio LibIPC)
 target_link_libraries(bt PRIVATE LibSymbolication)
 target_link_libraries(checksum PRIVATE LibCrypto)

commit 819d70a5535f2105a98e55c81382f30ca0e7f3de

aplay: Determine absolute path before raising the veil
This was a regression introduced in 25d2828e, #18807. In that commit, I
forgot to investigate why the order of operations was so "weird", so I
added a comment this time to prevent future regressions.

Userland/Utilities/aplay.cpp
@@ -35,8 +35,11 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
     args_parser.add_option(show_sample_progress, "Show playback progress in samples", "sample-progress", 's');
     args_parser.parse(arguments);
 
+    // Note: We must determine the absolute path *before* beginning to raise the veil.
+    auto absolute_path = TRY(FileSystem::absolute_path(path));
+
     TRY(Core::System::unveil("/tmp/session/%sid/portal/audio", "rw"));
-    TRY(Core::System::unveil(TRY(FileSystem::absolute_path(path)), "r"sv));
+    TRY(Core::System::unveil(absolute_path, "r"sv));
     TRY(Core::System::unveil(nullptr, nullptr));
 
     Core::EventLoop loop;