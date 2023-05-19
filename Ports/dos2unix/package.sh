01b85478c967cdfab68d1c293ce8de3a2e9650e3
Nathan Ell <nathanjamesell@gmail.com>
2023-05-18 23:57:36 -0600

    Ports/dos2unix: Update to 7.5.0
    Version 7.5.0 adds two new arguments for the command line, to add a line
    break to the last line if one isn't present, and to write output to
    stdout.

--- Ports/dos2unix/package.sh
+++ Ports/dos2unix/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=dos2unix
-version=7.4.4
+version=7.5.0
 workdir="${port}-${version}"
 files="https://waterlan.home.xs4all.nl/dos2unix/dos2unix-${version}.tar.gz ${port}-${version}.tar.gz
 https://waterlan.home.xs4all.nl/dos2unix/dos2unix-${version}.tar.gz.asc ${port}-${version}.tar.gz.asc"
