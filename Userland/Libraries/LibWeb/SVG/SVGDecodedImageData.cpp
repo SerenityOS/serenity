324c1c284fbcb8041c93a314463c23057251e926
Andreas Kling <kling@serenityos.org>
2023-05-20 16:34:52 +0200

    LibWeb: Stub out a new SVGDecodedImageData class
    This class will implement isolated SVG layout and rendering.

---
+++ Userland/Libraries/LibWeb/SVG/SVGDecodedImageData.cpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2023, Andreas Kling <kling@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibGfx/Bitmap.h>
+#include <LibWeb/SVG/SVGDecodedImageData.h>
+
+namespace Web::SVG {
+
+ErrorOr<NonnullRefPtr<SVGDecodedImageData>> SVGDecodedImageData::create(ByteBuffer)
+{
+    return adopt_nonnull_ref_or_enomem(new (nothrow) SVGDecodedImageData());
+}
+
+SVGDecodedImageData::SVGDecodedImageData()
+{
+}
+
+SVGDecodedImageData::~SVGDecodedImageData() = default;
+
+RefPtr<Gfx::Bitmap const> SVGDecodedImageData::bitmap(size_t) const
+{
+    return nullptr;
+}
+
+Optional<CSSPixels> SVGDecodedImageData::intrinsic_width() const
+{
+    return 0;
+}
+
+Optional<CSSPixels> SVGDecodedImageData::intrinsic_height() const
+{
+    return 0;
+}
+
+Optional<float> SVGDecodedImageData::intrinsic_aspect_ratio() const
+{
+    return 1;
+}
+
+}
