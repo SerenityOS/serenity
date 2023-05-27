79fc4c8a8283bbe89ea9c8badbe0368ac5b8873d
stelar7 <dudedbz@gmail.com>
2023-05-27 22:56:41 +0200

    LibWeb: Implement CSS `abs()`

--- Userland/Libraries/LibWeb/CSS/Parser/Parser.cpp
+++ Userland/Libraries/LibWeb/CSS/Parser/Parser.cpp
@@ -3570,6 +3570,18 @@ ErrorOr<OwnPtr<CalculationNode>> Parser::parse_clamp_function(Function const& fu
     return TRY(ClampCalculationNode::create(move(calculated_parameters[0]), move(calculated_parameters[1]), move(calculated_parameters[2])));
 }
 
+ErrorOr<OwnPtr<CalculationNode>> Parser::parse_abs_function(Function const& function)
+{
+    auto calculation_node = TRY(parse_a_calculation(function.values()));
+
+    if (!calculation_node) {
+        dbgln_if(CSS_PARSER_DEBUG, "abs() parameter must be a valid calculation"sv);
+        return nullptr;
+    }
+
+    return TRY(AbsCalculationNode::create(calculation_node.release_nonnull()));
+}
+
 ErrorOr<RefPtr<StyleValue>> Parser::parse_dynamic_value(ComponentValue const& component_value)
 {
     if (component_value.is_function()) {
@@ -3607,6 +3619,9 @@ ErrorOr<OwnPtr<CalculationNode>> Parser::parse_a_calc_function_node(Function con
 
     if (function.name().equals_ignoring_ascii_case("clamp"sv))
         return TRY(parse_clamp_function(function));
+
+    if (function.name().equals_ignoring_ascii_case("abs"sv))
+        return TRY(parse_abs_function(function));
 
     dbgln_if(CSS_PARSER_DEBUG, "We didn't implement `{}` function yet", function.name());
     return nullptr;
