4dc12bee8560628c1e9c58909ed80696ef6dc7c3
stelar7 <dudedbz@gmail.com>
2023-05-27 19:08:07 +0200

    LibWeb: Implement CSS `rem()`

--- Userland/Libraries/LibWeb/CSS/Parser/Parser.cpp
+++ Userland/Libraries/LibWeb/CSS/Parser/Parser.cpp
@@ -4071,6 +4071,46 @@ ErrorOr<OwnPtr<CalculationNode>> Parser::parse_mod_function(Function const& func
     return TRY(ModCalculationNode::create(node_a.release_nonnull(), node_b.release_nonnull()));
 }
 
+ErrorOr<OwnPtr<CalculationNode>> Parser::parse_rem_function(Function const& function)
+{
+    TokenStream stream { function.values() };
+    auto parameters = parse_a_comma_separated_list_of_component_values(stream);
+
+    if (parameters.size() != 2) {
+        dbgln_if(CSS_PARSER_DEBUG, "rem() must have exactly two parameters"sv);
+        return nullptr;
+    }
+
+    auto node_a = TRY(parse_a_calculation(parameters[0]));
+    auto node_b = TRY(parse_a_calculation(parameters[1]));
+
+    if (!node_a || !node_b) {
+        dbgln_if(CSS_PARSER_DEBUG, "rem() parameters must be valid calculations"sv);
+        return nullptr;
+    }
+
+    auto node_a_maybe_parameter_type = node_a->resolved_type();
+    if (!node_a_maybe_parameter_type.has_value()) {
+        dbgln_if(CSS_PARSER_DEBUG, "Failed to resolve type for rem() parameter 1"sv);
+        return nullptr;
+    }
+
+    auto node_b_maybe_parameter_type = node_b->resolved_type();
+    if (!node_b_maybe_parameter_type.has_value()) {
+        dbgln_if(CSS_PARSER_DEBUG, "Failed to resolve type for rem() parameter 2"sv);
+        return nullptr;
+    }
+
+    auto node_a_resolved_type = node_a_maybe_parameter_type.value();
+    auto node_b_resolved_type = node_b_maybe_parameter_type.value();
+    if (node_a_resolved_type != node_b_resolved_type) {
+        dbgln_if(CSS_PARSER_DEBUG, "rem() parameters must all be of the same type"sv);
+        return nullptr;
+    }
+
+    return TRY(RemCalculationNode::create(node_a.release_nonnull(), node_b.release_nonnull()));
+}
+
 ErrorOr<RefPtr<StyleValue>> Parser::parse_dynamic_value(ComponentValue const& component_value)
 {
     if (component_value.is_function()) {
@@ -4156,6 +4196,9 @@ ErrorOr<OwnPtr<CalculationNode>> Parser::parse_a_calc_function_node(Function con
 
     if (function.name().equals_ignoring_ascii_case("mod"sv))
         return TRY(parse_mod_function(function));
+
+    if (function.name().equals_ignoring_ascii_case("rem"sv))
+        return TRY(parse_rem_function(function));
 
     dbgln_if(CSS_PARSER_DEBUG, "We didn't implement `{}` function yet", function.name());
     return nullptr;
