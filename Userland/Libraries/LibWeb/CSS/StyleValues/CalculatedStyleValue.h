0d30fb5a6eeb6dc89cd6060317970f4ff4224aa6
stelar7 <dudedbz@gmail.com>
2023-05-28 11:31:50 +0200

    LibWeb: Implement CSS `sqrt()`

--- Userland/Libraries/LibWeb/CSS/StyleValues/CalculatedStyleValue.h
+++ Userland/Libraries/LibWeb/CSS/StyleValues/CalculatedStyleValue.h
@@ -550,4 +550,22 @@ private:
     NonnullOwnPtr<CalculationNode> m_y;
 };
 
+class SqrtCalculationNode final : public CalculationNode {
+public:
+    static ErrorOr<NonnullOwnPtr<SqrtCalculationNode>> create(NonnullOwnPtr<CalculationNode>);
+    ~SqrtCalculationNode();
+
+    virtual ErrorOr<String> to_string() const override;
+    virtual Optional<CalculatedStyleValue::ResolvedType> resolved_type() const override;
+    virtual bool contains_percentage() const override { return false; };
+    virtual CalculatedStyleValue::CalculationResult resolve(Optional<Length::ResolutionContext const&>, CalculatedStyleValue::PercentageBasis const&) const override;
+    virtual ErrorOr<void> for_each_child_node(Function<ErrorOr<void>(NonnullOwnPtr<CalculationNode>&)> const&) override;
+
+    virtual ErrorOr<void> dump(StringBuilder&, int indent) const override;
+
+private:
+    SqrtCalculationNode(NonnullOwnPtr<CalculationNode>);
+    NonnullOwnPtr<CalculationNode> m_value;
+};
+
 }
