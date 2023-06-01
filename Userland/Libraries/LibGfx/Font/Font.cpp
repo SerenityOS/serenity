cdac8a8ad3a05afc2faea4158ea04f907413eaa7
Jelle Raaijmakers <jelle@gmta.nl>
2023-06-01 12:25:50 +0200

    LibGfx: Do not use divisions when calculating font subpixel offsets
    No functional or performance changes; they were probably already
    optimized away by the compiler.

--- Userland/Libraries/LibGfx/Font/Font.cpp
+++ Userland/Libraries/LibGfx/Font/Font.cpp
@@ -13,7 +13,7 @@ GlyphRasterPosition GlyphRasterPosition::get_nearest_fit_for(FloatPoint position
     constexpr auto subpixel_divisions = GlyphSubpixelOffset::subpixel_divisions();
     auto fit = [](float pos, int& blit_pos, u8& subpixel_offset) {
         blit_pos = floorf(pos);
-        subpixel_offset = round_to<u8>((pos - blit_pos) / (1.0f / subpixel_divisions));
+        subpixel_offset = round_to<u8>((pos - blit_pos) * subpixel_divisions);
         if (subpixel_offset >= subpixel_divisions) {
             blit_pos += 1;
             subpixel_offset = 0;
