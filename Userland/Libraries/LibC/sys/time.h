7a2cc438b33cdcf051e8e8d180487c89da88c813
Emily Trau <emily@downunderctf.com>
2023-06-05 17:46:36 -0700

    LibC: Add `lutimes(2)` and `futimes(2)` implementations
    Needed for a port of nix

--- Userland/Libraries/LibC/sys/time.h
+++ Userland/Libraries/LibC/sys/time.h
@@ -20,7 +20,9 @@ struct timezone {
 int adjtime(const struct timeval* delta, struct timeval* old_delta);
 int gettimeofday(struct timeval* __restrict__, void* __restrict__);
 int settimeofday(struct timeval* __restrict__, void* __restrict__);
-int utimes(char const* pathname, const struct timeval[2]);
+int utimes(char const* pathname, struct timeval const times[2]);
+int lutimes(char const* pathname, struct timeval const times[2]);
+int futimes(int fd, struct timeval const times[2]);
 
 static inline void timeradd(const struct timeval* a, const struct timeval* b, struct timeval* out)
 {
