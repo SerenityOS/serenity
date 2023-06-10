9080d01beffaf9125fa2c013b3739862b3239170
Andreas Kling <kling@serenityos.org>
2023-06-10 08:31:58 +0200

    Revert "LibJS: Don't reuse GC memory"
    This reverts commit 208f4cd3ce39d2159d92934da56b28a3f67f4bf9.

--- Userland/Libraries/LibJS/Heap/HeapBlock.cpp
+++ Userland/Libraries/LibJS/Heap/HeapBlock.cpp
@@ -39,10 +39,6 @@ HeapBlock::HeapBlock(Heap& heap, size_t cell_size)
     ASAN_POISON_MEMORY_REGION(m_storage, block_size - sizeof(HeapBlock));
 }
 
-struct Tombstone final : public Cell {
-    JS_CELL(Tombstone, Cell);
-};
-
 void HeapBlock::deallocate(Cell* cell)
 {
     VERIFY(is_valid_cell_pointer(cell));
@@ -51,8 +47,20 @@ void HeapBlock::deallocate(Cell* cell)
     VERIFY(!cell->is_marked());
 
     cell->~Cell();
-    auto* tombstone = new (cell) Tombstone;
-    tombstone->set_state(Cell::State::Dead);
+    auto* freelist_entry = new (cell) FreelistEntry();
+    freelist_entry->set_state(Cell::State::Dead);
+    freelist_entry->next = m_freelist;
+    m_freelist = freelist_entry;
+
+#ifdef HAS_ADDRESS_SANITIZER
+    auto dword_after_freelist = round_up_to_power_of_two(reinterpret_cast<uintptr_t>(freelist_entry) + sizeof(FreelistEntry), 8);
+    VERIFY((dword_after_freelist - reinterpret_cast<uintptr_t>(freelist_entry)) <= m_cell_size);
+    VERIFY(m_cell_size >= sizeof(FreelistEntry));
+    // We can't poision the cell tracking data, nor the FreeListEntry's vtable or next pointer
+    // This means there's sizeof(FreelistEntry) data at the front of each cell that is always read/write
+    // On x86_64, this ends up being 24 bytes due to the size of the FreeListEntry's vtable, while on x86, it's only 12 bytes.
+    ASAN_POISON_MEMORY_REGION(reinterpret_cast<void*>(dword_after_freelist), m_cell_size - sizeof(FreelistEntry));
+#endif
 }
 
 }
