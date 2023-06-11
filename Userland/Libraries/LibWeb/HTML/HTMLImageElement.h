5c07aeb78e7c87241c6172dc9405f76c0beb253a
Andreas Kling <kling@serenityos.org>
2023-06-11 08:08:44 +0200

    LibWeb: Update HTML image loading algorithm for recent spec changes
    Changes: https://github.com/whatwg/html/commit/8a2375aac23f88137922c820cf12650364b72715
    
    Note that we don't support the full lazy loading machinery yet, so this
    isn't actually hooked up.

--- Userland/Libraries/LibWeb/HTML/HTMLImageElement.h
+++ Userland/Libraries/LibWeb/HTML/HTMLImageElement.h
@@ -51,7 +51,7 @@ public:
     virtual Optional<ARIA::Role> default_role() const override;
 
     // https://html.spec.whatwg.org/multipage/images.html#update-the-image-data
-    ErrorOr<void> update_the_image_data(bool restart_the_animations = false);
+    ErrorOr<void> update_the_image_data(bool restart_the_animations = false, bool maybe_omit_events = false);
 
     // https://html.spec.whatwg.org/multipage/images.html#use-srcset-or-picture
     [[nodiscard]] bool uses_srcset_or_picture() const;
@@ -93,7 +93,7 @@ private:
 
     virtual JS::GCPtr<Layout::Node> create_layout_node(NonnullRefPtr<CSS::StyleProperties>) override;
 
-    void handle_successful_fetch(AK::URL const&, StringView mime_type, ImageRequest&, ByteBuffer);
+    void handle_successful_fetch(AK::URL const&, StringView mime_type, ImageRequest&, ByteBuffer, bool maybe_omit_events, AK::URL const& previous_url);
     void handle_failed_fetch();
 
     void animate();
