c31af64b18dff1007abfec5de5405a6dfa1f5b17
Timothy Flynn <trflynn89@pm.me>
2023-06-12 13:52:30 -0400

    LibWeb: Implement the AudioTrack and AudioTrackList interfaces
    These are used to own and manage the playing of audio data.

---
+++ Userland/Libraries/LibWeb/HTML/AudioTrack.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2023, Tim Flynn <trflynn89@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/String.h>
+#include <AK/Time.h>
+#include <LibAudio/Forward.h>
+#include <LibWeb/Bindings/PlatformObject.h>
+
+namespace Web::HTML {
+
+class AudioTrack final : public Bindings::PlatformObject {
+    WEB_PLATFORM_OBJECT(AudioTrack, Bindings::PlatformObject);
+
+public:
+    virtual ~AudioTrack() override;
+
+    void set_audio_track_list(Badge<AudioTrackList>, JS::GCPtr<AudioTrackList> audio_track_list) { m_audio_track_list = audio_track_list; }
+
+    void play(Badge<HTMLAudioElement>);
+    void pause(Badge<HTMLAudioElement>);
+
+    Duration position() const;
+    Duration duration() const;
+
+    String const& id() const { return m_id; }
+    String const& kind() const { return m_kind; }
+    String const& label() const { return m_label; }
+    String const& language() const { return m_language; }
+
+    bool enabled() const { return m_enabled; }
+    void set_enabled(bool enabled);
+
+private:
+    AudioTrack(JS::Realm&, JS::NonnullGCPtr<HTMLMediaElement>, NonnullRefPtr<Audio::Loader>);
+
+    virtual JS::ThrowCompletionOr<void> initialize(JS::Realm&) override;
+    virtual void visit_edges(Cell::Visitor&) override;
+
+    Optional<FixedArray<Audio::Sample>> get_next_samples();
+    void play_next_samples();
+
+    // https://html.spec.whatwg.org/multipage/media.html#dom-audiotrack-id
+    String m_id;
+
+    // https://html.spec.whatwg.org/multipage/media.html#dom-audiotrack-kind
+    String m_kind;
+
+    // https://html.spec.whatwg.org/multipage/media.html#dom-audiotrack-label
+    String m_label;
+
+    // https://html.spec.whatwg.org/multipage/media.html#dom-audiotrack-language
+    String m_language;
+
+    // https://html.spec.whatwg.org/multipage/media.html#dom-audiotrack-enabled
+    bool m_enabled { false };
+
+    JS::NonnullGCPtr<HTMLMediaElement> m_media_element;
+    JS::GCPtr<AudioTrackList> m_audio_track_list;
+
+    NonnullOwnPtr<Platform::AudioCodecPlugin> m_audio_plugin;
+    NonnullRefPtr<Audio::Loader> m_loader;
+    NonnullRefPtr<Platform::Timer> m_sample_timer;
+};
+
+}
---
+++ Userland/Libraries/LibWeb/HTML/AudioTrackList.cpp
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2023, Tim Flynn <trflynn89@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibJS/Runtime/Realm.h>
+#include <LibJS/Runtime/VM.h>
+#include <LibWeb/Bindings/AudioTrackListPrototype.h>
+#include <LibWeb/Bindings/Intrinsics.h>
+#include <LibWeb/HTML/AudioTrackList.h>
+#include <LibWeb/HTML/EventNames.h>
+
+namespace Web::HTML {
+
+AudioTrackList::AudioTrackList(JS::Realm& realm)
+    : DOM::EventTarget(realm)
+    , m_audio_tracks(realm.heap())
+{
+}
+
+JS::ThrowCompletionOr<void> AudioTrackList::initialize(JS::Realm& realm)
+{
+    MUST_OR_THROW_OOM(Base::initialize(realm));
+    set_prototype(&Bindings::ensure_web_prototype<Bindings::AudioTrackListPrototype>(realm, "AudioTrackList"));
+
+    return {};
+}
+
+// https://html.spec.whatwg.org/multipage/media.html#dom-tracklist-item
+JS::ThrowCompletionOr<Optional<JS::PropertyDescriptor>> AudioTrackList::internal_get_own_property(JS::PropertyKey const& property_name) const
+{
+    // To determine the value of an indexed property for a given index index in an AudioTrackList or VideoTrackList
+    // object list, the user agent must return the AudioTrack or VideoTrack object that represents the indexth track
+    // in list.
+    if (property_name.is_number()) {
+        if (auto index = property_name.as_number(); index < m_audio_tracks.size()) {
+            JS::PropertyDescriptor descriptor;
+            descriptor.value = m_audio_tracks.at(index);
+
+            return descriptor;
+        }
+    }
+
+    return Base::internal_get_own_property(property_name);
+}
+
+ErrorOr<void> AudioTrackList::add_track(Badge<HTMLMediaElement>, JS::NonnullGCPtr<AudioTrack> audio_track)
+{
+    TRY(m_audio_tracks.try_append(audio_track));
+    audio_track->set_audio_track_list({}, this);
+
+    return {};
+}
+
+void AudioTrackList::remove_all_tracks(Badge<HTMLMediaElement>)
+{
+    m_audio_tracks.clear();
+}
+
+// https://html.spec.whatwg.org/multipage/media.html#dom-audiotracklist-gettrackbyid
+JS::GCPtr<AudioTrack> AudioTrackList::get_track_by_id(StringView id) const
+{
+    // The AudioTrackList getTrackById(id) and VideoTrackList getTrackById(id) methods must return the first AudioTrack
+    // or VideoTrack object (respectively) in the AudioTrackList or VideoTrackList object (respectively) whose identifier
+    // is equal to the value of the id argument (in the natural order of the list, as defined above).
+    auto it = m_audio_tracks.find_if([&](auto const& audio_track) {
+        return audio_track->id() == id;
+    });
+
+    // When no tracks match the given argument, the methods must return null.
+    if (it == m_audio_tracks.end())
+        return nullptr;
+
+    return *it;
+}
+
+bool AudioTrackList::has_enabled_track() const
+{
+    auto it = m_audio_tracks.find_if([&](auto const& audio_track) {
+        return audio_track->enabled();
+    });
+
+    return it != m_audio_tracks.end();
+}
+
+// https://html.spec.whatwg.org/multipage/media.html#handler-tracklist-onchange
+void AudioTrackList::set_onchange(WebIDL::CallbackType* event_handler)
+{
+    set_event_handler_attribute(HTML::EventNames::change, event_handler);
+}
+
+// https://html.spec.whatwg.org/multipage/media.html#handler-tracklist-onchange
+WebIDL::CallbackType* AudioTrackList::onchange()
+{
+    return event_handler_attribute(HTML::EventNames::change);
+}
+
+// https://html.spec.whatwg.org/multipage/media.html#handler-tracklist-onaddtrack
+void AudioTrackList::set_onaddtrack(WebIDL::CallbackType* event_handler)
+{
+    set_event_handler_attribute(HTML::EventNames::addtrack, event_handler);
+}
+
+// https://html.spec.whatwg.org/multipage/media.html#handler-tracklist-onaddtrack
+WebIDL::CallbackType* AudioTrackList::onaddtrack()
+{
+    return event_handler_attribute(HTML::EventNames::addtrack);
+}
+
+// https://html.spec.whatwg.org/multipage/media.html#handler-tracklist-onremovetrack
+void AudioTrackList::set_onremovetrack(WebIDL::CallbackType* event_handler)
+{
+    set_event_handler_attribute(HTML::EventNames::removetrack, event_handler);
+}
+
+// https://html.spec.whatwg.org/multipage/media.html#handler-tracklist-onremovetrack
+WebIDL::CallbackType* AudioTrackList::onremovetrack()
+{
+    return event_handler_attribute(HTML::EventNames::removetrack);
+}
+
+}
