5b45f672e21e35ad6224cc03f56d8dc9eb689c5e
Andreas Kling <kling@serenityos.org>
2023-06-19 15:19:25 +0200

    LibWeb: Allow IFC to size inline-flex boxes midway through flex layout
    The part in FFC where we ask the parent formatting context to size the
    flex container midway through layout is really weird, but let's at least
    be consistently weird for BFC and IFC. Since IFC always works within its
    parent BFC, it can simply forward these requests to the BFC.
    
    This fixes an issue where inline-flex containers incorrectly had main
    axis margins subtracted from their content size.

--- Userland/Libraries/LibWeb/Layout/InlineFormattingContext.h
+++ Userland/Libraries/LibWeb/Layout/InlineFormattingContext.h
@@ -34,6 +34,10 @@ public:
     bool any_floats_intrude_at_y(CSSPixels y) const;
     bool can_fit_new_line_at_y(CSSPixels y) const;
 
+    virtual bool can_determine_size_of_child() const override;
+    virtual void determine_width_of_child(Box const&, AvailableSpace const&) override;
+    virtual void determine_height_of_child(Box const&, AvailableSpace const&) override;
+
 private:
     void generate_line_boxes(LayoutMode);
     void apply_justification_to_fragments(CSS::TextJustify, LineBox&, bool is_last_line);
