SerenityOS/serenity

commit 1d76126abe9dd65251bd28a381350f70083b50f7

LibWeb: Don't use weird Length::resolved() for padding in BFC

Userland/Libraries/LibWeb/Layout/BlockFormattingContext.cpp
@@ -154,14 +154,14 @@ void BlockFormattingContext::compute_width(Box const& box, AvailableSpace const&
 
     auto margin_left = CSS::Length::make_auto();
     auto margin_right = CSS::Length::make_auto();
-    auto const padding_left = computed_values.padding().left().resolved(box, width_of_containing_block_as_length_for_resolve).resolved(box);
-    auto const padding_right = computed_values.padding().right().resolved(box, width_of_containing_block_as_length_for_resolve).resolved(box);
+    auto const padding_left = computed_values.padding().left().to_px(box, width_of_containing_block);
+    auto const padding_right = computed_values.padding().right().to_px(box, width_of_containing_block);
 
     auto& box_state = m_state.get_mutable(box);
     box_state.border_left = computed_values.border_left().width;
     box_state.border_right = computed_values.border_right().width;
-    box_state.padding_left = padding_left.to_px(box);
-    box_state.padding_right = padding_right.to_px(box);
+    box_state.padding_left = padding_left;
+    box_state.padding_right = padding_right;
 
     // NOTE: If we are calculating the min-content or max-content width of this box,
     //       and the width should be treated as auto, then we can simply return here,
@@ -174,7 +174,7 @@ void BlockFormattingContext::compute_width(Box const& box, AvailableSpace const&
         margin_left = computed_values.margin().left().resolved(box, width_of_containing_block_as_length_for_resolve).resolved(box);
         margin_right = computed_values.margin().right().resolved(box, width_of_containing_block_as_length_for_resolve).resolved(box);
         CSSPixels total_px = computed_values.border_left().width + computed_values.border_right().width;
-        for (auto& value : { margin_left, padding_left, width, padding_right, margin_right }) {
+        for (auto& value : { margin_left, CSS::Length::make_px(padding_left), width, CSS::Length::make_px(padding_right), margin_right }) {
             total_px += value.to_px(box);
         }
 
@@ -281,8 +281,8 @@ void BlockFormattingContext::compute_width_for_floating_box(Box const& box, Avai
 
     auto margin_left = computed_values.margin().left().resolved(box, width_of_containing_block_as_length_for_resolve).resolved(box);
     auto margin_right = computed_values.margin().right().resolved(box, width_of_containing_block_as_length_for_resolve).resolved(box);
-    auto const padding_left = computed_values.padding().left().resolved(box, width_of_containing_block_as_length_for_resolve).resolved(box);
-    auto const padding_right = computed_values.padding().right().resolved(box, width_of_containing_block_as_length_for_resolve).resolved(box);
+    auto const padding_left = computed_values.padding().left().to_px(box, width_of_containing_block);
+    auto const padding_right = computed_values.padding().right().to_px(box, width_of_containing_block);
 
     // If 'margin-left', or 'margin-right' are computed as 'auto', their used value is '0'.
     if (margin_left.is_auto())
@@ -298,8 +298,8 @@ void BlockFormattingContext::compute_width_for_floating_box(Box const& box, Avai
             // block minus the used values of 'margin-left', 'border-left-width', 'padding-left',
             // 'padding-right', 'border-right-width', 'margin-right', and the widths of any relevant scroll bars.
             auto available_width = width_of_containing_block
-                - margin_left.to_px(box) - computed_values.border_left().width - padding_left.to_px(box)
-                - padding_right.to_px(box) - computed_values.border_right().width - margin_right.to_px(box);
+                - margin_left.to_px(box) - computed_values.border_left().width - padding_left
+                - padding_right - computed_values.border_right().width - margin_right.to_px(box);
 
             auto result = calculate_shrink_to_fit_widths(box);
 
@@ -341,8 +341,8 @@ void BlockFormattingContext::compute_width_for_floating_box(Box const& box, Avai
     box_state.margin_right = margin_right.to_px(box);
     box_state.border_left = computed_values.border_left().width;
     box_state.border_right = computed_values.border_right().width;
-    box_state.padding_left = padding_left.to_px(box);
-    box_state.padding_right = padding_right.to_px(box);
+    box_state.padding_left = padding_left;
+    box_state.padding_right = padding_right;
 }
 
 void BlockFormattingContext::compute_width_for_block_level_replaced_element_in_normal_flow(ReplacedBox const& box, AvailableSpace const& available_space)

commit ca491823f34b169bcbbb01f9df52e27f405dd5f6

LibWeb: Resolve padding against 0 and not inf for indefinite width more
In particular, in BFC:
- Non-floating, non-replaced elements
- Floating, non-replaced elements
- Floating, replaced elements

The first two regressed in https://github.com/SerenityOS/serenity/commit/1d76126abe9dd65251bd28a381350f70083b50f7

The third one seems to have been introduced by this regression, as it
was seemingly copied from compute_width_for_floating_box in
https://github.com/SerenityOS/serenity/commit/7f9ede07bcf287a2b16ff29c1ebbcf64c47a221f

Userland/Libraries/LibWeb/Layout/BlockFormattingContext.cpp
@@ -164,8 +164,8 @@ void BlockFormattingContext::compute_width(Box const& box, AvailableSpace const&
 
     auto margin_left = CSS::Length::make_auto();
     auto margin_right = CSS::Length::make_auto();
-    auto const padding_left = computed_values.padding().left().to_px(box, width_of_containing_block);
-    auto const padding_right = computed_values.padding().right().to_px(box, width_of_containing_block);
+    auto const padding_left = computed_values.padding().left().resolved(box, width_of_containing_block_as_length_for_resolve).to_px(box);
+    auto const padding_right = computed_values.padding().right().resolved(box, width_of_containing_block_as_length_for_resolve).to_px(box);
 
     auto& box_state = m_state.get_mutable(box);
     box_state.border_left = computed_values.border_left().width;
@@ -291,8 +291,8 @@ void BlockFormattingContext::compute_width_for_floating_box(Box const& box, Avai
 
     auto margin_left = computed_values.margin().left().resolved(box, width_of_containing_block_as_length_for_resolve);
     auto margin_right = computed_values.margin().right().resolved(box, width_of_containing_block_as_length_for_resolve);
-    auto const padding_left = computed_values.padding().left().to_px(box, width_of_containing_block);
-    auto const padding_right = computed_values.padding().right().to_px(box, width_of_containing_block);
+    auto const padding_left = computed_values.padding().left().resolved(box, width_of_containing_block_as_length_for_resolve).to_px(box);
+    auto const padding_right = computed_values.padding().right().resolved(box, width_of_containing_block_as_length_for_resolve).to_px(box);
 
     // If 'margin-left', or 'margin-right' are computed as 'auto', their used value is '0'.
     if (margin_left.is_auto())
@@ -371,8 +371,8 @@ void BlockFormattingContext::compute_width_for_block_level_replaced_element_in_n
     // non-replaced block-level elements are applied to determine the margins.
     auto margin_left = computed_values.margin().left().resolved(box, width_of_containing_block_as_length_for_resolve);
     auto margin_right = computed_values.margin().right().resolved(box, width_of_containing_block_as_length_for_resolve);
-    auto const padding_left = computed_values.padding().left().to_px(box, width_of_containing_block);
-    auto const padding_right = computed_values.padding().right().to_px(box, width_of_containing_block);
+    auto const padding_left = computed_values.padding().left().resolved(box, width_of_containing_block_as_length_for_resolve).to_px(box);
+    auto const padding_right = computed_values.padding().right().resolved(box, width_of_containing_block_as_length_for_resolve).to_px(box);
 
     // If 'margin-left', or 'margin-right' are computed as 'auto', their used value is '0'.
     if (margin_left.is_auto())

Tests/LibWeb/Layout/input/floating-non-replaced-element-percentage-padding-against-indefinite-width.html
@@ -1,0 +1,11 @@
+<!DOCTYPE html><html><head><style>
+#box1 {
+  display: flex;
+}
+
+#box2 {
+  display: flex;
+  padding: 100%;
+  float: left;
+}
+</style></head><body><div id="box1"><div><div id="box2">

Tests/LibWeb/Layout/input/floating-replaced-element-percentage-padding-against-indefinite-width.html
@@ -1,0 +1,11 @@
+<!DOCTYPE html><html><head><style>
+#box1 {
+  display: flex;
+}
+
+#box2 {
+  display: flex;
+  padding: 100%;
+  float: left;
+}
+</style></head><body><div id="box1"><div><img id="box2"/>

Tests/LibWeb/Layout/input/non-floating-non-replaced-element-percentage-padding-against-indefinite-width.html
@@ -1,0 +1,10 @@
+<!DOCTYPE html><html><head><style>
+#box1 {
+  display: flex;
+}
+
+#box2 {
+  display: flex;
+  padding: 100%;
+}
+</style></head><body><div id="box1"><div><div id="box2">