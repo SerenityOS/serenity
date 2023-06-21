3277ef05f5c66a9415ba2d822a11a82634d3881f
Andreas Kling <kling@serenityos.org>
2023-06-21 13:53:09 +0200

    LibWeb: Add HTMLDocument class
    This class is currently not in the spec, but it *is* still in all the
    major browser engines. For compatibility reasons, let's do what other
    engines do.
    
    There is discussion about bringing HTMLDocument back into specs:
    - https://github.com/whatwg/html/issues/4792
    - https://github.com/whatwg/dom/issues/221

---
+++ Userland/Libraries/LibWeb/HTML/HTMLDocument.cpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2023, Andreas Kling <kling@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibWeb/HTML/HTMLDocument.h>
+
+namespace Web::HTML {
+
+HTMLDocument::HTMLDocument(JS::Realm& realm, AK::URL const& url)
+    : Document(realm, url)
+{
+}
+
+HTMLDocument::~HTMLDocument() = default;
+
+WebIDL::ExceptionOr<JS::NonnullGCPtr<HTMLDocument>> HTMLDocument::construct_impl(JS::Realm& realm)
+{
+    return HTMLDocument::create(realm);
+}
+
+WebIDL::ExceptionOr<JS::NonnullGCPtr<HTMLDocument>> HTMLDocument::create(JS::Realm& realm, AK::URL const& url)
+{
+    return MUST_OR_THROW_OOM(realm.heap().allocate<HTMLDocument>(realm, realm, url));
+}
+
+}
---
+++ Userland/Libraries/LibWeb/HTML/HTMLDocument.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2023, Andreas Kling <kling@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibWeb/DOM/Document.h>
+
+namespace Web::HTML {
+
+// NOTE: This class is not currently in the specifications but it *is* implemented by all major browsers.
+//       There is discussion about bringing it back:
+//       https://github.com/whatwg/html/issues/4792
+//       https://github.com/whatwg/dom/issues/221
+class HTMLDocument final : public DOM::Document {
+    JS_CELL(HTMLDocument, DOM::Document);
+
+public:
+    virtual ~HTMLDocument() override;
+
+    static WebIDL::ExceptionOr<JS::NonnullGCPtr<HTMLDocument>> create(JS::Realm&, AK::URL const& url = "about:blank"sv);
+    WebIDL::ExceptionOr<JS::NonnullGCPtr<HTMLDocument>> construct_impl(JS::Realm&);
+
+private:
+    HTMLDocument(JS::Realm&, AK::URL const&);
+};
+
+}
