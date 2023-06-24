152c72a236dc9ebb2c5a095603fefaf6f6ef6bf9
René Hickersberger <r@renehsz.com>
2023-06-24 11:59:20 +0200

    Ports: Add libfts port
    This ports an implementation of the FTS functions that can be used to
    traverse the file system. They are non-standard, but provided by glibc
    and most BSD systems. This ported library implements FTS for musl-based
    Linux systems and happens to work on Serenity.

---
+++ Ports/libfts/package.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port='libfts'
+version='1.2.7'
+files=(
+    "https://github.com/void-linux/musl-fts/archive/refs/tags/v${version}.tar.gz 49ae567a96dbab22823d045ffebe0d6b14b9b799925e9ca9274d47d26ff482a6"
+)
+workdir="musl-fts-${version}"
+useconfigure='true'
+
+pre_configure() {
+    pushd $workdir
+    ./bootstrap.sh
+    popd
+}
