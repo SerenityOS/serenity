SerenityOS/serenity

commit 1dc3ba6ed5bfbb0055d9335a32f580a73f3ab254

Applications: Convert `DeprecatedFile` usages to `LibFileSystem`

Userland/Applications/FileManager/DirectoryView.cpp
@@ -11,7 +11,6 @@
 #include <AK/NumberFormat.h>
 #include <AK/StringBuilder.h>
 #include <LibConfig/Client.h>
-#include <LibCore/DeprecatedFile.h>
 #include <LibCore/MimeData.h>
 #include <LibCore/StandardPaths.h>
 #include <LibFileSystem/FileSystem.h>
@@ -406,18 +405,21 @@ void DirectoryView::add_path_to_history(DeprecatedString path)
 
 bool DirectoryView::open(DeprecatedString const& path)
 {
-    auto real_path = Core::DeprecatedFile::real_path_for(path);
-    if (real_path.is_null() || !FileSystem::is_directory(path))
+    auto error_or_real_path = FileSystem::real_path(path);
+    if (error_or_real_path.is_error() || !FileSystem::is_directory(path))
         return false;
 
-    if (chdir(real_path.characters()) < 0) {
+    auto real_path = error_or_real_path.release_value();
+    if (Core::System::chdir(real_path).is_error()) {
         perror("chdir");
+        return false;
     }
-    if (model().root_path() == real_path) {
+
+    if (model().root_path() == real_path.to_deprecated_string()) {
         refresh();
     } else {
         set_active_widget(&current_view());
-        model().set_root_path(real_path);
+        model().set_root_path(real_path.to_deprecated_string());
     }
     return true;
 }

Userland/Applications/3DFileViewer/WavefrontOBJLoader.cpp
@@ -9,7 +9,6 @@
 #include "WavefrontOBJLoader.h"
 #include <AK/FixedArray.h>
 #include <AK/String.h>
-#include <LibCore/DeprecatedFile.h>
 #include <LibCore/File.h>
 #include <stdlib.h>
 

Userland/Applications/Browser/main.cpp
@@ -14,7 +14,6 @@
 #include <Applications/Browser/WindowActions.h>
 #include <LibConfig/Client.h>
 #include <LibCore/ArgsParser.h>
-#include <LibCore/DeprecatedFile.h>
 #include <LibCore/FileWatcher.h>
 #include <LibCore/StandardPaths.h>
 #include <LibCore/System.h>
@@ -131,16 +130,16 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
         }
     }
 
-    auto url_from_argument_string = [](DeprecatedString const& string) -> URL {
+    auto url_from_argument_string = [](DeprecatedString const& string) -> ErrorOr<URL> {
         if (FileSystem::exists(string)) {
-            return URL::create_with_file_scheme(Core::DeprecatedFile::real_path_for(string));
+            return URL::create_with_file_scheme(TRY(FileSystem::real_path(string)).to_deprecated_string());
         }
         return Browser::url_from_user_input(string);
     };
 
     URL first_url = Browser::url_from_user_input(Browser::g_home_url);
     if (!specified_urls.is_empty())
-        first_url = url_from_argument_string(specified_urls.first());
+        first_url = TRY(url_from_argument_string(specified_urls.first()));
 
     auto cookie_jar = TRY(Browser::CookieJar::create(*database));
     auto window = Browser::BrowserWindow::construct(cookie_jar, first_url);
@@ -176,7 +175,7 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
     };
 
     for (size_t i = 1; i < specified_urls.size(); ++i)
-        window->create_new_tab(url_from_argument_string(specified_urls[i]), Web::HTML::ActivateTab::No);
+        window->create_new_tab(TRY(url_from_argument_string(specified_urls[i])), Web::HTML::ActivateTab::No);
 
     window->show();
 

Userland/Applications/Escalator/CMakeLists.txt
@@ -16,4 +16,4 @@ set(GENERATED_SOURCES
 )
 
 serenity_app(Escalator ICON app-escalator)
-target_link_libraries(Escalator PRIVATE LibCore LibDesktop LibGfx LibGUI LibMain)
+target_link_libraries(Escalator PRIVATE LibCore LibFileSystem LibDesktop LibGfx LibGUI LibMain)

Userland/Applications/Escalator/main.cpp
@@ -9,8 +9,8 @@
 #include <AK/DeprecatedString.h>
 #include <LibCore/Account.h>
 #include <LibCore/ArgsParser.h>
-#include <LibCore/DeprecatedFile.h>
 #include <LibCore/System.h>
+#include <LibFileSystem/FileSystem.h>
 #include <LibGUI/Application.h>
 #include <LibGUI/Desktop.h>
 #include <LibGUI/MessageBox.h>
@@ -33,8 +33,8 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
 
     auto app = TRY(GUI::Application::try_create(arguments));
 
-    auto executable_path = Core::DeprecatedFile::resolve_executable_from_environment(command[0]);
-    if (!executable_path.has_value()) {
+    auto executable_path = FileSystem::resolve_executable_from_environment(command[0]);
+    if (executable_path.is_error()) {
         GUI::MessageBox::show_error(nullptr, DeprecatedString::formatted("Could not execute command {}: Command not found.", command[0]));
         return 127;
     }

Userland/Applications/FileManager/PropertiesWindow.cpp
@@ -10,7 +10,6 @@
 #include <AK/NumberFormat.h>
 #include <Applications/FileManager/DirectoryView.h>
 #include <Applications/FileManager/PropertiesWindowGeneralTabGML.h>
-#include <LibCore/DeprecatedFile.h>
 #include <LibCore/Directory.h>
 #include <LibCore/System.h>
 #include <LibDesktop/Launcher.h>
@@ -102,11 +101,11 @@ ErrorOr<void> PropertiesWindow::create_widgets(bool disable_rename)
     type->set_text(get_description(m_mode));
 
     if (S_ISLNK(m_mode)) {
-        auto link_destination_or_error = Core::DeprecatedFile::read_link(m_path);
+        auto link_destination_or_error = FileSystem::read_link(m_path);
         if (link_destination_or_error.is_error()) {
             perror("readlink");
         } else {
-            auto link_destination = link_destination_or_error.release_value();
+            auto link_destination = link_destination_or_error.release_value().to_deprecated_string();
             auto* link_location = general_tab->find_descendant_of_type_named<GUI::LinkLabel>("link_location");
             link_location->set_text(link_destination);
             link_location->on_click = [link_destination] {

Userland/Applications/FileManager/main.cpp
@@ -19,7 +19,6 @@
 #include <LibConfig/Client.h>
 #include <LibConfig/Listener.h>
 #include <LibCore/ArgsParser.h>
-#include <LibCore/DeprecatedFile.h>
 #include <LibCore/Process.h>
 #include <LibCore/StandardPaths.h>
 #include <LibCore/System.h>
@@ -109,8 +108,8 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
 
     LexicalPath path(initial_location);
     if (!initial_location.is_empty()) {
-        if (!ignore_path_resolution)
-            initial_location = Core::DeprecatedFile::real_path_for(initial_location);
+        if (auto error_or_path = FileSystem::real_path(initial_location); !ignore_path_resolution && !error_or_path.is_error())
+            initial_location = error_or_path.release_value().to_deprecated_string();
 
         if (!FileSystem::is_directory(initial_location)) {
             // We want to extract zips to a temporary directory when FileManager is launched with a .zip file as its first argument
@@ -142,8 +141,8 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
         }
     }
 
-    if (initial_location.is_empty())
-        initial_location = Core::DeprecatedFile::current_working_directory();
+    if (auto error_or_cwd = FileSystem::current_working_directory(); initial_location.is_empty() && !error_or_cwd.is_error())
+        initial_location = error_or_cwd.release_value().to_deprecated_string();
 
     if (initial_location.is_empty())
         initial_location = Core::StandardPaths::home_directory();

Userland/Applications/ImageViewer/ViewWidget.cpp
@@ -12,7 +12,6 @@
 #include "ViewWidget.h"
 #include <AK/LexicalPath.h>
 #include <AK/StringBuilder.h>
-#include <LibCore/DeprecatedFile.h>
 #include <LibCore/Directory.h>
 #include <LibCore/MappedFile.h>
 #include <LibCore/MimeData.h>

commit 08e7ab4a9a377126b19e6365dd43dece2bcded35

FileManager: Restore inline message behavior for inaccessible dirs
Print the correct error from Core::System::chdir() instead of errno, and
display the error in the DirectoryView instead of continuing to show the
previous location's contents.

This regressed in 1dc3ba6ed5bfbb0055d9335a32f580a73f3ab254.

Userland/Applications/FileManager/DirectoryView.cpp
@@ -408,9 +408,9 @@ bool DirectoryView::open(DeprecatedString const& path)
         return false;
 
     auto real_path = error_or_real_path.release_value();
-    if (Core::System::chdir(real_path).is_error()) {
-        perror("chdir");
-        return false;
+    if (auto result = Core::System::chdir(real_path); result.is_error()) {
+        dbgln("Failed to open '{}': {}", real_path, result.error());
+        warnln("Failed to open '{}': {}", real_path, result.error());
     }
 
     if (model().root_path() == real_path.to_deprecated_string()) {