c3341e0cd5a901fa81fc565bed4cc23ce7071b11
Timothy Flynn <trflynn89@pm.me>
2023-06-24 17:01:57 -0400

    LibJS: Implement Iterator.prototype [ @@toStringTag ]

--- Userland/Libraries/LibJS/Runtime/IteratorPrototype.cpp
+++ Userland/Libraries/LibJS/Runtime/IteratorPrototype.cpp
@@ -20,6 +20,10 @@ ThrowCompletionOr<void> IteratorPrototype::initialize(Realm& realm)
 {
     auto& vm = this->vm();
     MUST_OR_THROW_OOM(Base::initialize(realm));
+
+    // 3.1.3.13 Iterator.prototype [ @@toStringTag ], https://tc39.es/proposal-iterator-helpers/#sec-iteratorprototype-@@tostringtag
+    define_direct_property(vm.well_known_symbol_to_string_tag(), MUST_OR_THROW_OOM(PrimitiveString::create(vm, "Iterator"sv)), Attribute::Configurable | Attribute::Writable);
+
     u8 attr = Attribute::Writable | Attribute::Configurable;
     define_native_function(realm, vm.well_known_symbol_iterator(), symbol_iterator, 0, attr);
 
