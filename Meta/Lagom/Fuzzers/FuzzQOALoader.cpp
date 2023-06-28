8df714ff1ef196a05e3952d4788afbd4e8fa5752
kleines Filmröllchen <filmroellchen@serenityos.org>
2023-06-28 15:47:24 +0200

    Meta/Fuzzers: Extract common audio fuzzing code
    Apart from the class used audio fuzzers have identical behavior: Create
    a memory stream from the fuzzer input and pass this to the loader, then
    try to load audio until an error occurs. Since the loader plugins need
    to have the same static create() function anyways for LibAudio itself,
    we can unify the fuzzer implementations and reduce code duplication.

--- Meta/Lagom/Fuzzers/FuzzQOALoader.cpp
+++ Meta/Lagom/Fuzzers/FuzzQOALoader.cpp
@@ -4,29 +4,10 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#include <AK/MemoryStream.h>
+#include "AudioFuzzerCommon.h"
 #include <LibAudio/QOALoader.h>
-#include <stddef.h>
-#include <stdint.h>
 
 extern "C" int LLVMFuzzerTestOneInput(uint8_t const* data, size_t size)
 {
-    auto const qoa_bytes = ByteBuffer::copy(data, size).release_value();
-    auto qoa_data = try_make<FixedMemoryStream>(qoa_bytes).release_value();
-    auto qoa_or_error = Audio::QOALoaderPlugin::create(move(qoa_data));
-
-    if (qoa_or_error.is_error())
-        return 0;
-
-    auto qoa = qoa_or_error.release_value();
-
-    for (;;) {
-        auto samples = qoa->load_chunks(5 * KiB);
-        if (samples.is_error())
-            return 0;
-        if (samples.value().size() == 0)
-            break;
-    }
-
-    return 0;
+    return fuzz_audio_loader<Audio::QOALoaderPlugin>(data, size);
 }
--- Meta/Lagom/Fuzzers/FuzzWAVLoader.cpp
+++ Meta/Lagom/Fuzzers/FuzzWAVLoader.cpp
@@ -1,32 +1,13 @@
 /*
- * Copyright (c) 2021, the SerenityOS developers.
+ * Copyright (c) 2023, kleines Filmröllchen <filmroellchen@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#include <AK/MemoryStream.h>
+#include "AudioFuzzerCommon.h"
 #include <LibAudio/WavLoader.h>
-#include <stddef.h>
-#include <stdint.h>
 
 extern "C" int LLVMFuzzerTestOneInput(uint8_t const* data, size_t size)
 {
-    auto const wav_bytes = ByteBuffer::copy(data, size).release_value();
-    auto wav_data = try_make<FixedMemoryStream>(wav_bytes).release_value();
-    auto wav_or_error = Audio::WavLoaderPlugin::create(move(wav_data));
-
-    if (wav_or_error.is_error())
-        return 0;
-
-    auto wav = wav_or_error.release_value();
-
-    for (;;) {
-        auto samples = wav->load_chunks(4 * KiB);
-        if (samples.is_error())
-            return 0;
-        if (samples.value().size() == 0)
-            break;
-    }
-
-    return 0;
+    return fuzz_audio_loader<Audio::WavLoaderPlugin>(data, size);
 }
