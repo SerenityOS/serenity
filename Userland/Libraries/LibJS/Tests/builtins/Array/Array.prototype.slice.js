be280ff3e4ba3116ccd1a908648fc8c25bb35eb3
Shannon Booth <shannon.ml.booth@gmail.com>
2023-06-28 20:06:53 +1200

    LibJS/Tests: Enable some now passing Array length tests
    Now that serenity is 64 bit only these tests no longer need to be
    skipped :^)

--- Userland/Libraries/LibJS/Tests/builtins/Array/Array.prototype.slice.js
+++ Userland/Libraries/LibJS/Tests/builtins/Array/Array.prototype.slice.js
@@ -38,10 +38,7 @@ test("basic functionality", () => {
     expect(slice).toEqual(["hello", "friends", "serenity", 1]);
 });
 
-// FIXME: These tests are currently skipped because an invalid array length in this case is 2**32 or above.
-//        The codebase currently uses size_t for lengths, which is currently the same as u32 when building for Serenity.
-//        This means these lengths wrap around to 0, making the test not work correctly.
-test.skip("Invalid lengths", () => {
+test("Invalid lengths", () => {
     var length = Math.pow(2, 32);
 
     var obj = {
