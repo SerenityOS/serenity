bfd6deed1e860ce9516c7ca1613150a2c2c75f75
Andrew Kaster <akaster@serenityos.org>
2023-06-29 14:35:03 -0600

    AK+Meta: Disable consteval completely when building for oss-fuzz
    This was missed in 02b74e5a70e1eef2fe3123fe8aa157301424783a
    
    We need to disable consteval in AK::String as well as AK::StringView,
    and we need to disable it when building both the tools build and the
    fuzzer build.

--- AK/String.h
+++ AK/String.h
@@ -30,7 +30,8 @@ class StringData;
 }
 
 // FIXME: Remove this when OpenBSD Clang fully supports consteval.
-#if defined(AK_OS_OPENBSD)
+//        And once oss-fuzz updates to clang >15.
+#if defined(AK_OS_OPENBSD) || defined(OSS_FUZZ)
 #    define AK_SHORT_STRING_CONSTEVAL constexpr
 #else
 #    define AK_SHORT_STRING_CONSTEVAL consteval
