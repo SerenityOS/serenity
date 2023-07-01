c0a5787395e1a55d905f8022286e59955b7f1d56
Andreas Kling <kling@serenityos.org>
2023-07-01 08:12:42 +0200

    Ladybird: Don't scroll web view if wheel event is a Ctrl+Wheel zoom

--- Ladybird/WebContentView.h
+++ Ladybird/WebContentView.h
@@ -46,6 +46,7 @@ public:
 
     virtual void paintEvent(QPaintEvent*) override;
     virtual void resizeEvent(QResizeEvent*) override;
+    virtual void wheelEvent(QWheelEvent*) override;
     virtual void mouseMoveEvent(QMouseEvent*) override;
     virtual void mousePressEvent(QMouseEvent*) override;
     virtual void mouseReleaseEvent(QMouseEvent*) override;
