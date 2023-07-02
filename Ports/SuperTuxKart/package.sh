9b10b37546813cdf8cbf083208258ca4a5b81c37
Dominika Liberda <ja@sdomi.pl>
2023-07-02 07:30:25 +0200

    Ports: Add a Super Tux Kart port
    Co-Authored-By: Linus Groh <mail@linusgroh.de>
    Co-Authored-By: Panagiotis "Ivory" Vasilopoulos <git@n0toose.net>
    Co-Authored-By: lilylunatic <lily@acab.dev>
    Co-Authored-By: io_err <quint@guvernator.net>

---
+++ Ports/SuperTuxKart/package.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port=SuperTuxKart
+useconfigure='true'
+version='1.4'
+archive_hash='9890392419baf4715313f14d5ad60746f276eed36eb580636caf44e2532c0f03'
+files="https://github.com/supertuxkart/stk-code/releases/download/${version}/supertuxkart-${version}-src.tar.xz ${port}-${version}-src.tar.xz $archive_hash"
+auth_type='sha256'
+workdir="${port}-${version}-src"
+launcher_name='SuperTuxKart'
+launcher_category='Games'
+launcher_command='/usr/local/bin/supertuxkart'
+depends=(
+    'curl'
+    'freetype'
+    'glu'
+    'harfbuzz'
+    'libjpeg'
+    'libopenal'
+    'libogg'
+    'libpng'
+    'libvorbis'
+    'openssl'
+    'SDL2'
+    'sqlite'
+    'zlib'
+)
+
+configopts=(
+    '-DCMAKE_BUILD_TYPE=STKRelease'
+    "-DCMAKE_INSTALL_PREFIX=${SERENITY_INSTALL_ROOT}/opt/SuperTuxKart"
+    "-DSTK_INSTALL_DATA_DIR=${SERENITY_INSTALL_ROOT}/opt/SuperTuxKart"
+    '-DSUPERTUXKART_DATADIR=/opt/SuperTuxKart'
+    "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
+    '-DBUILD_RECORDER=OFF'
+    '-DBUILD_SHARED_LIBS=OFF'
+    '-DUSE_WIIUSE=OFF'
+    '-DUSE_DNS_C=ON'
+    '-Bbuild'
+    '-GNinja'
+    "-DCMAKE_C_FLAGS=-I${SERENITY_INSTALL_ROOT}/usr/include/LibGL -I${SERENITY_INSTALL_ROOT}/usr/local/include/SDL2 -I${SERENITY_INSTALL_ROOT}/usr/local/include -lcurl -lharfbuzz -lfreetype"
+    "-DCMAKE_CXX_FLAGS=-I${SERENITY_INSTALL_ROOT}/usr/include/LibGL -I${SERENITY_INSTALL_ROOT}/usr/local/include/SDL2 -I${SERENITY_INSTALL_ROOT}/usr/local/include -lcurl -lharfbuzz -lfreetype"
+    '-DUSE_IPV6=OFF'
+    '-DUSE_SYSTEM_ENET=OFF'
+)
+
+configure() {
+    run cmake "${configopts[@]}" .
+}
+
+build() {
+    run ninja -C build
+}
+
+install() {
+    run ninja -C build install
+}
---
+++ Ports/SuperTuxKart/patches/ReadMe.md
@@ -0,0 +1,27 @@
+# Patches for SuperTuxKart on SerenityOS
+
+## `0001-Add-build-system-support-for-Serenity.patch`
+
+Add build system support for Serenity
+
+
+## `0002-Remove-reserved-word-from-goto-in-mcpp.patch`
+
+Remove reserved word from goto in mcpp
+
+
+## `0003-Fixup-Vulkan-SDL-platform-quirks.patch`
+
+Fixup Vulkan/SDL platform quirks
+
+
+## `0004-Remove-IPv6-support.patch`
+
+Remove IPv6 support
+
+
+## `0005-Correct-the-resource-install-path.patch`
+
+Correct the resource install path
+
+
