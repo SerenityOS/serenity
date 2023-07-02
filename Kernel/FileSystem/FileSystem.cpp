251b17085b4390c00a979f7abd1cb5c0f7cc18d8
kleines Filmröllchen <filmroellchen@serenityos.org>
2023-07-02 14:25:16 +0200

    Kernel/Ext2: Check and set file system state
    This is supposed to detect whether a file system was unmounted
    cleanly or not.

--- Kernel/FileSystem/FileSystem.cpp
+++ Kernel/FileSystem/FileSystem.cpp
@@ -30,6 +30,7 @@ FileSystem::~FileSystem()
 ErrorOr<void> FileSystem::prepare_to_unmount(Inode& mount_guest_inode)
 {
     return m_attach_count.with([&](auto& attach_count) -> ErrorOr<void> {
+        dbgln_if(VFS_DEBUG, "VFS: File system {} (id {}) is attached {} time(s)", class_name(), m_fsid.value(), attach_count);
         if (attach_count == 1)
             return prepare_to_clear_last_mount(mount_guest_inode);
         return {};
