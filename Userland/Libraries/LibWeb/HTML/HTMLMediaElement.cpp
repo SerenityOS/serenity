74f4eb616f737c3d016a7bf487ea17f6a1180e6f
Timothy Flynn <trflynn89@pm.me>
2023-07-02 21:35:48 -0700

    LibWeb: Update the media element's display in a couple situations
    Mostly seen on macOS, but when we toggle playing a media element, we
    need to update its layout node's display to ensure the change is
    reflected on the playback button. Further, when setting the element's
    display time, we need to update the display to ensure the change is
    refelected on the media timeline.

--- Userland/Libraries/LibWeb/HTML/HTMLMediaElement.cpp
+++ Userland/Libraries/LibWeb/HTML/HTMLMediaElement.cpp
@@ -1578,6 +1578,9 @@ void HTMLMediaElement::set_paused(bool paused)
 
     if (m_paused)
         on_paused();
+
+    if (auto* layout_node = this->layout_node())
+        layout_node->set_needs_display();
 }
 
 // https://html.spec.whatwg.org/multipage/media.html#blocked-media-element
@@ -1856,6 +1859,9 @@ void HTMLMediaElement::set_layout_display_time(Badge<Painting::MediaPaintable>, 
     }
 
     m_display_time = move(display_time);
+
+    if (auto* layout_node = this->layout_node())
+        layout_node->set_needs_display();
 }
 
 double HTMLMediaElement::layout_display_time(Badge<Painting::MediaPaintable>) const
