541bf17a4a2b2cbcb38189a32f7eaabe2e911f43
volovikariel <16262535+volovikariel@users.noreply.github.com>
2023-07-05 15:30:16 -0400

    Meta: Add missing package in Devcontainer
    When using Ladybird in a Devcontainer, the qt6-multimedia-dev package is
    missing from the install.sh file.

--- .devcontainer/features/serenity/install.sh
+++ .devcontainer/features/serenity/install.sh
@@ -33,7 +33,7 @@ install_llvm_key() {
 apt update -y
 apt install -y build-essential cmake ninja-build ccache shellcheck
 if [ "${ENABLE_LADYBIRD}" = "true" ]; then
-    apt install -y libgl1-mesa-dev qt6-base-dev qt6-tools-dev-tools qt6-wayland
+    apt install -y libgl1-mesa-dev qt6-base-dev qt6-tools-dev-tools qt6-wayland qt6-multimedia-dev
 fi
 if [ "${ENABLE_SERENITY}" = "true" ]; then
     apt install -y curl libmpfr-dev libmpc-dev libgmp-dev e2fsprogs genext2fs qemu-system-gui qemu-system-x86 qemu-utils rsync unzip texinfo libssl-dev
