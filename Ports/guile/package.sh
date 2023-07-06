1f719db9585e5834accf0a7afe6c560658b9e101
Peter Elliott <pelliott@serenityos.org>
2023-07-06 12:39:56 -0600

    Ports/guile: Upgrade guile to v3.0.9

--- Ports/guile/package.sh
+++ Ports/guile/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=guile
-version=3.0.8
-files="https://ftpmirror.gnu.org/gnu/guile/guile-${version}.tar.gz guile-${version}.tar.gz f25ae0c26e911af1b5005292d4f56621879f74d6958b30741cf67d8b6feb2016"
+version=3.0.9
+files="https://ftpmirror.gnu.org/gnu/guile/guile-${version}.tar.gz guile-${version}.tar.gz 18525079ad29a0d46d15c76581b5d91c8702301bfd821666d2e1d13726162811"
 depends=("gmp" "libunistring" "libffi" "bdwgc" "libiconv")
 
 useconfigure=true
