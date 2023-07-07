a961447dbd0694f1d7eed8c7018d22c118c6e877
Beckett Normington <beckett@b0ba.dev>
2023-07-07 12:28:19 -0400

    Ports: Add jdupes port
    This commit ports the file deduplication utility `jdupes`.

---
+++ Ports/jdupes/package.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port='jdupes'
+version='1.26.1'
+files="https://github.com/jbruchon/jdupes/archive/refs/tags/v${version}.tar.gz jdupes-${version}.tar.gz 09153824320c65ad529f8f97cd3b7e792c50e9f9018192ea1a76f2e33a196225"
+auth_type='sha256'
+workdir="jdupes-${version}"
+depends=('libjodycode')
+makeopts+=("UNAME_S=serenity")
+installopts+=('DISABLE_DEDUPE=1')
+export LDFLAGS="-z noexecstack"
