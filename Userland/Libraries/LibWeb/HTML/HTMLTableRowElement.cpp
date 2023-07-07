176baf7cdbbff0c5b644ad72d6c6930eb23a00bc
Nicolas Ramz <nicolas.ramz@gmail.com>
2023-07-07 11:42:25 +0200

    LibWeb: Support background attribute on table elements

--- Userland/Libraries/LibWeb/HTML/HTMLTableRowElement.cpp
+++ Userland/Libraries/LibWeb/HTML/HTMLTableRowElement.cpp
@@ -7,6 +7,8 @@
 #include <LibWeb/Bindings/Intrinsics.h>
 #include <LibWeb/CSS/StyleProperties.h>
 #include <LibWeb/CSS/StyleValues/ColorStyleValue.h>
+#include <LibWeb/CSS/StyleValues/ImageStyleValue.h>
+#include <LibWeb/DOM/Document.h>
 #include <LibWeb/DOM/ElementFactory.h>
 #include <LibWeb/DOM/HTMLCollection.h>
 #include <LibWeb/HTML/HTMLTableCellElement.h>
@@ -42,6 +44,10 @@ void HTMLTableRowElement::apply_presentational_hints(CSS::StyleProperties& style
             auto color = parse_legacy_color_value(value);
             if (color.has_value())
                 style.set_property(CSS::PropertyID::BackgroundColor, CSS::ColorStyleValue::create(color.value()).release_value_but_fixme_should_propagate_errors());
+            return;
+        } else if (name == HTML::AttributeNames::background) {
+            if (auto parsed_value = document().parse_url(value); parsed_value.is_valid())
+                style.set_property(CSS::PropertyID::BackgroundImage, CSS::ImageStyleValue::create(parsed_value).release_value_but_fixme_should_propagate_errors());
             return;
         }
     });
