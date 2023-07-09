7974aac0be701b216b4999babfaaca22235ba147
Lucas CHOLLET <lucas.chollet@free.fr>
2023-07-08 23:52:52 -0400

    LibGfx: Provide a default implementation for `ImageDecoder::initialize`
    In order to ease the removal of this function, let's provide a no-op
    default implementation so decoders can stop implementing it one by one.
    
    First step of #19893

--- Userland/Libraries/LibGfx/ImageFormats/ImageDecoder.h
+++ Userland/Libraries/LibGfx/ImageFormats/ImageDecoder.h
@@ -31,7 +31,7 @@ public:
 
     virtual IntSize size() = 0;
 
-    virtual ErrorOr<void> initialize() = 0;
+    virtual ErrorOr<void> initialize() { return {}; }
 
     virtual bool is_animated() = 0;
     virtual size_t loop_count() = 0;
