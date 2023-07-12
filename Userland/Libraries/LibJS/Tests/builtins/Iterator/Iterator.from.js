17b4f2f7cf67574cf2c58de8d6b6e897964d43fc
Timothy Flynn <trflynn89@pm.me>
2023-07-12 17:52:36 -0400

    LibJS: Change error message for non-objects in GetIteratorFlattenable
    Matches other usages in e.g. Temporal now.

--- Userland/Libraries/LibJS/Tests/builtins/Iterator/Iterator.from.js
+++ Userland/Libraries/LibJS/Tests/builtins/Iterator/Iterator.from.js
@@ -2,7 +2,7 @@ describe("errors", () => {
     test("called with non-Object", () => {
         expect(() => {
             Iterator.from(Symbol.hasInstance);
-        }).toThrowWithMessage(TypeError, "obj is not an object");
+        }).toThrowWithMessage(TypeError, "Symbol(Symbol.hasInstance) is not an object");
     });
 
     test("@@iterator is not callable", () => {
@@ -35,7 +35,7 @@ describe("errors", () => {
 
         expect(() => {
             Iterator.from(iterable);
-        }).toThrowWithMessage(TypeError, "iterator is not an object");
+        }).toThrowWithMessage(TypeError, "Symbol(Symbol.hasInstance) is not an object");
     });
 });
 
