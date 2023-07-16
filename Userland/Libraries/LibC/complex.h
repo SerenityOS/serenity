SerenityOS/serenity

commit cccb6c7287924a400c40d4c6cd2a9bd6aaec54f0

LibC: Drop complex.cpp and move its definitions to complex.h
libc++ disallows including LibC's complex.h in C++ mode. This means that
a C++ file cannot expect LibC's complex.h to be included, and thus
cannot use c-prefixed complex number functions. As a result,
complex.cpp is broken when libc++ has a higher include priority
than LibC.

A check for __cplusplus has been added to complex.h to warn users of
toolchains that don't use libc++.

Userland/Libraries/LibC/complex.h
@@ -11,6 +11,10 @@
 
 #pragma once
 
+#ifdef __cplusplus
+#    error "C++ code must not include complex.h. Use AK/Complex.h instead."
+#endif
+
 #include <stddef.h>
 #include <sys/cdefs.h>
 
@@ -25,14 +29,6 @@ __BEGIN_DECLS
 #define CMPLXF(x, y) ((float complex)__builtin_complex((float)x, (float)y))
 #define CMPLXL(x, y) ((long double complex)__builtin_complex((long double)x, (long double)y))
 
-float crealf(float complex z);
-double creal(double complex z);
-long double creall(long double complex z);
-
-double cimag(double complex z);
-float cimagf(float complex z);
-long double cimagl(long double complex z);
-
 // These are macro implementations of the above functions, so that they will always be inlined.
 #define creal(z) ((double)__real__((double complex)z))
 #define crealf(z) ((float)__real__((float complex)z))
@@ -41,5 +37,41 @@ long double cimagl(long double complex z);
 #define cimag(z) ((double)__imag__((double complex)z))
 #define cimagf(z) ((float)__imag__((float complex)z))
 #define cimagl(z) ((long double)__imag__((long double complex)z))
+
+// Function definitions of this form "type (name)(args)" are intentional, to
+// prevent macro versions of "name" from being incorrectly expanded. These
+// functions are here to provide external linkage to their macro implementations.
+
+// https://pubs.opengroup.org/onlinepubs/9699919799/functions/creal.html
+inline float(crealf)(float complex z)
+{
+    return crealf(z);
+}
+
+inline double(creal)(double complex z)
+{
+    return creal(z);
+}
+
+inline long double(creall)(long double complex z)
+{
+    return creall(z);
+}
+
+// https://pubs.opengroup.org/onlinepubs/9699919799/functions/cimag.html
+inline double(cimag)(double complex z)
+{
+    return cimag(z);
+}
+
+inline float(cimagf)(float complex z)
+{
+    return cimagf(z);
+}
+
+inline long double(cimagl)(long double complex z)
+{
+    return cimagl(z);
+}
 
 __END_DECLS

Userland/Libraries/LibC/CMakeLists.txt
@@ -1,7 +1,6 @@
 set(LIBC_SOURCES
     arpa/inet.cpp
     assert.cpp
-    complex.cpp
     ctype.cpp
     cxxabi.cpp
     dirent.cpp

Userland/Libraries/LibC/complex.cpp
@@ -1,46 +1,0 @@
-/*
- * Copyright (c) 2022, Peter Elliott <pelliott@serenityos.org>
- *
- * SPDX-License-Identifier: BSD-2-Clause
- */
-
-#include <complex.h>
-
-extern "C" {
-
-// Function definitions of this form "type (name)(args)" are intentional, to
-// prevent macro versions of "name" from being incorrectly expanded. These
-// functions are here to provide external linkage to their macro implementations.
-
-// https://pubs.opengroup.org/onlinepubs/9699919799/functions/creal.html
-float(crealf)(float complex z)
-{
-    return crealf(z);
-}
-
-double(creal)(double complex z)
-{
-    return creal(z);
-}
-
-long double(creall)(long double complex z)
-{
-    return creall(z);
-}
-
-// https://pubs.opengroup.org/onlinepubs/9699919799/functions/cimag.html
-double(cimag)(double complex z)
-{
-    return cimag(z);
-}
-
-float(cimagf)(float complex z)
-{
-    return cimagf(z);
-}
-
-long double(cimagl)(long double complex z)
-{
-    return cimagl(z);
-}
-}

commit 9db22b203cc7580da9d14b2b53dd8b97fb44370d

LibC: Make inline complex functions static
Fixes regression introduced by cccb6c7287924a400c40d4c6cd2a9bd6aaec54f0
While C++ allows more than one definition of an inline function in a
compilation unit, C does not. This was breaking the guile port.

Userland/Libraries/LibC/complex.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, Peter Elliott <pelliott@serenityos.org>
+ * Copyright (c) 2022-2023, Peter Elliott <pelliott@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -43,33 +43,33 @@ __BEGIN_DECLS
 // functions are here to provide external linkage to their macro implementations.
 
 // https://pubs.opengroup.org/onlinepubs/9699919799/functions/creal.html
-inline float(crealf)(float complex z)
+static inline float(crealf)(float complex z)
 {
     return crealf(z);
 }
 
-inline double(creal)(double complex z)
+static inline double(creal)(double complex z)
 {
     return creal(z);
 }
 
-inline long double(creall)(long double complex z)
+static inline long double(creall)(long double complex z)
 {
     return creall(z);
 }
 
 // https://pubs.opengroup.org/onlinepubs/9699919799/functions/cimag.html
-inline double(cimag)(double complex z)
+static inline double(cimag)(double complex z)
 {
     return cimag(z);
 }
 
-inline float(cimagf)(float complex z)
+static inline float(cimagf)(float complex z)
 {
     return cimagf(z);
 }
 
-inline long double(cimagl)(long double complex z)
+static inline long double(cimagl)(long double complex z)
 {
     return cimagl(z);
 }