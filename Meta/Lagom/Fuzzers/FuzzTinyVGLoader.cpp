973522ef31498afbcc175b007bb35fca6aea9c88
MacDue <macdue@dueutil.tech>
2023-07-17 22:33:53 +0100

    Fuzzers: Add a TinyVG fuzzer

---
+++ Meta/Lagom/Fuzzers/FuzzTinyVGLoader.cpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2023, MacDue <macdue@dueutil.tech>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibGfx/ImageFormats/TinyVGLoader.h>
+
+extern "C" int LLVMFuzzerTestOneInput(uint8_t const* data, size_t size)
+{
+    auto decoder_or_error = Gfx::TinyVGImageDecoderPlugin::create({ data, size });
+    if (decoder_or_error.is_error())
+        return 0;
+    auto decoder = decoder_or_error.release_value();
+    (void)decoder->frame(0);
+    return 0;
+}
