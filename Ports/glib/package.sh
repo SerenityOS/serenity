02630522847b13285bdb146de2ab4145943d2f12
Kenneth Myhra <kennethmyhra@gmail.com>
2023-07-18 21:06:56 +0200

    Ports/glib: Reformat package.sh according to our current style

--- Ports/glib/package.sh
+++ Ports/glib/package.sh
@@ -3,8 +3,16 @@ port='glib'
 version='2.77.0'
 files="https://download.gnome.org/sources/glib/2.77/glib-${version}.tar.xz glib-${version}.tar.xz 1897fd8ad4ebb523c32fabe7508c3b0b039c089661ae1e7917df0956a320ac4d"
 useconfigure='true'
-configopts=("--cross-file" "${SERENITY_BUILD_DIR}/meson-cross-file.txt")
-depends=("libiconv" "libffi" "zlib" "gettext" "pcre")
+configopts=(
+    '--cross-file' "${SERENITY_BUILD_DIR}/meson-cross-file.txt"
+)
+depends=(
+    'gettext'
+    'libffi'
+    'libiconv'
+    'pcre'
+    'zlib'
+)
 
 configure() {
     # TODO: Figure out why GCC doesn't autodetect that libgcc_s is needed.
