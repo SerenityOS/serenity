66d6388b8a8c22a1e9ea83c5c94020b75fc49d71
Nicolas Ramz <nicolas.ramz@gmail.com>
2023-07-29 16:21:06 +0200

    LibGfx/WebP: Move FourCC into ImageDecoder.h
    It will be shared with upcoming ILBM decoder.

--- Userland/Libraries/LibGfx/ImageFormats/ImageDecoder.h
+++ Userland/Libraries/LibGfx/ImageFormats/ImageDecoder.h
@@ -87,4 +87,19 @@ private:
     NonnullOwnPtr<ImageDecoderPlugin> mutable m_plugin;
 };
 
+struct FourCC {
+    constexpr FourCC(char const* name)
+    {
+        cc[0] = name[0];
+        cc[1] = name[1];
+        cc[2] = name[2];
+        cc[3] = name[3];
+    }
+
+    bool operator==(FourCC const&) const = default;
+    bool operator!=(FourCC const&) const = default;
+
+    char cc[4];
+};
+
 }
--- Userland/Libraries/LibGfx/ImageFormats/WebPLoader.cpp
+++ Userland/Libraries/LibGfx/ImageFormats/WebPLoader.cpp
@@ -21,21 +21,6 @@ namespace Gfx {
 
 namespace {
 
-struct FourCC {
-    constexpr FourCC(char const* name)
-    {
-        cc[0] = name[0];
-        cc[1] = name[1];
-        cc[2] = name[2];
-        cc[3] = name[3];
-    }
-
-    bool operator==(FourCC const&) const = default;
-    bool operator!=(FourCC const&) const = default;
-
-    char cc[4];
-};
-
 // https://developers.google.com/speed/webp/docs/riff_container#webp_file_header
 struct WebPFileHeader {
     FourCC riff;
