SerenityOS/serenity

commit 921aee8c666fbab2dcdba27cbefc942f1a144180

LibWeb: Optimize stacking context rebuilds on opacity change
Stacking context tree rebuild can be avoided if opacity value changes
within the range below 1.

Userland/Libraries/LibWeb/DOM/Element.cpp
@@ -15,6 +15,7 @@
 #include <LibWeb/CSS/ResolvedCSSStyleDeclaration.h>
 #include <LibWeb/CSS/SelectorEngine.h>
 #include <LibWeb/CSS/StyleComputer.h>
+#include <LibWeb/CSS/StyleValues/NumberStyleValue.h>
 #include <LibWeb/DOM/Attr.h>
 #include <LibWeb/DOM/DOMTokenList.h>
 #include <LibWeb/DOM/Document.h>
@@ -420,8 +421,18 @@ static Element::RequiredInvalidationAfterStyleChange compute_required_invalidati
         } else if (CSS::property_affects_layout(property_id)) {
             invalidation.relayout = true;
         }
-        if (CSS::property_affects_stacking_context(property_id))
+        if (property_id == CSS::PropertyID::Opacity) {
+            // OPTIMIZATION: An element creates a stacking context when its opacity changes from 1 to less than 1
+            //               and stops to create one when opacity returns to 1. So stacking context tree rebuild is
+            //               not required for opacity changes within the range below 1.
+            auto old_value_opacity = old_value.has_value() ? old_value->style->as_number().number() : 1;
+            auto new_value_opacity = new_value.has_value() ? new_value->style->as_number().number() : 1;
+            if (old_value_opacity != new_value_opacity && (old_value_opacity == 1 || new_value_opacity == 1)) {
+                invalidation.rebuild_stacking_context_tree = true;
+            }
+        } else if (CSS::property_affects_stacking_context(property_id)) {
             invalidation.rebuild_stacking_context_tree = true;
+        }
         invalidation.repaint = true;
     }
     return invalidation;

commit 1398ba9bd23b2e5b702594f39fb1e6f3d83fcfeb

LibWeb: Don't assume opacity values are CSS numbers
...since they can also be percentages. Better to resolve them to a pair
of absolute values, and then compare those. :^)

Regressed in 921aee8c666fbab2dcdba27cbefc942f1a144180.

Userland/Libraries/LibWeb/DOM/Element.cpp
@@ -425,8 +425,8 @@ static Element::RequiredInvalidationAfterStyleChange compute_required_invalidati
             // OPTIMIZATION: An element creates a stacking context when its opacity changes from 1 to less than 1
             //               and stops to create one when opacity returns to 1. So stacking context tree rebuild is
             //               not required for opacity changes within the range below 1.
-            auto old_value_opacity = old_value.has_value() ? old_value->style->as_number().number() : 1;
-            auto new_value_opacity = new_value.has_value() ? new_value->style->as_number().number() : 1;
+            auto old_value_opacity = old_style.opacity();
+            auto new_value_opacity = new_style.opacity();
             if (old_value_opacity != new_value_opacity && (old_value_opacity == 1 || new_value_opacity == 1)) {
                 invalidation.rebuild_stacking_context_tree = true;
             }

Tests/LibWeb/Text/input/css/element-opacity-change-invalidation.html
@@ -1,0 +1,12 @@
+<script src="../include.js"></script>
+<script>
+    test(() => {
+        document.body.style.opacity = '0.5';
+        document.body.offsetWidth
+        document.body.style.opacity = '100.0%';
+        document.body.offsetWidth
+        document.body.style.opacity = '0.5';
+        document.body.offsetWidth
+        println("Didn't crash :^)");
+    });
+</script>