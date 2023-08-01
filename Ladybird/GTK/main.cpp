8c32dc1f8e2732e524251315eb843538ba7ca18a
Sergey Bugaev <bugaevc@serenityos.org>
2023-08-01 17:05:45 +0300

    Ladybird/GTK: Add EventLoopImplementationGLib

--- Ladybird/GTK/main.cpp
+++ Ladybird/GTK/main.cpp
@@ -1,8 +1,13 @@
+#include "EventLoopImplementationGLib.h"
 #include "Window.h"
+#include <LibCore/EventLoop.h>
 #include <adwaita.h>
 
 int main(int argc, char* argv[])
 {
+    Core::EventLoopManager::install(*new EventLoopManagerGLib);
+    [[maybe_unused]] Core::EventLoop serenity_event_loop;
+
     AdwApplication* app = adw_application_new("org.serenityos.ladybird-gtk4", G_APPLICATION_HANDLES_OPEN);
 
     g_signal_connect(app, "activate", G_CALLBACK(+[](GtkApplication* app) {
