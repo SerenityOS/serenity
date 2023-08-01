f24aab662f9723c9fda80f50b6a55333eecdb996
Tim Ledbetter <timledbetter@gmail.com>
2023-08-01 20:23:37 +0100

    Ports: Update freeciv to version 3.0.8
    This commit also removes a logging patch, as these changes have now
    been upstreamed.

--- Ports/freeciv/package.sh
+++ Ports/freeciv/package.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=freeciv
-version=3.0.0-beta2
+version=3.0.8
 useconfigure=true
 use_fresh_config_sub=true
 config_sub_paths=("bootstrap/config.sub")
 configopts=("--enable-client=sdl2" "--enable-fcmp=no")
-files="http://files.freeciv.org/beta/freeciv-${version}.tar.xz freeciv-${version}.tar.xz 9f44dc28068b239c18bda68192ddb27622030880f8ab9c17f777eac28391269e"
+files="http://files.freeciv.org/stable/freeciv-${version}.tar.xz freeciv-${version}.tar.xz 3b5aa32f628890be1741c3ac942cee82c79c065f8db6baff18d734a5c0e776d4"
 depends=("SDL2" "SDL2_image" "SDL2_mixer" "SDL2_ttf" "SDL2_gfx" "zstd" "libicu" "xz" "gettext" "curl")
 launcher_name=Freeciv
 launcher_category=Games
--- Ports/freeciv/patches/ReadMe.md
+++
@@ -1,7 +0,0 @@
-# Patches for freeciv on SerenityOS
-
-## `0001-Log-to-dev-null-if-the-log-file-doesn-t-exist.patch`
-
-Log to /dev/null if the log file doesn't exist
-
-
