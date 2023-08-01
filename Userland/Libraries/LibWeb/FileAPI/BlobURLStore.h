2335f2576ff383ca174c8fafd6af948e5afca407
Timothy Flynn <trflynn89@pm.me>
2023-08-01 18:29:54 -0400

    LibWeb: Implement an ephemeral Blob URL store
    The Blob URL store is intended to be a singleton across all WebContent
    instances. But for now, this implements a per-WebContent store, which
    only lives as long as the WebContent process itself.

---
+++ Userland/Libraries/LibWeb/FileAPI/BlobURLStore.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2023, Tim Flynn <trflynn89@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/HashMap.h>
+#include <AK/String.h>
+#include <LibJS/Heap/GCPtr.h>
+#include <LibJS/Heap/Handle.h>
+#include <LibWeb/Forward.h>
+
+namespace Web::FileAPI {
+
+// https://w3c.github.io/FileAPI/#blob-url-entry
+struct BlobURLEntry {
+    JS::Handle<Blob> object; // FIXME: This could also be a MediaSource after we implement MSE.
+    JS::Handle<HTML::EnvironmentSettingsObject> environment;
+};
+
+// https://w3c.github.io/FileAPI/#BlobURLStore
+using BlobURLStore = HashMap<String, BlobURLEntry>;
+
+BlobURLStore& blob_url_store();
+ErrorOr<String> generate_new_blob_url();
+ErrorOr<String> add_entry_to_blob_url_store(JS::NonnullGCPtr<Blob> object);
+ErrorOr<void> remove_entry_from_blob_url_store(StringView url);
+
+}
