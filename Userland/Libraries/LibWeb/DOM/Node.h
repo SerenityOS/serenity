bb39ca9b4a113925a84067fe367ff6af6c8fbf55
Andreas Kling <kling@serenityos.org>
2023-08-03 13:26:29 +0200

    LibWeb: Add fast_is<SVGGraphicsElement>()
    This dynamic_cast was ~6% of runtime when mousing around on Discord.

--- Userland/Libraries/LibWeb/DOM/Node.h
+++ Userland/Libraries/LibWeb/DOM/Node.h
@@ -74,6 +74,7 @@ public:
     virtual bool requires_svg_container() const { return false; }
     virtual bool is_svg_container() const { return false; }
     virtual bool is_svg_element() const { return false; }
+    virtual bool is_svg_graphics_element() const { return false; }
     virtual bool is_svg_svg_element() const { return false; }
     virtual bool is_svg_use_element() const { return false; }
 
