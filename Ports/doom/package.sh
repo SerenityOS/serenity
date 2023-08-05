83ef5ba857dfe3b3f556f6e459f74e77336c1b16
Tim Ledbetter <timledbetter@gmail.com>
2023-08-05 10:26:24 +0100

    Ports/doom: Use upstream doomgeneric SDL port
    Doing this means we no longer need to maintain our own Doom fork and
    we get  the newest features of the upstream repository, such as sound
    effects and music.

--- Ports/doom/package.sh
+++ Ports/doom/package.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=doom
-workdir=SerenityDOOM-master
-version=git
-files="https://github.com/SerenityPorts/SerenityDOOM/archive/master.tar.gz doom-git.tar.gz"
-makeopts=("-C" "doomgeneric/")
-installopts=("-C" "doomgeneric/")
-launcher_name=Doom
-launcher_category=Games
-launcher_command=doom
+port='doom'
+commit_hash='613f870b6fa83ede448a247de5a2571092fa729d'
+workdir="doomgeneric-${commit_hash}"
+version='git'
+files="https://github.com/ozkl/doomgeneric/archive/${commit_hash}.tar.gz doom-git.tar.gz fabc3e41aca92f58dfdd284754891c17875ec8c995948b49396ead6bc05b8676"
+depends=(
+    'SDL2'
+    'SDL2_mixer'
+)
+makeopts=(
+    '--directory=doomgeneric/'
+    '--file=Makefile.sdl'
+)
+installopts=(
+    '--directory=doomgeneric/'
+    '--file=Makefile.sdl'
+)
+launcher_name='Doom'
+launcher_category='Games'
+launcher_command='/usr/local/bin/doom'
