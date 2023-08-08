3e827bf55ec264896ec870fe1023edb00c14ff32
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2023-08-08 15:02:35 +0200

    LibWeb/Painting: Translate by scroll offset before painting descendants
    Fixes painting of nested nodes in scrollable containers by moving
    painter's scroll offset translation from paint_node() to
    before_children_paint() and after_children_paint().

--- Userland/Libraries/LibWeb/Painting/PaintableBox.h
+++ Userland/Libraries/LibWeb/Painting/PaintableBox.h
@@ -125,6 +125,9 @@ public:
     DOM::Document const& document() const { return layout_box().document(); }
     DOM::Document& document() { return layout_box().document(); }
 
+    virtual void before_children_paint(PaintContext&, PaintPhase) const override;
+    virtual void after_children_paint(PaintContext&, PaintPhase) const override;
+
     virtual void apply_clip_overflow_rect(PaintContext&, PaintPhase) const override;
     virtual void clear_clip_overflow_rect(PaintContext&, PaintPhase) const override;
 
