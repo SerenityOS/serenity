0e0cfad2ee19a508d9d77d8170e6b71c1d7fe28d
Daniel Bertalan <dani@danielbertalan.dev>
2023-08-08 09:53:14 +0200

    LibJS: Uncomment passing tests

--- Userland/Libraries/LibJS/Tests/builtins/Symbol/Symbol.prototype.valueOf.js
+++ Userland/Libraries/LibJS/Tests/builtins/Symbol/Symbol.prototype.valueOf.js
@@ -1,11 +1,11 @@
 test("basic functionality", () => {
     const local = Symbol("foo");
-    // const global = Symbol.for("foo");
+    const global = Symbol.for("foo");
     expect(local.valueOf()).toBe(local);
-    // expect(global.valueOf()).toBe(global);
+    expect(global.valueOf()).toBe(global);
 
     expect(Symbol.prototype.valueOf.call(local)).toBe(local);
-    // expect(Symbol.prototype.valueOf.call(global)).toBe(global);
+    expect(Symbol.prototype.valueOf.call(global)).toBe(global);
 });
 
 test("|this| must be a symbol", () => {
