c8f78066c4a7eba38966ecb4526c54f21e2d5c36
Tim Ledbetter <timledbetter@gmail.com>
2023-08-10 23:14:50 +0100

    Ports/SDL2: Update to version 2.28.2

--- Ports/SDL2/package.sh
+++ Ports/SDL2/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='SDL2'
-version='2.24.0'
+version='2.28.2'
 useconfigure='true'
 files=(
-    "https://github.com/libsdl-org/SDL/releases/download/release-${version}/SDL2-${version}.tar.gz 91e4c34b1768f92d399b078e171448c6af18cafda743987ed2064a28954d6d97"
+    "https://github.com/libsdl-org/SDL/releases/download/release-${version}/SDL2-${version}.tar.gz 64b1102fa22093515b02ef33dd8739dee1ba57e9dbba6a092942b8bbed1a1c5e"
 )
 configopts=(
     "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
