b0469406796ee4616047224b62c0b4d73455749d
Andrew Kaster <akaster@serenityos.org>
2023-08-10 15:45:19 -0600

    CMake: Download Commonmark and Wasm spec tests using download_file()
    This makes sure we have no bare file(DOWNLOAD) calls in the build

--- Meta/CMake/commonmark_spec.cmake
+++ Meta/CMake/commonmark_spec.cmake
@@ -4,6 +4,6 @@ set(MARKDOWN_TEST_PATH ${CMAKE_BINARY_DIR}/commonmark.spec.json)
 set(MARKDOWN_TEST_URL https://spec.commonmark.org/0.30/spec.json)
 
 if(ENABLE_COMMONMARK_SPEC_DOWNLOAD)
-    file(DOWNLOAD ${MARKDOWN_TEST_URL} ${MARKDOWN_TEST_PATH})
+    download_file(${MARKDOWN_TEST_URL} ${MARKDOWN_TEST_PATH})
     install(FILES ${MARKDOWN_TEST_PATH} DESTINATION home/anon/Tests)
 endif()
