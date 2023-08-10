9e22f01ebad1b08367b3a9719e79f396351ea175
Andreas Kling <kling@serenityos.org>
2023-08-10 09:55:15 +0200

    LibWeb: Stub out SVGMaskElement
    Just enough that we stop creating layout nodes for mask elements, which
    was making some SVG content look very wrong. :^)

---
+++ Userland/Libraries/LibWeb/SVG/SVGMaskElement.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2023, Andreas Kling <kling@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibWeb/Bindings/SVGMaskElementPrototype.h>
+#include <LibWeb/DOM/Document.h>
+#include <LibWeb/SVG/SVGMaskElement.h>
+
+namespace Web::SVG {
+
+SVGMaskElement::SVGMaskElement(DOM::Document& document, DOM::QualifiedName tag_name)
+    : SVGGraphicsElement(document, move(tag_name))
+{
+}
+
+SVGMaskElement::~SVGMaskElement() = default;
+
+void SVGMaskElement::initialize(JS::Realm& realm)
+{
+    Base::initialize(realm);
+    set_prototype(&Bindings::ensure_web_prototype<Bindings::SVGMaskElementPrototype>(realm, "SVGMaskElement"));
+}
+
+JS::GCPtr<Layout::Node> SVGMaskElement::create_layout_node(NonnullRefPtr<CSS::StyleProperties>)
+{
+    return nullptr;
+}
+
+}
