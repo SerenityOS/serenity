af6b0ad3d457bcf1571207f4e321f7b54294d50d
Beckett Normington <beckett@b0ba.dev>
2023-08-10 10:26:58 -0400

    Ports: Add xorriso port

---
+++ Ports/xorriso/package.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port='xorriso'
+version='1.5.6'
+files=(
+    "https://www.gnu.org/software/xorriso/xorriso-${version}.tar.gz d4b6b66bd04c49c6b358ee66475d806d6f6d7486e801106a47d331df1f2f8feb"
+)
+depends=(
+    'libiconv'
+)
+useconfigure='true'
+use_fresh_config_sub='true'
---
+++ Ports/xorriso/patches/ReadMe.md
@@ -0,0 +1,7 @@
+# Patches for xorriso on SerenityOS
+
+## `0001-parse_exec-replace-wait3-with-waitpid.patch`
+
+parse_exec: replace wait3() with waitpid()
+
+
