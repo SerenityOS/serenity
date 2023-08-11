8d910b1493d860174f1aa100e987da0eb65c2da4
Tim Ledbetter <timledbetter@gmail.com>
2023-08-11 15:22:18 +0100

    Ports/mgba: Explicitly exclude optional dependencies
    Previously, some optional dependencies could be unintentionally
    included if they were present on the host machine.

--- Ports/mgba/package.sh
+++ Ports/mgba/package.sh
@@ -5,7 +5,11 @@ files=(
     "https://github.com/mgba-emu/mgba/archive/refs/tags/${version}.tar.gz 692ff0ac50e18380df0ff3ee83071f9926715200d0dceedd9d16a028a59537a0"
 )
 configopts=(
+    '-DBUILD_QT=OFF'
     "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
+    '-DUSE_EDITLINE=OFF'
+    '-DUSE_ELF=OFF'
+    '-DUSE_EPOXY=OFF'
 )
 useconfigure=true
 depends=(
