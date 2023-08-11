SerenityOS/serenity

commit fb4db096cb16b774346137120278cdd07abab1e0

Ports: Remove the `filename` entry from `files`

Ports/.port_include.sh
@@ -312,13 +312,9 @@ do_download_file() {
 
 fetch_simple() {
     url="${1}"
-    filename="${2}"
-    checksum="${3}"
+    checksum="${2}"
 
-    if [ "${filename}" != "$(basename "${url}")" ]; then
-        echo "Filename '${filename}' does not match basename of '${url}'"
-        exit 1
-    fi
+    filename="$(basename "${url}")"
 
     tried_download_again=0
 
@@ -371,8 +367,8 @@ func_defined fetch || fetch() {
     pre_fetch
 
     for f in "${files[@]}"; do
-        read url filename auth_sum <<< $(echo "${f}")
-        fetch_simple "${url}" "${filename}" "${auth_sum}"
+        read url auth_sum <<< $(echo "${f}")
+        fetch_simple "${url}" "${auth_sum}"
     done
 
     post_fetch

commit 84e3fa0eddbcd8fee431a7a5c4fecc0405ae6b74

Ports: Fix `clean_dist` function in .port_include.sh
A bug was introduced in commit #20413, where the `clean_dist` function
was missed from refactoring to the new "files" format.

Ports/.port_include.sh
@@ -422,7 +422,8 @@ clean() {
 }
 clean_dist() {
     for f in "${files[@]}"; do
-        read url filename hash <<< $(echo "$f")
+        read url hash <<< "$f"
+        filename=$(basename "$url")
         rm -f "${PORT_META_DIR}/${filename}"
     done
 }