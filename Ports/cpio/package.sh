afdc03abda7e07709ca0203fc954fc8a7105ad4a
Liav A <liavalb@gmail.com>
2023-08-12 02:58:46 +0300

    Ports/cpio: Update to version 2.14
    We can also remove a patch that is no longer needed.

--- Ports/cpio/package.sh
+++ Ports/cpio/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='cpio'
-version='2.13'
+version='2.14'
 useconfigure='true'
 use_fresh_config_sub='true'
 config_sub_paths=('build-aux/config.sub')
 files=(
-    "https://ftpmirror.gnu.org/gnu/cpio/cpio-${version}.tar.gz e87470d9c984317f658567c03bfefb6b0c829ff17dbf6b0de48d71a4c8f3db88"
+    "https://ftpmirror.gnu.org/gnu/cpio/cpio-${version}.tar.gz 145a340fd9d55f0b84779a44a12d5f79d77c99663967f8cfa168d7905ca52454"
 )
