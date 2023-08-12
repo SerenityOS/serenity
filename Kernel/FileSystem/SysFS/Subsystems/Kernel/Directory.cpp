f363922fd0ee0e854675ab31ffc77b40b7906099
Liav A <liavalb@gmail.com>
2023-08-12 14:06:35 +0300

    Kernel: Remove the /sys/kernel/constants directory
    The name for this directory is a bit awkward. Also, the distinction of
    constant information is not really valuable as I thought it would be, so
    let's bring that information back into the /sys/kernel directory.

--- Kernel/FileSystem/SysFS/Subsystems/Kernel/Directory.cpp
+++ Kernel/FileSystem/SysFS/Subsystems/Kernel/Directory.cpp
@@ -6,10 +6,11 @@
 
 #include <AK/Error.h>
 #include <AK/Try.h>
+#include <Kernel/Boot/CommandLine.h>
 #include <Kernel/FileSystem/SysFS/Component.h>
 #include <Kernel/FileSystem/SysFS/Subsystems/Kernel/CPUInfo.h>
 #include <Kernel/FileSystem/SysFS/Subsystems/Kernel/Configuration/Directory.h>
-#include <Kernel/FileSystem/SysFS/Subsystems/Kernel/Constants/Directory.h>
+#include <Kernel/FileSystem/SysFS/Subsystems/Kernel/ConstantInformation.h>
 #include <Kernel/FileSystem/SysFS/Subsystems/Kernel/Directory.h>
 #include <Kernel/FileSystem/SysFS/Subsystems/Kernel/DiskUsage.h>
 #include <Kernel/FileSystem/SysFS/Subsystems/Kernel/GlobalInformation.h>
@@ -30,9 +31,7 @@ namespace Kernel {
 UNMAP_AFTER_INIT NonnullRefPtr<SysFSGlobalKernelStatsDirectory> SysFSGlobalKernelStatsDirectory::must_create(SysFSRootDirectory const& root_directory)
 {
     auto global_kernel_stats_directory = adopt_ref_if_nonnull(new (nothrow) SysFSGlobalKernelStatsDirectory(root_directory)).release_nonnull();
-    auto global_constants_directory = SysFSGlobalKernelConstantsDirectory::must_create(*global_kernel_stats_directory);
     MUST(global_kernel_stats_directory->m_child_components.with([&](auto& list) -> ErrorOr<void> {
-        list.append(global_constants_directory);
         list.append(SysFSDiskUsage::must_create(*global_kernel_stats_directory));
         list.append(SysFSMemoryStatus::must_create(*global_kernel_stats_directory));
         list.append(SysFSSystemStatistics::must_create(*global_kernel_stats_directory));
@@ -48,6 +47,32 @@ UNMAP_AFTER_INIT NonnullRefPtr<SysFSGlobalKernelStatsDirectory> SysFSGlobalKerne
 
         list.append(SysFSGlobalNetworkStatsDirectory::must_create(*global_kernel_stats_directory));
         list.append(SysFSKernelConfigurationDirectory::must_create(*global_kernel_stats_directory));
+
+        {
+            auto builder = TRY(KBufferBuilder::try_create());
+            MUST(builder.appendff("{}", kernel_load_base));
+            auto load_base_buffer = builder.build();
+            VERIFY(load_base_buffer);
+            list.append(SysFSSystemConstantInformation::must_create(*global_kernel_stats_directory, load_base_buffer.release_nonnull(), S_IRUSR, SysFSSystemConstantInformation::ReadableByJailedProcesses::No, SysFSSystemConstantInformation::NodeName::LoadBase));
+        }
+
+        {
+            auto builder = TRY(KBufferBuilder::try_create());
+            MUST(builder.append(kernel_command_line().string()));
+            MUST(builder.append('\n'));
+            auto command_line_buffer = builder.build();
+            VERIFY(command_line_buffer);
+            list.append(SysFSSystemConstantInformation::must_create(*global_kernel_stats_directory, command_line_buffer.release_nonnull(), S_IRUSR | S_IRGRP | S_IROTH, SysFSSystemConstantInformation::ReadableByJailedProcesses::No, SysFSSystemConstantInformation::NodeName::CommandLine));
+        }
+
+        {
+            auto builder = TRY(KBufferBuilder::try_create());
+            MUST(builder.append(kernel_command_line().system_mode()));
+            MUST(builder.append('\n'));
+            auto system_mode_buffer = builder.build();
+            VERIFY(system_mode_buffer);
+            list.append(SysFSSystemConstantInformation::must_create(*global_kernel_stats_directory, system_mode_buffer.release_nonnull(), S_IRUSR | S_IRGRP | S_IROTH, SysFSSystemConstantInformation::ReadableByJailedProcesses::No, SysFSSystemConstantInformation::NodeName::SystemMode));
+        }
         return {};
     }));
     return global_kernel_stats_directory;
