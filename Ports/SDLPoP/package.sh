3b6a910b93755d1357f71d2f73ba4fb250eb20c1
Tim Ledbetter <timledbetter@gmail.com>
2023-08-13 12:52:09 +0100

    Ports/SDLPoP: Update formatting to be consistent with other ports

--- Ports/SDLPoP/package.sh
+++ Ports/SDLPoP/package.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=SDLPoP
-useconfigure=true
-version=git
-depends=("SDL2" "SDL2_image")
-commitid="86988c668eeaa10f218e1d4938fc5b4e42314d68"
+port='SDLPoP'
+useconfigure='true'
+version='git'
+depends=(
+    'SDL2'
+    'SDL2_image'
+)
+commitid='86988c668eeaa10f218e1d4938fc5b4e42314d68'
 workdir="${port}-${commitid}"
-configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt")
+configopts=(
+    "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
+)
 files=(
     "https://github.com/NagyD/SDLPoP/archive/${commitid}.zip d18cae8541fb8cbcc374fd998316993d561429a83f92061bc0754337ada774c5"
 )
-launcher_name="Prince of Persia"
-launcher_category=Games
-launcher_command=/opt/PrinceOfPersia/prince
+launcher_name='Prince of Persia'
+launcher_category='Games'
+launcher_command='/opt/PrinceOfPersia/prince'
 icon_file='src/icon.ico'
 
 configure() {
