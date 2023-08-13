53c1dba06a3c0fbdf7059a9d978d1e013fa481b9
Tim Ledbetter <timledbetter@gmail.com>
2023-08-13 08:03:29 +0100

    Ports/hatari: Update to version 2.4.1

--- Ports/hatari/package.sh
+++ Ports/hatari/package.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=hatari
 useconfigure=true
-version=2.4.0-devel
+version='2.4.1'
 depends=("SDL2" "zlib")
-commit=6a86f054cc560a858bbe60c7529dafe2cf6ec604
-workdir="${port}-${commit}"
 configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt")
 files=(
-    "https://github.com/hatari/hatari/archive/${commit}.tar.gz bcb5d2e3bf3a3f8b34d21565354aa8eb085e3d92eb466c5d28e42e2022e7da3d"
+    "https://github.com/hatari/hatari/archive/refs/tags/v${version}.tar.gz 68c5edbe60db7a83e6e9b427eaac1136b62653846d64e415850e88d9a6a2cbc2"
 )
 launcher_name=Hatari
 launcher_category=Games
