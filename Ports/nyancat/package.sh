cc304b2954c0ae8013796aed0ae3b1364ac6c370
Tim Ledbetter <timledbetter@gmail.com>
2023-08-13 09:58:19 +0100

    Ports/nyancat: Fetch from a specific commit hash rather than master
    This avoids a potential sha256 mismatch whenever something is pushed
    to the `master` branch.

--- Ports/nyancat/package.sh
+++ Ports/nyancat/package.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=nyancat
 version=git
-workdir=nyancat-master
+commit_hash='5ffb6c5c03d0e9156db8f360599d4f0449bb16b9'
+workdir="nyancat-${commit_hash}"
 files=(
-    "https://github.com/klange/nyancat/archive/master.tar.gz cfd6c817f25adcecc9490321991ecb571bfdfe0d8c249663843d3df4194f935d"
+    "https://github.com/klange/nyancat/archive/${commit_hash}.tar.gz d9c3ea82ce59f0d7db86db9e8a626f8f8fa2fbd9544104557e4c59a31893ca31"
 )
 launcher_name=Nyancat
 launcher_category=Games
