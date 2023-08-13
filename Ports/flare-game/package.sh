2d31903e070ba1d539ad8f834060e13298f59514
Hans Joachim Desserud <github@desserud.org>
2023-08-13 14:13:12 +0200

    Ports: Add flare-game
    Add a playable campagin to flare-engine

---
+++ Ports/flare-game/package.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port='flare-game'
+useconfigure='true'
+version='1.14'
+depends=(
+    'flare-engine'
+)
+files=(
+    "https://github.com/flareteam/flare-game/archive/refs/tags/v${version}.tar.gz 65758462070aa88842084f8ee69083d8226e93cfbf83481663276d8307494b8e"
+)
+
+configopts=(
+    "-DCMAKE_INSTALL_PREFIX=${SERENITY_INSTALL_ROOT}/usr/local"
+    "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
+)
+
+configure() {
+    run cmake "${configopts[@]}"
+}
+
+install() {
+    run make "${installopts[@]}" install
+}
