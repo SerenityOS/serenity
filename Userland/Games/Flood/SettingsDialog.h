40bc8db44de2d44e6b25bb366d23c04d7aa6eeb0
implicitfield <114500360+implicitfield@users.noreply.github.com>
2023-08-13 14:06:46 +0300

    Flood: Propagate errors when creating SettingsDialog

--- Userland/Games/Flood/SettingsDialog.h
+++ Userland/Games/Flood/SettingsDialog.h
@@ -10,13 +10,14 @@
 #include <LibGUI/Dialog.h>
 
 class SettingsDialog : public GUI::Dialog {
-    C_OBJECT(SettingsDialog)
+    C_OBJECT_ABSTRACT(SettingsDialog)
 public:
+    static ErrorOr<NonnullRefPtr<SettingsDialog>> try_create(GUI::Window* parent, size_t board_rows, size_t board_columns);
     size_t board_rows() const { return m_board_rows; }
     size_t board_columns() const { return m_board_columns; }
 
 private:
-    SettingsDialog(GUI::Window* parent, size_t board_rows, size_t board_columns);
+    SettingsDialog(NonnullRefPtr<GUI::Widget> settings_widget, GUI::Window* parent, size_t board_rows, size_t board_columns);
 
     size_t m_board_rows;
     size_t m_board_columns;
