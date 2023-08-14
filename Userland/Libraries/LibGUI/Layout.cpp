b67909452961b4f9e18b942a70e332d567c5c83f
Andreas Kling <kling@serenityos.org>
2023-08-14 06:47:48 +0200

    LibGUI: Remove remaining fallible Layout APIs

--- Userland/Libraries/LibGUI/Layout.cpp
+++ Userland/Libraries/LibGUI/Layout.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2018-2023, Andreas Kling <kling@serenityos.org>
  * Copyright (c) 2022, the SerenityOS developers.
  *
  * SPDX-License-Identifier: BSD-2-Clause
@@ -60,17 +60,11 @@ void Layout::notify_disowned(Badge<Widget>, Widget& widget)
     m_entries.clear();
 }
 
-ErrorOr<void> Layout::try_add_entry(Entry&& entry)
-{
-    TRY(m_entries.try_append(move(entry)));
-    if (m_owner)
-        m_owner->notify_layout_changed({});
-    return {};
-}
-
 void Layout::add_entry(Entry&& entry)
 {
-    MUST(try_add_entry(move(entry)));
+    m_entries.append(move(entry));
+    if (m_owner)
+        m_owner->notify_layout_changed({});
 }
 
 void Layout::add_spacer()
@@ -86,36 +80,24 @@ void Layout::add_layout(OwnPtr<Layout>&& layout)
     add_entry(move(entry));
 }
 
-ErrorOr<void> Layout::try_add_widget(Widget& widget)
-{
-    TRY(try_add_entry(Entry {
-        .type = Entry::Type::Widget,
-        .widget = widget,
-    }));
-    return {};
-}
-
 void Layout::add_widget(Widget& widget)
 {
-    MUST(try_add_widget(widget));
-}
-
-ErrorOr<void> Layout::try_insert_widget_before(Widget& widget, Widget& before_widget)
-{
-    Entry entry;
-    entry.type = Entry::Type::Widget;
-    entry.widget = widget;
-    TRY(m_entries.try_insert_before_matching(move(entry), [&](auto& existing_entry) {
-        return existing_entry.type == Entry::Type::Widget && existing_entry.widget.ptr() == &before_widget;
-    }));
-    if (m_owner)
-        m_owner->notify_layout_changed({});
-    return {};
+    add_entry(Entry {
+        .type = Entry::Type::Widget,
+        .widget = widget,
+    });
 }
 
 void Layout::insert_widget_before(Widget& widget, Widget& before_widget)
 {
-    MUST(try_insert_widget_before(widget, before_widget));
+    Entry entry;
+    entry.type = Entry::Type::Widget;
+    entry.widget = widget;
+    m_entries.insert_before_matching(move(entry), [&](auto& existing_entry) {
+        return existing_entry.type == Entry::Type::Widget && existing_entry.widget.ptr() == &before_widget;
+    });
+    if (m_owner)
+        m_owner->notify_layout_changed({});
 }
 
 void Layout::remove_widget(Widget& widget)
