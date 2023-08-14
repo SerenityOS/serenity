3782903642abfe51c6a6d363c344070e01504f84
Tim Ledbetter <timledbetter@gmail.com>
2023-08-14 22:47:25 +0100

    Ports/brogue: Update formatting to be consistent with other ports

--- Ports/brogue/package.sh
+++ Ports/brogue/package.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='brogue'
-depends=("SDL2" "SDL2_image")
+depends=(
+    'SDL2'
+    'SDL2_image'
+)
 version='1.12'
 workdir="BrogueCE-${version}"
 files=(
     "https://github.com/tmewett/BrogueCE/archive/refs/tags/v${version}.tar.gz aeed3f6ca0f4e352137b0196e9dddbdce542a9e99dda9effd915e018923cd428"
 )
-makeopts+=("bin/brogue")
+makeopts+=(
+    'bin/brogue'
+)
 launcher_name='Brogue'
 launcher_category='Games'
 launcher_command='/usr/local/bin/brogue'
