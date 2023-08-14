b5f34b3cda1cebcc59906517ff65d228cd50b6a3
Tim Ledbetter <timledbetter@gmail.com>
2023-08-14 22:57:07 +0100

    Ports/brogue: Update to version 1.12

--- Ports/brogue/package.sh
+++ Ports/brogue/package.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='brogue'
 depends=("SDL2" "SDL2_image")
-version='1.11.1'
+version='1.12'
 workdir="BrogueCE-${version}"
 files=(
-    "https://github.com/tmewett/BrogueCE/archive/refs/tags/v${version}.tar.gz dc562cf774f88b12b6aeebdac5a00e62e8598b3f84da2130a54a67a60c5debf2"
+    "https://github.com/tmewett/BrogueCE/archive/refs/tags/v${version}.tar.gz aeed3f6ca0f4e352137b0196e9dddbdce542a9e99dda9effd915e018923cd428"
 )
 makeopts+=("bin/brogue")
 launcher_name='Brogue'
