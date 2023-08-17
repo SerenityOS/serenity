d7c1a1ecacf47c3f6c938c890276bd4c75e9e645
Tim Ledbetter <timledbetter@gmail.com>
2023-08-17 17:08:40 +0100

    Ports/libgd: Update formatting to be consistent with other ports

--- Ports/libgd/package.sh
+++ Ports/libgd/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=libgd
-version=2.3.3
-useconfigure=true
-use_fresh_config_sub=true
+port='libgd'
+version='2.3.3'
+useconfigure='true'
+use_fresh_config_sub='true'
 configopts=(
     "--with-sysroot=${SERENITY_INSTALL_ROOT}"
     '--without-avif'
@@ -13,7 +13,9 @@ configopts=(
     '--without-x'
     '--without-xpm'
 )
-config_sub_paths=("config/config.sub")
+config_sub_paths=(
+    'config/config.sub'
+)
 files=(
     "https://github.com/libgd/libgd/releases/download/gd-${version}/libgd-${version}.tar.gz dd3f1f0bb016edcc0b2d082e8229c822ad1d02223511997c80461481759b1ed2"
 )
