1f2aef4028a0617fb6f43c0ecb5054fca9225b92
Tim Ledbetter <timledbetter@gmail.com>
2023-08-17 17:05:40 +0100

    Ports/libgd: Explicitly specify which dependencies to include
    Previously, building `libgd` with `fontconfig` already installed would
    cause the build to fail.

--- Ports/libgd/package.sh
+++ Ports/libgd/package.sh
@@ -3,9 +3,25 @@ port=libgd
 version=2.3.3
 useconfigure=true
 use_fresh_config_sub=true
-configopts=("--without-x")
+configopts=(
+    "--with-sysroot=${SERENITY_INSTALL_ROOT}"
+    '--without-avif'
+    '--without-heif'
+    '--without-liq'
+    '--without-raqm'
+    '--without-webp'
+    '--without-x'
+    '--without-xpm'
+)
 config_sub_paths=("config/config.sub")
 files=(
     "https://github.com/libgd/libgd/releases/download/gd-${version}/libgd-${version}.tar.gz dd3f1f0bb016edcc0b2d082e8229c822ad1d02223511997c80461481759b1ed2"
 )
-depends=("libpng")
+depends=(
+    'fontconfig'
+    'freetype'
+    'libjpeg'
+    'libpng'
+    'libtiff'
+    'zlib'
+)
