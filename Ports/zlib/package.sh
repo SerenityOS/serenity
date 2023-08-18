a1ccc4b0cfd178d3cdc0009bd1c6dad43b4e854b
Danik2343 <38865696+Danik2343@users.noreply.github.com>
2023-08-18 23:53:06 +0300

    Ports/zlib: Update to version 1.3

--- Ports/zlib/package.sh
+++ Ports/zlib/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=zlib
-version=1.2.13
-useconfigure=true
+port='zlib'
+version='1.3'
+useconfigure='true'
 files=(
-    "https://www.zlib.net/zlib-${version}.tar.gz b3a24de97a8fdbc835b9833169501030b8977031bcb54b3b3ac13740f846ab30"
+    "https://www.zlib.net/zlib-${version}.tar.gz ff0ba4c292013dbc27530b3a81e1f9a813cd39de01ca5e0f8bf355702efa593e"
 )
 
 configure() {
