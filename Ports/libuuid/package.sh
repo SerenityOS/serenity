077ee5d90f0c03d2a6d7c3ae811d934d94b5841a
Tim Ledbetter <timledbetter@gmail.com>
2023-08-19 19:06:26 +0100

    Ports/libuuid: Update formatting to be consistent with other ports

--- Ports/libuuid/package.sh
+++ Ports/libuuid/package.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 
-port=libuuid
-version=2.38
+port='libuuid'
+version='2.38'
 workdir="util-linux-${version}"
-useconfigure=true
-configopts=("--prefix=/usr/local" "--disable-all-programs" "--enable-libuuid")
+useconfigure='true'
+configopts=(
+    '--disable-all-programs'
+    '--enable-libuuid'
+    '--prefix=/usr/local'
+)
 files=(
     "https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v${version}/util-linux-${version}.tar.gz c31d4e54f30b56b0f7ec8b342658c07de81378f2c067941c2b886da356f8ad42"
 )
-use_fresh_config_sub=true
-config_sub_paths=("config/config.sub")
+use_fresh_config_sub='true'
+config_sub_paths=(
+    'config/config.sub'
+)
