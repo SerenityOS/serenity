9d9058e6c98ddb96a1ef8a4409c70594a2207dc9
Tim Ledbetter <timledbetter@gmail.com>
2023-08-20 18:43:34 +0100

    Ports/freedink: Disable sound from launcher
    Currently, launching the game with sound effects enabled causes the
    game to segfault, so let's disable it until the sound issue is fixed.

--- Ports/freedink/package.sh
+++ Ports/freedink/package.sh
@@ -30,7 +30,7 @@ configopts=(
 
 launcher_name='FreeDink'
 launcher_category='Games'
-launcher_command='/usr/local/bin/freedink --software-rendering --truecolor'
+launcher_command='/usr/local/bin/freedink --software-rendering --truecolor --nosound'
 icon_file='src/media/freedink.ico'
 
 install() {
