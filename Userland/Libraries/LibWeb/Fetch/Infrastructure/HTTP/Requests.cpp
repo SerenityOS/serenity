SerenityOS/serenity

commit bdd3a16b16c99bd6342ad34b629b89a510942699

LibWeb: Make `Fetch::Infrastructure::Body` be GC allocated
Making the body GC-allocated allows us to avoid using `JS::Handle`
for `m_stream` in its members.

Userland/Libraries/LibWeb/Fetch/Infrastructure/HTTP/Requests.cpp
@@ -23,6 +23,9 @@ void Request::visit_edges(JS::Cell::Visitor& visitor)
     Base::visit_edges(visitor);
     visitor.visit(m_header_list);
     visitor.visit(m_client);
+    m_body.visit(
+        [&](JS::GCPtr<Body>& body) { visitor.visit(body); },
+        [](auto&) {});
     m_reserved_client.visit(
         [&](JS::GCPtr<HTML::EnvironmentSettingsObject> const& value) { visitor.visit(value); },
         [](auto const&) {});
@@ -249,8 +252,8 @@ JS::NonnullGCPtr<Request> Request::clone(JS::Realm& realm) const
     new_request->set_timing_allow_failed(m_timing_allow_failed);
 
     // 2. If request’s body is non-null, set newRequest’s body to the result of cloning request’s body.
-    if (auto const* body = m_body.get_pointer<Body>())
-        new_request->set_body(body->clone(realm));
+    if (auto const* body = m_body.get_pointer<JS::NonnullGCPtr<Body>>())
+        new_request->set_body((*body)->clone(realm));
 
     // 3. Return newRequest.
     return new_request;

Userland/Libraries/LibWeb/DOM/DocumentLoading.cpp
@@ -233,7 +233,7 @@ JS::GCPtr<DOM::Document> load_document(Optional<HTML::NavigationParams> navigati
 
     auto& realm = document->realm();
 
-    if (navigation_params->response->body().has_value()) {
+    if (navigation_params->response->body()) {
         auto process_body = [navigation_params, document](ByteBuffer bytes) {
             if (!parse_document(*document, bytes)) {
                 // FIXME: Load html page with an error if parsing failed.

Userland/Libraries/LibWeb/Fetch/Body.cpp
@@ -31,7 +31,7 @@ bool BodyMixin::is_unusable() const
 {
     // An object including the Body interface mixin is said to be unusable if its body is non-null and its body’s stream is disturbed or locked.
     auto const& body = body_impl();
-    return body.has_value() && (body->stream()->is_disturbed() || body->stream()->is_locked());
+    return body && (body->stream()->is_disturbed() || body->stream()->is_locked());
 }
 
 // https://fetch.spec.whatwg.org/#dom-body-body
@@ -39,7 +39,7 @@ JS::GCPtr<Streams::ReadableStream> BodyMixin::body() const
 {
     // The body getter steps are to return null if this’s body is null; otherwise this’s body’s stream.
     auto const& body = body_impl();
-    return body.has_value() ? body->stream().ptr() : nullptr;
+    return body ? body->stream().ptr() : nullptr;
 }
 
 // https://fetch.spec.whatwg.org/#dom-body-bodyused
@@ -47,7 +47,7 @@ bool BodyMixin::body_used() const
 {
     // The bodyUsed getter steps are to return true if this’s body is non-null and this’s body’s stream is disturbed; otherwise false.
     auto const& body = body_impl();
-    return body.has_value() && body->stream()->is_disturbed();
+    return body && body->stream()->is_disturbed();
 }
 
 // https://fetch.spec.whatwg.org/#dom-body-arraybuffer
@@ -197,7 +197,7 @@ WebIDL::ExceptionOr<JS::NonnullGCPtr<JS::Promise>> consume_body(JS::Realm& realm
 
     // 5. If object’s body is null, then run successSteps with an empty byte sequence.
     auto const& body = object.body_impl();
-    if (!body.has_value()) {
+    if (!body) {
         success_steps(ByteBuffer {});
     }
     // 6. Otherwise, fully read object’s body given successSteps, errorSteps, and object’s relevant global object.

Userland/Libraries/LibWeb/Fetch/Body.h
@@ -27,8 +27,8 @@ public:
     virtual ~BodyMixin();
 
     virtual ErrorOr<Optional<MimeSniff::MimeType>> mime_type_impl() const = 0;
-    virtual Optional<Infrastructure::Body&> body_impl() = 0;
-    virtual Optional<Infrastructure::Body const&> body_impl() const = 0;
+    virtual JS::GCPtr<Infrastructure::Body> body_impl() = 0;
+    virtual JS::GCPtr<Infrastructure::Body const> body_impl() const = 0;
     virtual Bindings::PlatformObject& as_platform_object() = 0;
     virtual Bindings::PlatformObject const& as_platform_object() const = 0;
 

Userland/Libraries/LibWeb/Fetch/BodyInit.cpp
@@ -136,7 +136,7 @@ WebIDL::ExceptionOr<Infrastructure::BodyWithType> extract_body(JS::Realm& realm,
     // FIXME: 12. If action is non-null, then run these steps in parallel:
 
     // 13. Let body be a body whose stream is stream, source is source, and length is length.
-    auto body = Infrastructure::Body { JS::make_handle(*stream), move(source), move(length) };
+    auto body = Infrastructure::Body::create(vm, *stream, move(source), move(length));
 
     // 14. Return (body, type).
     return Infrastructure::BodyWithType { .body = move(body), .type = move(type) };

Userland/Libraries/LibWeb/Fetch/FetchMethod.cpp
@@ -165,7 +165,7 @@ void abort_fetch(JS::Realm& realm, WebIDL::Promise const& promise, JS::NonnullGC
     WebIDL::reject_promise(realm, promise, error);
 
     // 2. If request’s body is non-null and is readable, then cancel request’s body with error.
-    if (auto* body = request->body().get_pointer<Infrastructure::Body>(); body != nullptr && body->stream()->is_readable()) {
+    if (auto* body = request->body().get_pointer<JS::NonnullGCPtr<Infrastructure::Body>>(); body != nullptr && (*body)->stream()->is_readable()) {
         // TODO: Implement cancelling streams
         (void)error;
     }
@@ -178,7 +178,7 @@ void abort_fetch(JS::Realm& realm, WebIDL::Promise const& promise, JS::NonnullGC
     auto response = response_object->response();
 
     // 5. If response’s body is non-null and is readable, then error response’s body with error.
-    if (response->body().has_value()) {
+    if (response->body()) {
         auto stream = response->body()->stream();
         if (stream->is_readable()) {
             // TODO: Implement erroring streams

Userland/Libraries/LibWeb/Fetch/Fetching/Fetching.cpp
@@ -498,7 +498,7 @@ WebIDL::ExceptionOr<Optional<JS::NonnullGCPtr<PendingResponse>>> main_fetch(JS::
                 };
 
                 // 2. If response’s body is null, then run processBodyError and abort these steps.
-                if (!response->body().has_value()) {
+                if (!response->body()) {
                     process_body_error({});
                     return;
                 }
@@ -644,7 +644,7 @@ WebIDL::ExceptionOr<void> fetch_response_handover(JS::Realm& realm, Infrastructu
     auto internal_response = response.is_network_error() ? JS::NonnullGCPtr { response } : response.unsafe_response();
 
     // 6. If internalResponse’s body is null, then run processResponseEndOfBody.
-    if (!internal_response->body().has_value()) {
+    if (!internal_response->body()) {
         process_response_end_of_body();
     }
     // 7. Otherwise:
@@ -672,7 +672,7 @@ WebIDL::ExceptionOr<void> fetch_response_handover(JS::Realm& realm, Infrastructu
 
         // 3. If internalResponse's body is null, then queue a fetch task to run processBody given null, with
         //    fetchParams’s task destination.
-        if (!internal_response->body().has_value()) {
+        if (!internal_response->body()) {
             Infrastructure::queue_fetch_task(task_destination, [process_body = move(process_body)]() {
                 process_body({});
             });
@@ -1118,7 +1118,7 @@ WebIDL::ExceptionOr<Optional<JS::NonnullGCPtr<PendingResponse>>> http_redirect_f
     //     return a network error.
     if (actual_response->status() != 303
         && !request->body().has<Empty>()
-        && request->body().get<Infrastructure::Body>().source().has<Empty>()) {
+        && request->body().get<JS::NonnullGCPtr<Infrastructure::Body>>()->source().has<Empty>()) {
         return PendingResponse::create(vm, request, Infrastructure::Response::network_error(vm, "Request has body but no body source"sv));
     }
 
@@ -1160,7 +1160,7 @@ WebIDL::ExceptionOr<Optional<JS::NonnullGCPtr<PendingResponse>>> http_redirect_f
     //     request’s body’s source.
     // NOTE: request’s body’s source’s nullity has already been checked.
     if (!request->body().has<Empty>()) {
-        auto const& source = request->body().get<Infrastructure::Body>().source();
+        auto const& source = request->body().get<JS::NonnullGCPtr<Infrastructure::Body>>()->source();
         // NOTE: BodyInitOrReadableBytes is a superset of Body::SourceType
         auto converted_source = source.has<ByteBuffer>()
             ? BodyInitOrReadableBytes { source.get<ByteBuffer>() }
@@ -1292,8 +1292,8 @@ WebIDL::ExceptionOr<JS::NonnullGCPtr<PendingResponse>> http_network_or_cache_fet
             include_credentials = IncludeCredentials::No;
 
         // 5. Let contentLength be httpRequest’s body’s length, if httpRequest’s body is non-null; otherwise null.
-        auto content_length = http_request->body().has<Infrastructure::Body>()
-            ? http_request->body().get<Infrastructure::Body>().length()
+        auto content_length = http_request->body().has<JS::NonnullGCPtr<Infrastructure::Body>>()
+            ? http_request->body().get<JS::NonnullGCPtr<Infrastructure::Body>>()->length()
             : Optional<u64> {};
 
         // 6. Let contentLengthHeaderValue be null.
@@ -1580,13 +1580,13 @@ WebIDL::ExceptionOr<JS::NonnullGCPtr<PendingResponse>> http_network_or_cache_fet
             // 2. If request’s body is non-null, then:
             if (!request->body().has<Empty>()) {
                 // 1. If request’s body’s source is null, then return a network error.
-                if (request->body().get<Infrastructure::Body>().source().has<Empty>()) {
+                if (request->body().get<JS::NonnullGCPtr<Infrastructure::Body>>()->source().has<Empty>()) {
                     returned_pending_response->resolve(Infrastructure::Response::network_error(vm, "Request has body but no body source"_string));
                     return;
                 }
 
                 // 2. Set request’s body to the body of the result of safely extracting request’s body’s source.
-                auto const& source = request->body().get<Infrastructure::Body>().source();
+                auto const& source = request->body().get<JS::NonnullGCPtr<Infrastructure::Body>>()->source();
                 // NOTE: BodyInitOrReadableBytes is a superset of Body::SourceType
                 auto converted_source = source.has<ByteBuffer>()
                     ? BodyInitOrReadableBytes { source.get<ByteBuffer>() }
@@ -1657,7 +1657,7 @@ WebIDL::ExceptionOr<JS::NonnullGCPtr<PendingResponse>> http_network_or_cache_fet
                 // - isNewConnectionFetch is false
                 && is_new_connection_fetch == IsNewConnectionFetch::No
                 // - request’s body is null, or request’s body is non-null and request’s body’s source is non-null
-                && (request->body().has<Empty>() || !request->body().get<Infrastructure::Body>().source().has<Empty>())
+                && (request->body().has<Empty>() || !request->body().get<JS::NonnullGCPtr<Infrastructure::Body>>()->source().has<Empty>())
                 // then:
             ) {
                 // 1. If fetchParams is canceled, then return the appropriate network error for fetchParams.
@@ -1738,8 +1738,8 @@ WebIDL::ExceptionOr<JS::NonnullGCPtr<PendingResponse>> nonstandard_resource_load
     load_request.set_method(DeprecatedString::copy(request->method()));
     for (auto const& header : *request->header_list())
         load_request.set_header(DeprecatedString::copy(header.name), DeprecatedString::copy(header.value));
-    if (auto const* body = request->body().get_pointer<Infrastructure::Body>()) {
-        TRY(body->source().visit(
+    if (auto const* body = request->body().get_pointer<JS::NonnullGCPtr<Infrastructure::Body>>()) {
+        TRY((*body)->source().visit(
             [&](ByteBuffer const& byte_buffer) -> WebIDL::ExceptionOr<void> {
                 load_request.set_body(TRY_OR_THROW_OOM(vm, ByteBuffer::copy(byte_buffer)));
                 return {};

Userland/Libraries/LibWeb/Fetch/Infrastructure/HTTP/Bodies.cpp
@@ -13,20 +13,36 @@
 
 namespace Web::Fetch::Infrastructure {
 
-Body::Body(JS::Handle<Streams::ReadableStream> stream)
+JS::NonnullGCPtr<Body> Body::create(JS::VM& vm, JS::NonnullGCPtr<Streams::ReadableStream> stream)
+{
+    return vm.heap().allocate_without_realm<Body>(stream);
+}
+
+JS::NonnullGCPtr<Body> Body::create(JS::VM& vm, JS::NonnullGCPtr<Streams::ReadableStream> stream, SourceType source, Optional<u64> length)
+{
+    return vm.heap().allocate_without_realm<Body>(stream, source, length);
+}
+
+Body::Body(JS::NonnullGCPtr<Streams::ReadableStream> stream)
     : m_stream(move(stream))
 {
 }
 
-Body::Body(JS::Handle<Streams::ReadableStream> stream, SourceType source, Optional<u64> length)
+Body::Body(JS::NonnullGCPtr<Streams::ReadableStream> stream, SourceType source, Optional<u64> length)
     : m_stream(move(stream))
     , m_source(move(source))
     , m_length(move(length))
 {
 }
 
+void Body::visit_edges(Cell::Visitor& visitor)
+{
+    Base::visit_edges(visitor);
+    visitor.visit(m_stream);
+}
+
 // https://fetch.spec.whatwg.org/#concept-body-clone
-Body Body::clone(JS::Realm& realm) const
+JS::NonnullGCPtr<Body> Body::clone(JS::Realm& realm) const
 {
     // To clone a body body, run these steps:
     // FIXME: 1. Let « out1, out2 » be the result of teeing body’s stream.
@@ -34,7 +50,7 @@ Body Body::clone(JS::Realm& realm) const
     auto out2 = realm.heap().allocate<Streams::ReadableStream>(realm, realm);
 
     // 3. Return a body whose stream is out2 and other members are copied from body.
-    return Body { JS::make_handle(out2), m_source, m_length };
+    return Body::create(realm.vm(), out2, m_source, m_length);
 }
 
 // https://fetch.spec.whatwg.org/#body-fully-read
@@ -80,7 +96,7 @@ WebIDL::ExceptionOr<void> Body::fully_read(JS::Realm& realm, Web::Fetch::Infrast
 }
 
 // https://fetch.spec.whatwg.org/#byte-sequence-as-a-body
-WebIDL::ExceptionOr<Body> byte_sequence_as_body(JS::Realm& realm, ReadonlyBytes bytes)
+WebIDL::ExceptionOr<JS::NonnullGCPtr<Body>> byte_sequence_as_body(JS::Realm& realm, ReadonlyBytes bytes)
 {
     // To get a byte sequence bytes as a body, return the body of the result of safely extracting bytes.
     auto [body, _] = TRY(safely_extract_body(realm, bytes));

commit 305743d3020c0124842f1ac836782204ca015b97

LibWeb: Fix `Request::visit_edges` to actually visit `m_body`
This fixes bug introduced in bdd3a16b16c99bd6342ad34b629b89a510942699
that `m_body` is not visited because `BodyType` variant has
`JS::NonnullGCPtr` instead of `JS::GCPtr`.

Userland/Libraries/LibWeb/Fetch/Infrastructure/HTTP/Requests.cpp
@@ -24,7 +24,7 @@ void Request::visit_edges(JS::Cell::Visitor& visitor)
     visitor.visit(m_header_list);
     visitor.visit(m_client);
     m_body.visit(
-        [&](JS::GCPtr<Body>& body) { visitor.visit(body); },
+        [&](JS::NonnullGCPtr<Body>& body) { visitor.visit(body); },
         [](auto&) {});
     m_reserved_client.visit(
         [&](JS::GCPtr<HTML::EnvironmentSettingsObject> const& value) { visitor.visit(value); },