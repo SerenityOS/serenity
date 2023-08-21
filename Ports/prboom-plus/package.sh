70d2d2b98a3f11bf3339463b11707a9066118d27
Tim Ledbetter <timledbetter@gmail.com>
2023-08-21 18:29:19 +0100

    Ports/prboom-plus: Update formatting to be consistent with other ports

--- Ports/prboom-plus/package.sh
+++ Ports/prboom-plus/package.sh
@@ -2,21 +2,29 @@
 port='prboom-plus'
 version='2.6.2'
 useconfigure='true'
-squashed_version=$(echo "${version}" | tr -d '.')
+squashed_version="$(echo ${version} | tr -d '.')"
 files=(
     "https://github.com/coelckers/prboom-plus/archive/refs/tags/v${version}.tar.gz 5cfeec96fbfe4fc3bd5dbc2b8d581ff5f6617dd74b2799680ba5b1e2e38c4aff"
     "https://github.com/coelckers/prboom-plus/releases/download/v${version}/prboom-plus-${squashed_version}-w32.zip 20313e00d8841a618e23e7c671d65870194bee634468fecd2f3697ac05f21476"
 )
 workdir="prboom-plus-${version}/prboom2"
-depends=("glu" "libmad" "libvorbis" "SDL2" "SDL2_image" "SDL2_mixer" "SDL2_net")
+depends=(
+    'glu'
+    'libmad'
+    'libvorbis'
+    'SDL2'
+    'SDL2_image'
+    'SDL2_mixer'
+    'SDL2_net'
+)
 configopts=(
     "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
-    "-DWITH_ALSA=OFF"
-    "-DWITH_DUMB=OFF"
-    "-DWITH_FLUIDSYNTH=OFF"
-    "-DWITH_PCRE=OFF"
-    "-DWITH_PORTMIDI=OFF"
-    "-Wno-dev"
+    '-DWITH_ALSA=OFF'
+    '-DWITH_DUMB=OFF'
+    '-DWITH_FLUIDSYNTH=OFF'
+    '-DWITH_PCRE=OFF'
+    '-DWITH_PORTMIDI=OFF'
+    '-Wno-dev'
 )
 
 launcher_name='PrBoom+'
