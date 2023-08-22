9e5d430fbd29a928dc62c7414c5cbece15b921a1
Tim Ledbetter <timledbetter@gmail.com>
2023-08-22 17:54:17 +0100

    Ports/SDL_sound: Update formatting to be consistent with other ports

--- Ports/SDL_sound/package.sh
+++ Ports/SDL_sound/package.sh
@@ -3,13 +3,18 @@ port='SDL_sound'
 version='1.0.3'
 useconfigure='true'
 use_fresh_config_sub='true'
-depends=("sdl12-compat" "libmikmod")
+depends=(
+    'libmikmod'
+    'sdl12-compat'
+)
 files=(
     "https://www.icculus.org/SDL_sound/downloads/${port}-${version}.tar.gz 3999fd0bbb485289a52be14b2f68b571cb84e380cc43387eadf778f64c79e6df"
 )
 configopts=(
-    "--with-sdl-prefix=${SERENITY_INSTALL_ROOT}/usr/local"
-    "--enable-ogg=no"
-    "--enable-modplug=no"
+    '--enable-modplug=no'
+    '--enable-ogg=no'
+    "--with-sdl-prefix=${SERENITY_INSTALL_ROOT}/usr/local"    
 )
-makeopts=("LDFLAGS=-lm")
+makeopts=(
+    'LDFLAGS=-lm'
+)
