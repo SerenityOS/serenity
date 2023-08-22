abd1ad77f232712ebdcfa3acfbe11a0e1b9549ba
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2023-08-22 18:33:20 +0200

    LibWeb: Use `navigate()` instead of `did_set_location_href` in Location

--- Userland/Libraries/LibWeb/HTML/Location.h
+++ Userland/Libraries/LibWeb/HTML/Location.h
@@ -51,7 +51,7 @@ public:
 
     void replace(String const& url) const;
     void reload() const;
-    WebIDL::ExceptionOr<void> assign(String const& url) const;
+    WebIDL::ExceptionOr<void> assign(String const& url);
 
     virtual JS::ThrowCompletionOr<JS::Object*> internal_get_prototype_of() const override;
     virtual JS::ThrowCompletionOr<bool> internal_set_prototype_of(Object* prototype) override;
