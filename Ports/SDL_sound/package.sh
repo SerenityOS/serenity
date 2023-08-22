4eb414bfb2b8a4e9c7d93193611bf5616bf7cd61
Tim Ledbetter <timledbetter@gmail.com>
2023-08-22 17:54:33 +0100

    Ports/SDL_sound: Specify which optional dependencies to include
    This fixes an issue where building `SDL_sound` with the `libphysfs`
    port installed would cause the build to fail.
    
    Ogg support has also been enabled. This allows playback of the sound
    effects in GLTron.
    
    Both FLAC and `libmodplug` support are currently disabled, even
    though the build succeeds with them enabled, as there is currently no
    way to test whether they would work or not.

--- Ports/SDL_sound/package.sh
+++ Ports/SDL_sound/package.sh
@@ -5,15 +5,19 @@ useconfigure='true'
 use_fresh_config_sub='true'
 depends=(
     'libmikmod'
+    'libogg'
     'sdl12-compat'
 )
 files=(
     "https://www.icculus.org/SDL_sound/downloads/${port}-${version}.tar.gz 3999fd0bbb485289a52be14b2f68b571cb84e380cc43387eadf778f64c79e6df"
 )
 configopts=(
+    '--enable-flac=no'
     '--enable-modplug=no'
-    '--enable-ogg=no'
-    "--with-sdl-prefix=${SERENITY_INSTALL_ROOT}/usr/local"    
+    '--enable-physfs=no'
+    '--enable-smpeg=no'
+    '--enable-speex=no'
+    "--with-sdl-prefix=${SERENITY_INSTALL_ROOT}/usr/local"
 )
 makeopts=(
     'LDFLAGS=-lm'
