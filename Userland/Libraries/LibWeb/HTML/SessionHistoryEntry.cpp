a45f04f000ec90813157b3d23289e4de46e6d73a
Andrew Kaster <akaster@serenityos.org>
2023-08-23 10:26:47 -0600

    LibWeb: Add fields to SessionHistoryEntry required by Navigation API

--- Userland/Libraries/LibWeb/HTML/SessionHistoryEntry.cpp
+++ Userland/Libraries/LibWeb/HTML/SessionHistoryEntry.cpp
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <LibWeb/Crypto/Crypto.h>
 #include <LibWeb/HTML/BrowsingContext.h>
 #include <LibWeb/HTML/DocumentState.h>
 #include <LibWeb/HTML/SessionHistoryEntry.h>
@@ -15,6 +16,14 @@ void SessionHistoryEntry::visit_edges(Cell::Visitor& visitor)
     Base::visit_edges(visitor);
     visitor.visit(document_state);
     visitor.visit(original_source_browsing_context);
+}
+
+SessionHistoryEntry::SessionHistoryEntry()
+    : classic_history_api_state(MUST(structured_serialize_for_storage(vm(), JS::js_null())))
+    , navigation_api_state(MUST(structured_serialize_for_storage(vm(), JS::js_undefined())))
+    , navigation_api_key(MUST(Crypto::generate_random_uuid()))
+    , navigation_api_id(MUST(Crypto::generate_random_uuid()))
+{
 }
 
 }
