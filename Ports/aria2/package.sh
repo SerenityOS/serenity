1909c25e68c8f5cc84b529d324b577bd502bfc50
Tim Ledbetter <timledbetter@gmail.com>
2023-08-23 20:44:07 +0100

    Ports/aria2: Update formatting to be consistent with other ports

--- Ports/aria2/package.sh
+++ Ports/aria2/package.sh
@@ -5,17 +5,19 @@ files=(
     "https://github.com/aria2/aria2/releases/download/release-${version}/aria2-${version}.tar.xz 58d1e7608c12404f0229a3d9a4953d0d00c18040504498b483305bcb3de907a5"
 )
 depends=(
-    "libssh2"
-    "libxml2"
-    "openssl"
-    "zlib"
-    "libuv"
+    'libssh2'
+    'libuv'
+    'libxml2'
+    'openssl'
+    'zlib'
 )
 useconfigure='true'
 use_fresh_config_sub='true'
-config_sub_paths+=("deps/wslay/config.sub")
+config_sub_paths+=(
+    'deps/wslay/config.sub'
+)
 configopts+=(
-    "--with-libuv"
+    '--with-libuv'
     "--with-sysroot=${SERENITY_INSTALL_ROOT}"
     '--without-libcares'
     '--without-sqlite3'
