6678a3ad00ac81d93c5f906933669f2483f6f296
Tim Ledbetter <timledbetter@gmail.com>
2023-08-23 19:48:27 +0100

    Ports/libssh2: Prefer `openssl` over `libgcrypt` as the crypto backend
    Previously,`openssl` would be used as the crypto backend for `libssh2`
    if the `openssl` port was installed and the `libgcrypt` dependency
    would be ignored.
    
    With this change we install `openssl` as a dependency and explicitly
    specify that it should be used as the crypto backend. We also add
    `zlib` as an explicit dependency and specify that `zlib` compression
    should be used.

--- Ports/libssh2/package.sh
+++ Ports/libssh2/package.sh
@@ -5,8 +5,16 @@ useconfigure=true
 files=(
     "https://www.libssh2.org/download/libssh2-${version}.tar.gz 2d64e90f3ded394b91d3a2e774ca203a4179f69aebee03003e5a6fa621e41d51"
 )
-depends=("libgcrypt")
-configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt" "-GNinja")
+depends=(
+    'openssl'
+    'zlib'
+)
+configopts=(
+    "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt" 
+    '-DCRYPTO_BACKEND=OpenSSL'
+    '-DENABLE_ZLIB_COMPRESSION=ON'
+    '-GNinja'
+)
 
 configure() {
     run cmake "${configopts[@]}" .
