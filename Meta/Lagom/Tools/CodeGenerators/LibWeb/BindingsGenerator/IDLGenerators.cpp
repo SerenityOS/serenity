e07edb2c4dd6a512019d86f430c2f26f9f7a4f48
Andrew Kaster <akaster@serenityos.org>
2023-08-23 10:53:11 -0600

    LibWeb: Add NavigationCurrentEntryChangeEvent, fired when navigating
    This event will be fired by Navigation when changing the current
    NavigationHistoryEntry.

--- Meta/Lagom/Tools/CodeGenerators/LibWeb/BindingsGenerator/IDLGenerators.cpp
+++ Meta/Lagom/Tools/CodeGenerators/LibWeb/BindingsGenerator/IDLGenerators.cpp
@@ -44,6 +44,7 @@ static bool is_platform_object(Type const& type)
         "Module"sv,
         "MutationRecord"sv,
         "NamedNodeMap"sv,
+        "NavigationHistoryEntry"sv,
         "Node"sv,
         "Path2D"sv,
         "PerformanceEntry"sv,
---
+++ Userland/Libraries/LibWeb/HTML/NavigationCurrentEntryChangeEvent.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2023, Andrew Kaster <akaster@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibWeb/DOM/Event.h>
+#include <LibWeb/HTML/NavigationType.h>
+
+namespace Web::HTML {
+
+struct NavigationCurrentEntryChangeEventInit : public DOM::EventInit {
+    Optional<Bindings::NavigationType> navigation_type = {};
+    JS::GCPtr<NavigationHistoryEntry> from;
+};
+
+class NavigationCurrentEntryChangeEvent final : public DOM::Event {
+    WEB_PLATFORM_OBJECT(NavigationCurrentEntryChangeEvent, DOM::Event);
+
+public:
+    [[nodiscard]] static JS::NonnullGCPtr<NavigationCurrentEntryChangeEvent> construct_impl(JS::Realm&, FlyString const& event_name, NavigationCurrentEntryChangeEventInit const&);
+
+    virtual ~NavigationCurrentEntryChangeEvent() override;
+
+    Optional<Bindings::NavigationType> const& navigation_type() const { return m_navigation_type; }
+    JS::NonnullGCPtr<NavigationHistoryEntry> from() const { return m_from; }
+
+private:
+    NavigationCurrentEntryChangeEvent(JS::Realm&, FlyString const& event_name, NavigationCurrentEntryChangeEventInit const& event_init);
+
+    virtual void initialize(JS::Realm&) override;
+    virtual void visit_edges(Cell::Visitor&) override;
+
+    Optional<Bindings::NavigationType> m_navigation_type;
+    JS::NonnullGCPtr<NavigationHistoryEntry> m_from;
+};
+
+}
