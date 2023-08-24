eb0f863efc02c2de9b4a35e065b9410749a4a4f5
Tim Ledbetter <timledbetter@gmail.com>
2023-08-24 17:45:36 +0100

    Ports/gnuplot: Update to version 5.4.8
    We also no longer build the tutorial, as that directory no longer
    exists.

--- Ports/gnuplot/package.sh
+++ Ports/gnuplot/package.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=gnuplot
-version=5.2.8
+version='5.4.8'
 useconfigure=true
 # Note: gnuplot's source code is hosted on SourceForge, but using the GitHub mirror makes downloading a versioned .tar.gz easier.
 files=(
-    "https://github.com/gnuplot/gnuplot/archive/${version}.tar.gz b55f591e2ad9d01ffca821f2f91ce781b481bb5dd602ce5188bfad3140f44ac0"
+    "https://github.com/gnuplot/gnuplot/archive/${version}.tar.gz 2b0c1841640b2e33f8421ac83cd91d972d8b0c6acf9753f16385d5eec8a61a73"
 )
 configopts=("--prefix=${SERENITY_INSTALL_ROOT}/usr/local" "--with-readline=builtin" "--without-latex")
 
