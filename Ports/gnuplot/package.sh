17bd200946dda870e4b2ed52bb05fc9119815717
Tim Ledbetter <timledbetter@gmail.com>
2023-08-24 17:45:46 +0100

    Ports/gnuplot: Explicitly specify which optional dependencies to enable
    This change explicitly enables support for `libgd` and the lua
    terminal in `gnuplot`, while explicitly disabling `cairo` terminals,
    which don't work with our `cairo` port. Adding `libgd` as a
    dependency requires us to manually link against the dependencies of
    `libgd`, as this is not done automatically by the configure script.
    
    This fixes an issue where building `gnuplot` would fail if `libgd`
    was already installed.

--- Ports/gnuplot/package.sh
+++ Ports/gnuplot/package.sh
@@ -6,7 +6,18 @@ useconfigure=true
 files=(
     "https://github.com/gnuplot/gnuplot/archive/${version}.tar.gz 2b0c1841640b2e33f8421ac83cd91d972d8b0c6acf9753f16385d5eec8a61a73"
 )
-configopts=("--prefix=${SERENITY_INSTALL_ROOT}/usr/local" "--with-readline=builtin" "--without-latex")
+depends=(
+    'libgd'
+    'lua'
+)
+configopts=(
+    "--prefix=${SERENITY_INSTALL_ROOT}/usr/local"
+    '--with-qt=no'
+    "--with-readline=builtin"
+    '--without-cairo'
+    "--without-latex"
+    'libgd_LIBS=-liconv -lfreetype -lfontconfig -lpng'
+)
 
 pre_configure() {
     run ./prepare
