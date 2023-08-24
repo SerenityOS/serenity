d730cd89f57ad672e307388391cc9ddd3b8c910e
Tim Ledbetter <timledbetter@gmail.com>
2023-08-24 17:45:50 +0100

    Ports/gnuplot: Update formatting to be consistent with other ports

--- Ports/gnuplot/package.sh
+++ Ports/gnuplot/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=gnuplot
+port='gnuplot'
 version='5.4.8'
-useconfigure=true
+useconfigure='true'
 # Note: gnuplot's source code is hosted on SourceForge, but using the GitHub mirror makes downloading a versioned .tar.gz easier.
 files=(
     "https://github.com/gnuplot/gnuplot/archive/${version}.tar.gz 2b0c1841640b2e33f8421ac83cd91d972d8b0c6acf9753f16385d5eec8a61a73"
@@ -13,9 +13,9 @@ depends=(
 configopts=(
     "--prefix=${SERENITY_INSTALL_ROOT}/usr/local"
     '--with-qt=no'
-    "--with-readline=builtin"
+    '--with-readline=builtin'
     '--without-cairo'
-    "--without-latex"
+    '--without-latex'
     'libgd_LIBS=-liconv -lfreetype -lfontconfig -lpng'
 )
 
