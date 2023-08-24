7436f4fec29b6d3bd515c774a0e614a751dd5b64
Sam Atkins <atkinssj@serenityos.org>
2023-08-24 11:01:09 +0100

    LibWebView: Add a standard "look like the OS" style sheet
    It's quite common to want the web-view to match the native OS widgets,
    so let's make that easy to do.

--- Userland/Libraries/LibWebView/OutOfProcessWebView.h
+++ Userland/Libraries/LibWebView/OutOfProcessWebView.h
@@ -55,6 +55,9 @@ public:
     void set_content_scales_to_viewport(bool);
 
     void set_user_style_sheet(String source);
+    // Load Native.css as the User style sheet, which attempts to make WebView content look as close to
+    // native GUI::Widgets as possible.
+    void use_native_user_style_sheet();
 
 private:
     OutOfProcessWebView();
