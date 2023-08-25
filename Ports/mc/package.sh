e367931a94860921e00b7799321e019837c6b47e
Tim Ledbetter <timledbetter@gmail.com>
2023-08-25 20:07:03 +0100

    Ports/mc: Update formatting to be consistent with other ports

--- Ports/mc/package.sh
+++ Ports/mc/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=mc
-version=4.8.29
-useconfigure=true
+port='mc'
+version='4.8.29'
+useconfigure='true'
 files=(
     "http://ftp.midnight-commander.org/mc-${version}.tar.xz 01d8a3b94f58180cca5bf17257b5078d1fd6fd27a9b5c0e970ec767549540ad4"
 )
@@ -13,12 +13,14 @@ depends=(
 )
 configopts=(
     "--with-sysroot=${SERENITY_INSTALL_ROOT}"
-    "--disable-largefile"
-    "--disable-vfs"
-    "--without-edit"
-    "--without-x"
-    "--with-homedir"
-    "--with-screen=ncurses"
+    '--disable-largefile'
+    '--disable-vfs'
+    '--without-edit'
+    '--without-x'
+    '--with-homedir'
+    '--with-screen=ncurses'
 )
-use_fresh_config_sub=true
-config_sub_paths=("config/config.sub")
+use_fresh_config_sub='true'
+config_sub_paths=(
+    'config/config.sub'
+)
