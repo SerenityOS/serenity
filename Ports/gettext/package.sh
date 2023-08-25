b91cd56a53cc28c04ccccf43e92180fb6bfe9279
Tim Ledbetter <timledbetter@gmail.com>
2023-08-25 18:57:04 +0100

    Ports/gettext: Update formatting to be consistent with other ports

--- Ports/gettext/package.sh
+++ Ports/gettext/package.sh
@@ -5,9 +5,14 @@ useconfigure='true'
 files=(
     "https://ftpmirror.gnu.org/gettext/gettext-${version}.tar.gz e8c3650e1d8cee875c4f355642382c1df83058bd5a11ee8555c0cf276d646d45"
 )
-depends=("libiconv")
+depends=(
+    'libiconv'
+)
 use_fresh_config_sub='true'
-config_sub_paths=("build-aux/config.sub" "libtextstyle/build-aux/config.sub")
+config_sub_paths=(
+    'build-aux/config.sub' 
+    'libtextstyle/build-aux/config.sub'
+)
 configopts=(
     '--disable-curses'    
 )
