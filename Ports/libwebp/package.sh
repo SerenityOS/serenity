db2701f2e21a08b336eb051c81706b5962656284
Liav A <liavalb@gmail.com>
2023-08-26 17:51:39 +0300

    Ports: Add libwebp
    This library includes webp converter to other formats as well and could
    be used as reference implementation for performance improvements.

---
+++ Ports/libwebp/package.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port='libwebp'
+version='1.3.1'
+useconfigure='true'
+files=(
+    "https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-${version}.tar.gz b3779627c2dfd31e3d8c4485962c2efe17785ef975e2be5c8c0c9e6cd3c4ef66"
+)
+depends=(
+    'libjpeg'
+    'libpng'
+    'libtiff'
+)
+configopts=(
+    "--with-pnglibdir=${SERENITY_INSTALL_ROOT}/usr/local/include"
+)
