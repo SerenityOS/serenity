519459245b1cd68ab74265252eae45ffbef0e692
Liav A <liavalb@gmail.com>
2023-08-26 20:46:09 +0300

    Ports: Add libtasn1
    This port will be used for the upcoming GnuTLS port.

---
+++ Ports/libtasn1/package.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port='libtasn1'
+version='4.19.0'
+files=(
+    "https://ftp.gnu.org/gnu/libtasn1/libtasn1-${version}.tar.gz 1613f0ac1cf484d6ec0ce3b8c06d56263cc7242f1c23b30d82d23de345a63f7a"
+)
+useconfigure='true'
