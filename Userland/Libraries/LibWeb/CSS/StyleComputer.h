240ec9aeed6e93de704423d9382c4aa5c9d2c49c
Sam Atkins <atkinssj@serenityos.org>
2023-08-26 12:08:07 +0100

    LibWeb: Treat invalid StyleValues that included var() as `unset`
    This means StyleComputer::resolve_unresolved_style_value() always
    returns a value, so we can change its return type.
    
    However, it does still return an UnresolvedStyleValue sometimes, so we
    can't remove those checks from the user code.

--- Userland/Libraries/LibWeb/CSS/StyleComputer.h
+++ Userland/Libraries/LibWeb/CSS/StyleComputer.h
@@ -153,7 +153,7 @@ private:
 
     void compute_defaulted_property_value(StyleProperties&, DOM::Element const*, CSS::PropertyID, Optional<CSS::Selector::PseudoElement>) const;
 
-    RefPtr<StyleValue> resolve_unresolved_style_value(DOM::Element&, Optional<CSS::Selector::PseudoElement>, PropertyID, UnresolvedStyleValue const&) const;
+    NonnullRefPtr<StyleValue> resolve_unresolved_style_value(DOM::Element&, Optional<CSS::Selector::PseudoElement>, PropertyID, UnresolvedStyleValue const&) const;
     bool expand_variables(DOM::Element&, Optional<CSS::Selector::PseudoElement>, StringView property_name, HashMap<FlyString, NonnullRefPtr<PropertyDependencyNode>>& dependencies, Parser::TokenStream<Parser::ComponentValue>& source, Vector<Parser::ComponentValue>& dest) const;
     bool expand_unresolved_values(DOM::Element&, StringView property_name, Parser::TokenStream<Parser::ComponentValue>& source, Vector<Parser::ComponentValue>& dest) const;
 
