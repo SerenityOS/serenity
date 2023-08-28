2a86bd8c919f2dc20665eb2cda5bf739aa66a153
Andreas Kling <kling@serenityos.org>
2023-08-28 09:14:36 +0200

    LibWeb: Fire the fail callback on SharedImageRequest when they fail
    This fixes an issue where failed image loads would delay the document
    load event forever.

--- Userland/Libraries/LibWeb/HTML/SharedImageRequest.cpp
+++ Userland/Libraries/LibWeb/HTML/SharedImageRequest.cpp
@@ -84,6 +84,8 @@ void SharedImageRequest::fetch_image(JS::Realm& realm, JS::NonnullGCPtr<Fetch::I
 
         if (response->body())
             response->body()->fully_read(realm, move(process_body), move(process_body_error), JS::NonnullGCPtr { realm.global_object() }).release_value_but_fixme_should_propagate_errors();
+        else
+            handle_failed_fetch();
     };
 
     m_state = State::Fetching;
