f642b54b5ef073ac7ad74773f2daf45c88c63208
Nathan Ell <nathanjamesell@gmail.com>
2023-08-30 12:43:22 -0600

    Ports/dos2unix: Update to 7.5.1
    Version 7.5.1 fixes a problem of converting a symbolic link target that
    is on another filesystem, and updates Chinese and Serbian translations.

--- Ports/dos2unix/package.sh
+++ Ports/dos2unix/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=dos2unix
-version=7.5.0
+version=7.5.1
 workdir="${port}-${version}"
 files=(
-    "https://waterlan.home.xs4all.nl/dos2unix/dos2unix-${version}.tar.gz 7a3b01d01e214d62c2b3e04c3a92e0ddc728a385566e4c0356efa66fd6eb95af"
+    "https://waterlan.home.xs4all.nl/dos2unix/dos2unix-${version}.tar.gz da07788bb2e029b0d63f6471d166f68528acd8da2cf14823a188e8a9d5c1fc15"
 )
 depends=("gettext")
