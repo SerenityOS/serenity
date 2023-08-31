3056ff6b11c9a60dd51dfcb83483853eaa404113
circl <circl.lastname@gmail.com>
2023-08-31 19:22:17 +0200

    LibWeb: Add presentational hints to the canvas element
    There is only one, width/height -> aspect-ratio. This brings us
    very slightly closer to spec and triggers a re-layout after
    updating these values from JavaScript, which wasn't the case
    before.

--- Userland/Libraries/LibWeb/HTML/HTMLCanvasElement.h
+++ Userland/Libraries/LibWeb/HTML/HTMLCanvasElement.h
@@ -44,6 +44,8 @@ private:
     virtual void initialize(JS::Realm&) override;
     virtual void visit_edges(Cell::Visitor&) override;
 
+    virtual void apply_presentational_hints(CSS::StyleProperties&) const override;
+
     virtual JS::GCPtr<Layout::Node> create_layout_node(NonnullRefPtr<CSS::StyleProperties>) override;
 
     enum class HasOrCreatedContext {
