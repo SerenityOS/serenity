c56fe70d3a95b205256bd94801c38c008a4bc026
EWouters <6179932+EWouters@users.noreply.github.com>
2023-09-02 00:44:25 +0200

    Ports/rsync: Update rsync to version 3.2.7
    The openssl and lz4 dependencies have been added for the Clang build,
    and have been disabled for the GNU build. The openssl library is not
    detected correctly, and the lz4 library doesn't build on the GNU build.

--- Ports/rsync/package.sh
+++ Ports/rsync/package.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='rsync'
-version='3.1.3'
+version='3.2.7'
 useconfigure='true'
 use_fresh_config_sub='true'
 files=(
-    "https://download.samba.org/pub/rsync/src/rsync-${version}.tar.gz#55cc554efec5fdaad70de921cd5a5eeb6c29a95524c715f3bbf849235b0800c0"
+    "https://download.samba.org/pub/rsync/src/rsync-${version}.tar.gz#4e7d9d3f6ed10878c58c5fb724a67dacf4b6aac7340b13e488fb2dc41346f2bb"
 )
 configopts=(
     "--target=${SERENITY_ARCH}-pc-serenity"
+    '--disable-xxhash'
 )
+if [ "${SERENITY_TOOLCHAIN}" = 'Clang' ]; then
+    depends=(
+        'lz4'
+        'openssl'
+    )
+else
+    configopts+=(
+        '--disable-lz4'
+        '--disable-openssl'
+    )
+fi
