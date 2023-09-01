198c0fa35500a685f1b57f474b481eb6545a65a6
EWouters <6179932+EWouters@users.noreply.github.com>
2023-09-01 21:06:11 +0200

    Ports/libunistring: Update libunistring to version 1.1
    This also fixes the Clang build of gettext by setting the sysroot.

--- Ports/libunistring/package.sh
+++ Ports/libunistring/package.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libunistring'
-version='1.0'
+version='1.1'
 files=(
-    "https://ftpmirror.gnu.org/gnu/libunistring/libunistring-${version}.tar.gz#3c0184c0e492d7c208ce31d25dd1d2c58f0c3ed6cbbe032c5b248cddad318544"
+    "https://ftpmirror.gnu.org/gnu/libunistring/libunistring-${version}.tar.gz#a2252beeec830ac444b9f68d6b38ad883db19919db35b52222cf827c385bdb6a"
 )
 useconfigure='true'
+configopts=(
+    "--with-sysroot=${SERENITY_INSTALL_ROOT}"
+)
