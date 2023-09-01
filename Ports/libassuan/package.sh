8239442341ae83765459c44d9758505c8eb9ec33
EWouters <6179932+EWouters@users.noreply.github.com>
2023-09-01 20:22:38 +0200

    Ports/libassuan: Update libassuan to version 2.5.6
    This also sets the sysroot to to allow it to find libgpg-error and
    libiconv when building with Clang.

--- Ports/libassuan/package.sh
+++ Ports/libassuan/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libassuan'
-version='2.5.5'
+version='2.5.6'
 useconfigure='true'
 use_fresh_config_sub='true'
 config_sub_paths=(
@@ -10,7 +10,7 @@ depends=(
     'libgpg-error'
 )
 files=(
-    "https://gnupg.org/ftp/gcrypt/libassuan/libassuan-${version}.tar.bz2#8e8c2fcc982f9ca67dcbb1d95e2dc746b1739a4668bc20b3a3c5be632edb34e4"
+    "https://gnupg.org/ftp/gcrypt/libassuan/libassuan-${version}.tar.bz2#e9fd27218d5394904e4e39788f9b1742711c3e6b41689a31aa3380bd5aa4f426"
 )
 
 pre_configure() {
@@ -23,5 +23,6 @@ configure() {
         --build="$("${workdir}/build-aux/config.guess")" \
         --disable-static \
         --enable-shared \
-        --with-libgpg-error-prefix="${SERENITY_INSTALL_ROOT}/usr/local"
+        --with-libgpg-error-prefix="${SERENITY_INSTALL_ROOT}/usr/local" \
+        --with-sysroot="${SERENITY_INSTALL_ROOT}"
 }
