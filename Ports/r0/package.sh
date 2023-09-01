0a9e3434560d7191df587b8c263a1f765df1f0cc
EWouters <6179932+EWouters@users.noreply.github.com>
2023-09-02 00:14:19 +0200

    Ports/r0: Update r0 to version 0.9

--- Ports/r0/package.sh
+++ Ports/r0/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='r0'
-version='0.8'
+version='0.9'
 files=(
-    "https://github.com/radareorg/r0/archive/$version.tar.gz#a31722838a21cd3391c41bbb2e63f60552544244362b21f6d9a30d6c24c43bbe"
+    "https://github.com/radareorg/r0/archive/$version.tar.gz#1da914b6f79c72334cd5e7ecbe846bf54914cdf2241dd90b543350794bd12edf"
 )
