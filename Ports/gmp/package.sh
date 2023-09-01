eb50682b1656eb45a793f39e19e6db27b3ae92f3
EWouters <6179932+EWouters@users.noreply.github.com>
2023-09-01 19:46:44 +0200

    Ports/gmp: Update gmp to version 6.3.0
    This also removes the patch as Serenity is now included in the bundled
    `configfsf.sub`.

--- Ports/gmp/package.sh
+++ Ports/gmp/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='gmp'
-version='6.2.1'
+version='6.3.0'
 useconfigure='true'
 files=(
-    "https://ftpmirror.gnu.org/gnu/gmp/gmp-${version}.tar.bz2#eae9326beb4158c386e39a356818031bd28f3124cf915f8c5b1dc4c7a36b4d7c"
+    "https://ftpmirror.gnu.org/gnu/gmp/gmp-${version}.tar.xz#a3c2b80201b89e68616f4ad30bc66aee4927c3ce50e33929ca819d5c43538898"
 )
--- Ports/gmp/patches/ReadMe.md
+++
@@ -1,7 +0,0 @@
-# Patches for gmp on SerenityOS
-
-## `0001-Teach-configfsf.sub-about-serenity.patch`
-
-Teach configfsf.sub about serenity
-
-
