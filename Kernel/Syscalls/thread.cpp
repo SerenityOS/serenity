2b7ec8c70d56cbfb500b02c6250cd2b2218a17d7
Liav A <liavalb@gmail.com>
2023-09-01 15:14:03 +0300

    Kernel: Mark sys$create_thread as not needing the big lock
    This syscall is already safe without using the big process lock so there
    is no need for taking it.

--- Kernel/Syscalls/thread.cpp
+++ Kernel/Syscalls/thread.cpp
@@ -14,7 +14,7 @@ namespace Kernel {
 
 ErrorOr<FlatPtr> Process::sys$create_thread(void* (*entry)(void*), Userspace<Syscall::SC_create_thread_params const*> user_params)
 {
-    VERIFY_PROCESS_BIG_LOCK_ACQUIRED(this);
+    VERIFY_NO_PROCESS_BIG_LOCK(this);
     TRY(require_promise(Pledge::thread));
     auto params = TRY(copy_typed_from_user(user_params));
 
