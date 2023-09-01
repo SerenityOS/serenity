d5eb7bb3692b14d5bc907b2e66a00d649c5f6fb1
EWouters <6179932+EWouters@users.noreply.github.com>
2023-09-01 21:26:43 +0200

    Ports/libxml2: Update libxml2 to version 2.11.5

--- Ports/libxml2/package.sh
+++ Ports/libxml2/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libxml2'
-version='2.9.14'
+version='2.11.5'
 files=(
-    "https://download.gnome.org/sources/libxml2/2.9/libxml2-${version}.tar.xz#60d74a257d1ccec0475e749cba2f21559e48139efba6ff28224357c7c798dfee"
+    "https://download.gnome.org/sources/libxml2/2.11/libxml2-${version}.tar.xz#3727b078c360ec69fa869de14bd6f75d7ee8d36987b071e6928d4720a28df3a6"
 )
 useconfigure='true'
 use_fresh_config_sub='true'
