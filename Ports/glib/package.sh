153993d573453d3fdcbe3fdd199a1f2146ffd834
EWouters <6179932+EWouters@users.noreply.github.com>
2023-09-01 19:44:51 +0200

    Ports/glib: Update glib to version 2.77.2

--- Ports/glib/package.sh
+++ Ports/glib/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='glib'
-version='2.77.0'
+version='2.77.2'
 files=(
-    "https://download.gnome.org/sources/glib/2.77/glib-${version}.tar.xz#1897fd8ad4ebb523c32fabe7508c3b0b039c089661ae1e7917df0956a320ac4d"
+    "https://download.gnome.org/sources/glib/2.77/glib-${version}.tar.xz#16279739e4d30ec47be3e82909f5aeaaa41a8206bae3bead10a23fb2deff02a6"
 )
 useconfigure='true'
 configopts=(
