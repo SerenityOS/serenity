1bf80c9ab3ef6e02e4beceabe1eeef903f91e17c
EWouters <6179932+EWouters@users.noreply.github.com>
2023-09-02 01:27:07 +0200

    Ports/sqlite: Update sqlite to version 3430000

--- Ports/sqlite/package.sh
+++ Ports/sqlite/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='sqlite'
-version='3410200'
+version='3430000'
 files=(
-    "https://www.sqlite.org/2023/sqlite-autoconf-${version}.tar.gz#e98c100dd1da4e30fa460761dab7c0b91a50b785e167f8c57acc46514fae9499"
+    "https://www.sqlite.org/2023/sqlite-autoconf-${version}.tar.gz#49008dbf3afc04d4edc8ecfc34e4ead196973034293c997adad2f63f01762ae1"
 )
 useconfigure='true'
 use_fresh_config_sub='true'
