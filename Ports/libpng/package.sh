a54799f2a57cfe91b227e9e30aa3b6d00efca49d
EWouters <6179932+EWouters@users.noreply.github.com>
2023-09-01 20:57:52 +0200

    Ports/libpng: Update libpng to version 1.6.40

--- Ports/libpng/package.sh
+++ Ports/libpng/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libpng'
-version='1.6.39'
+version='1.6.40'
 useconfigure='true'
 configopts=(
     '--disable-static'
@@ -8,7 +8,7 @@ configopts=(
 )
 use_fresh_config_sub='true'
 files=(
-    "https://download.sourceforge.net/libpng/libpng-${version}.tar.gz#af4fb7f260f839919e5958e5ab01a275d4fe436d45442a36ee62f73e5beb75ba"
+    "https://download.sourceforge.net/libpng/libpng-${version}.tar.gz#8f720b363aa08683c9bf2a563236f45313af2c55d542b5481ae17dd8d183bb42"
 )
 depends=(
     'zlib'
