6e926a7b8a6795d26c48af65bb903333e270e958
EWouters <6179932+EWouters@users.noreply.github.com>
2023-09-01 18:53:31 +0200

    Ports/diffutils: Update diffutils to version 3.10

--- Ports/diffutils/package.sh
+++ Ports/diffutils/package.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='diffutils'
-version='3.8'
+version='3.10'
 depends=(
     'libiconv'
 )
 files=(
-    "https://ftpmirror.gnu.org/gnu/diffutils/diffutils-${version}.tar.xz#a6bdd7d1b31266d11c4f4de6c1b748d4607ab0231af5188fc2533d0ae2438fec"
+    "https://ftpmirror.gnu.org/gnu/diffutils/diffutils-${version}.tar.xz#90e5e93cc724e4ebe12ede80df1634063c7a855692685919bfe60b556c9bd09e"
 )
 useconfigure='true'
 use_fresh_config_sub='true'
