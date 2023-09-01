de66d72651e1b2ffe66eb54e451ba9fcabae815e
EWouters <6179932+EWouters@users.noreply.github.com>
2023-09-01 20:54:59 +0200

    Ports/libmpg123: Update libmpg123 to version 1.31.3

--- Ports/libmpg123/package.sh
+++ Ports/libmpg123/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libmpg123'
-version='1.29.3'
+version='1.31.3'
 useconfigure='true'
 workdir="mpg123-${version}"
 use_fresh_config_sub='true'
@@ -8,5 +8,5 @@ config_sub_paths=(
     'build/config.sub'
 )
 files=(
-    "https://download.sourceforge.net/project/mpg123/mpg123/${version}/mpg123-${version}.tar.bz2#963885d8cc77262f28b77187c7d189e32195e64244de2530b798ddf32183e847"
+    "https://download.sourceforge.net/project/mpg123/mpg123/${version}/mpg123-${version}.tar.bz2#1ca77d3a69a5ff845b7a0536f783fee554e1041139a6b978f6afe14f5814ad1a"
 )
