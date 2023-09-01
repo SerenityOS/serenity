9823aa43df6ce815e4b0bbf2301a94989db44779
EWouters <6179932+EWouters@users.noreply.github.com>
2023-09-02 00:10:54 +0200

    Ports/qoi: Update qoi to version 351450e

--- Ports/qoi/package.sh
+++ Ports/qoi/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='qoi'
-version='edb8d7b1140be3168cc99ed87edc605c7c1cf31f'
+version='351450e00d6d7b062ab028110b72b317a98e03ea'
 files=(
-    "https://github.com/phoboslab/qoi/archive/${version}.zip#3d3df95fb0b59aca2113ce45396f887eaba1be914cd54b56804efc241f93f203"
+    "https://github.com/phoboslab/qoi/archive/${version}.zip#245c0262d64ed49fd3c9f05313d85912b9fcda1095bd492242f4f8130b02c192"
 )
 depends=(
     'libpng'
