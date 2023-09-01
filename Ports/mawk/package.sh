d7c1061ce6b4cb366c0b7461893cfed40993a00d
EWouters <6179932+EWouters@users.noreply.github.com>
2023-09-01 22:26:49 +0200

    Ports/mawk: Update mawk to version 1.3.4-20230808

--- Ports/mawk/package.sh
+++ Ports/mawk/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='mawk'
-version='1.3.4-20200120'
+version='1.3.4-20230808'
 files=(
-    "https://invisible-mirror.net/archives/mawk/mawk-${version}.tgz#7fd4cd1e1fae9290fe089171181bbc6291dfd9bca939ca804f0ddb851c8b8237"
+    "https://invisible-mirror.net/archives/mawk/mawk-${version}.tgz#88f55a632e2736ff5c5f69944abc151734d89d8298d5005921180f39ab7ba6d0"
 )
 useconfigure='true'
 use_fresh_config_sub='true'
