d146ab378ddc43898a6d7354993c88b841b11b45
EWouters <6179932+EWouters@users.noreply.github.com>
2023-09-02 01:46:21 +0200

    Ports/x264: Update x264 to version a8b68eb

--- Ports/x264/package.sh
+++ Ports/x264/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='x264'
-version='baee400fa9ced6f5481a728138fed6e867b0ff7f'
+version='a8b68ebfaa68621b5ac8907610d3335971839d52'
 files=(
-    "https://code.videolan.org/videolan/x264/-/archive/${version}/x264-master.tar.gz#436a2be54d8bc0cb05dd33ecbbcb7df9c3b57362714fcdaa3a5991189a33319b"
+    "https://code.videolan.org/videolan/x264/-/archive/${version}/x264-master.tar.gz#164688b63f11a6e4f6d945057fc5c57d5eefb97973d0029fb0303744e10839ff"
 )
 useconfigure='true'
 use_fresh_config_sub='true'
