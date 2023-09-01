fbe5253402ff7d2f6fdd44a6b6ddb396c9bc6052
EWouters <6179932+EWouters@users.noreply.github.com>
2023-09-01 22:44:19 +0200

    Ports/mpfr: Update mpfr to version 4.2.1

--- Ports/mpfr/package.sh
+++ Ports/mpfr/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='mpfr'
-version='4.1.0'
+version='4.2.1'
 useconfigure='true'
 use_fresh_config_sub='true'
 configopts=(
@@ -8,7 +8,7 @@ configopts=(
     '--with-sysroot=/'
 )
 files=(
-    "https://ftpmirror.gnu.org/gnu/mpfr/mpfr-${version}.tar.xz#0c98a3f1732ff6ca4ea690552079da9c597872d30e96ec28414ee23c95558a7f"
+    "https://ftpmirror.gnu.org/gnu/mpfr/mpfr-${version}.tar.xz#277807353a6726978996945af13e52829e3abd7a9a5b7fb2793894e18f1fcbb2"
 )
 depends=(
     'gmp'
