2c11efa8ab3e79ae5f73498798aad64bf0084a33
EWouters <6179932+EWouters@users.noreply.github.com>
2023-09-01 20:45:05 +0200

    Ports/libicu: Update libicu to version 73.2

--- Ports/libicu/package.sh
+++ Ports/libicu/package.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libicu'
-version='69.1'
+version='73.2'
 useconfigure='true'
 use_fresh_config_sub='true'
 workdir='icu/source'
 files=(
-    "https://github.com/unicode-org/icu/releases/download/release-${version//./-}/icu4c-${version//./_}-src.tgz#4cba7b7acd1d3c42c44bb0c14be6637098c7faf2b330ce876bc5f3b915d09745"
+    "https://github.com/unicode-org/icu/releases/download/release-${version//./-}/icu4c-${version//./_}-src.tgz#818a80712ed3caacd9b652305e01afc7fa167e6f2e94996da44b90c2ab604ce1"
 )
 
 configure() {
