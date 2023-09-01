6a14ef79de3f02f0e278dabccd22eb4708a6dde8
EWouters <6179932+EWouters@users.noreply.github.com>
2023-09-01 22:43:31 +0200

    Ports/mpc: Update mpc to version 1.3.1

--- Ports/mpc/package.sh
+++ Ports/mpc/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='mpc'
-version='1.2.1'
+version='1.3.1'
 useconfigure='true'
 use_fresh_config_sub='true'
 config_sub_paths=(
@@ -11,7 +11,7 @@ configopts=(
     "--with-sysroot=${SERENITY_INSTALL_ROOT}"
 )
 files=(
-    "https://ftpmirror.gnu.org/gnu/mpc/mpc-${version}.tar.gz#17503d2c395dfcf106b622dc142683c1199431d095367c6aacba6eec30340459"
+    "https://ftpmirror.gnu.org/gnu/mpc/mpc-${version}.tar.gz#ab642492f5cf882b74aa0cb730cd410a81edcdbec895183ce930e706c1c759b8"
 )
 depends=(
     'gmp'
