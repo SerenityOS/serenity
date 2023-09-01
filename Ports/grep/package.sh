5f4c2283204bfe9918d50d83cf3e614fdde2a98c
EWouters <6179932+EWouters@users.noreply.github.com>
2023-09-01 19:51:36 +0200

    Ports/grep: Update grep to version 3.11

--- Ports/grep/package.sh
+++ Ports/grep/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='grep'
-version='3.10'
+version='3.11'
 files=(
-    "https://ftpmirror.gnu.org/gnu/grep/grep-${version}.tar.gz#de7b21d8e3348ea6569c6fd5734e90a31169ef62429ea3dce48a6fc1dd85d260"
+    "https://ftpmirror.gnu.org/gnu/grep/grep-${version}.tar.gz#1f31014953e71c3cddcedb97692ad7620cb9d6d04fbdc19e0d8dd836f87622bb"
 )
 useconfigure='true'
 configopts=(
