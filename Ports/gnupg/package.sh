54635ec7bb737de93624d657679977a9d0f68f7b
EWouters <6179932+EWouters@users.noreply.github.com>
2023-09-02 16:18:14 +0200

    Ports/gnupg: Update gnupg to version 2.4.3
    The patch was upstreamed :^). Includes a fix to find libksba.

--- Ports/gnupg/package.sh
+++ Ports/gnupg/package.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='gnupg'
-version='2.3.0'
+version='2.4.3'
 useconfigure='true'
 use_fresh_config_sub='true'
 config_sub_paths=(
     'build-aux/config.sub'
 )
 files=(
-    "https://gnupg.org/ftp/gcrypt/gnupg/gnupg-${version}.tar.bz2#84c1ef39e8621cfb70f31463a5d1d8edeab44332bc1e0e1af9b78b6f9ed05bb4"
+    "https://gnupg.org/ftp/gcrypt/gnupg/gnupg-${version}.tar.bz2#a271ae6d732f6f4d80c258ad9ee88dd9c94c8fdc33c3e45328c4d7c126bd219d"
 )
 depends=(
     'libassuan'
@@ -31,6 +31,7 @@ configure() {
         --build="$("${workdir}/build-aux/config.guess")" \
         --with-libgpg-error-prefix="${SERENITY_INSTALL_ROOT}/usr/local" \
         --with-libgcrypt-prefix="${SERENITY_INSTALL_ROOT}/usr/local" \
+        --with-ksba-prefix="${SERENITY_INSTALL_ROOT}/usr/local" \
         --with-libassuan-prefix="${SERENITY_INSTALL_ROOT}/usr/local" \
         --with-ntbtls-prefix="${SERENITY_INSTALL_ROOT}/usr/local" \
         --with-npth-prefix="${SERENITY_INSTALL_ROOT}/usr/local" \
