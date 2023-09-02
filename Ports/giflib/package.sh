2a995cffc5848b52af6217c5dbaf3ed084bb013c
Liav A <liavalb@gmail.com>
2023-09-02 22:19:45 +0300

    Ports: Add giflib

---
+++ Ports/giflib/package.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port='giflib'
+version='5.2.1'
+files=(
+    "https://yer.dl.sourceforge.net/project/giflib/giflib-${version}.tar.gz 31da5562f44c5f15d63340a09a4fd62b48c45620cd302f77a6d9acf0077879bd"
+)
