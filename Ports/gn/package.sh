d94bffd708c87a721e3dad2116cb8a183aaaf56e
Tim Schumacher <timschumi@gmx.de>
2023-09-02 09:19:12 +0200

    Ports/gn: Switch to using the Git repository directly

--- Ports/gn/package.sh
+++ Ports/gn/package.sh
@@ -2,23 +2,14 @@
 port='gn'
 workdir='gn'
 version='2023.07.12'
-repository='https://gn.googlesource.com/gn'
-git_rev='fae280eabe5d31accc53100137459ece19a7a295'
 useconfigure='true'
-# FIXME: The files and auth_type are lies here. See #20004
-files=('git')
-auth_type='sha256'
+files=(
+    'git+https://gn.googlesource.com/gn#fae280eabe5d31accc53100137459ece19a7a295'
+)
 depends=(
     'ninja'
     'python3'
 )
-
-fetch() {
-    if [ ! -d ${workdir} ]; then
-        git clone ${repository} ${workdir}
-    fi
-    run git checkout ${git_rev}
-}
 
 configure() {
     run python3 build/gen.py --platform='serenity' --allow-warnings
