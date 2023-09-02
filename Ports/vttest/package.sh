03afeb369257f41f7945abc5b06509b0bbe955c2
EWouters <6179932+EWouters@users.noreply.github.com>
2023-09-02 16:03:00 +0200

    Ports/vttest: Update vttest to version 20230201

--- Ports/vttest/package.sh
+++ Ports/vttest/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='vttest'
-version='20220215'
+version='20230201'
 useconfigure='true'
 files=(
-    "https://invisible-mirror.net/archives/vttest/vttest-${version}.tgz#4a65998c5e12cf08ced2cfce119adb44fa842ac1495d0f150f21c8a6785915a1"
+    "https://invisible-mirror.net/archives/vttest/vttest-${version}.tgz#27f4300791e7cbcf9d15f88421f98e35d3aee66311368430624e2c1a5a8be683"
 )
