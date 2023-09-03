420a7a7b40727f8878c07f5231a4eec96f91b8f5
Andrew Kaster <akaster@serenityos.org>
2023-09-03 21:12:19 +0200

    Ladybird: Add Espresso test on Android to verify we can load our views
    The simple smoke test makes sure that we can boot up an android emulator
    with our package in it, and that the WebView is visible on boot.
    
    More tests to come with more features :^)

--- Ladybird/Android/build.gradle.kts
+++ Ladybird/Android/build.gradle.kts
@@ -75,5 +75,6 @@ dependencies {
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.ext:junit-ktx:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
