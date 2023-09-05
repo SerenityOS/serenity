ada220ebb46100ea3669067d0db1ca856973716c
Timothy Flynn <trflynn89@pm.me>
2023-09-05 14:49:30 -0400

    LibWeb: Introduce the slot concept for HTML slot elements
    A slot is an HTMLSlotElement. It may have any number of slottable nodes
    assigned to it.

--- Userland/Libraries/LibWeb/HTML/HTMLSlotElement.cpp
+++ Userland/Libraries/LibWeb/HTML/HTMLSlotElement.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2020, the SerenityOS developers.
+ * Copyright (c) 2023, Tim Flynn <trflynn89@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -20,6 +21,12 @@ void HTMLSlotElement::initialize(JS::Realm& realm)
 {
     Base::initialize(realm);
     set_prototype(&Bindings::ensure_web_prototype<Bindings::HTMLSlotElementPrototype>(realm, "HTMLSlotElement"));
+}
+
+void HTMLSlotElement::visit_edges(JS::Cell::Visitor& visitor)
+{
+    Base::visit_edges(visitor);
+    Slot::visit_edges(visitor);
 }
 
 }
