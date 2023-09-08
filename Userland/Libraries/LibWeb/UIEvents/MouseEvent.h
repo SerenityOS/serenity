d9bde050ac5e6aaa086e55d278271c7b585d1591
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2023-09-08 17:50:53 +0200

    LibWeb: Reorder MouseEvent items to follow spec more

--- Userland/Libraries/LibWeb/UIEvents/MouseEvent.h
+++ Userland/Libraries/LibWeb/UIEvents/MouseEvent.h
@@ -14,15 +14,12 @@
 namespace Web::UIEvents {
 
 struct MouseEventInit : public EventModifierInit {
-    double offset_x = 0;
-    double offset_y = 0;
+    double screen_x = 0;
+    double screen_y = 0;
     double client_x = 0;
     double client_y = 0;
-    double page_x = 0;
-    double page_y = 0;
     double movement_x = 0;
     double movement_y = 0;
-
     i16 button = 0;
     u16 buttons = 0;
 };
@@ -31,26 +28,25 @@ class MouseEvent : public UIEvent {
     WEB_PLATFORM_OBJECT(MouseEvent, UIEvent);
 
 public:
-    [[nodiscard]] static JS::NonnullGCPtr<MouseEvent> create(JS::Realm&, FlyString const& event_name, MouseEventInit const& = {});
-    static WebIDL::ExceptionOr<JS::NonnullGCPtr<MouseEvent>> create_from_platform_event(JS::Realm&, FlyString const& event_name, CSSPixelPoint offset, CSSPixelPoint client_offset, CSSPixelPoint page_offset, Optional<CSSPixelPoint> movement, unsigned buttons, unsigned mouse_button = 1);
+    [[nodiscard]] static JS::NonnullGCPtr<MouseEvent> create(JS::Realm&, FlyString const& event_name, MouseEventInit const& = {}, double page_x = 0, double page_y = 0, double offset_x = 0, double offset_y = 0);
+    static WebIDL::ExceptionOr<JS::NonnullGCPtr<MouseEvent>> create_from_platform_event(JS::Realm&, FlyString const& event_name, CSSPixelPoint page, CSSPixelPoint client, CSSPixelPoint offset, Optional<CSSPixelPoint> movement, unsigned button, unsigned buttons);
 
     virtual ~MouseEvent() override;
 
-    double offset_x() const { return m_offset_x; }
-    double offset_y() const { return m_offset_y; }
-
-    double client_x() const { return m_client_x; }
-    double client_y() const { return m_client_y; }
-
-    // FIXME: Make these actually different from clientX and clientY.
-    double screen_x() const { return m_client_x; }
-    double screen_y() const { return m_client_y; }
+    double screen_x() const { return m_screen_x; }
+    double screen_y() const { return m_screen_y; }
 
     double page_x() const { return m_page_x; }
     double page_y() const { return m_page_y; }
 
+    double client_x() const { return m_client_x; }
+    double client_y() const { return m_client_y; }
+
     double x() const { return client_x(); }
     double y() const { return client_y(); }
+
+    double offset_x() const { return m_offset_x; }
+    double offset_y() const { return m_offset_y; }
 
     double movement_x() const { return m_movement_x; }
     double movement_y() const { return m_movement_y; }
@@ -61,19 +57,21 @@ public:
     virtual u32 which() const override { return m_button + 1; }
 
 protected:
-    MouseEvent(JS::Realm&, FlyString const& event_name, MouseEventInit const& event_init);
+    MouseEvent(JS::Realm&, FlyString const& event_name, MouseEventInit const& event_init, double page_x, double page_y, double offset_x, double offset_y);
 
     virtual void initialize(JS::Realm&) override;
 
 private:
     void set_event_characteristics();
 
-    double m_offset_x { 0 };
-    double m_offset_y { 0 };
-    double m_client_x { 0 };
-    double m_client_y { 0 };
+    double m_screen_x { 0 };
+    double m_screen_y { 0 };
     double m_page_x { 0 };
     double m_page_y { 0 };
+    double m_client_x { 0 };
+    double m_client_y { 0 };
+    double m_offset_x { 0 };
+    double m_offset_y { 0 };
     double m_movement_x { 0 };
     double m_movement_y { 0 };
     i16 m_button { 0 };
