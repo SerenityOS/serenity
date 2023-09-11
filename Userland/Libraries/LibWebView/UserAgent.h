99858380907d1781ef26126edd87414cf9ca97e0
implicitfield <114500360+implicitfield@users.noreply.github.com>
2023-09-11 17:49:57 +0400

    Browser+Ladybird: Let the list of user agent strings be shared

---
+++ Userland/Libraries/LibWebView/UserAgent.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2023, the SerenityOS developers.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/HashMap.h>
+#include <AK/StringView.h>
+
+namespace WebView {
+
+extern HashMap<StringView, StringView> const user_agents;
+
+}
