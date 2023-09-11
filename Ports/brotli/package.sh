48a1fe83081ebcc8e55af6be8fd4f8b55a0053aa
Lucas CHOLLET <lucas.chollet@free.fr>
2023-09-11 13:54:35 +0200

    Ports/brotli: Update to 1.1.0

--- Ports/brotli/package.sh
+++ Ports/brotli/package.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 
 port='brotli'
-version='1.0.9'
+version='1.1.0'
 files=(
-    "https://github.com/google/brotli/archive/refs/tags/v${version}.tar.gz#f9e8d81d0405ba66d181529af42a3354f838c939095ff99930da6aa9cdf6fe46"
+    "https://github.com/google/brotli/archive/refs/tags/v${version}.tar.gz#e720a6ca29428b803f4ad165371771f5398faba397edf6778837a18599ea13ff"
+)
+configopts=(
+    "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
+    "-DCMAKE_BUILD_TYPE=Release"
 )
 useconfigure='true'
 
 configure() {
-    run ./configure-cmake
+    run cmake "${configopts[@]}" .
+}
+
+install() {
+    run make "${installopts[@]}" install
 }
