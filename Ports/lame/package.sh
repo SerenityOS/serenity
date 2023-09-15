080577a1b4025ad5fddac0df5e2207e15052b7fb
kleines Filmröllchen <filmroellchen@serenityos.org>
2023-09-16 01:01:11 +0200

    Ports: Add LAME

---
+++ Ports/lame/package.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port=lame
+version=3.100
+useconfigure=true
+use_fresh_config_sub=true
+files=(
+    "https://downloads.sourceforge.net/project/lame/lame/${version}/lame-${version}.tar.gz#ddfe36cab873794038ae2c1210557ad34857a4b6bdc515785d1da9e175b1da1e"
+)
