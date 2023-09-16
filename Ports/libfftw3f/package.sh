220bf5716984f163782045f8bfb6e6d52b559d9b
kleines Filmröllchen <filmroellchen@serenityos.org>
2023-09-16 13:39:46 +0200

    Ports/libfftw3f: Symlink fftw3.pc pkgconfig file
    This is how some ports like rubberband search for FFTW.

--- Ports/libfftw3f/package.sh
+++ Ports/libfftw3f/package.sh
@@ -8,3 +8,8 @@ files=(
     "http://fftw.org/fftw-${version}.tar.gz#56c932549852cddcfafdab3820b0200c7742675be92179e59e6215b340e26467"
 )
 workdir="fftw-${version}"
+
+post_install() {
+    # Some upstream libraries search for "fftw3" in pkgconfig, not "fftw3f".
+    ln -sf "fftw3f.pc" "${SERENITY_INSTALL_ROOT}/usr/local/lib/pkgconfig/fftw3.pc"
+}
