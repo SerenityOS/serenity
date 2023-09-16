c3b1cbddcef914071e3a19906c1dd5d347702e9f
Liav A <liavalb@gmail.com>
2023-09-16 11:43:18 +0300

    DynamicLoader: Add standard argument parsing for executing ELF binaries
    This change essentially puts the DynamicLoader with 2 roles - the first
    one is to be invoked by the kernel to dynamically link an ELF executable
    in runtime.
    The second role is to allow running ELF executables explicitly from
    userspace so the kernel runs the DynamicLoader as the "intended" program
    but now the DynamicLoader can do its own commandline argument parsing
    and run a specified binary, with arguments like running in secure-mode,
    etc.

--- Userland/DynamicLoader/main.cpp
+++ Userland/DynamicLoader/main.cpp
@@ -6,9 +6,11 @@
 
 #include <AK/ScopeGuard.h>
 #include <Kernel/API/POSIX/sys/stat.h>
+#include <LibCore/ArgsParser.h>
 #include <LibELF/AuxiliaryVector.h>
 #include <LibELF/DynamicLinker.h>
 #include <LibELF/Relocation.h>
+#include <LibMain/Main.h>
 #include <fcntl.h>
 #include <sys/internals.h>
 #include <syscall.h>
@@ -44,18 +46,73 @@ static void perform_self_relocations(auxv_t* auxvp)
         exit(1);
 }
 
-static void display_help()
+static ErrorOr<int> open_executable(StringView path)
 {
-    char const message[] =
-        R"(You have invoked `Loader.so'. This is the helper program for programs that
-use shared libraries. Special directives embedded in executables tell the
-kernel to load this program.
+    int rc = open(path.characters_without_null_termination(), O_RDONLY | O_EXEC, 0666);
+    if (rc < 0)
+        return Error::from_errno(errno);
+    int checked_fd = rc;
+    ArmedScopeGuard close_on_failure([checked_fd] {
+        close(checked_fd);
+    });
 
-This helper program loads the shared libraries needed by the program,
-prepares the program to run, and runs it. You do not need to invoke
-this helper program directly. If you still want to run a program with the loader,
-run: /usr/lib/Loader.so [ELF_BINARY])";
-    fprintf(stderr, "%s", message);
+    struct stat executable_stat;
+    rc = fstat(checked_fd, &executable_stat);
+    if (rc < 0)
+        return Error::from_errno(errno);
+    if (!S_ISREG(executable_stat.st_mode)) {
+        if (S_ISDIR(executable_stat.st_mode))
+            return Error::from_errno(EISDIR);
+        return Error::from_errno(EINVAL);
+    }
+
+    close_on_failure.disarm();
+    return checked_fd;
+}
+
+int main(int argc, char** argv, char** envp)
+{
+    tzset();
+
+    Vector<StringView> arguments;
+    arguments.ensure_capacity(argc);
+    for (int i = 0; i < argc; ++i)
+        arguments.unchecked_append({ argv[i], strlen(argv[i]) });
+
+    bool flag_secure { false };
+    Core::ArgsParser args_parser;
+    Vector<StringView> command;
+
+    args_parser.set_general_help("Run dynamically-linked ELF executables");
+    args_parser.set_stop_on_first_non_option(true);
+    args_parser.add_option(flag_secure, "Run in secure-mode", "secure", 's');
+    args_parser.add_positional_argument(command, "Command to execute", "command");
+    args_parser.parse(arguments.span());
+
+    if (command.is_empty()) {
+        args_parser.print_usage(stderr, arguments[0]);
+        return 1;
+    }
+
+    auto error_or_fd = open_executable(command[0]);
+    if (error_or_fd.is_error()) {
+        warnln("Loader.so: Loading {} failed: {}", command[0], strerror(error_or_fd.error().code()));
+        return 1;
+    }
+
+    int main_program_fd = error_or_fd.release_value();
+    DeprecatedString main_program_path = command[0];
+
+    // NOTE: We need to extract the command with its arguments to be able
+    // to run the actual requested executable with the requested parameters
+    // from argv.
+    VERIFY(command.size() <= static_cast<size_t>(argc));
+    auto command_with_args = command.span();
+    for (size_t index = 0; index < command.size(); index++)
+        argv[index] = const_cast<char*>(command_with_args[index].characters_without_null_termination());
+
+    ELF::DynamicLinker::linker_main(move(main_program_path), main_program_fd, flag_secure, command.size(), argv, envp);
+    VERIFY_NOT_REACHED();
 }
 
 extern "C" {
@@ -78,30 +135,6 @@ NAKED void _start(int, char**, char**)
         "push $0\n"
         "jmp _entry@plt\n");
 #endif
-}
-
-static ErrorOr<int> open_executable(char const* path)
-{
-    int rc = open(path, O_RDONLY | O_EXEC, 0666);
-    if (rc < 0)
-        return Error::from_errno(errno);
-    int checked_fd = rc;
-    ArmedScopeGuard close_on_failure([checked_fd] {
-        close(checked_fd);
-    });
-
-    struct stat executable_stat;
-    rc = fstat(checked_fd, &executable_stat);
-    if (rc < 0)
-        return Error::from_errno(errno);
-    if (!S_ISREG(executable_stat.st_mode)) {
-        if (S_ISDIR(executable_stat.st_mode))
-            return Error::from_errno(EISDIR);
-        return Error::from_errno(EINVAL);
-    }
-
-    close_on_failure.disarm();
-    return checked_fd;
 }
 
 void _entry(int argc, char** argv, char** envp)
@@ -131,23 +164,9 @@ void _entry(int argc, char** argv, char** envp)
 
     if (main_program_path == "/usr/lib/Loader.so"sv) {
         // We've been invoked directly as an executable rather than as the
-        // ELF interpreter for some other binary. The second argv string should
-        // be the path to the ELF executable, and if we don't have enough strings in argv
-        // (argc < 2), just fail with message to stderr about this.
-
-        if (argc < 2) {
-            display_help();
-            _exit(1);
-        }
-        auto error_or_fd = open_executable(argv[1]);
-        if (error_or_fd.is_error()) {
-            warnln("Loader.so: Loading {} failed: {}", argv[1], strerror(error_or_fd.error().code()));
-            _exit(1);
-        }
-        main_program_fd = error_or_fd.release_value();
-        main_program_path = argv[1];
-        argv++;
-        argc--;
+        // ELF interpreter for some other binary.
+        int exit_status = main(argc, argv, envp);
+        _exit(exit_status);
     }
 
     VERIFY(main_program_fd >= 0);
