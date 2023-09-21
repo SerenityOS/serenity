c230876d5e6a4ee92eca74b109e40743508e12a6
Shannon Booth <shannon@serenityos.org>
2023-09-21 21:11:50 +1200

    LibWeb: Add AttributeNames::list

--- Userland/Libraries/LibWeb/HTML/AttributeNames.h
+++ Userland/Libraries/LibWeb/HTML/AttributeNames.h
@@ -93,6 +93,7 @@ namespace AttributeNames {
     __ENUMERATE_HTML_ATTRIBUTE(lang)                       \
     __ENUMERATE_HTML_ATTRIBUTE(language)                   \
     __ENUMERATE_HTML_ATTRIBUTE(link)                       \
+    __ENUMERATE_HTML_ATTRIBUTE(list)                       \
     __ENUMERATE_HTML_ATTRIBUTE(loading)                    \
     __ENUMERATE_HTML_ATTRIBUTE(longdesc)                   \
     __ENUMERATE_HTML_ATTRIBUTE(loop)                       \
