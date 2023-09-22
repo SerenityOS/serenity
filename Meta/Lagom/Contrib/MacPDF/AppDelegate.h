46a0b61cef38574409e16428f06fda9826c92b22
Nico Weber <thakis@chromium.org>
2023-09-21 22:06:35 -0400

    MacPDF: Make pdfs without embedded fonts work again
    A prior change removed some (very hacky) code needed for this

--- Meta/Lagom/Contrib/MacPDF/AppDelegate.h
+++ Meta/Lagom/Contrib/MacPDF/AppDelegate.h
@@ -5,7 +5,10 @@
 //  Created by Nico Weber on 7/22/23.
 //
 
+// Several AK types conflict with MacOS types.
+#define FixedPoint FixedPointMacOS
 #import <Cocoa/Cocoa.h>
+#undef FixedPoint
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
