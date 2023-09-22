91798a532c47370ae69a239f36a708b0f56141c2
Martin Janiczek <martin@janiczek.cz>
2023-09-22 10:19:56 +0200

    Improve docs in TestResult

--- Userland/Libraries/LibTest/TestResult.h
+++ Userland/Libraries/LibTest/TestResult.h
@@ -1,10 +1,13 @@
 #pragma once
 
+/* TestResult signals to the TestSuite how the TestCase execution went.
+*/
 enum class TestResult {
     NotRun,
     Passed,   // test function ran to completion without setting any of the below flags:
     Failed,   // didn't get through EXPECT(...)
-    Rejected, // didn't get through the ASSUME(...) filter 15 times in a row
-    Overrun,  // ran out of RandomRun data
-    HitLimit, // hit RandomRun length limit
+    Rejected, // didn't get through the ASSUME(...) filter 15 times in a row (in a randomized test)
+              // alternatively, user used REJECT(...)
+    Overrun,  // ran out of RandomRun data (in a randomized test, when shrinking)
+    HitLimit, // hit RandomRun length limit (in a randomized test, when generating)
 };
