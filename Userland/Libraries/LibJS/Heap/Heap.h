e1cded8bd840d586bf779b733916bdb762c4b748
Hendiadyoin1 <leon.a@serenityos.org>
2023-09-23 13:47:16 +0200

    LibJS: Defer GC during cell construction
    This stops us from trying to collect not fully constructed Cells,
    which's vtables are not fully initialized, which would cause issues
    during GC.

--- Userland/Libraries/LibJS/Heap/Heap.h
+++ Userland/Libraries/LibJS/Heap/Heap.h
@@ -39,7 +39,9 @@ public:
     NonnullGCPtr<T> allocate_without_realm(Args&&... args)
     {
         auto* memory = allocate_cell(sizeof(T));
+        defer_gc();
         new (memory) T(forward<Args>(args)...);
+        undefer_gc();
         return *static_cast<T*>(memory);
     }
 
@@ -47,7 +49,9 @@ public:
     NonnullGCPtr<T> allocate(Realm& realm, Args&&... args)
     {
         auto* memory = allocate_cell(sizeof(T));
+        defer_gc();
         new (memory) T(forward<Args>(args)...);
+        undefer_gc();
         auto* cell = static_cast<T*>(memory);
         memory->initialize(realm);
         return *cell;
