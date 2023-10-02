24682f5dcf6af932baacd0145fc3d511a88f8a79
Dan Klishch <danilklishch@gmail.com>
2023-10-02 10:26:32 -0400

    Meta: Fix typos in shell script comments

--- Meta/shell_include.sh
+++ Meta/shell_include.sh
@@ -3,7 +3,7 @@
 # SC2034: "Variable appears unused. Verify it or export it."
 #         Those are intentional here, as the file is meant to be included elsewhere.
 
-# NOTE: If using another privilege escalation binary make sure it is configured or has the appropiate flag
+# NOTE: If using another privilege escalation binary make sure it is configured or has the appropriate flag
 #       to keep the current environment variables in the launched process (in sudo's case this is achieved
 #       through the -E flag described in sudo(8).
 die() {
