e7179327effb0e3700fced406bc9a10bf69f12ec
Shannon Booth <shannon@serenityos.org>
2023-10-08 12:48:20 +1300

    LibWeb: Remove some unused DeprecatedFlyString includes

--- Userland/Libraries/LibWeb/Encoding/TextEncoder.cpp
+++ Userland/Libraries/LibWeb/Encoding/TextEncoder.cpp
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#include <AK/DeprecatedFlyString.h>
 #include <LibJS/Runtime/TypedArray.h>
 #include <LibWeb/Bindings/Intrinsics.h>
 #include <LibWeb/Encoding/TextEncoder.h>
--- Userland/Libraries/LibWeb/FontCache.cpp
+++ Userland/Libraries/LibWeb/FontCache.cpp
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#include <AK/DeprecatedFlyString.h>
 #include <LibGfx/Font/Font.h>
 #include <LibWeb/FontCache.h>
 
