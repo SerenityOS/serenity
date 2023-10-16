0a35588c0cbece04bfa26cc7cae5eecd70c83194
kleines Filmröllchen <filmroellchen@serenityos.org>
2023-10-16 15:45:15 +0200

    LibDisassembly: Allow disassembler to disassemble any architecture
    We can easily detect the architecture from a given ELF file or just use
    the native architecture. Importantly, this allows every implemented
    target architecture to be disassembled on every host architecture.

--- Userland/Libraries/LibDisassembly/Disassembler.h
+++ Userland/Libraries/LibDisassembly/Disassembler.h
@@ -8,6 +8,7 @@
 
 #include <AK/NonnullOwnPtr.h>
 #include <AK/Optional.h>
+#include <LibDisassembly/Architecture.h>
 #include <LibDisassembly/Instruction.h>
 #include <LibDisassembly/InstructionStream.h>
 #include <LibDisassembly/x86/Instruction.h>
@@ -16,8 +17,9 @@ namespace Disassembly {
 
 class Disassembler {
 public:
-    explicit Disassembler(InstructionStream& stream)
+    explicit Disassembler(InstructionStream& stream, Architecture arch)
         : m_stream(stream)
+        , m_arch(arch)
     {
     }
 
@@ -25,16 +27,18 @@ public:
     {
         if (!m_stream.can_read())
             return {};
-#if ARCH(X86_64)
-        return make<X86::Instruction>(X86::Instruction::from_stream(m_stream, X86::ProcessorMode::Long));
-#else
-        dbgln("Unsupported platform");
-        return {};
-#endif
+        switch (m_arch) {
+        case Architecture::X86:
+            return make<X86::Instruction>(X86::Instruction::from_stream(m_stream, X86::ProcessorMode::Long));
+        default:
+            dbgln("Unsupported platform");
+            return {};
+        }
     }
 
 private:
     InstructionStream& m_stream;
+    Architecture m_arch;
 };
 
 }
