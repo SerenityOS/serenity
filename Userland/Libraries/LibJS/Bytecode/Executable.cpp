6de8ae035fc2d6ce3506e6deb1b188890ba4760e
Andreas Kling <kling@serenityos.org>
2023-10-20 12:21:30 +0200

    LibJS/JIT: Don't keep trying to JIT unsupported bytecode executables
    We now only try jitting each Bytecode::Executable once, and then cache
    the resulting NativeExecutable.

--- Userland/Libraries/LibJS/Bytecode/Executable.cpp
+++ Userland/Libraries/LibJS/Bytecode/Executable.cpp
@@ -7,6 +7,7 @@
 #include <LibJS/Bytecode/BasicBlock.h>
 #include <LibJS/Bytecode/Executable.h>
 #include <LibJS/Bytecode/RegexTable.h>
+#include <LibJS/JIT/Compiler.h>
 #include <LibJS/SourceCode.h>
 
 namespace JS::Bytecode {
@@ -50,6 +51,15 @@ void Executable::dump() const
         outln();
         identifier_table->dump();
     }
+}
+
+JIT::NativeExecutable const* Executable::get_or_create_native_executable()
+{
+    if (!m_did_try_jitting) {
+        m_did_try_jitting = true;
+        m_native_executable = JIT::Compiler::compile(*this);
+    }
+    return m_native_executable;
 }
 
 }
