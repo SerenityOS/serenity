8853c7fe52c1f22e6053014df5b4c4b2a13d2ac1
kleines Filmröllchen <filmroellchen@serenityos.org>
2023-10-21 23:55:41 +0200

    LibDisassembly: Add common priviledged instructions
    This does not include all instructions for S- and M-mode from the
    priviledged spec, but these are the common ones that we will definitely
    need (and use ourselves)

--- Userland/Libraries/LibDisassembly/riscv64/Formatting.cpp
+++ Userland/Libraries/LibDisassembly/riscv64/Formatting.cpp
@@ -5,6 +5,7 @@
  */
 
 #include "Formatting.h"
+#include "Priviledged.h"
 
 namespace Disassembly::RISCV64 {
 
@@ -312,6 +313,21 @@ String EnvironmentCall::mnemonic() const
 String InstructionFence::mnemonic() const
 {
     return "fence.i"_string;
+}
+
+String MachineModeTrapReturn::mnemonic() const
+{
+    return "mret"_string;
+}
+
+String SupervisorModeTrapReturn::mnemonic() const
+{
+    return "sret"_string;
+}
+
+String WaitForInterrupt::mnemonic() const
+{
+    return "wfi"_string;
 }
 
 String CSRImmediateInstruction::to_string(DisplayStyle display_style, u32, Optional<SymbolProvider const&>) const
