16187760704f880b88c8c9017db4fb1004c3d54a
Andreas Kling <kling@serenityos.org>
2023-10-26 15:13:08 +0200

    LibJS/Bytecode: Un-templatize throw_if_needed_for_call()

--- Userland/Libraries/LibJS/Bytecode/CommonImplementations.h
+++ Userland/Libraries/LibJS/Bytecode/CommonImplementations.h
@@ -18,8 +18,7 @@ ThrowCompletionOr<Value> get_by_value(Bytecode::Interpreter&, Value base_value, 
 ThrowCompletionOr<Value> get_global(Bytecode::Interpreter&, IdentifierTableIndex, u32 cache_index);
 ThrowCompletionOr<void> put_by_property_key(VM&, Value base, Value this_value, Value value, PropertyKey name, Op::PropertyKind kind);
 ThrowCompletionOr<Value> perform_call(Interpreter&, Value this_value, Op::CallType, Value callee, MarkedVector<Value> argument_values);
-template<typename InstructionType>
-ThrowCompletionOr<void> throw_if_needed_for_call(Interpreter&, InstructionType const&, Value callee);
+ThrowCompletionOr<void> throw_if_needed_for_call(Interpreter&, Value callee, Op::CallType, Optional<StringTableIndex> const& expression_string);
 ThrowCompletionOr<Value> typeof_variable(VM&, DeprecatedFlyString const&);
 ThrowCompletionOr<void> set_variable(VM&, DeprecatedFlyString const&, Value, Op::EnvironmentMode, Op::SetVariable::InitializationMode);
 Value new_function(VM&, FunctionExpression const&, Optional<IdentifierTableIndex> const& lhs_name, Optional<Register> const& home_object);
