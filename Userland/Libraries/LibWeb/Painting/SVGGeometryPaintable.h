311e9d555e18c16bd93d1415456c867a5b12a7af
MacDue <macdue@dueutil.tech>
2023-10-29 19:11:46 +0000

    LibWeb: Layout SVG <text> elements during layout (not while painting)
    Previously, all SVG <text> elements were zero-sized boxes, that were
    only actually positioned and sized during painting. This led to a number
    of problems, the most visible of which being that text could not be
    scaled based on the viewBox.
    
    Which this patch, <text> elements get a correctly sized layout box,
    that can be hit-tested and respects the SVG viewBox.
    
    To share code with SVGGeometryElement's the PathData (from the prior
    commit) has been split into a computed path and computed transforms.
    The computed path is specific to geometry elements, but the computed
    transforms are shared between all SVG graphics elements.

--- Userland/Libraries/LibWeb/Painting/SVGGeometryPaintable.h
+++ Userland/Libraries/LibWeb/Painting/SVGGeometryPaintable.h
@@ -15,41 +15,6 @@ class SVGGeometryPaintable final : public SVGGraphicsPaintable {
     JS_CELL(SVGGeometryPaintable, SVGGraphicsPaintable);
 
 public:
-    class PathData {
-    public:
-        PathData(Gfx::Path path, Gfx::AffineTransform svg_to_viewbox_transform, Gfx::AffineTransform svg_transform)
-            : m_computed_path(move(path))
-            , m_svg_to_viewbox_transform(svg_to_viewbox_transform)
-            , m_svg_transform(svg_transform)
-        {
-        }
-
-        Gfx::Path const& computed_path() const { return m_computed_path; }
-
-        Gfx::AffineTransform const& svg_to_viewbox_transform() const { return m_svg_to_viewbox_transform; }
-
-        Gfx::AffineTransform const& svg_transform() const { return m_svg_transform; }
-
-        Gfx::AffineTransform svg_to_css_pixels_transform(
-            Optional<Gfx::AffineTransform const&> additional_svg_transform = {}) const
-        {
-            return Gfx::AffineTransform {}.multiply(svg_to_viewbox_transform()).multiply(additional_svg_transform.value_or(Gfx::AffineTransform {})).multiply(svg_transform());
-        }
-
-        Gfx::AffineTransform svg_to_device_pixels_transform(
-            PaintContext const& context,
-            Gfx::AffineTransform const& additional_svg_transform) const
-        {
-            auto css_scale = context.device_pixels_per_css_pixel();
-            return Gfx::AffineTransform {}.scale({ css_scale, css_scale }).multiply(svg_to_css_pixels_transform(additional_svg_transform));
-        }
-
-    private:
-        Gfx::Path m_computed_path;
-        Gfx::AffineTransform m_svg_to_viewbox_transform;
-        Gfx::AffineTransform m_svg_transform;
-    };
-
     static JS::NonnullGCPtr<SVGGeometryPaintable> create(Layout::SVGGeometryBox const&);
 
     virtual Optional<HitTestResult> hit_test(CSSPixelPoint, HitTestType) const override;
@@ -58,17 +23,17 @@ public:
 
     Layout::SVGGeometryBox const& layout_box() const;
 
-    void set_path_data(PathData path_data)
+    void set_computed_path(Gfx::Path path)
     {
-        m_path_data = move(path_data);
+        m_computed_path = move(path);
     }
 
-    Optional<PathData> const& path_data() const { return m_path_data; }
+    Optional<Gfx::Path> const& computed_path() const { return m_computed_path; }
 
 protected:
     SVGGeometryPaintable(Layout::SVGGeometryBox const&);
 
-    Optional<PathData> m_path_data = {};
+    Optional<Gfx::Path> m_computed_path = {};
 };
 
 }
