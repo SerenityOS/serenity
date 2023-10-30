1b5bff8b71c067bc11aef03168f82522635a6ddc
Dan Klishch <danilklishch@gmail.com>
2023-10-30 16:08:01 -0400

    LibCore: Remove `strtol` call from `parse_sockets_from_system_server`

--- Userland/Libraries/LibCore/SystemServerTakeover.cpp
+++ Userland/Libraries/LibCore/SystemServerTakeover.cpp
@@ -26,7 +26,8 @@ static void parse_sockets_from_system_server()
 
     for (auto const socket : StringView { sockets, strlen(sockets) }.split_view(';')) {
         auto params = socket.split_view(':');
-        s_overtaken_sockets.set(params[0].to_deprecated_string(), strtol(params[1].to_deprecated_string().characters(), nullptr, 10));
+        VERIFY(params.size() == 2);
+        s_overtaken_sockets.set(params[0].to_deprecated_string(), params[1].to_int().value());
     }
 
     s_overtaken_sockets_parsed = true;
