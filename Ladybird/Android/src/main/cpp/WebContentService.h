c990db091340487191dbf9cdb0fc0390a13bd953
Andrew Kaster <akaster@serenityos.org>
2023-10-31 15:27:23 -0600

    Ladybird/Android: Create a service for ImageDecoder
    This follows the pattern for the other services spawned by WebContent.
    The notable quirk about this service is that it's actually spawned by
    the ImageCodecPlugin rather than in main.cpp in the non-Android port.
    
    As a result we needed to do some ifdef surgery to get all the pieces
    in place. But we can now load images in the Android port again :^).

--- Ladybird/Android/src/main/cpp/WebContentService.h
+++ Ladybird/Android/src/main/cpp/WebContentService.h
@@ -13,3 +13,4 @@ ErrorOr<NonnullRefPtr<Client>> bind_service(void (*bind_method)(int, int));
 
 void bind_request_server_java(int ipc_socket, int fd_passing_socket);
 void bind_web_socket_java(int ipc_socket, int fd_passing_socket);
+void bind_image_decoder_java(int ipc_socket, int fd_passing_socket);
