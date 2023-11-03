e0718bb713c7b5d0160488d638543abbdcf1aabf
Andreas Kling <kling@serenityos.org>
2023-11-03 08:54:50 +0100

    LibJS+LibJIT: Add fast path for Int32 ^ Int32

--- Userland/Libraries/LibJS/JIT/Compiler.h
+++ Userland/Libraries/LibJS/JIT/Compiler.h
@@ -54,7 +54,6 @@ private:
         O(LooselyEquals, abstract_equals)                       \
         O(StrictlyInequals, typed_inequals)                     \
         O(StrictlyEquals, typed_equals)                         \
-        O(BitwiseXor, bitwise_xor)                              \
         O(LeftShift, left_shift)                                \
         O(RightShift, right_shift)                              \
         O(UnsignedRightShift, unsigned_right_shift)
