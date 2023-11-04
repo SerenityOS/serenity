faaf5b96528ca6a8394de0a3d7667fbfef03e0f5
Matthew Olsson <matthewcolsson@gmail.com>
2023-11-04 14:00:09 -0700

    LibWeb: Add the AnimationEvent IDL objects

---
+++ Userland/Libraries/LibWeb/CSS/AnimationEvent.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2024, Matthew Olsson <mattco@serenityos.org>.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibWeb/Bindings/Intrinsics.h>
+#include <LibWeb/CSS/AnimationEvent.h>
+
+namespace Web::CSS {
+
+JS::NonnullGCPtr<AnimationEvent> AnimationEvent::create(JS::Realm& realm, FlyString const& type, AnimationEventInit const& event_init)
+{
+    return realm.heap().allocate<AnimationEvent>(realm, realm, type, event_init);
+}
+
+WebIDL::ExceptionOr<JS::NonnullGCPtr<AnimationEvent>> AnimationEvent::construct_impl(JS::Realm& realm, FlyString const& type, AnimationEventInit const& event_init)
+{
+    return create(realm, type, event_init);
+}
+
+AnimationEvent::AnimationEvent(JS::Realm& realm, FlyString const& type, AnimationEventInit const& event_init)
+    : DOM::Event(realm, type, event_init)
+    , m_animation_name(event_init.animation_name)
+    , m_elapsed_time(event_init.elapsed_time)
+    , m_pseudo_element(event_init.pseudo_element)
+{
+}
+
+void AnimationEvent::initialize(JS::Realm& realm)
+{
+    Base::initialize(realm);
+    set_prototype(&Bindings::ensure_web_prototype<Bindings::AnimationEventPrototype>(realm, "AnimationEvent"_fly_string));
+}
+
+}
