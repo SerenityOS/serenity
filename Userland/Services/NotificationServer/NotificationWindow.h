dcbb8cf0acf21fdd520d832ac551ed00b805bea2
Rummskartoffel <Rummskartoffel@protonmail.com>
2023-11-04 21:31:31 +0100

    NotificationServer: Make notifications not overlap when they appear
    Before this patch, if two or more notifications were created after one
    another, they would overlap. This was caused by the previously lowest
    notification's m_original_rect being used to calculate the position for
    each new notification instead of the notification's actual rect, which
    can be different.
    This patch makes notifications use each others' rect() method instead,
    which makes them appear in the correct position. With that,
    m_original_rect has no use anymore, so this patch removes it.

--- Userland/Services/NotificationServer/NotificationWindow.h
+++ Userland/Services/NotificationServer/NotificationWindow.h
@@ -16,7 +16,6 @@ class NotificationWindow final : public GUI::Window {
 
 public:
     virtual ~NotificationWindow() override = default;
-    void set_original_rect(Gfx::IntRect original_rect) { m_original_rect = original_rect; }
 
     void set_text(String const&);
     void set_title(String const&);
@@ -36,7 +35,6 @@ private:
     void resize_to_fit_text();
     void set_height(int);
 
-    Gfx::IntRect m_original_rect;
     i32 m_id;
 
     GUI::Label* m_text_label;
