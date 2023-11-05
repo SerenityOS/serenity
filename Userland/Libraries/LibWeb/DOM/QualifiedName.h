f2dc3fa6b56b060cffb3eeb604b4bff88d892f86
Shannon Booth <shannon@serenityos.org>
2023-11-05 15:43:15 +1300

    LibWeb: Remove some now unused deprecated functions from QualifiedName
    And related callers in Element.

--- Userland/Libraries/LibWeb/DOM/QualifiedName.h
+++ Userland/Libraries/LibWeb/DOM/QualifiedName.h
@@ -1,13 +1,13 @@
 /*
  * Copyright (c) 2020, the SerenityOS developers.
  * Copyright (c) 2022, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2023, Shannon Booth <shannon@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #pragma once
 
-#include <AK/DeprecatedFlyString.h>
 #include <AK/FlyString.h>
 #include <AK/Optional.h>
 
@@ -16,25 +16,10 @@ namespace Web::DOM {
 class QualifiedName {
 public:
     QualifiedName(FlyString const& local_name, Optional<FlyString> const& prefix, Optional<FlyString> const& namespace_);
-    QualifiedName(FlyString const& local_name, Optional<FlyString> const& prefix, DeprecatedFlyString const& namespace_);
 
     FlyString const& local_name() const { return m_impl->local_name; }
     Optional<FlyString> const& prefix() const { return m_impl->prefix; }
     Optional<FlyString> const& namespace_() const { return m_impl->namespace_; }
-
-    DeprecatedFlyString deprecated_prefix() const
-    {
-        if (!m_impl->prefix.has_value())
-            return {};
-        return m_impl->prefix->to_deprecated_fly_string();
-    }
-
-    DeprecatedFlyString deprecated_namespace_() const
-    {
-        if (!m_impl->namespace_.has_value())
-            return {};
-        return m_impl->namespace_->to_deprecated_fly_string();
-    }
 
     FlyString const& as_string() const { return m_impl->as_string; }
 
