ffc258b37691ef5d1edfbe8db2d3a49abe625c86
Sam Atkins <atkinssj@serenityos.org>
2023-11-07 12:10:16 +0000

    LibWeb: Add some helpers to PositionStyleValue
    Both of these are needed by the gradient style values.

--- Userland/Libraries/LibWeb/CSS/StyleValues/PositionStyleValue.h
+++ Userland/Libraries/LibWeb/CSS/StyleValues/PositionStyleValue.h
@@ -22,10 +22,18 @@ public:
     {
         return adopt_ref(*new (nothrow) PositionStyleValue(move(edge_x), move(edge_y)));
     }
+    static ValueComparingNonnullRefPtr<PositionStyleValue> create_center()
+    {
+        return adopt_ref(*new (nothrow) PositionStyleValue(
+            EdgeStyleValue::create(PositionEdge::Left, Percentage { 50 }),
+            EdgeStyleValue::create(PositionEdge::Top, Percentage { 50 })));
+    }
     virtual ~PositionStyleValue() override = default;
 
     ValueComparingNonnullRefPtr<EdgeStyleValue> edge_x() const { return m_properties.edge_x; }
     ValueComparingNonnullRefPtr<EdgeStyleValue> edge_y() const { return m_properties.edge_y; }
+    bool is_center() const;
+    CSSPixelPoint resolved(Layout::Node const&, CSSPixelRect const&) const;
 
     virtual String to_string() const override;
 
