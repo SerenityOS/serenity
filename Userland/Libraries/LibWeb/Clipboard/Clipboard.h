b3779b424eab22427e79b747b9a1fdb96cba3f2a
Timothy Flynn <trflynn89@pm.me>
2023-11-10 13:29:20 -0500

    LibWeb: Begin implementing the Clipboard API
    https://w3c.github.io/clipboard-apis/
    
    This implements enough for navigator.clipboard.writeText(String).

---
+++ Userland/Libraries/LibWeb/Clipboard/Clipboard.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2023, Tim Flynn <trflynn89@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/String.h>
+#include <LibJS/Forward.h>
+#include <LibJS/Heap/GCPtr.h>
+#include <LibWeb/DOM/EventTarget.h>
+#include <LibWeb/Forward.h>
+#include <LibWeb/WebIDL/ExceptionOr.h>
+
+namespace Web::Clipboard {
+
+class Clipboard final : public DOM::EventTarget {
+    WEB_PLATFORM_OBJECT(Clipboard, DOM::EventTarget);
+
+public:
+    static WebIDL::ExceptionOr<JS::NonnullGCPtr<Clipboard>> construct_impl(JS::Realm&);
+    virtual ~Clipboard() override;
+
+    JS::NonnullGCPtr<JS::Promise> write_text(String);
+
+private:
+    Clipboard(JS::Realm&);
+
+    virtual void initialize(JS::Realm&) override;
+};
+
+}
