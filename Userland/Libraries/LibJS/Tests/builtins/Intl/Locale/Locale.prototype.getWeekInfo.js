1d76738ddeea9991dc70ceeddfa3924ec1447ce6
Timothy Flynn <trflynn89@pm.me>
2023-11-13 10:18:25 -0500

    LibJS: Change Intl.Locale info APIs from property getters to methods
    This is a normative change in the Intl Locale Info spec. See:
    https://github.com/tc39/proposal-intl-locale-info/commit/e550152

--- Userland/Libraries/LibJS/Tests/builtins/Intl/Locale/Locale.prototype.getWeekInfo.js
+++ Userland/Libraries/LibJS/Tests/builtins/Intl/Locale/Locale.prototype.getWeekInfo.js
@@ -1,14 +1,14 @@
 describe("errors", () => {
     test("called on non-Locale object", () => {
         expect(() => {
-            Intl.Locale.prototype.weekInfo;
+            Intl.Locale.prototype.getWeekInfo();
         }).toThrowWithMessage(TypeError, "Not an object of type Intl.Locale");
     });
 });
 
 describe("normal behavior", () => {
     test("basic functionality", () => {
-        const weekInfo = new Intl.Locale("en-US").weekInfo;
+        const weekInfo = new Intl.Locale("en-US").getWeekInfo();
 
         expect(weekInfo).toBeDefined();
         expect(Object.getPrototypeOf(weekInfo)).toBe(Object.prototype);
@@ -27,40 +27,40 @@ describe("normal behavior", () => {
     });
 
     test("regions with CLDR-specified firstDay", () => {
-        expect(new Intl.Locale("en-AG").weekInfo.firstDay).toBe(7);
-        expect(new Intl.Locale("en-SY").weekInfo.firstDay).toBe(6);
-        expect(new Intl.Locale("en-MV").weekInfo.firstDay).toBe(5);
+        expect(new Intl.Locale("en-AG").getWeekInfo().firstDay).toBe(7);
+        expect(new Intl.Locale("en-SY").getWeekInfo().firstDay).toBe(6);
+        expect(new Intl.Locale("en-MV").getWeekInfo().firstDay).toBe(5);
     });
 
     test("firstDay falls back to default region 001", () => {
-        expect(new Intl.Locale("en-AC").weekInfo.firstDay).toBe(1);
+        expect(new Intl.Locale("en-AC").getWeekInfo().firstDay).toBe(1);
     });
 
     test("regions with CLDR-specified weekend", () => {
-        expect(new Intl.Locale("en-AF").weekInfo.weekend).toEqual([4, 5]);
-        expect(new Intl.Locale("en-IN").weekInfo.weekend).toEqual([7]);
-        expect(new Intl.Locale("en-YE").weekInfo.weekend).toEqual([5, 6]);
+        expect(new Intl.Locale("en-AF").getWeekInfo().weekend).toEqual([4, 5]);
+        expect(new Intl.Locale("en-IN").getWeekInfo().weekend).toEqual([7]);
+        expect(new Intl.Locale("en-YE").getWeekInfo().weekend).toEqual([5, 6]);
     });
 
     test("weekend falls back to default region 001", () => {
-        expect(new Intl.Locale("en-AC").weekInfo.weekend).toEqual([6, 7]);
+        expect(new Intl.Locale("en-AC").getWeekInfo().weekend).toEqual([6, 7]);
     });
 
     test("regions with CLDR-specified minimalDays", () => {
-        expect(new Intl.Locale("en-AD").weekInfo.minimalDays).toBe(4);
-        expect(new Intl.Locale("en-CZ").weekInfo.minimalDays).toBe(4);
+        expect(new Intl.Locale("en-AD").getWeekInfo().minimalDays).toBe(4);
+        expect(new Intl.Locale("en-CZ").getWeekInfo().minimalDays).toBe(4);
     });
 
     test("minimalDays falls back to default region 001", () => {
-        expect(new Intl.Locale("en-AC").weekInfo.minimalDays).toBe(1);
+        expect(new Intl.Locale("en-AC").getWeekInfo().minimalDays).toBe(1);
     });
 
     test("likely regional subtags are added to locales without a region", () => {
-        const defaultRegion = new Intl.Locale("en-001").weekInfo;
+        const defaultRegion = new Intl.Locale("en-001").getWeekInfo();
 
         // "en" expands to "en-US" when likely subtags are added.
-        const en = new Intl.Locale("en").weekInfo;
-        const enUS = new Intl.Locale("en-US").weekInfo;
+        const en = new Intl.Locale("en").getWeekInfo();
+        const enUS = new Intl.Locale("en-US").getWeekInfo();
 
         expect(en).toEqual(enUS);
         expect(en).not.toEqual(defaultRegion);
