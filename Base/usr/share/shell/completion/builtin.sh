66955b82a7d623276bfe01ba45c5795ee22eb522
Adam Harald Jørgensen <58829763+adamjoer@users.noreply.github.com>
2023-11-13 01:11:46 +0100

    Base: Add completion for cd that suggests only directories

--- Base/usr/share/shell/completion/builtin.sh
+++ Base/usr/share/shell/completion/builtin.sh
@@ -74,3 +74,17 @@ _complete_kill() {
     __complete_job_spec $*[-1]
   }
 }
+
+_complete_cd() {
+    if test $*[-1] = '--' {
+        invariant_offset=0
+        results=${concat_lists .*/ */}
+    } else {
+        invariant_offset=${length "$*[-1]"}
+        results=$(glob "$*[-1]*/")
+    }
+
+    for $results {
+        echo '{"kind":"plain","static_offset":0,"invariant_offset":'"$invariant_offset"',"completion":"'"${remove_suffix / $it}"'","trailing_trivia":"/"}'
+    }
+}
