4cd1a2d319d51579acb94520e10f135fb4efeb83
Nico Weber <thakis@chromium.org>
2023-11-14 08:29:11 -0500

    LibPDF: Add some scaffolding for type 3 fonts

---
+++ Userland/Libraries/LibPDF/Fonts/Type3Font.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2023, Nico Weber <thakis@chromium.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibPDF/Fonts/SimpleFont.h>
+
+namespace PDF {
+
+class Type3Font : public SimpleFont {
+public:
+    Optional<float> get_glyph_width(u8 char_code) const override;
+    void set_font_size(float font_size) override;
+    PDFErrorOr<void> draw_glyph(Gfx::Painter& painter, Gfx::FloatPoint point, float width, u8 char_code, Color color) override;
+
+protected:
+    PDFErrorOr<void> initialize(Document*, NonnullRefPtr<DictObject> const&, float font_size) override;
+};
+
+}
