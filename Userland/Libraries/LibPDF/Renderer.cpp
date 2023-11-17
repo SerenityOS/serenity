SerenityOS/serenity

commit 2340e834cdcd18d6f9a79c5e0eb5325b69fec945

LibPDF: Cache fonts per page
Previously, every time a page switched fonts, we'd completely
re-parse the font.

Now, we cache fonts in Renderer, effectively caching them per page.

It'd be nice to have an LRU cache across pages too, but that's a
bigger change, and this already helps a lot.

Font size is part of the cache key, which means we re-parse the same
font at different font sizes. That could be better too, but again,
it's a big help as-is already.

Takes rendering the 1310 pages of the PDF 1.7 reference with

    Build/lagom/bin/pdf --debugging-stats \
        ~/Downloads/pdf_reference_1-7.pdf

from 71 s to 11s :^)

Going through pages especially in the index is noticeably snappier.

(On the PDF 2.0 spec, ISO_32000-2-2020_sponsored.pdf, it's less
dramatic: From 19s to 16s.)

Userland/Libraries/LibPDF/Renderer.cpp
@@ -402,19 +402,32 @@ RENDERER_HANDLER(text_set_leading)
     return {};
 }
 
+PDFErrorOr<NonnullRefPtr<PDFFont>> Renderer::get_font(FontCacheKey const& key, Optional<NonnullRefPtr<DictObject>> extra_resources)
+{
+    auto it = m_font_cache.find(key);
+    if (it != m_font_cache.end())
+        return it->value;
+
+    auto resources = extra_resources.value_or(m_page.resources);
+    auto fonts_dictionary = MUST(resources->get_dict(m_document, CommonNames::Font));
+    auto font_dictionary = MUST(fonts_dictionary->get_dict(m_document, key.font_dictionary_key));
+
+    auto font = TRY(PDFFont::create(m_document, font_dictionary, key.font_size));
+    m_font_cache.set(key, font);
+    return font;
+}
+
 RENDERER_HANDLER(text_set_font)
 {
-    auto resources = extra_resources.value_or(m_page.resources);
     auto target_font_name = MUST(m_document->resolve_to<NameObject>(args[0]))->name();
-    auto fonts_dictionary = MUST(resources->get_dict(m_document, CommonNames::Font));
-    auto font_dictionary = MUST(fonts_dictionary->get_dict(m_document, target_font_name));
 
     text_state().font_size = args[1].to_float();
 
     auto& text_rendering_matrix = calculate_text_rendering_matrix();
     auto font_size = text_rendering_matrix.x_scale() * text_state().font_size;
-    auto font = TRY(PDFFont::create(m_document, font_dictionary, font_size));
-    text_state().font = font;
+
+    FontCacheKey cache_key { target_font_name, font_size };
+    text_state().font = TRY(get_font(cache_key, extra_resources));
 
     m_text_rendering_matrix_is_dirty = true;
     return {};

Userland/Libraries/LibPDF/Renderer.h
@@ -99,6 +99,13 @@ class Renderer {
 public:
     static PDFErrorsOr<void> render(Document&, Page const&, RefPtr<Gfx::Bitmap>, RenderingPreferences preferences);
 
+    struct FontCacheKey {
+        DeprecatedString font_dictionary_key;
+        float font_size;
+
+        bool operator==(FontCacheKey const&) const = default;
+    };
+
 private:
     Renderer(RefPtr<Document>, Page const&, RefPtr<Gfx::Bitmap>, RenderingPreferences);
 
@@ -139,6 +146,8 @@ private:
     Gfx::AffineTransform const& calculate_text_rendering_matrix();
     Gfx::AffineTransform calculate_image_space_transformation(int width, int height);
 
+    PDFErrorOr<NonnullRefPtr<PDFFont>> get_font(FontCacheKey const&, Optional<NonnullRefPtr<DictObject>> extra_resources);
+
     RefPtr<Document> m_document;
     RefPtr<Gfx::Bitmap> m_bitmap;
     Page const& m_page;
@@ -153,11 +162,21 @@ private:
 
     bool m_text_rendering_matrix_is_dirty { true };
     Gfx::AffineTransform m_text_rendering_matrix;
+
+    HashMap<FontCacheKey, NonnullRefPtr<PDFFont>> m_font_cache;
 };
 
 }
 
 namespace AK {
+
+template<>
+struct Traits<PDF::Renderer::FontCacheKey> : public GenericTraits<PDF::Renderer::FontCacheKey> {
+    static unsigned hash(PDF::Renderer::FontCacheKey const& key)
+    {
+        return pair_int_hash(key.font_dictionary_key.hash(), int_hash(bit_cast<u32>(key.font_size)));
+    }
+};
 
 template<>
 struct Formatter<PDF::LineCapStyle> : Formatter<StringView> {

commit a8f11f7c7e25c53f19c78d61be111a522864359c

LibPDF: Use font dictionary object as cache key, not resource name
In the main page contents, /T0 might refer to a different font than
it might refer to in an XObject. So don't use the `Tf` argument as
font cache key. Instead, use the address of the font dictionary object.

Fixes false cache sharing, and also allows us to share cache entries
if the same font dict is referred to by two different names.

Fixes a regression from 2340e834cd (but keeps the speed-up intact).

Userland/Libraries/LibPDF/Renderer.cpp
@@ -406,17 +406,13 @@ RENDERER_HANDLER(text_set_leading)
     return {};
 }
 
-PDFErrorOr<NonnullRefPtr<PDFFont>> Renderer::get_font(FontCacheKey const& key, Optional<NonnullRefPtr<DictObject>> extra_resources)
+PDFErrorOr<NonnullRefPtr<PDFFont>> Renderer::get_font(FontCacheKey const& key)
 {
     auto it = m_font_cache.find(key);
     if (it != m_font_cache.end())
         return it->value;
 
-    auto resources = extra_resources.value_or(m_page.resources);
-    auto fonts_dictionary = MUST(resources->get_dict(m_document, CommonNames::Font));
-    auto font_dictionary = MUST(fonts_dictionary->get_dict(m_document, key.font_dictionary_key));
-
-    auto font = TRY(PDFFont::create(m_document, font_dictionary, key.font_size));
+    auto font = TRY(PDFFont::create(m_document, key.font_dictionary, key.font_size));
     m_font_cache.set(key, font);
     return font;
 }
@@ -430,8 +426,12 @@ RENDERER_HANDLER(text_set_font)
     auto& text_rendering_matrix = calculate_text_rendering_matrix();
     auto font_size = text_rendering_matrix.x_scale() * text_state().font_size;
 
-    FontCacheKey cache_key { target_font_name, font_size };
-    text_state().font = TRY(get_font(cache_key, extra_resources));
+    auto resources = extra_resources.value_or(m_page.resources);
+    auto fonts_dictionary = MUST(resources->get_dict(m_document, CommonNames::Font));
+    auto font_dictionary = MUST(fonts_dictionary->get_dict(m_document, target_font_name));
+
+    FontCacheKey cache_key { move(font_dictionary), font_size };
+    text_state().font = TRY(get_font(cache_key));
 
     m_text_rendering_matrix_is_dirty = true;
     return {};

Userland/Libraries/LibPDF/Renderer.h
@@ -101,7 +101,7 @@ public:
     static PDFErrorsOr<void> render(Document&, Page const&, RefPtr<Gfx::Bitmap>, RenderingPreferences preferences);
 
     struct FontCacheKey {
-        DeprecatedString font_dictionary_key;
+        NonnullRefPtr<DictObject> font_dictionary;
         float font_size;
 
         bool operator==(FontCacheKey const&) const = default;
@@ -147,7 +147,7 @@ private:
     Gfx::AffineTransform const& calculate_text_rendering_matrix();
     Gfx::AffineTransform calculate_image_space_transformation(int width, int height);
 
-    PDFErrorOr<NonnullRefPtr<PDFFont>> get_font(FontCacheKey const&, Optional<NonnullRefPtr<DictObject>> extra_resources);
+    PDFErrorOr<NonnullRefPtr<PDFFont>> get_font(FontCacheKey const&);
 
     RefPtr<Document> m_document;
     RefPtr<Gfx::Bitmap> m_bitmap;
@@ -175,7 +175,7 @@ template<>
 struct Traits<PDF::Renderer::FontCacheKey> : public DefaultTraits<PDF::Renderer::FontCacheKey> {
     static unsigned hash(PDF::Renderer::FontCacheKey const& key)
     {
-        return pair_int_hash(key.font_dictionary_key.hash(), int_hash(bit_cast<u32>(key.font_size)));
+        return pair_int_hash(ptr_hash(key.font_dictionary.ptr()), int_hash(bit_cast<u32>(key.font_size)));
     }
 };
 