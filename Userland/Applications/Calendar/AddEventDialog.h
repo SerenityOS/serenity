5a5adbbb9b2141c79bbd5f38240e3e9da4ef502c
david072 <david.g.ganz@gmail.com>
2023-11-17 21:51:13 +0100

    Calendar/AddEventDialog: Prevent inputting invalid date ranges
    The calendar now verifies that the user can't input invalid date ranges,
    i.e. date ranges where the end is before the start. The UI elements do
    this implicitly, by adjusting the values when changing the dates in an
    illegal way (e.g. when picking an end date that is before the start
    date, the end date will change to the start date).

--- Userland/Applications/Calendar/AddEventDialog.h
+++ Userland/Applications/Calendar/AddEventDialog.h
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2019-2020, Ryan Grieb <ryan.m.grieb@gmail.com>
  * Copyright (c) 2022-2023, the SerenityOS developers.
+ * Copyright (c) 2023, David Ganz <david.g.ganz@gmail.com>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -29,11 +30,21 @@ public:
 private:
     AddEventDialog(Core::DateTime date_time, EventManager& event_manager, Window* parent_window = nullptr);
 
-    ErrorOr<void> add_event_to_calendar();
+    ErrorOr<bool> add_event_to_calendar();
+
+    void update_start_date();
+    void update_end_date();
 
     Core::DateTime m_start_date_time;
     Core::DateTime m_end_date_time;
     EventManager& m_event_manager;
+
+    RefPtr<GUI::TextBox> m_start_date_box;
+    RefPtr<GUI::TextBox> m_end_date_box;
+    RefPtr<GUI::SpinBox> m_start_hour_box;
+    RefPtr<GUI::SpinBox> m_start_minute_box;
+    RefPtr<GUI::SpinBox> m_end_hour_box;
+    RefPtr<GUI::SpinBox> m_end_minute_box;
 };
 
 }
