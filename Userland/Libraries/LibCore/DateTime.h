b46df29c89d39f981f06216d71a0f3276344dade
david072 <david.g.ganz@gmail.com>
2023-11-17 19:23:43 +0100

    LibCore/DateTime: Add helper for setting only the date from another date

--- Userland/Libraries/LibCore/DateTime.h
+++ Userland/Libraries/LibCore/DateTime.h
@@ -32,6 +32,7 @@ public:
 
     void set_time(int year, int month = 1, int day = 1, int hour = 0, int minute = 0, int second = 0);
     void set_time_only(int hour, int minute, Optional<int> second = {});
+    void set_date(Core::DateTime const& other);
     ErrorOr<String> to_string(StringView format = "%Y-%m-%d %H:%M:%S"sv) const;
     DeprecatedString to_deprecated_string(StringView format = "%Y-%m-%d %H:%M:%S"sv) const;
 
