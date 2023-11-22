640c9ffc0f8f5fded4360489a8e7c5cdf5344d22
Timothy Flynn <trflynn89@pm.me>
2023-11-22 11:54:17 -0500

    LibWebView: Extract CSS to highlight HTML source to a constant
    We will be re-using (and extending) this style in the WebView-based DOM
    inspector.

--- Userland/Libraries/LibWebView/SourceHighlighter.h
+++ Userland/Libraries/LibWebView/SourceHighlighter.h
@@ -13,4 +13,50 @@ namespace WebView {
 
 String highlight_source(URL const&, StringView);
 
+constexpr inline StringView HTML_HIGHLIGHTER_STYLE = R"~~~(
+    .html {
+        font-size: 10pt;
+        font-family: Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
+    }
+
+    .tag {
+        font-weight: 600;
+    }
+
+    @media (prefers-color-scheme: dark) {
+        /* FIXME: We should be able to remove the HTML style when "color-scheme" is supported */
+        html {
+            background-color: rgb(30, 30, 30);
+            color: white;
+        }
+        .comment {
+            color: lightgreen;
+        }
+        .tag {
+            color: orangered;
+        }
+        .attribute-name {
+            color: orange;
+        }
+        .attribute-value {
+            color: deepskyblue;
+        }
+    }
+
+    @media (prefers-color-scheme: light) {
+        .comment {
+            color: green;
+        }
+        .tag {
+            color: red;
+        }
+        .attribute-name {
+            color: darkorange;
+        }
+        .attribute-value {
+            color: blue;
+        }
+    }
+)~~~"sv;
+
 }
