a9a3dcd9525c808eec37d98d53a9a98c7ec40913
Shannon Booth <shannon@serenityos.org>
2023-11-25 22:18:43 +1300

    LibWeb: Use ArrayBufferView in ReadableStreamBYOBRequest

--- Userland/Libraries/LibWeb/Streams/ReadableStreamBYOBRequest.cpp
+++ Userland/Libraries/LibWeb/Streams/ReadableStreamBYOBRequest.cpp
@@ -1,18 +1,20 @@
 /*
  * Copyright (c) 2023, Matthew Olsson <mattco@serenityos.org>
+ * Copyright (c) 2023, Shannon Booth <shannon@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #include <LibWeb/Streams/ReadableByteStreamController.h>
 #include <LibWeb/Streams/ReadableStreamBYOBRequest.h>
+#include <LibWeb/WebIDL/Buffers.h>
 
 namespace Web::Streams {
 
 JS_DEFINE_ALLOCATOR(ReadableStreamBYOBRequest);
 
 // https://streams.spec.whatwg.org/#rs-byob-request-view
-JS::GCPtr<JS::TypedArrayBase> ReadableStreamBYOBRequest::view()
+JS::GCPtr<WebIDL::ArrayBufferView> ReadableStreamBYOBRequest::view()
 {
     // 1. Return this.[[view]].
     return m_view;
