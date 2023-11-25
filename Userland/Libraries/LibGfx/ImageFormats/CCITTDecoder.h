64912d4d0246cfbcd0a4e83508db6740d4fa900f
Lucas CHOLLET <lucas.chollet@free.fr>
2023-11-25 16:34:56 -0500

    LibGfx/TIFF: Add support for images with CCITT3 1D compression
    This compression (tag Compression=2) is not very popular on its own, but
    a base to implement CCITT3 2D and CCITT4 compressions.
    
    As the format has no real benefits, it is quite hard to find an app that
    accepts tho encode that for you. So I used the following program that
    calls `libtiff` directly:
    ```cpp
    #include <vector>
    #include <cstdlib>
    #include <iostream>
    
    #include <tiffio.h>
    
    // An array containing 0 and 1 of length width * height.
    extern std::vector<uint8_t> array;
    int main() {
        // From: https://stackoverflow.com/a/34257789
    
        TIFF *image = TIFFOpen("input.tif", "w");
        int const width = 400;
        int const height = 300;
        TIFFSetField(image, TIFFTAG_IMAGEWIDTH, width);
        TIFFSetField(image, TIFFTAG_IMAGELENGTH, height);
        TIFFSetField(image, TIFFTAG_PHOTOMETRIC, 0);
        TIFFSetField(image, TIFFTAG_COMPRESSION, COMPRESSION_CCITTRLE);
    
        TIFFSetField(image, TIFFTAG_BITSPERSAMPLE, 1);
        TIFFSetField(image, TIFFTAG_SAMPLESPERPIXEL, 1);
        TIFFSetField(image, TIFFTAG_ROWSPERSTRIP, 1);
    
        std::vector<uint8_t> scan_line(width / 8 + 8, 0);
        int count = 0;
        for (int i = 0; i < height; i++) {
            std::fill(scan_line.begin(), scan_line.end(), 0);
            for (int x = 0; x < width; ++x) {
                uint8_t eight_pixels = scan_line.at(x / 8);
                eight_pixels = eight_pixels << 1;
                eight_pixels |= !array.at(i * width + x);
                scan_line.at(x / 8) = eight_pixels;
            }
            int bytes = int(width / 8.0 + 0.5);
            if (TIFFWriteScanline(image, scan_line.data(), i, bytes) != 1)
                std::cerr << "Something went wrong\n";
        }
    
        TIFFClose(image);
    }
    ```

---
+++ Userland/Libraries/LibGfx/ImageFormats/CCITTDecoder.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2023, Lucas Chollet <lucas.chollet@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/ByteBuffer.h>
+
+namespace Gfx::CCITT {
+
+// You can find a great overview of CCITT compression schemes here:
+// https://www.fileformat.info/mirror/egff/ch09_05.htm
+
+// The CCITT3 specification is accessible at this page:
+// https://www.itu.int/rec/T-REC-T.4/en
+
+// The unidimensional scheme is originally described in:
+// 4.1 One-dimensional coding scheme
+// However, this function implements the TIFF variant (see TIFFLoader.h for a spec link),
+// differences are detailed in section:
+// Section 10: Modified Huffman Compression
+ErrorOr<ByteBuffer> decode_ccitt3_1d(ReadonlyBytes bytes, u32 image_width, u32 image_height);
+
+}
