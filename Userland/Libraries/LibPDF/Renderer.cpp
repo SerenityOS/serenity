SerenityOS/serenity

commit c8510b58a36632096784c984d391e655ebb1fb23

LibPDF: Cache fonts per page
Previously, every time a page switched fonts, we'd completely
re-parse the font.

Now, we cache fonts in Renderer, effectively caching them per page.

It'd be nice to have an LRU cache across pages too, but that's a
bigger change, and this already helps a lot.

Font size is part of the cache key, which means we re-parse the same
font at different font sizes. That could be better too, but again,
it's a big help as-is already.

Takes rendering the 1310 pages of the PDF 1.7 reference with

    Build/lagom/bin/pdf --debugging-stats \
        ~/Downloads/pdf_reference_1-7.pdf

from 71 s to 11s :^)

Going through pages especially in the index is noticeably snappier.

(On the PDF 2.0 spec, ISO_32000-2-2020_sponsored.pdf, it's less
dramatic: From 19s to 16s.)

Userland/Libraries/LibPDF/Renderer.cpp
@@ -402,19 +402,32 @@ RENDERER_HANDLER(text_set_leading)
     return {};
 }
 
+PDFErrorOr<NonnullRefPtr<PDFFont>> Renderer::get_font(FontCacheKey const& key, Optional<NonnullRefPtr<DictObject>> extra_resources)
+{
+    auto it = m_font_cache.find(key);
+    if (it != m_font_cache.end())
+        return it->value;
+
+    auto resources = extra_resources.value_or(m_page.resources);
+    auto fonts_dictionary = MUST(resources->get_dict(m_document, CommonNames::Font));
+    auto font_dictionary = MUST(fonts_dictionary->get_dict(m_document, key.font_dictionary_key));
+
+    auto font = TRY(PDFFont::create(m_document, font_dictionary, key.font_size));
+    m_font_cache.set(key, font);
+    return font;
+}
+
 RENDERER_HANDLER(text_set_font)
 {
-    auto resources = extra_resources.value_or(m_page.resources);
     auto target_font_name = MUST(m_document->resolve_to<NameObject>(args[0]))->name();
-    auto fonts_dictionary = MUST(resources->get_dict(m_document, CommonNames::Font));
-    auto font_dictionary = MUST(fonts_dictionary->get_dict(m_document, target_font_name));
 
     text_state().font_size = args[1].to_float();
 
     auto& text_rendering_matrix = calculate_text_rendering_matrix();
     auto font_size = text_rendering_matrix.x_scale() * text_state().font_size;
-    auto font = TRY(PDFFont::create(m_document, font_dictionary, font_size));
-    text_state().font = font;
+
+    FontCacheKey cache_key { target_font_name, font_size };
+    text_state().font = TRY(get_font(cache_key, extra_resources));
 
     m_text_rendering_matrix_is_dirty = true;
     return {};

Userland/Libraries/LibPDF/Renderer.h
@@ -99,6 +99,13 @@ class Renderer {
 public:
     static PDFErrorsOr<void> render(Document&, Page const&, RefPtr<Gfx::Bitmap>, RenderingPreferences preferences);
 
+    struct FontCacheKey {
+        DeprecatedString font_dictionary_key;
+        float font_size;
+
+        bool operator==(FontCacheKey const&) const = default;
+    };
+
 private:
     Renderer(RefPtr<Document>, Page const&, RefPtr<Gfx::Bitmap>, RenderingPreferences);
 
@@ -139,6 +146,8 @@ private:
     Gfx::AffineTransform const& calculate_text_rendering_matrix();
     Gfx::AffineTransform calculate_image_space_transformation(int width, int height);
 
+    PDFErrorOr<NonnullRefPtr<PDFFont>> get_font(FontCacheKey const&, Optional<NonnullRefPtr<DictObject>> extra_resources);
+
     RefPtr<Document> m_document;
     RefPtr<Gfx::Bitmap> m_bitmap;
     Page const& m_page;
@@ -153,11 +162,21 @@ private:
 
     bool m_text_rendering_matrix_is_dirty { true };
     Gfx::AffineTransform m_text_rendering_matrix;
+
+    HashMap<FontCacheKey, NonnullRefPtr<PDFFont>> m_font_cache;
 };
 
 }
 
 namespace AK {
+
+template<>
+struct Traits<PDF::Renderer::FontCacheKey> : public GenericTraits<PDF::Renderer::FontCacheKey> {
+    static unsigned hash(PDF::Renderer::FontCacheKey const& key)
+    {
+        return pair_int_hash(key.font_dictionary_key.hash(), int_hash(bit_cast<u32>(key.font_size)));
+    }
+};
 
 template<>
 struct Formatter<PDF::LineCapStyle> : Formatter<StringView> {

commit 010bc0944efa9b4317d0bd935b504ee1e86211ac

LibPDF: Update font size after getting font from cache
Page 1 of 0000277.pdf does:

    BT 22 0 0 22  59  28 Tm /TT2 1 Tf
        (Presented at Photonics West OPTO, February 17, 2016) Tj ET
    BT 32 0 0 32 269 426 Tm /TT1 1 Tf
        (Robert W. Boyd) Tj ET
    BT 22 0 0 22 253 357 Tm /TT2 1 Tf
        (Department of Physics and) Tj ET
    BT 22 0 0 22 105 326 Tm /TT2 1 Tf
        (Max-Planck Centre for Extreme and Quantum Photonics) Tj ET

Every line begins a text operation, then updates the font matrix,
selects a font (TT2, TT1, TT2, TT1), draws some text and ends the text
operation.

`Tm` (which sets the font matrix) contains a scale, and uses that
to update the font size of the currently-active font (cf #20084).
But in this file, we `Tm` first and `Tf` (font selection) second,
so this updates the size of the old font. So when we pull it out
of the cache again on line 3, it would still have the old size
from the `Tm` on line 2.

(The whole text scaling logic in LibPDF imho needs a rethink; the
current approach also causes issues with zero-width glyphs which
currently lead to divisions by zero. But that's for another PR.)

Fixes another regression from c8510b58a366320 (which I've accidentally
referred to by 2340e834cd in another commit).

Userland/Libraries/LibPDF/Renderer.cpp
@@ -433,8 +433,11 @@ RENDERER_HANDLER(text_set_leading)
 PDFErrorOr<NonnullRefPtr<PDFFont>> Renderer::get_font(FontCacheKey const& key)
 {
     auto it = m_font_cache.find(key);
-    if (it != m_font_cache.end())
+    if (it != m_font_cache.end()) {
+        // Update the potentially-stale size set in text_set_matrix_and_line_matrix().
+        it->value->set_font_size(key.font_size);
         return it->value;
+    }
 
     auto font = TRY(PDFFont::create(m_document, key.font_dictionary, key.font_size));
     m_font_cache.set(key, font);