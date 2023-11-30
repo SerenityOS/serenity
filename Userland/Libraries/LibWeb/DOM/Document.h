2805e16c1c1658c65fe5dabaa63b4a0e98346f1f
Sam Atkins <atkinssj@serenityos.org>
2023-11-30 21:09:36 +0000

    LibWeb: Let NavigableContainers delay the document load event

--- Userland/Libraries/LibWeb/DOM/Document.h
+++ Userland/Libraries/LibWeb/DOM/Document.h
@@ -473,6 +473,7 @@ public:
     Vector<JS::Handle<HTML::BrowsingContext>> list_of_descendant_browsing_contexts() const;
 
     Vector<JS::Handle<HTML::Navigable>> descendant_navigables();
+    Vector<JS::Handle<HTML::Navigable>> const descendant_navigables() const;
     Vector<JS::Handle<HTML::Navigable>> inclusive_descendant_navigables();
     Vector<JS::Handle<HTML::Navigable>> ancestor_navigables();
     Vector<JS::Handle<HTML::Navigable>> inclusive_ancestor_navigables();
