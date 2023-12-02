c4d31344366ec583becaa8681338a92da68eddba
Shannon Booth <shannon@serenityos.org>
2023-12-03 08:49:15 +1300

    LibWeb: Port NavigatorLanguage from DeprecatedString

--- Userland/Libraries/LibWeb/HTML/NavigatorLanguage.h
+++ Userland/Libraries/LibWeb/HTML/NavigatorLanguage.h
@@ -6,7 +6,7 @@
 
 #pragma once
 
-#include <AK/DeprecatedString.h>
+#include <AK/String.h>
 #include <AK/Vector.h>
 
 namespace Web::HTML {
@@ -14,15 +14,15 @@ namespace Web::HTML {
 class NavigatorLanguageMixin {
 public:
     // https://html.spec.whatwg.org/multipage/system-state.html#dom-navigator-language
-    DeprecatedString language() const { return m_current_preferred_languages[0]; }
+    String language() const { return m_current_preferred_languages[0]; }
 
     // https://html.spec.whatwg.org/multipage/system-state.html#dom-navigator-languages
     // FIXME: The same object must be returned until the user agent needs to return different values, or values in a
     //        different order.
-    Vector<DeprecatedString> languages() const { return m_current_preferred_languages; }
+    Vector<String> languages() const { return m_current_preferred_languages; }
 
 protected:
-    Vector<DeprecatedString> m_current_preferred_languages = { "en-US" };
+    Vector<String> m_current_preferred_languages = { "en-US"_string };
 };
 
 }
