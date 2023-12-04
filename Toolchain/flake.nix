20162874e04db8e218323602d8d7bdcc1b22afb9
networkException <git@nwex.de>
2023-12-04 02:02:46 +0100

    Toolchain: Use nixos-unstable instead of nixpkgs master in flake
    This patch updates the flake to track the nixos-unstable branch instead
    of master for the nixpkgs flake input.
    
    This ensures that hydra.nixos.org (the NixOS CI) has built all packages,
    made them available in the cache.nixos.org binary cache without the risk
    of a definitions (evalulation) being broken on master.

--- Toolchain/flake.nix
+++ Toolchain/flake.nix
@@ -2,7 +2,7 @@
   description = "Serenity OS";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, flake-utils, nixpkgs }:
