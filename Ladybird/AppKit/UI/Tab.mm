980e32b4fe2a5df2c803d827c86ef973af8ec943
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2023-12-04 19:55:20 +0100

    Ladybird/AppKit: Remove theme color changing background color

--- Ladybird/AppKit/UI/Tab.mm
+++ Ladybird/AppKit/UI/Tab.mm
@@ -100,8 +100,6 @@ static constexpr CGFloat const WINDOW_HEIGHT = 800;
                  object:[scroll_view contentView]];
 
         [self setContentView:scroll_view];
-        self.backgroundColor = [NSColor windowBackgroundColor];
-        self.titlebarAppearsTransparent = YES;
     }
 
     return self;
@@ -226,7 +224,6 @@ static constexpr CGFloat const WINDOW_HEIGHT = 800;
 - (void)onLoadStart:(URL const&)url isRedirect:(BOOL)is_redirect
 {
     if (url != self.last_url) {
-        self.backgroundColor = [NSColor windowBackgroundColor];
         self.last_url = url;
     }
 
@@ -256,23 +253,6 @@ static constexpr CGFloat const WINDOW_HEIGHT = 800;
 
     self.title = Ladybird::string_to_ns_string(title);
     [self updateTabTitleAndFavicon];
-}
-
-- (void)onThemeColorChange:(Color)color
-{
-    auto* nscolor = [NSColor colorWithRed:((CGFloat)color.red()) / 255.0
-                                    green:((CGFloat)color.green()) / 255.0
-                                     blue:((CGFloat)color.blue()) / 255.0
-                                    alpha:1.0];
-    CGFloat hue = 0.0;
-    CGFloat saturation = 0.0;
-    CGFloat brightness = 0.0;
-    [nscolor getHue:&hue saturation:&saturation brightness:&brightness alpha:nil];
-    if (brightness > 0.75)
-        brightness = 0.75;
-    nscolor = [NSColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1.0];
-    self.backgroundColor = nscolor;
-    self.titlebarAppearsTransparent = YES;
 }
 
 - (void)onFaviconChange:(Gfx::Bitmap const&)bitmap
