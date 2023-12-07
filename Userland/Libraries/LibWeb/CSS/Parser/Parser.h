90139d143226f734fac78acfff5ef90bf8e0c538
Sam Atkins <atkinssj@serenityos.org>
2023-12-07 17:42:43 +0000

    LibWeb: Parse grid-auto-flow property using TokenStream
    RefPtr is already `[[nodiscard]]` so we can remove that extra noise from
    the declaration.

--- Userland/Libraries/LibWeb/CSS/Parser/Parser.h
+++ Userland/Libraries/LibWeb/CSS/Parser/Parser.h
@@ -260,7 +260,7 @@ private:
     RefPtr<StyleValue> parse_transform_origin_value(Vector<ComponentValue> const&);
     RefPtr<StyleValue> parse_grid_track_size_list(Vector<ComponentValue> const&, bool allow_separate_line_name_blocks = false);
     RefPtr<StyleValue> parse_grid_auto_track_sizes(Vector<ComponentValue> const&);
-    [[nodiscard]] RefPtr<GridAutoFlowStyleValue> parse_grid_auto_flow_value(Vector<ComponentValue> const&);
+    RefPtr<GridAutoFlowStyleValue> parse_grid_auto_flow_value(TokenStream<ComponentValue>&);
     RefPtr<StyleValue> parse_grid_track_size_list_shorthand_value(PropertyID, Vector<ComponentValue> const&);
     RefPtr<StyleValue> parse_grid_track_placement(Vector<ComponentValue> const&);
     RefPtr<StyleValue> parse_grid_track_placement_shorthand_value(PropertyID, TokenStream<ComponentValue>&);
