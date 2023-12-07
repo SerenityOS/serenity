a1de20c561fd6fa413ea510017808cca2ee920e6
Sam Atkins <atkinssj@serenityos.org>
2023-12-07 15:21:17 +0000

    LibWeb: Parse background property using TokenStream

--- Userland/Libraries/LibWeb/CSS/Parser/Parser.h
+++ Userland/Libraries/LibWeb/CSS/Parser/Parser.h
@@ -227,7 +227,7 @@ private:
     RefPtr<StyleValue> parse_simple_comma_separated_value_list(PropertyID, TokenStream<ComponentValue>&);
 
     RefPtr<StyleValue> parse_aspect_ratio_value(TokenStream<ComponentValue>&);
-    RefPtr<StyleValue> parse_background_value(Vector<ComponentValue> const&);
+    RefPtr<StyleValue> parse_background_value(TokenStream<ComponentValue>&);
     RefPtr<StyleValue> parse_single_background_position_x_or_y_value(TokenStream<ComponentValue>&, PropertyID);
     RefPtr<StyleValue> parse_single_background_repeat_value(TokenStream<ComponentValue>&);
     RefPtr<StyleValue> parse_single_background_size_value(TokenStream<ComponentValue>&);
