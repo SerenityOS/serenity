a8f6bfae431fb10069d1d4bfcbe2ea900b848b79
Lucas CHOLLET <lucas.chollet@free.fr>
2023-12-08 23:02:22 -0500

    LibGfx/TIFF: Add support for bit-depth up to 32 bits per sample
    This makes us support every "minisblack" and "rgb-contig" images from
    the depth folder of libtiff's test suite:
    https://libtiff.gitlab.io/libtiff/images.html

--- Tests/LibGfx/TestImageDecoder.cpp
+++ Tests/LibGfx/TestImageDecoder.cpp
@@ -431,6 +431,18 @@ TEST_CASE(test_tiff_grayscale)
     EXPECT_EQ(frame.image->get_pixel(60, 75), Gfx::Color(130, 130, 130));
 }
 
+TEST_CASE(test_tiff_16_bits)
+{
+    auto file = MUST(Core::MappedFile::map(TEST_INPUT("tiff/16_bits.tiff"sv)));
+    EXPECT(Gfx::TIFFImageDecoderPlugin::sniff(file->bytes()));
+    auto plugin_decoder = TRY_OR_FAIL(Gfx::TIFFImageDecoderPlugin::create(file->bytes()));
+
+    auto frame = TRY_OR_FAIL(expect_single_frame_of_size(*plugin_decoder, { 400, 300 }));
+
+    EXPECT_EQ(frame.image->get_pixel(0, 0), Gfx::Color::NamedColor::White);
+    EXPECT_EQ(frame.image->get_pixel(60, 75), Gfx::Color::NamedColor::Red);
+}
+
 TEST_CASE(test_webp_simple_lossy)
 {
     auto file = MUST(Core::MappedFile::map(TEST_INPUT("webp/simple-vp8.webp"sv)));
