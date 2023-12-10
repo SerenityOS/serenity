afb99ae064626ad844eb2a9d8b6d2490511b5a67
Shannon Booth <shannon@serenityos.org>
2023-12-10 16:18:26 +1300

    LibWeb: Avoid calling FlyString::from_utf8 on FlyString's

--- Userland/Libraries/LibWeb/CSS/Parser/SelectorParsing.cpp
+++ Userland/Libraries/LibWeb/CSS/Parser/SelectorParsing.cpp
@@ -142,8 +142,8 @@ Optional<Selector::SimpleSelector::QualifiedName> Parser::parse_selector_qualifi
     };
     auto get_name = [](ComponentValue const& token) {
         if (token.is_delim('*'))
-            return FlyString::from_utf8("*"sv);
-        return FlyString::from_utf8(token.token().ident());
+            return "*"_fly_string;
+        return token.token().ident();
     };
 
     // There are 3 possibilities here:
@@ -167,7 +167,7 @@ Optional<Selector::SimpleSelector::QualifiedName> Parser::parse_selector_qualifi
             transaction.commit();
             return Selector::SimpleSelector::QualifiedName {
                 .namespace_type = Selector::SimpleSelector::QualifiedName::NamespaceType::None,
-                .name = get_name(name_token).release_value_but_fixme_should_propagate_errors(),
+                .name = get_name(name_token),
             };
         }
         return {};
@@ -179,8 +179,8 @@ Optional<Selector::SimpleSelector::QualifiedName> Parser::parse_selector_qualifi
     if (tokens.peek_token().is_delim('|') && is_name(tokens.peek_token(1))) {
         // Case 2: `<namespace>|<name>`
         (void)tokens.next_token(); // `|`
-        auto namespace_ = get_name(first_token).release_value_but_fixme_should_propagate_errors();
-        auto name = get_name(tokens.next_token()).release_value_but_fixme_should_propagate_errors();
+        auto namespace_ = get_name(first_token);
+        auto name = get_name(tokens.next_token());
 
         if (allow_wildcard_name == AllowWildcardName::No && name == "*"sv)
             return {};
@@ -205,7 +205,7 @@ Optional<Selector::SimpleSelector::QualifiedName> Parser::parse_selector_qualifi
     transaction.commit();
     return Selector::SimpleSelector::QualifiedName {
         .namespace_type = Selector::SimpleSelector::QualifiedName::NamespaceType::Default,
-        .name = get_name(name_token).release_value_but_fixme_should_propagate_errors(),
+        .name = get_name(name_token),
     };
 }
 
@@ -550,7 +550,7 @@ Parser::ParseErrorOr<Selector::SimpleSelector> Parser::parse_pseudo_simple_selec
                 }
 
                 auto language_string = language_token.is(Token::Type::String) ? language_token.token().string() : language_token.token().ident();
-                languages.append(MUST(FlyString::from_utf8(language_string)));
+                languages.append(language_string);
 
                 language_token_stream.skip_whitespace();
                 if (language_token_stream.has_next_token()) {
@@ -629,7 +629,7 @@ Parser::ParseErrorOr<Optional<Selector::SimpleSelector>> Parser::parse_simple_se
             }
             return Selector::SimpleSelector {
                 .type = Selector::SimpleSelector::Type::Class,
-                .value = Selector::SimpleSelector::Name { FlyString::from_utf8(class_name_value.token().ident()).release_value_but_fixme_should_propagate_errors() }
+                .value = Selector::SimpleSelector::Name { class_name_value.token().ident() }
             };
         }
         case '>':
@@ -653,7 +653,7 @@ Parser::ParseErrorOr<Optional<Selector::SimpleSelector>> Parser::parse_simple_se
         }
         return Selector::SimpleSelector {
             .type = Selector::SimpleSelector::Type::Id,
-            .value = Selector::SimpleSelector::Name { FlyString::from_utf8(first_value.token().hash_value()).release_value_but_fixme_should_propagate_errors() }
+            .value = Selector::SimpleSelector::Name { first_value.token().hash_value() }
         };
     }
 
