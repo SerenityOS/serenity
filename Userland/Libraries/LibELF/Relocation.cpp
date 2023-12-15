d869500246790ba69170b0ed4467e382cf0fe2bc
Sönke Holz <sholz8530@gmail.com>
2023-12-15 19:17:52 +0100

    WIP: Relocations (1/N)

--- Userland/Libraries/LibELF/Relocation.cpp
+++ Userland/Libraries/LibELF/Relocation.cpp
@@ -59,7 +59,7 @@ bool perform_relative_relocations(FlatPtr base_address)
     for (unsigned i = 0; i < relocation_count; ++i) {
         size_t offset_in_section = i * relocation_entry_size;
         auto* relocation = (Elf_Rela*)(relocation_section_addr + offset_in_section);
-        VERIFY(ELF64_R_TYPE(relocation->r_info) == R_X86_64_RELATIVE || ELF64_R_TYPE(relocation->r_info) == R_AARCH64_RELATIVE);
+        VERIFY(ELF64_R_TYPE(relocation->r_info) == R_X86_64_RELATIVE || ELF64_R_TYPE(relocation->r_info) == R_AARCH64_RELATIVE || ELF64_R_TYPE(relocation->r_info) == R_RISCV_RELATIVE);
         auto* patch_address = (FlatPtr*)(base_address + relocation->r_offset);
         FlatPtr relocated_address;
         if (use_addend) {
