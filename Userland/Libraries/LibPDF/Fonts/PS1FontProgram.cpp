SerenityOS/serenity

commit 2b3a41be74a3e80929c538ad2cd669171e9c1435

LibPDF: Remove the subroutine length limit for PS1 font programs
A limit of 1024 subroutines seemed like a sensible choice, but some
fonts actually do exceed it. We will now only assert that the specified
amount is positive.

Userland/Libraries/LibPDF/Fonts/PS1FontProgram.cpp
@@ -108,7 +108,7 @@ PDFErrorOr<Vector<ByteBuffer>> PS1FontProgram::parse_subroutines(Reader& reader)
         return error("Expected array length");
 
     auto length = TRY(parse_int(reader));
-    VERIFY(length <= 1024);
+    VERIFY(length > 0);
 
     Vector<ByteBuffer> array;
     TRY(array.try_resize(length));

commit 71a1eb777734d1b529abf7c567dc6c69f0833108

LibPDF: Tolerate 0-sized Subrs in PS1 font subprograms
This regressed in 2b3a41be74a3e8 in #18031.

Fixes a crash rendering page 2 and onward of
https://pyx-project.org/presentation_dantemv35_en.pdf

Userland/Libraries/LibPDF/Fonts/PS1FontProgram.cpp
@@ -107,7 +107,7 @@ PDFErrorOr<Vector<ByteBuffer>> PS1FontProgram::parse_subroutines(Reader& reader)
         return error("Expected array length");
 
     auto length = TRY(parse_int(reader));
-    VERIFY(length > 0);
+    VERIFY(length >= 0);
 
     Vector<ByteBuffer> array;
     TRY(array.try_resize(length));