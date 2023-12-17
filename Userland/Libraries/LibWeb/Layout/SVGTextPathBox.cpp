809c5b0b038d0f94fd13ba40a03b08c3be55ee07
MacDue <macdue@dueutil.tech>
2023-12-17 18:35:21 +0000

    LibWeb: Add initial support for SVG `<textPath>`
    This patch adds basic support for the SVG `<textPath>`, so it supports
    placing text along a path, but none of the extra attributes for
    controlling the layout of the text. This is enough to correctly display
    the MDN example.

---
+++ Userland/Libraries/LibWeb/Layout/SVGTextPathBox.cpp
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2023, MacDue <macdue@dueutil.tech>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibWeb/Layout/SVGTextPathBox.h>
+#include <LibWeb/Painting/SVGPathPaintable.h>
+
+namespace Web::Layout {
+
+SVGTextPathBox::SVGTextPathBox(DOM::Document& document, SVG::SVGTextPathElement& element, NonnullRefPtr<CSS::StyleProperties> properties)
+    : SVGGraphicsBox(document, element, properties)
+{
+}
+
+JS::GCPtr<Painting::Paintable> SVGTextPathBox::create_paintable() const
+{
+    return Painting::SVGPathPaintable::create(*this);
+}
+
+}
---
+++ Userland/Libraries/LibWeb/Layout/SVGTextPathBox.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2023, MacDue <macdue@dueutil.tech>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibWeb/Layout/SVGGraphicsBox.h>
+#include <LibWeb/SVG/SVGTextPathElement.h>
+
+namespace Web::Layout {
+
+class SVGTextPathBox final : public SVGGraphicsBox {
+    JS_CELL(SVGTextPathBox, SVGGraphicsBox);
+
+public:
+    SVGTextPathBox(DOM::Document&, SVG::SVGTextPathElement&, NonnullRefPtr<CSS::StyleProperties>);
+    virtual ~SVGTextPathBox() override = default;
+
+    SVG::SVGTextPathElement& dom_node() { return static_cast<SVG::SVGTextPathElement&>(SVGGraphicsBox::dom_node()); }
+    SVG::SVGTextPathElement const& dom_node() const { return static_cast<SVG::SVGTextPathElement const&>(SVGGraphicsBox::dom_node()); }
+
+    virtual JS::GCPtr<Painting::Paintable> create_paintable() const override;
+
+private:
+    CSSPixelPoint viewbox_origin() const;
+};
+
+}
