73c8b4865e4c76392eea28dd34384454dff807fd
Lucas CHOLLET <lucas.chollet@free.fr>
2023-12-23 00:09:26 -0500

    LibGfx/TIFF: Add AdobeDeflate compression support
    This new compression is quite popular and uses a basic Zlib compression
    to compress strips. Note that this is not part of the original TIFF
    specification but in the Technical Notes from 2002:
    https://web.archive.org/web/20160305055905/http://partners.adobe.com/public/developer/en/tiff/TIFFphotoshop.pdf
    
    The test case was generated with GIMP.

--- Tests/LibGfx/TestImageDecoder.cpp
+++ Tests/LibGfx/TestImageDecoder.cpp
@@ -462,6 +462,18 @@ TEST_CASE(test_tiff_lzw)
     EXPECT_EQ(frame.image->get_pixel(60, 75), Gfx::Color::NamedColor::Red);
 }
 
+TEST_CASE(test_tiff_defalte)
+{
+    auto file = MUST(Core::MappedFile::map(TEST_INPUT("tiff/deflate.tiff"sv)));
+    EXPECT(Gfx::TIFFImageDecoderPlugin::sniff(file->bytes()));
+    auto plugin_decoder = TRY_OR_FAIL(Gfx::TIFFImageDecoderPlugin::create(file->bytes()));
+
+    auto frame = TRY_OR_FAIL(expect_single_frame_of_size(*plugin_decoder, { 400, 300 }));
+
+    EXPECT_EQ(frame.image->get_pixel(0, 0), Gfx::Color::NamedColor::White);
+    EXPECT_EQ(frame.image->get_pixel(60, 75), Gfx::Color::NamedColor::Red);
+}
+
 TEST_CASE(test_tiff_packed_bits)
 {
     auto file = MUST(Core::MappedFile::map(TEST_INPUT("tiff/packed_bits.tiff"sv)));
