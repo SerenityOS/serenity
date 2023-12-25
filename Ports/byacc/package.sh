deb3e1eb7a1e54f2161225b1e75c30374486f06a
Mr.UNIX <mrunix00@protonmail.com>
2023-12-25 12:36:29 +0100

    Ports: Bump byacc to 20230521

--- Ports/byacc/package.sh
+++ Ports/byacc/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=byacc
-version=20220128
+version=20230521
 files=(
-    "https://invisible-mirror.net/archives/byacc/byacc-${version}.tgz#42c1805cc529314e6a76326fe1b33e80c70862a44b01474da362e2f7db2d749c"
+    "https://invisible-mirror.net/archives/byacc/byacc-${version}.tgz#5ad915a7d5833aa38a5e31bd077505666029c35e365dff8569fe4598eaa9fef2"
 )
 useconfigure=true
