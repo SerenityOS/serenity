bf05aa88bc8aeb1bea100978c008ce2f22fa2687
Sam Atkins <atkinssj@serenityos.org>
2023-12-28 17:37:37 +0000

    LibWeb: Add method for converting a FooOrCalculated to a StyleValue

--- Userland/Libraries/LibWeb/CSS/CalculatedOr.cpp
+++ Userland/Libraries/LibWeb/CSS/CalculatedOr.cpp
@@ -5,6 +5,15 @@
  */
 
 #include "CalculatedOr.h"
+#include <LibWeb/CSS/StyleValues/AngleStyleValue.h>
+#include <LibWeb/CSS/StyleValues/FlexStyleValue.h>
+#include <LibWeb/CSS/StyleValues/FrequencyStyleValue.h>
+#include <LibWeb/CSS/StyleValues/IntegerStyleValue.h>
+#include <LibWeb/CSS/StyleValues/LengthStyleValue.h>
+#include <LibWeb/CSS/StyleValues/NumberStyleValue.h>
+#include <LibWeb/CSS/StyleValues/PercentageStyleValue.h>
+#include <LibWeb/CSS/StyleValues/ResolutionStyleValue.h>
+#include <LibWeb/CSS/StyleValues/TimeStyleValue.h>
 
 namespace Web::CSS {
 
@@ -13,9 +22,19 @@ Angle AngleOrCalculated::resolve_calculated(NonnullRefPtr<CalculatedStyleValue> 
     return calculated->resolve_angle().value();
 }
 
+NonnullRefPtr<StyleValue> AngleOrCalculated::create_style_value() const
+{
+    return AngleStyleValue::create(value());
+}
+
 Flex FlexOrCalculated::resolve_calculated(NonnullRefPtr<CalculatedStyleValue> const& calculated, Layout::Node const&) const
 {
     return calculated->resolve_flex().value();
+}
+
+NonnullRefPtr<StyleValue> FlexOrCalculated::create_style_value() const
+{
+    return FlexStyleValue::create(value());
 }
 
 Frequency FrequencyOrCalculated::resolve_calculated(NonnullRefPtr<CalculatedStyleValue> const& calculated, Layout::Node const&) const
@@ -23,9 +42,19 @@ Frequency FrequencyOrCalculated::resolve_calculated(NonnullRefPtr<CalculatedStyl
     return calculated->resolve_frequency().value();
 }
 
+NonnullRefPtr<StyleValue> FrequencyOrCalculated::create_style_value() const
+{
+    return FrequencyStyleValue::create(value());
+}
+
 i64 IntegerOrCalculated::resolve_calculated(NonnullRefPtr<CalculatedStyleValue> const& calculated, Layout::Node const&) const
 {
     return calculated->resolve_integer().value();
+}
+
+NonnullRefPtr<StyleValue> IntegerOrCalculated::create_style_value() const
+{
+    return IntegerStyleValue::create(value());
 }
 
 Length LengthOrCalculated::resolve_calculated(NonnullRefPtr<CalculatedStyleValue> const& calculated, Layout::Node const& layout_node) const
@@ -40,9 +69,19 @@ Length LengthOrCalculated::resolved(Length::ResolutionContext const& context) co
     return value();
 }
 
+NonnullRefPtr<StyleValue> LengthOrCalculated::create_style_value() const
+{
+    return LengthStyleValue::create(value());
+}
+
 double NumberOrCalculated::resolve_calculated(NonnullRefPtr<CalculatedStyleValue> const& calculated, Layout::Node const&) const
 {
     return calculated->resolve_number().value();
+}
+
+NonnullRefPtr<StyleValue> NumberOrCalculated::create_style_value() const
+{
+    return NumberStyleValue::create(value());
 }
 
 Percentage PercentageOrCalculated::resolve_calculated(NonnullRefPtr<CalculatedStyleValue> const& calculated, Layout::Node const&) const
@@ -50,14 +89,29 @@ Percentage PercentageOrCalculated::resolve_calculated(NonnullRefPtr<CalculatedSt
     return calculated->resolve_percentage().value();
 }
 
+NonnullRefPtr<StyleValue> PercentageOrCalculated::create_style_value() const
+{
+    return PercentageStyleValue::create(value());
+}
+
 Resolution ResolutionOrCalculated::resolve_calculated(NonnullRefPtr<CalculatedStyleValue> const& calculated, Layout::Node const&) const
 {
     return calculated->resolve_resolution().value();
 }
 
+NonnullRefPtr<StyleValue> ResolutionOrCalculated::create_style_value() const
+{
+    return ResolutionStyleValue::create(value());
+}
+
 Time TimeOrCalculated::resolve_calculated(NonnullRefPtr<CalculatedStyleValue> const& calculated, Layout::Node const&) const
 {
     return calculated->resolve_time().value();
+}
+
+NonnullRefPtr<StyleValue> TimeOrCalculated::create_style_value() const
+{
+    return TimeStyleValue::create(value());
 }
 
 }
