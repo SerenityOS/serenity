815f8db7fd07ae1c143a73de5758d273d46cd7b1
Andreas Kling <kling@serenityos.org>
2023-12-29 13:20:11 +0100

    LibJS+AK: Make String.prototype.repeat() way faster
    Instead of using a StringBuilder, add a String::repeated(String, N)
    overload that takes advantage of knowing it's already all UTF-8.
    
    This makes the following microbenchmark go 4x faster:
    
        "foo".repeat(100_000_000)
    
    And for single character strings, we can even go 10x faster:
    
        "x".repeat(100_000_000)

--- AK/String.h
+++ AK/String.h
@@ -92,6 +92,9 @@ public:
     // Creates a new String with a single code point repeated N times.
     static ErrorOr<String> repeated(u32 code_point, size_t count);
 
+    // Creates a new String from another string, repeated N times.
+    static String repeated(String const&, size_t count);
+
     // Creates a new String by case-transforming this String. Using these methods require linking LibUnicode into your application.
     ErrorOr<String> to_lowercase(Optional<StringView> const& locale = {}) const;
     ErrorOr<String> to_uppercase(Optional<StringView> const& locale = {}) const;
