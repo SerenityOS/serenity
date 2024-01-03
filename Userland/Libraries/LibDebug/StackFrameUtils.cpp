c41336563b3f43ccbb41a48d0d8d13e598deb25f
Sönke Holz <sholz8530@gmail.com>
2024-01-03 18:51:18 +0100

    LibDebug: Add support for riscv64 to StackFrameInfo::get_info()

--- Userland/Libraries/LibDebug/StackFrameUtils.cpp
+++ Userland/Libraries/LibDebug/StackFrameUtils.cpp
@@ -10,8 +10,21 @@ namespace Debug::StackFrameUtils {
 
 Optional<StackFrameInfo> get_info(ProcessInspector const& inspector, FlatPtr current_ebp)
 {
-    auto return_address = inspector.peek(current_ebp + sizeof(FlatPtr));
-    auto next_ebp = inspector.peek(current_ebp);
+#if ARCH(X86_64)
+    constexpr ptrdiff_t frame_pointer_return_address_offset = 8;
+    constexpr ptrdiff_t frame_pointer_previous_frame_pointer_offset = 0;
+#elif ARCH(AARCH64)
+    constexpr ptrdiff_t frame_pointer_return_address_offset = 8;
+    constexpr ptrdiff_t frame_pointer_previous_frame_pointer_offset = 0;
+#elif ARCH(RISCV64)
+    constexpr ptrdiff_t frame_pointer_return_address_offset = -8;
+    constexpr ptrdiff_t frame_pointer_previous_frame_pointer_offset = -16;
+#else
+#    error Unknown architecture
+#endif
+
+    auto return_address = inspector.peek(current_ebp + frame_pointer_return_address_offset);
+    auto next_ebp = inspector.peek(current_ebp + frame_pointer_previous_frame_pointer_offset);
     if (!return_address.has_value() || !next_ebp.has_value())
         return {};
 
