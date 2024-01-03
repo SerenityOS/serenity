346023202d2ca9fc65a3dcfbcae3464d6601cb39
Rawley <75388349+rawleyfowler@users.noreply.github.com>
2024-01-03 14:12:14 -0600

    Ports: Bump perl5 to 5.38.2

--- Ports/perl5/package.sh
+++ Ports/perl5/package.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='perl5'
-version='5.38.0'
+version='5.38.2'
+cross_version='1.5.2'
 useconfigure='true'
 files=(
-    "https://www.cpan.org/src/5.0/perl-${version}.tar.xz#eca551caec3bc549a4e590c0015003790bdd1a604ffe19cc78ee631d51f7072e"
-    "https://github.com/arsv/perl-cross/releases/download/1.5/perl-cross-1.5.tar.gz#d744a390939e2ebb9a12f6725b4d9c19255a141d90031eff90ea183fdfcbf211"
+    "https://www.cpan.org/src/5.0/perl-${version}.tar.xz#d91115e90b896520e83d4de6b52f8254ef2b70a8d545ffab33200ea9f1cf29e8"
+    "https://github.com/arsv/perl-cross/releases/download/${cross_version}/perl-cross-${cross_version}.tar.gz#584dc54c48dca25e032b676a15bef377c1fed9de318b4fc140292a5dbf326e90"
 )
 configopts=(
     '-Dosname=serenity'
@@ -27,7 +28,7 @@ fi
 
 post_fetch() {
     run chmod -R +rw "${PWD}/perl-${version}"*
-    run cp -r "${PWD}/perl-cross-1.5/"* "${PWD}/perl-${version}/"
+    run cp -r "${PWD}/perl-cross-${cross_version}/"* "${PWD}/perl-${version}/"
 
     if [ "$(uname -s)" = 'Darwin' ]; then
         cat <<- 'EOH' > "${PWD}/perl-${version}/readelf"
