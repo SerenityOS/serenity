4773a0f50c7ca1edf30814a75fa0174ec368c125
Sanil <sanilg566@gmail.com>
2024-01-09 21:12:00 +0530

    Hearts: Port to GML compiler

---
+++ Userland/Games/Hearts/MainWidget.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2024, Sanil Gupta <sanilg566@gmail.com>.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibGUI/Widget.h>
+
+namespace Hearts {
+
+class MainWidget : public GUI::Widget {
+    C_OBJECT_ABSTRACT(MainWidget)
+public:
+    static ErrorOr<NonnullRefPtr<MainWidget>> try_create();
+    virtual ~MainWidget() override = default;
+
+private:
+    MainWidget() = default;
+};
+
+}
