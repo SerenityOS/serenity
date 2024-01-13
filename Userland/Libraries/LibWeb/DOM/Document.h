d47bacb9c66b2477f3fb1b8622e4f715ed69b7b4
Shannon Booth <shannon@serenityos.org>
2024-01-13 20:12:28 +1300

    LibWeb: Use FlyString in Document::get_elements_by_name

--- Userland/Libraries/LibWeb/DOM/Document.h
+++ Userland/Libraries/LibWeb/DOM/Document.h
@@ -219,7 +219,7 @@ public:
     void schedule_style_update();
     void schedule_layout_update();
 
-    JS::NonnullGCPtr<HTMLCollection> get_elements_by_name(String const&);
+    JS::NonnullGCPtr<HTMLCollection> get_elements_by_name(FlyString const&);
     JS::NonnullGCPtr<HTMLCollection> get_elements_by_class_name(StringView);
 
     JS::NonnullGCPtr<HTMLCollection> applets();
