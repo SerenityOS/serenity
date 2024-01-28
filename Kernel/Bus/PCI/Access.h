20af9a5fb7b81630d0c76c411b9384684593956f
Hendiadyoin1 <leon.a@serenityos.org>
2024-01-28 23:39:12 +0100

    Kernel/riscv64: Initialize PCI controllers using the device tree
    Currently the search path is limited to `/soc/pci*` but this is enough
    to get it to work on qemu.

--- Kernel/Bus/PCI/Access.h
+++ Kernel/Bus/PCI/Access.h
@@ -13,6 +13,7 @@
 #include <AK/Vector.h>
 #include <Kernel/Bus/PCI/Controller/HostController.h>
 #include <Kernel/Bus/PCI/Definitions.h>
+#include <Kernel/Bus/PCI/Initializer.h>
 #include <Kernel/Locking/Spinlock.h>
 
 namespace Kernel::PCI {
@@ -50,6 +51,8 @@ public:
     ErrorOr<void> add_host_controller_and_scan_for_devices(NonnullOwnPtr<HostController>);
 
 private:
+    friend void PCI::initialize();
+
     u8 read8_field(DeviceIdentifier const&, RegisterOffset field);
     u16 read16_field(DeviceIdentifier const&, RegisterOffset field);
 
