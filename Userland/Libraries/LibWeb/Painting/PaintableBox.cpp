SerenityOS/serenity

commit 0bf82f748f44c961eb03c76a97620783fef69a32

LibWeb: Move clip rect calculation to happen before painting
With this change, clip rectangles for boxes with hidden overflow or the
clip property are no longer calculated during the recording of painting
commands. Instead, it has moved to the "pre-paint" phase, along with
the assignment of scrolling offsets, and works in the following way:

1. The paintable tree is traversed to collect all paintable boxes that
   have hidden overflow or use the CSS clip property. For each of these
   boxes, the "final" clip rectangle is calculated by intersecting clip
   rectangles in the containing block chain for a box.
2. The paintable tree is traversed another time, and a clip rectangle
   is assigned for each paintable box contained by a node with hidden
   overflow or the clip property.

This way, clipping becomes much easier during the painting commands
recording phase, as it only concerns the use of already assigned clip
rectangles. The same approach is applied to handle scrolling offsets.

Also, clip rectangle calculation is now implemented more correctly, as
we no longer stop at the stacking context boundary while intersecting
clip rectangles in the containing block chain.

Fixes:
https://github.com/SerenityOS/serenity/issues/22932
https://github.com/SerenityOS/serenity/issues/22883
https://github.com/SerenityOS/serenity/issues/22679
https://github.com/SerenityOS/serenity/issues/22534

Userland/Libraries/LibWeb/Painting/PaintableBox.cpp
@@ -121,6 +121,26 @@ CSSPixelRect PaintableBox::compute_absolute_rect() const
     return rect;
 }
 
+CSSPixelRect PaintableBox::compute_absolute_padding_rect_with_css_transform_applied() const
+{
+    CSSPixelRect rect { offset(), content_size() };
+    for (auto const* block = containing_block(); block; block = block->containing_block()) {
+        auto offset = block->paintable_box()->offset();
+        auto affine_transform = Gfx::extract_2d_affine_transform(block->paintable_box()->transform());
+        offset.translate_by(affine_transform.translation().to_type<CSSPixels>());
+        rect.translate_by(offset);
+    }
+    auto affine_transform = Gfx::extract_2d_affine_transform(transform());
+    rect.translate_by(affine_transform.translation().to_type<CSSPixels>());
+
+    CSSPixelRect padding_rect;
+    padding_rect.set_x(rect.x() - box_model().padding.left);
+    padding_rect.set_width(content_width() + box_model().padding.left + box_model().padding.right);
+    padding_rect.set_y(rect.y() - box_model().padding.top);
+    padding_rect.set_height(content_height() + box_model().padding.top + box_model().padding.bottom);
+    return padding_rect;
+}
+
 CSSPixelRect PaintableBox::absolute_rect() const
 {
     if (!m_absolute_rect.has_value())
@@ -171,11 +191,8 @@ void PaintableBox::before_paint(PaintContext& context, [[maybe_unused]] PaintPha
     if (!is_visible())
         return;
 
-    auto clip_rect = get_clip_rect();
-    if (clip_rect.has_value()) {
-        context.recording_painter().save();
-        context.recording_painter().add_clip_rect(clip_rect->to_type<int>());
-    }
+    apply_scroll_offset(context, phase);
+    apply_clip_overflow_rect(context, phase);
 }
 
 void PaintableBox::after_paint(PaintContext& context, [[maybe_unused]] PaintPhase phase) const
@@ -183,8 +200,8 @@ void PaintableBox::after_paint(PaintContext& context, [[maybe_unused]] PaintPhas
     if (!is_visible())
         return;
 
-    if (get_clip_rect().has_value())
-        context.recording_painter().restore();
+    clear_clip_overflow_rect(context, phase);
+    reset_scroll_offset(context, phase);
 }
 
 void PaintableBox::paint(PaintContext& context, PaintPhase phase) const
@@ -358,110 +375,63 @@ BorderRadiiData PaintableBox::normalized_border_radii_data(ShrinkRadiiForBorders
     return border_radii_data;
 }
 
-Optional<CSSPixelRect> PaintableBox::calculate_overflow_clipped_rect() const
-{
-    if (layout_node().is_viewport()) {
-        return {};
-    }
-
-    if (!m_clip_rect.has_value()) {
-        // NOTE: stacking context should not be crossed while aggregating rectangle to
-        // clip `overflow: hidden` because intersecting rectangles with different
-        // transforms doesn't make sense
-        // TODO: figure out if there are cases when stacking context should be
-        // crossed to calculate correct clip rect
-        if (!stacking_context() && containing_block() && containing_block()->paintable_box()) {
-            m_clip_rect = containing_block()->paintable_box()->calculate_overflow_clipped_rect();
-        }
-
-        auto overflow_x = computed_values().overflow_x();
-        auto overflow_y = computed_values().overflow_y();
-
-        if (overflow_x != CSS::Overflow::Visible && overflow_y != CSS::Overflow::Visible) {
-            if (m_clip_rect.has_value()) {
-                m_clip_rect->intersect(absolute_padding_box_rect());
-            } else {
-                m_clip_rect = absolute_padding_box_rect();
-            }
-        }
-    }
-
-    return m_clip_rect;
-}
-
 void PaintableBox::apply_scroll_offset(PaintContext& context, PaintPhase) const
 {
-    if (context.scroll_frames().contains(this)) {
+    if (m_scroll_frame_id.has_value()) {
         context.recording_painter().save();
-        context.recording_painter().set_scroll_frame_id(context.scroll_frames().get(this)->id);
+        context.recording_painter().set_scroll_frame_id(m_scroll_frame_id.value());
     }
 }
 
 void PaintableBox::reset_scroll_offset(PaintContext& context, PaintPhase) const
 {
-    if (context.scroll_frames().contains(this)) {
+    if (m_scroll_frame_id.has_value())
         context.recording_painter().restore();
-    }
 }
 
 void PaintableBox::apply_clip_overflow_rect(PaintContext& context, PaintPhase phase) const
 {
-    if (!AK::first_is_one_of(phase, PaintPhase::Background, PaintPhase::Border, PaintPhase::Foreground))
+    if (!AK::first_is_one_of(phase, PaintPhase::Background, PaintPhase::Border, PaintPhase::Foreground, PaintPhase::Outline))
         return;
 
-    // FIXME: Support more overflow variations.
-    auto clip_rect = this->calculate_overflow_clipped_rect();
-    auto overflow_x = computed_values().overflow_x();
-    auto overflow_y = computed_values().overflow_y();
+    if (m_clip_rect.has_value()) {
+        auto overflow_clip_rect = m_clip_rect.value();
+        for (auto const* ancestor = &this->layout_box(); ancestor; ancestor = ancestor->containing_block()) {
+            auto affine_transform = Gfx::extract_2d_affine_transform(ancestor->paintable_box()->transform());
+            if (!affine_transform.is_identity()) {
+                // NOTE: Because the painting command executor applies CSS transform of the nearest stacking context
+                //       and the m_clip_rect is determined considering CSS transforms, here transform of the nearest
+                //       stacking context need to be compensated.
+                //       This adjustment ensures the transform is accounted for just once.
+                overflow_clip_rect.translate_by(-affine_transform.translation().to_type<CSSPixels>());
+                break;
+            }
+        }
 
-    auto css_clip_property = get_clip_rect();
-    if (css_clip_property.has_value()) {
-        if (clip_rect.has_value())
-            clip_rect->intersect(css_clip_property.value());
-        else
-            clip_rect = css_clip_property.value();
-    }
-
-    if (!clip_rect.has_value())
-        return;
-
-    if (!m_clipping_overflow) {
-        context.recording_painter().save();
-        context.recording_painter().add_clip_rect(context.enclosing_device_rect(*clip_rect).to_type<int>());
         m_clipping_overflow = true;
-    }
-
-    if (!clip_rect->is_empty() && overflow_y == CSS::Overflow::Hidden && overflow_x == CSS::Overflow::Hidden) {
-        auto border_radii_data = normalized_border_radii_data(ShrinkRadiiForBorders::Yes);
-        CornerRadii corner_radii {
-            .top_left = border_radii_data.top_left.as_corner(context),
-            .top_right = border_radii_data.top_right.as_corner(context),
-            .bottom_right = border_radii_data.bottom_right.as_corner(context),
-            .bottom_left = border_radii_data.bottom_left.as_corner(context)
-        };
-        if (corner_radii.has_any_radius()) {
+        context.recording_painter().save();
+        context.recording_painter().add_clip_rect(context.enclosing_device_rect(overflow_clip_rect).to_type<int>());
+        if (m_corner_clip_radii.has_value()) {
             VERIFY(!m_corner_clipper_id.has_value());
             m_corner_clipper_id = context.allocate_corner_clipper_id();
-            context.recording_painter().sample_under_corners(*m_corner_clipper_id, corner_radii, context.rounded_device_rect(*clip_rect).to_type<int>(), CornerClip::Outside);
+            context.recording_painter().sample_under_corners(*m_corner_clipper_id, *m_corner_clip_radii, context.rounded_device_rect(overflow_clip_rect).to_type<int>(), CornerClip::Outside);
         }
     }
 }
 
 void PaintableBox::clear_clip_overflow_rect(PaintContext& context, PaintPhase phase) const
 {
-    if (!AK::first_is_one_of(phase, PaintPhase::Background, PaintPhase::Border, PaintPhase::Foreground))
+    if (!AK::first_is_one_of(phase, PaintPhase::Background, PaintPhase::Border, PaintPhase::Foreground, PaintPhase::Outline))
         return;
 
-    // FIXME: Support more overflow variations.
     if (m_clipping_overflow) {
-        context.recording_painter().restore();
         m_clipping_overflow = false;
-    }
-    if (m_corner_clipper_id.has_value()) {
-        VERIFY(m_corner_clipper_id.has_value());
-        auto clip_rect = this->calculate_overflow_clipped_rect();
-        context.recording_painter().blit_corner_clipping(*m_corner_clipper_id, context.rounded_device_rect(*clip_rect).to_type<int>());
-        m_corner_clipper_id = {};
+        if (m_corner_clip_radii.has_value()) {
+            VERIFY(m_corner_clipper_id.has_value());
+            context.recording_painter().blit_corner_clipping(*m_corner_clipper_id, context.rounded_device_rect(*m_clip_rect).to_type<int>());
+            m_corner_clipper_id = {};
+        }
+        context.recording_painter().restore();
     }
 }
 

Tests/LibWeb/Ref/abspos-escapes-scroll-container.html
@@ -1,0 +1,29 @@
+<!DOCTYPE html>
+<link rel="match" href="reference/abspos-escapes-scroll-container-ref.html" />
+<style>
+#scrollable-box {
+    width: 300px;
+    height: 200px;
+    overflow: auto;
+    border: 1px solid black;
+}
+.content {
+    height: 600px;
+}
+.absolute-element {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    background-color: yellow;
+}
+</style>
+<div id="scrollable-box">
+    <div class="content"><span><span>Content that makes the container scrollable...</span></span></div>
+    <div class="absolute-element">
+        Abspos Element
+    </div>
+</div>
+<script>
+    const scrollContainer = document.getElementById("scrollable-box");
+    scrollContainer.scrollTop = 500;
+</script>

Tests/LibWeb/Ref/overflow-hidden-3.html
@@ -1,0 +1,23 @@
+<!doctype html>
+<link rel="match" href="reference/overflow-hidden-3-ref.html" />
+<style>
+body {
+    width: 100px;
+    height: 100px;
+}
+#outer {
+    width: 200px;
+    height: 100px;
+    overflow: hidden;
+}
+#inner {
+    position: relative;
+    z-index: 0;
+    overflow: hidden;
+}
+#green {
+    width: 200px;
+    height: 200px;
+    background: green;
+}
+</style><body><div id="outer"><div id="inner"><div id="green">

Tests/LibWeb/Ref/overflow-hidden-4.html
@@ -1,0 +1,28 @@
+<!DOCTYPE html>
+<link rel="match" href="reference/overflow-hidden-4-ref.html" />
+<style>
+* {
+    outline: black solid 1px;
+}
+.outer {
+    background: pink;
+    width: 100px;
+    height: 100px;
+    overflow: hidden;
+}
+.middle {
+    transform: perspective(1200px);
+    background: orange;
+}
+.inner {
+    width: 300px;
+    height: 300px;
+    background: magenta;
+    overflow: hidden;
+}
+.red {
+    background: red;
+    width: 300px;
+    height: 300px;
+}
+</style><body><div class="outer"><div class="middle"><div class="inner"><div class="red">

Tests/LibWeb/Ref/overflow-hidden-5.html
@@ -1,0 +1,30 @@
+<!DOCTYPE html>
+<head>
+<link rel="match" href="reference/overflow-hidden-5-ref.html" />
+<style type="text/css">
+    body {
+        margin: 0;
+    }
+
+    .wrapper {
+        margin-top: 100px;
+        margin-left: 100px;
+        width: 900px;
+        border: 5px solid magenta;
+    }
+
+    .container {
+        overflow: hidden;
+        display: block;
+        height: 400px;
+    }
+
+    .box {
+        background-color: darkblue;
+        width: 256px;
+        height: 256px;
+        transform: translate3d(-50px, -50px, 0px);
+    }
+</style>
+</head>
+<div class="wrapper"><div class="container"><div class="box"></div></div></div>

Tests/LibWeb/Ref/reference/abspos-escapes-scroll-container-ref.html
@@ -1,0 +1,16 @@
+<!DOCTYPE html>
+<style>
+#scrollable-box {
+    width: 300px;
+    height: 200px;
+    border: 1px solid black;
+}
+.absolute-element {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    background-color: yellow;
+}
+</style>
+<div id="scrollable-box"></div>
+<div class="absolute-element">Abspos Element</div>

Tests/LibWeb/Ref/reference/overflow-hidden-3-ref.html
@@ -1,0 +1,9 @@
+<!DOCTYPE html>
+<link rel="match" href="reference/overflow-hidden-3-ref.html" />
+<style>
+.green {
+    width: 200px;
+    height: 100px;
+    background: green;
+}
+</style><body><div class="green">

Tests/LibWeb/Ref/reference/overflow-hidden-4-ref.html
@@ -1,0 +1,12 @@
+<!DOCTYPE html>
+<link rel="match" href="reference/overflow-hidden-4-ref.html" />
+<style>
+* {
+    outline: black solid 1px;
+}
+.red {
+    background: red;
+    width: 100px;
+    height: 100px;
+}
+</style><body><div class="red">

commit 35d52dbbad48e18f2d72134e63e605707118c230

LibWeb: Fix hidden overflow clipping with nested CSS transforms
This is a fix for regression introduced in
0bf82f748f44c961eb03c76a97620783fef69a32

All CSS transforms need to be removed from the clip rectangle before
applying it. However, it is still necessary to calculate it with
applied transforms to find the correct intersection of all clip
rectangles in the containing block chain.

Userland/Libraries/LibWeb/Painting/PaintableBox.cpp
@@ -399,12 +399,10 @@ void PaintableBox::apply_clip_overflow_rect(PaintContext& context, PaintPhase ph
         for (auto const* ancestor = &this->layout_box(); ancestor; ancestor = ancestor->containing_block()) {
             auto affine_transform = Gfx::extract_2d_affine_transform(ancestor->paintable_box()->transform());
             if (!affine_transform.is_identity()) {
-                // NOTE: Because the painting command executor applies CSS transform of the nearest stacking context
-                //       and the m_clip_rect is determined considering CSS transforms, here transform of the nearest
-                //       stacking context need to be compensated.
-                //       This adjustment ensures the transform is accounted for just once.
+                // NOTE: Since the painting command executor applies a CSS transform and the clip rect is calculated
+                //       with this transform in account, we need to remove the transform from the clip rect.
+                //       Otherwise, the transform will be applied twice to the clip rect.
                 overflow_clip_rect.translate_by(-affine_transform.translation().to_type<CSSPixels>());
-                break;
             }
         }
 

Tests/LibWeb/Ref/overflow-hidden-6.html
@@ -1,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<link rel="match" href="reference/overflow-hidden-6-ref.html" />
+<style>
+    .display-flex-position-relative {
+        display: flex;
+        flex-direction: column;
+        position: relative;
+    }
+
+    .display-block {
+        display: block
+    }
+
+    .overflow-hidden {
+        overflow: hidden
+    }
+
+    .width-1050px {
+        width: 100px
+    }
+
+    .width-100-percent {
+        width: 100%
+    }
+
+    .left-0px {
+        left: 0px
+    }
+
+    .bottom-0px {
+        bottom: 0px
+    }
+
+    .height-100-percent {
+        height: 100%
+    }
+
+    .top-50-percent {
+        top: 50%
+    }
+
+    .left-50-percent {
+        left: 50%
+    }
+
+    .transform-50-percent {
+        transform: translateX(-50%) translateY(-50%);
+    }
+
+    .position-absolute {
+        position: absolute
+    }
+
+    .height-auto {
+        height: auto
+    }
+
+    .padding-bottom-100-percent {
+        padding-bottom: 100%;
+    }
+</style>
+<div class="display-flex-position-relative width-1050px">
+    <div class="width-100-percent padding-bottom-100-percent"></div>
+    <div class="position-absolute width-100-percent height-100-percent">
+        <div
+            class="height-100-percent width-100-percent position-absolute left-50-percent top-50-percent transform-50-percent">
+            <div
+                class="overflow-hidden position-absolute left-50-percent top-50-percent transform-50-percent width-100-percent height-100-percent ">
+                <div class="display-block width-100-percent padding-bottom-100-percent"></div>
+                <div class="bottom-0px left-0px position-absolute top-0px width-100-percent height-100-percent "
+                    style="background-color: green;"></div>
+            </div>
+        </div>
+    </div>
+</div>

Tests/LibWeb/Ref/reference/overflow-hidden-6-ref.html
@@ -1,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<link rel="match" href="reference/overflow-hidden-6-ref.html" />
+<style>
+    .box {
+        width: 100px;
+        height: 100px;
+        background-color: green;
+    }
+</style>
+<div class="box"></div>