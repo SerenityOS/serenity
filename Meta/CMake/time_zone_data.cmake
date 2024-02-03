3bf90fdab14f9829008d81036e9438aee2dd336d
Timothy Flynn <trflynn89@pm.me>
2024-02-03 14:16:38 -0500

    LibTimeZone: Update to TZDB version 2024a
    https://mm.icann.org/pipermail/tz-announce/2024-February/000081.html

--- Meta/CMake/time_zone_data.cmake
+++ Meta/CMake/time_zone_data.cmake
@@ -2,7 +2,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/utils.cmake)
 
 set(TZDB_PATH "${SERENITY_CACHE_DIR}/TZDB" CACHE PATH "Download location for TZDB files")
 
-set(TZDB_VERSION 2023d)
+set(TZDB_VERSION 2024a)
 set(TZDB_VERSION_FILE "${TZDB_PATH}/version.txt")
 
 set(TZDB_ZIP_URL "https://data.iana.org/time-zones/releases/tzdata${TZDB_VERSION}.tar.gz")
