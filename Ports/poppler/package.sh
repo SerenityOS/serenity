f7fcff8f1ef720ad64c85cabb4ce37f20b33c287
Fabian Dellwing <fabian@dellwing.net>
2024-02-04 13:21:11 +0100

    Ports: Update poppler to 24.02.0

--- Ports/poppler/package.sh
+++ Ports/poppler/package.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='poppler'
-version='23.08.0'
+version='24.02.0'
 version_data='0.4.12'
 files=(
-    "https://poppler.freedesktop.org/poppler-${version}.tar.xz#4a4bf7fc903b9f1a2ab7d04b7c5d8220db9bc6261cc73fdb9a826dc272f49aa8"
+    "https://poppler.freedesktop.org/poppler-${version}.tar.xz#19187a3fdd05f33e7d604c4799c183de5ca0118640c88b370ddcf3136343222e"
     "https://poppler.freedesktop.org/poppler-data-${version_data}.tar.gz#c835b640a40ce357e1b83666aabd95edffa24ddddd49b8daff63adb851cdab74"
 )
 depends=(
+    'curl'
     'fontconfig'
     'freetype'
     'libjpeg'
@@ -21,18 +22,19 @@ configopts=(
     '-DENABLE_BOOST=OFF'
     '-DENABLE_CPP=OFF'
     '-DBUILD_CPP_TESTS=OFF'
+    '-DENABLE_GLIB=OFF'
+    '-DENABLE_GOBJECT_INTROSPECTION=OFF'
+    '-DENABLE_GPGME=OFF'
+    '-DENABLE_GTK_DOC=OFF'
+    '-DENABLE_LCMS=OFF'
+    '-DENABLE_LIBOPENJPEG=unmaintained'
+    '-DENABLE_NSS3=OFF'
     '-DENABLE_QT5=OFF'
     '-DBUILD_QT5_TESTS=OFF'
     '-DENABLE_QT6=OFF'
     '-DBUILD_QT6_TESTS=OFF'
     '-DBUILD_GTK_TESTS=OFF'
-    '-DBUILD_MANUAL_TESTS=OFF'
-    '-DENABLE_GLIB=OFF'
-    '-DENABLE_GOBJECT_INTROSPECTION=OFF'
-    '-DENABLE_GTK_DOC=OFF'
-    '-DENABLE_LIBOPENJPEG=unmaintained'
-    '-DWITH_Gpgmepp=OFF'
-    '-DWITH_NSS3=OFF'
+    '-DBUILD_MANUAL_TESTS=OFF'   
 )
 
 configure() {
