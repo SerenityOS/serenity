8c5b9c2ea972633697944a12d64d3e5f0f90680b
Lucas CHOLLET <lucas.chollet@free.fr>
2024-02-11 18:46:21 -0500

    LibGfx/CCITT: Don't overrun the image width

--- Userland/Libraries/LibGfx/ImageFormats/CCITTDecoder.cpp
+++ Userland/Libraries/LibGfx/ImageFormats/CCITTDecoder.cpp
@@ -273,7 +273,7 @@ ErrorOr<void> decode_single_ccitt3_1d_line(BigEndianInputBitStream& input_bit_st
     u32 run_length = 0;
     u32 column = 0;
 
-    while (column < image_width) {
+    while (column < image_width - 1) {
         if (run_length > 0) {
             run_length--;
             TRY(decoded_bits.write_bits(current_color == ccitt_white ? 0u : 1u, 1));
