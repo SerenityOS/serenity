9e8e93d91d2cfabe02159ea4dd8a70ef69656465
Tom Finet <tom.codeninja@gmail.com>
2024-02-18 16:55:31 +0000

    Kernel: Remove storage_segment from IDTEntry constructor
    On x86_64 and x86, storage_segment (bit 12 counting from 0)
    is always 0 according to the Intel Software Developer's Manual,
    volume 3A, section 6.11 and section 6.14.1. It has therefore
    been removed as a parameter from IDTEntry's constructor and
    hardwired to 0.

--- Kernel/Arch/x86_64/Interrupts.cpp
+++ Kernel/Arch/x86_64/Interrupts.cpp
@@ -464,15 +464,13 @@ void unregister_generic_interrupt_handler(u8 interrupt_number, GenericInterruptH
 UNMAP_AFTER_INIT void register_interrupt_handler(u8 index, void (*handler)())
 {
     // FIXME: Is the Gate Type really required to be an Interrupt
-    // FIXME: What's up with that storage segment 0?
-    s_idt[index] = IDTEntry((FlatPtr)handler, GDT_SELECTOR_CODE0, IDTEntryType::InterruptGate32, 0, 0);
+    s_idt[index] = IDTEntry((FlatPtr)handler, GDT_SELECTOR_CODE0, IDTEntryType::InterruptGate32, 0);
 }
 
 UNMAP_AFTER_INIT void register_user_callable_interrupt_handler(u8 index, void (*handler)())
 {
     // FIXME: Is the Gate Type really required to be a Trap
-    // FIXME: What's up with that storage segment 0?
-    s_idt[index] = IDTEntry((FlatPtr)handler, GDT_SELECTOR_CODE0, IDTEntryType::TrapGate32, 0, 3);
+    s_idt[index] = IDTEntry((FlatPtr)handler, GDT_SELECTOR_CODE0, IDTEntryType::TrapGate32, 3);
 }
 
 UNMAP_AFTER_INIT void flush_idt()
