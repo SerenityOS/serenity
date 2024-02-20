99f8ac84ef0cbd10ff2ee42b9b2982df0f930816
Ali Mohammad Pur <ali.mpfard@gmail.com>
2024-02-20 03:46:06 +0330

    Tests/LibWeb: Make the WPT runner grab only the one commit it needs

--- Tests/LibWeb/WPT/run.sh
+++ Tests/LibWeb/WPT/run.sh
@@ -41,10 +41,13 @@ pushd "${SCRIPT_DIR}"
 
 if [ ! -d "${SCRIPT_DIR}/wpt" ]; then
     # Clone patched web-platform-tests repository
-    git clone --depth 10000 https://github.com/web-platform-tests/wpt.git
+    mkdir wpt
+    git -C wpt init
+    git -C wpt remote add origin https://github.com/web-platform-tests/wpt.git
 
     # Switch to the commit that was used to generate tests expectations. Requires periodic updates.
-    git -C wpt checkout eedf737ce39c512d0ca3471f988972e3ece11822
+    git -C wpt fetch --depth 1 origin eedf737ce39c512d0ca3471f988972e3ece11822
+    git -C wpt checkout FETCH_HEAD
 
     git apply 0001-tools-Pass-product-name-to-update-metadata-fallback-.patch
 
