a950e569bb20903999f05bbd22392e1d4445e4af
Fabian Dellwing <fabian@dellwing.net>
2024-02-21 14:23:12 +0100

    Ports: Enable more `poppler` features
    We now link against `boost`, `lcms2` and `openjpeg`.
    
    We also depend on `gpgme` but don't link against it because of #23557

--- Ports/poppler/package.sh
+++ Ports/poppler/package.sh
@@ -7,35 +7,40 @@ files=(
     "https://poppler.freedesktop.org/poppler-data-${version_data}.tar.gz#c835b640a40ce357e1b83666aabd95edffa24ddddd49b8daff63adb851cdab74"
 )
 depends=(
+    'boost'
     'curl'
     'fontconfig'
     'freetype'
+    'gpgme'
+    'lcms2'
     'libjpeg'
     'libpng'
     'libtiff'
+    'openjpeg'
 )
 useconfigure='true'
 configopts=(
     '-B build'
     "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
     '-DCMAKE_BUILD_TYPE=release'
-    '-DENABLE_BOOST=OFF'
     '-DENABLE_CPP=OFF'
     '-DBUILD_CPP_TESTS=OFF'
     '-DENABLE_GLIB=OFF'
     '-DENABLE_GOBJECT_INTROSPECTION=OFF'
-    '-DENABLE_GPGME=OFF'
-    '-DENABLE_GTK_DOC=OFF'
-    '-DENABLE_LCMS=OFF'
-    '-DENABLE_LIBOPENJPEG=unmaintained'
+    '-DENABLE_GPGME=OFF' # Enabling GPGME causes the program to crash (#23557)
     '-DENABLE_NSS3=OFF'
     '-DENABLE_QT5=OFF'
     '-DBUILD_QT5_TESTS=OFF'
     '-DENABLE_QT6=OFF'
     '-DBUILD_QT6_TESTS=OFF'
     '-DBUILD_GTK_TESTS=OFF'
-    '-DBUILD_MANUAL_TESTS=OFF'   
+    '-DBUILD_MANUAL_TESTS=OFF'
 )
+
+pre_configure() {
+    # Fix the GpgmeppConfig.cmake file to use the correct path for cross-compilation
+    run sed -i'.bak' "s#/usr/local#${SERENITY_INSTALL_ROOT}/usr/local#g" ${SERENITY_INSTALL_ROOT}/usr/local/lib/cmake/Gpgmepp/GpgmeppConfig.cmake
+}
 
 configure() {
     run cmake "${configopts[@]}"
@@ -49,4 +54,9 @@ install() {
     run make -C build install "${installopts[@]}"
     cd "poppler-data-${version_data}"
     run_nocd make install datadir=${SERENITY_INSTALL_ROOT}/usr/local
+}
+
+post_install() {
+    # Restore the GpgmeppConfig.cmake file
+    run mv ${SERENITY_INSTALL_ROOT}/usr/local/lib/cmake/Gpgmepp/GpgmeppConfig.cmake.bak ${SERENITY_INSTALL_ROOT}/usr/local/lib/cmake/Gpgmepp/GpgmeppConfig.cmake
 }
