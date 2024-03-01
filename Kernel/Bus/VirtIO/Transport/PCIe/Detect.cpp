9f1750902bbb4fb62e62fced0e68bf239f29e7e6
Liav A. <liavalb@gmail.com>
2024-03-01 12:59:32 +0200

    Kernel: Move VirtIO console code to the Devices/Serial/VirtIO directory

--- Kernel/Bus/VirtIO/Transport/PCIe/Detect.cpp
+++ Kernel/Bus/VirtIO/Transport/PCIe/Detect.cpp
@@ -9,10 +9,10 @@
 #include <Kernel/Boot/CommandLine.h>
 #include <Kernel/Bus/PCI/API.h>
 #include <Kernel/Bus/PCI/IDs.h>
-#include <Kernel/Bus/VirtIO/Console.h>
 #include <Kernel/Bus/VirtIO/Device.h>
 #include <Kernel/Bus/VirtIO/RNG.h>
 #include <Kernel/Bus/VirtIO/Transport/PCIe/Detect.h>
+#include <Kernel/Devices/Serial/VirtIO/Console.h>
 #include <Kernel/Sections.h>
 
 namespace Kernel::VirtIO {
