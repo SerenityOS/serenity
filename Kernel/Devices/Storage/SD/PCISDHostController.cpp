18091fcc3abc8e9febb3f834527c9f93c0940f72
Liav A. <liavalb@gmail.com>
2024-03-02 21:54:04 +0200

    Kernel/PCI: Add a driver for PCI SDHCI controllers

--- Kernel/Devices/Storage/SD/PCISDHostController.cpp
+++ Kernel/Devices/Storage/SD/PCISDHostController.cpp
@@ -4,14 +4,30 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#include <Kernel/Bus/PCI/API.h>
 #include <Kernel/Devices/Storage/SD/PCISDHostController.h>
 
 namespace Kernel {
 
-PCISDHostController::PCISDHostController(PCI::DeviceIdentifier const& device_identifier)
-    : PCI::Device(device_identifier)
-    , SDHostController()
+ErrorOr<NonnullRefPtr<PCISDHostController>> PCISDHostController::try_initialize(PCI::Device& pci_device)
+{
+    auto sdhc = TRY(adopt_nonnull_ref_or_enomem(new (nothrow) PCISDHostController(pci_device)));
+    TRY(sdhc->initialize());
+
+    pci_device.enable_bus_mastering();
+    pci_device.enable_memory_space();
+    sdhc->try_enable_dma();
+
+    return sdhc;
+}
+
+PCISDHostController::SlotInformationRegister PCISDHostController::read_slot_information() const
+{
+    return bit_cast<SlotInformationRegister>(m_pci_device->config_space_read8(static_cast<PCI::RegisterOffset>(0x40)));
+}
+
+PCISDHostController::PCISDHostController(PCI::Device& pci_device)
+    : SDHostController()
+    , m_pci_device(pci_device)
 {
     auto slot_information_register = read_slot_information();
 
@@ -21,7 +37,7 @@ PCISDHostController::PCISDHostController(PCI::DeviceIdentifier const& device_ide
     }
 
     auto physical_address_of_sdhc_registers = PhysicalAddress {
-        PCI::get_BAR(device_identifier, static_cast<PCI::HeaderType0BaseRegister>(slot_information_register.first_bar_number))
+        m_pci_device->resources()[slot_information_register.first_bar_number].physical_memory_address()
     };
     m_registers = Memory::map_typed_writable<SD::HostControlRegisterMap volatile>(physical_address_of_sdhc_registers).release_value_but_fixme_should_propagate_errors();
 }
