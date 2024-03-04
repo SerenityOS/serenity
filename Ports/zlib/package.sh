b9a5442b59485a9c7614b1253855379f631ed3e3
Tim Schumacher <timschumi@gmx.de>
2024-03-04 20:20:52 +0100

    Ports/zlib: Switch to tarball copies hosted on GitHub
    zlib.net uses DDoS protection, which results in broken downloads
    whenever a browser validation page is served instead. The tarballs that
    are uploaded to GitHub releases are identical with the ones that are
    served on zlib.net.

--- Ports/zlib/package.sh
+++ Ports/zlib/package.sh
@@ -3,7 +3,7 @@ port='zlib'
 version='1.3.1'
 useconfigure='true'
 files=(
-    "https://www.zlib.net/fossils/zlib-${version}.tar.gz#9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df23"
+    "https://github.com/madler/zlib/releases/download/v${version}/zlib-${version}.tar.gz#9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df23"
 )
 
 configure() {
