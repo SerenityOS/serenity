b633fa97fd9b83f636ce23445c1b3232bd6d9d16
circl <circl.lastname@gmail.com>
2024-03-17 17:40:14 +0100

    TakeScreenshot: Add graphical frontend to `shot`
    Co-authored-by: cubiclove <7754483+cubiclove@users.noreply.github.com>

---
+++ Userland/Applications/TakeScreenshot/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2024, circl <circl.lastname@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include "MainWindow.h"
+#include <LibCore/System.h>
+#include <LibGUI/Application.h>
+#include <LibMain/Main.h>
+
+ErrorOr<int> serenity_main(Main::Arguments arguments)
+{
+    TRY(Core::System::pledge("stdio recvfd sendfd thread cpath rpath wpath unix proc exec"));
+
+    auto app = TRY(GUI::Application::create(arguments));
+    auto window = TRY(TakeScreenshot::MainWindow::try_create());
+
+    window->show();
+
+    return app->exec();
+}
