f26dd29b4df660f1ce323f158a61a3318b50c7b6
Andrew Kaster <akaster@serenityos.org>
2024-03-20 06:43:17 -0600

    LibWeb: Print more information about thrown DOMExceptions in the console
    This doesn't quite match the behavior of other engines, but by golly is
    it helpful.

--- Userland/Libraries/LibWeb/HTML/Scripting/ExceptionReporter.cpp
+++ Userland/Libraries/LibWeb/HTML/Scripting/ExceptionReporter.cpp
@@ -11,6 +11,7 @@
 #include <LibJS/Runtime/Value.h>
 #include <LibWeb/Bindings/MainThreadVM.h>
 #include <LibWeb/HTML/Scripting/ExceptionReporter.h>
+#include <LibWeb/WebIDL/DOMException.h>
 
 namespace Web::HTML {
 
@@ -25,11 +26,19 @@ void report_exception_to_console(JS::Value value, JS::Realm& realm, ErrorInPromi
         auto message = object.get_without_side_effects(vm.names.message).value_or(JS::js_undefined());
         if (name.is_accessor() || message.is_accessor()) {
             // The result is not going to be useful, let's just print the value. This affects DOMExceptions, for example.
-            dbgln("\033[31;1mUnhandled JavaScript exception{}:\033[0m {}", error_in_promise == ErrorInPromise::Yes ? " (in promise)" : "", JS::Value(&object));
+            if (is<WebIDL::DOMException>(object)) {
+                auto const& exception = static_cast<WebIDL::DOMException const&>(object);
+                dbgln("\033[31;1mUnhandled JavaScript exception{}:\033[0m {}: {}", error_in_promise == ErrorInPromise::Yes ? " (in promise)" : "", exception.name(), exception.message());
+            } else {
+                dbgln("\033[31;1mUnhandled JavaScript exception{}:\033[0m {}", error_in_promise == ErrorInPromise::Yes ? " (in promise)" : "", JS::Value(&object));
+            }
         } else {
             dbgln("\033[31;1mUnhandled JavaScript exception{}:\033[0m [{}] {}", error_in_promise == ErrorInPromise::Yes ? " (in promise)" : "", name, message);
         }
         if (is<JS::Error>(object)) {
+            // FIXME: We should be doing this for DOMException as well
+            //        https://webidl.spec.whatwg.org/#js-DOMException-specialness
+            //        "Additionally, if an implementation gives native Error objects special powers or nonstandard properties (such as a stack property), it should also expose those on DOMException objects."
             auto const& error_value = static_cast<JS::Error const&>(object);
             dbgln("{}", error_value.stack_string(JS::CompactTraceback::Yes));
             console.report_exception(error_value, error_in_promise == ErrorInPromise::Yes);
