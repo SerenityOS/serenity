fe8d4b626960d0976fea2fce95439f9d10d1058d
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2024-03-20 18:19:58 +0100

    LibWeb: Always run layout and style updates from event loop processing
    Before this change, we ran style and layout updates from both event
    loop processing and update timers. This could have caused missed resize
    observer updates and unnecessary updating of style or layout more than
    once before repaint.
    
    Also, we can now be sure unnecessary style or layout updates won't
    happen in `EventLoop::spin_processing_tasks_with_source_until()`.

--- Userland/Libraries/LibWeb/DOM/Document.h
+++ Userland/Libraries/LibWeb/DOM/Document.h
@@ -656,9 +656,6 @@ private:
     Optional<Color> m_active_link_color;
     Optional<Color> m_visited_link_color;
 
-    RefPtr<Core::Timer> m_style_update_timer;
-    RefPtr<Core::Timer> m_layout_update_timer;
-
     JS::GCPtr<HTML::HTMLParser> m_parser;
     bool m_active_parser_was_aborted { false };
 
