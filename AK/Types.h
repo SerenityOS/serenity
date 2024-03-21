d762ec7b426e92e2923a8ca7d3bb12c3e9a30e76
Stanisław Wiśniewski <s.wisniewski1101@gmail.com>
2024-03-21 16:33:57 +0100

    AK: Use `else if constexpr` in explode_byte()

--- AK/Types.h
+++ AK/Types.h
@@ -171,7 +171,7 @@ static constexpr FlatPtr explode_byte(u8 b)
     FlatPtr value = b;
     if constexpr (sizeof(FlatPtr) == 4)
         return value << 24 | value << 16 | value << 8 | value;
-    else if (sizeof(FlatPtr) == 8)
+    else if constexpr (sizeof(FlatPtr) == 8)
         return value << 56 | value << 48 | value << 40 | value << 32 | value << 24 | value << 16 | value << 8 | value;
 }
 
