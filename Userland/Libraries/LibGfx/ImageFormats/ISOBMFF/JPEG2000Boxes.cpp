59bd378db88aa2e574c1a6411da8eea05a23cdba
Nico Weber <thakis@chromium.org>
2024-03-22 14:35:18 -0400

    LibGfx/ISOBMFF: Add JPEG2000ImageHeaderBox

--- Userland/Libraries/LibGfx/ImageFormats/ISOBMFF/JPEG2000Boxes.cpp
+++ Userland/Libraries/LibGfx/ImageFormats/ISOBMFF/JPEG2000Boxes.cpp
@@ -11,8 +11,13 @@ namespace Gfx::ISOBMFF {
 
 ErrorOr<void> JPEG2000HeaderBox::read_from_stream(BoxStream& stream)
 {
-    auto make_subbox = [](BoxType, BoxStream&) -> ErrorOr<Optional<NonnullOwnPtr<Box>>> {
-        return OptionalNone {};
+    auto make_subbox = [](BoxType type, BoxStream& stream) -> ErrorOr<Optional<NonnullOwnPtr<Box>>> {
+        switch (type) {
+        case BoxType::JPEG2000ImageHeaderBox:
+            return TRY(JPEG2000ImageHeaderBox::create_from_stream(stream));
+        default:
+            return OptionalNone {};
+        }
     };
 
     TRY(SuperBox::read_from_stream(stream, move(make_subbox)));
@@ -22,6 +27,35 @@ ErrorOr<void> JPEG2000HeaderBox::read_from_stream(BoxStream& stream)
 void JPEG2000HeaderBox::dump(String const& prepend) const
 {
     SuperBox::dump(prepend);
+}
+
+ErrorOr<void> JPEG2000ImageHeaderBox::read_from_stream(BoxStream& stream)
+{
+    height = TRY(stream.read_value<BigEndian<u32>>());
+    width = TRY(stream.read_value<BigEndian<u32>>());
+    num_components = TRY(stream.read_value<BigEndian<u16>>());
+    bits_per_component = TRY(stream.read_value<u8>());
+    compression_type = TRY(stream.read_value<u8>());
+    is_colorspace_unknown = TRY(stream.read_value<u8>());
+    contains_intellectual_property_rights = TRY(stream.read_value<u8>());
+    return {};
+}
+
+void JPEG2000ImageHeaderBox::dump(String const& prepend) const
+{
+    Box::dump(prepend);
+    outln("{}- height = {}", prepend, height);
+    outln("{}- width = {}", prepend, width);
+    outln("{}- num_components = {}", prepend, num_components);
+    if (bits_per_component == 0xFF) {
+        outln("{}- components vary in bit depth", prepend);
+    } else {
+        outln("{}- are_components_signed = {}", prepend, (bits_per_component & 0x80) != 0);
+        outln("{}- bits_per_component = {}", prepend, (bits_per_component & 0x7f) + 1);
+    }
+    outln("{}- compression_type = {}", prepend, compression_type);
+    outln("{}- is_colorspace_unknown = {}", prepend, is_colorspace_unknown);
+    outln("{}- contains_intellectual_property_rights = {}", prepend, contains_intellectual_property_rights);
 }
 
 ErrorOr<void> JPEG2000SignatureBox::read_from_stream(BoxStream& stream)
--- Userland/Libraries/LibGfx/ImageFormats/ISOBMFF/JPEG2000Boxes.h
+++ Userland/Libraries/LibGfx/ImageFormats/ISOBMFF/JPEG2000Boxes.h
@@ -14,6 +14,19 @@ struct JPEG2000HeaderBox final : public SuperBox {
     BOX_SUBTYPE(JPEG2000HeaderBox);
 };
 
+// I.5.3.1 Image Header box
+struct JPEG2000ImageHeaderBox final : public Box {
+    BOX_SUBTYPE(JPEG2000ImageHeaderBox);
+
+    u32 height { 0 };
+    u32 width { 0 };
+    u16 num_components { 0 };
+    u8 bits_per_component { 0 };
+    u8 compression_type { 0 };
+    u8 is_colorspace_unknown { 0 };
+    u8 contains_intellectual_property_rights { 0 };
+};
+
 struct JPEG2000SignatureBox final : public Box {
     BOX_SUBTYPE(JPEG2000SignatureBox);
 
