0879bb30bde314c6f83739e2722f42d2c27eda6c
Nico Weber <thakis@chromium.org>
2024-03-22 13:50:02 -0400

    LibGfx/ISOBMFF: Start creating JPEG2000 box types
    `isobmff` can now dump the id in a JPEG2000SignatureBox.
    Creates JPEG2000Boxes.{h,cpp} to house JPEG2000 box types.

---
+++ Userland/Libraries/LibGfx/ImageFormats/ISOBMFF/JPEG2000Boxes.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2024, Nico Weber <thakis@chromium.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include "JPEG2000Boxes.h"
+
+namespace Gfx::ISOBMFF {
+
+ErrorOr<void> JPEG2000SignatureBox::read_from_stream(BoxStream& stream)
+{
+    signature = TRY(stream.read_value<BigEndian<u32>>());
+    return {};
+}
+
+void JPEG2000SignatureBox::dump(String const& prepend) const
+{
+    Box::dump(prepend);
+    outln("{}- signature = {:#08x}", prepend, signature);
+}
+
+}
