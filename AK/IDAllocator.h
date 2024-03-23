eaeb72b80796ebaa54ef6c9d8015d693eb5faa44
jane400 <pmos@j4ne.de>
2024-03-23 23:53:34 +0100

    IDAllocator: add maximum_value and a increasing mode

--- AK/IDAllocator.h
+++ AK/IDAllocator.h
@@ -12,14 +12,19 @@
 
 namespace AK {
 
-// This class manages a pool of random ID's in the range N (default of 1) to INT32_MAX
+enum class IDAllocatorMode {
+    Random,
+    Increasing,
+};
+
+// This class manages a pool of random ID's in the range N1 (default of 1) to N2 (default of INT32_MAX)
 class IDAllocator {
 
 public:
-    IDAllocator() = default;
-
-    explicit IDAllocator(int minimum_value)
+    IDAllocator(int minimum_value = 1, int maximum_value = INT32_MAX, IDAllocatorMode mode = IDAllocatorMode::Random)
         : m_minimum_value(minimum_value)
+        , m_maximum_value(maximum_value)
+        , m_mode(mode)
     {
     }
 
@@ -27,15 +32,25 @@ public:
 
     int allocate()
     {
-        VERIFY(m_allocated_ids.size() < (INT32_MAX - 2));
+        VERIFY(static_cast<int>(m_allocated_ids.size()) < (m_maximum_value - 2));
         int id = 0;
-        for (;;) {
-            id = static_cast<int>(get_random_uniform(NumericLimits<int>::max()));
-            if (id < m_minimum_value)
-                continue;
-            if (m_allocated_ids.set(id) == AK::HashSetResult::InsertedNewEntry)
-                break;
+        if (m_mode == IDAllocatorMode::Random) {
+            for (;;) {
+                id = static_cast<int>(get_random_uniform(m_maximum_value));
+                if (id < m_minimum_value)
+                    continue;
+                if (m_allocated_ids.set(id) == AK::HashSetResult::InsertedNewEntry)
+                    break;
+            }
+        } else if (m_mode == IDAllocatorMode::Increasing) {
+            id = m_minimum_value;
+            for (;;) {
+                if (m_allocated_ids.set(id) == AK::HashSetResult::InsertedNewEntry)
+                    break;
+                ++id;
+            }
         }
+
         return id;
     }
 
@@ -46,7 +61,9 @@ public:
 
 private:
     HashTable<int> m_allocated_ids;
-    int m_minimum_value { 1 };
+    int m_minimum_value;
+    int m_maximum_value;
+    IDAllocatorMode m_mode;
 };
 }
 
