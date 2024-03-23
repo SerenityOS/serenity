bb5ad12e438fde2e53580c2e2949c88d94dc6d92
LekKit <50500857+LekKit@users.noreply.github.com>
2024-03-23 13:34:22 +0200

    Ports: Update rvvm to 0.6
    - New upstream stable version is available
    - Networking is now fully stable and enabled by default
    - SDL2 backend is now available alongside SDL1, so switch to it
    - Fixed a name collision of PAGE_SIZE with Serenity headers
    - Disable threaded IO on Serenity for now
    - Many other changes and fixes
    - See https://github.com/LekKit/RVVM/releases/tag/v0.6 for more

--- Ports/rvvm/package.sh
+++ Ports/rvvm/package.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env -S bash ../.port_include.sh
+
 port='rvvm'
-version='0.5'
-archive_hash='3a1dbb91ad04f068078bc6c6c27cc5792eebc111907cb5a14bde158fe6e757c9'
+version='0.6'
+archive_hash='97e98c95d8785438758b81fb5c695b8eafb564502c6af7f52555b056e3bb7d7a'
 files=(
     "https://github.com/LekKit/RVVM/archive/v${version}.tar.gz#${archive_hash}"
 )
 workdir="RVVM-${version}"
-depends=('sdl12-compat')
+depends=('SDL2')
 
 build_opts=(
-    'GIT_COMMIT=76796ba'
+    'GIT_COMMIT=f937fd8'
     'OS=SerenityOS'
-    'USE_NET=1'
-    'USE_SDL=1'
+    'USE_SDL=2'
 )
 makeopts+=("${build_opts[@]}" 'all' 'lib')
 installopts+=("${build_opts[@]}")
