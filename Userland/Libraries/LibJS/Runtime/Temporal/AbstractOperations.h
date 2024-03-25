c4b20c1d77dbbb019c2f87e1c1cd8ba4a81fb98f
Daniel La Rocque <larocq17@myumanitoba.ca>
2024-03-25 11:18:42 -0500

    LibJS: Remove ToTemporalDateTimeRoundingIncrement
    This is an editorial change in the Temporal spec.
    
    See: https://github.com/tc39/proposal-temporal/commit/aca38be

--- Userland/Libraries/LibJS/Runtime/Temporal/AbstractOperations.h
+++ Userland/Libraries/LibJS/Runtime/Temporal/AbstractOperations.h
@@ -147,7 +147,6 @@ ThrowCompletionOr<String> to_time_zone_name_option(VM&, Object const& normalized
 ThrowCompletionOr<String> to_show_offset_option(VM&, Object const& normalized_options);
 ThrowCompletionOr<double> to_temporal_rounding_increment(VM& vm, Object const& normalized_options);
 ThrowCompletionOr<u64> validate_temporal_rounding_increment(VM& vm, double increment, double dividend, bool inclusive);
-ThrowCompletionOr<u64> to_temporal_date_time_rounding_increment(VM&, Object const& normalized_options, StringView smallest_unit);
 ThrowCompletionOr<SecondsStringPrecision> to_seconds_string_precision_record(VM&, Object const& normalized_options);
 ThrowCompletionOr<Optional<String>> get_temporal_unit(VM&, Object const& normalized_options, PropertyKey const&, UnitGroup, TemporalUnitDefault const& default_, Vector<StringView> const& extra_values = {});
 
