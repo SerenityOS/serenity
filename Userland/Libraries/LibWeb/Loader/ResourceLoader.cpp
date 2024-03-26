3d78f86a2f738f2e4dd8d6bc98bb368a357e7d06
Andreas Kling <kling@serenityos.org>
2024-03-26 11:39:45 +0100

    LibWeb: Don't ask RequestServer to prefetch DNS for file: and data: URLs

--- Userland/Libraries/LibWeb/Loader/ResourceLoader.cpp
+++ Userland/Libraries/LibWeb/Loader/ResourceLoader.cpp
@@ -68,6 +68,9 @@ ResourceLoader::ResourceLoader(NonnullRefPtr<ResourceLoaderConnector> connector)
 
 void ResourceLoader::prefetch_dns(URL::URL const& url)
 {
+    if (url.scheme().is_one_of("file"sv, "data"sv))
+        return;
+
     if (ContentFilter::the().is_filtered(url)) {
         dbgln("ResourceLoader: Refusing to prefetch DNS for '{}': \033[31;1mURL was filtered\033[0m", url);
         return;
