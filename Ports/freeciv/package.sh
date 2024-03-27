df0890a1fb1bb837e9903babe1c724141281b624
Kenneth Myhra <kennethmyhra@gmail.com>
2024-03-27 20:22:04 +0100

    Ports: Format freeciv's package script
    This formats freeciv's package script according to our ruleset.

--- Ports/freeciv/package.sh
+++ Ports/freeciv/package.sh
@@ -1,15 +1,31 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=freeciv
-version=3.0.8
-useconfigure=true
-use_fresh_config_sub=true
-config_sub_paths=("bootstrap/config.sub")
-configopts=("--enable-client=sdl2" "--enable-fcmp=no")
+port='freeciv'
+version='3.0.8'
+useconfigure='true'
+use_fresh_config_sub='true'
+config_sub_paths=(
+    'bootstrap/config.sub'
+)
+configopts=(
+    '--enable-client=sdl2'
+    '--enable-fcmp=no'
+)
 files=(
     "http://files.freeciv.org/stable/freeciv-${version}.tar.xz#3b5aa32f628890be1741c3ac942cee82c79c065f8db6baff18d734a5c0e776d4"
 )
-depends=("SDL2" "SDL2_image" "SDL2_mixer" "SDL2_ttf" "SDL2_gfx" "zstd" "libicu" "xz" "gettext" "curl")
-launcher_name=Freeciv
+depends=(
+    'curl'
+    'gettext'
+    'libicu'
+    'SDL2'
+    'SDL2_gfx'
+    'SDL2_image'
+    'SDL2_mixer'
+    'SDL2_ttf'
+    'zstd'
+    'xz'
+)
+launcher_name='Freeciv'
 launcher_category='&Games'
-launcher_command=/usr/local/bin/freeciv-sdl2
-icon_file=windows/client.ico
+launcher_command='/usr/local/bin/freeciv-sdl2'
+icon_file='windows/client.ico'
