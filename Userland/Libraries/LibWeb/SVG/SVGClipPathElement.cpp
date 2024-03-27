c1b5fe61d17628267c91266a7a590cb11ed492fa
MacDue <macdue@dueutil.tech>
2024-03-27 00:13:16 +0000

    LibWeb: Lay out SVG `<clipPath>` uses
    This uses the same trick as done for masks in #23554. Each use of an
    SVG `<clipPath>` becomes it's own layout subtree rooted at it's user.
    This allows each use have it's own layout (which allows supporting
    features such as `clipPathUnits`).

--- Userland/Libraries/LibWeb/SVG/SVGClipPathElement.cpp
+++ Userland/Libraries/LibWeb/SVG/SVGClipPathElement.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <LibWeb/Bindings/Intrinsics.h>
+#include <LibWeb/SVG/AttributeNames.h>
 #include <LibWeb/SVG/SVGClipPathElement.h>
 
 namespace Web::SVG {
@@ -26,8 +27,16 @@ void SVGClipPathElement::initialize(JS::Realm& realm)
     WEB_SET_PROTOTYPE_FOR_INTERFACE(SVGClipPathElement);
 }
 
+void SVGClipPathElement::attribute_changed(FlyString const& name, Optional<String> const& value)
+{
+    SVGElement::attribute_changed(name, value);
+    if (name == AttributeNames::clipPathUnits)
+        m_clip_path_units = AttributeParser::parse_units(value.value_or(String {}));
+}
+
 JS::GCPtr<Layout::Node> SVGClipPathElement::create_layout_node(NonnullRefPtr<CSS::StyleProperties>)
 {
+    // Clip paths are handled as a special case in the TreeBuilder.
     return nullptr;
 }
 
--- Userland/Libraries/LibWeb/SVG/SVGClipPathElement.h
+++ Userland/Libraries/LibWeb/SVG/SVGClipPathElement.h
@@ -6,23 +6,47 @@
 
 #pragma once
 
+#include <LibWeb/SVG/AttributeParser.h>
 #include <LibWeb/SVG/SVGElement.h>
+#include <LibWeb/SVG/SVGViewport.h>
 
 namespace Web::SVG {
 
-class SVGClipPathElement final : public SVGElement {
+class SVGClipPathElement final : public SVGElement
+    , public SVGViewport {
     WEB_PLATFORM_OBJECT(SVGClipPathElement, SVGElement);
     JS_DECLARE_ALLOCATOR(SVGClipPathElement);
 
 public:
     virtual ~SVGClipPathElement();
 
+    virtual Optional<ViewBox> view_box() const override
+    {
+        // Same trick as SVGMaskElement.
+        if (clip_path_units() == MaskContentUnits::ObjectBoundingBox)
+            return ViewBox { 0, 0, 1, 1 };
+        return {};
+    }
+
+    virtual Optional<PreserveAspectRatio> preserve_aspect_ratio() const override
+    {
+        return PreserveAspectRatio { PreserveAspectRatio::Align::None, {} };
+    }
+
+    virtual void attribute_changed(FlyString const& name, Optional<String> const& value) override;
+
+    ClipPathUnits clip_path_units() const
+    {
+        return m_clip_path_units.value_or(ClipPathUnits::UserSpaceOnUse);
+    }
+
     virtual JS::GCPtr<Layout::Node> create_layout_node(NonnullRefPtr<CSS::StyleProperties>) override;
 
 private:
     SVGClipPathElement(DOM::Document&, DOM::QualifiedName);
-
     virtual void initialize(JS::Realm&) override;
+
+    Optional<ClipPathUnits> m_clip_path_units = {};
 };
 
 }
