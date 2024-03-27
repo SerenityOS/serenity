ebbda87317092f9830ee5bf6b248e27abcba3666
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2024-03-27 14:54:46 +0100

    LibWeb: Fix hanging location.reload() when SHE is populated by fetching
    Adds a hack to address spec bug https://github.com/whatwg/html/issues/9869

--- Userland/Libraries/LibWeb/HTML/DocumentState.cpp
+++ Userland/Libraries/LibWeb/HTML/DocumentState.cpp
@@ -27,4 +27,22 @@ void DocumentState::visit_edges(Cell::Visitor& visitor)
     }
 }
 
+JS::NonnullGCPtr<DocumentState> DocumentState::clone() const
+{
+    JS::NonnullGCPtr<DocumentState> cloned = *heap().allocate_without_realm<DocumentState>();
+    cloned->m_document = m_document;
+    cloned->m_history_policy_container = m_history_policy_container;
+    cloned->m_request_referrer = m_request_referrer;
+    cloned->m_request_referrer_policy = m_request_referrer_policy;
+    cloned->m_initiator_origin = m_initiator_origin;
+    cloned->m_origin = m_origin;
+    cloned->m_about_base_url = m_about_base_url;
+    cloned->m_nested_histories = m_nested_histories;
+    cloned->m_resource = m_resource;
+    cloned->m_reload_pending = m_reload_pending;
+    cloned->m_ever_populated = m_ever_populated;
+    cloned->m_navigable_target_name = m_navigable_target_name;
+    return cloned;
+}
+
 }
--- Userland/Libraries/LibWeb/HTML/SessionHistoryEntry.cpp
+++ Userland/Libraries/LibWeb/HTML/SessionHistoryEntry.cpp
@@ -28,6 +28,23 @@ SessionHistoryEntry::SessionHistoryEntry()
 {
 }
 
+JS::NonnullGCPtr<SessionHistoryEntry> SessionHistoryEntry::clone() const
+{
+    JS::NonnullGCPtr<SessionHistoryEntry> entry = *heap().allocate_without_realm<SessionHistoryEntry>();
+    entry->m_step = m_step;
+    entry->m_url = m_url;
+    entry->m_document_state = m_document_state->clone();
+    entry->m_classic_history_api_state = m_classic_history_api_state;
+    entry->m_navigation_api_state = m_navigation_api_state;
+    entry->m_navigation_api_key = m_navigation_api_key;
+    entry->m_navigation_api_id = m_navigation_api_id;
+    entry->m_scroll_restoration_mode = m_scroll_restoration_mode;
+    entry->m_policy_container = m_policy_container;
+    entry->m_browsing_context_name = m_browsing_context_name;
+    entry->m_original_source_browsing_context = m_original_source_browsing_context;
+    return entry;
+}
+
 // https://html.spec.whatwg.org/multipage/browsing-the-web.html#she-document
 JS::GCPtr<DOM::Document> SessionHistoryEntry::document() const
 {
