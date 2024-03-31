c5dced22b8df37c1071a586fee89e564be5ba862
MacDue <macdue@dueutil.tech>
2024-03-31 14:54:00 +0100

    LibWeb: Stub out `SVGTransformList`

---
+++ Userland/Libraries/LibWeb/SVG/SVGTransformList.cpp
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2024, MacDue <macdue@dueutil.tech>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibWeb/Bindings/Intrinsics.h>
+#include <LibWeb/SVG/SVGTransformList.h>
+#include <LibWeb/WebIDL/ExceptionOr.h>
+
+namespace Web::SVG {
+
+JS_DEFINE_ALLOCATOR(SVGTransformList);
+
+JS::NonnullGCPtr<SVGTransformList> SVGTransformList::create(JS::Realm& realm)
+{
+    return realm.heap().allocate<SVGTransformList>(realm, realm);
+}
+
+SVGTransformList::~SVGTransformList() = default;
+
+SVGTransformList::SVGTransformList(JS::Realm& realm)
+    : PlatformObject(realm) {};
+
+// https://svgwg.org/svg2-draft/single-page.html#types-__svg__SVGNameList__getItem
+WebIDL::ExceptionOr<JS::NonnullGCPtr<SVGTransform>> SVGTransformList::get_item(WebIDL::UnsignedLong index)
+{
+    // 1. If index is greater than or equal to the length of the list, then throw an IndexSizeError.
+    if (index >= m_transforms.size())
+        return WebIDL::IndexSizeError::create(realm(), "SVGTransformList index out of bounds"_fly_string);
+    // 2. Return the element in the list at position index.
+    return m_transforms.at(index);
+}
+
+// https://svgwg.org/svg2-draft/single-page.html#types-__svg__SVGNameList__appendItem
+JS::NonnullGCPtr<SVGTransform> SVGTransformList::append_item(JS::NonnullGCPtr<SVGTransform> new_item)
+{
+    // FIXME: This does not implement the steps from the specification.
+    m_transforms.append(new_item);
+    return new_item;
+}
+
+void SVGTransformList::initialize(JS::Realm& realm)
+{
+    Base::initialize(realm);
+    WEB_SET_PROTOTYPE_FOR_INTERFACE(SVGTransformList);
+}
+
+void SVGTransformList::visit_edges(Cell::Visitor& visitor)
+{
+    Base::visit_edges(visitor);
+    for (auto transform : m_transforms)
+        transform->visit_edges(visitor);
+}
+
+}
---
+++ Userland/Libraries/LibWeb/SVG/SVGTransformList.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2024, MacDue <macdue@dueutil.tech>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/Vector.h>
+#include <LibWeb/Bindings/PlatformObject.h>
+#include <LibWeb/SVG/SVGTransform.h>
+#include <LibWeb/WebIDL/Types.h>
+
+namespace Web::SVG {
+
+// https://svgwg.org/svg2-draft/single-page.html#coords-InterfaceSVGTransformList
+class SVGTransformList final : public Bindings::PlatformObject {
+    WEB_PLATFORM_OBJECT(SVGTransformList, Bindings::PlatformObject);
+    JS_DECLARE_ALLOCATOR(SVGTransformList);
+
+public:
+    [[nodiscard]] static JS::NonnullGCPtr<SVGTransformList> create(JS::Realm& realm);
+    virtual ~SVGTransformList() override;
+
+    WebIDL::ExceptionOr<JS::NonnullGCPtr<SVGTransform>> get_item(WebIDL::UnsignedLong index);
+
+    JS::NonnullGCPtr<SVGTransform> append_item(JS::NonnullGCPtr<SVGTransform> new_item);
+
+private:
+    SVGTransformList(JS::Realm& realm);
+
+    virtual void initialize(JS::Realm& realm) override;
+    virtual void visit_edges(Cell::Visitor& visitor) override;
+
+    Vector<JS::NonnullGCPtr<SVGTransform>> m_transforms;
+};
+
+}
