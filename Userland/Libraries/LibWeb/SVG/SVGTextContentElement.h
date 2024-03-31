329262d54f789509d5465e79816d4be364dfedb0
MacDue <macdue@dueutil.tech>
2024-04-01 00:45:54 +0100

    LibWeb: Stub `SVGTextContentElement.getStartPositionOfChar()`

--- Userland/Libraries/LibWeb/SVG/SVGTextContentElement.h
+++ Userland/Libraries/LibWeb/SVG/SVGTextContentElement.h
@@ -6,9 +6,11 @@
 
 #pragma once
 
+#include <LibWeb/Geometry/DOMPoint.h>
 #include <LibWeb/SVG/AttributeParser.h>
 #include <LibWeb/SVG/SVGGraphicsElement.h>
 #include <LibWeb/WebIDL/ExceptionOr.h>
+#include <LibWeb/WebIDL/Types.h>
 
 namespace Web::SVG {
 
@@ -17,11 +19,13 @@ class SVGTextContentElement : public SVGGraphicsElement {
     WEB_PLATFORM_OBJECT(SVGTextContentElement, SVGGraphicsElement);
 
 public:
-    WebIDL::ExceptionOr<int> get_number_of_chars() const;
+    WebIDL::ExceptionOr<WebIDL::Long> get_number_of_chars() const;
 
     Optional<TextAnchor> text_anchor() const;
 
     ByteString text_contents() const;
+
+    JS::NonnullGCPtr<Geometry::DOMPoint> get_start_position_of_char(WebIDL::UnsignedLong charnum);
 
 protected:
     SVGTextContentElement(DOM::Document&, DOM::QualifiedName);
