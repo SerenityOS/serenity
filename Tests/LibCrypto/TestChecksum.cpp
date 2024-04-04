1f1a2b3f7d10e9bd379548152b7c43c4b679e2a4
Timothy Flynn <trflynn89@pm.me>
2024-04-03 21:41:55 -0400

    LibCrypto: Avoid needless ByteString usage in unit tests

--- Tests/LibCrypto/TestChecksum.cpp
+++ Tests/LibCrypto/TestChecksum.cpp
@@ -16,11 +16,11 @@ TEST_CASE(test_adler32)
         EXPECT_EQ(digest, expected_result);
     };
 
-    do_test(ByteString("").bytes(), 0x1);
-    do_test(ByteString("a").bytes(), 0x00620062);
-    do_test(ByteString("abc").bytes(), 0x024d0127);
-    do_test(ByteString("message digest").bytes(), 0x29750586);
-    do_test(ByteString("abcdefghijklmnopqrstuvwxyz").bytes(), 0x90860b20);
+    do_test(""sv.bytes(), 0x1);
+    do_test("a"sv.bytes(), 0x00620062);
+    do_test("abc"sv.bytes(), 0x024d0127);
+    do_test("message digest"sv.bytes(), 0x29750586);
+    do_test("abcdefghijklmnopqrstuvwxyz"sv.bytes(), 0x90860b20);
 }
 
 TEST_CASE(test_cksum)
@@ -30,9 +30,9 @@ TEST_CASE(test_cksum)
         EXPECT_EQ(digest, expected_result);
     };
 
-    do_test(ByteString("").bytes(), 0xFFFFFFFF);
-    do_test(ByteString("The quick brown fox jumps over the lazy dog").bytes(), 0x7BAB9CE8);
-    do_test(ByteString("various CRC algorithms input data").bytes(), 0xEFB5CA4F);
+    do_test(""sv.bytes(), 0xFFFFFFFF);
+    do_test("The quick brown fox jumps over the lazy dog"sv.bytes(), 0x7BAB9CE8);
+    do_test("various CRC algorithms input data"sv.bytes(), 0xEFB5CA4F);
 }
 
 TEST_CASE(test_cksum_atomic_digest)
@@ -43,10 +43,10 @@ TEST_CASE(test_cksum_atomic_digest)
 
     Crypto::Checksum::cksum cksum;
 
-    cksum.update(ByteString("Well").bytes());
-    cksum.update(ByteString(" hello ").bytes());
+    cksum.update("Well"sv.bytes());
+    cksum.update(" hello "sv.bytes());
     cksum.digest();
-    cksum.update(ByteString("friends").bytes());
+    cksum.update("friends"sv.bytes());
     auto digest = cksum.digest();
 
     compare(digest, 0x2D65C7E0);
@@ -59,7 +59,7 @@ TEST_CASE(test_crc32)
         EXPECT_EQ(digest, expected_result);
     };
 
-    do_test(ByteString("").bytes(), 0x0);
-    do_test(ByteString("The quick brown fox jumps over the lazy dog").bytes(), 0x414FA339);
-    do_test(ByteString("various CRC algorithms input data").bytes(), 0x9BD366AE);
+    do_test(""sv.bytes(), 0x0);
+    do_test("The quick brown fox jumps over the lazy dog"sv.bytes(), 0x414FA339);
+    do_test("various CRC algorithms input data"sv.bytes(), 0x9BD366AE);
 }
