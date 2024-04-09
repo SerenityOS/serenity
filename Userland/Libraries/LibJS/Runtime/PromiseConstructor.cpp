cad95ce274d172feb6228a381da37473d50397ce
Linus Groh <mail@linusgroh.de>
2024-04-09 07:02:15 +0100

    LibJS: Implement Promise.try()
    See: https://github.com/tc39/proposal-promise-try

--- Userland/Libraries/LibJS/Runtime/PromiseConstructor.cpp
+++ Userland/Libraries/LibJS/Runtime/PromiseConstructor.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2023, Linus Groh <linusg@serenityos.org>
+ * Copyright (c) 2021-2024, Linus Groh <linusg@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -238,6 +238,7 @@ void PromiseConstructor::initialize(Realm& realm)
     define_native_function(realm, vm.names.race, race, 1, attr);
     define_native_function(realm, vm.names.reject, reject, 1, attr);
     define_native_function(realm, vm.names.resolve, resolve, 1, attr);
+    define_native_function(realm, vm.names.try_, try_, 1, attr);
     define_native_function(realm, vm.names.withResolvers, with_resolvers, 0, attr);
 
     define_native_accessor(realm, vm.well_known_symbol_species(), symbol_species_getter, {}, Attribute::Configurable);
@@ -456,6 +457,43 @@ JS_DEFINE_NATIVE_FUNCTION(PromiseConstructor::resolve)
 
     // 3. Return ? PromiseResolve(C, x).
     return TRY(promise_resolve(vm, constructor.as_object(), value));
+}
+
+// 1 Promise.try ( callbackfn, ...args ), https://tc39.es/proposal-promise-try/#sec-promise.try
+JS_DEFINE_NATIVE_FUNCTION(PromiseConstructor::try_)
+{
+    auto callbackfn = vm.argument(0);
+    Span<Value> args;
+    if (vm.argument_count() > 1) {
+        args = vm.running_execution_context().arguments.span().slice(1, vm.argument_count() - 1);
+    }
+
+    // 1. Let C be the this value.
+    auto constructor = vm.this_value();
+
+    // 2. If C is not an Object, throw a TypeError exception.
+    if (!constructor.is_object())
+        return vm.throw_completion<TypeError>(ErrorType::NotAnObject, constructor.to_string_without_side_effects());
+
+    // 3. Let promiseCapability be ? NewPromiseCapability(C).
+    auto promise_capability = TRY(new_promise_capability(vm, constructor));
+
+    // 4. Let status be Completion(Call(callbackfn, undefined, args)).
+    auto status = JS::call(vm, callbackfn, js_undefined(), args);
+
+    // 5. If status is an abrupt completion, then
+    if (status.is_throw_completion()) {
+        // a. Perform ? Call(promiseCapability.[[Reject]], undefined, « status.[[Value]] »).
+        TRY(JS::call(vm, *promise_capability->reject(), js_undefined(), *status.throw_completion().value()));
+    }
+    // 6. Else,
+    else {
+        // a. Perform ? Call(promiseCapability.[[Resolve]], undefined, « status.[[Value]] »).
+        TRY(JS::call(vm, *promise_capability->resolve(), js_undefined(), status.value()));
+    }
+
+    // 7. Return promiseCapability.[[Promise]].
+    return promise_capability->promise();
 }
 
 // 27.2.4.8 Promise.withResolvers ( ), https://tc39.es/ecma262/#sec-promise.withResolvers
