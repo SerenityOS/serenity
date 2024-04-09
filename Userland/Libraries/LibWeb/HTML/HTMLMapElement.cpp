7fa45c5fdf1dadd79cf1a3331fc7b059de41f45a
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2024-04-09 18:30:03 +0200

    LibWeb: Add map element areas property

--- Userland/Libraries/LibWeb/HTML/HTMLMapElement.cpp
+++ Userland/Libraries/LibWeb/HTML/HTMLMapElement.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <LibWeb/Bindings/Intrinsics.h>
+#include <LibWeb/HTML/HTMLAreaElement.h>
 #include <LibWeb/HTML/HTMLMapElement.h>
 
 namespace Web::HTML {
@@ -22,6 +23,24 @@ void HTMLMapElement::initialize(JS::Realm& realm)
 {
     Base::initialize(realm);
     WEB_SET_PROTOTYPE_FOR_INTERFACE(HTMLMapElement);
+}
+
+void HTMLMapElement::visit_edges(Cell::Visitor& visitor)
+{
+    Base::visit_edges(visitor);
+    visitor.visit(m_areas);
+}
+
+// https://html.spec.whatwg.org/multipage/image-maps.html#dom-map-areas
+JS::NonnullGCPtr<DOM::HTMLCollection> HTMLMapElement::areas()
+{
+    // The areas attribute must return an HTMLCollection rooted at the map element, whose filter matches only area elements.
+    if (!m_areas) {
+        m_areas = DOM::HTMLCollection::create(*this, DOM::HTMLCollection::Scope::Descendants, [](Element const& element) {
+            return is<HTML::HTMLAreaElement>(element);
+        });
+    }
+    return *m_areas;
 }
 
 }
