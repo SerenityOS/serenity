7475413946e73a60155ebad3b727b20c8abe1b26
Dan Klishch <danilklishch@gmail.com>
2024-04-26 22:46:12 -0400

    LibC: Modernize and simplify LibC/CMakeLists.txt

--- Userland/Libraries/LibC/ssp.cpp
+++ Userland/Libraries/LibC/ssp.cpp
@@ -11,18 +11,10 @@
 #include <sys/internals.h>
 #include <unistd.h>
 
-#if defined __SSP__ || defined __SSP_ALL__
-#    error "file must not be compiled with stack protection enabled on it. Use -fno-stack-protector"
-#endif
-
-extern "C" {
-
-__attribute__((noreturn)) void __stack_chk_fail()
+extern "C" [[gnu::noreturn, gnu::no_stack_protector]] void __stack_chk_fail()
 {
     dbgln("Error: USERSPACE({}) Stack protector failure, stack smashing detected!", getpid());
     if (__stdio_is_initialized)
         warnln("Error: Stack protector failure, stack smashing detected!");
     abort();
 }
-
-} // extern "C"
