21f5baba78f2fb5e695f1a25e227035f8dc70db2
Dan Klishch <danilklishch@gmail.com>
2024-04-27 23:09:43 -0400

    LibC: Remove now redundant NO_TLS guards
    Since DynamicLoader is compiled with -fdata-sections and --gc-sections,
    unused thread_local variables won't create TLS section in it anymore.

--- Userland/Libraries/LibC/netdb.h
+++ Userland/Libraries/LibC/netdb.h
@@ -55,11 +55,7 @@ struct protoent* getprotobynumber(int proto);
 struct protoent* getprotoent(void);
 void setprotoent(int stay_open);
 
-#ifdef NO_TLS
-extern int h_errno;
-#else
 extern __thread int h_errno;
-#endif
 
 #define HOST_NOT_FOUND 101
 #define NO_DATA 102
