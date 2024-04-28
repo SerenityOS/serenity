573771a77ef69e0e5e37911fbad89546acc54b15
Kenneth Myhra <kennethmyhra@gmail.com>
2024-04-28 10:05:12 +0200

    LibWeb: Implement AO transform_stream_set_up()

--- Userland/Libraries/LibWeb/Streams/AbstractOperations.h
+++ Userland/Libraries/LibWeb/Streams/AbstractOperations.h
@@ -179,6 +179,7 @@ JS::NonnullGCPtr<WebIDL::Promise> transform_stream_default_source_pull_algorithm
 void transform_stream_error(TransformStream&, JS::Value error);
 void transform_stream_error_writable_and_unblock_write(TransformStream&, JS::Value error);
 void transform_stream_set_backpressure(TransformStream&, bool backpressure);
+void transform_stream_set_up(TransformStream&, JS::NonnullGCPtr<TransformAlgorithm>, JS::GCPtr<FlushAlgorithm> = {}, JS::GCPtr<CancelAlgorithm> = {});
 
 bool is_non_negative_number(JS::Value);
 bool can_transfer_array_buffer(JS::ArrayBuffer const& array_buffer);
