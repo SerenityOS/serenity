3691ad14644ceb80013f770cadf4df7834879864
Liav A. <liavalb@gmail.com>
2024-05-04 15:06:18 +0300

    DeviceMapper: Use DeviceNodeType from Kernel API in all possible cases
    Having a separate enum is pointless, and opens the possibility of having
    duplicates elsewhere. Therefore, just unify everything under one known
    enum.

--- Userland/Services/DeviceMapper/DeviceEventLoop.h
+++ Userland/Services/DeviceMapper/DeviceEventLoop.h
@@ -18,16 +18,11 @@ namespace DeviceMapper {
 
 class DeviceEventLoop {
 public:
-    enum class UnixDeviceType {
-        BlockDevice,
-        CharacterDevice,
-    };
-
     struct DeviceNodeMatch {
         StringView permission_group;
         StringView family_type_literal;
         StringView path_pattern;
-        DeviceNodeFamily::Type unix_device_type;
+        DeviceNodeType device_node_type;
         MajorNumber major_number;
         mode_t create_mode;
     };
@@ -38,11 +33,11 @@ public:
     ErrorOr<void> drain_events_from_devctl();
 
 private:
-    Optional<DeviceNodeFamily&> find_device_node_family(DeviceNodeFamily::Type unix_device_type, MajorNumber major_number) const;
-    ErrorOr<NonnullRefPtr<DeviceNodeFamily>> find_or_register_new_device_node_family(DeviceNodeMatch const& match, DeviceNodeFamily::Type unix_device_type, MajorNumber major_number);
+    Optional<DeviceNodeFamily&> find_device_node_family(DeviceNodeType, MajorNumber major_number) const;
+    ErrorOr<NonnullRefPtr<DeviceNodeFamily>> find_or_register_new_device_node_family(DeviceNodeMatch const& match, DeviceNodeType, MajorNumber major_number);
 
-    ErrorOr<void> register_new_device(DeviceNodeFamily::Type unix_device_type, MajorNumber major_number, MinorNumber minor_number);
-    ErrorOr<void> unregister_device(DeviceNodeFamily::Type unix_device_type, MajorNumber major_number, MinorNumber minor_number);
+    ErrorOr<void> register_new_device(DeviceNodeType, MajorNumber major_number, MinorNumber minor_number);
+    ErrorOr<void> unregister_device(DeviceNodeType, MajorNumber major_number, MinorNumber minor_number);
 
     ErrorOr<void> read_one_or_eof(DeviceEvent& event);
 
