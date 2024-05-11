6cc8c34e4edc86335df751cddebf2f52db3a8ad5
Kenneth Myhra <kennethmyhra@gmail.com>
2024-05-11 22:06:09 +0200

    Ports: Add fluidsynth

---
+++ Ports/fluidsynth/package.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port='fluidsynth'
+version='2.3.5'
+useconfigure='true'
+configopts=(
+    '-G Ninja'
+    "-S fluidsynth-${version}"
+    "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
+)
+files=(
+    "https://github.com/FluidSynth/fluidsynth/archive/refs/tags/v${version}.tar.gz#f89e8e983ecfb4a5b4f5d8c2b9157ed18d15ed2e36246fa782f18abaea550e0d"
+)
+depends=(
+    'glib'
+)
+
+configure() {
+    cmake \
+        "${configopts[@]}" \
+        -B fluidsynth-build
+}
+
+build() {
+    ninja -C fluidsynth-build
+}
+
+install() {
+    ninja -C fluidsynth-build install
+}
