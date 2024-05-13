fd1cdfc3153d0ceb34fdfc5bfe151b00946382d0
Andrew Kaster <akaster@serenityos.org>
2024-05-13 13:55:43 -0600

    LibWeb: Stub out FontFaceSet.status and .ready attributes
    WPT wants to check these properties before running ref tests.
    This fixes a ton of crashes in our CI WPT runs.

--- Userland/Libraries/LibWeb/CSS/FontFaceSet.h
+++ Userland/Libraries/LibWeb/CSS/FontFaceSet.h
@@ -6,13 +6,14 @@
 
 #pragma once
 
+#include <LibWeb/Bindings/FontFaceSetPrototype.h>
 #include <LibWeb/Bindings/PlatformObject.h>
 #include <LibWeb/CSS/FontFace.h>
 
 namespace Web::CSS {
 
 class FontFaceSet final : public Bindings::PlatformObject {
-    WEB_PLATFORM_OBJECT(FontFace, Bindings::PlatformObject);
+    WEB_PLATFORM_OBJECT(FontFaceSet, Bindings::PlatformObject);
     JS_DECLARE_ALLOCATOR(FontFaceSet);
 
 public:
@@ -23,10 +24,17 @@ public:
     JS::NonnullGCPtr<FontFaceSet> add(JS::Handle<FontFace> face);
     JS::ThrowCompletionOr<JS::NonnullGCPtr<JS::Promise>> load(String const& font, String const& text);
 
+    JS::NonnullGCPtr<JS::Promise> ready() const;
+    Bindings::FontFaceSetLoadStatus status() const { return m_status; }
+
 private:
-    FontFaceSet(JS::Realm&, Vector<JS::Handle<FontFace>> initial_faces);
+    FontFaceSet(JS::Realm&, JS::NonnullGCPtr<WebIDL::Promise> ready_promise, Vector<JS::Handle<FontFace>> initial_faces);
 
     virtual void initialize(JS::Realm&) override;
+    virtual void visit_edges(Cell::Visitor&) override;
+
+    JS::GCPtr<WebIDL::Promise> m_ready_promise; // [[ReadyPromise]]
+    Bindings::FontFaceSetLoadStatus m_status { Bindings::FontFaceSetLoadStatus::Loading };
 };
 
 }
