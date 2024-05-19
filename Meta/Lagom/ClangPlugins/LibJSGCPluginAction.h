6801b94a3f8be9b9af548a3a1adfc4fe2c334ff5
Matthew Olsson <matthewcolsson@gmail.com>
2024-05-19 11:39:43 -0700

    ClangPlugin: Add a PPCallbacks instance to collect JS_CELL-like macros

--- Meta/Lagom/ClangPlugins/LibJSGCPluginAction.h
+++ Meta/Lagom/ClangPlugins/LibJSGCPluginAction.h
@@ -10,6 +10,47 @@
 #include <clang/AST/RecursiveASTVisitor.h>
 #include <clang/Frontend/FrontendAction.h>
 
+struct LibJSCellMacro {
+    enum class Type {
+        JSCell,
+        JSObject,
+        JSEnvironment,
+        JSPrototypeObject,
+        WebPlatformObject,
+    };
+
+    struct Arg {
+        std::string text;
+        clang::SourceLocation location;
+    };
+
+    clang::SourceRange range;
+    Type type;
+    std::vector<Arg> args;
+
+    static char const* type_name(Type);
+};
+
+using LibJSCellMacroMap = std::unordered_map<unsigned int, std::vector<LibJSCellMacro>>;
+
+class LibJSPPCallbacks : public clang::PPCallbacks {
+public:
+    LibJSPPCallbacks(clang::Preprocessor& preprocessor, LibJSCellMacroMap& macro_map)
+        : m_preprocessor(preprocessor)
+        , m_macro_map(macro_map)
+    {
+    }
+
+    virtual void LexedFileChanged(clang::FileID curr_fid, LexedFileChangeReason, clang::SrcMgr::CharacteristicKind, clang::FileID, clang::SourceLocation) override;
+
+    virtual void MacroExpands(clang::Token const& name_token, clang::MacroDefinition const& definition, clang::SourceRange range, clang::MacroArgs const* args) override;
+
+private:
+    clang::Preprocessor& m_preprocessor;
+    std::vector<unsigned int> m_curr_fid_hash_stack;
+    LibJSCellMacroMap& m_macro_map;
+};
+
 class LibJSGCVisitor : public clang::RecursiveASTVisitor<LibJSGCVisitor> {
 public:
     explicit LibJSGCVisitor(clang::ASTContext& context)
