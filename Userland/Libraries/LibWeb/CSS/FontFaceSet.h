5a1ffdef6fb19e0e1a0bc2b56d437205201c8752
Andrew Kaster <akaster@serenityos.org>
2024-05-21 21:05:36 -0600

    LibWeb: Mark FontFaceSet as a setlike IDL interface
    And implement more of the constructor logic.

--- Userland/Libraries/LibWeb/CSS/FontFaceSet.h
+++ Userland/Libraries/LibWeb/CSS/FontFaceSet.h
@@ -6,6 +6,8 @@
 
 #pragma once
 
+#include <LibJS/Runtime/Set.h>
+#include <LibJS/Runtime/SetIterator.h>
 #include <LibWeb/Bindings/FontFaceSetPrototype.h>
 #include <LibWeb/Bindings/PlatformObject.h>
 #include <LibWeb/CSS/FontFace.h>
@@ -17,23 +19,29 @@ class FontFaceSet final : public Bindings::PlatformObject {
     JS_DECLARE_ALLOCATOR(FontFaceSet);
 
 public:
-    [[nodiscard]] static JS::NonnullGCPtr<FontFaceSet> construct_impl(JS::Realm&, Vector<JS::Handle<FontFace>> initial_faces);
+    [[nodiscard]] static JS::NonnullGCPtr<FontFaceSet> construct_impl(JS::Realm&, Vector<JS::Handle<FontFace>> const& initial_faces);
     [[nodiscard]] static JS::NonnullGCPtr<FontFaceSet> create(JS::Realm&);
     virtual ~FontFaceSet() override = default;
 
-    JS::NonnullGCPtr<FontFaceSet> add(JS::Handle<FontFace> face);
+    JS::NonnullGCPtr<JS::Set> set_entries() const { return m_set_entries; }
+
+    JS::NonnullGCPtr<FontFaceSet> add(JS::Handle<FontFace>);
+    bool delete_(JS::Handle<FontFace>);
+    void clear();
     JS::ThrowCompletionOr<JS::NonnullGCPtr<JS::Promise>> load(String const& font, String const& text);
 
     JS::NonnullGCPtr<JS::Promise> ready() const;
     Bindings::FontFaceSetLoadStatus status() const { return m_status; }
 
 private:
-    FontFaceSet(JS::Realm&, JS::NonnullGCPtr<WebIDL::Promise> ready_promise, Vector<JS::Handle<FontFace>> initial_faces);
+    FontFaceSet(JS::Realm&, JS::NonnullGCPtr<WebIDL::Promise> ready_promise, JS::NonnullGCPtr<JS::Set> set_entries);
 
     virtual void initialize(JS::Realm&) override;
     virtual void visit_edges(Cell::Visitor&) override;
 
+    JS::NonnullGCPtr<JS::Set> m_set_entries;
     JS::GCPtr<WebIDL::Promise> m_ready_promise; // [[ReadyPromise]]
+
     Bindings::FontFaceSetLoadStatus m_status { Bindings::FontFaceSetLoadStatus::Loading };
 };
 
