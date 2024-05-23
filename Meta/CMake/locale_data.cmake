93dec2f0f672be30808c446dc488f3e36c49dd9f
Timothy Flynn <trflynn89@pm.me>
2024-05-23 12:18:53 -0400

    Meta: Use SHA-256 verification for downloaded CLDR files

--- Meta/CMake/locale_data.cmake
+++ Meta/CMake/locale_data.cmake
@@ -1,6 +1,8 @@
 include(${CMAKE_CURRENT_LIST_DIR}/utils.cmake)
 
-set(CLDR_VERSION 45.0.0)
+set(CLDR_VERSION "45.0.0")
+set(CLDR_SHA256 "ba934cdd40ad4fb6439004c7e746bef97fe2b597db1040fcaa6c7d0647742c1b")
+
 set(CLDR_PATH "${SERENITY_CACHE_DIR}/CLDR" CACHE PATH "Download location for CLDR files")
 set(CLDR_VERSION_FILE "${CLDR_PATH}/version.txt")
 
@@ -32,7 +34,7 @@ if (ENABLE_UNICODE_DATABASE_DOWNLOAD)
     remove_path_if_version_changed("${CLDR_VERSION}" "${CLDR_VERSION_FILE}" "${CLDR_PATH}")
 
     if (ENABLE_NETWORK_DOWNLOADS)
-        download_file("${CLDR_ZIP_URL}" "${CLDR_ZIP_PATH}")
+        download_file("${CLDR_ZIP_URL}" "${CLDR_ZIP_PATH}" SHA256 "${CLDR_SHA256}")
         extract_path("${CLDR_PATH}" "${CLDR_ZIP_PATH}" "${CLDR_BCP47_SOURCE}/**" "${CLDR_BCP47_PATH}")
         extract_path("${CLDR_PATH}" "${CLDR_ZIP_PATH}" "${CLDR_CORE_SOURCE}/**" "${CLDR_CORE_PATH}")
         extract_path("${CLDR_PATH}" "${CLDR_ZIP_PATH}" "${CLDR_DATES_SOURCE}/**" "${CLDR_DATES_PATH}")
