b42dfb4a2a686d3a82cbe7dc6e54d903d25bd29d
Lucas CHOLLET <lucas.chollet@free.fr>
2024-05-23 23:07:39 -0400

    WebContent+WebWorker: Don't set the color value to the index of an enum
    The NamedColor would be casted to its underlying int to fit the ARGB on
    the left hand side.

--- Userland/Services/WebWorker/PageHost.cpp
+++ Userland/Services/WebWorker/PageHost.cpp
@@ -42,8 +42,8 @@ void PageHost::setup_palette()
     VERIFY(!buffer_or_error.is_error());
     auto buffer = buffer_or_error.release_value();
     auto* theme = buffer.data<Gfx::SystemTheme>();
-    theme->color[to_underlying(Gfx::ColorRole::Window)] = Color::Magenta;
-    theme->color[to_underlying(Gfx::ColorRole::WindowText)] = Color::Cyan;
+    theme->color[to_underlying(Gfx::ColorRole::Window)] = Color(Color::Magenta).value();
+    theme->color[to_underlying(Gfx::ColorRole::WindowText)] = Color(Color::Cyan).value();
     m_palette_impl = Gfx::PaletteImpl::create_with_anonymous_buffer(buffer);
 }
 
