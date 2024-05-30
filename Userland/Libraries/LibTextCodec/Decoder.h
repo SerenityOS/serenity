71f1ff73bb4decc5d18493990f7ddb154c3d2c34
Simon Wanner <simon+git@skyrising.xyz>
2024-05-30 13:17:36 +0200

    LibTextCodec: Add Shift_JIS decoder

--- Userland/Libraries/LibTextCodec/Decoder.h
+++ Userland/Libraries/LibTextCodec/Decoder.h
@@ -115,6 +115,11 @@ public:
     virtual ErrorOr<void> process(StringView, Function<ErrorOr<void>(u32)> on_code_point) override;
 };
 
+class ShiftJISDecoder final : public Decoder {
+public:
+    virtual ErrorOr<void> process(StringView, Function<ErrorOr<void>(u32)> on_code_point) override;
+};
+
 Optional<Decoder&> decoder_for(StringView encoding);
 Optional<StringView> get_standardized_encoding(StringView encoding);
 
