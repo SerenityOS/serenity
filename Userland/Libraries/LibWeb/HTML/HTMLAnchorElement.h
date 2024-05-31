a6fd653afa6195ae8a87bba5da858dfce333a02e
Jamie Mansfield <jmansfield@cadixdev.org>
2024-05-31 21:14:26 +0100

    LibWeb: Use Reflect for HTMLAnchorElement.referrerPolicy

--- Userland/Libraries/LibWeb/HTML/HTMLAnchorElement.h
+++ Userland/Libraries/LibWeb/HTML/HTMLAnchorElement.h
@@ -29,9 +29,6 @@ public:
     String text() const;
     void set_text(String const&);
 
-    StringView referrer_policy() const;
-    WebIDL::ExceptionOr<void> set_referrer_policy(String const&);
-
     // ^EventTarget
     // https://html.spec.whatwg.org/multipage/interaction.html#the-tabindex-attribute:the-a-element
     virtual bool is_focusable() const override { return has_attribute(HTML::AttributeNames::href); }
