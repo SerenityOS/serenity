a61f7d0fa8f237226408de998da24560ecc9323f
Kenneth Myhra <kennethmyhra@gmail.com>
2024-06-02 21:43:56 +0200

    LibWeb: Add internal slots [[cancelAlgorithm]] and [[finishPromise]]
    This adds internal slots [[cancelAlgorithm]] and [[finishPromise]] to
    TransformStreamDefaultController.
    
    (cherry picked from commit ff5be1fd363c4cb9ba510679b1ee65a0c0b10bc2)

--- Userland/Libraries/LibWeb/Streams/TransformStreamDefaultController.h
+++ Userland/Libraries/LibWeb/Streams/TransformStreamDefaultController.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, Kenneth Myhra <kennethmyhra@serenityos.org>
+ * Copyright (c) 2023-2024, Kenneth Myhra <kennethmyhra@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -24,6 +24,12 @@ public:
     void error(Optional<JS::Value> reason = {});
     void terminate();
 
+    JS::GCPtr<CancelAlgorithm> cancel_algorithm() { return m_cancel_algorithm; }
+    void set_cancel_algorithm(JS::GCPtr<CancelAlgorithm> value) { m_cancel_algorithm = value; }
+
+    JS::GCPtr<JS::PromiseCapability> finish_promise() { return m_finish_promise; }
+    void set_finish_promise(JS::GCPtr<JS::PromiseCapability> value) { m_finish_promise = value; }
+
     JS::GCPtr<FlushAlgorithm> flush_algorithm() { return m_flush_algorithm; }
     void set_flush_algorithm(JS::GCPtr<FlushAlgorithm>&& value) { m_flush_algorithm = move(value); }
 
@@ -37,6 +43,12 @@ private:
     virtual void initialize(JS::Realm&) override;
 
     virtual void visit_edges(Cell::Visitor&) override;
+
+    // https://streams.spec.whatwg.org/#transformstreamdefaultcontroller-cancelalgorithm
+    JS::GCPtr<CancelAlgorithm> m_cancel_algorithm;
+
+    // https://streams.spec.whatwg.org/#transformstreamdefaultcontroller-finishpromise
+    JS::GCPtr<JS::PromiseCapability> m_finish_promise;
 
     // https://streams.spec.whatwg.org/#transformstreamdefaultcontroller-flushalgorithm
     JS::GCPtr<FlushAlgorithm> m_flush_algorithm;
