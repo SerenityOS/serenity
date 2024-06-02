4a1d02e7d817bc4f502ee311fc15bbc7a738321a
Jamie Mansfield <jmansfield@cadixdev.org>
2024-06-02 12:46:15 +0100

    LibWeb/SVG: Implement SVGTransformList.length

--- Userland/Libraries/LibWeb/SVG/SVGTransformList.cpp
+++ Userland/Libraries/LibWeb/SVG/SVGTransformList.cpp
@@ -23,6 +23,13 @@ SVGTransformList::~SVGTransformList() = default;
 SVGTransformList::SVGTransformList(JS::Realm& realm)
     : PlatformObject(realm) {};
 
+// https://svgwg.org/svg2-draft/single-page.html#types-__svg__SVGNameList__length
+WebIDL::UnsignedLong SVGTransformList::length()
+{
+    // The length and numberOfItems IDL attributes represents the length of the list, and on getting simply return the length of the list.
+    return m_transforms.size();
+}
+
 // https://svgwg.org/svg2-draft/single-page.html#types-__svg__SVGNameList__getItem
 WebIDL::ExceptionOr<JS::NonnullGCPtr<SVGTransform>> SVGTransformList::get_item(WebIDL::UnsignedLong index)
 {
