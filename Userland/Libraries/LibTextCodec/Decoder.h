2c13e8b6466264c82402530528aaac70546ebf2c
Simon Wanner <simon+git@skyrising.xyz>
2024-06-03 20:08:22 +0200

    LibTextCodec: Use generated lookup tables for all single byte decoders
    (cherry picked from commit 0ab4722cee11d27da79bb05c1d53693f39938cf6)

--- Userland/Libraries/LibTextCodec/Decoder.h
+++ Userland/Libraries/LibTextCodec/Decoder.h
@@ -66,25 +66,7 @@ public:
     virtual bool validate(StringView) override { return true; }
 };
 
-class Latin2Decoder final : public Decoder {
-public:
-    virtual ErrorOr<void> process(StringView, Function<ErrorOr<void>(u32)> on_code_point) override;
-    virtual bool validate(StringView) override { return true; }
-};
-
-class Latin9Decoder final : public Decoder {
-public:
-    virtual ErrorOr<void> process(StringView, Function<ErrorOr<void>(u32)> on_code_point) override;
-    virtual bool validate(StringView) override { return true; }
-};
-
 class PDFDocEncodingDecoder final : public Decoder {
-public:
-    virtual ErrorOr<void> process(StringView, Function<ErrorOr<void>(u32)> on_code_point) override;
-    virtual bool validate(StringView) override { return true; }
-};
-
-class TurkishDecoder final : public Decoder {
 public:
     virtual ErrorOr<void> process(StringView, Function<ErrorOr<void>(u32)> on_code_point) override;
     virtual bool validate(StringView) override { return true; }
