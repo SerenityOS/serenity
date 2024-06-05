269018a26f3c0869059f56ddc5edb60b747ba41c
Tim Ledbetter <timledbetter@gmail.com>
2024-06-05 17:20:58 +0100

    LibWeb: Ensure anchor node is not null when extending selection
    Previously, clicking while holding shift without having previously made
    any text selection would cause a crash.
    
    (cherry picked from commit c79041344d7f9957bf810cd4db5330d8006ef0e3)

--- Userland/Libraries/LibWeb/Page/EventHandler.cpp
+++ Userland/Libraries/LibWeb/Page/EventHandler.cpp
@@ -443,8 +443,9 @@ bool EventHandler::handle_mousedown(CSSPixelPoint viewport_position, CSSPixelPoi
                     auto& realm = document->realm();
                     m_navigable->set_cursor_position(DOM::Position::create(realm, *paintable->dom_node(), result->index_in_node));
                     if (auto selection = document->get_selection()) {
-                        if (modifiers & KeyModifier::Mod_Shift) {
-                            (void)selection->set_base_and_extent(*selection->anchor_node(), selection->anchor_offset(), *paintable->dom_node(), result->index_in_node);
+                        auto anchor_node = selection->anchor_node();
+                        if (anchor_node && modifiers & KeyModifier::Mod_Shift) {
+                            (void)selection->set_base_and_extent(*anchor_node, selection->anchor_offset(), *paintable->dom_node(), result->index_in_node);
                         } else {
                             (void)selection->set_base_and_extent(*paintable->dom_node(), result->index_in_node, *paintable->dom_node(), result->index_in_node);
                         }
