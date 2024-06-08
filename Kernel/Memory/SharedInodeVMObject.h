b5d34a7b534289815eeacea60b0e4d606275feb7
brody-qq <brody.47.qq@gmail.com>
2024-06-08 19:55:48 +0100

    Kernel/Memory: Make mmap objects track dirty pages
    InodeVMObjects now track dirty and clean pages. This tracking of
    dirty and clean pages is used by the msync and purge syscalls.
    
    dirty page tracking works using the following rules:
    * when a new InodeVMObject is made, all pages are marked clean.
    * writes to clean InodeVMObject pages will cause a page fault,
      the fault handler will mark the page as dirty.
    * writes to dirty InodeVMObject pages do not cause page faults.
    * if msync is called, only dirty pages are flushed to storage (and
      marked clean).
    * if purge syscall is called, only clean pages are discarded.

--- Kernel/Memory/SharedInodeVMObject.h
+++ Kernel/Memory/SharedInodeVMObject.h
@@ -19,7 +19,8 @@ public:
     static ErrorOr<NonnullLockRefPtr<SharedInodeVMObject>> try_create_with_inode_and_range(Inode&, u64 offset, size_t range_size);
     virtual ErrorOr<NonnullLockRefPtr<VMObject>> try_clone() override;
 
-    ErrorOr<void> sync(off_t offset_in_pages = 0, size_t pages = -1);
+    ErrorOr<void> sync(off_t offset_in_pages, size_t pages);
+    ErrorOr<void> sync_before_destroying();
 
 private:
     virtual bool is_shared_inode() const override { return true; }
@@ -30,6 +31,8 @@ private:
     virtual StringView class_name() const override { return "SharedInodeVMObject"sv; }
 
     SharedInodeVMObject& operator=(SharedInodeVMObject const&) = delete;
+
+    ErrorOr<void> sync_impl(off_t offset_in_pages, size_t pages, bool should_remap);
 };
 
 }
