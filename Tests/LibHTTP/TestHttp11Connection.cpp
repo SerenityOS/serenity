e2ec0ea4efb6b9622c84a853790de46a4c4f348e
Dan Klishch <danilklishch@gmail.com>
2024-06-12 14:50:36 -0400

    LibHTTP: Implement bare-bones HTTP/1.1 client using coroutines
    We don't have asynchronous TCP socket implementation, so its usefulness
    is a bit limited currently but we can still test it using memory
    streams. Additionally, it serves as a temporary {show,test}case for the
    asynchronous streams machinery.

---
+++ Tests/LibHTTP/TestHttp11Connection.cpp
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2024, Dan Klishch <danilklishch@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <AK/AsyncStreamHelpers.h>
+#include <LibHTTP/Http11Connection.h>
+#include <LibTest/AsyncTestCase.h>
+#include <LibTest/AsyncTestStreams.h>
+
+struct HTTPUnitTest {
+    StringView name;
+    HTTP::Method method;
+    StringView url;
+    Vector<HTTP::Header> headers;
+    StringView response;
+
+    StringView request_expectation;
+    StringView body_expectation;
+};
+
+Vector<HTTPUnitTest> const http_unit_tests = {
+    {
+        .name = "Basic"sv,
+        .method = HTTP::Method::GET,
+        .url = "/"sv,
+        .headers = {
+            { "Host", "localhost" },
+        },
+        .response = "HTTP/1.1 200 OK\r\n"
+                    "Content-Length: 16\r\n"
+                    "\r\n"
+                    "0123456789abcdef"sv,
+        .request_expectation = "GET / HTTP/1.1\r\n"
+                               "Host: localhost\r\n"
+                               "\r\n"sv,
+        .body_expectation = "0123456789abcdef"sv,
+    },
+};
+
+ASYNC_TEST_CASE(unit_tests_single)
+{
+    for (auto const& test : http_unit_tests) {
+        outln("Running '{}'...", test.name);
+
+        auto response_partitioning = Test::randomly_partition_input(1, AK::get_random_uniform(50) + 1, test.response.length());
+        outln("Input partitioning: {}", response_partitioning);
+        auto input = make<Test::AsyncMemoryInputStream>(test.response, Test::StreamCloseExpectation::Close, move(response_partitioning));
+        auto output = make<Test::AsyncMemoryOutputStream>(Test::StreamCloseExpectation::Close);
+
+        auto output_ref = output.ptr();
+
+        auto stream_pair = make<AsyncStreamPair>(move(input), move(output));
+        auto connection = make<HTTP::Http11Connection>(move(stream_pair));
+
+        CO_TRY_OR_FAIL(co_await connection->request(
+            {
+                .method = test.method,
+                .url = test.url,
+                .headers = test.headers,
+            },
+            [&](HTTP::Http11Response& response) -> Coroutine<ErrorOr<void>> {
+                auto body = CO_TRY(co_await Test::read_until_eof(response.body()));
+                EXPECT_EQ(StringView { body }, test.body_expectation);
+                co_return {};
+            }));
+
+        CO_TRY_OR_FAIL(co_await connection->close());
+
+        EXPECT_EQ(StringView { output_ref->view() }, test.request_expectation);
+    }
+}
---
+++ Userland/Libraries/LibHTTP/Http11Connection.h
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2024, Dan Klishch <danilklishch@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/AsyncStream.h>
+#include <AK/ByteString.h>
+#include <AK/NonnullOwnPtr.h>
+#include <AK/OwnPtr.h>
+#include <AK/TemporaryChange.h>
+#include <AK/Vector.h>
+
+namespace HTTP {
+
+class Http11Connection;
+class Http11Response;
+
+#define ENUMERATE_METHODS(F) \
+    F(Invalid)               \
+    F(HEAD)                  \
+    F(GET)                   \
+    F(POST)                  \
+    F(DELETE)                \
+    F(PATCH)                 \
+    F(OPTIONS)               \
+    F(TRACE)                 \
+    F(CONNECT)               \
+    F(PUT)
+
+enum class Method {
+#define ID(x) x,
+    ENUMERATE_METHODS(ID)
+#undef ID
+};
+
+struct Header {
+    ByteString header;
+    ByteString value;
+};
+
+struct RequestData {
+    struct PlainBody {
+        StringView data;
+    };
+
+    Method method;
+    StringView url;
+    Vector<Header> headers;
+    Variant<Empty, PlainBody> body = Empty {};
+};
+
+class Http11Response final : public StreamWrapper<AsyncInputStream> {
+public:
+    static Coroutine<ErrorOr<NonnullOwnPtr<Http11Response>>> create(Badge<Http11Connection>, RequestData&& data, AsyncStream& stream);
+
+    u16 status_code() const { return m_status_code; }
+    Vector<Header> const& headers() const { return m_headers; }
+
+    AsyncInputStream& body() { return *m_stream; }
+
+private:
+    Http11Response(NonnullOwnPtr<AsyncInputStream>&& body, u16 status_code, Vector<Header>&& headers)
+        : StreamWrapper(move(body))
+        , m_status_code(status_code)
+        , m_headers(move(headers))
+    {
+    }
+
+    u16 m_status_code { 0 };
+    Vector<Header> m_headers;
+};
+
+class Http11Connection final : public StreamWrapper<AsyncStream> {
+public:
+    using StreamWrapper::StreamWrapper;
+
+    template<
+        typename Func,
+        typename T = InvokeResult<Func, Http11Response&>::ReturnType::ResultType>
+    Coroutine<ErrorOr<T>> request(RequestData&& data, Func&& func)
+    {
+        VERIFY(!m_request_in_flight);
+        TemporaryChange request_in_flight { m_request_in_flight, true };
+
+        auto response = CO_TRY(co_await Http11Response::create({}, move(data), *m_stream));
+        auto result = co_await func(*response);
+        if (response->is_open()) {
+            auto close_result = co_await response->close();
+            if (!result.is_error()) // Preserve callback error in case it has failed.
+                CO_TRY(close_result);
+        }
+        co_return result;
+    }
+
+private:
+    bool m_request_in_flight { false };
+};
+
+}
