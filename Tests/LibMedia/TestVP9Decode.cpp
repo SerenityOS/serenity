2d04f893477c8c955a8d15900289e939f47a6751
Zaggy1024 <Zaggy1024@gmail.com>
2024-06-19 23:04:25 -0500

    Tests/LibMedia: Move video decode testing function to a header
    This will be useful for other video codecs, but putting all codecs in
    one file will be a little messy.
    
    (cherry picked from commit 48a21d5d6a203563eab60922f8c24202d0ed7e10)

--- Tests/LibMedia/TestVP9Decode.cpp
+++ Tests/LibMedia/TestVP9Decode.cpp
@@ -4,59 +4,23 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#include <LibTest/TestCase.h>
-
-#include <LibMedia/Containers/Matroska/Reader.h>
 #include <LibMedia/Video/VP9/Decoder.h>
 
-static void decode_video(StringView path, size_t expected_frame_count)
+#include "TestMediaCommon.h"
+
+static NonnullOwnPtr<Media::VideoDecoder> make_decoder(Media::Matroska::SampleIterator const&)
 {
-    auto matroska_reader = MUST(Media::Matroska::Reader::from_file(path));
-    u64 video_track = 0;
-    MUST(matroska_reader.for_each_track_of_type(Media::Matroska::TrackEntry::TrackType::Video, [&](Media::Matroska::TrackEntry const& track_entry) -> Media::DecoderErrorOr<IterationDecision> {
-        video_track = track_entry.track_number();
-        return IterationDecision::Break;
-    }));
-    VERIFY(video_track != 0);
-
-    auto iterator = MUST(matroska_reader.create_sample_iterator(video_track));
-    size_t frame_count = 0;
-    Media::Video::VP9::Decoder vp9_decoder;
-
-    while (frame_count <= expected_frame_count) {
-        auto block_result = iterator.next_block();
-        if (block_result.is_error() && block_result.error().category() == Media::DecoderErrorCategory::EndOfStream) {
-            VERIFY(frame_count == expected_frame_count);
-            return;
-        }
-
-        auto block = block_result.release_value();
-        for (auto const& frame : block.frames()) {
-            MUST(vp9_decoder.receive_sample(frame));
-            while (true) {
-                auto frame_result = vp9_decoder.get_decoded_frame();
-                if (frame_result.is_error()) {
-                    if (frame_result.error().category() == Media::DecoderErrorCategory::NeedsMoreInput) {
-                        break;
-                    }
-                    VERIFY_NOT_REACHED();
-                }
-            }
-            frame_count++;
-        }
-    }
-
-    VERIFY_NOT_REACHED();
+    return make<Media::Video::VP9::Decoder>();
 }
 
 TEST_CASE(webm_in_vp9)
 {
-    decode_video("./vp9_in_webm.webm"sv, 25);
+    decode_video("./vp9_in_webm.webm"sv, 25, make_decoder);
 }
 
 TEST_CASE(vp9_oob_blocks)
 {
-    decode_video("./vp9_oob_blocks.webm"sv, 240);
+    decode_video("./vp9_oob_blocks.webm"sv, 240, make_decoder);
 }
 
 TEST_CASE(vp9_malformed_frame)
@@ -78,10 +42,10 @@ TEST_CASE(vp9_malformed_frame)
 
 BENCHMARK_CASE(vp9_4k)
 {
-    decode_video("./vp9_4k.webm"sv, 2);
+    decode_video("./vp9_4k.webm"sv, 2, make_decoder);
 }
 
 BENCHMARK_CASE(vp9_clamp_reference_mvs)
 {
-    decode_video("./vp9_clamp_reference_mvs.webm"sv, 92);
+    decode_video("./vp9_clamp_reference_mvs.webm"sv, 92, make_decoder);
 }
