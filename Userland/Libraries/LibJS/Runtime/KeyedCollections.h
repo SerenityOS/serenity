0bbf42bca7d6da24e76174176c373cef0d062dc0
Timothy Flynn <trflynn89@pm.me>
2024-07-09 15:24:28 -0400

    LibJS: Introduce the CanonicalizeKeyedCollectionKey AO
    This is an editorial change in the ECMA-262 spec. See:
    https://github.com/tc39/ecma262/commit/30257dd
    
    (cherry picked from commit 55b4ef79157f299e68163824d8d03dfab31dd3d6)

---
+++ Userland/Libraries/LibJS/Runtime/KeyedCollections.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024, Tim Flynn <trflynn89@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibJS/Runtime/Value.h>
+
+namespace JS {
+
+Value canonicalize_keyed_collection_key(Value);
+
+}
