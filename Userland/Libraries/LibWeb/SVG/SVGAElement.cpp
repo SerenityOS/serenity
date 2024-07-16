a57d45482fa4dd289b555aa50a72d6d50a500e10
Jamie Mansfield <jmansfield@cadixdev.org>
2024-07-16 12:08:34 +0100

    LibWeb/SVG: Implement SVGAElement.relList
    (cherry picked from commit 6ca4c2beb08a81bc4aaa2571ee8c5b3207d0b3a2)

--- Userland/Libraries/LibWeb/SVG/SVGAElement.cpp
+++ Userland/Libraries/LibWeb/SVG/SVGAElement.cpp
@@ -1,10 +1,12 @@
 /*
  * Copyright (c) 2024, Andreas Kling <andreas@ladybird.org>
+ * Copyright (c) 2024, Jamie Mansfield <jmansfield@cadixdev.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #include <LibWeb/Bindings/SVGAElementPrototype.h>
+#include <LibWeb/DOM/DOMTokenList.h>
 #include <LibWeb/Layout/SVGGraphicsBox.h>
 #include <LibWeb/SVG/SVGAElement.h>
 
@@ -23,6 +25,30 @@ void SVGAElement::initialize(JS::Realm& realm)
 {
     Base::initialize(realm);
     WEB_SET_PROTOTYPE_FOR_INTERFACE(SVGAElement);
+}
+
+void SVGAElement::visit_edges(Cell::Visitor& visitor)
+{
+    Base::visit_edges(visitor);
+    visitor.visit(m_rel_list);
+}
+
+void SVGAElement::attribute_changed(FlyString const& name, Optional<String> const& old_value, Optional<String> const& value)
+{
+    Base::attribute_changed(name, old_value, value);
+    if (name == HTML::AttributeNames::rel) {
+        if (m_rel_list)
+            m_rel_list->associated_attribute_changed(value.value_or(String {}));
+    }
+}
+
+// https://svgwg.org/svg2-draft/linking.html#__svg__SVGAElement__relList
+JS::NonnullGCPtr<DOM::DOMTokenList> SVGAElement::rel_list()
+{
+    // The relList IDL attribute reflects the ‘rel’ content attribute.
+    if (!m_rel_list)
+        m_rel_list = DOM::DOMTokenList::create(*this, HTML::AttributeNames::rel);
+    return *m_rel_list;
 }
 
 JS::GCPtr<Layout::Node> SVGAElement::create_layout_node(NonnullRefPtr<CSS::StyleProperties> style)
