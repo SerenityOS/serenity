24254525a463eb0b2adf08551eef1336c04a8bdd
Sönke Holz <sholz8530@gmail.com>
2024-07-17 21:25:20 +0200

    EFIPrekernel: Make dbgln usable
    This simply makes dbgln use the EFI Simple Text Output Protocol.

---
+++ Kernel/EFIPrekernel/DebugOutput.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2024, Sönke Holz <sholz8530@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+namespace Kernel {
+
+void ucs2_dbgln(char16_t const* message);
+
+}
