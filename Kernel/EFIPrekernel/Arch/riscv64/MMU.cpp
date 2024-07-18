b3cead73d2b7ff0a1f3c001e9f34e510f9f6d77a
Sönke Holz <sholz8530@gmail.com>
2024-07-18 19:26:37 +0200

    EFIPrekernel/riscv64: Implement virtual memory management functions

--- Kernel/EFIPrekernel/Arch/riscv64/MMU.cpp
+++ Kernel/EFIPrekernel/Arch/riscv64/MMU.cpp
@@ -23,22 +23,68 @@ EFIErrorOr<void*> allocate_empty_root_page_table()
     return root_page_table;
 }
 
-static EFIErrorOr<void> map_single_page(void* root_page_table, FlatPtr vaddr, PhysicalPtr paddr, Access access)
+static u64* get_pte(u64* page_table, FlatPtr vaddr, size_t level)
 {
-    (void)root_page_table;
-    (void)vaddr;
-    (void)paddr;
-    (void)access;
-    TODO();
+    size_t pte_index_offset = (PAGE_TABLE_INDEX_BITS * level) + PAGE_OFFSET_BITS;
+    size_t pte_index = (vaddr >> pte_index_offset) & PAGE_TABLE_INDEX_MASK;
+
+    return &page_table[pte_index];
 }
 
 EFIErrorOr<void*> get_or_insert_page_table(void* root_page_table, FlatPtr vaddr, size_t level, bool has_to_be_new)
 {
-    (void)root_page_table;
-    (void)vaddr;
-    (void)level;
-    (void)has_to_be_new;
-    TODO();
+    VERIFY(root_page_table != nullptr);
+
+    if (level >= LEVELS - 1)
+        return EFI::Status::InvalidParameter;
+
+    u64* current_page_table = reinterpret_cast<u64*>(root_page_table);
+
+    for (size_t current_level = LEVELS - 1; current_level > level; current_level--) {
+        u64* pte = get_pte(current_page_table, vaddr, current_level);
+
+        if ((*pte & to_underlying(PageTableEntryBits::Valid)) != 0) {
+            if (current_level - 1 == level && has_to_be_new)
+                return EFI::Status::InvalidParameter;
+
+            current_page_table = bit_cast<u64*>((*pte >> PTE_PPN_OFFSET) << PADDR_PPN_OFFSET);
+        } else {
+            EFI::PhysicalAddress new_page_table_paddr = 0;
+            if (auto status = g_efi_system_table->boot_services->allocate_pages(EFI::AllocateType::AnyPages, EFI::MemoryType::LoaderData, 1, &new_page_table_paddr); status != EFI::Status::Success)
+                return status;
+
+            __builtin_memset(bit_cast<void*>(new_page_table_paddr), 0, PAGE_TABLE_SIZE);
+
+            *pte = ((new_page_table_paddr >> PADDR_PPN_OFFSET) << PTE_PPN_OFFSET) | to_underlying(PageTableEntryBits::Valid);
+
+            current_page_table = bit_cast<u64*>(new_page_table_paddr);
+        }
+    }
+
+    return current_page_table;
+}
+
+static EFIErrorOr<void> map_single_page(void* root_page_table, FlatPtr vaddr, PhysicalPtr paddr, Access access)
+{
+    auto* page_table = TRY(get_or_insert_page_table(root_page_table, vaddr));
+    u64* pte = get_pte(bit_cast<u64*>(page_table), vaddr, 0);
+
+    if ((*pte & to_underlying(PageTableEntryBits::Valid)) != 0)
+        return EFI::Status::InvalidParameter; // already mapped
+
+    // Always set the A/D bits as we don't know if the hardware updates them automatically (i.e. if Svadu is supported).
+    // If the hardware doesn't update them automatically they act like additional permission bits.
+    PageTableEntryBits flags = PageTableEntryBits::Valid | PageTableEntryBits::Accessed | PageTableEntryBits::Dirty;
+    if (to_underlying(access & Access::Read) != 0)
+        flags |= PageTableEntryBits::Readable;
+    if (to_underlying(access & Access::Write) != 0)
+        flags |= PageTableEntryBits::Writeable;
+    if (to_underlying(access & Access::Execute) != 0)
+        flags |= PageTableEntryBits::Executable;
+
+    *pte = ((paddr >> PADDR_PPN_OFFSET) << PTE_PPN_OFFSET) | to_underlying(flags);
+
+    return {};
 }
 
 EFIErrorOr<void> map_pages(void* root_page_table, FlatPtr start_vaddr, PhysicalPtr start_paddr, size_t page_count, Access access)
