bf5da6333db3107b16fa202f3f3c3de4902ba401
Andreas Kling <andreas@ladybird.org>
2024-07-21 11:43:59 +0200

    LibWeb: Make getElementById() always return first match in tree order
    We had a const and non-const version of this function, with slightly
    different behavior (oops!)
    
    This patch consolidates the implementations and keeps only the correct
    behavior in there.
    
    Fixes an issue where comments were not collapsible on Hacker News.
    
    (cherry picked from commit 98f88d49de852e1e524655accb39724f1134a23f)

--- Userland/Libraries/LibWeb/DOM/NonElementParentNode.h
+++ Userland/Libraries/LibWeb/DOM/NonElementParentNode.h
@@ -18,26 +18,13 @@ namespace Web::DOM {
 template<typename NodeType>
 class NonElementParentNode {
 public:
-    JS::GCPtr<Element const> get_element_by_id(FlyString const& id) const
+    JS::GCPtr<Element> get_element_by_id(FlyString const& id) const
     {
-        JS::GCPtr<Element const> found_element;
-        static_cast<NodeType const*>(this)->template for_each_in_inclusive_subtree_of_type<Element>([&](auto& element) {
+        JS::GCPtr<Element> found_element;
+        const_cast<NodeType*>(static_cast<NodeType const*>(this))->template for_each_in_inclusive_subtree_of_type<Element>([&](auto& element) {
             if (element.id() == id) {
                 found_element = &element;
                 return TraversalDecision::Break;
-            }
-            return TraversalDecision::Continue;
-        });
-        return found_element;
-    }
-
-    JS::GCPtr<Element> get_element_by_id(FlyString const& id)
-    {
-        JS::GCPtr<Element> found_element;
-        static_cast<NodeType*>(this)->template for_each_in_inclusive_subtree_of_type<Element>([&](auto& element) {
-            if (element.id() == id) {
-                found_element = &element;
-                return TraversalDecision::Continue;
             }
             return TraversalDecision::Continue;
         });
