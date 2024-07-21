71d95c15be712d565004b2e6335a33c3bbc6ff8d
EWouters <6179932+EWouters@users.noreply.github.com>
2024-07-21 17:26:53 +0200

    Ports/tree: Update tree to version 2.1.3

--- Ports/tree/package.sh
+++ Ports/tree/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='tree'
-version='2.1.1'
+version='2.1.3'
 files=(
-    "https://github.com/Old-Man-Programmer/tree/archive/refs/tags/${version}.tar.gz#1b70253994dca48a59d6ed99390132f4d55c486bf0658468f8520e7e63666a06"
+    "https://github.com/Old-Man-Programmer/tree/archive/refs/tags/${version}.tar.gz#3ffe2c8bb21194b088ad1e723f0cf340dd434453c5ff9af6a38e0d47e0c2723b"
 )
 
 build() {
