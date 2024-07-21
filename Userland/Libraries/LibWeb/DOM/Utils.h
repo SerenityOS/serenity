78a22f5098e7e2ae5515703695d91b19943f5923
circl <circl.lastname@gmail.com>
2024-07-21 15:06:05 +0200

    LibWeb: Replace templated retarget function with a regular one
    The templating is not necessary anywhere and was effectively just a cast
    
    (cherry picked from commit 37f93e4be13890c88f2a34a2669b41297fafa1c8)

--- Userland/Libraries/LibWeb/DOM/Utils.h
+++ Userland/Libraries/LibWeb/DOM/Utils.h
@@ -7,42 +7,10 @@
 
 #pragma once
 
-#include <LibWeb/DOM/Node.h>
-#include <LibWeb/DOM/ShadowRoot.h>
+#include <LibWeb/DOM/EventTarget.h>
 
 namespace Web::DOM {
 
-// https://dom.spec.whatwg.org/#retarget
-inline EventTarget* retarget_impl(EventTarget* a, EventTarget* b)
-{
-    // To retarget an object A against an object B, repeat these steps until they return an object:
-    for (;;) {
-        // 1. If one of the following is true then return A.
-        // - A is not a node
-        if (!is<Node>(a))
-            return a;
-
-        // - A’s root is not a shadow root
-        auto* a_node = verify_cast<Node>(a);
-        auto& a_root = a_node->root();
-        if (!is<ShadowRoot>(a_root))
-            return a;
-
-        // - B is a node and A’s root is a shadow-including inclusive ancestor of B
-        if (is<Node>(b) && a_root.is_shadow_including_inclusive_ancestor_of(verify_cast<Node>(*b)))
-            return a;
-
-        // 2. Set A to A’s root’s host.
-        auto& a_shadow_root = verify_cast<ShadowRoot>(a_root);
-        a = a_shadow_root.host();
-    }
-}
-
-// https://dom.spec.whatwg.org/#retarget
-template<typename T>
-T* retarget(T* a, T* b)
-{
-    return static_cast<T*>(retarget_impl(a, b));
-}
+EventTarget* retarget(EventTarget* a, EventTarget* b);
 
 }
