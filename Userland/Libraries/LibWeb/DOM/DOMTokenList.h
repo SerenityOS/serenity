f1cfe231afafcd387b8c10c71345a66d37c4d30e
Tim Ledbetter <tim.ledbetter@ladybird.org>
2024-07-23 13:10:50 +0100

    LibWeb: Perform DOMTokenList token validation in the correct order
    (cherry picked from commit 0127190dcf7fd0ee27a935cf1f3f5c74b77c0fc9)

--- Userland/Libraries/LibWeb/DOM/DOMTokenList.h
+++ Userland/Libraries/LibWeb/DOM/DOMTokenList.h
@@ -50,6 +50,8 @@ private:
     virtual void visit_edges(Cell::Visitor&) override;
 
     WebIDL::ExceptionOr<void> validate_token(StringView token) const;
+    WebIDL::ExceptionOr<void> validate_token_not_empty(StringView token) const;
+    WebIDL::ExceptionOr<void> validate_token_not_whitespace(StringView token) const;
     void run_update_steps();
 
     String serialize_ordered_set() const;
