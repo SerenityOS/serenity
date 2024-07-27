e4959ef9f313b982a2d0ef00eaa9b4de0091b608
bbb651 <bar.ye651@gmail.com>
2024-07-27 12:36:14 +0300

    WebAudio: Stub/implement more of `AudioNode`
    (cherry picked from commit 6d33cc2b3a3e8b2c570c80866c5aa666927d0ad5)

--- Userland/Libraries/LibWeb/WebAudio/AudioNode.cpp
+++ Userland/Libraries/LibWeb/WebAudio/AudioNode.cpp
@@ -24,18 +24,26 @@ AudioNode::~AudioNode() = default;
 // https://webaudio.github.io/web-audio-api/#dom-audionode-connect
 WebIDL::ExceptionOr<JS::NonnullGCPtr<AudioNode>> AudioNode::connect(JS::NonnullGCPtr<AudioNode> destination_node, WebIDL::UnsignedLong output, WebIDL::UnsignedLong input)
 {
+    // There can only be one connection between a given output of one specific node and a given input of another specific node.
+    // Multiple connections with the same termini are ignored.
+
+    // If the destination parameter is an AudioNode that has been created using another AudioContext, an InvalidAccessError MUST be thrown.
+    if (m_context != destination_node->m_context) {
+        return WebIDL::InvalidAccessError::create(realm(), "Cannot connect to an AudioNode in a different AudioContext"_fly_string);
+    }
+
     (void)output;
     (void)input;
-    dbgln("FIXME: Implement AudioNode::connect (AudioNode)");
+    dbgln("FIXME: Implement Audio::connect(AudioNode)");
     return destination_node;
 }
 
 // https://webaudio.github.io/web-audio-api/#dom-audionode-connect-destinationparam-output
-WebIDL::ExceptionOr<JS::NonnullGCPtr<AudioNode>> AudioNode::connect(JS::NonnullGCPtr<AudioParam> destination_param, WebIDL::UnsignedLong output)
+void AudioNode::connect(JS::NonnullGCPtr<AudioParam> destination_param, WebIDL::UnsignedLong output)
 {
     (void)destination_param;
     (void)output;
-    return WebIDL::NotSupportedError::create(realm(), "FIXME: Implement AudioNode::connect (AudioParam)"_fly_string);
+    dbgln("FIXME: Implement AudioNode::connect(AudioParam)");
 }
 
 // https://webaudio.github.io/web-audio-api/#dom-audionode-disconnect
@@ -88,6 +96,60 @@ void AudioNode::disconnect(JS::NonnullGCPtr<AudioParam> destination_param, WebID
     (void)destination_param;
     (void)output;
     dbgln("FIXME: Implement AudioNode::disconnect(destination_param, output)");
+}
+
+// https://webaudio.github.io/web-audio-api/#dom-audionode-numberofinputs
+WebIDL::UnsignedLong AudioNode::number_of_inputs()
+{
+    dbgln("FIXME: Implement AudioNode::number_of_inputs()");
+    return 0;
+}
+
+// https://webaudio.github.io/web-audio-api/#dom-audionode-numberofoutputs
+WebIDL::UnsignedLong AudioNode::number_of_outputs()
+{
+    dbgln("FIXME: Implement AudioNode::number_of_outputs()");
+    return 0;
+}
+
+// https://webaudio.github.io/web-audio-api/#dom-audionode-channelcount
+WebIDL::ExceptionOr<void> AudioNode::set_channel_count(WebIDL::UnsignedLong channel_count)
+{
+    (void)channel_count;
+    return WebIDL::NotSupportedError::create(realm(), "FIXME: Implement AudioNode::set_channel_count(channel_count)"_fly_string);
+}
+
+// https://webaudio.github.io/web-audio-api/#dom-audionode-channelcount
+WebIDL::UnsignedLong AudioNode::channel_count()
+{
+    dbgln("FIXME: Implement AudioNode::channel_count()");
+    return 2;
+}
+
+// https://webaudio.github.io/web-audio-api/#dom-audionode-channelcountmode
+WebIDL::ExceptionOr<void> AudioNode::set_channel_count_mode(Bindings::ChannelCountMode channel_count_mode)
+{
+    m_channel_count_mode = channel_count_mode;
+    return {};
+}
+
+// https://webaudio.github.io/web-audio-api/#dom-audionode-channelcountmode
+Bindings::ChannelCountMode AudioNode::channel_count_mode()
+{
+    return m_channel_count_mode;
+}
+
+// https://webaudio.github.io/web-audio-api/#dom-audionode-channelinterpretation
+WebIDL::ExceptionOr<void> AudioNode::set_channel_interpretation(Bindings::ChannelInterpretation channel_interpretation)
+{
+    m_channel_interpretation = channel_interpretation;
+    return {};
+}
+
+// https://webaudio.github.io/web-audio-api/#dom-audionode-channelinterpretation
+Bindings::ChannelInterpretation AudioNode::channel_interpretation()
+{
+    return m_channel_interpretation;
 }
 
 void AudioNode::initialize(JS::Realm& realm)
