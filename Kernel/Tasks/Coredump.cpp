SerenityOS/serenity

commit 01e1af732ba2eb7dd0c31d71fe9b008d8f5e9d42

Kernel/FileSystem: Introduce the VFSRootContext class
The VFSRootContext class, as its name suggests, holds a context for a
root directory with its mount table and the root custody/inode in the
same class.

The idea is derived from the Linux mount namespace mechanism.
It mimicks the concept of the ProcessList object, but it is adjusted for
a root directory tree context.
In contrast to the ProcessList concept, processes that share the default
VFSRootContext can't see other VFSRootContext related properties such as
as the mount table and root custody/inode.

To accommodate to this change progressively, we internally create 2 main
VFS root contexts for now - one for kernel processes (as they don't need
to care about VFS root contexts for the most part), and another for all
userspace programs.
This separation allows us to continue pretending for userspace that
everything is "normal" as it is used to be, until we introduce proper
interfaces in the mount-related syscalls as well as in the SysFS.

We make VFSRootContext objects being listed, as another preparation
before we could expose interfaces to userspace.
As a result, the PowerStateSwitchTask now iterates on all contexts
and tear them down one by one.

Kernel/Tasks/Coredump.cpp
@@ -96,7 +96,11 @@ Coredump::Coredump(NonnullRefPtr<Process> process, NonnullRefPtr<OpenFileDescrip
 ErrorOr<NonnullRefPtr<OpenFileDescription>> Coredump::try_create_target_file(Process const& process, StringView output_path)
 {
     auto output_directory = KLexicalPath::dirname(output_path);
-    auto dump_directory = TRY(VirtualFileSystem::the().open_directory(Process::current().credentials(), output_directory, VirtualFileSystem::the().root_custody()));
+    auto current_vfs_root_context = Process::current().vfs_root_context();
+    auto current_vfs_root_context_root_custody = current_vfs_root_context->root_custody().with([](auto& custody) -> NonnullRefPtr<Custody> {
+        return custody;
+    });
+    auto dump_directory = TRY(VirtualFileSystem::the().open_directory(current_vfs_root_context, Process::current().credentials(), output_directory, *current_vfs_root_context_root_custody));
     auto dump_directory_metadata = dump_directory->inode().metadata();
     if (dump_directory_metadata.uid != 0 || dump_directory_metadata.gid != 0 || dump_directory_metadata.mode != 040777) {
         dbgln("Refusing to put coredump in sketchy directory '{}'", output_directory);
@@ -105,6 +109,7 @@ ErrorOr<NonnullRefPtr<OpenFileDescription>> Coredump::try_create_target_file(Pro
 
     auto process_credentials = process.credentials();
     return TRY(VirtualFileSystem::the().open(
+        current_vfs_root_context,
         Process::current().credentials(),
         KLexicalPath::basename(output_path),
         O_CREAT | O_WRONLY | O_EXCL,

Kernel/Arch/init.cpp
@@ -310,6 +310,10 @@ extern "C" [[noreturn]] UNMAP_AFTER_INIT NO_SANITIZE_COVERAGE void init([[maybe_
 
     __stack_chk_guard = get_fast_random<uintptr_t>();
 
+    // NOTE: Initialize the empty VFS root context just before we need to create
+    // kernel processes.
+    VFSRootContext::initialize_empty_ramfs_root_context_for_kernel_processes();
+
     Process::initialize();
 
     Scheduler::initialize();
@@ -451,9 +455,13 @@ void init_stage2(void*)
         dbgln_if(STORAGE_DEVICE_DEBUG, "Boot device {} not found, sleeping 2 seconds", kernel_command_line().root_device());
         (void)Thread::current()->sleep(Duration::from_seconds(2));
     }
-    if (VirtualFileSystem::the().mount_root(StorageManagement::the().root_filesystem()).is_error()) {
-        PANIC("VirtualFileSystem::mount_root failed");
+
+    auto first_process_vfs_context_or_error = StorageManagement::the().create_first_vfs_root_context();
+    if (first_process_vfs_context_or_error.is_error()) {
+        PANIC("StorageManagement::create_first_vfs_root_context failed");
     }
+
+    auto first_process_vfs_context = first_process_vfs_context_or_error.release_value();
 
     // Switch out of early boot mode.
     g_not_in_early_boot.set();
@@ -472,7 +480,7 @@ void init_stage2(void*)
 
     dmesgln("Running first user process: {}", userspace_init);
     dmesgln("Init (first) process args: {}", init_args);
-    auto init_or_error = Process::create_user_process(userspace_init, UserID(0), GroupID(0), move(init_args), {}, tty0);
+    auto init_or_error = Process::create_user_process(userspace_init, UserID(0), GroupID(0), move(init_args), {}, move(first_process_vfs_context), tty0);
     if (init_or_error.is_error())
         PANIC("init_stage2: Error spawning init process: {}", init_or_error.error());
 

Kernel/CMakeLists.txt
@@ -226,6 +226,7 @@ set(KERNEL_SOURCES
     FileSystem/SysFS/Subsystems/Kernel/Configuration/DumpKmallocStack.cpp
     FileSystem/SysFS/Subsystems/Kernel/Configuration/StringVariable.cpp
     FileSystem/SysFS/Subsystems/Kernel/Configuration/UBSANDeadly.cpp
+    FileSystem/VFSRootContext.cpp
     FileSystem/VirtualFileSystem.cpp
     Firmware/ACPI/Initialize.cpp
     Firmware/ACPI/Parser.cpp

Kernel/Devices/Storage/StorageManagement.cpp
@@ -26,6 +26,7 @@
 #include <Kernel/Devices/Storage/StorageManagement.h>
 #include <Kernel/Devices/Storage/VirtIO/VirtIOBlockController.h>
 #include <Kernel/FileSystem/Ext2FS/FileSystem.h>
+#include <Kernel/FileSystem/MountFile.h>
 #include <Kernel/FileSystem/VirtualFileSystem.h>
 #include <Kernel/Library/Panic.h>
 #include <LibPartition/EBRPartitionTable.h>
@@ -42,6 +43,8 @@ static Atomic<u32> s_controller_id;
 static Atomic<u32> s_relative_ahci_controller_id;
 static Atomic<u32> s_relative_nvme_controller_id;
 static Atomic<u32> s_relative_sd_controller_id;
+
+static constexpr int root_mount_flags = 0;
 
 static constexpr StringView partition_uuid_prefix = "PARTUUID:"sv;
 
@@ -446,25 +449,40 @@ u32 StorageManagement::generate_controller_id()
     return s_controller_id.fetch_add(1);
 }
 
-NonnullRefPtr<FileSystem> StorageManagement::root_filesystem() const
+ErrorOr<NonnullRefPtr<VFSRootContext>> StorageManagement::create_first_vfs_root_context() const
 {
+    auto vfs_root_context = TRY(VFSRootContext::create_with_empty_ramfs());
+
+    auto const* fs_type_initializer = TRY(VirtualFileSystem::find_filesystem_type_initializer("ext2"sv));
+    VERIFY(fs_type_initializer);
+    auto mount_file = TRY(MountFile::create(*fs_type_initializer, root_mount_flags));
+
     auto boot_device_description = boot_block_device();
     if (!boot_device_description) {
         dump_storage_devices_and_partitions();
         PANIC("StorageManagement: Couldn't find a suitable device to boot from");
     }
-    auto description_or_error = OpenFileDescription::try_create(boot_device_description.release_nonnull());
-    VERIFY(!description_or_error.is_error());
+    auto description = TRY(OpenFileDescription::try_create(boot_device_description.release_nonnull()));
 
-    Array<u8, PAGE_SIZE> mount_specific_data;
-    mount_specific_data.fill(0);
-    auto file_system = Ext2FS::try_create(description_or_error.release_value(), mount_specific_data.span()).release_value();
+    RefPtr<FileSystem> fs;
+    TRY(VirtualFileSystem::the().m_file_backed_file_systems_list.with_exclusive([&](auto& list) -> ErrorOr<void> {
+        fs = TRY(VirtualFileSystem::create_and_initialize_filesystem_from_mount_file_and_description(list, mount_file, description));
+        // NOTE: Fake a mounted count of 1 so the called VirtualFileSystem function in the
+        // next pivot_root logic block thinks everything is OK.
+        fs->mounted_count(Badge<StorageManagement> {}).with([](auto& mounted_count) {
+            mounted_count++;
+        });
+        list.append(static_cast<FileBackedFileSystem&>(*fs));
+        return {};
+    }));
+    VERIFY(fs);
 
-    if (auto result = file_system->initialize(); result.is_error()) {
-        dump_storage_devices_and_partitions();
-        PANIC("StorageManagement: Couldn't open root filesystem: {}", result.error());
-    }
-    return file_system;
+    TRY(VirtualFileSystem::the().pivot_root_by_copying_mounted_fs_instance(vfs_root_context, *fs, root_mount_flags));
+    // NOTE: Return the mounted count to normal now we have it really mounted.
+    fs->mounted_count(Badge<StorageManagement> {}).with([](auto& mounted_count) {
+        mounted_count--;
+    });
+    return vfs_root_context;
 }
 
 UNMAP_AFTER_INIT void StorageManagement::initialize(bool poll)
@@ -491,5 +509,4 @@ StorageManagement& StorageManagement::the()
 {
     return *s_the;
 }
-
 }

Kernel/Devices/Storage/StorageManagement.h
@@ -29,7 +29,7 @@ public:
     static StorageManagement& the();
 
     bool determine_boot_device(StringView boot_argument);
-    NonnullRefPtr<FileSystem> root_filesystem() const;
+    ErrorOr<NonnullRefPtr<VFSRootContext>> create_first_vfs_root_context() const;
 
     static MinorNumber generate_storage_minor_number();
 

Kernel/FileSystem/CustodyBase.cpp
@@ -15,8 +15,11 @@ ErrorOr<NonnullRefPtr<Custody>> CustodyBase::resolve() const
 {
     if (m_base)
         return *m_base;
-    if (KLexicalPath::is_absolute(m_path))
-        return VirtualFileSystem::the().root_custody();
+    if (KLexicalPath::is_absolute(m_path)) {
+        return Process::current().vfs_root_context()->root_custody().with([](auto& custody) -> NonnullRefPtr<Custody> {
+            return custody;
+        });
+    }
     return Process::current().custody_for_dirfd({}, m_dirfd);
 }
 

Kernel/FileSystem/FileSystem.h
@@ -67,6 +67,8 @@ public:
     virtual u8 internal_file_type_to_directory_entry_type(DirectoryEntryView const& entry) const { return entry.file_type; }
 
     SpinlockProtected<size_t, LockRank::FileSystem>& mounted_count(Badge<VirtualFileSystem>) { return m_attach_count; }
+    SpinlockProtected<size_t, LockRank::FileSystem>& mounted_count(Badge<StorageManagement>) { return m_attach_count; }
+    SpinlockProtected<size_t, LockRank::FileSystem>& mounted_count(Badge<VFSRootContext>) { return m_attach_count; }
 
 protected:
     FileSystem();

Kernel/FileSystem/Inode.cpp
@@ -13,6 +13,7 @@
 #include <Kernel/FileSystem/Inode.h>
 #include <Kernel/FileSystem/InodeWatcher.h>
 #include <Kernel/FileSystem/OpenFileDescription.h>
+#include <Kernel/FileSystem/VFSRootContext.h>
 #include <Kernel/FileSystem/VirtualFileSystem.h>
 #include <Kernel/Library/KBufferBuilder.h>
 #include <Kernel/Memory/SharedInodeVMObject.h>
@@ -52,7 +53,7 @@ void Inode::sync()
     }
 }
 
-ErrorOr<NonnullRefPtr<Custody>> Inode::resolve_as_link(Credentials const& credentials, CustodyBase const& base, RefPtr<Custody>* out_parent, int options, int symlink_recursion_level) const
+ErrorOr<NonnullRefPtr<Custody>> Inode::resolve_as_link(VFSRootContext const& vfs_root_context, Credentials const& credentials, CustodyBase const& base, RefPtr<Custody>* out_parent, int options, int symlink_recursion_level) const
 {
     // The default implementation simply treats the stored
     // contents as a path and resolves that. That is, it
@@ -64,7 +65,7 @@ ErrorOr<NonnullRefPtr<Custody>> Inode::resolve_as_link(Credentials const& creden
 
     Array<u8, MAXPATHLEN> contents;
     auto read_bytes = TRY(read_until_filled_or_end(0, contents.size(), UserOrKernelBuffer::for_kernel_buffer(contents.data()), nullptr));
-    return VirtualFileSystem::the().resolve_path(credentials, StringView { contents.span().trim(read_bytes) }, base, out_parent, options, symlink_recursion_level);
+    return VirtualFileSystem::the().resolve_path(vfs_root_context, credentials, StringView { contents.span().trim(read_bytes) }, base, out_parent, options, symlink_recursion_level);
 }
 
 Inode::Inode(FileSystem& fs, InodeIndex index)

commit 76ccdc7140216ba984e19060e12ed07e10b4bfa0

Kernel: Create coredump by using the crashed process VFS root context
Resolve a regression caused by 01e1af732ba2eb7dd0c31d71fe9b008d8f5e9d42.

This unbreaks coredump generation, because we need to use the VFS root
context of the crashed process and not of the FinalizerTask, as it will
hold an empty VFS root context that is assigned to kernel processes.

Kernel/Tasks/Coredump.cpp
@@ -96,11 +96,11 @@ Coredump::Coredump(NonnullRefPtr<Process> process, NonnullRefPtr<OpenFileDescrip
 ErrorOr<NonnullRefPtr<OpenFileDescription>> Coredump::try_create_target_file(Process const& process, StringView output_path)
 {
     auto output_directory = KLexicalPath::dirname(output_path);
-    auto current_vfs_root_context = Process::current().vfs_root_context();
-    auto current_vfs_root_context_root_custody = current_vfs_root_context->root_custody().with([](auto& custody) -> NonnullRefPtr<Custody> {
+    auto process_vfs_root_context = process.vfs_root_context();
+    auto vfs_root_context_root_custody = process_vfs_root_context->root_custody().with([](auto& custody) -> NonnullRefPtr<Custody> {
         return custody;
     });
-    auto dump_directory = TRY(VirtualFileSystem::open_directory(current_vfs_root_context, Process::current().credentials(), output_directory, *current_vfs_root_context_root_custody));
+    auto dump_directory = TRY(VirtualFileSystem::open_directory(process_vfs_root_context, Process::current().credentials(), output_directory, *vfs_root_context_root_custody));
     auto dump_directory_metadata = dump_directory->inode().metadata();
     if (dump_directory_metadata.uid != 0 || dump_directory_metadata.gid != 0 || dump_directory_metadata.mode != 040777) {
         dbgln("Refusing to put coredump in sketchy directory '{}'", output_directory);
@@ -109,7 +109,7 @@ ErrorOr<NonnullRefPtr<OpenFileDescription>> Coredump::try_create_target_file(Pro
 
     auto process_credentials = process.credentials();
     return TRY(VirtualFileSystem::open(
-        current_vfs_root_context,
+        process_vfs_root_context,
         Process::current().credentials(),
         KLexicalPath::basename(output_path),
         O_CREAT | O_WRONLY | O_EXCL,