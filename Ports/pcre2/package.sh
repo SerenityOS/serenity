b5a5582c3428e3b31b2a6166b2c5a6a47d8f7da9
EWouters <6179932+EWouters@users.noreply.github.com>
2024-08-04 19:52:30 +0200

    Ports/pcre2: Update to version 10.44

--- Ports/pcre2/package.sh
+++ Ports/pcre2/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='pcre2'
-version='10.42'
+version='10.44'
 files=(
-    "https://github.com/PhilipHazel/pcre2/releases/download/pcre2-${version}/pcre2-${version}.tar.gz#c33b418e3b936ee3153de2c61cc638e7e4fe3156022a5c77d0711bcbb9d64f1f"
+    "https://github.com/PhilipHazel/pcre2/releases/download/pcre2-${version}/pcre2-${version}.tar.gz#86b9cb0aa3bcb7994faa88018292bc704cdbb708e785f7c74352ff6ea7d3175b"
 )
 useconfigure='true'
