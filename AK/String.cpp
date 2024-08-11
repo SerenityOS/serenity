6a9222ffd3b4a3098d2d646fd17f10adc8e6e84e
Shannon Booth <shannon@serenityos.org>
2024-08-11 14:48:30 +1200

    AK: Add fast-path in from_utf8_with_replacement_character for utf-8
    This ports the same optimization which was made in
    1a46d8df5fc81eb2c320d5c8a5597285d3d8fb3a to this function as well.
    
    (cherry picked from commit 1e8cc97b731871409316c121e637c32806135122)

--- AK/String.cpp
+++ AK/String.cpp
@@ -18,6 +18,9 @@ namespace AK {
 
 String String::from_utf8_with_replacement_character(StringView view)
 {
+    if (Utf8View(view).validate())
+        return String::from_utf8_without_validation(view.bytes());
+
     StringBuilder builder;
 
     for (auto c : Utf8View { view })
