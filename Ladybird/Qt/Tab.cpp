603f66017a3555a10f666719ba33494157ab8436
Jamie Mansfield <jmansfield@cadixdev.org>
2024-08-12 12:50:56 +0100

    Ladybird/Qt: Don't change to new tab with 'Open in New Tab'
    It now just opens a new tab, without changing the current tab.
    
    (cherry picked from commit df34ee058b1285d31386ee973f056d56f13d7bf6)

--- Ladybird/Qt/Tab.cpp
+++ Ladybird/Qt/Tab.cpp
@@ -852,7 +852,7 @@ void Tab::open_link(URL::URL const& url)
 
 void Tab::open_link_in_new_tab(URL::URL const& url)
 {
-    view().on_link_click(url, "_blank", 0);
+    view().on_link_click(url, "_blank", Web::UIEvents::Mod_Ctrl);
 }
 
 void Tab::copy_link_url(URL::URL const& url)
