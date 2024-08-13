a2b59b9c98d07b9e579b7133f7f47a3f0f99cba3
Nico Weber <thakis@chromium.org>
2024-08-12 21:56:37 -0400

    LibGfx/PNGWriter: Implement support for writing animated PNGs
    Based on #24021.
    
    Co-Authored-By: Pixel Brush <letsplaytvirtmann@gmail.com>

--- Userland/Libraries/LibGfx/ImageFormats/PNGWriter.h
+++ Userland/Libraries/LibGfx/ImageFormats/PNGWriter.h
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2021, Pierre Hoffmeister
  * Copyright (c) 2021, Andreas Kling <kling@serenityos.org>
+ * Copyright (c) 2024, Torben Jonas Virtmann
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -11,6 +12,7 @@
 #include <AK/Vector.h>
 #include <LibCompress/Zlib.h>
 #include <LibGfx/Forward.h>
+#include <LibGfx/ImageFormats/AnimationWriter.h>
 #include <LibGfx/ImageFormats/PNGShared.h>
 
 namespace Gfx {
@@ -26,6 +28,8 @@ struct PNGWriterOptions {
     Optional<ReadonlyBytes> icc_data;
 };
 
+struct fcTLData;
+
 class PNGWriter {
 public:
     using Options = PNGWriterOptions;
@@ -33,13 +37,20 @@ public:
     static ErrorOr<void> encode(Stream&, Bitmap const&, Options const& = {});
     static ErrorOr<ByteBuffer> encode(Gfx::Bitmap const&, Options options = Options {});
 
+    static ErrorOr<NonnullOwnPtr<AnimationWriter>> start_encoding_animation(SeekableStream&, IntSize dimensions, int loop_count = 0, Options const& = {});
+
 private:
+    friend class PNGAnimationWriter;
     PNGWriter(Stream&);
 
     Stream& m_stream;
 
     ErrorOr<void> add_chunk(PNGChunk&);
     ErrorOr<void> add_png_header();
+    ErrorOr<void> add_acTL_chunk(u32 num_frames, u32 loop_count);
+    ErrorOr<void> add_fcTL_chunk(fcTLData const& data);
+    template<bool include_alpha>
+    ErrorOr<void> add_fdAT_chunk(Gfx::Bitmap const&, u32 sequence_number, Compress::ZlibCompressionLevel);
     ErrorOr<void> add_IHDR_chunk(u32 width, u32 height, u8 bit_depth, PNG::ColorType color_type, u8 compression_method, u8 filter_method, u8 interlace_method);
     ErrorOr<void> add_iCCP_chunk(ReadonlyBytes icc_data, Compress::ZlibCompressionLevel);
     template<bool include_alpha>
