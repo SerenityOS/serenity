4155cd15e1641758ebe8be7e2339e1c1ea4a89f2
Sam Atkins <sam@ladybird.org>
2024-08-14 10:54:09 +0100

    LibWeb/CSS: Flatten ENUMERATE_STYLE_VALUE_TYPES macros
    Being constrained by this macro is going to make it more difficult to
    refactor our StyleValue types to match the Typed-OM spec, so let's
    reintroduce the boilerplate for now.
    
    No functional changes.
    
    (cherry picked from commit 2e1f62681cf8c11b00eccf041aabe2d4bd5c2941)

--- Userland/Libraries/LibWeb/CSS/StyleValue.h
+++ Userland/Libraries/LibWeb/CSS/StyleValue.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2018-2023, Andreas Kling <kling@serenityos.org>
  * Copyright (c) 2021, Tobias Christiansen <tobyase@serenityos.org>
- * Copyright (c) 2021-2023, Sam Atkins <atkinssj@serenityos.org>
+ * Copyright (c) 2021-2024, Sam Atkins <sam@ladybird.org>
  * Copyright (c) 2022-2023, MacDue <macdue@dueutil.tech>
  *
  * SPDX-License-Identifier: BSD-2-Clause
@@ -82,79 +82,63 @@ private:
 
 using StyleValueVector = Vector<ValueComparingNonnullRefPtr<StyleValue const>>;
 
-#define ENUMERATE_STYLE_VALUE_TYPES                                        \
-    __ENUMERATE_STYLE_VALUE_TYPE(Angle, angle)                             \
-    __ENUMERATE_STYLE_VALUE_TYPE(BackgroundRepeat, background_repeat)      \
-    __ENUMERATE_STYLE_VALUE_TYPE(BackgroundSize, background_size)          \
-    __ENUMERATE_STYLE_VALUE_TYPE(BasicShape, basic_shape)                  \
-    __ENUMERATE_STYLE_VALUE_TYPE(BorderRadius, border_radius)              \
-    __ENUMERATE_STYLE_VALUE_TYPE(Calculated, calculated)                   \
-    __ENUMERATE_STYLE_VALUE_TYPE(Color, color)                             \
-    __ENUMERATE_STYLE_VALUE_TYPE(ConicGradient, conic_gradient)            \
-    __ENUMERATE_STYLE_VALUE_TYPE(Content, content)                         \
-    __ENUMERATE_STYLE_VALUE_TYPE(Counter, counter)                         \
-    __ENUMERATE_STYLE_VALUE_TYPE(CounterDefinitions, counter_definitions)  \
-    __ENUMERATE_STYLE_VALUE_TYPE(CustomIdent, custom_ident)                \
-    __ENUMERATE_STYLE_VALUE_TYPE(Display, display)                         \
-    __ENUMERATE_STYLE_VALUE_TYPE(Easing, easing)                           \
-    __ENUMERATE_STYLE_VALUE_TYPE(Edge, edge)                               \
-    __ENUMERATE_STYLE_VALUE_TYPE(FilterValueList, filter_value_list)       \
-    __ENUMERATE_STYLE_VALUE_TYPE(Flex, flex)                               \
-    __ENUMERATE_STYLE_VALUE_TYPE(Frequency, frequency)                     \
-    __ENUMERATE_STYLE_VALUE_TYPE(GridAutoFlow, grid_auto_flow)             \
-    __ENUMERATE_STYLE_VALUE_TYPE(GridTemplateArea, grid_template_area)     \
-    __ENUMERATE_STYLE_VALUE_TYPE(GridTrackPlacement, grid_track_placement) \
-    __ENUMERATE_STYLE_VALUE_TYPE(GridTrackSizeList, grid_track_size_list)  \
-    __ENUMERATE_STYLE_VALUE_TYPE(Identifier, identifier)                   \
-    __ENUMERATE_STYLE_VALUE_TYPE(Image, image)                             \
-    __ENUMERATE_STYLE_VALUE_TYPE(Inherit, inherit)                         \
-    __ENUMERATE_STYLE_VALUE_TYPE(Initial, initial)                         \
-    __ENUMERATE_STYLE_VALUE_TYPE(Integer, integer)                         \
-    __ENUMERATE_STYLE_VALUE_TYPE(Length, length)                           \
-    __ENUMERATE_STYLE_VALUE_TYPE(LinearGradient, linear_gradient)          \
-    __ENUMERATE_STYLE_VALUE_TYPE(MathDepth, math_depth)                    \
-    __ENUMERATE_STYLE_VALUE_TYPE(Number, number)                           \
-    __ENUMERATE_STYLE_VALUE_TYPE(Percentage, percentage)                   \
-    __ENUMERATE_STYLE_VALUE_TYPE(Position, position)                       \
-    __ENUMERATE_STYLE_VALUE_TYPE(RadialGradient, radial_gradient)          \
-    __ENUMERATE_STYLE_VALUE_TYPE(Ratio, ratio)                             \
-    __ENUMERATE_STYLE_VALUE_TYPE(Rect, rect)                               \
-    __ENUMERATE_STYLE_VALUE_TYPE(Resolution, resolution)                   \
-    __ENUMERATE_STYLE_VALUE_TYPE(Revert, revert)                           \
-    __ENUMERATE_STYLE_VALUE_TYPE(ScrollbarGutter, scrollbar_gutter)        \
-    __ENUMERATE_STYLE_VALUE_TYPE(Shadow, shadow)                           \
-    __ENUMERATE_STYLE_VALUE_TYPE(Shorthand, shorthand)                     \
-    __ENUMERATE_STYLE_VALUE_TYPE(String, string)                           \
-    __ENUMERATE_STYLE_VALUE_TYPE(Time, time)                               \
-    __ENUMERATE_STYLE_VALUE_TYPE(Transformation, transformation)           \
-    __ENUMERATE_STYLE_VALUE_TYPE(Transition, transition)                   \
-    __ENUMERATE_STYLE_VALUE_TYPE(Unresolved, unresolved)                   \
-    __ENUMERATE_STYLE_VALUE_TYPE(Unset, unset)                             \
-    __ENUMERATE_STYLE_VALUE_TYPE(URL, url)                                 \
-    __ENUMERATE_STYLE_VALUE_TYPE(ValueList, value_list)
-
-// NOTE:
-using ValueListStyleValue = StyleValueList;
-
 class StyleValue : public RefCounted<StyleValue> {
 public:
     virtual ~StyleValue() = default;
 
     enum class Type {
-#define __ENUMERATE_STYLE_VALUE_TYPE(TitleCaseName, SnakeCaseName) \
-    TitleCaseName,
-        ENUMERATE_STYLE_VALUE_TYPES
-#undef __ENUMERATE_STYLE_VALUE_TYPE
+        Angle,
+        BackgroundRepeat,
+        BackgroundSize,
+        BasicShape,
+        BorderRadius,
+        Calculated,
+        Color,
+        ConicGradient,
+        Content,
+        Counter,
+        CounterDefinitions,
+        CustomIdent,
+        Display,
+        Easing,
+        Edge,
+        FilterValueList,
+        Flex,
+        Frequency,
+        GridAutoFlow,
+        GridTemplateArea,
+        GridTrackPlacement,
+        GridTrackSizeList,
+        Identifier,
+        Image,
+        Inherit,
+        Initial,
+        Integer,
+        Length,
+        LinearGradient,
+        MathDepth,
+        Number,
+        Percentage,
+        Position,
+        RadialGradient,
+        Ratio,
+        Rect,
+        Resolution,
+        Revert,
+        ScrollbarGutter,
+        Shadow,
+        Shorthand,
+        String,
+        Time,
+        Transformation,
+        Transition,
+        Unresolved,
+        Unset,
+        URL,
+        ValueList,
     };
 
     Type type() const { return m_type; }
-
-#define __ENUMERATE_STYLE_VALUE_TYPE(TitleCaseName, SnakeCaseName)            \
-    bool is_##SnakeCaseName() const { return type() == Type::TitleCaseName; } \
-    TitleCaseName##StyleValue const& as_##SnakeCaseName() const;              \
-    TitleCaseName##StyleValue& as_##SnakeCaseName() { return const_cast<TitleCaseName##StyleValue&>(const_cast<StyleValue const&>(*this).as_##SnakeCaseName()); }
-    ENUMERATE_STYLE_VALUE_TYPES
-#undef __ENUMERATE_STYLE_VALUE_TYPE
 
     bool is_abstract_image() const
     {
@@ -162,6 +146,202 @@ public:
     }
     AbstractImageStyleValue const& as_abstract_image() const;
     AbstractImageStyleValue& as_abstract_image() { return const_cast<AbstractImageStyleValue&>(const_cast<StyleValue const&>(*this).as_abstract_image()); }
+
+    bool is_angle() const { return type() == Type::Angle; }
+    AngleStyleValue const& as_angle() const;
+    AngleStyleValue& as_angle() { return const_cast<AngleStyleValue&>(const_cast<StyleValue const&>(*this).as_angle()); }
+
+    bool is_background_repeat() const { return type() == Type::BackgroundRepeat; }
+    BackgroundRepeatStyleValue const& as_background_repeat() const;
+    BackgroundRepeatStyleValue& as_background_repeat() { return const_cast<BackgroundRepeatStyleValue&>(const_cast<StyleValue const&>(*this).as_background_repeat()); }
+
+    bool is_background_size() const { return type() == Type::BackgroundSize; }
+    BackgroundSizeStyleValue const& as_background_size() const;
+    BackgroundSizeStyleValue& as_background_size() { return const_cast<BackgroundSizeStyleValue&>(const_cast<StyleValue const&>(*this).as_background_size()); }
+
+    bool is_basic_shape() const { return type() == Type::BasicShape; }
+    BasicShapeStyleValue const& as_basic_shape() const;
+    BasicShapeStyleValue& as_basic_shape() { return const_cast<BasicShapeStyleValue&>(const_cast<StyleValue const&>(*this).as_basic_shape()); }
+
+    bool is_border_radius() const { return type() == Type::BorderRadius; }
+    BorderRadiusStyleValue const& as_border_radius() const;
+    BorderRadiusStyleValue& as_border_radius() { return const_cast<BorderRadiusStyleValue&>(const_cast<StyleValue const&>(*this).as_border_radius()); }
+
+    bool is_calculated() const { return type() == Type::Calculated; }
+    CalculatedStyleValue const& as_calculated() const;
+    CalculatedStyleValue& as_calculated() { return const_cast<CalculatedStyleValue&>(const_cast<StyleValue const&>(*this).as_calculated()); }
+
+    bool is_color() const { return type() == Type::Color; }
+    ColorStyleValue const& as_color() const;
+    ColorStyleValue& as_color() { return const_cast<ColorStyleValue&>(const_cast<StyleValue const&>(*this).as_color()); }
+
+    bool is_conic_gradient() const { return type() == Type::ConicGradient; }
+    ConicGradientStyleValue const& as_conic_gradient() const;
+    ConicGradientStyleValue& as_conic_gradient() { return const_cast<ConicGradientStyleValue&>(const_cast<StyleValue const&>(*this).as_conic_gradient()); }
+
+    bool is_content() const { return type() == Type::Content; }
+    ContentStyleValue const& as_content() const;
+    ContentStyleValue& as_content() { return const_cast<ContentStyleValue&>(const_cast<StyleValue const&>(*this).as_content()); }
+
+    bool is_counter() const { return type() == Type::Counter; }
+    CounterStyleValue const& as_counter() const;
+    CounterStyleValue& as_counter() { return const_cast<CounterStyleValue&>(const_cast<StyleValue const&>(*this).as_counter()); }
+
+    bool is_counter_definitions() const { return type() == Type::CounterDefinitions; }
+    CounterDefinitionsStyleValue const& as_counter_definitions() const;
+    CounterDefinitionsStyleValue& as_counter_definitions() { return const_cast<CounterDefinitionsStyleValue&>(const_cast<StyleValue const&>(*this).as_counter_definitions()); }
+
+    bool is_custom_ident() const { return type() == Type::CustomIdent; }
+    CustomIdentStyleValue const& as_custom_ident() const;
+    CustomIdentStyleValue& as_custom_ident() { return const_cast<CustomIdentStyleValue&>(const_cast<StyleValue const&>(*this).as_custom_ident()); }
+
+    bool is_display() const { return type() == Type::Display; }
+    DisplayStyleValue const& as_display() const;
+    DisplayStyleValue& as_display() { return const_cast<DisplayStyleValue&>(const_cast<StyleValue const&>(*this).as_display()); }
+
+    bool is_easing() const { return type() == Type::Easing; }
+    EasingStyleValue const& as_easing() const;
+    EasingStyleValue& as_easing() { return const_cast<EasingStyleValue&>(const_cast<StyleValue const&>(*this).as_easing()); }
+
+    bool is_edge() const { return type() == Type::Edge; }
+    EdgeStyleValue const& as_edge() const;
+    EdgeStyleValue& as_edge() { return const_cast<EdgeStyleValue&>(const_cast<StyleValue const&>(*this).as_edge()); }
+
+    bool is_filter_value_list() const { return type() == Type::FilterValueList; }
+    FilterValueListStyleValue const& as_filter_value_list() const;
+    FilterValueListStyleValue& as_filter_value_list() { return const_cast<FilterValueListStyleValue&>(const_cast<StyleValue const&>(*this).as_filter_value_list()); }
+
+    bool is_flex() const { return type() == Type::Flex; }
+    FlexStyleValue const& as_flex() const;
+    FlexStyleValue& as_flex() { return const_cast<FlexStyleValue&>(const_cast<StyleValue const&>(*this).as_flex()); }
+
+    bool is_frequency() const { return type() == Type::Frequency; }
+    FrequencyStyleValue const& as_frequency() const;
+    FrequencyStyleValue& as_frequency() { return const_cast<FrequencyStyleValue&>(const_cast<StyleValue const&>(*this).as_frequency()); }
+
+    bool is_grid_auto_flow() const { return type() == Type::GridAutoFlow; }
+    GridAutoFlowStyleValue const& as_grid_auto_flow() const;
+    GridAutoFlowStyleValue& as_grid_auto_flow() { return const_cast<GridAutoFlowStyleValue&>(const_cast<StyleValue const&>(*this).as_grid_auto_flow()); }
+
+    bool is_grid_template_area() const { return type() == Type::GridTemplateArea; }
+    GridTemplateAreaStyleValue const& as_grid_template_area() const;
+    GridTemplateAreaStyleValue& as_grid_template_area() { return const_cast<GridTemplateAreaStyleValue&>(const_cast<StyleValue const&>(*this).as_grid_template_area()); }
+
+    bool is_grid_track_placement() const { return type() == Type::GridTrackPlacement; }
+    GridTrackPlacementStyleValue const& as_grid_track_placement() const;
+    GridTrackPlacementStyleValue& as_grid_track_placement() { return const_cast<GridTrackPlacementStyleValue&>(const_cast<StyleValue const&>(*this).as_grid_track_placement()); }
+
+    bool is_grid_track_size_list() const { return type() == Type::GridTrackSizeList; }
+    GridTrackSizeListStyleValue const& as_grid_track_size_list() const;
+    GridTrackSizeListStyleValue& as_grid_track_size_list() { return const_cast<GridTrackSizeListStyleValue&>(const_cast<StyleValue const&>(*this).as_grid_track_size_list()); }
+
+    bool is_identifier() const { return type() == Type::Identifier; }
+    IdentifierStyleValue const& as_identifier() const;
+    IdentifierStyleValue& as_identifier() { return const_cast<IdentifierStyleValue&>(const_cast<StyleValue const&>(*this).as_identifier()); }
+
+    bool is_image() const { return type() == Type::Image; }
+    ImageStyleValue const& as_image() const;
+    ImageStyleValue& as_image() { return const_cast<ImageStyleValue&>(const_cast<StyleValue const&>(*this).as_image()); }
+
+    bool is_inherit() const { return type() == Type::Inherit; }
+    InheritStyleValue const& as_inherit() const;
+    InheritStyleValue& as_inherit() { return const_cast<InheritStyleValue&>(const_cast<StyleValue const&>(*this).as_inherit()); }
+
+    bool is_initial() const { return type() == Type::Initial; }
+    InitialStyleValue const& as_initial() const;
+    InitialStyleValue& as_initial() { return const_cast<InitialStyleValue&>(const_cast<StyleValue const&>(*this).as_initial()); }
+
+    bool is_integer() const { return type() == Type::Integer; }
+    IntegerStyleValue const& as_integer() const;
+    IntegerStyleValue& as_integer() { return const_cast<IntegerStyleValue&>(const_cast<StyleValue const&>(*this).as_integer()); }
+
+    bool is_length() const { return type() == Type::Length; }
+    LengthStyleValue const& as_length() const;
+    LengthStyleValue& as_length() { return const_cast<LengthStyleValue&>(const_cast<StyleValue const&>(*this).as_length()); }
+
+    bool is_linear_gradient() const { return type() == Type::LinearGradient; }
+    LinearGradientStyleValue const& as_linear_gradient() const;
+    LinearGradientStyleValue& as_linear_gradient() { return const_cast<LinearGradientStyleValue&>(const_cast<StyleValue const&>(*this).as_linear_gradient()); }
+
+    bool is_math_depth() const { return type() == Type::MathDepth; }
+    MathDepthStyleValue const& as_math_depth() const;
+    MathDepthStyleValue& as_math_depth() { return const_cast<MathDepthStyleValue&>(const_cast<StyleValue const&>(*this).as_math_depth()); }
+
+    bool is_number() const { return type() == Type::Number; }
+    NumberStyleValue const& as_number() const;
+    NumberStyleValue& as_number() { return const_cast<NumberStyleValue&>(const_cast<StyleValue const&>(*this).as_number()); }
+
+    bool is_percentage() const { return type() == Type::Percentage; }
+    PercentageStyleValue const& as_percentage() const;
+    PercentageStyleValue& as_percentage() { return const_cast<PercentageStyleValue&>(const_cast<StyleValue const&>(*this).as_percentage()); }
+
+    bool is_position() const { return type() == Type::Position; }
+    PositionStyleValue const& as_position() const;
+
+    PositionStyleValue& as_position() { return const_cast<PositionStyleValue&>(const_cast<StyleValue const&>(*this).as_position()); }
+    bool is_radial_gradient() const { return type() == Type::RadialGradient; }
+    RadialGradientStyleValue const& as_radial_gradient() const;
+    RadialGradientStyleValue& as_radial_gradient() { return const_cast<RadialGradientStyleValue&>(const_cast<StyleValue const&>(*this).as_radial_gradient()); }
+
+    bool is_ratio() const { return type() == Type::Ratio; }
+    RatioStyleValue const& as_ratio() const;
+    RatioStyleValue& as_ratio() { return const_cast<RatioStyleValue&>(const_cast<StyleValue const&>(*this).as_ratio()); }
+
+    bool is_rect() const { return type() == Type::Rect; }
+    RectStyleValue const& as_rect() const;
+    RectStyleValue& as_rect() { return const_cast<RectStyleValue&>(const_cast<StyleValue const&>(*this).as_rect()); }
+
+    bool is_resolution() const { return type() == Type::Resolution; }
+    ResolutionStyleValue const& as_resolution() const;
+    ResolutionStyleValue& as_resolution() { return const_cast<ResolutionStyleValue&>(const_cast<StyleValue const&>(*this).as_resolution()); }
+
+    bool is_revert() const { return type() == Type::Revert; }
+    RevertStyleValue const& as_revert() const;
+    RevertStyleValue& as_revert() { return const_cast<RevertStyleValue&>(const_cast<StyleValue const&>(*this).as_revert()); }
+
+    bool is_scrollbar_gutter() const { return type() == Type::ScrollbarGutter; }
+    ScrollbarGutterStyleValue const& as_scrollbar_gutter() const;
+    ScrollbarGutterStyleValue& as_scrollbar_gutter() { return const_cast<ScrollbarGutterStyleValue&>(const_cast<StyleValue const&>(*this).as_scrollbar_gutter()); }
+
+    bool is_shadow() const { return type() == Type::Shadow; }
+    ShadowStyleValue const& as_shadow() const;
+    ShadowStyleValue& as_shadow() { return const_cast<ShadowStyleValue&>(const_cast<StyleValue const&>(*this).as_shadow()); }
+
+    bool is_shorthand() const { return type() == Type::Shorthand; }
+    ShorthandStyleValue const& as_shorthand() const;
+    ShorthandStyleValue& as_shorthand() { return const_cast<ShorthandStyleValue&>(const_cast<StyleValue const&>(*this).as_shorthand()); }
+
+    bool is_string() const { return type() == Type::String; }
+    StringStyleValue const& as_string() const;
+    StringStyleValue& as_string() { return const_cast<StringStyleValue&>(const_cast<StyleValue const&>(*this).as_string()); }
+
+    bool is_time() const { return type() == Type::Time; }
+    TimeStyleValue const& as_time() const;
+    TimeStyleValue& as_time() { return const_cast<TimeStyleValue&>(const_cast<StyleValue const&>(*this).as_time()); }
+
+    bool is_transformation() const { return type() == Type::Transformation; }
+    TransformationStyleValue const& as_transformation() const;
+    TransformationStyleValue& as_transformation() { return const_cast<TransformationStyleValue&>(const_cast<StyleValue const&>(*this).as_transformation()); }
+
+    bool is_transition() const { return type() == Type::Transition; }
+    TransitionStyleValue const& as_transition() const;
+    TransitionStyleValue& as_transition() { return const_cast<TransitionStyleValue&>(const_cast<StyleValue const&>(*this).as_transition()); }
+
+    bool is_unresolved() const { return type() == Type::Unresolved; }
+    UnresolvedStyleValue const& as_unresolved() const;
+    UnresolvedStyleValue& as_unresolved() { return const_cast<UnresolvedStyleValue&>(const_cast<StyleValue const&>(*this).as_unresolved()); }
+
+    bool is_unset() const { return type() == Type::Unset; }
+    UnsetStyleValue const& as_unset() const;
+    UnsetStyleValue& as_unset() { return const_cast<UnsetStyleValue&>(const_cast<StyleValue const&>(*this).as_unset()); }
+
+    bool is_url() const { return type() == Type::URL; }
+    URLStyleValue const& as_url() const;
+    URLStyleValue& as_url() { return const_cast<URLStyleValue&>(const_cast<StyleValue const&>(*this).as_url()); }
+
+    bool is_value_list() const { return type() == Type::ValueList; }
+    StyleValueList const& as_value_list() const;
+    StyleValueList& as_value_list() { return const_cast<StyleValueList&>(const_cast<StyleValue const&>(*this).as_value_list()); }
 
     // https://www.w3.org/TR/css-values-4/#common-keywords
     // https://drafts.csswg.org/css-cascade-4/#valdef-all-revert
