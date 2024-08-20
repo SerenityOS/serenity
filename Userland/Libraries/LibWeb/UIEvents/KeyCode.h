9c512129f7df176e0c39349c5cbac3048e5e665b
Timothy Flynn <trflynn89@pm.me>
2024-08-19 22:49:22 -0400

    LibWeb: Add a helper to lookup a key code from its string name
    (cherry picked from commit f14e7c7261a41d1843610167cdf8e3cd671f403a;
    amended to resolve conflict due to #25060)

--- Userland/Libraries/LibWeb/UIEvents/KeyCode.h
+++ Userland/Libraries/LibWeb/UIEvents/KeyCode.h
@@ -6,6 +6,8 @@
 
 #pragma once
 
+#include <AK/StringView.h>
+
 // FIXME: Stop including Kernel here eventually.
 #include <Kernel/API/KeyCode.h>
 
@@ -16,6 +18,17 @@ using enum KeyCode;
 
 using KeyModifier = ::KeyModifier;
 using enum KeyModifier;
+
+constexpr KeyCode key_code_from_string(StringView key_name)
+{
+#define __ENUMERATE_KEY_CODE(name, ui_name) \
+    if (key_name == ui_name##sv)            \
+        return KeyCode::Key_##name;
+    ENUMERATE_KEY_CODES
+#undef __ENUMERATE_KEY_CODE
+
+    VERIFY_NOT_REACHED();
+}
 
 inline KeyCode code_point_to_key_code(u32 code_point)
 {
