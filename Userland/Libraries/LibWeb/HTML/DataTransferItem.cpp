2dac492cded5c22f289c8443731237626b616b73
Timothy Flynn <trflynn89@pm.me>
2024-08-21 08:54:11 -0400

    LibWeb: Add a factory method to create a DataTransferItem
    A DataTransferItem is associated with a DataTransfer, and points to an
    item in the drag data store. We don't yet support removing items from
    the store, but when we do, we will clear the index stored here to set
    the DataTransferItem's mode to "disabled".
    
    (cherry picked from commit c82fec0a8cc871aa331de5a557db1adddca7711c)

--- Userland/Libraries/LibWeb/HTML/DataTransferItem.cpp
+++ Userland/Libraries/LibWeb/HTML/DataTransferItem.cpp
@@ -7,14 +7,22 @@
 #include <LibJS/Runtime/Realm.h>
 #include <LibWeb/Bindings/DataTransferItemPrototype.h>
 #include <LibWeb/Bindings/Intrinsics.h>
+#include <LibWeb/HTML/DataTransfer.h>
 #include <LibWeb/HTML/DataTransferItem.h>
 
 namespace Web::HTML {
 
 JS_DEFINE_ALLOCATOR(DataTransferItem);
 
-DataTransferItem::DataTransferItem(JS::Realm& realm)
+JS::NonnullGCPtr<DataTransferItem> DataTransferItem::create(JS::Realm& realm, JS::NonnullGCPtr<DataTransfer> data_transfer, size_t item_index)
+{
+    return realm.heap().allocate<DataTransferItem>(realm, realm, data_transfer, item_index);
+}
+
+DataTransferItem::DataTransferItem(JS::Realm& realm, JS::NonnullGCPtr<DataTransfer> data_transfer, size_t item_index)
     : PlatformObject(realm)
+    , m_data_transfer(data_transfer)
+    , m_item_index(item_index)
 {
 }
 
@@ -24,6 +32,12 @@ void DataTransferItem::initialize(JS::Realm& realm)
 {
     Base::initialize(realm);
     WEB_SET_PROTOTYPE_FOR_INTERFACE(DataTransferItem);
+}
+
+void DataTransferItem::visit_edges(JS::Cell::Visitor& visitor)
+{
+    Base::visit_edges(visitor);
+    visitor.visit(m_data_transfer);
 }
 
 }
--- Userland/Libraries/LibWeb/HTML/DataTransferItem.h
+++ Userland/Libraries/LibWeb/HTML/DataTransferItem.h
@@ -6,8 +6,10 @@
 
 #pragma once
 
+#include <AK/Optional.h>
 #include <LibJS/Forward.h>
 #include <LibWeb/Bindings/PlatformObject.h>
+#include <LibWeb/HTML/DragDataStore.h>
 
 namespace Web::HTML {
 
@@ -17,12 +19,17 @@ class DataTransferItem : public Bindings::PlatformObject {
     JS_DECLARE_ALLOCATOR(DataTransferItem);
 
 public:
+    static JS::NonnullGCPtr<DataTransferItem> create(JS::Realm&, JS::NonnullGCPtr<DataTransfer>, size_t item_index);
     virtual ~DataTransferItem() override;
 
 private:
-    DataTransferItem(JS::Realm&);
+    DataTransferItem(JS::Realm&, JS::NonnullGCPtr<DataTransfer>, size_t item_index);
 
     virtual void initialize(JS::Realm&) override;
+    virtual void visit_edges(JS::Cell::Visitor&) override;
+
+    JS::NonnullGCPtr<DataTransfer> m_data_transfer;
+    Optional<size_t> m_item_index;
 };
 
 }
