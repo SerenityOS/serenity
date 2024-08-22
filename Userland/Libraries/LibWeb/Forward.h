477d9f0aafa7e9d4d791c9453645773c9606169a
Tim Ledbetter <tim.ledbetter@ladybird.org>
2024-08-22 22:59:01 +0100

    LibWeb: Stub the `ServiceWorkerRegistration` interface
    (cherry picked from commit 0c0a4a6042f7d25186c65ca60aa04d066dc51e97)

--- Userland/Libraries/LibWeb/Forward.h
+++ Userland/Libraries/LibWeb/Forward.h
@@ -482,6 +482,7 @@ class Plugin;
 class PluginArray;
 class PromiseRejectionEvent;
 class SelectedFile;
+class ServiceWorkerRegistration;
 class SharedResourceRequest;
 class Storage;
 class SubmitEvent;
