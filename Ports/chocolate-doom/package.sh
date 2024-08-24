aec271056143359d310ba2ef6a25889ac2d84405
Mike Swanson <mikeonthecomputer@gmail.com>
2024-08-24 02:28:08 -0700

    Ports/chocolate-doom: Update to 3.1.0
    Since the release doesn't have pre-built autotools files anymore, that
    step needs to be performed at compilation time.  Otherwise, a
    straightforward update.  :-)

--- Ports/chocolate-doom/package.sh
+++ Ports/chocolate-doom/package.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='chocolate-doom'
-version='3.0.1'
+version='3.1.0'
+workdir="${port}-${port}-${version}"
 useconfigure='true'
-use_fresh_config_sub='true'
-config_sub_paths=('autotools/config.sub')
 files=(
-    "https://www.chocolate-doom.org/downloads/${version}/chocolate-doom-${version}.tar.gz#d435d6177423491d60be706da9f07d3ab4fabf3e077ec2a3fc216e394fcfc8c7"
+    "https://github.com/${port}/${port}/archive/refs/tags/${port}-${version}.tar.gz#f2c64843dcec312032b180c3b2f34b4cb26c4dcdaa7375a1601a3b1df11ef84d"
 )
 depends=(
     'libpng'
@@ -19,3 +18,7 @@ launcher_name='Chocolate Doom'
 launcher_category='&Games'
 launcher_command='/usr/local/bin/chocolate-doom'
 icon_file='data/doom.png'
+
+configure() {
+    run ./autogen.sh --host=x86_64-pc-serenity
+}
