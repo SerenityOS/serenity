695a66a0b33c87e13d5434805851bf19c013d1cc
famfo <famfo@famfo.xyz>
2024-09-10 00:48:49 +0200

    AK/IpAddressCidr: Add IPv6AddressCidr

--- AK/IpAddressCidr.h
+++ AK/IpAddressCidr.h
@@ -9,6 +9,7 @@
 #include <AK/Error.h>
 #include <AK/IPv4Address.h>
 #include <AK/IPv6Address.h>
+#include <AK/UFixedBigInt.h>
 
 namespace AK {
 
@@ -116,6 +117,12 @@ public:
     using IPAddress = IPv4Address;
 };
 
+template<>
+class AddressTraits<IPv6AddressCidr> {
+public:
+    using IPAddress = IPv6Address;
+};
+
 }
 
 class IPv4AddressCidr : public Details::IPAddressCidr<IPv4AddressCidr> {
@@ -191,8 +198,96 @@ struct Formatter<IPv4AddressCidr> : Formatter<StringView> {
 };
 #endif
 
+class IPv6AddressCidr : public Details::IPAddressCidr<IPv6AddressCidr> {
+public:
+    constexpr IPv6AddressCidr(IPv6Address address, u8 length)
+        : IPAddressCidr(address, length)
+    {
+    }
+
+    constexpr IPv6Address first_address_of_subnet() const
+    {
+        u8 address[16] = { 0 };
+        u8 free_bits = MAX_LENGTH - length();
+
+        if (free_bits != 128) {
+            NetworkOrdered<u128> mask = NumericLimits<u128>::max() << free_bits;
+            auto address_mask = bit_cast<Array<u8, 16>>(mask);
+
+            auto const* original_address = ip_address().to_in6_addr_t();
+            for (int i = 0; i < 16; i++) {
+                address[i] = original_address[i] & address_mask[i];
+            }
+        }
+
+        return address;
+    }
+
+    constexpr IPv6Address last_address_of_subnet() const
+    {
+        u8 address[16] = { 0 };
+        u8 free_bits = MAX_LENGTH - length();
+
+        NetworkOrdered<u128> inverse_mask = NumericLimits<u128>::max() >> (128 - free_bits);
+
+        if (free_bits != 128) {
+            NetworkOrdered<u128> mask = NumericLimits<u128>::max() << free_bits;
+            auto address_mask = bit_cast<Array<u8, 16>>(mask);
+
+            auto const* original_address = ip_address().to_in6_addr_t();
+            for (int i = 0; i < 16; i++) {
+                address[i] = original_address[i] & address_mask[i];
+            }
+        }
+
+        auto inverse_address_mask = bit_cast<Array<u8, 16>>(inverse_mask);
+
+        for (int i = 0; i < 16; i++) {
+            address[i] = address[i] | inverse_address_mask[i];
+        }
+
+        return address;
+    }
+
+    bool contains(IPv6Address other) const
+    {
+        IPv6AddressCidr other_cidr = IPv6AddressCidr::create(other, length()).value();
+        return first_address_of_subnet() == other_cidr.first_address_of_subnet();
+    }
+
+    static u8 const MAX_LENGTH = 128;
+};
+
+template<>
+struct Traits<IPv6AddressCidr> : public DefaultTraits<IPv6AddressCidr> {
+    static unsigned hash(IPv6AddressCidr const& address)
+    {
+        IPv6Address ip_address = address.ip_address();
+        return sip_hash_bytes<4, 8>({ &ip_address, address.length() });
+    }
+};
+
+#ifdef KERNEL
+template<>
+struct Formatter<IPv6AddressCidr> : Formatter<StringView> {
+    ErrorOr<void> format(FormatBuilder& builder, IPv6AddressCidr value)
+    {
+        return Formatter<StringView>::format(builder, TRY(value.to_string())->view());
+    }
+};
+#else
+template<>
+struct Formatter<IPv6AddressCidr> : Formatter<StringView> {
+    ErrorOr<void> format(FormatBuilder& builder, IPv6AddressCidr value)
+    {
+        return Formatter<StringView>::format(builder, TRY(value.to_string()));
+    }
+};
+#endif
+
 }
 
 #if USING_AK_GLOBALLY
 using AK::IPv4AddressCidr;
+using AK::IPv6AddressCidr;
 #endif
