b18dc0a0adbbebf247332aec02e68c11551a5082
Andrew Kaster <andrew@ladybird.org>
2024-09-14 20:02:27 -0600

    LibWeb: Implement StorageKey and related AOs from Storage specification
    (cherry picked from commit f0270b92f10f65951a437419d1b372385bfaa5f8)

---
+++ Userland/Libraries/LibWeb/StorageAPI/StorageKey.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2024, Andrew Kaster <andrew@ladybird.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/Optional.h>
+#include <LibWeb/Forward.h>
+#include <LibWeb/HTML/Origin.h>
+
+namespace Web::StorageAPI {
+
+// https://storage.spec.whatwg.org/#storage-keys
+struct StorageKey {
+
+    // A storage key is a tuple consisting of an origin (an origin). [HTML]
+    // NOTE: This is expected to change; see Client-Side Storage Partitioning https://privacycg.github.io/storage-partitioning/.
+    HTML::Origin origin;
+
+    friend bool operator==(StorageKey const& a, StorageKey const& b)
+    {
+        // To determine whether a storage key A equals storage key B, run these steps:
+        // 1. If A’s origin is not same origin with B’s origin, then return false.
+        // 2. Return true.
+        return a.origin.is_same_origin(b.origin);
+    }
+};
+
+Optional<StorageKey> obtain_a_storage_key(HTML::Environment const&);
+StorageKey obtain_a_storage_key_for_non_storage_purposes(HTML::Environment const&);
+
+}
