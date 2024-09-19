912aecc7190c032ab5562d3d3de060f958ebc408
Sam Atkins <sam@ladybird.org>
2024-09-19 12:58:38 +0100

    LibWeb/CSS: Extract interpolation code into its own files
    Apart from shrinking StyleComputer a little, we need the ability to get
    the current value of a transition from outside of it.
    
    (cherry picked from commit 70d99db9924b068d2a20dd64fec2bd71f7d7b346)

---
+++ Userland/Libraries/LibWeb/CSS/Interpolation.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2024, Sam Atkins <sam@ladybird.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibWeb/CSS/CSSStyleValue.h>
+#include <LibWeb/Forward.h>
+
+namespace Web::CSS {
+
+ValueComparingRefPtr<CSSStyleValue const> interpolate_property(DOM::Element&, PropertyID, CSSStyleValue const& from, CSSStyleValue const& to, float delta);
+
+NonnullRefPtr<CSSStyleValue const> interpolate_value(DOM::Element&, CSSStyleValue const& from, CSSStyleValue const& to, float delta);
+NonnullRefPtr<CSSStyleValue const> interpolate_box_shadow(DOM::Element&, CSSStyleValue const& from, CSSStyleValue const& to, float delta);
+RefPtr<CSSStyleValue const> interpolate_transform(DOM::Element&, CSSStyleValue const& from, CSSStyleValue const& to, float delta);
+
+Color interpolate_color(Color from, Color to, float delta);
+
+}
