6dca4b047782f360fbf6b2812ac91493e3bd6087
Nico Weber <thakis@chromium.org>
2024-09-24 22:21:24 -0400

    LibWeb: Hook up canvas lineCap and plumb it to LibGfx
    To rebaseline image test expecatations, I ran:
    
        out/gn/Ladybird.app/Contents/MacOS/headless-browser \
            --resources $PWD/out/gn/Ladybird.app/Contents/Resources \
            --dump-failed-ref-tests \
            --run-tests $PWD/Tests/LibWeb \
            --filter 'canvas-*'
    
    I then copied over the new baselines with
    
        D=Tests/LibWeb/Ref/reference/images
        cp test-dumps/canvas-implict-moves-and-lines.png \
            $D/canvas-implict-moves-and-lines-ref.png
    
    (Note: No `-ref` suffix on first path, yes suffix on second path.)
    
    We currently don't track if a path is open or closed, and paint
    butt linecaps at the end of closed paths too. We did that with
    round linecaps as well, but there that wasn't visible. This makes
    closed paths look a bit weird now; we'll have to fix this in a
    follow-up. In a way, this just exposes another not-yet-implemented
    feature.

--- Userland/Libraries/LibGfx/AntiAliasingPainter.h
+++ Userland/Libraries/LibGfx/AntiAliasingPainter.h
@@ -37,7 +37,7 @@ public:
     void fill_path(Path const&, Color, Painter::WindingRule rule = Painter::WindingRule::Nonzero);
     void fill_path(Path const&, PaintStyle const& paint_style, float opacity = 1.0f, Painter::WindingRule rule = Painter::WindingRule::Nonzero);
 
-    void stroke_path(Path const&, Color, float thickness);
+    void stroke_path(Path const&, Color, float thickness, Path::CapStyle cap_style = Path::CapStyle::Round);
     void stroke_path(Path const&, PaintStyle const& paint_style, float thickness, float opacity = 1.0f, Path::CapStyle cap_style = Path::CapStyle::Round);
 
     void translate(float dx, float dy) { m_transform.translate(dx, dy); }
