SerenityOS/serenity

commit ad73adef5d2f8e198553441657c544b278605e8a

Kernel/Net: Rename IPv4-specific files and structs

Kernel/Net/ICMP.h
@@ -7,9 +7,9 @@
 #pragma once
 
 #include <AK/MACAddress.h>
-#include <Kernel/Net/IPv4.h>
+#include <Kernel/Net/IPv4/IPv4.h>
 
-struct ICMPType {
+struct ICMPv4Type {
     enum {
         EchoReply = 0,
         EchoRequest = 8,

Kernel/Net/NetworkTask.cpp
@@ -7,12 +7,12 @@
 #include <Kernel/Debug.h>
 #include <Kernel/Locking/Mutex.h>
 #include <Kernel/Locking/MutexProtected.h>
-#include <Kernel/Net/ARP.h>
 #include <Kernel/Net/EtherType.h>
 #include <Kernel/Net/EthernetFrameHeader.h>
 #include <Kernel/Net/ICMP.h>
-#include <Kernel/Net/IPv4.h>
-#include <Kernel/Net/IPv4Socket.h>
+#include <Kernel/Net/IPv4/ARP.h>
+#include <Kernel/Net/IPv4/IPv4.h>
+#include <Kernel/Net/IPv4/Socket.h>
 #include <Kernel/Net/LoopbackAdapter.h>
 #include <Kernel/Net/NetworkTask.h>
 #include <Kernel/Net/NetworkingManagement.h>
@@ -242,7 +242,7 @@ void handle_icmp(EthernetFrameHeader const& eth, IPv4Packet const& ipv4_packet, 
     if (!adapter)
         return;
 
-    if (icmp_header.type() == ICMPType::EchoRequest) {
+    if (icmp_header.type() == ICMPv4Type::EchoRequest) {
         auto& request = reinterpret_cast<ICMPEchoPacket const&>(icmp_header);
         dbgln("handle_icmp: EchoRequest from {}: id={}, seq={}", ipv4_packet.source(), (u16)request.identifier, (u16)request.sequence_number);
         size_t icmp_packet_size = ipv4_packet.payload_size();
@@ -259,7 +259,7 @@ void handle_icmp(EthernetFrameHeader const& eth, IPv4Packet const& ipv4_packet, 
         adapter->fill_in_ipv4_header(*packet, adapter->ipv4_address(), eth.source(), ipv4_packet.source(), IPv4Protocol::ICMP, icmp_packet_size, 0, 64);
         memset(packet->buffer->data() + ipv4_payload_offset, 0, sizeof(ICMPEchoPacket));
         auto& response = *(ICMPEchoPacket*)(packet->buffer->data() + ipv4_payload_offset);
-        response.header.set_type(ICMPType::EchoReply);
+        response.header.set_type(ICMPv4Type::EchoReply);
         response.header.set_code(0);
         response.identifier = request.identifier;
         response.sequence_number = request.sequence_number;

Kernel/CMakeLists.txt
@@ -273,7 +273,7 @@ set(KERNEL_SOURCES
     Net/Intel/E1000NetworkAdapter.cpp
     Net/Realtek/RTL8168NetworkAdapter.cpp
     Net/VirtIO/VirtIONetworkAdapter.cpp
-    Net/IPv4Socket.cpp
+    Net/IPv4/Socket.cpp
     Net/LocalSocket.cpp
     Net/LoopbackAdapter.cpp
     Net/NetworkAdapter.cpp

Kernel/FileSystem/SysFS/Subsystems/Kernel/Network/ARP.cpp
@@ -6,7 +6,7 @@
 
 #include <AK/JsonObjectSerializer.h>
 #include <Kernel/FileSystem/SysFS/Subsystems/Kernel/Network/ARP.h>
-#include <Kernel/Net/ARP.h>
+#include <Kernel/Net/IPv4/ARP.h>
 #include <Kernel/Net/Routing.h>
 #include <Kernel/Sections.h>
 

Kernel/Net/IPv4/ARP.h
@@ -9,7 +9,7 @@
 #include <AK/Endian.h>
 #include <AK/MACAddress.h>
 #include <Kernel/Net/EtherType.h>
-#include <Kernel/Net/IPv4.h>
+#include <Kernel/Net/IPv4/IPv4.h>
 
 namespace Kernel {
 

Kernel/Net/IPv4/Socket.cpp
@@ -11,10 +11,10 @@
 #include <Kernel/API/POSIX/errno.h>
 #include <Kernel/Debug.h>
 #include <Kernel/FileSystem/OpenFileDescription.h>
-#include <Kernel/Net/ARP.h>
 #include <Kernel/Net/ICMP.h>
-#include <Kernel/Net/IPv4.h>
-#include <Kernel/Net/IPv4Socket.h>
+#include <Kernel/Net/IPv4/ARP.h>
+#include <Kernel/Net/IPv4/IPv4.h>
+#include <Kernel/Net/IPv4/Socket.h>
 #include <Kernel/Net/NetworkAdapter.h>
 #include <Kernel/Net/NetworkingManagement.h>
 #include <Kernel/Net/Routing.h>

Kernel/Net/IPv4/Socket.h
@@ -12,8 +12,8 @@
 #include <Kernel/Library/DoubleBuffer.h>
 #include <Kernel/Library/KBuffer.h>
 #include <Kernel/Locking/MutexProtected.h>
-#include <Kernel/Net/IPv4.h>
-#include <Kernel/Net/IPv4SocketTuple.h>
+#include <Kernel/Net/IPv4/IPv4.h>
+#include <Kernel/Net/IPv4/SocketTuple.h>
 #include <Kernel/Net/Socket.h>
 
 namespace Kernel {

commit 535fc18023a8399c07c5ae642881ba95394fcac2

Kernel/Net: Clean up the ICMP handler
This change modernizes various aspects of handle_icmp.

A few dbgln's got turned into dbgln_if's, in accordance with the
behavior throughout the rest of the file.

Furthermore, we revert a part of commit ad73adef5d2f8 which introduced
inconsistency between struct names. There's no ICMPv4, only ICMP and
ICMPv6. Thus, it makes more sense to revert the ICMPv4Type back to
ICMPType.

We also remove the FIXME about the TTL - as per RFC1700, TTL of 64 is
a recommended default, so the current behavior can stay.

Kernel/Net/ICMP.h
@@ -9,35 +9,15 @@
 #include <AK/MACAddress.h>
 #include <Kernel/Net/IPv4/IPv4.h>
 
-struct ICMPv4Type {
-    enum {
-        EchoReply = 0,
-        EchoRequest = 8,
-    };
+enum class ICMPType : u8 {
+    EchoReply = 0,
+    EchoRequest = 8,
 };
 
-class [[gnu::packed]] ICMPHeader {
-public:
-    ICMPHeader() = default;
-    ~ICMPHeader() = default;
-
-    u8 type() const { return m_type; }
-    void set_type(u8 b) { m_type = b; }
-
-    u8 code() const { return m_code; }
-    void set_code(u8 b) { m_code = b; }
-
-    u16 checksum() const { return m_checksum; }
-    void set_checksum(u16 w) { m_checksum = w; }
-
-    void const* payload() const { return this + 1; }
-    void* payload() { return this + 1; }
-
-private:
-    u8 m_type { 0 };
-    u8 m_code { 0 };
-    NetworkOrdered<u16> m_checksum { 0 };
-    // NOTE: The rest of the header is 4 bytes
+struct [[gnu::packed]] ICMPHeader {
+    ICMPType type { 0 };
+    u8 code { 0 };
+    NetworkOrdered<u16> checksum { 0 };
 };
 
 static_assert(AssertSize<ICMPHeader, 4>());
@@ -46,6 +26,5 @@ struct [[gnu::packed]] ICMPEchoPacket {
     ICMPHeader header;
     NetworkOrdered<u16> identifier;
     NetworkOrdered<u16> sequence_number;
-    void* payload() { return this + 1; }
-    void const* payload() const { return this + 1; }
+    u8 payload[];
 };

Kernel/Net/NetworkTask.cpp
@@ -224,7 +224,9 @@ void handle_ipv4(EthernetFrameHeader const& eth, size_t frame_size, UnixDateTime
 void handle_icmp(EthernetFrameHeader const& eth, IPv4Packet const& ipv4_packet, UnixDateTime const& packet_timestamp)
 {
     auto& icmp_header = *static_cast<ICMPHeader const*>(ipv4_packet.payload());
-    dbgln_if(ICMP_DEBUG, "handle_icmp: source={}, destination={}, type={:#02x}, code={:#02x}", ipv4_packet.source().to_string(), ipv4_packet.destination().to_string(), icmp_header.type(), icmp_header.code());
+    size_t const icmp_packet_size = ipv4_packet.payload_size();
+    size_t const icmp_header_size = sizeof(ICMPEchoPacket);
+    dbgln_if(ICMP_DEBUG, "handle_icmp: source={}, destination={}, type={:#02x}, code={:#02x}", ipv4_packet.source().to_string(), ipv4_packet.destination().to_string(), static_cast<u8>(icmp_header.type), icmp_header.code);
 
     {
         Vector<NonnullRefPtr<IPv4Socket>> icmp_sockets;
@@ -235,38 +237,42 @@ void handle_icmp(EthernetFrameHeader const& eth, IPv4Packet const& ipv4_packet, 
             }
         });
         for (auto& socket : icmp_sockets)
-            socket->did_receive(ipv4_packet.source(), 0, { &ipv4_packet, sizeof(IPv4Packet) + ipv4_packet.payload_size() }, packet_timestamp);
+            socket->did_receive(ipv4_packet.source(), 0, { &ipv4_packet, sizeof(IPv4Packet) + icmp_packet_size }, packet_timestamp);
     }
 
     auto adapter = NetworkingManagement::the().from_ipv4_address(ipv4_packet.destination());
     if (!adapter)
         return;
 
-    if (icmp_header.type() == ICMPv4Type::EchoRequest) {
+    if (icmp_header.type == ICMPType::EchoRequest) {
         auto& request = reinterpret_cast<ICMPEchoPacket const&>(icmp_header);
-        dbgln("handle_icmp: EchoRequest from {}: id={}, seq={}", ipv4_packet.source(), (u16)request.identifier, (u16)request.sequence_number);
-        size_t icmp_packet_size = ipv4_packet.payload_size();
-        if (icmp_packet_size < sizeof(ICMPEchoPacket)) {
-            dbgln("handle_icmp: EchoRequest packet is too small, ignoring.");
+
+        dbgln_if(ICMP_DEBUG, "handle_icmp: EchoRequest from {}: id={}, seq={}", ipv4_packet.source(), request.identifier, request.sequence_number);
+        if (icmp_packet_size < icmp_header_size) {
+            dbgln_if(ICMP_DEBUG, "handle_icmp: EchoRequest packet is too small, ignoring.");
             return;
         }
+
         auto ipv4_payload_offset = adapter->ipv4_payload_offset();
         auto packet = adapter->acquire_packet_buffer(ipv4_payload_offset + icmp_packet_size);
         if (!packet) {
             dbgln("Could not allocate packet buffer while sending ICMP packet");
             return;
         }
+
         adapter->fill_in_ipv4_header(*packet, adapter->ipv4_address(), eth.source(), ipv4_packet.source(), IPv4Protocol::ICMP, icmp_packet_size, 0, 64);
-        memset(packet->buffer->data() + ipv4_payload_offset, 0, sizeof(ICMPEchoPacket));
-        auto& response = *(ICMPEchoPacket*)(packet->buffer->data() + ipv4_payload_offset);
-        response.header.set_type(ICMPv4Type::EchoReply);
-        response.header.set_code(0);
+        memset(packet->buffer->data() + ipv4_payload_offset, 0, icmp_header_size);
+
+        auto& response = *reinterpret_cast<ICMPEchoPacket*>(packet->buffer->data() + ipv4_payload_offset);
+        response.header.type = ICMPType::EchoReply;
         response.identifier = request.identifier;
         response.sequence_number = request.sequence_number;
-        if (size_t icmp_payload_size = icmp_packet_size - sizeof(ICMPEchoPacket))
-            memcpy(response.payload(), request.payload(), icmp_payload_size);
-        response.header.set_checksum(internet_checksum(&response, icmp_packet_size));
-        // FIXME: What is the right TTL value here? Is 64 ok? Should we use the same TTL as the echo request?
+
+        if (icmp_packet_size > icmp_header_size)
+            memcpy(response.payload, request.payload, icmp_packet_size - icmp_header_size);
+
+        response.header.checksum = internet_checksum(&response, icmp_packet_size);
+
         adapter->send_packet(packet->bytes());
         adapter->release_packet_buffer(*packet);
     }