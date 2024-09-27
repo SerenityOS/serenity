9327500cb672d622a2a4d5d9521cbb21aaf5e030
famfo <famfo@famfo.xyz>
2024-09-27 23:54:46 +0200

    fixup! IPv4AddressCidr: use TRY_OR_FAIL, use C++ casts

--- Tests/AK/TestIPv4AddressCidr.cpp
+++ Tests/AK/TestIPv4AddressCidr.cpp
@@ -13,11 +13,9 @@ TEST_CASE(sanity_check)
 {
     auto address_result = IPv4AddressCidr::create(IPv4Address(192, 0, 2, 1), 32);
 
-    EXPECT(!address_result.is_error());
+    IPv4AddressCidr address = TRY_OR_FAIL(address_result);
 
-    IPv4AddressCidr address = address_result.release_value();
-
-    EXPECT_EQ(address.length(), (u32)32);
+    EXPECT_EQ(address.length(), 32l);
     EXPECT_EQ(address.ip_address(), IPv4Address(192, 0, 2, 1));
     EXPECT_EQ(address.first_address_of_subnet(), IPv4Address(192, 0, 2, 1));
     EXPECT_EQ(address.last_address_of_subnet(), IPv4Address(192, 0, 2, 1));
@@ -29,7 +27,7 @@ TEST_CASE(should_fail_on_invalid_length)
 {
     auto address_result = IPv4AddressCidr::create(IPv4Address(192, 0, 2, 1), 33);
     EXPECT(address_result.is_error());
-    EXPECT_EQ(address_result.error(), IPv4AddressCidr::IPAddressCidrError::CidrTooLong);
+    EXPECT_EQ(address_result.error(), AK::Details::IPAddressCidrError::CidrTooLong);
 }
 
 TEST_CASE(should_find_first_in_subnet)
@@ -44,7 +42,7 @@ TEST_CASE(should_find_last_in_subnet)
     EXPECT_EQ(address.last_address_of_subnet(), IPv4Address(192, 0, 2, 255));
 }
 
-TEST_CASE(should_return_matching_netask)
+TEST_CASE(should_return_matching_netmask)
 {
     IPv4AddressCidr address = IPv4AddressCidr::create(IPv4Address(192, 0, 2, 1), 24).release_value();
     EXPECT_EQ(address.netmask(), IPv4Address(255, 255, 255, 0));
@@ -68,10 +66,10 @@ TEST_CASE(should_set_address)
 TEST_CASE(should_set_length)
 {
     IPv4AddressCidr address = IPv4AddressCidr::create(IPv4Address(192, 0, 2, 1), 32).release_value();
-    EXPECT_EQ(address.length(), (u32)32);
+    EXPECT_EQ(address.length(), 32l);
 
     EXPECT(!address.set_length(24).is_error());
-    EXPECT_EQ(address.length(), (u32)24);
+    EXPECT_EQ(address.length(), 24l);
 }
 
 TEST_CASE(should_not_set_invalid_length)
@@ -103,21 +101,21 @@ TEST_CASE(should_not_parse_invalid_address)
 {
     auto address = IPv4AddressCidr::from_string("256.0.0.1/24"sv);
     EXPECT(address.is_error());
-    EXPECT_EQ(address.error(), IPv4AddressCidr::IPAddressCidrError::StringParsingFailed);
+    EXPECT_EQ(address.error(), AK::Details::IPAddressCidrError::StringParsingFailed);
 }
 
 TEST_CASE(should_not_parse_invalid_length)
 {
     auto address = IPv4AddressCidr::from_string("192.0.2.1/33"sv);
     EXPECT(address.is_error());
-    EXPECT_EQ(address.error(), IPv4AddressCidr::IPAddressCidrError::CidrTooLong);
+    EXPECT_EQ(address.error(), AK::Details::IPAddressCidrError::CidrTooLong);
 }
 
 TEST_CASE(should_not_parse_invalid_cidr_format)
 {
     auto address = IPv4AddressCidr::from_string("192.0.2.1"sv);
     EXPECT(address.is_error());
-    EXPECT_EQ(address.error(), IPv4AddressCidr::IPAddressCidrError::StringParsingFailed);
+    EXPECT_EQ(address.error(), AK::Details::IPAddressCidrError::StringParsingFailed);
 }
 
 TEST_CASE(should_format_cidr)
