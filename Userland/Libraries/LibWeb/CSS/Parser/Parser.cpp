d76b7744fdff10439892db24f37a60fc414c3a3b
Sam Atkins <sam@ladybird.org>
2024-09-27 12:54:08 +0100

    LibWeb/CSS: Implement legacy name aliases for properties
    When a property is a "legacy name alias", any time it is used in CSS or
    via the CSSOM its aliased name is used instead.
    (See https://drafts.csswg.org/css-cascade-5/#legacy-name-alias)
    
    This means we only care about the alias when parsing a string as a
    PropertyID - and we can just return the PropertyID it is an alias for.
    No need for a distinct PropertyID for it, and no need for LibWeb to
    care about it at all.
    
    Previously, we had a bunch of these properties, which misused our code
    for "logical aliases", some of which I've discovered were not even
    fully implemented. But with this change, all that code can go away, and
    making a legacy alias is just a case of putting it in the JSON. This
    also shrinks `StyleProperties` as it doesn't need to contain data for
    these aliases, and removes a whole load of `-webkit-*` spam from the
    style inspector.
    
    (cherry picked from commit fdcece2e88b91b9ec6cf63c3466525fb77540316;
    amended to:
    * resolve a conflict on height: in getComputedStyle-print-all.txt
    * run prettier on CSSGeneratedFiles.md)

--- Userland/Libraries/LibWeb/CSS/Parser/Parser.cpp
+++ Userland/Libraries/LibWeb/CSS/Parser/Parser.cpp
@@ -7214,8 +7214,6 @@ Parser::ParseErrorOr<NonnullRefPtr<CSSStyleValue>> Parser::parse_css_value(Prope
     case PropertyID::BackgroundClip:
     case PropertyID::BackgroundImage:
     case PropertyID::BackgroundOrigin:
-    case PropertyID::WebkitBackgroundClip:
-    case PropertyID::WebkitBackgroundOrigin:
         if (auto parsed_value = parse_simple_comma_separated_value_list(property_id, tokens))
             return parsed_value.release_nonnull();
         return ParseError::SyntaxError;
@@ -7248,20 +7246,14 @@ Parser::ParseErrorOr<NonnullRefPtr<CSSStyleValue>> Parser::parse_css_value(Prope
     case PropertyID::BorderTopRightRadius:
     case PropertyID::BorderBottomRightRadius:
     case PropertyID::BorderBottomLeftRadius:
-    case PropertyID::WebkitBorderTopLeftRadius:
-    case PropertyID::WebkitBorderTopRightRadius:
-    case PropertyID::WebkitBorderBottomRightRadius:
-    case PropertyID::WebkitBorderBottomLeftRadius:
         if (auto parsed_value = parse_border_radius_value(tokens); parsed_value && !tokens.has_next_token())
             return parsed_value.release_nonnull();
         return ParseError::SyntaxError;
     case PropertyID::BorderRadius:
-    case PropertyID::WebkitBorderRadius:
         if (auto parsed_value = parse_border_radius_shorthand_value(tokens); parsed_value && !tokens.has_next_token())
             return parsed_value.release_nonnull();
         return ParseError::SyntaxError;
     case PropertyID::BoxShadow:
-    case PropertyID::WebkitBoxShadow:
         if (auto parsed_value = parse_shadow_value(tokens, AllowInsetKeyword::Yes); parsed_value && !tokens.has_next_token())
             return parsed_value.release_nonnull();
         return ParseError::SyntaxError;
@@ -7290,12 +7282,10 @@ Parser::ParseErrorOr<NonnullRefPtr<CSSStyleValue>> Parser::parse_css_value(Prope
             return parsed_value.release_nonnull();
         return ParseError::SyntaxError;
     case PropertyID::Flex:
-    case PropertyID::WebkitFlex:
         if (auto parsed_value = parse_flex_shorthand_value(tokens); parsed_value && !tokens.has_next_token())
             return parsed_value.release_nonnull();
         return ParseError::SyntaxError;
     case PropertyID::FlexFlow:
-    case PropertyID::WebkitFlexFlow:
         if (auto parsed_value = parse_flex_flow_value(tokens); parsed_value && !tokens.has_next_token())
             return parsed_value.release_nonnull();
         return ParseError::SyntaxError;
@@ -7412,17 +7402,14 @@ Parser::ParseErrorOr<NonnullRefPtr<CSSStyleValue>> Parser::parse_css_value(Prope
             return parsed_value.release_nonnull();
         return ParseError::SyntaxError;
     case PropertyID::Transform:
-    case PropertyID::WebkitTransform:
         if (auto parsed_value = parse_transform_value(tokens); parsed_value && !tokens.has_next_token())
             return parsed_value.release_nonnull();
         return ParseError::SyntaxError;
     case PropertyID::TransformOrigin:
-    case PropertyID::WebkitTransformOrigin:
         if (auto parsed_value = parse_transform_origin_value(tokens); parsed_value && !tokens.has_next_token())
             return parsed_value.release_nonnull();
         return ParseError::SyntaxError;
     case PropertyID::Transition:
-    case PropertyID::WebkitTransition:
         if (auto parsed_value = parse_transition_value(tokens); parsed_value && !tokens.has_next_token())
             return parsed_value.release_nonnull();
         return ParseError::SyntaxError;
