c5106e15319f2fe50d093bf2b2b4b303ec73af41
Timothy Flynn <trflynn89@pm.me>
2024-09-27 10:19:23 -0400

    LibWeb: Add helpers to convert a MouseButton to/from its DOM event code
    Move the helper to convert MouseButton to its DOM event code to a public
    header. And add a helper to convert in the opposite direction.
    
    (cherry picked from commit 5e99715377cad26174344b9cca3e3cb8a7eb93ad)

--- Userland/Libraries/LibWeb/UIEvents/MouseButton.h
+++ Userland/Libraries/LibWeb/UIEvents/MouseButton.h
@@ -6,7 +6,9 @@
 
 #pragma once
 
+#include <AK/Assertions.h>
 #include <AK/EnumBits.h>
+#include <AK/Types.h>
 
 namespace Web::UIEvents {
 
@@ -20,5 +22,40 @@ enum MouseButton : u8 {
 };
 
 AK_ENUM_BITWISE_OPERATORS(MouseButton);
+
+// https://www.w3.org/TR/uievents/#dom-mouseevent-button
+constexpr i16 mouse_button_to_button_code(MouseButton button)
+{
+    switch (button) {
+    case MouseButton::Primary:
+        return 0;
+    case MouseButton::Middle:
+        return 1;
+    case MouseButton::Secondary:
+        return 2;
+    case MouseButton::Backward:
+        return 3;
+    case MouseButton::Forward:
+        return 4;
+    default:
+        VERIFY_NOT_REACHED();
+    }
+}
+
+// https://www.w3.org/TR/uievents/#dom-mouseevent-button
+constexpr MouseButton button_code_to_mouse_button(i16 button)
+{
+    if (button == 0)
+        return MouseButton::Primary;
+    if (button == 1)
+        return MouseButton::Middle;
+    if (button == 2)
+        return MouseButton::Secondary;
+    if (button == 3)
+        return MouseButton::Backward;
+    if (button == 4)
+        return MouseButton::Forward;
+    return MouseButton::None;
+}
 
 }
