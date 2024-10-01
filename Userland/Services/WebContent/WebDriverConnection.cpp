b956c848be0242121825f86de343bb38e32eaf3d
Timothy Flynn <trflynn89@pm.me>
2024-10-01 13:48:52 -0400

    WebContent: Do not wait for navigations on BCs that have no navigable
    This avoids a crash seen in WPT when a click event is fired on a BC that
    has been removed from the document.
    
    (cherry picked from commit 95bf6c98779bbb879b19319884389148e8cd2fd2)

--- Userland/Services/WebContent/WebDriverConnection.cpp
+++ Userland/Services/WebContent/WebDriverConnection.cpp
@@ -2225,6 +2225,10 @@ ErrorOr<void, Web::WebDriver::Error> WebDriverConnection::wait_for_navigation_to
     if (ensure_browsing_context_is_open(current_browsing_context()).is_error())
         return {};
 
+    auto navigable = current_browsing_context().active_document()->navigable();
+    if (!navigable || navigable->ongoing_navigation().has<Empty>())
+        return {};
+
     // 3. Start a timer. If this algorithm has not completed before timer reaches the session’s session page load timeout in milliseconds, return an error with error code timeout.
     auto page_load_timeout_fired = false;
     auto timer = Core::Timer::create_single_shot(m_timeouts_configuration.page_load_timeout, [&] {
@@ -2234,7 +2238,7 @@ ErrorOr<void, Web::WebDriver::Error> WebDriverConnection::wait_for_navigation_to
 
     // 4. If there is an ongoing attempt to navigate the current browsing context that has not yet matured, wait for navigation to mature.
     Web::Platform::EventLoopPlugin::the().spin_until([&] {
-        return page_load_timeout_fired || current_browsing_context().top_level_traversable()->ongoing_navigation() == Empty {};
+        return page_load_timeout_fired || navigable->ongoing_navigation().has<Empty>();
     });
 
     // 5. Let readiness target be the document readiness state associated with the current session’s page loading strategy, which can be found in the table of page load strategies.
