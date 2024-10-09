LadybirdBrowser/ladybird

Everywhere: Limit layout text fragments to use one font for all glyphs
The ChunkIterator now limits a chunk to using only one font (before, it
was possible to have a chunk with >1 font, when `unicode-range` CSS
property is used).

This change allows us to reduce some complexity in the text shaping and
painting code and makes us compatible with the APIs in Skia and
HarfBuzz.

Userland/Libraries/LibWeb/Painting/BackgroundPainting.cpp
@@ -62,11 +62,15 @@ static Vector<Gfx::Path> compute_text_clip_paths(PaintContext& context, Paintabl
 {
     Vector<Gfx::Path> text_clip_paths;
     auto add_text_clip_path = [&](PaintableFragment const& fragment) {
+        auto glyph_run = fragment.glyph_run();
+        if (!glyph_run || glyph_run->glyphs().is_empty())
+            return;
         // Scale to the device pixels.
         Gfx::Path glyph_run_path;
-        for (auto glyph : fragment.glyph_run().glyphs()) {
+        auto const& font = fragment.glyph_run()->font();
+        auto scaled_font = font.with_size(font.point_size() * static_cast<float>(context.device_pixels_per_css_pixel()));
+        for (auto glyph : fragment.glyph_run()->glyphs()) {
             glyph.visit([&](auto& glyph) {
-                glyph.font = glyph.font->with_size(glyph.font->point_size() * static_cast<float>(context.device_pixels_per_css_pixel()));
                 glyph.position = glyph.position.scaled(context.device_pixels_per_css_pixel());
             });
 
@@ -75,13 +79,12 @@ static Vector<Gfx::Path> compute_text_clip_paths(PaintContext& context, Paintabl
 
                 // Get the path for the glyph.
                 Gfx::Path glyph_path;
-                auto const& scaled_font = static_cast<Gfx::ScaledFont const&>(*draw_glyph.font);
-                auto glyph_id = scaled_font.glyph_id_for_code_point(draw_glyph.code_point);
-                scaled_font.append_glyph_path_to(glyph_path, glyph_id);
+                auto glyph_id = scaled_font->glyph_id_for_code_point(draw_glyph.code_point);
+                scaled_font->append_glyph_path_to(glyph_path, glyph_id);
 
                 // Transform the path to the fragment's position.
                 // FIXME: Record glyphs and use Painter::draw_glyphs() instead to avoid this duplicated code.
-                auto top_left = draw_glyph.position + Gfx::FloatPoint(scaled_font.glyph_left_bearing(draw_glyph.code_point), 0);
+                auto top_left = draw_glyph.position + Gfx::FloatPoint(scaled_font->glyph_left_bearing(draw_glyph.code_point), 0);
                 auto glyph_position = Gfx::GlyphRasterPosition::get_nearest_fit_for(top_left);
                 auto transform = Gfx::AffineTransform {}.translate(glyph_position.blit_position.to_type<float>());
                 glyph_run_path.append_path(glyph_path.copy_transformed(transform));

Userland/Libraries/LibWeb/Painting/PaintableBox.cpp
@@ -663,16 +663,20 @@ void paint_text_fragment(PaintContext& context, TextPaintable const& paintable, 
 
         auto text = paintable.text_for_rendering();
 
+        auto glyph_run = fragment.glyph_run();
+        if (!glyph_run)
+            return;
+
         DevicePixelPoint baseline_start { fragment_absolute_device_rect.x(), fragment_absolute_device_rect.y() + context.rounded_device_pixels(fragment.baseline()) };
         auto scale = context.device_pixels_per_css_pixel();
-        painter.draw_text_run(baseline_start.to_type<int>(), fragment.glyph_run(), paintable.computed_values().color(), fragment_absolute_device_rect.to_type<int>(), scale);
+        painter.draw_text_run(baseline_start.to_type<int>(), *glyph_run, paintable.computed_values().color(), fragment_absolute_device_rect.to_type<int>(), scale);
 
         auto selection_rect = context.enclosing_device_rect(fragment.selection_rect(paintable.layout_node().first_available_font())).to_type<int>();
         if (!selection_rect.is_empty()) {
             painter.fill_rect(selection_rect, CSS::SystemColor::highlight());
             DisplayListRecorderStateSaver saver(painter);
             painter.add_clip_rect(selection_rect);
-            painter.draw_text_run(baseline_start.to_type<int>(), fragment.glyph_run(), CSS::SystemColor::highlight_text(), fragment_absolute_device_rect.to_type<int>(), scale);
+            painter.draw_text_run(baseline_start.to_type<int>(), *glyph_run, CSS::SystemColor::highlight_text(), fragment_absolute_device_rect.to_type<int>(), scale);
         }
 
         paint_text_decoration(context, paintable, fragment);

SerenityOS/serenity

(cherry picked from commit 7181c3f2ea5fba73e77d98acbf9e46640b4a9015,
minorly amended to fix conflicts caused by:
* Our VectorFont not being renamed to Typeface
* Us cherry-picking https://github.com/LadybirdBrowser/ladybird/pull/502
  first
* Us still having bitmap fonts, and hence needing glyph_spacing()
Also amended for:
* AffineDisplayListPlayerCPU changes
* Removing pure virtuals for glyph_id_for_code_point and
  glyph_id_for_code_point in Font.h again since we still have BitmapFont
  which can't implement them
* Updating more Painter methods that we still had
  (Painter::draw_glyph_or_emoji(), Painter::draw_text_run())
)

Userland/Libraries/LibWeb/Painting/BackgroundPainting.cpp
@@ -62,11 +62,16 @@ static Vector<Gfx::Path> compute_text_clip_paths(PaintContext& context, Paintabl
 {
     Vector<Gfx::Path> text_clip_paths;
     auto add_text_clip_path = [&](PaintableFragment const& fragment) {
+        auto glyph_run = fragment.glyph_run();
+        if (!glyph_run || glyph_run->glyphs().is_empty())
+            return;
         // Scale to the device pixels.
         Gfx::Path glyph_run_path;
-        for (auto glyph : fragment.glyph_run().glyphs()) {
+        auto const& font = fragment.glyph_run()->font();
+        auto resized_font = font.with_size(font.point_size() * static_cast<float>(context.device_pixels_per_css_pixel()));
+        auto const* scaled_font = static_cast<Gfx::ScaledFont const*>(resized_font.ptr());
+        for (auto glyph : fragment.glyph_run()->glyphs()) {
             glyph.visit([&](auto& glyph) {
-                glyph.font = glyph.font->with_size(glyph.font->point_size() * static_cast<float>(context.device_pixels_per_css_pixel()));
                 glyph.position = glyph.position.scaled(context.device_pixels_per_css_pixel());
             });
 
@@ -75,13 +80,12 @@ static Vector<Gfx::Path> compute_text_clip_paths(PaintContext& context, Paintabl
 
                 // Get the path for the glyph.
                 Gfx::Path glyph_path;
-                auto const& scaled_font = static_cast<Gfx::ScaledFont const&>(*draw_glyph.font);
-                auto glyph_id = scaled_font.glyph_id_for_code_point(draw_glyph.code_point);
-                scaled_font.append_glyph_path_to(glyph_path, glyph_id);
+                auto glyph_id = scaled_font->glyph_id_for_code_point(draw_glyph.code_point);
+                scaled_font->append_glyph_path_to(glyph_path, glyph_id);
 
                 // Transform the path to the fragment's position.
                 // FIXME: Record glyphs and use Painter::draw_glyphs() instead to avoid this duplicated code.
-                auto top_left = draw_glyph.position + Gfx::FloatPoint(scaled_font.glyph_left_bearing(draw_glyph.code_point), 0);
+                auto top_left = draw_glyph.position + Gfx::FloatPoint(scaled_font->glyph_left_bearing(draw_glyph.code_point), 0);
                 auto glyph_position = Gfx::GlyphRasterPosition::get_nearest_fit_for(top_left);
                 auto transform = Gfx::AffineTransform {}.translate(glyph_position.blit_position.to_type<float>());
                 glyph_run_path.append_path(glyph_path.copy_transformed(transform));

Userland/Libraries/LibWeb/Painting/PaintableBox.cpp
@@ -673,16 +673,20 @@ void paint_text_fragment(PaintContext& context, TextPaintable const& paintable, 
 
         auto text = paintable.text_for_rendering();
 
+        auto glyph_run = fragment.glyph_run();
+        if (!glyph_run)
+            return;
+
         DevicePixelPoint baseline_start { fragment_absolute_device_rect.x(), fragment_absolute_device_rect.y() + context.rounded_device_pixels(fragment.baseline()) };
         auto scale = context.device_pixels_per_css_pixel();
-        painter.draw_text_run(baseline_start.to_type<int>(), fragment.glyph_run(), paintable.computed_values().webkit_text_fill_color(), fragment_absolute_device_rect.to_type<int>(), scale);
+        painter.draw_text_run(baseline_start.to_type<int>(), *glyph_run, paintable.computed_values().webkit_text_fill_color(), fragment_absolute_device_rect.to_type<int>(), scale);
 
         auto selection_rect = context.enclosing_device_rect(fragment.selection_rect(paintable.layout_node().first_available_font())).to_type<int>();
         if (!selection_rect.is_empty()) {
             painter.fill_rect(selection_rect, CSS::SystemColor::highlight());
             DisplayListRecorderStateSaver saver(painter);
             painter.add_clip_rect(selection_rect);
-            painter.draw_text_run(baseline_start.to_type<int>(), fragment.glyph_run(), CSS::SystemColor::highlight_text(), fragment_absolute_device_rect.to_type<int>(), scale);
+            painter.draw_text_run(baseline_start.to_type<int>(), *glyph_run, CSS::SystemColor::highlight_text(), fragment_absolute_device_rect.to_type<int>(), scale);
         }
 
         paint_text_decoration(context, paintable, fragment);