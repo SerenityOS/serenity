f681968657b82700f2908a201842d6f948592c12
Ali Mohammad Pur <ali.mpfard@gmail.com>
2024-10-14 15:38:10 +0200

    Toolchain: Update to latest jakt commit

--- Toolchain/BuildJakt.sh
+++ Toolchain/BuildJakt.sh
@@ -90,7 +90,7 @@ buildstep_ninja() {
 
 mkdir -p "$DIR/Tarballs"
 
-JAKT_COMMIT_HASH="d65f014cc54b986f629fe676d914af01d442b9f7"
+JAKT_COMMIT_HASH="1fed928d0abf08188e48fe765ab68e5047c05ec2"
 JAKT_NAME="jakt-${JAKT_COMMIT_HASH}"
 JAKT_TARBALL="${JAKT_NAME}.tar.gz"
 JAKT_GIT_URL="https://github.com/serenityos/jakt"
