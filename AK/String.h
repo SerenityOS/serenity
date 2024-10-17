shannonbooth/ladybird

AK: Add BOM handling to String::from_utf8_with_replacement_character

AK/String.h
@@ -51,8 +51,13 @@ public:
     // Creates a new String from a sequence of UTF-8 encoded code points.
     static ErrorOr<String> from_utf8(StringView);
 
+    enum class WithBOMHandling {
+        Yes,
+        No,
+    };
+
     // Creates a new String using the replacement character for invalid bytes
-    [[nodiscard]] static String from_utf8_with_replacement_character(StringView);
+    [[nodiscard]] static String from_utf8_with_replacement_character(StringView, WithBOMHandling = WithBOMHandling::No);
 
     template<typename T>
     requires(IsOneOf<RemoveCVReference<T>, ByteString, DeprecatedFlyString, FlyString, String>)

SerenityOS/serenity

(cherry picked from commit b3bf5c4ea84bfbd0ba14fdbc3a86e5d54e053702)

AK/String.h
@@ -50,8 +50,13 @@ public:
     // Creates a new String from a sequence of UTF-8 encoded code points.
     static ErrorOr<String> from_utf8(StringView);
 
+    enum class WithBOMHandling {
+        Yes,
+        No,
+    };
+
     // Creates a new String using the replacement character for invalid bytes
-    [[nodiscard]] static String from_utf8_with_replacement_character(StringView);
+    [[nodiscard]] static String from_utf8_with_replacement_character(StringView, WithBOMHandling = WithBOMHandling::Yes);
 
     template<typename T>
     requires(IsOneOf<RemoveCVReference<T>, ByteString, DeprecatedFlyString, FlyString, String>)