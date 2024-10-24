3544236559b6edb0b89d1d1747154baf6141579c
Andreas Kling <andreas@ladybird.org>
2024-10-24 23:02:10 +0200

    LibJS: Cache UTF-16 strings on the VM
    We were already caching UTF-8 and byte strings, so let's add a cache
    for UTF-16 strings as well. This is particularly profitable whenever we
    run regular expressions, since the output of regex execution is a set of
    UTF-16 strings.
    
    Note that this is a weak cache like the other JS string caches, meaning
    that strings are removed from the cache as they are garbage collected.
    
    This avoids billions of PrimitiveString allocations across a run of WPT,
    significantly reducing GC activity.
    
    (cherry picked from commit 206479b2b5fc2641a619eb0d05c1185d869ef844)

--- Userland/Libraries/LibJS/Runtime/PrimitiveString.cpp
+++ Userland/Libraries/LibJS/Runtime/PrimitiveString.cpp
@@ -47,6 +47,8 @@ PrimitiveString::~PrimitiveString()
 {
     if (has_utf8_string())
         vm().string_cache().remove(*m_utf8_string);
+    if (has_utf16_string())
+        vm().utf16_string_cache().remove(*m_utf16_string);
     if (has_byte_string())
         vm().byte_string_cache().remove(*m_byte_string);
 }
@@ -167,7 +169,13 @@ NonnullGCPtr<PrimitiveString> PrimitiveString::create(VM& vm, Utf16String string
             return vm.single_ascii_character_string(static_cast<u8>(code_unit));
     }
 
-    return vm.heap().allocate_without_realm<PrimitiveString>(move(string));
+    auto& string_cache = vm.utf16_string_cache();
+    if (auto it = string_cache.find(string); it != string_cache.end())
+        return *it->value;
+
+    auto new_string = vm.heap().allocate_without_realm<PrimitiveString>(string);
+    string_cache.set(move(string), new_string);
+    return *new_string;
 }
 
 NonnullGCPtr<PrimitiveString> PrimitiveString::create(VM& vm, String string)
