caf46dfb0711812e3e02c5628e56c8e7151bfba4
Linus Groh <mail@linusgroh.de>
2024-10-27 01:37:55 +0100

    Ports: Update libatomic_ops to 7.8.2

--- Ports/libatomic_ops/package.sh
+++ Ports/libatomic_ops/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libatomic_ops'
-version='7.6.14'
+version='7.8.2'
 useconfigure='true'
 use_fresh_config_sub='true'
 files=(
-    "https://www.hboehm.info/gc/gc_source/libatomic_ops-${version}.tar.gz#390f244d424714735b7050d056567615b3b8f29008a663c262fb548f1802d292"
+    "https://github.com/ivmai/libatomic_ops/releases/download/v${version}/libatomic_ops-${version}.tar.gz#d305207fe207f2b3fb5cb4c019da12b44ce3fcbc593dfd5080d867b1a2419b51"
 )
