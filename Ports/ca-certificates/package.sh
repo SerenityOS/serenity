ef1958c2ae99847232940584a0d36e4930ca47d7
Linus Groh <mail@linusgroh.de>
2024-10-27 01:08:10 +0000

    Ports: Update ca-certificates to 2024-09-24

--- Ports/ca-certificates/package.sh
+++ Ports/ca-certificates/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='ca-certificates'
-version='2024-07-02'
+version='2024-09-24'
 files=(
-    "https://curl.se/ca/cacert-${version}.pem#1bf458412568e134a4514f5e170a328d11091e071c7110955c9884ed87972ac9"
+    "https://curl.se/ca/cacert-${version}.pem#189d3cf6d103185fba06d76c1af915263c6d42225481a1759e853b33ac857540"
 )
 workdir='.'
 
