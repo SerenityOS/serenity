12c57e3026ec1b3f908732cfbaf64c36b9cf88ff
Lucas CHOLLET <lucas.chollet@free.fr>
2024-10-27 20:04:18 -0400

    LibWeb/CSS: Introduce a base class for LCH-based color values
    This will be used by both CSSLCH and CSSOKLCH.
    
    (cherry picked from commit e8fc731b8cde75a0af9571a3b491f6149038ea21)

--- Userland/Libraries/LibWeb/CSS/StyleValues/CSSLCHLike.cpp
+++ Userland/Libraries/LibWeb/CSS/StyleValues/CSSLCHLike.cpp
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#include "CSSOKLCH.h"
+#include "CSSLCHLike.h"
 #include <AK/Math.h>
 #include <AK/TypeCasts.h>
 #include <LibWeb/CSS/Serialize.h>
@@ -14,6 +14,17 @@
 
 namespace Web::CSS {
 
+bool CSSLCHLike::equals(CSSStyleValue const& other) const
+{
+    if (type() != other.type())
+        return false;
+    auto const& other_color = other.as_color();
+    if (color_type() != other_color.color_type())
+        return false;
+    auto const& other_oklch_like = verify_cast<CSSLCHLike>(other_color);
+    return m_properties == other_oklch_like.m_properties;
+}
+
 Color CSSOKLCH::to_color(Optional<Layout::NodeWithStyle const&>) const
 {
     auto const l_val = clamp(resolve_with_reference_value(m_properties.l, 1.0).value_or(0), 0, 1);
@@ -22,17 +33,6 @@ Color CSSOKLCH::to_color(Optional<Layout::NodeWithStyle const&>) const
     auto const alpha_val = resolve_alpha(m_properties.alpha).value_or(1);
 
     return Color::from_oklab(l_val, c_val * cos(h_val), c_val * sin(h_val), alpha_val);
-}
-
-bool CSSOKLCH::equals(CSSStyleValue const& other) const
-{
-    if (type() != other.type())
-        return false;
-    auto const& other_color = other.as_color();
-    if (color_type() != other_color.color_type())
-        return false;
-    auto const& other_oklch = verify_cast<CSSOKLCH>(other_color);
-    return m_properties == other_oklch.m_properties;
 }
 
 // https://www.w3.org/TR/css-color-4/#serializing-oklab-oklch
--- Userland/Libraries/LibWeb/CSS/StyleValues/CSSLCHLike.h
+++ Userland/Libraries/LibWeb/CSS/StyleValues/CSSLCHLike.h
@@ -11,33 +11,29 @@
 
 namespace Web::CSS {
 
-// https://drafts.css-houdini.org/css-typed-om-1/#cssoklch
-class CSSOKLCH final : public CSSColorValue {
+class CSSLCHLike : public CSSColorValue {
 public:
+    template<DerivedFrom<CSSLCHLike> T>
     static ValueComparingNonnullRefPtr<CSSOKLCH> create(ValueComparingNonnullRefPtr<CSSStyleValue> l, ValueComparingNonnullRefPtr<CSSStyleValue> c, ValueComparingNonnullRefPtr<CSSStyleValue> h, ValueComparingRefPtr<CSSStyleValue> alpha = {})
     {
         // alpha defaults to 1
         if (!alpha)
-            return adopt_ref(*new (nothrow) CSSOKLCH(move(l), move(c), move(h), NumberStyleValue::create(1)));
+            alpha = NumberStyleValue::create(1);
 
-        return adopt_ref(*new (nothrow) CSSOKLCH(move(l), move(c), move(h), alpha.release_nonnull()));
+        return adopt_ref(*new (nothrow) T({}, move(l), move(c), move(h), alpha.release_nonnull()));
     }
-    virtual ~CSSOKLCH() override = default;
+    virtual ~CSSLCHLike() override = default;
 
     CSSStyleValue const& l() const { return *m_properties.l; }
     CSSStyleValue const& c() const { return *m_properties.c; }
     CSSStyleValue const& h() const { return *m_properties.h; }
     CSSStyleValue const& alpha() const { return *m_properties.alpha; }
 
-    virtual Color to_color(Optional<Layout::NodeWithStyle const&>) const override;
-
-    String to_string() const override;
-
     virtual bool equals(CSSStyleValue const& other) const override;
 
-private:
-    CSSOKLCH(ValueComparingNonnullRefPtr<CSSStyleValue> l, ValueComparingNonnullRefPtr<CSSStyleValue> c, ValueComparingNonnullRefPtr<CSSStyleValue> h, ValueComparingNonnullRefPtr<CSSStyleValue> alpha)
-        : CSSColorValue(ColorType::OKLCH)
+protected:
+    CSSLCHLike(ColorType color_type, ValueComparingNonnullRefPtr<CSSStyleValue> l, ValueComparingNonnullRefPtr<CSSStyleValue> c, ValueComparingNonnullRefPtr<CSSStyleValue> h, ValueComparingNonnullRefPtr<CSSStyleValue> alpha)
+        : CSSColorValue(color_type)
         , m_properties { .l = move(l), .c = move(c), .h = move(h), .alpha = move(alpha) }
     {
     }
@@ -49,6 +45,20 @@ private:
         ValueComparingNonnullRefPtr<CSSStyleValue> alpha;
         bool operator==(Properties const&) const = default;
     } m_properties;
+};
+
+// https://drafts.css-houdini.org/css-typed-om-1/#cssoklch
+class CSSOKLCH final : public CSSLCHLike {
+public:
+    CSSOKLCH(Badge<CSSLCHLike>, ValueComparingNonnullRefPtr<CSSStyleValue> l, ValueComparingNonnullRefPtr<CSSStyleValue> c, ValueComparingNonnullRefPtr<CSSStyleValue> h, ValueComparingNonnullRefPtr<CSSStyleValue> alpha)
+        : CSSLCHLike(ColorType::OKLCH, move(l), move(c), move(h), move(alpha))
+    {
+    }
+    virtual ~CSSOKLCH() override = default;
+
+    virtual Color to_color(Optional<Layout::NodeWithStyle const&>) const override;
+
+    String to_string() const override;
 };
 
 }
