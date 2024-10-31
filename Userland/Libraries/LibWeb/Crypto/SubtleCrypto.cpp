3475a5cea5070d17d4b0fc1993d80d90d93cbf67
stelar7 <dudedbz@gmail.com>
2024-10-31 16:02:24 +0100

    LibWeb: Implement AES-GCM.exportKey
    (cherry picked from commit 3dd80d2a6e0db87f1bef628a81d4a5a89ac6f7eb)

--- Userland/Libraries/LibWeb/Crypto/SubtleCrypto.cpp
+++ Userland/Libraries/LibWeb/Crypto/SubtleCrypto.cpp
@@ -787,6 +787,7 @@ SupportedAlgorithmsMap supported_algorithms()
     // https://w3c.github.io/webcrypto/#aes-gcm-registration
     define_an_algorithm<AesGcm, AesDerivedKeyParams>("get key length"_string, "AES-GCM"_string);
     define_an_algorithm<AesGcm>("importKey"_string, "AES-GCM"_string);
+    define_an_algorithm<AesGcm>("exportKey"_string, "AES-GCM"_string);
 
     // https://w3c.github.io/webcrypto/#hkdf
     define_an_algorithm<HKDF>("importKey"_string, "HKDF"_string);
