8fd20d3b38e000703cd893a51ff7f782b2288fa3
Andreas Kling <andreas@ladybird.org>
2024-11-03 11:19:05 +0100

    LibWeb: Fix incomplete assertion in the "in body" parsing state
    We were incorrectly tripping an assertion failure on some WPT tests.
    
    (cherry picked from commit ecc9fdf3771c9b3143e63b92189193841be2de54)

--- Userland/Libraries/LibWeb/HTML/Parser/HTMLParser.cpp
+++ Userland/Libraries/LibWeb/HTML/Parser/HTMLParser.cpp
@@ -1776,10 +1776,11 @@ void HTMLParser::handle_in_body(HTMLToken& token)
         // Parse error.
         log_parse_error();
 
-        // If the stack of open elements has only one node on it, or if the second element on the stack of open elements is not a body element, then ignore the token. (fragment case)
+        // If the stack of open elements has only one node on it, or if the second element on the stack of open elements is not a body element, then ignore the token.
+        // (fragment case or there is a template element on the stack)
         if (m_stack_of_open_elements.elements().size() == 1
             || m_stack_of_open_elements.elements().at(1)->local_name() != HTML::TagNames::body) {
-            VERIFY(m_parsing_fragment);
+            VERIFY(m_parsing_fragment || m_stack_of_open_elements.contains(HTML::TagNames::template_));
             return;
         }
 
