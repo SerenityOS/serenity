93732e2ca31714f3139c06af5cca6641d245548e
Shannon Booth <shannon@serenityos.org>
2024-11-03 20:56:30 +1300

    LibJS: Use GetShadowRealmContext for ShadowRealmImportValue
    Removing one user of the ExecutionContext slot of ShadowRealm.
    
    (cherry picked from commit 6a55865bf61ba0ad8aebd1b72ea0be82d849600d)

--- Userland/Libraries/LibJS/Runtime/ShadowRealm.h
+++ Userland/Libraries/LibJS/Runtime/ShadowRealm.h
@@ -40,7 +40,7 @@ private:
 
 ThrowCompletionOr<void> copy_name_and_length(VM&, FunctionObject& function, FunctionObject& target, Optional<StringView> prefix = {}, Optional<unsigned> arg_count = {});
 ThrowCompletionOr<Value> perform_shadow_realm_eval(VM&, StringView source_text, Realm& caller_realm, Realm& eval_realm);
-ThrowCompletionOr<Value> shadow_realm_import_value(VM&, ByteString specifier_string, ByteString export_name_string, Realm& caller_realm, Realm& eval_realm, ExecutionContext& eval_context);
+ThrowCompletionOr<Value> shadow_realm_import_value(VM&, ByteString specifier_string, ByteString export_name_string, Realm& caller_realm, Realm& eval_realm);
 ThrowCompletionOr<Value> get_wrapped_value(VM&, Realm& caller_realm, Value);
 NonnullOwnPtr<ExecutionContext> get_shadow_realm_context(Realm& shadow_realm, bool strict_eval);
 
