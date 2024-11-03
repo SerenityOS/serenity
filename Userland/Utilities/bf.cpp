e3fe2c578237d7c5e1083238dd2552fbfefb9f69
Ninad Sachania <ninad.sachania@gmail.com>
2024-11-03 19:40:06 +0000

    Utilities: Address review comments

--- Userland/Utilities/bf.cpp
+++ Userland/Utilities/bf.cpp
@@ -5,14 +5,17 @@
  */
 
 #include <AK/StringView.h>
+#include <AK/Vector.h>
 #include <LibCore/ArgsParser.h>
 #include <LibCore/File.h>
 #include <LibCore/System.h>
 #include <LibMain/Main.h>
 
-static void execute(StringView const instructions)
+static void execute(StringView const instructions, u32 const array_size)
 {
-    Array<u8, 30000> m_data;
+    Vector<u8> data;
+    data.resize(array_size);
+
     u32 data_pointer = 0;
     u32 instruction_pointer = 0;
 
@@ -26,35 +29,35 @@ static void execute(StringView const instructions)
 
             instruction_pointer += 1;
         } else if (instructions[instruction_pointer] == '+') {
-            m_data[data_pointer] += 1;
+            data[data_pointer] += 1;
 
             instruction_pointer += 1;
         } else if (instructions[instruction_pointer] == '-') {
-            m_data[data_pointer] -= 1;
+            data[data_pointer] -= 1;
 
             instruction_pointer += 1;
         } else if (instructions[instruction_pointer] == '.') {
-            putchar(m_data[data_pointer]);
+            putchar(data[data_pointer]);
             (void)fflush(stdout);
 
             instruction_pointer += 1;
         } else if (instructions[instruction_pointer] == ',') {
             u8 input = getc(stdin);
 
-            m_data[data_pointer] = input;
+            data[data_pointer] = input;
 
             instruction_pointer += 1;
         } else if (instructions[instruction_pointer] == '[') {
-            if (m_data[data_pointer] == 0x00) {
+            if (data[data_pointer] == 0x00) {
                 int balance = 1;
 
                 auto ip = instruction_pointer + 1;
                 while (ip < instructions.length() && balance != 0) {
-                    if (instructions[ip] == ']') {
+                    if (instructions[ip] == ']')
                         balance -= 1;
-                    } else if (instructions[ip] == '[') {
+                    else if (instructions[ip] == '[')
                         balance += 1;
-                    }
+
                     ip += 1;
                 }
 
@@ -63,16 +66,15 @@ static void execute(StringView const instructions)
                 instruction_pointer += 1;
             }
         } else if (instructions[instruction_pointer] == ']') {
-            if (m_data[data_pointer] > 0x00) {
+            if (data[data_pointer] > 0x00) {
                 int balance = 1;
 
                 auto ip = instruction_pointer - 1;
                 while (balance != 0) {
-                    if (instructions[ip] == '[') {
+                    if (instructions[ip] == '[')
                         balance -= 1;
-                    } else if (instructions[ip] == ']') {
+                    else if (instructions[ip] == ']')
                         balance += 1;
-                    }
 
                     if (ip == 0)
                         break;
@@ -95,16 +97,23 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
 
     StringView path;
 
+    u32 array_size = 30000;
+
     Core::ArgsParser args_parser;
     args_parser.set_general_help("A Brainf**k interpreter.");
     args_parser.add_positional_argument(path, "Program path", "path", Core::ArgsParser::Required::No);
+    args_parser.add_option(array_size, "Size of the program array (default 30000)", "size", 's', "number");
     args_parser.parse(arguments);
 
-    auto file_or_error = TRY(Core::File::open_file_or_standard_stream(path, Core::File::OpenMode::Read));
+    if (array_size < 30000) {
+        warnln("The array size must be at least 30000.");
+        exit(1);
+    }
 
-    auto const content = TRY(file_or_error->read_until_eof());
-    StringView const content_view(content);
+    auto file = TRY(Core::File::open_file_or_standard_stream(path, Core::File::OpenMode::Read));
 
-    execute(content_view);
+    auto const content = TRY(file->read_until_eof());
+
+    execute(content, array_size);
     return 0;
 }
