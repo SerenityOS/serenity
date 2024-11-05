d7062832bd07c92552be9b5286ee9b1282162c1e
Ninad Sachania <ninad.sachania@gmail.com>
2024-11-05 09:55:15 +0000

    Utilities/dirname: Improve help message

--- Userland/Utilities/dirname.cpp
+++ Userland/Utilities/dirname.cpp
@@ -14,8 +14,9 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
     bool null_terminated = false;
     Vector<ByteString> paths;
     Core::ArgsParser args_parser;
+    args_parser.set_general_help("Return the directory portion of the given path(s).");
     args_parser.add_option(null_terminated, "End each output line with \\0, rather than \\n", "zero", 'z');
-    args_parser.add_positional_argument(paths, "Path", "path");
+    args_parser.add_positional_argument(paths, "Path to get dirname from", "path");
     args_parser.parse(arguments);
 
     auto const delimiter = null_terminated ? '\0' : '\n';
