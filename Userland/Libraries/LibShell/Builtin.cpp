7d1d5450a4b2e4e1c15c2eaf6b860a55ca9527f5
Ninad Sachania <ninad.sachania@gmail.com>
2024-11-06 07:02:10 +0000

    LibShell/Builtin: Add spaces to make the output more consistent

--- Userland/Libraries/LibShell/Builtin.cpp
+++ Userland/Libraries/LibShell/Builtin.cpp
@@ -1136,11 +1136,11 @@ ErrorOr<int> Shell::builtin_time(Main::Arguments arguments)
         warnln("Timing report: {} ms", iteration_times.sum());
         warnln("==============");
         warnln("Command:         {}", ByteString::join(' ', arguments.strings));
-        warnln("Average time:    {:.2} ms (median: {}, stddev: {:.2}, min: {}, max:{})",
+        warnln("Average time:    {:.2} ms (median: {}, stddev: {:.2}, min: {}, max: {})",
             iteration_times.average(), iteration_times.median(),
             iteration_times.standard_deviation(),
             iteration_times.min(), iteration_times.max());
-        warnln("Excluding first: {:.2} ms (median: {}, stddev: {:.2}, min: {}, max:{})",
+        warnln("Excluding first: {:.2} ms (median: {}, stddev: {:.2}, min: {}, max: {})",
             iteration_times_excluding_first.average(), iteration_times_excluding_first.median(),
             iteration_times_excluding_first.standard_deviation(),
             iteration_times_excluding_first.min(), iteration_times_excluding_first.max());
