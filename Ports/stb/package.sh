6a96e1c23f9e407bf5780dcbcbe2730cb86de2cc
Ninad Sachania <ninad.sachania@gmail.com>
2024-11-06 06:31:02 +0000

    Ports/stb: Bump version to 2e2bef4

--- Ports/stb/package.sh
+++ Ports/stb/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='stb'
-version='f7f20f39fe4f206c6f19e26ebfef7b261ee59ee4'
+version='2e2bef463a5b53ddf8bb788e25da6b8506314c08'
 files=(
-    "https://github.com/nothings/stb/archive/${version}.zip#8b5013c681233837eec18a773b45831cea076d913c5ef8b9771204d51e8ece15"
+    "https://github.com/nothings/stb/archive/${version}.zip#f6a4669309a29dd8634c3c2c7a955da72469c2dc61471f68d9c499e517ab823f"
 )
 
 build() {
