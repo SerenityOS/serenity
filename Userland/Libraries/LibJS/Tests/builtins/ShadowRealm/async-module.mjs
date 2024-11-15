89733ee3cb0687374af0319ba6a7a59a989e0589
Luke Wilde <lukew@serenityos.org>
2024-11-15 15:11:48 +0000

    LibJS: Perform TLA async function construction in the module context
    Previously it was only pushing the module context for the call to
    capture the module execution context. This is incorrect, as the capture
    occurs upon function construction. This resulted in it capturing the
    execution context that execute_module was called from, instead of the
    newly created module_context.
    https://github.com/LadybirdBrowser/ladybird/blob/f87041bf3a8c76dce547dbd1f64d32b119f5468c/Libraries/LibJS/Runtime/ECMAScriptFunctionObject.cpp#L92
    
    This can be demonstrated with the following setup:
    index.html:
    ```html
    <script>
        var foo = 1;
    </script>
    <script type="module">
        import {test} from "./scriptA.mjs";
    </script>
    ```
    
    scriptA.mjs:
    ```js
    function foo() {
    	return {a: "b"};
    }
    export let test = await foo();
    ```
    
    Before this fix, this would throw:
    ```
    [TypeError] 1 is not a function (evaluated from 'foo')
        at module code with top-level await
        at module code with top-level await
        at <unknown>
        at <unknown>
    ```
    
    Fixes #2245.
    
    (cherry picked from commit 6319dedbcd4d6e448b4cc2615e62eba5ac576887)

--- Userland/Libraries/LibJS/Tests/builtins/ShadowRealm/async-module.mjs
+++ Userland/Libraries/LibJS/Tests/builtins/ShadowRealm/async-module.mjs
@@ -9,3 +9,9 @@ export default "Default export";
 await Promise.resolve(2);
 
 export const bar = "'bar' export";
+
+async function baz() {
+    return "'qux' export";
+}
+
+export const qux = await baz();
