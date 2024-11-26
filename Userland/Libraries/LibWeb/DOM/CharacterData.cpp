LadybirdBrowser/ladybird

LibWeb: Add Document helpers to move its cursor to word boundaries
This implementation is based on the same feature I added to Serenity's
TextEditor:

https://github.com/SerenityOS/serenity/pull/17477

Userland/Libraries/LibWeb/DOM/CharacterData.cpp
@@ -130,6 +130,8 @@ WebIDL::ExceptionOr<void> CharacterData::replace_data(size_t offset, size_t coun
 
     if (m_grapheme_segmenter)
         m_grapheme_segmenter->set_segmented_text(m_data);
+    if (m_word_segmenter)
+        m_word_segmenter->set_segmented_text(m_data);
 
     return {};
 }
@@ -163,6 +165,16 @@ Unicode::Segmenter& CharacterData::grapheme_segmenter()
     }
 
     return *m_grapheme_segmenter;
+}
+
+Unicode::Segmenter& CharacterData::word_segmenter()
+{
+    if (!m_word_segmenter) {
+        m_word_segmenter = Unicode::Segmenter::create(Unicode::SegmenterGranularity::Word);
+        m_word_segmenter->set_segmented_text(m_data);
+    }
+
+    return *m_word_segmenter;
 }
 
 }

Userland/Libraries/LibWeb/DOM/CharacterData.h
@@ -41,6 +41,7 @@ public:
     WebIDL::ExceptionOr<void> replace_data(size_t offset_in_utf16_code_units, size_t count_in_utf16_code_units, String const&);
 
     Unicode::Segmenter& grapheme_segmenter();
+    Unicode::Segmenter& word_segmenter();
 
 protected:
     CharacterData(Document&, NodeType, String const&);
@@ -51,6 +52,7 @@ private:
     String m_data;
 
     OwnPtr<Unicode::Segmenter> m_grapheme_segmenter;
+    OwnPtr<Unicode::Segmenter> m_word_segmenter;
 };
 
 }

SerenityOS/serenity

(cherry picked from commit ecf2cc600bea8f8396061f5fe6aba8358a033bbb;
amended to change Unicode::Segmenter to Locale::Segmenter due to
LibLocale and LibUnicode being separate libraries in serenity)

Userland/Libraries/LibWeb/DOM/CharacterData.cpp
@@ -130,6 +130,8 @@ WebIDL::ExceptionOr<void> CharacterData::replace_data(size_t offset, size_t coun
 
     if (m_grapheme_segmenter)
         m_grapheme_segmenter->set_segmented_text(m_data);
+    if (m_word_segmenter)
+        m_word_segmenter->set_segmented_text(m_data);
 
     return {};
 }
@@ -163,6 +165,16 @@ Locale::Segmenter& CharacterData::grapheme_segmenter()
     }
 
     return *m_grapheme_segmenter;
+}
+
+Locale::Segmenter& CharacterData::word_segmenter()
+{
+    if (!m_word_segmenter) {
+        m_word_segmenter = Locale::Segmenter::create(Locale::SegmenterGranularity::Word);
+        m_word_segmenter->set_segmented_text(m_data);
+    }
+
+    return *m_word_segmenter;
 }
 
 }

Userland/Libraries/LibWeb/DOM/CharacterData.h
@@ -41,6 +41,7 @@ public:
     WebIDL::ExceptionOr<void> replace_data(size_t offset_in_utf16_code_units, size_t count_in_utf16_code_units, String const&);
 
     Locale::Segmenter& grapheme_segmenter();
+    Locale::Segmenter& word_segmenter();
 
 protected:
     CharacterData(Document&, NodeType, String const&);
@@ -51,6 +52,7 @@ private:
     String m_data;
 
     OwnPtr<Locale::Segmenter> m_grapheme_segmenter;
+    OwnPtr<Locale::Segmenter> m_word_segmenter;
 };
 
 }