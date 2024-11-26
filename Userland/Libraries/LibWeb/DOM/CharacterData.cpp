LadybirdBrowser/ladybird

LibWeb: Rename CharacterData's segmenter indicate it is for graphemes
We will be adding a word segmenter as well, so this is to disambiguate
the two.

Userland/Libraries/LibWeb/DOM/CharacterData.cpp
@@ -128,8 +128,8 @@ WebIDL::ExceptionOr<void> CharacterData::replace_data(size_t offset, size_t coun
 
     document().set_needs_layout();
 
-    if (m_segmenter)
-        m_segmenter->set_segmented_text(m_data);
+    if (m_grapheme_segmenter)
+        m_grapheme_segmenter->set_segmented_text(m_data);
 
     return {};
 }
@@ -155,14 +155,14 @@ WebIDL::ExceptionOr<void> CharacterData::delete_data(size_t offset, size_t count
     return replace_data(offset, count, String {});
 }
 
-Unicode::Segmenter& CharacterData::segmenter()
+Unicode::Segmenter& CharacterData::grapheme_segmenter()
 {
-    if (!m_segmenter) {
-        m_segmenter = Unicode::Segmenter::create(Unicode::SegmenterGranularity::Grapheme);
-        m_segmenter->set_segmented_text(m_data);
+    if (!m_grapheme_segmenter) {
+        m_grapheme_segmenter = Unicode::Segmenter::create(Unicode::SegmenterGranularity::Grapheme);
+        m_grapheme_segmenter->set_segmented_text(m_data);
     }
 
-    return *m_segmenter;
+    return *m_grapheme_segmenter;
 }
 
 }

Userland/Libraries/LibWeb/DOM/CharacterData.h
@@ -40,7 +40,7 @@ public:
     WebIDL::ExceptionOr<void> delete_data(size_t offset_in_utf16_code_units, size_t count_in_utf16_code_units);
     WebIDL::ExceptionOr<void> replace_data(size_t offset_in_utf16_code_units, size_t count_in_utf16_code_units, String const&);
 
-    Unicode::Segmenter& segmenter();
+    Unicode::Segmenter& grapheme_segmenter();
 
 protected:
     CharacterData(Document&, NodeType, String const&);
@@ -50,7 +50,7 @@ protected:
 private:
     String m_data;
 
-    OwnPtr<Unicode::Segmenter> m_segmenter;
+    OwnPtr<Unicode::Segmenter> m_grapheme_segmenter;
 };
 
 }

SerenityOS/serenity

(cherry picked from commit eece7697fdcaf50eed98fff069c5e678ac425784;
amended to fix conflict due to Serenity's Segmenter still being in
LibLocale, while upstream combined LibLocale and LibUnicode)

Userland/Libraries/LibWeb/DOM/CharacterData.cpp
@@ -128,8 +128,8 @@ WebIDL::ExceptionOr<void> CharacterData::replace_data(size_t offset, size_t coun
 
     document().set_needs_layout();
 
-    if (m_segmenter)
-        m_segmenter->set_segmented_text(m_data);
+    if (m_grapheme_segmenter)
+        m_grapheme_segmenter->set_segmented_text(m_data);
 
     return {};
 }
@@ -155,14 +155,14 @@ WebIDL::ExceptionOr<void> CharacterData::delete_data(size_t offset, size_t count
     return replace_data(offset, count, String {});
 }
 
-Locale::Segmenter& CharacterData::segmenter()
+Locale::Segmenter& CharacterData::grapheme_segmenter()
 {
-    if (!m_segmenter) {
-        m_segmenter = Locale::Segmenter::create(Locale::SegmenterGranularity::Grapheme);
-        m_segmenter->set_segmented_text(m_data);
+    if (!m_grapheme_segmenter) {
+        m_grapheme_segmenter = Locale::Segmenter::create(Locale::SegmenterGranularity::Grapheme);
+        m_grapheme_segmenter->set_segmented_text(m_data);
     }
 
-    return *m_segmenter;
+    return *m_grapheme_segmenter;
 }
 
 }

Userland/Libraries/LibWeb/DOM/CharacterData.h
@@ -40,7 +40,7 @@ public:
     WebIDL::ExceptionOr<void> delete_data(size_t offset_in_utf16_code_units, size_t count_in_utf16_code_units);
     WebIDL::ExceptionOr<void> replace_data(size_t offset_in_utf16_code_units, size_t count_in_utf16_code_units, String const&);
 
-    Locale::Segmenter& segmenter();
+    Locale::Segmenter& grapheme_segmenter();
 
 protected:
     CharacterData(Document&, NodeType, String const&);
@@ -50,7 +50,7 @@ protected:
 private:
     String m_data;
 
-    OwnPtr<Locale::Segmenter> m_segmenter;
+    OwnPtr<Locale::Segmenter> m_grapheme_segmenter;
 };
 
 }