b49927916245822eedfde1e0dcfb0735bd2aa5eb
Sönke Holz <sholz8530@gmail.com>
2024-12-03 18:33:43 +0100

    LibELF+Kernel: Don't define SERENITY_PAGE_SIZE in ELFABI.h
    ELFABI.h gets included during the toolchain build, so we shouldn't
    include AK headers from that file.
    SERENITY_PAGE_SIZE also isn't really related to ELF ABI, so move it to
    the serenity_limits.h header.

--- Kernel/API/serenity_limits.h
+++ Kernel/API/serenity_limits.h
@@ -7,3 +7,9 @@
 #pragma once
 
 #define PTHREAD_STACK_MAX (32 * 1024 * 1024) // 32MiB
+
+#ifdef __serenity__
+#    define SERENITY_PAGE_SIZE PAGE_SIZE
+#else
+#    define SERENITY_PAGE_SIZE 4096
+#endif
