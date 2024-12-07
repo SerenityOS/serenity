e294b33e982a02249da1d5d20b70cd2fe7856cdf
MacDue <macdue@dueutil.tech>
2024-12-07 20:11:33 +0000

    LibGfx: Add "Redmond Glass" window theme
    This adds a window theme that aims to imitate the default Windows 7
    theme (known as Aero). This initial version mainly focuses on window
    frames along with minor taskbar theming.
    
    Currently, the colors for the window frames are hardcoded, but the
    actual gradient painting uses the LibGfx gradient support, so it should
    be fairly easy to make configurable.

---
+++ Userland/Libraries/LibGfx/GlassWindowTheme.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2023, MacDue <macdue@dueutil.tech>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibGfx/ClassicWindowTheme.h>
+#include <LibGfx/Rect.h>
+
+namespace Gfx {
+
+class GlassWindowTheme : public ClassicWindowTheme {
+    virtual void paint_normal_frame(Painter& painter, WindowState window_state, WindowMode window_mode, IntRect const& window_rect, StringView window_title, Bitmap const& icon, Palette const& palette, IntRect const& leftmost_button_rect, int menu_row_count, bool window_modified) const override;
+    virtual IntRect titlebar_rect(WindowType, WindowMode, IntRect const& window_rect, Palette const&) const override;
+    virtual IntRect titlebar_text_rect(WindowType type, WindowMode mode, IntRect const& window_rect, Palette const& palette) const override
+    {
+        return titlebar_rect(type, mode, window_rect, palette);
+    }
+    virtual bool frame_uses_alpha(WindowState, Palette const&) const override
+    {
+        return true;
+    }
+    virtual bool taskbar_uses_alpha() const override
+    {
+        return true;
+    }
+    virtual Vector<IntRect> layout_buttons(WindowType, WindowMode, IntRect const& window_rect, Palette const&, size_t buttons) const override;
+
+    virtual float frame_alpha_hit_threshold(WindowState) const override { return 0.6f; }
+
+    virtual void paint_taskbar(Painter&, IntRect const& taskbar_rect, Palette const&) const override;
+};
+
+}
--- Userland/Libraries/LibGfx/Palette.cpp
+++ Userland/Libraries/LibGfx/Palette.cpp
@@ -8,6 +8,7 @@
 
 #include <AK/Badge.h>
 #include <LibGfx/ClassicWindowTheme.h>
+#include <LibGfx/GlassWindowTheme.h>
 #include <LibGfx/Palette.h>
 #include <LibGfx/PlasticWindowTheme.h>
 #include <string.h>
@@ -58,6 +59,10 @@ Gfx::WindowTheme& Palette::window_theme() const
     case WindowThemeProvider::RedmondPlastic: {
         static PlasticWindowTheme plastic_window_theme;
         return plastic_window_theme;
+    }
+    case WindowThemeProvider::RedmondGlass: {
+        static GlassWindowTheme glass_window_theme;
+        return glass_window_theme;
     }
     default:
         VERIFY_NOT_REACHED();
