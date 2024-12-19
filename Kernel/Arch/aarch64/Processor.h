f79490e7be64043194b6427b92e4667e6b4fe367
Sönke Holz <sholz8530@gmail.com>
2024-12-19 18:03:35 +0100

    Kernel/aarch64: Implement Processor::read_cycle_count

--- Kernel/Arch/aarch64/Processor.h
+++ Kernel/Arch/aarch64/Processor.h
@@ -205,8 +205,9 @@ ALWAYS_INLINE void ProcessorBase<T>::wait_check()
 template<typename T>
 ALWAYS_INLINE Optional<u64> ProcessorBase<T>::read_cycle_count()
 {
-    TODO_AARCH64();
-    return 0;
+    if (Processor::current().has_feature(CPUFeature::PMUv3))
+        return Aarch64::PMCCNTR_EL0::read().CCNT;
+    return {};
 }
 
 }
