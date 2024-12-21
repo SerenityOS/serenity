tcl3/ladybird

UI/Qt: Fix hover_label hiding URLs
m_hover_label did not have checks if the mouse is in the same location.
This caused clickable URLs to be hidden.
Also shortened the label text to not be longer than half of the window.

Ladybird/Qt/WebContentView.cpp
@@ -373,6 +373,7 @@ void WebContentView::mouseMoveEvent(QMouseEvent* event)
     }
 
     enqueue_native_event(Web::MouseEvent::Type::MouseMove, *event);
+    QAbstractScrollArea::mouseMoveEvent(event);
 }
 
 void WebContentView::mousePressEvent(QMouseEvent* event)

SerenityOS/serenity

(cherry picked from commit edf29857f8f8b669c45d673b088d9c0236659d73)

Ladybird/Qt/WebContentView.cpp
@@ -423,6 +423,7 @@ void WebContentView::mouseMoveEvent(QMouseEvent* event)
     }
 
     enqueue_native_event(Web::MouseEvent::Type::MouseMove, *event);
+    QWidget::mouseMoveEvent(event);
 }
 
 void WebContentView::mousePressEvent(QMouseEvent* event)