e21ccdc0873fdbce5b4814c7e0d54bd1bd554160
MacDue <macdue@dueutil.tech>
2024-12-27 17:27:22 +0000

    LibGfx: Add support for notifications to glass window theme

--- Userland/Libraries/LibGfx/GlassWindowTheme.h
+++ Userland/Libraries/LibGfx/GlassWindowTheme.h
@@ -13,6 +13,8 @@ namespace Gfx {
 
 class GlassWindowTheme : public ClassicWindowTheme {
     virtual void paint_normal_frame(Painter& painter, WindowState window_state, WindowMode window_mode, IntRect const& window_rect, StringView window_title, Bitmap const& icon, Palette const& palette, IntRect const& leftmost_button_rect, int menu_row_count, bool window_modified) const override;
+    virtual void paint_notification_frame(Painter&, WindowMode, IntRect const& window_rect, Palette const&, IntRect const& close_button_rect) const override;
+
     virtual IntRect titlebar_rect(WindowType, WindowMode, IntRect const& window_rect, Palette const&) const override;
     virtual IntRect titlebar_text_rect(WindowType type, WindowMode mode, IntRect const& window_rect, Palette const& palette) const override
     {
