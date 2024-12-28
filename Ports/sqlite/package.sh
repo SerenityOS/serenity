61f8fb3d56998ab673ddda917d7ced962395374b
djwisdom <djwisdom@gmail.com>
2024-12-28 15:41:40 +0800

    Ports: Update sqlite3 from 346 to 347

--- Ports/sqlite/package.sh
+++ Ports/sqlite/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='sqlite'
-version='3460000'
+version='3470000'
 files=(
-    "https://www.sqlite.org/2024/sqlite-autoconf-${version}.tar.gz#6f8e6a7b335273748816f9b3b62bbdc372a889de8782d7f048c653a447417a7d"
+    "https://www.sqlite.org/2024/sqlite-autoconf-${version}.tar.gz#83eb21a6f6a649f506df8bd3aab85a08f7556ceed5dbd8dea743ea003fc3a957"
 )
 useconfigure='true'
 use_fresh_config_sub='true'
