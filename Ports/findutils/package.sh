ea124c41c54cb7290897f22b349d5413df376954
butter1125 <butter011125@gmail.com>
2024-12-27 22:00:55 -0500

    Ports/findutils: Update to 4.10.0

--- Ports/findutils/package.sh
+++ Ports/findutils/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='findutils'
-version='4.9.0'
+version='4.10.0'
 useconfigure='true'
 use_fresh_config_sub='true'
 config_sub_paths=('build-aux/config.sub')
 files=(
-    "https://ftpmirror.gnu.org/gnu/findutils/findutils-${version}.tar.xz#a2bfb8c09d436770edc59f50fa483e785b161a3b7b9d547573cb08065fd462fe"
+    "https://ftpmirror.gnu.org/gnu/findutils/findutils-${version}.tar.xz#1387e0b67ff247d2abde998f90dfbf70c1491391a59ddfecb8ae698789f0a4f5"
 )
