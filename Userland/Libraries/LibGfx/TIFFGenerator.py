6fa29085e55c93c5390fef63e812f83b614d7f1d
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2024-12-29 10:19:56 +0100

    LibGfx: Add basic GPS exif tags parsing

--- Userland/Libraries/LibGfx/TIFFGenerator.py
+++ Userland/Libraries/LibGfx/TIFFGenerator.py
@@ -156,6 +156,14 @@ known_tags: List[Tag] = [
     Tag('339', [TIFFType.UnsignedShort], [], SampleFormat.Unsigned, "SampleFormat", SampleFormat),
     Tag('34665', [TIFFType.UnsignedLong, TIFFType.IFD], [1], None, "ExifIFD"),
     Tag('34675', [TIFFType.Undefined], [], None, "ICCProfile"),
+    Tag('34853', [TIFFType.UnsignedLong, TIFFType.IFD], [1], None, "GPSInfo"),
+
+    # GPS Tags (https://exiftool.org/TagNames/GPS.html)
+    Tag('0', [TIFFType.UnsignedLong], [], None, "GPSVersionID"),
+    Tag('1', [TIFFType.ASCII], [2], None, "GPSLatitudeRef"),
+    Tag('2', [TIFFType.UnsignedRational], [3], None, "GPSLatitude"),
+    Tag('3', [TIFFType.ASCII], [2], None, "GPSLongitudeRef"),
+    Tag('4', [TIFFType.UnsignedRational], [3], None, "GPSLongitude"),
 ]
 
 HANDLE_TAG_SIGNATURE_TEMPLATE = ("ErrorOr<void> {namespace}handle_tag(Function<ErrorOr<void>(u32)>&& subifd_handler, "
