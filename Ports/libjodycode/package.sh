531abd0508ea7f9e1a8d2f773244e9c0becea47e
butter1125 <butter011125@gmail.com>
2024-12-29 21:31:50 -0500

    Ports/libjodycode: Update to version 3.1.1

--- Ports/libjodycode/package.sh
+++ Ports/libjodycode/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libjodycode'
-version='3.1'
+version='3.1.1'
 files=(
-    "https://codeberg.org/jbruchon/libjodycode/archive/v${version}.tar.gz#837b660c305215f0cbd68b002e831f29fe1a1d823cace74d75203111eb433bbd"
+    "https://codeberg.org/jbruchon/libjodycode/archive/v${version}.tar.gz#82717625b91f92ea4e7435798385762c6906835fa2cf2c80c9f83d6734bfddad"
 )
 auth_type='sha256'
 workdir='libjodycode'
