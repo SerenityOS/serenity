0940f255cb1e3f74edaf8715ae9f3684200bd5f7
butter1125 <butter011125@gmail.com>
2024-12-30 04:48:50 -0500

    Ports/rsync: Update to version 3.3.0

--- Ports/rsync/package.sh
+++ Ports/rsync/package.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='rsync'
-version='3.2.7'
+version='3.3.0'
 useconfigure='true'
 use_fresh_config_sub='true'
 files=(
-    "https://download.samba.org/pub/rsync/src/rsync-${version}.tar.gz#4e7d9d3f6ed10878c58c5fb724a67dacf4b6aac7340b13e488fb2dc41346f2bb"
+    "https://download.samba.org/pub/rsync/src/rsync-${version}.tar.gz#7399e9a6708c32d678a72a63219e96f23be0be2336e50fd1348498d07041df90"
 )
 configopts=(
     "--target=${SERENITY_ARCH}-pc-serenity"
     '--disable-xxhash'
+)
+depends=(
+    'zstd'
 )
 if [ "${SERENITY_TOOLCHAIN}" = 'Clang' ]; then
     depends=(
