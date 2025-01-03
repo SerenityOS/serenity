2693e9c2db4933fb07766e1fcd143a1b681d2849
butter1125 <butter011125@gmail.com>
2025-01-02 22:52:15 -0500

    Ports/md4c: Update to version 0.5.2

--- Ports/md4c/package.sh
+++ Ports/md4c/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='md4c'
-version='0.4.8'
+version='0.5.2'
 workdir="md4c-release-${version}"
 files=(
-    "https://github.com/mity/md4c/archive/release-${version}.tar.gz#4a457df853425b6bb6e3457aa1d1a13bccec587a04c38c622b1013a0da41439f"
+    "https://github.com/mity/md4c/archive/release-${version}.tar.gz#55d0111d48fb11883aaee91465e642b8b640775a4d6993c2d0e7a8092758ef21"
 )
 useconfigure='true'
 
