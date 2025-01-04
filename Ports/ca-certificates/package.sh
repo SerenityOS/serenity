7337d32fa7c88259161b134b264f94f507d089c6
butter1125 <butter011125@gmail.com>
2025-01-03 23:01:25 -0500

    Ports/ca-certificates: Update to version 2024-12-31

--- Ports/ca-certificates/package.sh
+++ Ports/ca-certificates/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='ca-certificates'
-version='2024-11-26'
+version='2024-12-31'
 files=(
-    "https://curl.se/ca/cacert-${version}.pem#bb1782d281fe60d4a2dcf41bc229abe3e46c280212597d4abcc25bddf667739b"
+    "https://curl.se/ca/cacert-${version}.pem#a3f328c21e39ddd1f2be1cea43ac0dec819eaa20a90425d7da901a11531b3aa5"
 )
 workdir='.'
 
