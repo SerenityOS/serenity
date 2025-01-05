2b472624293057c6a2e5d7763e896ee677a3802b
butter1125 <butter011125@gmail.com>
2025-01-04 20:52:20 -0500

    Ports/lynx: Update to version 2.9.2

--- Ports/lynx/package.sh
+++ Ports/lynx/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='lynx'
-version='2.8.9rel.1'
+version='2.9.2'
 files=(
-    "https://invisible-island.net/archives/lynx/tarballs/lynx${version}.tar.bz2#387f193d7792f9cfada14c60b0e5c0bff18f227d9257a39483e14fa1aaf79595"
+    "https://invisible-island.net/archives/lynx/tarballs/lynx${version}.tar.bz2#7374b89936d991669e101f4e97f2c9592036e1e8cdaa7bafc259a77ab6fb07ce"
 )
 workdir="lynx${version}"
 depends=('ncurses' 'openssl' 'zlib')
