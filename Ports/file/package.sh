201d415f6078617b0eca0624c59db2d3e65be48f
butter1125 <butter011125@gmail.com>
2025-01-04 22:50:40 -0500

    Ports/file: Update to 5.46

--- Ports/file/package.sh
+++ Ports/file/package.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='file'
-version='5.45'
+version='5.46'
 useconfigure='true'
 use_fresh_config_sub='true'
 files=(
-    "http://ftp.astron.com/pub/file/file-${version}.tar.gz#fc97f51029bb0e2c9f4e3bffefdaf678f0e039ee872b9de5c002a6d09c784d82"
+    "http://ftp.astron.com/pub/file/file-${version}.tar.gz#c9cc77c7c560c543135edc555af609d5619dbef011997e988ce40a3d75d86088"
 )
 
 function pre_configure() {
