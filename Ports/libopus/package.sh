ff31756bef1a27674f741e43442e5cb748a37dd4
butter1125 <butter011125@gmail.com>
2025-01-05 04:26:15 -0500

    Ports/libopus: Update to 1.5.2

--- Ports/libopus/package.sh
+++ Ports/libopus/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libopus'
-version='1.3.1'
-workdir='opus-1.3.1'
+version='1.5.2'
+workdir='opus-1.5.2'
 useconfigure='true'
 use_fresh_config_sub='true'
 files=(
-    "https://archive.mozilla.org/pub/opus/opus-${version}.tar.gz#65b58e1e25b2a114157014736a3d9dfeaad8d41be1c8179866f144a2fb44ff9d"
+    "https://downloads.xiph.org/releases/opus/opus-${version}.tar.gz#65c1d2f78b9f2fb20082c38cbe47c951ad5839345876e46941612ee87f9a7ce1"
 )
