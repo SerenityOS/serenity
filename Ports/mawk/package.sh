374b4cf26d66ab82e81e5c27fbb69cf33ded6c0f
butter1125 <butter011125@gmail.com>
2025-01-05 10:35:30 -0500

    Ports/mawk: Update to 1.3.4-20240905

--- Ports/mawk/package.sh
+++ Ports/mawk/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='mawk'
-version='1.3.4-20230808'
+version='1.3.4-20240905'
 files=(
-    "https://invisible-mirror.net/archives/mawk/mawk-${version}.tgz#88f55a632e2736ff5c5f69944abc151734d89d8298d5005921180f39ab7ba6d0"
+    "https://invisible-mirror.net/archives/mawk/mawk-${version}.tgz#a39967927dfa1b0116efc45b944a0f5b5b4c34f8e842a4b223dcdd7b367399e0"
 )
 useconfigure='true'
 use_fresh_config_sub='true'
