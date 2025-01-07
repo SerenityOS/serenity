0d14aed8f5a6ff3943745a30bf9db681eaba8fc3
butter1125 <butter011125@gmail.com>
2025-01-06 21:00:30 -0500

    Ports/giflib: Update to 5.2.2

--- Ports/giflib/package.sh
+++ Ports/giflib/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='giflib'
-version='5.2.1'
+version='5.2.2'
 files=(
-    "https://downloads.sourceforge.net/project/giflib/giflib-${version}.tar.gz#31da5562f44c5f15d63340a09a4fd62b48c45620cd302f77a6d9acf0077879bd"
+    "https://downloads.sourceforge.net/project/giflib/giflib-${version}.tar.gz#be7ffbd057cadebe2aa144542fd90c6838c6a083b5e8a9048b8ee3b66b29d5fb"
 )
