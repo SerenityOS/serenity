a59e22000a49386e8f31225eb426f2930d17544b
butter1125 <butter011125@gmail.com>
2025-01-07 21:08:05 -0500

    Ports/imagemagick: Update to 7.1.1-43

--- Ports/imagemagick/package.sh
+++ Ports/imagemagick/package.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='imagemagick'
-version='7.1.1-15'
+version='7.1.1-43'
 workdir="ImageMagick-${version}"
 useconfigure='true'
 files=(
-    "https://github.com/ImageMagick/ImageMagick/archive/refs/tags/${version}.tar.gz#2372192a76af9be43c0543dd7ae6dfbf34b11fc0203583453ce3f9f707c36bcc"
+    "https://github.com/ImageMagick/ImageMagick/archive/refs/tags/${version}.tar.gz#ceb972266b23dc7c1cfce0da5a7f0c9acfb4dc81f40eb542a49476fedbc2618f"
 )
 configopts=(
     "--with-sysroot=${SERENITY_INSTALL_ROOT}"
