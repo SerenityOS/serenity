1e6162d826af09baf6395c6389ae8b2b92efef1f
butter1125 <butter011125@gmail.com>
2025-01-08 21:38:30 -0500

    Ports/lcms2: Update to 2.16

--- Ports/lcms2/package.sh
+++ Ports/lcms2/package.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='lcms2'
-version='2.15'
+version='2.16'
 useconfigure='true'
 use_fresh_config_sub='true'
 use_fresh_config_guess='true'
 files=(
-    "https://github.com/mm2/Little-CMS/releases/download/lcms${version}/lcms2-${version}.tar.gz#b20cbcbd0f503433be2a4e81462106fa61050a35074dc24a4e356792d971ab39"
+    "https://github.com/mm2/Little-CMS/releases/download/lcms${version}/lcms2-${version}.tar.gz#d873d34ad8b9b4cea010631f1a6228d2087475e4dc5e763eb81acc23d9d45a51"
 )
 depends=(
     'libtiff'
