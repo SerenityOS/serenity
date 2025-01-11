b397879ab4cb3aba89196b282ed7fb341954ae48
butter1125 <butter011125@gmail.com>
2025-01-10 22:23:10 -0500

    Ports/libmikmod: Update to 3.3.12

--- Ports/libmikmod/package.sh
+++ Ports/libmikmod/package.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=libmikmod
-version=3.3.11.1
+version=3.3.12
 useconfigure=true
 configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt")
 use_fresh_config_sub=true
 config_sub_paths=("autotools/config.sub")
 files=(
-    "https://downloads.sourceforge.net/project/mikmod/libmikmod/${version}/libmikmod-${version}.tar.gz#ad9d64dfc8f83684876419ea7cd4ff4a41d8bcd8c23ef37ecb3a200a16b46d19"
+    "https://downloads.sourceforge.net/project/mikmod/libmikmod/${version}/libmikmod-${version}.tar.gz#adef6214863516a4a5b44ebf2c71ef84ecdfeb3444973dacbac70911c9bc67e9"
 )
 
 configure() {
