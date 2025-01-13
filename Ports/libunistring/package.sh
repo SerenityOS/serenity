f7ba87954a502a629aab215b055e008102578d8e
butter1125 <butter011125@gmail.com>
2025-01-12 22:30:35 -0500

    Ports/libunistring: Update to 1.3

--- Ports/libunistring/package.sh
+++ Ports/libunistring/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libunistring'
-version='1.1'
+version='1.3'
 files=(
-    "https://ftpmirror.gnu.org/gnu/libunistring/libunistring-${version}.tar.gz#a2252beeec830ac444b9f68d6b38ad883db19919db35b52222cf827c385bdb6a"
+    "https://ftpmirror.gnu.org/gnu/libunistring/libunistring-${version}.tar.gz#8ea8ccf86c09dd801c8cac19878e804e54f707cf69884371130d20bde68386b7"
 )
 useconfigure='true'
 configopts=(
