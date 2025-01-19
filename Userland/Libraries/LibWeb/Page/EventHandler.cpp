b115ce25a36670d0392f08a62116028e24c96380
MacDue <macdue@dueutil.tech>
2025-01-19 11:21:51 +0000

    LibWeb: Send `page_did_click_link` no matter the modifiers
    d0e18b89 revived sending the `page_did_click_link` event, but only for
    a chrome feature where ctrl is also clicked. For the Help app to work
    normally (not just when pressing ctrl), we need this event to be sent
    no matter the modifier. The modifiers are sent as part of the event so
    chromes can ignore non-control clicks.

--- Userland/Libraries/LibWeb/Page/EventHandler.cpp
+++ Userland/Libraries/LibWeb/Page/EventHandler.cpp
@@ -333,8 +333,7 @@ EventResult EventHandler::handle_mouseup(CSSPixelPoint viewport_position, CSSPix
                     JS::NonnullGCPtr<DOM::Document> document = *m_navigable->active_document();
                     auto href = link->href();
                     auto url = document->parse_url(href);
-
-                    if (button == UIEvents::MouseButton::Primary && (modifiers & UIEvents::Mod_PlatformCtrl) != 0) {
+                    if (button == UIEvents::MouseButton::Primary) {
                         m_navigable->page().client().page_did_click_link(url, link->target().to_byte_string(), modifiers);
                     } else if (button == UIEvents::MouseButton::Middle) {
                         m_navigable->page().client().page_did_middle_click_link(url, link->target().to_byte_string(), modifiers);
