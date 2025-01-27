11a85453373abde78591606568fa81e513e79ae0
Nico Weber <thakis@chromium.org>
2025-01-27 14:00:57 -0500

    LibGfx/JPEG2000: Give Span2D a few more functions

--- Userland/Libraries/LibGfx/ImageFormats/JPEG2000Span2D.h
+++ Userland/Libraries/LibGfx/ImageFormats/JPEG2000Span2D.h
@@ -15,7 +15,37 @@ template<class T>
 struct Span2D {
     Span<T> data;
     IntSize size;
-    int pitch;
+    int pitch { 0 };
+
+    Span2D() = default;
+
+    Span2D(Span<T> data, IntSize size, int pitch)
+        : data(data)
+        , size(size)
+        , pitch(pitch)
+    {
+        VERIFY(size.is_empty() || static_cast<int>(data.size()) >= (size.height() - 1) * pitch + size.width());
+    }
+
+    [[nodiscard]] ALWAYS_INLINE constexpr Span<T> scanline(int y) const
+    {
+        return data.slice(y * pitch, size.width());
+    }
+
+    [[nodiscard]] ALWAYS_INLINE constexpr T const& operator[](size_t index) const
+    {
+        return data[index];
+    }
+
+    [[nodiscard]] ALWAYS_INLINE constexpr int width() const
+    {
+        return size.width();
+    }
+
+    [[nodiscard]] ALWAYS_INLINE constexpr int height() const
+    {
+        return size.height();
+    }
 };
 
 }
