6987a6e646527aad55c31bd1f7d5e3531a87037a
Andreas Kling <andreas@ladybird.org>
2025-02-08 20:50:28 +0100

    LibJS: Allocate RegExp.{lastMatch,leftContext,rightContext} lazily
    These properties are always substrings of the RegExp input string,
    and so we can store them as views and lazily construct strings if
    they're actually accessed (which most of the time they aren't).
    
    This avoids a bunch of unnecessary memory copying, saving roughly
    2.1 seconds per iteration of Speedometer.
    
    (cherry picked from commit dbf52a1b5fa9d7e81816ffc6c5a587c832148bd8)

--- Userland/Libraries/LibJS/Runtime/RegExpLegacyStaticProperties.h
+++ Userland/Libraries/LibJS/Runtime/RegExpLegacyStaticProperties.h
@@ -23,10 +23,25 @@ namespace JS {
 class RegExpLegacyStaticProperties {
 public:
     Optional<Utf16String> const& input() const { return m_input; }
-    Optional<Utf16String> const& last_match() const { return m_last_match; }
+    Optional<Utf16String> const& last_match() const
+    {
+        if (!m_last_match_string.has_value())
+            m_last_match_string = Utf16String::create(m_last_match);
+        return m_last_match_string;
+    }
     Optional<Utf16String> const& last_paren() const { return m_last_paren; }
-    Optional<Utf16String> const& left_context() const { return m_left_context; }
-    Optional<Utf16String> const& right_context() const { return m_right_context; }
+    Optional<Utf16String> const& left_context() const
+    {
+        if (!m_left_context_string.has_value())
+            m_left_context_string = Utf16String::create(m_left_context);
+        return m_left_context_string;
+    }
+    Optional<Utf16String> const& right_context() const
+    {
+        if (!m_right_context_string.has_value())
+            m_right_context_string = Utf16String::create(m_right_context);
+        return m_right_context_string;
+    }
     Optional<Utf16String> const& $1() const { return m_$1; }
     Optional<Utf16String> const& $2() const { return m_$2; }
     Optional<Utf16String> const& $3() const { return m_$3; }
@@ -38,10 +53,22 @@ public:
     Optional<Utf16String> const& $9() const { return m_$9; }
 
     void set_input(Utf16String input) { m_input = move(input); }
-    void set_last_match(Utf16String last_match) { m_last_match = move(last_match); }
+    void set_last_match(Utf16View last_match)
+    {
+        m_last_match = last_match;
+        m_last_match_string = {};
+    }
     void set_last_paren(Utf16String last_paren) { m_last_paren = move(last_paren); }
-    void set_left_context(Utf16String left_context) { m_left_context = move(left_context); }
-    void set_right_context(Utf16String right_context) { m_right_context = move(right_context); }
+    void set_left_context(Utf16View left_context)
+    {
+        m_left_context = left_context;
+        m_left_context_string = {};
+    }
+    void set_right_context(Utf16View right_context)
+    {
+        m_right_context = right_context;
+        m_right_context_string = {};
+    }
     void set_$1(Utf16String value) { m_$1 = move(value); }
     void set_$2(Utf16String value) { m_$2 = move(value); }
     void set_$3(Utf16String value) { m_$3 = move(value); }
@@ -55,10 +82,7 @@ public:
 
 private:
     Optional<Utf16String> m_input;
-    Optional<Utf16String> m_last_match;
     Optional<Utf16String> m_last_paren;
-    Optional<Utf16String> m_left_context;
-    Optional<Utf16String> m_right_context;
     Optional<Utf16String> m_$1;
     Optional<Utf16String> m_$2;
     Optional<Utf16String> m_$3;
@@ -68,6 +92,14 @@ private:
     Optional<Utf16String> m_$7;
     Optional<Utf16String> m_$8;
     Optional<Utf16String> m_$9;
+
+    // NOTE: These are views into m_input and we only turn them into full strings if/when needed.
+    Utf16View m_last_match;
+    Utf16View m_left_context;
+    Utf16View m_right_context;
+    mutable Optional<Utf16String> m_last_match_string;
+    mutable Optional<Utf16String> m_left_context_string;
+    mutable Optional<Utf16String> m_right_context_string;
 };
 
 ThrowCompletionOr<void> set_legacy_regexp_static_property(VM& vm, RegExpConstructor& constructor, Value this_value, void (RegExpLegacyStaticProperties::*property_setter)(Utf16String), Value value);
