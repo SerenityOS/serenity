SerenityOS/serenity

commit a7fbb7fd0bb02ec5518fdd07d44904aaabbc58b6

MacPDF: Make Debug menu work when sidebar has focus

Meta/Lagom/Contrib/MacPDF/MacPDFWindowController.mm
@@ -132,6 +132,18 @@
     [_pdfView goToPreviousPage:sender];
 }
 
+- (BOOL)validateMenuItem:(NSMenuItem*)item
+{
+    if ([_pdfView validateMenuItem:item])
+        return YES;
+    return [super validateMenuItem:item];
+}
+
+- (IBAction)toggleShowClippingPaths:(id)sender
+{
+    [_pdfView toggleShowClippingPaths:sender];
+}
+
 - (IBAction)showGoToPageDialog:(id)sender
 {
     auto alert = [[NSAlert alloc] init];

Meta/Lagom/Contrib/MacPDF/MacPDFWindowController.h
@@ -21,6 +21,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (IBAction)goToNextPage:(id)sender;
 - (IBAction)goToPreviousPage:(id)sender;
+- (IBAction)toggleShowClippingPaths:(id)sender;
 - (IBAction)showGoToPageDialog:(id)sender;
 
 - (void)pdfDidInitialize;

commit 7ac72c4cf4115ae9e7e160bf0b942b43f60b2d1e

MacPDF: Unbreak "Go to Page…" keyboard shortcut
I broke this in a7fbb7fd0bb02e of #22791.

Almost all of MacPDFWindowController's IBActions are forwarded
to `_pdfView`, but `showGoToPageDialog:` isn't and needs to be
explicitly handled in `validateMenuItem:`.

Meta/Lagom/Contrib/MacPDF/MacPDFWindowController.mm
@@ -136,6 +136,8 @@
 {
     if ([_pdfView validateMenuItem:item])
         return YES;
+    if (item.action == @selector(showGoToPageDialog:))
+        return _pdfDocument.pdf ? YES : NO;
     return [super validateMenuItem:item];
 }
 