8520748d968684efdf1c19c9043d25e7fd78edcc
Hendiadyoin1 <leon.a@serenityos.org>
2025-02-09 18:44:11 +0100

    Kernel/E1000: Add formatters for some RX descriptor fields
    This makes debugging a bit easier

--- Kernel/Net/Intel/E1000Descriptors.h
+++ Kernel/Net/Intel/E1000Descriptors.h
@@ -7,6 +7,7 @@
 #pragma once
 
 #include <AK/EnumBits.h>
+#include <AK/Format.h>
 #include <AK/StdLibExtraDetails.h>
 #include <AK/Types.h>
 
@@ -40,7 +41,7 @@ enum class RxDescriptorStatus : u8 {
     None = 0,
     DD = 1 << 0, // Descriptor Done
     EOP = 1 << 1,
-    Rsv = 1 << 2,
+    IXSM = 1 << 2,  // Ignore Checksum Indication (reserved on later NICs)
     VP = 1 << 3,    // VLAN Packet
     UDPCS = 1 << 4, // UDP Checksum calculated
     L4CS = 1 << 5,  // L4 Checksum calculated
@@ -69,7 +70,7 @@ enum class RxDescriptorExtendedStatus : u64 {
     DD = 1 << 0, // Descriptor Done
     EOP = 1 << 1,
     // Below only in last descriptor of a packet
-    // Rsv = 1 << 2,
+    IXSM = 1 << 2,  // Ignore Checksum Indication (Reserved)
     VP = 1 << 3,    // VLAN Packet
     UDPCS = 1 << 4, // UDP Checksum calculated
     L4CS = 1 << 5,  // L4 Checksum calculated
@@ -110,8 +111,21 @@ struct AdvancedRxDescriptorWriteBack {
         HASH_TCP_IPV6_EX = 0x8,
         // 0xA:0xF reserved
     };
+    enum class PacketType : u64 {
+        IPV4 = 1 << 0,
+        IPV4E = 1 << 1,
+        IPV6 = 1 << 2,
+        IPV6E = 1 << 3,
+        TCP = 1 << 4,
+        UDP = 1 << 5,
+        SCTP = 1 << 6,
+        NFS = 1 << 7,
+        // 10:8 = ETQF
+        L2 = 1 << 11,
+        VLAN = 1 << 12,
+    };
     RSSType rss_type : 4;
-    u64 packet_type : 13;
+    PacketType packet_type : 13;
     u64 : 4; // rsv-reserved, also the 82576 docs say this is 5 bits in one place, 4 in another
              // while the i211 docs say its 22 bits, which is clearly a typo, but there these may also contain two extra header_len bits
              // but those dont seem to be mentioned in the explanation later
@@ -125,6 +139,8 @@ struct AdvancedRxDescriptorWriteBack {
 };
 static_assert(AssertSize<AdvancedRxDescriptorWriteBack, 16>());
 
+AK_ENUM_BITWISE_OPERATORS(AdvancedRxDescriptorWriteBack::PacketType);
+
 union RxDescriptor {
     LegacyRxDescriptor legacy;
     AdvancedRxDescriptor advanced;
@@ -133,3 +149,145 @@ union RxDescriptor {
 static_assert(AssertSize<RxDescriptor, 16>());
 
 }
+
+namespace AK {
+template<>
+struct Formatter<Kernel::E1000::AdvancedRxDescriptorWriteBack::PacketType> : StandardFormatter {
+
+    ErrorOr<void> format(FormatBuilder& builder, Kernel::E1000::AdvancedRxDescriptorWriteBack::PacketType value)
+    {
+        if (has_flag(value, Kernel::E1000::AdvancedRxDescriptorWriteBack::PacketType::IPV4))
+            TRY(builder.put_literal("IPv4|"sv));
+        if (has_flag(value, Kernel::E1000::AdvancedRxDescriptorWriteBack::PacketType::IPV4E))
+            TRY(builder.put_literal("IPv4E|"sv));
+        if (has_flag(value, Kernel::E1000::AdvancedRxDescriptorWriteBack::PacketType::IPV6))
+            TRY(builder.put_literal("IPv6|"sv));
+        if (has_flag(value, Kernel::E1000::AdvancedRxDescriptorWriteBack::PacketType::IPV6E))
+            TRY(builder.put_literal("IPv6E|"sv));
+        if (has_flag(value, Kernel::E1000::AdvancedRxDescriptorWriteBack::PacketType::TCP))
+            TRY(builder.put_literal("TCP"sv));
+        if (has_flag(value, Kernel::E1000::AdvancedRxDescriptorWriteBack::PacketType::UDP))
+            TRY(builder.put_literal("UDP"sv));
+        if (has_flag(value, Kernel::E1000::AdvancedRxDescriptorWriteBack::PacketType::SCTP))
+            TRY(builder.put_literal("SCTP"sv));
+        if (has_flag(value, Kernel::E1000::AdvancedRxDescriptorWriteBack::PacketType::NFS))
+            TRY(builder.put_literal("NFS"sv));
+        if (has_flag(value, Kernel::E1000::AdvancedRxDescriptorWriteBack::PacketType::L2))
+            TRY(builder.put_literal("|L2"sv));
+        if (has_flag(value, Kernel::E1000::AdvancedRxDescriptorWriteBack::PacketType::VLAN))
+            TRY(builder.put_literal("|VLAN"sv));
+        return {};
+    }
+};
+
+template<>
+struct Formatter<Kernel::E1000::RxDescriptorExtendedStatus> : StandardFormatter {
+    using RxDescriptorExtendedStatus = Kernel::E1000::RxDescriptorExtendedStatus;
+    ErrorOr<void> format(FormatBuilder& builder, RxDescriptorExtendedStatus status)
+    {
+        if (to_underlying(status) == 0) {
+            TRY(builder.put_literal("None"sv));
+            return {};
+        }
+
+        if (has_flag(status, RxDescriptorExtendedStatus::DD)) {
+            TRY(builder.put_literal("DD"sv));
+            status = status & ~RxDescriptorExtendedStatus::DD;
+            if (to_underlying(status) != 0)
+                TRY(builder.put_literal("|"sv));
+        }
+        if (has_flag(status, RxDescriptorExtendedStatus::EOP)) {
+            TRY(builder.put_literal("EOP"sv));
+            status = status & ~RxDescriptorExtendedStatus::EOP;
+            if (to_underlying(status) != 0)
+                TRY(builder.put_literal("|"sv));
+        }
+        if (has_flag(status, RxDescriptorExtendedStatus::IXSM)) {
+            TRY(builder.put_literal("IXSM"sv));
+            status = status & ~RxDescriptorExtendedStatus::IXSM;
+            if (to_underlying(status) != 0)
+                TRY(builder.put_literal("|"sv));
+        }
+        if (has_flag(status, RxDescriptorExtendedStatus::VP)) {
+            TRY(builder.put_literal("VP"sv));
+            status = status & ~RxDescriptorExtendedStatus::VP;
+            if (to_underlying(status) != 0)
+                TRY(builder.put_literal("|"sv));
+        }
+        if (has_flag(status, RxDescriptorExtendedStatus::UDPCS)) {
+            TRY(builder.put_literal("UDPCS"sv));
+            status = status & ~RxDescriptorExtendedStatus::UDPCS;
+            if (to_underlying(status) != 0)
+                TRY(builder.put_literal("|"sv));
+        }
+        if (has_flag(status, RxDescriptorExtendedStatus::L4CS)) {
+            TRY(builder.put_literal("L4CS"sv));
+            status = status & ~RxDescriptorExtendedStatus::L4CS;
+            if (to_underlying(status) != 0)
+                TRY(builder.put_literal("|"sv));
+        }
+        if (has_flag(status, RxDescriptorExtendedStatus::IPCS)) {
+            TRY(builder.put_literal("IPCS"sv));
+            status = status & ~RxDescriptorExtendedStatus::IPCS;
+            if (to_underlying(status) != 0)
+                TRY(builder.put_literal("|"sv));
+        }
+        if (has_flag(status, RxDescriptorExtendedStatus::PIF)) {
+            TRY(builder.put_literal("PIF"sv));
+            status = status & ~RxDescriptorExtendedStatus::PIF;
+            if (to_underlying(status) != 0)
+                TRY(builder.put_literal("|"sv));
+        }
+        if (has_flag(status, RxDescriptorExtendedStatus::VEXT)) {
+            TRY(builder.put_literal("VEXT"sv));
+            status = status & ~RxDescriptorExtendedStatus::VEXT;
+            if (to_underlying(status) != 0)
+                TRY(builder.put_literal("|"sv));
+        }
+        if (has_flag(status, RxDescriptorExtendedStatus::UDPV)) {
+            TRY(builder.put_literal("UDPV"sv));
+            status = status & ~RxDescriptorExtendedStatus::UDPV;
+            if (to_underlying(status) != 0)
+                TRY(builder.put_literal("|"sv));
+        }
+        if (has_flag(status, RxDescriptorExtendedStatus::LLINT)) {
+            TRY(builder.put_literal("LLINT"sv));
+            status = status & ~RxDescriptorExtendedStatus::LLINT;
+            if (to_underlying(status) != 0)
+                TRY(builder.put_literal("|"sv));
+        }
+        if (has_flag(status, RxDescriptorExtendedStatus::StripCRC)) {
+            TRY(builder.put_literal("StripCRC"sv));
+            status = status & ~RxDescriptorExtendedStatus::StripCRC;
+            if (to_underlying(status) != 0)
+                TRY(builder.put_literal("|"sv));
+        }
+        if (has_flag(status, RxDescriptorExtendedStatus::TSIP)) {
+            TRY(builder.put_literal("TSIP"sv));
+            status = status & ~RxDescriptorExtendedStatus::TSIP;
+            if (to_underlying(status) != 0)
+                TRY(builder.put_literal("|"sv));
+        }
+        if (has_flag(status, RxDescriptorExtendedStatus::TS)) {
+            TRY(builder.put_literal("TS"sv));
+            status = status & ~RxDescriptorExtendedStatus::TS;
+            if (to_underlying(status) != 0)
+                TRY(builder.put_literal("|"sv));
+        }
+
+        if (to_underlying(status) != 0)
+            TRY(builder.put_u64(to_underlying(status), 16, true, false, false, false, FormatBuilder::Align::Right, 0, ' ', FormatBuilder::SignMode::OnlyIfNeeded, false));
+        return {};
+    }
+};
+
+template<>
+struct Formatter<Kernel::E1000::RxDescriptorStatus> : Formatter<Kernel::E1000::RxDescriptorExtendedStatus> {
+    ErrorOr<void> format(FormatBuilder& builder, Kernel::E1000::RxDescriptorStatus status)
+    {
+        auto extended_status = static_cast<Kernel::E1000::RxDescriptorExtendedStatus>(to_underlying(status));
+        return Formatter<Kernel::E1000::RxDescriptorExtendedStatus>::format(builder, extended_status);
+    }
+};
+
+};
