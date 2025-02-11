9ab90dbec1f49db0c87cba2a38b9e76bf26f61d6
Sönke Holz <sholz8530@gmail.com>
2025-02-12 00:54:10 +0100

    Kernel/riscv64: Implement `enumerate_interrupt_handlers`
    This function is used by `lsirq`.

---
+++ Kernel/Arch/riscv64/Interrupts.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2025, Sönke Holz <sholz8530@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/Types.h>
+
+#include <AK/Platform.h>
+VALIDATE_IS_RISCV64()
+
+namespace Kernel {
+
+static constexpr size_t GENERIC_INTERRUPT_HANDLERS_COUNT = 256;
+
+}
