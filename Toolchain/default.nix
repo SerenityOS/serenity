61a28994bba57b1955ae78c21ee5c6d2fc08c157
Vinicius Deolindo <andrade.vinicius934@gmail.com>
2025-02-25 14:52:47 -0300

    Nix: Add the necessary packages/build inputs for building on Darwin

--- Toolchain/default.nix
+++ Toolchain/default.nix
@@ -3,42 +3,59 @@
 }:
 with pkgs;
 
-mkShell.override { stdenv = gcc13Stdenv; } {
-  packages = [
-    ccache
-    cmake
-    curl
-    e2fsprogs
-    fuse2fs
-    gcc13
-    gmp
-    libmpc
-    mpfr
-    ninja
-    patch
-    pkg-config
-    rsync
-    texinfo
-    unzip
-    # To build the GRUB disk image
-    grub2
-    libxcrypt
-    openssl
-    parted
-    qemu
-    python3
-    # For building and installing ports
-    autoconf
-    automake
-    gperf
-    imagemagick
-    libtool
-    # For clangd and clang-format
-    clang-tools
-    # For LibWeb-related formatting
-    nodePackages.prettier
-    # For the pre-commit hooks
-    pre-commit
+mkShell.override { stdenv = gccStdenv; } {
+  packages =
+    [
+      ccache
+      cmake
+      curl
+      e2fsprogs
+      flex
+      git
+      gmp
+      libmpc
+      libxcrypt
+      mpfr
+      ninja
+      openssl
+      patch
+      pkg-config
+      python3
+      qemu
+      rsync
+      texinfo
+      unzip
+      wget
+      # For building and installing ports
+      autoconf
+      automake
+      gperf
+      imagemagick
+      libtool
+      # For development
+      clang-tools
+      nodePackages.prettier
+      pre-commit
+    ]
+    ++ lib.optionals stdenv.isLinux [
+      fuse
+      fuse-ext2
+      grub2
+      parted
+    ]
+    ++ lib.optionals stdenv.isDarwin [
+      genext2fs
+    ];
+
+  buildInputs = lib.optionals stdenv.isDarwin [
+    apple-sdk_13
+    (darwinMinVersionHook "13.3")
   ];
+
+  shellHook = ''
+    # The toolchain is built with `mtune=native` which nix warns about, but we don't care about that warning.
+    export NIX_ENFORCE_NO_NATIVE=0
+  '';
+
   hardeningDisable = [ "format" ];
 }
