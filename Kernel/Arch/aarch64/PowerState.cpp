4702a812ad235ab5d365e20abf49d6f2450854e0
implicitfield <114500360+implicitfield@users.noreply.github.com>
2025-03-14 15:02:22 +0200

    Kernel/aarch64: Add basic support for PSCI and HVC calls
    This makes it possible to shut down and reboot the QEMU virt machine.

--- Kernel/Arch/aarch64/PowerState.cpp
+++ Kernel/Arch/aarch64/PowerState.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <Kernel/Arch/PowerState.h>
+#include <Kernel/Arch/aarch64/PSCI.h>
 #include <Kernel/Arch/aarch64/RPi/Watchdog.h>
 #include <Kernel/Firmware/DeviceTree/DeviceTree.h>
 
@@ -12,10 +13,15 @@ namespace Kernel {
 
 void arch_specific_reboot()
 {
+    if (PSCI::is_supported())
+        PSCI::reset();
 }
 
 void arch_specific_poweroff()
 {
+    if (PSCI::is_supported())
+        PSCI::poweroff();
+
     if (DeviceTree::get().is_compatible_with("raspberrypi,3-model-b"sv) || DeviceTree::get().is_compatible_with("raspberrypi,4-model-b"sv))
         RPi::Watchdog::the().system_shutdown();
 }
