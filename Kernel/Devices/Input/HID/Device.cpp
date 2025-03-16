264dd6cf332efacb76e85d00919cae536cac1ab4
Sönke Holz <sholz8530@gmail.com>
2025-03-16 13:42:34 +0100

    Kernel/Input: Add basic HID driver infrastructure
    This new HID driver infrastructure is organized as follows:
    
    The HID transport (such as USB or I2C) creates an `HID::Device`
    for each HID device it detects and passes the constructor
    a `HID::TransportInterface`, which this new HID code uses to receive
    input reports from.
    
    The `HID::Device` parses the HID report descriptor it received from the
    HID transport and instantiates `HID::ApplicationCollectionDriver`s for
    all Application Collections with known Usages.
    
    The `HID::Device` calls the `HID::ApplicationCollectionDriver`s when
    it receives a new input report to parse and handle them.

---
+++ Kernel/Devices/Input/HID/Device.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2025, Sönke Holz <sholz8530@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <Kernel/Debug.h>
+#include <Kernel/Devices/Input/HID/Definitions.h>
+#include <Kernel/Devices/Input/HID/Device.h>
+
+namespace Kernel::HID {
+
+ErrorOr<NonnullOwnPtr<Device>> Device::create(NonnullOwnPtr<TransportInterface> transport_interface, ::HID::ParsedReportDescriptor parsed_report_descriptor)
+{
+    auto device = TRY(adopt_nonnull_own_or_enomem(new (nothrow) Device(move(transport_interface), move(parsed_report_descriptor))));
+    TRY(device->initialize());
+    return device;
+}
+
+::HID::ParsedReportDescriptor const& Device::report_descriptor() const
+{
+    return m_parsed_report_descriptor;
+}
+
+Device::Device(NonnullOwnPtr<TransportInterface> transport_interface, ::HID::ParsedReportDescriptor parsed_report_descriptor)
+    : m_transport_interface(move(transport_interface))
+    , m_parsed_report_descriptor(move(parsed_report_descriptor))
+{
+}
+
+ErrorOr<void> Device::initialize()
+{
+    for (auto const& application_collection : m_parsed_report_descriptor.application_collections) {
+        VERIFY(application_collection.type == ::HID::CollectionType::Application);
+
+        using enum HID::Usage;
+        switch (static_cast<HID::Usage>(application_collection.usage)) {
+        default:
+            dbgln_if(HID_DEBUG, "HID: Unsupported Application Collection Usage: {:#x}", application_collection.usage);
+            continue;
+        }
+    }
+
+    TRY(m_transport_interface->start_receiving_input_reports([this](ReadonlyBytes report_data) {
+        for (auto& application_collection_driver : m_application_collection_drivers) {
+            // FIXME: Rate limit this message?
+            if (auto result = application_collection_driver.on_report(report_data); result.is_error())
+                dbgln("HID: Failed to parse input report: {}", result.release_error());
+        }
+    }));
+
+    return {};
+}
+
+}
