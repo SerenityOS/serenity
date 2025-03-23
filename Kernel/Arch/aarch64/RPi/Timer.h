0b49092111090620a7f7e6e12a5bf1b46592726f
Sönke Holz <sholz8530@gmail.com>
2025-03-23 13:10:24 +0100

    Kernel/aarch64: Resolve some TODO_AARCH64s in RPi/Timer.h

--- Kernel/Arch/aarch64/RPi/Timer.h
+++ Kernel/Arch/aarch64/RPi/Timer.h
@@ -25,16 +25,16 @@ public:
     virtual HardwareTimerType timer_type() const override { return HardwareTimerType::RPiTimer; }
     virtual StringView model() const override { return "RPi Timer"sv; }
 
-    virtual bool is_periodic() const override { TODO_AARCH64(); }
-    virtual bool is_periodic_capable() const override { TODO_AARCH64(); }
-    virtual void set_periodic() override { TODO_AARCH64(); }
-    virtual void set_non_periodic() override { TODO_AARCH64(); }
-    virtual void disable() override { TODO_AARCH64(); }
+    virtual bool is_periodic() const override { return false; }
+    virtual bool is_periodic_capable() const override { return false; }
+    virtual void set_periodic() override { }
+    virtual void set_non_periodic() override { }
+    virtual void disable() override;
 
-    virtual void reset_to_default_ticks_per_second() override { TODO_AARCH64(); }
-    virtual bool try_to_set_frequency(size_t) override { TODO_AARCH64(); }
-    virtual bool is_capable_of_frequency(size_t) const override { TODO_AARCH64(); }
-    virtual size_t calculate_nearest_possible_frequency(size_t) const override { TODO_AARCH64(); }
+    virtual void reset_to_default_ticks_per_second() override { }
+    virtual bool try_to_set_frequency(size_t frequency) override { return frequency == m_frequency; }
+    virtual bool is_capable_of_frequency(size_t frequency) const override { return frequency == m_frequency; }
+    virtual size_t calculate_nearest_possible_frequency(size_t) const override { return m_frequency; }
 
     // FIXME: Share code with HPET::update_time
     u64 update_time(u64& seconds_since_boot, u32& ticks_this_second, bool query_only);
