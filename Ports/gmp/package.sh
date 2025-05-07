6868e6b989bfadf767dd8c43032e7d998375c602
Hendiadyoin1 <leon.a@serenityos.org>
2025-05-07 20:18:45 +0200

    Ports/gmp: Force C11 and 64 bit API
    This makes it actually build on compilers assuming C20 (like GCC 15) and
    makes configuration a bit faster, since we don't have to check for
    the 32 bit API.

--- Ports/gmp/package.sh
+++ Ports/gmp/package.sh
@@ -2,6 +2,13 @@
 port='gmp'
 version='6.3.0'
 useconfigure='true'
+configopts=(
+   # We do not support 32-bit ABIs on 64-bit systems,
+   # and only support 64-bit systems
+   "ABI=64"
+   # C20 does not allow incomplete prototypes, which gmp implicitly uses
+   "CFLAGS=-O2 -pedantic -std=c11"
+)
 files=(
     "https://ftpmirror.gnu.org/gnu/gmp/gmp-${version}.tar.xz#a3c2b80201b89e68616f4ad30bc66aee4927c3ce50e33929ca819d5c43538898"
 )
