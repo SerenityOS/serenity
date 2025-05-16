59d8d2a0beb68301f1ec75eff63cd622462b123f
implicitfield <114500360+implicitfield@users.noreply.github.com>
2025-05-17 01:01:42 +0300

    Kernel/Devices: Prefer non-recursive Spinlocks

--- Kernel/Devices/TTY/SlavePTY.cpp
+++ Kernel/Devices/TTY/SlavePTY.cpp
@@ -13,9 +13,9 @@
 
 namespace Kernel {
 
-static Singleton<RecursiveSpinlockProtected<SlavePTY::List, LockRank::None>> s_all_instances;
+static Singleton<SpinlockProtected<SlavePTY::List, LockRank::None>> s_all_instances;
 
-RecursiveSpinlockProtected<SlavePTY::List, LockRank::None>& SlavePTY::all_instances()
+SpinlockProtected<SlavePTY::List, LockRank::None>& SlavePTY::all_instances()
 {
     return s_all_instances;
 }
