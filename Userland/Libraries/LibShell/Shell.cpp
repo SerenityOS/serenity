SerenityOS/serenity

commit 1c7233f1481e38de71e537049a444621f32d2526

Shell: Initialize the line editor after running the init script
Generally, wait for the init script to do whatever it wants before
initializing the editor, to make sure we capture the correct terminal
state.

Userland/Shell/main.cpp
@@ -57,7 +57,6 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
         }
 
         editor = Line::Editor::construct(move(configuration));
-        editor->initialize();
 
         shell = Shell::Shell::construct(*editor, attempt_interactive, posix_mode);
         s_shell = shell.ptr();
@@ -69,9 +68,6 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
         sigaddset(&blocked, SIGTTOU);
         sigaddset(&blocked, SIGTTIN);
         pthread_sigmask(SIG_BLOCK, &blocked, nullptr);
-
-        shell->termios = editor->termios();
-        shell->default_termios = editor->default_termios();
 
         editor->on_display_refresh = [&](auto& editor) {
             editor.strip_styles();
@@ -266,6 +262,9 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
         }
     }
 
+    editor->initialize();
+    shell->termios = editor->termios();
+    shell->default_termios = editor->default_termios();
     shell->add_child(*editor);
 
     Core::EventLoop::current().post_event(*shell, make<Core::CustomEvent>(Shell::Shell::ShellEventType::ReadLine));

commit f734a8b57c0957d0ddecf13833b89017dc0f60b2

Shell: Don't use uninitialized termios data
1c7233f1481e38de71e537049a444621f32d2526 broke this by moving the
initialization later, while still having the shell use the invalid
termios data.
Also, this commit makes it so *only* the init script is run without the
termios populated, as 'Shell -c ...' should still behave like running
the command in the shell.

Userland/Libraries/LibShell/Shell.cpp
@@ -665,7 +665,9 @@ int Shell::run_command(StringView cmd, Optional<SourcePosition> source_position_
         return 1;
     }
 
-    tcgetattr(0, &termios);
+    struct termios current_termios;
+    tcgetattr(0, &current_termios);
+    termios = current_termios;
 
     (void)command->run(*this);
 
@@ -836,8 +838,8 @@ ErrorOr<RefPtr<Job>> Shell::run_command(const AST::Command& command)
 
         close(sync_pipe[0]);
 
-        if (!m_is_subshell && command.should_wait)
-            tcsetattr(0, TCSANOW, &default_termios);
+        if (default_termios.has_value() && !m_is_subshell && command.should_wait)
+            tcsetattr(0, TCSANOW, &*default_termios);
 
         m_is_subshell = true;
 
@@ -1182,7 +1184,8 @@ void Shell::restore_ios()
 {
     if (m_is_subshell)
         return;
-    tcsetattr(0, TCSANOW, &termios);
+    if (termios.has_value())
+        tcsetattr(0, TCSANOW, &*termios);
     tcsetpgrp(STDOUT_FILENO, m_pid);
     tcsetpgrp(STDIN_FILENO, m_pid);
 }

Userland/Libraries/LibShell/Shell.h
@@ -330,8 +330,8 @@ public:
 
     bool posix_mode() const { return m_in_posix_mode; }
 
-    struct termios termios;
-    struct termios default_termios;
+    Optional<struct termios> termios;
+    Optional<struct termios> default_termios;
     bool was_interrupted { false };
     bool was_resized { false };
 

Userland/Shell/main.cpp
@@ -240,6 +240,10 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
         shell->cache_path();
     }
 
+    editor->initialize();
+    shell->termios = editor->termios();
+    shell->default_termios = editor->default_termios();
+
     Vector<String> args_to_pass;
     TRY(args_to_pass.try_ensure_capacity(script_args.size()));
     for (auto& arg : script_args)
@@ -262,9 +266,6 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
         }
     }
 
-    editor->initialize();
-    shell->termios = editor->termios();
-    shell->default_termios = editor->default_termios();
     shell->add_child(*editor);
 
     Core::EventLoop::current().post_event(*shell, make<Core::CustomEvent>(Shell::Shell::ShellEventType::ReadLine));