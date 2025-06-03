f1a391e394814d95fd339ae43925845a0f9b3d22
Sönke Holz <sholz8530@gmail.com>
2025-06-03 21:26:27 +0200

    Kernel/riscv64: Print the ISA string during startup

--- Kernel/Arch/riscv64/ProcessorInfo.h
+++ Kernel/Arch/riscv64/ProcessorInfo.h
@@ -7,11 +7,14 @@
 #pragma once
 
 #include <AK/Types.h>
+#include <Kernel/Library/KString.h>
 
 #include <AK/Platform.h>
 VALIDATE_IS_RISCV64()
 
 namespace Kernel {
+
+class Processor;
 
 class ProcessorInfo {
 public:
@@ -21,11 +24,17 @@ public:
     FlatPtr marchid() const { return m_marchid; }
     FlatPtr mimpid() const { return m_mimpid; }
 
+    StringView isa_string() const { return m_isa_string->view(); }
+
+    void build_isa_string(Processor const&);
+
 private:
     // mvendorid, marchid, and mimpid can all be zero if they aren't implemented.
     u32 m_mvendorid { 0 };
     FlatPtr m_marchid { 0 };
     FlatPtr m_mimpid { 0 };
+
+    OwnPtr<KString> m_isa_string;
 };
 
 }
