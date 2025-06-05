11b89efd70e441d4369d57a811d8ad60a50d61dd
Hendiadyoin1 <leon.a@serenityos.org>
2025-06-06 00:32:41 +0200

    Meta+Kernel: Enable `-fno-sanitize-recover=undefined` by default
    This makes the Kernel UBSan deadly by compile time,
    it can be made recoverable by setting
    `ENABLE_KERNEL_UNDEFINED_SANITIZER_ALWAYS_DEADLY` to `OFF` in the
    CMake options, and the `ubsan_is_deadly` sysctl option to `0`.

--- Meta/CMake/serenity_options.cmake
+++ Meta/CMake/serenity_options.cmake
@@ -13,6 +13,7 @@ serenity_option(ENABLE_KERNEL_COVERAGE_COLLECTION  OFF CACHE BOOL "Enable KCOV a
 serenity_option(ENABLE_KERNEL_COVERAGE_COLLECTION_DEBUG  OFF CACHE BOOL "Enable KCOV and kernel coverage instrumentation debugging")
 serenity_option(ENABLE_KERNEL_LTO OFF CACHE BOOL "Build the kernel with link-time optimization")
 serenity_option(ENABLE_KERNEL_UNDEFINED_SANITIZER ON CACHE BOOL "Enable the Kernel Undefined Behavior Sanitizer (KUBSAN)")
+serenity_option(ENABLE_KERNEL_UNDEFINED_SANITIZER_ALWAYS_DEADLY ON CACHE BOOL "Make KUBSAN always deadly")
 serenity_option(ENABLE_EXTRA_KERNEL_DEBUG_SYMBOLS  OFF CACHE BOOL "Enable -Og and -ggdb3 options for Kernel code for easier debugging")
 serenity_option(ENABLE_MOLD_LINKER OFF CACHE BOOL "Link the SerenityOS userland with the mold linker")
 serenity_option(ENABLE_USERSPACE_COVERAGE_COLLECTION OFF CACHE BOOL "Enable code coverage instrumentation for userspace binaries in clang")
