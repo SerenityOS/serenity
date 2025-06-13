ef07673969217c4383c02b612df284ee14386878
Sönke Holz <sholz8530@gmail.com>
2025-06-13 13:55:21 +0200

    Ports: Build make with `-std=c17`
    C23 removed support for incomplete function prototypes.

--- Ports/make/package.sh
+++ Ports/make/package.sh
@@ -7,4 +7,4 @@ config_sub_paths=("build-aux/config.sub")
 files=(
     "https://ftpmirror.gnu.org/gnu/make/make-${version}.tar.gz#dd16fb1d67bfab79a72f5e8390735c49e3e8e70b4945a15ab1f81ddb78658fb3"
 )
-configopts=("--target=${SERENITY_ARCH}-pc-serenity" "--with-sysroot=/" "--without-guile")
+configopts=("--target=${SERENITY_ARCH}-pc-serenity" "--with-sysroot=/" "--without-guile" "CFLAGS=-std=c17")
