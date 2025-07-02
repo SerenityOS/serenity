b8cb65086aee546f88ff933c305157b09bfc1698
Jelle Raaijmakers <jelle@gmta.nl>
2025-07-02 11:29:29 +0200

    Ports: Compile libmpeg2 with `-std=gnu89`
    libmpeg2 defines `getopt()` without any parameters, which fails to
    compile on newer C standards.

--- Ports/libmpeg2/package.sh
+++ Ports/libmpeg2/package.sh
@@ -8,3 +8,5 @@ configopts=("--disable-sdl")
 files=(
     "https://libmpeg2.sourceforge.io/files/libmpeg2-${version}.tar.gz#dee22e893cb5fc2b2b6ebd60b88478ab8556cb3b93f9a0d7ce8f3b61851871d4"
 )
+
+export CFLAGS="-std=gnu89"
