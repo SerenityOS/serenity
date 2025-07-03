3030c8d926570a8bcc06968c5a567b715083aa2b
Nico Weber <thakis@chromium.org>
2025-07-03 23:07:05 +0200

    LibPDF: Rename Renderer::show_empty_image() to paint_empty_image()
    For consistency with paint_image_xobject().
    
    No behavior change.

--- Userland/Libraries/LibPDF/Renderer.h
+++ Userland/Libraries/LibPDF/Renderer.h
@@ -213,7 +213,7 @@ private:
     PDFErrorOr<LoadedImage> load_image(NonnullRefPtr<StreamObject>);
     PDFErrorOr<NonnullRefPtr<Gfx::Bitmap>> make_mask_bitmap_from_array(NonnullRefPtr<ArrayObject>, NonnullRefPtr<StreamObject>);
     PDFErrorOr<void> paint_image_xobject(NonnullRefPtr<StreamObject>);
-    void show_empty_image(Gfx::IntSize);
+    void paint_empty_image(Gfx::IntSize);
     PDFErrorOr<NonnullRefPtr<ColorSpace>> get_color_space_from_resources(Value const&, NonnullRefPtr<DictObject>);
     PDFErrorOr<NonnullRefPtr<ColorSpace>> get_color_space_from_document(NonnullRefPtr<Object>);
 
