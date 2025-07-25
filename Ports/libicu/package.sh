1d05e82fb35a8acb63ad166a1025b29d59d8eaae
Linus Groh <mail@linusgroh.de>
2025-07-25 12:18:34 +0200

    Ports: Update libicu to 77.1

--- Ports/libicu/package.sh
+++ Ports/libicu/package.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libicu'
-version='73.2'
+version='77.1'
 useconfigure='true'
 use_fresh_config_sub='true'
 workdir='icu/source'
 files=(
-    "https://github.com/unicode-org/icu/releases/download/release-${version//./-}/icu4c-${version//./_}-src.tgz#818a80712ed3caacd9b652305e01afc7fa167e6f2e94996da44b90c2ab604ce1"
+    "https://github.com/unicode-org/icu/releases/download/release-${version//./-}/icu4c-${version//./_}-src.tgz#588e431f77327c39031ffbb8843c0e3bc122c211374485fa87dc5f3faff24061"
 )
 
 configure() {
@@ -18,5 +18,5 @@ configure() {
         --with-cross-build="$(pwd)/${workdir}/../host-build"
 }
 
-export CFLAGS='-DU_HAVE_NL_LANGINFO_CODESET=0'
-export CXXFLAGS='-DU_HAVE_NL_LANGINFO_CODESET=0'
+export CFLAGS='-DU_HAVE_NL_LANGINFO_CODESET=0 -DU_HAVE_CHAR16_T=0'
+export CXXFLAGS='-DU_HAVE_NL_LANGINFO_CODESET=0 -DU_HAVE_CHAR16_T=0'
