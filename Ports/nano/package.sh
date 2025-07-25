5f8e928018f085918591cd5832215f46ecc91053
Linus Groh <mail@linusgroh.de>
2025-07-25 12:18:34 +0200

    Ports: Update nano to 8.5

--- Ports/nano/package.sh
+++ Ports/nano/package.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='nano'
-version='8.2'
+version='8.5'
 files=(
-    "https://www.nano-editor.org/dist/v8/nano-${version}.tar.xz#d5ad07dd862facae03051c54c6535e54c7ed7407318783fcad1ad2d7076fffeb"
+    "https://www.nano-editor.org/dist/v8/nano-${version}.tar.xz#000b011d339c141af9646d43288f54325ff5c6e8d39d6e482b787bbc6654c26a"
 )
 useconfigure='true'
 use_fresh_config_sub='true'
-configopts=("--target=${SERENITY_ARCH}-pc-serenity" "--disable-browser" "--disable-utf8")
+configopts=("--target=${SERENITY_ARCH}-pc-serenity" "--disable-utf8")
 depends=("ncurses")
 
 export PKG_CONFIG_PATH="${SERENITY_INSTALL_ROOT}/usr/local/lib/pkgconfig"
