87eb379f1a2f1c2923e2b7fd144ae23d7f66fd4c
Linus Groh <mail@linusgroh.de>
2025-07-25 12:18:34 +0200

    Ports: Update SDL2_ttf to 2.24.0

--- Ports/SDL2_ttf/package.sh
+++ Ports/SDL2_ttf/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='SDL2_ttf'
-version='2.20.2'
+version='2.24.0'
 useconfigure='true'
 files=(
-    "https://github.com/libsdl-org/SDL_ttf/releases/download/release-${version}/SDL2_ttf-${version}.tar.gz#9dc71ed93487521b107a2c4a9ca6bf43fb62f6bddd5c26b055e6b91418a22053"
+    "https://github.com/libsdl-org/SDL_ttf/releases/download/release-${version}/SDL2_ttf-${version}.tar.gz#0b2bf1e7b6568adbdbc9bb924643f79d9dedafe061fa1ed687d1d9ac4e453bfd"
 )
 depends=(
     'freetype'
