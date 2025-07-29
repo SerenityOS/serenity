0b9e31fc790b37b1ae550aaeac12995cc146b5d6
Zakhar Smokotov <zaharb840@gmail.com>
2025-07-29 14:34:26 +0300

    [ports] update mpg123 to 1.32.4

--- Ports/libmpg123/package.sh
+++ Ports/libmpg123/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libmpg123'
-version='1.31.3'
+version='1.32.4'
 useconfigure='true'
 workdir="mpg123-${version}"
 use_fresh_config_sub='true'
@@ -8,5 +8,5 @@ config_sub_paths=(
     'build/config.sub'
 )
 files=(
-    "https://download.sourceforge.net/project/mpg123/mpg123/${version}/mpg123-${version}.tar.bz2#1ca77d3a69a5ff845b7a0536f783fee554e1041139a6b978f6afe14f5814ad1a"
+    "https://download.sourceforge.net/project/mpg123/mpg123/${version}/mpg123-${version}.tar.bz2#5a99664338fb2f751b662f40ee25804d0c9db6b575dcb5ce741c6dc64224a08a"
 )
