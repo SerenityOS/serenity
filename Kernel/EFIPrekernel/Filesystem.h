7d166eb7ef7cf001901e82e5b2f331989153dcd4
Sönke Holz <sholz8530@gmail.com>
2025-07-30 14:50:39 +0200

    EFIPrekernel: Add functions for basic file I/O

---
+++ Kernel/EFIPrekernel/Filesystem.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2025, Sönke Holz <sholz8530@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/Forward.h>
+#include <Kernel/EFIPrekernel/Error.h>
+#include <Kernel/Firmware/EFI/Protocols/LoadedImage.h>
+#include <Kernel/Firmware/EFI/Protocols/MediaAccess.h>
+
+namespace Kernel {
+
+EFIErrorOr<EFI::FileProtocol*> open_root_directory(EFI::LoadedImageProtocol* loaded_image_protocol);
+EFIErrorOr<EFI::FileProtocol*> open_file(EFI::FileProtocol* base_directory, char16_t const* path, EFI::FileOpenMode open_mode, EFI::FileAttribute attributes = EFI::FileAttribute::None);
+EFIErrorOr<void> close_file(EFI::FileProtocol*);
+EFIErrorOr<ByteBuffer> read_entire_file(EFI::FileProtocol* file);
+
+}
