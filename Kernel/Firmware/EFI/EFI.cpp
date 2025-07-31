60b924b25f946a8299187f47866e42be3cf48bbe
Sönke Holz <sholz8530@gmail.com>
2025-07-31 19:12:09 +0200

    Kernel: Remove periods from EFI::status_description() messages
    These periods are usually not wanted in format strings.
    
    Remove the second sentence for EFI::Status::BufferTooSmall, as it's not
    useful when printing this error.

--- Kernel/Firmware/EFI/EFI.cpp
+++ Kernel/Firmware/EFI/EFI.cpp
@@ -13,73 +13,73 @@ Optional<StringView> status_description(Status status)
     using enum Kernel::EFI::Status;
     switch (status) {
     case Success:
-        return "The operation completed successfully."sv;
+        return "The operation completed successfully"sv;
     case LoadError:
-        return "The image failed to load."sv;
+        return "The image failed to load"sv;
     case InvalidParameter:
-        return "A parameter was incorrect."sv;
+        return "A parameter was incorrect"sv;
     case Unsupported:
-        return "The operation is not supported."sv;
+        return "The operation is not supported"sv;
     case BadBufferSize:
-        return "The buffer was not the proper size for the request."sv;
+        return "The buffer was not the proper size for the request"sv;
     case BufferTooSmall:
-        return "The buffer is not large enough to hold the requested data. The required buffer size is returned in the appropriate parameter when this error occurs."sv;
+        return "The buffer is not large enough to hold the requested data"sv;
     case NotReady:
-        return "There is no data pending upon return."sv;
+        return "There is no data pending upon return"sv;
     case DeviceError:
-        return "The physical device reported an error while attempting the operation."sv;
+        return "The physical device reported an error while attempting the operation"sv;
     case WriteProtected:
-        return "The device cannot be written to."sv;
+        return "The device cannot be written to"sv;
     case OutOfResources:
-        return "A resource has run out."sv;
+        return "A resource has run out"sv;
     case VolumeCorrupted:
-        return "An inconstancy was detected on the file system causing the operating to fail."sv;
+        return "An inconstancy was detected on the file system causing the operating to fail"sv;
     case VolumeFull:
-        return "There is no more space on the file system."sv;
+        return "There is no more space on the file system"sv;
     case NoMedia:
-        return "The device does not contain any medium to perform the operation."sv;
+        return "The device does not contain any medium to perform the operation"sv;
     case MediaChanged:
-        return "The medium in the device has changed since the last access."sv;
+        return "The medium in the device has changed since the last access"sv;
     case NotFound:
-        return "The item was not found."sv;
+        return "The item was not found"sv;
     case AccessDenied:
-        return "Access was denied."sv;
+        return "Access was denied"sv;
     case NoResponse:
-        return "The server was not found or did not respond to the request."sv;
+        return "The server was not found or did not respond to the request"sv;
     case NoMapping:
-        return "A mapping to a device does not exist."sv;
+        return "A mapping to a device does not exist"sv;
     case Timeout:
-        return "The timeout time expired."sv;
+        return "The timeout time expired"sv;
     case NotStarted:
-        return "The protocol has not been started."sv;
+        return "The protocol has not been started"sv;
     case AlreadyStarted:
-        return "The protocol has already been started."sv;
+        return "The protocol has already been started"sv;
     case Aborted:
-        return "The operation was aborted."sv;
+        return "The operation was aborted"sv;
     case ICMPError:
-        return "An ICMP error occurred during the network operation."sv;
+        return "An ICMP error occurred during the network operation"sv;
     case TFTPError:
-        return "A TFTP error occurred during the network operation."sv;
+        return "A TFTP error occurred during the network operation"sv;
     case ProtocolError:
-        return "A protocol error occurred during the network operation."sv;
+        return "A protocol error occurred during the network operation"sv;
     case IncompatibleVersion:
-        return "The function encountered an internal version that was incompatible with a version requested by the caller."sv;
+        return "The function encountered an internal version that was incompatible with a version requested by the caller"sv;
     case SecurityViolation:
-        return "The function was not performed due to a security violation."sv;
+        return "The function was not performed due to a security violation"sv;
     case CRCError:
-        return "A CRC error was detected."sv;
+        return "A CRC error was detected"sv;
     case EndOfMedia:
         return "Beginning or end of media was reached"sv;
     case EndOfFile:
-        return "The end of the file was reached."sv;
+        return "The end of the file was reached"sv;
     case InvalidLanguage:
-        return "The language specified was invalid."sv;
+        return "The language specified was invalid"sv;
     case CompromisedData:
-        return "The security status of the data is unknown or compromised and the data must be updated or replaced to restore a valid security status."sv;
+        return "The security status of the data is unknown or compromised and the data must be updated or replaced to restore a valid security status"sv;
     case IPAddressConflict:
         return "There is an address conflict address allocation"sv;
     case HTTPError:
-        return "A HTTP error occurred during the network operation."sv;
+        return "A HTTP error occurred during the network operation"sv;
     }
 
     return {};
