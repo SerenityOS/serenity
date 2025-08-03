5db3e93e1f5033656ccd9c3dd8485862d43ea10a
Linus Groh <mail@linusgroh.de>
2025-08-03 22:13:22 +0100

    Ports: Update pcre2 to 10.45

--- Ports/pcre2/package.sh
+++ Ports/pcre2/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='pcre2'
-version='10.44'
+version='10.45'
 files=(
-    "https://github.com/PhilipHazel/pcre2/releases/download/pcre2-${version}/pcre2-${version}.tar.gz#86b9cb0aa3bcb7994faa88018292bc704cdbb708e785f7c74352ff6ea7d3175b"
+    "https://github.com/PCRE2Project/pcre2/releases/download/pcre2-${version}/pcre2-${version}.tar.gz#0e138387df7835d7403b8351e2226c1377da804e0737db0e071b48f07c9d12ee"
 )
 useconfigure='true'
