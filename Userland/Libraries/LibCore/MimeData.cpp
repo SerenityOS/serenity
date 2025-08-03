22f5060ba4ba40a36d3d61c90165b15b8f393170
Lucas CHOLLET <lucas.chollet@free.fr>
2025-08-03 22:34:36 +0200

    LibCore: Add the MimeType for DICOM files

--- Userland/Libraries/LibCore/MimeData.cpp
+++ Userland/Libraries/LibCore/MimeData.cpp
@@ -66,6 +66,7 @@ static Array constexpr s_plaintext_suffixes = {
 // See https://www.iana.org/assignments/media-types/<mime-type> for a list of registered MIME types.
 // For example, https://www.iana.org/assignments/media-types/application/gzip
 static Array const s_registered_mime_type = {
+    MimeType { .name = "application/dicom"sv, .common_extensions = { ".dic"sv, ".dicom"sv }, .description = "DICOM medical image data"sv, .magic_bytes = Vector<u8> { 'D', 'I', 'C', 'M' }, .offset = 128 },
     MimeType { .name = "application/gzip"sv, .common_extensions = { ".gz"sv, ".gzip"sv }, .description = "GZIP compressed data"sv, .magic_bytes = Vector<u8> { 0x1F, 0x8B } },
     MimeType { .name = "application/javascript"sv, .common_extensions = { ".js"sv, ".mjs"sv }, .description = "JavaScript source"sv },
     MimeType { .name = "application/json"sv, .common_extensions = { ".json"sv }, .description = "JSON data"sv },
