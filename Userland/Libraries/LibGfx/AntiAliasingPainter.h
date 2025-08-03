3f8db332ee5b24bc9967cc43d8940d2138480d4c
MacDue <macdue@dueutil.tech>
2025-08-03 11:28:59 +0100

    LibGfx: Use stroke styles for dotted and dashed AA lines
    These generally look the same or better and can now handle
    non-axis-aligned lines.

--- Userland/Libraries/LibGfx/AntiAliasingPainter.h
+++ Userland/Libraries/LibGfx/AntiAliasingPainter.h
@@ -83,9 +83,6 @@ private:
 
     Range draw_ellipse_part(IntPoint a_rect, int radius_a, int radius_b, Color alternate_color, bool flip_x_and_y, Optional<Range> x_clip, BlendMode blend_mode);
 
-    void draw_anti_aliased_line(FloatPoint, FloatPoint, Color, float thickness, LineStyle, Color);
-    void draw_dotted_line(IntPoint, IntPoint, Gfx::Color, int thickness);
-
     Painter& m_underlying_painter;
     AffineTransform m_transform;
 };
