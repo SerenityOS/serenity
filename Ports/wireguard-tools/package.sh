f3d21e68033dabab10e82fd2d03271370d385e14
Linus Groh <mail@linusgroh.de>
2025-08-04 19:09:07 +0100

    Ports: Update wireguard-tools to 1.0.20250521

--- Ports/wireguard-tools/package.sh
+++ Ports/wireguard-tools/package.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=wireguard-tools
-version=1.0.20210914
+port='wireguard-tools'
+version='1.0.20250521'
 files=(
-    "https://git.zx2c4.com/wireguard-tools/snapshot/wireguard-tools-${version}.tar.xz#97ff31489217bb265b7ae850d3d0f335ab07d2652ba1feec88b734bc96bd05ac"
+    "https://git.zx2c4.com/wireguard-tools/snapshot/wireguard-tools-${version}.tar.xz#b6f2628b85b1b23cc06517ec9c74f82d52c4cdbd020f3dd2f00c972a1782950e"
 )
-makeopts=("--directory=src")
-# NOTE: PREFIX extends DESTDIR and therefore must not include $SERENITY_INSTALL_ROOT!
-installopts=("--directory=src" "PREFIX=/usr/local")
+makeopts=(
+    '--directory=src'
+)
+installopts=(
+    '--directory=src'
+    # NOTE: PREFIX extends DESTDIR and therefore must not include $SERENITY_INSTALL_ROOT!
+    'PREFIX=/usr/local'
+)
