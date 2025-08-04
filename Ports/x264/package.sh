e900f128a6dd02bc77919f909f464f21425379ee
Linus Groh <mail@linusgroh.de>
2025-08-04 23:54:41 +0100

    Ports: Update x264 to b35605a
    Also update the patch for the configure script to use a portable shebang
    (/bin/bash does not exist on NixOS) and add nasm to the nix shell to
    make it build:
    
    [x264/configure] + ./configure --host=x86_64-pc-serenity --enable-shared
    [x264/configure] Found no assembler
    [x264/configure] Minimum version is nasm-2.13
    [x264/configure] If you really want to compile without asm, configure
                     with --disable-asm.

--- Ports/x264/package.sh
+++ Ports/x264/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='x264'
-version='4613ac3c15fd75cebc4b9f65b7fb95e70a3acce1'
+version='b35605ace3ddf7c1a5d67a2eb553f034aef41d55'
 files=(
-    "https://code.videolan.org/videolan/x264/-/archive/${version}/x264-master.tar.gz#44ce79258656d7dbe06165321cb989cc242a66effa308e25d2f0a197b50f8398"
+    "https://code.videolan.org/videolan/x264/-/archive/${version}/x264-master.tar.gz#cd71a7515b0e9a012e1ac9b1f8415bebcaf6fc97d4db32286642ac4c0fbe24f9"
 )
 useconfigure='true'
 use_fresh_config_sub='true'
