4b166040ab642b368ca3d83ec1556033b9325c2e
Linus Groh <mail@linusgroh.de>
2025-08-04 19:09:07 +0100

    Ports: Update rsync to 3.4.1

--- Ports/rsync/package.sh
+++ Ports/rsync/package.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='rsync'
-version='3.3.0'
+version='3.4.1'
 useconfigure='true'
 use_fresh_config_sub='true'
 files=(
-    "https://download.samba.org/pub/rsync/src/rsync-${version}.tar.gz#7399e9a6708c32d678a72a63219e96f23be0be2336e50fd1348498d07041df90"
+    "https://download.samba.org/pub/rsync/src/rsync-${version}.tar.gz#2924bcb3a1ed8b551fc101f740b9f0fe0a202b115027647cf69850d65fd88c52"
 )
 configopts=(
     "--target=${SERENITY_ARCH}-pc-serenity"
