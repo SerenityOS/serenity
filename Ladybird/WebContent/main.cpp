0c59985214d268a208eaaac104120d72fab3f74b
Sönke Holz <sholz8530@gmail.com>
2025-08-08 22:14:14 +0200

    Ladybird: Remove the Qt Multimedia audio plugin
    Now that ladybird was moved moved to its own repository, we don't need
    to keep unnecessary features like the Qt Multimedia audio backend.
    This means that PulseAudio is now required to have working audio in the
    Qt chrome.
    
    Ladybird only still exists in the SerenityOS repo to make testing LibWeb
    easier.

--- Ladybird/WebContent/main.cpp
+++ Ladybird/WebContent/main.cpp
@@ -39,10 +39,6 @@
 #    include <Ladybird/Qt/EventLoopImplementationQt.h>
 #    include <Ladybird/Qt/RequestManagerQt.h>
 #    include <QCoreApplication>
-
-#    if defined(HAVE_QT_MULTIMEDIA)
-#        include <Ladybird/Qt/AudioCodecPluginQt.h>
-#    endif
 #endif
 
 #if defined(AK_OS_MACOS)
@@ -82,9 +78,7 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
     Web::Platform::ImageCodecPlugin::install(*new Ladybird::ImageCodecPlugin);
 
     Web::Platform::AudioCodecPlugin::install_creation_hook([](auto loader) {
-#if defined(HAVE_QT_MULTIMEDIA)
-        return Ladybird::AudioCodecPluginQt::create(move(loader));
-#elif defined(AK_OS_MACOS) || defined(HAVE_PULSEAUDIO)
+#if defined(AK_OS_MACOS) || defined(HAVE_PULSEAUDIO)
         return Web::Platform::AudioCodecPluginAgnostic::create(move(loader));
 #else
         (void)loader;
