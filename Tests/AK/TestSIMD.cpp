8ef078718764d7d2303225031ef32fdf03048638
Lucas CHOLLET <lucas.chollet@free.fr>
2025-08-09 22:10:47 +0200

    AK/SIMD: Make `clamp` accept any type of SIMDVector

--- Tests/AK/TestSIMD.cpp
+++ Tests/AK/TestSIMD.cpp
@@ -34,3 +34,18 @@ TEST_CASE(exp_approximate)
     EXPECT(fabsf(result[2] - 1.82211880f) <= accuracy);
     EXPECT(fabsf(result[3] - 2.22554093f) <= accuracy);
 }
+
+TEST_CASE(clamp)
+{
+    AK::SIMD::f32x4 v1 = { .2f, .4f, .6f, .8f };
+    v1 = AK::SIMD::clamp(v1, .4f, .6f);
+    EXPECT(v1[0] == v1[1] && v1[1] == .4f);
+    EXPECT(v1[2] == v1[3] && v1[3] == .6f);
+
+    AK::SIMD::i32x4 v2 = { -10, 0, 0, 10 };
+    v2 = AK::SIMD::clamp(v2, -5, 5);
+    EXPECT(v2[0] == -5);
+    EXPECT(v2[1] == 0);
+    EXPECT(v2[2] == 0);
+    EXPECT(v2[3] == 5);
+}
