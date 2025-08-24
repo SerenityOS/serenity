22f5b9f41eb464f293fa16ad97aa73a0a31f8334
Lucas CHOLLET <lucas.chollet@free.fr>
2025-08-24 17:00:59 +0200

    LibGfx/DICOM: Add support for reading basic metadata
    This includes the following tags:
     - Study Date
     - Institution Name
     - Patient's Name
     - Patient's Birth Date
     - Body Part Examined

--- Userland/Libraries/LibGfx/ImageFormats/DICOMLoader.cpp
+++ Userland/Libraries/LibGfx/ImageFormats/DICOMLoader.cpp
@@ -8,6 +8,7 @@
 #include <AK/Array.h>
 #include <AK/BitStream.h>
 #include <AK/CountingStream.h>
+#include <AK/String.h>
 
 namespace Gfx {
 
@@ -20,12 +21,14 @@ struct DataElement {
     u16 group_number {};
     u16 element_number {};
 
+    // Values and their representations are defined here:
+    // https://dicom.nema.org/medical/dicom/current/output/chtml/part05/sect_6.2.html
     Array<u8, 2> value_representation {};
     StringView value_representation_as_string { value_representation.span() };
 
     u32 value_length {};
 
-    u32 value {};
+    Variant<u32, String> value { 0 };
 };
 
 constexpr Array vr_with_small_length = { "AE"sv, "AS"sv, "AT"sv, "CS"sv, "DA"sv, "DS"sv, "DT"sv, "FL"sv, "FD"sv, "IS"sv, "LO"sv, "LT"sv, "PN"sv, "SH"sv, "SL"sv, "SS"sv, "ST"sv, "TM"sv, "UI"sv, "UL"sv, "US"sv };
@@ -59,11 +62,32 @@ ErrorOr<DataElement> read_data_element(Stream& stream, ShouldInterpretValue shou
         element.value_length = TRY(stream.read_value<u32>());
     }
 
+    auto read_string = [&]() -> ErrorOr<String> {
+        ByteBuffer buffer;
+        TRY(buffer.try_resize(element.value_length));
+        TRY(stream.read_until_filled(buffer));
+        return String::from_utf8(StringView { buffer.span() });
+    };
+
     if (should_interpret_value == ShouldInterpretValue::Yes) {
         if (element.value_representation_as_string == "UL") {
             element.value = TRY(stream.read_value<u32>());
         } else if (element.value_representation_as_string == "US") {
             element.value = TRY(stream.read_value<u16>());
+        } else if (element.value_representation_as_string == "PN") {
+            element.value = TRY(TRY(read_string()).replace("^"sv, " "sv, ReplaceMode::All));
+        } else if (element.value_representation_as_string == "LO" || element.value_representation_as_string == "CS") {
+            element.value = TRY(read_string());
+        } else if (element.value_representation_as_string == "DA") {
+            // Format YYYYMMDD
+            auto raw_date = TRY(read_string());
+            StringBuilder builder;
+            builder.append(raw_date.bytes_as_string_view().substring_view(6, 2));
+            builder.append('/');
+            builder.append(raw_date.bytes_as_string_view().substring_view(4, 2));
+            builder.append('/');
+            builder.append(raw_date.bytes_as_string_view().substring_view(0, 4));
+            element.value = TRY(builder.to_string());
         } else {
             // This is not a known type, let's skip the bytes for now.
             TRY(stream.discard(element.value_length));
@@ -90,10 +114,33 @@ ErrorOr<void> read_file_meta_information(Stream& stream)
         return Error::from_string_literal("DICOMImageDecoderPlugin: First element has to be 'File Meta Information Group Length'");
 
     // This simple decoder doesn't care about the other elements of the header, so let's skip it!
-    TRY(stream.discard(file_meta_information_group_length.value));
+    TRY(stream.discard(file_meta_information_group_length.value.get<u32>()));
 
     return {};
 }
+
+struct DICOMMetadata : public Metadata {
+    String institution_name {};
+    String study_date {};
+    String patient_name {};
+    String patient_birth_date {};
+    String body_part_examined {};
+
+private:
+    virtual void fill_main_tags() const override
+    {
+        if (!institution_name.is_empty())
+            m_main_tags.set("Institution Name"sv, institution_name);
+        if (!study_date.is_empty())
+            m_main_tags.set("Study Date"sv, study_date);
+        if (!patient_name.is_empty())
+            m_main_tags.set("Patient's Name"sv, patient_name);
+        if (!patient_birth_date.is_empty())
+            m_main_tags.set("Patient Birth Date"sv, patient_birth_date);
+        if (!body_part_examined.is_empty())
+            m_main_tags.set("Body Part Examined"sv, body_part_examined);
+    }
+};
 
 }
 
@@ -115,6 +162,42 @@ public:
         return {};
     }
 
+    ErrorOr<void> read_optional_metadata(DataElement const& element)
+    {
+        // "Study Date"
+        if (element.group_number == 0x08 && element.element_number == 0x20) {
+            if (element.value.has<String>())
+                m_metadata.study_date = element.value.get<String>();
+        }
+
+        // "Institution Name"
+        if (element.group_number == 0x08 && element.element_number == 0x80) {
+            if (element.value.has<String>())
+                m_metadata.institution_name = element.value.get<String>();
+        }
+
+        // "Patient's Name"
+        // https://dicom.nema.org/medical/dicom/current/output/chtml/part03/sect_C.2.2.html#table_C.2-2
+        if (element.group_number == 0x10 && element.element_number == 0x10) {
+            if (element.value.has<String>())
+                m_metadata.patient_name = element.value.get<String>();
+        }
+
+        // "Patient's Birth Date"
+        if (element.group_number == 0x10 && element.element_number == 0x30) {
+            if (element.value.has<String>())
+                m_metadata.patient_birth_date = element.value.get<String>();
+        }
+
+        // "Body Part Examined"
+        if (element.group_number == 0x18 && element.element_number == 0x15) {
+            if (element.value.has<String>())
+                m_metadata.body_part_examined = element.value.get<String>();
+        }
+
+        return {};
+    }
+
     ErrorOr<void> read_useful_elements()
     {
         // We try to find some useful data for decoding like the image size or the bit depth.
@@ -126,21 +209,23 @@ public:
         while (!last_element.has_value() || last_element->group_number <= us_image_module_group) {
             last_element = TRY(read_data_element(m_stream, ShouldInterpretValue::Yes));
 
+            TRY(read_optional_metadata(*last_element));
+
             if (last_element->group_number == us_image_module_group && last_element->element_number == 2) {
                 // SamplesPerPixel
                 if (last_element->value != 1)
                     return Error::from_string_literal("DICOMImageDecoderPlugin: Unsupported value of SamplesPerPixel");
             } else if (last_element->group_number == us_image_module_group && last_element->element_number == 16) {
                 // Rows
-                m_size.set_height(last_element->value);
+                m_size.set_height(last_element->value.get<u32>());
             } else if (last_element->group_number == us_image_module_group && last_element->element_number == 17) {
                 // Columns
-                m_size.set_width(last_element->value);
+                m_size.set_width(last_element->value.get<u32>());
             } else if (last_element->group_number == us_image_module_group && last_element->element_number == 257) {
                 // BitsStored
                 if (last_element->value != 8 && last_element->value != 16)
                     return Error::from_string_literal("DICOMImageDecoderPlugin: Unsupported value of BitsStored");
-                m_bit_depth = last_element->value;
+                m_bit_depth = last_element->value.get<u32>();
             }
         }
 
@@ -201,6 +286,11 @@ public:
         return m_bitmap;
     }
 
+    DICOMMetadata const& metadata() const
+    {
+        return m_metadata;
+    }
+
 private:
     FixedMemoryStream m_stream;
 
@@ -208,6 +298,8 @@ private:
     IntSize m_size {};
     u32 m_bit_depth {};
     RefPtr<Bitmap> m_bitmap {};
+
+    DICOMMetadata m_metadata;
 };
 
 DICOMImageDecoderPlugin::DICOMImageDecoderPlugin(NonnullOwnPtr<FixedMemoryStream> stream)
@@ -254,6 +346,11 @@ ErrorOr<ImageFrameDescriptor> DICOMImageDecoderPlugin::frame(size_t index, Optio
         TRY(m_context->decode());
 
     return ImageFrameDescriptor { m_context->bitmap(), 0 };
+}
+
+Optional<Metadata const&> DICOMImageDecoderPlugin::metadata()
+{
+    return m_context->metadata();
 }
 
 }
