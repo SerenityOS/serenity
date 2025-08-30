219e0b7032035e22bc9a8cf42f51fae18bbcd23e
Nico Weber <thakis@chromium.org>
2025-08-30 11:03:11 -0400

    LibGfx/JBIG2: Teach JBIG2Writer to write generic immediate regions
    JBIG2Writer can now write arithmetically-coded image data.
    This lets it write basically all images; all remaining stuff is
    just for better compression (and for adding more test files for
    the decoder, which is the main motivation here, really).
    
    generic_region_encoding_procedure() is very similar to
    generic_region_decoding_procedure(), only the loop at the end is
    different (to compress data instead of to decompress it). Maybe
    we can find a way to share more code in the future. Quite a bit
    of support code is already shared, though.
    
    I also need to come up with a way to pass many, many options to
    the encoder. For now, this just hardcodes all the options to
    somewhat useful defaults.
    
    Likewise, we also need a mechanism to manually pick which segments
    to write. A fully white or fully black image does not need an
    immediate generic region, for example. (But for normal `image`
    use, we should also automatically generate a schedule that makes
    sense for a given image.)

--- Userland/Libraries/LibGfx/ImageFormats/JBIG2Writer.cpp
+++ Userland/Libraries/LibGfx/ImageFormats/JBIG2Writer.cpp
@@ -8,18 +8,224 @@
 // https://www.itu.int/rec/T-REC-T.88-201808-I
 // JBIG2Loader.cpp has many spec notes.
 
+#include <AK/NonnullOwnPtr.h>
 #include <AK/StdLibExtras.h>
 #include <AK/Stream.h>
 #include <LibGfx/Bitmap.h>
+#include <LibGfx/ImageFormats/BilevelImage.h>
 #include <LibGfx/ImageFormats/JBIG2Writer.h>
+#include <LibGfx/ImageFormats/QMArithmeticCoder.h>
 
 namespace Gfx {
 
 namespace {
 
+// Similar to 6.2.2 Input parameters, but with an input image.
+struct GenericRegionEncodingInputParameters {
+    bool is_modified_modified_read { false }; // "MMR" in spec.
+    BilevelImage const& image;                // Of dimensions "GBW" x "GBH" in spec terms.
+    u8 gb_template { 0 };
+    bool is_typical_prediction_used { false };          // "TPGDON" in spec.
+    bool is_extended_reference_template_used { false }; // "EXTTEMPLATE" in spec.
+    Optional<BilevelImage const&> skip_pattern {};      // "USESKIP", "SKIP" in spec.
+
+    Array<JBIG2::AdaptiveTemplatePixel, 12> adaptive_template_pixels {}; // "GBATX" / "GBATY" in spec.
+    // FIXME: GBCOLS, GBCOMBOP, COLEXTFLAG
+
+    enum RequireEOFBAfterMMR {
+        No,
+        Yes,
+    } require_eof_after_mmr { RequireEOFBAfterMMR::No };
+};
+
+}
+
+// 6.2 Generic region decoding procedure, but in backwards.
+static ErrorOr<ByteBuffer> generic_region_encoding_procedure(GenericRegionEncodingInputParameters const& inputs, Optional<JBIG2::GenericContexts>& maybe_contexts)
+{
+    // FIXME: Try to come up with a way to share more code with generic_region_decoding_procedure().
+    auto width = inputs.image.width();
+    auto height = inputs.image.height();
+
+    if (inputs.is_modified_modified_read)
+        return Error::from_string_literal("JBIG2Writer: Cannot encode MMR yet");
+
+    auto& contexts = maybe_contexts.value();
+
+    // 6.2.5 Decoding using a template and arithmetic coding
+    if (inputs.is_extended_reference_template_used)
+        return Error::from_string_literal("JBIG2Writer: Cannot encode EXTTEMPLATE yet");
+
+    int number_of_adaptive_template_pixels = inputs.gb_template == 0 ? 4 : 1;
+    for (int i = 0; i < number_of_adaptive_template_pixels; ++i)
+        TRY(check_valid_adaptive_template_pixel(inputs.adaptive_template_pixels[i]));
+
+    if (inputs.skip_pattern.has_value() && (inputs.skip_pattern->width() != width || inputs.skip_pattern->height() != height))
+        return Error::from_string_literal("JBIG2Writer: Invalid USESKIP dimensions");
+
+    if (inputs.skip_pattern.has_value())
+        return Error::from_string_literal("JBIG2Writer: Cannot encode USESKIP yet");
+
+    static constexpr auto get_pixel = [](BilevelImage const& buffer, int x, int y) -> bool {
+        if (x < 0 || x >= (int)buffer.width() || y < 0)
+            return false;
+        return buffer.get_bit(x, y);
+    };
+
+    // Figure 3(a) – Template when GBTEMPLATE = 0 and EXTTEMPLATE = 0,
+    constexpr auto compute_context_0 = [](BilevelImage const& buffer, ReadonlySpan<JBIG2::AdaptiveTemplatePixel> adaptive_pixels, int x, int y) -> u16 {
+        u16 result = 0;
+        for (int i = 0; i < 4; ++i)
+            result = (result << 1) | (u16)get_pixel(buffer, x + adaptive_pixels[i].x, y + adaptive_pixels[i].y);
+        for (int i = 0; i < 3; ++i)
+            result = (result << 1) | (u16)get_pixel(buffer, x - 1 + i, y - 2);
+        for (int i = 0; i < 5; ++i)
+            result = (result << 1) | (u16)get_pixel(buffer, x - 2 + i, y - 1);
+        for (int i = 0; i < 4; ++i)
+            result = (result << 1) | (u16)get_pixel(buffer, x - 4 + i, y);
+        return result;
+    };
+
+    // Figure 4 – Template when GBTEMPLATE = 1
+    auto compute_context_1 = [](BilevelImage const& buffer, ReadonlySpan<JBIG2::AdaptiveTemplatePixel> adaptive_pixels, int x, int y) -> u16 {
+        u16 result = 0;
+        result = (result << 1) | (u16)get_pixel(buffer, x + adaptive_pixels[0].x, y + adaptive_pixels[0].y);
+        for (int i = 0; i < 4; ++i)
+            result = (result << 1) | (u16)get_pixel(buffer, x - 1 + i, y - 2);
+        for (int i = 0; i < 5; ++i)
+            result = (result << 1) | (u16)get_pixel(buffer, x - 2 + i, y - 1);
+        for (int i = 0; i < 3; ++i)
+            result = (result << 1) | (u16)get_pixel(buffer, x - 3 + i, y);
+        return result;
+    };
+
+    // Figure 5 – Template when GBTEMPLATE = 2
+    auto compute_context_2 = [](BilevelImage const& buffer, ReadonlySpan<JBIG2::AdaptiveTemplatePixel> adaptive_pixels, int x, int y) -> u16 {
+        u16 result = 0;
+        result = (result << 1) | (u16)get_pixel(buffer, x + adaptive_pixels[0].x, y + adaptive_pixels[0].y);
+        for (int i = 0; i < 3; ++i)
+            result = (result << 1) | (u16)get_pixel(buffer, x - 1 + i, y - 2);
+        for (int i = 0; i < 4; ++i)
+            result = (result << 1) | (u16)get_pixel(buffer, x - 2 + i, y - 1);
+        for (int i = 0; i < 2; ++i)
+            result = (result << 1) | (u16)get_pixel(buffer, x - 2 + i, y);
+        return result;
+    };
+
+    // Figure 6 – Template when GBTEMPLATE = 3
+    auto compute_context_3 = [](BilevelImage const& buffer, ReadonlySpan<JBIG2::AdaptiveTemplatePixel> adaptive_pixels, int x, int y) -> u16 {
+        u16 result = 0;
+        result = (result << 1) | (u16)get_pixel(buffer, x + adaptive_pixels[0].x, y + adaptive_pixels[0].y);
+        for (int i = 0; i < 5; ++i)
+            result = (result << 1) | (u16)get_pixel(buffer, x - 3 + i, y - 1);
+        for (int i = 0; i < 4; ++i)
+            result = (result << 1) | (u16)get_pixel(buffer, x - 4 + i, y);
+        return result;
+    };
+
+    u16 (*compute_context)(BilevelImage const&, ReadonlySpan<JBIG2::AdaptiveTemplatePixel>, int, int);
+    if (inputs.gb_template == 0)
+        compute_context = compute_context_0;
+    else if (inputs.gb_template == 1)
+        compute_context = compute_context_1;
+    else if (inputs.gb_template == 2)
+        compute_context = compute_context_2;
+    else {
+        VERIFY(inputs.gb_template == 3);
+        compute_context = compute_context_3;
+    }
+
+    // "The values of the pixels in this neighbourhood define a context. Each context has its own adaptive probability estimate
+    //  used by the arithmetic coder (see Annex E)."
+    // "* Decode the current pixel by invoking the arithmetic entropy decoding procedure, with CX set to the value formed by
+    //    concatenating the label "GB" and the 10-16 pixel values gathered in CONTEXT."
+    // Implementor's note: What this is supposed to mean is that we have a bunch of independent contexts, and we pick the
+    // context for the current pixel based on pixel values in the neighborhood. The "GB" part just means this context is
+    // independent from other contexts in the spec. They are passed in to this function.
+
+    // Figure 8 – Reused context for coding the SLTP value when GBTEMPLATE is 0
+    constexpr u16 sltp_context_for_template_0 = 0b10011'0110010'0101;
+
+    // Figure 9 – Reused context for coding the SLTP value when GBTEMPLATE is 1
+    constexpr u16 sltp_context_for_template_1 = 0b0011'110010'101;
+
+    // Figure 10 – Reused context for coding the SLTP value when GBTEMPLATE is 2
+    constexpr u16 sltp_context_for_template_2 = 0b001'11001'01;
+
+    // Figure 11 – Reused context for coding the SLTP value when GBTEMPLATE is 3
+    constexpr u16 sltp_context_for_template_3 = 0b011001'0101;
+
+    u16 sltp_context = [](u8 gb_template) {
+        if (gb_template == 0)
+            return sltp_context_for_template_0;
+        if (gb_template == 1)
+            return sltp_context_for_template_1;
+        if (gb_template == 2)
+            return sltp_context_for_template_2;
+        VERIFY(gb_template == 3);
+        return sltp_context_for_template_3;
+    }(inputs.gb_template);
+
+    // 6.2.5.7 Decoding the bitmap
+    QMArithmeticEncoder encoder = TRY(QMArithmeticEncoder::initialize(0));
+
+    // "1) Set:
+    //         LTP = 0"
+    bool ltp = false; // "Line (uses) Typical Prediction" maybe?
+
+    // " 2) Create a bitmap GBREG of width GBW and height GBH pixels."
+    // auto result = TRY(BilevelImage::create(inputs.region_width, inputs.region_height));
+
+    // "3) Decode each row as follows:"
+    for (size_t y = 0; y < height; ++y) {
+        // "a) If all GBH rows have been decoded then the decoding is complete; proceed to step 4)."
+        // "b) If TPGDON is 1, then decode a bit using the arithmetic entropy coder..."
+        if (inputs.is_typical_prediction_used) {
+            bool is_line_identical_to_previous_line = false;
+            if (y > 0) {
+                // "i) If the current row of GBREG is identical to the row immediately above, then SLTP = 1; otherwise SLTP = 0."
+                is_line_identical_to_previous_line = true;
+                for (size_t x = 0; x < width; ++x) {
+                    if (inputs.image.get_bit(x, y) != inputs.image.get_bit(x, y - 1)) {
+                        is_line_identical_to_previous_line = false;
+                        break;
+                    }
+                }
+            }
+
+            // "SLTP" in spec. "Swap LTP" or "Switch LTP" maybe?
+            bool sltp = ltp ^ is_line_identical_to_previous_line;
+            encoder.encode_bit(sltp, contexts.contexts[sltp_context]);
+            ltp = is_line_identical_to_previous_line;
+            if (ltp)
+                continue;
+        }
+
+        // "d) If LTP = 0 then, from left to right, decode each pixel of the current row of GBREG. The procedure for each
+        //     pixel is as follows:"
+        for (size_t x = 0; x < width; ++x) {
+            // "i) If USESKIP is 1 and the pixel in the bitmap SKIP at the location corresponding to the current pixel is 1,
+            //     then set the current pixel to 0."
+            if (inputs.skip_pattern.has_value() && inputs.skip_pattern->get_bit(x, y))
+                continue;
+
+            // "ii) Otherwise:"
+            u16 context = compute_context(inputs.image, inputs.adaptive_template_pixels, x, y);
+            encoder.encode_bit(inputs.image.get_bit(x, y), contexts.contexts[context]);
+        }
+    }
+
+    // "4) After all the rows have been decoded, the current contents of the bitmap GBREG are the results that shall be
+    //     obtained by every decoder, whether it performs this exact sequence of steps or not."
+    // In the encoding case, this means the compressed data is complete.
+    return encoder.finalize();
+}
+
+namespace {
+
 struct JBIG2WritingContext {
     JBIG2Writer::Options options;
-    Gfx::Bitmap const& bitmap;
+    Gfx::BilevelImage const& bilevel_image;
     u32 next_segment_number { 0 };
 };
 
@@ -126,6 +332,87 @@ static ErrorOr<void> encode_segment_header(Stream& stream, JBIG2::SegmentHeader 
     return {};
 }
 
+static ErrorOr<void> encode_region_segment_information_field(Stream& stream, JBIG2::RegionSegmentInformationField const& region_information)
+{
+    // 7.4.8 Page information segment syntax
+    TRY(stream.write_value<BigEndian<u32>>(region_information.width));
+    TRY(stream.write_value<BigEndian<u32>>(region_information.height));
+    TRY(stream.write_value<BigEndian<u32>>(region_information.x_location));
+    TRY(stream.write_value<BigEndian<u32>>(region_information.y_location));
+    TRY(stream.write_value<u8>(region_information.flags));
+
+    return {};
+}
+
+static ErrorOr<void> encode_immediate_lossless_generic_region(JBIG2WritingContext& context, Stream& stream)
+{
+    // FIXME: Add options for MMR, GBTEMPLATE, TPGDON, EXTTEMPLATE, USESKIP, GBATX/GBATY.
+    GenericRegionEncodingInputParameters inputs { .image = context.bilevel_image };
+    inputs.is_modified_modified_read = false;
+    inputs.gb_template = 0;
+    inputs.is_typical_prediction_used = true;
+    inputs.is_extended_reference_template_used = false;
+    inputs.adaptive_template_pixels[0] = { -1, -1 };
+    inputs.adaptive_template_pixels[1] = { 0, -1 };
+    inputs.adaptive_template_pixels[2] = { -1, 0 };
+    inputs.adaptive_template_pixels[3] = { 1, -1 };
+    inputs.require_eof_after_mmr = GenericRegionEncodingInputParameters::RequireEOFBAfterMMR::No;
+
+    int number_of_adaptive_template_pixels = inputs.gb_template == 0 ? 4 : 1;
+
+    Optional<JBIG2::GenericContexts> contexts = JBIG2::GenericContexts { inputs.gb_template };
+    auto data = TRY(generic_region_encoding_procedure(inputs, contexts));
+
+    // 7.4.6 Generic region segment syntax
+    JBIG2::SegmentHeader generic_region_segment_header;
+    generic_region_segment_header.segment_number = context.next_segment_number++;
+    generic_region_segment_header.type = JBIG2::SegmentType::ImmediateLosslessGenericRegion;
+    generic_region_segment_header.page_association = 1;
+
+    // FIXME: Add option to set size to OptionalNone for immediate generic regions (7.2.7).
+    // FIXME: Add option to write too-high height in region information field if data_length is OptionalNone
+    //        (the actual height is written after the data then).
+    generic_region_segment_header.data_length = sizeof(JBIG2::RegionSegmentInformationField) + 1 + 2 * number_of_adaptive_template_pixels + data.size();
+    TRY(encode_segment_header(stream, generic_region_segment_header));
+
+    JBIG2::RegionSegmentInformationField region_information;
+    region_information.width = context.bilevel_image.width();
+    region_information.height = context.bilevel_image.height();
+    region_information.x_location = 0;
+    region_information.y_location = 0;
+    region_information.flags = 0;
+
+    JBIG2::CombinationOperator combination_operator = context.options.default_combination_operator;
+    region_information.flags |= static_cast<u8>(combination_operator);
+    bool is_color_bitmap = false; // FIXME: Add support for colors one day.
+    if (is_color_bitmap)
+        region_information.flags |= 0x8;
+
+    TRY(encode_region_segment_information_field(stream, region_information));
+
+    u8 flags = 0;
+    bool uses_mmr = false; // FIXME: Add an option for this.
+    if (uses_mmr)
+        flags |= 1;
+    flags |= (inputs.gb_template & 3) << 1;
+    if (inputs.is_typical_prediction_used)
+        flags |= 0x8;
+    if (inputs.is_extended_reference_template_used)
+        flags |= 0x10;
+    TRY(stream.write_value<u8>(flags));
+
+    for (int i = 0; i < number_of_adaptive_template_pixels; ++i) {
+        TRY(stream.write_value<i8>(inputs.adaptive_template_pixels[i].x));
+        TRY(stream.write_value<i8>(inputs.adaptive_template_pixels[i].y));
+    }
+    TRY(stream.write_until_depleted(data));
+
+    if (!generic_region_segment_header.data_length.has_value())
+        TRY(stream.write_value<BigEndian<u32>>(region_information.height));
+
+    return {};
+}
+
 static ErrorOr<void> encode_page_information(JBIG2WritingContext& context, Stream& stream, JBIG2::PageInformationSegment const& page_information)
 {
     // 7.4.8 Page information segment syntax
@@ -159,8 +446,8 @@ static ErrorOr<void> encode_end_of_page(JBIG2WritingContext& context, Stream& st
 
 static ErrorOr<void> fill_page_information(JBIG2WritingContext& context, JBIG2::PageInformationSegment& page_information)
 {
-    page_information.bitmap_width = context.bitmap.width();
-    page_information.bitmap_height = context.bitmap.height();
+    page_information.bitmap_width = context.bilevel_image.width();
+    page_information.bitmap_height = context.bilevel_image.height();
     page_information.page_x_resolution = 0;
     page_information.page_y_resolution = 0;
 
@@ -198,12 +485,16 @@ static ErrorOr<void> fill_page_information(JBIG2WritingContext& context, JBIG2::
 
 ErrorOr<void> JBIG2Writer::encode(Stream& stream, Bitmap const& bitmap, Options const& options)
 {
-    JBIG2WritingContext context { .options = options, .bitmap = bitmap };
+    auto bilevel_image = TRY(BilevelImage::create_from_bitmap(bitmap, DitheringAlgorithm::FloydSteinberg));
+
+    JBIG2WritingContext context { .options = options, .bilevel_image = *bilevel_image };
     TRY(encode_jbig2_header(stream));
 
     JBIG2::PageInformationSegment page_info;
     TRY(fill_page_information(context, page_info));
     TRY(encode_page_information(context, stream, page_info));
+
+    TRY(encode_immediate_lossless_generic_region(context, stream));
 
     TRY(encode_end_of_page(context, stream));
 
