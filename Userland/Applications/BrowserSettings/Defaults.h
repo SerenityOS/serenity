564f1938f081947f408e29b33090ec82fd12505d
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2025-09-01 21:00:48 +0200

    Browser: Hide bookmarkers bar by default because it's empty
    Except for the anon user that already has a prefilled
    bookmarkers.json file in the .config folder.

--- Userland/Applications/BrowserSettings/Defaults.h
+++ Userland/Applications/BrowserSettings/Defaults.h
@@ -14,7 +14,7 @@ static constexpr StringView default_homepage_url = "resource://html/misc/welcome
 static constexpr StringView default_new_tab_url = "about:newtab"sv;
 static constexpr StringView default_color_scheme = "auto"sv;
 static constexpr bool default_enable_content_filters = true;
-static constexpr bool default_show_bookmarks_bar = true;
+static constexpr bool default_show_bookmarks_bar = false;
 static constexpr bool default_close_download_widget_on_finish = false;
 static constexpr bool default_allow_autoplay_on_all_websites = false;
 
