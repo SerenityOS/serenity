7ca2eb51f0b3e8d6d046ec1a1efaf2fa28110548
Nico Weber <thakis@chromium.org>
2025-09-01 10:39:21 -0400

    LibGfx/ImageFormats: Add BilevelImage::create_from_bitmap()
    For now, this computes a threshold using Otsu's method, and then
    either does global thresholding or Floyd-Steinberg error diffusion
    dithering. It converts to grayscale using luminosity, and then
    converts that to linear color before doing histogramming, threshold
    computation, and dithering.
    
    We should add more dithering methods in the future, and also come
    up with a way to make at least some of the dithering methods work
    for palettes created by median_cut() instead of just bilevel data,
    but for now this is a good start.
    
    This clamps pixel + error to 0..255. I think that shouldn't have
    a big visual effect, but it might be worth having two i32 helper
    scanlines to check if that looks any different.

--- Userland/Libraries/LibGfx/ImageFormats/BilevelImage.h
+++ Userland/Libraries/LibGfx/ImageFormats/BilevelImage.h
@@ -11,9 +11,19 @@
 
 namespace Gfx {
 
+enum class DitheringAlgorithm {
+    None,
+    FloydSteinberg,
+    // FIXME: Add Atkinson, Bayer in 2-3 sizes, BlueNoise, Hilbert / Peano space-filling, ...
+    // https://surma.dev/things/ditherpunk/
+    // https://tannerhelland.com/2012/12/28/dithering-eleven-algorithms-source-code.html
+};
+
 class BilevelImage {
 public:
     static ErrorOr<NonnullOwnPtr<BilevelImage>> create(size_t width, size_t height);
+    static ErrorOr<NonnullOwnPtr<BilevelImage>> create_from_bitmap(Gfx::Bitmap const& bitmap, DitheringAlgorithm dithering_algorithm);
+
     bool get_bit(size_t x, size_t y) const;
     void set_bit(size_t x, size_t y, bool b);
     void fill(bool b);
