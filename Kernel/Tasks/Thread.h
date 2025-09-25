c0d2a3c324512311d985cee6c514945b496ed838
implicitfield <114500360+implicitfield@users.noreply.github.com>
2025-09-25 13:17:10 +0300

    Kernel: Remove Thread::try_dispatch_one_pending_signal
    This is no longer used as of the last commit.

--- Kernel/Tasks/Thread.h
+++ Kernel/Tasks/Thread.h
@@ -876,7 +876,6 @@ public:
     void set_dump_backtrace_on_finalization() { m_dump_backtrace_on_finalization = true; }
 
     DispatchSignalResult dispatch_one_pending_signal();
-    DispatchSignalResult try_dispatch_one_pending_signal(u8 signal);
     DispatchSignalResult dispatch_signal(u8 signal);
     void check_dispatch_pending_signal();
     [[nodiscard]] bool has_unmasked_pending_signals() const { return m_have_any_unmasked_pending_signals.load(AK::memory_order_consume); }
