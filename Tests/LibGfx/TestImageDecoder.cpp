466795f84e7c03637a3b04a9544be18e4af4db1e
Nico Weber <thakis@chromium.org>
2025-09-27 14:13:13 -0400

    Tests/LibGfx: Add jbig2 test for 32-bit page associations, EOF segment
    This just works, but wasn't covered by tests previously.

--- Tests/LibGfx/TestImageDecoder.cpp
+++ Tests/LibGfx/TestImageDecoder.cpp
@@ -356,6 +356,7 @@ TEST_CASE(test_jbig2_decode)
 
     Array test_inputs = {
         TEST_INPUT("jbig2/bitmap.jbig2"sv),
+        TEST_INPUT("jbig2/bitmap-p32-eof.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-customat.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-tpgdon.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-customat-tpgdon.jbig2"sv),
