9fcfbe7631b428d23547e2f2e5169590384987d3
Nico Weber <thakis@chromium.org>
2025-09-28 21:06:44 -0400

    Tests/LibGfx: Add a JBIG2 file with one trailing 7FFF stripped
    Changing `trailing_7fff_handling` in jbig2_generic_region_from_json() to
    default to `Trailing7FFFHandling::Remove` showed that only a single
    generated jbig2 file changed: bitmap-customat.jbig2.
    
    This is a copy of bitmap-customat.json with
    `"strip_trailing_7fffs": true` added.
    
    The generated bitmap-trailing-7fff-stripped.jbig2 is identical to
    bitmap-trailing.jbig2 except that instead of ending in `ff 7fff ac`,
    the image data ends in `ffac` (and the field storing the size of
    the image data changes from 0111 to 010f to match).
    
    All of Preview.app, PDFium, pdf.js can decode this file.

--- Tests/LibGfx/TestImageDecoder.cpp
+++ Tests/LibGfx/TestImageDecoder.cpp
@@ -374,6 +374,7 @@ TEST_CASE(test_jbig2_decode)
         TEST_INPUT("jbig2/bitmap-template3-customat.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-template3-tpgdon.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-template3-customat-tpgdon.jbig2"sv),
+        TEST_INPUT("jbig2/bitmap-trailing-7fff-stripped.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-symbol.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-symbol-negative-sbdsoffset.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-symbol-textrefine.jbig2"sv),
