23ba2d49683c0b742b8ab68a6e1aed48eac96399
Nico Weber <thakis@chromium.org>
2025-10-05 19:55:49 -0400

    LibGfx/JBIG2: Allow striped JBIG2 with no EndOfStripe for last stripe
    This happens in practice, e.g. in 0000857.pdf page 1.
    
    The spec only says "A page whose height was originally unknown must
    contain at least one end of stripe segment." This suggests that at
    least for images with known height, no explicit terminating EndOfStripe
    is needed.
    
    Tweak the code to not error out for standalone JBIG2 files. In PDFs,
    no EndOfPage is present, so this code wasn't hit for PDFs.
    
    Also add a test.

--- Tests/LibGfx/TestImageDecoder.cpp
+++ Tests/LibGfx/TestImageDecoder.cpp
@@ -366,6 +366,7 @@ TEST_CASE(test_jbig2_decode)
         TEST_INPUT("jbig2/bitmap-mmr.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-stripe.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-stripe-initially-unknown-height.jbig2"sv),
+        TEST_INPUT("jbig2/bitmap-stripe-last-implicit.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-stripe-single.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-template1.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-template1-customat.jbig2"sv),
