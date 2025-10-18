e04179c246f5bdefd277f8c9cbf9036cce64d283
MacDue <macdue@dueutil.tech>
2025-10-18 20:49:06 +0100

    LibPDF: Move text rendering strategy selection into SimpleFont
    Seems a little neater. This excludes Type0Fonts for now, but it looks
    like not much of the implementation would be shared.
    
    No behaviour change intended.

--- Userland/Libraries/LibPDF/Fonts/SimpleFont.h
+++ Userland/Libraries/LibPDF/Fonts/SimpleFont.h
@@ -14,12 +14,12 @@ namespace PDF {
 class SimpleFont : public PDFFont {
 public:
     PDFErrorOr<Gfx::FloatPoint> draw_string(Gfx::Painter&, Gfx::FloatPoint, ByteString const&, Renderer const&) override;
-    PDFErrorOr<Gfx::FloatPoint> append_text_path(Gfx::Path&, Gfx::FloatPoint, ByteString const&, Renderer const&) override;
 
 protected:
     PDFErrorOr<void> initialize(Document* document, NonnullRefPtr<DictObject> const& dict, float font_size) override;
     virtual Optional<float> get_glyph_width(u8 char_code) const = 0;
     virtual PDFErrorOr<void> draw_glyph(Gfx::Painter& painter, Gfx::FloatPoint point, float width, u8 char_code, Renderer const&) = 0;
+
     virtual PDFErrorOr<void> append_glyph_path(Gfx::Path&, Gfx::FloatPoint, u8, Renderer const&)
     {
         return Error { Error::Type::RenderingUnsupported, "append_glyph_path not implemented for font" };
@@ -33,6 +33,10 @@ protected:
 private:
     template<typename Callback>
     PDFErrorOr<Gfx::FloatPoint> for_each_glyph_position(Gfx::FloatPoint, ByteString const&, Renderer const&, Callback callback);
+
+    PDFErrorOr<Gfx::FloatPoint> append_text_path(Gfx::Path&, Gfx::FloatPoint, ByteString const&, Renderer const&);
+    PDFErrorOr<Gfx::FloatPoint> draw_transformed_glyphs(Gfx::Painter&, Gfx::FloatPoint, ByteString const&, Renderer const&);
+    PDFErrorOr<Gfx::FloatPoint> draw_axis_aligned_glyphs(Gfx::Painter&, Gfx::FloatPoint, ByteString const&, Renderer const&);
 
     RefPtr<Encoding> m_encoding;
     RefPtr<StreamObject> m_to_unicode;
