SerenityOS/serenity

commit 0388c828be70e7c841f8fded915b6fb82765d861

LibGUI: Remove GUI::Variant::Variant(JsonValue const&)
Let's force callers of Variant constructor to know the type of the
object they are constructing.

Userland/Libraries/LibGUI/JsonArrayModel.cpp
@@ -137,7 +137,7 @@ Variant JsonArrayModel::data(ModelIndex const& index, ModelRole role) const
         if (!data.has_value())
             return "";
         if (data->is_number())
-            return data.value();
+            return data->serialized<StringBuilder>();
         return data->as_string();
     }
 

Userland/Libraries/LibGUI/Variant.cpp
@@ -13,54 +13,6 @@
 
 namespace GUI {
 
-Variant::Variant(JsonValue const& value)
-{
-    *this = value;
-}
-
-Variant& Variant::operator=(JsonValue const& value)
-{
-    if (value.is_null())
-        return *this;
-
-    if (value.is_i32()) {
-        set(value.as_i32());
-        return *this;
-    }
-
-    if (value.is_u32()) {
-        set(value.as_u32());
-        return *this;
-    }
-
-    if (value.is_i64()) {
-        set(value.as_i64());
-        return *this;
-    }
-
-    if (value.is_u64()) {
-        set(value.as_u64());
-        return *this;
-    }
-
-    if (value.is_double()) {
-        set(value.as_double());
-        return *this;
-    }
-
-    if (value.is_string()) {
-        set(value.as_string());
-        return *this;
-    }
-
-    if (value.is_bool()) {
-        set(Detail::Boolean { value.as_bool() });
-        return *this;
-    }
-
-    VERIFY_NOT_REACHED();
-}
-
 bool Variant::operator==(Variant const& other) const
 {
     return visit([&]<typename T>(T const& own_value) {

Userland/Libraries/LibGUI/Variant.h
@@ -29,8 +29,6 @@ public:
     using Detail::VariantUnderlyingType::Variant;
     using Detail::VariantUnderlyingType::operator=;
 
-    Variant(JsonValue const&);
-    Variant& operator=(JsonValue const&);
     Variant(bool v)
         : Variant(Detail::Boolean { v })
     {

commit 28df3d14f8f903276501064dc36da7d4f40947f2

LibGUI: Don't convert JSON numbers to strings in JsonArrayModel
This makes sorting the memory map entries by size in the process info
window work properly. Previously, the memory region sizes were strings,
so we sorted them in alphabetical order.

This likely regressed in 0388c828be.

Userland/Libraries/LibGUI/JsonArrayModel.cpp
@@ -139,7 +139,7 @@ Variant JsonArrayModel::data(ModelIndex const& index, ModelRole role) const
         if (!data.has_value())
             return "";
         if (data->is_number())
-            return data->serialized<StringBuilder>();
+            return data->as_number();
         return data->as_string();
     }
 