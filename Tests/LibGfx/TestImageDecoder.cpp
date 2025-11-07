07e71285adaf8913018d86bbbfc8efec87f11c19
Nico Weber <thakis@chromium.org>
2025-11-06 22:51:37 -0500

    LibGfx/JBIG2+Tests: Add tables C and E, test tables C, E, G, I, L
    The new json file is identical to bitmap-symbol-symhuff-texthuff,
    except that it sets height and width table selection in the symbol
    segment both to 1, and the two s and the one t table in the text
    segment all to 1.
    
    PDFium, pdf.js, mutool / jbig2dec can decode this, Preview.app can't.

--- Tests/LibGfx/TestImageDecoder.cpp
+++ Tests/LibGfx/TestImageDecoder.cpp
@@ -376,6 +376,7 @@ TEST_CASE(test_jbig2_decode)
         TEST_INPUT("jbig2/bitmap-symbol.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-symbol-negative-sbdsoffset.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-symbol-symhuff-texthuff.jbig2"sv),
+        TEST_INPUT("jbig2/bitmap-symbol-symhuffB5B3-texthuffB7B9B12.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-symbol-textrefine.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-symbol-textrefine-customat.jbig2"sv),
         TEST_INPUT("jbig2/symbol-textrefine-negative-delta-width.jbig2"sv),
