SerenityOS/serenity

commit 4498dff68b564f4a473f2a1c0c6d2f3995c7e076

AK: Make `Default` be the default formatting mode for Bytes
There is no way to opt out of the HexDump formatting mode. So let's use
the default `Span` formatter and allow the user to opt in the HexDump
mode for `Span<u8>` aka `Bytes`.

This is mainly useful to print `Vector<u8>`, as the `Vector` formatter
is implemented on top of the `Span` formatter.

AK/Format.h
@@ -438,19 +438,18 @@ struct Formatter<StringView> : StandardFormatter {
 
 template<typename T>
 requires(HasFormatter<T>)
-struct Formatter<ReadonlySpan<T>> : StandardFormatter {
-    Formatter() = default;
-    explicit Formatter(StandardFormatter formatter)
-        : StandardFormatter(move(formatter))
-    {
-    }
-
+struct Formatter<ReadonlySpan<T>> : Formatter<StringView> {
     ErrorOr<void> format(FormatBuilder& builder, ReadonlySpan<T> value)
     {
         if (m_mode == Mode::Pointer) {
             Formatter<FlatPtr> formatter { *this };
             TRY(formatter.format(builder, reinterpret_cast<FlatPtr>(value.data())));
             return {};
+        }
+
+        if constexpr (IsSame<T, u8>) {
+            if (m_mode == Mode::HexDump)
+                return Formatter<StringView>::format(builder, value);
         }
 
         if (m_sign_mode != FormatBuilder::SignMode::Default)
@@ -499,26 +498,6 @@ struct Formatter<Vector<T, inline_capacity>> : Formatter<ReadonlySpan<T>> {
     {
         return Formatter<ReadonlySpan<T>>::format(builder, value.span());
     }
-};
-
-template<>
-struct Formatter<ReadonlyBytes> : Formatter<StringView> {
-    ErrorOr<void> format(FormatBuilder& builder, ReadonlyBytes value)
-    {
-        if (m_mode == Mode::Pointer) {
-            Formatter<FlatPtr> formatter { *this };
-            return formatter.format(builder, reinterpret_cast<FlatPtr>(value.data()));
-        }
-        if (m_mode == Mode::Default || m_mode == Mode::HexDump) {
-            m_mode = Mode::HexDump;
-            return Formatter<StringView>::format(builder, value);
-        }
-        return Formatter<StringView>::format(builder, value);
-    }
-};
-
-template<>
-struct Formatter<Bytes> : Formatter<ReadonlyBytes> {
 };
 
 // FIXME: Printing raw char pointers is inherently dangerous. Remove this and

Tests/AK/TestFormat.cpp
@@ -397,6 +397,18 @@ TEST_CASE(vector_format)
     }
 }
 
+TEST_CASE(read_only_bytes_format)
+{
+    {
+        Vector<u8> v { 1, 2, 3, 4 };
+        EXPECT_EQ(ByteString::formatted("{}", v), "[ 1, 2, 3, 4 ]");
+    }
+    {
+        Vector<u8> v { 1, 2, 3, 4 };
+        EXPECT_EQ(ByteString::formatted("{:hex-dump}", v), "01020304");
+    }
+}
+
 TEST_CASE(format_wchar)
 {
     EXPECT_EQ(ByteString::formatted("{}", L'a'), "a");

commit 5e541646974b04f5f46c6020e165c56bb96f5dc0

AK: Format Bytes like a StringView in non-default modes
This fixes a crash when formatting Bytes with {:s}. This regression was
introduced in 4498dff as it only redirected the HexDump mode to
StringView.

AK/Format.h
@@ -448,7 +448,7 @@ struct Formatter<ReadonlySpan<T>> : Formatter<StringView> {
         }
 
         if constexpr (IsSame<T, u8>) {
-            if (m_mode == Mode::HexDump)
+            if (m_mode == Mode::Character || m_mode == Mode::HexDump || m_mode == Mode::String)
                 return Formatter<StringView>::format(builder, value);
         }
 

Tests/AK/TestFormat.cpp
@@ -407,6 +407,14 @@ TEST_CASE(read_only_bytes_format)
         Vector<u8> v { 1, 2, 3, 4 };
         EXPECT_EQ(ByteString::formatted("{:hex-dump}", v), "01020304");
     }
+    {
+        Vector<u8> v { 97, 98, 99, 100 };
+        EXPECT_EQ(ByteString::formatted("{:s}", v), "abcd");
+    }
+    {
+        Vector<u8> v { 97, 98, 99, 100 };
+        EXPECT_EQ(ByteString::formatted("{:c}", v), "abcd");
+    }
 }
 
 TEST_CASE(format_wchar)