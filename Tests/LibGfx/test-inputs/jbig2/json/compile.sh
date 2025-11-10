e7e20d48731bf5c196486f71312f0d4cbf79f04e
Nico Weber <thakis@chromium.org>
2025-11-10 13:43:20 -0500

    Tests/LibGfx: Make JBIG2 compilation script not echo command
    This was useful a while ago, but is just noise now.

--- Tests/LibGfx/test-inputs/jbig2/json/compile.sh
+++ Tests/LibGfx/test-inputs/jbig2/json/compile.sh
@@ -8,6 +8,5 @@ JBIG2_FROM_JSON=$LAGOM_BUILD/bin/jbig2-from-json
 
 for f in "$DIR"/*.json; do
   f_jb2="$DIR"/../$(basename "${f%.json}.jbig2")
-  echo "$JBIG2_FROM_JSON" -o "$f_jb2" "$f"
   "$JBIG2_FROM_JSON" -o "$f_jb2" "$f"
 done
