eb66e308699d96d381aee2e60e72019a879e8a72
Nico Weber <thakis@chromium.org>
2025-11-12 08:00:14 -0500

    Tests/LibGfx: Make jbig2 compiler script print failing command
    This script used to print all commands it ran. That was too noisy,
    but it's useful to see failing commands. So add back printing of
    commands, but only print them when they fail.

--- Tests/LibGfx/test-inputs/jbig2/json/compile.sh
+++ Tests/LibGfx/test-inputs/jbig2/json/compile.sh
@@ -8,5 +8,9 @@ JBIG2_FROM_JSON=$LAGOM_BUILD/bin/jbig2-from-json
 
 for f in "$DIR"/*.json; do
   f_jb2="$DIR"/../$(basename "${f%.json}.jbig2")
-  "$JBIG2_FROM_JSON" -o "$f_jb2" "$f"
+  if ! "$JBIG2_FROM_JSON" -o "$f_jb2" "$f"; then
+    echo failed to run:
+    echo "$JBIG2_FROM_JSON" -o "$f_jb2" "$f"
+    exit 1
+  fi
 done
