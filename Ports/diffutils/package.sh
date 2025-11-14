08d662b4b81829d510e2f68727d1f8ec61814ab0
Linus Groh <mail@linusgroh.de>
2025-11-14 00:59:18 +0000

    Ports: Update diffutils to 3.12

--- Ports/diffutils/package.sh
+++ Ports/diffutils/package.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='diffutils'
-version='3.10'
+version='3.12'
 depends=(
     'libiconv'
 )
 files=(
-    "https://ftpmirror.gnu.org/gnu/diffutils/diffutils-${version}.tar.xz#90e5e93cc724e4ebe12ede80df1634063c7a855692685919bfe60b556c9bd09e"
+    "https://ftpmirror.gnu.org/gnu/diffutils/diffutils-${version}.tar.xz#7c8b7f9fc8609141fdea9cece85249d308624391ff61dedaf528fcb337727dfd"
 )
 useconfigure='true'
-use_fresh_config_sub='true'
-config_sub_paths=(
-    'build-aux/config.sub'
+configopts=(
+    # [diffutils/configure] checking whether strcasecmp works...
+    # [diffutils/configure] configure: error: cannot run test program while cross compiling
+    'gl_cv_func_strcasecmp_works=yes'
 )
