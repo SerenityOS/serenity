640f6d86f6c9a55224727efe07e427b94cf47c4e
Linus Groh <mail@linusgroh.de>
2025-11-14 01:25:42 +0000

    Ports: Update libunistring to 1.4.1

--- Ports/libunistring/package.sh
+++ Ports/libunistring/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libunistring'
-version='1.3'
+version='1.4.1'
 files=(
-    "https://ftpmirror.gnu.org/gnu/libunistring/libunistring-${version}.tar.gz#8ea8ccf86c09dd801c8cac19878e804e54f707cf69884371130d20bde68386b7"
+    "https://ftpmirror.gnu.org/gnu/libunistring/libunistring-${version}.tar.gz#12542ad7619470efd95a623174dcd4b364f2483caf708c6bee837cb53a54cb9d"
 )
 useconfigure='true'
 configopts=(
