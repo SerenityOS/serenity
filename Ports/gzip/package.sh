c547f8a044e38f16bb88739f221746e1ea5e125c
Linus Groh <mail@linusgroh.de>
2025-11-14 01:11:29 +0000

    Ports: Update gzip to 1.14

--- Ports/gzip/package.sh
+++ Ports/gzip/package.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='gzip'
-version='1.13'
+version='1.14'
 useconfigure='true'
-use_fresh_config_sub='true'
-config_sub_paths=(
-    'build-aux/config.sub'
-)
 files=(
-    "https://ftpmirror.gnu.org/gnu/gzip/gzip-${version}.tar.gz#20fc818aeebae87cdbf209d35141ad9d3cf312b35a5e6be61bfcfbf9eddd212a"
+    "https://ftpmirror.gnu.org/gnu/gzip/gzip-${version}.tar.gz#613d6ea44f1248d7370c7ccdeee0dd0017a09e6c39de894b3c6f03f981191c6b"
 )
