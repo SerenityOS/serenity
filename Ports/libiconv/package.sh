f0c67e7fecad8f109e4f3d8ba4d84194c3b7b3b9
Linus Groh <mail@linusgroh.de>
2025-11-14 01:22:18 +0000

    Ports: Update libiconv to 1.18

--- Ports/libiconv/package.sh
+++ Ports/libiconv/package.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libiconv'
-version='1.17'
+version='1.18'
 files=(
-    "https://ftpmirror.gnu.org/gnu/libiconv/libiconv-${version}.tar.gz#8f74213b56238c85a50a5329f77e06198771e70dd9a739779f4c02f65d971313"
+    "https://ftpmirror.gnu.org/gnu/libiconv/libiconv-${version}.tar.gz#3b08f5f4f9b4eb82f151a7040bfd6fe6c6fb922efe4b1659c66ea933276965e8"
 )
 useconfigure='true'
-use_fresh_config_sub='true'
-config_sub_paths=("build-aux/config.sub" "libcharset/build-aux/config.sub")
 configopts=("--enable-shared" "--disable-nls" "CFLAGS=-std=c17")
