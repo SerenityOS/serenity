3cee7a7c7adfb88280ccac85f709960ecd1a5c54
Linus Groh <mail@linusgroh.de>
2025-11-14 01:24:49 +0000

    Ports: Update libtool to 2.5.4

--- Ports/libtool/package.sh
+++ Ports/libtool/package.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=libtool
-version=2.4.7
-useconfigure=true
-use_fresh_config_sub=true
-config_sub_paths=("build-aux/config.sub")
-depends=("bash" "sed")
+port='libtool'
+version='2.5.4'
+useconfigure='true'
+depends=(
+    'bash'
+    'sed'
+)
 files=(
-    "https://ftpmirror.gnu.org/gnu/libtool/libtool-${version}.tar.xz#4f7f217f057ce655ff22559ad221a0fd8ef84ad1fc5fcb6990cecc333aa1635d"
+    "https://ftpmirror.gnu.org/gnu/libtool/libtool-${version}.tar.xz#f81f5860666b0bc7d84baddefa60d1cb9fa6fceb2398cc3baca6afaa60266675"
 )
 configopts=("--prefix=/usr/local")
 
