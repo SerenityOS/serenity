95bf0feb4f099be1839808fe30300a992962d822
Linus Groh <mail@linusgroh.de>
2025-11-15 03:01:41 +0000

    Ports: Update SuperTuxKart to 1.5

--- Ports/SuperTuxKart/package.sh
+++ Ports/SuperTuxKart/package.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=SuperTuxKart
 useconfigure='true'
-version='1.4'
-archive_hash='9890392419baf4715313f14d5ad60746f276eed36eb580636caf44e2532c0f03'
+version='1.5'
+archive_hash='33cf8841e4ff4082d80b9248014295bbbea61d14683e86dff100e3ab8f7b27cb'
 files=(
-    "https://github.com/supertuxkart/stk-code/releases/download/${version}/supertuxkart-${version}-src.tar.xz#$archive_hash"
+    "https://github.com/supertuxkart/stk-code/releases/download/${version}/SuperTuxKart-${version}-src.tar.gz#$archive_hash"
 )
 workdir="${port}-${version}-src"
 launcher_name='SuperTuxKart'
@@ -37,6 +37,7 @@ configopts=(
     '-DBUILD_SHARED_LIBS=OFF'
     '-DUSE_WIIUSE=OFF'
     '-DUSE_DNS_C=ON'
+    '-DNO_SHADERC=ON'
     '-Bbuild'
     '-GNinja'
     "-DCMAKE_C_FLAGS=-I${SERENITY_INSTALL_ROOT}/usr/local/include/SDL2 -lcurl -lharfbuzz -lfreetype"
