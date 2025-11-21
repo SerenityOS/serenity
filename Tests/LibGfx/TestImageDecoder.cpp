55a747cb81a7f1d4edb94d011cf603606389b3ec
Nico Weber <thakis@chromium.org>
2025-11-20 19:58:55 -0500

    Tests/LibGfx: Add JBIG2 test for inner text region composition
    Text regions allow setting a background color, and a composition
    operator that's used when drawing symbol instances into the text region.
    (The final output of the text region is then composited onto the page
    with the region's *external* composition operator.)
    This adds tests for that.
    
    It follows the same strategy as the compositing tests in #26244:
    
    To test for 'or', paint a region (the mouth) twice in the same location,
    on a white background.
    
    To test for 'xor', paint a region (the nose) once in the correct
    location, and paint it twice on top of each other at some incorrect
    location, on a white background.
    
    To test for 'and', paint an inverted region (the upper eye) twice, on a
    black background. To make sure it shows up correctly, draw a black
    generic region below the text region, and use an external composition
    operator of xor for the two regions, to undo the region being inverted.
    
    To test for 'xnor', paint an inverted region (the lower eye) once in the
    correct location, and paint it twice at some other incorrect location,
    on a black background. Like for 'and', put a generic black region below.
    
    (Text regions cannot use "replace" for their composition operator.)

--- Tests/LibGfx/TestImageDecoder.cpp
+++ Tests/LibGfx/TestImageDecoder.cpp
@@ -394,6 +394,7 @@ TEST_CASE(test_jbig2_decode)
         TEST_INPUT("jbig2/bitmap-symbol-symhuff-texthuffB10B13.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-symbol-symhuffcustom-texthuffcustom.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-symbol-symhuffuncompressed-texthuff.jbig2"sv),
+        TEST_INPUT("jbig2/bitmap-symbol-textcomposite.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-symbol-textrefine.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-symbol-textrefine-customat.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-symbol-textrefine-negative-delta-width.jbig2"sv),
