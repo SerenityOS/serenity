2646e54445b2621a3e4b4dc9300d8942dc63ea11
Nico Weber <thakis@chromium.org>
2025-12-04 21:39:11 -0500

    LibGfx/ICC: Implement converting from PCS to Lut16TagData
    Makes this work:
    
        % echo 'pcsxyz(1.0, 0.0, 0.5)' | \
              Build/lagom/bin/icc -n XYZ --stdin-u8-from-pcs
        128, 0, 64
    
    (Note that 1.0 maps to 128, as mentioned in the previous commit
    message.)
    
    Also makes it possible to write a roundtrip test for the
    IdentityXYZ_D50() profile, so do so.
    
    This also allows doing roundabout lab->xyz conversion (albeit filtered
    through u8 resolution):
    
        % echo 'pcslab(100, -128, 127)' | \
              icc -n XYZ --stdin-u8-from-pcs | \
              icc -n XYZ --stdin-u8-to-pcs
        pcsxyz(0.3999939, 1.0039062, 0.039215088)

--- Tests/LibGfx/TestICCProfile.cpp
+++ Tests/LibGfx/TestICCProfile.cpp
@@ -300,6 +300,12 @@ TEST_CASE(roundtrip_lab_mft1)
     test_roundtrip(*profile);
 }
 
+TEST_CASE(roundtrip_xyz_mft2)
+{
+    auto profile = TRY_OR_FAIL(Gfx::ICC::IdentityXYZ_D50());
+    test_roundtrip(*profile);
+}
+
 TEST_CASE(roundtrip_sRGB_matrix_profile)
 {
     auto profile = TRY_OR_FAIL(Gfx::ICC::sRGB());
