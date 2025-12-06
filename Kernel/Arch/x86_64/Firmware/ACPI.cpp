239068049322b2be45fe137540a0fda935821f45
implicitfield <114500360+implicitfield@users.noreply.github.com>
2025-12-06 17:41:30 +0200

    Kernel: Validate ACPI checksums (and skip invalid tables)

--- Kernel/Arch/x86_64/Firmware/ACPI.cpp
+++ Kernel/Arch/x86_64/Firmware/ACPI.cpp
@@ -6,25 +6,77 @@
 
 #include <Kernel/Arch/x86_64/Firmware/ACPI.h>
 #include <Kernel/Arch/x86_64/Firmware/PCBIOS/Mapper.h>
+#include <Kernel/Firmware/ACPI/Definitions.h>
 #include <Kernel/Memory/MemoryManager.h>
 
 namespace Kernel::ACPI::StaticParsing {
+
+static bool is_rsdp_valid(u8 const* rsdp, size_t region_size)
+{
+    if (region_size < sizeof(Structures::RSDPDescriptor))
+        return false;
+
+    u8 revision = reinterpret_cast<Structures::RSDPDescriptor const*>(rsdp)->revision;
+
+    u8 checksum = 0;
+    for (size_t i = 0; i < sizeof(Structures::RSDPDescriptor); ++i)
+        checksum += rsdp[i];
+
+    if (checksum != 0)
+        return false;
+
+    if (revision == 0)
+        // Checksum matched and there's nothing more to check.
+        return true;
+
+    if (region_size < sizeof(Structures::RSDPDescriptor20))
+        return false;
+
+    checksum = 0;
+    for (size_t i = 0; i < sizeof(Structures::RSDPDescriptor20); ++i)
+        checksum += rsdp[i];
+    return checksum == 0;
+}
 
 // https://uefi.org/specs/ACPI/6.5/05_ACPI_Software_Programming_Model.html#finding-the-rsdp-on-ia-pc-systems
 Optional<PhysicalAddress> find_rsdp_in_ia_pc_specific_memory_locations()
 {
     constexpr auto signature = "RSD PTR "sv;
+
+    auto locate_rsdp = [&signature](Memory::MappedROM mapping) -> Optional<PhysicalAddress> {
+        constexpr size_t rsdp_alignment = 16;
+        size_t start_paddr = mapping.paddr.get() + mapping.offset;
+        size_t alignment_offset = align_up_to(start_paddr, rsdp_alignment) - start_paddr;
+
+        mapping.offset += alignment_offset;
+        mapping.size = align_down_to(mapping.size - alignment_offset, rsdp_alignment);
+
+        VERIFY((mapping.paddr.get() + mapping.offset) % rsdp_alignment == 0);
+        VERIFY(mapping.size % rsdp_alignment == 0);
+
+        while (true) {
+            u8 const* rsdp = mapping.pointer_to_chunk_starting_with(signature, rsdp_alignment);
+            if (!rsdp)
+                return {};
+            if (is_rsdp_valid(rsdp, static_cast<size_t>(mapping.end() - rsdp)))
+                return mapping.paddr_of(rsdp);
+            size_t mapping_offset = static_cast<size_t>(rsdp - mapping.base()) + rsdp_alignment;
+            mapping.offset += mapping_offset;
+            mapping.size -= mapping_offset;
+        }
+    };
+
     auto ebda_or_error = map_ebda();
     if (!ebda_or_error.is_error()) {
-        auto rsdp = ebda_or_error.value().find_chunk_starting_with(signature, 16);
-        if (rsdp.has_value())
-            return rsdp;
+        auto maybe_rsdp = locate_rsdp(ebda_or_error.release_value());
+        if (maybe_rsdp.has_value())
+            return maybe_rsdp.value();
     }
     auto bios_or_error = map_bios();
     if (!bios_or_error.is_error()) {
-        auto rsdp = bios_or_error.value().find_chunk_starting_with(signature, 16);
-        if (rsdp.has_value())
-            return rsdp;
+        auto maybe_rsdp = locate_rsdp(bios_or_error.release_value());
+        if (maybe_rsdp.has_value())
+            return maybe_rsdp.value();
     }
 
     // On some systems the RSDP may be located in ACPI NVS or reclaimable memory regions
@@ -52,9 +104,9 @@ Optional<PhysicalAddress> find_rsdp_in_ia_pc_specific_memory_locations()
         mapping.size = memory_range.length;
         mapping.paddr = memory_range.start;
 
-        auto rsdp = mapping.find_chunk_starting_with(signature, 16);
-        if (rsdp.has_value())
-            return rsdp;
+        auto maybe_rsdp = locate_rsdp(move(mapping));
+        if (maybe_rsdp.has_value())
+            return maybe_rsdp.value();
     }
 
     return {};
