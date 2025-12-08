1500a4ff505f790170d8a5df0c05d64ddf6e91ea
implicitfield <114500360+implicitfield@users.noreply.github.com>
2025-12-08 16:43:26 +0200

    Kernel: Use the same TrapFrame on all architectures
    These are all identical as of the last commit.

--- Kernel/Arch/TrapFrame.h
+++ Kernel/Arch/TrapFrame.h
@@ -7,25 +7,26 @@
 #pragma once
 
 #include <AK/Platform.h>
-
-// FIXME: There's only a minor difference between x86 and Aarch64/RISC-V trap frames; the prev_irq member.
-//        This seems to be unnecessary (see FIXME in Processor::enter_trap),
-//        so investigate whether we need it and either:
-//        (1) Remove the member and corresponding code from x86
-//        (2) Implement prev_irq in the assembly stubs of Aarch64 and RISC-V
-//        and then use the same TrapFrame on all architectures.
-
-#if ARCH(X86_64)
-#    include <Kernel/Arch/x86_64/TrapFrame.h>
-#elif ARCH(AARCH64)
-#    include <Kernel/Arch/aarch64/TrapFrame.h>
-#elif ARCH(RISCV64)
-#    include <Kernel/Arch/riscv64/TrapFrame.h>
-#else
-#    error "Unknown architecture"
-#endif
+#include <AK/StdLibExtraDetails.h>
 
 namespace Kernel {
+
+struct RegisterState;
+
+struct TrapFrame {
+    TrapFrame* next_trap;
+    RegisterState* regs; // must be last
+
+    TrapFrame() = delete;
+    TrapFrame(TrapFrame const&) = delete;
+    TrapFrame(TrapFrame&&) = delete;
+    TrapFrame& operator=(TrapFrame const&) = delete;
+    TrapFrame& operator=(TrapFrame&&) = delete;
+};
+
+#define TRAP_FRAME_SIZE (2 * 8)
+
+static_assert(AssertSize<TrapFrame, TRAP_FRAME_SIZE>());
 
 extern "C" void enter_trap_no_irq(TrapFrame* trap) __attribute__((used));
 extern "C" void enter_trap(TrapFrame*) __attribute__((used));
