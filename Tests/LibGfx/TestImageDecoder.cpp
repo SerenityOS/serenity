5e2c094f6a713bae1538d13b371d42aa1dd7297e
Nico Weber <thakis@chromium.org>
2025-12-20 20:14:17 -0500

    Tests/LibGfx: Add a JBIG2 halftone test with a non-identity grid
    This uses a slightly rotated grid. It doesn't cover the whole halftone
    region, but it does cover the bits that contain non-white pixels.
    The far corners of the halftone graymap are outside the region,
    to test that graymap clipping without `enable_skip` works.
    
    This test works in Preview.app, PDFium, pdf.js, Acrobat Reader,
    and mutool.

--- Tests/LibGfx/TestImageDecoder.cpp
+++ Tests/LibGfx/TestImageDecoder.cpp
@@ -379,6 +379,7 @@ TEST_CASE(test_jbig2_decode)
         TEST_INPUT("jbig2/bitmap-refine-template1.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-refine-template1-tpgron.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-halftone.jbig2"sv),
+        TEST_INPUT("jbig2/bitmap-halftone-grid.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-halftone-refine.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-halftone-skip-dummy.jbig2"sv),
         TEST_INPUT("jbig2/bitmap-halftone-template1.jbig2"sv),
