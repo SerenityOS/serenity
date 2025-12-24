9805280afe092fb28818eefcb5d90a6d89f7ad9c
Sönke Holz <soenke.holz@serenityos.org>
2025-12-24 15:56:53 +0100

    Kernel: Print the kernel cmdline later
    That way it will be visible in the early framebuffer console and on
    serial ports set up by the platform init functions.

--- Kernel/Boot/CommandLine.cpp
+++ Kernel/Boot/CommandLine.cpp
@@ -36,7 +36,6 @@ UNMAP_AFTER_INIT void CommandLine::initialize()
 {
     VERIFY(!s_the);
     s_the = new CommandLine({ s_cmd_line, strlen(s_cmd_line) });
-    dmesgln("Kernel Commandline: {}", kernel_command_line().string());
     // Validate the modes the user passed in.
     (void)s_the->panic_mode(Validate::Yes);
 }
