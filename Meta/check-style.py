4c5bb357b2dfd1118559de8fc7b3909685b58e07
Sönke Holz <soenke.holz@serenityos.org>
2026-01-03 12:50:23 +0100

    Kernel/Input: Create our own header for evdev definitions
    Instead of using a quite large FreeBSD header, which doesn't match our
    code style, roll our own header for the few definitions that we need.
    
    These are essentially API definitions, so it shouldn't be a problem
    that the original linux evdev definitions this is based on are under
    the GPL.

--- Meta/check-style.py
+++ Meta/check-style.py
@@ -70,8 +70,6 @@ def should_check_file(filename):
         return False
     if filename.startswith('Base/'):
         return False
-    if filename == 'Kernel/Devices/Input/VirtIO/EvDevDefinitions.h':
-        return False
     if filename == 'Kernel/FileSystem/Ext2FS/Definitions.h':
         return False
     if filename == 'Kernel/FileSystem/FUSE/Definitions.h':
