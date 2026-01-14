2348f7c2c2b2a5584619cf273407a882bed3461b
Lucas CHOLLET <lucas.chollet@free.fr>
2026-01-14 19:19:22 +0100

    Meta: Prefer `sudo`'s `--preserve-env` over `-E`
    The `-E` option is not supported by `sudo-rs` so give it the list of all
    our environment variables.
    
    Fix building on Ubuntu 25.10.

--- Meta/shell_include.sh
+++ Meta/shell_include.sh
@@ -14,7 +14,9 @@ die() {
 if [ "$(uname -s)" = "SerenityOS" ]; then
     SUDO="pls -E"
 elif command -v sudo >/dev/null; then
-    SUDO="sudo -E"
+    # Preserve all environment variables starting with SERENITY_.
+    SERENITY_VARS=$(env | cut -d= -f1 | grep '^SERENITY' | tr '\n' ',' | sed 's/,$//')
+    SUDO="sudo --preserve-env=$SERENITY_VARS"
 elif command -v doas >/dev/null; then
     if [ "$SUDO_UID" = '' ]; then
         SUDO_UID=$(id -u)
