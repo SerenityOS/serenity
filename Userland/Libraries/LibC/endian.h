338ae20c61601c5ff49eb576b76acfdc9d339a38
Sönke Holz <soenke.holz@serenityos.org>
2026-02-25 12:22:10 +0100

    LibC: Don't check for feature test macros in endian.h
    We don't check for feature test macros in any other LibC header, and
    especially not for _GNU_SOURCE or _BSD_SOURCE. With no feature test
    macro defined, this header would previously essentially be empty.
    
    POSIX doesn't seem to require any feature test macros for this header
    either.

--- Userland/Libraries/LibC/endian.h
+++ Userland/Libraries/LibC/endian.h
@@ -20,9 +20,7 @@ __BEGIN_DECLS
 #    include <bits/endian.h>
 #endif
 
-#if defined(_GNU_SOURCE) || defined(_BSD_SOURCE)
-
-#    include <stdint.h>
+#include <stdint.h>
 
 static __inline uint16_t __bswap16(uint16_t x)
 {
@@ -39,51 +37,49 @@ static __inline uint64_t __bswap64(uint64_t x)
     return __builtin_bswap64(x);
 }
 
-#    define LITTLE_ENDIAN __LITTLE_ENDIAN
-#    define BIG_ENDIAN __BIG_ENDIAN
-#    define PDP_ENDIAN __PDP_ENDIAN
-#    define BYTE_ORDER __BYTE_ORDER
+#define LITTLE_ENDIAN __LITTLE_ENDIAN
+#define BIG_ENDIAN __BIG_ENDIAN
+#define PDP_ENDIAN __PDP_ENDIAN
+#define BYTE_ORDER __BYTE_ORDER
 
-#    if __BYTE_ORDER == __LITTLE_ENDIAN
-#        define htole16(x) ((uint16_t)(x))
-#        define le16toh(x) ((uint16_t)(x))
-#        define letoh16(x) ((uint16_t)(x))
-#        define htole32(x) ((uint32_t)(x))
-#        define le32toh(x) ((uint32_t)(x))
-#        define letoh32(x) ((uint32_t)(x))
-#        define htole64(x) ((uint64_t)(x))
-#        define le64toh(x) ((uint64_t)(x))
-#        define letoh64(x) ((uint64_t)(x))
-#        define htobe16(x) (__builtin_bswap16(x))
-#        define be16toh(x) (__builtin_bswap16(x))
-#        define betoh16(x) (__builtin_bswap16(x))
-#        define htobe32(x) (__builtin_bswap32(x))
-#        define be32toh(x) (__builtin_bswap32(x))
-#        define betoh32(x) (__builtin_bswap32(x))
-#        define htobe64(x) (__builtin_bswap64(x))
-#        define be64toh(x) (__builtin_bswap64(x))
-#        define betoh64(x) (__builtin_bswap64(x))
-#    else
-#        define htole16(x) (__builtin_bswap16(x))
-#        define le16toh(x) (__builtin_bswap16(x))
-#        define letoh16(x) (__builtin_bswap16(x))
-#        define htole32(x) (__builtin_bswap32(x))
-#        define le32toh(x) (__builtin_bswap32(x))
-#        define letoh32(x) (__builtin_bswap32(x))
-#        define htole64(x) (__builtin_bswap64(x))
-#        define le64toh(x) (__builtin_bswap64(x))
-#        define letoh64(x) (__builtin_bswap64(x))
-#        define htobe16(x) ((uint16_t)(x))
-#        define be16toh(x) ((uint16_t)(x))
-#        define betoh16(x) ((uint16_t)(x))
-#        define htobe32(x) ((uint32_t)(x))
-#        define be32toh(x) ((uint32_t)(x))
-#        define betoh32(x) ((uint32_t)(x))
-#        define htobe64(x) ((uint64_t)(x))
-#        define be64toh(x) ((uint64_t)(x))
-#        define betoh64(x) ((uint64_t)(x))
-#    endif
-
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+#    define htole16(x) ((uint16_t)(x))
+#    define le16toh(x) ((uint16_t)(x))
+#    define letoh16(x) ((uint16_t)(x))
+#    define htole32(x) ((uint32_t)(x))
+#    define le32toh(x) ((uint32_t)(x))
+#    define letoh32(x) ((uint32_t)(x))
+#    define htole64(x) ((uint64_t)(x))
+#    define le64toh(x) ((uint64_t)(x))
+#    define letoh64(x) ((uint64_t)(x))
+#    define htobe16(x) (__builtin_bswap16(x))
+#    define be16toh(x) (__builtin_bswap16(x))
+#    define betoh16(x) (__builtin_bswap16(x))
+#    define htobe32(x) (__builtin_bswap32(x))
+#    define be32toh(x) (__builtin_bswap32(x))
+#    define betoh32(x) (__builtin_bswap32(x))
+#    define htobe64(x) (__builtin_bswap64(x))
+#    define be64toh(x) (__builtin_bswap64(x))
+#    define betoh64(x) (__builtin_bswap64(x))
+#else
+#    define htole16(x) (__builtin_bswap16(x))
+#    define le16toh(x) (__builtin_bswap16(x))
+#    define letoh16(x) (__builtin_bswap16(x))
+#    define htole32(x) (__builtin_bswap32(x))
+#    define le32toh(x) (__builtin_bswap32(x))
+#    define letoh32(x) (__builtin_bswap32(x))
+#    define htole64(x) (__builtin_bswap64(x))
+#    define le64toh(x) (__builtin_bswap64(x))
+#    define letoh64(x) (__builtin_bswap64(x))
+#    define htobe16(x) ((uint16_t)(x))
+#    define be16toh(x) ((uint16_t)(x))
+#    define betoh16(x) ((uint16_t)(x))
+#    define htobe32(x) ((uint32_t)(x))
+#    define be32toh(x) ((uint32_t)(x))
+#    define betoh32(x) ((uint32_t)(x))
+#    define htobe64(x) ((uint64_t)(x))
+#    define be64toh(x) ((uint64_t)(x))
+#    define betoh64(x) ((uint64_t)(x))
 #endif
 
 __END_DECLS
