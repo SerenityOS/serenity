a92ead8dcb5e017c1d86917dd88c6f06c11d190b
Sönke Holz <soenke.holz@serenityos.org>
2026-02-25 12:30:36 +0100

    LibC: Remove PDP_ENDIAN
    I don't expect SerenityOS to ever run on a PDP-11. This definition is
    not required by POSIX either.

--- Userland/Libraries/LibC/endian.h
+++ Userland/Libraries/LibC/endian.h
@@ -12,7 +12,6 @@ __BEGIN_DECLS
 
 #define __LITTLE_ENDIAN 1234
 #define __BIG_ENDIAN 4321
-#define __PDP_ENDIAN 3412
 
 #if defined(__GNUC__) && defined(__BYTE_ORDER__)
 #    define __BYTE_ORDER __BYTE_ORDER__
@@ -24,7 +23,6 @@ __BEGIN_DECLS
 
 #define LITTLE_ENDIAN __LITTLE_ENDIAN
 #define BIG_ENDIAN __BIG_ENDIAN
-#define PDP_ENDIAN __PDP_ENDIAN
 #define BYTE_ORDER __BYTE_ORDER
 
 #if __BYTE_ORDER == __LITTLE_ENDIAN
