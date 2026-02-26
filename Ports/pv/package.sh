9dd7d0d1539b064b943ae9ecc99f342ac9c24b1d
Lucas Chollet <lucas.chollet@free.fr>
2026-02-26 23:47:08 +0100

    Ports: Update pv to 1.10.3
    Looks like we don't need -Wno-deprecated-declarations anymore. Thanks
    for catching this Sönke!

--- Ports/pv/package.sh
+++ Ports/pv/package.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=pv
-version=1.6.20
+version=1.10.3
 useconfigure=true
 configopts=("--disable-nls")
 files=(
-    "http://www.ivarch.com/programs/sources/pv-${version}.tar.bz2#e831951eff0718fba9b1ef286128773b9d0e723e1fbfae88d5a3188814fdc603"
+    "https://ivarch.com/s/pv-${version}.tar.gz#aa1630c79af6960a8922ffa64d2c3e7f87486da21fcb57e277824294fd266742"
 )
-
-export CFLAGS=-Wno-deprecated-declarations
 
 # Map LD to the serenity Toolchain, otherwise the host LD is used.
 if [ "$SERENITY_TOOLCHAIN" = "Clang" ]; then
