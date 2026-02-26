e3678f3cd5997d4714d30b59c0dfd8d0d1b7836f
Lucas Chollet <lucas.chollet@free.fr>
2026-02-26 23:49:29 +0100

    Ports: Let the port system choose LD when building pv
    This logic is already implemented in hosted_defs.sh.

--- Ports/pv/package.sh
+++ Ports/pv/package.sh
@@ -6,10 +6,3 @@ configopts=("--disable-nls")
 files=(
     "https://ivarch.com/s/pv-${version}.tar.gz#aa1630c79af6960a8922ffa64d2c3e7f87486da21fcb57e277824294fd266742"
 )
-
-# Map LD to the serenity Toolchain, otherwise the host LD is used.
-if [ "$SERENITY_TOOLCHAIN" = "Clang" ]; then
-    export LD=$(CC)
-else
-    export LD="${SERENITY_ARCH}-pc-serenity-ld"
-fi
