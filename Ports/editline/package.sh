ab76cfbd6c715fae2dd3349afe4fc28b82eb39f5
Sönke Holz <soenke.holz@serenityos.org>
2026-02-27 14:41:06 +0100

    Ports: Unbreak editline
    This port uses incomplete function prototypes, which are no longer
    supported as of C23.
    
    Additionally, it failed to compile due to a missing <sys/select.h>
    include. For some reason, there was already an upstream patch for this,
    but it wasn't included in our patches.

--- Ports/editline/package.sh
+++ Ports/editline/package.sh
@@ -8,3 +8,6 @@ config_sub_paths=('aux/config.sub')
 files=(
     "https://github.com/troglobit/editline/releases/download/${version}/editline-${version}.tar.gz#781e03b6a935df75d99fb963551e2e9f09a714a8c49fc53280c716c90bf44d26"
 )
+configopts=(
+    'CFLAGS=-std=c17'
+)
