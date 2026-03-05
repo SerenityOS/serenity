6043b373f7f4d27cda043ab1d38e54a53ed252d1
Sönke Holz <soenke.holz@serenityos.org>
2026-03-05 11:40:54 +0100

    Kernel/SD: Propagate errors during PCISDHostController construction

--- Kernel/Devices/Storage/SD/PCISDHostController.cpp
+++ Kernel/Devices/Storage/SD/PCISDHostController.cpp
@@ -12,7 +12,17 @@ namespace Kernel {
 
 ErrorOr<NonnullRefPtr<PCISDHostController>> PCISDHostController::try_initialize(PCI::DeviceIdentifier const& device_identifier)
 {
-    auto sdhc = TRY(adopt_nonnull_ref_or_enomem(new (nothrow) PCISDHostController(device_identifier)));
+    auto slot_information_register = read_slot_information(device_identifier);
+
+    if (slot_information_register.slots_available() != 1) {
+        // TODO: Support multiple slots
+        dmesgln("SD Host Controller has {} slots, but we currently only support using only one", slot_information_register.slots_available());
+    }
+
+    auto bar = static_cast<PCI::HeaderType0BaseRegister>(slot_information_register.first_bar_number);
+    auto registers = TRY(PCI::map_bar<SD::HostControlRegisterMap volatile>(device_identifier, bar));
+
+    auto sdhc = TRY(adopt_nonnull_ref_or_enomem(new (nothrow) PCISDHostController(device_identifier, move(registers))));
     TRY(sdhc->initialize());
 
     PCI::enable_bus_mastering(sdhc->device_identifier());
@@ -22,18 +32,10 @@ ErrorOr<NonnullRefPtr<PCISDHostController>> PCISDHostController::try_initialize(
     return sdhc;
 }
 
-PCISDHostController::PCISDHostController(PCI::DeviceIdentifier const& device_identifier)
+PCISDHostController::PCISDHostController(PCI::DeviceIdentifier const& device_identifier, Memory::TypedMapping<SD::HostControlRegisterMap volatile> registers)
     : PCI::Device(device_identifier)
+    , m_registers(move(registers))
 {
-    auto slot_information_register = read_slot_information();
-
-    if (slot_information_register.slots_available() != 1) {
-        // TODO: Support multiple slots
-        dmesgln("SD Host Controller has {} slots, but we currently only support using only one", slot_information_register.slots_available());
-    }
-
-    auto bar = static_cast<PCI::HeaderType0BaseRegister>(slot_information_register.first_bar_number);
-    m_registers = PCI::map_bar<SD::HostControlRegisterMap volatile>(device_identifier, bar).release_value_but_fixme_should_propagate_errors();
 }
 
 }
