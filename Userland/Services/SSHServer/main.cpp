3ebe50e634f575205cd7024f035d4dc3635d0a97
Lucas Chollet <lucas.chollet@free.fr>
2026-03-07 14:18:03 +0100

    SSHServer: Allow providing the port as an argument
    This is useful to start the server as a low-privilege user or on Lagom.

--- Userland/Services/SSHServer/main.cpp
+++ Userland/Services/SSHServer/main.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <AK/String.h>
+#include <LibCore/ArgsParser.h>
 #include <LibCore/EventLoop.h>
 #include <LibCore/System.h>
 #include <LibCore/TCPServer.h>
@@ -14,10 +15,20 @@
 static constexpr auto DEFAULT_LISTEN_ADDRESS = "0.0.0.0"sv;
 static constexpr auto DEFAULT_PORT = 22;
 
-ErrorOr<int> serenity_main(Main::Arguments)
+ErrorOr<int> serenity_main(Main::Arguments args)
 {
     TRY(Core::System::pledge("stdio accept inet unix"));
     TRY(Core::System::unveil(nullptr, nullptr));
+
+    Optional<u32> port {};
+
+    Core::ArgsParser parser;
+    parser.add_option(port, "Port to listen on", "port", 'p', "port");
+
+    parser.parse(args);
+
+    if (port.has_value() && *port != static_cast<u16>(*port))
+        return Error::from_string_literal("Invalid port number");
 
     Core::EventLoop loop;
 
@@ -33,7 +44,7 @@ ErrorOr<int> serenity_main(Main::Arguments)
         auto client = SSH::Server::TCPClient::create(maybe_client_socket.release_value(), tcp_server);
     };
 
-    TRY(tcp_server->listen(*IPv4Address::from_string(DEFAULT_LISTEN_ADDRESS), DEFAULT_PORT));
+    TRY(tcp_server->listen(*IPv4Address::from_string(DEFAULT_LISTEN_ADDRESS), port.value_or(DEFAULT_PORT)));
 
     outln("Listening on {}:{}", tcp_server->local_address().value(), tcp_server->local_port());
 
