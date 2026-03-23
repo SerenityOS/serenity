15edb02847e4030ed6784bb8ac4226e4d1e7bb30
Lucas Chollet <lucas.chollet@free.fr>
2026-03-23 16:44:15 +0100

    LibSSH+SSHServer: Support basic channels and sessions
    This allows a client to execute commands on the server. This is very
    minimal and don't support interactive sessions yet.
    
    It is however enough to run simple commands like `uname` over the
    network :^)

---
+++ Userland/Libraries/LibSSH/Session.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2026, Lucas Chollet <lucas.chollet@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/ByteBuffer.h>
+#include <AK/Error.h>
+
+namespace SSH {
+
+// 6.1.  Opening a Session
+// https://datatracker.ietf.org/doc/html/rfc4254#section-6.1
+struct Session {
+    static ErrorOr<Session> create(u32 sender_channel_id, u32 window_size, u32 maximum_packet_size);
+
+    u32 local_channel_id {};
+    u32 sender_channel_id {};
+    u32 maximum_packet_size {};
+    ByteBuffer window {};
+};
+
+}
