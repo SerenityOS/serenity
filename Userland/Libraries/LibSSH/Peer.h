65bae2263b0525a90ba7ee78444f8fe69831d294
Lucas Chollet <lucas.chollet@free.fr>
2026-03-23 20:02:39 +0100

    Tests+LibSSH: Start testing the Peer class
    For the moment, it only includes a simple test for the NEW_KEYS packet
    handling.

--- Userland/Libraries/LibSSH/Peer.h
+++ Userland/Libraries/LibSSH/Peer.h
@@ -16,7 +16,7 @@ namespace SSH {
 // RFC 4253:  https://datatracker.ietf.org/doc/html/rfc4253
 class Peer {
 public:
-    Peer(Core::TCPSocket& tcp_socket)
+    Peer(Core::Socket& tcp_socket)
         : m_tcp_socket(tcp_socket)
         , m_cipher(make<IdentityCipher>())
     {
@@ -46,7 +46,7 @@ protected:
     }
 
 private:
-    Core::TCPSocket& m_tcp_socket;
+    Core::Socket& m_tcp_socket;
     NonnullOwnPtr<Cipher> m_cipher;
 
     Optional<Crypto::Hash::Digest<256>> m_session_id {};
