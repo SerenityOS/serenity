4a70dfc2db7f885d4ed1ad751ea5c10ece99a039
Sönke Holz <soenke.holz@serenityos.org>
2026-04-03 22:48:54 +0200

    Ports: Add tree-sitter-{c,lua,markdown,query,vim,vimdoc}
    These parsers are required by neovim.

---
+++ Ports/tree-sitter-c/package.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port='tree-sitter-c'
+version='0.24.1'
+files=(
+    "https://github.com/tree-sitter/tree-sitter-c/releases/download/v${version}/tree-sitter-c.tar.gz#5a82a33dc5483f46d9a571f8ee769f6825752202cc2694299007a9da9417b9e5"
+)
+workdir='.'
