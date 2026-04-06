fec334044af1d8c9a4a928e1e56210ceb6a69d51
Sönke Holz <soenke.holz@serenityos.org>
2026-04-06 13:29:46 +0200

    Meta+Userland: Remove leftovers from Android support
    These were missed in bb2d80a2bb3.

--- Meta/CMake/all_the_debug_macros.cmake
+++ Meta/CMake/all_the_debug_macros.cmake
@@ -278,7 +278,5 @@ set(XML_PARSER_DEBUG ON)
 # set(ELF_DEBUG ON)
 # False positive: A32_DEBUG_INTERFACE is the name of a CPU feature, not a debug flag.
 # set(IA32_DEBUG_INTERFACE ON)
-# False positive: ANDROID_LOG_DEBUG is a log level, not a debug flag
-# set(ANDROID_LOG_DEBUG ON)
 # False positive: DEBUG_STRIPPED is a PE characteristic, not a debug flag
 # set(DEBUG_STRIPPED ON)
