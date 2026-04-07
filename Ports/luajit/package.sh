b6054d0bf0226a102868fd62b388db49cca84a0d
Sönke Holz <soenke.holz@serenityos.org>
2026-04-07 17:29:27 +0200

    Ports: Remove multilib handling from luajit
    i686 support war removed, so there is no need to pass -m64 explicitly.
    Before i686 support was removed in b49c4eb94f2, we used to pass -m32
    for 32-bit systems here.
    
    Additionally, installing multilib development packages like
    libc6-dev-i386 should be unnecessary now, so the flashy warning message
    can be removed.
    
    This also makes the port compile on AArch64 now. Previously, it failed
    to build because M_FLAG was undefined.
    
    It still won't build on RISC-V since LuaJIT itself doesn't support
    RISC-V yet (https://github.com/LuaJIT/LuaJIT/pull/1267).

--- Ports/luajit/package.sh
+++ Ports/luajit/package.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=luajit
 version=18b087cd2cd4ddc4a79782bf155383a689d5093d
-useconfigure=true
 files=(
     "git+https://luajit.org/git/luajit.git#${version}"
 )
 workdir="LuaJIT-${version}"
 
-if [ ${SERENITY_ARCH} = "x86_64" ]; then
-    M_FLAG=-m64
-fi
-
-makeopts=("CROSS=${SERENITY_ARCH}-serenity-" "HOST_CC=${HOST_CC} ${M_FLAG}")
-
-configure() {
-    printf "\x1b[31m\x1b[5mATTENTION: \x1b[0m\x1b[31m\x1b[1mIf this fails, install either libc6-dev-i386 or libc6-dev-amd64\x1b[0m\n"
-}
+makeopts=("CROSS=${SERENITY_ARCH}-serenity-" "HOST_CC=${HOST_CC}")
