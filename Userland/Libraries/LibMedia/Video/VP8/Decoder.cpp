8966ba27a45bf10378fe0b8e702b14a04090e79d
Nico Weber <thakis@chromium.org>
2026-04-26 11:21:25 -0400

    LibMedia: Make VP8 decoder decode keyframes
    VP8 keyframes are (lossy) webp images, so we can use the webp decoder
    for these.
    
    For now, ignore non-keyframes and show the last keyframe for them
    instead.
    
    The WebP decoder currently just outputs a Gfx::Bitmap, so add a
    VideoFrame subclass that gets data from a Gfx::Bitmap.
    
    Eventually we'll want to have the webp decoder optionally return
    YUV data and use SubsampledYUVFrame for VP8. But for now, this
    lets us show VP8 keyframes with very little code.

--- Userland/Libraries/LibMedia/Video/VP8/Decoder.cpp
+++ Userland/Libraries/LibMedia/Video/VP8/Decoder.cpp
@@ -4,6 +4,9 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <LibGfx/ImageFormats/WebPLoaderLossy.h>
+#include <LibGfx/Painter.h>
+
 #include "Decoder.h"
 
 #if defined(AK_COMPILER_GCC)
@@ -11,6 +14,14 @@
 #endif
 
 namespace Media::Video::VP8 {
+
+DecoderErrorOr<void> BitmapVideoFrame::output_to_bitmap(Gfx::Bitmap& bitmap)
+{
+    // FIXME: This is a wasteful copy, see class-level FIXME.
+    Gfx::Painter painter(bitmap);
+    painter.blit({ 0, 0 }, *m_bitmap, m_bitmap->rect());
+    return {};
+}
 
 Decoder::Decoder()
 {
@@ -20,18 +31,45 @@ Decoder::~Decoder()
 {
 }
 
-DecoderErrorOr<void> Decoder::receive_sample(Duration, ReadonlyBytes)
+DecoderErrorOr<void> Decoder::receive_sample(Duration timestamp, ReadonlyBytes data)
 {
-    return DecoderError::format(DecoderErrorCategory::NotImplemented, "Can't receive VP8 samples yet");
+    NonnullRefPtr<Gfx::Bitmap> bitmap = DECODER_TRY(DecoderErrorCategory::Invalid, [&] -> ErrorOr<NonnullRefPtr<Gfx::Bitmap>> {
+        auto maybe_header = Gfx::decode_webp_chunk_VP8_header(data);
+        if (maybe_header.is_error()) {
+            if (!m_last_keyframe)
+                return maybe_header.release_error();
+
+            // For now, replace non-keyframes with the last keyframe.
+            // FIXME: Add support for decoding non-keyframes.
+            return *m_last_keyframe;
+        }
+
+        auto header = maybe_header.release_value();
+        auto bitmap = TRY(Gfx::decode_webp_chunk_VP8_contents(header, false));
+        m_last_keyframe = bitmap;
+        return bitmap;
+    }());
+
+    // These values are only used by SubsampledYUVFrame::output_to_bitmap() as far as I can tell.
+    // Since we don't currently use SubsampledYUVFrame, exact values here don't currently matter.
+    CodingIndependentCodePoints cicp(ColorPrimaries::BT601, TransferCharacteristics::SRGB, MatrixCoefficients::BT601, VideoFullRangeFlag::Studio);
+    auto video_frame = make<BitmapVideoFrame>(timestamp, Gfx::Size<u32>(bitmap->width(), bitmap->height()), 8, cicp, bitmap);
+    m_video_frame_queue.enqueue(move(video_frame));
+
+    return {};
 }
 
 DecoderErrorOr<NonnullOwnPtr<VideoFrame>> Decoder::get_decoded_frame()
 {
-    return DecoderError::format(DecoderErrorCategory::NotImplemented, "Can't decode VP8 yet");
+    if (m_video_frame_queue.is_empty())
+        return DecoderError::format(DecoderErrorCategory::NeedsMoreInput, "No video frame in queue.");
+
+    return m_video_frame_queue.dequeue();
 }
 
 void Decoder::flush()
 {
+    m_video_frame_queue.clear();
 }
 
 }
--- Userland/Libraries/LibMedia/Video/VP8/Decoder.h
+++ Userland/Libraries/LibMedia/Video/VP8/Decoder.h
@@ -9,12 +9,28 @@
 #include <AK/ByteBuffer.h>
 #include <AK/Error.h>
 #include <AK/NonnullOwnPtr.h>
+#include <AK/Queue.h>
 #include <AK/Span.h>
 #include <LibMedia/DecoderError.h>
 #include <LibMedia/VideoDecoder.h>
 #include <LibMedia/VideoFrame.h>
 
 namespace Media::Video::VP8 {
+
+// FIXME: Instead, teach the webp decoder to return YUV data and use SubsampledYUVFrame here.
+class BitmapVideoFrame final : public VideoFrame {
+public:
+    BitmapVideoFrame(Duration timestamp, Gfx::Size<u32> size, u8 bit_depth, CodingIndependentCodePoints cicp, NonnullRefPtr<Gfx::Bitmap> bitmap)
+        : VideoFrame(timestamp, size, bit_depth, cicp)
+        , m_bitmap(move(bitmap))
+    {
+    }
+
+    virtual DecoderErrorOr<void> output_to_bitmap(Gfx::Bitmap& bitmap) override;
+
+private:
+    NonnullRefPtr<Gfx::Bitmap> m_bitmap;
+};
 
 class Decoder final : public VideoDecoder {
 public:
@@ -26,6 +42,10 @@ public:
     DecoderErrorOr<NonnullOwnPtr<VideoFrame>> get_decoded_frame() override;
 
     void flush() override;
+
+private:
+    RefPtr<Gfx::Bitmap> m_last_keyframe;
+    Queue<NonnullOwnPtr<BitmapVideoFrame>, 1> m_video_frame_queue;
 };
 
 }
