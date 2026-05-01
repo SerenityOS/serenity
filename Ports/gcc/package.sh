e00fd8ab4e36ad80e1d9734b892a508d8e086289
Sönke Holz <soenke.holz@serenityos.org>
2026-05-01 19:15:19 +0200

    Toolchain+Ports: Update GCC to 16.1.0
    This also includes a cherry-picked commit to unbreak an optimization
    that caused volatile memory accesses to be done in a smaller access
    width than requested, leading to our NVMe driver triggering QEMU
    warnings.
    
    https://gcc.gnu.org/bugzilla/show_bug.cgi?id=125180
    https://gcc.gnu.org/pipermail/gcc-patches/2026-May/715709.html

--- Ports/gcc/package.sh
+++ Ports/gcc/package.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=gcc
-version=15.2.0
+version=16.1.0
 useconfigure=true
 configopts=("--target=${SERENITY_ARCH}-serenity" "--with-sysroot=/" "--with-build-sysroot=${SERENITY_INSTALL_ROOT}" "--enable-languages=c,c++" "--disable-lto" "--disable-nls" "--enable-shared" "--enable-default-pie" "--enable-host-shared" "--enable-threads=posix" "--enable-initfini-array" "--with-linker-hash-style=gnu")
 files=(
-    "https://ftpmirror.gnu.org/gnu/gcc/gcc-${version}/gcc-${version}.tar.xz#438fd996826b0c82485a29da03a72d71d6e3541a83ec702df4271f6fe025d24e"
+    "https://ftpmirror.gnu.org/gnu/gcc/gcc-${version}/gcc-${version}.tar.xz#50efb4d94c3397aff3b0d61a5abd748b4dd31d9d3f2ab7be05b171d36a510f79"
 )
 makeopts=("all-gcc" "all-target-libgcc" "all-target-libstdc++-v3" "-j$(nproc)")
 installopts=("DESTDIR=${SERENITY_INSTALL_ROOT}" "install-gcc" "install-target-libgcc" "install-target-libstdc++-v3")
