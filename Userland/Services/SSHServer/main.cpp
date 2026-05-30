SerenityOS/serenity

commit e8e1a1ee7331312f5ae7d87d26f889f0c426cadf

SSHServer: Reap children when they die
Since 4c8188d6, we fork the main server on new connections, but I forgot
to make the main server reap its children. This commit addresses the
shortcoming.

Userland/Services/SSHServer/main.cpp
@@ -81,6 +81,21 @@ ErrorOr<int> serenity_main(Main::Arguments args)
 
     Core::EventLoop loop;
 
+    Core::EventLoop::register_signal(SIGCHLD, [&](int) {
+        while (true) {
+            auto maybe_result = Core::System::waitpid(-1, WNOHANG);
+            if (maybe_result.is_error()) {
+                auto error = maybe_result.release_error();
+                if (error.code() == EINTR)
+                    continue;
+                if (error.code() == ECHILD)
+                    return;
+                dbgln("Error while reaping child processes: {}", error);
+                loop.quit(-1);
+            }
+        }
+    });
+
     g_tcp_server = TRY(Core::TCPServer::try_create());
 
     g_tcp_server->on_ready_to_accept = [] {

commit 18551432d22a991bca8e78a6f3eb700f38fe5720

SSHServer: Exit the reaping loop if remaining children are not zombies
Prior to this patch, if SSHServer had at least one alive child upon
receiving SIGCHLD, it would spin until all of its children are dead.

It basically removed the possibility of accepting parallel connections.
This regressed in e8e1a1ee.

Userland/Services/SSHServer/main.cpp
@@ -103,6 +103,9 @@ ErrorOr<int> serenity_main(Main::Arguments args)
                 dbgln("Error while reaping child processes: {}", error);
                 loop.quit(-1);
             }
+            auto wait_result = maybe_result.value();
+            if (wait_result.pid == 0)
+                return;
         }
     });
 