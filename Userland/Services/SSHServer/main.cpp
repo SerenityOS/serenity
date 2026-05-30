SerenityOS/serenity

commit 6a3be5a8bf5f6893f1635afea16547b4d04c7cf6

SSHServer: Call Core::EventLoop::notify_forked after the fork
This is required to get a fully functional EventLoop, more specifically
calling `wake` or `signals` won't be dispatched in the child process
otherwise.

Unfortunately, we now need to pass the socket as a raw fd to the child.
Otherwise, it would get closed as notifiers (as the one inside
Core::TCPSocket) are cleared in `notify_fork`.

This is preparation work to use signals to detect child's death.

Userland/Services/SSHServer/main.cpp
@@ -27,16 +27,26 @@ OwnPtr<SSH::Server::TCPClient> g_client;
 ErrorOr<void> accept_connection()
 {
     auto client_socket = TRY(g_tcp_server->accept());
+    auto fd = client_socket->fd();
 
     if (auto pid = TRY(Core::System::fork()); pid == 0) {
         // Close the server listening socket. This is deferred to not close
         // the server from within itself.
-        Core::EventLoop::current().deferred_invoke([socket = move(client_socket)]() mutable {
+
+        // We need to ensure the socket survives until the deferred_invoke, but
+        // fd will be closed at the end of the scope with client_socket.
+        auto fd_copy = TRY(Core::System::dup(fd));
+
+        Core::EventLoop::current().deferred_invoke([fd_copy]() mutable {
             g_tcp_server = nullptr;
             auto on_quit = []() {
                 g_client = nullptr;
                 Core::EventLoop::current().quit(0);
             };
+
+            Core::EventLoop::notify_forked(Core::EventLoop::ForkEvent::Child);
+
+            auto socket = MUST(Core::TCPSocket::adopt_fd(fd_copy));
             g_client = SSH::Server::TCPClient::create(move(socket), move(on_quit));
         });
     } else {

commit 6d44463751b445c5dcfb0a53be6ee771a75da93a

SSHServer: Don't let children inherit the ssh socket
While I was testing the previous commit, I realized that when killing
the connection process the ssh client would still wait until the command
was done before exiting with an error. As an exemple:

```bash
ssh 127.0.0.1 sleep 10
# In another terminal, kill the connection process.
# The ssh command above won't return before the end of the 10s sleep.
```

This was due to the socket fd being inherited by the sleep process, so
the connection to the client would still be preserved until the command
itself exited. This regressed in 6a3be5a8.

Userland/Services/SSHServer/main.cpp
@@ -36,6 +36,7 @@ ErrorOr<void> accept_connection()
         // We need to ensure the socket survives until the deferred_invoke, but
         // fd will be closed at the end of the scope with client_socket.
         auto fd_copy = TRY(Core::System::dup(fd));
+        TRY(Core::System::fcntl(fd_copy, F_SETFD, FD_CLOEXEC));
 
         Core::EventLoop::current().deferred_invoke([fd_copy]() mutable {
             g_tcp_server = nullptr;