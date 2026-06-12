4e2b97b9ad8dee6525ad4093bfcb39830849ee26
Sönke Holz <soenke.holz@serenityos.org>
2026-06-12 12:45:40 +0200

    Kernal/aarch64: Support RP1 interrupts
    The RP1 contains multiple interrupt sources for its peripherals.
    All of these sources have their own MSI-X vector.
    Since we don't support MSIs on the Pi 5 yet, we have to rely on legacy
    pin interrupts. Thankfully, the RP1 contains a pair of registers that
    tells you which MSI-X vectors are currently pending.
    This means we can just use the pin interrupt (which is then shared by
    all interrupt sources) and read these registers to determine which
    interrupts should be handled.
    
    Since our current interrupt handling infrastructure expects all
    interrupt controllers to have a N:N mapping of interrupt numbers, but we
    only have a 1:N mapping here, we can't use our normal interrupt APIs
    for the RP1 and its peripherals.

--- Kernel/Arch/aarch64/RPi/RP1/RP1.h
+++ Kernel/Arch/aarch64/RPi/RP1/RP1.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025, Sönke Holz <soenke.holz@serenityos.org>
+ * Copyright (c) 2025-2026, Sönke Holz <soenke.holz@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -8,12 +8,87 @@
 
 #include <AK/Forward.h>
 #include <Kernel/Bus/PCI/Definitions.h>
+#include <Kernel/Interrupts/PCIIRQHandler.h>
+#include <Kernel/Memory/TypedMapping.h>
 
 namespace Kernel::RPi {
 
-class RP1 {
+// 6.1. PCIe endpoint configuration registers
+struct RP1PCIeEndpointRegisters {
+    u32 dbi;
+    u32 control;
+
+    enum class MSIXConfiguration : u32 {
+        Enable = 1 << 0,
+        InterruptAcknowledge = 1 << 2,
+        EnableInterruptAcknowledge = 1 << 3,
+    };
+    MSIXConfiguration msix_configuration[64];
+
+    u32 interrupt_status_low;
+    u32 interrupt_status_high;
+    u32 phy_test;
+    u32 phy_param_ctrl0;
+    u32 phy_param_ctrl1;
+    u32 phy_cr_acc_ctrl;
+    u32 phy_cr_acc_data;
+    u32 ltssm_state_new;
+    u32 _;
+    u32 ltssm_state_filter[4];
+    u32 _;
+    u32 axi_lookup[16];
+    u32 vdm_config_reg_0;
+    u32 vdm_config_reg_1;
+    u32 vdm_panic_reg;
+    u32 vdm_test;
+    u32 vdm_header;
+    u32 pm_control;
+    u32 pm_status;
+    u32 monitor_0;
+    u32 monitor_1;
+    u32 monitor_2;
+    u32 raw_interrupts;
+    u32 interrupt_enable;
+    u32 interrupt_force;
+    u32 interrupt_status;
+};
+static_assert(AssertSize<RP1PCIeEndpointRegisters, 0x1b8>());
+
+AK_ENUM_BITWISE_OPERATORS(RP1PCIeEndpointRegisters::MSIXConfiguration)
+
+class RP1 final
+    : public PCI::Device
+    , public PCI::IRQHandler
+    , public AtomicRefCounted<RP1> {
 public:
-    static ErrorOr<void> try_to_initialize_xhci_controllers(PCI::DeviceIdentifier const&);
+    static ErrorOr<NonnullRefPtr<RP1>> create(PCI::DeviceIdentifier const& pci_identifier);
+
+    ErrorOr<void> initialize();
+
+    // ^PCI::Device
+    virtual StringView device_name() const override { return "RP1"sv; }
+
+    // ^PCI::IRQHandler
+    virtual StringView purpose() const override { return "RP1 IRQ Handler"sv; }
+
+    using InterruptHandler = Function<bool(InterruptNumber)>;
+
+    enum class InterruptTriggerMode {
+        Level,
+        Edge,
+    };
+    void register_interrupt_handler(InterruptNumber, InterruptTriggerMode, InterruptHandler handler);
+
+private:
+    RP1(PCI::DeviceIdentifier const&);
+
+    // ^PCI::IRQHandler
+    virtual bool handle_irq() override;
+
+    Memory::TypedMapping<RP1PCIeEndpointRegisters volatile> m_pcie_ep_registers;
+
+    Array<InterruptHandler, 64> m_interrupt_handlers {};
+    u64 m_interrupt_level_triggered { 0 };
 };
 
 }
