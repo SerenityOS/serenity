944e760f7dc3fa6dc8cd4604eabf98f958a5c001
Lucas Chollet <lucas.chollet@free.fr>
2026-06-30 10:30:40 +0200

    Kernel/x86: Prefer the TSC over the HPET to update the system's time
    As it requires MMIO access, reading the raw value of the HPET timer is
    quite expensive, so doing it every time the system timer fired (~4ms)
    resulted in a heavy overload over the system. As shown in [1] it is
    quite known that HPET timers are slow to read. Moreover, when using QEMU
    reading from MMIO causes a VM exit and makes the whole virtualized world
    stop. The consequence is that reading the HPET timer would not appear in
    profiles as it is not slow from the perspective of the VM.
    
    To solve that issue, we're now using the Time Stamp Counter (TSC) as our
    time-keeping source. Whenever the system interrupt fires, we read the
    TSC (which is cheap, see [1], and doesn't cause a VM exit) and compute
    the elapsed time based on how many cycle the CPU made during that time.
    
    [1] https://docs.redhat.com/en/documentation/red_hat_enterprise_linux_for_real_time/7/html/reference_guide/chap-timestamping#Reading_hardware_clock_sources

---
+++ Kernel/Arch/x86_64/Time/TSC.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2026, Lucas Chollet <lucas.chollet@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/Types.h>
+#include <Kernel/Time/HardwareTimer.h>
+
+namespace Kernel {
+
+class TSC {
+public:
+    static bool is_supported_and_invariant();
+    static TSC& the();
+
+    bool calibrate(HardwareTimerBase& calibration_source);
+    u64 frequency() const { return m_frequency; }
+    // FIXME: Share code with HPET::update_time
+    u64 update_time(u64& seconds_since_boot, u32& ticks_this_second, bool query_only);
+
+private:
+    u64 m_frequency { 0 };
+    u64 m_main_counter_last_read { 0 };
+};
+
+}
