9445167d56839ad9adf784a1b7c65758da8c89e3
Lucas Chollet <lucas.chollet@free.fr>
2026-07-27 10:16:52 +0200

    LibSSH/SFTP: Implement the FSTAT command
    Not exactly required for the moment, but that was easy to implement, and
    I was going to add StatType::FStat in the next commit anyway.

--- Tests/LibSSH/SFTP/TestSFTPStat.cpp
+++ Tests/LibSSH/SFTP/TestSFTPStat.cpp
@@ -7,6 +7,7 @@
 #include <AK/MemoryStream.h>
 #include <LibCore/File.h>
 #include <LibCore/System.h>
+#include <LibFileSystem/FileSystem.h>
 #include <LibFileSystem/TempFile.h>
 #include <LibSSH/DataTypes.h>
 #include <LibTest/TestCase.h>
@@ -14,10 +15,28 @@
 
 namespace {
 
-ErrorOr<ByteBuffer> make_stat_packet(StringView path, bool use_lstat = false)
+enum class StatType : u8 {
+    Stat,
+    LStat,
+    FStat,
+};
+
+ErrorOr<ByteBuffer> make_stat_packet(StringView path, StatType stat_type)
 {
     AllocatingMemoryStream inner;
-    TRY(inner.write_value(use_lstat ? SSH::SFTP::FXPMessageID::LSTAT : SSH::SFTP::FXPMessageID::STAT));
+    SSH::SFTP::FXPMessageID packet_type = SSH::SFTP::FXPMessageID::STAT;
+    switch (stat_type) {
+    case StatType::Stat:
+        packet_type = SSH::SFTP::FXPMessageID::STAT;
+        break;
+    case StatType::LStat:
+        packet_type = SSH::SFTP::FXPMessageID::LSTAT;
+        break;
+    case StatType::FStat:
+        packet_type = SSH::SFTP::FXPMessageID::FSTAT;
+        break;
+    }
+    TRY(inner.write_value(packet_type));
     TRY(inner.write_value<NetworkOrdered<u32>>(42));
     TRY(SSH::encode_string(inner, path));
     auto payload = TRY(inner.read_until_eof());
@@ -28,48 +47,68 @@ ErrorOr<ByteBuffer> make_stat_packet(StringView path, bool use_lstat = false)
     return TRY(packet.read_until_eof());
 }
 
-ErrorOr<void> run_server_with_callback(auto callback, StringView path, bool use_lstat)
+ErrorOr<void> run_server_with_callback(auto callback, StringView path, StatType stat_type, Optional<ByteBuffer&> handle = {})
 {
     auto server = make_initialized_server(move(callback));
-    auto request = TRY(make_stat_packet(path, use_lstat));
+    if (stat_type == StatType::FStat) {
+        auto request = TRY(make_open_packet(path, 43));
+        TRY(server_process_data(server, request));
+    }
+
+    auto request = TRY(make_stat_packet(handle.has_value() && !handle->is_empty() ? handle.value() : path, stat_type));
     return server_process_data(server, request);
 }
 
-ErrorOr<void> run_for_path_impl(StringView path, bool use_lstat, SSH::SFTP::Attributes const& expected)
+ErrorOr<void> run_for_path_impl(StringView path, StatType stat_type, SSH::SFTP::Attributes const& expected)
 {
-    auto callback = [=](Message message) -> ErrorOr<void> {
-        EXPECT_EQ(message.type, SSH::SFTP::FXPMessageID::ATTRS);
+    ByteBuffer handle;
+    auto callback = [=, &handle](Message message) -> ErrorOr<void> {
         FixedMemoryStream stream { message.payload };
+        if (handle.is_empty() && stat_type == StatType::FStat) {
+            EXPECT_EQ(message.type, SSH::SFTP::FXPMessageID::HANDLE);
+            TRY(stream.read_value<NetworkOrdered<u32>>());
+            handle = TRY(SSH::decode_string(stream));
+            return {};
+        }
+
+        EXPECT_EQ(message.type, SSH::SFTP::FXPMessageID::ATTRS);
         EXPECT_EQ(TRY(stream.read_value<NetworkOrdered<u32>>()), 42u);
         auto attributes = TRY(SSH::SFTP::Attributes::from_stream(stream));
         EXPECT_EQ(attributes, expected);
         return {};
     };
 
-    return run_server_with_callback(move(callback), path, use_lstat);
+    return run_server_with_callback(move(callback), path, stat_type, handle);
 }
 
-ErrorOr<void> run_for_path_impl(StringView path, bool use_lstat, SSH::SFTP::FXStatus expected_status)
+ErrorOr<void> run_for_path_impl(StringView path, StatType stat_type, SSH::SFTP::FXStatus expected_status)
 {
     auto callback = [=](Message message) -> ErrorOr<void> {
         return check_status_message(message, expected_status);
     };
 
-    return run_server_with_callback(move(callback), path, use_lstat);
+    return run_server_with_callback(move(callback), path, stat_type);
+}
+
+ErrorOr<void> run_for_path_no_fstat(StringView path)
+{
+    TRY(run_for_path_impl(path, StatType::Stat, SSH::SFTP::Attributes::from_stat(TRY(Core::System::stat(path)))));
+    TRY(run_for_path_impl(path, StatType::LStat, SSH::SFTP::Attributes::from_stat(TRY(Core::System::lstat(path)))));
+    return {};
 }
 
 ErrorOr<void> run_for_path(StringView path)
 {
-    TRY(run_for_path_impl(path, false, SSH::SFTP::Attributes::from_stat(TRY(Core::System::stat(path)))));
-    TRY(run_for_path_impl(path, true, SSH::SFTP::Attributes::from_stat(TRY(Core::System::lstat(path)))));
+    TRY(run_for_path_no_fstat(path));
+    TRY(run_for_path_impl(path, StatType::FStat, SSH::SFTP::Attributes::from_stat(TRY(Core::System::stat(path)))));
     return {};
 }
 
 ErrorOr<void> run_for_path_with_expected_stat_failure(StringView path, SSH::SFTP::FXStatus error_status)
 {
     EXPECT(Core::System::stat(path).is_error());
-    TRY(run_for_path_impl(path, false, error_status));
-    TRY(run_for_path_impl(path, true, SSH::SFTP::Attributes::from_stat(TRY(Core::System::lstat(path)))));
+    TRY(run_for_path_impl(path, StatType::Stat, error_status));
+    TRY(run_for_path_impl(path, StatType::LStat, SSH::SFTP::Attributes::from_stat(TRY(Core::System::lstat(path)))));
     return {};
 }
 
@@ -86,13 +125,16 @@ TEST_CASE(basic)
     }
     TRY_OR_FAIL(run_for_path(file->path()));
 
-    auto directory = TRY_OR_FAIL(FileSystem::TempFile::create_temp_directory());
-    TRY_OR_FAIL(run_for_path(directory->path()));
-
     TRY_OR_FAIL(run_for_path("/dev/null"sv));
+}
+
+TEST_CASE(non_openable_files)
+{
+    auto directory = TRY_OR_FAIL(FileSystem::TempFile::create_temp_directory());
+    TRY_OR_FAIL(run_for_path_no_fstat(directory->path()));
 
     // Different set of attributes.
-    TRY_OR_FAIL(run_for_path("/etc/passwd"sv));
+    TRY_OR_FAIL(run_for_path_no_fstat("/etc/passwd"sv));
 }
 
 TEST_CASE(symlink)
@@ -120,6 +162,6 @@ TEST_CASE(no_permission)
 
     auto unauthorized_path = ByteString::formatted("{}/i/dont/exist", tmp_dir->path());
 
-    TRY_OR_FAIL(run_for_path_impl(unauthorized_path, true, SSH::SFTP::FXStatus::PERMISSION_DENIED));
-    TRY_OR_FAIL(run_for_path_impl(unauthorized_path, false, SSH::SFTP::FXStatus::PERMISSION_DENIED));
+    TRY_OR_FAIL(run_for_path_impl(unauthorized_path, StatType::LStat, SSH::SFTP::FXStatus::PERMISSION_DENIED));
+    TRY_OR_FAIL(run_for_path_impl(unauthorized_path, StatType::Stat, SSH::SFTP::FXStatus::PERMISSION_DENIED));
 }
