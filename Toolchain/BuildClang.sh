b4af2a6a617fe40ca4f0254b77998cc4a0214521
Lucas Chollet <lucas.chollet@free.fr>
2026-07-30 14:48:24 +0200

    Toolchain+Ports: Port LLVM to trunk and update libcxx's main patch

--- Toolchain/BuildClang.sh
+++ Toolchain/BuildClang.sh
@@ -63,8 +63,8 @@ echo PREFIX is "$PREFIX"
 
 mkdir -p "$DIR/Tarballs"
 
-LLVM_COMMIT='9edf0e73b59540ccec25b355268ebff6d3bba4ef' # LLVM 23.0.0 28/04/2026
-LLVM_MD5SUM="492044407d5a76e630104bfb88c695e5"
+LLVM_COMMIT='fcf4bb1af7450f618504201610ef8c3ef52844d7' # LLVM 24.0.0git 30/07/2026
+LLVM_MD5SUM="f8b02735e40c8d0e53f7f45f7dcdf807"
 LLVM_NAME="llvm-project-$LLVM_COMMIT"
 LLVM_PKG="$LLVM_COMMIT.tar.gz"
 LLVM_URL="https://github.com/llvm/llvm-project/archive/$LLVM_PKG"
