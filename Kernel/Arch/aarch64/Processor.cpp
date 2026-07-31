5c3880d136ba92521cf90a9a55e31c0016e73700
Lucas Chollet <lucas.chollet@free.fr>
2026-07-31 12:15:42 +0200

    Kernel: Don't clear debug registers on every context switch
    While this doesn't have a high impact on performance, writing to these
    registers was usually near the top of KVM's vm-exit profiles on x86_64.

--- Kernel/Arch/aarch64/Processor.cpp
+++ Kernel/Arch/aarch64/Processor.cpp
@@ -410,14 +410,13 @@ extern "C" void enter_thread_context(Thread* from_thread, Thread* to_thread)
 
     Processor::load_fpu_state(to_thread->fpu_state());
 
-    if (from_thread->process().is_traced())
+    if (from_thread->process().is_traced()) {
         read_debug_registers_into(from_thread->debug_register_state());
-
-    if (to_thread->process().is_traced()) {
-        write_debug_registers_from(to_thread->debug_register_state());
-    } else {
         clear_debug_registers();
     }
+
+    if (to_thread->process().is_traced())
+        write_debug_registers_from(to_thread->debug_register_state());
 }
 
 StringView ProcessorBase::platform_string()
--- Kernel/Arch/x86_64/Processor.cpp
+++ Kernel/Arch/x86_64/Processor.cpp
@@ -1225,14 +1225,13 @@ extern "C" void enter_thread_context(Thread* from_thread, Thread* to_thread)
 
     Processor::store_fpu_state(from_thread->fpu_state());
 
-    if (from_thread->process().is_traced())
+    if (from_thread->process().is_traced()) {
         read_debug_registers_into(from_thread->debug_register_state());
-
-    if (to_thread->process().is_traced()) {
-        write_debug_registers_from(to_thread->debug_register_state());
-    } else {
         clear_debug_registers();
     }
+
+    if (to_thread->process().is_traced())
+        write_debug_registers_from(to_thread->debug_register_state());
 
     auto& processor = Processor::current();
     Processor::set_fs_base(to_thread->arch_specific_data().fs_base);
