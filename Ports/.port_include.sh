8b160a50d31ffdbfe4a61e010fde8161c7d0caef
Jelle Raaijmakers <jelle@gmta.nl>
2026-08-11 11:19:45 +0200

    Ports: Use cgit for config.sub and config.guess
    Use Savannah's cgit endpoints, since the old gitweb endpoints now return
    error pages instead of config.sub and config.guess.

--- Ports/.port_include.sh
+++ Ports/.port_include.sh
@@ -168,7 +168,7 @@ get_new_config_sub() {
         exit 1
     fi
     if ! run grep -q serenity "$config_sub"; then
-        run do_download_file "https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub" "${config_sub}" false
+        run do_download_file "https://cgit.git.savannah.gnu.org/cgit/config.git/plain/config.sub" "${config_sub}" false
     fi
 }
 
@@ -179,7 +179,7 @@ get_new_config_guess() {
         exit 1
     fi
     if ! run grep -q SerenityOS "$config_guess"; then
-        run do_download_file "https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess" "${config_guess}" false
+        run do_download_file "https://cgit.git.savannah.gnu.org/cgit/config.git/plain/config.guess" "${config_guess}" false
     fi
 }
 
