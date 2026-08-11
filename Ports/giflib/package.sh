f0b11ae0fffec090561d820d4c12a3d99e725e15
Jelle Raaijmakers <jelle@gmta.nl>
2026-08-11 11:10:05 +0200

    Ports: Use current giflib URL and SerenityOS build rules
    SourceForge moved the 5.x releases into a subdirectory. Pass SerenityOS
    as UNAME while building and installing so giflib selects ELF
    shared-library flags on macOS hosts.

--- Ports/giflib/package.sh
+++ Ports/giflib/package.sh
@@ -2,5 +2,7 @@
 port='giflib'
 version='5.2.2'
 files=(
-    "https://downloads.sourceforge.net/project/giflib/giflib-${version}.tar.gz#be7ffbd057cadebe2aa144542fd90c6838c6a083b5e8a9048b8ee3b66b29d5fb"
+    "https://downloads.sourceforge.net/project/giflib/giflib-5.x/giflib-${version}.tar.gz#be7ffbd057cadebe2aa144542fd90c6838c6a083b5e8a9048b8ee3b66b29d5fb"
 )
+makeopts+=('UNAME=SerenityOS')
+installopts+=('UNAME=SerenityOS')
