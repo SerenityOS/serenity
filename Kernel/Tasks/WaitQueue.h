478a5a858120955c893069d6673d58e7d23d6db5
Lucas Chollet <lucas.chollet@free.fr>
2026-08-13 21:16:07 +0200

    Kernel: Share implementation of WaitQueue::wait_until overloads

--- Kernel/Tasks/WaitQueue.h
+++ Kernel/Tasks/WaitQueue.h
@@ -22,47 +22,43 @@ class WaitQueue {
         template<CallableAs<bool> F>
         ErrorOr<void> wait_until(WaitQueue& wait_queue, F should_wake)
         {
-            bool was_interrupted = false;
-
-            SpinlockLocker scheduler_lock(g_scheduler_lock);
-
-            while (true) {
-                prepare(wait_queue);
-
-                if (should_wake())
-                    break;
-                if (was_interrupted = Thread::current()->was_interrupted(); was_interrupted)
-                    break;
-
-                scheduler_lock.unlock();
-
-                maybe_block();
-
-                scheduler_lock.lock();
-            }
-
-            clear();
-
-            scheduler_lock.unlock();
-
-            // This is always cleared since the thread might have been both interrupted and woken up.
-            // If both of those occurred during the same cycle, then we won't report the interrupt,
-            // but we still need to clear it.
-            Thread::current()->clear_interrupted();
-            if (was_interrupted) {
-                return EINTR;
-            }
-
-            return {};
+            // LockRank::None is just a dummy value.
+            return wait_until_impl<LockRank::None>(wait_queue, should_wake, OptionalNone {});
         }
 
         template<LockRank Rank, CallableAs<bool> F>
         ErrorOr<void> wait_until(WaitQueue& wait_queue, Spinlock<Rank>& lock, F should_wake)
         {
+            return wait_until_impl(wait_queue, should_wake, lock);
+        }
+
+        template<typename T, LockRank Rank, CallableAs<bool, T&> F>
+        ErrorOr<void> wait_until(WaitQueue& wait_queue, SpinlockProtected<T, Rank>& spinlock_protected, F should_wake)
+        {
+            return wait_until_impl<Rank>(wait_queue, [&spinlock_protected, &should_wake] { return should_wake(spinlock_protected.m_value); }, spinlock_protected.m_spinlock);
+        }
+
+        void notify(Badge<WaitQueue>);
+
+    private:
+        template<LockRank Rank, CallableAs<bool> F>
+        ErrorOr<void> wait_until_impl(WaitQueue& wait_queue, F should_wake, Optional<Spinlock<Rank>&> user_lock)
+        {
             bool was_interrupted = false;
 
             SpinlockLocker scheduler_lock(g_scheduler_lock);
-            auto interrupt_state = lock.lock();
+
+            auto lock = [&user_lock] -> InterruptsState {
+                if (user_lock.has_value())
+                    return user_lock->lock();
+                return {};
+            };
+            auto unlock = [&user_lock](InterruptsState interrupts_state) {
+                if (user_lock.has_value())
+                    user_lock->unlock(interrupts_state);
+            };
+
+            auto interrupt_state = lock();
 
             while (true) {
                 prepare(wait_queue);
@@ -72,18 +68,18 @@ class WaitQueue {
                 if (was_interrupted = Thread::current()->was_interrupted(); was_interrupted)
                     break;
 
-                lock.unlock(interrupt_state);
+                unlock(interrupt_state);
                 scheduler_lock.unlock();
 
                 maybe_block();
 
                 scheduler_lock.lock();
-                interrupt_state = lock.lock();
+                interrupt_state = lock();
             }
 
             clear();
 
-            lock.unlock(interrupt_state);
+            unlock(interrupt_state);
             scheduler_lock.unlock();
 
             // This is always cleared since the thread might have been both interrupted and woken up.
@@ -97,48 +93,6 @@ class WaitQueue {
             return {};
         }
 
-        template<typename T, LockRank Rank, CallableAs<bool, T&> F>
-        ErrorOr<void> wait_until(WaitQueue& wait_queue, SpinlockProtected<T, Rank>& spinlock_protected, F should_wake)
-        {
-            bool was_interrupted = false;
-
-            SpinlockLocker scheduler_lock(g_scheduler_lock);
-            auto interrupt_state = spinlock_protected.m_spinlock.lock();
-
-            while (true) {
-                prepare(wait_queue);
-
-                if (should_wake(spinlock_protected.m_value))
-                    break;
-                if (was_interrupted = Thread::current()->was_interrupted(); was_interrupted)
-                    break;
-
-                spinlock_protected.m_spinlock.unlock(interrupt_state);
-                scheduler_lock.unlock();
-
-                maybe_block();
-
-                scheduler_lock.lock();
-                interrupt_state = spinlock_protected.m_spinlock.lock();
-            }
-
-            clear();
-
-            spinlock_protected.m_spinlock.unlock(interrupt_state);
-            scheduler_lock.unlock();
-
-            // See the note in the above function.
-            Thread::current()->clear_interrupted();
-            if (was_interrupted) {
-                return EINTR;
-            }
-
-            return {};
-        }
-
-        void notify(Badge<WaitQueue>);
-
-    private:
         struct Association {
             WaitQueue& wait_queue;
             NonnullRefPtr<Thread> thread;
