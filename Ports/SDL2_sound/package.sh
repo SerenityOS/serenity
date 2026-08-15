b0b27082e652f5f72728895f1deaa076086c24e9
Linus Groh <mail@linusgroh.de>
2026-08-15 01:18:04 +0100

    Ports: Use commit as the version for already-pinned 'git' ports

--- Ports/SDL2_sound/package.sh
+++ Ports/SDL2_sound/package.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='SDL2_sound'
-version='git'
-_commit='301135a6d0d9bb77c9da0b7f809e9a10d579610f'
-workdir="SDL_sound-${_commit}"
+version='301135a6d0d9bb77c9da0b7f809e9a10d579610f'
+workdir="SDL_sound-${version}"
 useconfigure='true'
 depends=('SDL2')
 files=(
-    "https://github.com/icculus/SDL_sound/archive/${_commit}.zip#d29f90dd5abacf9f818f0b1567fab6b3dc6292d0a942e8e8d1e8f84130eea7a1"
+    "https://github.com/icculus/SDL_sound/archive/${version}.zip#d29f90dd5abacf9f818f0b1567fab6b3dc6292d0a942e8e8d1e8f84130eea7a1"
 )
 configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt")
 
--- Ports/doom/package.sh
+++ Ports/doom/package.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='doom'
-commit_hash='613f870b6fa83ede448a247de5a2571092fa729d'
-workdir="doomgeneric-${commit_hash}"
-version='git'
+version='613f870b6fa83ede448a247de5a2571092fa729d'
+workdir="doomgeneric-${version}"
 files=(
-    "https://github.com/ozkl/doomgeneric/archive/${commit_hash}.tar.gz#fabc3e41aca92f58dfdd284754891c17875ec8c995948b49396ead6bc05b8676"
+    "https://github.com/ozkl/doomgeneric/archive/${version}.tar.gz#fabc3e41aca92f58dfdd284754891c17875ec8c995948b49396ead6bc05b8676"
 )
 depends=(
     'SDL2'
