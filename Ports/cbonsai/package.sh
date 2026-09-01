982b616075fc5b83e23a198e74b50c30ce4c86b4
Linus Groh <mail@linusgroh.de>
2026-09-01 08:45:19 +0100

    Ports: Update cbonsai to 1.4.2

--- Ports/cbonsai/package.sh
+++ Ports/cbonsai/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='cbonsai'
-version='1.3.1'
+version='1.4.2'
 files=(
-    "https://gitlab.com/jallbrit/cbonsai/-/archive/v${version}/cbonsai-v${version}.tar.bz2#38531a5f5584185d63b7bcd4a308cad9f61cd829b676c221d254bdcb39c67427"
+    "https://gitlab.com/jallbrit/cbonsai/-/archive/v${version}/cbonsai-v${version}.tar.bz2#1ded632e90644d29ad6c142c62aa2e66eab3c7a6dee23dfb6f7a12a973c3fc50"
 )
 workdir="cbonsai-v${version}"
 makeopts+=(CC="${CC}")
