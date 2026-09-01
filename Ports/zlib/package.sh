fd7bc6239972048f3605312ea5c273d4d39c01b0
Linus Groh <mail@linusgroh.de>
2026-09-01 21:53:06 +0100

    Ports: Update zlib to 1.3.2

--- Ports/zlib/package.sh
+++ Ports/zlib/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='zlib'
-version='1.3.1'
+version='1.3.2'
 useconfigure='true'
 files=(
-    "https://github.com/madler/zlib/releases/download/v${version}/zlib-${version}.tar.gz#9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df23"
+    "https://github.com/madler/zlib/releases/download/v${version}/zlib-${version}.tar.gz#bb329a0a2cd0274d05519d61c667c062e06990d72e125ee2dfa8de64f0119d16"
 )
 
 configure() {
