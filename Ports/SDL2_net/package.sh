b07dffb00700d9267ac9c6048e5bf587f93e372f
Linus Groh <mail@linusgroh.de>
2026-09-01 21:09:30 +0100

    Ports: Update SDL2_net to 2.4.0

--- Ports/SDL2_net/package.sh
+++ Ports/SDL2_net/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='SDL2_net'
-version='2.2.0'
+version='2.4.0'
 useconfigure='true'
 configopts=(
     "--with-sdl-prefix=${SERENITY_INSTALL_ROOT}/usr/local"
@@ -8,6 +8,6 @@ configopts=(
     "--enable-shared"
 )
 files=(
-    "https://github.com/libsdl-org/SDL_net/releases/download/release-${version}/SDL2_net-${version}.tar.gz#4e4a891988316271974ff4e9585ed1ef729a123d22c08bd473129179dc857feb"
+    "https://github.com/libsdl-org/SDL_net/releases/download/release-${version}/SDL2_net-${version}.tar.gz#9cbca2527feb3f1a622d48ba65cc7dee9b1e3f2c55ceafb7d7720bb058aafb30"
 )
 depends=("SDL2")
