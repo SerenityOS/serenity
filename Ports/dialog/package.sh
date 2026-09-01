c09e6fdd9f202a715189ce341d6ba976e0dbe0b3
Linus Groh <mail@linusgroh.de>
2026-09-01 08:45:19 +0100

    Ports: Update dialog to 1.3-20260721

--- Ports/dialog/package.sh
+++ Ports/dialog/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='dialog'
-version='1.3-20220526'
+version='1.3-20260721'
 files=(
-    "https://invisible-mirror.net/archives/dialog/dialog-${version}.tgz#858c9a625b20fde19fb7b19949ee9e9efcade23c56d917b1adb30e98ff6d6b33"
+    "https://invisible-mirror.net/archives/dialog/dialog-${version}.tgz#62bdf59057d4f760a1cc2217827f07887b4a3eebf694c25eacd4803d2171cdc6"
 )
 useconfigure='true'
 use_fresh_config_sub='true'
