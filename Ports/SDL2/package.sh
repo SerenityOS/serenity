3f0e3b16f67ab496642c0310960662d313cc43d2
Linus Groh <mail@linusgroh.de>
2026-09-01 21:09:30 +0100

    Ports: Update SDL2 to 2.32.10

--- Ports/SDL2/package.sh
+++ Ports/SDL2/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='SDL2'
-version='2.32.8'
+version='2.32.10'
 useconfigure='true'
 files=(
-    "https://github.com/libsdl-org/SDL/releases/download/release-${version}/SDL2-${version}.tar.gz#0ca83e9c9b31e18288c7ec811108e58bac1f1bb5ec6577ad386830eac51c787e"
+    "https://github.com/libsdl-org/SDL/releases/download/release-${version}/SDL2-${version}.tar.gz#5f5993c530f084535c65a6879e9b26ad441169b3e25d789d83287040a9ca5165"
 )
 configopts=(
     "-DCMAKE_CXX_FLAGS=-I${SERENITY_BUILD_DIR}/Root/usr/include/Services/ -I${SERENITY_BUILD_DIR}/Root/usr/include/Userland/Services/"
