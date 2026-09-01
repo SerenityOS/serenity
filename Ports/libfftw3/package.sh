1bb29e71f7972f5b53337ed942dd98a7562df914
Linus Groh <mail@linusgroh.de>
2026-09-01 08:45:19 +0100

    Ports: Update libfftw3(f) to 3.3.11

--- Ports/libfftw3/package.sh
+++ Ports/libfftw3/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libfftw3'
-version='3.3.10'
+version='3.3.11'
 useconfigure='true'
 use_fresh_config_sub='true'
 configopts=(
@@ -9,6 +9,6 @@ configopts=(
     '--with-pic'
 )
 files=(
-    "http://fftw.org/fftw-${version}.tar.gz#56c932549852cddcfafdab3820b0200c7742675be92179e59e6215b340e26467"
+    "http://fftw.org/fftw-${version}.tar.gz#5630c24cdeb33b131612f7eb4b1a9934234754f9f388ff8617458d0be6f239a1"
 )
 workdir="fftw-${version}"
--- Ports/libfftw3f/package.sh
+++ Ports/libfftw3f/package.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=libfftw3f
-version=3.3.10
+version=3.3.11
 useconfigure=true
 configopts=("--enable-float")
 use_fresh_config_sub=true
 files=(
-    "http://fftw.org/fftw-${version}.tar.gz#56c932549852cddcfafdab3820b0200c7742675be92179e59e6215b340e26467"
+    "http://fftw.org/fftw-${version}.tar.gz#5630c24cdeb33b131612f7eb4b1a9934234754f9f388ff8617458d0be6f239a1"
 )
 workdir="fftw-${version}"
