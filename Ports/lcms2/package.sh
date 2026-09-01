39147a25f5cef8865d4ab403de23161361c46e59
Linus Groh <mail@linusgroh.de>
2026-09-01 20:31:28 +0100

    Ports: Update lcms2 to 2.19.1

--- Ports/lcms2/package.sh
+++ Ports/lcms2/package.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='lcms2'
-version='2.16'
+version='2.19.1'
 useconfigure='true'
-use_fresh_config_sub='true'
-use_fresh_config_guess='true'
 files=(
-    "https://github.com/mm2/Little-CMS/releases/download/lcms${version}/lcms2-${version}.tar.gz#d873d34ad8b9b4cea010631f1a6228d2087475e4dc5e763eb81acc23d9d45a51"
+    "https://github.com/mm2/Little-CMS/releases/download/lcms${version}/lcms2-${version}.tar.gz#bfc54f7bab59fbc921012014a8032e4cba4abd46db47d46b76416a8c0b2815c8"
 )
 depends=(
     'libtiff'
