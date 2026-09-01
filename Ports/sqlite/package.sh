56c0fae8178f7934f507a885e93497aebbcbe8ee
Linus Groh <mail@linusgroh.de>
2026-09-01 21:09:30 +0100

    Ports: Update sqlite to 3.53.4

--- Ports/sqlite/package.sh
+++ Ports/sqlite/package.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='sqlite'
-version='3.47.0'
+version='3.53.4'
 # Download URL uses a different version format
-_version='3470000'
+_version='3530400'
 files=(
-    "https://www.sqlite.org/2024/sqlite-autoconf-${_version}.tar.gz#83eb21a6f6a649f506df8bd3aab85a08f7556ceed5dbd8dea743ea003fc3a957"
+    "https://www.sqlite.org/2026/sqlite-autoconf-${_version}.tar.gz#0e9483900e92cd5de8fd48d16bf9200145a61f7fd5be542a5ac81d8a9516eb9c"
 )
+workdir="sqlite-autoconf-${_version}"
 useconfigure='true'
 launcher_name='SQLite'
 launcher_category='D&evelopment'
 launcher_command='/usr/local/bin/sqlite3 -interactive'
 launcher_run_in_terminal='true'
 #icon_file=FIXME
-use_fresh_config_sub='true'
-workdir="sqlite-autoconf-${_version}"
