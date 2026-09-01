947829d4a63fa32a2220cf66b545072e30837642
Linus Groh <mail@linusgroh.de>
2026-09-01 01:01:33 +0100

    Ports: Update sl to 5.05
    Various Linux distributions[1] have switched to a fork of sl that has a
    few additional tagged versions, this being the latest one.
    
    [1]: https://repology.org/project/sl/versions

--- Ports/sl/package.sh
+++ Ports/sl/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=sl
-version='923e7d7ebc5c1f009755bdeb789ac25658ccce03'
+version='5.05'
 files=(
-    "https://github.com/mtoyoda/sl/archive/${version}.tar.gz#0b90e669db80437b106c49536b89a5364b47e6a55d0a0164a8dda5d2dbd2aab0"
+    "https://github.com/eyJhb/sl/archive/${version}.tar.gz#6c941b526e3d01be7f91a3af4ae20a89d1e5d66b3b2d804c80123b1b1be96384"
 )
 depends=("ncurses")
 
