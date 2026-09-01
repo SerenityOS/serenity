2d637f2916a95535f1e80e416d1bcc61ad57e055
Linus Groh <mail@linusgroh.de>
2026-09-01 20:31:28 +0100

    Ports: Update jq to 1.8.2
    Dropped patches have been addressed upstream:
    
    - https://github.com/jqlang/jq/commit/a8ce2ff09af51b39be0d76091e6117caccd1b5c0
    - https://github.com/jqlang/jq/commit/f954e82b144429972a13a95acdf0297e8cc67fa3

--- Ports/jq/package.sh
+++ Ports/jq/package.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='jq'
-version='1.7.1'
+version='1.8.2'
 useconfigure='true'
 use_fresh_config_sub='true'
 config_sub_paths=('config/config.sub')
 files=(
-    "https://github.com/jqlang/jq/releases/download/jq-${version}/jq-${version}.tar.gz#478c9ca129fd2e3443fe27314b455e211e0d8c60bc8ff7df703873deeee580c2"
+    "https://github.com/jqlang/jq/releases/download/jq-${version}/jq-${version}.tar.gz#71b8d6e8f5fe81f6c6d0d110e3892251f6ce76ed095abd315e26e6e1193af3af"
 )
 depends=(
     'oniguruma'
