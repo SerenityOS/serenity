5300f4c7b035a2d4e864d0fad8fbbfe0026a97b9
Linus Groh <mail@linusgroh.de>
2026-09-01 21:09:30 +0100

    Ports: Update tig to 2.6.1

--- Ports/tig/package.sh
+++ Ports/tig/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='tig'
-version='2.5.8'
+version='2.6.1'
 useconfigure='true'
 files=(
-    "https://github.com/jonas/tig/releases/download/tig-${version}/tig-${version}.tar.gz#b70e0a42aed74a4a3990ccfe35262305917175e3164330c0889bd70580406391"
+    "https://github.com/jonas/tig/releases/download/tig-${version}/tig-${version}.tar.gz#5adeabdcd93aa0423d618da8b878b53482bef6e0e9e1fe224acc0f18031fe91e"
 )
 depends=(
     'libiconv'
