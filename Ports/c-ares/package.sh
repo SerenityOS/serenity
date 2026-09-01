1fc65ad960ffacf151b4988fc92195ee52d3b8b0
Linus Groh <mail@linusgroh.de>
2026-09-01 08:45:19 +0100

    Ports: Update c-ares to 1.34.8

--- Ports/c-ares/package.sh
+++ Ports/c-ares/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='c-ares'
-version='1.19.0'
+version='1.34.8'
 files=(
-    "https://github.com/c-ares/c-ares/releases/download/cares-${version//./_}/c-ares-${version}.tar.gz#bfceba37e23fd531293829002cac0401ef49a6dc55923f7f92236585b7ad1dd3"
+    "https://github.com/c-ares/c-ares/releases/download/v${version}/c-ares-${version}.tar.gz#c222b6d681096f9444d2c4863d2c1174019e27cacca0a4a5c114d36dd7d7bf78"
 )
 useconfigure=true
 configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt")
