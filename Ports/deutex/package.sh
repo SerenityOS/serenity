3940f2a7ece62425d253a378ad0a673cf1e3bf48
Linus Groh <mail@linusgroh.de>
2026-09-01 08:45:19 +0100

    Ports: Update deutex to 5.2.3

--- Ports/deutex/package.sh
+++ Ports/deutex/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='deutex'
-version='5.2.2'
+version='5.2.3'
 useconfigure='true'
 files=(
-    "https://github.com/Doom-Utils/deutex/releases/download/v${version}/deutex-${version}.tar.zst#10ed0e7a533ec97cb6d03548d4258fbec88852a45b5ea4cf5434376ad4174b5f"
+    "https://github.com/Doom-Utils/deutex/releases/download/v${version}/deutex-${version}.tar.zst#935dcae490fb574e8ad90ef54bdeb599c7055fedca117d79ce837cbc19d070ab"
 )
 depends=(
     'libpng'
