856cd2ea67e6316ec2473e6244d728016e99ab4a
Linus Groh <mail@linusgroh.de>
2026-09-01 08:45:19 +0100

    Ports: Update e2fsprogs to 1.47.4

--- Ports/e2fsprogs/package.sh
+++ Ports/e2fsprogs/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='e2fsprogs'
-version='1.47.0'
+version='1.47.4'
 files=(
-    "https://www.kernel.org/pub/linux/kernel/people/tytso/e2fsprogs/v${version}/e2fsprogs-${version}.tar.xz#144af53f2bbd921cef6f8bea88bb9faddca865da3fbc657cc9b4d2001097d5db"
+    "https://www.kernel.org/pub/linux/kernel/people/tytso/e2fsprogs/v${version}/e2fsprogs-${version}.tar.xz#fd5bf388cbdbe006a3d3b318d983b2948382440acc85a87f1e7d108653e8db0b"
 )
 useconfigure='true'
 configopts=(
