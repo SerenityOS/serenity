30df6c91531c6b3129d027a9d62e3776649803af
Linus Groh <mail@linusgroh.de>
2026-09-01 20:31:28 +0100

    Ports: Update imagemagick to 7.1.2-30

--- Ports/imagemagick/package.sh
+++ Ports/imagemagick/package.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='imagemagick'
-version='7.1.1-43'
+version='7.1.2-30'
 workdir="ImageMagick-${version}"
 useconfigure='true'
 files=(
-    "https://github.com/ImageMagick/ImageMagick/archive/refs/tags/${version}.tar.gz#ceb972266b23dc7c1cfce0da5a7f0c9acfb4dc81f40eb542a49476fedbc2618f"
+    "https://github.com/ImageMagick/ImageMagick/archive/refs/tags/${version}.tar.gz#3034a64f22398e15ee3dd1e6b1aa83d838cfc47df1bb246ae0eca9590e6ace72"
 )
 configopts=(
     "--with-sysroot=${SERENITY_INSTALL_ROOT}"
