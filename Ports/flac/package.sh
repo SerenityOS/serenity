dce4c668594d13363cbb44751ffe3170592af41b
Linus Groh <mail@linusgroh.de>
2026-09-01 08:45:20 +0100

    Ports: Update flac to 1.5.0

--- Ports/flac/package.sh
+++ Ports/flac/package.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='flac'
-version='1.4.3'
+version='1.5.0'
 useconfigure='true'
 depends=(
     'libogg'
 )
 files=(
-    "https://downloads.xiph.org/releases/flac/flac-${version}.tar.xz#6c58e69cd22348f441b861092b825e591d0b822e106de6eb0ee4d05d27205b70"
+    "https://downloads.xiph.org/releases/flac/flac-${version}.tar.xz#f2c1c76592a82ffff8413ba3c4a1299b6c7ab06c734dee03fd88630485c2b920"
 )
