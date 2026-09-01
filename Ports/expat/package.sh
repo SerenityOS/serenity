b462a31c1b9af952ff5ae8f5baeec378254f8b40
Linus Groh <mail@linusgroh.de>
2026-09-01 08:45:19 +0100

    Ports: Update expat to 2.8.4

--- Ports/expat/package.sh
+++ Ports/expat/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='expat'
-version='2.5.0'
-versionpath='2_5_0'
+version='2.8.4'
+versionpath='2_8_4'
 useconfigure='true'
 files=(
-    "https://github.com/libexpat/libexpat/releases/download/R_${versionpath}/expat-${version}.tar.xz#ef2420f0232c087801abf705e89ae65f6257df6b7931d37846a193ef2e8cdcbe"
+    "https://github.com/libexpat/libexpat/releases/download/R_${versionpath}/expat-${version}.tar.xz#656ae1cc8da3b4ea513bb4e254f33e6243938084c0ec6239da873376b09985a7"
 )
