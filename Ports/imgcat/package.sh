285a1cfcd541c2fda7c8124338f040e55243e06e
Linus Groh <mail@linusgroh.de>
2026-09-01 20:31:28 +0100

    Ports: Update imgcat to 2.6.0

--- Ports/imgcat/package.sh
+++ Ports/imgcat/package.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=imgcat
-version=2.5.1
-useconfigure=true
+port='imgcat'
+version='2.6.0'
+useconfigure='true'
 installopts=("PREFIX=${SERENITY_INSTALL_ROOT}/usr/local")
-depends=("ncurses" "libpng" "libjpeg")
+depends=(
+    'ncurses'
+    'libpng'
+    'libjpeg'
+)
 files=(
-    "https://github.com/eddieantonio/imgcat/releases/download/v${version}/imgcat-${version}.tar.gz#8faaac392df315b4973bb6927c0eec659e879df6c15ad6f8461073e05b70c537"
+    "https://github.com/eddieantonio/imgcat/releases/download/v${version}/imgcat-${version}.tar.gz#1e7e69670ad73e36ba1a9f0a09b6a787cf4e141dfe7885ae7ad77c293fb999a6"
 )
