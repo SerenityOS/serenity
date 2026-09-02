49a191aca43bcadfb348d2bedb16a6c33129e832
Linus Groh <mail@linusgroh.de>
2026-09-02 20:22:02 +0100

    Ports: Update nano to 9.2

--- Ports/nano/package.sh
+++ Ports/nano/package.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='nano'
-version='9.0'
+version='9.2'
 files=(
-    "https://www.nano-editor.org/dist/v9/nano-${version}.tar.xz#9f384374b496110a25b73ad5a5febb384783c6e3188b37063f677ac908013fde"
+    "https://www.nano-editor.org/dist/v9/nano-${version}.tar.xz#05ecb99247b782e8a5b3a25ed4101dd034b0236902f7449bc9795b717642f7e9"
 )
 useconfigure='true'
-use_fresh_config_sub='true'
 configopts=("--disable-utf8")
 depends=("ncurses")
 
