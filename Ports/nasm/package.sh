763b423390bbafa62a8af05b16d7adb0cfbc79a6
Linus Groh <mail@linusgroh.de>
2026-09-02 20:22:02 +0100

    Ports: Update nasm to 3.02

--- Ports/nasm/package.sh
+++ Ports/nasm/package.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='nasm'
-version='3.01'
+version='3.02'
 files=(
-    "https://www.nasm.us/pub/nasm/releasebuilds/${version}/nasm-${version}.tar.gz#aea120d4adb0241f08ae24d6add09e4a993bc1c4d9f754dbfc8020d6916c9be1"
+    "https://www.nasm.us/pub/nasm/releasebuilds/${version}/nasm-${version}.tar.gz#f504227b2f529e658d41629075f0503b38d67d790af345f34eba4af60c6a5998"
 )
-useconfigure=true
-makeopts=()
+useconfigure='true'
