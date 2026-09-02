02a75a2095571d9ba45acced0d3367364448482e
Linus Groh <mail@linusgroh.de>
2026-09-02 20:22:02 +0100

    Ports: Update awk to 20260426

--- Ports/awk/package.sh
+++ Ports/awk/package.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=awk
-version=20240728
+port='awk'
+version='20260426'
 useconfigure="false"
 files=(
-    "https://github.com/onetrueawk/awk/archive/refs/tags/${version}.tar.gz#2d479817f95d5997fc4348ecebb1d8a1b25c81cebedb46ca4f59434247e08543"
+    "https://github.com/onetrueawk/awk/archive/refs/tags/${version}.tar.gz#7ae5b9fc6a8149bc45ea0ba3ba434a69a16d1460d19f6d01b6f04cc885b8e02b"
 )
-patchlevel=1
 
 build() {
     run make "${makeopts[@]}"
