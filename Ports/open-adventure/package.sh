cf8d68eb63706dd623798cd6c57ef3616a4e9c13
Linus Groh <mail@linusgroh.de>
2026-09-02 20:22:02 +0100

    Ports: Update open-adventure to 1.22

--- Ports/open-adventure/package.sh
+++ Ports/open-adventure/package.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='open-adventure'
-version='1.20'
+version='1.22'
 files=(
-    "https://gitlab.com/esr/open-adventure/-/archive/1.20/open-adventure-${version}.zip#441637afaf4f7db0e96b3d27ab8fb17ca68e0df343c72a9e4416569de1b4ea50"
+    "https://gitlab.com/esr/open-adventure/-/archive/${version}/open-adventure-${version}.zip#f81b69edb1aed61cfcbceef02173d217527e83b22a867ccdf8d65936ab9b05db"
 )
-
 depends=(
     'editline'
 )
-
-bin_path='/usr/local/bin'
 
 build() {
     export PKG_CONFIG_PATH="${SERENITY_INSTALL_ROOT}/usr/local/lib/pkgconfig/"
@@ -17,6 +14,6 @@ build() {
 }
 
 install() {
-    run_nocd mkdir -p "${SERENITY_INSTALL_ROOT}/${bin_path}/"
-    run cp advent "${SERENITY_INSTALL_ROOT}/${bin_path}/"
+    run_nocd mkdir -p "${SERENITY_INSTALL_ROOT}/usr/local/bin"
+    run cp advent "${SERENITY_INSTALL_ROOT}/usr/local/bin/"
 }
