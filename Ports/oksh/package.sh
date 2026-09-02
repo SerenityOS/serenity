bb1ea1f276e952d31fa48abdbeea2c673350b2eb
Linus Groh <mail@linusgroh.de>
2026-09-02 20:22:02 +0100

    Ports: Update oksh to 7.9

--- Ports/oksh/package.sh
+++ Ports/oksh/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='oksh'
-version='7.1'
+version='7.9'
 files=(
-    "https://github.com/ibara/oksh/releases/download/oksh-${version}/oksh-${version}.tar.gz#9dc0b0578d9d64d10c834f9757ca11f526b562bc5454da64b2cb270122f52064"
+    "https://github.com/ibara/oksh/releases/download/oksh-${version}/oksh-${version}.tar.gz#51b2d92515950c959dbf24f6fc33336db8c0526c2a50fee4ca598a18a6114a49"
 )
 useconfigure='true'
 depends=("ncurses")
