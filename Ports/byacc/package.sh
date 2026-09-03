7e91bc8cebbcb7ac5aadaed9a7a54828a7a04568
Linus Groh <mail@linusgroh.de>
2026-09-03 20:28:35 +0100

    Ports: Update byacc to 20260126

--- Ports/byacc/package.sh
+++ Ports/byacc/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=byacc
-version=20230521
+port='byacc'
+version='20260126'
 files=(
-    "https://invisible-mirror.net/archives/byacc/byacc-${version}.tgz#5ad915a7d5833aa38a5e31bd077505666029c35e365dff8569fe4598eaa9fef2"
+    "https://invisible-mirror.net/archives/byacc/byacc-${version}.tgz#b618c5fb44c2f5f048843db90f7d1b24f78f47b07913c8c7ba8c942d3eb24b00"
 )
-useconfigure=true
+useconfigure='true'
