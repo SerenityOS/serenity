886b8ea02317f053a2539370ae1da6f6fa39e8a4
Linus Groh <mail@linusgroh.de>
2026-09-03 20:28:35 +0100

    Ports: Update chromaprint to 1.6.1

--- Ports/chromaprint/package.sh
+++ Ports/chromaprint/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='chromaprint'
 useconfigure='true'
-version='1.5.1'
+version='1.6.1'
 depends=(
     'ffmpeg'
 )
@@ -14,7 +14,7 @@ configopts=(
     "-DCMAKE_INSTALL_PREFIX=${SERENITY_INSTALL_ROOT}/usr/local"
 )
 files=(
-    "https://github.com/acoustid/chromaprint/releases/download/v${version}/chromaprint-${version}.tar.gz#a1aad8fa3b8b18b78d3755b3767faff9abb67242e01b478ec9a64e190f335e1c"
+    "https://github.com/acoustid/chromaprint/releases/download/v${version}/chromaprint-${version}.tar.gz#3368805af0ee47b9df74df10b5001a44569e01df2844dab520031720dde9ad23"
 )
 
 configure() {
