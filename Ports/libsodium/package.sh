16079f1c1a056026f15bcb094fd64b6495902c4e
Linus Groh <mail@linusgroh.de>
2026-09-04 19:02:26 +0100

    Ports: Update libsodium to 1.0.22

--- Ports/libsodium/package.sh
+++ Ports/libsodium/package.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=libsodium
-version=1.0.18
-useconfigure=true
+port='libsodium'
+version='1.0.22'
+useconfigure='true'
 configopts=("--disable-static" "--enable-shared")
-use_fresh_config_sub=true
-config_sub_paths=("build-aux/config.sub")
-workdir=libsodium-${version}
 files=(
-    "https://download.libsodium.org/libsodium/releases/libsodium-${version}.tar.gz#6f504490b342a4f8a4c4a02fc9b866cbef8622d5df4e5452b46be121e46636c1"
+    "https://download.libsodium.org/libsodium/releases/libsodium-${version}.tar.gz#adbdd8f16149e81ac6078a03aca6fc03b592b89ef7b5ed83841c086191be3349"
 )
