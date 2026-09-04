333039ce3786a975cc0a37da0003816ef07284c8
Linus Groh <mail@linusgroh.de>
2026-09-04 23:46:28 +0100

    Ports: Update mawk to 1.3.4-20260302

--- Ports/mawk/package.sh
+++ Ports/mawk/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='mawk'
-version='1.3.4-20240905'
+version='1.3.4-20260302'
 files=(
-    "https://invisible-mirror.net/archives/mawk/mawk-${version}.tgz#a39967927dfa1b0116efc45b944a0f5b5b4c34f8e842a4b223dcdd7b367399e0"
+    "https://invisible-mirror.net/archives/mawk/mawk-${version}.tgz#e2c08a77d0a84a01f9be454d1ca3872d4f103f9ada683d075198b0c6e965633d"
 )
 useconfigure='true'
 use_fresh_config_sub='true'
