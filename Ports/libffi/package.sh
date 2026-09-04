c27aafaffd5a82f9726ab55f6fcc32ec33d57dbc
Linus Groh <mail@linusgroh.de>
2026-09-04 19:02:26 +0100

    Ports: Update libffi to 3.8.0

--- Ports/libffi/package.sh
+++ Ports/libffi/package.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libffi'
-version='3.4.5'
+version='3.8.0'
 useconfigure='true'
-use_fresh_config_sub='true'
 files=(
-    "https://github.com/libffi/libffi/releases/download/v${version}/libffi-${version}.tar.gz#96fff4e589e3b239d888d9aa44b3ff30693c2ba1617f953925a70ddebcc102b2"
+    "https://github.com/libffi/libffi/releases/download/v${version}/libffi-${version}.tar.gz#7da3e2d9a171eb0a038f592ecad3ff2bb2550f3496d87b3b29ad0cf4430c0db4"
 )
