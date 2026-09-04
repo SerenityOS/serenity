b0fd8609e67746edebac45f7a33c06b4f8eef7f1
Linus Groh <mail@linusgroh.de>
2026-09-04 19:02:26 +0100

    Ports: Update libivonv to 1.19

--- Ports/libiconv/package.sh
+++ Ports/libiconv/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libiconv'
-version='1.18'
+version='1.19'
 files=(
-    "https://ftpmirror.gnu.org/gnu/libiconv/libiconv-${version}.tar.gz#3b08f5f4f9b4eb82f151a7040bfd6fe6c6fb922efe4b1659c66ea933276965e8"
+    "https://ftpmirror.gnu.org/gnu/libiconv/libiconv-${version}.tar.gz#88dd96a8c0464eca144fc791ae60cd31cd8ee78321e67397e25fc095c4a19aa6"
 )
 useconfigure='true'
 configopts=("--enable-shared" "--disable-nls" "CFLAGS=-std=c17")
