5ad34e4851b64f5d770d767bb937923dfed7804f
Linus Groh <mail@linusgroh.de>
2026-09-04 19:02:26 +0100

    Ports: Update libmpg123 to 1.33.7

--- Ports/libmpg123/package.sh
+++ Ports/libmpg123/package.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libmpg123'
-version='1.33.4'
+version='1.33.7'
 useconfigure='true'
 workdir="mpg123-${version}"
-use_fresh_config_sub='true'
-config_sub_paths=(
-    'build/config.sub'
-)
 files=(
-    "https://download.sourceforge.net/project/mpg123/mpg123/${version}/mpg123-${version}.tar.bz2#3ae8c9ff80a97bfc0e22e89fbcd74687eca4fc1db315b12607f27f01cb5a47d9"
+    "https://download.sourceforge.net/project/mpg123/mpg123/${version}/mpg123-${version}.tar.bz2#31d0e35a4ca567ec9b5ebda6c3062bb4435d6d3eacd6ef0d95cadd7854dc03ee"
 )
