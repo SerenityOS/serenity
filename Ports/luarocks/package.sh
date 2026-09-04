b40b558d100b8d710d077e89d4d677327758752a
Linus Groh <mail@linusgroh.de>
2026-09-04 23:46:28 +0100

    Ports: Update luarocks to 3.13.0

--- Ports/luarocks/package.sh
+++ Ports/luarocks/package.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=luarocks
-version=3.8.0
-useconfigure=true
-depends=("lua" "git" "readline")
-files=(
-    "https://luarocks.org/releases/luarocks-${version}.tar.gz#56ab9b90f5acbc42eb7a94cf482e6c058a63e8a1effdf572b8b2a6323a06d923"
+port='luarocks'
+version='3.13.0'
+useconfigure='true'
+depends=(
+    'lua'
+    'git'
+    'readline'
 )
+files=(
+    "https://luarocks.org/releases/luarocks-${version}.tar.gz#245bf6ec560c042cb8948e3d661189292587c5949104677f1eecddc54dbe7e37"
+)
+installopts=("INSTALL_TOP=${SERENITY_INSTALL_ROOT}/usr/local")
 
 configure() {
     run ./configure --with-lua-include=${SERENITY_INSTALL_ROOT}/usr/local/include --prefix=/usr/local --with-lua-interpreter=lua
 }
-
-installopts=("INSTALL_TOP=${SERENITY_INSTALL_ROOT}/usr/local")
