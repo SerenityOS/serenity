baf430551954c84164e85cea6e4fced917b1f10f
Linus Groh <mail@linusgroh.de>
2026-09-04 19:02:26 +0100

    Ports: Update libjodycode to 4.1.2

--- Ports/libjodycode/package.sh
+++ Ports/libjodycode/package.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libjodycode'
-version='3.1.1'
+version='4.1.2'
 files=(
-    "https://codeberg.org/jbruchon/libjodycode/archive/v${version}.tar.gz#bc98cc2e11428585cb00cc9ceae196423abd1f6f7d49bcd2a64314490b5b8160"
+    "https://codeberg.org/jbruchon/libjodycode/archive/v${version}.tar.gz#0343cf2ff53fb19887663b8c1f47210fc2d1599a4a8f95e292991d5b1034bc05"
 )
-auth_type='sha256'
 workdir='libjodycode'
 makeopts=("UNAME_S=serenity UNAME_M=${SERENITY_ARCH} CROSS_DETECT=cross")
