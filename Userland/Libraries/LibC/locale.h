c850d3aa7fcaa5718b6c7d0cea29691d004dfb92
Eduardo Casadei <educasadei@gmail.com>
2026-09-08 10:38:51 -0400

    LibC: Define LC_GLOBAL_LOCALE in locale.h
    POSIX left its specific value implementation defined so, at least for
    now, just set it to `(locale_t) -1`.

--- Userland/Libraries/LibC/locale.h
+++ Userland/Libraries/LibC/locale.h
@@ -35,6 +35,8 @@ enum {
 #define LC_MESSAGES_MASK (1 << LC_MESSAGES)
 #define LC_ALL_MASK (LC_NUMERIC_MASK | LC_CTYPE_MASK | LC_COLLATE_MASK | LC_TIME_MASK | LC_MONETARY_MASK | LC_MESSAGES_MASK)
 
+#define LC_GLOBAL_LOCALE ((locale_t) - 1)
+
 struct lconv {
     char* decimal_point;
     char* thousands_sep;
