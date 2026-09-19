9ab0c02628e20334b55be077dfc0c56fcaab9d4d
Linus Groh <mail@linusgroh.de>
2026-09-19 23:54:35 +0100

    Ports: Update fluidsynth to 2.6.1

--- Ports/fluidsynth/package.sh
+++ Ports/fluidsynth/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='fluidsynth'
-version='2.3.5'
+version='2.6.1'
 useconfigure='true'
 configopts=(
     '-G Ninja'
@@ -8,7 +8,7 @@ configopts=(
     "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
 )
 files=(
-    "https://github.com/FluidSynth/fluidsynth/archive/refs/tags/v${version}.tar.gz#f89e8e983ecfb4a5b4f5d8c2b9157ed18d15ed2e36246fa782f18abaea550e0d"
+    "https://github.com/FluidSynth/fluidsynth/archive/refs/tags/v${version}.tar.gz#3d258a3bf97cc20c59eeebfe62c2432fae88adda74d3ad098681c76e0ebf446b"
 )
 depends=(
     'glib'
