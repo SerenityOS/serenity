21a3549343265f9d9a39ae5869005bb5c704f563
Linus Groh <mail@linusgroh.de>
2026-09-19 23:54:34 +0100

    Ports: Update bzip3 to 1.5.4

--- Ports/bzip3/package.sh
+++ Ports/bzip3/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='bzip3'
-version='1.5.3'
+version='1.5.4'
 files=(
-    "https://github.com/kspalaiologos/bzip3/releases/download/${version}/bzip3-${version}.tar.gz#c48823353084df2a5a0dba44fd5295abd078e40b49f09700d08af4d9b1e31d67"
+    "https://github.com/kspalaiologos/bzip3/releases/download/${version}/bzip3-${version}.tar.gz#89a5e4bf29e4aae98b29bb1ef275addfa2d0806ba1ef60bf8a87263cdb21f581"
 )
 useconfigure='true'
 installopts=(
