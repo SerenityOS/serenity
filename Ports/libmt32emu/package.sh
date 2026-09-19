5e5e449bc9a96885f17a9d0bc2de0450808d5850
Linus Groh <mail@linusgroh.de>
2026-09-19 23:54:35 +0100

    Ports: Update libmt32emu to 2.8.2

--- Ports/libmt32emu/package.sh
+++ Ports/libmt32emu/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libmt32emu'
-version='2.7.1'
+version='2.8.2'
 workdir="munt-${port}_${version//./_}/mt32emu"
 useconfigure='true'
 configopts=(
@@ -10,7 +10,7 @@ configopts=(
     "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
 )
 files=(
-    "https://github.com/munt/munt/archive/refs/tags/libmt32emu_${version//./_}.tar.gz#e4524d52d6799a4e32a961a2e92074f14adcb2f110a4e7a06bede77050cfdaf4"
+    "https://github.com/munt/munt/archive/refs/tags/libmt32emu_${version//./_}.tar.gz#d4778cf89b054ba7ab410ffcb02ecf1629fa32b5b60838addec99eb93804fdcb"
 )
 
 configure() {
