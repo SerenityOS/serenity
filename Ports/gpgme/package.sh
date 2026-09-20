03c63d5fdb6199b0c1c16624c5dd4c184cd9090f
Linus Groh <mail@linusgroh.de>
2026-09-20 21:45:00 +0100

    Ports: Update gpgme to 2.2.0

--- Ports/gpgme/package.sh
+++ Ports/gpgme/package.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='gpgme'
-version='1.23.2'
+version='2.2.0'
 useconfigure='true'
-use_fresh_config_sub='true'
-config_sub_paths=(
-    'build-aux/config.sub'
-)
 files=(
-    "https://gnupg.org/ftp/gcrypt/gpgme/gpgme-${version}.tar.bz2#9499e8b1f33cccb6815527a1bc16049d35a6198a6c5fae0185f2bd561bce5224"
+    "https://gnupg.org/ftp/gcrypt/gpgme/gpgme-${version}.tar.bz2#7160e80e84dafd00d956c84891c533bb7ab16a6a54fbe1574b2f3acf0496977b"
 )
 depends=(
     'gnupg'
@@ -18,7 +14,3 @@ configopts=(
     "--with-libassuan-prefix=${SERENITY_INSTALL_ROOT}/usr/local"
     "--with-sysroot=${SERENITY_INSTALL_ROOT}"
 )
-
-post_install() {
-    run sed -i 's#/usr/local#${CMAKE_SYSROOT}/usr/local#g' ${SERENITY_INSTALL_ROOT}/usr/local/lib/cmake/Gpgmepp/GpgmeppConfig.cmake
-}
