9cfffb126f03b2cf57b804d974f593d5995287c6
Linus Groh <mail@linusgroh.de>
2026-09-20 15:13:36 +0100

    Ports: Update libopus to 1.6.1

--- Ports/libopus/package.sh
+++ Ports/libopus/package.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libopus'
-version='1.5.2'
-workdir='opus-1.5.2'
+version='1.6.1'
+workdir="opus-${version}"
 useconfigure='true'
-use_fresh_config_sub='true'
 files=(
-    "https://downloads.xiph.org/releases/opus/opus-${version}.tar.gz#65c1d2f78b9f2fb20082c38cbe47c951ad5839345876e46941612ee87f9a7ce1"
+    "https://downloads.xiph.org/releases/opus/opus-${version}.tar.gz#6ffcb593207be92584df15b32466ed64bbec99109f007c82205f0194572411a1"
 )
