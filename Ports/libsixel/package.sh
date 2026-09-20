ed93f42071add08dc0452f03dff2af993589061b
Linus Groh <mail@linusgroh.de>
2026-09-20 15:13:36 +0100

    Ports: Update libsixel to 1.8.7

--- Ports/libsixel/package.sh
+++ Ports/libsixel/package.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=libsixel
-version=1.8.6
+port='libsixel'
+version='1.8.7'
 files=(
-    "https://github.com/saitoha/libsixel/archive/refs/tags/v${version}.tar.gz#37611d60c7dbcee701346967336dbf135fdd5041024d5f650d52fae14c731ab9"
+    "https://github.com/saitoha/libsixel/archive/refs/tags/v${version}.tar.gz#ebed621b4820f96ff428127797d94f4671f2393560acd4bcc8b540809d391510"
 )
-useconfigure=true
-use_fresh_config_sub=true
+useconfigure='true'
 configopts=("--prefix=${SERENITY_INSTALL_ROOT}/usr/local")
+
+configure() {
+    export ac_cv_func_malloc_0_nonnull=yes
+    export ac_cv_func_realloc_0_nonnull=yes
+    run ./configure --host="${SERENITY_ARCH}-serenity" "${configopts[@]}"
+}
 
 install() {
     run make install
