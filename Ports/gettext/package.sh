219686770ac955b5adb6758e6806712da46a86c2
Linus Groh <mail@linusgroh.de>
2026-09-20 15:13:26 +0100

    Ports: Update gettext to 1.0

--- Ports/gettext/package.sh
+++ Ports/gettext/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='gettext'
-version='0.26'
+version='1.0'
 useconfigure='true'
 files=(
-    "https://ftpmirror.gnu.org/gettext/gettext-${version}.tar.gz#39acf4b0371e9b110b60005562aace5b3631fed9b1bb9ecccfc7f56e58bb1d7f"
+    "https://ftpmirror.gnu.org/gettext/gettext-${version}.tar.gz#85d99b79c981a404874c02e0342176cf75c7698e2b51fe41031cf6526d974f1a"
 )
 depends=(
     'libiconv'
