505d622616e576b36e0adce35c6c79c888b141ec
Linus Groh <mail@linusgroh.de>
2026-09-20 21:48:36 +0100

    Ports: Use GNU mirror for gettext

--- Ports/gettext/package.sh
+++ Ports/gettext/package.sh
@@ -3,7 +3,7 @@ port='gettext'
 version='1.0'
 useconfigure='true'
 files=(
-    "https://ftpmirror.gnu.org/gettext/gettext-${version}.tar.gz#85d99b79c981a404874c02e0342176cf75c7698e2b51fe41031cf6526d974f1a"
+    "mirror://gnu/gettext/gettext-${version}.tar.gz#85d99b79c981a404874c02e0342176cf75c7698e2b51fe41031cf6526d974f1a"
 )
 depends=(
     'libiconv'
