4abcd4799282ba4eb2c78d37404e4d50fc9d6d9b
Linus Groh <mail@linusgroh.de>
2026-09-20 15:13:36 +0100

    Ports: Update libmikmod to 3.3.14

--- Ports/libmikmod/package.sh
+++ Ports/libmikmod/package.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libmikmod'
-version='3.3.13'
+version='3.3.14'
 useconfigure='true'
 configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt")
 files=(
-    "https://downloads.sourceforge.net/project/mikmod/libmikmod/${version}/libmikmod-${version}.tar.gz#9fc1799f7ea6a95c7c5882de98be85fc7d20ba0a4a6fcacae11c8c6b382bb207"
+    "https://downloads.sourceforge.net/project/mikmod/libmikmod/${version}/libmikmod-${version}.tar.gz#dffd82b8f254c3489c32098da831f33eac7136843d1e7ccb802f1254ad5b4219"
 )
 
 configure() {
