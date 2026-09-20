0b912d509d58ebf254f559378f10e1792d5787f4
Linus Groh <mail@linusgroh.de>
2026-09-20 15:13:36 +0100

    Ports: Update libslirp to 4.9.4

--- Ports/libslirp/package.sh
+++ Ports/libslirp/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libslirp'
-version='4.8.0'
+version='4.9.4'
 workdir="libslirp-v${version}"
 files=(
-    "https://gitlab.freedesktop.org/slirp/libslirp/-/archive/v${version}/libslirp-v${version}.tar.gz#2a98852e65666db313481943e7a1997abff0183bd9bea80caec1b5da89fda28c"
+    "https://gitlab.freedesktop.org/slirp/libslirp/-/archive/v${version}/libslirp-v${version}.tar.gz#3998863b020aeda34bddc567097c6efba55a78cdf6eeee6bcd42c11ef23967da"
 )
 useconfigure='true'
 configopts=("--cross-file=${SERENITY_BUILD_DIR}/meson-cross-file.txt")
