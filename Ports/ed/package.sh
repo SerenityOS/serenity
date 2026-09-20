82f3bbb254ca9563cd7d0b02b679ff87d397e7af
Linus Groh <mail@linusgroh.de>
2026-09-20 15:13:26 +0100

    Ports: Update ed to 1.22.6

--- Ports/ed/package.sh
+++ Ports/ed/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='ed'
-version='1.22'
+version='1.22.6'
 files=(
-    "mirror://gnu/ed/ed-${version}.tar.lz#7eb22c30a99dcdb50a8630ef7ff3e4642491ac4f8cd1aa9f3182264df4f4ad08"
+    "mirror://gnu/ed/ed-${version}.tar.lz#3f33b22135219c39c3c695f7b7171c2567d3e2a17c798c0a90607320cbb268f2"
 )
 useconfigure='true'
 depends=(
