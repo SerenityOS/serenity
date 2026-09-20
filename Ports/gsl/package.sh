6cb0857451be20f8316d68272f40c8d19d75cf67
Linus Groh <mail@linusgroh.de>
2026-09-20 15:13:26 +0100

    Ports: Update gsl to 2.8

--- Ports/gsl/package.sh
+++ Ports/gsl/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 
-port=gsl
-version=2.7.1
-useconfigure=true
+port='gsl'
+version='2.8'
+useconfigure='true'
 files=(
-    "mirror://gnu/gsl/gsl-${version}.tar.gz#dcb0fbd43048832b757ff9942691a8dd70026d5da0ff85601e52687f6deeb34b"
+    "mirror://gnu/gsl/gsl-${version}.tar.gz#6a99eeed15632c6354895b1dd542ed5a855c0f15d9ad1326c6fe2b2c9e423190"
 )
-use_fresh_config_sub=true
+use_fresh_config_sub='true'
