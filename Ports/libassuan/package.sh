3e4d9755431010c58536da7153ef8ea5d372c3ca
Linus Groh <mail@linusgroh.de>
2026-09-20 15:13:36 +0100

    Ports: Update libassuan to 3.0.2

--- Ports/libassuan/package.sh
+++ Ports/libassuan/package.sh
@@ -1,21 +1,13 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libassuan'
-version='2.5.7'
+version='3.0.2'
 useconfigure='true'
-use_fresh_config_sub='true'
-config_sub_paths=(
-    'build-aux/config.sub'
-)
 depends=(
     'libgpg-error'
 )
 files=(
-    "https://gnupg.org/ftp/gcrypt/libassuan/libassuan-${version}.tar.bz2#0103081ffc27838a2e50479153ca105e873d3d65d8a9593282e9c94c7e6afb76"
+    "https://gnupg.org/ftp/gcrypt/libassuan/libassuan-${version}.tar.bz2#d2931cdad266e633510f9970e1a2f346055e351bb19f9b78912475b8074c36f6"
 )
-
-pre_configure() {
-    export ac_cv_lib_pthread_pthread_create='no'
-}
 
 configure() {
     run ./configure \
@@ -26,4 +18,11 @@ configure() {
         --with-libgpg-error-prefix="${SERENITY_INSTALL_ROOT}/usr/local" \
         --with-sysroot="${SERENITY_INSTALL_ROOT}" \
         LDFLAGS="-lintl"
+}
+
+build() {
+    host_env
+    run make -C src CC_FOR_BUILD="${HOST_CC}" mkheader
+    target_env
+    run make "${makeopts[@]}"
 }
