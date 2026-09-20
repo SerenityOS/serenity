7c6cf01aecc0bb59c14a7f59c2ba68d6fbca2f11
Linus Groh <mail@linusgroh.de>
2026-09-20 15:13:36 +0100

    Ports: Update libssh2 to 1.11.1

--- Ports/libssh2/package.sh
+++ Ports/libssh2/package.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libssh2'
-version='1.11.0'
+version='1.11.1'
 useconfigure='true'
 files=(
-    "https://www.libssh2.org/download/libssh2-${version}.tar.gz#3736161e41e2693324deb38c26cfdc3efe6209d634ba4258db1cecff6a5ad461"
+    "https://www.libssh2.org/download/libssh2-${version}.tar.gz#d9ec76cbe34db98eec3539fe2c899d26b0c837cb3eb466a56b0f109cabf658f7"
 )
 depends=(
     'openssl'
     'zlib'
 )
+
+export PKG_CONFIG_PATH="${SERENITY_INSTALL_ROOT}/usr/local/lib/pkgconfig"
+
 configopts=(
-    "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt" 
+    "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
     '-DCRYPTO_BACKEND=OpenSSL'
     '-DENABLE_ZLIB_COMPRESSION=ON'
     '-GNinja'
