2104bbc01a3a8c1702edda032e5e52289f5dc014
Linus Groh <mail@linusgroh.de>
2026-09-20 15:13:36 +0100

    Ports: Update libxmp to 4.7.3

--- Ports/libxmp/package.sh
+++ Ports/libxmp/package.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libxmp'
-version='4.6.1'
+version='4.7.3'
 files=(
-    "https://github.com/libxmp/libxmp/releases/download/libxmp-${version}/libxmp-${version}.tar.gz#af605e72c83b24abaf03269347e24ebc3fc06cd7b495652a2c619c1f514bc5cb"
+    "https://github.com/libxmp/libxmp/releases/download/libxmp-${version}/libxmp-${version}.tar.gz#b6a98797e4fb9c9a705f5d53112aa5214561857e929a644928b9e658930d9440"
 )
 useconfigure='true'
-use_fresh_config_sub='false'
