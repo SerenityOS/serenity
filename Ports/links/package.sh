7254aff63c0c99c92cabc04c2871e397345b2046
Linus Groh <mail@linusgroh.de>
2026-09-20 21:45:00 +0100

    Ports: Update links to 2.30

--- Ports/links/package.sh
+++ Ports/links/package.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='links'
-version='2.29'
+version='2.30'
 useconfigure='true'
 files=(
-    "http://links.twibright.com/download/links-${version}.tar.bz2#22aa96c0b38e1a6f8f7ed9d7a4167a47fc37246097759ef6059ecf8f9ead7998"
+    "http://links.twibright.com/download/links-${version}.tar.bz2#c4631c6b5a11527cdc3cb7872fc23b7f2b25c2b021d596be410dadb40315f166"
 )
+depends=(
+    'openssl'
+)
+
+export PKG_CONFIG_PATH="${SERENITY_INSTALL_ROOT}/usr/local/lib/pkgconfig"
