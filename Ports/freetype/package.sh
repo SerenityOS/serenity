ae3bdd8eb5bc3dcc626b7e3ede01252fe274ddf5
Linus Groh <mail@linusgroh.de>
2026-09-20 15:13:26 +0100

    Ports: Update freetype to 2.14.3

--- Ports/freetype/package.sh
+++ Ports/freetype/package.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='freetype'
-version='2.13.2'
+version='2.14.3'
 files=(
-    "https://download.savannah.gnu.org/releases/freetype/freetype-${version}.tar.gz#1ac27e16c134a7f2ccea177faba19801131116fd682efc1f5737037c5db224b5"
+    "https://download.savannah.gnu.org/releases/freetype/freetype-${version}.tar.gz#e61b31ab26358b946e767ed7eb7f4bb2e507da1cfefeb7a8861ace7fd5c899a1"
 )
 useconfigure='true'
-use_fresh_config_sub='true'
-config_sub_paths=(
-    'builds/unix/config.sub'
-)
 configopts=(
     "--with-sysroot=${SERENITY_INSTALL_ROOT}"
     '--with-brotli=no'
