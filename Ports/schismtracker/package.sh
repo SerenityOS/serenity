968069e222a8ddc9a3099e02788e25d4a6f45189
Linus Groh <mail@linusgroh.de>
2026-09-21 20:40:44 +0100

    Ports: Update schismtracker to 20260524

--- Ports/schismtracker/package.sh
+++ Ports/schismtracker/package.sh
@@ -1,12 +1,13 @@
-#!/bin/bash ../.port_include.sh
+#!/usr/bin/env -S bash ../.port_include.sh
 port='schismtracker'
-version='20240523'
+version='20260524'
 useconfigure='true'
 files=(
-    "https://github.com/schismtracker/schismtracker/releases/download/${version}/schismtracker-${version}.source.tar.gz#44b3de30ad0c72c540f60cfdfe4f3906c5756023fd17e23a0977646d425d0863"
+    "https://github.com/schismtracker/schismtracker/releases/download/${version}/schismtracker-${version}.source.tar.gz#3b211da6ea8f1db966bfc1c85e1774bda6420c5b5d24dadea9e9463fd9f66a18"
 )
 depends=(
     'SDL2'
+    'utf8proc'
 )
 launcher_name='SchismTracker'
 launcher_category='&Media'
