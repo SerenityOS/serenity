574ef17f6a792e6b154cccb1beacf63cd59a98de
Linus Groh <mail@linusgroh.de>
2026-09-21 20:40:44 +0100

    Ports: Update kakoune to 2026.05.21

--- Ports/kakoune/package.sh
+++ Ports/kakoune/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='kakoune'
-version='e605ad8582d8e015806ed9b4d7aba8ca1ea13d57'
+version='2026.05.21'
 files=(
-    "https://github.com/mawww/kakoune/archive/${version}.tar.gz#420823df611202f3c18a004def032cc9cec46c63b7754249aae2d8c1e72fb1b2"
+    "https://github.com/mawww/kakoune/archive/refs/tags/v${version}.tar.gz#7ef778bd199e05977841e6f69aad2bee6cd58fb23b0e4bc265d012d42de17580"
 )
 depends=(
     'bash'
