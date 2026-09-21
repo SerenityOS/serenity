d594eb1da8e76e6386a74c0622ebdb730a524d25
Linus Groh <mail@linusgroh.de>
2026-09-21 20:40:44 +0100

    Ports: Update imagemagick to 7.1.2-31
    Also set PKG_CONFIG_PATH as this port is another victim of 06c42df.

--- Ports/imagemagick/package.sh
+++ Ports/imagemagick/package.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='imagemagick'
-version='7.1.2-30'
+version='7.1.2-31'
 workdir="ImageMagick-${version}"
 useconfigure='true'
 files=(
-    "https://github.com/ImageMagick/ImageMagick/archive/refs/tags/${version}.tar.gz#3034a64f22398e15ee3dd1e6b1aa83d838cfc47df1bb246ae0eca9590e6ace72"
+    "https://github.com/ImageMagick/ImageMagick/archive/refs/tags/${version}.tar.gz#34d9cc3acddc3e3c429d23af60eda5ceaac477a8b296ddb9469f773f44a80a5f"
 )
 configopts=(
     "--with-sysroot=${SERENITY_INSTALL_ROOT}"
@@ -14,3 +14,5 @@ depends=(
     'libpng'
     'libtiff'
 )
+
+export PKG_CONFIG_PATH="${SERENITY_INSTALL_ROOT}/usr/local/lib/pkgconfig"
