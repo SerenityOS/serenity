2a933b117c6356a2afb1c5a35f857bad68da3856
Linus Groh <mail@linusgroh.de>
2026-09-21 20:40:44 +0100

    Ports: Update libjpeg to 10

--- Ports/libjpeg/package.sh
+++ Ports/libjpeg/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=libjpeg
-version=9e
+port='libjpeg'
+version='10'
 useconfigure=true
 configopts=("--disable-static" "--enable-shared")
 files=(
-    "https://ijg.org/files/jpegsrc.v${version}.tar.gz#4077d6a6a75aeb01884f708919d25934c93305e49f7e3f36db9129320e6f4f3d"
+    "https://ijg.org/files/jpegsrc.v${version}.tar.gz#8b9eaa13242690ebd03e1728ab1edf97a81a78ed6e83624d493655f31ac95ab5"
 )
 workdir="jpeg-$version"
