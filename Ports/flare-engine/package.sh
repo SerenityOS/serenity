b21ed3f0993799e1935f8c92d21f62b23afa1d99
Linus Groh <mail@linusgroh.de>
2026-09-21 20:40:44 +0100

    Ports: Update flare-engine to 1.15

--- Ports/flare-engine/package.sh
+++ Ports/flare-engine/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='flare-engine'
 useconfigure='true'
-version='1.14'
+version='1.15'
 depends=(
     'SDL2'
     'SDL2_image'
@@ -9,12 +9,16 @@ depends=(
     'SDL2_ttf'
 )
 files=(
-    "https://github.com/flareteam/flare-engine/archive/refs/tags/v${version}.tar.gz#2c1bafeaa5ac26c10449bfcb7151b06e8a22547aa7364d2a39bbcb135aa50a09"
+    "https://github.com/flareteam/flare-engine/archive/refs/tags/v${version}.tar.gz#642db16111487bf8a3fb7ebadcfc635909c7eaf91df7723ee22fae1caac9fe93"
 )
 
 configopts=(
     "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
     "-DSDL2_INCLUDE_DIR=${SERENITY_INSTALL_ROOT}/usr/local/include/SDL2"
+    # Toolchain sets CMAKE_INSTALL_PREFIX to /usr/local, which flare uses as a
+    # path prefix for install destinations. CMake only prepends DESTDIR to
+    # relative paths, so use a relative prefix to keep installs under DESTDIR.
+    "-DCMAKE_INSTALL_PREFIX=usr/local"
 )
 
 configure() {
