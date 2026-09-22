f789bfe1c786a736b496ee2a8d277096cb3571a5
Linus Groh <mail@linusgroh.de>
2026-09-22 21:28:03 +0100

    Ports: Update lynx to 2.9.3

--- Ports/lynx/package.sh
+++ Ports/lynx/package.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='lynx'
-version='2.9.2'
+version='2.9.3'
 files=(
-    "https://invisible-island.net/archives/lynx/tarballs/lynx${version}.tar.bz2#7374b89936d991669e101f4e97f2c9592036e1e8cdaa7bafc259a77ab6fb07ce"
+    "https://invisible-island.net/archives/lynx/tarballs/lynx${version}.tar.bz2#174b7f2866a60f3247ba75f5c7dbb10b124aede4a1359312de15f3bfebd2050f"
 )
 workdir="lynx${version}"
-depends=('ncurses' 'openssl' 'zlib')
+depends=(
+    'ncurses'
+    'openssl'
+    'zlib'
+)
 useconfigure='true'
-use_fresh_config_sub='true'
-configopts=('--without-system-type' '--with-screen=ncurses' '--with-ssl' '--with-zlib')
+configopts=(
+    '--with-screen=ncursesw'
+    '--with-ssl'
+    '--with-zlib'
+)
