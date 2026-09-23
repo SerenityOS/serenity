d59429f4f84478e3205ea73bc37fdaf3c3219bda
Linus Groh <mail@linusgroh.de>
2026-09-23 21:26:43 +0100

    Ports: Update gzip to 1.15

--- Ports/gzip/package.sh
+++ Ports/gzip/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='gzip'
-version='1.14'
+version='1.15'
 useconfigure='true'
 files=(
-    "mirror://gnu/gzip/gzip-${version}.tar.gz#613d6ea44f1248d7370c7ccdeee0dd0017a09e6c39de894b3c6f03f981191c6b"
+    "mirror://gnu/gzip/gzip-${version}.tar.gz#545886cf57fa88a65e967fbf705903d7fcb2567c82c7342493e82e8d7b1a210b"
 )
