7940f562b00d33706f3b5000b2cb682bb6a11ecb
Linus Groh <mail@linusgroh.de>
2026-09-23 21:26:43 +0100

    Ports: Update fio to 2.43

--- Ports/fio/package.sh
+++ Ports/fio/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='fio'
-version='3.42'
+version='3.43'
 files=(
-    "https://brick.kernel.dk/snaps/${port}-${version}.tar.gz#9128d0c81bd7bffab0dd06cbfb755a05ef92f3b8a0b0c61f1b3538df6750f1e0"
+    "https://brick.kernel.dk/snaps/${port}-${version}.tar.gz#6f57893c7a73d9ed72e9e2b6e15f136a99c137bd6a7645fdeeb4a40b3e9da78d"
 )
 depends=("zlib")
 
