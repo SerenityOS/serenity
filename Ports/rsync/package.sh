70d60c97f2f0e76624d56f5c9690cd80794a102d
Linus Groh <mail@linusgroh.de>
2026-09-23 21:26:43 +0100

    Ports: Update rsync to 3.5.1

--- Ports/rsync/package.sh
+++ Ports/rsync/package.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='rsync'
-version='3.5.0'
+version='3.5.1'
 useconfigure='true'
 files=(
-    "https://download.samba.org/pub/rsync/src/rsync-${version}.tar.gz#c7ffd1ef653e99540f661e47cb00b7f9cad1ee6b972399b16f93d672656e0d33"
+    "https://download.samba.org/pub/rsync/src/rsync-${version}.tar.gz#c55f9c9dc10fb8bec397b399a0fdded53cc9a2d8e30891bb0d63724d25c37bef"
 )
 configopts=(
+    '--disable-idn'
     '--disable-xxhash'
 )
 depends=(
