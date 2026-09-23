cb12c56952f189beb7dcebcdf1eb763605e3044d
Linus Groh <mail@linusgroh.de>
2026-09-23 21:26:43 +0100

    Ports: Update tree-sitter-c to 0.24.2

--- Ports/tree-sitter-c/package.sh
+++ Ports/tree-sitter-c/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='tree-sitter-c'
-version='0.24.1'
+version='0.24.2'
 files=(
-    "https://github.com/tree-sitter/tree-sitter-c/releases/download/v${version}/tree-sitter-c.tar.gz#5a82a33dc5483f46d9a571f8ee769f6825752202cc2694299007a9da9417b9e5"
+    "https://github.com/tree-sitter/tree-sitter-c/releases/download/v${version}/tree-sitter-c.tar.gz#f3a2cdfbca39c79f60baf2ef62b42084c609782c76485de457ba36ff65d51baf"
 )
 workdir='.'
