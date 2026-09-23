e84a41f9d4604072b35d66343f7d4713d375418d
Linus Groh <mail@linusgroh.de>
2026-09-23 21:26:43 +0100

    Ports: Update libuuid to 2.42.4

--- Ports/libuuid/package.sh
+++ Ports/libuuid/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='libuuid'
-version='2.42.3'
+version='2.42.4'
 workdir="util-linux-${version}"
 useconfigure='true'
 configopts=(
@@ -12,5 +12,5 @@ configopts=(
     '--prefix=/usr/local'
 )
 files=(
-    "https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v$(cut -d. -f1,2 <<< ${version})/util-linux-${version}.tar.xz#66ac7c0e725278eb2b039e3104f2c91119341d941b41bac7a285c695f940bd57"
+    "https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v$(cut -d. -f1,2 <<< ${version})/util-linux-${version}.tar.xz#fbd62a100ab7bb8746ba0661255c3c48185b1e9021507c624da01fbc696330ec"
 )
